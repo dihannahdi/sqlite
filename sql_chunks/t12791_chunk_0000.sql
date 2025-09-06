4044910cd93f75b46c175c751fc5d4cb21e34d1d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:20 +0700

    Upload SQL chunk: t12791_chunk_0000.sql

---
+++ sql_chunks/t12791_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t12791
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t12791" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12791
+INSERT INTO [t12791] VALUES (1,'مسألة','2','0','0');
+INSERT INTO [t12791] VALUES (2,'المرتبة الأولى','3','0','0');
+INSERT INTO [t12791] VALUES (3,'المرتبة الثانية','4','0','0');
+INSERT INTO [t12791] VALUES (4,'وأما وجه التفصيل','7','3','0');
+INSERT INTO [t12791] VALUES (5,'المرتبة الثالثة','11','0','0');
+INSERT INTO [t12791] VALUES (6,'الأولى','11','5','0');
+INSERT INTO [t12791] VALUES (7,'الرواية الثانية','12','5','0');
+INSERT INTO [t12791] VALUES (8,'الرواية الثالثة','12','5','0');
+INSERT INTO [t12791] VALUES (9,'الرواية الرابعة','15','5','0');
+INSERT INTO [t12791] VALUES (10,'الرواية الخامسة','17','5','0');
+INSERT INTO [t12791] VALUES (11,'الرواية السادسة','18','5','0');
+INSERT INTO [t12791] VALUES (12,'الرواية السابعة','19','5','0');
+INSERT INTO [t12791] VALUES (13,'الرواية الثامنة','21','5','0');
+INSERT INTO [t12791] VALUES (14,'الرواية التاسعة','23','5','0');
