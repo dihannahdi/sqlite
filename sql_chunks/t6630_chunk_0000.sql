d63226babf5f37b6a3c72ddbadb02013f69ee5ad
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:15 +0700

    Upload SQL chunk: t6630_chunk_0000.sql

---
+++ sql_chunks/t6630_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t6630
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t6630" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6630
+INSERT INTO [t6630] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6630] VALUES (2,'المائة الأولى من الهجرة','11','0','0');
+INSERT INTO [t6630] VALUES (3,'المائة الثانية','31','0','0');
+INSERT INTO [t6630] VALUES (4,'المائة الثالثة','111','0','0');
+INSERT INTO [t6630] VALUES (5,'المائة الخامسة','307','0','0');
+INSERT INTO [t6630] VALUES (6,'المائة السادسة','488','0','0');
+INSERT INTO [t6630] VALUES (7,'المائة السابعة','577','0','0');
+INSERT INTO [t6630] VALUES (8,'باب في الذين ما عثرت على أشعارهم فاقتصرت على نخب من أخبارهم','616','0','0');
+INSERT INTO [t6630] VALUES (9,'المائة الأولى من الهجرة','616','8','0');
+INSERT INTO [t6630] VALUES (10,'المائة الثانية','631','8','0');
+INSERT INTO [t6630] VALUES (11,'المائة الثالثة','660','8','0');
+INSERT INTO [t6630] VALUES (12,'المائة الرابعة','681','8','0');
