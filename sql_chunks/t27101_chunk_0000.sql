c76ebcba6d60b8bbf6e7db9946860030aed1c435
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:48 +0700

    Upload SQL chunk: t27101_chunk_0000.sql

---
+++ sql_chunks/t27101_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t27101
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t27101" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t27101
+INSERT INTO [t27101] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t27101] VALUES (2,'الجزء فيه مجلس من حديث الإمام أبي الحسن علي بن إبراهيم بن داود ابن العطار الشافعي','2','0','0');
+INSERT INTO [t27101] VALUES (3,'الحديث الأول','3','0','0');
+INSERT INTO [t27101] VALUES (4,'الحديث الثاني','4','0','0');
+INSERT INTO [t27101] VALUES (5,'الحديث الثالث','5','0','0');
+INSERT INTO [t27101] VALUES (6,'الحديث الرابع','6','0','0');
+INSERT INTO [t27101] VALUES (7,'الحديث الخامس','7','0','0');
+INSERT INTO [t27101] VALUES (8,'الحديث السادس','8','0','0');
+INSERT INTO [t27101] VALUES (9,'الحديث السابع','9','0','0');
