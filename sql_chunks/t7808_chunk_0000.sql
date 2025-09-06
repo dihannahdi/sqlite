c2a568a9e832f321ec7d2a8d515eab88040708ef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:13 +0700

    Upload SQL chunk: t7808_chunk_0000.sql

---
+++ sql_chunks/t7808_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t7808
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t7808" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7808
+INSERT INTO [t7808] VALUES (1,'فصل','14','0','0');
+INSERT INTO [t7808] VALUES (2,'فصل','24','0','0');
+INSERT INTO [t7808] VALUES (3,'فصل','40','0','0');
+INSERT INTO [t7808] VALUES (4,'فصل','79','0','0');
+INSERT INTO [t7808] VALUES (5,'فصل','134','0','0');
+INSERT INTO [t7808] VALUES (6,'فصل','160','0','0');
+INSERT INTO [t7808] VALUES (7,'فصل','163','0','0');
+INSERT INTO [t7808] VALUES (8,'فصل','165','0','0');
+INSERT INTO [t7808] VALUES (9,'فصل','167','0','0');
+INSERT INTO [t7808] VALUES (10,'فصل','170','0','0');
+INSERT INTO [t7808] VALUES (11,'فصل','172','0','0');
+INSERT INTO [t7808] VALUES (12,'فصل','176','0','0');
+INSERT INTO [t7808] VALUES (13,'فصل','179','0','0');
+INSERT INTO [t7808] VALUES (14,'فصل','184','0','0');
+INSERT INTO [t7808] VALUES (15,'فصل','187','0','0');
