dec91ea0b97b10d47d9e182a25594f4849033752
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:33 +0700

    Upload SQL chunk: t8891_chunk_0000.sql

---
+++ sql_chunks/t8891_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t8891
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t8891" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8891
+INSERT INTO [t8891] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8891] VALUES (2,'الكتب المصنفة في النحو بعنوان "البسيط":','2','0','0');
+INSERT INTO [t8891] VALUES (3,'أوهام المحققين المعاصرين في نسبة "البسيط " إلى ركن الدين الاستراباذي:','4','0','0');
+INSERT INTO [t8891] VALUES (4,'صاحب "البسيط" في النحو:','7','0','0');
+INSERT INTO [t8891] VALUES (5,'موطن صاحب البسيط وعصره:','8','0','0');
+INSERT INTO [t8891] VALUES (6,'تعريف موجز بصاحب البسيط:','10','0','0');
+INSERT INTO [t8891] VALUES (7,'أشهر المصنفين الذين نقلوا عن صاحب البسيط:','13','0','0');
+INSERT INTO [t8891] VALUES (8,'المنهج النحوي لصاحب البسيط:','19','0','0');
+INSERT INTO [t8891] VALUES (9,'من الآراء النحوية لصاحب البسيط:','25','0','0');
+INSERT INTO [t8891] VALUES (10,'مصادر ومراجع','30','0','0');
