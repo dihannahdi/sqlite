4ed90cc73315eac964ab20a98b8592066864c744
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:53 +0700

    Upload SQL chunk: t96853_chunk_0000.sql

---
+++ sql_chunks/t96853_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t96853
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t96853" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96853
+INSERT INTO [t96853] VALUES (1,'[مقدمة المحقق]','2','0','0');
+INSERT INTO [t96853] VALUES (2,'عملي في المخطوط','2','1','0');
+INSERT INTO [t96853] VALUES (3,'حول المخطوط','3','1','0');
+INSERT INTO [t96853] VALUES (4,'تحقيق نسبة المخطوط للمؤلف','5','1','0');
+INSERT INTO [t96853] VALUES (5,'ترجمة المصنف','6','1','0');
+INSERT INTO [t96853] VALUES (6,'اسمه ونسبه ولقبه وكنيته','6','5','0');
+INSERT INTO [t96853] VALUES (7,'مولده ونشأته','6','5','0');
+INSERT INTO [t96853] VALUES (8,'رحلاته','7','5','0');
+INSERT INTO [t96853] VALUES (9,'مكانته العلمية بين أبناء عصره','8','5','0');
+INSERT INTO [t96853] VALUES (10,'مؤلفاته','9','5','0');
+INSERT INTO [t96853] VALUES (11,'الوظائف التي تولاها','10','5','0');
+INSERT INTO [t96853] VALUES (12,'المؤاخذات عليه','10','5','0');
+INSERT INTO [t96853] VALUES (13,'صفاته الخلقية','10','5','0');
+INSERT INTO [t96853] VALUES (14,'نظمه','10','5','0');
+INSERT INTO [t96853] VALUES (15,'مرضه ووفاته','11','5','0');
+INSERT INTO [t96853] VALUES (16,'رفع الاشتباه من مسألة المياه','12','0','0');
