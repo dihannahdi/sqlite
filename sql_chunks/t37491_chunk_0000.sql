15909a87d2556cd1466d28ce3c53ba4aedc1fecf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:33 +0700

    Upload SQL chunk: t37491_chunk_0000.sql

---
+++ sql_chunks/t37491_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t37491
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t37491" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37491
+INSERT INTO [t37491] VALUES (1,'قافية الهمزة','4','0','0');
+INSERT INTO [t37491] VALUES (2,'قافية الباء','24','0','0');
+INSERT INTO [t37491] VALUES (3,'قافية التاء','90','0','0');
+INSERT INTO [t37491] VALUES (4,'قافية الجيم','95','0','0');
+INSERT INTO [t37491] VALUES (5,'قافية الحاء','97','0','0');
+INSERT INTO [t37491] VALUES (6,'قافية الدال','104','0','0');
+INSERT INTO [t37491] VALUES (7,'قافية الذال','162','0','0');
+INSERT INTO [t37491] VALUES (8,'قافية السين','183','0','0');
+INSERT INTO [t37491] VALUES (9,'قافية الشين','188','0','0');
+INSERT INTO [t37491] VALUES (10,'قافية الضاد','192','0','0');
+INSERT INTO [t37491] VALUES (11,'قافية العين','195','0','0');
+INSERT INTO [t37491] VALUES (12,'قافية الفاء','203','0','0');
+INSERT INTO [t37491] VALUES (13,'قافية القاف','204','0','0');
+INSERT INTO [t37491] VALUES (14,'القافية الكافية','215','0','0');
+INSERT INTO [t37491] VALUES (15,'القافية اللامية','220','0','0');
+INSERT INTO [t37491] VALUES (16,'القافية الميمية','285','0','0');
+INSERT INTO [t37491] VALUES (17,'القافية الهائية','354','0','0');
+INSERT INTO [t37491] VALUES (18,'القافية اليائية','364','0','0');
