5da6346dfc2aac0949dbfcd059dfc19313520a40
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:47:58 +0700

    Upload SQL chunk: t23652_chunk_0000.sql

---
+++ sql_chunks/t23652_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t23652
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t23652" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23652
+INSERT INTO [t23652] VALUES (1,'تقديم','1','0','0');
+INSERT INTO [t23652] VALUES (2,'المقدمة','4','0','0');
+INSERT INTO [t23652] VALUES (3,'المخطوط','6','0','0');
+INSERT INTO [t23652] VALUES (4,'الدراسة','8','0','0');
+INSERT INTO [t23652] VALUES (5,'ترجمة البوصيري','9','4','0');
+INSERT INTO [t23652] VALUES (6,'عناصر البردة','14','4','0');
+INSERT INTO [t23652] VALUES (7,'المدائح النبوية','22','4','0');
+INSERT INTO [t23652] VALUES (8,'التأثر بإعراب البردة','29','4','0');
+INSERT INTO [t23652] VALUES (9,'قصيدة البردة','32','4','0');
+INSERT INTO [t23652] VALUES (10,'صور المخطوط','42','4','0');
+INSERT INTO [t23652] VALUES (11,'إعراب (بسم الله الرحمن الرحيم)','51','4','0');
+INSERT INTO [t23652] VALUES (12,'إعراب أبيات البردة','54','4','0');
+INSERT INTO [t23652] VALUES (13,'[النسيب النبوي] :','54','12','0');
+INSERT INTO [t23652] VALUES (14,'[التحذير من هوى النفس] :','66','12','0');
+INSERT INTO [t23652] VALUES (15,'[مدح الرسول الكريم] :','79','12','0');
+INSERT INTO [t23652] VALUES (16,'[التحدث عن معجزات الرسول]','111','12','0');
+INSERT INTO [t23652] VALUES (17,'[التحدث عن القرآن الكريم]','124','12','0');
+INSERT INTO [t23652] VALUES (18,'[التحدث عن الإسراء والمعراج]','137','12','0');
+INSERT INTO [t23652] VALUES (19,'[التحدث عن جهاد الرسول وغزواته]','147','12','0');
+INSERT INTO [t23652] VALUES (20,'[التوسل والتشفع]','169','12','0');
+INSERT INTO [t23652] VALUES (21,'الفهارس','191','0','0');
+INSERT INTO [t23652] VALUES (22,'فهرس المسائل الأعرابية','191','21','0');
+INSERT INTO [t23652] VALUES (23,'وجوه إعراب','199','21','0');
+INSERT INTO [t23652] VALUES (24,'فهرس الصرف','200','21','0');
+INSERT INTO [t23652] VALUES (25,'فهرس الايات الكريمة','201','21','0');
+INSERT INTO [t23652] VALUES (26,'(فهرس الشعر)','205','21','0');
+INSERT INTO [t23652] VALUES (27,'فهرس الأعلام','207','21','0');
+INSERT INTO [t23652] VALUES (28,'فهرس الأماكن والبلدان','210','21','0');
+INSERT INTO [t23652] VALUES (29,'المصادر والمراجع','211','21','0');
+INSERT INTO [t23652] VALUES (30,'فهرس الموضوعات','215','21','0');
