63acb375c97ed0f587622e2df178ed9cae202419
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:12 +0700

    Upload SQL chunk: t414_chunk_0000.sql

---
+++ sql_chunks/t414_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t414
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t414" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t414
+INSERT INTO [t414] VALUES (1,'استقبال علماء الهند لوفد علماء مصر','2','0','0');
+INSERT INTO [t414] VALUES (2,'نماذج من تواصل العلماء قديما','2','0','0');
+INSERT INTO [t414] VALUES (3,'علماء العصر صاروا متقاطعين','3','0','0');
+INSERT INTO [t414] VALUES (4,'تحمل الصحابة والسلف لمشاق السفر رغبة في العلم','5','0','0');
+INSERT INTO [t414] VALUES (5,'احتياج العلماء إلى التعاون لحل قضايا العصر','7','0','0');
+INSERT INTO [t414] VALUES (6,'نماذج من التعاون بين دائرة المعارف وحكومة مصر والخزانة الخديوية','8','0','0');
+INSERT INTO [t414] VALUES (7,'شكر أعضاء الوفد على زيارتهم','10','0','0');
