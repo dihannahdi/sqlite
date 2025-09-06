008bf72ae4be3e52d60921731002a9501a9d6ff9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:12 +0700

    Upload SQL chunk: t5885_chunk_0000.sql

---
+++ sql_chunks/t5885_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t5885
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t5885" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5885
+INSERT INTO [t5885] VALUES (1,'طبقات أصحاب نافع مولى عبد الله بن عمر','1','0','0');
+INSERT INTO [t5885] VALUES (2,'الطبقة الأولى من أصحاب نافع مولى عبد الله بن عمر','1','0','0');
+INSERT INTO [t5885] VALUES (3,'الطبقة الثانية','1','0','0');
+INSERT INTO [t5885] VALUES (4,'الطبقة الثالثة','1','0','0');
+INSERT INTO [t5885] VALUES (5,'الطبقة الرابعة','1','0','0');
+INSERT INTO [t5885] VALUES (6,'الطبقة الخامسة','1','0','0');
+INSERT INTO [t5885] VALUES (7,'الطبقة السادسة','1','0','0');
+INSERT INTO [t5885] VALUES (8,'الطبقة السابعة','1','0','0');
+INSERT INTO [t5885] VALUES (9,'الطبقة الثامنة','1','0','0');
+INSERT INTO [t5885] VALUES (10,'الطبقة التاسعة وهم الضعفاء','1','0','0');
+INSERT INTO [t5885] VALUES (11,'الطبقة المتروك حديثهم','1','0','0');
+INSERT INTO [t5885] VALUES (12,'طبقات أصحاب الأعمش','2','0','0');
+INSERT INTO [t5885] VALUES (13,'الطبقة الأولى من أصحاب الأعمش','2','0','0');
+INSERT INTO [t5885] VALUES (14,'الطبقة الثانية','2','0','0');
+INSERT INTO [t5885] VALUES (15,'الطبقة الثالثة','2','0','0');
+INSERT INTO [t5885] VALUES (16,'الطبقة الرابعة','2','0','0');
+INSERT INTO [t5885] VALUES (17,'الطبقة الخامسة','2','0','0');
+INSERT INTO [t5885] VALUES (18,'الطبقة السادسة','2','0','0');
+INSERT INTO [t5885] VALUES (19,'الطبقة السابعة','2','0','0');
