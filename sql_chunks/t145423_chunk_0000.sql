fe2b20695580becc447be190dac48f90f1aeb910
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:20:00 +0700

    Upload SQL chunk: t145423_chunk_0000.sql

---
+++ sql_chunks/t145423_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t145423
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t145423" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t145423
+INSERT INTO [t145423] VALUES (1,'باعثها','2','0','0');
+INSERT INTO [t145423] VALUES (2,'تمهيد في بيان أغراض التصغير عند أئمة اللغة','5','0','0');
+INSERT INTO [t145423] VALUES (3,'فائدة: ذكر أبو حيان - رحمه الله - أن العرب نطقت بأسماء مصغرة، ولم تنطق بها مكبرة','12','0','0');
+INSERT INTO [t145423] VALUES (4,'والألقاب في سابق الدهور على ألوان شتى','14','0','0');
+INSERT INTO [t145423] VALUES (5,'ومنطقة «نجد» عامة شغوفة بالتصغير للأسماء','27','0','0');
+INSERT INTO [t145423] VALUES (6,'عدد الأسر المصغرة','31','0','0');
+INSERT INTO [t145423] VALUES (7,'ولا ينكر وجود غرض التمليح فيها، لكني أراه قليلا؛ لأمور','40','0','0');
+INSERT INTO [t145423] VALUES (8,'أخيرا','44','0','0');
