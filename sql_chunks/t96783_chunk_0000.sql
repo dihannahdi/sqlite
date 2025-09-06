be9cb2fabd8d81bc2f6a82d589b6b92a749c0d57
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:36 +0700

    Upload SQL chunk: t96783_chunk_0000.sql

---
+++ sql_chunks/t96783_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t96783
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t96783" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96783
+INSERT INTO [t96783] VALUES (1,'مقدمة الطبعة الثانية','1','0','0');
+INSERT INTO [t96783] VALUES (2,'المقدمة','3','0','0');
+INSERT INTO [t96783] VALUES (3,'حول العلم وفوائد في علم الحديث','4','0','0');
+INSERT INTO [t96783] VALUES (4,'وما هو العلم الذي يعد فريضة؟','8','3','0');
+INSERT INTO [t96783] VALUES (5,'أسئلة في المصطلح من أبي الحسن حفظه الله تعالى','19','0','0');
+INSERT INTO [t96783] VALUES (6,'أسئلة شباب لودر','121','0','0');
+INSERT INTO [t96783] VALUES (7,'أسئلة متفرقة في المصطلح','146','0','0');
+INSERT INTO [t96783] VALUES (8,'كيف تتعلم البحث','164','0','0');
+INSERT INTO [t96783] VALUES (9,'علم الحديث','182','0','0');
