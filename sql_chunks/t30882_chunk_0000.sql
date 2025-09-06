61da705694597128174afcd57972a43c32cf9614
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:23 +0700

    Upload SQL chunk: t30882_chunk_0000.sql

---
+++ sql_chunks/t30882_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t30882
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t30882" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30882
+INSERT INTO [t30882] VALUES (1,'المراد بقول لا إله إلا الله','1','0','0');
+INSERT INTO [t30882] VALUES (2,'الكفار يشهدون لله بتوحيد الربوبية','2','0','0');
+INSERT INTO [t30882] VALUES (3,'المشركون الذين قاتلهم الرسول كانوا يدعون الصالحين فكفروا بهذا مع إقرارهم لله بتوحيد الربوبية','3','0','0');
+INSERT INTO [t30882] VALUES (4,'كفر المشركين من أهل زماننا أعظم كفرا من الذين قاتلهم رسول الله صلى الله عليه وسلم','4','0','0');
