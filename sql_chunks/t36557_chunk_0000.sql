e2c986dfe9a6f42b8101f819679b374889e38284
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:09 +0700

    Upload SQL chunk: t36557_chunk_0000.sql

---
+++ sql_chunks/t36557_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t36557
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t36557" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36557
+INSERT INTO [t36557] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36557] VALUES (2,'قيام الليل','2','0','0');
+INSERT INTO [t36557] VALUES (3,'أخي الحبيب','3','0','0');
+INSERT INTO [t36557] VALUES (4,'هديه - صلى الله عليه وسلم - في قيام الليل','40','0','0');
+INSERT INTO [t36557] VALUES (5,'المصادر','43','0','0');
