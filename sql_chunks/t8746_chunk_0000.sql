bcacd803d27b305739c595b7c1f325ef115d8463
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:56 +0700

    Upload SQL chunk: t8746_chunk_0000.sql

---
+++ sql_chunks/t8746_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t8746
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t8746" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8746
+INSERT INTO [t8746] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8746] VALUES (2,'المذاهب الحديثة','7','0','0');
+INSERT INTO [t8746] VALUES (3,'الاشتراكية الشيوعية','7','2','0');
+INSERT INTO [t8746] VALUES (4,'أنواع الاشتراكية','9','2','0');
+INSERT INTO [t8746] VALUES (5,'النوع الأول','9','4','0');
+INSERT INTO [t8746] VALUES (6,'النوع الثاني','10','4','0');
+INSERT INTO [t8746] VALUES (7,'النوع الثالث','12','4','0');
