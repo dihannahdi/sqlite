6a82303553915f4cfbeae6c24fa569e9136e24c7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:01 +0700

    Upload SQL chunk: t634_chunk_0000.sql

---
+++ sql_chunks/t634_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t634
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t634" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t634
+INSERT INTO [t634] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t634] VALUES (2,'قال أبو محمد عبد الله بن محمد بن السيد البطليوسي رحمه الله','1','0','0');
+INSERT INTO [t634] VALUES (3,'أنشد أبو القاسم رحمه الله','1','0','0');
+INSERT INTO [t634] VALUES (4,'وهذا ما وجد من شرح أبيات الجمل','79','0','0');
