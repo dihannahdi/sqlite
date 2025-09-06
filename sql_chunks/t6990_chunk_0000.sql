6887efb947b6e38a9e1b6e81c63dd7c25ae7a4b6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:33 +0700

    Upload SQL chunk: t6990_chunk_0000.sql

---
+++ sql_chunks/t6990_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t6990
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t6990" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6990
+INSERT INTO [t6990] VALUES (1,'رسالة أي المشددة','1','0','0');
+INSERT INTO [t6990] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6990] VALUES (3,'الفصل الأول في أقسامها','2','0','0');
+INSERT INTO [t6990] VALUES (4,'الفصل الثاني فيما يلزمها من الإضافة','10','0','0');
+INSERT INTO [t6990] VALUES (5,'الفصل الثالث في الموصولة','11','0','0');
