e2b7063ef17cbe475d7e6b08c2ab41dec5b84618
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:58 +0700

    Upload SQL chunk: t11249_chunk_0000.sql

---
+++ sql_chunks/t11249_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11249
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11249" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11249
+INSERT INTO [t11249] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t11249] VALUES (2,'شرح العقيدة الواسطية من كلام شيخ الإسلام ابن تيمية','4','0','0');
