99d3b27b6ec4e01d1116ad22b211a361fd936a7f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:04 +0700

    Upload SQL chunk: t6233_chunk_0000.sql

---
+++ sql_chunks/t6233_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6233
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6233" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6233
+INSERT INTO [t6233] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6233] VALUES (2,'باب ذكر ما يقوله الطائف بين الركن اليماني والركن الأسود','15','0','0');
