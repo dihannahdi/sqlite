32c1c53f2f11eaa2a68373ed0e0e8f0d6f467e90
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:20 +0700

    Upload SQL chunk: t22756_chunk_0000.sql

---
+++ sql_chunks/t22756_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t22756
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t22756" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22756
+INSERT INTO [t22756] VALUES (1,'[ابن مالك]','1','0','0');
+INSERT INTO [t22756] VALUES (2,'الألفية','2','0','0');
+INSERT INTO [t22756] VALUES (3,'الشروح، وما كتب عليها من حواش وتقريرات','10','0','0');
