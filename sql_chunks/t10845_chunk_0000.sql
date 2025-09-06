aff2fcfb96ba980a0af4ffdd0211c412a548415c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:53 +0700

    Upload SQL chunk: t10845_chunk_0000.sql

---
+++ sql_chunks/t10845_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t10845
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t10845" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10845
+INSERT INTO [t10845] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10845] VALUES (2,'المبحث الأول','4','0','0');
+INSERT INTO [t10845] VALUES (3,'المبحث الثاني','35','0','0');
+INSERT INTO [t10845] VALUES (4,'المبحث الثالث','40','0','0');
+INSERT INTO [t10845] VALUES (5,'الخاتمة','47','0','0');
