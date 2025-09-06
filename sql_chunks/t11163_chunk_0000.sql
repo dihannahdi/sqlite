fcbaf759229b2979584d7336571b50f9441be659
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:51 +0700

    Upload SQL chunk: t11163_chunk_0000.sql

---
+++ sql_chunks/t11163_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t11163
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t11163" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11163
+INSERT INTO [t11163] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11163] VALUES (2,'الفصل الأول: فيمن يجاهد في سبيل الله','3','0','0');
+INSERT INTO [t11163] VALUES (3,'الفصل الثاني: كيف ندعو ونقاتل','6','0','0');
+INSERT INTO [t11163] VALUES (4,'الفصل الثالث: في حرمة الخروج عن الحاكم','8','0','0');
+INSERT INTO [t11163] VALUES (5,'الفصل الرابع: في بيان البديل عن جماعتا التكفير','11','0','0');
+INSERT INTO [t11163] VALUES (6,'الخاتمة','16','0','0');
