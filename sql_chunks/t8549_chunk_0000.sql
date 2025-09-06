fc7b5037c8de93334d0fe13240ed406062c1697a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:06 +0700

    Upload SQL chunk: t8549_chunk_0000.sql

---
+++ sql_chunks/t8549_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t8549
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t8549" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8549
+INSERT INTO [t8549] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8549] VALUES (2,'القسم الأول: الدراسة','7','0','0');
+INSERT INTO [t8549] VALUES (3,'الفصل الأول: تعريف بمؤلف الكتاب','7','2','0');
+INSERT INTO [t8549] VALUES (4,'المبحث الأول: ترجمة موجزة لمؤلف النص','7','3','0');
+INSERT INTO [t8549] VALUES (5,'المبحث الثاني:ترجمة الشارح','10','3','0');
+INSERT INTO [t8549] VALUES (6,'المبحث الثالث:منهج الشرح','13','3','0');
+INSERT INTO [t8549] VALUES (7,'المبحث الرابع: طبعات الكتاب','15','3','0');
+INSERT INTO [t8549] VALUES (8,'المبحث الخامس: وصف النسخة الخطية','17','3','0');
+INSERT INTO [t8549] VALUES (9,'الفصل الثاني: في الجاهلية','18','2','0');
+INSERT INTO [t8549] VALUES (10,'المبحث الثالث: حكم مخالفة أهل الجاهلية','18','9','0');
+INSERT INTO [t8549] VALUES (11,'المبحث الثاني: أنواع الجاهلية','24','9','0');
+INSERT INTO [t8549] VALUES (12,'المبحث الأول: تعريف الجاهلية لغة واصطلاحا','28','9','0');
+INSERT INTO [t8549] VALUES (13,'القسم الثاني: النص المحقق','32','0','0');
+INSERT INTO [t8549] VALUES (14,'مصادر ومراجع','247','0','0');
