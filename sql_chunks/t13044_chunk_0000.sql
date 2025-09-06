731ecbf5a4367c0d9c891399920f77ecaac1cb00
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:09:35 +0700

    Upload SQL chunk: t13044_chunk_0000.sql

---
+++ sql_chunks/t13044_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t13044
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t13044" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13044
+INSERT INTO [t13044] VALUES (1,'باب','14','0','0');
+INSERT INTO [t13044] VALUES (2,'باب في الكبر','41','0','0');
