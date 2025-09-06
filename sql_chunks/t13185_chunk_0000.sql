2bfe0ba7dceac62e807dbf0f3e4e8a1468ca74f5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:55 +0700

    Upload SQL chunk: t13185_chunk_0000.sql

---
+++ sql_chunks/t13185_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t13185
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t13185" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13185
+INSERT INTO [t13185] VALUES (1,'آخر حديث أبي مسهر','21','0','0');
+INSERT INTO [t13185] VALUES (2,'نسخة يحيى بن صالح الوحاظي','50','0','0');
