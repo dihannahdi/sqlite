5fe488b9ba5ccb055a2c37c12e8fd672f7a3de22
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:47 +0700

    Upload SQL chunk: t9895_chunk_0000.sql

---
+++ sql_chunks/t9895_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9895
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9895" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9895
+INSERT INTO [t9895] VALUES (1,'بيان حقيقة التوحيد','1','0','0');
+INSERT INTO [t9895] VALUES (2,'تأليف: صالح بن فوزان بن عبد الله الفوزان','1','0','0');
