f9a692d7bb5cd1c086bd4d043391e42b8a1ee930
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:27 +0700

    Upload SQL chunk: t28170_chunk_0000.sql

---
+++ sql_chunks/t28170_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t28170
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t28170" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t28170
+INSERT INTO [t28170] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t28170] VALUES (2,'من قبائل خندف','2','0','0');
+INSERT INTO [t28170] VALUES (3,'من بطون هذيل بن مدركة','6','0','0');
+INSERT INTO [t28170] VALUES (4,'من قبائل عمرو العنبر','7','0','0');
+INSERT INTO [t28170] VALUES (5,'بطون هوازن بن منصور','13','0','0');
+INSERT INTO [t28170] VALUES (6,'قبائل اليمن','18','0','0');
+INSERT INTO [t28170] VALUES (7,'نسب الأزد','21','0','0');
