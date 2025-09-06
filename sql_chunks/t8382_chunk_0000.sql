9aa647ab596ea6ea5964c762b7ff1c05961d25a2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:46:08 +0700

    Upload SQL chunk: t8382_chunk_0000.sql

---
+++ sql_chunks/t8382_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t8382
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t8382" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8382
+INSERT INTO [t8382] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8382] VALUES (2,'قصة البشرية','3','0','0');
+INSERT INTO [t8382] VALUES (3,'بعثة النبي محمد وخلاصة سيرتة صلى الله علية وسلم','6','0','0');
+INSERT INTO [t8382] VALUES (4,'مقدمة','6','3','0');
+INSERT INTO [t8382] VALUES (5,'مهيئات النبوة','12','3','0');
+INSERT INTO [t8382] VALUES (6,'نبذة عن نسب النبي صلى الله علية وسلم','13','3','0');
+INSERT INTO [t8382] VALUES (7,'بدء الوحي','17','3','0');
+INSERT INTO [t8382] VALUES (8,'من أخلاق النبي صلى الله علية وسلم','21','3','0');
+INSERT INTO [t8382] VALUES (9,'شهادة الفيلسوف الإنجليزي توماس كارليل على صدق رسالة النبي صلى الله عليه وسلم:','25','3','0');
+INSERT INTO [t8382] VALUES (10,'من خصائص دين الاسلام','31','0','0');
+INSERT INTO [t8382] VALUES (11,'من محاسن الدين الاسلامي','42','0','0');
+INSERT INTO [t8382] VALUES (12,'شرح أركان الاسلام','48','0','0');
+INSERT INTO [t8382] VALUES (13,'أسس العقيدة','51','0','0');
+INSERT INTO [t8382] VALUES (14,'شرح أسس العقيدة الاسلامية','52','0','0');
+INSERT INTO [t8382] VALUES (15,'الايمان بالله','52','14','0');
+INSERT INTO [t8382] VALUES (16,'الايمان بالملائكة','60','14','0');
+INSERT INTO [t8382] VALUES (17,'الايمان بالكتب','62','14','0');
+INSERT INTO [t8382] VALUES (18,'الايمان بالرسل','67','14','0');
+INSERT INTO [t8382] VALUES (19,'الايمان باليوم الآخر','69','14','0');
+INSERT INTO [t8382] VALUES (20,'الايمان بالقدر','75','14','0');
+INSERT INTO [t8382] VALUES (21,'العبادة في الاسلام','77','0','0');
+INSERT INTO [t8382] VALUES (22,'الردة عن الاسلام','81','0','0');
+INSERT INTO [t8382] VALUES (23,'مكانة المرأة في الاسلام','85','0','0');
+INSERT INTO [t8382] VALUES (24,'تساؤل','95','0','0');
+INSERT INTO [t8382] VALUES (25,'خاتمة ودعوة','102','0','0');
