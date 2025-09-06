375c2bd4b842a47b304ed0d667490b3b29658d18
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:53 +0700

    Upload SQL chunk: t26595_chunk_0000.sql

---
+++ sql_chunks/t26595_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t26595
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t26595" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26595
+INSERT INTO [t26595] VALUES (1,'من روى عن النبي من الصحابة في الكبائر للبرديجي','2','0','0');
+INSERT INTO [t26595] VALUES (2,'١- منهم عبد الله بن مسعود رضي الله عنه:','2','0','0');
+INSERT INTO [t26595] VALUES (3,'٢- وابن عباس رضي الله عنه:','3','0','0');
+INSERT INTO [t26595] VALUES (4,'٣- وعبد الله بن عمرو:','4','0','0');
+INSERT INTO [t26595] VALUES (5,'٤- وأبو بكرة رضي الله عنه:','5','0','0');
+INSERT INTO [t26595] VALUES (6,'٥- وأبو هريرة من ثلاثة أوجه:','6','0','0');
+INSERT INTO [t26595] VALUES (7,'٦- وأنس بن مالك رضي الله عنه:','7','0','0');
+INSERT INTO [t26595] VALUES (8,'٧- وعمران بن حصين رضي الله عنه:','8','0','0');
+INSERT INTO [t26595] VALUES (9,'٨- وخريم بن فاتك رضي الله عنه:','9','0','0');
+INSERT INTO [t26595] VALUES (10,'٩- وابن عمر رضي الله عنهما:','10','0','0');
+INSERT INTO [t26595] VALUES (11,'١٠- وأبو أيوب رضي الله عنه:','11','0','0');
+INSERT INTO [t26595] VALUES (12,'١١- عبد الله بن أنيس رضي الله عنه:','12','0','0');
