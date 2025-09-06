e6e5ce856933931944d2c414f0d62db71dc7d938
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:11 +0700

    Upload SQL chunk: t443_chunk_0000.sql

---
+++ sql_chunks/t443_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t443
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t443" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t443
+INSERT INTO [t443] VALUES (1,'سبب تأليف الرسالة','2','0','0');
+INSERT INTO [t443] VALUES (2,'مذهب الشافعية في المسألة وحجتهم والجواب عن حجج الآخرين','2','0','0');
+INSERT INTO [t443] VALUES (3,'رسالة من المؤلف إلى الصيلمي ورد الأخير عليها','12','0','0');
