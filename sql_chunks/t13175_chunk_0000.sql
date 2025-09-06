159ee011cbdd2c27ff255c968af6b86ab9160c2d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:44 +0700

    Upload SQL chunk: t13175_chunk_0000.sql

---
+++ sql_chunks/t13175_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t13175
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t13175" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13175
+INSERT INTO [t13175] VALUES (1,'باب المحمدين صلى الله على محمد وآله وسلم','1','0','0');
+INSERT INTO [t13175] VALUES (2,'باب الألف','67','0','0');
+INSERT INTO [t13175] VALUES (3,'باب إبراهيم','90','2','0');
+INSERT INTO [t13175] VALUES (4,'باب إسحاق','104','2','0');
+INSERT INTO [t13175] VALUES (5,'باب إسماعيل','111','2','0');
+INSERT INTO [t13175] VALUES (6,'باب الباء','119','0','0');
+INSERT INTO [t13175] VALUES (7,'باب الجيم','125','0','0');
+INSERT INTO [t13175] VALUES (8,'باب الحاء','133','0','0');
+INSERT INTO [t13175] VALUES (9,'باب الخاء','165','0','0');
+INSERT INTO [t13175] VALUES (10,'باب الدال','172','0','0');
+INSERT INTO [t13175] VALUES (11,'باب الراء','175','0','0');
+INSERT INTO [t13175] VALUES (12,'باب الزاي','181','0','0');
+INSERT INTO [t13175] VALUES (13,'باب السين','188','0','0');
+INSERT INTO [t13175] VALUES (14,'باب الشين','208','0','0');
+INSERT INTO [t13175] VALUES (15,'باب الصاد','214','0','0');
+INSERT INTO [t13175] VALUES (16,'باب العين','221','0','0');
+INSERT INTO [t13175] VALUES (17,'باب الفاء','292','0','0');
+INSERT INTO [t13175] VALUES (18,'باب القاف','296','0','0');
+INSERT INTO [t13175] VALUES (19,'باب الكاف','300','0','0');
+INSERT INTO [t13175] VALUES (20,'باب الميم','302','0','0');
+INSERT INTO [t13175] VALUES (21,'باب النون','323','0','0');
+INSERT INTO [t13175] VALUES (22,'باب الهاء','327','0','0');
+INSERT INTO [t13175] VALUES (23,'باب الواو','337','0','0');
+INSERT INTO [t13175] VALUES (24,'باب الياء','343','0','0');
