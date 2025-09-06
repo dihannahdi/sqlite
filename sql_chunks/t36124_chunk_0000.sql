1c51cd6c7ef382f1110ba68495508df56a9c7dc8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:38 +0700

    Upload SQL chunk: t36124_chunk_0000.sql

---
+++ sql_chunks/t36124_chunk_0000.sql
@@ -0,0 +1,52 @@
+-- Table: t36124
+-- Chunk: 1
+-- Rows: 45
+
+CREATE TABLE "t36124" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36124
+INSERT INTO [t36124] VALUES (1,'١','2','0','0');
+INSERT INTO [t36124] VALUES (2,'بداية الدرس','2','1','0');
+INSERT INTO [t36124] VALUES (3,'خاتمة الدرس','21','1','0');
+INSERT INTO [t36124] VALUES (4,'٢','22','0','0');
+INSERT INTO [t36124] VALUES (5,'بداية الدرس','22','4','0');
+INSERT INTO [t36124] VALUES (6,'خاتمة الدرس','51','4','0');
+INSERT INTO [t36124] VALUES (7,'٣','52','0','0');
+INSERT INTO [t36124] VALUES (8,'بداية الدرس','52','7','0');
+INSERT INTO [t36124] VALUES (9,'خاتمة الدرس','76','7','0');
+INSERT INTO [t36124] VALUES (10,'٤','77','0','0');
+INSERT INTO [t36124] VALUES (11,'بداية الدرس','77','10','0');
+INSERT INTO [t36124] VALUES (12,'خاتمة الدرس','109','10','0');
+INSERT INTO [t36124] VALUES (13,'٥','110','0','0');
+INSERT INTO [t36124] VALUES (14,'بداية الدرس','110','13','0');
+INSERT INTO [t36124] VALUES (15,'خاتمة الدرس','136','13','0');
+INSERT INTO [t36124] VALUES (16,'٦','137','0','0');
+INSERT INTO [t36124] VALUES (17,'بداية الدرس','137','16','0');
+INSERT INTO [t36124] VALUES (18,'خاتمة الدرس','168','16','0');
+INSERT INTO [t36124] VALUES (19,'٧','169','0','0');
+INSERT INTO [t36124] VALUES (20,'بداية الدرس','169','19','0');
+INSERT INTO [t36124] VALUES (21,'خاتمة الدرس','185','19','0');
+INSERT INTO [t36124] VALUES (22,'٨','186','0','0');
+INSERT INTO [t36124] VALUES (23,'بداية الدرس','186','22','0');
+INSERT INTO [t36124] VALUES (24,'خاتمة الدرس','199','22','0');
+INSERT INTO [t36124] VALUES (25,'٩','200','0','0');
+INSERT INTO [t36124] VALUES (26,'بداية الدرس','200','25','0');
+INSERT INTO [t36124] VALUES (27,'خاتمة الدرس','227','25','0');
+INSERT INTO [t36124] VALUES (28,'١٠','228','0','0');
+INSERT INTO [t36124] VALUES (29,'بداية الدرس','228','28','0');
+INSERT INTO [t36124] VALUES (30,'خاتمة الدرس','256','28','0');
+INSERT INTO [t36124] VALUES (31,'١١','257','0','0');
+INSERT INTO [t36124] VALUES (32,'بداية الدرس','257','31','0');
+INSERT INTO [t36124] VALUES (33,'خاتمة الدرس','286','31','0');
+INSERT INTO [t36124] VALUES (34,'١٢','287','0','0');
+INSERT INTO [t36124] VALUES (35,'بداية الدرس','287','34','0');
+INSERT INTO [t36124] VALUES (36,'خاتمة الدرس','315','34','0');
+INSERT INTO [t36124] VALUES (37,'١٣','316','0','0');
+INSERT INTO [t36124] VALUES (38,'بداية الدرس','316','37','0');
+INSERT INTO [t36124] VALUES (39,'خاتمة الدرس','331','37','0');
+INSERT INTO [t36124] VALUES (40,'١٤','332','0','0');
+INSERT INTO [t36124] VALUES (41,'بداية الدرس','332','40','0');
+INSERT INTO [t36124] VALUES (42,'خاتمة الدرس','350','40','0');
+INSERT INTO [t36124] VALUES (43,'١٥','351','0','0');
+INSERT INTO [t36124] VALUES (44,'بداية الدرس','351','43','0');
+INSERT INTO [t36124] VALUES (45,'خاتمة الدرس','371','43','0');
