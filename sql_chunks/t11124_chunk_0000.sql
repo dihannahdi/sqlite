1d1b4a77ecdc6da08249dde130df214d28e6f220
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:28 +0700

    Upload SQL chunk: t11124_chunk_0000.sql

---
+++ sql_chunks/t11124_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t11124
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t11124" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11124
+INSERT INTO [t11124] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11124] VALUES (2,'المبحث الأول: قواعد أولية لا بد من ذكرها قبل الشروع في الرسالة','4','0','0');
+INSERT INTO [t11124] VALUES (3,'المبحث الثاني: في تعريف الصيد وحكمه','11','0','0');
+INSERT INTO [t11124] VALUES (4,'المبحث الثالث: ذكر بعض الأحكام المتعلقة بالصائد','13','0','0');
+INSERT INTO [t11124] VALUES (5,'المبحث الرابع: الأحكام المتعلقة بالصيد','21','0','0');
+INSERT INTO [t11124] VALUES (6,'المبحث الخامس: في ذكر بعض آداب الصيد','26','0','0');
+INSERT INTO [t11124] VALUES (7,'المبحث السادس: فيما يتعلق بصيد الضب','30','0','0');
+INSERT INTO [t11124] VALUES (8,'المبحث السابع: في الصيد بالجوارح','36','0','0');
+INSERT INTO [t11124] VALUES (9,'المبحث الثامن: في مسائل الصيد','46','0','0');
