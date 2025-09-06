b76dedeeaae0d5ad20dc90aa869bcbca9f780bfd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:29 +0700

    Upload SQL chunk: t11125_chunk_0000.sql

---
+++ sql_chunks/t11125_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11125
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11125" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11125
+INSERT INTO [t11125] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t11125] VALUES (2,'رتبت تللك المصطلحات على حسب ورودها في الكتاب','2','0','0');
