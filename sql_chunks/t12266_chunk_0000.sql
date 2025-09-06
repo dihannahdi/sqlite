4291736df36fa7ce3c6d7490aa3212193db651e3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:03:49 +0700

    Upload SQL chunk: t12266_chunk_0000.sql

---
+++ sql_chunks/t12266_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t12266
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t12266" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12266
+INSERT INTO [t12266] VALUES (1,'التراويح لغة','1','0','0');
+INSERT INTO [t12266] VALUES (2,'الترغيب في قيام رمضان','2','0','0');
+INSERT INTO [t12266] VALUES (3,'حكم صلاة التراويح بالجماعة','3','0','0');
+INSERT INTO [t12266] VALUES (4,'حديث عائشة','6','0','0');
+INSERT INTO [t12266] VALUES (5,'عدد ركعات صلاة التراويح','10','0','0');
+INSERT INTO [t12266] VALUES (6,'دراسة الآثار المروية عن عمر بن الخطاب رضي الله عنه بخصوص عدد ركعات صلاة التراويح','15','0','0');
