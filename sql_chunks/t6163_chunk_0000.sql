fbef6dc69cedc191d7a45b1a3f8125341a96ecd7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:36 +0700

    Upload SQL chunk: t6163_chunk_0000.sql

---
+++ sql_chunks/t6163_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6163
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6163" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6163
+INSERT INTO [t6163] VALUES (1,'وبيانه من وجوه','6','0','0');
+INSERT INTO [t6163] VALUES (2,'الفصل الثاني','66','0','0');
+INSERT INTO [t6163] VALUES (3,'الفصل الثالث','67','0','0');
