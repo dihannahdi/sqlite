439db1335f3a7c5877c88f7ceec9113792238445
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:26:11 +0700

    Upload SQL chunk: t16435_chunk_0000.sql

---
+++ sql_chunks/t16435_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t16435
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t16435" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t16435
+INSERT INTO [t16435] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t16435] VALUES (2,'تمهيد','6','1','0');
+INSERT INTO [t16435] VALUES (3,'توثيق الشرح','7','1','0');
+INSERT INTO [t16435] VALUES (4,'الشارح: حياته وآثاره ووفاته','30','1','0');
+INSERT INTO [t16435] VALUES (5,'مخطوطة الشرح: ناسخها ووصفها','60','1','0');
+INSERT INTO [t16435] VALUES (6,'منهجنا وعملنا في خدمة هذا الشرح','62','1','0');
+INSERT INTO [t16435] VALUES (7,'باب الحماسة ومعنى الحماسة الشدة والشجاعة','68','0','0');
+INSERT INTO [t16435] VALUES (8,'باب المراثي','359','0','0');
+INSERT INTO [t16435] VALUES (9,'باب الأدب','491','0','0');
+INSERT INTO [t16435] VALUES (10,'باب النسيب','546','0','0');
+INSERT INTO [t16435] VALUES (11,'باب الهجاء','658','0','0');
+INSERT INTO [t16435] VALUES (12,'باب الأضياف','729','0','0');
+INSERT INTO [t16435] VALUES (13,'باب الصفات','849','0','0');
+INSERT INTO [t16435] VALUES (14,'باب السير والنعاس','854','0','0');
+INSERT INTO [t16435] VALUES (15,'باب الملح','864','0','0');
+INSERT INTO [t16435] VALUES (16,'باب مذمة النساء','885','0','0');
