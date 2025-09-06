70b571d6d8f3988488cf603f19c72fdd10cb7daf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:46 +0700

    Upload SQL chunk: t28740_chunk_0000.sql

---
+++ sql_chunks/t28740_chunk_0000.sql
@@ -0,0 +1,67 @@
+-- Table: t28740
+-- Chunk: 1
+-- Rows: 60
+
+CREATE TABLE "t28740" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t28740
+INSERT INTO [t28740] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t28740] VALUES (2,'اللهم صلي على محمد وعلى آل محمد رب يسر برحمتك','1','1','0');
+INSERT INTO [t28740] VALUES (3,'حرف الألف','2','0','0');
+INSERT INTO [t28740] VALUES (4,'من اسمه أحمد','2','3','0');
+INSERT INTO [t28740] VALUES (5,'من اسمه إبراهيم','50','3','0');
+INSERT INTO [t28740] VALUES (6,'من اسمه إسماعيل','64','3','0');
+INSERT INTO [t28740] VALUES (7,'في الإفراد','64','3','0');
+INSERT INTO [t28740] VALUES (8,'حرف الجيم','65','0','0');
+INSERT INTO [t28740] VALUES (9,'من اسمه جعفر','65','8','0');
+INSERT INTO [t28740] VALUES (10,'في الإفراد','67','8','0');
+INSERT INTO [t28740] VALUES (11,'حرف الحاء','68','0','0');
+INSERT INTO [t28740] VALUES (12,'من اسمه حسن','68','11','0');
+INSERT INTO [t28740] VALUES (13,'من اسمه حسين','73','11','0');
+INSERT INTO [t28740] VALUES (14,'حرف الخاء','77','0','0');
+INSERT INTO [t28740] VALUES (15,'من اسمه خلف','77','14','0');
+INSERT INTO [t28740] VALUES (16,'في الأفراد','81','14','0');
+INSERT INTO [t28740] VALUES (17,'حرف الراء','82','0','0');
+INSERT INTO [t28740] VALUES (18,'حرف الزاي','83','0','0');
+INSERT INTO [t28740] VALUES (19,'حرف الطاء','86','0','0');
+INSERT INTO [t28740] VALUES (20,'من اسمه طاهر','86','19','0');
+INSERT INTO [t28740] VALUES (21,'في الأفراد','88','19','0');
+INSERT INTO [t28740] VALUES (22,'حرف الظاء','89','0','0');
+INSERT INTO [t28740] VALUES (23,'حرف اللام','89','0','0');
+INSERT INTO [t28740] VALUES (24,'حرف الميم','90','0','0');
+INSERT INTO [t28740] VALUES (25,'من اسمه محمد','90','24','0');
+INSERT INTO [t28740] VALUES (26,'من اسمه موسى','183','24','0');
+INSERT INTO [t28740] VALUES (27,'من اسمه مروان','187','24','0');
+INSERT INTO [t28740] VALUES (28,'من اسمه منصور','189','24','0');
+INSERT INTO [t28740] VALUES (29,'الأفراد','192','24','0');
+INSERT INTO [t28740] VALUES (30,'حرف النون','195','0','0');
+INSERT INTO [t28740] VALUES (31,'من اسمه نصر','195','30','0');
+INSERT INTO [t28740] VALUES (32,'في الأفراد','197','30','0');
+INSERT INTO [t28740] VALUES (33,'حرف العين','197','0','0');
+INSERT INTO [t28740] VALUES (34,'من اسمه عبد الله','197','33','0');
+INSERT INTO [t28740] VALUES (35,'من اسمه عبيد الله','225','33','0');
+INSERT INTO [t28740] VALUES (36,'من اسمه عبد الرحمن','227','33','0');
+INSERT INTO [t28740] VALUES (37,'من اسمه عبد الرحيم','241','33','0');
+INSERT INTO [t28740] VALUES (38,'من اسمه عبد الملك','243','33','0');
+INSERT INTO [t28740] VALUES (39,'من اسمه عبد العزيز','250','33','0');
+INSERT INTO [t28740] VALUES (40,'من اسمه عبد الحق','255','33','0');
+INSERT INTO [t28740] VALUES (41,'من اسمه عبد الجليل','258','33','0');
+INSERT INTO [t28740] VALUES (42,'من اسمه عبد الصمد','258','33','0');
+INSERT INTO [t28740] VALUES (43,'الأفراد','258','33','0');
+INSERT INTO [t28740] VALUES (44,'من اسمه عمر','261','33','0');
+INSERT INTO [t28740] VALUES (45,'من اسمه علي','263','33','0');
+INSERT INTO [t28740] VALUES (46,'من اسمه عيسى','282','33','0');
+INSERT INTO [t28740] VALUES (47,'من اسمه عتيق','283','33','0');
+INSERT INTO [t28740] VALUES (48,'الأفراد في حرف العين','285','33','0');
+INSERT INTO [t28740] VALUES (49,'من الكنى في هذا الباب','291','33','0');
+INSERT INTO [t28740] VALUES (50,'حرف الفاء','292','0','0');
+INSERT INTO [t28740] VALUES (51,'حرف القاف','293','0','0');
+INSERT INTO [t28740] VALUES (52,'ممن عرف بكنيته','294','51','0');
+INSERT INTO [t28740] VALUES (53,'حرف السين','294','0','0');
+INSERT INTO [t28740] VALUES (54,'من اسمه سليمان','294','53','0');
+INSERT INTO [t28740] VALUES (55,'من اسمه سعيد','297','53','0');
+INSERT INTO [t28740] VALUES (56,'الأفراد','297','53','0');
+INSERT INTO [t28740] VALUES (57,'حرف الياء','300','0','0');
+INSERT INTO [t28740] VALUES (58,'من اسمه يحيى','301','57','0');
+INSERT INTO [t28740] VALUES (59,'من اسمه يوسف','305','57','0');
+INSERT INTO [t28740] VALUES (60,'الأفراد','311','57','0');
