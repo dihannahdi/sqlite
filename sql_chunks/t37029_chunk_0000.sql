1794b274520ba6ce9ac949680157efd0207673fa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:11:40 +0700

    Upload SQL chunk: t37029_chunk_0000.sql

---
+++ sql_chunks/t37029_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t37029
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t37029" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37029
+INSERT INTO [t37029] VALUES (1,'ما يقول فقهاء الدين','1','0','0');
+INSERT INTO [t37029] VALUES (2,'وأقول','2','0','0');
