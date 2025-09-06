55e528599cf3526662285b612fbe6d59b5b84731
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:00 +0700

    Upload SQL chunk: t521_chunk_0000.sql

---
+++ sql_chunks/t521_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t521
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t521" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t521
+INSERT INTO [t521] VALUES (1,'قراءات','2','0','0');
+INSERT INTO [t521] VALUES (2,'تفسير','3','0','0');
+INSERT INTO [t521] VALUES (3,'حديث','5','0','0');
