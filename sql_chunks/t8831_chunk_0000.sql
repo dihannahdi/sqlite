cce33091537d64e12ecb904cea40eb1337b3c7ca
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:21 +0700

    Upload SQL chunk: t8831_chunk_0000.sql

---
+++ sql_chunks/t8831_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t8831
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t8831" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8831
+INSERT INTO [t8831] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8831] VALUES (2,'باب مظاهر الغزو الفكري','4','0','0');
+INSERT INTO [t8831] VALUES (3,'فكرة فصل الدين عن الدولة','4','2','0');
+INSERT INTO [t8831] VALUES (4,'فكرة القوميات والعصبيات الجاهلية','6','2','0');
+INSERT INTO [t8831] VALUES (5,'فكرة الوطنية','8','2','0');
+INSERT INTO [t8831] VALUES (6,'فكرة العلمانية','9','2','0');
+INSERT INTO [t8831] VALUES (7,'شعارات المدنية والحضارة والتقدم','11','2','0');
+INSERT INTO [t8831] VALUES (8,'الحركة النسائية وفكرة تحرير المرأة','13','2','0');
+INSERT INTO [t8831] VALUES (9,'باب وسائل الغزو الفكري','14','0','0');
+INSERT INTO [t8831] VALUES (10,'برامج التعليم','14','9','0');
+INSERT INTO [t8831] VALUES (11,'وسائل الإعلام','17','9','0');
+INSERT INTO [t8831] VALUES (12,'تنظيم القوى الكافرة والغازية والإهتمام بمراكز القوة','18','9','0');
