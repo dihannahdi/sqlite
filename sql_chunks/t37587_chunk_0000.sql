719de3d6dfd919b8e653c9b9d83e1539f83dddd1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:14:18 +0700

    Upload SQL chunk: t37587_chunk_0000.sql

---
+++ sql_chunks/t37587_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t37587
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t37587" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37587
+INSERT INTO [t37587] VALUES (1,'إهداء','3','0','0');
+INSERT INTO [t37587] VALUES (2,'مقدمة الطبعة الثامنة','5','0','0');
+INSERT INTO [t37587] VALUES (3,'مقدمة الطبعة الأولى','9','0','0');
+INSERT INTO [t37587] VALUES (4,'الطريق','11','0','0');
+INSERT INTO [t37587] VALUES (5,'١ - أصلح نفسك','13','0','0');
+INSERT INTO [t37587] VALUES (6,'نظرة إلى الواقع','13','5','0');
+INSERT INTO [t37587] VALUES (7,'من صور عذاب أهل النار','15','5','0');
+INSERT INTO [t37587] VALUES (8,'خوف الصحابة والسلف من الحساب','17','5','0');
+INSERT INTO [t37587] VALUES (9,'إلى العمل','20','5','0');
+INSERT INTO [t37587] VALUES (10,'من معينات الطريق','21','5','0');
+INSERT INTO [t37587] VALUES (11,'٢ - ادع غيرك','23','0','0');
+INSERT INTO [t37587] VALUES (12,'واقع الأمة الإسلامية','23','11','0');
+INSERT INTO [t37587] VALUES (13,'فرضية التغيير','25','11','0');
+INSERT INTO [t37587] VALUES (14,'الأدلة الشرعية على فرضية التغيير','26','11','0');
+INSERT INTO [t37587] VALUES (15,'دفع شبهة','27','11','0');
+INSERT INTO [t37587] VALUES (16,'بركة الدعوة إلى الله','30','11','0');
+INSERT INTO [t37587] VALUES (17,'٣ - طريق التغيير','32','0','0');
+INSERT INTO [t37587] VALUES (18,'التمكين للمؤمنين سنة كونية','34','17','0');
+INSERT INTO [t37587] VALUES (19,'إنها الجنة تبغى ثمنا','35','17','0');
+INSERT INTO [t37587] VALUES (20,'الابتلاء من سنن الدعوات','36','17','0');
+INSERT INTO [t37587] VALUES (21,'الابتلاء بالخير والشر','36','17','0');
+INSERT INTO [t37587] VALUES (22,'فلنكن على حذر','37','17','0');
+INSERT INTO [t37587] VALUES (23,'٤ - المعادلة الصعبة','40','0','0');
+INSERT INTO [t37587] VALUES (24,'الشيطان مع الواحد','40','23','0');
+INSERT INTO [t37587] VALUES (25,'ضرورة التآخي في الله','41','23','0');
+INSERT INTO [t37587] VALUES (26,'وصايا على الطريق','43','23','0');
+INSERT INTO [t37587] VALUES (27,'خاتمة ودعاء','46','0','0');
