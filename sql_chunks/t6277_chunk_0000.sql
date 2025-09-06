9dc1f347e80c2f0e3b158e0a9804acfbe46060d6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:22 +0700

    Upload SQL chunk: t6277_chunk_0000.sql

---
+++ sql_chunks/t6277_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6277
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6277" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6277
+INSERT INTO [t6277] VALUES (1,'السماع لابن القيسراني','1','0','0');
+INSERT INTO [t6277] VALUES (2,'فصل','63','0','0');
