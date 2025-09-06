5123fb7bf8e6b64d170a4038a1837203b7af1611
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:12 +0700

    Upload SQL chunk: t147091_chunk_0000.sql

---
+++ sql_chunks/t147091_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t147091
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t147091" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t147091
+INSERT INTO [t147091] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t147091] VALUES (2,'يقال لهؤلاء الذين لا يأخذون بالسنة','3','0','0');
+INSERT INTO [t147091] VALUES (3,'خلاصة مذهب منكري السنة','12','0','0');
