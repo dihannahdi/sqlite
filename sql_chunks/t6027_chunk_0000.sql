71fbab36d8aad04d514deee1cbc31399a8cf8963
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:32 +0700

    Upload SQL chunk: t6027_chunk_0000.sql

---
+++ sql_chunks/t6027_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6027
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6027" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6027
+INSERT INTO [t6027] VALUES (1,'ذكر خبر يدل على ان في رواة الحديث كذابين ويحث على معرفة المعدلين من رواة الأخبار من المجروحين','25','0','0');
+INSERT INTO [t6027] VALUES (2,'تم الكتاب','155','0','0');
