fb780edcc8665a6ec32ae95d52e908cf23632638
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:27 +0700

    Upload SQL chunk: t30623_chunk_0000.sql

---
+++ sql_chunks/t30623_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t30623
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t30623" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30623
+INSERT INTO [t30623] VALUES (1,'من حديث ابن بشران.','2','0','0');
+INSERT INTO [t30623] VALUES (2,'من حديث الشافعي','10','0','0');
+INSERT INTO [t30623] VALUES (3,'حديث عبد الكريم الديرعاقولي، وبعده من حديث الشافعي','13','0','0');
+INSERT INTO [t30623] VALUES (4,'من حديث علي بن عبد العزيز، عن أبي عبيد','20','0','0');
+INSERT INTO [t30623] VALUES (5,'من حديث ابن غيلان','24','0','0');
+INSERT INTO [t30623] VALUES (6,'من حديث الجوهري','28','0','0');
+INSERT INTO [t30623] VALUES (7,'من حديث ظريف الحيري','31','0','0');
+INSERT INTO [t30623] VALUES (8,'حديث واحد عن ابن النرسي','35','0','0');
+INSERT INTO [t30623] VALUES (9,'حديث واحد عن ابن حيد','37','0','0');
+INSERT INTO [t30623] VALUES (10,'من حديث ابن شاهين , فهو المعلى','39','0','0');
+INSERT INTO [t30623] VALUES (11,'من حديث الشافعي.','42','0','0');
+INSERT INTO [t30623] VALUES (12,'من حديث الشافعي.','46','0','0');
+INSERT INTO [t30623] VALUES (13,'من حديثه أيضا.','50','0','0');
+INSERT INTO [t30623] VALUES (14,'من حديث ابن السمرقندي.','53','0','0');
+INSERT INTO [t30623] VALUES (15,'من حديث الشافعي.','58','0','0');
+INSERT INTO [t30623] VALUES (16,'من حديثه أيضا.','62','0','0');
+INSERT INTO [t30623] VALUES (17,'من حديث الجوهري.','66','0','0');
+INSERT INTO [t30623] VALUES (18,'حديث واحد , عن أبي ياسر بن عائش.','71','0','0');
+INSERT INTO [t30623] VALUES (19,'من فوائد ابن السمرقندي.','73','0','0');
+INSERT INTO [t30623] VALUES (20,'من حديث ابن المسلمة','76','0','0');
+INSERT INTO [t30623] VALUES (21,'حديث مسلسل من رواية ابن السراج.','80','0','0');
+INSERT INTO [t30623] VALUES (22,'من فوائد السراج.','82','0','0');
+INSERT INTO [t30623] VALUES (23,'من فوائد ابن السمرقندي','83','0','0');
