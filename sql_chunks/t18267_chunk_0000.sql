132190438e3759c3ed3131cc4977c3fb476d3236
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:33:33 +0700

    Upload SQL chunk: t18267_chunk_0000.sql

---
+++ sql_chunks/t18267_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t18267
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t18267" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t18267
+INSERT INTO [t18267] VALUES (1,'هذا باب علم (ما) الكلم من العربية','1','0','0');
+INSERT INTO [t18267] VALUES (2,'هذا باب ما ائتلف من هذه الألفاظ الثلاثة','11','0','0');
+INSERT INTO [t18267] VALUES (3,'هذا باب معرفة ما كان شاذا من كلامهم','24','0','0');
+INSERT INTO [t18267] VALUES (4,'هذا باب الإعراب والبناء','57','0','0');
