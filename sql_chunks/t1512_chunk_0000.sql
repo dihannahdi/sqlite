66d505e952c2200d8a48ec2d4371146ab2283679
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:24:50 +0700

    Upload SQL chunk: t1512_chunk_0000.sql

---
+++ sql_chunks/t1512_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t1512
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t1512" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1512
+INSERT INTO [t1512] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t1512] VALUES (2,'فصل في أمراض القلوب وشفائها','1','0','0');
