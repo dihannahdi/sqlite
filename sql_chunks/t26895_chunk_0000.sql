f0da4a532c20129357dac0071b9a5d8f8de394fc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:14 +0700

    Upload SQL chunk: t26895_chunk_0000.sql

---
+++ sql_chunks/t26895_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t26895
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t26895" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26895
+INSERT INTO [t26895] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26895] VALUES (2,'[١]- باب الألف','2','0','0');
+INSERT INTO [t26895] VALUES (3,'[٢]- باب الباء','5','0','0');
+INSERT INTO [t26895] VALUES (4,'لم يذكر التاء','6','0','0');
+INSERT INTO [t26895] VALUES (5,'[٣]- باب الثاء','6','0','0');
+INSERT INTO [t26895] VALUES (6,'[٤]- باب الجيم','8','0','0');
+INSERT INTO [t26895] VALUES (7,'[٥]- باب الحاء','9','0','0');
+INSERT INTO [t26895] VALUES (8,'[٦]- باب الخاء','11','0','0');
+INSERT INTO [t26895] VALUES (9,'[٧]- باب الدال','12','0','0');
+INSERT INTO [t26895] VALUES (10,'[٨]- باب الذال','13','0','0');
+INSERT INTO [t26895] VALUES (11,'[٩]- باب الراء','13','0','0');
+INSERT INTO [t26895] VALUES (12,'[١٠]- باب الزاي','14','0','0');
+INSERT INTO [t26895] VALUES (13,'[١١]- باب السين','15','0','0');
+INSERT INTO [t26895] VALUES (14,'[١٢]- باب الشين','20','0','0');
+INSERT INTO [t26895] VALUES (15,'[١٣]- باب الصاد','23','0','0');
+INSERT INTO [t26895] VALUES (16,'[١٤]- باب الضاد','24','0','0');
+INSERT INTO [t26895] VALUES (17,'[١٥]- باب الطاء','25','0','0');
+INSERT INTO [t26895] VALUES (18,'[١٦]- باب الظاء','26','0','0');
+INSERT INTO [t26895] VALUES (19,'[١٧]- باب العين','26','0','0');
+INSERT INTO [t26895] VALUES (20,'[١٨]- باب الغين','35','0','0');
+INSERT INTO [t26895] VALUES (21,'[١٩]- باب الفاء','36','0','0');
+INSERT INTO [t26895] VALUES (22,'[٢٠]- باب القاف','38','0','0');
+INSERT INTO [t26895] VALUES (23,'[٢١]- باب الكاف','40','0','0');
+INSERT INTO [t26895] VALUES (24,'[٢٢]- باب اللام','40','0','0');
+INSERT INTO [t26895] VALUES (25,'[٢٣]- باب الميم','41','0','0');
+INSERT INTO [t26895] VALUES (26,'[٢٤]- باب النون','47','0','0');
+INSERT INTO [t26895] VALUES (27,'[٢٥]- باب الهاء','48','0','0');
+INSERT INTO [t26895] VALUES (28,'[٢٦]- باب الواو','49','0','0');
+INSERT INTO [t26895] VALUES (29,'[٢٧]- باب اللام والألف','50','0','0');
+INSERT INTO [t26895] VALUES (30,'[٢٨]- باب الياء','51','0','0');
