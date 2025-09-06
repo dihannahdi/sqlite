d838e536003c670b2c0e252f7304f61b66cbbc5c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:40 +0700

    Upload SQL chunk: t1962_chunk_0000.sql

---
+++ sql_chunks/t1962_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t1962
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t1962" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1962
+INSERT INTO [t1962] VALUES (1,'تقديم','1','0','0');
+INSERT INTO [t1962] VALUES (2,'بيانات عامة عن السلسلة','5','0','0');
+INSERT INTO [t1962] VALUES (3,'أهداف السلسلة','7','0','0');
+INSERT INTO [t1962] VALUES (4,'محتويات الكتاب','9','0','0');
+INSERT INTO [t1962] VALUES (5,'ضوابط الكتابة','11','0','0');
+INSERT INTO [t1962] VALUES (6,'شروط تقديم النص','15','0','0');
+INSERT INTO [t1962] VALUES (7,'عنوان المراسلة','16','0','0');
+INSERT INTO [t1962] VALUES (8,'استبانة التحكيم','17','0','0');
+INSERT INTO [t1962] VALUES (9,'رأي المحكم في صلاحية الكتاب للنشر','18','0','0');
+INSERT INTO [t1962] VALUES (10,'دعوة للعلماء والباحثين','19','0','0');
+INSERT INTO [t1962] VALUES (11,'محتويات الدليل','20','0','0');
+INSERT INTO [t1962] VALUES (12,'استبانة جمع معلومات','22','0','0');
