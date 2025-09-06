9d98124a6923d6f7b73bde14dc0ff57997202f85
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:03 +0700

    Upload SQL chunk: t324_chunk_0000.sql

---
+++ sql_chunks/t324_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t324
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t324" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t324
+INSERT INTO [t324] VALUES (1,'الحمد لله تعالى الذي علا في سماواته','1','0','0');
+INSERT INTO [t324] VALUES (2,'* لما نزل الموت بعمرو بن العاص - رضي الله عنه - قال له ابنه','2','0','0');
+INSERT INTO [t324] VALUES (3,'أخي في الله','3','0','0');
+INSERT INTO [t324] VALUES (4,'أخي المسلم: لمثل هذا اليوم','5','0','0');
+INSERT INTO [t324] VALUES (5,'أخي في الله','5','0','0');
+INSERT INTO [t324] VALUES (6,'ومرحبا بدار البقاء ..','6','0','0');
+INSERT INTO [t324] VALUES (7,'لقد جازاها الله تعالى جزاء أعدائه','7','0','0');
+INSERT INTO [t324] VALUES (8,'أخي في الله','10','0','0');
+INSERT INTO [t324] VALUES (9,'أخي في الله','13','0','0');
+INSERT INTO [t324] VALUES (10,'أخي: إذا شغلتك الدنيا','14','0','0');
+INSERT INTO [t324] VALUES (11,'فأين الذي جمعته من المال','15','0','0');
+INSERT INTO [t324] VALUES (12,'أخي: تذكر ..','17','0','0');
