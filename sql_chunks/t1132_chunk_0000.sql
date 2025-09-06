d6159c1f3082d34c99a8d2219261c5c34abd24c5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:05 +0700

    Upload SQL chunk: t1132_chunk_0000.sql

---
+++ sql_chunks/t1132_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t1132
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t1132" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1132
+INSERT INTO [t1132] VALUES (1,'تمهيد','1','0','0');
+INSERT INTO [t1132] VALUES (2,'تعريف الحوار','9','0','0');
+INSERT INTO [t1132] VALUES (3,'تعريف الدين','11','0','0');
+INSERT INTO [t1132] VALUES (4,'تعريف الثقافة','13','0','0');
+INSERT INTO [t1132] VALUES (5,'هدف الحوار','15','0','0');
+INSERT INTO [t1132] VALUES (6,'القرآن الكريم وحوار الثقافات','17','0','0');
+INSERT INTO [t1132] VALUES (7,'شبهات حول جدوى الحوار','21','0','0');
+INSERT INTO [t1132] VALUES (8,'صدام الحضارات','26','0','0');
+INSERT INTO [t1132] VALUES (9,'قضايا لا يجوز الحوار فيها','38','0','0');
+INSERT INTO [t1132] VALUES (10,'مؤهلات المحاور المسلم','41','0','0');
+INSERT INTO [t1132] VALUES (11,'من آداب الحوار','53','0','0');
+INSERT INTO [t1132] VALUES (12,'خاتمة','58','0','0');
+INSERT INTO [t1132] VALUES (13,'المراجع','61','0','0');
