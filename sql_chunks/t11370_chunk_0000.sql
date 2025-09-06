9470380cf929430b96d3c8acf2f916970028bca3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:37 +0700

    Upload SQL chunk: t11370_chunk_0000.sql

---
+++ sql_chunks/t11370_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t11370
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t11370" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11370
+INSERT INTO [t11370] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11370] VALUES (2,'كتاب الطهارة','2','0','0');
+INSERT INTO [t11370] VALUES (3,'كتاب الصلاة','7','0','0');
+INSERT INTO [t11370] VALUES (4,'كتاب الزكاة','16','0','0');
+INSERT INTO [t11370] VALUES (5,'كتاب الصوم','19','0','0');
+INSERT INTO [t11370] VALUES (6,'كتاب الحج','21','0','0');
+INSERT INTO [t11370] VALUES (7,'كتاب البيوع وغيرها من المعاملات','23','0','0');
+INSERT INTO [t11370] VALUES (8,'كتاب الفرائض والوصايا','31','0','0');
+INSERT INTO [t11370] VALUES (9,'كتاب النكاح وما يتعلق به من الأحكام والقضايا','34','0','0');
+INSERT INTO [t11370] VALUES (10,'كتاب الجنايات','41','0','0');
+INSERT INTO [t11370] VALUES (11,'كتاب الحدود','43','0','0');
+INSERT INTO [t11370] VALUES (12,'كتاب الجهاد','46','0','0');
+INSERT INTO [t11370] VALUES (13,'كتاب الصيد والذبائح','49','0','0');
+INSERT INTO [t11370] VALUES (14,'كتاب السبق والرمي','51','0','0');
+INSERT INTO [t11370] VALUES (15,'كتاب الأيمان والنذور','52','0','0');
+INSERT INTO [t11370] VALUES (16,'كتاب الأقضية والشهادات','53','0','0');
+INSERT INTO [t11370] VALUES (17,'كتاب العتق','56','0','0');
