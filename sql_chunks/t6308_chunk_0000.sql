5e8cad1514ebd6ba36df266b323d05d8e2458478
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:37 +0700

    Upload SQL chunk: t6308_chunk_0000.sql

---
+++ sql_chunks/t6308_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6308
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6308" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6308
+INSERT INTO [t6308] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t6308] VALUES (2,'الرابع عشر الاحتقار ألقوا ما أنتم ملقون الخامس عشر التكوين كن فيكون السادس عشر الخبر فاصنع ما شئت وعكسه والوالدات يرضعن أولادهن','71','0','0');
+INSERT INTO [t6308] VALUES (3,'ومن شرطها أن تكون متعدية فلا عبرة بالقاصرة','114','0','0');
