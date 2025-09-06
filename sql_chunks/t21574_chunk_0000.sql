a23240e4b9e75ddd3c09bfcd1e26588bcdf91633
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:54 +0700

    Upload SQL chunk: t21574_chunk_0000.sql

---
+++ sql_chunks/t21574_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t21574
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t21574" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21574
+INSERT INTO [t21574] VALUES (1,'الأعشى','1','0','0');
+INSERT INTO [t21574] VALUES (2,'طرفة بن العبد','22','0','0');
+INSERT INTO [t21574] VALUES (3,'النابغة والمعلقة','67','0','0');
+INSERT INTO [t21574] VALUES (4,'عبيد بن الأبرص','83','0','0');
+INSERT INTO [t21574] VALUES (5,'امرؤ القيس','95','0','0');
+INSERT INTO [t21574] VALUES (6,'المعلقة','101','0','0');
+INSERT INTO [t21574] VALUES (7,'زهير بن أبي سلمى','162','0','0');
+INSERT INTO [t21574] VALUES (8,'المعلقة وزهير بن أبي سلمى','163','0','0');
+INSERT INTO [t21574] VALUES (9,'عنترة','197','0','0');
+INSERT INTO [t21574] VALUES (10,'معلقة عنترة','198','0','0');
+INSERT INTO [t21574] VALUES (11,'لبيد بن ربيعة العامري','245','0','0');
+INSERT INTO [t21574] VALUES (12,'لبيد والمعلقة','247','0','0');
+INSERT INTO [t21574] VALUES (13,'عمرو بن كلثوم والمعلقة','288','0','0');
+INSERT INTO [t21574] VALUES (14,'الحارث بن حلزة اليشكري','335','0','0');
+INSERT INTO [t21574] VALUES (15,'المعلقة','337','0','0');
