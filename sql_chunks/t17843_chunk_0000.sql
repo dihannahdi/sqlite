899356b878ea0d282545cd89de4404f545b0db56
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:00 +0700

    Upload SQL chunk: t17843_chunk_0000.sql

---
+++ sql_chunks/t17843_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t17843
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t17843" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17843
+INSERT INTO [t17843] VALUES (1,'باب (لا)','28','0','0');
+INSERT INTO [t17843] VALUES (2,'باب (لا) في النهي','31','0','0');
+INSERT INTO [t17843] VALUES (3,'باب (لا) مع الأسماء (المخفوضة)','32','0','0');
+INSERT INTO [t17843] VALUES (4,'باب (لا) مع حروف الجزاء','33','0','0');
+INSERT INTO [t17843] VALUES (5,'باب (لا) مع حروف الج','34','0','0');
+INSERT INTO [t17843] VALUES (6,'باب (إن) مع (ما)','36','0','0');
+INSERT INTO [t17843] VALUES (7,'باب تسمية حروف الجزاء','54','0','0');
