66804721fda9dc348aab75d6189813d138333bf6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:10 +0700

    Upload SQL chunk: t10903_chunk_0000.sql

---
+++ sql_chunks/t10903_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t10903
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t10903" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10903
+INSERT INTO [t10903] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10903] VALUES (2,'الوحي والأنسان قراءة تاريخية','4','0','0');
+INSERT INTO [t10903] VALUES (3,'المعرفة بين العقل والوحي','40','0','0');
+INSERT INTO [t10903] VALUES (4,'مفهوم الوحي','43','0','0');
+INSERT INTO [t10903] VALUES (5,'مفهوم العقل','50','0','0');
+INSERT INTO [t10903] VALUES (6,'مدارك العقول عالم الغيب وعالم الشهادة','62','0','0');
+INSERT INTO [t10903] VALUES (7,'وظيفة العقل في عالم الشهادة','64','0','0');
+INSERT INTO [t10903] VALUES (8,'علاقة العقل بعالم الغيب','74','0','0');
+INSERT INTO [t10903] VALUES (9,'مستويات الغيب','75','0','0');
+INSERT INTO [t10903] VALUES (10,'معرفة الغيب بين منهجين','79','0','0');
+INSERT INTO [t10903] VALUES (11,'بين العقل والوحي','91','0','0');
+INSERT INTO [t10903] VALUES (12,'الوحي والعلم تحديد المفهوم','104','0','0');
+INSERT INTO [t10903] VALUES (13,'الوحي والكون قراءة معرفية','132','0','0');
+INSERT INTO [t10903] VALUES (14,'الكون موضوع المعرفة','148','0','0');
+INSERT INTO [t10903] VALUES (15,'الوحي والواقع','178','0','0');
+INSERT INTO [t10903] VALUES (16,'الوحي ضرورة اجتماعية','198','0','0');
+INSERT INTO [t10903] VALUES (17,'الوحي حاجة نفسية','210','0','0');
+INSERT INTO [t10903] VALUES (18,'الوحي حاجة عقلية','217','0','0');
+INSERT INTO [t10903] VALUES (19,'الوحي حاجة إنسانية','226','0','0');
+INSERT INTO [t10903] VALUES (20,'فهرس','231','0','0');
