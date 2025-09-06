71f7caf86236f9d317c25c2e931f1f39b50a283b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:11 +0700

    Upload SQL chunk: t7052_chunk_0000.sql

---
+++ sql_chunks/t7052_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t7052
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t7052" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7052
+INSERT INTO [t7052] VALUES (1,'باب السيوف ونعوتها','1','0','0');
+INSERT INTO [t7052] VALUES (2,'باب الرماح والأسنة','3','0','0');
+INSERT INTO [t7052] VALUES (3,'باب ما يشبه الرماح','5','0','0');
+INSERT INTO [t7052] VALUES (4,'باب المتسلح من الرجال','5','0','0');
+INSERT INTO [t7052] VALUES (5,'باب القسي ونعوتها','6','0','0');
+INSERT INTO [t7052] VALUES (6,'باب نعوت ما في القوس','7','0','0');
+INSERT INTO [t7052] VALUES (7,'باب السهام ونعوتها','8','0','0');
+INSERT INTO [t7052] VALUES (8,'باب نعوت ما في السهم','8','0','0');
+INSERT INTO [t7052] VALUES (9,'باب ريش السهام','9','0','0');
+INSERT INTO [t7052] VALUES (10,'باب نصال السهام','10','0','0');
+INSERT INTO [t7052] VALUES (11,'باب نعوت السهم إذا رمي بها','11','0','0');
+INSERT INTO [t7052] VALUES (12,'باب عيون السهام','11','0','0');
+INSERT INTO [t7052] VALUES (13,'باب الدروع ونعوتها والبيض','12','0','0');
+INSERT INTO [t7052] VALUES (14,'باب أسماء حملة السلاح','14','0','0');
+INSERT INTO [t7052] VALUES (15,'باب أسماء الترس','14','0','0');
+INSERT INTO [t7052] VALUES (16,'باب أسماء الجعاب','15','0','0');
+INSERT INTO [t7052] VALUES (17,'باب ما يقاتل الرجل عنه ويحميه','15','0','0');
+INSERT INTO [t7052] VALUES (18,'باب الضرب بالسلاح وترك حمل السلاح','16','0','0');
+INSERT INTO [t7052] VALUES (19,'باب الطعن ونعوته والعرق','16','0','0');
+INSERT INTO [t7052] VALUES (20,'باب الضرب على الرأس','18','0','0');
+INSERT INTO [t7052] VALUES (21,'باب الضرب بالعصا','18','0','0');
+INSERT INTO [t7052] VALUES (22,'باب الضرب بالسوط','19','0','0');
+INSERT INTO [t7052] VALUES (23,'باب الضرب حتى يسقط صاحبه من ضربة واحدة','19','0','0');
+INSERT INTO [t7052] VALUES (24,'باب حمل الرجل صاحبه حتى يضرب به الأرض','20','0','0');
+INSERT INTO [t7052] VALUES (25,'باب مختلف من الضرب','21','0','0');
+INSERT INTO [t7052] VALUES (26,'باب موضع القتال','21','0','0');
+INSERT INTO [t7052] VALUES (27,'باب الضرب باليد والحجر','22','0','0');
+INSERT INTO [t7052] VALUES (28,'باب السهم لا يعلم من رماه','22','0','0');
+INSERT INTO [t7052] VALUES (29,'باب الحمل بالسيف','23','0','0');
+INSERT INTO [t7052] VALUES (30,'باب السكين وما فيها','23','0','0');
+INSERT INTO [t7052] VALUES (31,'باب إحداد الحديدة','24','0','0');
+INSERT INTO [t7052] VALUES (32,'باب التثقيل على الناس','24','0','0');
