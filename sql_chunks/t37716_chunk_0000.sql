4a4c0e3ba1a90a6d7af7bb569f9db03ccfe97598
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:15:32 +0700

    Upload SQL chunk: t37716_chunk_0000.sql

---
+++ sql_chunks/t37716_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t37716
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t37716" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37716
+INSERT INTO [t37716] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t37716] VALUES (2,'فصل منه','7','0','0');
+INSERT INTO [t37716] VALUES (3,'فصل منه','16','0','0');
+INSERT INTO [t37716] VALUES (4,'فصل منه','18','0','0');
+INSERT INTO [t37716] VALUES (5,'فصل منه','21','0','0');
+INSERT INTO [t37716] VALUES (6,'فصل منه','31','0','0');
+INSERT INTO [t37716] VALUES (7,'فصل منه','32','0','0');
+INSERT INTO [t37716] VALUES (8,'فصل منه','32','0','0');
+INSERT INTO [t37716] VALUES (9,'فصل منه','36','0','0');
+INSERT INTO [t37716] VALUES (10,'فصل منه','37','0','0');
