aa66bafa6245b86c85bb34e9907f969a23b9766f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:08 +0700

    Upload SQL chunk: t124335_chunk_0000.sql

---
+++ sql_chunks/t124335_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t124335
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t124335" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t124335
+INSERT INTO [t124335] VALUES (1,'١ - قصيدة امرئ القيس بن حجر','1','0','0');
+INSERT INTO [t124335] VALUES (2,'٢ - قصيدة طرفة بن العبد','112','0','0');
+INSERT INTO [t124335] VALUES (3,'٣ - قصيدة زهير بن أبي سلمى','230','0','0');
+INSERT INTO [t124335] VALUES (4,'٤ - قصيدة عنترة بن شداد','287','0','0');
+INSERT INTO [t124335] VALUES (5,'٥ - قصيدة عمرو بن كلثوم','287','0','0');
+INSERT INTO [t124335] VALUES (6,'٦ - قصيدة الحارث بن حلزة','423','0','0');
+INSERT INTO [t124335] VALUES (7,'٧ - قصيدة لبيد بن ربيعة','495','0','0');
