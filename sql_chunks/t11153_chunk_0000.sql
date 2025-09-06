072b08d345537bf42ed5d1812fa45cf71799dacb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:46 +0700

    Upload SQL chunk: t11153_chunk_0000.sql

---
+++ sql_chunks/t11153_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t11153
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t11153" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11153
+INSERT INTO [t11153] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11153] VALUES (2,'أولا: المتواتر','4','0','0');
+INSERT INTO [t11153] VALUES (3,'ما يفيده المتواتر:','4','2','0');
+INSERT INTO [t11153] VALUES (4,'ثانيا: الآحاد','7','0','0');
+INSERT INTO [t11153] VALUES (5,'ما يفيده خبر الآحاد:','7','4','0');
+INSERT INTO [t11153] VALUES (6,'مصادر ومراجع','31','0','0');
