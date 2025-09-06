b9b8d8486d0c633180f7d6d5043a3aa51acbd364
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:01 +0700

    Upload SQL chunk: t11251_chunk_0000.sql

---
+++ sql_chunks/t11251_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t11251
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t11251" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11251
+INSERT INTO [t11251] VALUES (1,'[باب]','1','0','0');
+INSERT INTO [t11251] VALUES (2,'تعريف الصلاة','1','1','0');
+INSERT INTO [t11251] VALUES (3,'وجوب الصلاة وأدلة ذلك','7','1','0');
+INSERT INTO [t11251] VALUES (4,'مسألة: فمن جحد وجوبها بجهله','25','1','0');
+INSERT INTO [t11251] VALUES (5,'مسألة: ولا يحل تأخيرها عن وقتها إلا لناو جمعها أو مشتغل عنها بشرطها','27','1','0');
+INSERT INTO [t11251] VALUES (6,'مسألة: فإن تركها تهاونا أستتيب ثلاثا فإن تاب وإلا قتل','34','1','0');
+INSERT INTO [t11251] VALUES (7,'باب الأذان والإقامة','69','0','0');
+INSERT INTO [t11251] VALUES (8,'باب شروط الصلاة','117','0','0');
+INSERT INTO [t11251] VALUES (9,'الشرط الأول: الطهارة','117','8','0');
+INSERT INTO [t11251] VALUES (10,'الشرط الثاني: الوقت','119','8','0');
+INSERT INTO [t11251] VALUES (11,'الشرط الثالث: ستر العورة','228','8','0');
+INSERT INTO [t11251] VALUES (12,'الشرط الرابع: الطهارة من النجاسة','373','8','0');
+INSERT INTO [t11251] VALUES (13,'الشرط الخامس: استقبال القبلة','494','8','0');
+INSERT INTO [t11251] VALUES (14,'الشرط السادس: النية','549','8','0');
+INSERT INTO [t11251] VALUES (15,'باب أدب المشي إلى الصلاة','569','0','0');
