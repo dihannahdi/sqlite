7aef0f332066af81a0651648b3b7d146bfc3cc81
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:13 +0700

    Upload SQL chunk: t8811_chunk_0000.sql

---
+++ sql_chunks/t8811_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t8811
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t8811" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8811
+INSERT INTO [t8811] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8811] VALUES (2,'العبادة في اللغة','1','1','0');
+INSERT INTO [t8811] VALUES (3,'العبادة في الشرع','3','1','0');
+INSERT INTO [t8811] VALUES (4,'أثر العبادات الإسلامية في تضامن المسلمين إجمالا','8','0','0');
+INSERT INTO [t8811] VALUES (5,'مدخل','8','4','0');
+INSERT INTO [t8811] VALUES (6,'الصلاة','14','4','0');
+INSERT INTO [t8811] VALUES (7,'الزكاة','20','4','0');
+INSERT INTO [t8811] VALUES (8,'الصيام','25','4','0');
+INSERT INTO [t8811] VALUES (9,'الحج','28','4','0');
