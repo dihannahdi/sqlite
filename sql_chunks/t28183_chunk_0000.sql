8a21afff29ce93d85d05b4f197055a39db087155
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:34 +0700

    Upload SQL chunk: t28183_chunk_0000.sql

---
+++ sql_chunks/t28183_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t28183
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t28183" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t28183
+INSERT INTO [t28183] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t28183] VALUES (2,'سماعات النسخة','57','0','0');
+INSERT INTO [t28183] VALUES (3,'سماع الصفحة الأخيرة ص ٥٠','59','2','0');
