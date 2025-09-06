f1dbd67b345a08c080b647e90e0adc164b02452e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:15 +0700

    Upload SQL chunk: t3727_chunk_0000.sql

---
+++ sql_chunks/t3727_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t3727
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t3727" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3727
+INSERT INTO [t3727] VALUES (1,'المسألة الأولى','1','0','0');
+INSERT INTO [t3727] VALUES (2,'المسألة الثانية والثالثة','2','0','0');
+INSERT INTO [t3727] VALUES (3,'المسألة الرابعة','2','0','0');
+INSERT INTO [t3727] VALUES (4,'المسألة الخامسة','3','0','0');
+INSERT INTO [t3727] VALUES (5,'المسألة السادسة','4','0','0');
+INSERT INTO [t3727] VALUES (6,'المسألة السابعة والثامنة والتاسعة','4','0','0');
+INSERT INTO [t3727] VALUES (7,'المسألة العاشرة','5','0','0');
+INSERT INTO [t3727] VALUES (8,'المسألة الحادية عشرة','5','0','0');
+INSERT INTO [t3727] VALUES (9,'المسألة الثانية عشرة','6','0','0');
+INSERT INTO [t3727] VALUES (10,'المسألة الثالثة عشر','6','0','0');
+INSERT INTO [t3727] VALUES (11,'المسألة الرابعة عشر','6','0','0');
+INSERT INTO [t3727] VALUES (12,'المسألة الخامسة عشر','7','0','0');
+INSERT INTO [t3727] VALUES (13,'المسألة السادسة عشر','7','0','0');
+INSERT INTO [t3727] VALUES (14,'المسألة السابعة عشر','8','0','0');
+INSERT INTO [t3727] VALUES (15,'المسألة الثامنة عشر','8','0','0');
+INSERT INTO [t3727] VALUES (16,'المسألة التاسعة عشر','9','0','0');
