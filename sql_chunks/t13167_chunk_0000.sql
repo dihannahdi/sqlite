8cc17040225c1889eb40903b1e77d795c559725e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:36 +0700

    Upload SQL chunk: t13167_chunk_0000.sql

---
+++ sql_chunks/t13167_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t13167
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t13167" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13167
+INSERT INTO [t13167] VALUES (1,'وسفيان بن عيينة عن هشام بن عروة','14','0','0');
+INSERT INTO [t13167] VALUES (2,'حديث جرير بن عبد الحميد عن هشام بن عروة','27','0','0');
+INSERT INTO [t13167] VALUES (3,'عبدة عن هشام','42','0','0');
