113ec33e5bc861b4e23ff70d388ccc3b3430c447
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:51 +0700

    Upload SQL chunk: t8241_chunk_0000.sql

---
+++ sql_chunks/t8241_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t8241
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t8241" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8241
+INSERT INTO [t8241] VALUES (1,'باب ذم المراء','1','0','0');
+INSERT INTO [t8241] VALUES (2,'باب ما جاء في ذم التقعر في الكلام','9','0','0');
+INSERT INTO [t8241] VALUES (3,'باب ذم الخصومات','16','0','0');
+INSERT INTO [t8241] VALUES (4,'باب الغيبة وذمها','26','0','0');
+INSERT INTO [t8241] VALUES (5,'تفسير الغيبة','73','0','0');
+INSERT INTO [t8241] VALUES (6,'باب الغيبة التي يحل لصاحبها الكلام بها','84','0','0');
+INSERT INTO [t8241] VALUES (7,'باب ذب المسلم عن عرض أخيه','105','0','0');
+INSERT INTO [t8241] VALUES (8,'باب ما جاء في ذم النميمة','119','0','0');
+INSERT INTO [t8241] VALUES (9,'باب ذم ذي اللسانين','142','0','0');
+INSERT INTO [t8241] VALUES (10,'باب ما نهي عنه العباد من أن يسخر بعضهم من بعض','151','0','0');
+INSERT INTO [t8241] VALUES (11,'باب كفارة الاغتياب','161','0','0');
