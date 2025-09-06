d41b21f8486ad0924fbfa95c9405ff5027638836
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:06 +0700

    Upload SQL chunk: t854_chunk_0000.sql

---
+++ sql_chunks/t854_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t854
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t854" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t854
+INSERT INTO [t854] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t854] VALUES (2,'(١) ما يجب على المريض','10','0','0');
+INSERT INTO [t854] VALUES (3,'(٢) تلقين المحتضر','17','0','0');
+INSERT INTO [t854] VALUES (4,'(٣) ما على الحاضرين بعد موته','19','0','0');
+INSERT INTO [t854] VALUES (5,'(٤) ما يجوز للحاضرين وغيرهم','27','0','0');
+INSERT INTO [t854] VALUES (6,'(٥) ما يجب على أقارب الميت','29','0','0');
+INSERT INTO [t854] VALUES (7,'(٦) ما يحرم على أقارب الميت','34','0','0');
+INSERT INTO [t854] VALUES (8,'(٧) النعي الجائز','39','0','0');
+INSERT INTO [t854] VALUES (9,'(٨) علامات حسن الخاتمة','41','0','0');
+INSERT INTO [t854] VALUES (10,'(٩) ثناء الناس على الميت','51','0','0');
+INSERT INTO [t854] VALUES (11,'الوفاة عند الكسوف','53','10','0');
+INSERT INTO [t854] VALUES (12,'(١٠) غسل الميت','54','0','0');
+INSERT INTO [t854] VALUES (13,'(١١) تكفين الميت','64','0','0');
+INSERT INTO [t854] VALUES (14,'(١٢) حمل الجنازة واتباعها','73','0','0');
+INSERT INTO [t854] VALUES (15,'(١٣) الصلاة على الجنازة','86','0','0');
+INSERT INTO [t854] VALUES (16,'(١٤) الدفن وتوابعه','139','0','0');
+INSERT INTO [t854] VALUES (17,'(١٥) التعزية','169','0','0');
+INSERT INTO [t854] VALUES (18,'ما ينتفع به الميت','175','17','0');
+INSERT INTO [t854] VALUES (19,'زيارة القبور','185','17','0');
+INSERT INTO [t854] VALUES (20,'ما يحرم عند القبور','210','17','0');
+INSERT INTO [t854] VALUES (21,'بدع الجنائز','246','0','0');
+INSERT INTO [t854] VALUES (22,'قبل الوفاة','249','21','0');
+INSERT INTO [t854] VALUES (23,'بعد الوفاة','250','21','0');
+INSERT INTO [t854] VALUES (24,'غسل الميت','253','21','0');
+INSERT INTO [t854] VALUES (25,'الكفن والخروج بالجنازة','254','21','0');
+INSERT INTO [t854] VALUES (26,'الصلاة عليها','258','21','0');
+INSERT INTO [t854] VALUES (27,'الدفن وتوابعه','259','21','0');
+INSERT INTO [t854] VALUES (28,'التعزية وملحقاتها','261','21','0');
+INSERT INTO [t854] VALUES (29,'زيارة القبور','264','21','0');
+INSERT INTO [t854] VALUES (30,'خاتمة الطبع','274','0','0');
