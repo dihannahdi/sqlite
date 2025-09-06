9a39b1cf334a9a0a0978ec47635106599c0ed82a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:48:45 +0700

    Upload SQL chunk: t23745_chunk_0000.sql

---
+++ sql_chunks/t23745_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t23745
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t23745" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23745
+INSERT INTO [t23745] VALUES (1,'عمدة الفقه - كتاب الصيام (١)','2','0','0');
+INSERT INTO [t23745] VALUES (2,'عمدة الفقه - كتاب الصيام (٢)','21','0','0');
+INSERT INTO [t23745] VALUES (3,'عمدة الفقه - كتاب الصيام (٣)','28','0','0');
