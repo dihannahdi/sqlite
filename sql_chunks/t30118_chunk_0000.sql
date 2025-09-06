feece591a20e2c9029c4dd8619f59b4148a9355c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:48 +0700

    Upload SQL chunk: t30118_chunk_0000.sql

---
+++ sql_chunks/t30118_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t30118
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t30118" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30118
+INSERT INTO [t30118] VALUES (1,'الجزء الرابع والثلاثون من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30118] VALUES (2,'من فوائد أبي الحسن العتيقي','2','0','0');
+INSERT INTO [t30118] VALUES (3,'من فوائد أبي محمد الجوهري','4','0','0');
+INSERT INTO [t30118] VALUES (4,'من حديث عبد الله بن أحمد بن حنبل عن أبيه','6','0','0');
+INSERT INTO [t30118] VALUES (5,'من حديث أبي طالب بن حمامة','13','0','0');
+INSERT INTO [t30118] VALUES (6,'من حديث الحسين بن الكميت','17','0','0');
+INSERT INTO [t30118] VALUES (7,'من فوائد وهب بن منبه الغزنوي','20','0','0');
+INSERT INTO [t30118] VALUES (8,'حديث واحد','21','0','0');
+INSERT INTO [t30118] VALUES (9,'من حديث ابن شاهين','23','0','0');
+INSERT INTO [t30118] VALUES (10,'من فوائد ابن السلماسي','26','0','0');
+INSERT INTO [t30118] VALUES (11,'من فوائد أبي محمد السراج','27','0','0');
+INSERT INTO [t30118] VALUES (12,'من حديث أبي عمرو بن حكيم','28','0','0');
+INSERT INTO [t30118] VALUES (13,'من حديث أبي بكر القطيعي','32','0','0');
+INSERT INTO [t30118] VALUES (14,'حديث واحد','35','0','0');
+INSERT INTO [t30118] VALUES (15,'حديث واحد','37','0','0');
+INSERT INTO [t30118] VALUES (16,'من فوائد ابن مالك القطيعي','39','0','0');
+INSERT INTO [t30118] VALUES (17,'من فوائد العتيقي','43','0','0');
