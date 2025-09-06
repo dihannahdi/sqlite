d4b26f559c6c9d3ec346c4126aab49442bd6f2cd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:12 +0700

    Upload SQL chunk: t17738_chunk_0000.sql

---
+++ sql_chunks/t17738_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t17738
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t17738" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17738
+INSERT INTO [t17738] VALUES (1,'الرسالة','2','0','0');
+INSERT INTO [t17738] VALUES (2,'إجازة الشيخ الدسوقي للشيخ جمال الدين القاسمي','12','0','0');
