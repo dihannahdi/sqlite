601d37a99fc416b47bc6b91e5f9ada2f309ac0bd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:53 +0700

    Upload SQL chunk: t30500_chunk_0000.sql

---
+++ sql_chunks/t30500_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30500
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30500" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30500
+INSERT INTO [t30500] VALUES (1,'الجزء الحادي عشر من الفوائد المنتقاة الحسان الصحاح والغرائب','1','0','0');
