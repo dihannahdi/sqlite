1100a9436ddac73ac7166acd93e468658e8ccd7e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:59 +0700

    Upload SQL chunk: t5208_chunk_0000.sql

---
+++ sql_chunks/t5208_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5208
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5208" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5208
+INSERT INTO [t5208] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t5208] VALUES (2,'شرح الصدور بتحريم رفع القبور','7','0','0');
