00e68ceeca61f9eaba45df739af8d64d2a9c904a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:24:01 +0700

    Upload SQL chunk: t151131_chunk_0000.sql

---
+++ sql_chunks/t151131_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t151131
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t151131" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t151131
+INSERT INTO [t151131] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t151131] VALUES (2,'فصل: [في وصف حال المفقود الذي يجوز أن تتزوج زوجته]','14','0','0');
+INSERT INTO [t151131] VALUES (3,'فصل: [متى يفرق بين الغائب وامرأته؟]','18','0','0');
