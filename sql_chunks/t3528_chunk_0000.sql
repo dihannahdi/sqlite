32c3d43a1d8c1698b276ec2550451c7074c184c1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:24 +0700

    Upload SQL chunk: t3528_chunk_0000.sql

---
+++ sql_chunks/t3528_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t3528
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t3528" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3528
+INSERT INTO [t3528] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t3528] VALUES (2,'الفصل الأول: التعريف بلقمان الحكيم','7','0','0');
+INSERT INTO [t3528] VALUES (3,'اسمه ونسبه','7','2','0');
+INSERT INTO [t3528] VALUES (4,'أوصافه','8','2','0');
+INSERT INTO [t3528] VALUES (5,'صفاته','9','2','0');
+INSERT INTO [t3528] VALUES (6,'مهنته','10','2','0');
+INSERT INTO [t3528] VALUES (7,'هل كان لقمان نبيا أم حكيما؟','12','2','0');
+INSERT INTO [t3528] VALUES (8,'الفصل الثاني','15','0','0');
+INSERT INTO [t3528] VALUES (9,'المبحث الأول: لقمان لإبنه','15','8','0');
+INSERT INTO [t3528] VALUES (10,'المبحث الثاني: الجوانب التربوية لوصايا لقمان','18','8','0');
+INSERT INTO [t3528] VALUES (11,'المبحث الثالث: معالم أصول التربية الإسلامية من خلال وصايا لقمان','45','8','0');
+INSERT INTO [t3528] VALUES (12,'الخاتمة','65','0','0');
