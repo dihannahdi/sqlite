55d23c31be41e8e42b8071fa7bb6e78f27cabaa0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:37:45 +0700

    Upload SQL chunk: t20986_chunk_0000.sql

---
+++ sql_chunks/t20986_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t20986
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t20986" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t20986
+INSERT INTO [t20986] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t20986] VALUES (2,'باب البيان الأول وهو الاعتبار','18','0','0');
+INSERT INTO [t20986] VALUES (3,'باب البيان الثاني وهو الاعتقاد','39','0','0');
+INSERT INTO [t20986] VALUES (4,'باب البيان الثالث وهو العبارة','45','0','0');
+INSERT INTO [t20986] VALUES (5,'باب البيان الرابع وهو الكتاب','207','0','0');
+INSERT INTO [t20986] VALUES (6,'خاتمة الكتاب','315','0','0');
