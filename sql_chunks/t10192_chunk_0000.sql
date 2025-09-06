d933445cbdacbb454c5b862d4e3b6dd3586311a3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:14 +0700

    Upload SQL chunk: t10192_chunk_0000.sql

---
+++ sql_chunks/t10192_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t10192
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t10192" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10192
+INSERT INTO [t10192] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10192] VALUES (2,'الإقتصاد في نظام الإشتراكية','3','0','0');
+INSERT INTO [t10192] VALUES (3,'المنهج الإسلامي','4','0','0');
+INSERT INTO [t10192] VALUES (4,'خصائص ومقومات الإقتصاد الإسلامي','6','0','0');
+INSERT INTO [t10192] VALUES (5,'نظرة الإسلام للمال','13','0','0');
+INSERT INTO [t10192] VALUES (6,'أوجه صرف المال','16','0','0');
+INSERT INTO [t10192] VALUES (7,'مزية الإقتصاد الإسلامي','18','0','0');
