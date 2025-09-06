e5f2c446c0dffafbffba5068d4abbb76fc804d1f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:28 +0700

    Upload SQL chunk: t12067_chunk_0000.sql

---
+++ sql_chunks/t12067_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t12067
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t12067" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12067
+INSERT INTO [t12067] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12067] VALUES (2,'تمهيد','7','0','0');
+INSERT INTO [t12067] VALUES (3,'عبد الله بن المبارك','11','0','0');
+INSERT INTO [t12067] VALUES (4,'من غرر كلامه وشعره','36','0','0');
+INSERT INTO [t12067] VALUES (5,'هذا والله السلطان لا سلطان هارون الرشيد','43','0','0');
+INSERT INTO [t12067] VALUES (6,'دروس وعبر','46','0','0');
+INSERT INTO [t12067] VALUES (7,'مصادر ومراجع','49','0','0');
