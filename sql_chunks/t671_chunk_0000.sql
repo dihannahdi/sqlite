a02fb8da0798bcf24e6a8ffd77598f4337fcf664
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:11 +0700

    Upload SQL chunk: t671_chunk_0000.sql

---
+++ sql_chunks/t671_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t671
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t671" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t671
+INSERT INTO [t671] VALUES (1,'المعمرون والوصايا','1','0','0');
+INSERT INTO [t671] VALUES (2,'قال الشيخ أبو حاتم سهل بن عثمان السجستاني','1','0','0');
