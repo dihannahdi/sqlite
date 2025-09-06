b64a879ed9efe37082f2ae5336e15c300c4eac53
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:37 +0700

    Upload SQL chunk: t6769_chunk_0000.sql

---
+++ sql_chunks/t6769_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6769
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6769" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6769
+INSERT INTO [t6769] VALUES (1,'أخبرنا الشيخ الإمام الزاهد','1','0','0');
+INSERT INTO [t6769] VALUES (2,'تم كتاب ذم الثقلاء','62','0','0');
