d9fe4b065a7a32208ba7031c896b2377173b032d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:11 +0700

    Upload SQL chunk: t8801_chunk_0000.sql

---
+++ sql_chunks/t8801_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t8801
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t8801" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8801
+INSERT INTO [t8801] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8801] VALUES (2,'باب: ابن أبي شيبة','5','0','0');
+INSERT INTO [t8801] VALUES (3,'باب: منهجه في كتاب التاريخ','11','0','0');
+INSERT INTO [t8801] VALUES (4,'باب: فروع التاريخ عند ابن أبي شيبة','37','0','0');
+INSERT INTO [t8801] VALUES (5,'الخاتمة','38','0','0');
