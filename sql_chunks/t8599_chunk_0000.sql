a57b0ef7118eed7c8c3731df2278a0816e54b94b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:39 +0700

    Upload SQL chunk: t8599_chunk_0000.sql

---
+++ sql_chunks/t8599_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t8599
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t8599" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8599
+INSERT INTO [t8599] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8599] VALUES (2,'فتاوى علاج المسحور عند المشعوذ','11','0','0');
+INSERT INTO [t8599] VALUES (3,'حكم تعليق التمائم والحجب','13','0','0');
