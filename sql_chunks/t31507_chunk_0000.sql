182dadaa630a1be77037baf2260ee1affcd62bba
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:36 +0700

    Upload SQL chunk: t31507_chunk_0000.sql

---
+++ sql_chunks/t31507_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t31507
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t31507" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31507
+INSERT INTO [t31507] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t31507] VALUES (2,'مفهوم القناعة','2','0','0');
+INSERT INTO [t31507] VALUES (3,'فوائد القناعة','5','0','0');
+INSERT INTO [t31507] VALUES (4,'صور من قناعة النبي صلى الله عليه وسلم','12','0','0');
+INSERT INTO [t31507] VALUES (5,'أولا قناعته صلى الله عليه وسلم في أكله','13','4','0');
+INSERT INTO [t31507] VALUES (6,'ثانيا قناعته صلى الله عليه وسلم في فراشه','14','4','0');
+INSERT INTO [t31507] VALUES (7,'ثالثا تربيته صلى الله عليه وسلم أهله على القناعة','15','4','0');
+INSERT INTO [t31507] VALUES (8,'صورة من قناعة الصحابة والسلف الصالح','17','0','0');
+INSERT INTO [t31507] VALUES (9,'أسباب تحول دون القناعة','20','0','0');
+INSERT INTO [t31507] VALUES (10,'السبيل إلى القناعة','22','0','0');
