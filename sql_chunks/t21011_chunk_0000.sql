5fc8778afae0fbbffbd88af1fd9a3698d2c27640
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:37:51 +0700

    Upload SQL chunk: t21011_chunk_0000.sql

---
+++ sql_chunks/t21011_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t21011
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t21011" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21011
+INSERT INTO [t21011] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t21011] VALUES (2,'مقارنة بين الدعوة والقتال','5','0','0');
+INSERT INTO [t21011] VALUES (3,'مقارنة بين جهد الدعوة وجهد العبادة','39','0','0');
+INSERT INTO [t21011] VALUES (4,'مقارنة بين فقه الدعوة وفقه التعليم','94','0','0');
+INSERT INTO [t21011] VALUES (5,'مقارنة بين جهد الحق وجهد الباطل','109','0','0');
+INSERT INTO [t21011] VALUES (6,'مقارنة بين الحاجة والمقصد','134','0','0');
+INSERT INTO [t21011] VALUES (7,'مقارنة بين جهد الداعي وجهد السياسي','153','0','0');
+INSERT INTO [t21011] VALUES (8,'من صفات اليهود','159','0','0');
+INSERT INTO [t21011] VALUES (9,'اليقين علي أنه لا إله إلا الله','182','0','0');
+INSERT INTO [t21011] VALUES (10,'البيئة الإيمانية','219','0','0');
+INSERT INTO [t21011] VALUES (11,'خواطر إيمانية حول سورة الفاتحة','225','0','0');
+INSERT INTO [t21011] VALUES (12,'مذاكرة في جهد الدعوة إلي الله','260','0','0');
+INSERT INTO [t21011] VALUES (13,'وختاما','296','0','0');
+INSERT INTO [t21011] VALUES (14,'المراجع','297','0','0');
