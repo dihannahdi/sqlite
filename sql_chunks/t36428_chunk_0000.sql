3c7f1f9fb85f09a3ad3de934f05dcee2660e0588
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:15 +0700

    Upload SQL chunk: t36428_chunk_0000.sql

---
+++ sql_chunks/t36428_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t36428
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t36428" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36428
+INSERT INTO [t36428] VALUES (1,'الحمد لله تعالى الذي علا في سماواته','1','0','0');
+INSERT INTO [t36428] VALUES (2,'أخي في الله','3','1','0');
+INSERT INTO [t36428] VALUES (3,'أخي المسلم','5','1','0');
+INSERT INTO [t36428] VALUES (4,'أخي في الله','5','1','0');
+INSERT INTO [t36428] VALUES (5,'ومرحبا بدار البقاء ..','6','1','0');
+INSERT INTO [t36428] VALUES (6,'أخي في الله','10','1','0');
+INSERT INTO [t36428] VALUES (7,'أخي: (إنه هادم اللذات! )','10','1','0');
+INSERT INTO [t36428] VALUES (8,'قال الربيع بن برة','13','1','0');
+INSERT INTO [t36428] VALUES (9,'أخي: إذا شغلتك الدنيا','14','1','0');
+INSERT INTO [t36428] VALUES (10,'أخي في الله','15','1','0');
+INSERT INTO [t36428] VALUES (11,'أخي: كن على حذر','17','1','0');
