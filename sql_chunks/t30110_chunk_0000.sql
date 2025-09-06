73b9a35e16553d1804d43f9cb6cdc5cb42d69e92
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:44 +0700

    Upload SQL chunk: t30110_chunk_0000.sql

---
+++ sql_chunks/t30110_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t30110
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t30110" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30110
+INSERT INTO [t30110] VALUES (1,'الجزء الحادي والعشرون من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30110] VALUES (2,'من حديث أبي بكر الجوهري','2','0','0');
+INSERT INTO [t30110] VALUES (3,'من فوائد أبي الحسين بن بشران','12','0','0');
+INSERT INTO [t30110] VALUES (4,'من فوائد أبي سعد بن الخراساني','14','0','0');
+INSERT INTO [t30110] VALUES (5,'من حديث بن بشران','15','0','0');
+INSERT INTO [t30110] VALUES (6,'من حديث زياد بن أيوب','18','0','0');
+INSERT INTO [t30110] VALUES (7,'من حديث إدريس بن عبد الكريم الأزدي','20','0','0');
+INSERT INTO [t30110] VALUES (8,'من حديث أبي القاسم التنوخي','23','0','0');
+INSERT INTO [t30110] VALUES (9,'من حديث أبي الحسين بن بشران','44','0','0');
+INSERT INTO [t30110] VALUES (10,'من حديث ابن قشيش','47','0','0');
+INSERT INTO [t30110] VALUES (11,'من حديث دعلج','50','0','0');
+INSERT INTO [t30110] VALUES (12,'من حديث أبي القاسم بن بشران عن أبي بكر','58','0','0');
