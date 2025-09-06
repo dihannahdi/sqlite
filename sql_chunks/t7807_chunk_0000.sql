f8df362ba96affefab22c74c1727736744af6421
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:13 +0700

    Upload SQL chunk: t7807_chunk_0000.sql

---
+++ sql_chunks/t7807_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t7807
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t7807" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7807
+INSERT INTO [t7807] VALUES (1,'١ - المقصود بالصيام','4','0','0');
+INSERT INTO [t7807] VALUES (2,'٢ - بما يثبت الشهر','5','0','0');
+INSERT INTO [t7807] VALUES (3,'٣ - اختلاف المطالع','5','0','0');
+INSERT INTO [t7807] VALUES (4,'٤ - حكم من رأى الهلال وحده','7','0','0');
+INSERT INTO [t7807] VALUES (5,'٥ - أركان الصوم','10','0','0');
+INSERT INTO [t7807] VALUES (6,'٦ - على من يجب الصوم','13','0','0');
+INSERT INTO [t7807] VALUES (7,'٧ - صيام الصبي','14','0','0');
+INSERT INTO [t7807] VALUES (8,'٨ - أقسام الصائمين','15','0','0');
+INSERT INTO [t7807] VALUES (9,'الحامل والمرضع','16','8','0');
+INSERT INTO [t7807] VALUES (10,'ما هو السفر المبيح للفطر','19','8','0');
+INSERT INTO [t7807] VALUES (11,'٩ - آداب الصيام','21','0','0');
+INSERT INTO [t7807] VALUES (12,'١٠ - مباحات الصيام','27','0','0');
+INSERT INTO [t7807] VALUES (13,'١١ - مبطلات الصيام','34','0','0');
+INSERT INTO [t7807] VALUES (14,'١٢ - قضاء رمضان','40','0','0');
+INSERT INTO [t7807] VALUES (15,'١٣ - من مات وعليه صيام','42','0','0');
+INSERT INTO [t7807] VALUES (16,'١٤ - التقدير في البلاد التي يطول نهارها ويقصر ليلها','43','0','0');
+INSERT INTO [t7807] VALUES (17,'١٥ - ليلة القدر','44','0','0');
+INSERT INTO [t7807] VALUES (18,'١٦ - قيام رمضان أو صلاة التراويح','46','0','0');
+INSERT INTO [t7807] VALUES (19,'١٧ - زكاة الفطر','49','0','0');
+INSERT INTO [t7807] VALUES (20,'وقت وجوبها ووقت إخراجها','51','19','0');
+INSERT INTO [t7807] VALUES (21,'تنبيهات','53','0','0');
