f1e5d8662b03e70e65ce8a9d3eedd208141718a2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:17:54 +0700

    Upload SQL chunk: t38209_chunk_0000.sql

---
+++ sql_chunks/t38209_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t38209
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t38209" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t38209
+INSERT INTO [t38209] VALUES (1,'تصدير','5','0','0');
+INSERT INTO [t38209] VALUES (2,'القسم الأول: الطفل','9','0','0');
+INSERT INTO [t38209] VALUES (3,'مقدمة','10','2','0');
+INSERT INTO [t38209] VALUES (4,'القسم الثاني: الطالب','194','0','0');
+INSERT INTO [t38209] VALUES (5,'مقدمة','195','4','0');
