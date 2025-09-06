223db315431e7eea31bf4eb40098e02dd9642bb4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:24 +0700

    Upload SQL chunk: t95563_chunk_0000.sql

---
+++ sql_chunks/t95563_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t95563
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t95563" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95563
+INSERT INTO [t95563] VALUES (1,'مقدمة المحقق','2','0','0');
+INSERT INTO [t95563] VALUES (2,'النسخة المطبوعة','4','0','0');
+INSERT INTO [t95563] VALUES (3,'النص','5','0','0');
