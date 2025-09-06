745167e06a992fa204063479930fafe7806c2000
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:29:20 +0700

    Upload SQL chunk: t17324_chunk_0000.sql

---
+++ sql_chunks/t17324_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t17324
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t17324" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17324
+INSERT INTO [t17324] VALUES (1,'المقدمة','5','0','0');
+INSERT INTO [t17324] VALUES (2,'الخاتمة','60','0','0');
