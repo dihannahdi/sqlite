bba90b8d3b15813cdf1c6f8dbc1f40561d7a9bc8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:41 +0700

    Upload SQL chunk: t5075_chunk_0000.sql

---
+++ sql_chunks/t5075_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5075
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5075" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5075
+INSERT INTO [t5075] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t5075] VALUES (2,'محمد بن عبد الوهاب داعية التوحيد والتجديد في العصر الحديث','6','0','0');
