02ee6e972d6b1a452adcf2708f158cbc8521d5ad
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:18 +0700

    Upload SQL chunk: t5789_chunk_0000.sql

---
+++ sql_chunks/t5789_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t5789
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t5789" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5789
+INSERT INTO [t5789] VALUES (1,'سنة ثلاث وستين وأربعمئة','1','0','0');
+INSERT INTO [t5789] VALUES (2,'سنة أربع وستين وأربعمئة','5','0','0');
+INSERT INTO [t5789] VALUES (3,'سنة خمس وستين وأربعمئة','7','0','0');
+INSERT INTO [t5789] VALUES (4,'سنة ست وستين وأربعمئة','8','0','0');
+INSERT INTO [t5789] VALUES (5,'سنة سبع وستين وأربعمئة','13','0','0');
+INSERT INTO [t5789] VALUES (6,'سنة ثمان وستين وأربعمئة','18','0','0');
+INSERT INTO [t5789] VALUES (7,'سنة تسع وستين وأربعمئة','21','0','0');
+INSERT INTO [t5789] VALUES (8,'سنة سبعين وأربعمئة','23','0','0');
+INSERT INTO [t5789] VALUES (9,'سنة إحدى وسبعين وأربعمئة','25','0','0');
+INSERT INTO [t5789] VALUES (10,'سنة اثنتين وسبعين وأربعمئة','26','0','0');
+INSERT INTO [t5789] VALUES (11,'سنة ثلاث وسبعين وأربعمئة','26','0','0');
+INSERT INTO [t5789] VALUES (12,'سنة أربع وسبعين وأربعمئة','27','0','0');
+INSERT INTO [t5789] VALUES (13,'سنة ست وسبعين وأربعمئة','29','0','0');
+INSERT INTO [t5789] VALUES (14,'سنة سبع وسبعين وأربعمئة','30','0','0');
+INSERT INTO [t5789] VALUES (15,'سنة ثمان وسبعين وأربعمئة','31','0','0');
+INSERT INTO [t5789] VALUES (16,'سنة تسع وسبعين وأربعمئة','32','0','0');
+INSERT INTO [t5789] VALUES (17,'سنة ثمانين وأربعمئة','32','0','0');
+INSERT INTO [t5789] VALUES (18,'سنة اثنتين وثماني وأربعمئة','33','0','0');
+INSERT INTO [t5789] VALUES (19,'سنة ثلاث وثمانين وأربعمئة','33','0','0');
+INSERT INTO [t5789] VALUES (20,'سنة أبع وثمانين وأربعمئة','34','0','0');
+INSERT INTO [t5789] VALUES (21,'سنة خمس وثمانين وأربعمئة','35','0','0');
