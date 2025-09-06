f9b892ee22f54486f3f7189b38e0e9ae73eb4c03
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:33 +0700

    Upload SQL chunk: t6659_chunk_0000.sql

---
+++ sql_chunks/t6659_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t6659
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t6659" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6659
+INSERT INTO [t6659] VALUES (1,'الفصل الأول','1','0','0');
+INSERT INTO [t6659] VALUES (2,'الفصل الثاني','5','0','0');
+INSERT INTO [t6659] VALUES (3,'الفصل الثالث','15','0','0');
+INSERT INTO [t6659] VALUES (4,'الفصل الرابع','21','0','0');
+INSERT INTO [t6659] VALUES (5,'الفصل الخامس','27','0','0');
+INSERT INTO [t6659] VALUES (6,'الفصل السادس','30','0','0');
+INSERT INTO [t6659] VALUES (7,'الفصل السابع','33','0','0');
+INSERT INTO [t6659] VALUES (8,'الفصل الثامن','38','0','0');
+INSERT INTO [t6659] VALUES (9,'الفصل التاسع','41','0','0');
+INSERT INTO [t6659] VALUES (10,'في ذكر كرمه رضي الله عنه','48','0','0');
+INSERT INTO [t6659] VALUES (11,'الفصل الحادي عشر','52','0','0');
+INSERT INTO [t6659] VALUES (12,'في ذكر قوة قلبه وشجاعته','52','0','0');
+INSERT INTO [t6659] VALUES (13,'الفصل الثاني عشر','60','0','0');
+INSERT INTO [t6659] VALUES (14,'الفصل الثالث عشر','63','0','0');
+INSERT INTO [t6659] VALUES (15,'الفصل الرابع عشر','66','0','0');
