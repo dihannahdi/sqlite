4ada90438303bd88b2b699f1ba384ddc2be502d9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:01 +0700

    Upload SQL chunk: t9510_chunk_0000.sql

---
+++ sql_chunks/t9510_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9510
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9510" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9510
+INSERT INTO [t9510] VALUES (1,'ذكر أبي بكر عبد الله بن محمد بن عبيد بن سفيان بن قيس القرشي مولى بني أمية المعروف بابن أبي الدنيا البغدادي صاحب كتاب الرقائق، وما وقع لي عاليا من حديثه','1','0','0');
+INSERT INTO [t9510] VALUES (2,'آخره، والحمد لله رب العالمين وحده','25','0','0');
