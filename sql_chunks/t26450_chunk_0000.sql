c6f8f285844a089ab9f6abc16e6b2ecda8a7fb15
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:31 +0700

    Upload SQL chunk: t26450_chunk_0000.sql

---
+++ sql_chunks/t26450_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26450
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26450" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26450
+INSERT INTO [t26450] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26450] VALUES (2,'فوائد حديث الحافظ عبد الغني بن سعيد الأزدي عن شيوخه','2','0','0');
