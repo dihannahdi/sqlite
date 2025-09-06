30371189ec232231c9df35ee9531b3d009d9a68b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:19 +0700

    Upload SQL chunk: t1482_chunk_0000.sql

---
+++ sql_chunks/t1482_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t1482
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t1482" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1482
+INSERT INTO [t1482] VALUES (1,'المناسبات.. وترتيب الآيات والسور','1','0','0');
+INSERT INTO [t1482] VALUES (2,'ترتيب الآيات','2','1','0');
+INSERT INTO [t1482] VALUES (3,'ترتيب السور','5','1','0');
+INSERT INTO [t1482] VALUES (4,'آراء العلماء في المناسبات','10','0','0');
+INSERT INTO [t1482] VALUES (5,'الإتجاه الأول','11','4','0');
+INSERT INTO [t1482] VALUES (6,'الإتجاه الثاني','14','4','0');
+INSERT INTO [t1482] VALUES (7,'الاتجاه الثالث','15','4','0');
+INSERT INTO [t1482] VALUES (8,'الاتجاه الرابع','17','4','0');
+INSERT INTO [t1482] VALUES (9,'وقفة عند كل اتجاه','21','0','0');
+INSERT INTO [t1482] VALUES (10,'رأي الشوكاني','22','9','0');
+INSERT INTO [t1482] VALUES (11,'رأي العز بن عبد السلام','32','9','0');
+INSERT INTO [t1482] VALUES (12,'رأي الجمهور','34','9','0');
+INSERT INTO [t1482] VALUES (13,'رأي دراز والفراهي','37','9','0');
