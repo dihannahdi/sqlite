d89bde32c970678efff8f03cd04e6d76f4f2a298
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:58 +0700

    Upload SQL chunk: t2191_chunk_0000.sql

---
+++ sql_chunks/t2191_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t2191
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t2191" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2191
+INSERT INTO [t2191] VALUES (1,'توجيهات إسلامية','1','0','0');
+INSERT INTO [t2191] VALUES (2,'دعوة المرسلين','4','0','0');
+INSERT INTO [t2191] VALUES (3,'من محاسن الإسلام','9','0','0');
+INSERT INTO [t2191] VALUES (4,'واعتصموا بحبل الله جميعا ولا تفرقوا','11','0','0');
+INSERT INTO [t2191] VALUES (5,'من أحكام الحج','13','0','0');
