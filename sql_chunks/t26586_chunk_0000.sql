d195a7d25679a422718ff1a3d4ee97d4c2c3a49e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:50 +0700

    Upload SQL chunk: t26586_chunk_0000.sql

---
+++ sql_chunks/t26586_chunk_0000.sql
@@ -0,0 +1,57 @@
+-- Table: t26586
+-- Chunk: 1
+-- Rows: 50
+
+CREATE TABLE "t26586" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26586
+INSERT INTO [t26586] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t26586] VALUES (2,'خطبة الكتاب','1','0','0');
+INSERT INTO [t26586] VALUES (3,'مقامة المراشد','8','0','0');
+INSERT INTO [t26586] VALUES (4,'مقامة التقوى','12','0','0');
+INSERT INTO [t26586] VALUES (5,'مقامة الرضوان','14','0','0');
+INSERT INTO [t26586] VALUES (6,'مقامة الإرعواء','17','0','0');
+INSERT INTO [t26586] VALUES (7,'مقامة الزاد','20','0','0');
+INSERT INTO [t26586] VALUES (8,'مقامة الزهد','22','0','0');
+INSERT INTO [t26586] VALUES (9,'مقامة الإنابة','27','0','0');
+INSERT INTO [t26586] VALUES (10,'مقامة الحذر','31','0','0');
+INSERT INTO [t26586] VALUES (11,'مقامة الاعتبار','35','0','0');
+INSERT INTO [t26586] VALUES (12,'مقامة التسليم','38','0','0');
+INSERT INTO [t26586] VALUES (13,'مقامة الصمت','42','0','0');
+INSERT INTO [t26586] VALUES (14,'مقامة الطاعة','45','0','0');
+INSERT INTO [t26586] VALUES (15,'مقامة المنذر','49','0','0');
+INSERT INTO [t26586] VALUES (16,'مقامة الاستقامة','53','0','0');
+INSERT INTO [t26586] VALUES (17,'مقامة الطيب','55','0','0');
+INSERT INTO [t26586] VALUES (18,'مقامة القناعة','59','0','0');
+INSERT INTO [t26586] VALUES (19,'مقامة التوقي','63','0','0');
+INSERT INTO [t26586] VALUES (20,'مقامة الظلف','67','0','0');
+INSERT INTO [t26586] VALUES (21,'مقامة العزلة','74','0','0');
+INSERT INTO [t26586] VALUES (22,'مقامة العفة','81','0','0');
+INSERT INTO [t26586] VALUES (23,'مقامة الندم','88','0','0');
+INSERT INTO [t26586] VALUES (24,'مقامة الولاية','93','0','0');
+INSERT INTO [t26586] VALUES (25,'مقامة الصلاح','96','0','0');
+INSERT INTO [t26586] VALUES (26,'مقامة العمل','103','0','0');
+INSERT INTO [t26586] VALUES (27,'مقامة التوحيد','109','0','0');
+INSERT INTO [t26586] VALUES (28,'مقامة العبادة','113','0','0');
+INSERT INTO [t26586] VALUES (29,'مقامة التصبر','117','0','0');
+INSERT INTO [t26586] VALUES (30,'مقامة الخشية','122','0','0');
+INSERT INTO [t26586] VALUES (31,'مقامة اجتناب الظلمة','126','0','0');
+INSERT INTO [t26586] VALUES (32,'مقامة التهجد','132','0','0');
+INSERT INTO [t26586] VALUES (33,'مقامة الدعاء','135','0','0');
+INSERT INTO [t26586] VALUES (34,'مقامة التصدق','139','0','0');
+INSERT INTO [t26586] VALUES (35,'مقامة الشكر','143','0','0');
+INSERT INTO [t26586] VALUES (36,'مقامة الاسوة','150','0','0');
+INSERT INTO [t26586] VALUES (37,'مقامة النصح','153','0','0');
+INSERT INTO [t26586] VALUES (38,'مقامة المراقبة','155','0','0');
+INSERT INTO [t26586] VALUES (39,'مقامة الموت','159','0','0');
+INSERT INTO [t26586] VALUES (40,'مقامة الفرقان','164','0','0');
+INSERT INTO [t26586] VALUES (41,'مقامة النهي عن الهوى','170','0','0');
+INSERT INTO [t26586] VALUES (42,'مقامة التماسك','174','0','0');
+INSERT INTO [t26586] VALUES (43,'مقامة الشهامة','177','0','0');
+INSERT INTO [t26586] VALUES (44,'مقامة الخمول','180','0','0');
+INSERT INTO [t26586] VALUES (45,'مقامة العزم','184','0','0');
+INSERT INTO [t26586] VALUES (46,'مقامة الصدق','186','0','0');
+INSERT INTO [t26586] VALUES (47,'مقامة النحو','190','0','0');
+INSERT INTO [t26586] VALUES (48,'مقامة العروض','194','0','0');
+INSERT INTO [t26586] VALUES (49,'مقامة الديوان','214','0','0');
+INSERT INTO [t26586] VALUES (50,'مقامة أيام العرب','222','0','0');
