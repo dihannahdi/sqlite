09a255835f2012e55495fce71d0a880adc2ed2ab
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:05 +0700

    Upload SQL chunk: t21587_chunk_0000.sql

---
+++ sql_chunks/t21587_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t21587
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t21587" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21587
+INSERT INTO [t21587] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t21587] VALUES (2,'الدين الإسلامي','2','0','0');
+INSERT INTO [t21587] VALUES (3,'أركان الإسلام','4','0','0');
+INSERT INTO [t21587] VALUES (4,'أسس العقيدة الإسلامية','6','3','0');
+INSERT INTO [t21587] VALUES (5,'الإيمان بالله تعالى','6','3','0');
+INSERT INTO [t21587] VALUES (6,'الإيمان بالملائكة','14','3','0');
+INSERT INTO [t21587] VALUES (7,'الإيمان بالكتب','18','3','0');
+INSERT INTO [t21587] VALUES (8,'الإيمان بالرسل','20','3','0');
+INSERT INTO [t21587] VALUES (9,'الإيمان باليوم الآخر','24','3','0');
+INSERT INTO [t21587] VALUES (10,'الإيمان بالقدر','32','3','0');
+INSERT INTO [t21587] VALUES (11,'أهداف العقيدة الإسلامية','38','0','0');
