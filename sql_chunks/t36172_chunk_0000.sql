50ae723bee01ea0e840531d7fdd9f8fdbb6535ab
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:03 +0700

    Upload SQL chunk: t36172_chunk_0000.sql

---
+++ sql_chunks/t36172_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t36172
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t36172" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36172
+INSERT INTO [t36172] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t36172] VALUES (2,'الأصل في الهجر، وذكر بعض أقوال وأحوال العلماء حوله.','6','0','0');
+INSERT INTO [t36172] VALUES (3,'فصل.','11','2','0');
+INSERT INTO [t36172] VALUES (4,'ذكر بعض من يهجر من الصحابة.','15','0','0');
+INSERT INTO [t36172] VALUES (5,'الهجر فوق ثلاث.','17','0','0');
+INSERT INTO [t36172] VALUES (6,'فصل.','22','5','0');
+INSERT INTO [t36172] VALUES (7,'المداهنة.','26','0','0');
+INSERT INTO [t36172] VALUES (8,'المقصود بالهجر.','34','0','0');
+INSERT INTO [t36172] VALUES (9,'أعوان الظلمة.','38','0','0');
+INSERT INTO [t36172] VALUES (10,'فصل.','41','9','0');
+INSERT INTO [t36172] VALUES (11,'فصل.','43','9','0');
+INSERT INTO [t36172] VALUES (12,'دعوى محبة بلا غيرة ولا غضب!.','52','0','0');
+INSERT INTO [t36172] VALUES (13,'هجر الأقارب والأرحام.','57','0','0');
+INSERT INTO [t36172] VALUES (14,'فصل.','62','13','0');
+INSERT INTO [t36172] VALUES (15,'فصل.','67','13','0');
+INSERT INTO [t36172] VALUES (16,'فصل.','68','13','0');
+INSERT INTO [t36172] VALUES (17,'الوسطية والدين يسر!.','75','0','0');
+INSERT INTO [t36172] VALUES (18,'حرية الرأي والتعبير!.','95','0','0');
+INSERT INTO [t36172] VALUES (19,'إنكار البغض في الله والهجر فيه عز وجل، وأبيات للعلامة (ابن سحمان) في ذلك.','104','0','0');
+INSERT INTO [t36172] VALUES (20,'ذم الباطل وأهله.','114','0','0');
+INSERT INTO [t36172] VALUES (21,'قصيدة للمؤلف في موضوع البغض في الله والهجر فيه تبارك وتعالى.','116','0','0');
+INSERT INTO [t36172] VALUES (22,'من كتب ومؤلفات فضيلة الشيخ / عبد الكريم بن صالح الحميد','118','0','0');
