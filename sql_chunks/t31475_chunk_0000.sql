3fbee5d36fb021746714cbb06a3e5b7bf22fd781
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:32 +0700

    Upload SQL chunk: t31475_chunk_0000.sql

---
+++ sql_chunks/t31475_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t31475
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t31475" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31475
+INSERT INTO [t31475] VALUES (1,'كلمة معالي الشيخ العلامة صالح بن محمد اللحيدان في أحداث ١١ سبتمبر','1','0','0');
+INSERT INTO [t31475] VALUES (2,'حكم الإسلام في ذلك','1','1','0');
+INSERT INTO [t31475] VALUES (3,'من يحدث مثل هذه الجرائم يعد في النظر الإسلامي من أخطر الناس جرما','4','1','0');
+INSERT INTO [t31475] VALUES (4,'هذه الأعمال من أخبث الأعمال وأشدها ضررا على بني الإنسان','6','1','0');
+INSERT INTO [t31475] VALUES (5,'الشريعة الإسلامية جاءت بالوقاية التي هي خير من العلاج','8','1','0');
