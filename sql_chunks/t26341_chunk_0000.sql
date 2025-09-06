e976a7aa9e0d672bedaf9eec18e4b84b691fab47
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:55 +0700

    Upload SQL chunk: t26341_chunk_0000.sql

---
+++ sql_chunks/t26341_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26341
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26341" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26341
+INSERT INTO [t26341] VALUES (1,'نسخة طالوت بن عباد','1','0','0');
