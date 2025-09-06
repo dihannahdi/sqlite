907df92184138301c6f2f723ab09ebbe99d7d26c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:09:42 +0700

    Upload SQL chunk: t13054_chunk_0000.sql

---
+++ sql_chunks/t13054_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t13054
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t13054" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13054
+INSERT INTO [t13054] VALUES (1,'باب دعاء النبي صلى الله عليه وسلم بالقناعة','7','0','0');
+INSERT INTO [t13054] VALUES (2,'باب الحث على لزوم القناعة والصبر عليها','12','0','0');
+INSERT INTO [t13054] VALUES (3,'باب في ذكر ما يبعث على استعمال القناعة','14','0','0');
+INSERT INTO [t13054] VALUES (4,'باب أقنع الناس هم أغنى الناس','18','0','0');
+INSERT INTO [t13054] VALUES (5,'باب ما يحبب إلى الإنسان القناعة','21','0','0');
+INSERT INTO [t13054] VALUES (6,'باب الأسباب التي تهون القناعة على الإنسان','26','0','0');
+INSERT INTO [t13054] VALUES (7,'باب','30','0','0');
+INSERT INTO [t13054] VALUES (8,'باب','36','0','0');
+INSERT INTO [t13054] VALUES (9,'باب الاستغناء عن استعمال الأردية','42','0','0');
+INSERT INTO [t13054] VALUES (10,'باب','49','0','0');
+INSERT INTO [t13054] VALUES (11,'باب','53','0','0');
+INSERT INTO [t13054] VALUES (12,'باب','56','0','0');
+INSERT INTO [t13054] VALUES (13,'باب','59','0','0');
+INSERT INTO [t13054] VALUES (14,'باب','61','0','0');
+INSERT INTO [t13054] VALUES (15,'باب ما يدفع به الإنسان عن نفسه معرة الغنى','70','0','0');
+INSERT INTO [t13054] VALUES (16,'باب','72','0','0');
+INSERT INTO [t13054] VALUES (17,'باب','76','0','0');
