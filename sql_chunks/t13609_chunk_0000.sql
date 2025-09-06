467bb830381a12a764759e964740385ca1be8742
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:14:54 +0700

    Upload SQL chunk: t13609_chunk_0000.sql

---
+++ sql_chunks/t13609_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t13609
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t13609" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13609
+INSERT INTO [t13609] VALUES (1,'الجزء الأول','1','0','0');
+INSERT INTO [t13609] VALUES (2,'خبر يوم قشاوة','24','1','0');
+INSERT INTO [t13609] VALUES (3,'خبر يوم ذات كهف ويوم طخفة','75','1','0');
+INSERT INTO [t13609] VALUES (4,'خبر يوم أعشاش ويوم صحراء فلج','85','1','0');
+INSERT INTO [t13609] VALUES (5,'حديث داحس عن الكلبي','93','1','0');
+INSERT INTO [t13609] VALUES (6,'وهذا يوم عبيد الله بن زياد بن أبيه','126','1','0');
+INSERT INTO [t13609] VALUES (7,'أول ابتداء الفرزدق','139','1','0');
+INSERT INTO [t13609] VALUES (8,'يوم جدود','159','1','0');
+INSERT INTO [t13609] VALUES (9,'هذا يوم الكلاب الثاني','165','1','0');
+INSERT INTO [t13609] VALUES (10,'الجزء الثاني','208','0','0');
+INSERT INTO [t13609] VALUES (11,'مقتل عمارة','209','10','0');
+INSERT INTO [t13609] VALUES (12,'حديث البراجم','234','10','0');
+INSERT INTO [t13609] VALUES (13,'حديث الشقيقة','250','10','0');
+INSERT INTO [t13609] VALUES (14,'حديث النسار','255','10','0');
+INSERT INTO [t13609] VALUES (15,'وهذا يوم الوقيط','320','10','0');
+INSERT INTO [t13609] VALUES (16,'حديث يوم الغبيط','328','10','0');
+INSERT INTO [t13609] VALUES (17,'يوم الصرائم','350','10','0');
+INSERT INTO [t13609] VALUES (18,'حديث يوم الفروقين','427','10','0');
+INSERT INTO [t13609] VALUES (19,'حديث المراعي وعرادة النميري','436','10','0');
+INSERT INTO [t13609] VALUES (20,'وهذا حديث يوم الكلاب','461','10','0');
+INSERT INTO [t13609] VALUES (21,'يوم فيف الريح','479','10','0');
+INSERT INTO [t13609] VALUES (22,'يوم الاياد','575','10','0');
+INSERT INTO [t13609] VALUES (23,'وهذا حديث يوم ذي نجب','582','10','0');
+INSERT INTO [t13609] VALUES (24,'الجزء الثالث','594','0','0');
+INSERT INTO [t13609] VALUES (25,'هذا حديث يوم ذي قار','633','24','0');
+INSERT INTO [t13609] VALUES (26,'وهذا حديث يوم أقرن','651','24','0');
+INSERT INTO [t13609] VALUES (27,'حديث يوم ذي طلوح','744','24','0');
+INSERT INTO [t13609] VALUES (28,'قصة عمرو بن عمران الصيداوي مع حري','876','24','0');
+INSERT INTO [t13609] VALUES (29,'حديث يوم تياس','944','24','0');
