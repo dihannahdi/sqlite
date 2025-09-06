1781109b914de0d10c3f74171819796f865bfbef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:08:00 +0700

    Upload SQL chunk: t12855_chunk_0000.sql

---
+++ sql_chunks/t12855_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t12855
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t12855" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12855
+INSERT INTO [t12855] VALUES (1,'الحديث الأول','1','0','0');
+INSERT INTO [t12855] VALUES (2,'الحديث الثاني','1','0','0');
+INSERT INTO [t12855] VALUES (3,'الحديث الثالث','2','0','0');
+INSERT INTO [t12855] VALUES (4,'الحديث الرابع','2','0','0');
+INSERT INTO [t12855] VALUES (5,'الحديث الخامس','2','0','0');
+INSERT INTO [t12855] VALUES (6,'الحديث السادس','3','0','0');
+INSERT INTO [t12855] VALUES (7,'الحديث السابع','3','0','0');
+INSERT INTO [t12855] VALUES (8,'الحديث الثامن','4','0','0');
+INSERT INTO [t12855] VALUES (9,'الحديث التاسع','5','0','0');
+INSERT INTO [t12855] VALUES (10,'الحديث العاشر','5','0','0');
+INSERT INTO [t12855] VALUES (11,'الحديث الحادي عشر','6','0','0');
+INSERT INTO [t12855] VALUES (12,'الحديث الثاني عشر','7','0','0');
+INSERT INTO [t12855] VALUES (13,'الحديث الثالث عشر','7','0','0');
+INSERT INTO [t12855] VALUES (14,'الحديث الرابع عشر','7','0','0');
+INSERT INTO [t12855] VALUES (15,'الحديث الخامس عشر','8','0','0');
+INSERT INTO [t12855] VALUES (16,'الحديث السادس عشر','8','0','0');
