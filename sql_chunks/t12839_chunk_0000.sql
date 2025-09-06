2b272ab34d2500e2615f717d22e170ec92665a44
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:50 +0700

    Upload SQL chunk: t12839_chunk_0000.sql

---
+++ sql_chunks/t12839_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t12839
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t12839" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12839
+INSERT INTO [t12839] VALUES (1,'تقسيم الدين إلى قشر ولب بدعة وضلالة','8','0','0');
+INSERT INTO [t12839] VALUES (2,'قضية "مبدأ"','24','0','0');
+INSERT INTO [t12839] VALUES (3,'الارتباط بين الظاهر والباطن','25','0','0');
+INSERT INTO [t12839] VALUES (4,'هويتنا في خطر','28','0','0');
+INSERT INTO [t12839] VALUES (5,'لكم "قشرتكم" .. ولنا "قشرتنا"','28','0','0');
+INSERT INTO [t12839] VALUES (6,'دعوا السنة تمضي، لا تعرضوا لها بالرأي','30','0','0');
+INSERT INTO [t12839] VALUES (7,'فائدة','35','0','0');
+INSERT INTO [t12839] VALUES (8,'درء تعارض التمسك بالهدي الظاهر مع الاهتمام بقضايا الأمة الكبرى','36','0','0');
+INSERT INTO [t12839] VALUES (9,'قياس فاسد','41','0','0');
+INSERT INTO [t12839] VALUES (10,'هذه هي القشور!','44','0','0');
+INSERT INTO [t12839] VALUES (11,'مفارقات عجيبة','52','0','0');
+INSERT INTO [t12839] VALUES (12,'في سبيل التطوس','53','0','0');
+INSERT INTO [t12839] VALUES (13,'ومن مظاهر استعباد " القشور " كثيرا من المسلمين','54','0','0');
+INSERT INTO [t12839] VALUES (14,'معالي الأمور .. لا قشور','59','0','0');
+INSERT INTO [t12839] VALUES (15,'الخاتمة','62','0','0');
