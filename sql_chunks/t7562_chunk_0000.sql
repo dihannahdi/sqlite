1042a57b5a5f9b1819a921b538683c0ba0a2ba41
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:54 +0700

    Upload SQL chunk: t7562_chunk_0000.sql

---
+++ sql_chunks/t7562_chunk_0000.sql
@@ -0,0 +1,64 @@
+-- Table: t7562
+-- Chunk: 1
+-- Rows: 57
+
+CREATE TABLE "t7562" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7562
+INSERT INTO [t7562] VALUES (1,'كلمة شكر','6','0','0');
+INSERT INTO [t7562] VALUES (2,'المقدمة','7','0','0');
+INSERT INTO [t7562] VALUES (3,'أسباب اختيار الموضوع:','9','2','0');
+INSERT INTO [t7562] VALUES (4,'الإمام أبو عيسى الترمذي','11','2','0');
+INSERT INTO [t7562] VALUES (5,'الإمام السيوطي','18','2','0');
+INSERT INTO [t7562] VALUES (6,'أقوال العلماء فيه -السيوطي-','25','5','0');
+INSERT INTO [t7562] VALUES (7,'قوت المغتذي على جامع الترمذي','40','2','0');
+INSERT INTO [t7562] VALUES (8,'مكانة الكتاب وأهميته','41','7','0');
+INSERT INTO [t7562] VALUES (9,'منهج الإمام السيوطي في شرحه على جامع الترمذي','54','2','0');
+INSERT INTO [t7562] VALUES (10,'منهجي في تحقيق الكتاب:','63','2','0');
+INSERT INTO [t7562] VALUES (11,'منهجي في التعليق على الكتاب:','63','2','0');
+INSERT INTO [t7562] VALUES (12,'التعريف بمخطوطات قوت المغتذي على جامع الترمذي:','64','2','0');
+INSERT INTO [t7562] VALUES (13,'وصف النسخ:','64','2','0');
+INSERT INTO [t7562] VALUES (14,'نماذج من صور المخطوطات','66','2','0');
+INSERT INTO [t7562] VALUES (15,'النص المحقق','67','0','0');
+INSERT INTO [t7562] VALUES (16,'مقدمة','68','0','0');
+INSERT INTO [t7562] VALUES (17,'أبواب الطهارة','94','0','0');
+INSERT INTO [t7562] VALUES (18,'(أبواب الصلاة)','215','0','0');
+INSERT INTO [t7562] VALUES (19,'أبواب الزكاة','479','0','0');
+INSERT INTO [t7562] VALUES (20,'أبواب الصوم','516','0','0');
+INSERT INTO [t7562] VALUES (21,'أبواب الحج','562','0','0');
+INSERT INTO [t7562] VALUES (22,'أبواب الجنائز','614','0','0');
+INSERT INTO [t7562] VALUES (23,'أبواب النكاح','663','0','0');
+INSERT INTO [t7562] VALUES (24,'أبواب البيوع','710','0','0');
+INSERT INTO [t7562] VALUES (25,'أبواب الأحكام','756','0','0');
+INSERT INTO [t7562] VALUES (26,'أبواب الديات','770','0','0');
+INSERT INTO [t7562] VALUES (27,'أبواب الحدود','782','0','0');
+INSERT INTO [t7562] VALUES (28,'أبواب الصيد','797','0','0');
+INSERT INTO [t7562] VALUES (29,'أبواب الأضاحي','813','0','0');
+INSERT INTO [t7562] VALUES (30,'أبواب النذور والأيمان','834','0','0');
+INSERT INTO [t7562] VALUES (31,'أبواب السير','854','0','0');
+INSERT INTO [t7562] VALUES (32,'أبواب فضائل الجهاد','869','0','0');
+INSERT INTO [t7562] VALUES (33,'أبواب الجهاد','891','0','0');
+INSERT INTO [t7562] VALUES (34,'أبواب اللباس','918','0','0');
+INSERT INTO [t7562] VALUES (35,'أبواب الأطعمة','950','0','0');
+INSERT INTO [t7562] VALUES (36,'أبواب الأشربة','978','0','0');
+INSERT INTO [t7562] VALUES (37,'أبواب البر والصلة','989','0','0');
+INSERT INTO [t7562] VALUES (38,'أبواب الطب','1027','0','0');
+INSERT INTO [t7562] VALUES (39,'أبواب الولاء','1044','0','0');
+INSERT INTO [t7562] VALUES (40,'أبواب القدر','1050','0','0');
+INSERT INTO [t7562] VALUES (41,'أبواب الفتن','1085','0','0');
+INSERT INTO [t7562] VALUES (42,'أبواب الرؤيا','1140','0','0');
+INSERT INTO [t7562] VALUES (43,'أبواب الشهادات','1155','0','0');
+INSERT INTO [t7562] VALUES (44,'أبواب الزهد','1157','0','0');
+INSERT INTO [t7562] VALUES (45,'أبواب صفة الجنة','1261','0','0');
+INSERT INTO [t7562] VALUES (46,'أبواب صفة جهنم','1283','0','0');
+INSERT INTO [t7562] VALUES (47,'أبواب الإيمان','1298','0','0');
+INSERT INTO [t7562] VALUES (48,'أبواب العلم','1351','0','0');
+INSERT INTO [t7562] VALUES (49,'أبواب الاستنذان والآداب','1388','0','0');
+INSERT INTO [t7562] VALUES (50,'أبواب الأدب','1411','0','0');
+INSERT INTO [t7562] VALUES (51,'أبواب الأمثال','1445','0','0');
+INSERT INTO [t7562] VALUES (52,'أبواب فضائل القرآن','1459','0','0');
+INSERT INTO [t7562] VALUES (53,'أبواب القراءات','1509','0','0');
+INSERT INTO [t7562] VALUES (54,'أبواب تفسير القرآن','1515','0','0');
+INSERT INTO [t7562] VALUES (55,'أبواب الدعوات','1663','0','0');
+INSERT INTO [t7562] VALUES (56,'أبواب المناقب','1881','0','0');
+INSERT INTO [t7562] VALUES (57,'الخاتمة','2067','0','0');
