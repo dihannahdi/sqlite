1ba0eda05a887009b2903827f408350d42736fdd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:00:06 +0700

    Upload SQL chunk: t1184_chunk_0000.sql

---
+++ sql_chunks/t1184_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t1184
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t1184" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1184
+INSERT INTO [t1184] VALUES (1,'الجزء ١','1','0','0');
+INSERT INTO [t1184] VALUES (2,'الجزء ٢','364','0','0');
+INSERT INTO [t1184] VALUES (3,'الجزء ٣','667','0','0');
+INSERT INTO [t1184] VALUES (4,'الجزء ٤','880','0','0');
+INSERT INTO [t1184] VALUES (5,'الجزء ٥','1123','0','0');
+INSERT INTO [t1184] VALUES (6,'الجزء ٦','1320','0','0');
+INSERT INTO [t1184] VALUES (7,'الجزء ٧','1509','0','0');
+INSERT INTO [t1184] VALUES (8,'الجزء ٨','1793','0','0');
+INSERT INTO [t1184] VALUES (9,'الجزء ٩','1978','0','0');
+INSERT INTO [t1184] VALUES (10,'الجزء ١٠','2128','0','0');
