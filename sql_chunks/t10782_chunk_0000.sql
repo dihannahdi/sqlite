34b2bf032d8f610037b38ef746e0415a721d2a28
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:27 +0700

    Upload SQL chunk: t10782_chunk_0000.sql

---
+++ sql_chunks/t10782_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t10782
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t10782" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10782
+INSERT INTO [t10782] VALUES (1,'بدأ الإسلام غريبا وسيعود غريبا','1','0','0');
+INSERT INTO [t10782] VALUES (2,'تحول الإسلام من ضعف إلى قوة','4','0','0');
+INSERT INTO [t10782] VALUES (3,'فتنتا الشبهات والشهوات','4','0','0');
+INSERT INTO [t10782] VALUES (4,'الإسلام لا يذهب ولكن يذهب أهل السنة','6','0','0');
+INSERT INTO [t10782] VALUES (5,'السنة الكاملة هي الطريق السالمة من الشبهات والشهوات','7','0','0');
+INSERT INTO [t10782] VALUES (6,'تقسيم حملة العلم','10','0','0');
+INSERT INTO [t10782] VALUES (7,'من صفات حملة العلم','12','0','0');
+INSERT INTO [t10782] VALUES (8,'الغربة عند أهل الطريقة','15','0','0');
