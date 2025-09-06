ac61f9f6beadbc3329eefca5fb2670e5e1a2f4fc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:58 +0700

    Upload SQL chunk: t6474_chunk_0000.sql

---
+++ sql_chunks/t6474_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6474
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6474" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6474
+INSERT INTO [t6474] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6474] VALUES (2,'رسالة الإمام أحمد إلى الخليفة المتوكل في مسألة القرآن','14','0','0');
