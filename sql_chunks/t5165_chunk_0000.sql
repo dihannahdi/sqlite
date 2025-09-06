6c70e1b921920f5ae27b94e3c21479e96a76029e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:51 +0700

    Upload SQL chunk: t5165_chunk_0000.sql

---
+++ sql_chunks/t5165_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5165
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5165" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5165
+INSERT INTO [t5165] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5165] VALUES (2,'منهج التشريع الإسلامي وحكمته','1','0','0');
