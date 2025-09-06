89089813bfbbd2abdf98ec957efe6d84a6f07116
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:58 +0700

    Upload SQL chunk: t6475_chunk_0000.sql

---
+++ sql_chunks/t6475_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t6475
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t6475" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6475
+INSERT INTO [t6475] VALUES (1,'رسالة في إثبات الاستواء والفوقية','1','0','0');
+INSERT INTO [t6475] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6475] VALUES (3,'فصل','34','0','0');
+INSERT INTO [t6475] VALUES (4,'فصل','39','0','0');
+INSERT INTO [t6475] VALUES (5,'فصل','41','0','0');
+INSERT INTO [t6475] VALUES (6,'فإن فهموا في هذه الصفات ذلك فيلزمهم أن يفهموا في الصفات السبع صفات المخلوقين من الأعراض فما يلزمونا في تلك الصفات من التشبيه والجسمية نلزمهم به في هذه الصفات من العرضية وما ينزهوا ربهم به في الصفات السبع وينفون عنه عوارض الجسم فيها فكذلك نحن نعمل في','43','0','0');
+INSERT INTO [t6475] VALUES (7,'فصل','44','0','0');
+INSERT INTO [t6475] VALUES (8,'فصل','49','0','0');
+INSERT INTO [t6475] VALUES (9,'فصل','50','0','0');
+INSERT INTO [t6475] VALUES (10,'تمت الرسالة','53','0','0');
