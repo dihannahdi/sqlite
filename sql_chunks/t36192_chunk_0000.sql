e66839cf7531165d9e0102345e22b6f6c75f38e9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:12 +0700

    Upload SQL chunk: t36192_chunk_0000.sql

---
+++ sql_chunks/t36192_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t36192
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t36192" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36192
+INSERT INTO [t36192] VALUES (1,'السنة كلها تشرع لموسى شاهين لاشين','2','0','0');
+INSERT INTO [t36192] VALUES (2,'المباح تكليف شرعي:','26','0','0');
+INSERT INTO [t36192] VALUES (3,'فعل المباح طاعة لله ورسوله يثاب عليه:','29','0','0');
+INSERT INTO [t36192] VALUES (4,'التفريط والإفراط والضعف في فهم الشريعة وأصولها ومقاصدها:','31','0','0');
+INSERT INTO [t36192] VALUES (5,'الظروف والملابسات عنصر أساسي في صدور الحكم:','32','0','0');
+INSERT INTO [t36192] VALUES (6,'ماذا يريد أصحاب تقسيم السنة إلى تشريعية وغير تشريعية؟:','36','0','0');
