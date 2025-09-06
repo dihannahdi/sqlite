4f05a3a2259d980bb7992c99278b940e529b0171
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:41 +0700

    Upload SQL chunk: t26237_chunk_0000.sql

---
+++ sql_chunks/t26237_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t26237
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t26237" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26237
+INSERT INTO [t26237] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t26237] VALUES (2,'رب أعن','1','1','0');
+INSERT INTO [t26237] VALUES (3,'ومن الجزء الخمس','29','1','0');
+INSERT INTO [t26237] VALUES (4,'ومن الجزء الثاني','41','1','0');
+INSERT INTO [t26237] VALUES (5,'ومن الجزء الثالث','49','1','0');
+INSERT INTO [t26237] VALUES (6,'ومن الجزء السادس','85','1','0');
+INSERT INTO [t26237] VALUES (7,'ومن الجزء السابع','110','1','0');
+INSERT INTO [t26237] VALUES (8,'ملحق بأمالي ابن دريد في امالي القالي ومظهر السيوطي','139','1','0');
