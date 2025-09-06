ba65ba63070dc3108e243fcdb35f7b29c4f1590c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:00 +0700

    Upload SQL chunk: t12719_chunk_0000.sql

---
+++ sql_chunks/t12719_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t12719
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t12719" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12719
+INSERT INTO [t12719] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t12719] VALUES (2,'باب في الإخلاص وإحضار النية','6','0','0');
+INSERT INTO [t12719] VALUES (3,'في جميع الأعمال الظاهرة والباطنة','6','0','0');
+INSERT INTO [t12719] VALUES (4,'فصل في حقيقة الاخلاص والصدق','21','3','0');
+INSERT INTO [t12719] VALUES (5,'فصل','24','3','0');
+INSERT INTO [t12719] VALUES (6,'فصل','24','3','0');
+INSERT INTO [t12719] VALUES (7,'باب في نفائس مأثورة','33','0','0');
+INSERT INTO [t12719] VALUES (8,'باب في كرامات الأولياء ومواهبهم','52','0','0');
+INSERT INTO [t12719] VALUES (9,'فصل','57','8','0');
+INSERT INTO [t12719] VALUES (10,'فصل','57','8','0');
+INSERT INTO [t12719] VALUES (11,'فصل','57','8','0');
+INSERT INTO [t12719] VALUES (12,'فصل','57','8','0');
+INSERT INTO [t12719] VALUES (13,'فصل','57','8','0');
+INSERT INTO [t12719] VALUES (14,'فصل','57','8','0');
+INSERT INTO [t12719] VALUES (15,'فصل','59','8','0');
+INSERT INTO [t12719] VALUES (16,'فصل','59','8','0');
+INSERT INTO [t12719] VALUES (17,'فصل','59','8','0');
+INSERT INTO [t12719] VALUES (18,'فصل','59','8','0');
+INSERT INTO [t12719] VALUES (19,'فصل','59','8','0');
+INSERT INTO [t12719] VALUES (20,'فصل','59','8','0');
+INSERT INTO [t12719] VALUES (21,'في منثور حكايات في المواهب والكرامات','62','8','0');
+INSERT INTO [t12719] VALUES (22,'باب في حكايات مستظرفة','67','0','0');
