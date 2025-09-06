b6996f0f1108bc367ea562d822e93567d25bb6c8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:24 +0700

    Upload SQL chunk: t5369_chunk_0000.sql

---
+++ sql_chunks/t5369_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t5369
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t5369" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5369
+INSERT INTO [t5369] VALUES (1,'أول الكتاب','1','0','0');
+INSERT INTO [t5369] VALUES (2,'وهذا ما جاء في أول النسخة المصورة','23','0','0');
+INSERT INTO [t5369] VALUES (3,'تكملة','24','0','0');
+INSERT INTO [t5369] VALUES (4,'١- الأغاني','24','0','0');
+INSERT INTO [t5369] VALUES (5,'٢- الأغاني','24','0','0');
+INSERT INTO [t5369] VALUES (6,'٣-الأغاني','25','0','0');
+INSERT INTO [t5369] VALUES (7,'٤- الورقة ((لابن الجراح))','25','0','0');
+INSERT INTO [t5369] VALUES (8,'٥-طبقات الشعراء لابن المعتز','25','0','0');
+INSERT INTO [t5369] VALUES (9,'٦-طبقات الشعراء لابن المعتز','26','0','0');
+INSERT INTO [t5369] VALUES (10,'٧-طبقات لاشعراء لابن المعتز','26','0','0');
+INSERT INTO [t5369] VALUES (11,'٨- طبقات الشعراء لابن المعتز','26','0','0');
+INSERT INTO [t5369] VALUES (12,'٩- معجم البلدان مادة كنارك','28','0','0');
+INSERT INTO [t5369] VALUES (13,'١٠- ديوان أبي نواس','28','0','0');
+INSERT INTO [t5369] VALUES (14,'١١- أخبار أبي نواس لابن منظور','28','0','0');
+INSERT INTO [t5369] VALUES (15,'١٢- أخبار أبي نواس لابن المنظور','28','0','0');
+INSERT INTO [t5369] VALUES (16,'١٣- أخبار أبي نواس لابن منظور','28','0','0');
+INSERT INTO [t5369] VALUES (17,'١٤- الموشح','29','0','0');
+INSERT INTO [t5369] VALUES (18,'١٥- زهر الآداب','29','0','0');
+INSERT INTO [t5369] VALUES (19,'١٦- الأمالي','29','0','0');
+INSERT INTO [t5369] VALUES (20,'١٧- تهذيب ابن عساكر','29','0','0');
+INSERT INTO [t5369] VALUES (21,'١٨- تاريخ ابن عساكر','29','0','0');
