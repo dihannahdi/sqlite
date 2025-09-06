8df992b0a93d06c667158e00cb687eb463e78bb1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:59 +0700

    Upload SQL chunk: t115125_chunk_0000.sql

---
+++ sql_chunks/t115125_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t115125
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t115125" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t115125
+INSERT INTO [t115125] VALUES (1,'الحمد لله تعالى','1','0','0');
+INSERT INTO [t115125] VALUES (2,'* أيها المذنب! المعصية حظ الشيطان','1','0','0');
+INSERT INTO [t115125] VALUES (3,'* أيها المذنب! هل استفدت من عقلك','2','0','0');
+INSERT INTO [t115125] VALUES (4,'* أيها المذنب! كيف تستبدل بشرف الطاعات ذل المعاصي','3','0','0');
+INSERT INTO [t115125] VALUES (5,'* أيها المذنب! هل علمت أن أفضل العبادة ترك الحرام','4','0','0');
+INSERT INTO [t115125] VALUES (6,'* أيها المذنب! لقد اخترت ظلام الذنب على نور الطاعة','5','0','0');
+INSERT INTO [t115125] VALUES (7,'* أيها المذنب! بئس العبد عبد همه هواه','8','0','0');
+INSERT INTO [t115125] VALUES (8,'* أيها الغافل! أتدري علامة المطيع والعاصي','9','0','0');
+INSERT INTO [t115125] VALUES (9,'أيها المذنب','10','0','0');
