d1640e6efa53617d9f02ec802ccf06e6c16dfc7e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:03 +0700

    Upload SQL chunk: t6488_chunk_0000.sql

---
+++ sql_chunks/t6488_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6488
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6488" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6488
+INSERT INTO [t6488] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6488] VALUES (2,'فصل','12','0','0');
+INSERT INTO [t6488] VALUES (3,'فتييان تتعلقان بتكفير الجهمية وأن الصلاة لا تصح خلف من لا يكفر الجهمية ومسائل أخر','29','0','0');
