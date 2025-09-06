c47b59f2c3eb0595b3998218f2eb971b5e037318
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:08:20 +0700

    Upload SQL chunk: t12883_chunk_0000.sql

---
+++ sql_chunks/t12883_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t12883
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t12883" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12883
+INSERT INTO [t12883] VALUES (1,'مخطوط الأربعون حديثا حق الفقراء لأبي طاهر السلفي','1','0','0');
