01b4bef5e104a71e960e07bb1d763c4279e820d3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:28 +0700

    Upload SQL chunk: t26440_chunk_0000.sql

---
+++ sql_chunks/t26440_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t26440
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t26440" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26440
+INSERT INTO [t26440] VALUES (1,'الجزء الخامس [من] مسند مالك','1','0','0');
+INSERT INTO [t26440] VALUES (2,'مسند حديث مالك لإسماعيل القاضي','2','0','0');
+INSERT INTO [t26440] VALUES (3,'حديث هشام بن عروة بن الزبير','2','0','0');
+INSERT INTO [t26440] VALUES (4,'حديث مالك عن أبي الزبير محمد بن مسلم','51','0','0');
+INSERT INTO [t26440] VALUES (5,'حديث حميد بن قيس المكي','61','0','0');
+INSERT INTO [t26440] VALUES (6,'حديث عطاء الخراساني','63','0','0');
+INSERT INTO [t26440] VALUES (7,'حديث أيوب بن أبي تميمة','64','0','0');
+INSERT INTO [t26440] VALUES (8,'حديث حميد الطويل','67','0','0');
+INSERT INTO [t26440] VALUES (9,'حديث يحيى بن سعيد الأنصاري','73','0','0');
+INSERT INTO [t26440] VALUES (10,'حديث الفضيل بن أبي عبد الله','117','0','0');
