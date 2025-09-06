4943e225b3916322f11424aa8211e60acd04b0de
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:45 +0700

    Upload SQL chunk: t12090_chunk_0000.sql

---
+++ sql_chunks/t12090_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t12090
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t12090" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12090
+INSERT INTO [t12090] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12090] VALUES (2,'نصائح بين يدي الحج','3','0','0');
+INSERT INTO [t12090] VALUES (3,'لاحرج لاحرج','7','0','0');
+INSERT INTO [t12090] VALUES (4,'بين يدي الإحرام','9','0','0');
+INSERT INTO [t12090] VALUES (5,'المواقيت','12','0','0');
+INSERT INTO [t12090] VALUES (6,'أمره صلى الله عليه وسلم بالتمتع','13','0','0');
+INSERT INTO [t12090] VALUES (7,'الاشتراط','14','0','0');
+INSERT INTO [t12090] VALUES (8,'الصلاة بوادي العقيق','15','0','0');
+INSERT INTO [t12090] VALUES (9,'التلبية ورفع الصوت بها','16','0','0');
+INSERT INTO [t12090] VALUES (10,'الاغتسال لدخول مكة','20','0','0');
+INSERT INTO [t12090] VALUES (11,'طواف القدوم','22','0','0');
+INSERT INTO [t12090] VALUES (12,'التزام ما بين الركن والباب','25','0','0');
+INSERT INTO [t12090] VALUES (13,'السعي بين الصفا والمروة','28','0','0');
+INSERT INTO [t12090] VALUES (14,'الإهلال بالحج يوم التروية','32','0','0');
+INSERT INTO [t12090] VALUES (15,'الانطلاق إلى عرفة','33','0','0');
+INSERT INTO [t12090] VALUES (16,'الوقوف في عرفة','34','0','0');
+INSERT INTO [t12090] VALUES (17,'الإفاضة من عرفات','35','0','0');
+INSERT INTO [t12090] VALUES (18,'صلاة الفجر في مزدلفة','36','0','0');
+INSERT INTO [t12090] VALUES (19,'الذبح والنحر','40','0','0');
+INSERT INTO [t12090] VALUES (20,'طواف الإفاضة','44','0','0');
+INSERT INTO [t12090] VALUES (21,'البيات في منى','46','0','0');
+INSERT INTO [t12090] VALUES (22,'طواف الوداع','50','0','0');
+INSERT INTO [t12090] VALUES (23,'بدع الحج والعمرة والزيارة','52','0','0');
+INSERT INTO [t12090] VALUES (24,'بدع ماقبل الإحرام','56','0','0');
+INSERT INTO [t12090] VALUES (25,'بدع الإحرام والتلبية وغيرها','59','0','0');
+INSERT INTO [t12090] VALUES (26,'بدع الطواف','61','0','0');
+INSERT INTO [t12090] VALUES (27,'بدع السعي بين الصفا والمروة','64','0','0');
+INSERT INTO [t12090] VALUES (28,'بدع عرفة','65','0','0');
+INSERT INTO [t12090] VALUES (29,'بدع المزدلفة','67','0','0');
+INSERT INTO [t12090] VALUES (30,'بدع الرمي','68','0','0');
+INSERT INTO [t12090] VALUES (31,'بدع الذبح والحلق','69','0','0');
+INSERT INTO [t12090] VALUES (32,'بدع متنوعة','71','0','0');
+INSERT INTO [t12090] VALUES (33,'بدع الزيارة في المدينة المنورة','72','0','0');
+INSERT INTO [t12090] VALUES (34,'بدع بيت المقدس','76','0','0');
