d8c8e0e3df85c57fcf703d985dd14097bcbcdeed
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:03 +0700

    Upload SQL chunk: t115130_chunk_0000.sql

---
+++ sql_chunks/t115130_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t115130
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t115130" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t115130
+INSERT INTO [t115130] VALUES (1,'المقدمة','3','0','0');
+INSERT INTO [t115130] VALUES (2,'الإهداء','3','1','0');
+INSERT INTO [t115130] VALUES (3,'إلى من يهمه الأمر','4','1','0');
+INSERT INTO [t115130] VALUES (4,'كيف عثرت على الكتاب؟','9','1','0');
+INSERT INTO [t115130] VALUES (5,'من هو المؤلف؟','11','1','0');
+INSERT INTO [t115130] VALUES (6,'وفاته وأثاره','14','1','0');
+INSERT INTO [t115130] VALUES (7,'أهمية الكتاب','15','1','0');
+INSERT INTO [t115130] VALUES (8,'أسباب تأليف الكتاب ومنهجه','18','1','0');
+INSERT INTO [t115130] VALUES (9,'الأمازيغ في سوريا','21','1','0');
+INSERT INTO [t115130] VALUES (10,'مواقفه وفلسفته','26','1','0');
+INSERT INTO [t115130] VALUES (11,'وحدة الوطن','27','1','0');
+INSERT INTO [t115130] VALUES (12,'وحدة التاريخ','34','1','0');
+INSERT INTO [t115130] VALUES (13,'وحدة اللغة','41','1','0');
+INSERT INTO [t115130] VALUES (14,'وحدة المزاج','44','1','0');
+INSERT INTO [t115130] VALUES (15,'وحدة العشيرة','53','1','0');
+INSERT INTO [t115130] VALUES (16,'الوحدة التربوية','60','1','0');
+INSERT INTO [t115130] VALUES (17,'وحدة العقل','72','1','0');
+INSERT INTO [t115130] VALUES (18,'هدية الكتاب','81','0','0');
+INSERT INTO [t115130] VALUES (19,'فضل علم التاريخ','84','0','0');
+INSERT INTO [t115130] VALUES (20,'نسب الزواوة','90','0','0');
+INSERT INTO [t115130] VALUES (21,'محامد الزواوة وخصائصهم','101','0','0');
+INSERT INTO [t115130] VALUES (22,'زوايا الزواوة وعلماؤهم','117','0','0');
+INSERT INTO [t115130] VALUES (23,'بعض عادات الزواوة','124','0','0');
+INSERT INTO [t115130] VALUES (24,'الإصلاح المطلوب','133','0','0');
+INSERT INTO [t115130] VALUES (25,'الإصلاح الإسلامي','142','0','0');
+INSERT INTO [t115130] VALUES (26,'اللائحة نظام التعليم وبيان طرقه','149','0','0');
+INSERT INTO [t115130] VALUES (27,'طرق التعليم التعليم الابتدائي','151','0','0');
+INSERT INTO [t115130] VALUES (28,'الكتب التي يدرسونها','153','0','0');
+INSERT INTO [t115130] VALUES (29,'الإدارة والتفتيش للزوايا','155','0','0');
+INSERT INTO [t115130] VALUES (30,'تتمة أجواد ونجباء الزواوة الوارد ذكرهم','157','0','0');
+INSERT INTO [t115130] VALUES (31,'تقريظ الكتاب','161','0','0');
+INSERT INTO [t115130] VALUES (32,'الفهرس','164','0','0');
