4d5d396673d84cedf7ee81c50c1b5269b8048e7b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:28 +0700

    Upload SQL chunk: t96241_chunk_0000.sql

---
+++ sql_chunks/t96241_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t96241
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t96241" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96241
+INSERT INTO [t96241] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t96241] VALUES (2,'مدخل عام لآفات اللسان','1','0','0');
+INSERT INTO [t96241] VALUES (3,'تفصيل بعض آفات السان','4','0','0');
+INSERT INTO [t96241] VALUES (4,'الغيبة','4','0','0');
+INSERT INTO [t96241] VALUES (5,'القذف','8','0','0');
+INSERT INTO [t96241] VALUES (6,'النميمة','10','0','0');
+INSERT INTO [t96241] VALUES (7,'السماع','10','0','0');
+INSERT INTO [t96241] VALUES (8,'اللعان','11','0','0');
+INSERT INTO [t96241] VALUES (9,'الكذب','12','0','0');
+INSERT INTO [t96241] VALUES (10,'المجادلة والمناظرة','14','0','0');
+INSERT INTO [t96241] VALUES (11,'الشعر','15','0','0');
+INSERT INTO [t96241] VALUES (12,'المدح','17','0','0');
