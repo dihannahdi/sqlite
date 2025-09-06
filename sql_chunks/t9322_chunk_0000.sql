8839f786f15c0df9b2955513fc3e3d1596d3d948
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:52:46 +0700

    Upload SQL chunk: t9322_chunk_0000.sql

---
+++ sql_chunks/t9322_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9322
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9322" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9322
+INSERT INTO [t9322] VALUES (1,'ومن الزيادات المضافة إلى الجزء من الفضائل','137','0','0');
+INSERT INTO [t9322] VALUES (2,'بقية الزيادات عن ابن مالك، في الجزء الثاني من فضائل عثمان بن عفان رضي الله عنه','156','0','0');
