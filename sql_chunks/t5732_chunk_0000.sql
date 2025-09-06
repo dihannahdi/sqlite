64fc3a5b2599e47755e46a4f1b22918fc439bbb7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:53 +0700

    Upload SQL chunk: t5732_chunk_0000.sql

---
+++ sql_chunks/t5732_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5732
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5732" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5732
+INSERT INTO [t5732] VALUES (1,'حديث شعبة','1','0','0');
+INSERT INTO [t5732] VALUES (2,'تم الجزء','124','0','0');
