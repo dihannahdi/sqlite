71a1a64b499ae61e2b73704234bd3ca5ae2e6683
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:31 +0700

    Upload SQL chunk: t95571_chunk_0000.sql

---
+++ sql_chunks/t95571_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t95571
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t95571" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95571
+INSERT INTO [t95571] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t95571] VALUES (2,'المؤلف','4','1','0');
+INSERT INTO [t95571] VALUES (3,'كتاب المجيد في إعراب القرآن المجيد','6','1','0');
+INSERT INTO [t95571] VALUES (4,'مصادره:','6','3','0');
+INSERT INTO [t95571] VALUES (5,'منهجه:','7','3','0');
+INSERT INTO [t95571] VALUES (6,'أهميته:','8','3','0');
+INSERT INTO [t95571] VALUES (7,'مخطوطات الكتاب:','9','3','0');
+INSERT INTO [t95571] VALUES (8,'الأولى- نسخة دار الكتب الظاهرية المرقمة ٥٣٠ (تفسير) وهي الأصل.','9','7','0');
+INSERT INTO [t95571] VALUES (9,'الثانية- نسخة دار الكتب المصرية المرقمة ٢٢٢ (تفسير). وقد رمزنا إليها بالرمز (د).','9','7','0');
+INSERT INTO [t95571] VALUES (10,'إعراب البسملة','17','0','0');
+INSERT INTO [t95571] VALUES (11,'إعراب الفاتحة','30','0','0');
+INSERT INTO [t95571] VALUES (12,'ثبت المصادر والمراجع','58','0','0');
