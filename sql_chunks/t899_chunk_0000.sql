b4097f6c13911254eb8b5de3ec54d990cfb3e3d6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:12 +0700

    Upload SQL chunk: t899_chunk_0000.sql

---
+++ sql_chunks/t899_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t899
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t899" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t899
+INSERT INTO [t899] VALUES (1,'حقيقة البلوتوث','1','0','0');
+INSERT INTO [t899] VALUES (2,'حقيقة التسمية بهذا الاسم','2','0','0');
+INSERT INTO [t899] VALUES (3,'ماذا يخبئ لنا البلوتوث؟','3','0','0');
+INSERT INTO [t899] VALUES (4,'قديم جديد','4','0','0');
+INSERT INTO [t899] VALUES (5,'البلوتوث نعمة أم نقمة؟','5','0','0');
+INSERT INTO [t899] VALUES (6,'بندقية البلوتوث','6','0','0');
+INSERT INTO [t899] VALUES (7,'لست ضد البلوتوث','7','0','0');
+INSERT INTO [t899] VALUES (8,'رسالة عاجلة:','8','0','0');
+INSERT INTO [t899] VALUES (9,'إحصائيات وأرقام:','9','0','0');
+INSERT INTO [t899] VALUES (10,'net. البلوتوث. www = Com. فضائح.www','10','0','0');
+INSERT INTO [t899] VALUES (11,'حذف ٧٠ ألف ملف بلوتوث!!','11','0','0');
+INSERT INTO [t899] VALUES (12,'معادلة ١٠٠%','12','0','0');
+INSERT INTO [t899] VALUES (13,'الطالبة الشريرة:','13','0','0');
+INSERT INTO [t899] VALUES (14,'مراهقة شاب!!','14','0','0');
+INSERT INTO [t899] VALUES (15,'عندما تفحصت جهاز زوجي:','15','0','0');
+INSERT INTO [t899] VALUES (16,'قتلتني خادمة أمي','16','0','0');
+INSERT INTO [t899] VALUES (17,'مشاغل الخياطة','17','0','0');
+INSERT INTO [t899] VALUES (18,'لحظات الوضوء','18','0','0');
+INSERT INTO [t899] VALUES (19,'قصور الأفراح','19','0','0');
+INSERT INTO [t899] VALUES (20,'جميلة اسم على مسمى!!','20','0','0');
+INSERT INTO [t899] VALUES (21,'المشكلة عآمة','21','0','0');
+INSERT INTO [t899] VALUES (22,'اللقطات الفكاهية','22','0','0');
+INSERT INTO [t899] VALUES (23,'قتلت نفسها والسبب لقطة بلوتوث!!','23','0','0');
+INSERT INTO [t899] VALUES (24,'البلوتوث الدعوي','24','0','0');
+INSERT INTO [t899] VALUES (25,'طلب ورجاء','25','0','0');
