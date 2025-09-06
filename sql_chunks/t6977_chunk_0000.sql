cbfc723190afea8da25e01f1a927d5c96ab3ee2f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:21 +0700

    Upload SQL chunk: t6977_chunk_0000.sql

---
+++ sql_chunks/t6977_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t6977
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t6977" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6977
+INSERT INTO [t6977] VALUES (1,'خطبة الكتاب','1','0','0');
+INSERT INTO [t6977] VALUES (2,'مقدمة','3','0','0');
+INSERT INTO [t6977] VALUES (3,'حرف الهمزة','5','0','0');
+INSERT INTO [t6977] VALUES (4,'حرف الباء','20','0','0');
+INSERT INTO [t6977] VALUES (5,'حرف التاء','22','0','0');
+INSERT INTO [t6977] VALUES (6,'حرف الثاء','24','0','0');
+INSERT INTO [t6977] VALUES (7,'حرف الجيم','26','0','0');
+INSERT INTO [t6977] VALUES (8,'حرف الحاء المهملة','30','0','0');
+INSERT INTO [t6977] VALUES (9,'حرف الخاء المعجمة','34','0','0');
+INSERT INTO [t6977] VALUES (10,'حرف الدال المهملة','36','0','0');
+INSERT INTO [t6977] VALUES (11,'حرف الذال المعجمة','38','0','0');
+INSERT INTO [t6977] VALUES (12,'حرف الراء','39','0','0');
+INSERT INTO [t6977] VALUES (13,'حرف الزاي','42','0','0');
+INSERT INTO [t6977] VALUES (14,'حرف السين المهملة','44','0','0');
+INSERT INTO [t6977] VALUES (15,'حرف الشين المعجمة','47','0','0');
+INSERT INTO [t6977] VALUES (16,'حرف الصاد المهملة','49','0','0');
+INSERT INTO [t6977] VALUES (17,'حرف الضاد المعجمة','50','0','0');
+INSERT INTO [t6977] VALUES (18,'حرف الطاء المهملة طب الإنسان بتشديد الموحدة مبني للمجهول معناه سحر قاله ابن القوطية طحل بالحاء المهملة واللام كعني طحلا من طحالة وأما عظم الطحال فيقال فيه طحل كفرح فهو طحل وكذلك يقال في الماء إذا فسد وأنتن من حمأ طرفت العين','52','0','0');
+INSERT INTO [t6977] VALUES (19,'حرف الظاء المعجمة','56','0','0');
+INSERT INTO [t6977] VALUES (20,'حرف العين المهمة','57','0','0');
+INSERT INTO [t6977] VALUES (21,'حرف الغين المعجمة','60','0','0');
+INSERT INTO [t6977] VALUES (22,'حرف الفاء','62','0','0');
+INSERT INTO [t6977] VALUES (23,'حرف القاف','64','0','0');
+INSERT INTO [t6977] VALUES (24,'حرف الكاف','66','0','0');
+INSERT INTO [t6977] VALUES (25,'حرف اللام','68','0','0');
+INSERT INTO [t6977] VALUES (26,'حرف الميم','70','0','0');
+INSERT INTO [t6977] VALUES (27,'حرف النون','74','0','0');
+INSERT INTO [t6977] VALUES (28,'حرف الهاء','79','0','0');
+INSERT INTO [t6977] VALUES (29,'حرف الواو','81','0','0');
+INSERT INTO [t6977] VALUES (30,'حرف الياء المثناة التحتية','84','0','0');
+INSERT INTO [t6977] VALUES (31,'خاتمة','84','0','0');
+INSERT INTO [t6977] VALUES (32,'قال باب فعل بالضم','84','0','0');
