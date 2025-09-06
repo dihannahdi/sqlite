ae1417a6ed1947b6076191f0d7b9e2e310ee1c88
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:13 +0700

    Upload SQL chunk: t147093_chunk_0000.sql

---
+++ sql_chunks/t147093_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t147093
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t147093" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t147093
+INSERT INTO [t147093] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t147093] VALUES (2,'المسألة الأولى: الأكل والشرب حتى بعد طلوع الفجر الصادق','1','0','0');
+INSERT INTO [t147093] VALUES (3,'المسألة الثانية: الإمساك قبل أذان الفجر الصادق','4','0','0');
+INSERT INTO [t147093] VALUES (4,'المسألة الثالثة: تحريم تقديم الأذان وتأخيره عن وقته.','5','0','0');
+INSERT INTO [t147093] VALUES (5,'المسألة الرابعة: ما الحكم إذا تبين أن المؤذن يتقدم أو يتأخر، أو أن التقويم غير صحيح؟ !','6','0','0');
