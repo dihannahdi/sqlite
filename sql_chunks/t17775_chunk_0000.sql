6c242b35f10e74dc451b88ff373813f54fc709cb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:25 +0700

    Upload SQL chunk: t17775_chunk_0000.sql

---
+++ sql_chunks/t17775_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t17775
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t17775" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17775
+INSERT INTO [t17775] VALUES (1,'القاعدة الأولى','12','0','0');
+INSERT INTO [t17775] VALUES (2,'القاعدة الثانية','14','0','0');
+INSERT INTO [t17775] VALUES (3,'القاعدة الثالثة','18','0','0');
+INSERT INTO [t17775] VALUES (4,'القاعدة الرابعة','22','0','0');
