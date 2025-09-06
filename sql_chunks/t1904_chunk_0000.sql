424e4903406638db34bbb57e45a1ac55f3b1d457
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:54 +0700

    Upload SQL chunk: t1904_chunk_0000.sql

---
+++ sql_chunks/t1904_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t1904
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t1904" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1904
+INSERT INTO [t1904] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1904] VALUES (2,'أولا آداب دخول المنزل والخروج منه','3','0','0');
+INSERT INTO [t1904] VALUES (3,'ثانيا آداب الطعام والشراب','6','0','0');
+INSERT INTO [t1904] VALUES (4,'آداب الطعام','6','3','0');
+INSERT INTO [t1904] VALUES (5,'آداب الشراب','9','3','0');
+INSERT INTO [t1904] VALUES (6,'ثالثا آداب العطاس والتثاؤب','13','0','0');
+INSERT INTO [t1904] VALUES (7,'رابعا آداب الخلاء','16','0','0');
+INSERT INTO [t1904] VALUES (8,'خامسا آداب النوم والاستيقاظ','21','0','0');
+INSERT INTO [t1904] VALUES (9,'سادسا آداب اللباس والزينة والمظهر','24','0','0');
+INSERT INTO [t1904] VALUES (10,'سابعا آداب الطريق','34','0','0');
+INSERT INTO [t1904] VALUES (11,'ثامنا آداب المسجد','37','0','0');
