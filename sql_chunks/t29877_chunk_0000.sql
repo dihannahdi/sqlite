e1d2572a4e334c2464d3d45d4a72861a9836a765
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:58:18 +0700

    Upload SQL chunk: t29877_chunk_0000.sql

---
+++ sql_chunks/t29877_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t29877
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t29877" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29877
+INSERT INTO [t29877] VALUES (1,'مقدمة المحقق','2','0','0');
+INSERT INTO [t29877] VALUES (2,'وصف المخطوطة','4','0','0');
+INSERT INTO [t29877] VALUES (3,'مقدمة وسبب التأليف','5','0','0');
+INSERT INTO [t29877] VALUES (4,'حديث عبد الله بن عمر','5','0','0');
+INSERT INTO [t29877] VALUES (5,'حديث أبي سعيد الخدري رضي الله عنه','6','0','0');
+INSERT INTO [t29877] VALUES (6,'حديث أبى هريرة رضى الله عنه','6','0','0');
+INSERT INTO [t29877] VALUES (7,'حديث أبى هريرة المسبل في الصلاة','7','0','0');
+INSERT INTO [t29877] VALUES (8,'حديث أبى جحيفة','9','0','0');
+INSERT INTO [t29877] VALUES (9,'حديث ابن عمر رضى الله عنه','10','0','0');
+INSERT INTO [t29877] VALUES (10,'تفسير الإسبال والسدل','12','0','0');
+INSERT INTO [t29877] VALUES (11,'تحرير المقال فى الاسبال','14','0','0');
+INSERT INTO [t29877] VALUES (12,'فى الصلاة','14','11','0');
+INSERT INTO [t29877] VALUES (13,'كلام ابن حزم في بطلان صلاة المسبل','14','12','0');
+INSERT INTO [t29877] VALUES (14,'صلاة العبد الآبق','18','12','0');
+INSERT INTO [t29877] VALUES (15,'العلاقة بين انتفاء القبول وانتفاء الصحة','19','12','0');
+INSERT INTO [t29877] VALUES (16,'الإسبال في غير الصلاة','21','11','0');
+INSERT INTO [t29877] VALUES (17,'حديث أبى بكر رضى الله عنه','23','0','0');
+INSERT INTO [t29877] VALUES (18,'القول بمفهوم الصفة','25','0','0');
+INSERT INTO [t29877] VALUES (19,'الرد على ابن حجر الهيتمي في إدعائه إباحة الإسبال لأنه صار شعار العلماء','28','0','0');
+INSERT INTO [t29877] VALUES (20,'خاتمة','31','0','0');
