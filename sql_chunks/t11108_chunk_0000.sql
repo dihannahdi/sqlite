bda3d5d1baf42af22d59e11141338912d12cf78b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:21 +0700

    Upload SQL chunk: t11108_chunk_0000.sql

---
+++ sql_chunks/t11108_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t11108
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t11108" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11108
+INSERT INTO [t11108] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11108] VALUES (2,'مايجب على المريض','3','0','0');
+INSERT INTO [t11108] VALUES (3,'تلقين المحتضر','7','0','0');
+INSERT INTO [t11108] VALUES (4,'ماعلى الحاضرين بعد موته','9','0','0');
+INSERT INTO [t11108] VALUES (5,'ما يجوز للحاضرين وغيرهم','11','0','0');
+INSERT INTO [t11108] VALUES (6,'ما يجب على أقارب الميت','12','0','0');
+INSERT INTO [t11108] VALUES (7,'ما يحرم على أقارب الميت','14','0','0');
+INSERT INTO [t11108] VALUES (8,'النعي الجائز','16','0','0');
+INSERT INTO [t11108] VALUES (9,'علامات حسن الخاتمة','17','0','0');
+INSERT INTO [t11108] VALUES (10,'ثناء الناس على الميت','21','0','0');
+INSERT INTO [t11108] VALUES (11,'غسل الميت','24','0','0');
+INSERT INTO [t11108] VALUES (12,'تكفين الميت','30','0','0');
+INSERT INTO [t11108] VALUES (13,'حمل الجنازة واتباعها','34','0','0');
+INSERT INTO [t11108] VALUES (14,'الصلاة على الجنازة','40','0','0');
+INSERT INTO [t11108] VALUES (15,'صفة صلاة الجنازة','50','0','0');
+INSERT INTO [t11108] VALUES (16,'الدفن وتوابعه','54','0','0');
+INSERT INTO [t11108] VALUES (17,'التعزية','66','0','0');
