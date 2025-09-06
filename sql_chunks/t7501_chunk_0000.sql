9f7c331d2ae42beb0e8cea0111528d61a7289260
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:05 +0700

    Upload SQL chunk: t7501_chunk_0000.sql

---
+++ sql_chunks/t7501_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t7501
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t7501" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7501
+INSERT INTO [t7501] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7501] VALUES (2,'أسئلة وأجوبة في إعراب القرآن','7','0','0');
+INSERT INTO [t7501] VALUES (3,'مصادر ومراجع','49','0','0');
