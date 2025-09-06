eac0f548d6d2a327a41b1453d71ebd29f64dfcbe
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:40 +0700

    Upload SQL chunk: t6998_chunk_0000.sql

---
+++ sql_chunks/t6998_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6998
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6998" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6998
+INSERT INTO [t6998] VALUES (1,'قال الشنفرى الأزدي','1','0','0');
+INSERT INTO [t6998] VALUES (2,'تمت القصيدة بشرحها','92','0','0');
