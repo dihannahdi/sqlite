f359272c1fbc7ffd49ff90232956451069a5281e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:53:57 +0700

    Upload SQL chunk: t9495_chunk_0000.sql

---
+++ sql_chunks/t9495_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t9495
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t9495" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9495
+INSERT INTO [t9495] VALUES (1,'نهي الصحبة عن النزول بالركبة','8','0','0');
+INSERT INTO [t9495] VALUES (2,'الوجه الأول','8','1','0');
+INSERT INTO [t9495] VALUES (3,'الوجه الثاني','12','1','0');
+INSERT INTO [t9495] VALUES (4,'الوجه الثالث','12','1','0');
+INSERT INTO [t9495] VALUES (5,'الوجه الرابع','13','1','0');
+INSERT INTO [t9495] VALUES (6,'الوجه الخامس','15','1','0');
+INSERT INTO [t9495] VALUES (7,'الوجه السادس','16','1','0');
+INSERT INTO [t9495] VALUES (8,'الوجه السابع','17','1','0');
+INSERT INTO [t9495] VALUES (9,'الوجه الثامن','18','1','0');
+INSERT INTO [t9495] VALUES (10,'الشاهد الأول','19','9','0');
+INSERT INTO [t9495] VALUES (11,'الشاهد الثاني','21','9','0');
+INSERT INTO [t9495] VALUES (12,'الشاهد الثالث','22','9','0');
+INSERT INTO [t9495] VALUES (13,'الشاهد الرابع','22','9','0');
+INSERT INTO [t9495] VALUES (14,'الوجه التاسع','24','1','0');
+INSERT INTO [t9495] VALUES (15,'فصل','25','1','0');
+INSERT INTO [t9495] VALUES (16,'تنبيهات','26','1','0');
+INSERT INTO [t9495] VALUES (17,'الأول','26','16','0');
+INSERT INTO [t9495] VALUES (18,'الثاني','27','16','0');
+INSERT INTO [t9495] VALUES (19,'الثالث','28','16','0');
+INSERT INTO [t9495] VALUES (20,'الرابع','29','16','0');
+INSERT INTO [t9495] VALUES (21,'الخامس','29','16','0');
+INSERT INTO [t9495] VALUES (22,'السادس','29','16','0');
+INSERT INTO [t9495] VALUES (23,'السابع:','30','16','0');
+INSERT INTO [t9495] VALUES (24,'أبحاث أخرى في المسألة','31','0','0');
+INSERT INTO [t9495] VALUES (25,'فتح الودود في مسألة النزول للسجود','32','24','0');
+INSERT INTO [t9495] VALUES (26,'جلاء العينين عن النزول بالركبتين','55','24','0');
+INSERT INTO [t9495] VALUES (27,'أيهما تقدم اليدين أم الركبتين حال السجود','62','24','0');
+INSERT INTO [t9495] VALUES (28,'المصلي عند نزوله من الركوع إلى السجود، هل يكون على يديه أم ركبتيه','75','24','0');
+INSERT INTO [t9495] VALUES (29,'ماذا يقدم المصلي إذا سجد','86','24','0');
