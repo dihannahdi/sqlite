19ef94aa945d094846e0707d4b3ab2920a269a20
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:30 +0700

    Upload SQL chunk: t6988_chunk_0000.sql

---
+++ sql_chunks/t6988_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t6988
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t6988" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6988
+INSERT INTO [t6988] VALUES (1,'سألني بعض الإخوان وأنا على جناح السفر','1','0','0');
