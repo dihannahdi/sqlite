c12b3d2800e5ec16517dd28a426541e8af9c7b37
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:39 +0700

    Upload SQL chunk: t21557_chunk_0000.sql

---
+++ sql_chunks/t21557_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t21557
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t21557" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21557
+INSERT INTO [t21557] VALUES (1,'المقدمة','3','0','0');
+INSERT INTO [t21557] VALUES (2,'المبحث الأول: تعريف التجويد لغة واصطلاحا','5','0','0');
+INSERT INTO [t21557] VALUES (3,'المبحث الثاني: نشأة التجويد','7','0','0');
+INSERT INTO [t21557] VALUES (4,'المبحث الثالث: عناية الأمة بالأخذ بالتجويد','18','0','0');
+INSERT INTO [t21557] VALUES (5,'المبحث الرابع: كيف يتلقى القرآن','32','0','0');
+INSERT INTO [t21557] VALUES (6,'المبحث الخامس: حكم الأخذ بالتجويد','45','0','0');
+INSERT INTO [t21557] VALUES (7,'المبحث السادس: اللحن في القراءة','52','0','0');
+INSERT INTO [t21557] VALUES (8,'الخاتمة','61','0','0');
+INSERT INTO [t21557] VALUES (9,'مصادر ومراجع','63','0','0');
