e5910304e5eb1b91411b1713252fd4b784317c3d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:42 +0700

    Upload SQL chunk: t36512_chunk_0000.sql

---
+++ sql_chunks/t36512_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t36512
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t36512" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36512
+INSERT INTO [t36512] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36512] VALUES (2,'الخاتمة','10','0','0');
