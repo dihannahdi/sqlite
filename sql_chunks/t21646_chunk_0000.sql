c10180241137554b78e89f39ac5c427c9dc51bc0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:56 +0700

    Upload SQL chunk: t21646_chunk_0000.sql

---
+++ sql_chunks/t21646_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21646
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21646" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21646
+INSERT INTO [t21646] VALUES (1,'طوالات الأخبار لأبي موسى المديني - مخطوط','2','0','0');
