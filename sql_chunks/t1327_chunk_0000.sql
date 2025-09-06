aefc4540ed6b4c943ed870762a642bb673f0e558
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:13:13 +0700

    Upload SQL chunk: t1327_chunk_0000.sql

---
+++ sql_chunks/t1327_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t1327
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t1327" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1327
+INSERT INTO [t1327] VALUES (1,'من الأدب الرمزي','2','0','0');
+INSERT INTO [t1327] VALUES (2,'تعدد الزوجات في الشرائع السابقة','5','0','0');
+INSERT INTO [t1327] VALUES (3,'حوار هادئ:','6','0','0');
+INSERT INTO [t1327] VALUES (4,'هل تعدد الزوجات يمثل ظاهرة في بلادنا؟','8','0','0');
+INSERT INTO [t1327] VALUES (5,'الإسلام قيد التعدد:','10','0','0');
+INSERT INTO [t1327] VALUES (6,'(اعدلوا هو أقرب للتقوى)','12','0','0');
+INSERT INTO [t1327] VALUES (7,'العرض المادي هل يسقط القسمة؟','13','0','0');
+INSERT INTO [t1327] VALUES (8,'المساواة في غير القسمة:','14','0','0');
+INSERT INTO [t1327] VALUES (9,'متاعب التعدد وموضوع الغيرة:','16','0','0');
+INSERT INTO [t1327] VALUES (10,'الغيرة دليل على الحب','17','0','0');
+INSERT INTO [t1327] VALUES (11,'التعدد حل لمشلكة طبيعية:','18','0','0');
+INSERT INTO [t1327] VALUES (12,'نماذج من كتاب دكتور شحرور:','27','0','0');
+INSERT INTO [t1327] VALUES (13,'مفهوم العورة في السنة:','29','0','0');
+INSERT INTO [t1327] VALUES (14,'* (فماذا بعد الحق إلا الضلال)','35','0','0');
+INSERT INTO [t1327] VALUES (15,'العدل أم الإحسان؟','38','0','0');
+INSERT INTO [t1327] VALUES (16,'العدل بين الأولاد:','40','0','0');
+INSERT INTO [t1327] VALUES (17,'* الهبة لسبب:','41','0','0');
+INSERT INTO [t1327] VALUES (18,'خامسا: مراحل حياة النبي - صلى الله عليه وسلم - من ناحية الزواج','45','0','0');
+INSERT INTO [t1327] VALUES (19,'ليله صلاة وعبادة ونهاره دعوة وجهاد','46','0','0');
