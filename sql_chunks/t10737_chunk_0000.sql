5eece5d82471fc71243c5791924c37ff1a02666e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:03 +0700

    Upload SQL chunk: t10737_chunk_0000.sql

---
+++ sql_chunks/t10737_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t10737
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t10737" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10737
+INSERT INTO [t10737] VALUES (1,'خطبة مصنف الكتاب','1','0','0');
+INSERT INTO [t10737] VALUES (2,'مصنفات ابن الجوزى','61','0','0');
+INSERT INTO [t10737] VALUES (3,'عودة إلى كتاب المفردات','73','0','0');
+INSERT INTO [t10737] VALUES (4,'مصنفات ابن قدامة الحنبلي','80','0','0');
+INSERT INTO [t10737] VALUES (5,'فصل في كتب شيخ الإسلام تقى الدين أو ابن تيمية','102','0','0');
