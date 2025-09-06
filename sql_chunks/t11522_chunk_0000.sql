4bc9bf537b176182df996e9fc87c35801a53d17a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:09 +0700

    Upload SQL chunk: t11522_chunk_0000.sql

---
+++ sql_chunks/t11522_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t11522
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t11522" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11522
+INSERT INTO [t11522] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t11522] VALUES (2,'١ - اختيار الأم','5','0','0');
+INSERT INTO [t11522] VALUES (3,'نماذج تخرجت من مدرسة الأم','6','2','0');
+INSERT INTO [t11522] VALUES (4,'٢ - الدعاء','18','0','0');
+INSERT INTO [t11522] VALUES (5,'٣ - أذكار البناء','19','0','0');
+INSERT INTO [t11522] VALUES (6,'٤ - أذكار الجماع','20','0','0');
+INSERT INTO [t11522] VALUES (7,'٥ - التأذين في أذن المولود','21','0','0');
+INSERT INTO [t11522] VALUES (8,'٦ - تحنيك المولود','23','0','0');
+INSERT INTO [t11522] VALUES (9,'٧ - اختيار الاسم الحسن','25','0','0');
+INSERT INTO [t11522] VALUES (10,'٨ - العقيقة','26','0','0');
+INSERT INTO [t11522] VALUES (11,'٩ - التربية الإسلامية المتكاملة','27','0','0');
+INSERT INTO [t11522] VALUES (12,'أصول التربية','32','11','0');
+INSERT INTO [t11522] VALUES (13,'أولا: التربية الإيمانية','32','12','0');
+INSERT INTO [t11522] VALUES (14,'ثانيا: التربية الخلقية','38','12','0');
+INSERT INTO [t11522] VALUES (15,'الأخلاق الهابطة','41','14','0');
+INSERT INTO [t11522] VALUES (16,'١ - خلق الكذب','41','15','0');
+INSERT INTO [t11522] VALUES (17,'٢ - خلق السرقة','42','15','0');
+INSERT INTO [t11522] VALUES (18,'٣ - خلق السباب والشتائم','43','15','0');
+INSERT INTO [t11522] VALUES (19,'٤ - خلق الميوعة والانحلال','46','15','0');
+INSERT INTO [t11522] VALUES (20,'ثالثا: التربية الفكرية','49','12','0');
+INSERT INTO [t11522] VALUES (21,'رابعا: التربية الجسمية','51','12','0');
+INSERT INTO [t11522] VALUES (22,'وأخيرا','52','12','0');
