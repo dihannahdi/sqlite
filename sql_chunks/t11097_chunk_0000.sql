9b212abdb02b68d05fc0f7ec3d8d27a3332aea28
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:13 +0700

    Upload SQL chunk: t11097_chunk_0000.sql

---
+++ sql_chunks/t11097_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t11097
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t11097" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11097
+INSERT INTO [t11097] VALUES (1,'مقدمه','1','0','0');
+INSERT INTO [t11097] VALUES (2,'مدخل','3','0','0');
+INSERT INTO [t11097] VALUES (3,'باب فيمن أكل ثم خرج سفرا','4','0','0');
+INSERT INTO [t11097] VALUES (4,'يقول محمد ناصر الدين','16','0','0');
+INSERT INTO [t11097] VALUES (5,'شهادة القرآن للحديث','36','0','0');
+INSERT INTO [t11097] VALUES (6,'شاهد للحديث من السنة','38','0','0');
+INSERT INTO [t11097] VALUES (7,'آثار صحيحة تشهد للحديث','42','0','0');
+INSERT INTO [t11097] VALUES (8,'الخلاصة','59','0','0');
+INSERT INTO [t11097] VALUES (9,'الخاتمة','61','0','0');
