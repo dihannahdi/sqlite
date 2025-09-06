5d739ad42cbc9372df5fb99cd2d01cce7cce55fc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:14:11 +0700

    Upload SQL chunk: t37579_chunk_0000.sql

---
+++ sql_chunks/t37579_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t37579
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t37579" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37579
+INSERT INTO [t37579] VALUES (1,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t37579] VALUES (2,'قد جاءت السنة والآثار بل وآيات الكتاب ببيان الأمر على حقيقته','3','0','0');
+INSERT INTO [t37579] VALUES (3,'كتبه/ عبد الكريم بن صالح الحميد','27','0','0');
