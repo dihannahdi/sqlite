0b351f32bdfbb3db02721e23db8e6238e8a1a163
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:01:05 +0700

    Upload SQL chunk: t9766_chunk_0000.sql

---
+++ sql_chunks/t9766_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t9766
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t9766" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9766
+INSERT INTO [t9766] VALUES (1,'الفصل الأول الإيمان بالقدر من أصول الإيمان','1','0','0');
+INSERT INTO [t9766] VALUES (2,'الفصل الثاني نظرة في تاريخ القدر','5','0','0');
+INSERT INTO [t9766] VALUES (3,'الفصل الثالث التعريف بالقضاء والقدر','10','0','0');
+INSERT INTO [t9766] VALUES (4,'الفصل الرابع حدود نظر العقل في القدر','33','0','0');
+INSERT INTO [t9766] VALUES (5,'الفصل الخامس المذاهب في القدر','40','0','0');
+INSERT INTO [t9766] VALUES (6,'الفصل السادس أسباب الضلال في القدر','91','0','0');
+INSERT INTO [t9766] VALUES (7,'الفصل السابع ثمار الإيمان بالقدر','95','0','0');
