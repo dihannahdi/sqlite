183d1b6dd814ba2784d3cf2c40ec4ae49c72da93
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:46 +0700

    Upload SQL chunk: t21565_chunk_0000.sql

---
+++ sql_chunks/t21565_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t21565
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t21565" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21565
+INSERT INTO [t21565] VALUES (1,'جواب في الحلف بغير الله والصلاة إلى القبور','2','0','0');
+INSERT INTO [t21565] VALUES (2,'أما الحلف بغير الله:','3','1','0');
+INSERT INTO [t21565] VALUES (3,'وأما الصلاة عند القبور، والصلاة إليها، أو اتخاذ المساجد على القبور، أو إيقاد المصابيح عليها:','9','1','0');
+INSERT INTO [t21565] VALUES (4,'فصل في الاستغاثة','20','0','0');
