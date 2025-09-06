afb6f2850fa8a71ec90cf578d4efe187b9bf30ad
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:42 +0700

    Upload SQL chunk: t1964_chunk_0000.sql

---
+++ sql_chunks/t1964_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t1964
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t1964" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1964
+INSERT INTO [t1964] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t1964] VALUES (2,'الأمر بالمعروف والنهي عن المنكر','5','0','0');
+INSERT INTO [t1964] VALUES (3,'اختزال الأمة لمصلحة من','9','0','0');
+INSERT INTO [t1964] VALUES (4,'إخوان العلانية أعداء السريرة','17','0','0');
+INSERT INTO [t1964] VALUES (5,'ناصحوا ولاة الأمر وادعوا لهم بالصلاح','20','0','0');
+INSERT INTO [t1964] VALUES (6,'النخبة خطر يهدد الأجيال','23','0','0');
+INSERT INTO [t1964] VALUES (7,'الاستلاب الفكري','29','0','0');
+INSERT INTO [t1964] VALUES (8,'معالم في طريق الأمة الواحدة','36','0','0');
+INSERT INTO [t1964] VALUES (9,'الخاتمة','45','0','0');
