9da48abb7b5040075c5fe0cf0fab4ce84ecb28a8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:50 +0700

    Upload SQL chunk: t96849_chunk_0000.sql

---
+++ sql_chunks/t96849_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t96849
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t96849" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96849
+INSERT INTO [t96849] VALUES (1,'[مقدمة التحقيق]','2','0','0');
+INSERT INTO [t96849] VALUES (2,'عملي في المخطوط','2','1','0');
+INSERT INTO [t96849] VALUES (3,'حول الرسالة','3','1','0');
+INSERT INTO [t96849] VALUES (4,'وصف النسخ الخطية','3','3','0');
+INSERT INTO [t96849] VALUES (5,'توثيق نسبتها للمؤلف','6','3','0');
+INSERT INTO [t96849] VALUES (6,'ترجمة المؤلف','6','1','0');
+INSERT INTO [t96849] VALUES (7,'تصانيفه','7','6','0');
+INSERT INTO [t96849] VALUES (8,'إتحاف ذوي الإتقان بحكم الرهان','8','0','0');
