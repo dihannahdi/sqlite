65cacbd46ed573567275ec33dbc9786fa418677b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:04 +0700

    Upload SQL chunk: t6079_chunk_0000.sql

---
+++ sql_chunks/t6079_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t6079
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t6079" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6079
+INSERT INTO [t6079] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t6079] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6079] VALUES (3,'حرف الألف','3','0','0');
+INSERT INTO [t6079] VALUES (4,'حرف الباء','19','0','0');
+INSERT INTO [t6079] VALUES (5,'حرف التاء','22','0','0');
+INSERT INTO [t6079] VALUES (6,'حرف الثاء','25','0','0');
+INSERT INTO [t6079] VALUES (7,'حرف الجيم','26','0','0');
+INSERT INTO [t6079] VALUES (8,'حرف الحاء','28','0','0');
+INSERT INTO [t6079] VALUES (9,'حرف الخاء','31','0','0');
+INSERT INTO [t6079] VALUES (10,'حرف الدال','35','0','0');
+INSERT INTO [t6079] VALUES (11,'حرف الذال','38','0','0');
+INSERT INTO [t6079] VALUES (12,'حرف الراء','39','0','0');
+INSERT INTO [t6079] VALUES (13,'حرف الزاي','42','0','0');
+INSERT INTO [t6079] VALUES (14,'حرف السين','43','0','0');
+INSERT INTO [t6079] VALUES (15,'حرف الشين','45','0','0');
+INSERT INTO [t6079] VALUES (16,'حرف الصاد','48','0','0');
+INSERT INTO [t6079] VALUES (17,'حرف الضاد','52','0','0');
+INSERT INTO [t6079] VALUES (18,'حرف الطاء','53','0','0');
+INSERT INTO [t6079] VALUES (19,'حرف الظاء','55','0','0');
+INSERT INTO [t6079] VALUES (20,'حرف العين','56','0','0');
+INSERT INTO [t6079] VALUES (21,'حرف الغين','63','0','0');
+INSERT INTO [t6079] VALUES (22,'حرف الفاء','64','0','0');
+INSERT INTO [t6079] VALUES (23,'حرف القاف','67','0','0');
+INSERT INTO [t6079] VALUES (24,'حرف الكاف','69','0','0');
+INSERT INTO [t6079] VALUES (25,'فائدة','73','0','0');
+INSERT INTO [t6079] VALUES (26,'حرف اللام','74','0','0');
+INSERT INTO [t6079] VALUES (27,'حرف الميم','81','0','0');
+INSERT INTO [t6079] VALUES (28,'حرف النون','103','0','0');
+INSERT INTO [t6079] VALUES (29,'حرف الهاء','105','0','0');
+INSERT INTO [t6079] VALUES (30,'حرف الواو','106','0','0');
+INSERT INTO [t6079] VALUES (31,'حرف اللام ألف','107','0','0');
+INSERT INTO [t6079] VALUES (32,'حرف الياء','111','0','0');
