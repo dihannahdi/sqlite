64f4b49c9ffda86fda45c6a0e8fa03b4ffa24b13
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:59 +0700

    Upload SQL chunk: t40_chunk_0000.sql

---
+++ sql_chunks/t40_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t40
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t40" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t40
+INSERT INTO [t40] VALUES (1,'أحاديث العطار عن شيوخه','1','0','0');
+INSERT INTO [t40] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
