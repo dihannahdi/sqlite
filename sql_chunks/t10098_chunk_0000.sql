2dca6e9bd9711ec2282d5105e484e5f264e43aac
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:49 +0700

    Upload SQL chunk: t10098_chunk_0000.sql

---
+++ sql_chunks/t10098_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10098
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10098" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10098
+INSERT INTO [t10098] VALUES (1,'تنبيه الحذاق على بطلان حديث النور','1','0','0');
+INSERT INTO [t10098] VALUES (2,'تأليف: محمد أحمد عبد القادر الشنقيطي المدني','1','0','0');
