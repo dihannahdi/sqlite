7d728c224e841e38bc0370405c6a6cc9feb12114
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:50 +0700

    Upload SQL chunk: t1892_chunk_0000.sql

---
+++ sql_chunks/t1892_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t1892
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t1892" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1892
+INSERT INTO [t1892] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t1892] VALUES (2,'حرف الألف','3','0','0');
+INSERT INTO [t1892] VALUES (3,'حرف الباء','50','0','0');
+INSERT INTO [t1892] VALUES (4,'حرف الثاء','60','0','0');
+INSERT INTO [t1892] VALUES (5,'حرف الجيم','63','0','0');
+INSERT INTO [t1892] VALUES (6,'حرف الحاء','73','0','0');
+INSERT INTO [t1892] VALUES (7,'حرف الخاء','101','0','0');
+INSERT INTO [t1892] VALUES (8,'حرف الدال','107','0','0');
+INSERT INTO [t1892] VALUES (9,'حرف الراء','112','0','0');
+INSERT INTO [t1892] VALUES (10,'حرف الزاء','117','0','0');
+INSERT INTO [t1892] VALUES (11,'حرف السين','122','0','0');
+INSERT INTO [t1892] VALUES (12,'حرف الشين','157','0','0');
+INSERT INTO [t1892] VALUES (13,'حرف الصاد','165','0','0');
+INSERT INTO [t1892] VALUES (14,'حرف الضاد','167','0','0');
+INSERT INTO [t1892] VALUES (15,'حرف الطاء','168','0','0');
+INSERT INTO [t1892] VALUES (16,'حرف العين','171','0','0');
+INSERT INTO [t1892] VALUES (17,'حرف الغين','276','0','0');
+INSERT INTO [t1892] VALUES (18,'حرف الفاء','277','0','0');
+INSERT INTO [t1892] VALUES (19,'حرف القاف','281','0','0');
+INSERT INTO [t1892] VALUES (20,'حرف الكاف','289','0','0');
+INSERT INTO [t1892] VALUES (21,'حرف الميم','292','0','0');
+INSERT INTO [t1892] VALUES (22,'حرف النون','351','0','0');
+INSERT INTO [t1892] VALUES (23,'حرف الهاء','354','0','0');
+INSERT INTO [t1892] VALUES (24,'حرف الواو','362','0','0');
+INSERT INTO [t1892] VALUES (25,'حرف الياء','369','0','0');
+INSERT INTO [t1892] VALUES (26,'الكنى','394','0','0');
