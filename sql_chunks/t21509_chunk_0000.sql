424860ca0842e45046a2b2121744cef12c3b56a4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:55 +0700

    Upload SQL chunk: t21509_chunk_0000.sql

---
+++ sql_chunks/t21509_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t21509
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t21509" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21509
+INSERT INTO [t21509] VALUES (1,'شرح متن الآجرومية (١)','2','0','0');
+INSERT INTO [t21509] VALUES (2,'شرح متن الآجرومية (٢)','28','0','0');
+INSERT INTO [t21509] VALUES (3,'شرح متن الآجرومية (٣)','54','0','0');
+INSERT INTO [t21509] VALUES (4,'شرح متن الآجرومية (٤)','82','0','0');
+INSERT INTO [t21509] VALUES (5,'شرح متن الآجرومية (٥)','108','0','0');
+INSERT INTO [t21509] VALUES (6,'شرح متن الآجرومية (٦)','136','0','0');
+INSERT INTO [t21509] VALUES (7,'شرح متن الآجرومية (٧)','164','0','0');
+INSERT INTO [t21509] VALUES (8,'شرح متن الآجرومية (٨)','184','0','0');
