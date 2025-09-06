a136dce5eca63f5e3c93e435be844476b4237bb9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:47 +0700

    Upload SQL chunk: t3598_chunk_0000.sql

---
+++ sql_chunks/t3598_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t3598
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t3598" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3598
+INSERT INTO [t3598] VALUES (1,'الأزمنة البسيطة','6','0','0');
+INSERT INTO [t3598] VALUES (2,'١ـ الماضي المطلق: THE PAST UNCONDITIONAL','6','1','0');
+INSERT INTO [t3598] VALUES (3,'٢ـ الحال المطلق THE PRESENT UNCONDITIONAL TENSE','7','1','0');
+INSERT INTO [t3598] VALUES (4,'٣ ـ المستقبل المطلق: THE FUTURE UNCONDITIONAL TENSE','7','1','0');
+INSERT INTO [t3598] VALUES (5,'الأزمنة المركبة','8','0','0');
+INSERT INTO [t3598] VALUES (6,'الماضي المقيد: THE PAST CONDITIONAL TENSE','9','5','0');
+INSERT INTO [t3598] VALUES (7,'الباب الأول من الماضي المقيد: الماضي القريب: THE PERESENT PERFECT TENSE','10','6','0');
+INSERT INTO [t3598] VALUES (8,'الباب الثاني من الماضي المقيد: الماضي الجازم:','10','6','0');
+INSERT INTO [t3598] VALUES (9,'الباب الثالث من الماضي المقيد: الماضي الروائي: THE PAST CONTINUOUS TENSE','12','6','0');
+INSERT INTO [t3598] VALUES (10,'حكاية الماضي الروائي: THE PAST PERFECT TENSE','13','9','0');
+INSERT INTO [t3598] VALUES (11,'الحال المقيد: THE PRESENT CONTIDIONAL TENSE','13','5','0');
+INSERT INTO [t3598] VALUES (12,'المستقبل المقيد: THE FTURE CONDITIONAL TENSES','13','5','0');
+INSERT INTO [t3598] VALUES (13,'الباب الأول منه، المستقبل المعلق: THE FTURE BOUND TENSE','13','12','0');
+INSERT INTO [t3598] VALUES (14,'الباب الثاني من المستقبل المقيد: المستقبل الروائي: THE FUTURE PERFECT TENSE','14','12','0');
+INSERT INTO [t3598] VALUES (15,'الباب الثالث من المستقبل المقيد: المستقبل السلبي الجازم: THE FUTURE NEGATIVE TENSE','14','12','0');
+INSERT INTO [t3598] VALUES (16,'الباب الرابع من المستقبل المقيد: حكاية المستقبل الروائي: THE PAST FUTURE PERFECT TENSE','14','12','0');
