c0ebe601d1c5c21e0e2127344988a8fd333ee709
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:28 +0700

    Upload SQL chunk: t96734_chunk_0000.sql

---
+++ sql_chunks/t96734_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t96734
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t96734" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96734
+INSERT INTO [t96734] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t96734] VALUES (2,'بيان أن الجمعة والعيد كانا إذا اجتمعا في يوم واحد صلى رسول الله صلى الله عليه وسلم الصلاتين كليهما','3','0','0');
+INSERT INTO [t96734] VALUES (3,'بيان الروايات الواهيات التي رخصت لمن صلي العيد في ترك فريضة الجمعة','4','0','0');
+INSERT INTO [t96734] VALUES (4,'[١] حديث أبي هريرة رضي الله عنه','4','3','0');
+INSERT INTO [t96734] VALUES (5,'[٢] حديث عبد الله بن عمر رضي الله عنه','6','3','0');
+INSERT INTO [t96734] VALUES (6,'[٣] حديث زيد بن أرقم رضي الله عنه','6','3','0');
+INSERT INTO [t96734] VALUES (7,'[٤] حديث ابن عباس رضي الله عنه','8','3','0');
