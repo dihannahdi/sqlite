64e606d4f44d6eaec625881aeece9f296bf40edd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:59 +0700

    Upload SQL chunk: t10447_chunk_0000.sql

---
+++ sql_chunks/t10447_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10447
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10447" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10447
+INSERT INTO [t10447] VALUES (1,'الأربعين الأبدال التساعيات للبخاري ومسلم للدمياطي - مخطوط','2','0','0');
