c62f2d4e66e29b7bc33ab83d295ca83e04c80fa1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:58 +0700

    Upload SQL chunk: t7495_chunk_0000.sql

---
+++ sql_chunks/t7495_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t7495
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t7495" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7495
+INSERT INTO [t7495] VALUES (1,'باب: آدب المشي إلى الصلاة','1','0','0');
+INSERT INTO [t7495] VALUES (2,'باب: صفة الصلاة','3','0','0');
+INSERT INTO [t7495] VALUES (3,'باب: صلاة التطوع','16','0','0');
+INSERT INTO [t7495] VALUES (4,'باب: صلاة الجماعة','23','0','0');
+INSERT INTO [t7495] VALUES (5,'باب: صلاة أهل الأعذار','28','0','0');
+INSERT INTO [t7495] VALUES (6,'باب: صلاة الجمعة','30','0','0');
+INSERT INTO [t7495] VALUES (7,'باب: صلاة العيد','32','0','0');
+INSERT INTO [t7495] VALUES (8,'باب: صلاة الكسوف','33','0','0');
+INSERT INTO [t7495] VALUES (9,'باب: صلاة الاستسقاء','34','0','0');
+INSERT INTO [t7495] VALUES (10,'باب: الجنائز','36','0','0');
+INSERT INTO [t7495] VALUES (11,'كتاب الزكاة','40','0','0');
+INSERT INTO [t7495] VALUES (12,'باب: زكاة بهيمة الأنعام','41','0','0');
+INSERT INTO [t7495] VALUES (13,'باب: زكاة الخارج من الأرض','43','0','0');
+INSERT INTO [t7495] VALUES (14,'باب: زكاة النقدين','44','0','0');
+INSERT INTO [t7495] VALUES (15,'باب: زكاة العروض','45','0','0');
+INSERT INTO [t7495] VALUES (16,'باب: زكاة الفطر','46','0','0');
+INSERT INTO [t7495] VALUES (17,'باب: إخراج الزكاة','47','0','0');
+INSERT INTO [t7495] VALUES (18,'باب: أهل الزكاة','48','0','0');
+INSERT INTO [t7495] VALUES (19,'كتاب الصيام','50','0','0');
+INSERT INTO [t7495] VALUES (20,'باب: ما يفسد الصوم','51','0','0');
