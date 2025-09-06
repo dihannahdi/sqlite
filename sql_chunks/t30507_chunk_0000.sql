b5434422657755103be2d29281029fafd08831ae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:58 +0700

    Upload SQL chunk: t30507_chunk_0000.sql

---
+++ sql_chunks/t30507_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30507
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30507" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30507
+INSERT INTO [t30507] VALUES (1,'الجزء السابع من الفوائد المنتقاة الحسان من الصحاح والغرائب','1','0','0');
