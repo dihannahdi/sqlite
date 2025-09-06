82e7caafb5316a95fc7f9cafaca12d9fd67b9866
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:30 +0700

    Upload SQL chunk: t17795_chunk_0000.sql

---
+++ sql_chunks/t17795_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t17795
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t17795" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17795
+INSERT INTO [t17795] VALUES (1,'مقدمة المعتني بالرسالة','2','0','0');
+INSERT INTO [t17795] VALUES (2,'وصف المخطوطة','2','1','0');
+INSERT INTO [t17795] VALUES (3,'أول الرسالة','6','0','0');
+INSERT INTO [t17795] VALUES (4,'خاتمة الرسالة وتاريخ نسخها','10','0','0');
