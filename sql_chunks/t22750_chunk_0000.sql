f6d6909a76463d379e876a3834ef6a43e5432977
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:19 +0700

    Upload SQL chunk: t22750_chunk_0000.sql

---
+++ sql_chunks/t22750_chunk_0000.sql
@@ -0,0 +1,61 @@
+-- Table: t22750
+-- Chunk: 1
+-- Rows: 54
+
+CREATE TABLE "t22750" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22750
+INSERT INTO [t22750] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t22750] VALUES (2,'الوصية الأولى','11','0','0');
+INSERT INTO [t22750] VALUES (3,'البحث اللغوي','11','2','0');
+INSERT INTO [t22750] VALUES (4,'الإيضاح','28','2','0');
+INSERT INTO [t22750] VALUES (5,'الأحكام','33','2','0');
+INSERT INTO [t22750] VALUES (6,'الوصية الثانية','35','0','0');
+INSERT INTO [t22750] VALUES (7,'المناسبة','35','6','0');
+INSERT INTO [t22750] VALUES (8,'البحث اللغوي','36','6','0');
+INSERT INTO [t22750] VALUES (9,'الإيضاح','37','6','0');
+INSERT INTO [t22750] VALUES (10,'الأحكام','43','6','0');
+INSERT INTO [t22750] VALUES (11,'الوصية الثالثة','44','0','0');
+INSERT INTO [t22750] VALUES (12,'المناسبة','44','11','0');
+INSERT INTO [t22750] VALUES (13,'البحث اللغوي','46','11','0');
+INSERT INTO [t22750] VALUES (14,'الإيضاح','47','11','0');
+INSERT INTO [t22750] VALUES (15,'الأحكام','49','11','0');
+INSERT INTO [t22750] VALUES (16,'الوصية الرابعة','50','0','0');
+INSERT INTO [t22750] VALUES (17,'المناسبة','50','16','0');
+INSERT INTO [t22750] VALUES (18,'المباحث النحوية','52','16','0');
+INSERT INTO [t22750] VALUES (19,'الإيضاح','54','16','0');
+INSERT INTO [t22750] VALUES (20,'الأحكام','57','16','0');
+INSERT INTO [t22750] VALUES (21,'الوصية الخامسة','58','0','0');
+INSERT INTO [t22750] VALUES (22,'المناسية','58','21','0');
+INSERT INTO [t22750] VALUES (23,'المبحث اللغوي','59','21','0');
+INSERT INTO [t22750] VALUES (24,'الإيضاح','61','21','0');
+INSERT INTO [t22750] VALUES (25,'الأحكام','70','21','0');
+INSERT INTO [t22750] VALUES (26,'رموز مصادر البحث','71','21','0');
+INSERT INTO [t22750] VALUES (27,'الوصية السادسة','72','0','0');
+INSERT INTO [t22750] VALUES (28,'المناسبة','72','27','0');
+INSERT INTO [t22750] VALUES (29,'البحث اللغوي','76','27','0');
+INSERT INTO [t22750] VALUES (30,'الإضاح','77','27','0');
+INSERT INTO [t22750] VALUES (31,'الأحكام','125','27','0');
+INSERT INTO [t22750] VALUES (32,'الوصية السابعة','126','0','0');
+INSERT INTO [t22750] VALUES (33,'المناسبة','126','32','0');
+INSERT INTO [t22750] VALUES (34,'البحث اللغوي','127','32','0');
+INSERT INTO [t22750] VALUES (35,'الإيضاح','128','32','0');
+INSERT INTO [t22750] VALUES (36,'الأحكام','129','32','0');
+INSERT INTO [t22750] VALUES (37,'الوصية الثامنة','130','0','0');
+INSERT INTO [t22750] VALUES (38,'المناسبة','130','37','0');
+INSERT INTO [t22750] VALUES (39,'البحث الغوي','131','37','0');
+INSERT INTO [t22750] VALUES (40,'الإيضاح','132','37','0');
+INSERT INTO [t22750] VALUES (41,'الأحكام','133','37','0');
+INSERT INTO [t22750] VALUES (42,'الوصية التاسعة','134','0','0');
+INSERT INTO [t22750] VALUES (43,'المناسبة','134','42','0');
+INSERT INTO [t22750] VALUES (44,'البحث اللغوي','135','42','0');
+INSERT INTO [t22750] VALUES (45,'القراءات','136','42','0');
+INSERT INTO [t22750] VALUES (46,'الإيضاح','137','42','0');
+INSERT INTO [t22750] VALUES (47,'الأحكام','138','42','0');
+INSERT INTO [t22750] VALUES (48,'الوصية العاشرة','139','0','0');
+INSERT INTO [t22750] VALUES (49,'المناسبة','139','48','0');
+INSERT INTO [t22750] VALUES (50,'البحث اللغوي','140','48','0');
+INSERT INTO [t22750] VALUES (51,'القراءات','141','48','0');
+INSERT INTO [t22750] VALUES (52,'الإيضاح','142','48','0');
+INSERT INTO [t22750] VALUES (53,'خاتمة البحث','143','48','0');
+INSERT INTO [t22750] VALUES (54,'مصادر ومراجع','144','0','0');
