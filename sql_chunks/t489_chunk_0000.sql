460ddd0c8a8c9a5bb35545bc639a6b9cb4f768b6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:16 +0700

    Upload SQL chunk: t489_chunk_0000.sql

---
+++ sql_chunks/t489_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t489
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t489" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t489
+INSERT INTO [t489] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t489] VALUES (2,'أغاليط الشعراء','4','0','0');
+INSERT INTO [t489] VALUES (3,'عود إلى أغاليط الشعراء','10','2','0');
+INSERT INTO [t489] VALUES (4,'الشعر','15','0','0');
+INSERT INTO [t489] VALUES (5,'السهل الممتنع من شعر البحتري','25','0','0');
+INSERT INTO [t489] VALUES (6,'العذب من شعر جرير','29','0','0');
+INSERT INTO [t489] VALUES (7,'الحشو في الشعر','31','0','0');
+INSERT INTO [t489] VALUES (8,'بدء الوساطة','49','0','0');
+INSERT INTO [t489] VALUES (9,'تفاوت شعر أبي نواس','55','0','0');
+INSERT INTO [t489] VALUES (10,'تفاوت شعر أبي تمام','65','0','0');
+INSERT INTO [t489] VALUES (11,'شعر المتنبي','82','0','0');
+INSERT INTO [t489] VALUES (12,'السرقات الشعرية','183','0','0');
+INSERT INTO [t489] VALUES (13,'ادعاء السرقة في شعر البحتري وأبي نواس وأبي تمام','209','0','0');
+INSERT INTO [t489] VALUES (14,'سرقات المتنبي','216','0','0');
+INSERT INTO [t489] VALUES (15,'مواقع الكلام','412','0','0');
+INSERT INTO [t489] VALUES (16,'دفاع المؤلف عن أبي الطيب','415','0','0');
+INSERT INTO [t489] VALUES (17,'غلو القدامى','418','0','0');
+INSERT INTO [t489] VALUES (18,'عودة الى الدفاع عن أبي الطيب','422','17','0');
+INSERT INTO [t489] VALUES (19,'الإفراط في الاستعارة','427','0','0');
+INSERT INTO [t489] VALUES (20,'من مآخذ العلماء على أبي الطيب','432','0','0');
+INSERT INTO [t489] VALUES (21,'ودفاع المؤلف عنه','432','20','0');
+INSERT INTO [t489] VALUES (22,'ما عاب العلماء على أبي الطيب','439','0','0');
