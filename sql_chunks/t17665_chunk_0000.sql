ca174bd212c7efbf6b030f63a614632e06f03da9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:30:41 +0700

    Upload SQL chunk: t17665_chunk_0000.sql

---
+++ sql_chunks/t17665_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t17665
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t17665" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17665
+INSERT INTO [t17665] VALUES (1,'مقدمة','4','0','0');
+INSERT INTO [t17665] VALUES (2,'أنا','7','0','0');
+INSERT INTO [t17665] VALUES (3,'أنا والنجوم','18','0','0');
+INSERT INTO [t17665] VALUES (4,'جواب على كتاب','24','0','0');
+INSERT INTO [t17665] VALUES (5,'من دموع القلب','32','0','0');
+INSERT INTO [t17665] VALUES (6,'في الكتاب','41','0','0');
+INSERT INTO [t17665] VALUES (7,'في معهد الحقوق','51','0','0');
+INSERT INTO [t17665] VALUES (8,'شهادة ليسانس للبيع','57','0','0');
+INSERT INTO [t17665] VALUES (9,'مشروع مقال','62','0','0');
+INSERT INTO [t17665] VALUES (10,'قصة معلم','66','0','0');
+INSERT INTO [t17665] VALUES (11,'إلى حلبون','71','0','0');
+INSERT INTO [t17665] VALUES (12,'عيدي الذي فقدته','82','0','0');
+INSERT INTO [t17665] VALUES (13,'على أبواب الثلاثين','91','0','0');
+INSERT INTO [t17665] VALUES (14,'صورة المؤلف بقلمه','97','0','0');
+INSERT INTO [t17665] VALUES (15,'زفرة مصدور','103','0','0');
+INSERT INTO [t17665] VALUES (16,'زفرة أخرى','110','0','0');
+INSERT INTO [t17665] VALUES (17,'كتاب مفتوح إلى الأستاذ أحمد أمين','120','0','0');
+INSERT INTO [t17665] VALUES (18,'الشفاء','130','0','0');
+INSERT INTO [t17665] VALUES (19,'الوحدة','135','0','0');
+INSERT INTO [t17665] VALUES (20,'ذكريات','140','0','0');
+INSERT INTO [t17665] VALUES (21,'مما حدث لي','150','0','0');
+INSERT INTO [t17665] VALUES (22,'مقدمة ديوان','155','0','0');
+INSERT INTO [t17665] VALUES (23,'أستاذنا الجندي','170','0','0');
+INSERT INTO [t17665] VALUES (24,'أول مقالة نشرتها وأول درس ألقيته','187','0','0');
+INSERT INTO [t17665] VALUES (25,'وقفة على طلل','196','0','0');
+INSERT INTO [t17665] VALUES (26,'بعد المرض','204','0','0');
+INSERT INTO [t17665] VALUES (27,'من التعليم إلى القضاء','213','0','0');
+INSERT INTO [t17665] VALUES (28,'أنا والقلم','220','0','0');
+INSERT INTO [t17665] VALUES (29,'على عتبة الأربعين','226','0','0');
+INSERT INTO [t17665] VALUES (30,'بيوتنا هدمناها بأيدينا','234','0','0');
+INSERT INTO [t17665] VALUES (31,'الدرس الأخير','243','0','0');
+INSERT INTO [t17665] VALUES (32,'عدد ١٠٠٠ من الرسالة','251','0','0');
+INSERT INTO [t17665] VALUES (33,'من رسائل الصيف','259','0','0');
+INSERT INTO [t17665] VALUES (34,'في لج البحر','264','0','0');
+INSERT INTO [t17665] VALUES (35,'شكوى','274','0','0');
+INSERT INTO [t17665] VALUES (36,'بعد الخمسين','282','0','0');
