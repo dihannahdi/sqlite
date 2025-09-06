977052da35a179cdb8bf113bb934ca90bd1e832e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:35 +0700

    Upload SQL chunk: t6903_chunk_0000.sql

---
+++ sql_chunks/t6903_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t6903
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t6903" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6903
+INSERT INTO [t6903] VALUES (1,'حرف الهمزة','2','0','0');
+INSERT INTO [t6903] VALUES (2,'حرف الباء','9','0','0');
+INSERT INTO [t6903] VALUES (3,'حرف التاء','28','0','0');
+INSERT INTO [t6903] VALUES (4,'حرف الثاء','32','0','0');
+INSERT INTO [t6903] VALUES (5,'حرف الجيم','34','0','0');
+INSERT INTO [t6903] VALUES (6,'حرف الحاء','36','0','0');
+INSERT INTO [t6903] VALUES (7,'حرف الخاء','38','0','0');
+INSERT INTO [t6903] VALUES (8,'حرف الدال','39','0','0');
+INSERT INTO [t6903] VALUES (9,'حرف الذال','52','0','0');
+INSERT INTO [t6903] VALUES (10,'حرف الراء','53','0','0');
+INSERT INTO [t6903] VALUES (11,'حرف الزاي','68','0','0');
+INSERT INTO [t6903] VALUES (12,'حرف السين','69','0','0');
+INSERT INTO [t6903] VALUES (13,'حرف الشين','71','0','0');
+INSERT INTO [t6903] VALUES (14,'حرف الصاد','71','0','0');
+INSERT INTO [t6903] VALUES (15,'حرف الضاد','72','0','0');
+INSERT INTO [t6903] VALUES (16,'حرف الطاء','74','0','0');
+INSERT INTO [t6903] VALUES (17,'حرف الظاء','75','0','0');
+INSERT INTO [t6903] VALUES (18,'حرف العين','75','0','0');
+INSERT INTO [t6903] VALUES (19,'حرف الغين','80','0','0');
+INSERT INTO [t6903] VALUES (20,'حرف الفاء','80','0','0');
+INSERT INTO [t6903] VALUES (21,'حرف القاف','82','0','0');
+INSERT INTO [t6903] VALUES (22,'حرف الكاف','85','0','0');
+INSERT INTO [t6903] VALUES (23,'حرف اللام','87','0','0');
+INSERT INTO [t6903] VALUES (24,'حرف الميم','97','0','0');
+INSERT INTO [t6903] VALUES (25,'حرف النون','104','0','0');
+INSERT INTO [t6903] VALUES (26,'حرف الهاء','110','0','0');
+INSERT INTO [t6903] VALUES (27,'حرف الواو','111','0','0');
+INSERT INTO [t6903] VALUES (28,'حرف اللام ألف','112','0','0');
+INSERT INTO [t6903] VALUES (29,'حرف الياء','113','0','0');
