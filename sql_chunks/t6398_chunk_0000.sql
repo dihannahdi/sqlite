40713402b88a9388a2acaa88166a60e541075d00
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:26 +0700

    Upload SQL chunk: t6398_chunk_0000.sql

---
+++ sql_chunks/t6398_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t6398
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t6398" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6398
+INSERT INTO [t6398] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6398] VALUES (2,'العالم وحدوثه','2','0','0');
+INSERT INTO [t6398] VALUES (3,'الأصل في حدوث العالم ووجود الصانع','2','0','0');
+INSERT INTO [t6398] VALUES (4,'الله وصفاته','9','0','0');
+INSERT INTO [t6398] VALUES (5,'ظ فصل','9','0','0');
+INSERT INTO [t6398] VALUES (6,'فصل','10','0','0');
+INSERT INTO [t6398] VALUES (7,'فصل','11','0','0');
+INSERT INTO [t6398] VALUES (8,'فصل','13','0','0');
+INSERT INTO [t6398] VALUES (9,'فصل','13','0','0');
+INSERT INTO [t6398] VALUES (10,'فصل','14','0','0');
+INSERT INTO [t6398] VALUES (11,'فصل','15','0','0');
+INSERT INTO [t6398] VALUES (12,'فصل','18','0','0');
+INSERT INTO [t6398] VALUES (13,'فصل','19','0','0');
+INSERT INTO [t6398] VALUES (14,'فصل','21','0','0');
+INSERT INTO [t6398] VALUES (15,'فصل','25','0','0');
+INSERT INTO [t6398] VALUES (16,'إرادة الله وإرادة العبد','26','0','0');
+INSERT INTO [t6398] VALUES (17,'رؤية الله','31','0','0');
+INSERT INTO [t6398] VALUES (18,'فصل','31','0','0');
+INSERT INTO [t6398] VALUES (19,'الرب والخلق','36','0','0');
+INSERT INTO [t6398] VALUES (20,'فصل','36','0','0');
+INSERT INTO [t6398] VALUES (21,'فصل','37','0','0');
+INSERT INTO [t6398] VALUES (22,'فصل','38','0','0');
+INSERT INTO [t6398] VALUES (23,'فصل','39','0','0');
+INSERT INTO [t6398] VALUES (24,'القول في إثبات النبوات','39','0','0');
+INSERT INTO [t6398] VALUES (25,'الرسالة والنبوة والمعجزة','40','0','0');
+INSERT INTO [t6398] VALUES (26,'فصل','40','0','0');
+INSERT INTO [t6398] VALUES (27,'فصل','41','0','0');
+INSERT INTO [t6398] VALUES (28,'فصل','42','0','0');
+INSERT INTO [t6398] VALUES (29,'فصل','42','0','0');
+INSERT INTO [t6398] VALUES (30,'الإمامة','44','0','0');
+INSERT INTO [t6398] VALUES (31,'فصل','44','0','0');
+INSERT INTO [t6398] VALUES (32,'إمامة المسلمين','44','0','0');
+INSERT INTO [t6398] VALUES (33,'فصل','45','0','0');
+INSERT INTO [t6398] VALUES (34,'فصل','46','0','0');
