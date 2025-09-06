c26be29de9baa7630bb0d00a67d0d01d572057ad
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:17:35 +0700

    Upload SQL chunk: t14272_chunk_0000.sql

---
+++ sql_chunks/t14272_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t14272
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t14272" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14272
+INSERT INTO [t14272] VALUES (1,'المقدمة','4','0','0');
+INSERT INTO [t14272] VALUES (2,'الطهارة','12','0','0');
+INSERT INTO [t14272] VALUES (3,'أولا: باب المياه','12','2','0');
+INSERT INTO [t14272] VALUES (4,'ثانيا: باب النجاسات','19','2','0');
+INSERT INTO [t14272] VALUES (5,'ثالثا: باب قضاء الحاجة','42','2','0');
+INSERT INTO [t14272] VALUES (6,'رابعا: باب الوضوء','56','2','0');
+INSERT INTO [t14272] VALUES (7,'خامسا: باب الغسل','84','2','0');
+INSERT INTO [t14272] VALUES (8,'سادسا: باب التيمم','96','2','0');
+INSERT INTO [t14272] VALUES (9,'سابعا: باب الحيض والنفاس','105','2','0');
+INSERT INTO [t14272] VALUES (10,'الصلاة','112','0','0');
+INSERT INTO [t14272] VALUES (11,'١ - باب مواقيت الصلاة','112','10','0');
+INSERT INTO [t14272] VALUES (12,'٢ - باب: الأذان','127','10','0');
+INSERT INTO [t14272] VALUES (13,'٣ - باب: شروط الصلاة','132','10','0');
+INSERT INTO [t14272] VALUES (14,'١ - طهارة الثوب','132','13','0');
+INSERT INTO [t14272] VALUES (15,'٢ - طهارة البدن','132','13','0');
+INSERT INTO [t14272] VALUES (16,'٣ - طهارة المكان','132','13','0');
+INSERT INTO [t14272] VALUES (17,'٤ - باب: كيفية الصلاة','137','10','0');
+INSERT INTO [t14272] VALUES (18,'١٠ - باب صلاة الجمعة','184','10','0');
+INSERT INTO [t14272] VALUES (19,'١١ - باب: صلاة العيدين','198','10','0');
+INSERT INTO [t14272] VALUES (20,'١٢ - باب: صلاة الخوف','199','10','0');
+INSERT INTO [t14272] VALUES (21,'١٣ - باب صلاة السفر','200','10','0');
+INSERT INTO [t14272] VALUES (22,'١٤ - باب: صلاة الكسوفين','205','10','0');
+INSERT INTO [t14272] VALUES (23,'١٥ - باب: صلاة الاستسقاء','208','10','0');
+INSERT INTO [t14272] VALUES (24,'الجنائز','210','0','0');
+INSERT INTO [t14272] VALUES (25,'١ - أحكام المحتضر','210','24','0');
+INSERT INTO [t14272] VALUES (26,'الزكاة','248','0','0');
+INSERT INTO [t14272] VALUES (27,'الصيام','281','0','0');
+INSERT INTO [t14272] VALUES (28,'١ - باب: أحكام الصيام','281','27','0');
+INSERT INTO [t14272] VALUES (29,'الحج','319','0','0');
+INSERT INTO [t14272] VALUES (30,'المصادر والمراجع','354','0','0');
