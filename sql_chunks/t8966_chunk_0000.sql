7eecea5be0cc26dd24448e7f33593826d4c4bc8e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:00 +0700

    Upload SQL chunk: t8966_chunk_0000.sql

---
+++ sql_chunks/t8966_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t8966
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t8966" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8966
+INSERT INTO [t8966] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8966] VALUES (2,'مدخل','1','1','0');
+INSERT INTO [t8966] VALUES (3,'عصر البعثة النبوية','5','0','0');
+INSERT INTO [t8966] VALUES (4,'عصر البعثة النبوية','5','3','0');
+INSERT INTO [t8966] VALUES (5,'اصطفاء الذات النبوية','13','0','0');
+INSERT INTO [t8966] VALUES (6,'اصطفاء الذات النبوية','13','5','0');
+INSERT INTO [t8966] VALUES (7,'إعداد الله لعبده للرسالة','16','0','0');
+INSERT INTO [t8966] VALUES (8,'مدخل','16','7','0');
+INSERT INTO [t8966] VALUES (9,'الاعداد النفسي','18','7','0');
+INSERT INTO [t8966] VALUES (10,'التمهيد التاريخي للرسول','23','7','0');
+INSERT INTO [t8966] VALUES (11,'التهيئة الاجتماعية','26','7','0');
+INSERT INTO [t8966] VALUES (12,'القدوة','34','0','0');
+INSERT INTO [t8966] VALUES (13,'مدخل','34','12','0');
+INSERT INTO [t8966] VALUES (14,'الاقتراب','35','12','0');
+INSERT INTO [t8966] VALUES (15,'الحب','38','12','0');
+INSERT INTO [t8966] VALUES (16,'العقيدة فوق الثروة والمال','39','12','0');
+INSERT INTO [t8966] VALUES (17,'الصبر والثبات والبطونة','42','12','0');
+INSERT INTO [t8966] VALUES (18,'المسؤولية والعهد','45','12','0');
+INSERT INTO [t8966] VALUES (19,'خاتمة','49','0','0');
