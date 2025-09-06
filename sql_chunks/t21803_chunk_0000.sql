2cea76829c48d1bc6c1ce5991eb909bd61e48220
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:31 +0700

    Upload SQL chunk: t21803_chunk_0000.sql

---
+++ sql_chunks/t21803_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t21803
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t21803" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21803
+INSERT INTO [t21803] VALUES (1,'شرح جوامع الأخبار (١)','2','0','0');
+INSERT INTO [t21803] VALUES (2,'شرح جوامع الأخبار (٢)','34','0','0');
+INSERT INTO [t21803] VALUES (3,'شرح جوامع الأخبار (٣)','69','0','0');
+INSERT INTO [t21803] VALUES (4,'شرح جوامع الأخبار (٤)','101','0','0');
+INSERT INTO [t21803] VALUES (5,'شرح جوامع الأخبار (٥)','131','0','0');
+INSERT INTO [t21803] VALUES (6,'شرح جوامع الأخبار (٦)','165','0','0');
+INSERT INTO [t21803] VALUES (7,'شرح جوامع الأخبار (٧)','196','0','0');
+INSERT INTO [t21803] VALUES (8,'شرح جوامع الأخبار (٨)','229','0','0');
+INSERT INTO [t21803] VALUES (9,'شرح جوامع الأخبار (٩)','264','0','0');
+INSERT INTO [t21803] VALUES (10,'شرح جوامع الأخبار (١٠)','286','0','0');
