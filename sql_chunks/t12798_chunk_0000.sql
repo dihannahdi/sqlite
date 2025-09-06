32b6b8c1cb81d3df01225c95444a7c65fcaf6e1d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:21 +0700

    Upload SQL chunk: t12798_chunk_0000.sql

---
+++ sql_chunks/t12798_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t12798
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t12798" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12798
+INSERT INTO [t12798] VALUES (1,'بين يدي الكتاب','2','0','0');
+INSERT INTO [t12798] VALUES (2,'مقدمة','6','0','0');
+INSERT INTO [t12798] VALUES (3,'الباب الأول التطرف من لوازم التصوف','13','0','0');
+INSERT INTO [t12798] VALUES (4,'الباب الثاني التصوف ومخالفة الشريعة','65','0','0');
+INSERT INTO [t12798] VALUES (5,'الباب الثالث التصوف مؤامرة ضد الإسلام','119','0','0');
+INSERT INTO [t12798] VALUES (6,'الباب الرابع التصوف .. بدعه وخصائصه','151','0','0');
+INSERT INTO [t12798] VALUES (7,'الباب الخامس طرق التصوف وأعيانها','222','0','0');
+INSERT INTO [t12798] VALUES (8,'الطريقة الرفاعية','222','7','0');
+INSERT INTO [t12798] VALUES (9,'الطريقة الشاذلية','244','7','0');
+INSERT INTO [t12798] VALUES (10,'القادرية','258','7','0');
+INSERT INTO [t12798] VALUES (11,'التيجانية','272','7','0');
+INSERT INTO [t12798] VALUES (12,'الباب السادس مصطلحات الصوفية','291','0','0');
+INSERT INTO [t12798] VALUES (13,'مصادر الكتاب ومراجعه','323','0','0');
+INSERT INTO [t12798] VALUES (14,'كتب الصوفية','323','13','0');
+INSERT INTO [t12798] VALUES (15,'كتب غير الصوفية من المسلمين','336','13','0');
+INSERT INTO [t12798] VALUES (16,'كتب الشيعة والإسماعيلية','340','13','0');
+INSERT INTO [t12798] VALUES (17,'كتب غير المسلمين','342','13','0');
