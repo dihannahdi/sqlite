6dd31fd1c49b4ee3e2f48ead99dcec43baceef78
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:26 +0700

    Upload SQL chunk: t12746_chunk_0000.sql

---
+++ sql_chunks/t12746_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t12746
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t12746" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12746
+INSERT INTO [t12746] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t12746] VALUES (2,'كتاب قيام الليل','2','0','0');
+INSERT INTO [t12746] VALUES (3,'باب فيمن كان له ورد من الليل يقومه فشغله عنه مرض أو عذر ونام عنه ومن نيته القيام','29','0','0');
+INSERT INTO [t12746] VALUES (4,'باب ذكر ما يستحب أن يفعله القائم المتهجد','37','0','0');
