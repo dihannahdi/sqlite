506c6b72649bfe5f48cb7aba689090efe9694fb1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:20 +0700

    Upload SQL chunk: t37327_chunk_0000.sql

---
+++ sql_chunks/t37327_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t37327
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t37327" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37327
+INSERT INTO [t37327] VALUES (1,'الجزء الأول','1','0','0');
+INSERT INTO [t37327] VALUES (2,'الكافوريات','56','1','0');
+INSERT INTO [t37327] VALUES (3,'الجزء الثاني','183','0','0');
+INSERT INTO [t37327] VALUES (4,'العميديات','256','3','0');
+INSERT INTO [t37327] VALUES (5,'العضديات','291','3','0');
