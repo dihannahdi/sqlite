9ac612e7e203e6a9f1e3fc6e7b555db362a049a3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:16 +0700

    Upload SQL chunk: t1019_chunk_0000.sql

---
+++ sql_chunks/t1019_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t1019
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t1019" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1019
+INSERT INTO [t1019] VALUES (1,'تاريخ السير عند المسلمين','5','0','0');
+INSERT INTO [t1019] VALUES (2,'نحو السيرة الفنية','35','0','0');
+INSERT INTO [t1019] VALUES (3,'الدرجة الفنية في السيرة','71','0','0');
+INSERT INTO [t1019] VALUES (4,'السير الذاتية نظرة عامة','96','0','0');
+INSERT INTO [t1019] VALUES (5,'نظرة عامة','96','0','0');
+INSERT INTO [t1019] VALUES (6,'السيرة الذاتية في الأدب العربي','118','0','0');
