a74e7d1357ac19c8ac5c937c19402f146c055ee4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:40 +0700

    Upload SQL chunk: t96788_chunk_0000.sql

---
+++ sql_chunks/t96788_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t96788
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t96788" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96788
+INSERT INTO [t96788] VALUES (1,'مقدمة الشيخ يحيى بن علي الحجوري','2','0','0');
+INSERT INTO [t96788] VALUES (2,'مشاهداتي في المملكة العربية السعودية','8','0','0');
