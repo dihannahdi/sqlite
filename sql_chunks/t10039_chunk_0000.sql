76630336b60b51efebc77d883f78a36f5d031782
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:23 +0700

    Upload SQL chunk: t10039_chunk_0000.sql

---
+++ sql_chunks/t10039_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10039
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10039" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10039
+INSERT INTO [t10039] VALUES (1,'تأملات في مماثلة المؤمن للنخلة','1','0','0');
+INSERT INTO [t10039] VALUES (2,'مصادر ومراجع','33','0','0');
