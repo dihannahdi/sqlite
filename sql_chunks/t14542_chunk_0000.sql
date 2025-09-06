76c5ba169a313847a5a598d1ab65755fe0b19928
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:20:06 +0700

    Upload SQL chunk: t14542_chunk_0000.sql

---
+++ sql_chunks/t14542_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t14542
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t14542" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14542
+INSERT INTO [t14542] VALUES (1,'حليمة السعدية','2','0','0');
+INSERT INTO [t14542] VALUES (2,'صفية بنت عبد المطلب','16','0','0');
+INSERT INTO [t14542] VALUES (3,'فاطمة الزهراء','30','0','0');
+INSERT INTO [t14542] VALUES (4,'أسماء بنت أبي بكر','42','0','0');
+INSERT INTO [t14542] VALUES (5,'نسيبة المازنية','56','0','0');
+INSERT INTO [t14542] VALUES (6,'رملة بنت أبي سفيان','73','0','0');
+INSERT INTO [t14542] VALUES (7,'الغميصاء بنت ملحان','87','0','0');
+INSERT INTO [t14542] VALUES (8,'أم سلمة','100','0','0');
