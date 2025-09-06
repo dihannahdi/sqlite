0939300f80d49c001c559acbf1d1e7788bce9771
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:08 +0700

    Upload SQL chunk: t6498_chunk_0000.sql

---
+++ sql_chunks/t6498_chunk_0000.sql
@@ -0,0 +1,56 @@
+-- Table: t6498
+-- Chunk: 1
+-- Rows: 49
+
+CREATE TABLE "t6498" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6498
+INSERT INTO [t6498] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6498] VALUES (2,'مجمل الاعتقاد','1','0','0');
+INSERT INTO [t6498] VALUES (3,'توحيد الله','2','0','0');
+INSERT INTO [t6498] VALUES (4,'الله قادر حي عالم','2','0','0');
+INSERT INTO [t6498] VALUES (5,'صفته تعالى السميع العليم','2','0','0');
+INSERT INTO [t6498] VALUES (6,'وجه الله تعالى','3','0','0');
+INSERT INTO [t6498] VALUES (7,'اليدان','4','0','0');
+INSERT INTO [t6498] VALUES (8,'علم الله','4','0','0');
+INSERT INTO [t6498] VALUES (9,'قدرة الله','5','0','0');
+INSERT INTO [t6498] VALUES (10,'إرادة الله','6','0','0');
+INSERT INTO [t6498] VALUES (11,'كلام الله','6','0','0');
+INSERT INTO [t6498] VALUES (12,'كلام الله','7','0','0');
+INSERT INTO [t6498] VALUES (13,'إستواؤه جل شأنه','7','0','0');
+INSERT INTO [t6498] VALUES (14,'بائن من خلفه','8','0','0');
+INSERT INTO [t6498] VALUES (15,'إن لله نفسا','10','0','0');
+INSERT INTO [t6498] VALUES (16,'لا يجوز أن يسمى جسما','11','0','0');
+INSERT INTO [t6498] VALUES (17,'الإسم والمسمى','13','0','0');
+INSERT INTO [t6498] VALUES (18,'أفعال العباد مخلوقة','13','0','0');
+INSERT INTO [t6498] VALUES (19,'الإستطاعة','14','0','0');
+INSERT INTO [t6498] VALUES (20,'عدل الله تعالى','14','0','0');
+INSERT INTO [t6498] VALUES (21,'المشيئة لله','14','0','0');
+INSERT INTO [t6498] VALUES (22,'عدل الله تعالى','16','0','0');
+INSERT INTO [t6498] VALUES (23,'الطاعة والمعصية','16','0','0');
+INSERT INTO [t6498] VALUES (24,'الإيمان قول وعمل','17','0','0');
+INSERT INTO [t6498] VALUES (25,'مسألة عن الإيمان','17','0','0');
+INSERT INTO [t6498] VALUES (26,'الكتب كلام الله','18','0','0');
+INSERT INTO [t6498] VALUES (27,'القرآن معجز في نفسه','18','0','0');
+INSERT INTO [t6498] VALUES (28,'الإيمان يزيد وينقص','18','0','0');
+INSERT INTO [t6498] VALUES (29,'الإيمان غير الإسلام','18','0','0');
+INSERT INTO [t6498] VALUES (30,'لا يكفر إلا تارك الصلاة','20','0','0');
+INSERT INTO [t6498] VALUES (31,'مسائل شتى في الفضائل','20','0','0');
+INSERT INTO [t6498] VALUES (32,'الميزان','20','0','0');
+INSERT INTO [t6498] VALUES (33,'الذنوب والإستغفار والتوبة','21','0','0');
+INSERT INTO [t6498] VALUES (34,'الشهداء أحياء يرزقون','21','0','0');
+INSERT INTO [t6498] VALUES (35,'الصراط','21','0','0');
+INSERT INTO [t6498] VALUES (36,'سؤال الملكين','22','0','0');
+INSERT INTO [t6498] VALUES (37,'الإجابة','22','0','0');
+INSERT INTO [t6498] VALUES (38,'مخالفة الإجماع والتواتر','22','0','0');
+INSERT INTO [t6498] VALUES (39,'خير الناس بعد الرسول صلى الله عليه وسلم','23','0','0');
+INSERT INTO [t6498] VALUES (40,'لا معصوم إلا الرسول صلى الله عليه وسلم','23','0','0');
+INSERT INTO [t6498] VALUES (41,'الإجماع','23','0','0');
+INSERT INTO [t6498] VALUES (42,'القدر خيره وشره من الله تعالى','23','0','0');
+INSERT INTO [t6498] VALUES (43,'الملائكة الحفظة','23','0','0');
+INSERT INTO [t6498] VALUES (44,'واجب الغزو','23','0','0');
+INSERT INTO [t6498] VALUES (45,'مسائل شتى','24','0','0');
+INSERT INTO [t6498] VALUES (46,'كرامات الأولياء','25','0','0');
+INSERT INTO [t6498] VALUES (47,'التفاضل بين الأنبياء','26','0','0');
+INSERT INTO [t6498] VALUES (48,'الوصية','26','0','0');
+INSERT INTO [t6498] VALUES (49,'الأذكار','26','0','0');
