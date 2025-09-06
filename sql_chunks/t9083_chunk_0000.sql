db2c4226b1b051f2600a3160fa49adce5df5d347
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:47 +0700

    Upload SQL chunk: t9083_chunk_0000.sql

---
+++ sql_chunks/t9083_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t9083
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t9083" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9083
+INSERT INTO [t9083] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t9083] VALUES (2,'وصايا مهمة','4','0','0');
+INSERT INTO [t9083] VALUES (3,'نواقض الإسلام','8','0','0');
+INSERT INTO [t9083] VALUES (4,'كيف تؤدي مناسك الحج والعمرة وتزور مسجد الرسول صلى الله عليه وسلم','14','0','0');
+INSERT INTO [t9083] VALUES (5,'صفة العمرة','16','0','0');
+INSERT INTO [t9083] VALUES (6,'صفة الحج','19','0','0');
+INSERT INTO [t9083] VALUES (7,'ما يجب على المحرم','24','0','0');
+INSERT INTO [t9083] VALUES (8,'صفة زيارة مسجد الرسول صلى الله عليه وسلم','28','0','0');
+INSERT INTO [t9083] VALUES (9,'أخطاء يرتكبها بعض الحجاج','31','0','0');
+INSERT INTO [t9083] VALUES (10,'أولا أخطاء في الإحرام','31','9','0');
+INSERT INTO [t9083] VALUES (11,'[ثانيا أخطاء في الطواف]','32','9','0');
+INSERT INTO [t9083] VALUES (12,'ثالثا أخطاء في السعي','34','9','0');
+INSERT INTO [t9083] VALUES (13,'رابعا أخطاء في عرفات','35','9','0');
+INSERT INTO [t9083] VALUES (14,'خامسا أخطاء في مزدلفة','36','9','0');
+INSERT INTO [t9083] VALUES (15,'سادسا أخطاء عند الرمي','37','9','0');
+INSERT INTO [t9083] VALUES (16,'سابعا أخطاء في طواف الوداع','39','9','0');
+INSERT INTO [t9083] VALUES (17,'ثامنا أخطاء عند الزيارة للمسجد النبوي','40','9','0');
+INSERT INTO [t9083] VALUES (18,'توجيهات موجزة للحاج والمعتمر وزائر مسجد الرسول صلى الله عليه وسلم','42','0','0');
+INSERT INTO [t9083] VALUES (19,'أدعية في عرفات والمشعر الحرام وغيرها','53','0','0');
