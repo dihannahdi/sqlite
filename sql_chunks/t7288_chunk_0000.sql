f32c3f7b2fcf9a054a91f39595adf0bc84dcd316
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:08 +0700

    Upload SQL chunk: t7288_chunk_0000.sql

---
+++ sql_chunks/t7288_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t7288
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t7288" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7288
+INSERT INTO [t7288] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7288] VALUES (2,'فضل الخشوع','2','0','0');
+INSERT INTO [t7288] VALUES (3,'قبل الصلاة','6','0','0');
+INSERT INTO [t7288] VALUES (4,'الاستعداد للصلاة قبل الصلاة','10','0','0');
+INSERT INTO [t7288] VALUES (5,'أثناء الصلاة','21','0','0');
+INSERT INTO [t7288] VALUES (6,'الخشوع في الركوع:','32','0','0');
+INSERT INTO [t7288] VALUES (7,'الخشوع في السجود:','35','0','0');
+INSERT INTO [t7288] VALUES (8,'الخشوع في التشهد:','38','0','0');
+INSERT INTO [t7288] VALUES (9,'خشوع السلف:','43','0','0');
