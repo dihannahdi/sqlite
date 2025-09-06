f89c2c2f79bb4f7b2d3082ad55b342f21ccc0723
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:21 +0700

    Upload SQL chunk: t6008_chunk_0000.sql

---
+++ sql_chunks/t6008_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t6008
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t6008" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6008
+INSERT INTO [t6008] VALUES (1,'مقدمة المحقق','1','0','0');
+INSERT INTO [t6008] VALUES (2,'فضل شرف علم الحديث وأهله','1','1','0');
+INSERT INTO [t6008] VALUES (3,'فصل طالب العلم','8','1','0');
+INSERT INTO [t6008] VALUES (4,'أثر لطيف رواه القاضي عياض','14','1','0');
+INSERT INTO [t6008] VALUES (5,'ترجمة الحافظ اللوبدي كاتب المخطوطة','18','1','0');
+INSERT INTO [t6008] VALUES (6,'ترجمة الحافظ العراقي','20','1','0');
+INSERT INTO [t6008] VALUES (7,'الأمالي','27','1','0');
+INSERT INTO [t6008] VALUES (8,'توثيق النص','31','1','0');
+INSERT INTO [t6008] VALUES (9,'أهمية الامالي','31','1','0');
+INSERT INTO [t6008] VALUES (10,'وكتب الامالي في الحديث كثيرة فمنه','33','1','0');
+INSERT INTO [t6008] VALUES (11,'الاصل المعتمد للتحقيق','35','1','0');
+INSERT INTO [t6008] VALUES (12,'فوائد الاملاء','35','1','0');
+INSERT INTO [t6008] VALUES (13,'[عملي في الكتاب]','36','1','0');
+INSERT INTO [t6008] VALUES (14,'المجلس الأول','37','0','0');
+INSERT INTO [t6008] VALUES (15,'المجلس الثاني','49','0','0');
+INSERT INTO [t6008] VALUES (16,'المجلس الثالث','57','0','0');
+INSERT INTO [t6008] VALUES (17,'المجلس الرابع','69','0','0');
+INSERT INTO [t6008] VALUES (18,'المجلس الخامس','79','0','0');
+INSERT INTO [t6008] VALUES (19,'المجلس السادس','87','0','0');
+INSERT INTO [t6008] VALUES (20,'المجلس السابع','96','0','0');
