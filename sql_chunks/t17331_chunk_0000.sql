955649fc4473585a3ea7d74827de6964439b1f3c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:29:26 +0700

    Upload SQL chunk: t17331_chunk_0000.sql

---
+++ sql_chunks/t17331_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t17331
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t17331" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17331
+INSERT INTO [t17331] VALUES (1,'فصل','4','0','0');
+INSERT INTO [t17331] VALUES (2,'فصل','7','0','0');
+INSERT INTO [t17331] VALUES (3,'فصل','22','0','0');
+INSERT INTO [t17331] VALUES (4,'فصل','25','0','0');
+INSERT INTO [t17331] VALUES (5,'فصل','25','0','0');
+INSERT INTO [t17331] VALUES (6,'فصل','26','0','0');
+INSERT INTO [t17331] VALUES (7,'فصل','29','0','0');
+INSERT INTO [t17331] VALUES (8,'فصل','30','0','0');
+INSERT INTO [t17331] VALUES (9,'فصل','39','0','0');
+INSERT INTO [t17331] VALUES (10,'فصل','40','0','0');
+INSERT INTO [t17331] VALUES (11,'فصل','41','0','0');
+INSERT INTO [t17331] VALUES (12,'فصل','43','0','0');
+INSERT INTO [t17331] VALUES (13,'فصل','43','0','0');
+INSERT INTO [t17331] VALUES (14,'فصل','44','0','0');
+INSERT INTO [t17331] VALUES (15,'فصل','45','0','0');
