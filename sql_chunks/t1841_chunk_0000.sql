4b675c764e9e298519e663ba8870499195b1639d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:08 +0700

    Upload SQL chunk: t1841_chunk_0000.sql

---
+++ sql_chunks/t1841_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t1841
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t1841" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1841
+INSERT INTO [t1841] VALUES (1,'محاضرات في علوم الحديث','1','0','0');
+INSERT INTO [t1841] VALUES (2,'المحاضرة الأولى الحديث الصحيح','1','0','0');
+INSERT INTO [t1841] VALUES (3,'المحاضرة الثانية تتمات الصحيح','4','0','0');
+INSERT INTO [t1841] VALUES (4,'المحاضرة الثالثة الحديث الحسن','7','0','0');
+INSERT INTO [t1841] VALUES (5,'المحاضرة الرابعة الحديث الضعيف','12','0','0');
+INSERT INTO [t1841] VALUES (6,'المحاضرة الخامسة طرق تخريج الحديث','15','0','0');
+INSERT INTO [t1841] VALUES (7,'الحكم على الأسانيد على النحو الآتي','17','0','0');
+INSERT INTO [t1841] VALUES (8,'المحاضرة السادسة أقسام الضعيف','18','0','0');
+INSERT INTO [t1841] VALUES (9,'المحاضرة السابعة تتمات أقسام الضعيف','21','0','0');
+INSERT INTO [t1841] VALUES (10,'المحاضرة الثامنة تتمات الضعيف','26','0','0');
+INSERT INTO [t1841] VALUES (11,'المحاضرة التاسعة الشاذ','32','0','0');
