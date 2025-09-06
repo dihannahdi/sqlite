d38e0c034894f8619cf211fe5163321918b6ecc2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:55 +0700

    Upload SQL chunk: t36051_chunk_0000.sql

---
+++ sql_chunks/t36051_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t36051
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t36051" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36051
+INSERT INTO [t36051] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t36051] VALUES (2,'المبحث الأول: التدليس لغة واصطلاحا','2','0','0');
+INSERT INTO [t36051] VALUES (3,'المطلب الأول: حده','2','2','0');
+INSERT INTO [t36051] VALUES (4,'المطلب الثاني: أقسام التدليس','3','2','0');
+INSERT INTO [t36051] VALUES (5,'المبحث الثاني: تدليس الإسناد','5','0','0');
+INSERT INTO [t36051] VALUES (6,'المطلب الأول: تعريفه','5','5','0');
+INSERT INTO [t36051] VALUES (7,'المطلب الثاني: أنواع تدليس الإسناد','8','5','0');
+INSERT INTO [t36051] VALUES (8,'المطلب الثالث: حكم تدليس الإسناد','13','5','0');
+INSERT INTO [t36051] VALUES (9,'المبحث الثالث: تدليس الشيوخ','19','0','0');
+INSERT INTO [t36051] VALUES (10,'المطلب الأول: تعريفه','19','9','0');
+INSERT INTO [t36051] VALUES (11,'المطلب الثاني: حكم تدليس الشيوخ','22','9','0');
+INSERT INTO [t36051] VALUES (12,'تدليس الإجازة','23','11','0');
+INSERT INTO [t36051] VALUES (13,'تدليس المتون','24','11','0');
+INSERT INTO [t36051] VALUES (14,'المبحث الرابع: طبقات المدلسين','24','0','0');
