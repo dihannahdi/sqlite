9de93265226a97ee678e47b4aca86626dad183e0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:00 +0700

    Upload SQL chunk: t36545_chunk_0000.sql

---
+++ sql_chunks/t36545_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t36545
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t36545" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36545
+INSERT INTO [t36545] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36545] VALUES (2,'القصة الأولى','2','0','0');
+INSERT INTO [t36545] VALUES (3,'المرأة العفيفة','6','0','0');
+INSERT INTO [t36545] VALUES (4,'مكانة الزواج','8','0','0');
+INSERT INTO [t36545] VALUES (5,'فوائد النكاح','9','0','0');
+INSERT INTO [t36545] VALUES (6,'شريك في الإثم','12','0','0');
+INSERT INTO [t36545] VALUES (7,'المستشارة','14','0','0');
+INSERT INTO [t36545] VALUES (8,'السماحة في الزواج','16','0','0');
+INSERT INTO [t36545] VALUES (9,'أنت مؤتمن','17','0','0');
+INSERT INTO [t36545] VALUES (10,'نداء','19','0','0');
+INSERT INTO [t36545] VALUES (11,'من تجاربهم','20','0','0');
+INSERT INTO [t36545] VALUES (12,'أسباب التأخر عن الزواج','21','0','0');
+INSERT INTO [t36545] VALUES (13,'الحلول المقترحة','24','0','0');
