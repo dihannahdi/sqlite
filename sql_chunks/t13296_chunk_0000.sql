a23315979b322a53bdcb06af51a6228cc46b698e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:13:25 +0700

    Upload SQL chunk: t13296_chunk_0000.sql

---
+++ sql_chunks/t13296_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t13296
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t13296" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13296
+INSERT INTO [t13296] VALUES (1,'المحتوى','6','0','0');
+INSERT INTO [t13296] VALUES (2,'الإهداء','7','0','0');
+INSERT INTO [t13296] VALUES (3,'تصدير','8','0','0');
+INSERT INTO [t13296] VALUES (4,'مقدمة المترجم','15','0','0');
+INSERT INTO [t13296] VALUES (5,'نص رسالة المؤلف بن نبي إلى الناشر','22','0','0');
+INSERT INTO [t13296] VALUES (6,'لبيك: حج الفقراء','25','0','0');
