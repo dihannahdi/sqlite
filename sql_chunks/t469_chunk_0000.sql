bccb00ef866cd4e20d97ee6a94f95e5762219016
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:49 +0700

    Upload SQL chunk: t469_chunk_0000.sql

---
+++ sql_chunks/t469_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t469
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t469" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t469
+INSERT INTO [t469] VALUES (1,'الفسخ بالإعسار ثابت في المذهب الشافعي','2','0','0');
+INSERT INTO [t469] VALUES (2,'علته تعذر تسليم العوض','2','0','0');
