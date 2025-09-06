6dbb6c45fc925711b66887a0f1c1370a30068f03
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:02 +0700

    Upload SQL chunk: t1014_chunk_0000.sql

---
+++ sql_chunks/t1014_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t1014
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t1014" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1014
+INSERT INTO [t1014] VALUES (1,'تاريخ النقد الأدبي عند العرب','1','0','0');
+INSERT INTO [t1014] VALUES (2,'الإهداء','1','0','0');
+INSERT INTO [t1014] VALUES (3,'تمهيد','2','0','0');
+INSERT INTO [t1014] VALUES (4,'نظرة عامة في قواعد النقد حتى أواخر هذا القرن','35','0','0');
+INSERT INTO [t1014] VALUES (5,'المحاولات النقدية في القرن الثالث','50','0','0');
+INSERT INTO [t1014] VALUES (6,'الاتجاهات النقدية في القرن الرابع','112','0','0');
+INSERT INTO [t1014] VALUES (7,'النقد الأدبي في القرن الخامس','344','0','0');
+INSERT INTO [t1014] VALUES (8,'النقد في الأندلس في القرنين السادس والسابع','476','0','0');
+INSERT INTO [t1014] VALUES (9,'فصل ختامي النقد الأدبي عند ابن خلدون','595','0','0');
