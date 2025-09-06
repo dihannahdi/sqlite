f270f3a4ebb048f52abe06f82692d3b3f0ddd68a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:01 +0700

    Upload SQL chunk: t96175_chunk_0000.sql

---
+++ sql_chunks/t96175_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t96175
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t96175" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96175
+INSERT INTO [t96175] VALUES (1,'المقدمة','3','0','0');
+INSERT INTO [t96175] VALUES (2,'باب حد الكلام والكلمة وأقسامها','6','0','0');
+INSERT INTO [t96175] VALUES (3,'باب أقسام الإعراب','10','0','0');
+INSERT INTO [t96175] VALUES (4,'باب إعراب الاسم المفرد وجمع التكسير','14','0','0');
+INSERT INTO [t96175] VALUES (5,'باب الأسماء الخمسة','17','0','0');
+INSERT INTO [t96175] VALUES (6,'باب المثنى','19','0','0');
+INSERT INTO [t96175] VALUES (7,'باب جمع المذكر السالم','21','0','0');
+INSERT INTO [t96175] VALUES (8,'باب جمع المؤنث السالم','22','0','0');
+INSERT INTO [t96175] VALUES (9,'باب الأفعال الخمسة','23','0','0');
+INSERT INTO [t96175] VALUES (10,'باب قسمة الأفعال','24','0','0');
+INSERT INTO [t96175] VALUES (11,'باب النواصب','27','0','0');
+INSERT INTO [t96175] VALUES (12,'باب الجوازم','33','0','0');
+INSERT INTO [t96175] VALUES (13,'باب النكرة والمعرفة','38','0','0');
+INSERT INTO [t96175] VALUES (14,'باب مرفوعات الأسماء','39','0','0');
+INSERT INTO [t96175] VALUES (15,'باب المنصوبات من الأسماء','44','0','0');
+INSERT INTO [t96175] VALUES (16,'باب إعمال اسم الفاعل','49','0','0');
+INSERT INTO [t96175] VALUES (17,'باب إعمال المصدر','50','0','0');
+INSERT INTO [t96175] VALUES (18,'باب الجر','51','0','0');
