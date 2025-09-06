273a74228ab644ae171df041fff3af0cdce5c1d5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:30 +0700

    Upload SQL chunk: t6749_chunk_0000.sql

---
+++ sql_chunks/t6749_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t6749
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t6749" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6749
+INSERT INTO [t6749] VALUES (1,'باب ما ورد في فضل الدعاء والحث عليه','1','0','0');
+INSERT INTO [t6749] VALUES (2,'باب في الأوقات التي يدعى فيها','22','0','0');
+INSERT INTO [t6749] VALUES (3,'باب ذم العجلة في الدعاء','43','0','0');
+INSERT INTO [t6749] VALUES (4,'باب ما ورد في اسم الله عز وجل الأعظم','48','0','0');
+INSERT INTO [t6749] VALUES (5,'باب في دعاء الحاجة','55','0','0');
+INSERT INTO [t6749] VALUES (6,'باب أدعية متفرقة','61','0','0');
+INSERT INTO [t6749] VALUES (7,'باب في جوامع الدعاء','71','0','0');
+INSERT INTO [t6749] VALUES (8,'باب في الدعاء عقيب الصلوات','77','0','0');
+INSERT INTO [t6749] VALUES (9,'باب في الدعاء عند الصباح والمساء','87','0','0');
+INSERT INTO [t6749] VALUES (10,'باب في الدعاء عند أكل الطعام','115','0','0');
+INSERT INTO [t6749] VALUES (11,'باب في الدعاء للمريض','118','0','0');
+INSERT INTO [t6749] VALUES (12,'باب ما يقول إذا دخل المسجد وخرج منه','126','0','0');
+INSERT INTO [t6749] VALUES (13,'باب في دعاء المسافر','129','0','0');
+INSERT INTO [t6749] VALUES (14,'باب ما يقول عند الرجوع من السفر','137','0','0');
+INSERT INTO [t6749] VALUES (15,'باب ما يودع به المسافر','140','0','0');
+INSERT INTO [t6749] VALUES (16,'باب ما يدعو به إذا حزبه أمر وأصابه غم','144','0','0');
