54f96293413eb07ee8b97eaf8b55e8679a00c491
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:26 +0700

    Upload SQL chunk: t6983_chunk_0000.sql

---
+++ sql_chunks/t6983_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t6983
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t6983" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6983
+INSERT INTO [t6983] VALUES (1,'يرفلن في ملاحف جياد','24','0','0');
+INSERT INTO [t6983] VALUES (2,'ذكر العشير وما جاء على وزنه من أسماء الكسور','55','0','0');
+INSERT INTO [t6983] VALUES (3,'ومن الأسماء الواقعة على الأعداد','56','0','0');
+INSERT INTO [t6983] VALUES (4,'المقادير والألفاظ الدالة على الأعداد من غير ما تقدم','57','0','0');
