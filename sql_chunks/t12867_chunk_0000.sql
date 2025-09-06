b358c140af19aba3a035d54c06eff6bb8bae2509
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:08:10 +0700

    Upload SQL chunk: t12867_chunk_0000.sql

---
+++ sql_chunks/t12867_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t12867
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t12867" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12867
+INSERT INTO [t12867] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t12867] VALUES (2,'ترجمة موجزة: للشيخ فيصل بن عبد العزيز آل مبارك رحمه الله','5','0','0');
+INSERT INTO [t12867] VALUES (3,'* اسمه ونسبه:','5','2','0');
+INSERT INTO [t12867] VALUES (4,'* مولده ونشأته:','5','2','0');
+INSERT INTO [t12867] VALUES (5,'* طلبه للعلم:','6','2','0');
+INSERT INTO [t12867] VALUES (6,'* صفاته الخلقية والخلقية:','8','2','0');
+INSERT INTO [t12867] VALUES (7,'* زهده وورعه وعبادته:','8','2','0');
+INSERT INTO [t12867] VALUES (8,'* أعماله ومناصبه:','9','2','0');
+INSERT INTO [t12867] VALUES (9,'* تلاميذه:','9','2','0');
+INSERT INTO [t12867] VALUES (10,'* مصنفاته:','10','2','0');
+INSERT INTO [t12867] VALUES (11,'* في العقيدة:','11','10','0');
+INSERT INTO [t12867] VALUES (12,'* في التفسير:','11','10','0');
+INSERT INTO [t12867] VALUES (13,'* في الحديث:','11','10','0');
+INSERT INTO [t12867] VALUES (14,'* في الفقه وأصوله:','12','10','0');
+INSERT INTO [t12867] VALUES (15,'* في الفرائض:','13','10','0');
+INSERT INTO [t12867] VALUES (16,'* في النحو:','13','10','0');
+INSERT INTO [t12867] VALUES (17,'* وفاته:','14','2','0');
+INSERT INTO [t12867] VALUES (18,'* عقبه:','14','2','0');
+INSERT INTO [t12867] VALUES (19,'الحديث عن الرسالة','14','0','0');
+INSERT INTO [t12867] VALUES (20,'- الموضوع:','14','19','0');
+INSERT INTO [t12867] VALUES (21,'- نسبتها:','15','19','0');
+INSERT INTO [t12867] VALUES (22,'- النسخ:','15','19','0');
+INSERT INTO [t12867] VALUES (23,'أما المطبوعة','15','22','0');
+INSERT INTO [t12867] VALUES (24,'وأما النسخة الخطية','16','22','0');
+INSERT INTO [t12867] VALUES (25,'عمل المحقق اشتمل على ما يلي:','16','0','0');
+INSERT INTO [t12867] VALUES (26,'هذه نبذة في معرفة أصول الفقه','18','0','0');
