2b13dcb2a4ed4f8fb7c27cf03d278ce3eb886679
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:46 +0700

    Upload SQL chunk: t5947_chunk_0000.sql

---
+++ sql_chunks/t5947_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t5947
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t5947" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5947
+INSERT INTO [t5947] VALUES (1,'آخر ما انتهى اليه المنقول من الأصل','131','0','0');
