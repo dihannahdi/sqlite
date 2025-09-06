9d91519dcaf21def25503b06c4bf9970e2df05ed
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:18 +0700

    Upload SQL chunk: t4185_chunk_0000.sql

---
+++ sql_chunks/t4185_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t4185
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t4185" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4185
+INSERT INTO [t4185] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t4185] VALUES (2,'أولا: العقائد في القرأن الكريم','9','0','0');
+INSERT INTO [t4185] VALUES (3,'الزكاة في القرآن الكريم','83','0','0');
+INSERT INTO [t4185] VALUES (4,'الصوم في القرآن الكريم','98','0','0');
+INSERT INTO [t4185] VALUES (5,'الأمر بالمعروف والنهي عن المنكر','105','0','0');
+INSERT INTO [t4185] VALUES (6,'الإحسان إلى الوالدين في القرآن الكريم','124','0','0');
+INSERT INTO [t4185] VALUES (7,'نظام الأسرة في القرآن الكريم','131','0','0');
