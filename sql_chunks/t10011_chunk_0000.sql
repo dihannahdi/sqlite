37e3036caf92d9949e1a556239aa1cfe33cbad6a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:08 +0700

    Upload SQL chunk: t10011_chunk_0000.sql

---
+++ sql_chunks/t10011_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t10011
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t10011" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10011
+INSERT INTO [t10011] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10011] VALUES (2,'الخلاصة','25','0','0');
+INSERT INTO [t10011] VALUES (3,'المصادر والمراجع','33','0','0');
+INSERT INTO [t10011] VALUES (4,'فهرس الموضوعات','42','0','0');
