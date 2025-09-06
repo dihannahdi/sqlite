c943269346ec527eb1296f58e18541dd131b64a6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:55 +0700

    Upload SQL chunk: t10542_chunk_0000.sql

---
+++ sql_chunks/t10542_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t10542
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t10542" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10542
+INSERT INTO [t10542] VALUES (1,'باب الألف','2','0','0');
+INSERT INTO [t10542] VALUES (2,'باب من اسمه إسماعيل','5','1','0');
+INSERT INTO [t10542] VALUES (3,'باب من اسمه إسحاق','7','1','0');
+INSERT INTO [t10542] VALUES (4,'باب من اسمه أيوب','8','1','0');
+INSERT INTO [t10542] VALUES (5,'باب من اسمه أبان','10','1','0');
+INSERT INTO [t10542] VALUES (6,'باب من اسمه أسد','11','1','0');
+INSERT INTO [t10542] VALUES (7,'باب من اسمه أصرم','11','1','0');
+INSERT INTO [t10542] VALUES (8,'باب الباء','12','0','0');
+INSERT INTO [t10542] VALUES (9,'باب الثاء','14','0','0');
+INSERT INTO [t10542] VALUES (10,'باب الجيم','14','0','0');
+INSERT INTO [t10542] VALUES (11,'باب الحاء','18','0','0');
+INSERT INTO [t10542] VALUES (12,'باب الخاء','29','0','0');
+INSERT INTO [t10542] VALUES (13,'باب الدال','32','0','0');
+INSERT INTO [t10542] VALUES (14,'باب الذال','33','0','0');
+INSERT INTO [t10542] VALUES (15,'باب الراء','34','0','0');
+INSERT INTO [t10542] VALUES (16,'باب الزاء','36','0','0');
+INSERT INTO [t10542] VALUES (17,'باب السين','39','0','0');
+INSERT INTO [t10542] VALUES (18,'باب الشين','47','0','0');
+INSERT INTO [t10542] VALUES (19,'باب الصاد','48','0','0');
+INSERT INTO [t10542] VALUES (20,'باب الطاء','51','0','0');
+INSERT INTO [t10542] VALUES (21,'باب الظاء','53','0','0');
+INSERT INTO [t10542] VALUES (22,'باب العين','53','0','0');
+INSERT INTO [t10542] VALUES (23,'باب النون','103','0','0');
+INSERT INTO [t10542] VALUES (24,'باب الواو','106','0','0');
+INSERT INTO [t10542] VALUES (25,'باب الهاء','107','0','0');
+INSERT INTO [t10542] VALUES (26,'باب الياء','108','0','0');
+INSERT INTO [t10542] VALUES (27,'باب الكنى','114','0','0');
