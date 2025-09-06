60998a97a6b5ffb50c50cc3a647349712a333fcd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:22 +0700

    Upload SQL chunk: t11354_chunk_0000.sql

---
+++ sql_chunks/t11354_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t11354
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t11354" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11354
+INSERT INTO [t11354] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11354] VALUES (2,'تحقيق نسبة الكتاب إلى المؤلف','7','0','0');
+INSERT INTO [t11354] VALUES (3,'تحقيق اسم الكتاب','8','0','0');
+INSERT INTO [t11354] VALUES (4,'طبعات الكتاب','10','0','0');
+INSERT INTO [t11354] VALUES (5,'أمثلة السقط','11','0','0');
+INSERT INTO [t11354] VALUES (6,'أمثلة التحريف','13','0','0');
+INSERT INTO [t11354] VALUES (7,'أمثلة الزيادة','15','0','0');
+INSERT INTO [t11354] VALUES (8,'وصف النسخ الخطية','18','0','0');
+INSERT INTO [t11354] VALUES (9,'منهجي في التحقيق','21','0','0');
+INSERT INTO [t11354] VALUES (10,'ترجمة موجزة للشيخ عبد الرحمن رحمه الله','23','0','0');
+INSERT INTO [t11354] VALUES (11,'ابن جرجيس، وموقف آئمة الدعوة السلفية منه','25','0','0');
+INSERT INTO [t11354] VALUES (12,'نماذج مصورة من النسخ الخطية','28','0','0');
+INSERT INTO [t11354] VALUES (13,'مقدمة المؤلف','36','0','0');
+INSERT INTO [t11354] VALUES (14,'فصل','60','0','0');
+INSERT INTO [t11354] VALUES (15,'فصل','81','0','0');
+INSERT INTO [t11354] VALUES (16,'فصل','92','0','0');
+INSERT INTO [t11354] VALUES (17,'فصل','125','0','0');
+INSERT INTO [t11354] VALUES (18,'فصل','128','0','0');
+INSERT INTO [t11354] VALUES (19,'فصل','185','0','0');
+INSERT INTO [t11354] VALUES (20,'فصل','253','0','0');
+INSERT INTO [t11354] VALUES (21,'خاتمة','319','0','0');
+INSERT INTO [t11354] VALUES (22,'تتمة','332','0','0');
+INSERT INTO [t11354] VALUES (23,'فهرس الأحاديث','347','0','0');
+INSERT INTO [t11354] VALUES (24,'فهرس الموضوعات','352','0','0');
