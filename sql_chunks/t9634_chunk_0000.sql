d6ca615e978f9ff01282f255787c3fd4d07d8fd2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:57:14 +0700

    Upload SQL chunk: t9634_chunk_0000.sql

---
+++ sql_chunks/t9634_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t9634
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t9634" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9634
+INSERT INTO [t9634] VALUES (1,'الجزء الأول','1','0','0');
+INSERT INTO [t9634] VALUES (2,'الجزء الثاني','110','0','0');
+INSERT INTO [t9634] VALUES (3,'الجزء الثالث','228','0','0');
+INSERT INTO [t9634] VALUES (4,'الجزء الرابع','285','0','0');
+INSERT INTO [t9634] VALUES (5,'الجزء الخامس','365','0','0');
+INSERT INTO [t9634] VALUES (6,'الجزء السادس','484','0','0');
+INSERT INTO [t9634] VALUES (7,'الجزء السابع','542','0','0');
+INSERT INTO [t9634] VALUES (8,'الجزء الثامن','625','0','0');
+INSERT INTO [t9634] VALUES (9,'الجزء التاسع','730','0','0');
+INSERT INTO [t9634] VALUES (10,'الجزء العاشر','819','0','0');
+INSERT INTO [t9634] VALUES (11,'الجزء الحادي عشر','900','0','0');
+INSERT INTO [t9634] VALUES (12,'الجزء الثاني عشر','960','0','0');
+INSERT INTO [t9634] VALUES (13,'الجزء الثالث عشر','1016','0','0');
+INSERT INTO [t9634] VALUES (14,'الجزء الرابع عشر','1071','0','0');
+INSERT INTO [t9634] VALUES (15,'الجزء الخامس عشر','1145','0','0');
+INSERT INTO [t9634] VALUES (16,'الجزء السادس عشر','1218','0','0');
+INSERT INTO [t9634] VALUES (17,'الجزء السابع عشر','1274','0','0');
