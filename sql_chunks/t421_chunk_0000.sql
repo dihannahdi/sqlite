fb69635c17f70776319cc981d8484432637bf0b1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:23 +0700

    Upload SQL chunk: t421_chunk_0000.sql

---
+++ sql_chunks/t421_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t421
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t421" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t421
+INSERT INTO [t421] VALUES (1,'دفاع عن الحديث النبوي والسيرة','1','0','0');
