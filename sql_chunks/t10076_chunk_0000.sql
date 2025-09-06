95493963926d2c8dc98e863bc6b39eb40fdc7a3e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:40 +0700

    Upload SQL chunk: t10076_chunk_0000.sql

---
+++ sql_chunks/t10076_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10076
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10076" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10076
+INSERT INTO [t10076] VALUES (1,'أحاديث منتخبة من مشيخة ابن صصري - مخطوط','2','0','0');
