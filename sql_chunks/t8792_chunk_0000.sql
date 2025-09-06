31ad3f22871b6d52db9ad4e81142cbb7027ad9fb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:06 +0700

    Upload SQL chunk: t8792_chunk_0000.sql

---
+++ sql_chunks/t8792_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t8792
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t8792" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8792
+INSERT INTO [t8792] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8792] VALUES (2,'المراد بالبدهيات','3','0','0');
+INSERT INTO [t8792] VALUES (3,'صلة البدهيات بالبلاغة','7','0','0');
+INSERT INTO [t8792] VALUES (4,'مدخل','7','3','0');
+INSERT INTO [t8792] VALUES (5,'الإطناب','9','3','0');
+INSERT INTO [t8792] VALUES (6,'التأكيد','16','3','0');
+INSERT INTO [t8792] VALUES (7,'أنواع البدهيات','20','0','0');
+INSERT INTO [t8792] VALUES (8,'مدخل','20','7','0');
+INSERT INTO [t8792] VALUES (9,'أولا: البدهية الحسابية','21','7','0');
+INSERT INTO [t8792] VALUES (10,'ثانيا: البدهية اللغوية','30','7','0');
+INSERT INTO [t8792] VALUES (11,'ثالثا: البدهية العادية','37','7','0');
