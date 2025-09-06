07b773703abe9faf4b675753ddd36a6a2dcff51c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:55 +0700

    Upload SQL chunk: t1988_chunk_0000.sql

---
+++ sql_chunks/t1988_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t1988
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t1988" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1988
+INSERT INTO [t1988] VALUES (1,'المبحث الأول: مدخل الدراسة','1','0','0');
+INSERT INTO [t1988] VALUES (2,'المبحث الثاني: الغزو التربوي','4','0','0');
+INSERT INTO [t1988] VALUES (3,'أساليب الغزو التربوي','4','2','0');
+INSERT INTO [t1988] VALUES (4,'استخدام المصطلحات الغربية','8','2','0');
+INSERT INTO [t1988] VALUES (5,'الديمقراطية','8','4','0');
+INSERT INTO [t1988] VALUES (6,'الاشتراكية','12','4','0');
+INSERT INTO [t1988] VALUES (7,'المبحث الثالث: حقيقة الفلسفة','17','0','0');
+INSERT INTO [t1988] VALUES (8,'مفهوم الفلسفة','17','7','0');
+INSERT INTO [t1988] VALUES (9,'مدخل','17','8','0');
+INSERT INTO [t1988] VALUES (10,'الفلسفة عبر العصور','27','7','0');
+INSERT INTO [t1988] VALUES (11,'أثر الفلسفة في المجتمع','31','7','0');
+INSERT INTO [t1988] VALUES (12,'أوالا: جماعة إخوان الصفا','31','11','0');
+INSERT INTO [t1988] VALUES (13,'ثانيا: جماعة المعتزلة','33','11','0');
+INSERT INTO [t1988] VALUES (14,'الفلسفة الحديثة','35','7','0');
+INSERT INTO [t1988] VALUES (15,'ميدان الفلسفة','38','7','0');
+INSERT INTO [t1988] VALUES (16,'المبحث الرابع: الفلسفة من منظور المنهج الإسلامي','43','0','0');
+INSERT INTO [t1988] VALUES (17,'أولا: نماذج من الأخطاء والأخطار الفلسفية','43','16','0');
+INSERT INTO [t1988] VALUES (18,'ثانيا: إلصاق الفلسفة بالإسلام','49','16','0');
+INSERT INTO [t1988] VALUES (19,'آراء العلماء في الفلسفة','55','16','0');
+INSERT INTO [t1988] VALUES (20,'الخاتمة','60','0','0');
+INSERT INTO [t1988] VALUES (21,'النتائج والتوصيات','62','0','0');
+INSERT INTO [t1988] VALUES (22,'أولا: النتائج','62','21','0');
+INSERT INTO [t1988] VALUES (23,'التوصيات','64','21','0');
+INSERT INTO [t1988] VALUES (24,'مصادر ومراجع','66','0','0');
