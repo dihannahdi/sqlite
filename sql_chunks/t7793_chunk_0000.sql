301b6a0dea9d8cbb928c053d3e7e9ab9694f2dde
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:04 +0700

    Upload SQL chunk: t7793_chunk_0000.sql

---
+++ sql_chunks/t7793_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t7793
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t7793" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7793
+INSERT INTO [t7793] VALUES (1,'مقدمة كتاب حكم الرطوبة','2','0','0');
+INSERT INTO [t7793] VALUES (2,'أهمية الموضوع','3','0','0');
+INSERT INTO [t7793] VALUES (3,'السبب الباعث على البحث:','4','0','0');
+INSERT INTO [t7793] VALUES (4,'تمهيد في السوائل الخارجة من المرأة','5','0','0');
+INSERT INTO [t7793] VALUES (5,'أنواع السوائل:','5','0','0');
+INSERT INTO [t7793] VALUES (6,'(١) البول:','5','5','0');
+INSERT INTO [t7793] VALUES (7,'(٢) المذي:','7','5','0');
+INSERT INTO [t7793] VALUES (8,'(٣) المني:','7','5','0');
+INSERT INTO [t7793] VALUES (9,'(٤) الودي:','7','5','0');
+INSERT INTO [t7793] VALUES (10,'(٥) الحيض:','7','5','0');
+INSERT INTO [t7793] VALUES (11,'(٦) الاستحاضة:','8','5','0');
+INSERT INTO [t7793] VALUES (12,'(٧) السوائل الخارجة من سائر الجسد:','8','5','0');
+INSERT INTO [t7793] VALUES (13,'(٨) رطوبة فرج المرأة:','8','5','0');
+INSERT INTO [t7793] VALUES (14,'حكم الصفرة والكدرة في زمن الحيض','9','0','0');
+INSERT INTO [t7793] VALUES (15,'بم تطهر الحائض؟','15','0','0');
+INSERT INTO [t7793] VALUES (16,'حكم الرطوبة في غير أيام الحيض','19','0','0');
+INSERT INTO [t7793] VALUES (17,'١ـ طهارة الرطوبة','19','0','0');
+INSERT INTO [t7793] VALUES (18,'القائلون بنجاسة الرطوبة مطلقا:','20','17','0');
+INSERT INTO [t7793] VALUES (19,'القائلون بنجاسة الرطوبة الباطنة:','21','17','0');
+INSERT INTO [t7793] VALUES (20,'القائلون بطهارة الرطوبة:','24','17','0');
+INSERT INTO [t7793] VALUES (21,'٢ـ هل تنقض الرطوبة الوضوء:','27','0','0');
+INSERT INTO [t7793] VALUES (22,'وبعد هذا البحث في أقوال الفقهاء يترجح لدي أن الرطوبة لا تنقض الوضوء لأمور:','33','0','0');
+INSERT INTO [t7793] VALUES (23,'الشك في الطهارة وكيف تعالج الوسوسة','41','0','0');
+INSERT INTO [t7793] VALUES (24,'خاتمة','46','0','0');
+INSERT INTO [t7793] VALUES (25,'قائمة المراجع','47','0','0');
