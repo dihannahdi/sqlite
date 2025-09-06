8356997060fd8a009a7742dec51694daa56c70f4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:46 +0700

    Upload SQL chunk: t21818_chunk_0000.sql

---
+++ sql_chunks/t21818_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t21818
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t21818" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21818
+INSERT INTO [t21818] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t21818] VALUES (2,'الفصل الأول في حكم الصيام','1','0','0');
+INSERT INTO [t21818] VALUES (3,'الفصل الثاني في حكم الصيام وفوائده','3','0','0');
+INSERT INTO [t21818] VALUES (4,'الفصل الثالث في حكم صيام المريض والمسافر','5','0','0');
+INSERT INTO [t21818] VALUES (5,'الفصل الرابع في مفسدات الصوم وهي المفطرات','8','0','0');
+INSERT INTO [t21818] VALUES (6,'الفصل الخامس في التراويح','10','0','0');
+INSERT INTO [t21818] VALUES (7,'الفصل السادس في الزكاة وفوائدها','12','0','0');
+INSERT INTO [t21818] VALUES (8,'الفصل السابع في أهل الزكاة','17','0','0');
+INSERT INTO [t21818] VALUES (9,'الفصل الثامن في زكاة الفطر','19','0','0');
