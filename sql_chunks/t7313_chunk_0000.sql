703412532205379875b209bbea37b32cc3c72230
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:23 +0700

    Upload SQL chunk: t7313_chunk_0000.sql

---
+++ sql_chunks/t7313_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t7313
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t7313" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7313
+INSERT INTO [t7313] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7313] VALUES (2,'طبقات لحظ الألحاظ','4','0','0');
+INSERT INTO [t7313] VALUES (3,'الطبقة الخامسة والعشرون','49','0','0');
+INSERT INTO [t7313] VALUES (4,'الطبقة السادسة والعشرون','64','0','0');
+INSERT INTO [t7313] VALUES (5,'الطبقة السابعة والعشرون','75','0','0');
+INSERT INTO [t7313] VALUES (6,'طبقة أخرى صغرى','113','0','0');
+INSERT INTO [t7313] VALUES (7,'الخاتمة','173','0','0');
