ec9dd43c85b01d41bd47a49fc82efa51711cc6af
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:38 +0700

    Upload SQL chunk: t22817_chunk_0000.sql

---
+++ sql_chunks/t22817_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t22817
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t22817" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22817
+INSERT INTO [t22817] VALUES (1,'أول واجب على المكلف عبادة الله تعالى وضوح ذلك من كتاب الله ودعوات الرسل','1','0','0');
+INSERT INTO [t22817] VALUES (2,'ثمرة التوحيد','8','0','0');
