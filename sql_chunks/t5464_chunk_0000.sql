3cfd2214101ad36fe9a7e8fe09c6acd8d56581ed
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:23:11 +0700

    Upload SQL chunk: t5464_chunk_0000.sql

---
+++ sql_chunks/t5464_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t5464
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t5464" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5464
+INSERT INTO [t5464] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t5464] VALUES (2,'باب مخارج الحروف','4','0','0');
+INSERT INTO [t5464] VALUES (3,'باب صفات الحروف اللازمة','10','0','0');
+INSERT INTO [t5464] VALUES (4,'باب أحكام النون الساكنة والتنوين','16','0','0');
+INSERT INTO [t5464] VALUES (5,'باب أحكام الميم الساكنة','21','0','0');
+INSERT INTO [t5464] VALUES (6,'باب اللامات السواكن','22','0','0');
+INSERT INTO [t5464] VALUES (7,'باب المتماثلين والمتقاربين والمتجانسين والمتباعدين','24','0','0');
+INSERT INTO [t5464] VALUES (8,'باب المد والقصر','28','0','0');
+INSERT INTO [t5464] VALUES (9,'باب التفخيم والترقيق','34','0','0');
+INSERT INTO [t5464] VALUES (10,'كيفية الوقف على أواخر الكلم','37','0','0');
+INSERT INTO [t5464] VALUES (11,'باب الوقف، والابتداء، والقطع، والسكت','41','0','0');
+INSERT INTO [t5464] VALUES (12,'باب المقطوع والموصول','45','0','0');
+INSERT INTO [t5464] VALUES (13,'باب هاء التأنيث التي كتبت تاء مجرورة','51','0','0');
+INSERT INTO [t5464] VALUES (14,'باب الإثبات والحذف','54','0','0');
+INSERT INTO [t5464] VALUES (15,'باب همزة الوصل','57','0','0');
+INSERT INTO [t5464] VALUES (16,'أمور تراعى لحفص','59','0','0');
+INSERT INTO [t5464] VALUES (17,'المراجع','60','0','0');
