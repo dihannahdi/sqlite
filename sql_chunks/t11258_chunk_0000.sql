be7a88f9bdd97b1d08ad482a9a00cbf2c8765166
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:07 +0700

    Upload SQL chunk: t11258_chunk_0000.sql

---
+++ sql_chunks/t11258_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11258
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11258" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11258
+INSERT INTO [t11258] VALUES (1,'شرح حديث النزول','1','0','0');
+INSERT INTO [t11258] VALUES (2,'[حديث النزول]','1','0','0');
