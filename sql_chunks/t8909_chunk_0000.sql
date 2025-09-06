1660d36e0d34c9f0d096a120c9c1672d79c9d36b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:39 +0700

    Upload SQL chunk: t8909_chunk_0000.sql

---
+++ sql_chunks/t8909_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t8909
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t8909" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8909
+INSERT INTO [t8909] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8909] VALUES (2,'المدبج','7','0','0');
+INSERT INTO [t8909] VALUES (3,'تعريفة','7','2','0');
+INSERT INTO [t8909] VALUES (4,'امثلة المدبج','10','2','0');
+INSERT INTO [t8909] VALUES (5,'رواية الأقران','16','0','0');
+INSERT INTO [t8909] VALUES (6,'تعريفة','16','5','0');
+INSERT INTO [t8909] VALUES (7,'امثلة رواية الأقران','18','5','0');
+INSERT INTO [t8909] VALUES (8,'الصلة بين رواية الأقران والمدبج','23','0','0');
+INSERT INTO [t8909] VALUES (9,'مدخل','23','8','0');
+INSERT INTO [t8909] VALUES (10,'معرفة طبقات العلماء','25','8','0');
+INSERT INTO [t8909] VALUES (11,'معرفة تواريخ الرواة والوفيات','27','8','0');
+INSERT INTO [t8909] VALUES (12,'معرفة رواية الأكابر عن الأصاغر','29','8','0');
+INSERT INTO [t8909] VALUES (13,'معرفة رواية الأباء عن الأبناء','32','8','0');
+INSERT INTO [t8909] VALUES (14,'فوائد معرفة المدبج','34','0','0');
+INSERT INTO [t8909] VALUES (15,'أشهر المصنفات في المدبج، ورواية الأقران','36','0','0');
+INSERT INTO [t8909] VALUES (16,'عناية الإمام الحافظ أبي عبد الله محمد بن إسماعيل البخاري برواية الأقران في كتابه (الجامع الصحيح)','38','0','0');
+INSERT INTO [t8909] VALUES (17,'عناية كتب التراجم برواية الأقران','60','0','0');
+INSERT INTO [t8909] VALUES (18,'الخاتمة','62','0','0');
+INSERT INTO [t8909] VALUES (19,'مصادر ومراجع','64','0','0');
