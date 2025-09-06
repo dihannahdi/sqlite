920cfe764adb5456cb88e37bb0fdc5334608c4dc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:40 +0700

    Upload SQL chunk: t21494_chunk_0000.sql

---
+++ sql_chunks/t21494_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t21494
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t21494" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21494
+INSERT INTO [t21494] VALUES (1,'باب الباء','1199','0','0');
+INSERT INTO [t21494] VALUES (2,'باب التاء','1221','0','0');
+INSERT INTO [t21494] VALUES (3,'باب الجيم','1227','0','0');
+INSERT INTO [t21494] VALUES (4,'باب الحاء','1503','0','0');
+INSERT INTO [t21494] VALUES (5,'باب الخاء','1528','0','0');
+INSERT INTO [t21494] VALUES (6,'باب الدال','1543','0','0');
+INSERT INTO [t21494] VALUES (7,'حديث الترقفي','1790','0','0');
+INSERT INTO [t21494] VALUES (8,'باب (ن)','2331','0','0');
+INSERT INTO [t21494] VALUES (9,'باب الهاء','2340','0','0');
+INSERT INTO [t21494] VALUES (10,'باب ي','2355','0','0');
