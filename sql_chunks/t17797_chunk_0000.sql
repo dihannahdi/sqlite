136b530cf2a55cdd02cb4e65f6c335ff15bab246
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:32 +0700

    Upload SQL chunk: t17797_chunk_0000.sql

---
+++ sql_chunks/t17797_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t17797
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t17797" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17797
+INSERT INTO [t17797] VALUES (1,'مقدمة المعتني','5','0','0');
+INSERT INTO [t17797] VALUES (2,'وصف النسخ المعتمدة في التحقيق','6','1','0');
+INSERT INTO [t17797] VALUES (3,'ترجمة العلامة صفي الدين البخاري','8','1','0');
+INSERT INTO [t17797] VALUES (4,'أول الرسالة','19','0','0');
+INSERT INTO [t17797] VALUES (5,'أقسام المسلسلات','20','0','0');
+INSERT INTO [t17797] VALUES (6,'تبيان طرق الشيخ صفي الدين البخاري في سماع الحديث المسلسل بالأولية','21','0','0');
+INSERT INTO [t17797] VALUES (7,'خاتمة','43','0','0');
