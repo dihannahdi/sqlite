37f83a37b7ee2e41418998954f05242be82ff016
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:22 +0700

    Upload SQL chunk: t11753_chunk_0000.sql

---
+++ sql_chunks/t11753_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t11753
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t11753" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11753
+INSERT INTO [t11753] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11753] VALUES (2,'أولا: كرمة الإنسان','5','0','0');
+INSERT INTO [t11753] VALUES (3,'ثانيا: العنصرية والسلالية','7','0','0');
+INSERT INTO [t11753] VALUES (4,'ثالثا: حقوق المرأة','9','0','0');
+INSERT INTO [t11753] VALUES (5,'رابعا: حقوق الأقليات','10','0','0');
+INSERT INTO [t11753] VALUES (6,'خامسا: حرية التعبير','13','0','0');
+INSERT INTO [t11753] VALUES (7,'سادسا: حوار الحضارات','15','0','0');
+INSERT INTO [t11753] VALUES (8,'سابعا: رفض الإرهاب','17','0','0');
+INSERT INTO [t11753] VALUES (9,'ثامنا: الحقوق الإقتصادية','19','0','0');
+INSERT INTO [t11753] VALUES (10,'تاسعا: حقوق العمال','21','0','0');
+INSERT INTO [t11753] VALUES (11,'عاشرا: الإسلام والتعاون العالمي','23','0','0');
+INSERT INTO [t11753] VALUES (12,'أحد عشر: قوانين الحرب','26','0','0');
+INSERT INTO [t11753] VALUES (13,'ثاني عشر: حقوق الإنسان والقضايا الإسلامية','28','0','0');
+INSERT INTO [t11753] VALUES (14,'قضية فلسطين','28','13','0');
+INSERT INTO [t11753] VALUES (15,'قضية الشيشان','32','13','0');
+INSERT INTO [t11753] VALUES (16,'مسلمو البلقان','35','13','0');
+INSERT INTO [t11753] VALUES (17,'قضية جامو وكشمير','39','13','0');
+INSERT INTO [t11753] VALUES (18,'خاتمة','42','0','0');
