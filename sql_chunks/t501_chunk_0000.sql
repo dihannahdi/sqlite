49741630231f28f211a6b4f8ada39963b8a4fa7a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:34 +0700

    Upload SQL chunk: t501_chunk_0000.sql

---
+++ sql_chunks/t501_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t501
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t501" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t501
+INSERT INTO [t501] VALUES (1,'نشأة اللغة','3','0','0');
+INSERT INTO [t501] VALUES (2,'تنور','11','0','0');
+INSERT INTO [t501] VALUES (3,'تفاح','18','0','0');
+INSERT INTO [t501] VALUES (4,'ضمير الشأن والقصة','19','0','0');
+INSERT INTO [t501] VALUES (5,'كاد','24','0','0');
