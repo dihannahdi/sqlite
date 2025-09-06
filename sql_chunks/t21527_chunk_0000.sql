4181dfca4f4e5bd74bb0405b417df27328791dcf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:15 +0700

    Upload SQL chunk: t21527_chunk_0000.sql

---
+++ sql_chunks/t21527_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t21527
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t21527" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21527
+INSERT INTO [t21527] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t21527] VALUES (2,'اللقاء الأول','5','0','0');
+INSERT INTO [t21527] VALUES (3,'اللقاء الثاني','34','0','0');
+INSERT INTO [t21527] VALUES (4,'اللقاء الثالث','58','0','0');
+INSERT INTO [t21527] VALUES (5,'اللقاء الرابع','100','0','0');
+INSERT INTO [t21527] VALUES (6,'اللقاء الخامس','129','0','0');
+INSERT INTO [t21527] VALUES (7,'اللقاء السادس','162','0','0');
+INSERT INTO [t21527] VALUES (8,'اللقاء السابع','186','0','0');
