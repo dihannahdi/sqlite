7ae8f67b5c6cd3d2d34d015c67fc5e8d66e9c7e7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:12:32 +0700

    Upload SQL chunk: t132428_chunk_0000.sql

---
+++ sql_chunks/t132428_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t132428
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t132428" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t132428
+INSERT INTO [t132428] VALUES (1,'تقديم','1','0','0');
+INSERT INTO [t132428] VALUES (2,'الآية (١)','7','0','0');
+INSERT INTO [t132428] VALUES (3,'الآية (٢)','9','0','0');
+INSERT INTO [t132428] VALUES (4,'الآية (٣)','12','0','0');
+INSERT INTO [t132428] VALUES (5,'الآية (٤)','15','0','0');
+INSERT INTO [t132428] VALUES (6,'الآية (٥)','18','0','0');
+INSERT INTO [t132428] VALUES (7,'الآية (٦)','20','0','0');
+INSERT INTO [t132428] VALUES (8,'الآية (٧)','36','0','0');
+INSERT INTO [t132428] VALUES (9,'الآيتان (٨، ٩)','41','0','0');
+INSERT INTO [t132428] VALUES (10,'الآية (١٠)','49','0','0');
+INSERT INTO [t132428] VALUES (11,'الآية (١١)','63','0','0');
+INSERT INTO [t132428] VALUES (12,'الآية (١٢)','67','0','0');
+INSERT INTO [t132428] VALUES (13,'الآية (١٣)','74','0','0');
+INSERT INTO [t132428] VALUES (14,'الآية (١٤)','80','0','0');
+INSERT INTO [t132428] VALUES (15,'الآية (١٥)','86','0','0');
+INSERT INTO [t132428] VALUES (16,'الآية (١٦)','96','0','0');
+INSERT INTO [t132428] VALUES (17,'الآية (١٧)','101','0','0');
+INSERT INTO [t132428] VALUES (18,'الآية (١٨)','106','0','0');
+INSERT INTO [t132428] VALUES (19,'الآية (١٩)','109','0','0');
+INSERT INTO [t132428] VALUES (20,'الآية (٢٠)','115','0','0');
+INSERT INTO [t132428] VALUES (21,'الآية (٢١)','124','0','0');
+INSERT INTO [t132428] VALUES (22,'الآية (٢٢)','129','0','0');
+INSERT INTO [t132428] VALUES (23,'الآية (٢٣)','134','0','0');
+INSERT INTO [t132428] VALUES (24,'الآية (٢٤)','139','0','0');
+INSERT INTO [t132428] VALUES (25,'الآية (٢٥)','144','0','0');
+INSERT INTO [t132428] VALUES (26,'الآية (٢٦)','152','0','0');
+INSERT INTO [t132428] VALUES (27,'الآية (٢٧)','158','0','0');
+INSERT INTO [t132428] VALUES (28,'الآية (٢٨)','163','0','0');
+INSERT INTO [t132428] VALUES (29,'الآية (٢٩)','167','0','0');
+INSERT INTO [t132428] VALUES (30,'الآية (٣٠)','174','0','0');
+INSERT INTO [t132428] VALUES (31,'الآية (٣١)','178','0','0');
+INSERT INTO [t132428] VALUES (32,'الآية (٣٢)','182','0','0');
+INSERT INTO [t132428] VALUES (33,'الآية (٣٣)','189','0','0');
+INSERT INTO [t132428] VALUES (34,'الآية (٣٤)','197','0','0');
