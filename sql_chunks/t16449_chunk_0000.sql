ff1d06f11e9c3bb42d41382a242b8595c5697b8b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:26:13 +0700

    Upload SQL chunk: t16449_chunk_0000.sql

---
+++ sql_chunks/t16449_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t16449
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t16449" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t16449
+INSERT INTO [t16449] VALUES (1,'بسم الله الرحمن الرحيم','3','0','0');
+INSERT INTO [t16449] VALUES (2,'أصل العبادة هو المحبة','8','0','0');
+INSERT INTO [t16449] VALUES (3,'من أحب في الله','13','0','0');
+INSERT INTO [t16449] VALUES (4,'أسماء من كان يزجر بالهجر من الصحابة والتابعين','26','0','0');
+INSERT INTO [t16449] VALUES (5,'نشكو إليك ولا لغيرك نلتجي','38','0','0');
