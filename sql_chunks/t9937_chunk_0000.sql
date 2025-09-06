71a78b897530727aaa810c598a1f5037bfa3ba60
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:05 +0700

    Upload SQL chunk: t9937_chunk_0000.sql

---
+++ sql_chunks/t9937_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t9937
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t9937" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9937
+INSERT INTO [t9937] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t9937] VALUES (2,'توطئة تاريخية','4','0','0');
+INSERT INTO [t9937] VALUES (3,'مدخل','4','2','0');
+INSERT INTO [t9937] VALUES (4,'بوادر اللحن','6','2','0');
+INSERT INTO [t9937] VALUES (5,'نشأة النحو','25','2','0');
+INSERT INTO [t9937] VALUES (6,'المدرستان الأوليان','30','0','0');
+INSERT INTO [t9937] VALUES (7,'مدرسة البصرة','30','6','0');
+INSERT INTO [t9937] VALUES (8,'الطبقة الأولى من البصريين','30','7','0');
+INSERT INTO [t9937] VALUES (9,'الطبقة الثانية من البصريين','33','7','0');
+INSERT INTO [t9937] VALUES (10,'مدرسة الكوفة','38','6','0');
+INSERT INTO [t9937] VALUES (11,'نشأة الخلاف واحتكاك المدرستين','42','6','0');
+INSERT INTO [t9937] VALUES (12,'مدخل','42','11','0');
+INSERT INTO [t9937] VALUES (13,'بين الكسائي والأصمعي','44','11','0');
+INSERT INTO [t9937] VALUES (14,'بين الكسائي وسيبويه','46','11','0');
+INSERT INTO [t9937] VALUES (15,'بين الكسائي والبريدي','49','11','0');
+INSERT INTO [t9937] VALUES (16,'الفروق بين المذهبين البصري والكوفي','69','6','0');
+INSERT INTO [t9937] VALUES (17,'أمر السماع','69','16','0');
+INSERT INTO [t9937] VALUES (18,'أمر القياس','72','16','0');
+INSERT INTO [t9937] VALUES (19,'نموذج من الخلاف بين المذهبين','80','6','0');
+INSERT INTO [t9937] VALUES (20,'مسألة سوف','80','19','0');
+INSERT INTO [t9937] VALUES (21,'أثر العصبية في الخلاف','84','19','0');
+INSERT INTO [t9937] VALUES (22,'كتب الخلاف بين المذهبين','95','19','0');
+INSERT INTO [t9937] VALUES (23,'المذهب البغدادي','98','0','0');
+INSERT INTO [t9937] VALUES (24,'المدرسة الأندلسية','101','0','0');
+INSERT INTO [t9937] VALUES (25,'خاتمة','114','0','0');
+INSERT INTO [t9937] VALUES (26,'كتب ونصوص','116','0','0');
+INSERT INTO [t9937] VALUES (27,'الكتاب لسيبويه','116','26','0');
+INSERT INTO [t9937] VALUES (28,'ابن جني وكتاباه','125','26','0');
+INSERT INTO [t9937] VALUES (29,'الزمخشري وكتابه المفصل','140','26','0');
+INSERT INTO [t9937] VALUES (30,'ابن الأنباري كمال الدين','150','26','0');
+INSERT INTO [t9937] VALUES (31,'ابن مالك','181','26','0');
+INSERT INTO [t9937] VALUES (32,'ابن هشام الأنصاري','193','26','0');
+INSERT INTO [t9937] VALUES (33,'سيبويه','213','26','0');
+INSERT INTO [t9937] VALUES (34,'فهرس','215','0','0');
