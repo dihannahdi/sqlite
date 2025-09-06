f0aab5f09fc94f190673a3e97800a9ca291be690
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:15 +0700

    Upload SQL chunk: t416_chunk_0000.sql

---
+++ sql_chunks/t416_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t416
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t416" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t416
+INSERT INTO [t416] VALUES (1,'حكم تارك الصلاة','1','0','0');
