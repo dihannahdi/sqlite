23a79b801fc9aff97ccdd4fb7001eebe37f9a76a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:12 +0700

    Upload SQL chunk: t7053_chunk_0000.sql

---
+++ sql_chunks/t7053_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7053
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7053" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7053
+INSERT INTO [t7053] VALUES (1,'هذا كتاب العشرات','1','0','0');
+INSERT INTO [t7053] VALUES (2,'تم كتاب العشرات','65','0','0');
