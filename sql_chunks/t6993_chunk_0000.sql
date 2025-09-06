1d8753ca6006734ba1ea33a7f0bea355971b6bb8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:36 +0700

    Upload SQL chunk: t6993_chunk_0000.sql

---
+++ sql_chunks/t6993_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6993
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6993" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6993
+INSERT INTO [t6993] VALUES (1,'جزء جمعت فيه الأخبار المروية في سبب وضع علم العربية','1','0','0');
+INSERT INTO [t6993] VALUES (2,'فصل','32','0','0');
