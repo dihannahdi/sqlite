a653c0ca5ecc50265248b8fd962c441c480c3d13
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:33 +0700

    Upload SQL chunk: t454_chunk_0000.sql

---
+++ sql_chunks/t454_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t454
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t454" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t454
+INSERT INTO [t454] VALUES (1,'مقدمات في البيع','2','0','0');
+INSERT INTO [t454] VALUES (2,'فصل: في البيوع المنهي عنها','3','1','0');
+INSERT INTO [t454] VALUES (3,'باب الربا','5','0','0');
+INSERT INTO [t454] VALUES (4,'باب بيع الأصول والثمار','8','0','0');
+INSERT INTO [t454] VALUES (5,'فصل: في بيع الثمر على الشجر','8','4','0');
+INSERT INTO [t454] VALUES (6,'باب الخيار','10','0','0');
+INSERT INTO [t454] VALUES (7,'باب السلم','13','0','0');
+INSERT INTO [t454] VALUES (8,'باب القرض','15','0','0');
+INSERT INTO [t454] VALUES (9,'باب أحكام الدين','16','0','0');
