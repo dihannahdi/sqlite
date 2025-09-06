e8fbd59c7364984668d96fc863c7ce3c9f49ace4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:34 +0700

    Upload SQL chunk: t95576_chunk_0000.sql

---
+++ sql_chunks/t95576_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t95576
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t95576" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95576
+INSERT INTO [t95576] VALUES (1,'فصل','4','0','0');
+INSERT INTO [t95576] VALUES (2,'في ذكر من وضع الخط وأصله ووصله وفصله','4','0','0');
+INSERT INTO [t95576] VALUES (3,'فصل','7','2','0');
+INSERT INTO [t95576] VALUES (4,'في فضل الخط وما قيل فيه','7','0','0');
+INSERT INTO [t95576] VALUES (5,'فصل','13','4','0');
+INSERT INTO [t95576] VALUES (6,'في القلم، وما لهم فيه من الحكم','13','0','0');
+INSERT INTO [t95576] VALUES (7,'فصل','18','6','0');
+INSERT INTO [t95576] VALUES (8,'في الدواة وصفتها وآلاتها','18','0','0');
+INSERT INTO [t95576] VALUES (9,'فصل','23','8','0');
+INSERT INTO [t95576] VALUES (10,'في المداد','23','0','0');
+INSERT INTO [t95576] VALUES (11,'فصل','26','10','0');
+INSERT INTO [t95576] VALUES (12,'في بري الأقلام','26','0','0');
+INSERT INTO [t95576] VALUES (13,'فصل','31','12','0');
+INSERT INTO [t95576] VALUES (14,'في النقط','31','0','0');
+INSERT INTO [t95576] VALUES (15,'فصل','33','14','0');
+INSERT INTO [t95576] VALUES (16,'في الشكل','33','0','0');
+INSERT INTO [t95576] VALUES (17,'فصل','35','16','0');
+INSERT INTO [t95576] VALUES (18,'في ذكر حروف المعجم وسرها في تعيين العدد','35','0','0');
+INSERT INTO [t95576] VALUES (19,'فصل','38','18','0');
+INSERT INTO [t95576] VALUES (20,'في ذكر الكتبة الكرام','38','0','0');
+INSERT INTO [t95576] VALUES (21,'خاتمة','68','0','0');
+INSERT INTO [t95576] VALUES (22,'الأول: في بيان أدب التلميذ مع الشيخ','68','21','0');
+INSERT INTO [t95576] VALUES (23,'الثاني: نصيحة لسائر الخطاطين','69','21','0');
