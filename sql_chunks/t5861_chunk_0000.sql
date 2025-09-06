ee97f027e411b8e380e49c585d33e0148153d34c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:04 +0700

    Upload SQL chunk: t5861_chunk_0000.sql

---
+++ sql_chunks/t5861_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t5861
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t5861" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5861
+INSERT INTO [t5861] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5861] VALUES (2,'أما بعد','1','0','0');
+INSERT INTO [t5861] VALUES (3,'حرف الألف','2','0','0');
+INSERT INTO [t5861] VALUES (4,'حرف الباء الموحدة','12','0','0');
+INSERT INTO [t5861] VALUES (5,'حرف التاء المثناة','15','0','0');
+INSERT INTO [t5861] VALUES (6,'حرف الثاء المثلثة','16','0','0');
+INSERT INTO [t5861] VALUES (7,'حرف الجيم','17','0','0');
+INSERT INTO [t5861] VALUES (8,'حرف الحاء المهملة','22','0','0');
+INSERT INTO [t5861] VALUES (9,'حرف الخاء المعجمة إلى السين','33','0','0');
+INSERT INTO [t5861] VALUES (10,'حرف الذال المعجمة','35','0','0');
+INSERT INTO [t5861] VALUES (11,'حرف الراء','36','0','0');
+INSERT INTO [t5861] VALUES (12,'حرف الزاي','38','0','0');
+INSERT INTO [t5861] VALUES (13,'حرف السين المهملة','45','0','0');
+INSERT INTO [t5861] VALUES (14,'حرف الشين المعجمة','59','0','0');
+INSERT INTO [t5861] VALUES (15,'حرف الصاد إلى العين','62','0','0');
+INSERT INTO [t5861] VALUES (16,'حرف الضاد المعجمة','64','0','0');
+INSERT INTO [t5861] VALUES (17,'حرف الطاء','65','0','0');
+INSERT INTO [t5861] VALUES (18,'حرف العين المهملة','68','0','0');
+INSERT INTO [t5861] VALUES (19,'حرف الغين المعجمة','113','0','0');
+INSERT INTO [t5861] VALUES (20,'حرف الفاء خال','114','0','0');
+INSERT INTO [t5861] VALUES (21,'حرف القاف','115','0','0');
+INSERT INTO [t5861] VALUES (22,'حرف الكاف','117','0','0');
+INSERT INTO [t5861] VALUES (23,'حرف اللام','119','0','0');
+INSERT INTO [t5861] VALUES (24,'حرف الميم','120','0','0');
+INSERT INTO [t5861] VALUES (25,'حرف النون','144','0','0');
+INSERT INTO [t5861] VALUES (26,'حرف الهاء','145','0','0');
+INSERT INTO [t5861] VALUES (27,'حرف الواو','147','0','0');
+INSERT INTO [t5861] VALUES (28,'حرف الياء آخر الحروف','149','0','0');
+INSERT INTO [t5861] VALUES (29,'فصل في الكنى','155','0','0');
+INSERT INTO [t5861] VALUES (30,'فصل فيمن لم يسم على سياق الرواة عنهم مرتبا','174','0','0');
