48479c2908f23d6f3c9e857bf806c6ca416af27a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:40 +0700

    Upload SQL chunk: t10085_chunk_0000.sql

---
+++ sql_chunks/t10085_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10085
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10085" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10085
+INSERT INTO [t10085] VALUES (1,'المذهب الاول','1','0','0');
+INSERT INTO [t10085] VALUES (2,'المذهب الثاني','3','0','0');
+INSERT INTO [t10085] VALUES (3,'المذهب الثالث','5','0','0');
