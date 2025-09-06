9e106a5950f425ac752beefcee8ec8a9e2fb53ee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:32 +0700

    Upload SQL chunk: t5385_chunk_0000.sql

---
+++ sql_chunks/t5385_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t5385
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t5385" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5385
+INSERT INTO [t5385] VALUES (1,'حرف الهمزة','1','0','0');
+INSERT INTO [t5385] VALUES (2,'حرف الباء','10','0','0');
+INSERT INTO [t5385] VALUES (3,'حرف التاء','10','0','0');
+INSERT INTO [t5385] VALUES (4,'حرف الجيم','12','0','0');
+INSERT INTO [t5385] VALUES (5,'حرف الحاء','13','0','0');
+INSERT INTO [t5385] VALUES (6,'حرف الخاء','14','0','0');
+INSERT INTO [t5385] VALUES (7,'حرف الدال','15','0','0');
+INSERT INTO [t5385] VALUES (8,'حرف الذال','16','0','0');
+INSERT INTO [t5385] VALUES (9,'حرف الراء','17','0','0');
+INSERT INTO [t5385] VALUES (10,'حرف الزاي','18','0','0');
+INSERT INTO [t5385] VALUES (11,'حرف السين','18','0','0');
+INSERT INTO [t5385] VALUES (12,'حرف الشين','19','0','0');
+INSERT INTO [t5385] VALUES (13,'حرف الصاد','20','0','0');
+INSERT INTO [t5385] VALUES (14,'حرف الضاد','20','0','0');
+INSERT INTO [t5385] VALUES (15,'حرف الطاء','21','0','0');
+INSERT INTO [t5385] VALUES (16,'حرف الظاء','22','0','0');
+INSERT INTO [t5385] VALUES (17,'حرف العين','22','0','0');
+INSERT INTO [t5385] VALUES (18,'حرف الغين','24','0','0');
+INSERT INTO [t5385] VALUES (19,'حرف الفاء','24','0','0');
+INSERT INTO [t5385] VALUES (20,'حرف القاف','26','0','0');
+INSERT INTO [t5385] VALUES (21,'حرف الكاف','27','0','0');
+INSERT INTO [t5385] VALUES (22,'حرف اللام','29','0','0');
+INSERT INTO [t5385] VALUES (23,'حرف الميم','31','0','0');
+INSERT INTO [t5385] VALUES (24,'حرف النون','34','0','0');
+INSERT INTO [t5385] VALUES (25,'حرف الهاء','35','0','0');
+INSERT INTO [t5385] VALUES (26,'حرف الواو','36','0','0');
+INSERT INTO [t5385] VALUES (27,'حرف اللام ألف','44','0','0');
+INSERT INTO [t5385] VALUES (28,'حرف الياء','46','0','0');
