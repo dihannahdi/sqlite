fc232a3aaf941448dd2862e04d36071b032fe7e7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:28 +0700

    Upload SQL chunk: t29575_chunk_0000.sql

---
+++ sql_chunks/t29575_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t29575
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t29575" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29575
+INSERT INTO [t29575] VALUES (1,'من كلام الشيخ الإمام العلامة جمال الدين أبي عبد الله محمد بن عبد الله بن مالك، رحمه الله.','1','0','0');
+INSERT INTO [t29575] VALUES (2,'الرباعي','13','1','0');
+INSERT INTO [t29575] VALUES (3,'الخماسي','17','1','0');
+INSERT INTO [t29575] VALUES (4,'ومن الأسماء الموجودة في المفصل، ولم يتكلم عليها','18','0','0');
+INSERT INTO [t29575] VALUES (5,'المصادر والمراجع','20','0','0');
