b29677b11420680e4922db23d936edc7e1042472
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:45:36 +0700

    Upload SQL chunk: t8338_chunk_0000.sql

---
+++ sql_chunks/t8338_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t8338
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t8338" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8338
+INSERT INTO [t8338] VALUES (1,'آخر الجزء','27','0','0');
