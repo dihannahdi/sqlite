f0f9e8c4250c096caed03cf71d91ccc5cbbed495
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:16 +0700

    Upload SQL chunk: t6873_chunk_0000.sql

---
+++ sql_chunks/t6873_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6873
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6873" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6873
+INSERT INTO [t6873] VALUES (1,'زيادة المرء في دنياه نقصان ... وربحه غير محض الخير خسران','1','0','0');
+INSERT INTO [t6873] VALUES (2,'ما ضر حسانها والطبع صائغها ... أن لم يصغها قريع الشعر حسان','10','0','0');
