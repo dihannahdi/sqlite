ae1288b52980277c47f4f7962122f784f91a8cae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:00 +0700

    Upload SQL chunk: t11324_chunk_0000.sql

---
+++ sql_chunks/t11324_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t11324
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t11324" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11324
+INSERT INTO [t11324] VALUES (1,'فتاوى ومسائل','1','0','0');
+INSERT INTO [t11324] VALUES (2,'المسألة الأولى','1','0','0');
+INSERT INTO [t11324] VALUES (3,'المسألة الثانية','5','0','0');
+INSERT INTO [t11324] VALUES (4,'المسألة الثالثة','8','0','0');
+INSERT INTO [t11324] VALUES (5,'المسألة الرابعة','12','0','0');
+INSERT INTO [t11324] VALUES (6,'المسألة الخامسة','23','0','0');
+INSERT INTO [t11324] VALUES (7,'المسألة السادسة','30','0','0');
+INSERT INTO [t11324] VALUES (8,'المسألة السابعة','38','0','0');
+INSERT INTO [t11324] VALUES (9,'المسألة الثامنة','39','0','0');
+INSERT INTO [t11324] VALUES (10,'المسألة التاسعة','40','0','0');
+INSERT INTO [t11324] VALUES (11,'المسألة العاشرة','43','0','0');
+INSERT INTO [t11324] VALUES (12,'المسألة الحادية عشرة','44','0','0');
+INSERT INTO [t11324] VALUES (13,'والمسألة الثانية عشرة','48','0','0');
+INSERT INTO [t11324] VALUES (14,'المسألة الثالثة عشرة','52','0','0');
+INSERT INTO [t11324] VALUES (15,'المسألة الرابعة عشرة','54','0','0');
+INSERT INTO [t11324] VALUES (16,'المسألة الحادية عشرة','65','0','0');
+INSERT INTO [t11324] VALUES (17,'المسألة الخامسة عشرة','68','0','0');
+INSERT INTO [t11324] VALUES (18,'المسألة السادسة عشرة','71','0','0');
+INSERT INTO [t11324] VALUES (19,'المسألة السابعة عشرة','76','0','0');
+INSERT INTO [t11324] VALUES (20,'المسألة الثامنة عشرة','78','0','0');
+INSERT INTO [t11324] VALUES (21,'المسألة التاسعة عشرة','81','0','0');
+INSERT INTO [t11324] VALUES (22,'المسألة العشرين','82','0','0');
+INSERT INTO [t11324] VALUES (23,'المسألة الحادية والعشرون','83','0','0');
+INSERT INTO [t11324] VALUES (24,'المسألة الثانية والعشرون','84','0','0');
+INSERT INTO [t11324] VALUES (25,'هذه مسائل أجاب عنها الشيخ محمد بن عبد الوهاب','88','0','0');
+INSERT INTO [t11324] VALUES (26,'هذه المسائل التي في السؤال','90','0','0');
