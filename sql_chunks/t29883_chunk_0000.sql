e83b1a67a4f083890561257bca03c082e498d892
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:58:24 +0700

    Upload SQL chunk: t29883_chunk_0000.sql

---
+++ sql_chunks/t29883_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t29883
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t29883" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29883
+INSERT INTO [t29883] VALUES (1,'مقدمة الطبعة الثالثة','2','0','0');
+INSERT INTO [t29883] VALUES (2,'المقدمة','4','0','0');
+INSERT INTO [t29883] VALUES (3,'١ - منها إحداث هيئة في المصافة للصلاة:','8','0','0');
+INSERT INTO [t29883] VALUES (4,'٢ - ومنها: وضع اليدين على النحر تحت الذقن:','15','0','0');
+INSERT INTO [t29883] VALUES (5,'٣ - زيادة الانفراش والتمدد في السجود:','32','0','0');
+INSERT INTO [t29883] VALUES (6,'٤ - الإشارة:','35','0','0');
+INSERT INTO [t29883] VALUES (7,'٥ - ومنها: صفة «العجن»:','44','0','0');
+INSERT INTO [t29883] VALUES (8,'٦ - التطبيق العملي الجديد لحديث عبد الله بن الزبير:','46','0','0');
+INSERT INTO [t29883] VALUES (9,'٧ - ومنها: قصر عقد التسبيح وعده على أصابع اليد اليمنى.','49','0','0');
+INSERT INTO [t29883] VALUES (10,'٨ - ومنها ضم العقبين في السجود:','62','0','0');
+INSERT INTO [t29883] VALUES (11,'وختاما:','73','0','0');
