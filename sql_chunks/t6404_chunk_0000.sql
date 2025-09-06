42fbd02af44cccd6f91ed99e18e8af8de3b5d849
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:28 +0700

    Upload SQL chunk: t6404_chunk_0000.sql

---
+++ sql_chunks/t6404_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t6404
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t6404" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6404
+INSERT INTO [t6404] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6404] VALUES (2,'قول عبد الله بن عباس رضي الله عنهما في ذلك','6','0','0');
+INSERT INTO [t6404] VALUES (3,'ما روي عن عمرو بن دينار رضي الله عنه','8','0','0');
+INSERT INTO [t6404] VALUES (4,'ما ذكر عن أحمد بن سنان رضي الله عنه','13','0','0');
