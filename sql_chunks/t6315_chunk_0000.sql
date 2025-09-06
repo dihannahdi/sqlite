685d3dfe8347ffaa8581a3e82fa9445833b40d54
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:41 +0700

    Upload SQL chunk: t6315_chunk_0000.sql

---
+++ sql_chunks/t6315_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t6315
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t6315" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6315
+INSERT INTO [t6315] VALUES (1,'الباب الأول في الأحكام الشرعية','14','0','0');
+INSERT INTO [t6315] VALUES (2,'الباب الثاني في الأدلة','36','0','0');
+INSERT INTO [t6315] VALUES (3,'الباب الثالث في المنطوق والمفهوم','214','0','0');
+INSERT INTO [t6315] VALUES (4,'الباب الرابع حقيقة الكلام مع المجاز','246','0','0');
+INSERT INTO [t6315] VALUES (5,'الباب الخامس في الأمر والنهي','257','0','0');
+INSERT INTO [t6315] VALUES (6,'الباب السادس في العام والإطلاق','281','0','0');
+INSERT INTO [t6315] VALUES (7,'الباب السابع في المجمل المذكور في الخطاب','334','0','0');
+INSERT INTO [t6315] VALUES (8,'الباب الثامن في النسخ','351','0','0');
+INSERT INTO [t6315] VALUES (9,'الباب العاشر في الترجيح والتصحيح','401','0','0');
