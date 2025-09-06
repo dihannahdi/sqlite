938b975913a3a8ebab3de9a1f2b22768cd0ea8b3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:37:07 +0700

    Upload SQL chunk: t2084_chunk_0000.sql

---
+++ sql_chunks/t2084_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t2084
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t2084" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2084
+INSERT INTO [t2084] VALUES (1,'نظم الأجرومية','1','0','0');
+INSERT INTO [t2084] VALUES (2,'باب الكلام','2','0','0');
+INSERT INTO [t2084] VALUES (3,'باب الإعراب','3','0','0');
+INSERT INTO [t2084] VALUES (4,'باب علامات الإعراب','4','0','0');
+INSERT INTO [t2084] VALUES (5,'باب علامات النصب','5','0','0');
+INSERT INTO [t2084] VALUES (6,'باب علامات الخفض','6','0','0');
+INSERT INTO [t2084] VALUES (7,'باب علامات الجزم','7','0','0');
+INSERT INTO [t2084] VALUES (8,'فصل','8','0','0');
+INSERT INTO [t2084] VALUES (9,'باب المعرفة والنكرة','9','0','0');
+INSERT INTO [t2084] VALUES (10,'باب الأفعال','10','0','0');
+INSERT INTO [t2084] VALUES (11,'باب إعراب الفعل','11','0','0');
+INSERT INTO [t2084] VALUES (12,'باب مرفوعات الأسماء','12','0','0');
+INSERT INTO [t2084] VALUES (13,'باب نائب الفاعل','13','0','0');
+INSERT INTO [t2084] VALUES (14,'باب المبتدإ والخبر','14','0','0');
+INSERT INTO [t2084] VALUES (15,'كان وأخواتها','15','0','0');
+INSERT INTO [t2084] VALUES (16,'إن وأخواتها','16','0','0');
+INSERT INTO [t2084] VALUES (17,'ظن وأخواتها','17','0','0');
+INSERT INTO [t2084] VALUES (18,'باب النعت','18','0','0');
+INSERT INTO [t2084] VALUES (19,'باب العطف','19','0','0');
+INSERT INTO [t2084] VALUES (20,'باب التوكيد','20','0','0');
+INSERT INTO [t2084] VALUES (21,'باب البدل','21','0','0');
+INSERT INTO [t2084] VALUES (22,'باب منصوبات الأسماء','22','0','0');
+INSERT INTO [t2084] VALUES (23,'باب المصدر','23','0','0');
+INSERT INTO [t2084] VALUES (24,'باب الظرف','24','0','0');
+INSERT INTO [t2084] VALUES (25,'باب الحال','25','0','0');
+INSERT INTO [t2084] VALUES (26,'باب التمييز','26','0','0');
+INSERT INTO [t2084] VALUES (27,'باب الاستثناء','27','0','0');
+INSERT INTO [t2084] VALUES (28,'باب لا العاملة عمل إن','28','0','0');
+INSERT INTO [t2084] VALUES (29,'باب النداء','29','0','0');
+INSERT INTO [t2084] VALUES (30,'باب المفعول لأجله','30','0','0');
+INSERT INTO [t2084] VALUES (31,'باب المفعول معه','31','0','0');
+INSERT INTO [t2084] VALUES (32,'باب مخفوضات الأسماء','32','0','0');
+INSERT INTO [t2084] VALUES (33,'باب الإضافة','33','0','0');
