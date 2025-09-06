fe031ef80ba8724228bea78d1494a345c1287abb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:53 +0700

    Upload SQL chunk: t4349_chunk_0000.sql

---
+++ sql_chunks/t4349_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t4349
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t4349" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4349
+INSERT INTO [t4349] VALUES (1,'إتحاف الأحباب بما ثبت في مسألة الحجاب','1','0','0');
+INSERT INTO [t4349] VALUES (2,'بقلم الشيخ عبد القادر بن حبيب الله السندي','1','0','0');
