9fd95d459ee87dde088e41ef9d82c5fdb1dd1301
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:04 +0700

    Upload SQL chunk: t411_chunk_0000.sql

---
+++ sql_chunks/t411_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t411
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t411" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t411
+INSERT INTO [t411] VALUES (1,'حقوق النساء في الإسلام','1','0','0');
