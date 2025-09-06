5f4a394bc430e1f93488f77a07c85e198bb4c8f3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:08:19 +0700

    Upload SQL chunk: t12882_chunk_0000.sql

---
+++ sql_chunks/t12882_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t12882
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t12882" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12882
+INSERT INTO [t12882] VALUES (1,'الأربعون الودعانية لابن ودعان ٤٩٤ - مخطوط','2','0','0');
