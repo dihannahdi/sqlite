9bda6245eef8163f0e971a3daeff278cccf1d152
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:47 +0700

    Upload SQL chunk: t10831_chunk_0000.sql

---
+++ sql_chunks/t10831_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10831
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10831" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10831
+INSERT INTO [t10831] VALUES (1,'كتاب العوالي لشمس الدين محمد بن محمد بن محمد الجزري','2','0','0');
