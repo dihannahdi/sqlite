41d5876820181796f3649449bfb2fbca91673bd3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:27 +0700

    Upload SQL chunk: t7318_chunk_0000.sql

---
+++ sql_chunks/t7318_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t7318
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t7318" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7318
+INSERT INTO [t7318] VALUES (1,'المقدمات','1','0','0');
+INSERT INTO [t7318] VALUES (2,'تقديم بقلم: زهير الشاويش','1','1','0');
+INSERT INTO [t7318] VALUES (3,'مقدمة: الإمام الألباني','3','1','0');
+INSERT INTO [t7318] VALUES (4,'باب: وصف المخطوطة','6','0','0');
+INSERT INTO [t7318] VALUES (5,'باب: موضوع الكتاب وخطورته','20','0','0');
+INSERT INTO [t7318] VALUES (6,'باب: شبهات وجوابها','65','0','0');
+INSERT INTO [t7318] VALUES (7,'الشبهة الأولى: التشبيه','65','6','0');
+INSERT INTO [t7318] VALUES (8,'الشبهة الثانية: الجهة','67','6','0');
+INSERT INTO [t7318] VALUES (9,'الشبهة الثالثة: المكان','70','6','0');
+INSERT INTO [t7318] VALUES (10,'باب: كتاب العلو','77','0','0');
+INSERT INTO [t7318] VALUES (11,'باب: تعقيب','286','0','0');
+INSERT INTO [t7318] VALUES (12,'باب: فهرس الموضوعات','288','0','0');
