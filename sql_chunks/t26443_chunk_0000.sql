4a738bb38c0757b4d488c9da51765917007e7515
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:29 +0700

    Upload SQL chunk: t26443_chunk_0000.sql

---
+++ sql_chunks/t26443_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26443
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26443" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26443
+INSERT INTO [t26443] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26443] VALUES (2,'الجزء فيه الأول مما رواه الأكابر عن الأصاغر من المحدثين من الأفراد','2','0','0');
