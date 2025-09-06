2907c67253ab67257f0bdf0a9ac011552b47363f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:44 +0700

    Upload SQL chunk: t400_chunk_0000.sql

---
+++ sql_chunks/t400_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t400
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t400" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t400
+INSERT INTO [t400] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t400] VALUES (2,'وبه نستعين','1','1','0');
+INSERT INTO [t400] VALUES (3,'تابع لسنة ٦٢٦هـ','1','0','0');
+INSERT INTO [t400] VALUES (4,'سنة سبع وعشرين وستمائة','3','0','0');
+INSERT INTO [t400] VALUES (5,'سنة ثمان وعشرين وستمائة','4','0','0');
+INSERT INTO [t400] VALUES (6,'سنة تسع وعشرون وستمائة','6','0','0');
+INSERT INTO [t400] VALUES (7,'ذكر عزل الوزير مؤيد الدين القمي','8','6','0');
+INSERT INTO [t400] VALUES (8,'وولاية نصير الدين أبي الازهر أحمد بن الناقد','8','6','0');
+INSERT INTO [t400] VALUES (9,'سنة ثلاثين وستمائة','9','0','0');
+INSERT INTO [t400] VALUES (10,'ذكر فتح أربل','11','9','0');
+INSERT INTO [t400] VALUES (11,'ذكر عدة حوادث','13','9','0');
+INSERT INTO [t400] VALUES (12,'سنة أحدى وثلاثين وستمائة','13','0','0');
+INSERT INTO [t400] VALUES (13,'ذكر فتح المدرسة المستنصرية','14','12','0');
+INSERT INTO [t400] VALUES (14,'تلخيص شروط هذه المدرسة','15','12','0');
+INSERT INTO [t400] VALUES (15,'ذكر عدة حوادث','15','12','0');
+INSERT INTO [t400] VALUES (16,'سنة اثنتين وثلاثين وستمائة','19','0','0');
+INSERT INTO [t400] VALUES (17,'ذكر فتح المدرسة الشرقية الشرابية بواسط','21','16','0');
+INSERT INTO [t400] VALUES (18,'سنة ثلاث وثلاثين وستمائة','21','0','0');
+INSERT INTO [t400] VALUES (19,'سنة أربع وثلاثين وستمائة','24','0','0');
+INSERT INTO [t400] VALUES (20,'ذكر حصر أربل','27','19','0');
+INSERT INTO [t400] VALUES (21,'سنة خمس وثلاثين وستمائة','27','0','0');
+INSERT INTO [t400] VALUES (22,'ذكر وصول عساكر المغول نواحي العراق','30','21','0');
