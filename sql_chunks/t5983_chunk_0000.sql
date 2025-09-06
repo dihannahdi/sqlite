334b522835743e6aa3f5c428cc043a212602e410
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:03 +0700

    Upload SQL chunk: t5983_chunk_0000.sql

---
+++ sql_chunks/t5983_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t5983
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t5983" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5983
+INSERT INTO [t5983] VALUES (1,'مقدمة المصنف','1','0','0');
+INSERT INTO [t5983] VALUES (2,'رواية صاحب البدعة المكفرة والمفسقة','2','0','0');
+INSERT INTO [t5983] VALUES (3,'مسألة قبول كافر التأويل وفاسقه','7','0','0');
+INSERT INTO [t5983] VALUES (4,'الغلو في الدين','9','0','0');
+INSERT INTO [t5983] VALUES (5,'أقسام التشيع','11','0','0');
+INSERT INTO [t5983] VALUES (6,'تعريف النصب','14','0','0');
+INSERT INTO [t5983] VALUES (7,'حقيقة البدعة','22','0','0');
+INSERT INTO [t5983] VALUES (8,'اشتراط العدالة في رسم الصحيح والحسن','24','0','0');
+INSERT INTO [t5983] VALUES (9,'ذم المبتدعة','26','0','0');
+INSERT INTO [t5983] VALUES (10,'تفسير العدالة','30','0','0');
+INSERT INTO [t5983] VALUES (11,'نقد العدالة بالملكة المذكورة','32','0','0');
+INSERT INTO [t5983] VALUES (12,'قبول فاسق التأويل','42','0','0');
+INSERT INTO [t5983] VALUES (13,'كلام مستحسن للشافعي في العدالة','48','0','0');
+INSERT INTO [t5983] VALUES (14,'قف على هذه النكتة','60','0','0');
+INSERT INTO [t5983] VALUES (15,'ثقة المبتدع','60','0','0');
+INSERT INTO [t5983] VALUES (16,'مناقشة غريبة','68','0','0');
+INSERT INTO [t5983] VALUES (17,'أقسام الرواة','69','0','0');
+INSERT INTO [t5983] VALUES (18,'الفرق بين الشهادة والرواية','70','0','0');
+INSERT INTO [t5983] VALUES (19,'فسق التأويل','73','0','0');
+INSERT INTO [t5983] VALUES (20,'المبتدع الداعية إلى بدعت','79','0','0');
+INSERT INTO [t5983] VALUES (21,'تنبيه','79','0','0');
+INSERT INTO [t5983] VALUES (22,'جلالة الصحابة','81','0','0');
+INSERT INTO [t5983] VALUES (23,'نتائج البحث','92','0','0');
