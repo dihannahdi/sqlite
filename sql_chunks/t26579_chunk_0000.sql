ca3f8710a9ae00f50d748cacad27a58dfcbc0e2d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:43 +0700

    Upload SQL chunk: t26579_chunk_0000.sql

---
+++ sql_chunks/t26579_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t26579
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t26579" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26579
+INSERT INTO [t26579] VALUES (1,'جزء فيه ستة مجالس من أمالي أبي يعلى الفراء','1','0','0');
+INSERT INTO [t26579] VALUES (2,'المجلس الأول','2','0','0');
+INSERT INTO [t26579] VALUES (3,'المجلس الثاني','15','0','0');
+INSERT INTO [t26579] VALUES (4,'المجلس الثالث','29','0','0');
+INSERT INTO [t26579] VALUES (5,'المجلس الرابع','47','0','0');
+INSERT INTO [t26579] VALUES (6,'المجلس الخامس','62','0','0');
+INSERT INTO [t26579] VALUES (7,'المجلس السادس','74','0','0');
