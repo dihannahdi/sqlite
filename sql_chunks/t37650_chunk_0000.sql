463efaffe13a130bdf8e789358b7423c70c02952
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:14:55 +0700

    Upload SQL chunk: t37650_chunk_0000.sql

---
+++ sql_chunks/t37650_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t37650
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t37650" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37650
+INSERT INTO [t37650] VALUES (1,'المحتوى','5','0','0');
+INSERT INTO [t37650] VALUES (2,'الإهداء','9','0','0');
+INSERT INTO [t37650] VALUES (3,'مقدمة الطبعة الثانية','12','0','0');
+INSERT INTO [t37650] VALUES (4,'مقدمة','15','0','0');
+INSERT INTO [t37650] VALUES (5,'تنبيه','18','0','0');
+INSERT INTO [t37650] VALUES (6,'تمهيد','19','0','0');
+INSERT INTO [t37650] VALUES (7,'الجزء الأول','27','0','0');
+INSERT INTO [t37650] VALUES (8,'أبناء المستعمرات الأفرسيوية وعالم الكبار','28','7','0');
+INSERT INTO [t37650] VALUES (9,'التعايش أو الوجود المشترك والاستعمار المشترك','51','7','0');
+INSERT INTO [t37650] VALUES (10,'مشكلة الرجل الأفرسيوي','70','7','0');
+INSERT INTO [t37650] VALUES (11,'الجزء الثاني','88','0','0');
+INSERT INTO [t37650] VALUES (12,'صفحة من التاريخ','89','11','0');
+INSERT INTO [t37650] VALUES (13,'أوان المسؤولية','102','11','0');
+INSERT INTO [t37650] VALUES (14,'الكتلة العربية الآسيوية','114','11','0');
+INSERT INTO [t37650] VALUES (15,'مشكلة الحضارة','121','11','0');
+INSERT INTO [t37650] VALUES (16,'نظرات عامه في الثفافة الأفرسيوية','132','11','0');
+INSERT INTO [t37650] VALUES (17,'مبادئ اقتصاد أفرسيوي فعال','146','11','0');
+INSERT INTO [t37650] VALUES (18,'الجزء الثالث','166','0','0');
+INSERT INTO [t37650] VALUES (19,'فكرة الأفرسيوية والتعايش','167','18','0');
+INSERT INTO [t37650] VALUES (20,'فكرة الأفرسيوية والعالمية','189','18','0');
+INSERT INTO [t37650] VALUES (21,'العالم الإسلامي وفكرة الأفرسيوية','215','18','0');
+INSERT INTO [t37650] VALUES (22,'أوروبه وفكرة الأفرسيوية','245','18','0');
+INSERT INTO [t37650] VALUES (23,'نتيجة البحث','259','0','0');
