8a28893efb10fe9b9642ddbbd9cd32ca78f3aa20
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:24 +0700

    Upload SQL chunk: t8663_chunk_0000.sql

---
+++ sql_chunks/t8663_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t8663
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t8663" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8663
+INSERT INTO [t8663] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t8663] VALUES (2,'القسم الأول: قسم الدراسة','5','0','0');
+INSERT INTO [t8663] VALUES (3,'ترجمة ابن الملقن','5','2','0');
+INSERT INTO [t8663] VALUES (4,'ترجمة الإمام السخاوي','10','2','0');
+INSERT INTO [t8663] VALUES (5,'نسخ الكتاب','14','2','0');
+INSERT INTO [t8663] VALUES (6,'عملي في التحقيق','17','2','0');
+INSERT INTO [t8663] VALUES (7,'توثيق نسبة الكتاب','20','2','0');
+INSERT INTO [t8663] VALUES (8,'القسم الثاني: قسم التحقيق','22','0','0');
+INSERT INTO [t8663] VALUES (9,'النص المحقق','22','8','0');
+INSERT INTO [t8663] VALUES (10,'الفهارس','108','0','0');
+INSERT INTO [t8663] VALUES (11,'المخطوطات','133','0','0');
