4d0ed354e8bad88c29b1c0f4dfc33dbbdff3be38
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:20:22 +0700

    Upload SQL chunk: t14567_chunk_0000.sql

---
+++ sql_chunks/t14567_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t14567
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t14567" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14567
+INSERT INTO [t14567] VALUES (1,'العظة والعضة','2','0','0');
+INSERT INTO [t14567] VALUES (2,'الحافظ والحافض','3','0','0');
+INSERT INTO [t14567] VALUES (3,'الحاظر والحاضر','4','0','0');
+INSERT INTO [t14567] VALUES (4,'الظن والضن','6','0','0');
+INSERT INTO [t14567] VALUES (5,'الفظ والفض','7','0','0');
+INSERT INTO [t14567] VALUES (6,'الغيظ والغيض','8','0','0');
+INSERT INTO [t14567] VALUES (7,'العظ والعض','9','0','0');
+INSERT INTO [t14567] VALUES (8,'الحظ والحض','10','0','0');
+INSERT INTO [t14567] VALUES (9,'الظرار والضرار','10','0','0');
+INSERT INTO [t14567] VALUES (10,'العظم والعضم','11','0','0');
+INSERT INTO [t14567] VALUES (11,'الظلع والضلع','14','0','0');
+INSERT INTO [t14567] VALUES (12,'الظال والضال','15','0','0');
+INSERT INTO [t14567] VALUES (13,'الظمآن والضمان','16','0','0');
+INSERT INTO [t14567] VALUES (14,'الظهر والضهر','17','0','0');
+INSERT INTO [t14567] VALUES (15,'المظ والمض','18','0','0');
+INSERT INTO [t14567] VALUES (16,'البظ والبض','19','0','0');
+INSERT INTO [t14567] VALUES (17,'الظيان والضيان','20','0','0');
+INSERT INTO [t14567] VALUES (18,'العظل والعضل','20','0','0');
+INSERT INTO [t14567] VALUES (19,'القارظ والقارض','21','0','0');
+INSERT INTO [t14567] VALUES (20,'القيظ والقيض','23','0','0');
+INSERT INTO [t14567] VALUES (21,'الفيظ والفيض','24','0','0');
+INSERT INTO [t14567] VALUES (22,'اللظلظة واللضلضة','25','0','0');
+INSERT INTO [t14567] VALUES (23,'الظفرة والضفرة','26','0','0');
+INSERT INTO [t14567] VALUES (24,'الظراب والضراب','27','0','0');
+INSERT INTO [t14567] VALUES (25,'البيظ والبيض','28','0','0');
+INSERT INTO [t14567] VALUES (26,'الحنظل والحنضل','29','0','0');
