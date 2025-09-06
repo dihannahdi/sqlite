04d976dea18480f53367f26baa98169f670bacb7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:36 +0700

    Upload SQL chunk: t23427_chunk_0000.sql

---
+++ sql_chunks/t23427_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t23427
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t23427" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23427
+INSERT INTO [t23427] VALUES (1,'القسم الأول','1','0','0');
+INSERT INTO [t23427] VALUES (2,'القسم الثاني','34','0','0');
+INSERT INTO [t23427] VALUES (3,'القسم الثالث','48','0','0');
+INSERT INTO [t23427] VALUES (4,'القسم الرابع','53','0','0');
+INSERT INTO [t23427] VALUES (5,'(حرف الهمزة)','73','0','0');
+INSERT INTO [t23427] VALUES (6,'(حرف الباء)','79','0','0');
+INSERT INTO [t23427] VALUES (7,'(حرف التاء)','84','0','0');
+INSERT INTO [t23427] VALUES (8,'(حرف الثاء)','86','0','0');
+INSERT INTO [t23427] VALUES (9,'(حرف الجيم)','87','0','0');
+INSERT INTO [t23427] VALUES (10,'(حرف الحاء)','92','0','0');
+INSERT INTO [t23427] VALUES (11,'(حرف الخاء)','102','0','0');
+INSERT INTO [t23427] VALUES (12,'(حرف الدال)','106','0','0');
+INSERT INTO [t23427] VALUES (13,'(حرف الذال)','111','0','0');
+INSERT INTO [t23427] VALUES (14,'(حرف الراء)','113','0','0');
+INSERT INTO [t23427] VALUES (15,'(حرف الزاي)','120','0','0');
+INSERT INTO [t23427] VALUES (16,'(حرف السين)','124','0','0');
+INSERT INTO [t23427] VALUES (17,'(حرف الشين)','134','0','0');
+INSERT INTO [t23427] VALUES (18,'(حرف الصاد)','140','0','0');
+INSERT INTO [t23427] VALUES (19,'(حرف الضاد)','145','0','0');
+INSERT INTO [t23427] VALUES (20,'(حرف الطاء)','147','0','0');
+INSERT INTO [t23427] VALUES (21,'(حرف الظاء)','149','0','0');
+INSERT INTO [t23427] VALUES (22,'(حرف العين)','150','0','0');
+INSERT INTO [t23427] VALUES (23,'(حرف الغين)','162','0','0');
+INSERT INTO [t23427] VALUES (24,'(حرف الفاء)','167','0','0');
+INSERT INTO [t23427] VALUES (25,'(حرف القاف)','170','0','0');
+INSERT INTO [t23427] VALUES (26,'(حرف الكاف)','179','0','0');
+INSERT INTO [t23427] VALUES (27,'(حرف اللام)','183','0','0');
+INSERT INTO [t23427] VALUES (28,'(حرف الميم)','186','0','0');
+INSERT INTO [t23427] VALUES (29,'(حرف النون)','192','0','0');
+INSERT INTO [t23427] VALUES (30,'(حرف الواو)','198','0','0');
+INSERT INTO [t23427] VALUES (31,'(حرف الهاء)','201','0','0');
+INSERT INTO [t23427] VALUES (32,'(حرف الياء)','202','0','0');
