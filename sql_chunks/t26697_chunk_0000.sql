9df4936fee7c899ecb770183794109e647428612
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:12 +0700

    Upload SQL chunk: t26697_chunk_0000.sql

---
+++ sql_chunks/t26697_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t26697
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t26697" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26697
+INSERT INTO [t26697] VALUES (1,'كانت أبرز مقالاتهم هي الاعتماد على كتب النوادر والمحاضرات','9','0','0');
+INSERT INTO [t26697] VALUES (2,'كتاب جولدتسيهر " العقيدة والشريعة "','10','0','0');
+INSERT INTO [t26697] VALUES (3,'تتركز شكوك المستشرقين في السنة حول تأخر تدوين الحديث','12','0','0');
+INSERT INTO [t26697] VALUES (4,'رد عليهم مصطفى السباعي، وأبو الحسن الندوي، وصبحي الصالح وعشرات','13','0','0');
+INSERT INTO [t26697] VALUES (5,'" معراج ابن عباس " والكتاب مكذوب','17','0','0');
+INSERT INTO [t26697] VALUES (6,'وهذا هو الخطر الكامن وراء مهاجمة السنة','20','0','0');
