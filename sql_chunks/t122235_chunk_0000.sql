98704a79c22ae1774064c4d050a908cf5ab8647e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:03:16 +0700

    Upload SQL chunk: t122235_chunk_0000.sql

---
+++ sql_chunks/t122235_chunk_0000.sql
@@ -0,0 +1,52 @@
+-- Table: t122235
+-- Chunk: 1
+-- Rows: 45
+
+CREATE TABLE "t122235" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t122235
+INSERT INTO [t122235] VALUES (1,'[تعريف التصريف]','1','0','0');
+INSERT INTO [t122235] VALUES (2,'[أنواع الأبنية]','1','0','0');
+INSERT INTO [t122235] VALUES (3,'[الميزان الصرفي]','1','0','0');
+INSERT INTO [t122235] VALUES (4,'[القلب المكاني]','2','0','0');
+INSERT INTO [t122235] VALUES (5,'[الصحيح والمعتل]','2','0','0');
+INSERT INTO [t122235] VALUES (6,'[أبنية الاسم الثلاثي المجرد]','2','0','0');
+INSERT INTO [t122235] VALUES (7,'[رد بعض الأبنية إلى بعض]','3','0','0');
+INSERT INTO [t122235] VALUES (8,'[أبنية الاسم الرباعي المجرد]','3','0','0');
+INSERT INTO [t122235] VALUES (9,'[أبنية الاسم الخماسي المجرد]','3','0','0');
+INSERT INTO [t122235] VALUES (10,'[أبنية الاسم المزيد فيه]','3','0','0');
+INSERT INTO [t122235] VALUES (11,'[أحوال الأبنية]','3','0','0');
+INSERT INTO [t122235] VALUES (12,'الماضي','4','11','0');
+INSERT INTO [t122235] VALUES (13,'[أبنية الفعل الثلاثي المجرد]','4','12','0');
+INSERT INTO [t122235] VALUES (14,'[أبنية الفعل الثلاثي المزيد]','4','12','0');
+INSERT INTO [t122235] VALUES (15,'[بناء الفعل الرباعي]','6','12','0');
+INSERT INTO [t122235] VALUES (16,'المضارع','6','11','0');
+INSERT INTO [t122235] VALUES (17,'الصفة المشبهة','7','11','0');
+INSERT INTO [t122235] VALUES (18,'المصدر','8','11','0');
+INSERT INTO [t122235] VALUES (19,'[المصدر من الثلاثي المجرد]','8','18','0');
+INSERT INTO [t122235] VALUES (20,'[المصدر من الثلاثي المزيد والرباعي]','9','18','0');
+INSERT INTO [t122235] VALUES (21,'المصدر الميمي','9','18','0');
+INSERT INTO [t122235] VALUES (22,'اسم المرة','9','18','0');
+INSERT INTO [t122235] VALUES (23,'اسما الزمان والمكان','9','18','0');
+INSERT INTO [t122235] VALUES (24,'اسم الآلة','10','18','0');
+INSERT INTO [t122235] VALUES (25,'التصغير','10','11','0');
+INSERT INTO [t122235] VALUES (26,'النسب','12','11','0');
+INSERT INTO [t122235] VALUES (27,'الجمع','14','11','0');
+INSERT INTO [t122235] VALUES (28,'الثلاثي','14','27','0');
+INSERT INTO [t122235] VALUES (29,'ما زيادته مدة ثالثة','16','27','0');
+INSERT INTO [t122235] VALUES (30,'التقاء الساكنين','19','11','0');
+INSERT INTO [t122235] VALUES (31,'الابتداء','20','11','0');
+INSERT INTO [t122235] VALUES (32,'الوقف','21','11','0');
+INSERT INTO [t122235] VALUES (33,'المقصور والممدود','22','11','0');
+INSERT INTO [t122235] VALUES (34,'ذو الزيادة','23','11','0');
+INSERT INTO [t122235] VALUES (35,'الإمالة','27','11','0');
+INSERT INTO [t122235] VALUES (36,'تخفيف الهمزة','28','11','0');
+INSERT INTO [t122235] VALUES (37,'الإعلال','30','11','0');
+INSERT INTO [t122235] VALUES (38,'العين','31','37','0');
+INSERT INTO [t122235] VALUES (39,'اللام','34','37','0');
+INSERT INTO [t122235] VALUES (40,'الإبدال','35','11','0');
+INSERT INTO [t122235] VALUES (41,'الإدغام','37','11','0');
+INSERT INTO [t122235] VALUES (42,'الحذف','42','11','0');
+INSERT INTO [t122235] VALUES (43,'وهذه مسائل التمرين','42','0','0');
+INSERT INTO [t122235] VALUES (44,'الخط','45','0','0');
+INSERT INTO [t122235] VALUES (45,'ثبت المراجع','49','0','0');
