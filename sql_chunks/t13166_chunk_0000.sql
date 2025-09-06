c41727fdbf824ec9f9d8e433302e638f6ed13b34
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:36 +0700

    Upload SQL chunk: t13166_chunk_0000.sql

---
+++ sql_chunks/t13166_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t13166
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t13166" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13166
+INSERT INTO [t13166] VALUES (1,'جابر بن سمرة، عن سعد رضي الله عنهما','1','0','0');
+INSERT INTO [t13166] VALUES (2,'ابن عباس عن سعد','5','0','0');
+INSERT INTO [t13166] VALUES (3,'عامر بن سعد عن أبيه','7','0','0');
+INSERT INTO [t13166] VALUES (4,'ما روى مصعب بن سعد عن أبيه','38','0','0');
+INSERT INTO [t13166] VALUES (5,'عمر بن سعد عن أبيه','62','0','0');
+INSERT INTO [t13166] VALUES (6,'إبراهيم بن سعد عن أبيه','68','0','0');
+INSERT INTO [t13166] VALUES (7,'محمد بن سعد عن أبيه','73','0','0');
+INSERT INTO [t13166] VALUES (8,'يحيى بن سعد عن أبيه','75','0','0');
+INSERT INTO [t13166] VALUES (9,'عائشة بنت سعد','79','0','0');
