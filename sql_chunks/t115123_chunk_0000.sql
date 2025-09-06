604a57d6a175cb2d5db6fe6817d5dd3e6128e1f5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:57 +0700

    Upload SQL chunk: t115123_chunk_0000.sql

---
+++ sql_chunks/t115123_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t115123
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t115123" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t115123
+INSERT INTO [t115123] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t115123] VALUES (2,'الرجل والفكرة','13','0','0');
+INSERT INTO [t115123] VALUES (3,'الرجل في الميزان','21','0','0');
+INSERT INTO [t115123] VALUES (4,'عبقرية فذة .. وبصيرة نافذة','22','3','0');
+INSERT INTO [t115123] VALUES (5,'شجاعة علي .. وحكمة معاوية','34','3','0');
+INSERT INTO [t115123] VALUES (6,'مؤذن .. ومصباح','45','3','0');
+INSERT INTO [t115123] VALUES (7,'الفكرة تحت المجهر','53','0','0');
+INSERT INTO [t115123] VALUES (8,'الفكرة نحو بعث جديد','54','7','0');
+INSERT INTO [t115123] VALUES (9,'المبادئ في صياغة جديدة','60','7','0');
+INSERT INTO [t115123] VALUES (10,'ماذا يريد الإخوان المسلمون؟','63','7','0');
+INSERT INTO [t115123] VALUES (11,'إلى أي شيء ندعو الناس؟','64','7','0');
+INSERT INTO [t115123] VALUES (12,'لكن ما هي الوسائل؟','66','7','0');
+INSERT INTO [t115123] VALUES (13,'بين النظر ... والتطبيق','68','0','0');
+INSERT INTO [t115123] VALUES (14,'ولكن ما هي الخطوة الثانية في إيضاح؟','70','13','0');
+INSERT INTO [t115123] VALUES (15,'القاعدة والشواذ','75','0','0');
+INSERT INTO [t115123] VALUES (16,'في مجال الابتلاء','76','15','0');
+INSERT INTO [t115123] VALUES (17,'أحقاد الصغار','84','15','0');
+INSERT INTO [t115123] VALUES (18,'الناكثون والناكصون','97','15','0');
+INSERT INTO [t115123] VALUES (19,'كلمات أخيرة','103','0','0');
