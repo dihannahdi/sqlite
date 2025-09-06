170c81ffb0ea47daf431fc7440ab46056811ce91
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:53 +0700

    Upload SQL chunk: t1010_chunk_0000.sql

---
+++ sql_chunks/t1010_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t1010
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t1010" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1010
+INSERT INTO [t1010] VALUES (1,'النقد الأدبي ومدارسه الحديثة','1','0','0');
+INSERT INTO [t1010] VALUES (2,'تصدير','1','0','0');
+INSERT INTO [t1010] VALUES (3,'مقدمة','5','0','0');
+INSERT INTO [t1010] VALUES (4,'الفصل الأول','33','0','0');
+INSERT INTO [t1010] VALUES (5,'الفصل الثاني','86','0','0');
+INSERT INTO [t1010] VALUES (6,'الفصل الثالث','128','0','0');
+INSERT INTO [t1010] VALUES (7,'الفصل الرابع','181','0','0');
+INSERT INTO [t1010] VALUES (8,'الفصل الخامس','213','0','0');
+INSERT INTO [t1010] VALUES (9,'الفصل السادس','240','0','0');
+INSERT INTO [t1010] VALUES (10,'الفصل السابع','282','0','0');
+INSERT INTO [t1010] VALUES (11,'الفصل الثامن','336','0','0');
+INSERT INTO [t1010] VALUES (12,'الفصل التاسع','381','0','0');
+INSERT INTO [t1010] VALUES (13,'الفصل العاشر','443','0','0');
+INSERT INTO [t1010] VALUES (14,'الفصل الحادي عشر','510','0','0');
