8ceeb950bc346e137682733b1cb9f913ebbc7284
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:03:48 +0700

    Upload SQL chunk: t12255_chunk_0000.sql

---
+++ sql_chunks/t12255_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t12255
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t12255" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12255
+INSERT INTO [t12255] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t12255] VALUES (2,'شعره السياسي','2','0','0');
+INSERT INTO [t12255] VALUES (3,'التطور في أفكاره السياسية','6','0','0');
+INSERT INTO [t12255] VALUES (4,'شعر شوقي الاجتماعي','9','0','0');
+INSERT INTO [t12255] VALUES (5,'شعره الوصفي','14','0','0');
+INSERT INTO [t12255] VALUES (6,'الرثاء في شعر شوقي','18','0','0');
+INSERT INTO [t12255] VALUES (7,'شعر شوقي التمثيلي','22','0','0');
+INSERT INTO [t12255] VALUES (8,'مسرحيات شوقي','25','0','0');
