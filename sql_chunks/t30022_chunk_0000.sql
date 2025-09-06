336be5b736613f55e5031f4178c99256b56e34d2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:13 +0700

    Upload SQL chunk: t30022_chunk_0000.sql

---
+++ sql_chunks/t30022_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t30022
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t30022" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30022
+INSERT INTO [t30022] VALUES (1,'إلى هنا عن أبي زرعة','16','0','0');
+INSERT INTO [t30022] VALUES (2,'مدرج إلى عبد الله','18','0','0');
+INSERT INTO [t30022] VALUES (3,'مدرج إلى أحمد','43','0','0');
+INSERT INTO [t30022] VALUES (4,'إلى هنا عن أحمد بن أبي الحواري','55','0','0');
