705751cf7b128ac10ba8c77962494785a90594c4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:34 +0700

    Upload SQL chunk: t2512_chunk_0000.sql

---
+++ sql_chunks/t2512_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t2512
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t2512" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2512
+INSERT INTO [t2512] VALUES (1,'الباب الأول في: الفخر والحماسة','2','0','0');
+INSERT INTO [t2512] VALUES (2,'الباب الثاني في: المراثي','68','0','0');
+INSERT INTO [t2512] VALUES (3,'الباب الثالث في: النسيب','164','0','0');
+INSERT INTO [t2512] VALUES (4,'الباب الرابع في: المديح','233','0','0');
+INSERT INTO [t2512] VALUES (5,'الباب الخامس في: الهجاء','289','0','0');
+INSERT INTO [t2512] VALUES (6,'الباب السادس في: الأدب','298','0','0');
+INSERT INTO [t2512] VALUES (7,'الباب السابع في: العتاب','327','0','0');
+INSERT INTO [t2512] VALUES (8,'الباب الثامن في: الأوطان','339','0','0');
+INSERT INTO [t2512] VALUES (9,'الباب التاسع في: الصفات','356','0','0');
+INSERT INTO [t2512] VALUES (10,'الباب العاشر في: الخمر','370','0','0');
