5d74bfc5c431e815a98bdfb4218f0481f760cc41
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:14:03 +0700

    Upload SQL chunk: t37566_chunk_0000.sql

---
+++ sql_chunks/t37566_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t37566
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t37566" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37566
+INSERT INTO [t37566] VALUES (1,'المقدمة.','4','0','0');
+INSERT INTO [t37566] VALUES (2,'إذا قيل إنك جميلة','5','0','0');
+INSERT INTO [t37566] VALUES (3,'سكر الهوى','7','0','0');
+INSERT INTO [t37566] VALUES (4,'دواء للنساء والرجال','9','0','0');
+INSERT INTO [t37566] VALUES (5,'الحجاب (حوار مع فتاة نصرانية)','11','0','0');
+INSERT INTO [t37566] VALUES (6,'المرأة الصالحة','15','0','0');
+INSERT INTO [t37566] VALUES (7,'آثار المعاصي على القلوب','16','0','0');
+INSERT INTO [t37566] VALUES (8,'طب الأبدان وطب الأديان','19','0','0');
+INSERT INTO [t37566] VALUES (9,'من العقوبات الشرعية المترتبة على الذنوب','21','0','0');
+INSERT INTO [t37566] VALUES (10,'من العقوبات العاجلة والمترتبة على الذنوب','23','0','0');
+INSERT INTO [t37566] VALUES (11,'الثياب الشفافة','27','0','0');
+INSERT INTO [t37566] VALUES (12,'النقاب','29','0','0');
+INSERT INTO [t37566] VALUES (13,'الحب والحبيب','30','0','0');
+INSERT INTO [t37566] VALUES (14,'الخلوة جالبة للشقوة','37','0','0');
+INSERT INTO [t37566] VALUES (15,'الأزياء وجمال الخرق المصبوغة','39','0','0');
+INSERT INTO [t37566] VALUES (16,'سفور وفجور','43','0','0');
+INSERT INTO [t37566] VALUES (17,'رد على مؤيد للسفور مبتهج به','48','0','0');
+INSERT INTO [t37566] VALUES (18,'كسوف شمس الهدى','51','0','0');
+INSERT INTO [t37566] VALUES (19,'قصيدة في الرد على الشيخ الألباني في مسألة الحجاب','53','0','0');
+INSERT INTO [t37566] VALUES (20,'الشاشة','55','0','0');
+INSERT INTO [t37566] VALUES (21,'السخرية بالدين علامة النفاق','58','0','0');
+INSERT INTO [t37566] VALUES (22,'عبرة','60','0','0');
+INSERT INTO [t37566] VALUES (23,'عجيبة','62','0','0');
+INSERT INTO [t37566] VALUES (24,'الأجل أقرب من الأمل','63','0','0');
+INSERT INTO [t37566] VALUES (25,'نور من الرحمن','64','0','0');
+INSERT INTO [t37566] VALUES (26,'السرور المخلد','66','0','0');
+INSERT INTO [t37566] VALUES (27,'المتاب قبل غلق الباب','68','0','0');
