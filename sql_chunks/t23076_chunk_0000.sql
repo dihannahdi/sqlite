0e33bca0ebda4c30d2a1d274e89e24faebf21e67
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:44 +0700

    Upload SQL chunk: t23076_chunk_0000.sql

---
+++ sql_chunks/t23076_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t23076
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t23076" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23076
+INSERT INTO [t23076] VALUES (1,'مدخل','19','0','0');
+INSERT INTO [t23076] VALUES (2,'الأول: التقارض بين اللفظين في الهيئة والشكل','20','0','0');
+INSERT INTO [t23076] VALUES (3,'النوع الثالث: التقارض بين اللفظين في المعاني','21','0','0');
+INSERT INTO [t23076] VALUES (4,'إلى - اللام','23','0','0');
+INSERT INTO [t23076] VALUES (5,'أما "اللام" فتفيد غالبا المعاني التالية','25','0','0');
+INSERT INTO [t23076] VALUES (6,'قال الزجاجي في باب اللام التي بمعنى إلى','26','0','0');
+INSERT INTO [t23076] VALUES (7,'إلى - في','27','0','0');
+INSERT INTO [t23076] VALUES (8,'الباء - في','28','0','0');
+INSERT INTO [t23076] VALUES (9,'الباء - عن','29','0','0');
+INSERT INTO [t23076] VALUES (10,'الباء - على','32','0','0');
+INSERT INTO [t23076] VALUES (11,'الباء - من','34','0','0');
+INSERT INTO [t23076] VALUES (12,'على- عن','36','0','0');
+INSERT INTO [t23076] VALUES (13,'على - اللام','38','0','0');
+INSERT INTO [t23076] VALUES (14,'على - في','40','0','0');
+INSERT INTO [t23076] VALUES (15,'على - من','42','0','0');
+INSERT INTO [t23076] VALUES (16,'عن - اللام','44','0','0');
+INSERT INTO [t23076] VALUES (17,'عن - من','45','0','0');
+INSERT INTO [t23076] VALUES (18,'في - اللام','47','0','0');
+INSERT INTO [t23076] VALUES (19,'في - من','48','0','0');
+INSERT INTO [t23076] VALUES (20,'اللام - من','49','0','0');
+INSERT INTO [t23076] VALUES (21,'أو - الواو','51','0','0');
+INSERT INTO [t23076] VALUES (22,'مع - بعد','53','0','0');
