d809bbe218e68367d54f3cd3eb0d72e72a5dca74
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:54 +0700

    Upload SQL chunk: t6068_chunk_0000.sql

---
+++ sql_chunks/t6068_chunk_0000.sql
@@ -0,0 +1,65 @@
+-- Table: t6068
+-- Chunk: 1
+-- Rows: 58
+
+CREATE TABLE "t6068" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6068
+INSERT INTO [t6068] VALUES (1,'التوحيد','1','0','0');
+INSERT INTO [t6068] VALUES (2,'الإيمان','9','0','0');
+INSERT INTO [t6068] VALUES (3,'المبتدأ','12','0','0');
+INSERT INTO [t6068] VALUES (4,'الحسن والصفات','19','0','0');
+INSERT INTO [t6068] VALUES (5,'كبر السن','25','0','0');
+INSERT INTO [t6068] VALUES (6,'العلم','40','0','0');
+INSERT INTO [t6068] VALUES (7,'السنة','62','0','0');
+INSERT INTO [t6068] VALUES (8,'من الفضائل','68','0','0');
+INSERT INTO [t6068] VALUES (9,'باب مما وضع في أبي بكر رضي الله عنه','74','0','0');
+INSERT INTO [t6068] VALUES (10,'ومما وضع في عمر الفاروق رضي الله عنه','82','0','0');
+INSERT INTO [t6068] VALUES (11,'ومما وضع في عثمان رضي الله عنه','88','0','0');
+INSERT INTO [t6068] VALUES (12,'مما وضع لعي عليه السلام','93','0','0');
+INSERT INTO [t6068] VALUES (13,'ومما وضع في فضل الحسن والحسين وأمهما عليهم السلام','125','0','0');
+INSERT INTO [t6068] VALUES (14,'فصل منه في أهل البيت','135','0','0');
+INSERT INTO [t6068] VALUES (15,'باب في فضل طائفة','139','0','0');
+INSERT INTO [t6068] VALUES (16,'فضائل الأماكن','144','0','0');
+INSERT INTO [t6068] VALUES (17,'ذكر الأيام','150','0','0');
+INSERT INTO [t6068] VALUES (18,'ومن الثاني من موضوعات ابن الجوزي الطهارة','152','0','0');
+INSERT INTO [t6068] VALUES (19,'الصلاة','156','0','0');
+INSERT INTO [t6068] VALUES (20,'الزكاة والصدقة','176','0','0');
+INSERT INTO [t6068] VALUES (21,'السخاء','184','0','0');
+INSERT INTO [t6068] VALUES (22,'الصيام','186','0','0');
+INSERT INTO [t6068] VALUES (23,'الحج','193','0','0');
+INSERT INTO [t6068] VALUES (24,'السفر','197','0','0');
+INSERT INTO [t6068] VALUES (25,'الجهاد','198','0','0');
+INSERT INTO [t6068] VALUES (26,'السودان','201','0','0');
+INSERT INTO [t6068] VALUES (27,'البيوع','205','0','0');
+INSERT INTO [t6068] VALUES (28,'النكاح','212','0','0');
+INSERT INTO [t6068] VALUES (29,'الأطعمة','222','0','0');
+INSERT INTO [t6068] VALUES (30,'الديك','230','0','0');
+INSERT INTO [t6068] VALUES (31,'الدجاج والحمام','232','0','0');
+INSERT INTO [t6068] VALUES (32,'الجراد','235','0','0');
+INSERT INTO [t6068] VALUES (33,'الطير','236','0','0');
+INSERT INTO [t6068] VALUES (34,'السمك','237','0','0');
+INSERT INTO [t6068] VALUES (35,'الأشربة','244','0','0');
+INSERT INTO [t6068] VALUES (36,'كتاب اللباس','246','0','0');
+INSERT INTO [t6068] VALUES (37,'الزينة','249','0','0');
+INSERT INTO [t6068] VALUES (38,'كتاب النوم','255','0','0');
+INSERT INTO [t6068] VALUES (39,'الأدب','256','0','0');
+INSERT INTO [t6068] VALUES (40,'كتاب البر','262','0','0');
+INSERT INTO [t6068] VALUES (41,'الهدايا','265','0','0');
+INSERT INTO [t6068] VALUES (42,'كتاب الأحكام','266','0','0');
+INSERT INTO [t6068] VALUES (43,'كتاب الإيمان والنذور','270','0','0');
+INSERT INTO [t6068] VALUES (44,'كتاب المعاصي','271','0','0');
+INSERT INTO [t6068] VALUES (45,'الحدود','280','0','0');
+INSERT INTO [t6068] VALUES (46,'كتاب الزهد','283','0','0');
+INSERT INTO [t6068] VALUES (47,'الدعاء','298','0','0');
+INSERT INTO [t6068] VALUES (48,'كتاب المواعظ','301','0','0');
+INSERT INTO [t6068] VALUES (49,'كتاب الوصايا','303','0','0');
+INSERT INTO [t6068] VALUES (50,'كتاب الملاحم والفتن','306','0','0');
+INSERT INTO [t6068] VALUES (51,'كتاب المرض','312','0','0');
+INSERT INTO [t6068] VALUES (52,'كتاب الجنة','319','0','0');
+INSERT INTO [t6068] VALUES (53,'المواريث','325','0','0');
+INSERT INTO [t6068] VALUES (54,'القبور','326','0','0');
+INSERT INTO [t6068] VALUES (55,'كتاب البعث','331','0','0');
+INSERT INTO [t6068] VALUES (56,'الجنة','334','0','0');
+INSERT INTO [t6068] VALUES (57,'كتاب النار','338','0','0');
+INSERT INTO [t6068] VALUES (58,'والمستبشع من الموضوع على الصحابة','341','0','0');
