b4084b58022dacce269342dcf3be182d2c95968e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:53:49 +0700

    Upload SQL chunk: t9450_chunk_0000.sql

---
+++ sql_chunks/t9450_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t9450
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t9450" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9450
+INSERT INTO [t9450] VALUES (1,'أحب أن آكل من عمل يدي "','1','0','0');
+INSERT INTO [t9450] VALUES (2,'ازهد في الدنيا لا تنازع أهلها، وكن فيها كالنحلة، إن وقعت على عود لم تكسره، وإن أكلت أكلت','2','0','0');
+INSERT INTO [t9450] VALUES (3,'فليت، وكان معتوها، وكانت له خالة عجوز كبيرة لها عقل ودين، قال: فقالت له: فليت! أيسرك أنك','2','0','0');
+INSERT INTO [t9450] VALUES (4,'أي شيء أولى بك؟ قال: العمل الصالح "','3','0','0');
