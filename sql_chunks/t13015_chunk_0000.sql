cee2ef7d9a7c4133b302850574f67d00976cf2f2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:09:07 +0700

    Upload SQL chunk: t13015_chunk_0000.sql

---
+++ sql_chunks/t13015_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t13015
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t13015" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13015
+INSERT INTO [t13015] VALUES (1,'ما روى أبو أمامة','1','0','0');
+INSERT INTO [t13015] VALUES (2,'ما روى أبو سعيد الخدري','4','0','0');
+INSERT INTO [t13015] VALUES (3,'ما روى عبد الله بن عمر','7','0','0');
+INSERT INTO [t13015] VALUES (4,'ما رواه عقبة بن عامر','12','0','0');
+INSERT INTO [t13015] VALUES (5,'ما روى عتبة بن عبد السلمي','14','0','0');
+INSERT INTO [t13015] VALUES (6,'ما روى حذيفة بن أسيد','17','0','0');
+INSERT INTO [t13015] VALUES (7,'ما روى زيد بن أرقم','19','0','0');
+INSERT INTO [t13015] VALUES (8,'ما روى ثوبان','21','0','0');
+INSERT INTO [t13015] VALUES (9,'ما روى حذيفة بن اليمان','24','0','0');
+INSERT INTO [t13015] VALUES (10,'ما روى جندب بن عبد الله','26','0','0');
+INSERT INTO [t13015] VALUES (11,'ما روى جابر بن سمرة','28','0','0');
+INSERT INTO [t13015] VALUES (12,'ما روى أبو ذر','30','0','0');
+INSERT INTO [t13015] VALUES (13,'ما روى الصنابحي','32','0','0');
+INSERT INTO [t13015] VALUES (14,'ما روى أنس بن مالك في الكوثر','34','0','0');
+INSERT INTO [t13015] VALUES (15,'ما روى عبد الله بن عمر','39','0','0');
+INSERT INTO [t13015] VALUES (16,'ما روت أم سلمة','42','0','0');
+INSERT INTO [t13015] VALUES (17,'ما روى أسامة بن زيد وحمزة بن عبد المطلب','44','0','0');
+INSERT INTO [t13015] VALUES (18,'ما رواه عبد الله بن عمرو','46','0','0');
