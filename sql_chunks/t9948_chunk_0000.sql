03a9e4fc52c068bfdc98042e8fbff2f7c50d7a2f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:13 +0700

    Upload SQL chunk: t9948_chunk_0000.sql

---
+++ sql_chunks/t9948_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t9948
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t9948" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9948
+INSERT INTO [t9948] VALUES (1,'من مقدمة المحقق','2','0','0');
+INSERT INTO [t9948] VALUES (2,'مدحه وثناء العلماء عليه.','2','1','0');
+INSERT INTO [t9948] VALUES (3,'كلام الدارقطني فيه.','3','1','0');
+INSERT INTO [t9948] VALUES (4,'المصنف في عداد من يحتج به، ويقبل حديثه','6','1','0');
+INSERT INTO [t9948] VALUES (5,'الجزء الأول','8','0','0');
+INSERT INTO [t9948] VALUES (6,'الجزء الثاني','173','0','0');
+INSERT INTO [t9948] VALUES (7,'الجزء الثالث','345','0','0');
+INSERT INTO [t9948] VALUES (8,'الجزء الرابع','506','0','0');
+INSERT INTO [t9948] VALUES (9,'الجزء الخامس','638','0','0');
+INSERT INTO [t9948] VALUES (10,'الجزء السادس','782','0','0');
+INSERT INTO [t9948] VALUES (11,'الجزء السابع','943','0','0');
+INSERT INTO [t9948] VALUES (12,'الجزء الثامن','1075','0','0');
+INSERT INTO [t9948] VALUES (13,'الجزء التاسع','1222','0','0');
+INSERT INTO [t9948] VALUES (14,'الجزء العاشر','1398','0','0');
+INSERT INTO [t9948] VALUES (15,'الجزء الحادي عشر','1541','0','0');
+INSERT INTO [t9948] VALUES (16,'الجزء الثاني عشر','1700','0','0');
+INSERT INTO [t9948] VALUES (17,'الجزء الثالث عشر','1910','0','0');
+INSERT INTO [t9948] VALUES (18,'الجزء الرابع عشر','2073','0','0');
+INSERT INTO [t9948] VALUES (19,'الجزء الخامس عشر','2221','0','0');
+INSERT INTO [t9948] VALUES (20,'الجزء السادس عشر','2354','0','0');
+INSERT INTO [t9948] VALUES (21,'الجزء السابع عشر','2456','0','0');
+INSERT INTO [t9948] VALUES (22,'الجزء الثامن عشر','2614','0','0');
+INSERT INTO [t9948] VALUES (23,'الجزء التاسع عشر','2792','0','0');
+INSERT INTO [t9948] VALUES (24,'الجزء العشرون','2956','0','0');
+INSERT INTO [t9948] VALUES (25,'الجزء الحادي والعشرون','3031','0','0');
+INSERT INTO [t9948] VALUES (26,'الجزء الثاني والعشرون','3176','0','0');
+INSERT INTO [t9948] VALUES (27,'الجزء الثالث والعشرون','3307','0','0');
+INSERT INTO [t9948] VALUES (28,'الجزء الرابع والعشرون','3374','0','0');
+INSERT INTO [t9948] VALUES (29,'الجزء الخامس والعشرون','3530','0','0');
+INSERT INTO [t9948] VALUES (30,'الجزء السادس والعشرون','3713','0','0');
+INSERT INTO [t9948] VALUES (31,'استدراك فيه','3855','0','0');
