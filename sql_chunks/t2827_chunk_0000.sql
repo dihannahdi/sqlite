813cc7147f2d044be88580b9655f3327f3d196bf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:37 +0700

    Upload SQL chunk: t2827_chunk_0000.sql

---
+++ sql_chunks/t2827_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t2827
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t2827" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2827
+INSERT INTO [t2827] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t2827] VALUES (2,'الأسماء والصفات','114','0','0');
+INSERT INTO [t2827] VALUES (3,'الإتباع والتسليم','413','0','0');
+INSERT INTO [t2827] VALUES (4,'كلام الله','551','0','0');
+INSERT INTO [t2827] VALUES (5,'الحوض','774','0','0');
+INSERT INTO [t2827] VALUES (6,'علم الكلام','807','0','0');
+INSERT INTO [t2827] VALUES (7,'التوحيد','954','0','0');
+INSERT INTO [t2827] VALUES (8,'القدر','1234','0','0');
+INSERT INTO [t2827] VALUES (9,'النبوة','1565','0','0');
+INSERT INTO [t2827] VALUES (10,'الرؤية','1720','0','0');
+INSERT INTO [t2827] VALUES (11,'الإسراء والمعراج','1799','0','0');
+INSERT INTO [t2827] VALUES (12,'الشفاعة','1883','0','0');
