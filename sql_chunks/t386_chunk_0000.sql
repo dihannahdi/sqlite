42f86e249530d4e8a838914b4ad8b0d64d7cc522
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:04 +0700

    Upload SQL chunk: t386_chunk_0000.sql

---
+++ sql_chunks/t386_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t386
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t386" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t386
+INSERT INTO [t386] VALUES (1,'تفسير الآية (٢٨) من سورة البقرة','2','0','0');
+INSERT INTO [t386] VALUES (2,'تفسير الآيات (٩ - ١٢) من سورة فصلت','2','0','0');
+INSERT INTO [t386] VALUES (3,'تفسير الآيات (٢٧ - ٣٢) من سورة النازعات','6','0','0');
+INSERT INTO [t386] VALUES (4,'الرد على القول بأن خلق السماء مقدم على خلق الأرض','7','0','0');
+INSERT INTO [t386] VALUES (5,'المراد بالدحو في آية النازعات','8','0','0');
+INSERT INTO [t386] VALUES (6,'الرد على القول بأن {أخرج منها ماءها} حال بإضمار «قد»','9','0','0');
