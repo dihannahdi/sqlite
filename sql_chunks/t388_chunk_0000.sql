db61fff7e74a287a229dbe5897c66e2538cea1f5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:10 +0700

    Upload SQL chunk: t388_chunk_0000.sql

---
+++ sql_chunks/t388_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t388
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t388" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t388
+INSERT INTO [t388] VALUES (1,'السؤال الأول: ألم يكمل الفخر الرازي تفسيره؟','5','0','0');
+INSERT INTO [t388] VALUES (2,'السؤال الثاني: إن لم يكمله فمن أكمله؟','7','0','0');
+INSERT INTO [t388] VALUES (3,'السؤال الثالث: إذا لم يكمل الفخر تفسيره، فهذا التفسير المتداول الكامل مشتمل على الأصل والتكملة، فهل يعرف أحدهما من الآخر؟','9','0','0');
+INSERT INTO [t388] VALUES (4,'ملخص الجواب عن السؤال الثالث','29','3','0');
