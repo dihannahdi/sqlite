cd0989278c1c25352f88427310e74916f11b205d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:17:22 +0700

    Upload SQL chunk: t14252_chunk_0000.sql

---
+++ sql_chunks/t14252_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t14252
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t14252" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14252
+INSERT INTO [t14252] VALUES (1,'تعريف أصول الفقه','8','0','0');
+INSERT INTO [t14252] VALUES (2,'تعريف الفقه','11','0','0');
+INSERT INTO [t14252] VALUES (3,'تعريف الحكم','14','0','0');
+INSERT INTO [t14252] VALUES (4,'تعريف الحسن والقبح','17','0','0');
+INSERT INTO [t14252] VALUES (5,'الأحكام الشرعية','23','0','0');
+INSERT INTO [t14252] VALUES (6,'تقسيم الحكم إلى رخصة وعزيمة','42','0','0');
+INSERT INTO [t14252] VALUES (7,'الكتاب الأول في (الكتاب) ومباحث الأقوال','90','0','0');
+INSERT INTO [t14252] VALUES (8,'المنطوق والمفهوم','99','7','0');
+INSERT INTO [t14252] VALUES (9,'التخصيص','285','7','0');
+INSERT INTO [t14252] VALUES (10,'المطلق والمقيد','326','7','0');
+INSERT INTO [t14252] VALUES (11,'الظاهر والمؤول','333','7','0');
+INSERT INTO [t14252] VALUES (12,'البيان والمبين','345','7','0');
+INSERT INTO [t14252] VALUES (13,'النسخ','350','7','0');
+INSERT INTO [t14252] VALUES (14,'خاتمة','366','7','0');
+INSERT INTO [t14252] VALUES (15,'الكتاب الثاني في السنة','369','0','0');
+INSERT INTO [t14252] VALUES (16,'الكلام في الأخبار','369','15','0');
+INSERT INTO [t14252] VALUES (17,'الكتاب الثالث في الإجماع','470','0','0');
+INSERT INTO [t14252] VALUES (18,'الكتاب الرابع في القياس','499','0','0');
+INSERT INTO [t14252] VALUES (19,'الكتاب الخامس الاستدلال','621','0','0');
+INSERT INTO [t14252] VALUES (20,'الاستحسان','633','19','0');
+INSERT INTO [t14252] VALUES (21,'الكتاب السادس في التعادل والتراجيح','646','0','0');
+INSERT INTO [t14252] VALUES (22,'الكتاب السابع في الاجتهاد','678','0','0');
