e5dca010eea3149f6de9f33f688add0f069a3125
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:52 +0700

    Upload SQL chunk: t667_chunk_0000.sql

---
+++ sql_chunks/t667_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t667
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t667" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t667
+INSERT INTO [t667] VALUES (1,'«مقدمة التحقيق» :','2','0','0');
+INSERT INTO [t667] VALUES (2,'ترجمة المؤلف','7','0','0');
+INSERT INTO [t667] VALUES (3,'«النص المحقق» :','16','0','0');
+INSERT INTO [t667] VALUES (4,'أما بعد:','17','0','0');
