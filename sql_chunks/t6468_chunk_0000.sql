73dac84b83da5c95b3fecd2001882c6129635261
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:55 +0700

    Upload SQL chunk: t6468_chunk_0000.sql

---
+++ sql_chunks/t6468_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6468
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6468" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6468
+INSERT INTO [t6468] VALUES (1,'تفسير أسماء الله تعالى التسعة والتسعين','1','0','0');
+INSERT INTO [t6468] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
