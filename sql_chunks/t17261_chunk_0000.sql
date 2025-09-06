37d6fb13a4d49f2397624535fd7b0da089c326d1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:29:12 +0700

    Upload SQL chunk: t17261_chunk_0000.sql

---
+++ sql_chunks/t17261_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t17261
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t17261" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17261
+INSERT INTO [t17261] VALUES (1,'مقدمة المحقق','2','0','0');
+INSERT INTO [t17261] VALUES (2,'ترجمة الإمام شرف الدين بن البارزي','2','1','0');
+INSERT INTO [t17261] VALUES (3,'موضوع الجزء','4','1','0');
+INSERT INTO [t17261] VALUES (4,'وصف النسخ وعملي فيها','9','1','0');
+INSERT INTO [t17261] VALUES (5,'أول المسائل محققة','11','0','0');
+INSERT INTO [t17261] VALUES (6,'الخاتمة','15','0','0');
