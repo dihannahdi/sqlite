048233861eea0bbf18a7494908f00f61aed48483
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:47 +0700

    Upload SQL chunk: t11236_chunk_0000.sql

---
+++ sql_chunks/t11236_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t11236
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t11236" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11236
+INSERT INTO [t11236] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11236] VALUES (2,'ذكر أهل الكوفة','76','0','0');
+INSERT INTO [t11236] VALUES (3,'ذكر أهل البصرة','201','0','0');
+INSERT INTO [t11236] VALUES (4,'مصادر ومراجع','354','0','0');
