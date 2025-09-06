527824fe8c55dfd5cb8b9366adf6d031e0b7f825
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:34 +0700

    Upload SQL chunk: t37349_chunk_0000.sql

---
+++ sql_chunks/t37349_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t37349
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t37349" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37349
+INSERT INTO [t37349] VALUES (1,'موقف دانتي من الرسول - صلى الله عليه وسلم -:','9','0','0');
+INSERT INTO [t37349] VALUES (2,'نسب حسنات الإسلام إلى اليهودية والنصرانية:','10','0','0');
+INSERT INTO [t37349] VALUES (3,'القصص الديني:','10','2','0');
+INSERT INTO [t37349] VALUES (4,'ترجمات معاني القرآن:','11','2','0');
+INSERT INTO [t37349] VALUES (5,'المستشرقون والسنة:','17','0','0');
+INSERT INTO [t37349] VALUES (6,'نماذج من نقد المستشرقين للأحاديث:','21','0','0');
+INSERT INTO [t37349] VALUES (7,'الخاتمة:','23','0','0');
