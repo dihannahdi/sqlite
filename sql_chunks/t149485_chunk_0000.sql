3790b7827e49b9c63c375d32c72209fe1a6dce6c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:50 +0700

    Upload SQL chunk: t149485_chunk_0000.sql

---
+++ sql_chunks/t149485_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t149485
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t149485" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t149485
+INSERT INTO [t149485] VALUES (1,'الشعر','1','0','0');
+INSERT INTO [t149485] VALUES (2,'ما ينسب لسويد ولغيره من الشعراء','27','0','0');
+INSERT INTO [t149485] VALUES (3,'مستدرك','32','0','0');
