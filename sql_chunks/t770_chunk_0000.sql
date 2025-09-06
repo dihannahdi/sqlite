b8a256316094e0a697459b3a09864e2927775173
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:42:11 +0700

    Upload SQL chunk: t770_chunk_0000.sql

---
+++ sql_chunks/t770_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t770
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t770" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t770
+INSERT INTO [t770] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t770] VALUES (2,'تسمية خيل بني هاشم','2','0','0');
+INSERT INTO [t770] VALUES (3,'خيل قريش','3','0','0');
+INSERT INTO [t770] VALUES (4,'خيل الأنصار','6','0','0');
+INSERT INTO [t770] VALUES (5,'خيل بني أسد','7','0','0');
+INSERT INTO [t770] VALUES (6,'خيل بني ضبة','9','0','0');
+INSERT INTO [t770] VALUES (7,'خيل بني سعد بن زيد مناة بن تميم','14','0','0');
+INSERT INTO [t770] VALUES (8,'خيل عمرو بن تميم','15','0','0');
+INSERT INTO [t770] VALUES (9,'خليل بني حنظلة','16','0','0');
+INSERT INTO [t770] VALUES (10,'خيل باهلة','20','0','0');
+INSERT INTO [t770] VALUES (11,'خيل غني بن أعصر','21','0','0');
+INSERT INTO [t770] VALUES (12,'خيل غطفان بن سعد','22','0','0');
+INSERT INTO [t770] VALUES (13,'خيل بني سليم','26','0','0');
+INSERT INTO [t770] VALUES (14,'خيل هوازن','29','0','0');
+INSERT INTO [t770] VALUES (15,'خيل ربيعة بن نزار','37','0','0');
+INSERT INTO [t770] VALUES (16,'خيل بنو ضبيعة بن نزار','37','0','0');
+INSERT INTO [t770] VALUES (17,'خيل عنزة بن أسد','39','0','0');
+INSERT INTO [t770] VALUES (18,'خيل عبد القيس بن أفصى','39','0','0');
+INSERT INTO [t770] VALUES (19,'خيل النمر بن قاسط','41','0','0');
+INSERT INTO [t770] VALUES (20,'خيل بني وائل','42','0','0');
+INSERT INTO [t770] VALUES (21,'من بني شيبان','45','0','0');
+INSERT INTO [t770] VALUES (22,'من بني قيس بن ثعلبة','45','0','0');
+INSERT INTO [t770] VALUES (23,'خيل بني ذهل بن ثعلبة','45','0','0');
+INSERT INTO [t770] VALUES (24,'خيل عجل بن لجيم','53','0','0');
+INSERT INTO [t770] VALUES (25,'خيل حنيفة بن لجيم','54','0','0');
+INSERT INTO [t770] VALUES (26,'خيل إياد بن نزار','54','0','0');
+INSERT INTO [t770] VALUES (27,'خيل اليمن','55','0','0');
+INSERT INTO [t770] VALUES (28,'خيل همدان','56','0','0');
