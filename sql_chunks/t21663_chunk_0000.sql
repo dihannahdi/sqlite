b180dc0fc4e0b3abe5d892c82fa52c565eec8ce1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:04 +0700

    Upload SQL chunk: t21663_chunk_0000.sql

---
+++ sql_chunks/t21663_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21663
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21663" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21663
+INSERT INTO [t21663] VALUES (1,'مجلس من أمالي ابن منده - مخطوط','3','0','0');
