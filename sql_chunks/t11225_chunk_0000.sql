bae8956ab2ee3b4f8a6a2fb935f36100ba4b9807
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:38 +0700

    Upload SQL chunk: t11225_chunk_0000.sql

---
+++ sql_chunks/t11225_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t11225
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t11225" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11225
+INSERT INTO [t11225] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t11225] VALUES (2,'أسباب سجود السهو ثلاثة.','2','0','0');
+INSERT INTO [t11225] VALUES (3,'سجود السهو على المأموم.','10','0','0');
+INSERT INTO [t11225] VALUES (4,'الخلاصة','12','0','0');
