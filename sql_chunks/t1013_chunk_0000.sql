c882439578f920865ac37d95cbf3719ab4720e2d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:00 +0700

    Upload SQL chunk: t1013_chunk_0000.sql

---
+++ sql_chunks/t1013_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t1013
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t1013" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1013
+INSERT INTO [t1013] VALUES (1,'مقدمة الطبعة الثانية','1','0','0');
+INSERT INTO [t1013] VALUES (2,'مقدمة الطبعة الأولى','3','0','0');
+INSERT INTO [t1013] VALUES (3,'شعر الخوارج هلال ناجي','5','0','0');
+INSERT INTO [t1013] VALUES (4,'نظرة في شعر الخوارج','6','0','0');
+INSERT INTO [t1013] VALUES (5,'الخوارج أيام علي','26','0','0');
+INSERT INTO [t1013] VALUES (6,'الخوارج أيام معاوية ويزيد','36','0','0');
+INSERT INTO [t1013] VALUES (7,'الخوارج فيما بين موت يزيد وولاية عبد الملك','62','0','0');
+INSERT INTO [t1013] VALUES (8,'ملحق','238','0','0');
+INSERT INTO [t1013] VALUES (9,'الجمعية الإسلامية للخدمات الثقافية','244','0','0');
+INSERT INTO [t1013] VALUES (10,'قصائد نادرة.','246','0','0');
+INSERT INTO [t1013] VALUES (11,'الاستدراكات','252','0','0');
+INSERT INTO [t1013] VALUES (12,'التيار القومي في الشعر العراقي الحديث','254','0','0');
