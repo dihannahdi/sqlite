2bbb9e4912cdceaad931664b740f55a9ea67bdea
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:31 +0700

    Upload SQL chunk: t21804_chunk_0000.sql

---
+++ sql_chunks/t21804_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t21804
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t21804" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21804
+INSERT INTO [t21804] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t21804] VALUES (2,'موضوع الرسالة','2','0','0');
+INSERT INTO [t21804] VALUES (3,'خصائص الداعية','5','0','0');
+INSERT INTO [t21804] VALUES (4,'تخلق الداعية بما يدعو إلية','9','0','0');
+INSERT INTO [t21804] VALUES (5,'أن يكون بصيرا بما يدعو اليه','10','0','0');
+INSERT INTO [t21804] VALUES (6,'أن يكون صبورا','12','0','0');
+INSERT INTO [t21804] VALUES (7,'لا ينبغي أن يكون داعية لشخص','13','0','0');
+INSERT INTO [t21804] VALUES (8,'من آداب الدعاة','14','0','0');
+INSERT INTO [t21804] VALUES (9,'أسئلة','20','0','0');
