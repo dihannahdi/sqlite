927c68a7868829370c4dea490b8d2387baf910ba
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:37 +0700

    Upload SQL chunk: t10065_chunk_0000.sql

---
+++ sql_chunks/t10065_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10065
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10065" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10065
+INSERT INTO [t10065] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10065] VALUES (2,'تصحيح المفاهيم في جوانب العقيدة','4','0','0');
