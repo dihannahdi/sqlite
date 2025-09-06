be6561296d09217a2a4d3c364e726041939ecc76
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:56 +0700

    Upload SQL chunk: t25814_chunk_0000.sql

---
+++ sql_chunks/t25814_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t25814
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t25814" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t25814
+INSERT INTO [t25814] VALUES (1,'شرح منسك شيخ الإسلام ابن تيمية (١)','2','0','0');
+INSERT INTO [t25814] VALUES (2,'شرح منسك شيخ الإسلام ابن تيمية (٢)','22','0','0');
+INSERT INTO [t25814] VALUES (3,'شرح منسك شيخ الإسلام ابن تيمية (٣)','48','0','0');
+INSERT INTO [t25814] VALUES (4,'شرح منسك شيخ الإسلام ابن تيمية (٤)','71','0','0');
+INSERT INTO [t25814] VALUES (5,'شرح منسك شيخ الإسلام ابن تيمية (٥)','96','0','0');
+INSERT INTO [t25814] VALUES (6,'شرح منسك شيخ الإسلام ابن تيمية (٦)','119','0','0');
