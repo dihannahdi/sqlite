340cf6a23b9daf87af703bdafd4b1c0f4f1edb2e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:50 +0700

    Upload SQL chunk: t5834_chunk_0000.sql

---
+++ sql_chunks/t5834_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t5834
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t5834" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5834
+INSERT INTO [t5834] VALUES (1,'(باب الألف)','1','0','0');
+INSERT INTO [t5834] VALUES (2,'(باب الباء)','90','0','0');
+INSERT INTO [t5834] VALUES (3,'(باب الشين)','261','0','0');
+INSERT INTO [t5834] VALUES (4,'(باب الصاد)','266','0','0');
+INSERT INTO [t5834] VALUES (5,'(باب الضاد)','280','0','0');
+INSERT INTO [t5834] VALUES (6,'(باب الطاء)','282','0','0');
+INSERT INTO [t5834] VALUES (7,'(باب الظاء)','286','0','0');
+INSERT INTO [t5834] VALUES (8,'(باب العين)','287','0','0');
+INSERT INTO [t5834] VALUES (9,'(باب الغين)','484','0','0');
+INSERT INTO [t5834] VALUES (10,'(باب الفاء)','487','0','0');
+INSERT INTO [t5834] VALUES (11,'(باب القاف)','495','0','0');
+INSERT INTO [t5834] VALUES (12,'(باب الكاف)','505','0','0');
+INSERT INTO [t5834] VALUES (13,'(باب اللام)','515','0','0');
+INSERT INTO [t5834] VALUES (14,'(باب الميم)','518','0','0');
+INSERT INTO [t5834] VALUES (15,'(باب النون)','642','0','0');
+INSERT INTO [t5834] VALUES (16,'(باب الواو)','669','0','0');
+INSERT INTO [t5834] VALUES (17,'(باب الهاء)','680','0','0');
+INSERT INTO [t5834] VALUES (18,'(باب الياء)','693','0','0');
+INSERT INTO [t5834] VALUES (19,'(باب الكنى)','740','0','0');
