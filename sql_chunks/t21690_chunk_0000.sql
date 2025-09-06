842ec88e4b0970e2d52522452a0b5e305d193762
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:17 +0700

    Upload SQL chunk: t21690_chunk_0000.sql

---
+++ sql_chunks/t21690_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21690
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21690" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21690
+INSERT INTO [t21690] VALUES (1,'من حديث ابن السواق - مخطوط','1','0','0');
