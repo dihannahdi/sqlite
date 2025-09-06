8256f7630a70419ddd3dc4ed67672c6841f7dc9d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:53:13 +0700

    Upload SQL chunk: t9381_chunk_0000.sql

---
+++ sql_chunks/t9381_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9381
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9381" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9381
+INSERT INTO [t9381] VALUES (1,'كتاب التوحيد لله عز وجل','1','0','0');
+INSERT INTO [t9381] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
