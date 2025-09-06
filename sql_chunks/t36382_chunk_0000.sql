51aec4cc0a6e717043f56b1985c2d61a6757dfaf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:39 +0700

    Upload SQL chunk: t36382_chunk_0000.sql

---
+++ sql_chunks/t36382_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t36382
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t36382" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36382
+INSERT INTO [t36382] VALUES (1,'أين قلبك من الإيمان الصادق؟ !','2','0','0');
+INSERT INTO [t36382] VALUES (2,'وهل تفقدت الخير في قلبك؟ !','3','0','0');
+INSERT INTO [t36382] VALUES (3,'وهل قلبك قلب شاكر؟ !','4','0','0');
+INSERT INTO [t36382] VALUES (4,'وأين أنت من خشوع القلب؟ !','4','0','0');
+INSERT INTO [t36382] VALUES (5,'وأيضا: أين أنت من لين القلب ورقته؟ !','5','0','0');
+INSERT INTO [t36382] VALUES (6,'وأين قلبك من الوجل والخوف من الله تعالى؟ !','5','0','0');
+INSERT INTO [t36382] VALUES (7,'وهل أنت سليم القلب؟ !','7','0','0');
+INSERT INTO [t36382] VALUES (8,'واحذر المعاصي','9','0','0');
