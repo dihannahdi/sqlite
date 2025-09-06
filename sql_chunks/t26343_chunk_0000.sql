99116408d5d344ff1a886760bf30e80a0b98a0cf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:56 +0700

    Upload SQL chunk: t26343_chunk_0000.sql

---
+++ sql_chunks/t26343_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26343
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26343" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26343
+INSERT INTO [t26343] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26343] VALUES (2,'الجزء فيه من أخبار ابن أبي ذئب رحمه الله','2','0','0');
