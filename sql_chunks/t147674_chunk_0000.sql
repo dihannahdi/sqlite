767ee028bec6a3097579286e03e36fd0d11ffa56
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:52 +0700

    Upload SQL chunk: t147674_chunk_0000.sql

---
+++ sql_chunks/t147674_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t147674
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t147674" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t147674
+INSERT INTO [t147674] VALUES (1,'مقدمة التحقيق','2','0','0');
+INSERT INTO [t147674] VALUES (2,'عن الكتاب','2','1','0');
+INSERT INTO [t147674] VALUES (3,'مميزات "الاختيارات" للبرهان','2','2','0');
+INSERT INTO [t147674] VALUES (4,'الطبعات السابقة للجزء','4','2','0');
+INSERT INTO [t147674] VALUES (5,'وصف النسخة الخطية','5','2','0');
+INSERT INTO [t147674] VALUES (6,'خطة التحقيق','7','1','0');
+INSERT INTO [t147674] VALUES (7,'نبذة عن المؤلف','8','1','0');
+INSERT INTO [t147674] VALUES (8,'اسمه ونسبه','8','7','0');
+INSERT INTO [t147674] VALUES (9,'مولده','8','7','0');
+INSERT INTO [t147674] VALUES (10,'شيوخه','9','7','0');
+INSERT INTO [t147674] VALUES (11,'ثناء العلماء عليه','9','7','0');
+INSERT INTO [t147674] VALUES (12,'أعماله','10','7','0');
+INSERT INTO [t147674] VALUES (13,'آثاره','10','7','0');
+INSERT INTO [t147674] VALUES (14,'من نوادره','11','7','0');
+INSERT INTO [t147674] VALUES (15,'وفاته','11','7','0');
+INSERT INTO [t147674] VALUES (16,'مقدمة المصنف','15','0','0');
+INSERT INTO [t147674] VALUES (17,'القسم الأول: ما يندر القائل به ويخفى على كثير من الناس','17','0','0');
+INSERT INTO [t147674] VALUES (18,'القسم الثاني: ما هو خارج عن مذاهب الأئمة الأربعة','25','0','0');
+INSERT INTO [t147674] VALUES (19,'القسم الثالث: ما هو خارج عن مذهب الإمام أحمد','29','0','0');
+INSERT INTO [t147674] VALUES (20,'القسم الرابع: ما أفتى به واختاره مما هو خلاف المشهور في مذهب أحمد','34','0','0');
