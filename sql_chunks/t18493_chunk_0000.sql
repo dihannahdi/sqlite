333117a55cd2aa2efb5b184d5b2c082f3f631ffc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:10 +0700

    Upload SQL chunk: t18493_chunk_0000.sql

---
+++ sql_chunks/t18493_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t18493
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t18493" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t18493
+INSERT INTO [t18493] VALUES (1,'[مقدمة]','2','0','0');
+INSERT INTO [t18493] VALUES (2,'١ - الباب الأول في دليل مشروعيته','4','0','0');
+INSERT INTO [t18493] VALUES (3,'٢ - الباب الثاني في محال فرصة وندبة وحرمته','5','0','0');
+INSERT INTO [t18493] VALUES (4,'٣ - الباب الثالث في المغير وشروطه','8','0','0');
+INSERT INTO [t18493] VALUES (5,'٤ - الباب الرابع في كيفية التغيير ووجه تناوله','11','0','0');
+INSERT INTO [t18493] VALUES (6,'٥ - الباب الخامس في مراتب التغيير','12','0','0');
+INSERT INTO [t18493] VALUES (7,'فصل','14','6','0');
+INSERT INTO [t18493] VALUES (8,'٦ - الباب السادس في معرفة وجوه الكشف عنه','21','0','0');
