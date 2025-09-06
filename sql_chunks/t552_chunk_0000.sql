b08cd7f6489ed0ca0fde7dd8e1b4324a93357bee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:23:28 +0700

    Upload SQL chunk: t552_chunk_0000.sql

---
+++ sql_chunks/t552_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t552
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t552" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t552
+INSERT INTO [t552] VALUES (1,'مقدمة المحقق.','2','0','0');
+INSERT INTO [t552] VALUES (2,'ترجمة المؤلف.','4','0','0');
+INSERT INTO [t552] VALUES (3,'مقدمة المؤلف.','6','0','0');
+INSERT INTO [t552] VALUES (4,'[الكتاب]','8','0','0');
+INSERT INTO [t552] VALUES (5,'الفصل الأول: حياة عثمان (١) بن عفان رضي الله عنه.','9','4','0');
+INSERT INTO [t552] VALUES (6,'الفصل الثاني: خلافة عثمان بن عفان (١) رضي الله عنه (٢٤ هـ/ ٦٤٤ م) .','44','4','0');
+INSERT INTO [t552] VALUES (7,'الفصل الثالث: كتب عثمان بن عفان رضي الله عنه.','48','4','0');
+INSERT INTO [t552] VALUES (8,'الفصل الرابع: وفاة كبار الصحابة.','78','4','0');
+INSERT INTO [t552] VALUES (9,'الفصل الخامس: الفتنة.','89','4','0');
+INSERT INTO [t552] VALUES (10,'الفصل السادس: قتل عثمان "- ونبذ من أخباره','125','4','0');
+INSERT INTO [t552] VALUES (11,'الفصل السابع: آراء ومواقف في مقتل عثمان رضي الله عنه.','140','4','0');
