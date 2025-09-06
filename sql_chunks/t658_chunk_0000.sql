71afab171bf8cf04d183a6fe1094d99bc3d7912e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:50 +0700

    Upload SQL chunk: t658_chunk_0000.sql

---
+++ sql_chunks/t658_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t658
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t658" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t658
+INSERT INTO [t658] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t658] VALUES (2,'فصل','9','1','0');
+INSERT INTO [t658] VALUES (3,'فصل','13','1','0');
+INSERT INTO [t658] VALUES (4,'أساس الشعر','13','0','0');
+INSERT INTO [t658] VALUES (5,'فصل','35','4','0');
+INSERT INTO [t658] VALUES (6,'تركيب بحور الشعر','35','0','0');
+INSERT INTO [t658] VALUES (7,'فصل','40','6','0');
+INSERT INTO [t658] VALUES (8,'كيفية تقطيع الأبيات','40','0','0');
+INSERT INTO [t658] VALUES (9,'فصل','46','8','0');
+INSERT INTO [t658] VALUES (10,'أبيات شواهد','46','0','0');
+INSERT INTO [t658] VALUES (11,'أبيات الشواهد','47','10','0');
+INSERT INTO [t658] VALUES (12,'الطويل','57','10','0');
+INSERT INTO [t658] VALUES (13,'المديد','61','10','0');
+INSERT INTO [t658] VALUES (14,'البسيط','63','10','0');
+INSERT INTO [t658] VALUES (15,'الوافر','71','10','0');
+INSERT INTO [t658] VALUES (16,'الكامل','75','10','0');
+INSERT INTO [t658] VALUES (17,'الهزج','82','10','0');
+INSERT INTO [t658] VALUES (18,'الرجز','85','10','0');
+INSERT INTO [t658] VALUES (19,'الرمل','90','10','0');
+INSERT INTO [t658] VALUES (20,'السريع','94','10','0');
+INSERT INTO [t658] VALUES (21,'المنسرح','99','10','0');
+INSERT INTO [t658] VALUES (22,'الخفيف','102','10','0');
+INSERT INTO [t658] VALUES (23,'المضارع','106','10','0');
+INSERT INTO [t658] VALUES (24,'المقتضب','108','10','0');
+INSERT INTO [t658] VALUES (25,'المجتث','109','10','0');
+INSERT INTO [t658] VALUES (26,'المتقارب','111','10','0');
+INSERT INTO [t658] VALUES (27,'الركض','115','10','0');
