cc32573c97b51fdbbbc38ce6b827d28cc41e7a43
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:48 +0700

    Upload SQL chunk: t11311_chunk_0000.sql

---
+++ sql_chunks/t11311_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11311
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11311" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11311
+INSERT INTO [t11311] VALUES (1,'عناية المسلمين باللغة العربية خدمة للقرآن الكريم','1','0','0');
+INSERT INTO [t11311] VALUES (2,'الحمد لله، والصلاة والسلام على رسول الله','1','0','0');
+INSERT INTO [t11311] VALUES (3,'بيان تلازم علوم القرآن وعلوم العربية','2','0','0');
