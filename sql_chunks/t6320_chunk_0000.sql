67af3915ab542f0b94402bce5507c88a36e16eba
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:43 +0700

    Upload SQL chunk: t6320_chunk_0000.sql

---
+++ sql_chunks/t6320_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6320
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6320" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6320
+INSERT INTO [t6320] VALUES (1,'فصل في استصحاب الحال','14','0','0');
+INSERT INTO [t6320] VALUES (2,'فصل ما يتوجه على الفعل من الاعتراض','46','0','0');
+INSERT INTO [t6320] VALUES (3,'فصل في الإقرار','53','0','0');
