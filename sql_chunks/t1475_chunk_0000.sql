9bbcdc977ecc079f4e9ffb4b3b0666cccf4c5522
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:33 +0700

    Upload SQL chunk: t1475_chunk_0000.sql

---
+++ sql_chunks/t1475_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t1475
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t1475" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1475
+INSERT INTO [t1475] VALUES (1,'أصول وشواهد النظر العقلي في القرآن والفكر الإسلامي','1','0','0');
+INSERT INTO [t1475] VALUES (2,'أصول وشواهد النظر العقلي في القرآن والفكر الإسلامي','2','0','0');
