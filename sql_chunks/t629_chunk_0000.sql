e5246807de27c4cdf9862b49f12da893cc98b472
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:33 +0700

    Upload SQL chunk: t629_chunk_0000.sql

---
+++ sql_chunks/t629_chunk_0000.sql
@@ -0,0 +1,56 @@
+-- Table: t629
+-- Chunk: 1
+-- Rows: 49
+
+CREATE TABLE "t629" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t629
+INSERT INTO [t629] VALUES (1,'المظفر بن الفضل العلوي','1','0','0');
+INSERT INTO [t629] VALUES (2,'الفصل الأول','2','1','0');
+INSERT INTO [t629] VALUES (3,'في وصف الشعر وأحكامه','2','0','0');
+INSERT INTO [t629] VALUES (4,'وبيان أحواله وأقسامه','2','3','0');
+INSERT INTO [t629] VALUES (5,'باب الإشارة','8','3','0');
+INSERT INTO [t629] VALUES (6,'باب الكناية','8','3','0');
+INSERT INTO [t629] VALUES (7,'باب الموازنة','9','3','0');
+INSERT INTO [t629] VALUES (8,'باب التجنيس','10','3','0');
+INSERT INTO [t629] VALUES (9,'التجنيس المقارب','13','3','0');
+INSERT INTO [t629] VALUES (10,'تجنيس المعنى','13','3','0');
+INSERT INTO [t629] VALUES (11,'المجنس المطمع','14','3','0');
+INSERT INTO [t629] VALUES (12,'التجنيس المبذل','14','3','0');
+INSERT INTO [t629] VALUES (13,'المجنس المختلف','14','3','0');
+INSERT INTO [t629] VALUES (14,'تجنيس الخط','15','3','0');
+INSERT INTO [t629] VALUES (15,'ومنه تجنيس البعض: قال القطامي:','15','3','0');
+INSERT INTO [t629] VALUES (16,'المجنس المتمم','15','3','0');
+INSERT INTO [t629] VALUES (17,'تجنيس القوافي','16','3','0');
+INSERT INTO [t629] VALUES (18,'التنجيس المماثل','17','3','0');
+INSERT INTO [t629] VALUES (19,'باب المطابقة','17','3','0');
+INSERT INTO [t629] VALUES (20,'باب التصدير','18','3','0');
+INSERT INTO [t629] VALUES (21,'باب الالتفات','19','3','0');
+INSERT INTO [t629] VALUES (22,'باب الاستطراد','19','3','0');
+INSERT INTO [t629] VALUES (23,'باب التقسيم','20','3','0');
+INSERT INTO [t629] VALUES (24,'باب التسهيم','20','3','0');
+INSERT INTO [t629] VALUES (25,'باب الترصيع','21','3','0');
+INSERT INTO [t629] VALUES (26,'باب الترديد','22','3','0');
+INSERT INTO [t629] VALUES (27,'باب المقابلة','22','3','0');
+INSERT INTO [t629] VALUES (28,'باب الاستثناء','22','3','0');
+INSERT INTO [t629] VALUES (29,'باب الإيغال','23','3','0');
+INSERT INTO [t629] VALUES (30,'باب الاستعارة','23','3','0');
+INSERT INTO [t629] VALUES (31,'باب التشبيه','26','3','0');
+INSERT INTO [t629] VALUES (32,'باب الحشو السديد في المعنى المفيد','31','3','0');
+INSERT INTO [t629] VALUES (33,'باب المتابعة','31','3','0');
+INSERT INTO [t629] VALUES (34,'باب المخلص المليح إلى الهجاء والمديح','32','3','0');
+INSERT INTO [t629] VALUES (35,'باب التضمين','32','3','0');
+INSERT INTO [t629] VALUES (36,'باب تجاهل التعارف','33','3','0');
+INSERT INTO [t629] VALUES (37,'باب المماتنة والإنفاد والإجازة','33','3','0');
+INSERT INTO [t629] VALUES (38,'باب السرقة','35','3','0');
+INSERT INTO [t629] VALUES (39,'الفصل الثاني','41','0','0');
+INSERT INTO [t629] VALUES (40,'فيما يجوز للشاعر استعماله وما لا يجوز','41','0','0');
+INSERT INTO [t629] VALUES (41,'وما يدرك به صواب القول ويجوز','41','40','0');
+INSERT INTO [t629] VALUES (42,'الفصل الثالث','51','40','0');
+INSERT INTO [t629] VALUES (43,'في فضله ومنافعه وتأثيره في القلوب ومواقعه','51','0','0');
+INSERT INTO [t629] VALUES (44,'الفصل الرابع','64','43','0');
+INSERT INTO [t629] VALUES (45,'في كشف ما مدح به وذم بسببه','64','0','0');
+INSERT INTO [t629] VALUES (46,'وهل تعاطيه أصلح، أم رفضه أوفر وأرجح','64','45','0');
+INSERT INTO [t629] VALUES (47,'الفصل الخامس','75','45','0');
+INSERT INTO [t629] VALUES (48,'فيما يجب أن يتوخاه الشاعر ويتجنبه','75','0','0');
+INSERT INTO [t629] VALUES (49,'ويطرحه ويتطلبه','75','48','0');
