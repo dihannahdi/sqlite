4cf00394ae07ddea2e3be85935d09b3eb4ba3a52
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:22 +0700

    Upload SQL chunk: t6978_chunk_0000.sql

---
+++ sql_chunks/t6978_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t6978
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t6978" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6978
+INSERT INTO [t6978] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6978] VALUES (2,'أحدها','2','0','0');
+INSERT INTO [t6978] VALUES (3,'الثانية','2','0','0');
+INSERT INTO [t6978] VALUES (4,'الثالثة','2','0','0');
+INSERT INTO [t6978] VALUES (5,'الرابعة','4','0','0');
+INSERT INTO [t6978] VALUES (6,'الخامسة','4','0','0');
+INSERT INTO [t6978] VALUES (7,'الثاني','15','0','0');
+INSERT INTO [t6978] VALUES (8,'الثالث','15','0','0');
+INSERT INTO [t6978] VALUES (9,'المذهب الثاني','17','0','0');
+INSERT INTO [t6978] VALUES (10,'المذهب الثالث','22','0','0');
