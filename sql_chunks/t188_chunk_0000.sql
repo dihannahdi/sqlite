2031c5cfedddf78cbbabf8d19524bbb201967149
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:49 +0700

    Upload SQL chunk: t188_chunk_0000.sql

---
+++ sql_chunks/t188_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t188
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t188" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t188
+INSERT INTO [t188] VALUES (1,'إزالة الدهش والوله','1','0','0');
