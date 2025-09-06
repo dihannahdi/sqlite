4b775f49e07f4f6ed1c9e7d888b89aecba8f0042
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:49 +0700

    Upload SQL chunk: t26488_chunk_0000.sql

---
+++ sql_chunks/t26488_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t26488
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t26488" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26488
+INSERT INTO [t26488] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26488] VALUES (2,'الفرائض للثوري','2','0','0');
+INSERT INTO [t26488] VALUES (3,'١- باب الأكدرية','13','0','0');
+INSERT INTO [t26488] VALUES (4,'٢- باب في امرأة وأبوين','15','0','0');
+INSERT INTO [t26488] VALUES (5,'٣- باب ابنة وابنة ابن وأخت','19','0','0');
+INSERT INTO [t26488] VALUES (6,'٤- باب إخوة لأم وأم','20','0','0');
+INSERT INTO [t26488] VALUES (7,'٥- باب ابنة وأخت','21','0','0');
+INSERT INTO [t26488] VALUES (8,'٦- باب في زوج وأم وإخوة','23','0','0');
+INSERT INTO [t26488] VALUES (9,'٧- باب ابنة أخ وعمة','25','0','0');
+INSERT INTO [t26488] VALUES (10,'٨- باب بني عم أحدهم أخ لأم','26','0','0');
+INSERT INTO [t26488] VALUES (11,'٩- باب في الجد','27','0','0');
+INSERT INTO [t26488] VALUES (12,'١٠- باب في الجدة','30','0','0');
+INSERT INTO [t26488] VALUES (13,'١١- باب في الموالي','33','0','0');
+INSERT INTO [t26488] VALUES (14,'[باب في المدبر]','58','0','0');
+INSERT INTO [t26488] VALUES (15,'[باب في المكاتب]','65','0','0');
