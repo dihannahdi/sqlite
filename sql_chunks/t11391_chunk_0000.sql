836b1301e2e3bc8a8d11136f6e3f4ce02b9fc0ae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:55 +0700

    Upload SQL chunk: t11391_chunk_0000.sql

---
+++ sql_chunks/t11391_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t11391
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t11391" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11391
+INSERT INTO [t11391] VALUES (1,'مركز بحوث ودراسات المدينة المنورة ودوره في خدمة السيرة النبوية','1','0','0');
+INSERT INTO [t11391] VALUES (2,'هيكل المركز','5','0','0');
+INSERT INTO [t11391] VALUES (3,'المجلس العلمي للمركز','7','0','0');
+INSERT INTO [t11391] VALUES (4,'أقسام المركز','8','0','0');
+INSERT INTO [t11391] VALUES (5,'إنجازات المركز','9','0','0');
+INSERT INTO [t11391] VALUES (6,'أصدر المركز الكتب التالية','18','0','0');
+INSERT INTO [t11391] VALUES (7,'دور المركز في خدمة السيرة النبوية','20','0','0');
