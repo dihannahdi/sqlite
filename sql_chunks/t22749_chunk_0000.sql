8f0a4aa1439d24f584f74415bd744e7287c0a134
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:18 +0700

    Upload SQL chunk: t22749_chunk_0000.sql

---
+++ sql_chunks/t22749_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t22749
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t22749" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22749
+INSERT INTO [t22749] VALUES (1,'مزدك','10','0','0');
+INSERT INTO [t22749] VALUES (2,'مدخل','10','1','0');
+INSERT INTO [t22749] VALUES (3,'تاريخها','11','1','0');
+INSERT INTO [t22749] VALUES (4,'الماركسية','13','0','0');
+INSERT INTO [t22749] VALUES (5,'مدخل','13','4','0');
+INSERT INTO [t22749] VALUES (6,'حياته','14','4','0');
+INSERT INTO [t22749] VALUES (7,'نفسيته','15','4','0');
+INSERT INTO [t22749] VALUES (8,'المذهب الماركسي','18','0','0');
+INSERT INTO [t22749] VALUES (9,'روحه وصورته','18','8','0');
+INSERT INTO [t22749] VALUES (10,'المادية','19','8','0');
+INSERT INTO [t22749] VALUES (11,'الدين','21','8','0');
+INSERT INTO [t22749] VALUES (12,'الاخلاق والادب','24','8','0');
+INSERT INTO [t22749] VALUES (13,'المعارف والعلوم','25','8','0');
+INSERT INTO [t22749] VALUES (14,'المخططات الرئيسية لقيام المجتمع الشيوعي','27','0','0');
+INSERT INTO [t22749] VALUES (15,'مدخل','27','14','0');
+INSERT INTO [t22749] VALUES (16,'استيلاء الطبقة الكادحة على مقاليد الحكم','28','14','0');
+INSERT INTO [t22749] VALUES (17,'تاميم وسائل الانتاج ومصادر الثروة','30','14','0');
+INSERT INTO [t22749] VALUES (18,'القضاء على راس المال','32','14','0');
+INSERT INTO [t22749] VALUES (19,'القضاء على الطبقات','35','14','0');
+INSERT INTO [t22749] VALUES (20,'القضاء على الحكومة','38','14','0');
