3f317d0371f7fabb5e4e1557c01a5f13b02bca5b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:24:05 +0700

    Upload SQL chunk: t151140_chunk_0000.sql

---
+++ sql_chunks/t151140_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t151140
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t151140" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t151140
+INSERT INTO [t151140] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t151140] VALUES (2,'[مطلب]','13','0','0');
+INSERT INTO [t151140] VALUES (3,'[مطلب]','14','0','0');
+INSERT INTO [t151140] VALUES (4,'[العلم النافع] (*)','21','0','0');
+INSERT INTO [t151140] VALUES (5,'[علامة العلم الغير نافع]','25','0','0');
+INSERT INTO [t151140] VALUES (6,'فصل: [في مشابهة علماء السوء من المسلمين بأهل الكتاب]','32','0','0');
