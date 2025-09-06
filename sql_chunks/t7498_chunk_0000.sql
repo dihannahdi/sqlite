3a9aad43d17bbf22653b945853b01f24a693a112
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:01 +0700

    Upload SQL chunk: t7498_chunk_0000.sql

---
+++ sql_chunks/t7498_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t7498
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t7498" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7498
+INSERT INTO [t7498] VALUES (1,'آداب الخلاء وأذكاره','1','0','0');
+INSERT INTO [t7498] VALUES (2,'أذكار الوضوء','11','0','0');
+INSERT INTO [t7498] VALUES (3,'أذكار الخروج إلى الصلاة ودخول المسجد والخروج منه','20','0','0');
+INSERT INTO [t7498] VALUES (4,'ما يقوله من سمع الأذان','29','0','0');
+INSERT INTO [t7498] VALUES (5,'أذكار استفتاح الصلاة','37','0','0');
+INSERT INTO [t7498] VALUES (6,'أنواع استفتاحات الصلاة','46','0','0');
+INSERT INTO [t7498] VALUES (7,'أذكار الركوع والقيام منه والسجود والجلسة بين السجدتين','54','0','0');
+INSERT INTO [t7498] VALUES (8,'ومن أذكار الصلاة','63','0','0');
+INSERT INTO [t7498] VALUES (9,'ومن الأذكار المتعلقة بالصلاة','71','0','0');
+INSERT INTO [t7498] VALUES (10,'أذكار التشهد','79','0','0');
+INSERT INTO [t7498] VALUES (11,'الدعاء الوارد ما بين التشهد والتسليم','87','0','0');
+INSERT INTO [t7498] VALUES (12,'شرح حديث عمار في الذكر بين التشهد والتسليم','95','0','0');
+INSERT INTO [t7498] VALUES (13,'الأذكار بعد السلام','105','0','0');
+INSERT INTO [t7498] VALUES (14,'دعاء القنوت في صلاة الوتر','114','0','0');
