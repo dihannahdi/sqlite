0d85354ef136e226ec294c8a7a7b50e7483b56b5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:49 +0700

    Upload SQL chunk: t7344_chunk_0000.sql

---
+++ sql_chunks/t7344_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t7344
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t7344" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7344
+INSERT INTO [t7344] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7344] VALUES (2,'المسألة الأولى: أن من سب النبي صلى الله عليه وسلم من مسلم أو كافر فإنه يجب قتله.','4','0','0');
+INSERT INTO [t7344] VALUES (3,'المسألة الثانية: أنه يتعين قتله ولا يجوز استرقاقه ولا المن عليه ولا فداءه.','255','0','0');
+INSERT INTO [t7344] VALUES (4,'المسألة الثالثة: أنه يقتل ولا يستتاب سواء كان مسلما أو كافرا.','303','0','0');
+INSERT INTO [t7344] VALUES (5,'المسألة الرابعة: في بيان السب المذكور والفرق بينه وبين مجرد الكفر.','516','0','0');
