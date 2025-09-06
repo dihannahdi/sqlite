494332b4e2a796c86de1595226387bd2e5f6f080
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:03 +0700

    Upload SQL chunk: t8187_chunk_0000.sql

---
+++ sql_chunks/t8187_chunk_0000.sql
@@ -0,0 +1,49 @@
+-- Table: t8187
+-- Chunk: 1
+-- Rows: 42
+
+CREATE TABLE "t8187" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8187
+INSERT INTO [t8187] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8187] VALUES (2,'اللفظ والمعنى','1','1','0');
+INSERT INTO [t8187] VALUES (3,'وأقسام الكلام المدونة','3','1','0');
+INSERT INTO [t8187] VALUES (4,'القول في التجنيس','5','0','0');
+INSERT INTO [t8187] VALUES (5,'فصل في','17','4','0');
+INSERT INTO [t8187] VALUES (6,'قسمة التجنيس وتنويعه','17','4','0');
+INSERT INTO [t8187] VALUES (7,'المقصد','24','4','0');
+INSERT INTO [t8187] VALUES (8,'تعريف الاستعارة','28','0','0');
+INSERT INTO [t8187] VALUES (9,'تقسيم الاستعارة','28','8','0');
+INSERT INTO [t8187] VALUES (10,'فصل','39','8','0');
+INSERT INTO [t8187] VALUES (11,'القول في الاستعارة المفيدة','40','8','0');
+INSERT INTO [t8187] VALUES (12,'فصل','53','8','0');
+INSERT INTO [t8187] VALUES (13,'فصل','85','8','0');
+INSERT INTO [t8187] VALUES (14,'التشبيه والتمثيل','87','0','0');
+INSERT INTO [t8187] VALUES (15,'أقسام التشبيه','88','14','0');
+INSERT INTO [t8187] VALUES (16,'الفرق بين التشبيه والتمثيل','93','14','0');
+INSERT INTO [t8187] VALUES (17,'فصل','96','14','0');
+INSERT INTO [t8187] VALUES (18,'فصل','98','14','0');
+INSERT INTO [t8187] VALUES (19,'فصل','102','14','0');
+INSERT INTO [t8187] VALUES (20,'فصل في مواقع التمثيل وتأثيره','113','14','0');
+INSERT INTO [t8187] VALUES (21,'فصل آخر','137','14','0');
+INSERT INTO [t8187] VALUES (22,'فصل','153','14','0');
+INSERT INTO [t8187] VALUES (23,'فصل','178','14','0');
+INSERT INTO [t8187] VALUES (24,'فصل في التشبيه المتعدد والفرق بينه وبين المركب','190','14','0');
+INSERT INTO [t8187] VALUES (25,'فصل','202','14','0');
+INSERT INTO [t8187] VALUES (26,'الفرق بين الاستعارة والتمثيل','236','0','0');
+INSERT INTO [t8187] VALUES (27,'فصل','256','26','0');
+INSERT INTO [t8187] VALUES (28,'الأخذ والسرقة','261','0','0');
+INSERT INTO [t8187] VALUES (29,'القسم العقلي','261','28','0');
+INSERT INTO [t8187] VALUES (30,'لقسم التخييلي','265','28','0');
+INSERT INTO [t8187] VALUES (31,'فصل وهذا','294','28','0');
+INSERT INTO [t8187] VALUES (32,'نوع آخر في التعليل','294','28','0');
+INSERT INTO [t8187] VALUES (33,'فصل في','300','28','0');
+INSERT INTO [t8187] VALUES (34,'تخييل بغير تعليل','300','28','0');
+INSERT INTO [t8187] VALUES (35,'الفرق بين التشبيه والاستعارة','318','0','0');
+INSERT INTO [t8187] VALUES (36,'الاتفاق في الأخذ والسرقة','336','0','0');
+INSERT INTO [t8187] VALUES (37,'حدي الحقيقة والمجاز','348','0','0');
+INSERT INTO [t8187] VALUES (38,'المجاز العقلي والمجاز اللغوي','364','0','0');
+INSERT INTO [t8187] VALUES (39,'فصل','379','0','0');
+INSERT INTO [t8187] VALUES (40,'هذا كلام في ذكر المجاز وفي بيان معناه وحقيقته','393','0','0');
+INSERT INTO [t8187] VALUES (41,'تقسيم المجاز','406','40','0');
+INSERT INTO [t8187] VALUES (42,'الحذف والزيادة','414','40','0');
