afb65cfba84c4c57506b5443a61d94c04de97b12
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:10 +0700

    Upload SQL chunk: t26678_chunk_0000.sql

---
+++ sql_chunks/t26678_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t26678
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t26678" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26678
+INSERT INTO [t26678] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26678] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t26678] VALUES (3,'باب: ذكر مواسم العمر','3','0','0');
+INSERT INTO [t26678] VALUES (4,'الباب الأول في ذكر الموسم الأول','4','0','0');
+INSERT INTO [t26678] VALUES (5,'فصل','6','4','0');
+INSERT INTO [t26678] VALUES (6,'فصل','8','4','0');
+INSERT INTO [t26678] VALUES (7,'الباب الثاني في الموسم الثاني وهو من زمن البلوغ إلى منتهى الشباب','10','0','0');
+INSERT INTO [t26678] VALUES (8,'فصل','12','7','0');
+INSERT INTO [t26678] VALUES (9,'الباب الثالث في الموسم الثالث وهو حالة الكهولة','16','0','0');
+INSERT INTO [t26678] VALUES (10,'الباب الرابع في الموسم الرابع وهو الشيخوخة','18','0','0');
+INSERT INTO [t26678] VALUES (11,'الباب الخامس في الموسم الخامس وهو حال الهرم','23','0','0');
