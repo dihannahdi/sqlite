7afa9c1144f7556db66695009f0a4337795b39a3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:54 +0700

    Upload SQL chunk: t96854_chunk_0000.sql

---
+++ sql_chunks/t96854_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t96854
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t96854" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96854
+INSERT INTO [t96854] VALUES (1,'[مقدمة المحقق]','2','0','0');
+INSERT INTO [t96854] VALUES (2,'عملي في المخطوط','2','1','0');
+INSERT INTO [t96854] VALUES (3,'حول الرسالة','3','1','0');
+INSERT INTO [t96854] VALUES (4,'وصف النسخ الخطية','3','3','0');
+INSERT INTO [t96854] VALUES (5,'توثيق نسبتها للمؤلف','6','3','0');
+INSERT INTO [t96854] VALUES (6,'ترجمة المؤلف','6','1','0');
+INSERT INTO [t96854] VALUES (7,'سعادة الماجد بعمارة المساجد','8','0','0');
+INSERT INTO [t96854] VALUES (8,'سؤال في وقف على مدرسة أو مسجد تخرب','8','7','0');
+INSERT INTO [t96854] VALUES (9,'الجواب','9','7','0');
