1a0a7cdf1c01e08ace7341358d802aabb7501ee5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:22 +0700

    Upload SQL chunk: t12802_chunk_0000.sql

---
+++ sql_chunks/t12802_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t12802
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t12802" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12802
+INSERT INTO [t12802] VALUES (1,'الإهداء','3','0','0');
+INSERT INTO [t12802] VALUES (2,'المقدمة','4','0','0');
+INSERT INTO [t12802] VALUES (3,'بداية النهاية','6','0','0');
+INSERT INTO [t12802] VALUES (4,'سقوط الامبراطور','25','0','0');
+INSERT INTO [t12802] VALUES (5,'الماكرون','31','0','0');
+INSERT INTO [t12802] VALUES (6,'تصدير الثورة','41','0','0');
+INSERT INTO [t12802] VALUES (7,'ولاية الفقيه','46','0','0');
+INSERT INTO [t12802] VALUES (8,'الطابور الخامس في الاسلام','51','0','0');
+INSERT INTO [t12802] VALUES (9,'حرب الاحقاد','71','0','0');
+INSERT INTO [t12802] VALUES (10,'رفض الصلح','89','0','0');
+INSERT INTO [t12802] VALUES (11,'مهزلة الرهائن','98','0','0');
+INSERT INTO [t12802] VALUES (12,'النظام الايراني واسرائيل','119','0','0');
+INSERT INTO [t12802] VALUES (13,'الارهاب باسم الله','124','0','0');
+INSERT INTO [t12802] VALUES (14,'محاكم الثورة الاسلامية','132','0','0');
+INSERT INTO [t12802] VALUES (15,'الخميني في الميزان','144','0','0');
+INSERT INTO [t12802] VALUES (16,'الرعب المدمر','191','0','0');
+INSERT INTO [t12802] VALUES (17,'المطاف الاخير','208','0','0');
+INSERT INTO [t12802] VALUES (18,'سيرة المؤلف','216','0','0');
