d55b25b65c1a58c83b1b911475b491c9511736e3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:18:00 +0700

    Upload SQL chunk: t14315_chunk_0000.sql

---
+++ sql_chunks/t14315_chunk_0000.sql
@@ -0,0 +1,44 @@
+-- Table: t14315
+-- Chunk: 1
+-- Rows: 37
+
+CREATE TABLE "t14315" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14315
+INSERT INTO [t14315] VALUES (1,'مقدمة بقلم الشيخ علي بن حسن الحلبي الأثري','5','0','0');
+INSERT INTO [t14315] VALUES (2,'المقدمة','7','0','0');
+INSERT INTO [t14315] VALUES (3,'مدخل','10','0','0');
+INSERT INTO [t14315] VALUES (4,'الأحاديث التي عزاها بعض أهل العلم إلى "فوائد تمام" وليست في "الروض البسام"','14','0','0');
+INSERT INTO [t14315] VALUES (5,'كتاب الإيمان','21','0','0');
+INSERT INTO [t14315] VALUES (6,'كتاب العلم','28','0','0');
+INSERT INTO [t14315] VALUES (7,'كتاب الطهارة','46','0','0');
+INSERT INTO [t14315] VALUES (8,'كتاب الصلاة','66','0','0');
+INSERT INTO [t14315] VALUES (9,'كتاب الجنائز','99','0','0');
+INSERT INTO [t14315] VALUES (10,'كتاب الزكاة','106','0','0');
+INSERT INTO [t14315] VALUES (11,'كتاب الصوم','111','0','0');
+INSERT INTO [t14315] VALUES (12,'كتاب الحج','114','0','0');
+INSERT INTO [t14315] VALUES (13,'كتاب البيوع','125','0','0');
+INSERT INTO [t14315] VALUES (14,'كتاب العتق','128','0','0');
+INSERT INTO [t14315] VALUES (15,'كتاب النكاح','129','0','0');
+INSERT INTO [t14315] VALUES (16,'كتاب الطلاق','138','0','0');
+INSERT INTO [t14315] VALUES (17,'كتاب القصاص والحدود','139','0','0');
+INSERT INTO [t14315] VALUES (18,'كتاب الجهاد','143','0','0');
+INSERT INTO [t14315] VALUES (19,'كتاب الإمارة والقضاء','147','0','0');
+INSERT INTO [t14315] VALUES (20,'كتاب الأيمان والنذر','151','0','0');
+INSERT INTO [t14315] VALUES (21,'كتاب الصيد والذبائح','152','0','0');
+INSERT INTO [t14315] VALUES (22,'كتاب الأطعمة','153','0','0');
+INSERT INTO [t14315] VALUES (23,'كتاب الأشربة','161','0','0');
+INSERT INTO [t14315] VALUES (24,'كتاب الطب','164','0','0');
+INSERT INTO [t14315] VALUES (25,'كتاب اللباس والزينة','167','0','0');
+INSERT INTO [t14315] VALUES (26,'كتاب الأدب','169','0','0');
+INSERT INTO [t14315] VALUES (27,'كتاب البر والصلة','191','0','0');
+INSERT INTO [t14315] VALUES (28,'كتاب التفسير','200','0','0');
+INSERT INTO [t14315] VALUES (29,'كتاب المغازي','215','0','0');
+INSERT INTO [t14315] VALUES (30,'كتاب علامات النبوة','216','0','0');
+INSERT INTO [t14315] VALUES (31,'كتاب الأنبياء - عليهم السلام -','217','0','0');
+INSERT INTO [t14315] VALUES (32,'كتاب المناقب','219','0','0');
+INSERT INTO [t14315] VALUES (33,'كتاب الدعوات','235','0','0');
+INSERT INTO [t14315] VALUES (34,'كتاب الزهد والرقائق','239','0','0');
+INSERT INTO [t14315] VALUES (35,'كتاب الفتن','246','0','0');
+INSERT INTO [t14315] VALUES (36,'كتاب البعث وصفة النار والجنة','250','0','0');
+INSERT INTO [t14315] VALUES (37,'الأخطاء اللغوية','254','0','0');
