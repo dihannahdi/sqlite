83be3e69974c55c215ed86e6abc911cb4f0cf669
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:58 +0700

    Upload SQL chunk: t4098_chunk_0000.sql

---
+++ sql_chunks/t4098_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t4098
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t4098" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4098
+INSERT INTO [t4098] VALUES (1,'تعليقات على شرح لمعة الاعتقاد','1','0','0');
+INSERT INTO [t4098] VALUES (2,'قواعد في الأسماء والصفات','1','0','0');
+INSERT INTO [t4098] VALUES (3,'مقدمة صاحب المتن (ابن قدامة)','19','0','0');
+INSERT INTO [t4098] VALUES (4,'ما تضمنته خطبة الكتاب','20','0','0');
