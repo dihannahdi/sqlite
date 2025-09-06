528b5c9a55339bc7625fbf8a079d064095f921d0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:05 +0700

    Upload SQL chunk: t8972_chunk_0000.sql

---
+++ sql_chunks/t8972_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t8972
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t8972" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8972
+INSERT INTO [t8972] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8972] VALUES (2,'الرسول صلى الله عليه وسلم والنقد الأدبي','2','0','0');
+INSERT INTO [t8972] VALUES (3,'الخليفة عمر بن الخطاب رضي الله عنه والنقد الأدبي','11','0','0');
+INSERT INTO [t8972] VALUES (4,'الخلاصة','16','0','0');
