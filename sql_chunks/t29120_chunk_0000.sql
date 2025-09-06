70094b4f7e97c4123d86b587cb0b7d69968e458a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:54 +0700

    Upload SQL chunk: t29120_chunk_0000.sql

---
+++ sql_chunks/t29120_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t29120
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t29120" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29120
+INSERT INTO [t29120] VALUES (1,'كتاب الإيمان والإسلام والقدر والشفاعة','1','0','0');
+INSERT INTO [t29120] VALUES (2,'كتاب العلم','29','0','0');
+INSERT INTO [t29120] VALUES (3,'كتاب الطهارات','41','0','0');
+INSERT INTO [t29120] VALUES (4,'كتاب الصلاة','80','0','0');
+INSERT INTO [t29120] VALUES (5,'كتاب الزكاة','197','0','0');
+INSERT INTO [t29120] VALUES (6,'كتاب الصوم','201','0','0');
+INSERT INTO [t29120] VALUES (7,'كتاب الحج','224','0','0');
+INSERT INTO [t29120] VALUES (8,'كتاب النكاح','262','0','0');
+INSERT INTO [t29120] VALUES (9,'كتاب الاستبراء','288','0','0');
+INSERT INTO [t29120] VALUES (10,'كتاب الرضاع','290','0','0');
+INSERT INTO [t29120] VALUES (11,'كتاب الطلاق','293','0','0');
+INSERT INTO [t29120] VALUES (12,'كتاب النفقات','308','0','0');
+INSERT INTO [t29120] VALUES (13,'كتاب التدبير والولاء','311','0','0');
+INSERT INTO [t29120] VALUES (14,'كتاب الأيمان','315','0','0');
+INSERT INTO [t29120] VALUES (15,'كتاب الحدود','323','0','0');
+INSERT INTO [t29120] VALUES (16,'كتاب الجهاد والسير','330','0','0');
+INSERT INTO [t29120] VALUES (17,'كتاب البيوع','338','0','0');
+INSERT INTO [t29120] VALUES (18,'كتاب الرهن','361','0','0');
+INSERT INTO [t29120] VALUES (19,'كتاب الشفعة','363','0','0');
+INSERT INTO [t29120] VALUES (20,'كتاب المزارعة','367','0','0');
+INSERT INTO [t29120] VALUES (21,'كتاب الفضائل والشمائل','370','0','0');
+INSERT INTO [t29120] VALUES (22,'كتاب فضل أمته رضي الله عنه','401','0','0');
+INSERT INTO [t29120] VALUES (23,'كتاب الأطعمة والأشربة والشرب والضحايا والصيد والذبائح','408','0','0');
+INSERT INTO [t29120] VALUES (24,'كتاب اللباس والزينة','444','0','0');
+INSERT INTO [t29120] VALUES (25,'كتاب الطب وفضل المرض والرقى والدعوات','453','0','0');
+INSERT INTO [t29120] VALUES (26,'كتاب الأدب','468','0','0');
+INSERT INTO [t29120] VALUES (27,'كتاب الرقاق','500','0','0');
+INSERT INTO [t29120] VALUES (28,'كتاب الجنايات','504','0','0');
+INSERT INTO [t29120] VALUES (29,'كتاب الأحكام','508','0','0');
+INSERT INTO [t29120] VALUES (30,'كتاب الفتن','518','0','0');
+INSERT INTO [t29120] VALUES (31,'كتاب التفسير','522','0','0');
+INSERT INTO [t29120] VALUES (32,'كتاب الوصايا والفرائض','536','0','0');
+INSERT INTO [t29120] VALUES (33,'كتاب القيامة وصفة الجنة','543','0','0');
