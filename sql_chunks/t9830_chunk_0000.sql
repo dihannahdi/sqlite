3183ee911b4f377f28730cd56f9caffcef520cdd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:01 +0700

    Upload SQL chunk: t9830_chunk_0000.sql

---
+++ sql_chunks/t9830_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t9830
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t9830" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9830
+INSERT INTO [t9830] VALUES (1,'مقدمة المترجم: هل لهذا الكون من إله؟','4','0','0');
+INSERT INTO [t9830] VALUES (2,'نشأة العالم: هل هو مصادفة أو قصد؟','8','0','0');
+INSERT INTO [t9830] VALUES (3,'اختبار شامل','14','0','0');
+INSERT INTO [t9830] VALUES (4,'النتيجة الحتمية','24','0','0');
+INSERT INTO [t9830] VALUES (5,'استخدام الاسلوب العلمي','34','0','0');
+INSERT INTO [t9830] VALUES (6,'الأدلة الطبيعية على وجود الله','38','0','0');
+INSERT INTO [t9830] VALUES (7,'الكشوف العلمية تثبت وجود الله','42','0','0');
+INSERT INTO [t9830] VALUES (8,'الماء يروي لك القصة','45','0','0');
+INSERT INTO [t9830] VALUES (9,'الله والكون المعقد','49','0','0');
+INSERT INTO [t9830] VALUES (10,'المادية وحدها لا تكفي','54','0','0');
+INSERT INTO [t9830] VALUES (11,'الحائر الصغير يفكر','58','0','0');
+INSERT INTO [t9830] VALUES (12,'حقائق من سجل الغابات','62','0','0');
+INSERT INTO [t9830] VALUES (13,'ما وعاه ابن صاحب البستان','71','0','0');
+INSERT INTO [t9830] VALUES (14,'الخلايا الحية تؤدي رسالتها','76','0','0');
+INSERT INTO [t9830] VALUES (15,'منطق الإيمان','81','0','0');
+INSERT INTO [t9830] VALUES (16,'موجهات جيولوجية','87','0','0');
+INSERT INTO [t9830] VALUES (17,'المبدع الأعظم','91','0','0');
+INSERT INTO [t9830] VALUES (18,'نظرة إلى ماوراء القوانين الطبيعية','95','0','0');
+INSERT INTO [t9830] VALUES (19,'الله والقوانين الكيماوية','99','0','0');
+INSERT INTO [t9830] VALUES (20,'العلم تدعم إيماني بالله','107','0','0');
+INSERT INTO [t9830] VALUES (21,'صحة الدين','114','0','0');
+INSERT INTO [t9830] VALUES (22,'عجائب التربة','119','0','0');
+INSERT INTO [t9830] VALUES (23,'الإنسان ذاته هو الدليل','129','0','0');
+INSERT INTO [t9830] VALUES (24,'التوافق بين العلوم','132','0','0');
+INSERT INTO [t9830] VALUES (25,'الله والعلاج الطبي','137','0','0');
+INSERT INTO [t9830] VALUES (26,'الزهر وطيور بالتيمور','142','0','0');
+INSERT INTO [t9830] VALUES (27,'وجود الله حقيقة مطلقة','147','0','0');
+INSERT INTO [t9830] VALUES (28,'تعليق المراجع','166','0','0');
