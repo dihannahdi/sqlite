8f068c95354cbe0d30dcdec91af0fd8daae71be7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:37 +0700

    Upload SQL chunk: t95866_chunk_0000.sql

---
+++ sql_chunks/t95866_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t95866
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t95866" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95866
+INSERT INTO [t95866] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t95866] VALUES (2,'باب الإعراب ومعرفة علاماته','3','0','0');
+INSERT INTO [t95866] VALUES (3,'باب الأفعال','5','0','0');
+INSERT INTO [t95866] VALUES (4,'باب مرفوعات الأسماء','5','0','0');
+INSERT INTO [t95866] VALUES (5,'باب الفاعل','6','0','0');
+INSERT INTO [t95866] VALUES (6,'باب النائب عن الفاعل','6','0','0');
+INSERT INTO [t95866] VALUES (7,'باب المبتدأ والخبر','6','0','0');
+INSERT INTO [t95866] VALUES (8,'باب نواسخ الابتداء "وهي كان وأخواتها وإن وأخواتها وظن وأخواتها"','7','0','0');
+INSERT INTO [t95866] VALUES (9,'باب النعت','8','0','0');
+INSERT INTO [t95866] VALUES (10,'باب العطف','9','0','0');
+INSERT INTO [t95866] VALUES (11,'باب التوكيد','9','0','0');
+INSERT INTO [t95866] VALUES (12,'باب البدل','10','0','0');
+INSERT INTO [t95866] VALUES (13,'باب المفعول به','10','0','0');
+INSERT INTO [t95866] VALUES (14,'باب المصدر','10','0','0');
+INSERT INTO [t95866] VALUES (15,'باب الظرف','11','0','0');
+INSERT INTO [t95866] VALUES (16,'باب الحال','11','0','0');
+INSERT INTO [t95866] VALUES (17,'باب التمييز','12','0','0');
+INSERT INTO [t95866] VALUES (18,'باب الاستثناء','12','0','0');
+INSERT INTO [t95866] VALUES (19,'باب لا التي لنفي الجنس','13','0','0');
+INSERT INTO [t95866] VALUES (20,'باب المنادى','13','0','0');
+INSERT INTO [t95866] VALUES (21,'باب المفعول من أجله','14','0','0');
+INSERT INTO [t95866] VALUES (22,'باب المفعول معه','14','0','0');
+INSERT INTO [t95866] VALUES (23,'باب مخفوضات الأسماء','14','0','0');
