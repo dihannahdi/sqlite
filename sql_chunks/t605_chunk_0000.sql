aedda68c4079e665cbe5839957d7247c725d911a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:48 +0700

    Upload SQL chunk: t605_chunk_0000.sql

---
+++ sql_chunks/t605_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t605
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t605" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t605
+INSERT INTO [t605] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t605] VALUES (2,'الجزء الأول','1','0','0');
+INSERT INTO [t605] VALUES (3,'مجلس','6','2','0');
+INSERT INTO [t605] VALUES (4,'الجزء الثاني','12','0','0');
+INSERT INTO [t605] VALUES (5,'الأجزاء في القرآن','13','4','0');
+INSERT INTO [t605] VALUES (6,'مجلس','16','4','0');
+INSERT INTO [t605] VALUES (7,'الجزء الثالث','22','0','0');
+INSERT INTO [t605] VALUES (8,'مجلس','30','7','0');
+INSERT INTO [t605] VALUES (9,'الجزء الرابع','32','0','0');
+INSERT INTO [t605] VALUES (10,'مجلس','37','9','0');
+INSERT INTO [t605] VALUES (11,'الجزء الخامس','41','0','0');
+INSERT INTO [t605] VALUES (12,'مجلس','44','11','0');
+INSERT INTO [t605] VALUES (13,'الجزء السادس','50','0','0');
+INSERT INTO [t605] VALUES (14,'مجلس','58','13','0');
+INSERT INTO [t605] VALUES (15,'الجزء السابع','59','0','0');
+INSERT INTO [t605] VALUES (16,'الجزء الثامن','68','0','0');
+INSERT INTO [t605] VALUES (17,'الجزء التاسع','78','0','0');
+INSERT INTO [t605] VALUES (18,'الجزء العاشر','88','0','0');
+INSERT INTO [t605] VALUES (19,'الجزء الحادي عشر','99','0','0');
+INSERT INTO [t605] VALUES (20,'الجزء الثاني عشر','107','0','0');
