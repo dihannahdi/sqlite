a3c447dbcfcde3c7b993745a578ea7e86208b766
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:08 +0700

    Upload SQL chunk: t1016_chunk_0000.sql

---
+++ sql_chunks/t1016_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t1016
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t1016" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1016
+INSERT INTO [t1016] VALUES (1,'تاريخ الأدب الأندلسي (عصر سيادة قرطبة)','1','0','0');
+INSERT INTO [t1016] VALUES (2,'تصدير','1','0','0');
+INSERT INTO [t1016] VALUES (3,'المقدمة','5','0','0');
+INSERT INTO [t1016] VALUES (4,'فصول في الشعر الأندلسي','27','0','0');
