6e071273fa6719ab1963a12b926e7cfb92a6537e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:50 +0700

    Upload SQL chunk: t1179_chunk_0000.sql

---
+++ sql_chunks/t1179_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t1179
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t1179" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1179
+INSERT INTO [t1179] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t1179] VALUES (2,'خطة البحث','7','0','0');
+INSERT INTO [t1179] VALUES (3,'تمهيد نبذة عن الدعوة السلفية بنجد','8','0','0');
+INSERT INTO [t1179] VALUES (4,'المبحث الأول فقه ما قبل الإنكار','13','0','0');
+INSERT INTO [t1179] VALUES (5,'المطلب الأول حقيقة فاعل المنكر','13','4','0');
+INSERT INTO [t1179] VALUES (6,'المطلب الثاني التثبت وعدم العجلة','18','4','0');
+INSERT INTO [t1179] VALUES (7,'المطلب الثالث العلم','21','4','0');
+INSERT INTO [t1179] VALUES (8,'المبحث الثاني فقه ما يجب حال الإنكار','38','0','0');
+INSERT INTO [t1179] VALUES (9,'المطلب الأول الحكم إذا ترتب على الإنكار مفسدة أعظم','38','8','0');
+INSERT INTO [t1179] VALUES (10,'المطلب الثاني أسلوب الإنكار','46','8','0');
+INSERT INTO [t1179] VALUES (11,'المطلب الثالث الإنكار على ولاة الأمور الحكام','60','8','0');
+INSERT INTO [t1179] VALUES (12,'الخاتمة','70','0','0');
+INSERT INTO [t1179] VALUES (13,'فهرس المراجع والمصادر','72','0','0');
