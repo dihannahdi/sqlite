d1e2e8416545d07f6094078aaca113a4a571723d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:03 +0700

    Upload SQL chunk: t36076_chunk_0000.sql

---
+++ sql_chunks/t36076_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t36076
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t36076" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36076
+INSERT INTO [t36076] VALUES (1,'مقدمة في مناسبة هذه الآية لما قبلها','5','0','0');
+INSERT INTO [t36076] VALUES (2,'خاتمة','72','0','0');
