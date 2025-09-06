bc0e8d99e580d4caa1708a02acca09141ebab545
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:29 +0700

    Upload SQL chunk: t1938_chunk_0000.sql

---
+++ sql_chunks/t1938_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t1938
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t1938" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1938
+INSERT INTO [t1938] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1938] VALUES (2,'ترجمة معاني القرآن الكريم عبر التاريخ','4','0','0');
+INSERT INTO [t1938] VALUES (3,'المبحث الأول المقصود بالنشر الإلكتروني وخصائصه ومزاياه','7','0','0');
+INSERT INTO [t1938] VALUES (4,'الشبكة العنكبوتية العالمية World Wide Web','7','3','0');
+INSERT INTO [t1938] VALUES (5,'النشر الإلكتروني Electronic Publishing','8','3','0');
+INSERT INTO [t1938] VALUES (6,'أشكال النشر الإلكتروني','12','3','0');
+INSERT INTO [t1938] VALUES (7,'مزايا النشر الإلكتروني وخصائصه','13','3','0');
+INSERT INTO [t1938] VALUES (8,'الدعوة الإسلامية والنشر الإلكتروني','17','3','0');
+INSERT INTO [t1938] VALUES (9,'المبحث الثاني استخدام النشر الإلكتروني لترجمات معاني القرآن الكريم في خدمة الدعوة','19','0','0');
+INSERT INTO [t1938] VALUES (10,'توصيات الدراسة','29','0','0');
+INSERT INTO [t1938] VALUES (11,'المصادر والمراجع','32','0','0');
