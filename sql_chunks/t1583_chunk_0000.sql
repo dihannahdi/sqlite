3f792efbcfd3dd78833e205fb51c3e00f08e7654
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:28 +0700

    Upload SQL chunk: t1583_chunk_0000.sql

---
+++ sql_chunks/t1583_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t1583
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t1583" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1583
+INSERT INTO [t1583] VALUES (1,'المجلد الأول','1','0','0');
+INSERT INTO [t1583] VALUES (2,'مقدمة','1','1','0');
+INSERT INTO [t1583] VALUES (3,'الطبقة الأولي من الكتاب','4','1','0');
+INSERT INTO [t1583] VALUES (4,'الطبقة الثانية من الكتاب','34','1','0');
+INSERT INTO [t1583] VALUES (5,'الطبقة الثالثة من الكتاب','52','1','0');
+INSERT INTO [t1583] VALUES (6,'الطبقة الرابعة من الكتاب','77','1','0');
+INSERT INTO [t1583] VALUES (7,'الطبقة الخامسة من الكتاب','116','1','0');
+INSERT INTO [t1583] VALUES (8,'الطبقة السادسة من الكتاب','175','1','0');
+INSERT INTO [t1583] VALUES (9,'الطبقة السابعة من الكتاب','236','1','0');
+INSERT INTO [t1583] VALUES (10,'فهرس المحتويات','298','1','0');
+INSERT INTO [t1583] VALUES (11,'المجلد الثاني','299','0','0');
+INSERT INTO [t1583] VALUES (12,'الطبقة الثامنة','299','11','0');
+INSERT INTO [t1583] VALUES (13,'الطبقة التاسعة','383','11','0');
+INSERT INTO [t1583] VALUES (14,'الطبقة العاشرة','447','11','0');
+INSERT INTO [t1583] VALUES (15,'فهرس المحتويات','538','11','0');
+INSERT INTO [t1583] VALUES (16,'المجلد الثالث','539','0','0');
+INSERT INTO [t1583] VALUES (17,'الطبقة الحادية عشرة','539','16','0');
+INSERT INTO [t1583] VALUES (18,'الطبقة الثانية عشرة','601','16','0');
+INSERT INTO [t1583] VALUES (19,'الطبقة الثالثة عشرة','672','16','0');
+INSERT INTO [t1583] VALUES (20,'الطبقة الرابعة عشرة','744','16','0');
+INSERT INTO [t1583] VALUES (21,'فهرس','796','16','0');
+INSERT INTO [t1583] VALUES (22,'المجلد الرابع','797','0','0');
+INSERT INTO [t1583] VALUES (23,'الطبقة الخامسة عشرة','797','22','0');
+INSERT INTO [t1583] VALUES (24,'الطبقة السادسة عشرة','852','22','0');
+INSERT INTO [t1583] VALUES (25,'الطبقة السابعة عشرة','884','22','0');
+INSERT INTO [t1583] VALUES (26,'الطبقة الثامنة عشرة','921','22','0');
+INSERT INTO [t1583] VALUES (27,'الطبقة التاسعة عشرة','953','22','0');
+INSERT INTO [t1583] VALUES (28,'الطبقة العشرون','966','22','0');
+INSERT INTO [t1583] VALUES (29,'الطبقة الحادية والعشرون','979','22','0');
+INSERT INTO [t1583] VALUES (30,'شيوخ صاحب التذكرة','989','22','0');
+INSERT INTO [t1583] VALUES (31,'الفهرس','998','22','0');
