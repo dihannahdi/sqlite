b15974a11071acd835ddf7b4f9425c57423a446f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:35 +0700

    Upload SQL chunk: t37494_chunk_0000.sql

---
+++ sql_chunks/t37494_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t37494
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t37494" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37494
+INSERT INTO [t37494] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t37494] VALUES (2,'وموجب هذه المقدمة الواعظة','5','0','0');
