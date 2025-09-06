f517b74021d892df1e4a4cf0a4264c95610e67b6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:04 +0700

    Upload SQL chunk: t6492_chunk_0000.sql

---
+++ sql_chunks/t6492_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6492
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6492" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6492
+INSERT INTO [t6492] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6492] VALUES (2,'مسألة في العقل','28','0','0');
