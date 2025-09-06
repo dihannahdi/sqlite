72bdde039c592c81a632c36c3ab3ea8d2b2087c4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:44 +0700

    Upload SQL chunk: t5820_chunk_0000.sql

---
+++ sql_chunks/t5820_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t5820
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t5820" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5820
+INSERT INTO [t5820] VALUES (1,'تسمية فقهاء الأنصار من أصحاب رسول الله صلى الله عليه وسلم ومن بعدهم من أهل المدينة','1','0','0');
+INSERT INTO [t5820] VALUES (2,'ومن التابعين','1','0','0');
+INSERT INTO [t5820] VALUES (3,'ومن بعد التابعين','2','0','0');
+INSERT INTO [t5820] VALUES (4,'وبعد هؤلاء','2','0','0');
+INSERT INTO [t5820] VALUES (5,'وأصحاب مالك من أهل المدينة','2','0','0');
+INSERT INTO [t5820] VALUES (6,'ومن أهل مصر','2','0','0');
+INSERT INTO [t5820] VALUES (7,'وأصحاب عبد الله بن عباس من أهل مكة','2','0','0');
+INSERT INTO [t5820] VALUES (8,'وبعد هؤلاء','2','0','0');
+INSERT INTO [t5820] VALUES (9,'وبعد هؤلاء','2','0','0');
+INSERT INTO [t5820] VALUES (10,'وأصحاب الشافعي','3','0','0');
+INSERT INTO [t5820] VALUES (11,'والفقهاء من أهل الكوفة','3','0','0');
+INSERT INTO [t5820] VALUES (12,'ومن فقهاء التابعين','3','0','0');
+INSERT INTO [t5820] VALUES (13,'وبعد هؤلاء','3','0','0');
+INSERT INTO [t5820] VALUES (14,'وبعد هذين','3','0','0');
+INSERT INTO [t5820] VALUES (15,'وبعد هؤلاء','3','0','0');
+INSERT INTO [t5820] VALUES (16,'وبعد هؤلاء','3','0','0');
+INSERT INTO [t5820] VALUES (17,'وأصحاب الحسن بن حي','3','0','0');
+INSERT INTO [t5820] VALUES (18,'ومن فقهاء أهل البصرة','3','0','0');
+INSERT INTO [t5820] VALUES (19,'ومن التابعين','4','0','0');
+INSERT INTO [t5820] VALUES (20,'وبعد هؤلاء','4','0','0');
+INSERT INTO [t5820] VALUES (21,'وبعد هؤلاء','4','0','0');
+INSERT INTO [t5820] VALUES (22,'وبعد هؤلاء','4','0','0');
+INSERT INTO [t5820] VALUES (23,'وبعد هؤلاء','4','0','0');
+INSERT INTO [t5820] VALUES (24,'وبعد هؤلاء','4','0','0');
+INSERT INTO [t5820] VALUES (25,'ومن فقهاء الشام','4','0','0');
+INSERT INTO [t5820] VALUES (26,'وبعد هؤلاء','4','0','0');
+INSERT INTO [t5820] VALUES (27,'وبعده','4','0','0');
+INSERT INTO [t5820] VALUES (28,'ومن فقهاء أهل مصر','4','0','0');
+INSERT INTO [t5820] VALUES (29,'وبعد هؤلاء','4','0','0');
+INSERT INTO [t5820] VALUES (30,'وبعد هؤلاء','5','0','0');
+INSERT INTO [t5820] VALUES (31,'ومن فقهاء أهل خراسان','5','0','0');
+INSERT INTO [t5820] VALUES (32,'وبعد هؤلاء','5','0','0');
+INSERT INTO [t5820] VALUES (33,'وأثبت أصحاب الأوزاعي','5','0','0');
+INSERT INTO [t5820] VALUES (34,'وأثبت أصحاب أيوب','5','0','0');
+INSERT INTO [t5820] VALUES (35,'وأثبت أصحاب حماد بن سلمة','5','0','0');
+INSERT INTO [t5820] VALUES (36,'وأثبت أصحاب سعيد بن أبي عروبة','5','0','0');
