3efd8e8a7c5f33a9b81d635864183635fe08782c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:20:09 +0700

    Upload SQL chunk: t145432_chunk_0000.sql

---
+++ sql_chunks/t145432_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t145432
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t145432" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t145432
+INSERT INTO [t145432] VALUES (1,'والأصل في الفرح وإظهاره في العيد','4','0','0');
+INSERT INTO [t145432] VALUES (2,'ما العيد؟','9','0','0');
+INSERT INTO [t145432] VALUES (3,'فوجدت مثلا','16','0','0');
+INSERT INTO [t145432] VALUES (4,'وهكذا دواليك، كل جيل يندب العيد! فما السبب؟','18','0','0');
+INSERT INTO [t145432] VALUES (5,'الأطفال والجواري والشباب أولا','21','0','0');
+INSERT INTO [t145432] VALUES (6,'وهل يسعد من كان الشيطان قرينه؟ !','32','0','0');
+INSERT INTO [t145432] VALUES (7,'وصلاة الفجر مفتاح السعادة والسرور','32','0','0');
+INSERT INTO [t145432] VALUES (8,'أخيرا','33','0','0');
