944f234dc5c972c7c6722bdf6fff3c7b4176ee67
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:21 +0700

    Upload SQL chunk: t8566_chunk_0000.sql

---
+++ sql_chunks/t8566_chunk_0000.sql
@@ -0,0 +1,56 @@
+-- Table: t8566
+-- Chunk: 1
+-- Rows: 49
+
+CREATE TABLE "t8566" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8566
+INSERT INTO [t8566] VALUES (1,'فصل -١-','1','0','0');
+INSERT INTO [t8566] VALUES (2,'فصل -٢-','16','0','0');
+INSERT INTO [t8566] VALUES (3,'فصل-٣-','20','0','0');
+INSERT INTO [t8566] VALUES (4,'فصل-٤ -','23','0','0');
+INSERT INTO [t8566] VALUES (5,'فصل-٥-','25','0','0');
+INSERT INTO [t8566] VALUES (6,'فصل-٦-','32','0','0');
+INSERT INTO [t8566] VALUES (7,'فصل-٧-','33','0','0');
+INSERT INTO [t8566] VALUES (8,'فصل-٨-','36','0','0');
+INSERT INTO [t8566] VALUES (9,'فصل-٩ -','38','0','0');
+INSERT INTO [t8566] VALUES (10,'فصل-١٠-','39','0','0');
+INSERT INTO [t8566] VALUES (11,'فصل -١١-','41','0','0');
+INSERT INTO [t8566] VALUES (12,'فصل-١٢-','43','0','0');
+INSERT INTO [t8566] VALUES (13,'فصل-١٣-','45','0','0');
+INSERT INTO [t8566] VALUES (14,'فصل-١٤-','46','0','0');
+INSERT INTO [t8566] VALUES (15,'فصل-١٥-','48','0','0');
+INSERT INTO [t8566] VALUES (16,'فصل-١٦-','49','0','0');
+INSERT INTO [t8566] VALUES (17,'فصل-١٧-','58','0','0');
+INSERT INTO [t8566] VALUES (18,'فصل- ١٨-','62','0','0');
+INSERT INTO [t8566] VALUES (19,'فصل -١٩-','66','0','0');
+INSERT INTO [t8566] VALUES (20,'فصل-٢٠-','68','0','0');
+INSERT INTO [t8566] VALUES (21,'فصل -٢١-','75','0','0');
+INSERT INTO [t8566] VALUES (22,'فصل-٢٢-','78','0','0');
+INSERT INTO [t8566] VALUES (23,'فصل-٢٣ -','79','0','0');
+INSERT INTO [t8566] VALUES (24,'فصل-٢٤-','81','0','0');
+INSERT INTO [t8566] VALUES (25,'فصل -٢٥-','81','0','0');
+INSERT INTO [t8566] VALUES (26,'فصل -٢٦-','82','0','0');
+INSERT INTO [t8566] VALUES (27,'فصل-٢٧-','86','0','0');
+INSERT INTO [t8566] VALUES (28,'فصل-٢٨-','88','0','0');
+INSERT INTO [t8566] VALUES (29,'فصل -٢٩-','89','0','0');
+INSERT INTO [t8566] VALUES (30,'فصل-٣٠-','90','0','0');
+INSERT INTO [t8566] VALUES (31,'فصل-٣١-','91','0','0');
+INSERT INTO [t8566] VALUES (32,'فصل-٣٢-','93','0','0');
+INSERT INTO [t8566] VALUES (33,'فصل -٣٤-','96','0','0');
+INSERT INTO [t8566] VALUES (34,'فصل-٣٥-','96','0','0');
+INSERT INTO [t8566] VALUES (35,'فصل-٣٦-','96','0','0');
+INSERT INTO [t8566] VALUES (36,'فصل-٣٧-','97','0','0');
+INSERT INTO [t8566] VALUES (37,'فصل-٣٨-','99','0','0');
+INSERT INTO [t8566] VALUES (38,'فصل -٣٩-','99','0','0');
+INSERT INTO [t8566] VALUES (39,'فصل -٤٠-','102','0','0');
+INSERT INTO [t8566] VALUES (40,'فصل -٤١-','103','0','0');
+INSERT INTO [t8566] VALUES (41,'فصل - ٤٢-','107','0','0');
+INSERT INTO [t8566] VALUES (42,'فصل -٤٣-','110','0','0');
+INSERT INTO [t8566] VALUES (43,'فصل -٤٤ -','111','0','0');
+INSERT INTO [t8566] VALUES (44,'فصل-٤٥-','113','0','0');
+INSERT INTO [t8566] VALUES (45,'فصل -٤٦-','114','0','0');
+INSERT INTO [t8566] VALUES (46,'فصل -٤٧-','117','0','0');
+INSERT INTO [t8566] VALUES (47,'فصل-٤٨-','119','0','0');
+INSERT INTO [t8566] VALUES (48,'فصل - ٤٩-','119','0','0');
+INSERT INTO [t8566] VALUES (49,'فصل -٥٠-','121','0','0');
