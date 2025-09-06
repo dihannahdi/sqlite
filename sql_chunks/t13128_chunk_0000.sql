cadf261acd8a0e8e6fc8e4e1b6c90c30fb83321c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:10:56 +0700

    Upload SQL chunk: t13128_chunk_0000.sql

---
+++ sql_chunks/t13128_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t13128
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t13128" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13128
+INSERT INTO [t13128] VALUES (1,'التعوذ بالله من النار بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t13128] VALUES (2,'أبواب جهنم','7','0','0');
+INSERT INTO [t13128] VALUES (3,'باب صفة جهنم وسعتها','14','0','0');
+INSERT INTO [t13128] VALUES (4,'جبال النار وأوديتها','32','0','0');
+INSERT INTO [t13128] VALUES (5,'باب مقامع أهل النار وسلاسلها وأغلالها','59','0','0');
+INSERT INTO [t13128] VALUES (6,'الحميم والصديد والمهل والغسلين شراب أهل النار وطعامهم','79','0','0');
+INSERT INTO [t13128] VALUES (7,'الحيات والعقارب','99','0','0');
+INSERT INTO [t13128] VALUES (8,'تلفح وجوههم النار وهم فيها كالحون','117','0','0');
+INSERT INTO [t13128] VALUES (9,'ألوان العذاب','130','0','0');
+INSERT INTO [t13128] VALUES (10,'بكاء أهل النار','218','0','0');
