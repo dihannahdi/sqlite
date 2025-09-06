dbdaf9cdf887069870f1dac9ae31e13a967545c1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:32 +0700

    Upload SQL chunk: t28181_chunk_0000.sql

---
+++ sql_chunks/t28181_chunk_0000.sql
@@ -0,0 +1,60 @@
+-- Table: t28181
+-- Chunk: 1
+-- Rows: 53
+
+CREATE TABLE "t28181" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t28181
+INSERT INTO [t28181] VALUES (1,'رب يسر لإتمامه','1','0','0');
+INSERT INTO [t28181] VALUES (2,'الفصل الأول','2','1','0');
+INSERT INTO [t28181] VALUES (3,'حد الشعر','2','0','0');
+INSERT INTO [t28181] VALUES (4,'صناعة الشعر','2','3','0');
+INSERT INTO [t28181] VALUES (5,'صفات الشعر','3','3','0');
+INSERT INTO [t28181] VALUES (6,'معاني الشعر','3','3','0');
+INSERT INTO [t28181] VALUES (7,'النعوت','7','0','0');
+INSERT INTO [t28181] VALUES (8,'الفصل الثاني','7','7','0');
+INSERT INTO [t28181] VALUES (9,'نعت اللفظ','7','7','0');
+INSERT INTO [t28181] VALUES (10,'نعت الوزن','9','7','0');
+INSERT INTO [t28181] VALUES (11,'الترصيع','10','7','0');
+INSERT INTO [t28181] VALUES (12,'نعت القوافي','13','7','0');
+INSERT INTO [t28181] VALUES (13,'نعوت المعاني الدال عليها الشعر','16','7','0');
+INSERT INTO [t28181] VALUES (14,'الغلو والاقتصار','16','7','0');
+INSERT INTO [t28181] VALUES (15,'نعت المدح','19','7','0');
+INSERT INTO [t28181] VALUES (16,'أقسام المدح','25','7','0');
+INSERT INTO [t28181] VALUES (17,'مدح الملوك','25','7','0');
+INSERT INTO [t28181] VALUES (18,'مدح ذوي الصناعات','26','7','0');
+INSERT INTO [t28181] VALUES (19,'مدح القائد','26','7','0');
+INSERT INTO [t28181] VALUES (20,'مدح السوقة','27','7','0');
+INSERT INTO [t28181] VALUES (21,'نعت الهجاء','28','7','0');
+INSERT INTO [t28181] VALUES (22,'نعت المراثي','32','7','0');
+INSERT INTO [t28181] VALUES (23,'نعت التشبيه','35','7','0');
+INSERT INTO [t28181] VALUES (24,'التصرف في التشبيه','38','7','0');
+INSERT INTO [t28181] VALUES (25,'نعت الوصف','40','7','0');
+INSERT INTO [t28181] VALUES (26,'نعت النسيب','41','7','0');
+INSERT INTO [t28181] VALUES (27,'المعاني الشعرية','45','7','0');
+INSERT INTO [t28181] VALUES (28,'صحة التقسيم','45','7','0');
+INSERT INTO [t28181] VALUES (29,'صحة المقابلات','46','7','0');
+INSERT INTO [t28181] VALUES (30,'صحة التفسير','47','7','0');
+INSERT INTO [t28181] VALUES (31,'أنواع نعوت المعاني','48','7','0');
+INSERT INTO [t28181] VALUES (32,'التتميم','48','7','0');
+INSERT INTO [t28181] VALUES (33,'المبالغة','49','7','0');
+INSERT INTO [t28181] VALUES (34,'التكافؤ','50','7','0');
+INSERT INTO [t28181] VALUES (35,'نعت ائتلاف اللفظ مع المعنى:','53','7','0');
+INSERT INTO [t28181] VALUES (36,'نعت ائتلاف اللفظ والوزن:','60','7','0');
+INSERT INTO [t28181] VALUES (37,'نعت ائتلاف المعنى والوزن:','61','7','0');
+INSERT INTO [t28181] VALUES (38,'نعت ائتلاف القافية مع ما يدل عليه سائر البيت:','61','7','0');
+INSERT INTO [t28181] VALUES (39,'التوشيح','62','7','0');
+INSERT INTO [t28181] VALUES (40,'الفصل الثالث','63','7','0');
+INSERT INTO [t28181] VALUES (41,'عيوب الشعر','63','0','0');
+INSERT INTO [t28181] VALUES (42,'عيوب اللفظ','64','41','0');
+INSERT INTO [t28181] VALUES (43,'ذكر المديح','70','41','0');
+INSERT INTO [t28181] VALUES (44,'ذكر عيوب الهجاء','72','41','0');
+INSERT INTO [t28181] VALUES (45,'عيب التشبيه','74','41','0');
+INSERT INTO [t28181] VALUES (46,'عيب الوصف','74','41','0');
+INSERT INTO [t28181] VALUES (47,'الغزل','74','41','0');
+INSERT INTO [t28181] VALUES (48,'فساد المقابلات','76','41','0');
+INSERT INTO [t28181] VALUES (49,'فساد التفسير','77','41','0');
+INSERT INTO [t28181] VALUES (50,'الاستحالة والتناقض','78','41','0');
+INSERT INTO [t28181] VALUES (51,'التناقض على طريق القنية والعدم','80','41','0');
+INSERT INTO [t28181] VALUES (52,'التناقض على طريق الإيجاب والسلب','81','41','0');
+INSERT INTO [t28181] VALUES (53,'إيقاع الممتنع','82','41','0');
