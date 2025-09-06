86ae4fcd5724eb09f3b8a47e4366cfec144bf63f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:53 +0700

    Upload SQL chunk: t8951_chunk_0000.sql

---
+++ sql_chunks/t8951_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t8951
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t8951" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8951
+INSERT INTO [t8951] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8951] VALUES (2,'المماليك','2','0','0');
+INSERT INTO [t8951] VALUES (3,'أساليب تربية وتدريب هؤلاء المماليك','7','0','0');
+INSERT INTO [t8951] VALUES (4,'مميزات المماليك','10','0','0');
+INSERT INTO [t8951] VALUES (5,'قيام دولة المماليك البحرية','12','0','0');
+INSERT INTO [t8951] VALUES (6,'مقتل السلطان تورانشاة بن الملك الصالح نجم الدين أيوب','15','0','0');
+INSERT INTO [t8951] VALUES (7,'حكم شجر الدر','17','0','0');
+INSERT INTO [t8951] VALUES (8,'تولي الأتابك عز الدين أيبك السلطة','20','0','0');
+INSERT INTO [t8951] VALUES (9,'استيلاء المظفر قطز على السلطنة','24','0','0');
+INSERT INTO [t8951] VALUES (10,'تولي السلطان الظاهر بيبرس السلطنة','27','0','0');
+INSERT INTO [t8951] VALUES (11,'جهاد المماليك البحرية ضد الصليبيين','28','0','0');
+INSERT INTO [t8951] VALUES (12,'دور بيبرس في التصدي للصليبيين','32','0','0');
+INSERT INTO [t8951] VALUES (13,'سقوط أنطاكية بيد السلطان بيبرس','36','0','0');
+INSERT INTO [t8951] VALUES (14,'جهاد السلطان الناصر قلاوون ضد الصليبيين','39','0','0');
+INSERT INTO [t8951] VALUES (15,'مصادر ومراجع','45','0','0');
