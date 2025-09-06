2f0d6d41a86b53dcbe83ce9be76502b4b7b45b06
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:49 +0700

    Upload SQL chunk: t6060_chunk_0000.sql

---
+++ sql_chunks/t6060_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t6060
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t6060" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6060
+INSERT INTO [t6060] VALUES (1,'باب الألف','2','0','0');
+INSERT INTO [t6060] VALUES (2,'باب أتى من هذا الحرف','23','1','0');
+INSERT INTO [t6060] VALUES (3,'باب الباء الموحدة','891','0','0');
+INSERT INTO [t6060] VALUES (4,'باب التاء','933','0','0');
+INSERT INTO [t6060] VALUES (5,'باب الثاء','981','0','0');
+INSERT INTO [t6060] VALUES (6,'باب الجيم','995','0','0');
+INSERT INTO [t6060] VALUES (7,'باب الحاء المهملة','1037','0','0');
+INSERT INTO [t6060] VALUES (8,'باب الخاء المعجمة','1068','0','0');
+INSERT INTO [t6060] VALUES (9,'باب الدال المهملة','1127','0','0');
+INSERT INTO [t6060] VALUES (10,'باب الذال المعجمة','1148','0','0');
+INSERT INTO [t6060] VALUES (11,'باب الراء المهملة','1159','0','0');
+INSERT INTO [t6060] VALUES (12,'باب الزاي','1213','0','0');
+INSERT INTO [t6060] VALUES (13,'باب السين المهملة','1222','0','0');
+INSERT INTO [t6060] VALUES (14,'باب الشين المعجمة','1289','0','0');
+INSERT INTO [t6060] VALUES (15,'باب الصاد المهملة','1309','0','0');
+INSERT INTO [t6060] VALUES (16,'باب الضاد المعجمة','1347','0','0');
+INSERT INTO [t6060] VALUES (17,'باب الطاء المهملة','1352','0','0');
+INSERT INTO [t6060] VALUES (18,'باب العين المهملة','1365','0','0');
+INSERT INTO [t6060] VALUES (19,'باب الغين المعجمة','1403','0','0');
+INSERT INTO [t6060] VALUES (20,'باب الفاء','1409','0','0');
+INSERT INTO [t6060] VALUES (21,'باب القاف','1440','0','0');
+INSERT INTO [t6060] VALUES (22,'باب الكاف','1510','0','0');
+INSERT INTO [t6060] VALUES (23,'باب كان رسول الله - صلى الله عليه وسلم -، وكان النبي - صلى الله عليه وسلم - وهي الشمائل الشريفة','1525','22','0');
+INSERT INTO [t6060] VALUES (24,'باب اللام','1715','0','0');
+INSERT INTO [t6060] VALUES (25,'باب الميم','1834','0','0');
+INSERT INTO [t6060] VALUES (26,'باب " من " من هذا الحرف','1945','25','0');
+INSERT INTO [t6060] VALUES (27,'باب " من " من هذا الحرف','1950','25','0');
+INSERT INTO [t6060] VALUES (28,'باب النون','2253','0','0');
+INSERT INTO [t6060] VALUES (29,'باب المناهي','2270','28','0');
+INSERT INTO [t6060] VALUES (30,'باب الواو','2325','0','0');
+INSERT INTO [t6060] VALUES (31,'باب الهاء','2355','0','0');
+INSERT INTO [t6060] VALUES (32,'باب لا','2367','0','0');
+INSERT INTO [t6060] VALUES (33,'باب الياء','2518','0','0');
