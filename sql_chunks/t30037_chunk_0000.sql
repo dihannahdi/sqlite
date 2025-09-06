e96ceb8d5e7e1a12d76308a6a82ad71acaae11bc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:17 +0700

    Upload SQL chunk: t30037_chunk_0000.sql

---
+++ sql_chunks/t30037_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t30037
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t30037" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30037
+INSERT INTO [t30037] VALUES (1,'حديث أم زرع','9','0','0');
+INSERT INTO [t30037] VALUES (2,'وصية فاطمة ابنة رسول الله صلى الله عليه وسلم وعلى آله أجمعين','12','0','0');
