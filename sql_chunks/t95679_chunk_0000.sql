503fcd31812addbc8379fd71809806993cc1c61b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:06 +0700

    Upload SQL chunk: t95679_chunk_0000.sql

---
+++ sql_chunks/t95679_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t95679
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t95679" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95679
+INSERT INTO [t95679] VALUES (1,'نسبها ومولدها','1','0','0');
+INSERT INTO [t95679] VALUES (2,'نشأتها','1','0','0');
+INSERT INTO [t95679] VALUES (3,'زواجها','2','0','0');
+INSERT INTO [t95679] VALUES (4,'الفراق','2','0','0');
+INSERT INTO [t95679] VALUES (5,'صلح الحديبية','3','0','0');
+INSERT INTO [t95679] VALUES (6,'عمرة القضاء','4','0','0');
+INSERT INTO [t95679] VALUES (7,'خلوا بني الكفار عن سبيله','5','0','0');
+INSERT INTO [t95679] VALUES (8,'ميمونة تعرض نفسها على رسول الله','6','0','0');
+INSERT INTO [t95679] VALUES (9,'موقف النبي - صلى الله عليه وسلم -','6','0','0');
+INSERT INTO [t95679] VALUES (10,'أخرج عنا','7','0','0');
+INSERT INTO [t95679] VALUES (11,'في بيت النبوة','8','0','0');
+INSERT INTO [t95679] VALUES (12,'الوفاة','8','0','0');
