4a26112c6235d4ddf97a3a0b23ce858e6ecab919
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:51 +0700

    Upload SQL chunk: t21569_chunk_0000.sql

---
+++ sql_chunks/t21569_chunk_0000.sql
@@ -0,0 +1,42 @@
+-- Table: t21569
+-- Chunk: 1
+-- Rows: 35
+
+CREATE TABLE "t21569" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21569
+INSERT INTO [t21569] VALUES (1,'ابراهيم الفقي','1','0','0');
+INSERT INTO [t21569] VALUES (2,'الطريق إلى الامتياز','5','0','0');
+INSERT INTO [t21569] VALUES (3,'الارتباط بالله عز وجل','29','0','0');
+INSERT INTO [t21569] VALUES (4,'* التسامح:','30','0','0');
+INSERT INTO [t21569] VALUES (5,'* الحب في الله:','32','0','0');
+INSERT INTO [t21569] VALUES (6,'* العطاء:','35','0','0');
+INSERT INTO [t21569] VALUES (7,'* الإيمان بالله:','36','0','0');
+INSERT INTO [t21569] VALUES (8,'* الطاعة:','39','0','0');
+INSERT INTO [t21569] VALUES (9,'* الصلاة:','41','0','0');
+INSERT INTO [t21569] VALUES (10,'* الإخلاص:','44','0','0');
+INSERT INTO [t21569] VALUES (11,'* الوفاء:','46','0','0');
+INSERT INTO [t21569] VALUES (12,'* التوكل على الله:','50','0','0');
+INSERT INTO [t21569] VALUES (13,'* التفاؤل:','51','0','0');
+INSERT INTO [t21569] VALUES (14,'* الدعاء والذكر:','52','0','0');
+INSERT INTO [t21569] VALUES (15,'الأخلاق','55','0','0');
+INSERT INTO [t21569] VALUES (16,'* التعاطف:','64','0','0');
+INSERT INTO [t21569] VALUES (17,'* التبسم:','66','0','0');
+INSERT INTO [t21569] VALUES (18,'* العفو:','70','0','0');
+INSERT INTO [t21569] VALUES (19,'وقل اعملوا','83','0','0');
+INSERT INTO [t21569] VALUES (20,'* الرؤية الواضحة:','88','0','0');
+INSERT INTO [t21569] VALUES (21,'* الغاية:','90','0','0');
+INSERT INTO [t21569] VALUES (22,'* الغرض:','92','0','0');
+INSERT INTO [t21569] VALUES (23,'* الأهداف:','95','0','0');
+INSERT INTO [t21569] VALUES (24,'أولا: القراءة','99','0','0');
+INSERT INTO [t21569] VALUES (25,'ثانيا: الاستماع','101','0','0');
+INSERT INTO [t21569] VALUES (26,'ثالثا: المشاهدة','102','0','0');
+INSERT INTO [t21569] VALUES (27,'* الفعل الاستراتجي:','105','0','0');
+INSERT INTO [t21569] VALUES (28,'* الوعود الكاذبة:','123','0','0');
+INSERT INTO [t21569] VALUES (29,'* الكذب:','124','0','0');
+INSERT INTO [t21569] VALUES (30,'* عدم الصبر:','128','0','0');
+INSERT INTO [t21569] VALUES (31,'* الالتزام:','131','0','0');
+INSERT INTO [t21569] VALUES (32,'* الإصرار:','132','0','0');
+INSERT INTO [t21569] VALUES (33,'* الانضباط:','132','0','0');
+INSERT INTO [t21569] VALUES (34,'الدكتور الفقي في سطور','141','0','0');
+INSERT INTO [t21569] VALUES (35,'قالوا عن الدكتور/ إبراهيم الفقي','144','0','0');
