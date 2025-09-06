b89318d7489a84dbe07b41c2c8d8ca815e24d1e9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:55 +0700

    Upload SQL chunk: t22649_chunk_0000.sql

---
+++ sql_chunks/t22649_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t22649
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t22649" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22649
+INSERT INTO [t22649] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t22649] VALUES (2,'خطبة الحاجة','2','1','0');
+INSERT INTO [t22649] VALUES (3,'الأسباب الدافعة إلى العمل في هذا الكتاب','3','1','0');
+INSERT INTO [t22649] VALUES (4,'الكتب التي ألفت في التوسل','7','1','0');
+INSERT INTO [t22649] VALUES (5,'عملي في هذا الكتاب','8','1','0');
+INSERT INTO [t22649] VALUES (6,'لمحة عن حياة الإمام ابن تيمية','10','1','0');
+INSERT INTO [t22649] VALUES (7,'موضوع كتاب التوسل والوسيلة','17','1','0');
+INSERT INTO [t22649] VALUES (8,'منهج شيخ الإسلام في هذا الكتاب','21','1','0');
+INSERT INTO [t22649] VALUES (9,'اسم الكتاب','33','1','0');
+INSERT INTO [t22649] VALUES (10,'وصف المخطوطة','35','1','0');
+INSERT INTO [t22649] VALUES (11,'الرموز','36','1','0');
+INSERT INTO [t22649] VALUES (12,'النص المحقق','37','0','0');
+INSERT INTO [t22649] VALUES (13,'الفصل الثاني','53','12','0');
+INSERT INTO [t22649] VALUES (14,'الفصل الثالث','121','12','0');
+INSERT INTO [t22649] VALUES (15,'الفصل الرابع','267','12','0');
+INSERT INTO [t22649] VALUES (16,'الملحق','303','12','0');
+INSERT INTO [t22649] VALUES (17,'الفصل الخامس','363','12','0');
+INSERT INTO [t22649] VALUES (18,'الفهارس','375','0','0');
+INSERT INTO [t22649] VALUES (19,'١ - فهرس الآيات','376','18','0');
+INSERT INTO [t22649] VALUES (20,'٢ - فهرس الأحاديث','388','18','0');
+INSERT INTO [t22649] VALUES (21,'٣ - فهرس الآثار','399','18','0');
+INSERT INTO [t22649] VALUES (22,'٤ - فهرس الأعلام والكنى','402','18','0');
+INSERT INTO [t22649] VALUES (23,'أ - الأعلام','402','22','0');
+INSERT INTO [t22649] VALUES (24,'ب - الكنى','415','22','0');
+INSERT INTO [t22649] VALUES (25,'٥ - فهرس الطوائف والفرق','420','18','0');
+INSERT INTO [t22649] VALUES (26,'٦ - فهرس موارد المؤلف','422','18','0');
+INSERT INTO [t22649] VALUES (27,'٧ - فهرس مصادر ومراجع التحقيق والتخريج','425','18','0');
+INSERT INTO [t22649] VALUES (28,'٨ - فهرس الموضوعات','438','18','0');
+INSERT INTO [t22649] VALUES (29,'٩ - فهرس المقدمة','455','18','0');
