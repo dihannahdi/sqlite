af4241963e16606735608073c46c90f9a920c448
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:02 +0700

    Upload SQL chunk: t36406_chunk_0000.sql

---
+++ sql_chunks/t36406_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t36406
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t36406" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36406
+INSERT INTO [t36406] VALUES (1,'المقدمة','3','0','0');
+INSERT INTO [t36406] VALUES (2,'إجابة غير متوقعة','4','0','0');
+INSERT INTO [t36406] VALUES (3,'منحة في طيات المحنة','6','0','0');
+INSERT INTO [t36406] VALUES (4,'لا سفينة تحملنا ولا خشبة نتعلق بها','8','0','0');
+INSERT INTO [t36406] VALUES (5,'الفرج قريب فكيف نبلغه؟!','10','0','0');
+INSERT INTO [t36406] VALUES (6,'معركتنا تربوية','12','0','0');
+INSERT INTO [t36406] VALUES (7,'هيا بنا نعج إلى الله ونستغيث به','13','0','0');
+INSERT INTO [t36406] VALUES (8,'من الرابح؟!','15','0','0');
