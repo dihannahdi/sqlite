e53f6aa2b03e1011f27b942c7ad55d5eef5b6cf6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:17 +0700

    Upload SQL chunk: t3622_chunk_0000.sql

---
+++ sql_chunks/t3622_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t3622
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t3622" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3622
+INSERT INTO [t3622] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t3622] VALUES (2,'المبحث الأول: المؤلفات الأصولية المالكية','12','0','0');
+INSERT INTO [t3622] VALUES (3,'المبحث الثاني: المتون الأصولية الحنفية المشروحة من قبل علماء المالكية','31','0','0');
+INSERT INTO [t3622] VALUES (4,'مصادر ومراجع','38','0','0');
