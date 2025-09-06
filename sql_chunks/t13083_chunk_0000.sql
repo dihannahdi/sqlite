17e7760794c7488c9b4a29dc9d5f9a5d7e789e92
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:10:12 +0700

    Upload SQL chunk: t13083_chunk_0000.sql

---
+++ sql_chunks/t13083_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t13083
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t13083" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13083
+INSERT INTO [t13083] VALUES (1,'باب الكناية','91','0','0');
+INSERT INTO [t13083] VALUES (2,'باب الكناية ورد مفسرا','96','0','0');
+INSERT INTO [t13083] VALUES (3,'باب التشبيه','100','0','0');
+INSERT INTO [t13083] VALUES (4,'في نعت الجنة','120','0','0');
+INSERT INTO [t13083] VALUES (5,'في نعت النار','122','0','0');
+INSERT INTO [t13083] VALUES (6,'في نعت الدنيا','124','0','0');
+INSERT INTO [t13083] VALUES (7,'في نعت النساء','126','0','0');
+INSERT INTO [t13083] VALUES (8,'في نعت القبائل','129','0','0');
+INSERT INTO [t13083] VALUES (9,'في نعت الخيل','136','0','0');
+INSERT INTO [t13083] VALUES (10,'في نعت السحاب','144','0','0');
+INSERT INTO [t13083] VALUES (11,'باب من المثنى','148','0','0');
+INSERT INTO [t13083] VALUES (12,'باب من الألفاظ النادرة','155','0','0');
+INSERT INTO [t13083] VALUES (13,'النوادر','158','0','0');
