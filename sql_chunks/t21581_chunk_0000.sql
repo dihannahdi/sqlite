16977de94bd55bc0e053e2d38ae713a9b96e0b0f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:01 +0700

    Upload SQL chunk: t21581_chunk_0000.sql

---
+++ sql_chunks/t21581_chunk_0000.sql
@@ -0,0 +1,48 @@
+-- Table: t21581
+-- Chunk: 1
+-- Rows: 41
+
+CREATE TABLE "t21581" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21581
+INSERT INTO [t21581] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t21581] VALUES (2,'القسم الأول من كتاب (مصطلح الحديث)','3','0','0');
+INSERT INTO [t21581] VALUES (3,'مصطلح الحديث:','3','2','0');
+INSERT INTO [t21581] VALUES (4,'أقسام الخبر باعتبار طرق نقله إلينا:','4','2','0');
+INSERT INTO [t21581] VALUES (5,'الأول - المتواتر:','4','4','0');
+INSERT INTO [t21581] VALUES (6,'الثاني - الآحاد:','5','4','0');
+INSERT INTO [t21581] VALUES (7,'شرح تعريف الصحيح لذاته:','8','2','0');
+INSERT INTO [t21581] VALUES (8,'الجمع بين وصفي الصحة والحسن في حديث واحد:','11','2','0');
+INSERT INTO [t21581] VALUES (9,'منقطع السند:','11','2','0');
+INSERT INTO [t21581] VALUES (10,'التدليس:','13','2','0');
+INSERT INTO [t21581] VALUES (11,'المضطرب:','14','2','0');
+INSERT INTO [t21581] VALUES (12,'الإدراج في المتن:','15','2','0');
+INSERT INTO [t21581] VALUES (13,'الزيادة في الحديث:','16','2','0');
+INSERT INTO [t21581] VALUES (14,'اختصار الحديث:','17','2','0');
+INSERT INTO [t21581] VALUES (15,'رواية الحديث بالمعنى:','19','2','0');
+INSERT INTO [t21581] VALUES (16,'الموضوع:','20','2','0');
+INSERT INTO [t21581] VALUES (17,'الجرح والتعديل:','23','2','0');
+INSERT INTO [t21581] VALUES (18,'الجرح:','23','17','0');
+INSERT INTO [t21581] VALUES (19,'التعديل:','25','17','0');
+INSERT INTO [t21581] VALUES (20,'تعارض الجرح والتعديل:','26','17','0');
+INSERT INTO [t21581] VALUES (21,'القسم الثاني من كتاب (مصطلح الحديث)','28','0','0');
+INSERT INTO [t21581] VALUES (22,'أقسام الخبر باعتبار من يضاف إليه:','28','21','0');
+INSERT INTO [t21581] VALUES (23,'الصحابي:','31','21','0');
+INSERT INTO [t21581] VALUES (24,'المخضرم:','33','21','0');
+INSERT INTO [t21581] VALUES (25,'التابعي:','33','21','0');
+INSERT INTO [t21581] VALUES (26,'الإسناد:','34','21','0');
+INSERT INTO [t21581] VALUES (27,'المسلسل:','36','21','0');
+INSERT INTO [t21581] VALUES (28,'تحمل الحديث وأداؤه:','37','21','0');
+INSERT INTO [t21581] VALUES (29,'كتابة الحديث:','39','21','0');
+INSERT INTO [t21581] VALUES (30,'تدوين الحديث:','43','21','0');
+INSERT INTO [t21581] VALUES (31,'طرق تصنيف الحديث:','43','21','0');
+INSERT INTO [t21581] VALUES (32,'الأمهات الست','44','21','0');
+INSERT INTO [t21581] VALUES (33,'١ - صحيح البخاري:','44','32','0');
+INSERT INTO [t21581] VALUES (34,'٢ - صحيح مسلم:','46','32','0');
+INSERT INTO [t21581] VALUES (35,'٣ - سنن النسائي:','48','32','0');
+INSERT INTO [t21581] VALUES (36,'٤ - سنن أبي داود:','49','32','0');
+INSERT INTO [t21581] VALUES (37,'٥ - سنن الترمذي:','50','32','0');
+INSERT INTO [t21581] VALUES (38,'٦ - سنن ابن ماجه:','52','32','0');
+INSERT INTO [t21581] VALUES (39,'مسند الإمام أحمد','53','21','0');
+INSERT INTO [t21581] VALUES (40,'* أحمد بن حنبل:','54','39','0');
+INSERT INTO [t21581] VALUES (41,'آداب العالم والمتعلم','55','21','0');
