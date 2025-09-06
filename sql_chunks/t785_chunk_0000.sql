b6e18c13b40e78116189995ab63c2545d95cbf67
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:26 +0700

    Upload SQL chunk: t785_chunk_0000.sql

---
+++ sql_chunks/t785_chunk_0000.sql
@@ -0,0 +1,68 @@
+-- Table: t785
+-- Chunk: 1
+-- Rows: 61
+
+CREATE TABLE "t785" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t785
+INSERT INTO [t785] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t785] VALUES (2,'وصلى الله على سيدنا محمد وآله أجمعين.','1','1','0');
+INSERT INTO [t785] VALUES (3,'فضل الغناء','1','0','0');
+INSERT INTO [t785] VALUES (4,'القيان عند العرب','3','0','0');
+INSERT INTO [t785] VALUES (5,'الحيوان الطروب','3','0','0');
+INSERT INTO [t785] VALUES (6,'الموسيقار','4','0','0');
+INSERT INTO [t785] VALUES (7,'الغناء وأثره','4','0','0');
+INSERT INTO [t785] VALUES (8,'أخبار المغنين','4','0','0');
+INSERT INTO [t785] VALUES (9,'الطبقة الأولى','4','8','0');
+INSERT INTO [t785] VALUES (10,'حابل أبي دهبل','4','8','0');
+INSERT INTO [t785] VALUES (11,'أبو الخز','4','8','0');
+INSERT INTO [t785] VALUES (12,'الوليد بن يزيد','4','8','0');
+INSERT INTO [t785] VALUES (13,'عبد الله بن معاويةالباهلي','4','8','0');
+INSERT INTO [t785] VALUES (14,'الطبقة الثانية','4','8','0');
+INSERT INTO [t785] VALUES (15,'سياط','5','8','0');
+INSERT INTO [t785] VALUES (16,'زيد الأنصاري','5','8','0');
+INSERT INTO [t785] VALUES (17,'أشعب أبو العلاء بي جبير','5','8','0');
+INSERT INTO [t785] VALUES (18,'محمد بن الأبجر','5','8','0');
+INSERT INTO [t785] VALUES (19,'محمد بن الصامة','5','8','0');
+INSERT INTO [t785] VALUES (20,'الطبقة الرابعة من المغنين','5','8','0');
+INSERT INTO [t785] VALUES (21,'صباح الخياط','5','8','0');
+INSERT INTO [t785] VALUES (22,'عزور','6','8','0');
+INSERT INTO [t785] VALUES (23,'محمد نعجه','6','8','0');
+INSERT INTO [t785] VALUES (24,'سليمان','6','8','0');
+INSERT INTO [t785] VALUES (25,'ابن حودره','6','8','0');
+INSERT INTO [t785] VALUES (26,'الدلال','6','8','0');
+INSERT INTO [t785] VALUES (27,'طريفه بن الشوتري','6','8','0');
+INSERT INTO [t785] VALUES (28,'فند أبو زيد','6','8','0');
+INSERT INTO [t785] VALUES (29,'سجيه','6','8','0');
+INSERT INTO [t785] VALUES (30,'شبيب','7','8','0');
+INSERT INTO [t785] VALUES (31,'صعتر','7','8','0');
+INSERT INTO [t785] VALUES (32,'النساء القدماء','7','8','0');
+INSERT INTO [t785] VALUES (33,'سيرين','7','8','0');
+INSERT INTO [t785] VALUES (34,'قيان جبلة بن الأيهم الغساني','7','8','0');
+INSERT INTO [t785] VALUES (35,'عزة الميلا','7','8','0');
+INSERT INTO [t785] VALUES (36,'جميلة','7','8','0');
+INSERT INTO [t785] VALUES (37,'سلامة','7','8','0');
+INSERT INTO [t785] VALUES (38,'سلامة القس','8','8','0');
+INSERT INTO [t785] VALUES (39,'ريا','8','8','0');
+INSERT INTO [t785] VALUES (40,'حبابة','8','8','0');
+INSERT INTO [t785] VALUES (41,'سعده','8','8','0');
+INSERT INTO [t785] VALUES (42,'جارية امرأة ابن أبي عتيق','8','8','0');
+INSERT INTO [t785] VALUES (43,'الطبقة الثانية في المغنين','8','8','0');
+INSERT INTO [t785] VALUES (44,'يونس الكاتب','8','8','0');
+INSERT INTO [t785] VALUES (45,'الأبجر','9','8','0');
+INSERT INTO [t785] VALUES (46,'مدار','9','8','0');
+INSERT INTO [t785] VALUES (47,'ابن صاحب الوضوء','9','8','0');
+INSERT INTO [t785] VALUES (48,'سنان الكاتب','9','8','0');
+INSERT INTO [t785] VALUES (49,'عمر الوادي','10','8','0');
+INSERT INTO [t785] VALUES (50,'حنين بن بلوع','10','8','0');
+INSERT INTO [t785] VALUES (51,'دحمان الأشقر','10','8','0');
+INSERT INTO [t785] VALUES (52,'زكير بن يزيد','10','8','0');
+INSERT INTO [t785] VALUES (53,'خالد المروق','11','8','0');
+INSERT INTO [t785] VALUES (54,'هشام ابن المرية','11','8','0');
+INSERT INTO [t785] VALUES (55,'الدارمي','11','8','0');
+INSERT INTO [t785] VALUES (56,'ابن نجاد الأعمى','11','8','0');
+INSERT INTO [t785] VALUES (57,'ابن أبي قباحة','11','8','0');
+INSERT INTO [t785] VALUES (58,'الأفرك','11','8','0');
+INSERT INTO [t785] VALUES (59,'الدجاني','11','8','0');
+INSERT INTO [t785] VALUES (60,'أقوال في السماع والغناء والطرب','11','0','0');
+INSERT INTO [t785] VALUES (61,'الإيقاع في الغناء وأجناسه','12','0','0');
