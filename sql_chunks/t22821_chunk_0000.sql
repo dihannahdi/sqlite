eddb18d916fe679717ad9057829dd284baf5d471
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:39 +0700

    Upload SQL chunk: t22821_chunk_0000.sql

---
+++ sql_chunks/t22821_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t22821
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t22821" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22821
+INSERT INTO [t22821] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t22821] VALUES (2,'تخريج الحديث','4','0','0');
+INSERT INTO [t22821] VALUES (3,'إسناد الحديث','7','0','0');
+INSERT INTO [t22821] VALUES (4,'معنى الحديث','13','0','0');
+INSERT INTO [t22821] VALUES (5,'متى كان خلق آدم','20','0','0');
+INSERT INTO [t22821] VALUES (6,'لماذا لم يرد ذكر خلق السماوات في الحديث','25','0','0');
+INSERT INTO [t22821] VALUES (7,'خلق آدم في يوم الجمعة','27','0','0');
