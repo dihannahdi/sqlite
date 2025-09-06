867c9a2c465ba05a88244184c7f49c37e6b97ab4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:55 +0700

    Upload SQL chunk: t1138_chunk_0000.sql

---
+++ sql_chunks/t1138_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t1138
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t1138" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1138
+INSERT INTO [t1138] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t1138] VALUES (2,'مفهوم اليسر','7','0','0');
+INSERT INTO [t1138] VALUES (3,'أولا الدين الإسلامي قائم على اليسر والسماحة','8','0','0');
+INSERT INTO [t1138] VALUES (4,'ثانيا مرتكزات منهج التيسير في الشريعة الإسلامية','10','0','0');
+INSERT INTO [t1138] VALUES (5,'الرخصة','10','4','0');
+INSERT INTO [t1138] VALUES (6,'الأصل في الأشياء الإباحة','12','4','0');
+INSERT INTO [t1138] VALUES (7,'الخطأ والنسيان والإكراه','14','4','0');
+INSERT INTO [t1138] VALUES (8,'النهي عن الغلو في الدين','16','4','0');
+INSERT INTO [t1138] VALUES (9,'التوبة','18','4','0');
+INSERT INTO [t1138] VALUES (10,'ثالثا مجالات التيسير والسماحة في الإسلام','21','0','0');
+INSERT INTO [t1138] VALUES (11,'في العقيدة','21','10','0');
+INSERT INTO [t1138] VALUES (12,'في العبادات','23','10','0');
+INSERT INTO [t1138] VALUES (13,'في المعاملات','35','10','0');
+INSERT INTO [t1138] VALUES (14,'في العقوبات','38','10','0');
+INSERT INTO [t1138] VALUES (15,'رابعا القواعد الشرعية المستنبطة من النصوص الواردة في اليسر','45','0','0');
+INSERT INTO [t1138] VALUES (16,'خامسا آثار الابتعاد عن منهج التيسير','47','0','0');
+INSERT INTO [t1138] VALUES (17,'خلاصة البحث','55','0','0');
+INSERT INTO [t1138] VALUES (18,'المراجع','58','0','0');
