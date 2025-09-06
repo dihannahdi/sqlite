4fd16e0ba9209438058052f5f9f4454c6d9e1b20
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:15 +0700

    Upload SQL chunk: t1922_chunk_0000.sql

---
+++ sql_chunks/t1922_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t1922
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t1922" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1922
+INSERT INTO [t1922] VALUES (1,'الفتاوى الحديثية لابن حجر الهيتمي','1','0','0');
+INSERT INTO [t1922] VALUES (2,'تم الكتاب بعون الملك الوهاب','240','0','0');
