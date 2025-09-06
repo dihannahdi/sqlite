4b959152854c5266cfcb3347cd9db19419165a8c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:07 +0700

    Upload SQL chunk: t3052_chunk_0000.sql

---
+++ sql_chunks/t3052_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t3052
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t3052" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3052
+INSERT INTO [t3052] VALUES (1,'الدرس الأول مدارس التفسير','3','0','0');
+INSERT INTO [t3052] VALUES (2,'الدرس الثاني','30','0','0');
+INSERT INTO [t3052] VALUES (3,'الدرس الثالث','38','0','0');
+INSERT INTO [t3052] VALUES (4,'الدرس الرابع','48','0','0');
+INSERT INTO [t3052] VALUES (5,'الدرس الخامس','65','0','0');
+INSERT INTO [t3052] VALUES (6,'الدرس السادس','82','0','0');
+INSERT INTO [t3052] VALUES (7,'الدرس السابع','93','0','0');
+INSERT INTO [t3052] VALUES (8,'الدرس الثامن','107','0','0');
+INSERT INTO [t3052] VALUES (9,'الدرس التاسع','124','0','0');
+INSERT INTO [t3052] VALUES (10,'الدرس العاشر','137','0','0');
+INSERT INTO [t3052] VALUES (11,'الدرس الحادي عشر','150','0','0');
+INSERT INTO [t3052] VALUES (12,'الدرس الثاني عشر','162','0','0');
