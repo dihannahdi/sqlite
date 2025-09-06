9951ace6752cfd99a884988d6038cc887c2fb66a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:50 +0700

    Upload SQL chunk: t25793_chunk_0000.sql

---
+++ sql_chunks/t25793_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t25793
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t25793" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t25793
+INSERT INTO [t25793] VALUES (1,'الاختلاف في الأشربة','5','0','0');
+INSERT INTO [t25793] VALUES (2,'حجج المحرمين لجميع ما أسكر','14','0','0');
+INSERT INTO [t25793] VALUES (3,'حجج المحلين لما دون السكر','48','0','0');
+INSERT INTO [t25793] VALUES (4,'تبيين غلط الفرق بالغلو','104','0','0');
+INSERT INTO [t25793] VALUES (5,'عدل القول في الشراب','112','0','0');
