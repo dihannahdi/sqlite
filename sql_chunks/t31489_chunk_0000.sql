bfa1a83e455e7b0831d1837a4c3cad6a6cb5548a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:34 +0700

    Upload SQL chunk: t31489_chunk_0000.sql

---
+++ sql_chunks/t31489_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t31489
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t31489" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31489
+INSERT INTO [t31489] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t31489] VALUES (2,'حقوق الإنسان في الفكر الغربي','7','0','0');
+INSERT INTO [t31489] VALUES (3,'نقد النظرة الأوروبية لحقوق الإنسان','14','0','0');
+INSERT INTO [t31489] VALUES (4,'خصائص النظرة الإسلامية لحقوق الإنسان','31','0','0');
+INSERT INTO [t31489] VALUES (5,'الفرق بين النظر الإسلامي والفكر الغربي في فكرة الحق','45','0','0');
+INSERT INTO [t31489] VALUES (6,'حق الحياة وسلامة البدن والعقل والعرض','51','0','0');
+INSERT INTO [t31489] VALUES (7,'حرية الإنسان','56','0','0');
+INSERT INTO [t31489] VALUES (8,'المساواة','64','0','0');
+INSERT INTO [t31489] VALUES (9,'التكافل الاجتماعي','77','0','0');
+INSERT INTO [t31489] VALUES (10,'المملكة العربية السعودية وحقوق الإنسان','87','0','0');
+INSERT INTO [t31489] VALUES (11,'حقوق الإنسان في النظام الأساسي للحكم والأنظمة الأخرى في المملكة','92','0','0');
+INSERT INTO [t31489] VALUES (12,'الخاتمة','106','0','0');
