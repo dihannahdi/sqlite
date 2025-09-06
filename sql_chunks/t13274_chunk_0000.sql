37c05fc09feba6d133104d1bfc0eb34246b92c01
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:13:10 +0700

    Upload SQL chunk: t13274_chunk_0000.sql

---
+++ sql_chunks/t13274_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t13274
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t13274" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13274
+INSERT INTO [t13274] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t13274] VALUES (2,'باب "فعلت بفتح العين"','11','0','0');
+INSERT INTO [t13274] VALUES (3,'باب "فعلت بكسر العين"','138','0','0');
+INSERT INTO [t13274] VALUES (4,'باب "فعلت بغير ألف"','221','0','0');
+INSERT INTO [t13274] VALUES (5,'باب "فعل بضم الفاء"','300','0','0');
+INSERT INTO [t13274] VALUES (6,'باب "فعلت وفعلت باختلاف المعنى"','364','0','0');
+INSERT INTO [t13274] VALUES (7,'باب «فعلت وأفعلت باختلاف المعنى»','415','0','0');
