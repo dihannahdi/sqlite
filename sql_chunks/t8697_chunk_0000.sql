521303d9a54d583acfa2d659258e2902eba28dff
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:41 +0700

    Upload SQL chunk: t8697_chunk_0000.sql

---
+++ sql_chunks/t8697_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t8697
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t8697" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8697
+INSERT INTO [t8697] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8697] VALUES (2,'إعراب لا إله إلا الله','2','0','0');
+INSERT INTO [t8697] VALUES (3,'دراسة للاسم الواقع بعد إلا في الشواهد اللغوية','6','0','0');
+INSERT INTO [t8697] VALUES (4,'نسخة الرسالة الخطية','10','0','0');
+INSERT INTO [t8697] VALUES (5,'مصادر ومراجع','30','0','0');
