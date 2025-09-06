5838ca22c462f72a993ef344fe3dad8d6407b42e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:12 +0700

    Upload SQL chunk: t1921_chunk_0000.sql

---
+++ sql_chunks/t1921_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t1921
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t1921" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1921
+INSERT INTO [t1921] VALUES (1,'مفهوم المقاصد','1','0','0');
+INSERT INTO [t1921] VALUES (2,'أقسام المقاصد في الشريعة الإسلامية','2','0','0');
+INSERT INTO [t1921] VALUES (3,'المقصد العام','2','2','0');
+INSERT INTO [t1921] VALUES (4,'المقاصد الخاصة','3','2','0');
+INSERT INTO [t1921] VALUES (5,'مراتب المصالح البشرية','4','0','0');
+INSERT INTO [t1921] VALUES (6,'الضروريات','4','5','0');
+INSERT INTO [t1921] VALUES (7,'الحاجيات','5','5','0');
+INSERT INTO [t1921] VALUES (8,'التحسينيات','6','5','0');
+INSERT INTO [t1921] VALUES (9,'الكليات الخمس','7','0','0');
+INSERT INTO [t1921] VALUES (10,'أولا حفظ الدين','7','9','0');
+INSERT INTO [t1921] VALUES (11,'ثانيا حفظ النفس','14','9','0');
+INSERT INTO [t1921] VALUES (12,'ثالثا حفظ العقل','19','9','0');
+INSERT INTO [t1921] VALUES (13,'رابعا حفظ النسل','24','9','0');
+INSERT INTO [t1921] VALUES (14,'خامسا حفظ المال','27','9','0');
