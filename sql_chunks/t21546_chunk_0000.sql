1148cbb1f41ab05a2deef55179b9e971b079a5f8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:28 +0700

    Upload SQL chunk: t21546_chunk_0000.sql

---
+++ sql_chunks/t21546_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t21546
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t21546" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21546
+INSERT INTO [t21546] VALUES (1,'الجزء الأول','1','0','0');
+INSERT INTO [t21546] VALUES (2,'الجزء الثاني','138','0','0');
+INSERT INTO [t21546] VALUES (3,'الجزء الثالث','313','0','0');
