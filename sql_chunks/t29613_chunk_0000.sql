4746b240ce74017ef511ea66fe12dc48c333bda8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:37 +0700

    Upload SQL chunk: t29613_chunk_0000.sql

---
+++ sql_chunks/t29613_chunk_0000.sql
@@ -0,0 +1,50 @@
+-- Table: t29613
+-- Chunk: 1
+-- Rows: 43
+
+CREATE TABLE "t29613" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29613
+INSERT INTO [t29613] VALUES (1,'[مقدمات]','3','0','0');
+INSERT INTO [t29613] VALUES (2,'كلمة الدار','3','1','0');
+INSERT INTO [t29613] VALUES (3,'تصدير','4','1','0');
+INSERT INTO [t29613] VALUES (4,'ترجمة حياة المؤلف','7','1','0');
+INSERT INTO [t29613] VALUES (5,'مقدمة المؤلف','11','1','0');
+INSERT INTO [t29613] VALUES (6,'ترتيب الكتاب','70','1','0');
+INSERT INTO [t29613] VALUES (7,'الرموز الواردة في الكتاب','78','1','0');
+INSERT INTO [t29613] VALUES (8,'جداول الأوزان والمكاييل','83','1','0');
+INSERT INTO [t29613] VALUES (9,'ما عربه المؤلف الشيخ أحمد رضا','89','1','0');
+INSERT INTO [t29613] VALUES (10,'ما عربه مجمع اللغة العربية الملكي بمصر','98','1','0');
+INSERT INTO [t29613] VALUES (11,'ما عربه المجمع العربي بدمشق','112','1','0');
+INSERT INTO [t29613] VALUES (12,'ما عربه مجمع مصر الأول','119','1','0');
+INSERT INTO [t29613] VALUES (13,'ما عربه المجمع الثاني المصري','120','1','0');
+INSERT INTO [t29613] VALUES (14,'أوضاع نشرها أحمد تيمور اللغوي المصري','124','1','0');
+INSERT INTO [t29613] VALUES (15,'أوضاع الأب أنستاس الكرملي','126','1','0');
+INSERT INTO [t29613] VALUES (16,'أ','129','0','0');
+INSERT INTO [t29613] VALUES (17,'ب','229','0','0');
+INSERT INTO [t29613] VALUES (18,'ت','379','0','0');
+INSERT INTO [t29613] VALUES (19,'ث','418','0','0');
+INSERT INTO [t29613] VALUES (20,'ج','458','0','0');
+INSERT INTO [t29613] VALUES (21,'ح','609','0','0');
+INSERT INTO [t29613] VALUES (22,'خ','817','0','0');
+INSERT INTO [t29613] VALUES (23,'د','970','0','0');
+INSERT INTO [t29613] VALUES (24,'ذ','1086','0','0');
+INSERT INTO [t29613] VALUES (25,'ر','1122','0','0');
+INSERT INTO [t29613] VALUES (26,'ز','1296','0','0');
+INSERT INTO [t29613] VALUES (27,'س','1376','0','0');
+INSERT INTO [t29613] VALUES (28,'ش','1551','0','0');
+INSERT INTO [t29613] VALUES (29,'ص','1700','0','0');
+INSERT INTO [t29613] VALUES (30,'ض','1814','0','0');
+INSERT INTO [t29613] VALUES (31,'ط','1868','0','0');
+INSERT INTO [t29613] VALUES (32,'ظ','1946','0','0');
+INSERT INTO [t29613] VALUES (33,'ع','1965','0','0');
+INSERT INTO [t29613] VALUES (34,'غ','2220','0','0');
+INSERT INTO [t29613] VALUES (35,'ف','2307','0','0');
+INSERT INTO [t29613] VALUES (36,'ق','2437','0','0');
+INSERT INTO [t29613] VALUES (37,'ك','2650','0','0');
+INSERT INTO [t29613] VALUES (38,'ل','2778','0','0');
+INSERT INTO [t29613] VALUES (39,'م','2881','0','0');
+INSERT INTO [t29613] VALUES (40,'ن','3020','0','0');
+INSERT INTO [t29613] VALUES (41,'هـ','3230','0','0');
+INSERT INTO [t29613] VALUES (42,'و','3335','0','0');
+INSERT INTO [t29613] VALUES (43,'ي','3473','0','0');
