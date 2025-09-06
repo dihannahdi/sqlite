1535bf4806ac08857e4e455c830ba23ba147310b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:17:51 +0700

    Upload SQL chunk: t38194_chunk_0000.sql

---
+++ sql_chunks/t38194_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t38194
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t38194" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t38194
+INSERT INTO [t38194] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t38194] VALUES (2,'المقدمة','2','0','0');
+INSERT INTO [t38194] VALUES (3,'مصنفات أبي العباس الأصم','3','0','0');
+INSERT INTO [t38194] VALUES (4,'الثاني والثالث من حديث أبي العباس الأصم','3','3','0');
+INSERT INTO [t38194] VALUES (5,'[جزء الأصم]','320','3','0');
+INSERT INTO [t38194] VALUES (6,'مجلسان من أمالي أبي العباس الأصم','384','3','0');
+INSERT INTO [t38194] VALUES (7,'المجلس الأول','385','6','0');
+INSERT INTO [t38194] VALUES (8,'مجلس آخر','399','6','0');
+INSERT INTO [t38194] VALUES (9,'جزء فيه من حديث أبي العباس الأصم','414','3','0');
+INSERT INTO [t38194] VALUES (10,'مصنفات إسماعيل الصفار','523','0','0');
+INSERT INTO [t38194] VALUES (11,'الجزء فيه حديث إسماعيل الصفار','523','10','0');
+INSERT INTO [t38194] VALUES (12,'[جزء إسماعيل الصفار]','593','10','0');
+INSERT INTO [t38194] VALUES (13,'[السادس من حديث الصفار]','632','10','0');
