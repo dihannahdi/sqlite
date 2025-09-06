8920832459eddd1805749827687f79e82b623abb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:11 +0700

    Upload SQL chunk: t36559_chunk_0000.sql

---
+++ sql_chunks/t36559_chunk_0000.sql
@@ -0,0 +1,60 @@
+-- Table: t36559
+-- Chunk: 1
+-- Rows: 53
+
+CREATE TABLE "t36559" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36559
+INSERT INTO [t36559] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36559] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t36559] VALUES (3,'أخي الحبيب ... أين نحن من هؤلاء','7','2','0');
+INSERT INTO [t36559] VALUES (4,'أخي الحبيب','9','2','0');
+INSERT INTO [t36559] VALUES (5,'أخي المسلم','11','2','0');
+INSERT INTO [t36559] VALUES (6,'أخي الحبيب','13','2','0');
+INSERT INTO [t36559] VALUES (7,'أخي الحبيب','16','2','0');
+INSERT INTO [t36559] VALUES (8,'أخي المسلم','19','2','0');
+INSERT INTO [t36559] VALUES (9,'أخي الحبيب','19','2','0');
+INSERT INTO [t36559] VALUES (10,'أخي الحبيب','21','2','0');
+INSERT INTO [t36559] VALUES (11,'أخي الحبيب','23','2','0');
+INSERT INTO [t36559] VALUES (12,'أخي الحبيب .. أين نحن من هؤلاء','23','2','0');
+INSERT INTO [t36559] VALUES (13,'أخي الحبيب','24','2','0');
+INSERT INTO [t36559] VALUES (14,'أخي المسلم','24','2','0');
+INSERT INTO [t36559] VALUES (15,'أخي','26','2','0');
+INSERT INTO [t36559] VALUES (16,'أخي المسلم','28','2','0');
+INSERT INTO [t36559] VALUES (17,'أخي الحبيب','29','2','0');
+INSERT INTO [t36559] VALUES (18,'أخي الحبيب','31','2','0');
+INSERT INTO [t36559] VALUES (19,'أخي المسلم','32','2','0');
+INSERT INTO [t36559] VALUES (20,'أخي المسلم: هذه الدنيا','33','2','0');
+INSERT INTO [t36559] VALUES (21,'أخي الحبيب','35','2','0');
+INSERT INTO [t36559] VALUES (22,'أخي الحبيب','38','2','0');
+INSERT INTO [t36559] VALUES (23,'أخي المسلم','39','2','0');
+INSERT INTO [t36559] VALUES (24,'أخي الحبيب','40','2','0');
+INSERT INTO [t36559] VALUES (25,'أخي الحبيب .. أين نحن من هؤلاء','41','2','0');
+INSERT INTO [t36559] VALUES (26,'أخي .. ألا عودة من قريب ..','42','2','0');
+INSERT INTO [t36559] VALUES (27,'أخي الحبيب','43','2','0');
+INSERT INTO [t36559] VALUES (28,'أخي الحبيب','44','2','0');
+INSERT INTO [t36559] VALUES (29,'أخي','46','2','0');
+INSERT INTO [t36559] VALUES (30,'أخي الحبيب','46','2','0');
+INSERT INTO [t36559] VALUES (31,'أخي الحبيب','48','2','0');
+INSERT INTO [t36559] VALUES (32,'أخي الحبيب','49','2','0');
+INSERT INTO [t36559] VALUES (33,'أخي الحبيب','50','2','0');
+INSERT INTO [t36559] VALUES (34,'أخي الحبيب','51','2','0');
+INSERT INTO [t36559] VALUES (35,'أخي الحبيب','54','2','0');
+INSERT INTO [t36559] VALUES (36,'أخي الحبيب','55','2','0');
+INSERT INTO [t36559] VALUES (37,'أخي الحبيب','57','2','0');
+INSERT INTO [t36559] VALUES (38,'أخي المسلم','59','2','0');
+INSERT INTO [t36559] VALUES (39,'أخي الحبيب','61','2','0');
+INSERT INTO [t36559] VALUES (40,'أخي الحبيب','61','2','0');
+INSERT INTO [t36559] VALUES (41,'أخي المسلم','61','2','0');
+INSERT INTO [t36559] VALUES (42,'أخي المسلم','65','2','0');
+INSERT INTO [t36559] VALUES (43,'أخي الحبيب','67','2','0');
+INSERT INTO [t36559] VALUES (44,'أخي المسلم','69','2','0');
+INSERT INTO [t36559] VALUES (45,'أخي الحبيب ... أين نحن من هؤلاء','70','2','0');
+INSERT INTO [t36559] VALUES (46,'أخي المسلم','78','2','0');
+INSERT INTO [t36559] VALUES (47,'ويا أخي الحبيب','79','2','0');
+INSERT INTO [t36559] VALUES (48,'أخي الحبيب: أين نحن من هؤلاء','79','2','0');
+INSERT INTO [t36559] VALUES (49,'أخي المسلم','81','2','0');
+INSERT INTO [t36559] VALUES (50,'أخي المسلم','82','2','0');
+INSERT INTO [t36559] VALUES (51,'أخي الحبيب','82','2','0');
+INSERT INTO [t36559] VALUES (52,'خاتمة','83','0','0');
+INSERT INTO [t36559] VALUES (53,'المصادر','84','0','0');
