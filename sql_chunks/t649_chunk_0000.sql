4bccf8dcd05468a389499349d1da5718a43b359c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:09 +0700

    Upload SQL chunk: t649_chunk_0000.sql

---
+++ sql_chunks/t649_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t649
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t649" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t649
+INSERT INTO [t649] VALUES (1,'الفصل الأول آداب العالم في علمه','1','0','0');
+INSERT INTO [t649] VALUES (2,'النوع الأول:','1','1','0');
+INSERT INTO [t649] VALUES (3,'الثاني:','1','1','0');
+INSERT INTO [t649] VALUES (4,'الثالث:','1','1','0');
+INSERT INTO [t649] VALUES (5,'الرابع:','2','1','0');
+INSERT INTO [t649] VALUES (6,'الخامس:','2','1','0');
+INSERT INTO [t649] VALUES (7,'السادس:','3','1','0');
+INSERT INTO [t649] VALUES (8,'السابع:','3','1','0');
+INSERT INTO [t649] VALUES (9,'الثامن:','3','1','0');
+INSERT INTO [t649] VALUES (10,'التاسع:','4','1','0');
+INSERT INTO [t649] VALUES (11,'العاشر:','4','1','0');
+INSERT INTO [t649] VALUES (12,'الحادي عشر:','5','1','0');
+INSERT INTO [t649] VALUES (13,'الثاني عشر:','5','1','0');
+INSERT INTO [t649] VALUES (14,'الفصل الثاني في آداب العالم في درسه','5','0','0');
+INSERT INTO [t649] VALUES (15,'الأول:','5','14','0');
+INSERT INTO [t649] VALUES (16,'الثاني:','6','14','0');
+INSERT INTO [t649] VALUES (17,'الثالث:','7','14','0');
+INSERT INTO [t649] VALUES (18,'الرابع:','7','14','0');
+INSERT INTO [t649] VALUES (19,'الخامس:','7','14','0');
+INSERT INTO [t649] VALUES (20,'السادس:','7','14','0');
+INSERT INTO [t649] VALUES (21,'السابع:','7','14','0');
+INSERT INTO [t649] VALUES (22,'الثامن:','8','14','0');
+INSERT INTO [t649] VALUES (23,'التاسع:','8','14','0');
+INSERT INTO [t649] VALUES (24,'العاشر:','8','14','0');
+INSERT INTO [t649] VALUES (25,'الحادي عشر:','8','14','0');
+INSERT INTO [t649] VALUES (26,'الثاني عشر:','9','14','0');
+INSERT INTO [t649] VALUES (27,'الفصل الثالث في آداب العالم مع طلبته','9','0','0');
+INSERT INTO [t649] VALUES (28,'الفصل الرابع في آداب المتعلم في نفسه','12','0','0');
+INSERT INTO [t649] VALUES (29,'الفصل الخامس في آداب المتعلم مع شيخه وقدوته','14','0','0');
+INSERT INTO [t649] VALUES (30,'الفصل السادس في آداب المتعلم في درسه وقراءته في الحلقة وما يعتمد فيها الشيخ والرفقة','18','0','0');
+INSERT INTO [t649] VALUES (31,'الفصل السابع في الآداب مع الكتب التي هي آلة العلم','22','0','0');
+INSERT INTO [t649] VALUES (32,'خاتمة في ذكر ما ينبغي لأهل البيت النبوي من الآداب الزكية والأخلاق السنية والهمم العلية','24','0','0');
