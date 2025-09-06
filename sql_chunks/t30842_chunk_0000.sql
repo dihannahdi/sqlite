a26de8aaf0d41fe33d436abb1258303e953ebcec
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:07 +0700

    Upload SQL chunk: t30842_chunk_0000.sql

---
+++ sql_chunks/t30842_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t30842
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t30842" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30842
+INSERT INTO [t30842] VALUES (1,'صفات المنافقين','1','0','0');
+INSERT INTO [t30842] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t30842] VALUES (3,'فإن النفاق هو الداء العضال','1','0','0');
+INSERT INTO [t30842] VALUES (4,'وهو نوعان: أكبر، وأصغر','1','0','0');
