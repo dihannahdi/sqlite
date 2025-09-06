8d9d728c191d481e82c3be745148eac943b2ff10
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:39 +0700

    Upload SQL chunk: t26776_chunk_0000.sql

---
+++ sql_chunks/t26776_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t26776
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t26776" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26776
+INSERT INTO [t26776] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26776] VALUES (2,'جزء فيه مسلسل العيدين وغير ذلك','2','0','0');
+INSERT INTO [t26776] VALUES (3,'مسلسل العيدين للكتاني','3','0','0');
+INSERT INTO [t26776] VALUES (4,'قال الشيخ إبراهيم الطبري','29','0','0');
+INSERT INTO [t26776] VALUES (5,'مسلسل العيدين للخطيب','31','0','0');
