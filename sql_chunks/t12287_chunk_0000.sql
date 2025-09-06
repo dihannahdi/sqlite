065fd577284e771faf9a337f4ecff895e97b7af4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:03:57 +0700

    Upload SQL chunk: t12287_chunk_0000.sql

---
+++ sql_chunks/t12287_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t12287
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t12287" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12287
+INSERT INTO [t12287] VALUES (1,'مجلس من امالي أبي بكر الشيرازي - مخطوط','2','0','0');
