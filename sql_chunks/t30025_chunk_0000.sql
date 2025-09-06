1252a344bf2bea8cd8f79d16bc2ec977d1905704
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:15 +0700

    Upload SQL chunk: t30025_chunk_0000.sql

---
+++ sql_chunks/t30025_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t30025
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t30025" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30025
+INSERT INTO [t30025] VALUES (1,'مجلس أملي يوم الجمعة لأربع بقين من شوال سنة تسع وخمسين وأربع مائة','14','0','0');
+INSERT INTO [t30025] VALUES (2,'مجلس آخر أملي يوم الجمعة ثالث المحرم سنة تسع وخمسين وأربع مائة','27','0','0');
+INSERT INTO [t30025] VALUES (3,'مجلس أملي يوم الجمعة عاشر المحرم سنة تسع وخمسين','39','0','0');
