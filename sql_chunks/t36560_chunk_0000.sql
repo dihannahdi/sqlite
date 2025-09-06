9db43d881790733bd5e98581978ad47d39c8c006
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:12 +0700

    Upload SQL chunk: t36560_chunk_0000.sql

---
+++ sql_chunks/t36560_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t36560
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t36560" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36560
+INSERT INTO [t36560] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36560] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t36560] VALUES (3,'هؤلاء هم خصماؤك','4','0','0');
+INSERT INTO [t36560] VALUES (4,'من أنواع الظلم','7','0','0');
+INSERT INTO [t36560] VALUES (5,'كيف هو المخرج','20','0','0');
+INSERT INTO [t36560] VALUES (6,'الخاتمة','22','0','0');
