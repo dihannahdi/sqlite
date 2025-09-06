71398347a8378ebae19b545cde04ebc8f4001c7b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:47 +0700

    Upload SQL chunk: t9893_chunk_0000.sql

---
+++ sql_chunks/t9893_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9893
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9893" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9893
+INSERT INTO [t9893] VALUES (1,'بنو إسرائيل في ضوء الإسلام','1','0','0');
+INSERT INTO [t9893] VALUES (2,'بقلم: الشيخ محمد أمين سليم','1','0','0');
