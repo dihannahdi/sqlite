b385ecd87406dc35a092d87bfcebb7e4de982e9d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:45:12 +0700

    Upload SQL chunk: t8269_chunk_0000.sql

---
+++ sql_chunks/t8269_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t8269
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t8269" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8269
+INSERT INTO [t8269] VALUES (1,'باب قبلة الخد','17','0','0');
+INSERT INTO [t8269] VALUES (2,'باب قبلة الفم','20','0','0');
+INSERT INTO [t8269] VALUES (3,'باب قبلة البطن والجسد','25','0','0');
+INSERT INTO [t8269] VALUES (4,'باب قبلة السرة','29','0','0');
+INSERT INTO [t8269] VALUES (5,'قبلة الميت','31','0','0');
+INSERT INTO [t8269] VALUES (6,'باب قبلة الشيء يمس يد النبي صلى الله تعالى عليه وسلم','40','0','0');
+INSERT INTO [t8269] VALUES (7,'باب قبلة ما بين العينين','44','0','0');
+INSERT INTO [t8269] VALUES (8,'تقبيل الرجل ابنته الكبيرة','47','0','0');
+INSERT INTO [t8269] VALUES (9,'تقبيل يد الرجل ورجله ورأسه','50','0','0');
