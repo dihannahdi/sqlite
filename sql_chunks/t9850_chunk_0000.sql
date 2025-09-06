3b51e8b7ee851fd63226f0c3f2e7b4c7ceefabea
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:17 +0700

    Upload SQL chunk: t9850_chunk_0000.sql

---
+++ sql_chunks/t9850_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t9850
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t9850" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9850
+INSERT INTO [t9850] VALUES (1,'مقدمة الطبعة الثالثة','2','0','0');
+INSERT INTO [t9850] VALUES (2,'سبب التأليف','3','0','0');
+INSERT INTO [t9850] VALUES (3,'المقدمة','30','0','0');
+INSERT INTO [t9850] VALUES (4,'تمهيد','31','0','0');
+INSERT INTO [t9850] VALUES (5,'الوهابية أو الوهبية. . من هم؟ !','50','0','0');
+INSERT INTO [t9850] VALUES (6,'الاستعمار. . ومواجهة الدعوة','55','0','0');
+INSERT INTO [t9850] VALUES (7,'الدولة العثمانية.... والدعوة','67','0','0');
+INSERT INTO [t9850] VALUES (8,'شبهات الخصوم','74','0','0');
+INSERT INTO [t9850] VALUES (9,'عودة لإثارة الشبهات','79','0','0');
+INSERT INTO [t9850] VALUES (10,'خصوم الدعوة من داخل المنطقة','82','0','0');
+INSERT INTO [t9850] VALUES (11,'الهدف من التسمية','88','0','0');
+INSERT INTO [t9850] VALUES (12,'من نتائج الخصومة','95','0','0');
+INSERT INTO [t9850] VALUES (13,'وبعد','105','0','0');
+INSERT INTO [t9850] VALUES (14,'أولا: رسالة الشيخ محمد بن عبد الوهاب إلى أهل القصيم لما سألوه عن عقيدته','108','13','0');
+INSERT INTO [t9850] VALUES (15,'الملحق','108','0','0');
+INSERT INTO [t9850] VALUES (16,'ثانيا: رسالة لسليمان فيها البشارة برجوعه عن مذهب الأول','113','15','0');
+INSERT INTO [t9850] VALUES (17,'ثالثا: رسالة الشيخ محمد عبد الوهاب لأهالي المغرب','118','15','0');
+INSERT INTO [t9850] VALUES (18,'رابعا: دور الملك عبد العزيز في تصحيح الخطأ','124','15','0');
+INSERT INTO [t9850] VALUES (19,'المصادر والمراجع','129','0','0');
