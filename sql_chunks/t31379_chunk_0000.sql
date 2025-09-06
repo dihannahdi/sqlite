e4033710f1d6af687bb5e68980826fb2bfb034e2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:24 +0700

    Upload SQL chunk: t31379_chunk_0000.sql

---
+++ sql_chunks/t31379_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t31379
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t31379" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31379
+INSERT INTO [t31379] VALUES (1,'مقدمة المصنف','1','0','0');
+INSERT INTO [t31379] VALUES (2,'إشارة النسيم','6','0','0');
+INSERT INTO [t31379] VALUES (3,'إشارة الورد','9','0','0');
+INSERT INTO [t31379] VALUES (4,'إشارة المرسين','11','0','0');
+INSERT INTO [t31379] VALUES (5,'إشار البان','13','0','0');
+INSERT INTO [t31379] VALUES (6,'إشارة النرجس','16','0','0');
+INSERT INTO [t31379] VALUES (7,'إشارة اللينوفر','18','0','0');
+INSERT INTO [t31379] VALUES (8,'إشارة البنفسج','20','0','0');
+INSERT INTO [t31379] VALUES (9,'إشارة المنثور','22','0','0');
+INSERT INTO [t31379] VALUES (10,'إشارة الياسمين','24','0','0');
+INSERT INTO [t31379] VALUES (11,'إشارة الريحان','25','0','0');
+INSERT INTO [t31379] VALUES (12,'إشارة الأقحوان','26','0','0');
+INSERT INTO [t31379] VALUES (13,'إشارة الشقيق','30','0','0');
+INSERT INTO [t31379] VALUES (14,'إشارة السحاب','32','0','0');
+INSERT INTO [t31379] VALUES (15,'إشارات الأطيار وأولها إشارة الهزار','34','0','0');
+INSERT INTO [t31379] VALUES (16,'إشارة الباز','35','0','0');
+INSERT INTO [t31379] VALUES (17,'إشارة الحمامة','37','0','0');
+INSERT INTO [t31379] VALUES (18,'إشارة الخطاف','39','0','0');
+INSERT INTO [t31379] VALUES (19,'إشارة البوم','41','0','0');
+INSERT INTO [t31379] VALUES (20,'إشارة الطاووس','43','0','0');
+INSERT INTO [t31379] VALUES (21,'إشارة الدره','46','0','0');
+INSERT INTO [t31379] VALUES (22,'إشارة الخفاش','48','0','0');
+INSERT INTO [t31379] VALUES (23,'إشارة الديك','51','0','0');
