cc6ae26a0887751a880f5a9626618d001390f8a6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:44 +0700

    Upload SQL chunk: t5940_chunk_0000.sql

---
+++ sql_chunks/t5940_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t5940
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t5940" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5940
+INSERT INTO [t5940] VALUES (1,'تنبيه','1','0','0');
+INSERT INTO [t5940] VALUES (2,'مقدمة','2','0','0');
+INSERT INTO [t5940] VALUES (3,'الخبر إما أن يكون له','3','0','0');
+INSERT INTO [t5940] VALUES (4,'أنواع الحديث','4','0','0');
+INSERT INTO [t5940] VALUES (5,'الغرابة','4','4','0');
+INSERT INTO [t5940] VALUES (6,'الفرد النسبي','5','4','0');
+INSERT INTO [t5940] VALUES (7,'المقبول','6','4','0');
+INSERT INTO [t5940] VALUES (8,'المردود','7','4','0');
+INSERT INTO [t5940] VALUES (9,'السقط','8','4','0');
+INSERT INTO [t5940] VALUES (10,'الطعن','9','4','0');
+INSERT INTO [t5940] VALUES (11,'الوهم','10','4','0');
+INSERT INTO [t5940] VALUES (12,'المخالفة','11','4','0');
+INSERT INTO [t5940] VALUES (13,'الجهالة','12','4','0');
+INSERT INTO [t5940] VALUES (14,'سوء الحفظ','13','4','0');
+INSERT INTO [t5940] VALUES (15,'الاسناد','14','0','0');
+INSERT INTO [t5940] VALUES (16,'صيغ الأداء','15','0','0');
+INSERT INTO [t5940] VALUES (17,'الرواة','16','0','0');
+INSERT INTO [t5940] VALUES (18,'خاتمة','17','0','0');
+INSERT INTO [t5940] VALUES (19,'فصل معرفة الكنى وغيرها','18','0','0');
