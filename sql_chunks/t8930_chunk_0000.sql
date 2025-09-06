c08c857e24935692f1743c58054d76b27afd71b2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:44 +0700

    Upload SQL chunk: t8930_chunk_0000.sql

---
+++ sql_chunks/t8930_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t8930
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t8930" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8930
+INSERT INTO [t8930] VALUES (1,'حقيقة المسكر','1','0','0');
+INSERT INTO [t8930] VALUES (2,'السكر ضد الفطرة السليمة','4','0','0');
+INSERT INTO [t8930] VALUES (3,'ما هو الإدمان الكحولي؟','6','0','0');
+INSERT INTO [t8930] VALUES (4,'الأضرار:','8','0','0');
+INSERT INTO [t8930] VALUES (5,'أسباب الإدمان','11','0','0');
+INSERT INTO [t8930] VALUES (6,'إزالة هذه المغاطة','13','0','0');
+INSERT INTO [t8930] VALUES (7,'العلاج','16','0','0');
+INSERT INTO [t8930] VALUES (8,'مصادر ومراجع','23','0','0');
