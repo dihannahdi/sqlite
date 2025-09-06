d4c85b30d39de63248184e1c3a2eab6cc2e3f287
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:43 +0700

    Upload SQL chunk: t464_chunk_0000.sql

---
+++ sql_chunks/t464_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t464
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t464" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t464
+INSERT INTO [t464] VALUES (1,'سبب التاليف','2','0','0');
+INSERT INTO [t464] VALUES (2,'الأمور التي استدل بها الفقهاء على جوازه','3','0','0');
+INSERT INTO [t464] VALUES (3,'النظر في هذه الأمور واحدا واحدا','4','0','0');
