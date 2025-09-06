d056e286ebea38ef3caf02dcdff03f8f7599f75d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:49 +0700

    Upload SQL chunk: t11158_chunk_0000.sql

---
+++ sql_chunks/t11158_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t11158
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t11158" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11158
+INSERT INTO [t11158] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11158] VALUES (2,'المسائل الاربع','8','0','0');
+INSERT INTO [t11158] VALUES (3,'العلم','8','2','0');
+INSERT INTO [t11158] VALUES (4,'العمل بالعلم، دليله','12','2','0');
+INSERT INTO [t11158] VALUES (5,'الدعوة إلى توحيد الله وطاعته','14','2','0');
+INSERT INTO [t11158] VALUES (6,'الصبر على الأذى في الدعوة إلى الله','16','2','0');
+INSERT INTO [t11158] VALUES (7,'المسائل الثالث','23','0','0');
+INSERT INTO [t11158] VALUES (8,'*','23','7','0');
+INSERT INTO [t11158] VALUES (9,'توحيد الربوبية وأدلتة','24','7','0');
+INSERT INTO [t11158] VALUES (10,'توحيد الألوهية','30','7','0');
+INSERT INTO [t11158] VALUES (11,'الولاء والبراء','32','7','0');
+INSERT INTO [t11158] VALUES (12,'الأصول الثلاثة','48','0','0');
+INSERT INTO [t11158] VALUES (13,'*','48','12','0');
+INSERT INTO [t11158] VALUES (14,'الأصل الأول: معرفة العبد ربه','50','12','0');
+INSERT INTO [t11158] VALUES (15,'*','50','14','0');
+INSERT INTO [t11158] VALUES (16,'آيات الله نوعان','53','14','0');
+INSERT INTO [t11158] VALUES (17,'أنواع العبادة التي أمر الله بها','67','14','0');
+INSERT INTO [t11158] VALUES (18,'الأصل الثاني: معرفة دين الإسلام بالأدلة','99','12','0');
+INSERT INTO [t11158] VALUES (19,'المرتبة الأولى: الإسلام','99','18','0');
+INSERT INTO [t11158] VALUES (20,'المرتبة الثانية: الإيمان','122','18','0');
+INSERT INTO [t11158] VALUES (21,'المرتبة الثالثة: الإحسان','134','18','0');
+INSERT INTO [t11158] VALUES (22,'الأصل الثالث: معرفة نبينا محمد صلى الله عليه وسلم','150','12','0');
+INSERT INTO [t11158] VALUES (23,'اسمه ونسبة','150','22','0');
+INSERT INTO [t11158] VALUES (24,'تعريف الهجرة','163','22','0');
+INSERT INTO [t11158] VALUES (25,'فرض بقية شرائع الإسلام','174','22','0');
+INSERT INTO [t11158] VALUES (26,'وفاته صلى الله عليه وسلم','178','22','0');
+INSERT INTO [t11158] VALUES (27,'رؤوس الطواغيت','196','22','0');
