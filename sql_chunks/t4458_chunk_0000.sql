1df3329f24a099500f9ff6d33e62cc35e7e1e951
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:15 +0700

    Upload SQL chunk: t4458_chunk_0000.sql

---
+++ sql_chunks/t4458_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t4458
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t4458" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4458
+INSERT INTO [t4458] VALUES (1,'{والذين يتوفون منكم ويذرون أزواجا يتربصن بأنفسهن أربعة أشهر وعشرا}','1','0','0');
+INSERT INTO [t4458] VALUES (2,'حتى إذا أتيا أهل قرية استطعما أهلها','4','0','0');
