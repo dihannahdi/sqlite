0a6c2b8788bb64f17767346746220def804ff15a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:06 +0700

    Upload SQL chunk: t7573_chunk_0000.sql

---
+++ sql_chunks/t7573_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t7573
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t7573" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7573
+INSERT INTO [t7573] VALUES (1,'ما يرقى به المريض','1','0','0');
+INSERT INTO [t7573] VALUES (2,'التعوذ من السحر والعين والحسد','12','0','0');
+INSERT INTO [t7573] VALUES (3,'ما يقال للمريض','23','0','0');
+INSERT INTO [t7573] VALUES (4,'أذكار الكرب','33','0','0');
+INSERT INTO [t7573] VALUES (5,'دعاء الغم والهم والحزن','42','0','0');
+INSERT INTO [t7573] VALUES (6,'ما يقول إذا أصابته مصيبة','51','0','0');
