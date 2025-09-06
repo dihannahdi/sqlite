50e135954326b9e21f375aff7a6b8e7a7bb70240
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:17 +0700

    Upload SQL chunk: t2135_chunk_0000.sql

---
+++ sql_chunks/t2135_chunk_0000.sql
@@ -0,0 +1,50 @@
+-- Table: t2135
+-- Chunk: 1
+-- Rows: 43
+
+CREATE TABLE "t2135" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2135
+INSERT INTO [t2135] VALUES (1,'مقدمة الطبعة الثانية','2','0','0');
+INSERT INTO [t2135] VALUES (2,'الكلمة وأقسامها','3','0','0');
+INSERT INTO [t2135] VALUES (3,'أنواع الإعراب وعلاماته','26','0','0');
+INSERT INTO [t2135] VALUES (4,'إعراب الفعل المضارع','47','0','0');
+INSERT INTO [t2135] VALUES (5,'النكرة والمعرفة','73','0','0');
+INSERT INTO [t2135] VALUES (6,'أنواع المعارف','74','0','0');
+INSERT INTO [t2135] VALUES (7,'باب المبتدأ والخبر','93','0','0');
+INSERT INTO [t2135] VALUES (8,'باب (إن) وأخواتها','120','0','0');
+INSERT INTO [t2135] VALUES (9,'باب (لا) النافية للجنس','133','0','0');
+INSERT INTO [t2135] VALUES (10,'باب ظن وأخوتها','136','0','0');
+INSERT INTO [t2135] VALUES (11,'باب الفاعل','141','0','0');
+INSERT INTO [t2135] VALUES (12,'نائب الفاعل','154','0','0');
+INSERT INTO [t2135] VALUES (13,'باب الاشتغال','158','0','0');
+INSERT INTO [t2135] VALUES (14,'باب في التنازع','164','0','0');
+INSERT INTO [t2135] VALUES (15,'باب في ذكر منصوبات الأسماء','167','0','0');
+INSERT INTO [t2135] VALUES (16,'فصل في الاستغاثة والندبة','181','0','0');
+INSERT INTO [t2135] VALUES (17,'المفعول المطلق','184','0','0');
+INSERT INTO [t2135] VALUES (18,'المفعول له','187','0','0');
+INSERT INTO [t2135] VALUES (19,'المفعول فيه','190','0','0');
+INSERT INTO [t2135] VALUES (20,'المفعول معه','193','0','0');
+INSERT INTO [t2135] VALUES (21,'باب الحال','196','0','0');
+INSERT INTO [t2135] VALUES (22,'باب التمييز','200','0','0');
+INSERT INTO [t2135] VALUES (23,'باب المستثنى','206','0','0');
+INSERT INTO [t2135] VALUES (24,'باب مخفوضات الأسماء','213','0','0');
+INSERT INTO [t2135] VALUES (25,'الإضافة','218','0','0');
+INSERT INTO [t2135] VALUES (26,'باب - في ذكر الأسماء العاملة عمل أفعالها','223','0','0');
+INSERT INTO [t2135] VALUES (27,'١- اسم الفعل','223','0','0');
+INSERT INTO [t2135] VALUES (28,'٢- المصدر','226','0','0');
+INSERT INTO [t2135] VALUES (29,'٣- اسم الفاعل','230','0','0');
+INSERT INTO [t2135] VALUES (30,'٤- أمثلة المبالغة','234','0','0');
+INSERT INTO [t2135] VALUES (31,'٥- اسم المفعول','236','0','0');
+INSERT INTO [t2135] VALUES (32,'٦- الصفة المشبهة باسم الفاعل','237','0','0');
+INSERT INTO [t2135] VALUES (33,'اسم التفضيل','240','0','0');
+INSERT INTO [t2135] VALUES (34,'باب التوابع','244','0','0');
+INSERT INTO [t2135] VALUES (35,'١- النعت','244','0','0');
+INSERT INTO [t2135] VALUES (36,'٢- التوكيد','249','0','0');
+INSERT INTO [t2135] VALUES (37,'٣- عطف البيان','256','0','0');
+INSERT INTO [t2135] VALUES (38,'٤- عطف النسق','260','0','0');
+INSERT INTO [t2135] VALUES (39,'٥- البدل','264','0','0');
+INSERT INTO [t2135] VALUES (40,'باب في ذكر موانع الصرف','270','0','0');
+INSERT INTO [t2135] VALUES (41,'باب في التعجب','278','0','0');
+INSERT INTO [t2135] VALUES (42,'باب في الوقف وبعض مسائل الخط','283','0','0');
+INSERT INTO [t2135] VALUES (43,'فصل في الكلام على مواضع همزة الوصل','288','0','0');
