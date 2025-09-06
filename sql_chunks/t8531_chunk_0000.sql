9d02ac535db78fa417efb04064cea96244553ab5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:46:53 +0700

    Upload SQL chunk: t8531_chunk_0000.sql

---
+++ sql_chunks/t8531_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8531
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8531" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8531
+INSERT INTO [t8531] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8531] VALUES (2,'كتاب الكبائر','5','0','0');
