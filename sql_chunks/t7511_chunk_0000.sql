9b092d53fd07943b559e0f61099e739ae617f00d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:15 +0700

    Upload SQL chunk: t7511_chunk_0000.sql

---
+++ sql_chunks/t7511_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t7511
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t7511" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7511
+INSERT INTO [t7511] VALUES (1,'اعتماد دعوة الشيخ محمد بن عبد الوهاب على الكتاب والسنة','1','0','0');
+INSERT INTO [t7511] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t7511] VALUES (3,'والله من وراء القصد','29','0','0');
