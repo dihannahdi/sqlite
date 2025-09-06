d78aa7862ae2240ef18cff63fe2650c797f77223
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:30 +0700

    Upload SQL chunk: t11126_chunk_0000.sql

---
+++ sql_chunks/t11126_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11126
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11126" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11126
+INSERT INTO [t11126] VALUES (1,'ثلاثة الأصول','1','0','0');
+INSERT INTO [t11126] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
