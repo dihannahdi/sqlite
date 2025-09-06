748b665d2a9487e55b7896cdbbdd5560b4693b5e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:02 +0700

    Upload SQL chunk: t30822_chunk_0000.sql

---
+++ sql_chunks/t30822_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t30822
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t30822" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30822
+INSERT INTO [t30822] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t30822] VALUES (2,'حقيقة التوحيد والشرك','3','0','0');
+INSERT INTO [t30822] VALUES (3,'توحيد المرسلين وما يضاده من الكفر والشرك','39','0','0');
+INSERT INTO [t30822] VALUES (4,'توضيح معنى الشرك بالله','94','0','0');
