fcbb9f83485aeffd7d6d48b1b7d1858c2e92cb01
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:37 +0700

    Upload SQL chunk: t36626_chunk_0000.sql

---
+++ sql_chunks/t36626_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t36626
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t36626" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36626
+INSERT INTO [t36626] VALUES (1,'- بسم الله الرحمن الرحيم -','2','0','0');
+INSERT INTO [t36626] VALUES (2,'المولد: هل له أصل في الشرع؟ أو هو بدعة وحدث في الدين','3','0','0');
+INSERT INTO [t36626] VALUES (3,'لا أعلم لهذا المولد أصلا في كتاب ولا سنة، ولا ينقل عمله عن أحد من علماء الأمة','3','0','0');
+INSERT INTO [t36626] VALUES (4,'بل هو بدعة أحدثها البطالون، وشهوة نفس اغتنى بها الأكالون','4','0','0');
