fbe365c2c71b65ec1f0e0078e40becedf68a099d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:24 +0700

    Upload SQL chunk: t11118_chunk_0000.sql

---
+++ sql_chunks/t11118_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11118
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11118" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11118
+INSERT INTO [t11118] VALUES (1,'المنتخب','2','0','0');
