51f18fd5422c470b1ff580014900fe811f84e03e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:53 +0700

    Upload SQL chunk: t10108_chunk_0000.sql

---
+++ sql_chunks/t10108_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t10108
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t10108" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10108
+INSERT INTO [t10108] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10108] VALUES (2,'حرف الألف','3','0','0');
+INSERT INTO [t10108] VALUES (3,'حرف الباء','5','0','0');
+INSERT INTO [t10108] VALUES (4,'حرف التاء والثاء','6','0','0');
+INSERT INTO [t10108] VALUES (5,'حرف الجيم','7','0','0');
+INSERT INTO [t10108] VALUES (6,'حرف الحاء','9','0','0');
+INSERT INTO [t10108] VALUES (7,'حرف الخاء','11','0','0');
+INSERT INTO [t10108] VALUES (8,'حرف الدال','12','0','0');
+INSERT INTO [t10108] VALUES (9,'حرف الذال والراء والزاي','13','0','0');
+INSERT INTO [t10108] VALUES (10,'حرف السين','14','0','0');
+INSERT INTO [t10108] VALUES (11,'حرف الشين','15','0','0');
+INSERT INTO [t10108] VALUES (12,'حرف الصاد','16','0','0');
+INSERT INTO [t10108] VALUES (13,'حرف الضاد','17','0','0');
+INSERT INTO [t10108] VALUES (14,'حرف الطاء والظاء والعين','18','0','0');
+INSERT INTO [t10108] VALUES (15,'حرف الغين','21','0','0');
+INSERT INTO [t10108] VALUES (16,'حرف الفاء','22','0','0');
+INSERT INTO [t10108] VALUES (17,'حرف القاف والكاف واللام والميم','23','0','0');
+INSERT INTO [t10108] VALUES (18,'حرف النون','26','0','0');
+INSERT INTO [t10108] VALUES (19,'حرف الهاء والواو','27','0','0');
+INSERT INTO [t10108] VALUES (20,'حرف الياء','28','0','0');
+INSERT INTO [t10108] VALUES (21,'الكنى','29','0','0');
+INSERT INTO [t10108] VALUES (22,'مصادر ومراجع','31','0','0');
