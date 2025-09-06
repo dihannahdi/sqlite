59e4d85d2b6547ead268748c8a9e6b39d64e9bd8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:49 +0700

    Upload SQL chunk: t6589_chunk_0000.sql

---
+++ sql_chunks/t6589_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t6589
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t6589" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6589
+INSERT INTO [t6589] VALUES (1,'تعريف بالإمام المناوي','1','0','0');
+INSERT INTO [t6589] VALUES (2,'(مقدمة المصنف)','2','0','0');
+INSERT INTO [t6589] VALUES (3,'(مقدمة الشارح)','4','0','0');
+INSERT INTO [t6589] VALUES (4,'الشمائل الشريفة','7','0','0');
+INSERT INTO [t6589] VALUES (5,'باب كان وهي الشمائل الشريفة','8','0','0');
