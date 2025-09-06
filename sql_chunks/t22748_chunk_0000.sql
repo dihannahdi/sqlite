96a20595975dd87666f7224e2efbaa90d360ee5d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:17 +0700

    Upload SQL chunk: t22748_chunk_0000.sql

---
+++ sql_chunks/t22748_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t22748
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t22748" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22748
+INSERT INTO [t22748] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t22748] VALUES (2,'ما هو أسلوب الدعوة؟','2','0','0');
+INSERT INTO [t22748] VALUES (3,'خصائص هذه الأساليب','3','0','0');
+INSERT INTO [t22748] VALUES (4,'الأساليب العقائدية ومنهج القرآن في تقريرها','9','0','0');
+INSERT INTO [t22748] VALUES (5,'منهج القرآن في الإستدلال على صدق العقيدة الإسلامية','16','0','0');
+INSERT INTO [t22748] VALUES (6,'نماذج من هذا الإستدلال','18','0','0');
+INSERT INTO [t22748] VALUES (7,'العقيدة في الأساليب الخبرية والإنشائية','21','0','0');
+INSERT INTO [t22748] VALUES (8,'في الأساليب الإنشائية','24','0','0');
+INSERT INTO [t22748] VALUES (9,'العقيدة في أساليب المحاورة والمحججة','25','0','0');
+INSERT INTO [t22748] VALUES (10,'العقيدة في أسلوب القصة','27','0','0');
