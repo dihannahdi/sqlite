580bd43e4e0ae80197596628355565b79b0de421
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:03 +0700

    Upload SQL chunk: t9523_chunk_0000.sql

---
+++ sql_chunks/t9523_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t9523
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t9523" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9523
+INSERT INTO [t9523] VALUES (1,'ذكر من اسمه أحمد ممن سمعت منهم من أصحاب الإمام أبي عبد الله','15','0','0');
+INSERT INTO [t9523] VALUES (2,'زياد','30','0','0');
+INSERT INTO [t9523] VALUES (3,'سليمان','31','0','0');
+INSERT INTO [t9523] VALUES (4,'شجاع','32','0','0');
+INSERT INTO [t9523] VALUES (5,'شيبان','33','0','0');
+INSERT INTO [t9523] VALUES (6,'طلحة','35','0','0');
+INSERT INTO [t9523] VALUES (7,'ذكر العبادلة','36','0','0');
+INSERT INTO [t9523] VALUES (8,'من اسمه: عبد الله','36','0','0');
+INSERT INTO [t9523] VALUES (9,'ومن اسمه عبد الرحمن','37','0','0');
+INSERT INTO [t9523] VALUES (10,'عبد الوهاب','41','0','0');
+INSERT INTO [t9523] VALUES (11,'عبد الواحد','44','0','0');
+INSERT INTO [t9523] VALUES (12,'عبيد الله','46','0','0');
+INSERT INTO [t9523] VALUES (13,'عبد الرزاق','47','0','0');
+INSERT INTO [t9523] VALUES (14,'عمر','48','0','0');
+INSERT INTO [t9523] VALUES (15,'علي','50','0','0');
+INSERT INTO [t9523] VALUES (16,'ومن اسمه الفضل','52','0','0');
+INSERT INTO [t9523] VALUES (17,'من اسمه محمد منهم','55','0','0');
+INSERT INTO [t9523] VALUES (18,'ومن بقية الميم','61','0','0');
+INSERT INTO [t9523] VALUES (19,'ومن النساء','63','0','0');
+INSERT INTO [t9523] VALUES (20,'ذكر من سمعت منه من أهل بيت أبي عبد الله بن منده رحمهم الله','65','0','0');
+INSERT INTO [t9523] VALUES (21,'ومن نساء أهل بيته','73','0','0');
+INSERT INTO [t9523] VALUES (22,'ومن موالي أهل بيته','74','0','0');
