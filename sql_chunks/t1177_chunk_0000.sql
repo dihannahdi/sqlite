84f4e143248a56790857e815d0a68b30fab5ac86
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:36 +0700

    Upload SQL chunk: t1177_chunk_0000.sql

---
+++ sql_chunks/t1177_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t1177
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t1177" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1177
+INSERT INTO [t1177] VALUES (1,'العولمة وخطاب ما بعد الحداثة','1','0','0');
+INSERT INTO [t1177] VALUES (2,'التحولات العالمية وعنف العولمة','3','0','0');
+INSERT INTO [t1177] VALUES (3,'هوية العولمة','8','0','0');
+INSERT INTO [t1177] VALUES (4,'العولمة والعالمية الإسلامية','15','0','0');
+INSERT INTO [t1177] VALUES (5,'إيديولوجيا العولمة','15','4','0');
+INSERT INTO [t1177] VALUES (6,'وسائل تمرير العولمة','18','4','0');
+INSERT INTO [t1177] VALUES (7,'العولمة في الخطاب العربي','22','0','0');
+INSERT INTO [t1177] VALUES (8,'عنف العولمة','25','0','0');
+INSERT INTO [t1177] VALUES (9,'ردود الفعل ضد العولمة','27','0','0');
+INSERT INTO [t1177] VALUES (10,'تأثيرات العولمة في المنطقة العربية','29','0','0');
+INSERT INTO [t1177] VALUES (11,'كيف نواجه العولمة','33','0','0');
+INSERT INTO [t1177] VALUES (12,'أولا ومن الناحية النظرية','33','11','0');
+INSERT INTO [t1177] VALUES (13,'ثانيا من الناحية العملية','34','11','0');
