433941201a53876bf34857d0fdf9fa59ba9ff3f9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:39 +0700

    Upload SQL chunk: t6039_chunk_0000.sql

---
+++ sql_chunks/t6039_chunk_0000.sql
@@ -0,0 +1,65 @@
+-- Table: t6039
+-- Chunk: 1
+-- Rows: 58
+
+CREATE TABLE "t6039" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6039
+INSERT INTO [t6039] VALUES (1,'مقدمة الكتاب','1','0','0');
+INSERT INTO [t6039] VALUES (2,'كتاب الطهارة','1','0','0');
+INSERT INTO [t6039] VALUES (3,'كتاب الصلاة','9','0','0');
+INSERT INTO [t6039] VALUES (4,'كتاب الزكاة','15','0','0');
+INSERT INTO [t6039] VALUES (5,'كتاب الصوم','19','0','0');
+INSERT INTO [t6039] VALUES (6,'كتاب المناسك','26','0','0');
+INSERT INTO [t6039] VALUES (7,'كتاب النكاح','36','0','0');
+INSERT INTO [t6039] VALUES (8,'كتاب الرضاع','48','0','0');
+INSERT INTO [t6039] VALUES (9,'كتاب الطلاق','50','0','0');
+INSERT INTO [t6039] VALUES (10,'كتاب العتاق','62','0','0');
+INSERT INTO [t6039] VALUES (11,'كتاب المكاتب','64','0','0');
+INSERT INTO [t6039] VALUES (12,'كتاب الولاء','64','0','0');
+INSERT INTO [t6039] VALUES (13,'كتاب الأيمان','65','0','0');
+INSERT INTO [t6039] VALUES (14,'كتاب الحدود','71','0','0');
+INSERT INTO [t6039] VALUES (15,'كتاب السرقة','75','0','0');
+INSERT INTO [t6039] VALUES (16,'كتاب السير','78','0','0');
+INSERT INTO [t6039] VALUES (17,'كتاب الاستحسان','88','0','0');
+INSERT INTO [t6039] VALUES (18,'كتاب التحري','89','0','0');
+INSERT INTO [t6039] VALUES (19,'كتاب اللقيط','91','0','0');
+INSERT INTO [t6039] VALUES (20,'كتاب اللقطة','92','0','0');
+INSERT INTO [t6039] VALUES (21,'كتاب الإباق','93','0','0');
+INSERT INTO [t6039] VALUES (22,'كتاب المفقود','94','0','0');
+INSERT INTO [t6039] VALUES (23,'كتاب الغصب','95','0','0');
+INSERT INTO [t6039] VALUES (24,'كتاب الوديعة','97','0','0');
+INSERT INTO [t6039] VALUES (25,'كتاب العارية','97','0','0');
+INSERT INTO [t6039] VALUES (26,'كتاب الشركة','98','0','0');
+INSERT INTO [t6039] VALUES (27,'كتاب الصيد','99','0','0');
+INSERT INTO [t6039] VALUES (28,'كتاب الذبائح','103','0','0');
+INSERT INTO [t6039] VALUES (29,'كتاب الأضاحي','104','0','0');
+INSERT INTO [t6039] VALUES (30,'كتاب الوقف','104','0','0');
+INSERT INTO [t6039] VALUES (31,'كتاب الهبة','105','0','0');
+INSERT INTO [t6039] VALUES (32,'كتاب البيع','107','0','0');
+INSERT INTO [t6039] VALUES (33,'كتاب الصرف','112','0','0');
+INSERT INTO [t6039] VALUES (34,'كتاب الشفعة','118','0','0');
+INSERT INTO [t6039] VALUES (35,'كتاب القسمة','119','0','0');
+INSERT INTO [t6039] VALUES (36,'كتاب الإجارات','123','0','0');
+INSERT INTO [t6039] VALUES (37,'كتاب أدب القاضي','128','0','0');
+INSERT INTO [t6039] VALUES (38,'كتاب الشهادات','131','0','0');
+INSERT INTO [t6039] VALUES (39,'كتاب الرجوع','132','0','0');
+INSERT INTO [t6039] VALUES (40,'كتاب الدعوى','133','0','0');
+INSERT INTO [t6039] VALUES (41,'كتاب الإقرار','135','0','0');
+INSERT INTO [t6039] VALUES (42,'كتاب الوكالة','136','0','0');
+INSERT INTO [t6039] VALUES (43,'كتاب الكفالة والحوالة','138','0','0');
+INSERT INTO [t6039] VALUES (44,'كتاب الصلح','143','0','0');
+INSERT INTO [t6039] VALUES (45,'كتاب الرهن','145','0','0');
+INSERT INTO [t6039] VALUES (46,'كتاب المضاربة','147','0','0');
+INSERT INTO [t6039] VALUES (47,'كتاب المزارعة','148','0','0');
+INSERT INTO [t6039] VALUES (48,'كتاب الشرب','153','0','0');
+INSERT INTO [t6039] VALUES (49,'كتاب الأشربة','156','0','0');
+INSERT INTO [t6039] VALUES (50,'كتاب الإكراه','160','0','0');
+INSERT INTO [t6039] VALUES (51,'كتاب الحجر','161','0','0');
+INSERT INTO [t6039] VALUES (52,'كتاب المأذون','161','0','0');
+INSERT INTO [t6039] VALUES (53,'كتاب الديات','162','0','0');
+INSERT INTO [t6039] VALUES (54,'كتاب الوصايا','168','0','0');
+INSERT INTO [t6039] VALUES (55,'كتاب الفرائض','169','0','0');
+INSERT INTO [t6039] VALUES (56,'كتاب الخنثى','170','0','0');
+INSERT INTO [t6039] VALUES (57,'كتاب الحيل','170','0','0');
+INSERT INTO [t6039] VALUES (58,'كتاب الاستحلاف والتزكية','170','0','0');
