ea7cdf690919f30699e97da0968d821b4e96a4fb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:48 +0700

    Upload SQL chunk: t37545_chunk_0000.sql

---
+++ sql_chunks/t37545_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t37545
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t37545" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37545
+INSERT INTO [t37545] VALUES (1,'تنبيه مهم','3','0','0');
+INSERT INTO [t37545] VALUES (2,'شرح مقدمة الكتاب','5','0','0');
+INSERT INTO [t37545] VALUES (3,'كتاب الطهارة','29','0','0');
+INSERT INTO [t37545] VALUES (4,'باب الآنية','63','3','0');
+INSERT INTO [t37545] VALUES (5,'باب الإستنجاء','80','3','0');
+INSERT INTO [t37545] VALUES (6,'باب السواك وسنن الوضوء','123','3','0');
+INSERT INTO [t37545] VALUES (7,'باب فروض الوضوء وصفته','155','3','0');
+INSERT INTO [t37545] VALUES (8,'باب المسح على الخفين','203','3','0');
+INSERT INTO [t37545] VALUES (9,'باب نواقض الوضوء','229','3','0');
+INSERT INTO [t37545] VALUES (10,'باب الغسل','266','3','0');
+INSERT INTO [t37545] VALUES (11,'باب التيمم','308','3','0');
+INSERT INTO [t37545] VALUES (12,'باب إزالة النجاسة','343','3','0');
+INSERT INTO [t37545] VALUES (13,'باب الحيض','381','3','0');
