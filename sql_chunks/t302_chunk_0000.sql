ff80583f791d676f06ec2c527096456f057b7772
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:17 +0700

    Upload SQL chunk: t302_chunk_0000.sql

---
+++ sql_chunks/t302_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t302
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t302" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t302
+INSERT INTO [t302] VALUES (1,'الحكمة من الحكم بالملك بالرق','2','0','0');
+INSERT INTO [t302] VALUES (2,'سؤال: إذا كان الرقيق مسلما فما وجه ملكه بالرق','6','0','0');
+INSERT INTO [t302] VALUES (3,'الترغيب في عتق المسلم، وأبوابه الكثيرة','7','0','0');
+INSERT INTO [t302] VALUES (4,'الرد على دعوى أن الإسلام جاء بالرق في فترة معينة فقط','8','0','0');
+INSERT INTO [t302] VALUES (5,'الرد على من زعم أن القرآن يمنع الرق بقوله {فإما منا بعد وإما فداء}','8','0','0');
+INSERT INTO [t302] VALUES (6,'سؤالان وجوابهما قد يردان في ذهن الطالب','12','0','0');
