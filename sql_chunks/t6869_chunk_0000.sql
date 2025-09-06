4b26c859605718352e354edafa44cb4ecec4715b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:14 +0700

    Upload SQL chunk: t6869_chunk_0000.sql

---
+++ sql_chunks/t6869_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t6869
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t6869" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6869
+INSERT INTO [t6869] VALUES (1,'قال الفقيه الزاهد ابو إسحاق إبراهيم بن مسعود الألبيري','1','0','0');
