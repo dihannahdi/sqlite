ad396a09aa9ed5b6f59a0f738371ad92e9f31bc1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:24:02 +0700

    Upload SQL chunk: t151132_chunk_0000.sql

---
+++ sql_chunks/t151132_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t151132
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t151132" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t151132
+INSERT INTO [t151132] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t151132] VALUES (2,'إحداهما','3','0','0');
+INSERT INTO [t151132] VALUES (3,'الصورة الثانية','7','0','0');
+INSERT INTO [t151132] VALUES (4,'وفي ذلك قولان مشهوران للعلماء:','7','3','0');
