5c0a0af272072542418d9f111b93ae28d17e42e7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:48 +0700

    Upload SQL chunk: t36527_chunk_0000.sql

---
+++ sql_chunks/t36527_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t36527
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t36527" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36527
+INSERT INTO [t36527] VALUES (1,'(لا بد من أصل يرجع إليه عند الاختلاف)','3','0','0');
+INSERT INTO [t36527] VALUES (2,'(آيات أنزلها الله ثناء على الصحابة)','3','0','0');
+INSERT INTO [t36527] VALUES (3,'(السنة الصحيحة ما صححه أئمة الحديث الثقات المشهورون)','7','0','0');
+INSERT INTO [t36527] VALUES (4,'(الاجتهاد والتقليد)','14','0','0');
+INSERT INTO [t36527] VALUES (5,'(صحبة أبي بكر)','15','0','0');
+INSERT INTO [t36527] VALUES (6,'(براءة عائشة)','15','0','0');
+INSERT INTO [t36527] VALUES (7,'(تفضيل الخلفاء الأربعة)','16','0','0');
+INSERT INTO [t36527] VALUES (8,'(التقية)','17','0','0');
+INSERT INTO [t36527] VALUES (9,'(حقوق القرابة وحقوق الصحبة والمؤازرة والنصرة)','20','0','0');
+INSERT INTO [t36527] VALUES (10,'(التحذير من انتقاص الصحابة أو سبهم)','21','0','0');
