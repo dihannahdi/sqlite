23c4c489f9d323df1a5b0b8dedb908a793d2c09f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:01 +0700

    Upload SQL chunk: t7036_chunk_0000.sql

---
+++ sql_chunks/t7036_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t7036
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t7036" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7036
+INSERT INTO [t7036] VALUES (1,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t7036] VALUES (2,'أما بعد فلما كانت الألفاظ المتداولة','2','0','0');
+INSERT INTO [t7036] VALUES (3,'الخبر ما له نسبة في الخارج','22','0','0');
