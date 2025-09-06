ca526669212a77c9069aee112fc265ac6df6eb86
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:59 +0700

    Upload SQL chunk: t27194_chunk_0000.sql

---
+++ sql_chunks/t27194_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t27194
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t27194" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t27194
+INSERT INTO [t27194] VALUES (1,'المنظومة الميمية (١)','2','0','0');
+INSERT INTO [t27194] VALUES (2,'المنظومة الميمية (٢)','21','0','0');
+INSERT INTO [t27194] VALUES (3,'المنظومة الميمية (٣)','39','0','0');
+INSERT INTO [t27194] VALUES (4,'نبذة في وصية طالب العلم','44','3','0');
+INSERT INTO [t27194] VALUES (5,'المنظومة الميمية (٤)','57','0','0');
+INSERT INTO [t27194] VALUES (6,'الوصية بكتاب الله -عز وجل-','67','5','0');
+INSERT INTO [t27194] VALUES (7,'المنظومة الميمية (٥)','74','0','0');
+INSERT INTO [t27194] VALUES (8,'المنظومة الميمية (٦)','92','0','0');
+INSERT INTO [t27194] VALUES (9,'الوصية بالسنة:','92','8','0');
+INSERT INTO [t27194] VALUES (10,'المنظومة الميمية (٧)','111','0','0');
+INSERT INTO [t27194] VALUES (11,'في الفرائض والآلة والتحذير من العلوم المبتدعة:','111','10','0');
+INSERT INTO [t27194] VALUES (12,'المنظومة الميمية (٨)','124','0','0');
+INSERT INTO [t27194] VALUES (13,'خاتمة في تحصيل ثمرات العلم النافعة واجتناء قطوفه الدانية اليانعة:','124','12','0');
