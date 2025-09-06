464fac9f37a81496b2c00908b8ae7a4feb0b62d2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:47 +0700

    Upload SQL chunk: t6585_chunk_0000.sql

---
+++ sql_chunks/t6585_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t6585
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t6585" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6585
+INSERT INTO [t6585] VALUES (1,'بسم الله الرحمن الرحيم','3','0','0');
+INSERT INTO [t6585] VALUES (2,'الخدم والموالي من الرجال','3','0','0');
+INSERT INTO [t6585] VALUES (3,'الكنى والألقاب والمجاهيل من الرجال من خدم رسول الله صلى الله عليه وسلم ومواليه','37','0','0');
+INSERT INTO [t6585] VALUES (4,'الخدم والإماء من النساء','45','0','0');
