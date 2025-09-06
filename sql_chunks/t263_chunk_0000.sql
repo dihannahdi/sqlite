7511014ccbd5b08661e3d9684bb10b75fde63dc5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:16 +0700

    Upload SQL chunk: t263_chunk_0000.sql

---
+++ sql_chunks/t263_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t263
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t263" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t263
+INSERT INTO [t263] VALUES (1,'باب ذكر أهون أهل النار عذابا','6','0','0');
+INSERT INTO [t263] VALUES (2,'باب ذكر أودية جهنم وجبالها','13','0','0');
+INSERT INTO [t263] VALUES (3,'باب ذكر حيات النار وعقاربها','24','0','0');
+INSERT INTO [t263] VALUES (4,'باب ذكر شراب أهل النار','31','0','0');
+INSERT INTO [t263] VALUES (5,'باب ذكر شدة عذاب أهل النار','42','0','0');
+INSERT INTO [t263] VALUES (6,'باب ذكر الصراط والممر عليه','49','0','0');
+INSERT INTO [t263] VALUES (7,'باب نزول الله تبارك وتعالى في ظلل من الغمام للحساب','59','0','0');
+INSERT INTO [t263] VALUES (8,'باب شفاعة النبي صلى الله عليه وسلم لأهل الموقف','69','0','0');
+INSERT INTO [t263] VALUES (9,'باب ذكر الموازين يوم القيامة','75','0','0');
+INSERT INTO [t263] VALUES (10,'باب وضع الحساب يوم القيامة','81','0','0');
+INSERT INTO [t263] VALUES (11,'باب ذكر ما يدعى يوم القيامة','88','0','0');
+INSERT INTO [t263] VALUES (12,'باب ذكر محاسبة الله تبارك وتعالى العباد يوم القيامة','94','0','0');
+INSERT INTO [t263] VALUES (13,'باب ذكر القصاص يوم القيامة','111','0','0');
