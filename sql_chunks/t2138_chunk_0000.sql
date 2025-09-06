64bdc3ee7d40ffa8f52844c806ca3b756f0b8ab5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:18 +0700

    Upload SQL chunk: t2138_chunk_0000.sql

---
+++ sql_chunks/t2138_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t2138
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t2138" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2138
+INSERT INTO [t2138] VALUES (1,'تحفة المجالس في التعليقات علي فهرس الفهارس','1','0','0');
+INSERT INTO [t2138] VALUES (2,'والله تعالى أعلم، وصلى الله وسلم على خير خلقه وآله وصحبه أجمعين','65','0','0');
