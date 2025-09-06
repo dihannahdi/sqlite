b6e5e62fdeb844f81b46e35fa6b6ea93e9a39cb0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:17 +0700

    Upload SQL chunk: t32881_chunk_0000.sql

---
+++ sql_chunks/t32881_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t32881
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t32881" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t32881
+INSERT INTO [t32881] VALUES (1,'منهج القرآن في القضاء والقدر','1','0','0');
+INSERT INTO [t32881] VALUES (2,'مقدمة','2','0','0');
+INSERT INTO [t32881] VALUES (3,'عدل الله','6','0','0');
+INSERT INTO [t32881] VALUES (4,'النوع الأول: قضاء يستوجب الصبر','10','0','0');
+INSERT INTO [t32881] VALUES (5,'النوع الثاني: قضاء يستوجب المعالجة:','11','0','0');
+INSERT INTO [t32881] VALUES (6,'النوع الثالث: قضاء أنت فيه حر، وفي حدود الحرية محاسب','13','0','0');
+INSERT INTO [t32881] VALUES (7,'قسمة جائرة','16','0','0');
+INSERT INTO [t32881] VALUES (8,'القلب حرم آمن','21','0','0');
+INSERT INTO [t32881] VALUES (9,'ساعة وساعة','24','0','0');
+INSERT INTO [t32881] VALUES (10,'آيات تفسرها آيات','29','0','0');
+INSERT INTO [t32881] VALUES (11,'وهؤلاء يهديهم الله','32','0','0');
+INSERT INTO [t32881] VALUES (12,'هداية الله','33','0','0');
+INSERT INTO [t32881] VALUES (13,'ثانيا: هداية الإرشاد','35','0','0');
+INSERT INTO [t32881] VALUES (14,'هداية التوفيق','36','0','0');
+INSERT INTO [t32881] VALUES (15,'رابعا: هداية المؤمنين لمنازلهم في الجنة','40','0','0');
+INSERT INTO [t32881] VALUES (16,'مفاهيم مظلومة','41','0','0');
+INSERT INTO [t32881] VALUES (17,'انيا: أمر الله','43','0','0');
+INSERT INTO [t32881] VALUES (18,'ثالثا: إرادة الله','47','0','0');
+INSERT INTO [t32881] VALUES (19,'علم الله','49','0','0');
+INSERT INTO [t32881] VALUES (20,'خير من الجدل','51','0','0');
+INSERT INTO [t32881] VALUES (21,'من تجارب الحياة','53','0','0');
