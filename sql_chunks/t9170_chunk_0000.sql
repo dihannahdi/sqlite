41645a9ccd73e8008eb5003a09f5d88feee019ef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:51:20 +0700

    Upload SQL chunk: t9170_chunk_0000.sql

---
+++ sql_chunks/t9170_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t9170
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t9170" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9170
+INSERT INTO [t9170] VALUES (1,'التقدمة وترجمة المؤلف','1','0','0');
+INSERT INTO [t9170] VALUES (2,'باب: الألف','44','0','0');
+INSERT INTO [t9170] VALUES (3,'باب: الباء الموحدة','74','0','0');
+INSERT INTO [t9170] VALUES (4,'باب: التاء المثناة من فوق','85','0','0');
+INSERT INTO [t9170] VALUES (5,'باب: الثاء المثلثة','86','0','0');
+INSERT INTO [t9170] VALUES (6,'باب: الجيم','90','0','0');
+INSERT INTO [t9170] VALUES (7,'باب: الحاء المهملة','98','0','0');
+INSERT INTO [t9170] VALUES (8,'باب: الخاء','137','0','0');
+INSERT INTO [t9170] VALUES (9,'باب: الدال المهملة','144','0','0');
+INSERT INTO [t9170] VALUES (10,'باب: الذال المعجمة','147','0','0');
+INSERT INTO [t9170] VALUES (11,'باب: الراء','148','0','0');
+INSERT INTO [t9170] VALUES (12,'باب: الزاي','160','0','0');
+INSERT INTO [t9170] VALUES (13,'باب: السين المهملة','170','0','0');
+INSERT INTO [t9170] VALUES (14,'باب: الشين','211','0','0');
+INSERT INTO [t9170] VALUES (15,'باب: الصاد','222','0','0');
+INSERT INTO [t9170] VALUES (16,'باب: الضاد المعجمة','228','0','0');
+INSERT INTO [t9170] VALUES (17,'باب: الطاء','230','0','0');
+INSERT INTO [t9170] VALUES (18,'باب: الظاء المعجمة','236','0','0');
+INSERT INTO [t9170] VALUES (19,'باب: العين المهملة','237','0','0');
+INSERT INTO [t9170] VALUES (20,'باب: الغين','379','0','0');
+INSERT INTO [t9170] VALUES (21,'باب: الفاء','380','0','0');
+INSERT INTO [t9170] VALUES (22,'باب: القاف','384','0','0');
+INSERT INTO [t9170] VALUES (23,'باب: الكاف','394','0','0');
+INSERT INTO [t9170] VALUES (24,'باب: اللام','398','0','0');
+INSERT INTO [t9170] VALUES (25,'باب: الميم','399','0','0');
+INSERT INTO [t9170] VALUES (26,'باب: النون','446','0','0');
+INSERT INTO [t9170] VALUES (27,'باب: الهاء','454','0','0');
+INSERT INTO [t9170] VALUES (28,'باب: الواو','463','0','0');
+INSERT INTO [t9170] VALUES (29,'باب: الياء','468','0','0');
+INSERT INTO [t9170] VALUES (30,'باب: الكنى','489','0','0');
+INSERT INTO [t9170] VALUES (31,'باب: فيمن قال فيه ابن فلان','516','0','0');
+INSERT INTO [t9170] VALUES (32,'باب: الأنساب، مولى الربيع بن خثيم، فيمن نزل الكوفة وغيرها من الصحابة، الألف','518','0','0');
+INSERT INTO [t9170] VALUES (33,'مخطوطات','527','0','0');
+INSERT INTO [t9170] VALUES (34,'محتوى الفهارس','528','0','0');
+INSERT INTO [t9170] VALUES (35,'الآيات القرآنية الكريمة','528','34','0');
+INSERT INTO [t9170] VALUES (36,'مسرد الأحاديث النبوية الشريفة','530','34','0');
+INSERT INTO [t9170] VALUES (37,'المراجع','533','34','0');
+INSERT INTO [t9170] VALUES (38,'فهرس مواضيع الكتاب ومسرد كتاب الثقات حسب ورود التراجم فيه','538','34','0');
