f960b3619c49131d2457c34bc41b581c69ba6dc7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:56 +0700

    Upload SQL chunk: t2915_chunk_0000.sql

---
+++ sql_chunks/t2915_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t2915
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t2915" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2915
+INSERT INTO [t2915] VALUES (1,'معجم المصطلحات القرآنية','1','0','0');
+INSERT INTO [t2915] VALUES (2,'أسماء السور','2','0','0');
+INSERT INTO [t2915] VALUES (3,'الإشمام','3','0','0');
+INSERT INTO [t2915] VALUES (4,'الإمالة','4','0','0');
+INSERT INTO [t2915] VALUES (5,'ج','5','0','0');
+INSERT INTO [t2915] VALUES (6,'الجزء','6','0','0');
+INSERT INTO [t2915] VALUES (7,'الحزب','7','0','0');
+INSERT INTO [t2915] VALUES (8,'الرسم العثماني','8','0','0');
+INSERT INTO [t2915] VALUES (9,'الركوع','9','0','0');
+INSERT INTO [t2915] VALUES (10,'السبع الطوال','10','0','0');
+INSERT INTO [t2915] VALUES (11,'سجدة التلاوة','11','0','0');
+INSERT INTO [t2915] VALUES (12,'صلى','12','0','0');
+INSERT INTO [t2915] VALUES (13,'الضبط','13','0','0');
+INSERT INTO [t2915] VALUES (14,'علامات الوقف','14','0','0');
+INSERT INTO [t2915] VALUES (15,'القراءات','15','0','0');
+INSERT INTO [t2915] VALUES (16,'قلى','16','0','0');
+INSERT INTO [t2915] VALUES (17,'المعانقة','17','0','0');
+INSERT INTO [t2915] VALUES (18,'المعوذتان','18','0','0');
+INSERT INTO [t2915] VALUES (19,'المفصل','19','0','0');
+INSERT INTO [t2915] VALUES (20,'الوقف اللازم','20','0','0');
