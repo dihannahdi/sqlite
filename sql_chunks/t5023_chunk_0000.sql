2e8120d7cd1a00251a58057df69062d263613611
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:35 +0700

    Upload SQL chunk: t5023_chunk_0000.sql

---
+++ sql_chunks/t5023_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5023
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5023" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5023
+INSERT INTO [t5023] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t5023] VALUES (2,'كيف نفهم التوحيد','3','0','0');
