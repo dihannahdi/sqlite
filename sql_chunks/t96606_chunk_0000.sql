c75265c61f69ff1781bc49511bda2eacebc37265
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:10 +0700

    Upload SQL chunk: t96606_chunk_0000.sql

---
+++ sql_chunks/t96606_chunk_0000.sql
@@ -0,0 +1,42 @@
+-- Table: t96606
+-- Chunk: 1
+-- Rows: 35
+
+CREATE TABLE "t96606" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96606
+INSERT INTO [t96606] VALUES (1,'باب الغصب','4','0','0');
+INSERT INTO [t96606] VALUES (2,'باب الاختلاف في العيب','8','0','0');
+INSERT INTO [t96606] VALUES (3,'باب بيع الثمار قبل أن يبدو صلاحها','13','0','0');
+INSERT INTO [t96606] VALUES (4,'باب المضاربة','23','0','0');
+INSERT INTO [t96606] VALUES (5,'باب السلم','26','0','0');
+INSERT INTO [t96606] VALUES (6,'باب الشفعة','28','0','0');
+INSERT INTO [t96606] VALUES (7,'باب المزارعة','34','0','0');
+INSERT INTO [t96606] VALUES (8,'باب الدعوى والصلح','36','0','0');
+INSERT INTO [t96606] VALUES (9,'باب الصدقة والهبة','38','0','0');
+INSERT INTO [t96606] VALUES (10,'باب في الوديعة','43','0','0');
+INSERT INTO [t96606] VALUES (11,'باب في الرهن','46','0','0');
+INSERT INTO [t96606] VALUES (12,'باب الحوالة والكفالة في الدين','47','0','0');
+INSERT INTO [t96606] VALUES (13,'باب في الدين','54','0','0');
+INSERT INTO [t96606] VALUES (14,'باب في الأيمان','71','0','0');
+INSERT INTO [t96606] VALUES (15,'باب الوصايا','74','0','0');
+INSERT INTO [t96606] VALUES (16,'باب المواريث','76','0','0');
+INSERT INTO [t96606] VALUES (17,'باب في الأوصياء','83','0','0');
+INSERT INTO [t96606] VALUES (18,'باب في الشركة والعتق وغيره','86','0','0');
+INSERT INTO [t96606] VALUES (19,'باب في المكاتب','91','0','0');
+INSERT INTO [t96606] VALUES (20,'باب في الأيمان','93','0','0');
+INSERT INTO [t96606] VALUES (21,'باب في العارية وأكل الغلة','97','0','0');
+INSERT INTO [t96606] VALUES (22,'باب في الأجير والإجارة','98','0','0');
+INSERT INTO [t96606] VALUES (23,'باب القسمة','101','0','0');
+INSERT INTO [t96606] VALUES (24,'باب الصلاة','102','0','0');
+INSERT INTO [t96606] VALUES (25,'باب صلاة الخوف','108','0','0');
+INSERT INTO [t96606] VALUES (26,'باب الزكاة','115','0','0');
+INSERT INTO [t96606] VALUES (27,'باب الصيام','124','0','0');
+INSERT INTO [t96606] VALUES (28,'باب في الحج','129','0','0');
+INSERT INTO [t96606] VALUES (29,'باب الديات','136','0','0');
+INSERT INTO [t96606] VALUES (30,'باب السرقة','145','0','0');
+INSERT INTO [t96606] VALUES (31,'باب القضاء','150','0','0');
+INSERT INTO [t96606] VALUES (32,'باب الفرية','155','0','0');
+INSERT INTO [t96606] VALUES (33,'باب النكاح','161','0','0');
+INSERT INTO [t96606] VALUES (34,'باب الطلاق','177','0','0');
+INSERT INTO [t96606] VALUES (35,'باب الحدود','207','0','0');
