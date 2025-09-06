6e41b96155a4ca35e90755983bd35abb727017dc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:28 +0700

    Upload SQL chunk: t743_chunk_0000.sql

---
+++ sql_chunks/t743_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t743
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t743" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t743
+INSERT INTO [t743] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t743] VALUES (2,'الباب الأول','2','1','0');
+INSERT INTO [t743] VALUES (3,'الحماسة والافتخار','2','0','0');
+INSERT INTO [t743] VALUES (4,'الباب الثاني','24','3','0');
+INSERT INTO [t743] VALUES (5,'الأدب والحكم والأمثال','24','0','0');
+INSERT INTO [t743] VALUES (6,'الباب الثالث','43','5','0');
+INSERT INTO [t743] VALUES (7,'النسيب','43','0','0');
