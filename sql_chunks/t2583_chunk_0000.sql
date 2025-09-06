0d5eb0aeb1543fe5c21df4b30e7d6a21c7520c02
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:00 +0700

    Upload SQL chunk: t2583_chunk_0000.sql

---
+++ sql_chunks/t2583_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t2583
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t2583" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2583
+INSERT INTO [t2583] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t2583] VALUES (2,'أهمية الدراسة','3','0','0');
+INSERT INTO [t2583] VALUES (3,'أهداف الدراسة','6','0','0');
+INSERT INTO [t2583] VALUES (4,'تساؤلات الدراسة','8','0','0');
+INSERT INTO [t2583] VALUES (5,'فروض الدراسة','10','0','0');
+INSERT INTO [t2583] VALUES (6,'منهج الدراسة','11','0','0');
+INSERT INTO [t2583] VALUES (7,'حدود الدراسة','12','0','0');
+INSERT INTO [t2583] VALUES (8,'تعريفات المصطلحات','13','0','0');
+INSERT INTO [t2583] VALUES (9,'أولا: التعريف اللغوي للترويح','13','8','0');
+INSERT INTO [t2583] VALUES (10,'ثانيا: التعريف الإصطلاحي والإجرائي للترويح','14','8','0');
+INSERT INTO [t2583] VALUES (11,'الجانب النظري','15','0','0');
+INSERT INTO [t2583] VALUES (12,'أولا: الفرق بين الترويح المباح وغير المباح','15','11','0');
+INSERT INTO [t2583] VALUES (13,'ثانيا: الأحادبث الواردة في ترويحه صلى الله عليه وسلم للأطفال','21','11','0');
+INSERT INTO [t2583] VALUES (14,'ثالثا: آراء العلماء المسلمين نحو الترويح','26','11','0');
+INSERT INTO [t2583] VALUES (15,'الدراسات السابقة','33','0','0');
+INSERT INTO [t2583] VALUES (16,'تعليق على الدراسات السابقة','36','0','0');
+INSERT INTO [t2583] VALUES (17,'الطريقة وإجراءات الدراسة','39','0','0');
+INSERT INTO [t2583] VALUES (18,'عينة الدراسة','40','0','0');
+INSERT INTO [t2583] VALUES (19,'جدول رقم (١)','40','18','0');
+INSERT INTO [t2583] VALUES (20,'جدول رقم (٢)','41','18','0');
+INSERT INTO [t2583] VALUES (21,'جدول رقم (٣)','45','18','0');
+INSERT INTO [t2583] VALUES (22,'جدول رقم (٤)','48','18','0');
+INSERT INTO [t2583] VALUES (23,'جدول رقم (٥)','50','18','0');
+INSERT INTO [t2583] VALUES (24,'جدول رقم (٦)','51','18','0');
+INSERT INTO [t2583] VALUES (25,'جدول رقم (٧)','53','18','0');
+INSERT INTO [t2583] VALUES (26,'جدول رقم (٨)','56','18','0');
+INSERT INTO [t2583] VALUES (27,'جدول رقم (٩)','58','18','0');
+INSERT INTO [t2583] VALUES (28,'جدول رقم (١٠)','60','18','0');
+INSERT INTO [t2583] VALUES (29,'الخلاصة والتوصيات والمقترحات','63','0','0');
+INSERT INTO [t2583] VALUES (30,'الخلاصة','63','29','0');
+INSERT INTO [t2583] VALUES (31,'التوصيات','65','29','0');
+INSERT INTO [t2583] VALUES (32,'المقترحات','67','29','0');
+INSERT INTO [t2583] VALUES (33,'الملحقات','68','0','0');
+INSERT INTO [t2583] VALUES (34,'ملحق رقم (١)','68','33','0');
+INSERT INTO [t2583] VALUES (35,'ملحق رقم (٢)','69','33','0');
+INSERT INTO [t2583] VALUES (36,'مصادر ومراجع','73','0','0');
