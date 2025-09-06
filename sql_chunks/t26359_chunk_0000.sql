42a8057a52345077e4c2e41d9945bd7fc811711f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:02 +0700

    Upload SQL chunk: t26359_chunk_0000.sql

---
+++ sql_chunks/t26359_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t26359
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t26359" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26359
+INSERT INTO [t26359] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26359] VALUES (2,'من حديث محمد بن عثمان بن كرامة ومن حديث طاهر بن خالد بن نزار الأيلي رواية محمد بن مخلد العطار عنهما','2','0','0');
+INSERT INTO [t26359] VALUES (3,'[أحاديث طاهر بن خالد بن نزار]','4','0','0');
+INSERT INTO [t26359] VALUES (4,'[أحاديث محمد بن عثمان بن كرامة]','29','0','0');
+INSERT INTO [t26359] VALUES (5,'[أحاديث الحسن بن عرفة العبدي]','66','0','0');
+INSERT INTO [t26359] VALUES (6,'[أحاديث لأبي عبد الله محمد بن مخلد العطار عن شيوخه]','75','0','0');
