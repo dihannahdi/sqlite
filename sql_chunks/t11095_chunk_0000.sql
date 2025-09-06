223962779936a268d0aadb3a141c347dfa8edf96
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:12 +0700

    Upload SQL chunk: t11095_chunk_0000.sql

---
+++ sql_chunks/t11095_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t11095
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t11095" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11095
+INSERT INTO [t11095] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11095] VALUES (2,'علم الفرائض','6','0','0');
+INSERT INTO [t11095] VALUES (3,'الإرث','13','0','0');
+INSERT INTO [t11095] VALUES (4,'فروع تتعلق بأسباب الإرث','17','0','0');
+INSERT INTO [t11095] VALUES (5,'موانع الإرث','23','0','0');
+INSERT INTO [t11095] VALUES (6,'فروع تتعلق بموانع الإرث','27','0','0');
+INSERT INTO [t11095] VALUES (7,'أقسام الإرث','29','0','0');
+INSERT INTO [t11095] VALUES (8,'أصحاب الفروض','30','0','0');
+INSERT INTO [t11095] VALUES (9,'قواعد في الفروض وأهلها','52','0','0');
+INSERT INTO [t11095] VALUES (10,'العصبة','54','0','0');
+INSERT INTO [t11095] VALUES (11,'الحجب','63','0','0');
+INSERT INTO [t11095] VALUES (12,'باب ذوي الأرحام','67','0','0');
+INSERT INTO [t11095] VALUES (13,'التأصيل والتصحيح','76','0','0');
+INSERT INTO [t11095] VALUES (14,'التصحيح','85','0','0');
+INSERT INTO [t11095] VALUES (15,'المناسخات','91','0','0');
+INSERT INTO [t11095] VALUES (16,'عمل مسائل الرد','104','0','0');
+INSERT INTO [t11095] VALUES (17,'قسمة التركات','108','0','0');
+INSERT INTO [t11095] VALUES (18,'ميراث الحمل','116','0','0');
+INSERT INTO [t11095] VALUES (19,'ميراث المفقود','121','0','0');
+INSERT INTO [t11095] VALUES (20,'الخنثى المشكل','126','0','0');
+INSERT INTO [t11095] VALUES (21,'الغرقى والهدمى','127','0','0');
