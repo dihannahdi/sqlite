fec9a102831e71964bbef68e1c1faac76d2d772c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:49 +0700

    Upload SQL chunk: t5954_chunk_0000.sql

---
+++ sql_chunks/t5954_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t5954
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t5954" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5954
+INSERT INTO [t5954] VALUES (1,'تم الجزء','97','0','0');
