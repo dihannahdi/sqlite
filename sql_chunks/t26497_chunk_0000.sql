d22832033e41220328a09be433b24811b7c01922
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:53 +0700

    Upload SQL chunk: t26497_chunk_0000.sql

---
+++ sql_chunks/t26497_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26497
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26497" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26497
+INSERT INTO [t26497] VALUES (1,'مجلس من أمالي الحافظ (أحمد) بن موسى بن مردويه','1','0','0');
+INSERT INTO [t26497] VALUES (2,'مجلس آخر من الآمالي وهو في صفات الله عز وجل','19','0','0');
