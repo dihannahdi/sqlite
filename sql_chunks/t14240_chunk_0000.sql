15f414af13209f9870ec6f399bd2e6b4b883340e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:17:11 +0700

    Upload SQL chunk: t14240_chunk_0000.sql

---
+++ sql_chunks/t14240_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t14240
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t14240" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14240
+INSERT INTO [t14240] VALUES (1,'أحمد محمد شاكر "إمام المحدثين" -بقلم- محمود محمد شاكر','3','0','0');
+INSERT INTO [t14240] VALUES (2,'مقدمة المؤلف','11','0','0');
+INSERT INTO [t14240] VALUES (3,'الكتاب والسنة، يجب أن يكونا مصدر القوانين في مصر','15','0','0');
+INSERT INTO [t14240] VALUES (4,'الخطة العملية لاقتباس القوانين من الشريعة','44','0','0');
+INSERT INTO [t14240] VALUES (5,'الشرع واللغة','49','0','0');
+INSERT INTO [t14240] VALUES (6,'عبد العزيز فهمي باشا وعداؤه للعربية.','50','5','0');
