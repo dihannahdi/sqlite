b990d99045d2f535d651010340cad8265a985801
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:28 +0700

    Upload SQL chunk: t30625_chunk_0000.sql

---
+++ sql_chunks/t30625_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t30625
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t30625" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30625
+INSERT INTO [t30625] VALUES (1,'من حديث عبد العزيز الأزجي','4','0','0');
+INSERT INTO [t30625] VALUES (2,'من حديث ابن الطيوري','31','0','0');
+INSERT INTO [t30625] VALUES (3,'من حديث الشريف أبي عبد الله العلوي','34','0','0');
+INSERT INTO [t30625] VALUES (4,'من فوائد الشريف أبي منصور بن الذبخ','51','0','0');
+INSERT INTO [t30625] VALUES (5,'من حديث الحارث بن أبي أسامة','54','0','0');
+INSERT INTO [t30625] VALUES (6,'من فوائد الطيوري','61','0','0');
+INSERT INTO [t30625] VALUES (7,'من حديث عمر بن أيوب السقطي','62','0','0');
+INSERT INTO [t30625] VALUES (8,'من حديث الأزجي','66','0','0');
+INSERT INTO [t30625] VALUES (9,'من حديث السمرقندي','68','0','0');
+INSERT INTO [t30625] VALUES (10,'من حديث النهرواني','78','0','0');
+INSERT INTO [t30625] VALUES (11,'من فوائد السمرقندي','81','0','0');
+INSERT INTO [t30625] VALUES (12,'من حديث أبي زكريا العاري','86','0','0');
