1ed2d46dc8ad6fb1aefa0bdc0db4f0aad4856f2a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:05 +0700

    Upload SQL chunk: t7043_chunk_0000.sql

---
+++ sql_chunks/t7043_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7043
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7043" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7043
+INSERT INTO [t7043] VALUES (1,'بسم الله الرحمن الرحيم وبه نستعين','1','0','0');
+INSERT INTO [t7043] VALUES (2,'فصل فيما يؤنث من الأعضاء','271','0','0');
