a003678873f41dde4d0343282baae78202072d12
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:58 +0700

    Upload SQL chunk: t36066_chunk_0000.sql

---
+++ sql_chunks/t36066_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t36066
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t36066" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36066
+INSERT INTO [t36066] VALUES (1,'المقدمة في تعريف الصحابي','6','0','0');
+INSERT INTO [t36066] VALUES (2,'الفصل الأول ففي بيان أن الصحابة عدول','10','0','0');
+INSERT INTO [t36066] VALUES (3,'الفصل الثاني فيما شجر بين الصحابة','31','0','0');
+INSERT INTO [t36066] VALUES (4,'وقعة الجمل','31','3','0');
+INSERT INTO [t36066] VALUES (5,'وقعة صفين','37','3','0');
+INSERT INTO [t36066] VALUES (6,'الفصل الثالث في بيان حكم سب الصحابة','44','0','0');
+INSERT INTO [t36066] VALUES (7,'الخاتمة في تفاوت الصحابة في الفضل','60','0','0');
