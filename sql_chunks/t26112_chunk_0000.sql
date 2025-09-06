32249903fd2e2e147ba792b5ce995daa6ba7bc2a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:29 +0700

    Upload SQL chunk: t26112_chunk_0000.sql

---
+++ sql_chunks/t26112_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t26112
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t26112" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26112
+INSERT INTO [t26112] VALUES (1,'الطبقة الأولى','5','0','0');
+INSERT INTO [t26112] VALUES (2,'الطبقة الثانية','7','0','0');
+INSERT INTO [t26112] VALUES (3,'الطبقة الثالثة','10','0','0');
+INSERT INTO [t26112] VALUES (4,'الطبقة الرابعة','15','0','0');
+INSERT INTO [t26112] VALUES (5,'الطبقة الخامسة','22','0','0');
+INSERT INTO [t26112] VALUES (6,'الطبقة السادسة','28','0','0');
+INSERT INTO [t26112] VALUES (7,'الطبقة السابعة','32','0','0');
+INSERT INTO [t26112] VALUES (8,'الطبقة الثامنة','34','0','0');
+INSERT INTO [t26112] VALUES (9,'الطبقة التاسعة','37','0','0');
+INSERT INTO [t26112] VALUES (10,'الطبقة العاشرة','38','0','0');
+INSERT INTO [t26112] VALUES (11,'الطبقة الحادية عشر','39','0','0');
+INSERT INTO [t26112] VALUES (12,'الطبقة الثانية عشرة','42','0','0');
+INSERT INTO [t26112] VALUES (13,'الطبقة الثالثة عشرة','43','0','0');
+INSERT INTO [t26112] VALUES (14,'الطبقة الرابعة عشرة','44','0','0');
+INSERT INTO [t26112] VALUES (15,'الطبقة الخامسة عشر','44','0','0');
+INSERT INTO [t26112] VALUES (16,'الطبقة السادسة عشر','45','0','0');
+INSERT INTO [t26112] VALUES (17,'الطبقة السابعة عشرة','47','0','0');
+INSERT INTO [t26112] VALUES (18,'الطبقة الثامنة عشرة','49','0','0');
+INSERT INTO [t26112] VALUES (19,'الطبقة التاسعة عشرة','50','0','0');
+INSERT INTO [t26112] VALUES (20,'الطبقة العشرون','51','0','0');
+INSERT INTO [t26112] VALUES (21,'الطبقة الحادية والعشرون','53','0','0');
+INSERT INTO [t26112] VALUES (22,'الطبقة الثانية والعشرون','55','0','0');
