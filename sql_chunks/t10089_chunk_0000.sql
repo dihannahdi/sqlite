b2a103728fdc9bb2a9653fdb535683773ab3eba7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:43 +0700

    Upload SQL chunk: t10089_chunk_0000.sql

---
+++ sql_chunks/t10089_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t10089
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t10089" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10089
+INSERT INTO [t10089] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10089] VALUES (2,'نشأة التصوف وتطوره:','3','0','0');
+INSERT INTO [t10089] VALUES (3,'التفسير الصوفي النظري أو الفلسفي','6','0','0');
+INSERT INTO [t10089] VALUES (4,'ابن عربي والتفسير النظري الفلسفي','8','0','0');
+INSERT INTO [t10089] VALUES (5,'كلمة الحق في التفسير النظري الفلسفي','11','0','0');
+INSERT INTO [t10089] VALUES (6,'التفسير الصوفي الفيضي أو الإشاري','15','0','0');
+INSERT INTO [t10089] VALUES (7,'موقف ابن عربي من التفسير الإشاري','21','0','0');
+INSERT INTO [t10089] VALUES (8,'رأينا في مقالة ابن عربي','27','0','0');
+INSERT INTO [t10089] VALUES (9,'حقيقة التفسير المنسوب لابن عربي','30','0','0');
