6d9b275aeeeafe94c478b1816bd36f6708a59914
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:28:39 +0700

    Upload SQL chunk: t17090_chunk_0000.sql

---
+++ sql_chunks/t17090_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t17090
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t17090" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17090
+INSERT INTO [t17090] VALUES (1,'الفصل الأول: الصلوات الخمس كفارة لما بينها','2','0','0');
+INSERT INTO [t17090] VALUES (2,'الفصل الثاني: ورد غفران جميع الذنوب من غير تقييد بالتوبة','13','0','0');
+INSERT INTO [t17090] VALUES (3,'الفصل الثالث: الخصال المكفرة للذنوب المتقدمة والمتأخرة','23','0','0');
+INSERT INTO [t17090] VALUES (4,'الطهارة','23','3','0');
+INSERT INTO [t17090] VALUES (5,'سماع الآذان','23','3','0');
+INSERT INTO [t17090] VALUES (6,'صلاة التسبيح','23','3','0');
+INSERT INTO [t17090] VALUES (7,'التأمين في الصلاة','25','3','0');
+INSERT INTO [t17090] VALUES (8,'صلاة الضحى','25','3','0');
+INSERT INTO [t17090] VALUES (9,'القراءة بعد الجمعة','25','3','0');
+INSERT INTO [t17090] VALUES (10,'قيام رمضان وصيامه وإحياء ليلة القدر','25','3','0');
+INSERT INTO [t17090] VALUES (11,'صيام يوم عرفة','27','3','0');
+INSERT INTO [t17090] VALUES (12,'إن صح الإهلال من المسجد الأقصى','27','3','0');
+INSERT INTO [t17090] VALUES (13,'الحج الخالص','28','3','0');
+INSERT INTO [t17090] VALUES (14,'القراءة والأذكار','29','3','0');
+INSERT INTO [t17090] VALUES (15,'فضائل الرباط بعكا','30','3','0');
+INSERT INTO [t17090] VALUES (16,'قود الأعمى','30','3','0');
+INSERT INTO [t17090] VALUES (17,'فضل السعي في حاجة المسلم','30','3','0');
+INSERT INTO [t17090] VALUES (18,'فضائل المصافحة','30','3','0');
+INSERT INTO [t17090] VALUES (19,'فضل حمد لله بعد الأكل واللبس','30','3','0');
+INSERT INTO [t17090] VALUES (20,'فضل التعمير في الإسلام','31','3','0');
