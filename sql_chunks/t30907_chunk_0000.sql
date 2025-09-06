fba2e4dd0ce1d718eecd5bfbeb1beb4f3b7b9bf4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:34 +0700

    Upload SQL chunk: t30907_chunk_0000.sql

---
+++ sql_chunks/t30907_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t30907
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t30907" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30907
+INSERT INTO [t30907] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t30907] VALUES (2,'مقصود القدوة ومعناها','4','0','0');
+INSERT INTO [t30907] VALUES (3,'أنواع القدوة','4','2','0');
+INSERT INTO [t30907] VALUES (4,'أهمية القدوة الحسنة','6','0','0');
+INSERT INTO [t30907] VALUES (5,'أصول القدوة','11','0','0');
+INSERT INTO [t30907] VALUES (6,'الأصل الأول الصلاح','11','5','0');
+INSERT INTO [t30907] VALUES (7,'الأصل الثاني حسن الخلق','14','5','0');
+INSERT INTO [t30907] VALUES (8,'الصدق','14','7','0');
+INSERT INTO [t30907] VALUES (9,'الصبر','16','7','0');
+INSERT INTO [t30907] VALUES (10,'الرحمة','18','7','0');
+INSERT INTO [t30907] VALUES (11,'التواضع','20','7','0');
+INSERT INTO [t30907] VALUES (12,'الرفق','22','7','0');
+INSERT INTO [t30907] VALUES (13,'الأصل الثالث موافقة القول العمل','27','5','0');
+INSERT INTO [t30907] VALUES (14,'الحث على موافقة القول العمل والتحذير من مخالفة ذلك','27','13','0');
+INSERT INTO [t30907] VALUES (15,'شواهد حية في مواقف القدوة','31','13','0');
+INSERT INTO [t30907] VALUES (16,'توضيح موقف وتفسير تصرف','31','15','0');
+INSERT INTO [t30907] VALUES (17,'نبي الملحمة','34','15','0');
+INSERT INTO [t30907] VALUES (18,'أمنة للصحب الكرام','36','15','0');
+INSERT INTO [t30907] VALUES (19,'كرهت أن يحبسني','36','15','0');
