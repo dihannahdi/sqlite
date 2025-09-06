a8d2c75188e5e08ffce8b0402f63cc88ea8c6ac5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:27 +0700

    Upload SQL chunk: t26558_chunk_0000.sql

---
+++ sql_chunks/t26558_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t26558
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t26558" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26558
+INSERT INTO [t26558] VALUES (1,'أمالي عبد الغافر الفارسي','2','0','0');
+INSERT INTO [t26558] VALUES (2,'مجلس آخر من أماليه','7','0','0');
+INSERT INTO [t26558] VALUES (3,'مجلس آخر من أماليه','10','0','0');
+INSERT INTO [t26558] VALUES (4,'مجلس آخر من أماليه','16','0','0');
