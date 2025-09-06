76264480bf011f339b1aaace102d3e5a6aeeb87a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:32 +0700

    Upload SQL chunk: t5701_chunk_0000.sql

---
+++ sql_chunks/t5701_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t5701
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t5701" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5701
+INSERT INTO [t5701] VALUES (1,'تقديم','2','0','0');
+INSERT INTO [t5701] VALUES (2,'المقدمة','4','0','0');
+INSERT INTO [t5701] VALUES (3,'حقيقة العين والسحر، وطرق العلاج','14','0','0');
+INSERT INTO [t5701] VALUES (4,'التعريف بالرقية وبيان أنواعها','18','0','0');
+INSERT INTO [t5701] VALUES (5,'ضابط الرقية المشروعة','19','0','0');
+INSERT INTO [t5701] VALUES (6,'شروط الانتفاع التام بالرقية','20','0','0');
+INSERT INTO [t5701] VALUES (7,'أسباب تساعد في تعجيل الشفاء','22','0','0');
+INSERT INTO [t5701] VALUES (8,'أسباب عشرة يندفع بها شر الحاسد وغيره','31','0','0');
+INSERT INTO [t5701] VALUES (9,'أمور تهون من أمر المصيبة','34','0','0');
+INSERT INTO [t5701] VALUES (10,'القسم الأول (رقية موجزة)','41','0','0');
+INSERT INTO [t5701] VALUES (11,'القسم الثاني (رقية متوسطة)','50','0','0');
+INSERT INTO [t5701] VALUES (12,'القسم الثالث (رقية مطولة)','76','0','0');
+INSERT INTO [t5701] VALUES (13,'(الملحق الأول) العلاج بالماء والسدر والزيت والعسل','151','0','0');
+INSERT INTO [t5701] VALUES (14,'(الملحق الثاني) الأذكار والتحصينات','161','0','0');
+INSERT INTO [t5701] VALUES (15,'(الملحق الثالث) بعض أعراض السحر والمس والعين','178','0','0');
+INSERT INTO [t5701] VALUES (16,'خاتمة','189','0','0');
