608b72d849dd9f1fe06346dfa92af924d31fdb46
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:22 +0700

    Upload SQL chunk: t6009_chunk_0000.sql

---
+++ sql_chunks/t6009_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6009
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6009" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6009
+INSERT INTO [t6009] VALUES (1,'بسم الله الرحمن الرحيم وبه نستعي','1','0','0');
+INSERT INTO [t6009] VALUES (2,'النساء','38','0','0');
+INSERT INTO [t6009] VALUES (3,'بقية روايات الصحابة عن التابعين','48','0','0');
