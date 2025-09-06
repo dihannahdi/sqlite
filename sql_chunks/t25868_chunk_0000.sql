f0f46b8bfd21fb98e12232f3a860daf84ebbad43
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:06 +0700

    Upload SQL chunk: t25868_chunk_0000.sql

---
+++ sql_chunks/t25868_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t25868
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t25868" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t25868
+INSERT INTO [t25868] VALUES (1,'ذكر المدلسين','1','0','0');
+INSERT INTO [t25868] VALUES (2,'١. الحسن','1','0','0');
+INSERT INTO [t25868] VALUES (3,'٢. وقتادة','2','0','0');
+INSERT INTO [t25868] VALUES (4,'٣. وحميد الطويل','3','0','0');
+INSERT INTO [t25868] VALUES (5,'٤. ويحيى بن أبي كثير','4','0','0');
+INSERT INTO [t25868] VALUES (6,'٥. والتيمي','5','0','0');
+INSERT INTO [t25868] VALUES (7,'٦. ويونس بن عبيد','6','0','0');
+INSERT INTO [t25868] VALUES (8,'٧. وابن أبي عروبة','7','0','0');
+INSERT INTO [t25868] VALUES (9,'٨. وهشيم','8','0','0');
+INSERT INTO [t25868] VALUES (10,'٩. وأبو إسحاق السبيعي','9','0','0');
+INSERT INTO [t25868] VALUES (11,'١٠. وإسماعيل بن أبي خالد','10','0','0');
+INSERT INTO [t25868] VALUES (12,'١١. والحكم','11','0','0');
+INSERT INTO [t25868] VALUES (13,'١٢. والحجاج بن أرطاة','12','0','0');
+INSERT INTO [t25868] VALUES (14,'١٣. ومغيرة','13','0','0');
+INSERT INTO [t25868] VALUES (15,'١٤. والثوري','14','0','0');
+INSERT INTO [t25868] VALUES (16,'١٥. وأبو الزبير المكي','15','0','0');
+INSERT INTO [t25868] VALUES (17,'١٦. وابن أبي نجيح','16','0','0');
+INSERT INTO [t25868] VALUES (18,'١٧. [وابن جريج]','17','0','0');
+INSERT INTO [t25868] VALUES (19,'١٨. وابن عيينة','18','0','0');
+INSERT INTO [t25868] VALUES (20,'ملحق','19','0','0');
+INSERT INTO [t25868] VALUES (21,'١. سليمان بن مهران الأعمش [الطبقة الثانية] .','19','20','0');
+INSERT INTO [t25868] VALUES (22,'٢. وعباد بن منصور الناجي البصري [الطبقة الرابعة]','19','20','0');
+INSERT INTO [t25868] VALUES (23,'٣. وميمون بن موسى المرئي [الطبقة الثالثة]','19','20','0');
+INSERT INTO [t25868] VALUES (24,'ويحيى بن أبي حية الكلبي [الطبقة الخامسة]','19','20','0');
