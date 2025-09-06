1e551ef039661b883f89e96ba71fa5197848106f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:27 +0700

    Upload SQL chunk: t6136_chunk_0000.sql

---
+++ sql_chunks/t6136_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6136
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6136" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6136
+INSERT INTO [t6136] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t6136] VALUES (2,'خاتمة','337','0','0');
