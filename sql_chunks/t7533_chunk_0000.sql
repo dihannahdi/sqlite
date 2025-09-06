9cad29fbbcde232e0580d2e41d38c683e6547bfd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:31 +0700

    Upload SQL chunk: t7533_chunk_0000.sql

---
+++ sql_chunks/t7533_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7533
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7533" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7533
+INSERT INTO [t7533] VALUES (1,'القصيدة النونية','1','0','0');
+INSERT INTO [t7533] VALUES (2,'بسم الله الرحمن الرحيم وبه نستعين','1','0','0');
