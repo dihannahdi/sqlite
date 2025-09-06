4f431570f8acb709af84de710b9602894c76adb8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:27 +0700

    Upload SQL chunk: t12066_chunk_0000.sql

---
+++ sql_chunks/t12066_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t12066
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t12066" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12066
+INSERT INTO [t12066] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12066] VALUES (2,'الإمام أحمد بن حنبل','3','0','0');
+INSERT INTO [t12066] VALUES (3,'شيخ الاسلام أحمد بن تيمية','17','0','0');
+INSERT INTO [t12066] VALUES (4,'شيخ الإسلام محمد بن عبد الوهاب','43','0','0');
