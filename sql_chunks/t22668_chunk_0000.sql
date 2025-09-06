998e0a3101d81c72b75e92cdc19119976d239023
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:05 +0700

    Upload SQL chunk: t22668_chunk_0000.sql

---
+++ sql_chunks/t22668_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t22668
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t22668" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22668
+INSERT INTO [t22668] VALUES (1,'مسألة فيما إذا كان في العبد محبة','1','0','0');
+INSERT INTO [t22668] VALUES (2,'طرق العلم الثلاثة','3','0','0');
+INSERT INTO [t22668] VALUES (3,'١- البصر','3','0','0');
+INSERT INTO [t22668] VALUES (4,'٢- السمع','3','0','0');
+INSERT INTO [t22668] VALUES (5,'٣- العقل (القلب)','4','0','0');
