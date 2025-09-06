0ef4c7e1f1c26d090fc468768a7ac5b521fa3094
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:07 +0700

    Upload SQL chunk: t26886_chunk_0000.sql

---
+++ sql_chunks/t26886_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26886
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26886" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26886
+INSERT INTO [t26886] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26886] VALUES (2,'جزء فيه المنظوم والمنثور','2','0','0');
