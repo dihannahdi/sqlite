bbf749586bc49f284b4d215409e66b90e322d932
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:46 +0700

    Upload SQL chunk: t8937_chunk_0000.sql

---
+++ sql_chunks/t8937_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8937
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8937" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8937
+INSERT INTO [t8937] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8937] VALUES (2,'المصالح المرسلة','4','0','0');
