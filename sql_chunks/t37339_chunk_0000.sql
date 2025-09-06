bd042ca6a3a34a39acc0c63e153d267e5db6de73
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:24 +0700

    Upload SQL chunk: t37339_chunk_0000.sql

---
+++ sql_chunks/t37339_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t37339
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t37339" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37339
+INSERT INTO [t37339] VALUES (1,'فضل تعلم القرآن وتعليمه','2','0','0');
+INSERT INTO [t37339] VALUES (2,'بين يدي تفسير الآية','7','0','0');
+INSERT INTO [t37339] VALUES (3,'الموضوع العام للسورة','7','0','0');
+INSERT INTO [t37339] VALUES (4,'تفسير الآية','8','0','0');
+INSERT INTO [t37339] VALUES (5,'المفردات','8','0','0');
+INSERT INTO [t37339] VALUES (6,'المعنى الإجمالي','8','0','0');
+INSERT INTO [t37339] VALUES (7,'المعنى التفصيلي','8','0','0');
