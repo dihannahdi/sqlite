2b78df69ad5fe8649b6a1096509e5b39006e4d50
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:24 +0700

    Upload SQL chunk: t30051_chunk_0000.sql

---
+++ sql_chunks/t30051_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t30051
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t30051" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30051
+INSERT INTO [t30051] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t30051] VALUES (2,'وقد فضل الله تعالى نبينا محمد صلى الله عليه وسلم بوجوه','3','0','0');
+INSERT INTO [t30051] VALUES (3,'أولها: أنه ساد الكل','3','0','0');
+INSERT INTO [t30051] VALUES (4,'وحسبنا الله ونعم الوكيل، ولا حول ولا قوة إلا بالله العلي العظيم','23','0','0');
