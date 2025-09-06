b5c037dd338b9024f2779988bf3f07bfa0d79174
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:18 +0700

    Upload SQL chunk: t29555_chunk_0000.sql

---
+++ sql_chunks/t29555_chunk_0000.sql
@@ -0,0 +1,55 @@
+-- Table: t29555
+-- Chunk: 1
+-- Rows: 48
+
+CREATE TABLE "t29555" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29555
+INSERT INTO [t29555] VALUES (1,'مسألة [١]','3','0','0');
+INSERT INTO [t29555] VALUES (2,'مسألة [٣]','6','0','0');
+INSERT INTO [t29555] VALUES (3,'مسألة [٤]','8','0','0');
+INSERT INTO [t29555] VALUES (4,'مسألة [٥]','11','0','0');
+INSERT INTO [t29555] VALUES (5,'مسألة [٦]','13','0','0');
+INSERT INTO [t29555] VALUES (6,'مسألة [٧]','14','0','0');
+INSERT INTO [t29555] VALUES (7,'مسألة [٨]','17','0','0');
+INSERT INTO [t29555] VALUES (8,'مسألة [٩]','19','0','0');
+INSERT INTO [t29555] VALUES (9,'مسألة [١٠]','24','0','0');
+INSERT INTO [t29555] VALUES (10,'مسألة [١١]','25','0','0');
+INSERT INTO [t29555] VALUES (11,'مسألة [١٢]','28','0','0');
+INSERT INTO [t29555] VALUES (12,'مسألة [١٣]','32','0','0');
+INSERT INTO [t29555] VALUES (13,'مسألة [١٤]','33','0','0');
+INSERT INTO [t29555] VALUES (14,'مسألة [١٥]','35','0','0');
+INSERT INTO [t29555] VALUES (15,'مسألة [١٦]','37','0','0');
+INSERT INTO [t29555] VALUES (16,'مسألة [١٧]','39','0','0');
+INSERT INTO [t29555] VALUES (17,'مسألة [١٨]','41','0','0');
+INSERT INTO [t29555] VALUES (18,'مسألة [١٩]','41','0','0');
+INSERT INTO [t29555] VALUES (19,'مسألة [٢٠]','42','0','0');
+INSERT INTO [t29555] VALUES (20,'مسألة [٢١]','45','0','0');
+INSERT INTO [t29555] VALUES (21,'مسألة [٢٢]','45','0','0');
+INSERT INTO [t29555] VALUES (22,'مسألة [٢٣]','47','0','0');
+INSERT INTO [t29555] VALUES (23,'مسألة [٢٤]','48','0','0');
+INSERT INTO [t29555] VALUES (24,'مسألة [٢٥]','50','0','0');
+INSERT INTO [t29555] VALUES (25,'مسألة [٢٦]','52','0','0');
+INSERT INTO [t29555] VALUES (26,'مسألة [٢٧]','53','0','0');
+INSERT INTO [t29555] VALUES (27,'مسألة [٢٨]','56','0','0');
+INSERT INTO [t29555] VALUES (28,'مسألة [٢٩]','58','0','0');
+INSERT INTO [t29555] VALUES (29,'مسألة [٣٠]','60','0','0');
+INSERT INTO [t29555] VALUES (30,'مسألة [٣١]','61','0','0');
+INSERT INTO [t29555] VALUES (31,'مسألة [٣٢]','62','0','0');
+INSERT INTO [t29555] VALUES (32,'مسألة [٣٣]','63','0','0');
+INSERT INTO [t29555] VALUES (33,'مسألة [٣٤]','65','0','0');
+INSERT INTO [t29555] VALUES (34,'مسألة [٣٥]','67','0','0');
+INSERT INTO [t29555] VALUES (35,'مسألة [٣٦]','68','0','0');
+INSERT INTO [t29555] VALUES (36,'مسألة [٣٧]','69','0','0');
+INSERT INTO [t29555] VALUES (37,'مسألة [٣٨]','71','0','0');
+INSERT INTO [t29555] VALUES (38,'مسألة [٣٩]','73','0','0');
+INSERT INTO [t29555] VALUES (39,'مسألة [٤٠]','74','0','0');
+INSERT INTO [t29555] VALUES (40,'مسألة [٥٦]','100','0','0');
+INSERT INTO [t29555] VALUES (41,'مسألة [٥٧]','102','0','0');
+INSERT INTO [t29555] VALUES (42,'مسألة [١٢٧]','222','0','0');
+INSERT INTO [t29555] VALUES (43,'مسألة [١٢٨]','223','0','0');
+INSERT INTO [t29555] VALUES (44,'مسألة [١٢٩]','224','0','0');
+INSERT INTO [t29555] VALUES (45,'مسألة [١٣٠]','226','0','0');
+INSERT INTO [t29555] VALUES (46,'مسألة [١٣١]','227','0','0');
+INSERT INTO [t29555] VALUES (47,'مسألة [١٣٢]','228','0','0');
+INSERT INTO [t29555] VALUES (48,'مسألة [١٣٣]','230','0','0');
