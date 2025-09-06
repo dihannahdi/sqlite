4ae9425316b82b591e4109d386766b4b87785c80
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:36:48 +0700

    Upload SQL chunk: t2073_chunk_0000.sql

---
+++ sql_chunks/t2073_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t2073
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t2073" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2073
+INSERT INTO [t2073] VALUES (1,'أصول الفقه','1','0','0');
+INSERT INTO [t2073] VALUES (2,'الأحكام الشرعية','2','0','0');
+INSERT INTO [t2073] VALUES (3,'الصحيح والفاسد','5','0','0');
+INSERT INTO [t2073] VALUES (4,'أقسام اللفظ من حيث الدلالة','6','0','0');
+INSERT INTO [t2073] VALUES (5,'تقسيم الكلام إلى حقيقة ومجاز','11','0','0');
+INSERT INTO [t2073] VALUES (6,'الأمر','14','0','0');
+INSERT INTO [t2073] VALUES (7,'العام','22','0','0');
+INSERT INTO [t2073] VALUES (8,'الخاص','26','0','0');
+INSERT INTO [t2073] VALUES (9,'النسخ','31','0','0');
+INSERT INTO [t2073] VALUES (10,'الإجماع','34','0','0');
+INSERT INTO [t2073] VALUES (11,'الأخبار','39','0','0');
+INSERT INTO [t2073] VALUES (12,'تقسيم الخبر إلى متواتر وآحاد','40','0','0');
+INSERT INTO [t2073] VALUES (13,'القياس','48','0','0');
+INSERT INTO [t2073] VALUES (14,'الاجتهاد والتقليد','58','0','0');
