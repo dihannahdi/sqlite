c2eb630d1c2fb72451b9bc784897223228baa252
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:40 +0700

    Upload SQL chunk: t11228_chunk_0000.sql

---
+++ sql_chunks/t11228_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t11228
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t11228" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11228
+INSERT INTO [t11228] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11228] VALUES (2,'نعمة النطق والبيان','8','0','0');
+INSERT INTO [t11228] VALUES (3,'حفظ اللسان من الكلام إلا في خير','11','0','0');
+INSERT INTO [t11228] VALUES (4,'الظن والتجسس','22','0','0');
+INSERT INTO [t11228] VALUES (5,'الرفق واللين','26','0','0');
+INSERT INTO [t11228] VALUES (6,'موقف أهل السنة من العالم إذا أخطأ أنه يعذر فلا يبدع ولا يهجر','29','0','0');
+INSERT INTO [t11228] VALUES (7,'فتنة التجريح والهجر من بعض أهل السنة في هذا العصر، وطريق السلامة منها','42','0','0');
