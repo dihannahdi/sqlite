61336069c54040c8495bed4669eba82a19dd8115
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:11 +0700

    Upload SQL chunk: t36422_chunk_0000.sql

---
+++ sql_chunks/t36422_chunk_0000.sql
@@ -0,0 +1,55 @@
+-- Table: t36422
+-- Chunk: 1
+-- Rows: 48
+
+CREATE TABLE "t36422" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36422
+INSERT INTO [t36422] VALUES (1,'طرفة بن العبد','1','0','0');
+INSERT INTO [t36422] VALUES (2,'قافية الباء','8','0','0');
+INSERT INTO [t36422] VALUES (3,'كل من عليها فان','8','2','0');
+INSERT INTO [t36422] VALUES (4,'أدوا الحقوق','9','2','0');
+INSERT INTO [t36422] VALUES (5,'قافية التاء','10','0','0');
+INSERT INTO [t36422] VALUES (6,'الخيل المغيرة','10','5','0');
+INSERT INTO [t36422] VALUES (7,'قافية الحاء','11','0','0');
+INSERT INTO [t36422] VALUES (8,'القلب السقيم','11','7','0');
+INSERT INTO [t36422] VALUES (9,'هل من نصيح؟','12','7','0');
+INSERT INTO [t36422] VALUES (10,'الليلة مثل البارحة','13','7','0');
+INSERT INTO [t36422] VALUES (11,'قافية الدال','14','0','0');
+INSERT INTO [t36422] VALUES (12,'أطلال خولة','14','11','0');
+INSERT INTO [t36422] VALUES (13,'نبلاء السعي','25','11','0');
+INSERT INTO [t36422] VALUES (14,'أرى الموت','27','11','0');
+INSERT INTO [t36422] VALUES (15,'أبناء لبنة','28','11','0');
+INSERT INTO [t36422] VALUES (16,'الخير الخير','28','11','0');
+INSERT INTO [t36422] VALUES (17,'قافية الراء','29','0','0');
+INSERT INTO [t36422] VALUES (18,'القوم الكرام','29','17','0');
+INSERT INTO [t36422] VALUES (19,'رأيت القوافي','31','17','0');
+INSERT INTO [t36422] VALUES (20,'لا نحل ولا نسير','32','17','0');
+INSERT INTO [t36422] VALUES (21,'ولقد كنت عليكم عاتبا','33','17','0');
+INSERT INTO [t36422] VALUES (22,'أرانب قي ثياب الملوك','41','17','0');
+INSERT INTO [t36422] VALUES (23,'خالط الناس','43','17','0');
+INSERT INTO [t36422] VALUES (24,'لا بد يوما أن تصادي','43','17','0');
+INSERT INTO [t36422] VALUES (25,'قافية الصاد','44','0','0');
+INSERT INTO [t36422] VALUES (26,'لا توص حكيما','44','25','0');
+INSERT INTO [t36422] VALUES (27,'قافية الضاد','45','0','0');
+INSERT INTO [t36422] VALUES (28,'خذو حذركم','45','27','0');
+INSERT INTO [t36422] VALUES (29,'قافية الفاء','47','0','0');
+INSERT INTO [t36422] VALUES (30,'حماة الحي','47','29','0');
+INSERT INTO [t36422] VALUES (31,'قافية القاف','49','0','0');
+INSERT INTO [t36422] VALUES (32,'انع نفسك','49','31','0');
+INSERT INTO [t36422] VALUES (33,'شر الناس من سرق','49','31','0');
+INSERT INTO [t36422] VALUES (34,'قافية الكاف','50','0','0');
+INSERT INTO [t36422] VALUES (35,'فلم تر عيني مثل سعد بن مالك','50','34','0');
+INSERT INTO [t36422] VALUES (36,'قافية اللام','52','0','0');
+INSERT INTO [t36422] VALUES (37,'إذا جاء الذي لا بد منه','52','36','0');
+INSERT INTO [t36422] VALUES (38,'أتعرف رسم الدار','54','36','0');
+INSERT INTO [t36422] VALUES (39,'العبد الذليل','57','36','0');
+INSERT INTO [t36422] VALUES (40,'قافية الميم','59','0','0');
+INSERT INTO [t36422] VALUES (41,'الموت حق','59','40','0');
+INSERT INTO [t36422] VALUES (42,'يا عجبا','60','40','0');
+INSERT INTO [t36422] VALUES (43,'إن تعيدوها نعد لكم','61','40','0');
+INSERT INTO [t36422] VALUES (44,'أقسم ما هجوتك','64','40','0');
+INSERT INTO [t36422] VALUES (45,'يوم تحلاق اللمم','65','40','0');
+INSERT INTO [t36422] VALUES (46,'أبلغ قتادة','68','40','0');
+INSERT INTO [t36422] VALUES (47,'قافية الهاء','70','0','0');
+INSERT INTO [t36422] VALUES (48,'لولا الملك القاعد','70','47','0');
