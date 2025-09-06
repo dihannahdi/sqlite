b762970d14def1ccb4043e97ee6559bcc744af1c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:48 +0700

    Upload SQL chunk: t468_chunk_0000.sql

---
+++ sql_chunks/t468_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t468
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t468" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t468
+INSERT INTO [t468] VALUES (1,'تتبع مظان المسألة في كتب المذهب الشافعي','2','0','0');
+INSERT INTO [t468] VALUES (2,'قول «لا أدري» هل يعتبر إقرارا؟','3','0','0');
