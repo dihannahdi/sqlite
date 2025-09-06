1e5a16cc96456fa85641da16cec17b251f36b7ae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:58 +0700

    Upload SQL chunk: t115124_chunk_0000.sql

---
+++ sql_chunks/t115124_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t115124
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t115124" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t115124
+INSERT INTO [t115124] VALUES (1,'الحمد لله تعالى الواهب من غير إحصاء','1','0','0');
+INSERT INTO [t115124] VALUES (2,'حقا لو تأمل الناس معنى هذه الكلمة بقلوبهم','1','0','0');
+INSERT INTO [t115124] VALUES (3,'أخي المسلم','3','0','0');
+INSERT INTO [t115124] VALUES (4,'فأمر الجماعة شديد','4','0','0');
+INSERT INTO [t115124] VALUES (5,'فيا من أعرضت عن إجابة نداء الفلاح','5','0','0');
+INSERT INTO [t115124] VALUES (6,'فيا من سمعت النداء وأعرضت عن إجابته','6','0','0');
+INSERT INTO [t115124] VALUES (7,'فإن المسلم الصادق تجده في شوق إلى عمارة بيوت الله تعالى ..','7','0','0');
+INSERT INTO [t115124] VALUES (8,'أخي المسلم','9','0','0');
