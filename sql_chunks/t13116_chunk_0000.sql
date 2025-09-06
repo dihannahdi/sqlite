0ee0d8925d77b3f3d8f6f414ef8da6606df175f4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:10:48 +0700

    Upload SQL chunk: t13116_chunk_0000.sql

---
+++ sql_chunks/t13116_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t13116
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t13116" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13116
+INSERT INTO [t13116] VALUES (1,'باب ما روي أن النبي صلى الله عليه وسلم كان يدعو في الشيء القليل من الطعام فيجعل فيه البركة حتى يشبع منه الخلق الكثير','1','0','0');
+INSERT INTO [t13116] VALUES (2,'باب ما روي أن النبي صلى الله عليه وسلم كان يدعو أو يضع يده في الشيء من الماء فيروى منه الخلق الكثير','19','0','0');
