b6f1c7dd243ae47ed4d301b2daf25ed21b477808
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:36 +0700

    Upload SQL chunk: t6035_chunk_0000.sql

---
+++ sql_chunks/t6035_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t6035
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t6035" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6035
+INSERT INTO [t6035] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6035] VALUES (2,'وهذا حين الشروع في الأجوبة','8','0','0');
+INSERT INTO [t6035] VALUES (3,'فالحديث الأول','10','2','0');
+INSERT INTO [t6035] VALUES (4,'الحديث الثاني والثالث','14','2','0');
+INSERT INTO [t6035] VALUES (5,'الحديث الرابع','18','2','0');
+INSERT INTO [t6035] VALUES (6,'الحديث الخامس والسادس','20','2','0');
+INSERT INTO [t6035] VALUES (7,'الحديث السابع','22','2','0');
+INSERT INTO [t6035] VALUES (8,'الحديث الثامن','25','2','0');
+INSERT INTO [t6035] VALUES (9,'الحديث التاسع','26','2','0');
+INSERT INTO [t6035] VALUES (10,'جواب الكلام عن الأحاديث التي حكم عليها ابن الجوزي بالوضع ولم يذكره الحافظ العراقي','26','0','0');
+INSERT INTO [t6035] VALUES (11,'الحديث الأول','26','10','0');
+INSERT INTO [t6035] VALUES (12,'الحديث الثاني','27','10','0');
+INSERT INTO [t6035] VALUES (13,'الحديث الثالث','29','10','0');
+INSERT INTO [t6035] VALUES (14,'الحديث الرابع','30','10','0');
+INSERT INTO [t6035] VALUES (15,'الحديث الخامس','31','10','0');
+INSERT INTO [t6035] VALUES (16,'الحديث السادس','32','10','0');
+INSERT INTO [t6035] VALUES (17,'الحديث السابع','33','10','0');
+INSERT INTO [t6035] VALUES (18,'الحديث الثامن','36','10','0');
+INSERT INTO [t6035] VALUES (19,'الحديث التاسع','37','10','0');
+INSERT INTO [t6035] VALUES (20,'الحديث العاشر','38','10','0');
+INSERT INTO [t6035] VALUES (21,'الحديث الحادي عشر','38','10','0');
+INSERT INTO [t6035] VALUES (22,'الحديث الثاني عشر','39','10','0');
+INSERT INTO [t6035] VALUES (23,'الحديث الثالث عشر','40','10','0');
+INSERT INTO [t6035] VALUES (24,'الحديث الرابع عشر','40','10','0');
+INSERT INTO [t6035] VALUES (25,'الحديث الخامس عشر','41','10','0');
