987611602c958b05c188eb10853ca7cfd03d0fca
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:58:07 +0700

    Upload SQL chunk: t96497_chunk_0000.sql

---
+++ sql_chunks/t96497_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t96497
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t96497" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96497
+INSERT INTO [t96497] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t96497] VALUES (2,'الشرط الأول: الإسلام','4','0','0');
+INSERT INTO [t96497] VALUES (3,'الشرط الثاني: العقل','5','0','0');
+INSERT INTO [t96497] VALUES (4,'الشرط الثالث: التمييز','5','0','0');
+INSERT INTO [t96497] VALUES (5,'الشرط الرابع: رفع الحدث','6','0','0');
+INSERT INTO [t96497] VALUES (6,'الشرط الخامس: إزالة النجاسة','7','0','0');
+INSERT INTO [t96497] VALUES (7,'الشرط السادس: ستر العورة مع القدرة','9','0','0');
+INSERT INTO [t96497] VALUES (8,'الشرط السابع: دخول الوقت','13','0','0');
+INSERT INTO [t96497] VALUES (9,'الشرط الثامن: استقبال القبلة','30','0','0');
+INSERT INTO [t96497] VALUES (10,'الشرط التاسع: النية','36','0','0');
