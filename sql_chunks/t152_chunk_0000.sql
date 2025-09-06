c0bc263de3c208a7e5011ad1919a106024d4c5bc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:00 +0700

    Upload SQL chunk: t152_chunk_0000.sql

---
+++ sql_chunks/t152_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t152
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t152" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t152
+INSERT INTO [t152] VALUES (1,'تقديم الشيخ عبد الله السعد','2','0','0');
+INSERT INTO [t152] VALUES (2,'المقدمة','7','0','0');
+INSERT INTO [t152] VALUES (3,'١. أنس بن مالك','13','0','0');
+INSERT INTO [t152] VALUES (4,'٢. البراء بن عازب','18','0','0');
+INSERT INTO [t152] VALUES (5,'٣. بريدة بن الحصيب','19','0','0');
+INSERT INTO [t152] VALUES (6,'٤. جابر بن عبد الله','20','0','0');
+INSERT INTO [t152] VALUES (7,'٥. سعد بن مالك','22','0','0');
+INSERT INTO [t152] VALUES (8,'٦. سهل بن سعد الساعدي','23','0','0');
+INSERT INTO [t152] VALUES (9,'٧. عبد الله بن عباس','24','0','0');
+INSERT INTO [t152] VALUES (10,'٨. عبد الله بن عمر','26','0','0');
+INSERT INTO [t152] VALUES (11,'٩. عبد الله بن عمرو بن العاص','29','0','0');
+INSERT INTO [t152] VALUES (12,'١٠. أبو موسى الأشعري','30','0','0');
+INSERT INTO [t152] VALUES (13,'١١. عبد الله بن مسعود','31','0','0');
+INSERT INTO [t152] VALUES (14,'١٢. عمر بن الخطاب','32','0','0');
+INSERT INTO [t152] VALUES (15,'١٣. معاوية بن حيدة','33','0','0');
+INSERT INTO [t152] VALUES (16,'١٤. أبو هريرة','34','0','0');
+INSERT INTO [t152] VALUES (17,'١٥. عائشة بنت أبي بكر الصديق','41','0','0');
