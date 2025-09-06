0239ae55d9a051ac6297ff79d38c53a3f4712bb5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:47 +0700

    Upload SQL chunk: t594_chunk_0000.sql

---
+++ sql_chunks/t594_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t594
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t594" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t594
+INSERT INTO [t594] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t594] VALUES (2,'باب ما جاء في القناعة','1','0','0');
+INSERT INTO [t594] VALUES (3,'باب ما جاء في المقادير','3','0','0');
+INSERT INTO [t594] VALUES (4,'باب مدح اليأس واستدعائه','4','0','0');
+INSERT INTO [t594] VALUES (5,'وما يعرض للطالب من الذلة والحيرة','4','4','0');
+INSERT INTO [t594] VALUES (6,'باب راحة اليأس وعزه','5','0','0');
+INSERT INTO [t594] VALUES (7,'باب إحماد التؤدة والرفق','6','0','0');
+INSERT INTO [t594] VALUES (8,'باب ذم الحرص','6','0','0');
+INSERT INTO [t594] VALUES (9,'باب ما يعرض للطالب من المكاره','6','0','0');
+INSERT INTO [t594] VALUES (10,'باب الإجمال في الطلب','7','0','0');
+INSERT INTO [t594] VALUES (11,'باب ذم الفقر','8','0','0');
+INSERT INTO [t594] VALUES (12,'باب مدح الفقر','9','0','0');
+INSERT INTO [t594] VALUES (13,'باب الحث على الطلب','10','0','0');
+INSERT INTO [t594] VALUES (14,'باب ذم التواني والتسويف','11','0','0');
+INSERT INTO [t594] VALUES (15,'باب ذم العجلة','11','0','0');
+INSERT INTO [t594] VALUES (16,'باب اختيار المأمول','12','0','0');
+INSERT INTO [t594] VALUES (17,'باب الإهداء إلى من يلتمس فضله','12','0','0');
+INSERT INTO [t594] VALUES (18,'باب ارتجاع الهدية ممن أملت منه فضلا فلم تنل ذلك','13','0','0');
