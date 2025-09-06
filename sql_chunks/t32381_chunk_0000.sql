50f004cc9f6aec8fd219d9ec7143b0dd7ab3a5bf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:59 +0700

    Upload SQL chunk: t32381_chunk_0000.sql

---
+++ sql_chunks/t32381_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t32381
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t32381" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t32381
+INSERT INTO [t32381] VALUES (1,'[مقدمة التحقيق]','2','0','0');
+INSERT INTO [t32381] VALUES (2,'ترجمة المؤلف','4','1','0');
+INSERT INTO [t32381] VALUES (3,'اسمه ونسبه','5','2','0');
+INSERT INTO [t32381] VALUES (4,'مولده','9','2','0');
+INSERT INTO [t32381] VALUES (5,'طلبه للعلم','10','2','0');
+INSERT INTO [t32381] VALUES (6,'شيوخه','15','2','0');
+INSERT INTO [t32381] VALUES (7,'تدريسه','30','2','0');
+INSERT INTO [t32381] VALUES (8,'تلاميذه','35','2','0');
+INSERT INTO [t32381] VALUES (9,'تصانيفه','43','2','0');
+INSERT INTO [t32381] VALUES (10,'وفاته','67','2','0');
+INSERT INTO [t32381] VALUES (11,'دراسة موجزة بين يدي الكتاب','68','1','0');
+INSERT INTO [t32381] VALUES (12,'توثيق الكتاب، ووصف النسخة الخطية','69','11','0');
+INSERT INTO [t32381] VALUES (13,'توثيق الكتاب','69','12','0');
+INSERT INTO [t32381] VALUES (14,'النسخة الخطية التي اعتمدتها في إخراج هذا الكتاب','70','12','0');
+INSERT INTO [t32381] VALUES (15,'تعريف بالناسخ','73','11','0');
+INSERT INTO [t32381] VALUES (16,'منهج الحافظ مغلطاي في الكتاب','77','11','0');
+INSERT INTO [t32381] VALUES (17,'دراسة إحصائية لكتب وأبواب وأحاديث الكتاب','84','16','0');
+INSERT INTO [t32381] VALUES (18,'خدمتي للكتاب','86','11','0');
+INSERT INTO [t32381] VALUES (19,'[مقدمة المصنف]','88','0','0');
+INSERT INTO [t32381] VALUES (20,'كتاب الطهارة','89','0','0');
+INSERT INTO [t32381] VALUES (21,'كتاب الصلاة','160','0','0');
+INSERT INTO [t32381] VALUES (22,'كتاب الزكاة واللقطة','239','0','0');
+INSERT INTO [t32381] VALUES (23,'كتاب الحج','249','0','0');
+INSERT INTO [t32381] VALUES (24,'كتاب النكاح والطلاق','256','0','0');
+INSERT INTO [t32381] VALUES (25,'كتاب الصيام','294','0','0');
+INSERT INTO [t32381] VALUES (26,'كتاب الجهاد','324','0','0');
+INSERT INTO [t32381] VALUES (27,'كتاب الوصايا والضحايا والفرائض','334','0','0');
+INSERT INTO [t32381] VALUES (28,'[كتاب] الجنائز','358','0','0');
+INSERT INTO [t32381] VALUES (29,'[كتاب] الأيمان والنذور','385','0','0');
+INSERT INTO [t32381] VALUES (30,'كتاب البيوع','398','0','0');
+INSERT INTO [t32381] VALUES (31,'كتاب القضاء','428','0','0');
+INSERT INTO [t32381] VALUES (32,'كتاب الأشربة','439','0','0');
+INSERT INTO [t32381] VALUES (33,'كتاب الأطعمة، والحدود والديات، وغير ذلك','444','0','0');
+INSERT INTO [t32381] VALUES (34,'فهرس المصادر','500','0','0');
