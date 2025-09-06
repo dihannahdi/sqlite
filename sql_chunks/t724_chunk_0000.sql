8815788eee45148e45b15a1938057d18c5d3c667
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:49 +0700

    Upload SQL chunk: t724_chunk_0000.sql

---
+++ sql_chunks/t724_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t724
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t724" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t724
+INSERT INTO [t724] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t724] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
+INSERT INTO [t724] VALUES (3,'وصف في الشباب والخضاب والمشيب','6','0','0');
+INSERT INTO [t724] VALUES (4,'وصف في الغزل والنسيب','10','0','0');
+INSERT INTO [t724] VALUES (5,'وصف في الخمر','57','0','0');
+INSERT INTO [t724] VALUES (6,'وما يتصل بها وذكر مجالسها وما ينضاف إليها ويناسبها من الغناء والمغنين','57','5','0');
+INSERT INTO [t724] VALUES (7,'وصف الغناء','77','0','0');
+INSERT INTO [t724] VALUES (8,'وما يتعلق به','77','7','0');
+INSERT INTO [t724] VALUES (9,'وصف في الربيع وأزهاره','79','0','0');
+INSERT INTO [t724] VALUES (10,'وما يلازمه من نعت أنهاره وتغريد أطياره وصوت بلبله وهزاره.','79','9','0');
+INSERT INTO [t724] VALUES (11,'وصف في السحاب والغيث','89','0','0');
+INSERT INTO [t724] VALUES (12,'والبرق والمياه وما يتصل بذلك','89','11','0');
+INSERT INTO [t724] VALUES (13,'وصف في الليل والنجوم','91','0','0');
+INSERT INTO [t724] VALUES (14,'والمجرة والهلال والصبح والشمس وما يتعلق بذلك','91','13','0');
+INSERT INTO [t724] VALUES (15,'وصف في المدح والفخر','94','0','0');
+INSERT INTO [t724] VALUES (16,'والتهاني وما يضاف إليها','94','15','0');
