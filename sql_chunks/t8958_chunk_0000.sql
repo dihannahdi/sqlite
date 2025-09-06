3c0e34326de529c8a99bde933cb702b61995bb36
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:58 +0700

    Upload SQL chunk: t8958_chunk_0000.sql

---
+++ sql_chunks/t8958_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t8958
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t8958" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8958
+INSERT INTO [t8958] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8958] VALUES (2,'آداب السفر','3','0','0');
+INSERT INTO [t8958] VALUES (3,'سفر المرأة','5','0','0');
+INSERT INTO [t8958] VALUES (4,'صلاة المسافر','7','0','0');
+INSERT INTO [t8958] VALUES (5,'المواقيت','9','0','0');
+INSERT INTO [t8958] VALUES (6,'أنواع الأنساك','11','0','0');
+INSERT INTO [t8958] VALUES (7,'المحرم الذي يلزمه الهدي','13','0','0');
+INSERT INTO [t8958] VALUES (8,'صفة العمرة','16','0','0');
+INSERT INTO [t8958] VALUES (9,'صفة الحج','20','0','0');
+INSERT INTO [t8958] VALUES (10,'زيارة المسجد النبوي','24','0','0');
+INSERT INTO [t8958] VALUES (11,'الفوائد','26','0','0');
