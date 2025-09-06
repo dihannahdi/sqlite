b62183708bddaee562ef0c58fa56fd4ac852bd9e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:03 +0700

    Upload SQL chunk: t30823_chunk_0000.sql

---
+++ sql_chunks/t30823_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t30823
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t30823" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30823
+INSERT INTO [t30823] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t30823] VALUES (2,'توطئة','4','0','0');
+INSERT INTO [t30823] VALUES (3,'المساواة','12','0','0');
+INSERT INTO [t30823] VALUES (4,'الحرية مقدمة حرية الفكر لا حرية الكفر','18','0','0');
+INSERT INTO [t30823] VALUES (5,'الحرية الحقيقية','23','0','0');
+INSERT INTO [t30823] VALUES (6,'لا إكراه في الدين','26','0','0');
+INSERT INTO [t30823] VALUES (7,'حكم الردة','31','0','0');
+INSERT INTO [t30823] VALUES (8,'الرق','41','0','0');
+INSERT INTO [t30823] VALUES (9,'الإسلام والرق','43','0','0');
+INSERT INTO [t30823] VALUES (10,'موقف اليهود من الرقيق','55','0','0');
+INSERT INTO [t30823] VALUES (11,'موقف النصرانية من الرقيق','58','0','0');
+INSERT INTO [t30823] VALUES (12,'أوربا المعاصرة والرقيق','61','0','0');
+INSERT INTO [t30823] VALUES (13,'المرأة','66','0','0');
+INSERT INTO [t30823] VALUES (14,'الميراث','71','0','0');
+INSERT INTO [t30823] VALUES (15,'الطلاق','75','0','0');
+INSERT INTO [t30823] VALUES (16,'الحضانة','78','0','0');
+INSERT INTO [t30823] VALUES (17,'تعدد الزوجات','79','0','0');
+INSERT INTO [t30823] VALUES (18,'تطبيق الشريعة','87','0','0');
+INSERT INTO [t30823] VALUES (19,'الحدود والعقوبات الجسدية','92','0','0');
+INSERT INTO [t30823] VALUES (20,'الجهاد في سبيل الله','98','0','0');
+INSERT INTO [t30823] VALUES (21,'حقيقة الجهاد','101','0','0');
+INSERT INTO [t30823] VALUES (22,'أمم الأرض والقوة','105','0','0');
+INSERT INTO [t30823] VALUES (23,'أهم المصادر','109','0','0');
+INSERT INTO [t30823] VALUES (24,'الأسئلة','110','0','0');
