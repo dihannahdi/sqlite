e9c289e15049c150e7d2d9f24fff1c02cd026abd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:36 +0700

    Upload SQL chunk: t26758_chunk_0000.sql

---
+++ sql_chunks/t26758_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26758
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26758" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26758
+INSERT INTO [t26758] VALUES (1,'أخبار الثقلاء للخلال','2','0','0');
