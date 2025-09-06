e07e3c5977429c651b352fb0001aded064ba9e51
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:16 +0700

    Upload SQL chunk: t36429_chunk_0000.sql

---
+++ sql_chunks/t36429_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t36429
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t36429" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36429
+INSERT INTO [t36429] VALUES (1,'الحمد لله تعالى رجاء المؤمنين','1','0','0');
+INSERT INTO [t36429] VALUES (2,'أخي المسلم','5','1','0');
+INSERT INTO [t36429] VALUES (3,'أخي في الله','6','1','0');
+INSERT INTO [t36429] VALUES (4,'أخي في الله','8','1','0');
+INSERT INTO [t36429] VALUES (5,'أخي: يا لهول وشدة عذاب النار','9','1','0');
+INSERT INTO [t36429] VALUES (6,'أخي: ولا تسل عن أودية النار','10','1','0');
+INSERT INTO [t36429] VALUES (7,'أخي: يا لشقاء قوم إن استغاثوا لم يغاثوا','11','1','0');
+INSERT INTO [t36429] VALUES (8,'أخي المسلم','16','1','0');
