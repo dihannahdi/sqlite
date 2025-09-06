62cd49036370f7d139b2150388ef488f61dbe541
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:46 +0700

    Upload SQL chunk: t26474_chunk_0000.sql

---
+++ sql_chunks/t26474_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26474
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26474" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26474
+INSERT INTO [t26474] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26474] VALUES (2,'جزء فيه ما انتقى أبو بكر أحمد بن موسى بن مردويه','2','0','0');
