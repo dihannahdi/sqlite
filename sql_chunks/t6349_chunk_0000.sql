3cd3ac1a975bfd337e745edf0776433fbcd3a3c8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:00 +0700

    Upload SQL chunk: t6349_chunk_0000.sql

---
+++ sql_chunks/t6349_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t6349
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t6349" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6349
+INSERT INTO [t6349] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
