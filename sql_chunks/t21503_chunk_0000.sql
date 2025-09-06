644ad252429130e0871911e26b9be769b8f4a16a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:47 +0700

    Upload SQL chunk: t21503_chunk_0000.sql

---
+++ sql_chunks/t21503_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t21503
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t21503" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21503
+INSERT INTO [t21503] VALUES (1,'الغرض من الكتاب','1','0','0');
+INSERT INTO [t21503] VALUES (2,'أجناس المخاطبات','10','0','0');
+INSERT INTO [t21503] VALUES (3,'القول من المغلطات من المعاني','27','0','0');
+INSERT INTO [t21503] VALUES (4,'القياس المغالطي','83','0','0');
+INSERT INTO [t21503] VALUES (5,'القول في النقض','126','0','0');
