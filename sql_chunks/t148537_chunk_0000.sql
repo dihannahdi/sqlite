ab4821f767b79b854d8caf2f8ce1eaa8e33d898e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:34 +0700

    Upload SQL chunk: t148537_chunk_0000.sql

---
+++ sql_chunks/t148537_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t148537
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t148537" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t148537
+INSERT INTO [t148537] VALUES (1,'ديوان ابن الخياط','1','0','0');
