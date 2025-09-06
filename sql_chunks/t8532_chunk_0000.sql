9ace09a1fa3bb021df6a4045363be299f0c3701f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:46:54 +0700

    Upload SQL chunk: t8532_chunk_0000.sql

---
+++ sql_chunks/t8532_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8532
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8532" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8532
+INSERT INTO [t8532] VALUES (1,'الكلمات النافعة في المفكرات الواقعة','1','0','0');
+INSERT INTO [t8532] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
