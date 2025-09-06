edabfcd0d8a3044870dc578faf0d0e65f280d620
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:32 +0700

    Upload SQL chunk: t31241_chunk_0000.sql

---
+++ sql_chunks/t31241_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t31241
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t31241" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31241
+INSERT INTO [t31241] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t31241] VALUES (2,'خطبة الكتاب','4','0','0');
+INSERT INTO [t31241] VALUES (3,'مقدمة في ذكر بعض ما ورد في فضل هذا الفن','6','0','0');
+INSERT INTO [t31241] VALUES (4,'باب أسباب الميراث','8','0','0');
+INSERT INTO [t31241] VALUES (5,'باب موانع الإرث','11','0','0');
+INSERT INTO [t31241] VALUES (6,'باب الوارثين من الرجال','12','0','0');
+INSERT INTO [t31241] VALUES (7,'باب الوارثات من النساء','12','0','0');
+INSERT INTO [t31241] VALUES (8,'باب الفروض المقدرة في كتاب الله تعالى','13','0','0');
+INSERT INTO [t31241] VALUES (9,'باب من يرث النصف','14','0','0');
+INSERT INTO [t31241] VALUES (10,'باب من يرث الربع','15','0','0');
+INSERT INTO [t31241] VALUES (11,'باب من يرث الثمن','16','0','0');
+INSERT INTO [t31241] VALUES (12,'باب من يرث الثلثين','16','0','0');
+INSERT INTO [t31241] VALUES (13,'باب من يرث الثلث','17','0','0');
+INSERT INTO [t31241] VALUES (14,'باب من يرث السدس','18','0','0');
+INSERT INTO [t31241] VALUES (15,'باب التعصيب','20','0','0');
+INSERT INTO [t31241] VALUES (16,'باب الحجب','25','0','0');
+INSERT INTO [t31241] VALUES (17,'باب المشركة','29','0','0');
+INSERT INTO [t31241] VALUES (18,'باب الجد والإخوة','30','0','0');
+INSERT INTO [t31241] VALUES (19,'باب الأكدرية','36','0','0');
+INSERT INTO [t31241] VALUES (20,'باب الحساب','38','0','0');
+INSERT INTO [t31241] VALUES (21,'باب المناسخة','50','0','0');
+INSERT INTO [t31241] VALUES (22,'باب قسمة التركات','56','0','0');
+INSERT INTO [t31241] VALUES (23,'باب ميراث الخنثى المشكل والحمل والمفقود','65','0','0');
+INSERT INTO [t31241] VALUES (24,'ميراث الخنثى المشكل','65','23','0');
+INSERT INTO [t31241] VALUES (25,'فصل في حكم الحمل','74','23','0');
+INSERT INTO [t31241] VALUES (26,'فصل في أحكام المفقود','79','23','0');
+INSERT INTO [t31241] VALUES (27,'باب ميراث الغرقى ونحوهم','82','0','0');
+INSERT INTO [t31241] VALUES (28,'باب الرد وبيان من يستحقه','88','0','0');
+INSERT INTO [t31241] VALUES (29,'باب ميراث ذوي الأرحام','95','0','0');
