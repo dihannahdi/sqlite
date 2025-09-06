b994fd7914f1302d6da8a84abcb399ec6c624582
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:37:29 +0700

    Upload SQL chunk: t20892_chunk_0000.sql

---
+++ sql_chunks/t20892_chunk_0000.sql
@@ -0,0 +1,51 @@
+-- Table: t20892
+-- Chunk: 1
+-- Rows: 44
+
+CREATE TABLE "t20892" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t20892
+INSERT INTO [t20892] VALUES (1,'[مقدمة المحقق]','1','0','0');
+INSERT INTO [t20892] VALUES (2,'مقدمة في علم الفقه','9','0','0');
+INSERT INTO [t20892] VALUES (3,'أصول الفقه','11','0','0');
+INSERT INTO [t20892] VALUES (4,'فصل والفرع حكم الشرع','14','0','0');
+INSERT INTO [t20892] VALUES (5,'كتاب القرآن ومباحث الأقوال','73','0','0');
+INSERT INTO [t20892] VALUES (6,'«المنطوق والمفهوم»','84','0','0');
+INSERT INTO [t20892] VALUES (7,'فصل في الاشتقاق','108','0','0');
+INSERT INTO [t20892] VALUES (8,'فصل في الترادف','115','0','0');
+INSERT INTO [t20892] VALUES (9,'المشترك','119','0','0');
+INSERT INTO [t20892] VALUES (10,'الحقيقة','122','0','0');
+INSERT INTO [t20892] VALUES (11,'المجاز','125','0','0');
+INSERT INTO [t20892] VALUES (12,'المعرب','137','0','0');
+INSERT INTO [t20892] VALUES (13,'الكناية والتعريض','139','0','0');
+INSERT INTO [t20892] VALUES (14,'الأمر','142','0','0');
+INSERT INTO [t20892] VALUES (15,'الواجب الموسع','182','0','0');
+INSERT INTO [t20892] VALUES (16,'ذو الكفاية','187','0','0');
+INSERT INTO [t20892] VALUES (17,'النهي (أي النفسي)','196','0','0');
+INSERT INTO [t20892] VALUES (18,'العام','201','0','0');
+INSERT INTO [t20892] VALUES (19,'ما عدم العموم فيه أصح (أي من العموم)','223','0','0');
+INSERT INTO [t20892] VALUES (20,'التخصيص','227','0','0');
+INSERT INTO [t20892] VALUES (21,'المخصص المتصل','236','0','0');
+INSERT INTO [t20892] VALUES (22,'المقيد والمطلق','259','0','0');
+INSERT INTO [t20892] VALUES (23,'التأويل والمحكم والمجمل','264','0','0');
+INSERT INTO [t20892] VALUES (24,'البيان','272','0','0');
+INSERT INTO [t20892] VALUES (25,'النسخ','281','0','0');
+INSERT INTO [t20892] VALUES (26,'كتاب السنة','301','0','0');
+INSERT INTO [t20892] VALUES (27,'كيفية رواية الصحابي','360','0','0');
+INSERT INTO [t20892] VALUES (28,'كيفية رواية غيره عن شيخه','364','0','0');
+INSERT INTO [t20892] VALUES (29,'كتاب الإجماع','372','0','0');
+INSERT INTO [t20892] VALUES (30,'كتاب القياس','396','0','0');
+INSERT INTO [t20892] VALUES (31,'أركانه','406','0','0');
+INSERT INTO [t20892] VALUES (32,'مسالك العلة','446','0','0');
+INSERT INTO [t20892] VALUES (33,'القوادح','501','0','0');
+INSERT INTO [t20892] VALUES (34,'(خاتمة) أي لكتاب القياس','537','0','0');
+INSERT INTO [t20892] VALUES (35,'كتاب الاستدلال','547','0','0');
+INSERT INTO [t20892] VALUES (36,'(كتاب التعادل والتراجيح)','565','0','0');
+INSERT INTO [t20892] VALUES (37,'(الترجيح باعتبار حال الراوي)','574','0','0');
+INSERT INTO [t20892] VALUES (38,'(الترجيح باعتبار حال المروي)','582','0','0');
+INSERT INTO [t20892] VALUES (39,'الترجيح باعتبار حال المدلول','591','0','0');
+INSERT INTO [t20892] VALUES (40,'(ترجيح الإجماعات)','596','0','0');
+INSERT INTO [t20892] VALUES (41,'ترجيح الأقيسة والحدود','599','0','0');
+INSERT INTO [t20892] VALUES (42,'(كتاب الاجتهاد في الفروع)','607','0','0');
+INSERT INTO [t20892] VALUES (43,'فصل في التقليد في الفروع','627','0','0');
+INSERT INTO [t20892] VALUES (44,'خاتمة الكتاب','645','0','0');
