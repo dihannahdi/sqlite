9f478d41869bd9cd3fa3615e94378784c8a3e144
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:55 +0700

    Upload SQL chunk: t10306_chunk_0000.sql

---
+++ sql_chunks/t10306_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t10306
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t10306" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10306
+INSERT INTO [t10306] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10306] VALUES (2,'باب اشادة الإسلام بالعلم والعلماء','3','0','0');
+INSERT INTO [t10306] VALUES (3,'باب وجوب التعلم والتعليم','8','0','0');
+INSERT INTO [t10306] VALUES (4,'باب سياسة التعليم في بلاد المسلمين','11','0','0');
+INSERT INTO [t10306] VALUES (5,'باب كيف تكون مناهج التعليم في بلاد المسلمين؟','17','0','0');
+INSERT INTO [t10306] VALUES (6,'باب مواد التعليم في بلاد المسلمين. وكيف تكون؟','19','0','0');
+INSERT INTO [t10306] VALUES (7,'باب غايات التعليم في بلاد المسلمين','23','0','0');
+INSERT INTO [t10306] VALUES (8,'باب شهادات يعتز يها المسلمون','25','0','0');
