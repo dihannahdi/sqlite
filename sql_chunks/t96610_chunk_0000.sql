42da98d59b8baf763b3aadf8df0f19b391c3f735
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:13 +0700

    Upload SQL chunk: t96610_chunk_0000.sql

---
+++ sql_chunks/t96610_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t96610
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t96610" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96610
+INSERT INTO [t96610] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t96610] VALUES (2,'بداية الشرح','3','0','0');
