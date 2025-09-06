e70d95df5103e50fa471b19e7f0e9e0dc7854eca
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:09 +0700

    Upload SQL chunk: t211_chunk_0000.sql

---
+++ sql_chunks/t211_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t211
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t211" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t211
+INSERT INTO [t211] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t211] VALUES (2,'آداب القارىء','1','0','0');
+INSERT INTO [t211] VALUES (3,'آداب مس المصحف وحمله وكتابته','4','0','0');
+INSERT INTO [t211] VALUES (4,'آداب المعلم وشرطه','4','0','0');
+INSERT INTO [t211] VALUES (5,'آداب المتعلم','5','0','0');
+INSERT INTO [t211] VALUES (6,'آداب الناس والسامعين','6','0','0');
