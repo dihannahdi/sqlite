1593b86e20636da0c92876103744d4631ec641f2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:05 +0700

    Upload SQL chunk: t96602_chunk_0000.sql

---
+++ sql_chunks/t96602_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t96602
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t96602" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96602
+INSERT INTO [t96602] VALUES (1,'النص المحقق','2','0','0');
