96da89c659568b2a5944e012b3af12bb96567912
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:58 +0700

    Upload SQL chunk: t26655_chunk_0000.sql

---
+++ sql_chunks/t26655_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26655
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26655" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26655
+INSERT INTO [t26655] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26655] VALUES (2,'عوالي مالك رواية سليم الرازي','2','0','0');
