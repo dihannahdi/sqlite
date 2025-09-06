28a68e604b744c8149a2bfc3870d81b949b3fe3a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:40 +0700

    Upload SQL chunk: t9589_chunk_0000.sql

---
+++ sql_chunks/t9589_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t9589
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t9589" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9589
+INSERT INTO [t9589] VALUES (1,'أسرار البيان في التعبير القرآني','1','0','0');
+INSERT INTO [t9589] VALUES (2,'تذكير الفعل أو تأنيثه مع الفاعل المؤنث','13','0','0');
+INSERT INTO [t9589] VALUES (3,'التقديم والتأخير في القرآن الكريم','24','0','0');
+INSERT INTO [t9589] VALUES (4,'أمثلة أخرى على التقديم والتأخير','60','0','0');
+INSERT INTO [t9589] VALUES (5,'موضوع القطع في القرآن الكريم','71','0','0');
+INSERT INTO [t9589] VALUES (6,'الفاصلة القرآنية من حيث المعنى','87','0','0');
+INSERT INTO [t9589] VALUES (7,'موضوع التشابه والإختلاف في القرآن الكريم','92','0','0');
