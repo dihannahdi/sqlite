ab1cb7e8d425857b37cf5e8c5a0e16f70fb5bf23
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:51:22 +0700

    Upload SQL chunk: t918_chunk_0000.sql

---
+++ sql_chunks/t918_chunk_0000.sql
@@ -0,0 +1,47 @@
+-- Table: t918
+-- Chunk: 1
+-- Rows: 40
+
+CREATE TABLE "t918" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t918
+INSERT INTO [t918] VALUES (1,'كتاب الأشربة','3','0','0');
+INSERT INTO [t918] VALUES (2,'كتاب الصيد','18','0','0');
+INSERT INTO [t918] VALUES (3,'كتاب الرهن','36','0','0');
+INSERT INTO [t918] VALUES (4,'باب ما يجوز ارتهانه وما لا يجوز','49','3','0');
+INSERT INTO [t918] VALUES (5,'باب الرهن يوضع على يد عدل','63','3','0');
+INSERT INTO [t918] VALUES (6,'باب التصرف في الرهن والجناية عليه وجنايته على غيره','69','3','0');
+INSERT INTO [t918] VALUES (7,'كتاب الجنايات','91','0','0');
+INSERT INTO [t918] VALUES (8,'باب القود فيما دون النفس','117','7','0');
+INSERT INTO [t918] VALUES (9,'باب الشهادة في القتل واعتبار حالته','136','7','0');
+INSERT INTO [t918] VALUES (10,'كتاب الديات','143','0','0');
+INSERT INTO [t918] VALUES (11,'باب ما يحدثه الرجل في الطريق وغيره','164','10','0');
+INSERT INTO [t918] VALUES (12,'باب جناية البهيمة والجناية عليها','175','10','0');
+INSERT INTO [t918] VALUES (13,'باب جناية المملوك والجناية عليه','186','10','0');
+INSERT INTO [t918] VALUES (14,'باب القسامة','201','10','0');
+INSERT INTO [t918] VALUES (15,'كتاب المعاقل','218','0','0');
+INSERT INTO [t918] VALUES (16,'كتاب الوصايا','226','0','0');
+INSERT INTO [t918] VALUES (17,'باب الوصية بثلث المال','248','16','0');
+INSERT INTO [t918] VALUES (18,'باب العتق في المرض','261','16','0');
+INSERT INTO [t918] VALUES (19,'باب الوصية للاقارب وغيرهم','265','16','0');
+INSERT INTO [t918] VALUES (20,'باب الوصية بالخدمة والسكنى والثمرة','275','16','0');
+INSERT INTO [t918] VALUES (21,'باب الوصي','284','16','0');
+INSERT INTO [t918] VALUES (22,'كتاب الخنثى','315','0','0');
+INSERT INTO [t918] VALUES (23,'كتاب الفرائض','349','0','0');
+INSERT INTO [t918] VALUES (24,'باب العول','380','23','0');
+INSERT INTO [t918] VALUES (25,'باب توريث ذوي الارحام','386','23','0');
+INSERT INTO [t918] VALUES (26,'باب المخارج','399','23','0');
+INSERT INTO [t918] VALUES (27,'كتاب الشهادات','472','0','0');
+INSERT INTO [t918] VALUES (28,'باب القبول وعدمه','518','27','0');
+INSERT INTO [t918] VALUES (29,'كتاب الدعوى','571','0','0');
+INSERT INTO [t918] VALUES (30,'باب التحالف','644','29','0');
+INSERT INTO [t918] VALUES (31,'باب دعوى الرجلين','686','29','0');
+INSERT INTO [t918] VALUES (32,'باب دعوى النسب','756','29','0');
+INSERT INTO [t918] VALUES (33,'كتاب الاقرار','784','0','0');
+INSERT INTO [t918] VALUES (34,'باب الاستثناء','834','33','0');
+INSERT INTO [t918] VALUES (35,'باب إقرار المريض','852','33','0');
+INSERT INTO [t918] VALUES (36,'كتاب الصلح','913','0','0');
+INSERT INTO [t918] VALUES (37,'كتاب المضاربة','977','0','0');
+INSERT INTO [t918] VALUES (38,'باب المضارب','1002','37','0');
+INSERT INTO [t918] VALUES (39,'كتاب الايداع','1032','0','0');
+INSERT INTO [t918] VALUES (40,'كتاب العارية','1090','0','0');
