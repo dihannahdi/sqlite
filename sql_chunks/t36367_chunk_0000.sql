272eb6c6b11f876d89fc949052dff1569ddf6e5f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:34 +0700

    Upload SQL chunk: t36367_chunk_0000.sql

---
+++ sql_chunks/t36367_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t36367
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t36367" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36367
+INSERT INTO [t36367] VALUES (1,'التعريف بنبي الرحمة صلى الله عليه وآله وسلم','1','0','0');
+INSERT INTO [t36367] VALUES (2,'محمد بن عبد الله بن عبد المطلب، رسول الله، ونبيه','2','0','0');
+INSERT INTO [t36367] VALUES (3,'صاحب الجمال المسربل بالجلال، المكسو بجميل الخصال','2','0','0');
+INSERT INTO [t36367] VALUES (4,'عن أنس قال','23','0','0');
+INSERT INTO [t36367] VALUES (5,'وفي حديث معاذ','24','0','0');
+INSERT INTO [t36367] VALUES (6,'وفي وصف ابن أبي هالة','25','0','0');
+INSERT INTO [t36367] VALUES (7,'روى البخاري عن عبد الله بن عمرو','35','0','0');
+INSERT INTO [t36367] VALUES (8,'وروى البخاري أن أبا هريرة قال','36','0','0');
+INSERT INTO [t36367] VALUES (9,'وروى الامام أحمد عن ابن عباس','37','0','0');
+INSERT INTO [t36367] VALUES (10,'فهذا هو غيض من فيض في وصف نبي الرحمة','37','0','0');
