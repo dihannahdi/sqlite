43cfba6ea36f2230d31e647e4102548bd4d39563
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:26 +0700

    Upload SQL chunk: t36107_chunk_0000.sql

---
+++ sql_chunks/t36107_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t36107
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t36107" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36107
+INSERT INTO [t36107] VALUES (1,'١','2','0','0');
+INSERT INTO [t36107] VALUES (2,'عناصر الدرس','2','1','0');
+INSERT INTO [t36107] VALUES (3,'٢','31','0','0');
+INSERT INTO [t36107] VALUES (4,'عناصر الدرس','31','3','0');
+INSERT INTO [t36107] VALUES (5,'٣','60','0','0');
+INSERT INTO [t36107] VALUES (6,'عناصر الدرس','60','5','0');
+INSERT INTO [t36107] VALUES (7,'٤','89','0','0');
+INSERT INTO [t36107] VALUES (8,'عناصر الدرس','89','7','0');
+INSERT INTO [t36107] VALUES (9,'٥','113','0','0');
+INSERT INTO [t36107] VALUES (10,'عناصر الدرس','113','9','0');
+INSERT INTO [t36107] VALUES (11,'٦','133','0','0');
+INSERT INTO [t36107] VALUES (12,'عناصر الدرس','133','11','0');
+INSERT INTO [t36107] VALUES (13,'٧','164','0','0');
+INSERT INTO [t36107] VALUES (14,'عناصر الدرس','164','13','0');
+INSERT INTO [t36107] VALUES (15,'٨','188','0','0');
+INSERT INTO [t36107] VALUES (16,'عناصر الدرس','188','15','0');
+INSERT INTO [t36107] VALUES (17,'٩','209','0','0');
+INSERT INTO [t36107] VALUES (18,'عناصر الدرس','209','17','0');
+INSERT INTO [t36107] VALUES (19,'١٠','237','0','0');
+INSERT INTO [t36107] VALUES (20,'عناصر الدرس','237','19','0');
