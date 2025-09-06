be7f908c9c2f2ff958541c055ec21684475dabe1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:00:11 +0700

    Upload SQL chunk: t11928_chunk_0000.sql

---
+++ sql_chunks/t11928_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t11928
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t11928" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11928
+INSERT INTO [t11928] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11928] VALUES (2,'تمهيد','3','0','0');
+INSERT INTO [t11928] VALUES (3,'المعجم المفهرس لألفاظ القرآن الكريم','4','0','0');
+INSERT INTO [t11928] VALUES (4,'معجم الأدوات والضمائر في القرآن الكريم','5','0','0');
+INSERT INTO [t11928] VALUES (5,'معجم حروف المعاني في القرآن الكريم','8','0','0');
+INSERT INTO [t11928] VALUES (6,'دراسات لأساليب القرآن الكريم','12','0','0');
+INSERT INTO [t11928] VALUES (7,'نتائج واقتراحات','16','0','0');
+INSERT INTO [t11928] VALUES (8,'اللام المزحلقة','17','0','0');
+INSERT INTO [t11928] VALUES (9,'اللام الموطئة للقسم','29','0','0');
+INSERT INTO [t11928] VALUES (10,'مصادر ومراجع','35','0','0');
