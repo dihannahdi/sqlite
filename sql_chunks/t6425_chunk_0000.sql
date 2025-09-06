ae504e736079fb28f6625c4a353304b338b77883
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:34 +0700

    Upload SQL chunk: t6425_chunk_0000.sql

---
+++ sql_chunks/t6425_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t6425
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t6425" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6425
+INSERT INTO [t6425] VALUES (1,'ذم التأويل','1','0','0');
+INSERT INTO [t6425] VALUES (2,'في بيان مذهبهم في صفات الله تعالى','4','0','0');
+INSERT INTO [t6425] VALUES (3,'الباب الثاني في بيان وجوب اتباعهم والحث على لزوم مذهبهم وسلوك سبيلهم وبيان ذلك من الكتاب والسنة وأقوال الأئمة','21','0','0');
+INSERT INTO [t6425] VALUES (4,'الباب الثالث في بيان أن الصواب ما ذهب إليه السلف رحمة الله عليهم بالأدلة الجلية والحجج المرضية وبيان ذلك من الكتاب والسنة والإجماع والمعنى','30','0','0');
+INSERT INTO [t6425] VALUES (5,'فصل','40','0','0');
