ac089d55fa5b90860231f48e9a8109badba8078c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:35 +0700

    Upload SQL chunk: t314_chunk_0000.sql

---
+++ sql_chunks/t314_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t314
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t314" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t314
+INSERT INTO [t314] VALUES (1,'الرد المفحم','1','0','0');
