e981edf10f2e0cef10dc09d9bbc18c4e2ad74bfe
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:23:25 +0700

    Upload SQL chunk: t150958_chunk_0000.sql

---
+++ sql_chunks/t150958_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t150958
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t150958" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t150958
+INSERT INTO [t150958] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t150958] VALUES (2,'١. قال سيبويه','4','0','0');
+INSERT INTO [t150958] VALUES (3,'٢. قال ابن السراج','6','0','0');
+INSERT INTO [t150958] VALUES (4,'٣. وقال ابن خالويه','7','0','0');
+INSERT INTO [t150958] VALUES (5,'٤. وقال ابن جني','7','0','0');
+INSERT INTO [t150958] VALUES (6,'٥. قال الجوهري','8','0','0');
+INSERT INTO [t150958] VALUES (7,'٦. قال ابن فارس','8','0','0');
+INSERT INTO [t150958] VALUES (8,'٧. قال ابن سيده','9','0','0');
+INSERT INTO [t150958] VALUES (9,'٨. قال الجواليقي','9','0','0');
+INSERT INTO [t150958] VALUES (10,'٩. وقال سلامة الأنباري','9','0','0');
+INSERT INTO [t150958] VALUES (11,'١٠. قال العكبري','10','0','0');
+INSERT INTO [t150958] VALUES (12,'١١. قال القرطبي','10','0','0');
+INSERT INTO [t150958] VALUES (13,'١٢. قال الرضي محمد الأستراباذي','10','0','0');
+INSERT INTO [t150958] VALUES (14,'١٣. قال أبو حيان','10','0','0');
+INSERT INTO [t150958] VALUES (15,'١٤. قال ابن القيم الجوزية','11','0','0');
+INSERT INTO [t150958] VALUES (16,'١٥. قال ابن حجر','11','0','0');
+INSERT INTO [t150958] VALUES (17,'١٦. قال أبو العباس شهاب الدين أحمد بن محمد الدمشقي','11','0','0');
+INSERT INTO [t150958] VALUES (18,'١٧. وقال العصام إبراهيم بن محمد الاسفرائيني','12','0','0');
+INSERT INTO [t150958] VALUES (19,'١٨. قال محمد الطاهر عاشور','12','0','0');
+INSERT INTO [t150958] VALUES (20,'١٩. قال الرافعي','12','0','0');
+INSERT INTO [t150958] VALUES (21,'٢٠. انتقد العلامة أحمد شاكر','12','0','0');
+INSERT INTO [t150958] VALUES (22,'وكم من ألفاظ وضعتها المجامع اللغوية','16','0','0');
