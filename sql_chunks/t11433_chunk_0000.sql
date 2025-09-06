79ab7123aa3cba39695c158bdd2fec3fccc50680
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:21 +0700

    Upload SQL chunk: t11433_chunk_0000.sql

---
+++ sql_chunks/t11433_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11433
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11433" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11433
+INSERT INTO [t11433] VALUES (1,'مخطوط_حدي..','1','0','0');
