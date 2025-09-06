1fc51b2efda6442401e6a05b40f600545efb5834
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:47 +0700

    Upload SQL chunk: t1602_chunk_0000.sql

---
+++ sql_chunks/t1602_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t1602
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t1602" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1602
+INSERT INTO [t1602] VALUES (1,'مقدمة المصنف','2','0','0');
+INSERT INTO [t1602] VALUES (2,'أسئلة في الطهارة','4','0','0');
+INSERT INTO [t1602] VALUES (3,'أسئلة من كتاب الصلاة','48','0','0');
+INSERT INTO [t1602] VALUES (4,'أسئلة من كتاب الزكاة','115','0','0');
+INSERT INTO [t1602] VALUES (5,'أسئلة من كتاب الصيام','125','0','0');
+INSERT INTO [t1602] VALUES (6,'أسئلة في الحج والعمرة وتوابعها','132','0','0');
