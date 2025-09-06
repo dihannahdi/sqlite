bac87ea5eb65e866bf015740d6c54facd6354776
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:43 +0700

    Upload SQL chunk: t463_chunk_0000.sql

---
+++ sql_chunks/t463_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t463
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t463" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t463
+INSERT INTO [t463] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t463] VALUES (2,'المعمور خلف خط الاستواء إلى الجنوب','1','0','0');
+INSERT INTO [t463] VALUES (3,'الإقليم الأول','6','2','0');
+INSERT INTO [t463] VALUES (4,'الإقليم الثاني','19','2','0');
+INSERT INTO [t463] VALUES (5,'الإقليم الثالث','25','2','0');
+INSERT INTO [t463] VALUES (6,'الإقليم الرابع','32','2','0');
+INSERT INTO [t463] VALUES (7,'الإقليم الخامس','48','2','0');
+INSERT INTO [t463] VALUES (8,'الإقليم السادس','55','2','0');
+INSERT INTO [t463] VALUES (9,'الإقليم السابع','64','2','0');
+INSERT INTO [t463] VALUES (10,'المعمور في شمالي الأقاليم السبعة','67','0','0');
