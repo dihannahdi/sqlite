c4610f8cde6ec8434e37bed288ef84dd4c35163d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:57 +0700

    Upload SQL chunk: t2150_chunk_0000.sql

---
+++ sql_chunks/t2150_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t2150
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t2150" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2150
+INSERT INTO [t2150] VALUES (1,'عزف','1','0','0');
+INSERT INTO [t2150] VALUES (2,'المدخل إلى المنهج','9','0','0');
+INSERT INTO [t2150] VALUES (3,'مفهوم التدبر.','11','0','0');
+INSERT INTO [t2150] VALUES (4,'توطئة','21','0','0');
+INSERT INTO [t2150] VALUES (5,'الفصل الأول: فقه موقع السورة على مدرجة السياق القرآنى','25','0','0');
+INSERT INTO [t2150] VALUES (6,'الفصل الثانى: فقه وحدة سياق السورة ومقصودها الأعظم','80','0','0');
+INSERT INTO [t2150] VALUES (7,'الفصل الثالث: تقسيم السورة إلى معاقد كلية','130','0','0');
+INSERT INTO [t2150] VALUES (8,'الفصل الرابع: التحليل البياني','141','0','0');
