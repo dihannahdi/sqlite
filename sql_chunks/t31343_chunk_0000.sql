3ca54a162d93df2f89a3ecbb0c348a69f2383670
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:07 +0700

    Upload SQL chunk: t31343_chunk_0000.sql

---
+++ sql_chunks/t31343_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t31343
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t31343" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31343
+INSERT INTO [t31343] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t31343] VALUES (2,'التمهيد','8','0','0');
+INSERT INTO [t31343] VALUES (3,'المبحث الأول المآخذ على القنوات الفضائية','18','0','0');
+INSERT INTO [t31343] VALUES (4,'المبحث الثاني إيجابيات القنوات الفضائية','36','0','0');
+INSERT INTO [t31343] VALUES (5,'الخاتمة','50','0','0');
+INSERT INTO [t31343] VALUES (6,'خلاصة البحث','50','5','0');
+INSERT INTO [t31343] VALUES (7,'نتائج البحث','52','5','0');
+INSERT INTO [t31343] VALUES (8,'التوصيات','53','5','0');
+INSERT INTO [t31343] VALUES (9,'فهرس المراجع','53','0','0');
