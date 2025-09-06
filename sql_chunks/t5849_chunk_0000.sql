c7eaa725b71fa410edfc17908b3f5a5ffe0535a7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:57 +0700

    Upload SQL chunk: t5849_chunk_0000.sql

---
+++ sql_chunks/t5849_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t5849
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t5849" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5849
+INSERT INTO [t5849] VALUES (1,'حرف الهمزة','1','0','0');
+INSERT INTO [t5849] VALUES (2,'حرف الباء','3','0','0');
+INSERT INTO [t5849] VALUES (3,'حرف الثاء','4','0','0');
+INSERT INTO [t5849] VALUES (4,'حرف الجيم','4','0','0');
+INSERT INTO [t5849] VALUES (5,'حرف الحاء','5','0','0');
+INSERT INTO [t5849] VALUES (6,'حرف الخاء','6','0','0');
+INSERT INTO [t5849] VALUES (7,'حرف الدال','6','0','0');
+INSERT INTO [t5849] VALUES (8,'حرف الذال','6','0','0');
+INSERT INTO [t5849] VALUES (9,'حرف الراء','6','0','0');
+INSERT INTO [t5849] VALUES (10,'حرف الزاي','7','0','0');
+INSERT INTO [t5849] VALUES (11,'حرف السين','8','0','0');
+INSERT INTO [t5849] VALUES (12,'حرف الشين','10','0','0');
+INSERT INTO [t5849] VALUES (13,'حرف الصاد','10','0','0');
+INSERT INTO [t5849] VALUES (14,'حرف الضاد','11','0','0');
+INSERT INTO [t5849] VALUES (15,'حرف الطاء','11','0','0');
+INSERT INTO [t5849] VALUES (16,'حرف العين','11','0','0');
+INSERT INTO [t5849] VALUES (17,'حرف الفاء','21','0','0');
+INSERT INTO [t5849] VALUES (18,'حرف القاف','21','0','0');
+INSERT INTO [t5849] VALUES (19,'حرف الكاف','21','0','0');
+INSERT INTO [t5849] VALUES (20,'حرف الميم','22','0','0');
+INSERT INTO [t5849] VALUES (21,'حرف النون','25','0','0');
+INSERT INTO [t5849] VALUES (22,'حرف الهاء','26','0','0');
+INSERT INTO [t5849] VALUES (23,'حرف الواو','26','0','0');
+INSERT INTO [t5849] VALUES (24,'حرف الياء','27','0','0');
+INSERT INTO [t5849] VALUES (25,'باب في الكنى','27','0','0');
+INSERT INTO [t5849] VALUES (26,'باب في الأبناء والأنساب','30','0','0');
+INSERT INTO [t5849] VALUES (27,'باب في المبهمات','30','0','0');
+INSERT INTO [t5849] VALUES (28,'باب النساء','31','0','0');
+INSERT INTO [t5849] VALUES (29,'فصل في الكنى','32','0','0');
+INSERT INTO [t5849] VALUES (30,'فصل في المبهمات','33','0','0');
