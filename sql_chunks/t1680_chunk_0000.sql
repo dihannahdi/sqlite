0fe7a714a9e16f6c995c35b965d646810b7ce3fb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:27:07 +0700

    Upload SQL chunk: t1680_chunk_0000.sql

---
+++ sql_chunks/t1680_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t1680
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t1680" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1680
+INSERT INTO [t1680] VALUES (1,'مقدمة التحقيق','2','0','0');
+INSERT INTO [t1680] VALUES (2,'التعريف بالدارقطني','7','1','0');
+INSERT INTO [t1680] VALUES (3,'التعريف بالسلمي','28','1','0');
+INSERT INTO [t1680] VALUES (4,'التعريف بكتب السؤالات','36','1','0');
+INSERT INTO [t1680] VALUES (5,'التعريف بكتاب "سؤالات السلمي":','43','4','0');
+INSERT INTO [t1680] VALUES (6,'مقدمة المصنف','75','0','0');
+INSERT INTO [t1680] VALUES (7,'باب الألف','76','0','0');
+INSERT INTO [t1680] VALUES (8,'باب الباء','121','0','0');
+INSERT INTO [t1680] VALUES (9,'باب التاء','135','0','0');
+INSERT INTO [t1680] VALUES (10,'باب الثاء','136','0','0');
+INSERT INTO [t1680] VALUES (11,'باب الجيم','137','0','0');
+INSERT INTO [t1680] VALUES (12,'باب الحاء','145','0','0');
+INSERT INTO [t1680] VALUES (13,'باب الخاء','152','0','0');
+INSERT INTO [t1680] VALUES (14,'باب الدال','156','0','0');
+INSERT INTO [t1680] VALUES (15,'باب الذال','157','0','0');
+INSERT INTO [t1680] VALUES (16,'باب الراء','158','0','0');
+INSERT INTO [t1680] VALUES (17,'باب الزاي','164','0','0');
+INSERT INTO [t1680] VALUES (18,'باب السين','169','0','0');
+INSERT INTO [t1680] VALUES (19,'باب الشين','182','0','0');
+INSERT INTO [t1680] VALUES (20,'باب الصاد','186','0','0');
+INSERT INTO [t1680] VALUES (21,'باب الضاد','188','0','0');
+INSERT INTO [t1680] VALUES (22,'باب الطاء','189','0','0');
+INSERT INTO [t1680] VALUES (23,'باب العين','190','0','0');
+INSERT INTO [t1680] VALUES (24,'باب الفاء','251','0','0');
+INSERT INTO [t1680] VALUES (25,'باب القاف','258','0','0');
+INSERT INTO [t1680] VALUES (26,'باب الكاف','261','0','0');
+INSERT INTO [t1680] VALUES (27,'باب الميم','263','0','0');
+INSERT INTO [t1680] VALUES (28,'باب النون','305','0','0');
+INSERT INTO [t1680] VALUES (29,'باب الواو','308','0','0');
+INSERT INTO [t1680] VALUES (30,'باب الهاء','312','0','0');
+INSERT INTO [t1680] VALUES (31,'باب الياء','315','0','0');
+INSERT INTO [t1680] VALUES (32,'باب','347','0','0');
+INSERT INTO [t1680] VALUES (33,'فهارس كتاب سؤالات السلمي','358','0','0');
+INSERT INTO [t1680] VALUES (34,'١- فهرس الآيات القرآنية','359','33','0');
+INSERT INTO [t1680] VALUES (35,'٢- فهرس الأحاديث والآثار والأقوال','360','33','0');
+INSERT INTO [t1680] VALUES (36,'٣- فهرس الأعلام','365','33','0');
+INSERT INTO [t1680] VALUES (37,'٤- فهرس مراجع التحقيق','401','33','0');
+INSERT INTO [t1680] VALUES (38,'٥- فهرس الموضوعات','441','33','0');
