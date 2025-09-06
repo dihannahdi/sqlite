7e74bf2baaebe9bece4be6bd4d8e352fde088829
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:56 +0700

    Upload SQL chunk: t95669_chunk_0000.sql

---
+++ sql_chunks/t95669_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t95669
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t95669" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95669
+INSERT INTO [t95669] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95669] VALUES (2,'الزوج والزوجة','2','0','0');
+INSERT INTO [t95669] VALUES (3,'فلذات الأكباد','5','0','0');
+INSERT INTO [t95669] VALUES (4,'حفظ كتاب الله عز وجل','7','0','0');
+INSERT INTO [t95669] VALUES (5,'قال في مختصر منهاج القاصدين','8','4','0');
+INSERT INTO [t95669] VALUES (6,'الأبناء','9','0','0');
+INSERT INTO [t95669] VALUES (7,'التحول الكبير','11','0','0');
+INSERT INTO [t95669] VALUES (8,'المصائب والمحن','14','0','0');
+INSERT INTO [t95669] VALUES (9,'الداعية','16','0','0');
+INSERT INTO [t95669] VALUES (10,'رحمة الله','22','0','0');
+INSERT INTO [t95669] VALUES (11,'غرفة الأحزان','24','0','0');
+INSERT INTO [t95669] VALUES (12,'تأملات في القصة','35','0','0');
+INSERT INTO [t95669] VALUES (13,'الخاتمة','38','0','0');
