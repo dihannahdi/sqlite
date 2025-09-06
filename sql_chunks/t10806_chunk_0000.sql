329d5a740eba5c8b28a6c0c8b6841a093df0e9d0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:39 +0700

    Upload SQL chunk: t10806_chunk_0000.sql

---
+++ sql_chunks/t10806_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10806
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10806" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10806
+INSERT INTO [t10806] VALUES (1,'الجزء الأول','2','0','0');
+INSERT INTO [t10806] VALUES (2,'الجزء الثاني','69','0','0');
