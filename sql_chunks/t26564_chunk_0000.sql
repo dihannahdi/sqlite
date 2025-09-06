5b7adbebe2a13d2822d9f7151a25f69e8622b5ed
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:32 +0700

    Upload SQL chunk: t26564_chunk_0000.sql

---
+++ sql_chunks/t26564_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26564
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26564" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26564
+INSERT INTO [t26564] VALUES (1,'جزء الحسن بن رشيق العسكري','2','0','0');
