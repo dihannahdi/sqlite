799abb3e5051715d9ca720f7dc641cdbb32c3cda
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:16 +0700

    Upload SQL chunk: t10911_chunk_0000.sql

---
+++ sql_chunks/t10911_chunk_0000.sql
@@ -0,0 +1,47 @@
+-- Table: t10911
+-- Chunk: 1
+-- Rows: 40
+
+CREATE TABLE "t10911" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10911
+INSERT INTO [t10911] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t10911] VALUES (2,'الباب الثاني','15','1','0');
+INSERT INTO [t10911] VALUES (3,'وزن الشعر وما يلحقه','15','0','0');
+INSERT INTO [t10911] VALUES (4,'ما يلحق آخر الشطر','16','3','0');
+INSERT INTO [t10911] VALUES (5,'التقفية والتصريع','16','3','0');
+INSERT INTO [t10911] VALUES (6,'والإقعاد، والتخميع، والوقف','16','3','0');
+INSERT INTO [t10911] VALUES (7,'ما يلحق آخر الشطر','26','3','0');
+INSERT INTO [t10911] VALUES (8,'الباب الثالث','32','3','0');
+INSERT INTO [t10911] VALUES (9,'لوازم القافية','32','0','0');
+INSERT INTO [t10911] VALUES (10,'الكلام في الحروف اللازمة','33','9','0');
+INSERT INTO [t10911] VALUES (11,'الروي','33','9','0');
+INSERT INTO [t10911] VALUES (12,'التآسيس','46','9','0');
+INSERT INTO [t10911] VALUES (13,'الردف','46','9','0');
+INSERT INTO [t10911] VALUES (14,'الصلة','48','9','0');
+INSERT INTO [t10911] VALUES (15,'وتسمى الوصل أيضا','48','9','0');
+INSERT INTO [t10911] VALUES (16,'الخروج','49','9','0');
+INSERT INTO [t10911] VALUES (17,'الإقواء','49','9','0');
+INSERT INTO [t10911] VALUES (18,'الحركات اللازمة','49','9','0');
+INSERT INTO [t10911] VALUES (19,'الرس','49','9','0');
+INSERT INTO [t10911] VALUES (20,'الإشباع','49','9','0');
+INSERT INTO [t10911] VALUES (21,'المجرى','49','9','0');
+INSERT INTO [t10911] VALUES (22,'الحذو','49','9','0');
+INSERT INTO [t10911] VALUES (23,'التوجيه','49','9','0');
+INSERT INTO [t10911] VALUES (24,'النفاذ','49','9','0');
+INSERT INTO [t10911] VALUES (25,'الباب الرابع','51','9','0');
+INSERT INTO [t10911] VALUES (26,'عدد القوافي','51','0','0');
+INSERT INTO [t10911] VALUES (27,'الباب الخامس','51','26','0');
+INSERT INTO [t10911] VALUES (28,'اللين في القوافي','51','0','0');
+INSERT INTO [t10911] VALUES (29,'ما يلزمه اللين في القوافي','51','28','0');
+INSERT INTO [t10911] VALUES (30,'المد واللين في الوصل','52','28','0');
+INSERT INTO [t10911] VALUES (31,'النشيد والترنم','52','28','0');
+INSERT INTO [t10911] VALUES (32,'الباب السادس','53','28','0');
+INSERT INTO [t10911] VALUES (33,'عيوب القافية','53','0','0');
+INSERT INTO [t10911] VALUES (34,'الإكفاء','54','33','0');
+INSERT INTO [t10911] VALUES (35,'البدل','55','33','0');
+INSERT INTO [t10911] VALUES (36,'السناد','56','33','0');
+INSERT INTO [t10911] VALUES (37,'الإجازة','57','33','0');
+INSERT INTO [t10911] VALUES (38,'التضمين','57','33','0');
+INSERT INTO [t10911] VALUES (39,'المعاظلة','57','33','0');
+INSERT INTO [t10911] VALUES (40,'التحريد','57','33','0');
