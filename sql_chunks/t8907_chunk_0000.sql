ee710b90d244e683464498a7849e6baed6f82033
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:38 +0700

    Upload SQL chunk: t8907_chunk_0000.sql

---
+++ sql_chunks/t8907_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t8907
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t8907" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8907
+INSERT INTO [t8907] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8907] VALUES (2,'الإسلام والتنمية','3','0','0');
+INSERT INTO [t8907] VALUES (3,'التنمية تكليف إسلامي','5','0','0');
+INSERT INTO [t8907] VALUES (4,'موقف الإسلام من الخمر والمخدرات','7','0','0');
+INSERT INTO [t8907] VALUES (5,'نظريات التنمية والتخلف','9','0','0');
+INSERT INTO [t8907] VALUES (6,'الإستعمار والمخدرات','11','0','0');
+INSERT INTO [t8907] VALUES (7,'أخطار المخدرات','13','0','0');
+INSERT INTO [t8907] VALUES (8,'المخدرات في العالم الإسلامي','15','0','0');
+INSERT INTO [t8907] VALUES (9,'المخدرات والتنمية في لبنان','17','0','0');
+INSERT INTO [t8907] VALUES (10,'مشاكل القات والتنمية','19','0','0');
+INSERT INTO [t8907] VALUES (11,'الجهود الدينية والإعلامية','22','0','0');
+INSERT INTO [t8907] VALUES (12,'المخدرات والأسرة','24','0','0');
+INSERT INTO [t8907] VALUES (13,'المخدرات والجريمة','26','0','0');
+INSERT INTO [t8907] VALUES (14,'ضحايا التنمية','29','0','0');
+INSERT INTO [t8907] VALUES (15,'المخدرات والفئات العامة','31','0','0');
+INSERT INTO [t8907] VALUES (16,'المخدرات والشباب والجاسوسية','33','0','0');
+INSERT INTO [t8907] VALUES (17,'المخدرات أخطر معوقات التنيمة','35','0','0');
+INSERT INTO [t8907] VALUES (18,'كلمة ختامية','37','0','0');
