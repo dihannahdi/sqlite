d0f2e82ef0391a8eb122e7fec15d53e38130e990
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:36 +0700

    Upload SQL chunk: t31246_chunk_0000.sql

---
+++ sql_chunks/t31246_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t31246
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t31246" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31246
+INSERT INTO [t31246] VALUES (1,'باب آداب المشي إلى الصلاة','1','0','0');
+INSERT INTO [t31246] VALUES (2,'باب صفة الصلاة','4','0','0');
+INSERT INTO [t31246] VALUES (3,'جهر الإمام بالتكبير','6','2','0');
+INSERT INTO [t31246] VALUES (4,'الصلاة في مكان شديد الحر أو شديد البرد','16','2','0');
+INSERT INTO [t31246] VALUES (5,'الذكر والدعاء والاستغفار عقب الصلاة','24','2','0');
+INSERT INTO [t31246] VALUES (6,'الصلاة إلى سترة','29','2','0');
+INSERT INTO [t31246] VALUES (7,'الواجبات التي تسقط سهوا','33','2','0');
+INSERT INTO [t31246] VALUES (8,'سجود السهو','36','2','0');
+INSERT INTO [t31246] VALUES (9,'العمل اليسير في الصلاة','38','2','0');
+INSERT INTO [t31246] VALUES (10,'من شك في عدد الركعات','39','2','0');
+INSERT INTO [t31246] VALUES (11,'باب صلاة التطوع','41','0','0');
