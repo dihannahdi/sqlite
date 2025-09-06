1bae1d4adfc628f0980828fb086dc6ace90b27e2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:43 +0700

    Upload SQL chunk: t12088_chunk_0000.sql

---
+++ sql_chunks/t12088_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t12088
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t12088" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12088
+INSERT INTO [t12088] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12088] VALUES (2,'تمهيد','3','0','0');
+INSERT INTO [t12088] VALUES (3,'الفصل الأول: أسس منهج طلب العلم عند السلف','12','0','0');
+INSERT INTO [t12088] VALUES (4,'الفصل الثاني: ما هو العلم المراد تعلمه','26','0','0');
+INSERT INTO [t12088] VALUES (5,'الفصل الثالث: البرنامج العملي للطالب أثناء تعلمه','35','0','0');
+INSERT INTO [t12088] VALUES (6,'الملحق الأول: طريقة تعليم المتعلم، وإرشاد المعلم في تعليمه','60','0','0');
+INSERT INTO [t12088] VALUES (7,'الملحق الثاني','67','0','0');
+INSERT INTO [t12088] VALUES (8,'مصادر ومراجع','69','0','0');
