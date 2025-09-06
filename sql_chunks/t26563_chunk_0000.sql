f1f055ab93f123719fb3065cfa302c91215f9741
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:31 +0700

    Upload SQL chunk: t26563_chunk_0000.sql

---
+++ sql_chunks/t26563_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26563
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26563" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26563
+INSERT INTO [t26563] VALUES (1,'جزء أبي العباس العصمي','1','0','0');
