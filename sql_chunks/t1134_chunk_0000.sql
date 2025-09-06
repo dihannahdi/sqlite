c8ee01f3389deb1b682b4ace74ef76131e8d2304
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:19 +0700

    Upload SQL chunk: t1134_chunk_0000.sql

---
+++ sql_chunks/t1134_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t1134
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t1134" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1134
+INSERT INTO [t1134] VALUES (1,'أولا الخلفيات والأصول العلمية والتاريخية للغلو في الدين','1','0','0');
+INSERT INTO [t1134] VALUES (2,'ثانيا أهم أسباب الغلو','5','0','0');
+INSERT INTO [t1134] VALUES (3,'أسباب داخل المجتمع السعودي','5','2','0');
+INSERT INTO [t1134] VALUES (4,'الأسباب العامة في ظهور الغلو والتشدد في كل زمان وكل مكان','10','2','0');
+INSERT INTO [t1134] VALUES (5,'أسباب ظهور الغلو ومظاهره في العصر الحديث','12','2','0');
+INSERT INTO [t1134] VALUES (6,'ثالثا العلاج','21','0','0');
