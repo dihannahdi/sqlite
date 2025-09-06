7225ed145d56e21f545735a73f3c6e7ec6a5de4e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:23:37 +0700

    Upload SQL chunk: t5550_chunk_0000.sql

---
+++ sql_chunks/t5550_chunk_0000.sql
@@ -0,0 +1,61 @@
+-- Table: t5550
+-- Chunk: 1
+-- Rows: 54
+
+CREATE TABLE "t5550" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5550
+INSERT INTO [t5550] VALUES (1,'شرح وتلخيص','1','0','0');
+INSERT INTO [t5550] VALUES (2,'أفي تنزيه الله','1','0','0');
+INSERT INTO [t5550] VALUES (3,'ب في العقل','2','0','0');
+INSERT INTO [t5550] VALUES (4,'القسم الأول','2','0','0');
+INSERT INTO [t5550] VALUES (5,'أفضائل القرآن','2','0','0');
+INSERT INTO [t5550] VALUES (6,'ب فضائل القراء','3','0','0');
+INSERT INTO [t5550] VALUES (7,'القسم الثاني','4','0','0');
+INSERT INTO [t5550] VALUES (8,'القسم الثالث','4','0','0');
+INSERT INTO [t5550] VALUES (9,'- صفات الله وأسماؤه','4','0','0');
+INSERT INTO [t5550] VALUES (10,'- وأخباره تعالى عما كان ويكون','4','0','0');
+INSERT INTO [t5550] VALUES (11,'البدوات وحدوث الارادات','5','0','0');
+INSERT INTO [t5550] VALUES (12,'السمع والبصر','6','0','0');
+INSERT INTO [t5550] VALUES (13,'العلو','6','0','0');
+INSERT INTO [t5550] VALUES (14,'الحشوية والنسخ في الأخبار','7','0','0');
+INSERT INTO [t5550] VALUES (15,'أين يجوز النسخ','7','0','0');
+INSERT INTO [t5550] VALUES (16,'القول بخلق القرآن','8','0','0');
+INSERT INTO [t5550] VALUES (17,'القسم الرابع مع المعتزلة دفاع والتزامات','9','0','0');
+INSERT INTO [t5550] VALUES (18,'في الوعد والوعيد','10','0','0');
+INSERT INTO [t5550] VALUES (19,'الزام المعتزلة','10','0','0');
+INSERT INTO [t5550] VALUES (20,'أامتناع الخوف والرجاء','10','0','0');
+INSERT INTO [t5550] VALUES (21,'ب امتناع العفو','11','0','0');
+INSERT INTO [t5550] VALUES (22,'القسم الخامس','11','0','0');
+INSERT INTO [t5550] VALUES (23,'القسم السادس','15','0','0');
+INSERT INTO [t5550] VALUES (24,'التقديم والتأخير','15','0','0');
+INSERT INTO [t5550] VALUES (25,'الإضمار','15','0','0');
+INSERT INTO [t5550] VALUES (26,'الحروف الزوائدة','15','0','0');
+INSERT INTO [t5550] VALUES (27,'الفصل والوصل','16','0','0');
+INSERT INTO [t5550] VALUES (28,'فهم القرآن','18','0','0');
+INSERT INTO [t5550] VALUES (29,'في تنزيه الله','18','0','0');
+INSERT INTO [t5550] VALUES (30,'في العقل','21','0','0');
+INSERT INTO [t5550] VALUES (31,'القسم الأول','26','0','0');
+INSERT INTO [t5550] VALUES (32,'فضائل القراء','45','0','0');
+INSERT INTO [t5550] VALUES (33,'القسم الثاني','57','0','0');
+INSERT INTO [t5550] VALUES (34,'القسم الثالث','80','0','0');
+INSERT INTO [t5550] VALUES (35,'القسم الرابع - باب ما لا يجوز فيه النسخ وما يجوز ذلك فيه','87','0','0');
+INSERT INTO [t5550] VALUES (36,'أخباره تعالى عما كان ويكون','87','0','0');
+INSERT INTO [t5550] VALUES (37,'البداوات وحدوث الإرادات','96','0','0');
+INSERT INTO [t5550] VALUES (38,'السمع والبصر','99','0','0');
+INSERT INTO [t5550] VALUES (39,'العلو','101','0','0');
+INSERT INTO [t5550] VALUES (40,'الحشوية والنسخ في الأخبار','111','0','0');
+INSERT INTO [t5550] VALUES (41,'القول بخلق القرآن','118','0','0');
+INSERT INTO [t5550] VALUES (42,'القسم الخامس','125','0','0');
+INSERT INTO [t5550] VALUES (43,'دفاع عن أهل السنة','125','0','0');
+INSERT INTO [t5550] VALUES (44,'في الوعد والوعيد','127','0','0');
+INSERT INTO [t5550] VALUES (45,'وأما قولهم لا يخلو من ثلاث خلال','131','0','0');
+INSERT INTO [t5550] VALUES (46,'وأحسن الأعمال التوحيد','135','0','0');
+INSERT INTO [t5550] VALUES (47,'فكل من أقر فقد آمن','139','0','0');
+INSERT INTO [t5550] VALUES (48,'إمتناع الخوف والرجاء','140','0','0');
+INSERT INTO [t5550] VALUES (49,'إمتناع العفو','144','0','0');
+INSERT INTO [t5550] VALUES (50,'امتناع شفاعة النبي','145','0','0');
+INSERT INTO [t5550] VALUES (51,'القسم السادس','149','0','0');
+INSERT INTO [t5550] VALUES (52,'القسم السابع','231','0','0');
+INSERT INTO [t5550] VALUES (53,'الحروف الزوائد','243','0','0');
+INSERT INTO [t5550] VALUES (54,'المفصل والموصول','247','0','0');
