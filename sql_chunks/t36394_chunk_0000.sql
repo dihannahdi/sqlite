2808196dd08c4e106a25d5a974959ae211f0bef7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:47 +0700

    Upload SQL chunk: t36394_chunk_0000.sql

---
+++ sql_chunks/t36394_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t36394
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t36394" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36394
+INSERT INTO [t36394] VALUES (1,'سنة الدعوات','3','0','0');
+INSERT INTO [t36394] VALUES (2,'النظرة المتكاملة','3','0','0');
+INSERT INTO [t36394] VALUES (3,'أولى هذه النقاط: ضرورة التحليل العميق والمتكامل لحدث الابتلاء','3','2','0');
+INSERT INTO [t36394] VALUES (4,'ثانيا: الابتلاء من عند الله','4','2','0');
+INSERT INTO [t36394] VALUES (5,'ثالثا: على قدر المنزلة تكون المسئولية','5','2','0');
+INSERT INTO [t36394] VALUES (6,'رابعا: سبب الابتلاء قد يكون من جانب أبناء الدعوة','5','2','0');
+INSERT INTO [t36394] VALUES (7,'خامسا: أأنا السبب؟!','6','2','0');
+INSERT INTO [t36394] VALUES (8,'سادسا: الابتلاء قد يكون منحة ربانية','7','2','0');
+INSERT INTO [t36394] VALUES (9,'من حكم التمحيص قبل التمكين','8','0','0');
+INSERT INTO [t36394] VALUES (10,'ولماذا يبتلى الأنبياء؟!','8','0','0');
+INSERT INTO [t36394] VALUES (11,'نموذج عملي لكيفية تحليل حدث الابتلاء','8','0','0');
