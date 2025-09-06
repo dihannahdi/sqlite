9949caaf3d53f88d7eff5a4d5495ff38189430c1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:33 +0700

    Upload SQL chunk: t11217_chunk_0000.sql

---
+++ sql_chunks/t11217_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11217
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11217" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11217
+INSERT INTO [t11217] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11217] VALUES (2,'رسالة في الرد على الرافضة','3','0','0');
