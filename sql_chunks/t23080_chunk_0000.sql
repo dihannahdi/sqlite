a9087a468a519764d2a166515a61c859ae9bdeae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:48 +0700

    Upload SQL chunk: t23080_chunk_0000.sql

---
+++ sql_chunks/t23080_chunk_0000.sql
@@ -0,0 +1,48 @@
+-- Table: t23080
+-- Chunk: 1
+-- Rows: 41
+
+CREATE TABLE "t23080" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23080
+INSERT INTO [t23080] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t23080] VALUES (2,'الباب الأول: باب (التعرف على الله تعالى)','5','0','0');
+INSERT INTO [t23080] VALUES (3,'مدخل','5','2','0');
+INSERT INTO [t23080] VALUES (4,'مفهوم الأسماء والصفات','6','2','0');
+INSERT INTO [t23080] VALUES (5,'أسماء الله الحسنى','6','4','0');
+INSERT INTO [t23080] VALUES (6,'بيان الأسماء الحسنى التي وردت النصوص بها في الكتاب والسنة','14','4','0');
+INSERT INTO [t23080] VALUES (7,'ما ورد في القرآن الكريم (سورة الفاتحة)','14','6','0');
+INSERT INTO [t23080] VALUES (8,'مدخل','27','0','0');
+INSERT INTO [t23080] VALUES (9,'المحيط','29','0','0');
+INSERT INTO [t23080] VALUES (10,'القدير','30','0','0');
+INSERT INTO [t23080] VALUES (11,'القادر','31','0','0');
+INSERT INTO [t23080] VALUES (12,'المقتدر','32','0','0');
+INSERT INTO [t23080] VALUES (13,'العليم','33','0','0');
+INSERT INTO [t23080] VALUES (14,'العالم','34','0','0');
+INSERT INTO [t23080] VALUES (15,'العلام','35','0','0');
+INSERT INTO [t23080] VALUES (16,'الحكيم','37','0','0');
+INSERT INTO [t23080] VALUES (17,'التواب','38','0','0');
+INSERT INTO [t23080] VALUES (18,'البارئ','39','0','0');
+INSERT INTO [t23080] VALUES (19,'البصير','40','0','0');
+INSERT INTO [t23080] VALUES (20,'الواسع، (الموسع)','41','0','0');
+INSERT INTO [t23080] VALUES (21,'السميع','43','0','0');
+INSERT INTO [t23080] VALUES (22,'المستمع','45','0','0');
+INSERT INTO [t23080] VALUES (23,'العزيز','46','0','0');
+INSERT INTO [t23080] VALUES (24,'الرءوف','47','0','0');
+INSERT INTO [t23080] VALUES (25,'الشاكر و (الشكور)','48','0','0');
+INSERT INTO [t23080] VALUES (26,'الواحد و (الأحد)','50','0','0');
+INSERT INTO [t23080] VALUES (27,'الغفور','52','0','0');
+INSERT INTO [t23080] VALUES (28,'والغفار','55','0','0');
+INSERT INTO [t23080] VALUES (29,'والغافر','56','0','0');
+INSERT INTO [t23080] VALUES (30,'القريب','57','0','0');
+INSERT INTO [t23080] VALUES (31,'مدخل','60','0','0');
+INSERT INTO [t23080] VALUES (32,'الحي','62','0','0');
+INSERT INTO [t23080] VALUES (33,'القيوم','66','0','0');
+INSERT INTO [t23080] VALUES (34,'العلي','72','0','0');
+INSERT INTO [t23080] VALUES (35,'المتعالي','78','0','0');
+INSERT INTO [t23080] VALUES (36,'الأعلى','79','0','0');
+INSERT INTO [t23080] VALUES (37,'العظيم','82','0','0');
+INSERT INTO [t23080] VALUES (38,'الغني','85','0','0');
+INSERT INTO [t23080] VALUES (39,'مدخل','96','0','0');
+INSERT INTO [t23080] VALUES (40,'المولى','97','0','0');
+INSERT INTO [t23080] VALUES (41,'الولي','106','0','0');
