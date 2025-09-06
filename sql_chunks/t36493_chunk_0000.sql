3438f942ee0fd8e24e0583f16f167bd2dcf8db55
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:28 +0700

    Upload SQL chunk: t36493_chunk_0000.sql

---
+++ sql_chunks/t36493_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t36493
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t36493" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36493
+INSERT INTO [t36493] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36493] VALUES (2,'العابدة المطلقة','2','0','0');
+INSERT INTO [t36493] VALUES (3,'ثلاثة أجزاء','4','0','0');
+INSERT INTO [t36493] VALUES (4,'النفقة','5','0','0');
+INSERT INTO [t36493] VALUES (5,'وقفة للمشمرين','7','0','0');
+INSERT INTO [t36493] VALUES (6,'مصلى الجامعة','8','0','0');
+INSERT INTO [t36493] VALUES (7,'( ... ولو مرة)','9','0','0');
+INSERT INTO [t36493] VALUES (8,'العجوز العاملة','10','0','0');
+INSERT INTO [t36493] VALUES (9,'العزة لمن؟','12','0','0');
+INSERT INTO [t36493] VALUES (10,'الشاب وقيام الليل','15','0','0');
+INSERT INTO [t36493] VALUES (11,'الجارة الداعية','17','0','0');
+INSERT INTO [t36493] VALUES (12,'«كتاب الله عز وجل»','20','0','0');
+INSERT INTO [t36493] VALUES (13,'النية','22','0','0');
+INSERT INTO [t36493] VALUES (14,'المربية الصالحة','24','0','0');
+INSERT INTO [t36493] VALUES (15,'سنة التعدد','26','0','0');
+INSERT INTO [t36493] VALUES (16,'الزوجة البارة','28','0','0');
+INSERT INTO [t36493] VALUES (17,'بر الوالدين','30','0','0');
