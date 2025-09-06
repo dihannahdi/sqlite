bbe5f5c01fe1605a2bfc50d481aa85dbcc534503
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:45 +0700

    Upload SQL chunk: t511_chunk_0000.sql

---
+++ sql_chunks/t511_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t511
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t511" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t511
+INSERT INTO [t511] VALUES (1,'نبذة في علم العروض','3','0','0');
+INSERT INTO [t511] VALUES (2,'الباب الأول: في بيان ألقاب الزحاف والعلل وتعاريفها','5','1','0');
+INSERT INTO [t511] VALUES (3,'الباب الثاني: في البحور وموازنها وعروضها','8','1','0');
+INSERT INTO [t511] VALUES (4,'الطويل','8','3','0');
+INSERT INTO [t511] VALUES (5,'المديد ــ البسيط','9','3','0');
+INSERT INTO [t511] VALUES (6,'الوافر ــ الكامل','10','3','0');
+INSERT INTO [t511] VALUES (7,'الهزج ــ الرجز','11','3','0');
+INSERT INTO [t511] VALUES (8,'الرمل ــ السريع','13','3','0');
+INSERT INTO [t511] VALUES (9,'المنسرح','14','3','0');
+INSERT INTO [t511] VALUES (10,'الخفيف ــ المضارع','15','3','0');
+INSERT INTO [t511] VALUES (11,'المقتضب','16','3','0');
+INSERT INTO [t511] VALUES (12,'المجتث ــ المتقارب','17','3','0');
+INSERT INTO [t511] VALUES (13,'المتدارك أو المخترع أو الخبب وطرد الخيل والمحدث','18','3','0');
+INSERT INTO [t511] VALUES (14,'خاتمة في ألقاب الأبيات ونحوها','19','1','0');
+INSERT INTO [t511] VALUES (15,'العلم الثاني: وهو علم القوافي','21','0','0');
+INSERT INTO [t511] VALUES (16,'تعريف القافية وحروفها','21','15','0');
+INSERT INTO [t511] VALUES (17,'أنواع القافية','23','15','0');
+INSERT INTO [t511] VALUES (18,'نظم بحور العروض','28','15','0');
+INSERT INTO [t511] VALUES (19,'ملحق فوائد وتدقيقات لغوية من تعليقات الشيخ المعلمي على المعاني الكبير ونحوه','32','15','0');
