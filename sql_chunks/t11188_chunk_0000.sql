12a323598892f34ea40864b7e62f69ccd68faae0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:08 +0700

    Upload SQL chunk: t11188_chunk_0000.sql

---
+++ sql_chunks/t11188_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11188
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11188" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11188
+INSERT INTO [t11188] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11188] VALUES (2,'تمهيد','13','0','0');
+INSERT INTO [t11188] VALUES (3,'الشيخ محمد بن عبد الوهاب، وأكابر علماء ديوبند','40','0','0');
