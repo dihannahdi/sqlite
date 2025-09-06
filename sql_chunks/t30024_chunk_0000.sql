c43a68c838e84cf1873cce809bdd16df77ed9f00
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:14 +0700

    Upload SQL chunk: t30024_chunk_0000.sql

---
+++ sql_chunks/t30024_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t30024
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t30024" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30024
+INSERT INTO [t30024] VALUES (1,'إلى هنا عن أبي مسرور المقرئ عن ابن نجيد','5','0','0');
+INSERT INTO [t30024] VALUES (2,'إلى هنا عن النسوي','10','0','0');
+INSERT INTO [t30024] VALUES (3,'إلى هنا عن اليزدي','12','0','0');
+INSERT INTO [t30024] VALUES (4,'إلى هنا عن القرشي','14','0','0');
+INSERT INTO [t30024] VALUES (5,'إلى هنا عن ناصر عن شيوخه','19','0','0');
+INSERT INTO [t30024] VALUES (6,'إلى هنا عن الصابوني','26','0','0');
+INSERT INTO [t30024] VALUES (7,'ومن حديث ناصر العمري أيضا','27','0','0');
