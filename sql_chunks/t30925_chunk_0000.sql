7ba99ad5e4b3bcc91e9c2ad054c3e15806af96ac
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:48 +0700

    Upload SQL chunk: t30925_chunk_0000.sql

---
+++ sql_chunks/t30925_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t30925
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t30925" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30925
+INSERT INTO [t30925] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t30925] VALUES (2,'تمهيد','4','0','0');
+INSERT INTO [t30925] VALUES (3,'العلماء ومنزلتهم وخصائصهم','8','0','0');
+INSERT INTO [t30925] VALUES (4,'المفهوم الشرعي للعلماء','9','0','0');
+INSERT INTO [t30925] VALUES (5,'خصائص العلماء وسماتهم','9','0','0');
+INSERT INTO [t30925] VALUES (6,'المسجد ورسالته','10','0','0');
+INSERT INTO [t30925] VALUES (7,'المعنيون بتحقيق رسالة المسجد','13','0','0');
+INSERT INTO [t30925] VALUES (8,'أهم الأمور التي يمكن أن يحققها العلماء من خلال المسجد','15','0','0');
+INSERT INTO [t30925] VALUES (9,'الإمامة','15','8','0');
+INSERT INTO [t30925] VALUES (10,'الخطابة','17','8','0');
+INSERT INTO [t30925] VALUES (11,'الفتوى','17','8','0');
+INSERT INTO [t30925] VALUES (12,'الدروس','18','8','0');
+INSERT INTO [t30925] VALUES (13,'المحاضرات والندوات','22','8','0');
+INSERT INTO [t30925] VALUES (14,'الكلمات والتوجيهات والمواعظ','22','8','0');
+INSERT INTO [t30925] VALUES (15,'أعمال الحسبة الأخرى','25','8','0');
+INSERT INTO [t30925] VALUES (16,'اقتراحات عامة','25','0','0');
