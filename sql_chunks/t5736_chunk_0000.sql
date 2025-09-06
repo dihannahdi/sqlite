1f4df4770a26883f6e9154d6a5118baad9abad27
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:55 +0700

    Upload SQL chunk: t5736_chunk_0000.sql

---
+++ sql_chunks/t5736_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t5736
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t5736" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5736
+INSERT INTO [t5736] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t5736] VALUES (2,'منتخب من حديث الزهري','2','0','0');
+INSERT INTO [t5736] VALUES (3,'فوائد سمويه','36','0','0');
+INSERT INTO [t5736] VALUES (4,'فوائد قاسم المطرز','122','0','0');
+INSERT INTO [t5736] VALUES (5,'حديث ابن مخلد البزاز عن شيوخه','265','0','0');
+INSERT INTO [t5736] VALUES (6,'حديث ابن السماك والخلدي','348','0','0');
+INSERT INTO [t5736] VALUES (7,'فوائد المؤمل بن أحمد الشيباني','417','0','0');
+INSERT INTO [t5736] VALUES (8,'فوائد العيسوي','474','0','0');
+INSERT INTO [t5736] VALUES (9,'أمالي ابن النحاس','548','0','0');
+INSERT INTO [t5736] VALUES (10,'حديث مكي بن أبي طالب ومحمود المزاحمي','580','0','0');
+INSERT INTO [t5736] VALUES (11,'مجلس ابن فاخر الأصبهاني','618','0','0');
