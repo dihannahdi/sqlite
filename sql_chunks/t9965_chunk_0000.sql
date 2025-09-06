ae83e788ea0217bf78f1334fca2dc24e04154650
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:20 +0700

    Upload SQL chunk: t9965_chunk_0000.sql

---
+++ sql_chunks/t9965_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9965
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9965" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9965
+INSERT INTO [t9965] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t9965] VALUES (2,'جزء فيه تبيين الامتنان بالأمر بالاختتان','2','0','0');
