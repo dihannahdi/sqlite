8d921e338527331a7a6f0350b352e46e348768ce
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:08 +0700

    Upload SQL chunk: t26887_chunk_0000.sql

---
+++ sql_chunks/t26887_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t26887
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t26887" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26887
+INSERT INTO [t26887] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t26887] VALUES (2,'الزهري وكتابه','3','1','0');
+INSERT INTO [t26887] VALUES (3,'كتاب الناسخ والمنسوخ المنسوب إلى الزهري','4','1','0');
+INSERT INTO [t26887] VALUES (4,'الناسخ والمنسوخ للزهري','9','0','0');
+INSERT INTO [t26887] VALUES (5,'أول الناسخ ما رواه محمد بن مسلم الزهري','13','4','0');
+INSERT INTO [t26887] VALUES (6,'تنزيل القرآن بمكة والمدينة','32','0','0');
+INSERT INTO [t26887] VALUES (7,'فأول ما أنزل الله بمكة','32','6','0');
+INSERT INTO [t26887] VALUES (8,'ثم يأتي ما أنزل بالمدينة','36','6','0');
+INSERT INTO [t26887] VALUES (9,'فهرس المصادر','38','0','0');
