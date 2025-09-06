fb15c5c7670d4f8c270bc92dee750e1c121a4168
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:02:45 +0700

    Upload SQL chunk: t98135_chunk_0000.sql

---
+++ sql_chunks/t98135_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t98135
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t98135" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t98135
+INSERT INTO [t98135] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t98135] VALUES (2,'إعفاء اللحية طاعة','5','0','0');
+INSERT INTO [t98135] VALUES (3,'حلق اللحية معصية','6','0','0');
+INSERT INTO [t98135] VALUES (4,'إعفاء اللحية سنة محمدية','7','0','0');
+INSERT INTO [t98135] VALUES (5,'حلق اللحية تطرف وانحراف','10','0','0');
+INSERT INTO [t98135] VALUES (6,'إعفاء اللحية فطرة إنسانية','11','0','0');
+INSERT INTO [t98135] VALUES (7,'حلق اللحية تغيير لخلق الله سبحانه','13','0','0');
+INSERT INTO [t98135] VALUES (8,'إعفاء اللحية من سمت الأنبياء','14','0','0');
+INSERT INTO [t98135] VALUES (9,'إعفاء اللحية سبيل المؤمنين','15','0','0');
+INSERT INTO [t98135] VALUES (10,'حلق اللحية تشبه بالكافرين','17','0','0');
+INSERT INTO [t98135] VALUES (11,'إعفاء اللحية رجولة وفحولة','22','0','0');
+INSERT INTO [t98135] VALUES (12,'حلق اللحية تشبه بالنساء','23','0','0');
+INSERT INTO [t98135] VALUES (13,'إعفاء اللحية زينة وتكريم','25','0','0');
+INSERT INTO [t98135] VALUES (14,'حلق اللحية مهانة','27','0','0');
+INSERT INTO [t98135] VALUES (15,'حلق اللحية مثلة','28','0','0');
+INSERT INTO [t98135] VALUES (16,'الخاتمة','30','0','0');
