d0e4d0dfab1f9863dd0665ecc31f14b86f203b99
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:38 +0700

    Upload SQL chunk: t6434_chunk_0000.sql

---
+++ sql_chunks/t6434_chunk_0000.sql
@@ -0,0 +1,65 @@
+-- Table: t6434
+-- Chunk: 1
+-- Rows: 58
+
+CREATE TABLE "t6434" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6434
+INSERT INTO [t6434] VALUES (1,'كتاب النعوت الأسماء والصفات','1','0','0');
+INSERT INTO [t6434] VALUES (2,'ذكر أسماء الله تبارك وتعالى','1','0','0');
+INSERT INTO [t6434] VALUES (3,'بسم الله وبالله','2','0','0');
+INSERT INTO [t6434] VALUES (4,'الله الواحد الأحد الصمد لم يلد ولم يولد ولم يكن له كفوا أحد','7','0','0');
+INSERT INTO [t6434] VALUES (5,'قوله جل ثناؤه الأول والآخر والظاهر والباطن','10','0','0');
+INSERT INTO [t6434] VALUES (6,'الرحيم','12','0','0');
+INSERT INTO [t6434] VALUES (7,'الحميد المجيد','13','0','0');
+INSERT INTO [t6434] VALUES (8,'الحليم الكريم','15','0','0');
+INSERT INTO [t6434] VALUES (9,'العظيم الحليم','16','0','0');
+INSERT INTO [t6434] VALUES (10,'الأعلى','18','0','0');
+INSERT INTO [t6434] VALUES (11,'العلي العظيم','19','0','0');
+INSERT INTO [t6434] VALUES (12,'السميع القريب','21','0','0');
+INSERT INTO [t6434] VALUES (13,'السميع العليم','23','0','0');
+INSERT INTO [t6434] VALUES (14,'الحي القيوم','25','0','0');
+INSERT INTO [t6434] VALUES (15,'الحي','27','0','0');
+INSERT INTO [t6434] VALUES (16,'اللطيف الخبير','28','0','0');
+INSERT INTO [t6434] VALUES (17,'الواحد القهار','30','0','0');
+INSERT INTO [t6434] VALUES (18,'العزيز الغفار','31','0','0');
+INSERT INTO [t6434] VALUES (19,'الجبار','32','0','0');
+INSERT INTO [t6434] VALUES (20,'الرب','34','0','0');
+INSERT INTO [t6434] VALUES (21,'الملك','35','0','0');
+INSERT INTO [t6434] VALUES (22,'المليك','36','0','0');
+INSERT INTO [t6434] VALUES (23,'العزيز','38','0','0');
+INSERT INTO [t6434] VALUES (24,'المتكبر','39','0','0');
+INSERT INTO [t6434] VALUES (25,'الخالق','40','0','0');
+INSERT INTO [t6434] VALUES (26,'فاطر السماوات والأرض','42','0','0');
+INSERT INTO [t6434] VALUES (27,'السلام','43','0','0');
+INSERT INTO [t6434] VALUES (28,'المنان','44','0','0');
+INSERT INTO [t6434] VALUES (29,'الرفيق','45','0','0');
+INSERT INTO [t6434] VALUES (30,'الحق','46','0','0');
+INSERT INTO [t6434] VALUES (31,'النور','47','0','0');
+INSERT INTO [t6434] VALUES (32,'السميع','49','0','0');
+INSERT INTO [t6434] VALUES (33,'قول الله عز وجل هو الرزاق','50','0','0');
+INSERT INTO [t6434] VALUES (34,'الرحمن','52','0','0');
+INSERT INTO [t6434] VALUES (35,'الغفور الرحيم','53','0','0');
+INSERT INTO [t6434] VALUES (36,'أرحم الراحمين','54','0','0');
+INSERT INTO [t6434] VALUES (37,'العفو','55','0','0');
+INSERT INTO [t6434] VALUES (38,'قوله عز وجل ونقلب أفئدتهم وأبصارهم مقلب القلوب','56','0','0');
+INSERT INTO [t6434] VALUES (39,'فالق الحب والنوى','57','0','0');
+INSERT INTO [t6434] VALUES (40,'عالم الغيب والشهادة','58','0','0');
+INSERT INTO [t6434] VALUES (41,'ذو الجلال والإكرام','59','0','0');
+INSERT INTO [t6434] VALUES (42,'ذو العزة','61','0','0');
+INSERT INTO [t6434] VALUES (43,'السؤال باسماء الله عز وجل وصفاته والاستعاذة بها','63','0','0');
+INSERT INTO [t6434] VALUES (44,'سبوح قدوس','66','0','0');
+INSERT INTO [t6434] VALUES (45,'العزة والقدرة','67','0','0');
+INSERT INTO [t6434] VALUES (46,'العزيز الكريم','68','0','0');
+INSERT INTO [t6434] VALUES (47,'كلمات الله سبحانه وتعالى','69','0','0');
+INSERT INTO [t6434] VALUES (48,'قوله جل جلاله عالم الغيب فلا يظهر على غيبه أحدا','71','0','0');
+INSERT INTO [t6434] VALUES (49,'علام الغيوب','72','0','0');
+INSERT INTO [t6434] VALUES (50,'قوله تعالى تعلم ما في نفسي ولا أعلم ما في نفسك','73','0','0');
+INSERT INTO [t6434] VALUES (51,'قوله سبحانه كل شيء هالك إلا وجهه','74','0','0');
+INSERT INTO [t6434] VALUES (52,'قوله ولتصنع على عيني','76','0','0');
+INSERT INTO [t6434] VALUES (53,'رحمتي وعذابي','83','0','0');
+INSERT INTO [t6434] VALUES (54,'الحب والكراهية','87','0','0');
+INSERT INTO [t6434] VALUES (55,'الحب والبغض','90','0','0');
+INSERT INTO [t6434] VALUES (56,'الرضا والسخط','91','0','0');
+INSERT INTO [t6434] VALUES (57,'الرحمة والغضب','93','0','0');
+INSERT INTO [t6434] VALUES (58,'المعافاة والعقوبة','95','0','0');
