51fa513893426ab1e307f04fbfff5328cc3f8e77
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:28 +0700

    Upload SQL chunk: t6137_chunk_0000.sql

---
+++ sql_chunks/t6137_chunk_0000.sql
@@ -0,0 +1,62 @@
+-- Table: t6137
+-- Chunk: 1
+-- Rows: 55
+
+CREATE TABLE "t6137" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6137
+INSERT INTO [t6137] VALUES (1,'الجزء ١','1','0','0');
+INSERT INTO [t6137] VALUES (2,'القسم الاول المفتي المستقل وشرطه أن يكون مع ما ذكرناه قيما بمعرفة أدلة الأحكام الشرعية من الكتاب والسنة والإجماع والقياس وما التحق بها على التفصيل وقد فصلت في كتب الفقه وغيرها فتيسرت والحمد لله عالما بما يشترط','17','1','0');
+INSERT INTO [t6137] VALUES (3,'القسم الثاني','23','1','0');
+INSERT INTO [t6137] VALUES (4,'فرعان','56','1','0');
+INSERT INTO [t6137] VALUES (5,'القسم الأول','122','1','0');
+INSERT INTO [t6137] VALUES (6,'القسم الثالث','185','1','0');
+INSERT INTO [t6137] VALUES (7,'القسم الرابع','201','1','0');
+INSERT INTO [t6137] VALUES (8,'الجزء ٢','383','0','0');
+INSERT INTO [t6137] VALUES (9,'ومن كتاب الأطعمة','450','8','0');
+INSERT INTO [t6137] VALUES (10,'ومن كتاب السبق والرمي','453','8','0');
+INSERT INTO [t6137] VALUES (11,'ومن كتاب الايمان','456','8','0');
+INSERT INTO [t6137] VALUES (12,'ومن كتاب الشهادات','476','8','0');
+INSERT INTO [t6137] VALUES (13,'ومن كتاب الدعاوى والبينات','498','8','0');
+INSERT INTO [t6137] VALUES (14,'القسم الرابع','525','8','0');
+INSERT INTO [t6137] VALUES (15,'مسائل ابن الصلاح','526','8','0');
+INSERT INTO [t6137] VALUES (16,'باب زكاة البقر','526','8','0');
+INSERT INTO [t6137] VALUES (17,'باب تعجيل الزكاة','527','8','0');
+INSERT INTO [t6137] VALUES (18,'باب الخلطة','529','8','0');
+INSERT INTO [t6137] VALUES (19,'باب النية','529','8','0');
+INSERT INTO [t6137] VALUES (20,'باب زكاة الثمار','530','8','0');
+INSERT INTO [t6137] VALUES (21,'باب زكاة الزرع','531','8','0');
+INSERT INTO [t6137] VALUES (22,'باب الربا','549','8','0');
+INSERT INTO [t6137] VALUES (23,'باب السلم','559','8','0');
+INSERT INTO [t6137] VALUES (24,'مسائل باب الرهن','561','8','0');
+INSERT INTO [t6137] VALUES (25,'باب التفليس','566','8','0');
+INSERT INTO [t6137] VALUES (26,'باب الحجر','567','8','0');
+INSERT INTO [t6137] VALUES (27,'باب الصلح','567','8','0');
+INSERT INTO [t6137] VALUES (28,'باب الحوالة','571','8','0');
+INSERT INTO [t6137] VALUES (29,'باب الضمان','572','8','0');
+INSERT INTO [t6137] VALUES (30,'باب الشركة','574','8','0');
+INSERT INTO [t6137] VALUES (31,'باب الوكالة','574','8','0');
+INSERT INTO [t6137] VALUES (32,'باب الإقرار','579','8','0');
+INSERT INTO [t6137] VALUES (33,'باب الغصب','585','8','0');
+INSERT INTO [t6137] VALUES (34,'باب المساقاة','599','8','0');
+INSERT INTO [t6137] VALUES (35,'باب إحياء الموات','609','8','0');
+INSERT INTO [t6137] VALUES (36,'باب الوقف','610','8','0');
+INSERT INTO [t6137] VALUES (37,'باب اللقطة والتقاط المنبوذ','613','8','0');
+INSERT INTO [t6137] VALUES (38,'في العطايا والهبات','614','8','0');
+INSERT INTO [t6137] VALUES (39,'باب المواريث','615','8','0');
+INSERT INTO [t6137] VALUES (40,'مسائل الوصايا','615','8','0');
+INSERT INTO [t6137] VALUES (41,'باب الصداق','642','8','0');
+INSERT INTO [t6137] VALUES (42,'باب الخلع','648','8','0');
+INSERT INTO [t6137] VALUES (43,'باب النفقات','670','8','0');
+INSERT INTO [t6137] VALUES (44,'كتاب الجراح','671','8','0');
+INSERT INTO [t6137] VALUES (45,'صول الفحل','680','8','0');
+INSERT INTO [t6137] VALUES (46,'الجزية','683','8','0');
+INSERT INTO [t6137] VALUES (47,'الصيد','684','8','0');
+INSERT INTO [t6137] VALUES (48,'الضحايا','687','8','0');
+INSERT INTO [t6137] VALUES (49,'الايمان','688','8','0');
+INSERT INTO [t6137] VALUES (50,'النذور','690','8','0');
+INSERT INTO [t6137] VALUES (51,'الشهادات','694','8','0');
+INSERT INTO [t6137] VALUES (52,'الدعاوى','699','8','0');
+INSERT INTO [t6137] VALUES (53,'في النفقات والتدبير','718','8','0');
+INSERT INTO [t6137] VALUES (54,'في الولاء','725','8','0');
+INSERT INTO [t6137] VALUES (55,'الكتابة','726','8','0');
