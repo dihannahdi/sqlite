98eccc470a29b43350d4e172020347abeac0bde8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:47 +0700

    Upload SQL chunk: t10289_chunk_0000.sql

---
+++ sql_chunks/t10289_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t10289
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t10289" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10289
+INSERT INTO [t10289] VALUES (1,'لمحة لغوية','1','0','0');
+INSERT INTO [t10289] VALUES (2,'سر الإهتمام بالشباب','3','0','0');
+INSERT INTO [t10289] VALUES (3,'لماذا نعد الشباب؟','5','0','0');
+INSERT INTO [t10289] VALUES (4,'الشباب عنوان الأمة','7','0','0');
+INSERT INTO [t10289] VALUES (5,'مظاهر الاهتمام بالشباب','9','0','0');
+INSERT INTO [t10289] VALUES (6,'إشادة وتكريم','11','0','0');
+INSERT INTO [t10289] VALUES (7,'الشباب والتقليد','13','0','0');
+INSERT INTO [t10289] VALUES (8,'واجب الآباء','15','0','0');
+INSERT INTO [t10289] VALUES (9,'العلماء والشباب','18','0','0');
