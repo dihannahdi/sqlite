cf22be89931e46cfcc51ece27b278d7c2a0fc06e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:50 +0700

    Upload SQL chunk: t36040_chunk_0000.sql

---
+++ sql_chunks/t36040_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t36040
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t36040" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36040
+INSERT INTO [t36040] VALUES (1,'الأحاديث الوارد في تقلد القضاء','3','0','0');
+INSERT INTO [t36040] VALUES (2,'نصيحة الرسول - صلى الله عليه وسلم - لأبي ذر','7','0','0');
+INSERT INTO [t36040] VALUES (3,'شدة حساب القاضي العدل','8','0','0');
+INSERT INTO [t36040] VALUES (4,'حكم من قضى بجور أو جهل','9','0','0');
+INSERT INTO [t36040] VALUES (5,'أول الأمارة ملامة','11','0','0');
+INSERT INTO [t36040] VALUES (6,'هل القضاة أول من يدعى للحساب؟','15','0','0');
+INSERT INTO [t36040] VALUES (7,'رجل يحب أن يحشر مع العلماء','16','0','0');
+INSERT INTO [t36040] VALUES (8,'فرار السلف الصالح من القضاء','17','0','0');
+INSERT INTO [t36040] VALUES (9,'أحوال القضاة في بني إسرائيل','20','0','0');
