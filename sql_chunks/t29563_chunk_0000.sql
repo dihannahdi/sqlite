218f611a99c5f5ac45eeeb88de98676a37c4f38f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:24 +0700

    Upload SQL chunk: t29563_chunk_0000.sql

---
+++ sql_chunks/t29563_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t29563
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t29563" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29563
+INSERT INTO [t29563] VALUES (1,'المجلس الأول','1','0','0');
+INSERT INTO [t29563] VALUES (2,'حديث أبي رافع رضي الله عنه','5','0','0');
+INSERT INTO [t29563] VALUES (3,'المجلس الثاني','7','0','0');
+INSERT INTO [t29563] VALUES (4,'حديث أنس رضي الله عنه','8','0','0');
+INSERT INTO [t29563] VALUES (5,'حديث ابن عباس','10','0','0');
+INSERT INTO [t29563] VALUES (6,'الطريق الأول','11','0','0');
+INSERT INTO [t29563] VALUES (7,'المجلس الثالث','13','0','0');
+INSERT INTO [t29563] VALUES (8,'الطريق الثاني','14','0','0');
+INSERT INTO [t29563] VALUES (9,'الطريق الثالث','15','0','0');
+INSERT INTO [t29563] VALUES (10,'الطريق الرابع','16','0','0');
+INSERT INTO [t29563] VALUES (11,'المجلس الرابع','18','0','0');
+INSERT INTO [t29563] VALUES (12,'الطريق الخامس','19','0','0');
+INSERT INTO [t29563] VALUES (13,'الطريق السادس','21','0','0');
+INSERT INTO [t29563] VALUES (14,'الطريق السابع','22','0','0');
+INSERT INTO [t29563] VALUES (15,'المجلس الخامس','25','0','0');
+INSERT INTO [t29563] VALUES (16,'حديث عبد الله بن عمرو','26','0','0');
+INSERT INTO [t29563] VALUES (17,'الطريق الأول','27','0','0');
+INSERT INTO [t29563] VALUES (18,'الطريق الثاني','29','0','0');
+INSERT INTO [t29563] VALUES (19,'الطريق الثاني','31','0','0');
+INSERT INTO [t29563] VALUES (20,'حديث علي ابن أبي طالب','32','0','0');
+INSERT INTO [t29563] VALUES (21,'الطريق الأول','33','0','0');
+INSERT INTO [t29563] VALUES (22,'الطريق الثاني','34','0','0');
+INSERT INTO [t29563] VALUES (23,'المجلس السابع','35','0','0');
+INSERT INTO [t29563] VALUES (24,'الطريق الثالث','36','0','0');
+INSERT INTO [t29563] VALUES (25,'حديث جعفر بن أبي طالب','37','0','0');
+INSERT INTO [t29563] VALUES (26,'حديث عبد الله بن جعفر','38','0','0');
+INSERT INTO [t29563] VALUES (27,'حديث أم سلمة','39','0','0');
+INSERT INTO [t29563] VALUES (28,'المجلس الثامن','41','0','0');
+INSERT INTO [t29563] VALUES (29,'حديث الأنصاري','42','0','0');
+INSERT INTO [t29563] VALUES (30,'المجلس التاسع','43','0','0');
