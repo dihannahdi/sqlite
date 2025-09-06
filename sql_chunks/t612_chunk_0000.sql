af01b8ed58af19c6d8738897eee449b28a01f60b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:23 +0700

    Upload SQL chunk: t612_chunk_0000.sql

---
+++ sql_chunks/t612_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t612
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t612" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t612
+INSERT INTO [t612] VALUES (1,'فصل غاياته ألف','1','0','0');
+INSERT INTO [t612] VALUES (2,'فصل غاياته باء','12','0','0');
+INSERT INTO [t612] VALUES (3,'فصل غاياته تاء','26','0','0');
+INSERT INTO [t612] VALUES (4,'فصل غاياته ثاء','50','0','0');
+INSERT INTO [t612] VALUES (5,'فصل غاياته جيم','75','0','0');
+INSERT INTO [t612] VALUES (6,'فصل غاياته حاء','99','0','0');
+INSERT INTO [t612] VALUES (7,'فصل غاياته خاء','124','0','0');
