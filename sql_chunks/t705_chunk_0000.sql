f8efcc92b5afd440df07958d077e174c3b66dbdb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:17 +0700

    Upload SQL chunk: t705_chunk_0000.sql

---
+++ sql_chunks/t705_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t705
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t705" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t705
+INSERT INTO [t705] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t705] VALUES (2,'منزلة السنة في الإسلام وبيان أنه لا يستغنى عنها بالقرآن','4','0','0');
+INSERT INTO [t705] VALUES (3,'وظيفة السنة في القرآن','6','0','0');
+INSERT INTO [t705] VALUES (4,'ضرورة السنة لفهم القرآن وأمثلة على ذلك','7','0','0');
+INSERT INTO [t705] VALUES (5,'ضلال المستغنين بالقرآن عن السنة','12','0','0');
+INSERT INTO [t705] VALUES (6,'عدم كفاية اللغة لفهم القرآن','15','0','0');
+INSERT INTO [t705] VALUES (7,'ضعف حديث معاذ في الرأي وما يستنكر منه','21','0','0');
