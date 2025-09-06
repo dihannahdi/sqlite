d14afe9ffd47ffc7395b2468f1d84367bcd6bd5b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:15 +0700

    Upload SQL chunk: t10193_chunk_0000.sql

---
+++ sql_chunks/t10193_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10193
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10193" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10193
+INSERT INTO [t10193] VALUES (1,'خطاب الماردي','1','0','0');
+INSERT INTO [t10193] VALUES (2,'منهجه في النحو','15','0','0');
+INSERT INTO [t10193] VALUES (3,'مصادر ومراجع','36','0','0');
