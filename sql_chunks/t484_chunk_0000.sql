cdf5803e4a55b035c69ab1e7abbf57fd33fc2c34
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:08 +0700

    Upload SQL chunk: t484_chunk_0000.sql

---
+++ sql_chunks/t484_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t484
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t484" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t484
+INSERT INTO [t484] VALUES (1,'الأحاديث الواردة في هذا الباب','2','0','0');
+INSERT INTO [t484] VALUES (2,'المباحث المتعلقة بها','5','0','0');
+INSERT INTO [t484] VALUES (3,'صغر سن المسور بن مخرمة (راوي القصة)، ومن كان في هذه السن فالغالب أنه لا يضبط','6','0','0');
+INSERT INTO [t484] VALUES (4,'تحقيق الكلام في سن المسور','6','0','0');
