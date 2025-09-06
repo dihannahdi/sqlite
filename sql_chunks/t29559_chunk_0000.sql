1498453371a3b7dd1cf41b5eac38b6ab5ed78ce5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:20 +0700

    Upload SQL chunk: t29559_chunk_0000.sql

---
+++ sql_chunks/t29559_chunk_0000.sql
@@ -0,0 +1,42 @@
+-- Table: t29559
+-- Chunk: 1
+-- Rows: 35
+
+CREATE TABLE "t29559" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29559
+INSERT INTO [t29559] VALUES (1,'المقدمة','3','0','0');
+INSERT INTO [t29559] VALUES (2,'ترجمة المؤلف','3','1','0');
+INSERT INTO [t29559] VALUES (3,'إجازاته العلمية','3','2','0');
+INSERT INTO [t29559] VALUES (4,'تلاميذه','4','2','0');
+INSERT INTO [t29559] VALUES (5,'من أبرزهم','4','4','0');
+INSERT INTO [t29559] VALUES (6,'مؤلفاته','4','2','0');
+INSERT INTO [t29559] VALUES (7,'الشيخ فيصل وجهوده الفقهيه','5','2','0');
+INSERT INTO [t29559] VALUES (8,'وفاته','6','2','0');
+INSERT INTO [t29559] VALUES (9,'١ - التعريف بالرسالة الأولى','7','1','0');
+INSERT INTO [t29559] VALUES (10,'٢ - التعريف بالرسالة الثانية','8','1','0');
+INSERT INTO [t29559] VALUES (11,'باب أسباب الميراث','17','0','0');
+INSERT INTO [t29559] VALUES (12,'باب موانع الإرث','17','0','0');
+INSERT INTO [t29559] VALUES (13,'باب الوارثين من الرجال','18','0','0');
+INSERT INTO [t29559] VALUES (14,'باب الوارثات من النساء','20','0','0');
+INSERT INTO [t29559] VALUES (15,'باب الفروض المقدرة في كتاب الله تعالى','21','0','0');
+INSERT INTO [t29559] VALUES (16,'باب من يرث النصف','22','0','0');
+INSERT INTO [t29559] VALUES (17,'باب من يرث الربع','24','0','0');
+INSERT INTO [t29559] VALUES (18,'باب من يرث الثمن','24','0','0');
+INSERT INTO [t29559] VALUES (19,'باب من يرث الثلثين','25','0','0');
+INSERT INTO [t29559] VALUES (20,'باب من يرث الثلث','26','0','0');
+INSERT INTO [t29559] VALUES (21,'باب من يرث السدس','27','0','0');
+INSERT INTO [t29559] VALUES (22,'باب التعصيب','31','0','0');
+INSERT INTO [t29559] VALUES (23,'باب الحجب','33','0','0');
+INSERT INTO [t29559] VALUES (24,'باب المشركة','35','0','0');
+INSERT INTO [t29559] VALUES (25,'باب الجد والإخوة','36','0','0');
+INSERT INTO [t29559] VALUES (26,'باب الأكدرية','37','0','0');
+INSERT INTO [t29559] VALUES (27,'باب الحساب','38','0','0');
+INSERT INTO [t29559] VALUES (28,'باب المناسخات','46','0','0');
+INSERT INTO [t29559] VALUES (29,'فصل','49','28','0');
+INSERT INTO [t29559] VALUES (30,'باب ميراث الخنثى المشكل والمفقود والحمل','50','0','0');
+INSERT INTO [t29559] VALUES (31,'فصل في المفقود','51','30','0');
+INSERT INTO [t29559] VALUES (32,'فصل في الحمل','51','30','0');
+INSERT INTO [t29559] VALUES (33,'باب ميراث الغرقى والهدمى ونحوهم','52','0','0');
+INSERT INTO [t29559] VALUES (34,'تتمة في الرد وذوي الأرحام','53','0','0');
+INSERT INTO [t29559] VALUES (35,'فصل','53','34','0');
