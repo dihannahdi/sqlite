69745b4774fdfa2195922e5afb2cb9e4eb606fd5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:38 +0700

    Upload SQL chunk: t10066_chunk_0000.sql

---
+++ sql_chunks/t10066_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t10066
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t10066" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10066
+INSERT INTO [t10066] VALUES (1,'تصديق القرآن الكريم للكتب السماوية','1','0','0');
+INSERT INTO [t10066] VALUES (2,'هيمنة القرآن على الكتب السماوية','6','0','0');
+INSERT INTO [t10066] VALUES (3,'علاقة الهيمنة بالتصديق','7','0','0');
+INSERT INTO [t10066] VALUES (4,'مظاهر هيمنة القرآن على الكتب السماوية','9','0','0');
+INSERT INTO [t10066] VALUES (5,'خاتمة','13','0','0');
