fe5d3fd346de03f7afe8b3eb97a21cf22193052f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:33 +0700

    Upload SQL chunk: t7229_chunk_0000.sql

---
+++ sql_chunks/t7229_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t7229
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t7229" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7229
+INSERT INTO [t7229] VALUES (1,'أنواع السياسات','1','0','0');
+INSERT INTO [t7229] VALUES (2,'الطعام','3','0','0');
+INSERT INTO [t7229] VALUES (3,'الشرب','4','0','0');
+INSERT INTO [t7229] VALUES (4,'السهر','5','0','0');
+INSERT INTO [t7229] VALUES (5,'الحمام','6','0','0');
+INSERT INTO [t7229] VALUES (6,'الرياضة','6','0','0');
+INSERT INTO [t7229] VALUES (7,'تقوى الله','7','0','0');
+INSERT INTO [t7229] VALUES (8,'العدل والإحسان','7','0','0');
+INSERT INTO [t7229] VALUES (9,'اللذة للحاكم جميل الذكر','7','0','0');
+INSERT INTO [t7229] VALUES (10,'أداء الأعمال في وقتها','8','0','0');
+INSERT INTO [t7229] VALUES (11,'طاعة محبة لا طاعة رهبة','8','0','0');
+INSERT INTO [t7229] VALUES (12,'إنجاز الوعد والوعيد','8','0','0');
+INSERT INTO [t7229] VALUES (13,'إحراز فضائل النفس','9','0','0');
+INSERT INTO [t7229] VALUES (14,'الوقوف على أخبار الماضيين','10','0','0');
+INSERT INTO [t7229] VALUES (15,'إصلاح أخلاق الخاصة','11','0','0');
+INSERT INTO [t7229] VALUES (16,'تفقد أحوال الخاصة','11','0','0');
+INSERT INTO [t7229] VALUES (17,'خصال الحاكم مع الخاصة','12','0','0');
+INSERT INTO [t7229] VALUES (18,'مظالم الناس','15','0','0');
+INSERT INTO [t7229] VALUES (19,'حزم الملك بحسن سياسة الرعية','16','0','0');
+INSERT INTO [t7229] VALUES (20,'التعرف على طبقات العامة وعراتهم','16','0','0');
+INSERT INTO [t7229] VALUES (21,'إكرام الأخبار','18','0','0');
+INSERT INTO [t7229] VALUES (22,'الرشا أس الجور والفساد','19','0','0');
+INSERT INTO [t7229] VALUES (23,'المخابرات','20','0','0');
+INSERT INTO [t7229] VALUES (24,'ختام','21','0','0');
