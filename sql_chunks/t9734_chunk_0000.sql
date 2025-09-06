bc72c05645ef596d62b96648e2c703d43a089346
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:00:46 +0700

    Upload SQL chunk: t9734_chunk_0000.sql

---
+++ sql_chunks/t9734_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t9734
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t9734" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9734
+INSERT INTO [t9734] VALUES (1,'حي بن يقظان','1','0','0');
+INSERT INTO [t9734] VALUES (2,'ذكر سلفنا الصالح - رضي الله عنهم -','1','0','0');
+INSERT INTO [t9734] VALUES (3,'والسلام عليك أيها الأخ المفترض إسعافه ورحمة الله وبركاته','30','0','0');
