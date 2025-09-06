d84a03031e1cc2e0642866e23475e8230b80a871
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:50 +0700

    Upload SQL chunk: t36530_chunk_0000.sql

---
+++ sql_chunks/t36530_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t36530
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t36530" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36530
+INSERT INTO [t36530] VALUES (1,'الحمد لله تعالى بيده مفاتيح الأرزاق','1','0','0');
+INSERT INTO [t36530] VALUES (2,'أخي المسلم','1','1','0');
+INSERT INTO [t36530] VALUES (3,'أخي المسلم','1','1','0');
+INSERT INTO [t36530] VALUES (4,'أخي المسلم','2','1','0');
+INSERT INTO [t36530] VALUES (5,'أخي المسلم','3','1','0');
+INSERT INTO [t36530] VALUES (6,'أخي المسلم','8','1','0');
+INSERT INTO [t36530] VALUES (7,'أخي المسلم','9','1','0');
+INSERT INTO [t36530] VALUES (8,'أخي المسلم','10','1','0');
+INSERT INTO [t36530] VALUES (9,'فبادر ..','10','0','0');
