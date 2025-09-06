2453485eea62fd16082c231f3d545bb2ac4a9f58
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:46 +0700

    Upload SQL chunk: t4023_chunk_0000.sql

---
+++ sql_chunks/t4023_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t4023
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t4023" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4023
+INSERT INTO [t4023] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t4023] VALUES (2,'المسألة الأولى','2','0','0');
+INSERT INTO [t4023] VALUES (3,'المسألة الثانية','11','0','0');
+INSERT INTO [t4023] VALUES (4,'المسألة الثالثة','14','0','0');
+INSERT INTO [t4023] VALUES (5,'المسألة الرابعة','24','0','0');
+INSERT INTO [t4023] VALUES (6,'المسألة الخامسة','28','0','0');
+INSERT INTO [t4023] VALUES (7,'الخلاصة','40','0','0');
