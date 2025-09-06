50f70b94eb797f156eb659598d418b7bb4e7e657
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:18 +0700

    Upload SQL chunk: t3519_chunk_0000.sql

---
+++ sql_chunks/t3519_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t3519
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t3519" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3519
+INSERT INTO [t3519] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t3519] VALUES (2,'الفصل الرابع: الشيعة الإمامية الإثنا عشرية وأهم تعاليمهم','7','0','0');
+INSERT INTO [t3519] VALUES (3,'الفصل الخامس: الشيعة الزيدية','24','0','0');
+INSERT INTO [t3519] VALUES (4,'الفصل السادس: الإسماعيلية الباطنية','25','0','0');
+INSERT INTO [t3519] VALUES (5,'الفصل السابع: النصيرية العلويون','26','0','0');
+INSERT INTO [t3519] VALUES (6,'الفصل الثامن: الدروز','34','0','0');
+INSERT INTO [t3519] VALUES (7,'إقتراح','36','0','0');
