bcfc1dbe958de0473e736af60b440c9d5c0690b7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:34 +0700

    Upload SQL chunk: t9677_chunk_0000.sql

---
+++ sql_chunks/t9677_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9677
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9677" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9677
+INSERT INTO [t9677] VALUES (1,'قرار هيئة كبار العلماء عن حادث المسجد الحرام','1','0','0');
+INSERT INTO [t9677] VALUES (2,'استشهاد سماحة الشيخ الإمام عبد العزيز بن باز رحمه الله على حرمة هذا الأمر','4','0','0');
