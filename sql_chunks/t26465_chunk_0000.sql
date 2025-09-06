69088f2993292cb0550550d7f006bac15115ed15
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:40 +0700

    Upload SQL chunk: t26465_chunk_0000.sql

---
+++ sql_chunks/t26465_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26465
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26465" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26465
+INSERT INTO [t26465] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26465] VALUES (2,'المجلس الثاني والثلاثون في التوبة','2','0','0');
