aef38bb0ba7aa8b5c50cc64a1381b3fa97cba3df
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:59 +0700

    Upload SQL chunk: t478_chunk_0000.sql

---
+++ sql_chunks/t478_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t478
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t478" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t478
+INSERT INTO [t478] VALUES (1,'الجزء الأول','1','0','0');
+INSERT INTO [t478] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
+INSERT INTO [t478] VALUES (3,'وصلاة على سيدنا وعلى آله وصحبه','1','1','0');
+INSERT INTO [t478] VALUES (4,'الجزء الثاني','82','0','0');
+INSERT INTO [t478] VALUES (5,'بسم الله الرحمن الرحيم','82','4','0');
+INSERT INTO [t478] VALUES (6,'وصلاة على سيدنا محمد وعلى آله وصحبه','82','4','0');
+INSERT INTO [t478] VALUES (7,'بسم الله الرحمن الرحيم','143','4','0');
+INSERT INTO [t478] VALUES (8,'وصلى الله على سيدنا محمد وآله وصحبه وسلم تسليما','143','4','0');
