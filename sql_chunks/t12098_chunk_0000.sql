7189168c6822d78572dec1a0cf73b92203ed01d3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:52 +0700

    Upload SQL chunk: t12098_chunk_0000.sql

---
+++ sql_chunks/t12098_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t12098
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t12098" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12098
+INSERT INTO [t12098] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12098] VALUES (2,'المبحث الأول: حركة التأليف في غريب الحديث','6','0','0');
+INSERT INTO [t12098] VALUES (3,'المبحث الثاني: التعريف بمجد الدين بن الأثير','17','0','0');
+INSERT INTO [t12098] VALUES (4,'المبحث الثالث: منهج ابن الأثير في النهاية','24','0','0');
+INSERT INTO [t12098] VALUES (5,'المبحث الرابع: أهمية النهاية','66','0','0');
+INSERT INTO [t12098] VALUES (6,'المبحث الخامس: مآخذ على ابن الأثير','70','0','0');
+INSERT INTO [t12098] VALUES (7,'الخاتمة','81','0','0');
+INSERT INTO [t12098] VALUES (8,'مصادر ومراجع','83','0','0');
