0db8633b696e77ae69ec0de4b642bae00114b5fa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:14:20 +0700

    Upload SQL chunk: t1342_chunk_0000.sql

---
+++ sql_chunks/t1342_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t1342
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t1342" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1342
+INSERT INTO [t1342] VALUES (1,'أحاديث منتخبة من مغازي موسى بن عقبة','3','0','0');
