366f88c806612c75857ab40d63263bef87aea9af
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:35 +0700

    Upload SQL chunk: t7456_chunk_0000.sql

---
+++ sql_chunks/t7456_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t7456
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t7456" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7456
+INSERT INTO [t7456] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t7456] VALUES (2,'فصل','3','0','0');
+INSERT INTO [t7456] VALUES (3,'فصل','3','0','0');
+INSERT INTO [t7456] VALUES (4,'فصل','4','0','0');
+INSERT INTO [t7456] VALUES (5,'فصل','5','0','0');
+INSERT INTO [t7456] VALUES (6,'فصل','7','0','0');
