9f2a4eb44931954320380cc88f6ce248e8d0673e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:43 +0700

    Upload SQL chunk: t666_chunk_0000.sql

---
+++ sql_chunks/t666_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t666
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t666" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t666
+INSERT INTO [t666] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t666] VALUES (2,'شرح المسألة','2','0','0');
+INSERT INTO [t666] VALUES (3,'مسألة في القرآن','27','0','0');
+INSERT INTO [t666] VALUES (4,'مسألة من الصلات','27','0','0');
+INSERT INTO [t666] VALUES (5,'مسألة','34','0','0');
+INSERT INTO [t666] VALUES (6,'مسألة في الأسماء لم لم تجزم والأفعال لم لم تخفض','35','0','0');
+INSERT INTO [t666] VALUES (7,'مسألة','37','0','0');
+INSERT INTO [t666] VALUES (8,'مسالة','62','0','0');
+INSERT INTO [t666] VALUES (9,'مسألة','63','0','0');
