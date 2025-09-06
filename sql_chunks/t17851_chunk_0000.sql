e27abc1f37ab1d1fac120c0b6f69cc9c00565b40
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:06 +0700

    Upload SQL chunk: t17851_chunk_0000.sql

---
+++ sql_chunks/t17851_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t17851
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t17851" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17851
+INSERT INTO [t17851] VALUES (1,'[مقدمة التحقيق]','4','0','0');
+INSERT INTO [t17851] VALUES (2,'ترجمة المؤلف','6','1','0');
+INSERT INTO [t17851] VALUES (3,'اسمه وولادته','6','2','0');
+INSERT INTO [t17851] VALUES (4,'طلبه للعلم','6','2','0');
+INSERT INTO [t17851] VALUES (5,'مشايخه','6','2','0');
+INSERT INTO [t17851] VALUES (6,'مؤلفاته','7','2','0');
+INSERT INTO [t17851] VALUES (7,'وفاته','7','2','0');
+INSERT INTO [t17851] VALUES (8,'وصف المخطوطة','8','1','0');
+INSERT INTO [t17851] VALUES (9,'مقدمة المؤلف','12','0','0');
+INSERT INTO [t17851] VALUES (10,'سبب تأليفها سؤال','12','0','0');
+INSERT INTO [t17851] VALUES (11,'الجواب عن السؤال','14','0','0');
+INSERT INTO [t17851] VALUES (12,'حث الشارع على صلاة الجماعة','15','0','0');
+INSERT INTO [t17851] VALUES (13,'صلاة المأموم منوطة بالإمام','15','0','0');
+INSERT INTO [t17851] VALUES (14,'موقع المأموم من الإمام','16','0','0');
+INSERT INTO [t17851] VALUES (15,'موقع المأموم بحسب جهات الكعبة','16','0','0');
+INSERT INTO [t17851] VALUES (16,'معنى الصف التربيعي','17','0','0');
+INSERT INTO [t17851] VALUES (17,'لو كان المقتدي مسامتا للركن','17','0','0');
+INSERT INTO [t17851] VALUES (18,'خاتمة','23','0','0');
