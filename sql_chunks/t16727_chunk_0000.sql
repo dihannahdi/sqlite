e1f2bf8f66aeccec5f0a21d12d300a9849136770
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:26:51 +0700

    Upload SQL chunk: t16727_chunk_0000.sql

---
+++ sql_chunks/t16727_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t16727
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t16727" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t16727
+INSERT INTO [t16727] VALUES (1,'[مقدمة التحقيق]','3','0','0');
+INSERT INTO [t16727] VALUES (2,'المقدمة','3','1','0');
+INSERT INTO [t16727] VALUES (3,'رسالة "إخلاص الوداد في صدق الميعاد"','5','1','0');
+INSERT INTO [t16727] VALUES (4,'رسالة "ما يفعله الأطباء والداعون بدفع شر الطاعون"','7','1','0');
+INSERT INTO [t16727] VALUES (5,'منهج الفقيه مرعي الكرمي في كتابه "ما يفعله الأطباء والداعون"','10','1','0');
+INSERT INTO [t16727] VALUES (6,'ترجمة المؤلف','14','1','0');
+INSERT INTO [t16727] VALUES (7,'اسمه ونسبه','14','6','0');
+INSERT INTO [t16727] VALUES (8,'مولده ونشأته','14','6','0');
+INSERT INTO [t16727] VALUES (9,'عقيدته ومذهبه','15','6','0');
+INSERT INTO [t16727] VALUES (10,'ثناء العلماء عليه وشيوخه','15','6','0');
+INSERT INTO [t16727] VALUES (11,'آثاره العلمية','16','6','0');
+INSERT INTO [t16727] VALUES (12,'وفاته','17','6','0');
+INSERT INTO [t16727] VALUES (13,'وصف النسخ المعتمدة في إخراج الرسالتين','18','1','0');
+INSERT INTO [t16727] VALUES (14,'قال العبد الفقير إلى الله تعالى مرعي بن يوسف الحنبلي المقدسي','24','0','0');
