d4f30f2356a18a5948623482436360f8e682508d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:14 +0700

    Upload SQL chunk: t5353_chunk_0000.sql

---
+++ sql_chunks/t5353_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t5353
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t5353" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5353
+INSERT INTO [t5353] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5353] VALUES (2,'اللامات','1','0','0');
+INSERT INTO [t5353] VALUES (3,'الألفات','1','0','0');
+INSERT INTO [t5353] VALUES (4,'الهاءات','1','0','0');
+INSERT INTO [t5353] VALUES (5,'الياءات','2','0','0');
+INSERT INTO [t5353] VALUES (6,'النونات','2','0','0');
+INSERT INTO [t5353] VALUES (7,'التاءات','3','0','0');
+INSERT INTO [t5353] VALUES (8,'وجوه ما','3','0','0');
+INSERT INTO [t5353] VALUES (9,'والخمسة الأخر','4','0','0');
+INSERT INTO [t5353] VALUES (10,'من','4','0','0');
+INSERT INTO [t5353] VALUES (11,'أي','5','0','0');
+INSERT INTO [t5353] VALUES (12,'أن المخففة','6','0','0');
+INSERT INTO [t5353] VALUES (13,'إن','6','0','0');
+INSERT INTO [t5353] VALUES (14,'حتى','6','0','0');
+INSERT INTO [t5353] VALUES (15,'من','6','0','0');
+INSERT INTO [t5353] VALUES (16,'لام الإضافة','7','0','0');
+INSERT INTO [t5353] VALUES (17,'رويد','7','0','0');
+INSERT INTO [t5353] VALUES (18,'الخبر','7','0','0');
