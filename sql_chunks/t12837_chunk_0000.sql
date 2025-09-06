e5826d77875176487e7f9124e46bfb2aef5af352
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:48 +0700

    Upload SQL chunk: t12837_chunk_0000.sql

---
+++ sql_chunks/t12837_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t12837
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t12837" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12837
+INSERT INTO [t12837] VALUES (1,'تقديم','2','0','0');
+INSERT INTO [t12837] VALUES (2,'١ - مناظرات ابن تيمية للنصارى:','13','0','0');
+INSERT INTO [t12837] VALUES (3,'٢- مناظرات ابن تيمية لأهل الاتحاد ووحدة الوجود:','22','0','0');
+INSERT INTO [t12837] VALUES (4,'٣- مناظرات ابن تيمية للقبوريين:','40','0','0');
+INSERT INTO [t12837] VALUES (5,'٤- مناظرات ابن تيمية للأحمدية (الرفاعية البطحائية)','47','0','0');
+INSERT INTO [t12837] VALUES (6,'٥- مناظرات ابن تيمية للرافضة:','57','0','0');
+INSERT INTO [t12837] VALUES (7,'٦- مناظرات ابن تيمية لنفاة الصفات:','65','0','0');
+INSERT INTO [t12837] VALUES (8,'خاتمة','78','0','0');
+INSERT INTO [t12837] VALUES (9,'المراجع','82','0','0');
