c7adfa660c799651b09604b271a00067c356880e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:05 +0700

    Upload SQL chunk: t36551_chunk_0000.sql

---
+++ sql_chunks/t36551_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t36551
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t36551" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36551
+INSERT INTO [t36551] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36551] VALUES (2,'الزيارة','4','0','0');
+INSERT INTO [t36551] VALUES (3,'الرحلة','6','0','0');
+INSERT INTO [t36551] VALUES (4,'صفة الرسول - صلى الله عليه وسلم -','9','0','0');
+INSERT INTO [t36551] VALUES (5,'كلام الرسول - صلى الله عليه وسلم -','11','0','0');
+INSERT INTO [t36551] VALUES (6,'داخل البيت','13','0','0');
+INSERT INTO [t36551] VALUES (7,'الأقارب','15','0','0');
+INSERT INTO [t36551] VALUES (8,'الرسول - صلى الله عليه وسلم - في بيته','18','0','0');
+INSERT INTO [t36551] VALUES (9,'هديه وسمته - صلى الله عليه وسلم -','21','0','0');
+INSERT INTO [t36551] VALUES (10,'بناته - صلى الله عليه وسلم -','24','0','0');
+INSERT INTO [t36551] VALUES (11,'معاملة الزوجة','27','0','0');
+INSERT INTO [t36551] VALUES (12,'تعدد الزوجات','31','0','0');
+INSERT INTO [t36551] VALUES (13,'مزاح رسول الله - صلى الله عليه وسلم -','34','0','0');
+INSERT INTO [t36551] VALUES (14,'نوم النبي - صلى الله عليه وسلم -','37','0','0');
+INSERT INTO [t36551] VALUES (15,'قيام الليل','39','0','0');
+INSERT INTO [t36551] VALUES (16,'بعد الفجر','41','0','0');
+INSERT INTO [t36551] VALUES (17,'صلاة الضحى','42','0','0');
+INSERT INTO [t36551] VALUES (18,'صلاة النوافل في البيت','43','0','0');
+INSERT INTO [t36551] VALUES (19,'بكاء النبي - صلى الله عليه وسلم -','44','0','0');
+INSERT INTO [t36551] VALUES (20,'تواضعه - صلى الله عليه وسلم -','46','0','0');
+INSERT INTO [t36551] VALUES (21,'خادمه - صلى الله عليه وسلم -','49','0','0');
+INSERT INTO [t36551] VALUES (22,'الهدية والضيف','52','0','0');
+INSERT INTO [t36551] VALUES (23,'الرحمة بالأطفال','56','0','0');
+INSERT INTO [t36551] VALUES (24,'الحلم والرفق والصبر','60','0','0');
+INSERT INTO [t36551] VALUES (25,'طعامه - صلى الله عليه وسلم -','65','0','0');
+INSERT INTO [t36551] VALUES (26,'الذب عن أعراض الآخرين','69','0','0');
+INSERT INTO [t36551] VALUES (27,'كثرة ذكر الله تعالى','71','0','0');
+INSERT INTO [t36551] VALUES (28,'الجار','73','0','0');
+INSERT INTO [t36551] VALUES (29,'حسن المعاشرة','74','0','0');
+INSERT INTO [t36551] VALUES (30,'أداء الحقوق','75','0','0');
+INSERT INTO [t36551] VALUES (31,'شجاعته وصبره - صلى الله عليه وسلم -','76','0','0');
+INSERT INTO [t36551] VALUES (32,'دعاؤه - صلى الله عليه وسلم -','80','0','0');
+INSERT INTO [t36551] VALUES (33,'نهاية الزيارة','82','0','0');
+INSERT INTO [t36551] VALUES (34,'الوداع','84','0','0');
