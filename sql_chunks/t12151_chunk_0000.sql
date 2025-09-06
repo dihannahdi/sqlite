393afd729b924198284370fa4b58346b1eac1d66
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:35 +0700

    Upload SQL chunk: t12151_chunk_0000.sql

---
+++ sql_chunks/t12151_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t12151
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t12151" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12151
+INSERT INTO [t12151] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12151] VALUES (2,'مبدأ التأمين وأصلها','2','0','0');
+INSERT INTO [t12151] VALUES (3,'تعريف عقد التأمين','4','0','0');
+INSERT INTO [t12151] VALUES (4,'حكم عقود التأمين','7','0','0');
+INSERT INTO [t12151] VALUES (5,'الغرر','7','4','0');
+INSERT INTO [t12151] VALUES (6,'الربا','12','4','0');
+INSERT INTO [t12151] VALUES (7,'القمار','14','4','0');
+INSERT INTO [t12151] VALUES (8,'بيع الدين بالدين','16','4','0');
+INSERT INTO [t12151] VALUES (9,'احتجاج المجوزين للتأمين والرد عليهم','17','0','0');
+INSERT INTO [t12151] VALUES (10,'عقد التأمين عقد جديد','17','9','0');
+INSERT INTO [t12151] VALUES (11,'عقد الحراسة','20','9','0');
+INSERT INTO [t12151] VALUES (12,'ضمان خطر الطريق','21','9','0');
+INSERT INTO [t12151] VALUES (13,'الحاق عقود التأمين بولاء الموالاة عند الحنفية','23','9','0');
+INSERT INTO [t12151] VALUES (14,'إلحاق عقد التأمين بالوعد الملزم عند بعض المالكية','25','9','0');
+INSERT INTO [t12151] VALUES (15,'إلحاق عفد التأمين بمسألة عقل العاقلة','27','9','0');
+INSERT INTO [t12151] VALUES (16,'الإحتجاج بالمصلحة','29','9','0');
+INSERT INTO [t12151] VALUES (17,'الإحتجاج بأن التأمين ضرورة إقتصادية','31','9','0');
+INSERT INTO [t12151] VALUES (18,'الإحتجاج بنظام التقاعد','33','9','0');
+INSERT INTO [t12151] VALUES (19,'الإحتجاج بالتراضي','35','9','0');
+INSERT INTO [t12151] VALUES (20,'الحاق عقود التأمين بالجعالة','36','9','0');
+INSERT INTO [t12151] VALUES (21,'عقد التأمين التعاوني','38','0','0');
