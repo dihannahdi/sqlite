c26c8a6a150000ca2808866be6978f77a7ae65a2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:24 +0700

    Upload SQL chunk: t22949_chunk_0000.sql

---
+++ sql_chunks/t22949_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t22949
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t22949" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22949
+INSERT INTO [t22949] VALUES (1,'التدليس','1','0','0');
+INSERT INTO [t22949] VALUES (2,'أقسام التدليس','1','0','0');
+INSERT INTO [t22949] VALUES (3,'(الهمزة)','4','0','0');
+INSERT INTO [t22949] VALUES (4,'الباء','6','0','0');
+INSERT INTO [t22949] VALUES (5,'التاء','6','0','0');
+INSERT INTO [t22949] VALUES (6,'الثاء','6','0','0');
+INSERT INTO [t22949] VALUES (7,'الجيم','7','0','0');
+INSERT INTO [t22949] VALUES (8,'الحاء','7','0','0');
+INSERT INTO [t22949] VALUES (9,'الخاء','9','0','0');
+INSERT INTO [t22949] VALUES (10,'الدال والذال: فارغتان وكذلك الراء','9','0','0');
+INSERT INTO [t22949] VALUES (11,'الزاي','9','0','0');
+INSERT INTO [t22949] VALUES (12,'السين','10','0','0');
+INSERT INTO [t22949] VALUES (13,'الشين','12','0','0');
+INSERT INTO [t22949] VALUES (14,'الصاد','13','0','0');
+INSERT INTO [t22949] VALUES (15,'الطاء','14','0','0');
+INSERT INTO [t22949] VALUES (16,'العين','14','0','0');
+INSERT INTO [t22949] VALUES (17,'الفاء','22','0','0');
+INSERT INTO [t22949] VALUES (18,'القاف','22','0','0');
+INSERT INTO [t22949] VALUES (19,'الكاف فارغ','22','0','0');
+INSERT INTO [t22949] VALUES (20,'اللام فارغ','23','0','0');
+INSERT INTO [t22949] VALUES (21,'الميم','23','0','0');
+INSERT INTO [t22949] VALUES (22,'النون فارغ','31','0','0');
+INSERT INTO [t22949] VALUES (23,'الهاء','31','0','0');
+INSERT INTO [t22949] VALUES (24,'الواو','32','0','0');
+INSERT INTO [t22949] VALUES (25,'لا','32','0','0');
+INSERT INTO [t22949] VALUES (26,'الياء','33','0','0');
