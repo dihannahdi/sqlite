ea279dc54afd43369c3cd6b704cbfb085e40a9b4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:38 +0700

    Upload SQL chunk: t12159_chunk_0000.sql

---
+++ sql_chunks/t12159_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t12159
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t12159" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12159
+INSERT INTO [t12159] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t12159] VALUES (2,'التعريق بالإمام','2','0','0');
+INSERT INTO [t12159] VALUES (3,'رجوع أبي الحسن الأشعري عن الإعتزال إلى عقيدة السلف','6','0','0');
+INSERT INTO [t12159] VALUES (4,'كتاب الإبانة','11','0','0');
