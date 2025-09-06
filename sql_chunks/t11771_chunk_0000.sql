6d30109b868bf57629b9943844aa0bd541958d3b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:34 +0700

    Upload SQL chunk: t11771_chunk_0000.sql

---
+++ sql_chunks/t11771_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11771
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11771" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11771
+INSERT INTO [t11771] VALUES (1,'طرق حديث النبي حيث كان في الحائط للضياء المقدسي - مخطوط','2','0','0');
