51a3e2a09afe58717cda819d95becc4f0b1352ff
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:47 +0700

    Upload SQL chunk: t7550_chunk_0000.sql

---
+++ sql_chunks/t7550_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7550
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7550" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7550
+INSERT INTO [t7550] VALUES (1,'الإمام محمد بن عبد الوهاب في مدينة الوصل','1','0','0');
+INSERT INTO [t7550] VALUES (2,'مصادر ومراجع','14','0','0');
