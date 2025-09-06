cf826ab318bbbfcf89d0f38646aa6516763e28ba
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:50 +0700

    Upload SQL chunk: t26851_chunk_0000.sql

---
+++ sql_chunks/t26851_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26851
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26851" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26851
+INSERT INTO [t26851] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26851] VALUES (2,'جزء فيه حديث من حديث أهل حردان','2','0','0');
