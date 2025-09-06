da7d2cb3b10d487fb5a6303393ba56d048a33fcd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:19 +0700

    Upload SQL chunk: t111_chunk_0000.sql

---
+++ sql_chunks/t111_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t111
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t111" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t111
+INSERT INTO [t111] VALUES (1,'سنة أربع وستين وسبع مئة','2','0','0');
+INSERT INTO [t111] VALUES (2,'سنة خمس وستين وسبع مئة','11','0','0');
+INSERT INTO [t111] VALUES (3,'سنة ست وستين وسبع مئة','17','0','0');
+INSERT INTO [t111] VALUES (4,'سنة سبع وستين وسبع مئة','19','0','0');
+INSERT INTO [t111] VALUES (5,'سنة ثمان وستين وسبع مئة','24','0','0');
+INSERT INTO [t111] VALUES (6,'سنة تسع وستين وسبع مئة','28','0','0');
+INSERT INTO [t111] VALUES (7,'سنة سبعين وسبع مئة','37','0','0');
+INSERT INTO [t111] VALUES (8,'سنة أحدى وسبعين وسبع مئة','41','0','0');
+INSERT INTO [t111] VALUES (9,'سنة اثنتين وسبعين وسبع مئة','45','0','0');
+INSERT INTO [t111] VALUES (10,'سنة ثلاث وسبعين وسبع مئة','52','0','0');
+INSERT INTO [t111] VALUES (11,'سنة أربع وسبعين وسبع مئة','57','0','0');
+INSERT INTO [t111] VALUES (12,'سنة ست وسبعين وسبع مئة','65','0','0');
+INSERT INTO [t111] VALUES (13,'سنة سبع وسبعين وسبع مئة','77','0','0');
+INSERT INTO [t111] VALUES (14,'سنة ثمان وسبعين وسبع مئة','83','0','0');
