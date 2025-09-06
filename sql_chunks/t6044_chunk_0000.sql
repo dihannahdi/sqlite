66299c4f378721d326620cef35e03d9607e55ce6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:40 +0700

    Upload SQL chunk: t6044_chunk_0000.sql

---
+++ sql_chunks/t6044_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6044
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6044" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6044
+INSERT INTO [t6044] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6044] VALUES (2,'مسائل صالح بن الإمام أحمد','99','0','0');
+INSERT INTO [t6044] VALUES (3,'رواية أبي الحسن عبد الملك بن عبد الحميد الميموني','106','0','0');
