c4cabf0814a877f56460d334e3f9aa4926eddca4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:32 +0700

    Upload SQL chunk: t6900_chunk_0000.sql

---
+++ sql_chunks/t6900_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t6900
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t6900" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6900
+INSERT INTO [t6900] VALUES (1,'ويمد بها رسول الله صلى الله عليه وسلم صوته','12','0','0');
+INSERT INTO [t6900] VALUES (2,'محمد بن كثير','15','0','0');
+INSERT INTO [t6900] VALUES (3,'باب ما ورد في ذم الشعر','42','0','0');
+INSERT INTO [t6900] VALUES (4,'ملحق في أحاديث الشعر التي لم يوردها المقدسي في جزئه هذا ما ورد في الشعر','57','0','0');
