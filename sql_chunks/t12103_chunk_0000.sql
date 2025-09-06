11222b685806ba1b7b73fdb716057c9bc681209a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:59 +0700

    Upload SQL chunk: t12103_chunk_0000.sql

---
+++ sql_chunks/t12103_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t12103
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t12103" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12103
+INSERT INTO [t12103] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12103] VALUES (2,'تمهيد','10','0','0');
+INSERT INTO [t12103] VALUES (3,'ابن كثير والسيرة النبوية','13','0','0');
+INSERT INTO [t12103] VALUES (4,'السيرة النبوية عند ابن كثير','18','0','0');
+INSERT INTO [t12103] VALUES (5,'مراجعات ابن كثير ونقده:','29','0','0');
+INSERT INTO [t12103] VALUES (6,'مقاييس ابن كثير في نقد المتن:','40','0','0');
+INSERT INTO [t12103] VALUES (7,'عرض الرواية على رواية أصح منها:','43','0','0');
+INSERT INTO [t12103] VALUES (8,'استدعاء تاريخ التشريع:','46','0','0');
+INSERT INTO [t12103] VALUES (9,'استدعاء الحقائق والمعلومات التاريخية','50','0','0');
+INSERT INTO [t12103] VALUES (10,'المحاكمة العقلية للمتون:','55','0','0');
+INSERT INTO [t12103] VALUES (11,'أقوال شيوخه:','61','0','0');
+INSERT INTO [t12103] VALUES (12,'ما أهمل ابن كثير نقده من متون:','64','0','0');
+INSERT INTO [t12103] VALUES (13,'الخاتمة','71','0','0');
+INSERT INTO [t12103] VALUES (14,'مصادر ومراجع','72','0','0');
