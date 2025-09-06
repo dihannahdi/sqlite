8688655e20183201f43d3549f0b824533752614f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:51 +0700

    Upload SQL chunk: t147673_chunk_0000.sql

---
+++ sql_chunks/t147673_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t147673
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t147673" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t147673
+INSERT INTO [t147673] VALUES (1,'مقدمة التحقيق','3','0','0');
+INSERT INTO [t147673] VALUES (2,'اختيارات شيخ الإسلام ابن تيمية الفقهية','4','1','0');
+INSERT INTO [t147673] VALUES (3,'عناية ابن عبد الهادي بمؤلفات ابن تيمية واختياراته','6','1','0');
+INSERT INTO [t147673] VALUES (4,'توثيق نسبة الجزء','9','1','0');
+INSERT INTO [t147673] VALUES (5,'وصف النسخة الخطية','11','1','0');
+INSERT INTO [t147673] VALUES (6,'اسم الجزء','13','1','0');
+INSERT INTO [t147673] VALUES (7,'الفصل الأول','17','0','0');
+INSERT INTO [t147673] VALUES (8,'الفصل الثاني','46','0','0');
+INSERT INTO [t147673] VALUES (9,'الفصل الثالث','78','0','0');
