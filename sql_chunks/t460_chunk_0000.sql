8afadb4dd0506e2251c3b42a41402115e4db45fb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:40 +0700

    Upload SQL chunk: t460_chunk_0000.sql

---
+++ sql_chunks/t460_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t460
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t460" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t460
+INSERT INTO [t460] VALUES (1,'جواب المؤلف لأحد العلماء في هذه المسألة','2','0','0');
+INSERT INTO [t460] VALUES (2,'لا يلزم من بطلان صلاة الإمام بطلان صلاة المأموم، إلا إذا حصل من المأموم تقصير','4','0','0');
