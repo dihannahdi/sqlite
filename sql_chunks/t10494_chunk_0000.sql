ddc9b3fa3ff37a8fa52b63c4a1982d6ac9709a7d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:26 +0700

    Upload SQL chunk: t10494_chunk_0000.sql

---
+++ sql_chunks/t10494_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10494
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10494" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10494
+INSERT INTO [t10494] VALUES (1,'الحربيات - الجزء الثاني - مخطوط','1','0','0');
