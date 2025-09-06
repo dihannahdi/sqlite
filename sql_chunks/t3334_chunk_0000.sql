ffb6525f0747ffc416f72d99baf33dedd223eff0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:27 +0700

    Upload SQL chunk: t3334_chunk_0000.sql

---
+++ sql_chunks/t3334_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t3334
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t3334" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3334
+INSERT INTO [t3334] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t3334] VALUES (2,'باب المقدمات','11','0','0');
+INSERT INTO [t3334] VALUES (3,'مقدمة الطبعة الرابعة','11','2','0');
+INSERT INTO [t3334] VALUES (4,'الفصل الأول: الفصاحة والبلاغة','28','0','0');
+INSERT INTO [t3334] VALUES (5,'الفصاحة والبلاغة','28','4','0');
+INSERT INTO [t3334] VALUES (6,'علم المعاني','42','4','0');
+INSERT INTO [t3334] VALUES (7,'الفصل الثاني: أحوال الإسناد الخبري','44','0','0');
+INSERT INTO [t3334] VALUES (8,'مدخل','44','7','0');
+INSERT INTO [t3334] VALUES (9,'أغراض الخبر','46','7','0');
+INSERT INTO [t3334] VALUES (10,'أضرب الخبر','49','7','0');
+INSERT INTO [t3334] VALUES (11,'التجوز في الإسناد','70','7','0');
+INSERT INTO [t3334] VALUES (12,'الفصل الثالث: أحوال المسند إليه','122','0','0');
+INSERT INTO [t3334] VALUES (13,'الحذف','122','12','0');
+INSERT INTO [t3334] VALUES (14,'ذكر المسند إليه','149','12','0');
+INSERT INTO [t3334] VALUES (15,'تعريف المسند إليه','162','12','0');
+INSERT INTO [t3334] VALUES (16,'اسم الإشارة','171','12','0');
+INSERT INTO [t3334] VALUES (17,'التعريف','182','12','0');
+INSERT INTO [t3334] VALUES (18,'التعريف باللام','182','17','0');
+INSERT INTO [t3334] VALUES (19,'التعريف بالإضافة','184','17','0');
+INSERT INTO [t3334] VALUES (20,'تنكير المسند إليه','186','12','0');
+INSERT INTO [t3334] VALUES (21,'تقديم المسند إليه','194','12','0');
+INSERT INTO [t3334] VALUES (22,'المظهر والمضمر','215','12','0');
+INSERT INTO [t3334] VALUES (23,'الالتفات','223','12','0');
+INSERT INTO [t3334] VALUES (24,'مخالفة مقتضى الظاهر في صيغ الأفعال','236','12','0');
+INSERT INTO [t3334] VALUES (25,'أسلوب الحكيم','244','12','0');
+INSERT INTO [t3334] VALUES (26,'الفصل الرابع: أحوال المسند','246','0','0');
+INSERT INTO [t3334] VALUES (27,'حذف المسند','246','26','0');
+INSERT INTO [t3334] VALUES (28,'ذكر المسند','263','26','0');
+INSERT INTO [t3334] VALUES (29,'مجئ المسند فعلا أو اسما','271','26','0');
+INSERT INTO [t3334] VALUES (30,'تعريف المسند','276','26','0');
+INSERT INTO [t3334] VALUES (31,'تقديم المسند','285','26','0');
+INSERT INTO [t3334] VALUES (32,'الفصل الخامس: أحوال متعلقات الفعل','291','0','0');
+INSERT INTO [t3334] VALUES (33,'مدخل','291','32','0');
+INSERT INTO [t3334] VALUES (34,'أغراض تقييد الفعل','292','32','0');
+INSERT INTO [t3334] VALUES (35,'حذف المفعول','315','32','0');
+INSERT INTO [t3334] VALUES (36,'التقديم في المتعلقات','338','32','0');
+INSERT INTO [t3334] VALUES (37,'مراجع الكتاب','347','0','0');
+INSERT INTO [t3334] VALUES (38,'محتويات الكتاب','349','0','0');
