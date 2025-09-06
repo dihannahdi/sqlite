f2073e63e43cc3ae2ba3454b27c33703bfad6580
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:20 +0700

    Upload SQL chunk: t7307_chunk_0000.sql

---
+++ sql_chunks/t7307_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t7307
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t7307" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7307
+INSERT INTO [t7307] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7307] VALUES (2,'الطبقة الثانية والعشرون وعدتهم سبعة أنفس','4','0','0');
+INSERT INTO [t7307] VALUES (3,'الطبقة الثالثة والعشرون وعدتهم خمسة','19','0','0');
+INSERT INTO [t7307] VALUES (4,'الطبقة الرابعة والعشرون وعدتهم عشرة','29','0','0');
