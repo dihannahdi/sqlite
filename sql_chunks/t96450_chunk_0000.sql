ad9e99b696439949d1314e883d6a0f54d858b1e2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:57:19 +0700

    Upload SQL chunk: t96450_chunk_0000.sql

---
+++ sql_chunks/t96450_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t96450
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t96450" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96450
+INSERT INTO [t96450] VALUES (1,'فوائد الذكر','3','0','0');
+INSERT INTO [t96450] VALUES (2,'ورد الصباح والمساء','6','0','0');
