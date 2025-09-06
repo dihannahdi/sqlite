a874de9aec3eca43abae17a83a325e73a0886571
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:48:11 +0700

    Upload SQL chunk: t23674_chunk_0000.sql

---
+++ sql_chunks/t23674_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t23674
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t23674" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23674
+INSERT INTO [t23674] VALUES (1,'ملاحظات أساسية','3','0','0');
+INSERT INTO [t23674] VALUES (2,'١','3','1','0');
+INSERT INTO [t23674] VALUES (3,'٢','5','1','0');
+INSERT INTO [t23674] VALUES (4,'٣','6','1','0');
+INSERT INTO [t23674] VALUES (5,'٤','8','1','0');
+INSERT INTO [t23674] VALUES (6,'٥','8','1','0');
+INSERT INTO [t23674] VALUES (7,'٦','10','1','0');
+INSERT INTO [t23674] VALUES (8,'٧','11','1','0');
+INSERT INTO [t23674] VALUES (9,'تطور الموقف (الغربي) من السيرة','15','0','0');
+INSERT INTO [t23674] VALUES (10,'١','15','9','0');
+INSERT INTO [t23674] VALUES (11,'٢','19','9','0');
+INSERT INTO [t23674] VALUES (12,'أولا: المبالغة في الشك، والافتراض، والنفي الكيفي، واعتماد الضعيف الشاذ:','20','11','0');
+INSERT INTO [t23674] VALUES (13,'ثانيا: إسقاط الرؤية الوضعية، العلمانية، والتأثيرات البيئية المعاصرة على الوقائع التاريخية:','23','11','0');
+INSERT INTO [t23674] VALUES (14,'ثالثا: رد معطيات السيرة إلى أصول نصرانية أو يهودية:','29','11','0');
+INSERT INTO [t23674] VALUES (15,'٣','35','9','0');
+INSERT INTO [t23674] VALUES (16,'محمد في مكة (١)','43','0','0');
+INSERT INTO [t23674] VALUES (17,'١ - النزعة الشكية والافتراض والنفي الكيفي','43','16','0');
+INSERT INTO [t23674] VALUES (18,'١','43','17','0');
+INSERT INTO [t23674] VALUES (19,'٢','50','17','0');
+INSERT INTO [t23674] VALUES (20,'٣','53','17','0');
+INSERT INTO [t23674] VALUES (21,'٤','61','17','0');
+INSERT INTO [t23674] VALUES (22,'٥','69','17','0');
+INSERT INTO [t23674] VALUES (23,'محمد في مكة (٢)','79','0','0');
+INSERT INTO [t23674] VALUES (24,'٢ - إسقاط الرؤية العقلية المعاصرة على السيرة','79','23','0');
+INSERT INTO [t23674] VALUES (25,'١','79','24','0');
+INSERT INTO [t23674] VALUES (26,'٢','82','24','0');
+INSERT INTO [t23674] VALUES (27,'٣','85','24','0');
+INSERT INTO [t23674] VALUES (28,'٤','94','24','0');
+INSERT INTO [t23674] VALUES (29,'٥','99','24','0');
