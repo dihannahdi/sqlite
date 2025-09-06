db2a3d44dbbc5dc5c61c55f35da103d0d69f76e6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:05 +0700

    Upload SQL chunk: t21767_chunk_0000.sql

---
+++ sql_chunks/t21767_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t21767
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t21767" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21767
+INSERT INTO [t21767] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t21767] VALUES (2,'معنى القرآن','4','0','0');
+INSERT INTO [t21767] VALUES (3,'من أوجه إعجاز كتاب الله','11','0','0');
+INSERT INTO [t21767] VALUES (4,'فضل تلاوة القرآن','14','0','0');
+INSERT INTO [t21767] VALUES (5,'صاحب القرآن هو المقدم في أول منازل الآخرة','16','0','0');
+INSERT INTO [t21767] VALUES (6,'فضل حفظ كتاب الله','20','0','0');
+INSERT INTO [t21767] VALUES (7,'القدر الواجب حفظه من القرآن','23','0','0');
+INSERT INTO [t21767] VALUES (8,'من الأمور التي ينبغي العناية بها لمن أراد تلاوة القرآن وحفظه','24','0','0');
+INSERT INTO [t21767] VALUES (9,'آداب قارئ القرآن','38','0','0');
