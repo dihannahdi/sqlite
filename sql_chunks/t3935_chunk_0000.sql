6b0f3dd3042351db8ce08f0ef51b3302c92482b9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:22 +0700

    Upload SQL chunk: t3935_chunk_0000.sql

---
+++ sql_chunks/t3935_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t3935
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t3935" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3935
+INSERT INTO [t3935] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t3935] VALUES (2,'ما هو العقل؟','2','0','0');
+INSERT INTO [t3935] VALUES (3,'العقل في القرآن','2','0','0');
+INSERT INTO [t3935] VALUES (4,'العقل في الحديث النبوي','3','0','0');
+INSERT INTO [t3935] VALUES (5,'العقل عند العلماء','4','0','0');
+INSERT INTO [t3935] VALUES (6,'محل العقل','6','0','0');
+INSERT INTO [t3935] VALUES (7,'العقل عند الفلاسفة','19','0','0');
+INSERT INTO [t3935] VALUES (8,'ظهور المدرسة العقلية عند الغرب','41','0','0');
+INSERT INTO [t3935] VALUES (9,'العقل عند المعتزلة','68','0','0');
+INSERT INTO [t3935] VALUES (10,'المدرسة العقلية الحديثة','84','0','0');
+INSERT INTO [t3935] VALUES (11,'الرد على العقلانيين','86','0','0');
+INSERT INTO [t3935] VALUES (12,'الوجه الأول','87','11','0');
+INSERT INTO [t3935] VALUES (13,'الوجه الثاني','89','11','0');
+INSERT INTO [t3935] VALUES (14,'الوجه الثالث','98','11','0');
+INSERT INTO [t3935] VALUES (15,'الوجه الرابع','101','11','0');
+INSERT INTO [t3935] VALUES (16,'الوجه الخامس','134','11','0');
+INSERT INTO [t3935] VALUES (17,'الوجه السادس','135','11','0');
+INSERT INTO [t3935] VALUES (18,'الوجه السابع','135','11','0');
+INSERT INTO [t3935] VALUES (19,'الوجه الثامن','138','11','0');
+INSERT INTO [t3935] VALUES (20,'الوجه التاسع','140','11','0');
+INSERT INTO [t3935] VALUES (21,'الوجه العاشر','140','11','0');
+INSERT INTO [t3935] VALUES (22,'الوجه الثالث والسبعون','249','11','0');
+INSERT INTO [t3935] VALUES (23,'نشر " العقلانية " في العالم الإسلامي هدف من أهداف اليهود','250','0','0');
+INSERT INTO [t3935] VALUES (24,'عقلانيون يتوبون قبل الموت','251','0','0');
+INSERT INTO [t3935] VALUES (25,'خاتمة','252','0','0');
