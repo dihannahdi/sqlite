3735c5b87fb7875b7abd0290af6fedf5e7c0e909
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:17 +0700

    Upload SQL chunk: t36567_chunk_0000.sql

---
+++ sql_chunks/t36567_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t36567
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t36567" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36567
+INSERT INTO [t36567] VALUES (1,'زيد بن محمد','1','0','0');
+INSERT INTO [t36567] VALUES (2,'زينب فتاة قريش','2','0','0');
+INSERT INTO [t36567] VALUES (3,'زوجها من زيد المولى','2','0','0');
+INSERT INTO [t36567] VALUES (4,'حتى كان المفترق','3','0','0');
+INSERT INTO [t36567] VALUES (5,'قلب الرسول البشر','4','0','0');
+INSERT INTO [t36567] VALUES (6,'الزواج','5','0','0');
+INSERT INTO [t36567] VALUES (7,'الأمر بالحجاب','6','0','0');
+INSERT INTO [t36567] VALUES (8,'فرحة زينب - رضي الله عنها -','6','0','0');
+INSERT INTO [t36567] VALUES (9,'مفاخرتها نساء النبي - صلى الله عليه وسلم - وإعجابها بنفسها','6','0','0');
+INSERT INTO [t36567] VALUES (10,'المتصدقة بالأموال','7','0','0');
+INSERT INTO [t36567] VALUES (11,'المتصدقة بالأكفان','8','0','0');
+INSERT INTO [t36567] VALUES (12,'والصلاة عليها ودفنها','8','0','0');
