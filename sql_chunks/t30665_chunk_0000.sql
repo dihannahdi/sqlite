2d8fec3448e4ddc6726fd3a3d1412ec988a45da1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:36 +0700

    Upload SQL chunk: t30665_chunk_0000.sql

---
+++ sql_chunks/t30665_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30665
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30665" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30665
+INSERT INTO [t30665] VALUES (1,'جزء فيه المسلسلات من الأحاديث والآثار','1','0','0');
