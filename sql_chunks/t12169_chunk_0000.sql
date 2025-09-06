68e54bbdce5290b3319dfa2eccbe5dbe3843d69e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:43 +0700

    Upload SQL chunk: t12169_chunk_0000.sql

---
+++ sql_chunks/t12169_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t12169
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t12169" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12169
+INSERT INTO [t12169] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12169] VALUES (2,'الوقفة الأولى: من خلال مقارنة معتقداتهم في الله تعالى','2','0','0');
+INSERT INTO [t12169] VALUES (3,'الوقفة الثانية: من خلال مقارنة معتقداتهم في الأنبياء','15','0','0');
+INSERT INTO [t12169] VALUES (4,'الوقفة الثالثة: من خلال مفارنة معتقداتهم في اليوم الآخر','20','0','0');
+INSERT INTO [t12169] VALUES (5,'الوقفة الرابعة: من خلال مقارنة موقفهم من الزهد','29','0','0');
+INSERT INTO [t12169] VALUES (6,'الخاتمة','33','0','0');
+INSERT INTO [t12169] VALUES (7,'مصادر ومراجع','34','0','0');
