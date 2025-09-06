09d70ef4806c9ae13350b0b7f5b28076b8813ac4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:00:08 +0700

    Upload SQL chunk: t1188_chunk_0000.sql

---
+++ sql_chunks/t1188_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t1188
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t1188" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1188
+INSERT INTO [t1188] VALUES (1,'الباب الأول: بل هو قرآن مجيد في لوح محفوظ','8','0','0');
+INSERT INTO [t1188] VALUES (2,'الباب الثاني: قد جاءكم من الله نور وكتاب مبين','28','0','0');
+INSERT INTO [t1188] VALUES (3,'الباب الثالث: قال تعالى: قل انظروا ماذا في السماوات والأرض','50','0','0');
