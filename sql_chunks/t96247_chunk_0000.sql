34abb774901069a576fda2b5192ce279e7ac0f38
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:33 +0700

    Upload SQL chunk: t96247_chunk_0000.sql

---
+++ sql_chunks/t96247_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t96247
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t96247" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96247
+INSERT INTO [t96247] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t96247] VALUES (2,'خلاصة حكم مودة المؤمن للكافر','1','0','0');
+INSERT INTO [t96247] VALUES (3,'حكم مودة المؤمن للكافر التفصيلي','2','0','0');
+INSERT INTO [t96247] VALUES (4,'تحقيق مسألة الإذن من الزواج من الكتابية المحصنة العفيفة','2','0','0');
+INSERT INTO [t96247] VALUES (5,'بيان المتفق عليه بين أهل العلم في باب التعامل مع الكافر','9','0','0');
+INSERT INTO [t96247] VALUES (6,'بيان خلاف أهل العلم في حكم مودة المؤمن للكافر','10','0','0');
+INSERT INTO [t96247] VALUES (7,'الفريق الأول','11','6','0');
+INSERT INTO [t96247] VALUES (8,'الفريق الثاني','28','6','0');
+INSERT INTO [t96247] VALUES (9,'الفريق الثالث','35','6','0');
+INSERT INTO [t96247] VALUES (10,'الفريق الرابع','46','6','0');
+INSERT INTO [t96247] VALUES (11,'حكم معاملة الكافر بما يدل على مودات القلوب','50','0','0');
+INSERT INTO [t96247] VALUES (12,'حكم مودة الكافر للمؤمن','50','0','0');
+INSERT INTO [t96247] VALUES (13,'حكم طلب مودة الكفار غير المحاربين','50','0','0');
+INSERT INTO [t96247] VALUES (14,'الخاتمة','51','0','0');
