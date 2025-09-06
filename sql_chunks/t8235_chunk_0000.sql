a6f6fb3897a1cc9963572c7dc6d081ac5f85280a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:46 +0700

    Upload SQL chunk: t8235_chunk_0000.sql

---
+++ sql_chunks/t8235_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8235
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8235" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8235
+INSERT INTO [t8235] VALUES (1,'باب ما روي في صفة المنافق وأن من كان فيه ثلاث خصال فهو منافق حقا','1','0','0');
+INSERT INTO [t8235] VALUES (2,'باب فيمن كان يخاف النفاق ويشفق منه ولا يأمنه على نفسه','69','0','0');
