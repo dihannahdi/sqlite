3b0214b52414280ff650e07ed2f380ee2e1915c4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:53 +0700

    Upload SQL chunk: t6934_chunk_0000.sql

---
+++ sql_chunks/t6934_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6934
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6934" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6934
+INSERT INTO [t6934] VALUES (1,'الإتباع لأبي على القالي','1','0','0');
+INSERT INTO [t6934] VALUES (2,'الصدوق يعطي ثلاث خصال الهيبة والملحة والمحبة فقال يمكن أن تكون الملحة من قولهم تملحت الإبل إذا سمنت فكأنه يعطي الزيادة والفضل ويقولون أجمعون أكتعون فأكتعون بمعنى أجمعين وقال أبو بكر ابن دريد كتع الرجل إذا تقبض','15','0','0');
