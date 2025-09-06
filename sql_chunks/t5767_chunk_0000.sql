7368909dea81ba737dbeec862fb969cc4d684565
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:09 +0700

    Upload SQL chunk: t5767_chunk_0000.sql

---
+++ sql_chunks/t5767_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5767
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5767" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5767
+INSERT INTO [t5767] VALUES (1,'خرج الإمام أحمد','1','0','0');
+INSERT INTO [t5767] VALUES (2,'تم هذا الحديث وشرحه','122','0','0');
