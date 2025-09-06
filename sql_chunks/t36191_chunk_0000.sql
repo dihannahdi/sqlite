673799c9dece8e5493ef5fec86e5c4e90fa39fd3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:11 +0700

    Upload SQL chunk: t36191_chunk_0000.sql

---
+++ sql_chunks/t36191_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t36191
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t36191" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36191
+INSERT INTO [t36191] VALUES (1,'مقدمة المحقق للطبعة الأولى','3','0','0');
+INSERT INTO [t36191] VALUES (2,'وصف البلاغة المحمدية','10','0','0');
+INSERT INTO [t36191] VALUES (3,'السمو الروحي الأعظم والجمال الفني في البلاغة النبوية','13','0','0');
