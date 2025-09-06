6cbd41f9b86c0093f0caa58a12321cbd4b5cfeda
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:26 +0700

    Upload SQL chunk: t5799_chunk_0000.sql

---
+++ sql_chunks/t5799_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t5799
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t5799" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5799
+INSERT INTO [t5799] VALUES (1,'فمن ذلك في باب أحمد','2','0','0');
+INSERT INTO [t5799] VALUES (2,'ومن ذلك','3','0','0');
+INSERT INTO [t5799] VALUES (3,'ومن ذلك في باب إبراهيم وغيره من الأسماء','4','0','0');
+INSERT INTO [t5799] VALUES (4,'وممن ذكر في باب الباء','9','0','0');
+INSERT INTO [t5799] VALUES (5,'ومن الجيم','11','0','0');
+INSERT INTO [t5799] VALUES (6,'ومن الحاء','12','0','0');
+INSERT INTO [t5799] VALUES (7,'ومن الدال','16','0','0');
+INSERT INTO [t5799] VALUES (8,'ومن السين','17','0','0');
+INSERT INTO [t5799] VALUES (9,'ومن العين','20','0','0');
+INSERT INTO [t5799] VALUES (10,'ومن الميم','25','0','0');
