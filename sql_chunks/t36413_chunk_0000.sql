d38ea43e447818a7158854ded3e501a9b68f7ec3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:07 +0700

    Upload SQL chunk: t36413_chunk_0000.sql

---
+++ sql_chunks/t36413_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t36413
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t36413" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36413
+INSERT INTO [t36413] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t36413] VALUES (2,'شهر المغفرة','3','0','0');
+INSERT INTO [t36413] VALUES (3,'هل من مشمر للجنة','3','2','0');
+INSERT INTO [t36413] VALUES (4,'أحوال الناس مع رمضان','3','2','0');
+INSERT INTO [t36413] VALUES (5,'علامات صلاح القلب','4','2','0');
+INSERT INTO [t36413] VALUES (6,'القسم الأول مع الله','5','5','0');
+INSERT INTO [t36413] VALUES (7,'الصيام','5','6','0');
+INSERT INTO [t36413] VALUES (8,'التعلق بالمساجد','5','6','0');
+INSERT INTO [t36413] VALUES (9,'القرآن الكريم','6','6','0');
+INSERT INTO [t36413] VALUES (10,'قيام الليل','7','6','0');
+INSERT INTO [t36413] VALUES (11,'الإستفادة من الأوقات الفاضلة','8','6','0');
+INSERT INTO [t36413] VALUES (12,'الإعتكاف','9','6','0');
+INSERT INTO [t36413] VALUES (13,'الدعاء','10','6','0');
+INSERT INTO [t36413] VALUES (14,'الصدقة','10','6','0');
+INSERT INTO [t36413] VALUES (15,'الفكر والذكر','10','6','0');
+INSERT INTO [t36413] VALUES (16,'محاسبة النفس','11','6','0');
+INSERT INTO [t36413] VALUES (17,'خلاصة القسم الأول','12','6','0');
+INSERT INTO [t36413] VALUES (18,'القسم الثاني مع الناس','13','5','0');
+INSERT INTO [t36413] VALUES (19,'١ - الإحسان إلى الزوجة والأولاد','13','18','0');
+INSERT INTO [t36413] VALUES (20,'٢ - الجود والكرم','13','18','0');
+INSERT INTO [t36413] VALUES (21,'٣ - صلة الرحم','14','18','0');
+INSERT INTO [t36413] VALUES (22,'٤ - إطعام الطعام','14','18','0');
+INSERT INTO [t36413] VALUES (23,'٥ - الإصلاح بين الناس','15','18','0');
+INSERT INTO [t36413] VALUES (24,'٦ - قضاء حوائج الناس','15','18','0');
+INSERT INTO [t36413] VALUES (25,'٧ - أنقذ غيرك','15','18','0');
+INSERT INTO [t36413] VALUES (26,'خلاصة القسم الثاني','17','18','0');
+INSERT INTO [t36413] VALUES (27,'أتعهد','18','0','0');
