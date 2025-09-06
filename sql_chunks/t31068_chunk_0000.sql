7fe7c01d660badbc153e26349b5d8a3c629ccc9a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:04 +0700

    Upload SQL chunk: t31068_chunk_0000.sql

---
+++ sql_chunks/t31068_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t31068
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t31068" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31068
+INSERT INTO [t31068] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t31068] VALUES (2,'مدخل','6','0','0');
+INSERT INTO [t31068] VALUES (3,'تعريف الخطبة وأغراضها وأثرها','6','0','0');
+INSERT INTO [t31068] VALUES (4,'أنواع الخطبة','10','0','0');
+INSERT INTO [t31068] VALUES (5,'إعداد الخطبة وبناؤها','12','0','0');
+INSERT INTO [t31068] VALUES (6,'توطئة','12','5','0');
+INSERT INTO [t31068] VALUES (7,'عناصر البناء','14','5','0');
+INSERT INTO [t31068] VALUES (8,'الإيجاد والابتكار','14','7','0');
+INSERT INTO [t31068] VALUES (9,'التنسيق والترتيب','15','7','0');
+INSERT INTO [t31068] VALUES (10,'اللسان المعبر','17','7','0');
+INSERT INTO [t31068] VALUES (11,'طريقة البناء','21','5','0');
+INSERT INTO [t31068] VALUES (12,'المقدمة','21','11','0');
+INSERT INTO [t31068] VALUES (13,'الموضوع','23','11','0');
+INSERT INTO [t31068] VALUES (14,'التعريف والإيضاح','23','13','0');
+INSERT INTO [t31068] VALUES (15,'الاستدلال','24','13','0');
+INSERT INTO [t31068] VALUES (16,'الخاتمة','25','11','0');
+INSERT INTO [t31068] VALUES (17,'وحدة الموضوع','26','11','0');
+INSERT INTO [t31068] VALUES (18,'الجدة والتغيير','27','11','0');
+INSERT INTO [t31068] VALUES (19,'طول الخطبة','27','11','0');
+INSERT INTO [t31068] VALUES (20,'صفات الخطيب وآدابه','28','0','0');
+INSERT INTO [t31068] VALUES (21,'لكل خطيب متميز خصوصيته','28','20','0');
+INSERT INTO [t31068] VALUES (22,'الصفات الفطرية','28','20','0');
+INSERT INTO [t31068] VALUES (23,'الصفات المكتسبة','31','20','0');
+INSERT INTO [t31068] VALUES (24,'إضافات وصفات وآداب عامة','33','20','0');
+INSERT INTO [t31068] VALUES (25,'مصادر الخطبة','37','0','0');
+INSERT INTO [t31068] VALUES (26,'خلاصة ونتائج','42','0','0');
