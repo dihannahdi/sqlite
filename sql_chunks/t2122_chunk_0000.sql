96b0b203f6542fe7d5fd5f5f70f4263c46c62f02
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:12 +0700

    Upload SQL chunk: t2122_chunk_0000.sql

---
+++ sql_chunks/t2122_chunk_0000.sql
@@ -0,0 +1,59 @@
+-- Table: t2122
+-- Chunk: 1
+-- Rows: 52
+
+CREATE TABLE "t2122" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2122
+INSERT INTO [t2122] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t2122] VALUES (2,'الدرس الأول','1','0','0');
+INSERT INTO [t2122] VALUES (3,'الدرس الثاني','16','0','0');
+INSERT INTO [t2122] VALUES (4,'بسم الله الرحمن الرحيم','24','0','0');
+INSERT INTO [t2122] VALUES (5,'باب الكلام','25','0','0');
+INSERT INTO [t2122] VALUES (6,'الدرس الثالث','31','0','0');
+INSERT INTO [t2122] VALUES (7,'الدرس الرابع','46','0','0');
+INSERT INTO [t2122] VALUES (8,'باب الإعراب','51','0','0');
+INSERT INTO [t2122] VALUES (9,'باب علامات الإعراب','57','0','0');
+INSERT INTO [t2122] VALUES (10,'الدرس الخامس','60','0','0');
+INSERT INTO [t2122] VALUES (11,'الدرس السادس','74','0','0');
+INSERT INTO [t2122] VALUES (12,'باب علامات النصب','79','0','0');
+INSERT INTO [t2122] VALUES (13,'باب علامات الخفض','85','0','0');
+INSERT INTO [t2122] VALUES (14,'الدرس السابع','91','0','0');
+INSERT INTO [t2122] VALUES (15,'باب علامات الجزم','94','0','0');
+INSERT INTO [t2122] VALUES (16,'فصل','96','0','0');
+INSERT INTO [t2122] VALUES (17,'باب الأفعال','98','0','0');
+INSERT INTO [t2122] VALUES (18,'الدرس الثامن','107','0','0');
+INSERT INTO [t2122] VALUES (19,'الدرس التاسع','122','0','0');
+INSERT INTO [t2122] VALUES (20,'باب مرفوعات الأسماء','124','0','0');
+INSERT INTO [t2122] VALUES (21,'الدرس العاشر','135','0','0');
+INSERT INTO [t2122] VALUES (22,'باب نائب الفاعل','136','0','0');
+INSERT INTO [t2122] VALUES (23,'باب المبتدأ والخبر','145','0','0');
+INSERT INTO [t2122] VALUES (24,'الدرس الحادي عشر','149','0','0');
+INSERT INTO [t2122] VALUES (25,'باب كان وأخواتها','159','0','0');
+INSERT INTO [t2122] VALUES (26,'الدرس الثاني عشر','165','0','0');
+INSERT INTO [t2122] VALUES (27,'باب إن وأخواتها','167','0','0');
+INSERT INTO [t2122] VALUES (28,'باب ظن وأخواتها','171','0','0');
+INSERT INTO [t2122] VALUES (29,'الدرس الثالث عشر','180','0','0');
+INSERT INTO [t2122] VALUES (30,'باب النعت','180','0','0');
+INSERT INTO [t2122] VALUES (31,'الدرس الرابع عشر','195','0','0');
+INSERT INTO [t2122] VALUES (32,'الدرس الخامس عشر','210','0','0');
+INSERT INTO [t2122] VALUES (33,'باب العطف','213','0','0');
+INSERT INTO [t2122] VALUES (34,'باب التوكيد','220','0','0');
+INSERT INTO [t2122] VALUES (35,'الدرس السادس عشر','226','0','0');
+INSERT INTO [t2122] VALUES (36,'باب البدل','226','0','0');
+INSERT INTO [t2122] VALUES (37,'باب المنصوبات','231','0','0');
+INSERT INTO [t2122] VALUES (38,'باب المفعول به','232','0','0');
+INSERT INTO [t2122] VALUES (39,'باب المصدر (المفعول المطلق)','237','0','0');
+INSERT INTO [t2122] VALUES (40,'الدرس السابع عشر','240','0','0');
+INSERT INTO [t2122] VALUES (41,'باب الظرف','242','0','0');
+INSERT INTO [t2122] VALUES (42,'باب الحال','249','0','0');
+INSERT INTO [t2122] VALUES (43,'الدرس الثامن عشر','256','0','0');
+INSERT INTO [t2122] VALUES (44,'باب التمييز','259','0','0');
+INSERT INTO [t2122] VALUES (45,'باب الاستثناء','265','0','0');
+INSERT INTO [t2122] VALUES (46,'الدرس التاسع عشر','271','0','0');
+INSERT INTO [t2122] VALUES (47,'باب لا النافية للجنس','274','0','0');
+INSERT INTO [t2122] VALUES (48,'باب المنادى','278','0','0');
+INSERT INTO [t2122] VALUES (49,'باب المفعول لأجله','281','0','0');
+INSERT INTO [t2122] VALUES (50,'باب المخفوضات من الأسماء','284','0','0');
+INSERT INTO [t2122] VALUES (51,'الدرس العشرون','286','0','0');
+INSERT INTO [t2122] VALUES (52,'باب المخفوض بالإضافة','289','0','0');
