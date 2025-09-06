7a774e9b909caedd6b78cde9e67604f0d0d2b587
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:43 +0700

    Upload SQL chunk: t21562_chunk_0000.sql

---
+++ sql_chunks/t21562_chunk_0000.sql
@@ -0,0 +1,48 @@
+-- Table: t21562
+-- Chunk: 1
+-- Rows: 41
+
+CREATE TABLE "t21562" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21562
+INSERT INTO [t21562] VALUES (1,'الباب الثاني','1','0','0');
+INSERT INTO [t21562] VALUES (2,'الباب الثالث','4','0','0');
+INSERT INTO [t21562] VALUES (3,'الباب الرابع','7','0','0');
+INSERT INTO [t21562] VALUES (4,'الباب الخامس','9','0','0');
+INSERT INTO [t21562] VALUES (5,'الباب السادس','12','0','0');
+INSERT INTO [t21562] VALUES (6,'الباب السابع','14','0','0');
+INSERT INTO [t21562] VALUES (7,'الباب الثامن','16','0','0');
+INSERT INTO [t21562] VALUES (8,'الباب التاسع','18','0','0');
+INSERT INTO [t21562] VALUES (9,'الباب العاشر','24','0','0');
+INSERT INTO [t21562] VALUES (10,'الباب الحادي عشر','27','0','0');
+INSERT INTO [t21562] VALUES (11,'الباب الثاني عشر','32','0','0');
+INSERT INTO [t21562] VALUES (12,'الباب الثالث عشر','34','0','0');
+INSERT INTO [t21562] VALUES (13,'الباب الرابع عشر','38','0','0');
+INSERT INTO [t21562] VALUES (14,'الباب الخامس عشر','40','0','0');
+INSERT INTO [t21562] VALUES (15,'الباب السادس عشر','43','0','0');
+INSERT INTO [t21562] VALUES (16,'فصل','44','0','0');
+INSERT INTO [t21562] VALUES (17,'فصل','44','0','0');
+INSERT INTO [t21562] VALUES (18,'فصل','44','0','0');
+INSERT INTO [t21562] VALUES (19,'الباب السابع عشر','48','0','0');
+INSERT INTO [t21562] VALUES (20,'الباب الثامن عشر','52','0','0');
+INSERT INTO [t21562] VALUES (21,'الباب التاسع عشر','55','0','0');
+INSERT INTO [t21562] VALUES (22,'فصل','56','0','0');
+INSERT INTO [t21562] VALUES (23,'الباب العشرون','58','0','0');
+INSERT INTO [t21562] VALUES (24,'الباب الحادي والعشرون','62','0','0');
+INSERT INTO [t21562] VALUES (25,'الباب الثاني والعشرون','64','0','0');
+INSERT INTO [t21562] VALUES (26,'الباب الثالث والعشرون','65','0','0');
+INSERT INTO [t21562] VALUES (27,'فصل','67','0','0');
+INSERT INTO [t21562] VALUES (28,'فصل','68','0','0');
+INSERT INTO [t21562] VALUES (29,'الباب الرابع والعشرون','69','0','0');
+INSERT INTO [t21562] VALUES (30,'الباب الخامس والعشرون','71','0','0');
+INSERT INTO [t21562] VALUES (31,'الباب السادس والعشرون','74','0','0');
+INSERT INTO [t21562] VALUES (32,'الباب السابع والعشرون','76','0','0');
+INSERT INTO [t21562] VALUES (33,'الباب الثامن والعشرون','78','0','0');
+INSERT INTO [t21562] VALUES (34,'الباب التاسع والعشرون','80','0','0');
+INSERT INTO [t21562] VALUES (35,'فصل','81','0','0');
+INSERT INTO [t21562] VALUES (36,'الباب الثلاثون','83','0','0');
+INSERT INTO [t21562] VALUES (37,'الباب الحادي والثلاثون','88','0','0');
+INSERT INTO [t21562] VALUES (38,'فصل','91','0','0');
+INSERT INTO [t21562] VALUES (39,'الباب الثاني والثلاثون','93','0','0');
+INSERT INTO [t21562] VALUES (40,'فصل','94','0','0');
+INSERT INTO [t21562] VALUES (41,'الباب الثالث والثلاثون','98','0','0');
