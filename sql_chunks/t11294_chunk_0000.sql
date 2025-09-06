13446bd6f15207f4cf7d4998468515faf9956f45
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:33 +0700

    Upload SQL chunk: t11294_chunk_0000.sql

---
+++ sql_chunks/t11294_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t11294
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t11294" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11294
+INSERT INTO [t11294] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11294] VALUES (2,'تمهيد','2','0','0');
+INSERT INTO [t11294] VALUES (3,'المبحث الأول: معالم في مناهج كتب التخريج','7','0','0');
+INSERT INTO [t11294] VALUES (4,'المطلب الأول: تنوع موضوعات هذه الكتب','7','3','0');
+INSERT INTO [t11294] VALUES (5,'المطلب الثاني: تنوع هذه المصنفات بين الإطناب والاختصار','25','3','0');
+INSERT INTO [t11294] VALUES (6,'المبحث الثاني: دور علم التخريج في حفظ السنة النبوية','33','0','0');
+INSERT INTO [t11294] VALUES (7,'مدخل','33','6','0');
+INSERT INTO [t11294] VALUES (8,'المطلب الأول: دور علم التخريج في حفظ السنة رواية','35','6','0');
+INSERT INTO [t11294] VALUES (9,'المطلب الثاني: دور علم التخريج في حفظ السنة دراية','42','6','0');
+INSERT INTO [t11294] VALUES (10,'المطلب الثالث: دور علم التخريج في تقرير الأحكام الحديثة','55','6','0');
+INSERT INTO [t11294] VALUES (11,'الخاتمة','61','0','0');
