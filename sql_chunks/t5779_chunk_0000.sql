9bd350287903f502237f07b544f1dc08be708f28
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:15 +0700

    Upload SQL chunk: t5779_chunk_0000.sql

---
+++ sql_chunks/t5779_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t5779
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t5779" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5779
+INSERT INTO [t5779] VALUES (1,'باب الألف','2','0','0');
+INSERT INTO [t5779] VALUES (2,'باب الباء','5','0','0');
+INSERT INTO [t5779] VALUES (3,'باب التاء','8','0','0');
+INSERT INTO [t5779] VALUES (4,'باب الثاء','9','0','0');
+INSERT INTO [t5779] VALUES (5,'باب الجيم','9','0','0');
+INSERT INTO [t5779] VALUES (6,'باب الحاء','11','0','0');
+INSERT INTO [t5779] VALUES (7,'باب الخاء','14','0','0');
+INSERT INTO [t5779] VALUES (8,'باب الدال','14','0','0');
+INSERT INTO [t5779] VALUES (9,'باب الذال','15','0','0');
+INSERT INTO [t5779] VALUES (10,'باب الراي','15','0','0');
+INSERT INTO [t5779] VALUES (11,'باب الزاي','17','0','0');
+INSERT INTO [t5779] VALUES (12,'باب السين','18','0','0');
+INSERT INTO [t5779] VALUES (13,'باب الشين','22','0','0');
+INSERT INTO [t5779] VALUES (14,'باب الصاد','22','0','0');
+INSERT INTO [t5779] VALUES (15,'باب الضاد','23','0','0');
+INSERT INTO [t5779] VALUES (16,'باب الطاء','23','0','0');
+INSERT INTO [t5779] VALUES (17,'باب الظاء','24','0','0');
+INSERT INTO [t5779] VALUES (18,'باب العين','24','0','0');
+INSERT INTO [t5779] VALUES (19,'باب الغين','28','0','0');
+INSERT INTO [t5779] VALUES (20,'باب الفاء','28','0','0');
+INSERT INTO [t5779] VALUES (21,'باب القاف','28','0','0');
+INSERT INTO [t5779] VALUES (22,'باب الكاف','30','0','0');
+INSERT INTO [t5779] VALUES (23,'باب اللام','31','0','0');
+INSERT INTO [t5779] VALUES (24,'باب الميم','31','0','0');
+INSERT INTO [t5779] VALUES (25,'باب النون','34','0','0');
+INSERT INTO [t5779] VALUES (26,'باب الهاء','34','0','0');
+INSERT INTO [t5779] VALUES (27,'باب الواو','36','0','0');
+INSERT INTO [t5779] VALUES (28,'باب لا','38','0','0');
