eec6c1140372917494405dbbe50d0caab40e0240
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:54 +0700

    Upload SQL chunk: t264_chunk_0000.sql

---
+++ sql_chunks/t264_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t264
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t264" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t264
+INSERT INTO [t264] VALUES (1,'الإيمان لابن تيمية','1','0','0');
