06fb44de00701695583dc26195361ba85d156ab0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:02 +0700

    Upload SQL chunk: t8185_chunk_0000.sql

---
+++ sql_chunks/t8185_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t8185
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t8185" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8185
+INSERT INTO [t8185] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t8185] VALUES (2,'وصلى الله على سيدنا محمد وآله وسلم','1','0','0');
+INSERT INTO [t8185] VALUES (3,'رسالة في آداب الأكل','1','0','0');
