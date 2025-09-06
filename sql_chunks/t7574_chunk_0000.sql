a56355d938ca4d7a9bf21582940d490abbe8796f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:07 +0700

    Upload SQL chunk: t7574_chunk_0000.sql

---
+++ sql_chunks/t7574_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7574
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7574" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7574
+INSERT INTO [t7574] VALUES (1,'التحذير من تعظيم الآثار غير المشروعة','1','0','0');
+INSERT INTO [t7574] VALUES (2,'للدكتور: عمر كامل، بعنوان: "لا خوف على بلاد الحرمين من الشرك والوثنية','1','0','0');
