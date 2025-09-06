6487edfb49a76554b6f1116f27a0288c22d86176
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:35 +0700

    Upload SQL chunk: t4272_chunk_0000.sql

---
+++ sql_chunks/t4272_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t4272
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t4272" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4272
+INSERT INTO [t4272] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t4272] VALUES (2,'أسس تربية النبي صلى الله عليه وسلم؛ وفيها ثلاث وقفات','3','0','0');
+INSERT INTO [t4272] VALUES (3,'الوقفة الأولى: في التربية على العقيدة والعبادة.','3','2','0');
+INSERT INTO [t4272] VALUES (4,'الوقفة الثانية: في التربية الأخلاقية','16','2','0');
+INSERT INTO [t4272] VALUES (5,'الوقفة الثالثة: التربية بالقدوة','27','2','0');
+INSERT INTO [t4272] VALUES (6,'أساليب تربيته صلى الله عليه وسلم؛ وفيها ثلاث وقفات','33','0','0');
+INSERT INTO [t4272] VALUES (7,'الوقفة الرابعة: التربية بالثواب والعقاب.','33','6','0');
+INSERT INTO [t4272] VALUES (8,'الوقفة الخامسة: التربية بالقرآن.','41','6','0');
+INSERT INTO [t4272] VALUES (9,'الوقفة السادسة: التربية المتوازنة.','48','6','0');
+INSERT INTO [t4272] VALUES (10,'الخاتمة','53','0','0');
+INSERT INTO [t4272] VALUES (11,'مصادر ومراجع','54','0','0');
