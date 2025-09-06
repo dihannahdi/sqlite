41907d86b6823e5d03bc9e243450d1bc6955f9ca
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:28 +0700

    Upload SQL chunk: t11564_chunk_0000.sql

---
+++ sql_chunks/t11564_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t11564
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t11564" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11564
+INSERT INTO [t11564] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t11564] VALUES (2,'فضل قيام الليل','5','0','0');
+INSERT INTO [t11564] VALUES (3,'الشيطان يثبط الإنسان عن قيام الليل','18','0','0');
+INSERT INTO [t11564] VALUES (4,'استهزاء الشيطان بمن أهمل قيام الليل','30','0','0');
+INSERT INTO [t11564] VALUES (5,'احب القيام إلى الله','33','0','0');
+INSERT INTO [t11564] VALUES (6,'مقدار قيام الليل','36','0','0');
+INSERT INTO [t11564] VALUES (7,'صور من قيام السلف الصالح رحمهم الله','39','0','0');
+INSERT INTO [t11564] VALUES (8,'فضل عبادة الليل على عبادة النهار','47','0','0');
+INSERT INTO [t11564] VALUES (9,'فوائد قيام الليل','50','0','0');
+INSERT INTO [t11564] VALUES (10,'آداب قيام الليل','52','0','0');
+INSERT INTO [t11564] VALUES (11,'الأمور الميسرة لقيام الليل','55','0','0');
