5cd992c6fa7da0dbd6114562f426b4bee14d2929
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:56 +0700

    Upload SQL chunk: t6344_chunk_0000.sql

---
+++ sql_chunks/t6344_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t6344
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t6344" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6344
+INSERT INTO [t6344] VALUES (1,'الاجتهاد من فروض الكفايات','1','0','0');
+INSERT INTO [t6344] VALUES (2,'فصل لا يجوز خلو الزمان عن مجتهد','4','0','0');
+INSERT INTO [t6344] VALUES (3,'اختيار ابن دقيق العيد','4','0','0');
+INSERT INTO [t6344] VALUES (4,'قول محب الدين والد ابن دقيق العيد','7','0','0');
+INSERT INTO [t6344] VALUES (5,'قول النووي','7','0','0');
+INSERT INTO [t6344] VALUES (6,'قول أبي طالب المكي','7','0','0');
+INSERT INTO [t6344] VALUES (7,'فصل شروط الاجتهاد','9','0','0');
+INSERT INTO [t6344] VALUES (8,'عند الرافعي والنووي','11','0','0');
+INSERT INTO [t6344] VALUES (9,'عند الغزالي','12','0','0');
+INSERT INTO [t6344] VALUES (10,'عند أبي منصور التميمي','14','0','0');
+INSERT INTO [t6344] VALUES (11,'عند الرازي والأرموي','15','0','0');
+INSERT INTO [t6344] VALUES (12,'عند ابن الصلاح','17','0','0');
+INSERT INTO [t6344] VALUES (13,'فصل شروط الاجتهاد عند السيوطي','18','0','0');
+INSERT INTO [t6344] VALUES (14,'فائدة دليل استكمال شرائط الاجتهاد','22','0','0');
+INSERT INTO [t6344] VALUES (15,'تنبيه','24','0','0');
+INSERT INTO [t6344] VALUES (16,'فصل الاتفاق على أن تقي الدين السبكي مجتهد مطلق','26','0','0');
+INSERT INTO [t6344] VALUES (17,'خاتمة كيفية الاجتهاد وترتيبه','27','0','0');
+INSERT INTO [t6344] VALUES (18,'فصل المجتهد مجدد للدين في كل قرن','29','0','0');
+INSERT INTO [t6344] VALUES (19,'فصل الاجتهاد لم ينقطع','33','0','0');
+INSERT INTO [t6344] VALUES (20,'فصل التحدث بنعمة الله','37','0','0');
+INSERT INTO [t6344] VALUES (21,'فصل رد السيوطي على معارضيه','39','0','0');
