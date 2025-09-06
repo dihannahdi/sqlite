10ff2c80b28877186d23560f8808ca1dcfe99af7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:40 +0700

    Upload SQL chunk: t7543_chunk_0000.sql

---
+++ sql_chunks/t7543_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t7543
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t7543" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7543
+INSERT INTO [t7543] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7543] VALUES (2,'أنواع المخدرات وأضرارها','4','0','0');
+INSERT INTO [t7543] VALUES (3,'الحشيشة وأضرارها','4','2','0');
+INSERT INTO [t7543] VALUES (4,'القات حكمه وأضراره','10','2','0');
+INSERT INTO [t7543] VALUES (5,'تعريف القات وأماكن زراعته','10','4','0');
+INSERT INTO [t7543] VALUES (6,'حكم استعمال القات','12','4','0');
+INSERT INTO [t7543] VALUES (7,'أدلة تحريم القات','14','4','0');
+INSERT INTO [t7543] VALUES (8,'أضرار ومفاسد القات','20','4','0');
+INSERT INTO [t7543] VALUES (9,'الأفيون ومشتقاته','25','2','0');
+INSERT INTO [t7543] VALUES (10,'الكوكائين وأضراره','29','2','0');
+INSERT INTO [t7543] VALUES (11,'جوزة الطيب وأضرارها','32','2','0');
+INSERT INTO [t7543] VALUES (12,'الخمر','34','0','0');
+INSERT INTO [t7543] VALUES (13,'الأضرار العامة للمخدرات','38','0','0');
+INSERT INTO [t7543] VALUES (14,'مصادر ومراجع','45','0','0');
