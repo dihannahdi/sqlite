d9cdc85b70614a6ebf88c5b64196583f0ad1b9d3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:11:44 +0700

    Upload SQL chunk: t37033_chunk_0000.sql

---
+++ sql_chunks/t37033_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t37033
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t37033" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37033
+INSERT INTO [t37033] VALUES (1,'كتاب الرياضة','1','0','0');
+INSERT INTO [t37033] VALUES (2,'تم كتاب الرياضة، بحمد الله ومنه','48','0','0');
