f5a222d4af56fbeffd0d3140b4611d57479dfaa7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:38 +0700

    Upload SQL chunk: t9874_chunk_0000.sql

---
+++ sql_chunks/t9874_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t9874
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t9874" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9874
+INSERT INTO [t9874] VALUES (1,'الفصل الأول تعريف وبيان','1','0','0');
+INSERT INTO [t9874] VALUES (2,'الفصل الثاني الجن مكلفون','29','0','0');
+INSERT INTO [t9874] VALUES (3,'الفصل الثالث العداء بين الإنسان والشيطان','38','0','0');
+INSERT INTO [t9874] VALUES (4,'الفصل الرابع','75','0','0');
+INSERT INTO [t9874] VALUES (5,'الفصل الخامس أسلحة المؤمن في حربه مع الشيطان','104','0','0');
+INSERT INTO [t9874] VALUES (6,'الفصل السادس الحكمة من خلق الشيطان','132','0','0');
