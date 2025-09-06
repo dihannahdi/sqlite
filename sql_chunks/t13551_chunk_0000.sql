4c63fdaed27e829815ee0a6e7473145c7080a5a2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:14:31 +0700

    Upload SQL chunk: t13551_chunk_0000.sql

---
+++ sql_chunks/t13551_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t13551
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t13551" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13551
+INSERT INTO [t13551] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t13551] VALUES (2,'مذكرات أحمد باي','5','0','0');
