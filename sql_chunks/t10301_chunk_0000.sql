fa8d0ce68c8ff9fcba3df593b15f3cf7e145dc62
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:54 +0700

    Upload SQL chunk: t10301_chunk_0000.sql

---
+++ sql_chunks/t10301_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10301
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10301" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10301
+INSERT INTO [t10301] VALUES (1,'التعجب من فعل المفعول بين المانعين والمجيزين','1','0','0');
+INSERT INTO [t10301] VALUES (2,'مصادر ومراجع','27','0','0');
