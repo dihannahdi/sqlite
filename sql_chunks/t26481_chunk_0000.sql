ef31271f5ece897625f6e593e07384313ef5b5d9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:47 +0700

    Upload SQL chunk: t26481_chunk_0000.sql

---
+++ sql_chunks/t26481_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26481
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26481" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26481
+INSERT INTO [t26481] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26481] VALUES (2,'ما قرب سنده من حديث أبي اقاسم السمرقندي','2','0','0');
