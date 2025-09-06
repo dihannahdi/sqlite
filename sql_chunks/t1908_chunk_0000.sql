b7021234612f5aafdea265ffea31047de12f62cc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:58 +0700

    Upload SQL chunk: t1908_chunk_0000.sql

---
+++ sql_chunks/t1908_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t1908
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t1908" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1908
+INSERT INTO [t1908] VALUES (1,'مفهوم ونطاق التكافل الاجتماعي','1','0','0');
+INSERT INTO [t1908] VALUES (2,'مجال التكافل الاجتماعي','4','0','0');
+INSERT INTO [t1908] VALUES (3,'التكافل بين المرء وذاته','4','2','0');
+INSERT INTO [t1908] VALUES (4,'التكافل داخل الأسرة','6','2','0');
+INSERT INTO [t1908] VALUES (5,'التكافل داخل الجماعة','12','2','0');
+INSERT INTO [t1908] VALUES (6,'مظاهر التكافل الاجتماعي','18','0','0');
+INSERT INTO [t1908] VALUES (7,'كفالة كبار السن','18','6','0');
+INSERT INTO [t1908] VALUES (8,'كفالة الصغار والأيتام','21','6','0');
+INSERT INTO [t1908] VALUES (9,'كفالة الفقراء والمساكين','24','6','0');
+INSERT INTO [t1908] VALUES (10,'رعاية حق الجار','27','6','0');
+INSERT INTO [t1908] VALUES (11,'حقوق الضيف والغريب','28','6','0');
+INSERT INTO [t1908] VALUES (12,'وسائل الإسلام في تحقيق التكافل','29','0','0');
+INSERT INTO [t1908] VALUES (13,'الوسائل المنوطة بأفراد المجتمع','29','12','0');
+INSERT INTO [t1908] VALUES (14,'الوسائل الفردية الإلزامية','30','12','0');
