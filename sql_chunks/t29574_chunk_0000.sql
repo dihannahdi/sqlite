76814535379fc3480c3048a5869b982f82309f33
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:27 +0700

    Upload SQL chunk: t29574_chunk_0000.sql

---
+++ sql_chunks/t29574_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t29574
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t29574" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29574
+INSERT INTO [t29574] VALUES (1,'مقدمة التحقيق','2','0','0');
+INSERT INTO [t29574] VALUES (2,'وصف النسخة الخطية','2','1','0');
+INSERT INTO [t29574] VALUES (3,'توثيق نسبة الكتاب','4','1','0');
+INSERT INTO [t29574] VALUES (4,'اسم الكتاب','6','1','0');
+INSERT INTO [t29574] VALUES (5,'فصل في الكلام على تفسير الفاتحة','10','0','0');
+INSERT INTO [t29574] VALUES (6,'الفصل الأول: في موضع نزولها','11','5','0');
+INSERT INTO [t29574] VALUES (7,'وفيه ثلاثة أقوال','11','6','0');
+INSERT INTO [t29574] VALUES (8,'الفصل الثاني: في عددها','14','5','0');
+INSERT INTO [t29574] VALUES (9,'الفصل الثالث: في أسمائها','16','5','0');
+INSERT INTO [t29574] VALUES (10,'الفصل الرابع في فضائلها وخصائصها','30','5','0');
+INSERT INTO [t29574] VALUES (11,'الفصل الخامس وهو: أحكام الفاتحة','44','5','0');
+INSERT INTO [t29574] VALUES (12,'فصل في الكلام على قوله: {الحمد لله رب العالمين}','53','0','0');
+INSERT INTO [t29574] VALUES (13,'الفصل الأول في الحمد','54','12','0');
