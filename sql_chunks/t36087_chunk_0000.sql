472ee727b19f8494c240c5e70f8e1d639fc118dd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:14 +0700

    Upload SQL chunk: t36087_chunk_0000.sql

---
+++ sql_chunks/t36087_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t36087
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t36087" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36087
+INSERT INTO [t36087] VALUES (1,'مقدمة الشارح','2','0','0');
+INSERT INTO [t36087] VALUES (2,'المبادئ العشرة لعلم النحو','3','0','0');
+INSERT INTO [t36087] VALUES (3,'مقدمة الناظم','4','0','0');
+INSERT INTO [t36087] VALUES (4,'باب الكلام','21','0','0');
+INSERT INTO [t36087] VALUES (5,'باب الإعراب','85','0','0');
+INSERT INTO [t36087] VALUES (6,'باب علامات الرفع','110','0','0');
+INSERT INTO [t36087] VALUES (7,'باب علامات النصب','157','0','0');
+INSERT INTO [t36087] VALUES (8,'باب علامات الخفض','174','0','0');
+INSERT INTO [t36087] VALUES (9,'باب علامات الجزم','207','0','0');
+INSERT INTO [t36087] VALUES (10,'باب قسمة الأفعال وأحكامها','224','0','0');
+INSERT INTO [t36087] VALUES (11,'باب نواصب المضارع','259','0','0');
+INSERT INTO [t36087] VALUES (12,'باب جوازم المضارع','283','0','0');
+INSERT INTO [t36087] VALUES (13,'المرفوعات من الأسماء','299','0','0');
+INSERT INTO [t36087] VALUES (14,'باب الفاعل','300','0','0');
+INSERT INTO [t36087] VALUES (15,'باب النائب عن الفاعل','316','0','0');
+INSERT INTO [t36087] VALUES (16,'باب المبتدا والخبر','325','0','0');
+INSERT INTO [t36087] VALUES (17,'باب كان وأخواتها','349','0','0');
+INSERT INTO [t36087] VALUES (18,'باب إن وأخواتها','369','0','0');
+INSERT INTO [t36087] VALUES (19,'باب ظن وأخواتها','385','0','0');
+INSERT INTO [t36087] VALUES (20,'التوابع','404','0','0');
+INSERT INTO [t36087] VALUES (21,'باب النعت','407','0','0');
+INSERT INTO [t36087] VALUES (22,'المعرفة والنكرة','424','0','0');
+INSERT INTO [t36087] VALUES (23,'باب العطف','444','0','0');
+INSERT INTO [t36087] VALUES (24,'باب التوكيد','465','0','0');
+INSERT INTO [t36087] VALUES (25,'باب البدل','480','0','0');
+INSERT INTO [t36087] VALUES (26,'المنصوبات من الأسماء','493','0','0');
+INSERT INTO [t36087] VALUES (27,'باب المفعول به','494','0','0');
+INSERT INTO [t36087] VALUES (28,'باب المفعول المطلق','505','0','0');
+INSERT INTO [t36087] VALUES (29,'باب الظرف','513','0','0');
+INSERT INTO [t36087] VALUES (30,'باب الحال','523','0','0');
+INSERT INTO [t36087] VALUES (31,'باب التمييز','543','0','0');
+INSERT INTO [t36087] VALUES (32,'باب الاستثناء','553','0','0');
+INSERT INTO [t36087] VALUES (33,'باب لا','578','0','0');
+INSERT INTO [t36087] VALUES (34,'باب المنادى','591','0','0');
+INSERT INTO [t36087] VALUES (35,'باب المفعول لأجله','609','0','0');
+INSERT INTO [t36087] VALUES (36,'باب المفعول معه','617','0','0');
+INSERT INTO [t36087] VALUES (37,'المخفوضات من الأسماء','628','0','0');
+INSERT INTO [t36087] VALUES (38,'باب الإضافة','630','0','0');
+INSERT INTO [t36087] VALUES (39,'خاتمة','644','0','0');
