657ad957782fe3d8379849bd17c5969890b82283
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:03 +0700

    Upload SQL chunk: t10325_chunk_0000.sql

---
+++ sql_chunks/t10325_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10325
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10325" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10325
+INSERT INTO [t10325] VALUES (1,'الثقافة التي نحتاج إليها','1','0','0');
+INSERT INTO [t10325] VALUES (2,'الدكتور محمد تقي الدين الهلالي: المدرس في الجامعة','1','0','0');
+INSERT INTO [t10325] VALUES (3,'لغة القرآن','12','0','0');
