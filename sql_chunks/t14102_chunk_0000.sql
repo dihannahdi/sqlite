92c93776637864a1122fd6057873e5d4f02eec34
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:35 +0700

    Upload SQL chunk: t14102_chunk_0000.sql

---
+++ sql_chunks/t14102_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t14102
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t14102" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14102
+INSERT INTO [t14102] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t14102] VALUES (2,'نص الحديث','3','0','0');
+INSERT INTO [t14102] VALUES (3,'تخريجه','7','0','0');
+INSERT INTO [t14102] VALUES (4,'الكلام على رفعه ووقفه','9','0','0');
+INSERT INTO [t14102] VALUES (5,'أسماؤهن','11','0','0');
+INSERT INTO [t14102] VALUES (6,'قول الأولى','12','0','0');
+INSERT INTO [t14102] VALUES (7,'قول الثانية','15','0','0');
+INSERT INTO [t14102] VALUES (8,'قول الثالثة','18','0','0');
+INSERT INTO [t14102] VALUES (9,'قول الرابعة','20','0','0');
+INSERT INTO [t14102] VALUES (10,'قول الخامسة','22','0','0');
+INSERT INTO [t14102] VALUES (11,'قول السادسة','24','0','0');
+INSERT INTO [t14102] VALUES (12,'قول السابعة','27','0','0');
+INSERT INTO [t14102] VALUES (13,'قول الثامنة','30','0','0');
+INSERT INTO [t14102] VALUES (14,'قول التاسعة','32','0','0');
+INSERT INTO [t14102] VALUES (15,'قول العاشرة','34','0','0');
+INSERT INTO [t14102] VALUES (16,'قول الحادية عشرة','37','0','0');
+INSERT INTO [t14102] VALUES (17,'كلام النبي صلى الله عليه وسلم لعائشة','56','0','0');
+INSERT INTO [t14102] VALUES (18,'فوائد الحديث','57','0','0');
