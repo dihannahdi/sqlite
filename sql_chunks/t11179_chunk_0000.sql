9a82b67a4e5d2ecfa084b4a92b43af711ef81299
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:01 +0700

    Upload SQL chunk: t11179_chunk_0000.sql

---
+++ sql_chunks/t11179_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t11179
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t11179" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11179
+INSERT INTO [t11179] VALUES (1,'أصول الإيمان','1','0','0');
+INSERT INTO [t11179] VALUES (2,'وهي أصول ستة','1','0','0');
+INSERT INTO [t11179] VALUES (3,'فبين سبحانه وتعالى هنا أربعة أصول','1','0','0');
+INSERT INTO [t11179] VALUES (4,'ومما جاء في السنة عن رسول الله صلى الله عليه وسلم','2','0','0');
