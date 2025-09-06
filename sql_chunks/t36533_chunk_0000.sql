298d0b526a51c728aad9dcb937643da92c42f2d9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:51 +0700

    Upload SQL chunk: t36533_chunk_0000.sql

---
+++ sql_chunks/t36533_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t36533
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t36533" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36533
+INSERT INTO [t36533] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t36533] VALUES (2,'معنى الصحبة وما يرادفها من الألفاظ','6','0','0');
+INSERT INTO [t36533] VALUES (3,'فضل الصحبة والأخوة','12','0','0');
+INSERT INTO [t36533] VALUES (4,'مراتب الصحبة وأسبابها','23','0','0');
+INSERT INTO [t36533] VALUES (5,'مقامات الإخوان ومراتبهم','35','0','0');
+INSERT INTO [t36533] VALUES (6,'فيمن لا ترجى عشرته من الأشرار، ومن تؤثر صحبته من الأخيار','45','0','0');
+INSERT INTO [t36533] VALUES (7,'حقوق الصحبة وآدابها ظاهرا وباطنا','74','0','0');
