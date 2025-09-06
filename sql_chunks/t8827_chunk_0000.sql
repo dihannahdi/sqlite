787c7882973a7445a16a8d2f31538553fd813301
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:19 +0700

    Upload SQL chunk: t8827_chunk_0000.sql

---
+++ sql_chunks/t8827_chunk_0000.sql
@@ -0,0 +1,48 @@
+-- Table: t8827
+-- Chunk: 1
+-- Rows: 41
+
+CREATE TABLE "t8827" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8827
+INSERT INTO [t8827] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t8827] VALUES (2,'الحديث الأول','2','0','0');
+INSERT INTO [t8827] VALUES (3,'الحديث الثاني','3','0','0');
+INSERT INTO [t8827] VALUES (4,'الحديث الثالث','4','0','0');
+INSERT INTO [t8827] VALUES (5,'الحديث الرابع','5','0','0');
+INSERT INTO [t8827] VALUES (6,'الحديث الخامس','7','0','0');
+INSERT INTO [t8827] VALUES (7,'الحديث السادس','9','0','0');
+INSERT INTO [t8827] VALUES (8,'الحديث السابع','11','0','0');
+INSERT INTO [t8827] VALUES (9,'الحديث الثامن','12','0','0');
+INSERT INTO [t8827] VALUES (10,'الحديث التاسع','13','0','0');
+INSERT INTO [t8827] VALUES (11,'الحديث العاشر','14','0','0');
+INSERT INTO [t8827] VALUES (12,'الحديث الحادي عشر','15','0','0');
+INSERT INTO [t8827] VALUES (13,'الحديث الثاني عشر','16','0','0');
+INSERT INTO [t8827] VALUES (14,'الحديث الثالث عشر','17','0','0');
+INSERT INTO [t8827] VALUES (15,'الحديث الرابع عشر','18','0','0');
+INSERT INTO [t8827] VALUES (16,'الحديث الخامس عشر','20','0','0');
+INSERT INTO [t8827] VALUES (17,'الحديث السادس عشر','22','0','0');
+INSERT INTO [t8827] VALUES (18,'الحديث السابع عشر','24','0','0');
+INSERT INTO [t8827] VALUES (19,'الحديث الثامن عشر','26','0','0');
+INSERT INTO [t8827] VALUES (20,'الحديث التاسع عشر','28','0','0');
+INSERT INTO [t8827] VALUES (21,'الحديث العشرون','30','0','0');
+INSERT INTO [t8827] VALUES (22,'الحديث الحادي والعشرون','32','0','0');
+INSERT INTO [t8827] VALUES (23,'الحديث الثاني والعشرون','34','0','0');
+INSERT INTO [t8827] VALUES (24,'الحديث الثالث والعشرون','35','0','0');
+INSERT INTO [t8827] VALUES (25,'الحديث الرابع والعشرون','36','0','0');
+INSERT INTO [t8827] VALUES (26,'الحديث الخامس والعشرون','37','0','0');
+INSERT INTO [t8827] VALUES (27,'الحديث السادس والعشرون','38','0','0');
+INSERT INTO [t8827] VALUES (28,'الحديث السابع والعشرون','39','0','0');
+INSERT INTO [t8827] VALUES (29,'الحديث الثامن والعشرون','40','0','0');
+INSERT INTO [t8827] VALUES (30,'الحديث التاسع والعشرون','41','0','0');
+INSERT INTO [t8827] VALUES (31,'الحديث الثلاثون','42','0','0');
+INSERT INTO [t8827] VALUES (32,'الحديث الحادي والثلاثون','43','0','0');
+INSERT INTO [t8827] VALUES (33,'الحديث الثاني والثلاثون','44','0','0');
+INSERT INTO [t8827] VALUES (34,'الحديث الثالث والثلاثون','45','0','0');
+INSERT INTO [t8827] VALUES (35,'الحديث الرابع والثلاثون','46','0','0');
+INSERT INTO [t8827] VALUES (36,'الحديث الخامس والثلاثون','47','0','0');
+INSERT INTO [t8827] VALUES (37,'الحديث السادس والثلاثون','48','0','0');
+INSERT INTO [t8827] VALUES (38,'الحديث السابع والثلاثون','49','0','0');
+INSERT INTO [t8827] VALUES (39,'الحديث الثامن والثلاثون','50','0','0');
+INSERT INTO [t8827] VALUES (40,'الحديث التاسع والثلاثون','51','0','0');
+INSERT INTO [t8827] VALUES (41,'الحديث الأربعون','52','0','0');
