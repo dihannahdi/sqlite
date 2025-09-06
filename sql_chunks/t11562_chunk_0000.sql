6671b1738edd13f329b4ed61d3dbb10b508cf0e7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:26 +0700

    Upload SQL chunk: t11562_chunk_0000.sql

---
+++ sql_chunks/t11562_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t11562
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t11562" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11562
+INSERT INTO [t11562] VALUES (1,'القسم الأول: الدراسة','2','0','0');
+INSERT INTO [t11562] VALUES (2,'أولا: المؤلف','3','1','0');
+INSERT INTO [t11562] VALUES (3,'ثانيا: الكتاب','6','1','0');
+INSERT INTO [t11562] VALUES (4,'ثالثا: معالم التحقيق','10','1','0');
+INSERT INTO [t11562] VALUES (5,'القسم الثاني: كتاب المفتاح في الصرف','19','0','0');
+INSERT INTO [t11562] VALUES (6,'خطبة الكتاب','22','5','0');
+INSERT INTO [t11562] VALUES (7,'التصريف','22','5','0');
+INSERT INTO [t11562] VALUES (8,'أبنية الأسماء','25','5','0');
+INSERT INTO [t11562] VALUES (9,'أبنية الأفعال','32','5','0');
+INSERT INTO [t11562] VALUES (10,'فصل: المضاعف','35','9','0');
+INSERT INTO [t11562] VALUES (11,'فصل: المهموز','36','9','0');
+INSERT INTO [t11562] VALUES (12,'فصل: المثال','36','9','0');
+INSERT INTO [t11562] VALUES (13,'فصل: الأجوف','37','9','0');
+INSERT INTO [t11562] VALUES (14,'فصل: الناقص','38','9','0');
+INSERT INTO [t11562] VALUES (15,'فصل:اللفيف','38','9','0');
+INSERT INTO [t11562] VALUES (16,'الأفعال المنشعبة','40','5','0');
+INSERT INTO [t11562] VALUES (17,'المعاني في الأفعال','44','5','0');
+INSERT INTO [t11562] VALUES (18,'المصدر','48','5','0');
+INSERT INTO [t11562] VALUES (19,'الفعل','49','5','0');
+INSERT INTO [t11562] VALUES (20,'الاشتقاق','58','5','0');
+INSERT INTO [t11562] VALUES (21,'أبنية المصادر','59','5','0');
+INSERT INTO [t11562] VALUES (22,'الأمثلة','63','5','0');
+INSERT INTO [t11562] VALUES (23,'الزيادة','82','5','0');
+INSERT INTO [t11562] VALUES (24,'الإبدال','88','5','0');
+INSERT INTO [t11562] VALUES (25,'الحذف','96','5','0');
+INSERT INTO [t11562] VALUES (26,'العقد','100','5','0');
+INSERT INTO [t11562] VALUES (27,'القسم الثالث: المسارد الفنية','108','0','0');
