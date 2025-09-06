d71944709cf0d0c98adfec74a2795c10f9494d1a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:18 +0700

    Upload SQL chunk: t26545_chunk_0000.sql

---
+++ sql_chunks/t26545_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26545
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26545" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26545
+INSERT INTO [t26545] VALUES (1,'منتقى حديث أبي نعيم الأزهري','1','0','0');
