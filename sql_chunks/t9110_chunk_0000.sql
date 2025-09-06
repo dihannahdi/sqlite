f3afa82fe1b5710abe2abe2ba1ad04b9759ffce1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:51:10 +0700

    Upload SQL chunk: t9110_chunk_0000.sql

---
+++ sql_chunks/t9110_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t9110
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t9110" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9110
+INSERT INTO [t9110] VALUES (1,'شريعة القرآن من دلائل إعجازه','2','0','0');
+INSERT INTO [t9110] VALUES (2,'تمهيد','2','0','0');
+INSERT INTO [t9110] VALUES (3,'ولم يعتبر الإسلام الزكاة إحسانا مذلا','10','0','0');
+INSERT INTO [t9110] VALUES (4,'موازنة','13','0','0');
+INSERT INTO [t9110] VALUES (5,'ونحن وإذا حاولنا أن نبين فضل الشريعة في الزواج والطلاق','16','0','0');
+INSERT INTO [t9110] VALUES (6,'أما تعدد الزوجات','23','0','0');
+INSERT INTO [t9110] VALUES (7,'إن دراسة الطلاق الذي جاء به الإسلام','27','0','0');
+INSERT INTO [t9110] VALUES (8,'إن الشريعة الإسلامية أتت بنظام في الميراث لم تسبق بمثله','41','0','0');
+INSERT INTO [t9110] VALUES (9,'حكم القرآن.... والحريات','48','0','0');
+INSERT INTO [t9110] VALUES (10,'والإسلام حرم الخنزير واعتبره رجسا','51','0','0');
+INSERT INTO [t9110] VALUES (11,'وإذا كانت شريعة القرآن كفلت حرية التدين لمخالفيه','61','0','0');
+INSERT INTO [t9110] VALUES (12,'دعائم ثلاث','64','0','0');
+INSERT INTO [t9110] VALUES (13,'أما الشورى','88','0','0');
+INSERT INTO [t9110] VALUES (14,'وبعد','90','0','0');
