a8a4a48ab2384fadd1e7e90fc3dc87217a090d48
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:15 +0700

    Upload SQL chunk: t11429_chunk_0000.sql

---
+++ sql_chunks/t11429_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11429
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11429" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11429
+INSERT INTO [t11429] VALUES (1,'تأخير الظلامة للسيوطي - مخطوط','2','0','0');
