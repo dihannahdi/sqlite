318865ba7611eea6e4c59e97d0e6c8eda650af16
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:56 +0700

    Upload SQL chunk: t149875_chunk_0000.sql

---
+++ sql_chunks/t149875_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t149875
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t149875" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t149875
+INSERT INTO [t149875] VALUES (1,'قصائد الديوان','37','0','0');
+INSERT INTO [t149875] VALUES (2,'قافية الهمزة','38','1','0');
+INSERT INTO [t149875] VALUES (3,'قافية الباء','39','1','0');
+INSERT INTO [t149875] VALUES (4,'قافية الجيم','41','1','0');
+INSERT INTO [t149875] VALUES (5,'قافية الدال','43','1','0');
+INSERT INTO [t149875] VALUES (6,'قافية السين','44','1','0');
+INSERT INTO [t149875] VALUES (7,'قافية الشين','45','1','0');
+INSERT INTO [t149875] VALUES (8,'قافية العين','46','1','0');
+INSERT INTO [t149875] VALUES (9,'قافية القاف','47','1','0');
+INSERT INTO [t149875] VALUES (10,'قافية الكاف','51','1','0');
+INSERT INTO [t149875] VALUES (11,'قافية اللام','52','1','0');
+INSERT INTO [t149875] VALUES (12,'قافية الميم','53','1','0');
+INSERT INTO [t149875] VALUES (13,'قافية النون','59','1','0');
+INSERT INTO [t149875] VALUES (14,'الشعر المنسوب للحارث بن حلزة ولغيره من الشعراء','62','0','0');
