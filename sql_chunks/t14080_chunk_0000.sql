211ced0cac8a0cb92b0911d1f8ec082c7f3b8ab6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:29 +0700

    Upload SQL chunk: t14080_chunk_0000.sql

---
+++ sql_chunks/t14080_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t14080
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t14080" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14080
+INSERT INTO [t14080] VALUES (1,'فصل: في فن آخر من السؤال','23','0','0');
+INSERT INTO [t14080] VALUES (2,'فصل: في الذي يلزم القائلين بالصرفة','31','0','0');
+INSERT INTO [t14080] VALUES (3,'فصل','42','0','0');
