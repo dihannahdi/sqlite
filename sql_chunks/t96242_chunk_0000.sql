86ba9b323b211733df16b5737221404ca34945c4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:28 +0700

    Upload SQL chunk: t96242_chunk_0000.sql

---
+++ sql_chunks/t96242_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t96242
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t96242" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96242
+INSERT INTO [t96242] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t96242] VALUES (2,'أحكام الألفاظ','1','0','0');
+INSERT INTO [t96242] VALUES (3,'أحكام الطلاق','6','0','0');
+INSERT INTO [t96242] VALUES (4,'أحكام الأيمان','10','0','0');
+INSERT INTO [t96242] VALUES (5,'أحكام النذور','13','0','0');
