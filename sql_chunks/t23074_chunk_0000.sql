f58791eb9972ff2c2f95a65e8e7ce94f9b2ee311
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:42 +0700

    Upload SQL chunk: t23074_chunk_0000.sql

---
+++ sql_chunks/t23074_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t23074
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t23074" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23074
+INSERT INTO [t23074] VALUES (1,'حكم صلاة الجماعة','1','0','0');
+INSERT INTO [t23074] VALUES (2,'كيف تحصل فضيلة الجماعة؟','10','0','0');
+INSERT INTO [t23074] VALUES (3,'العدد الصحيح لمضاعفة فضل صلاة الجماعة على الفذ','13','0','0');
+INSERT INTO [t23074] VALUES (4,'الرخصة في إعادة الجماعة لمن صلاها منفردا','15','0','0');
+INSERT INTO [t23074] VALUES (5,'مذاهب العلماء في الصلوات التي تجوز إعادتها','22','0','0');
+INSERT INTO [t23074] VALUES (6,'المسألة الثانية: أي الصلاتين تكون مكتوبة؟','27','0','0');
+INSERT INTO [t23074] VALUES (7,'حضور النساء في الجماعة','30','0','0');
