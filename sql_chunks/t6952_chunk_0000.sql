8fa45e8e634b5818e016f8d3b8e538eec2cf24ff
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:08 +0700

    Upload SQL chunk: t6952_chunk_0000.sql

---
+++ sql_chunks/t6952_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t6952
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t6952" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6952
+INSERT INTO [t6952] VALUES (1,'الباب الأول','4','0','0');
+INSERT INTO [t6952] VALUES (2,'فيما ذكر عن الرجال','4','0','0');
+INSERT INTO [t6952] VALUES (3,'القسم الأول','5','0','0');
+INSERT INTO [t6952] VALUES (4,'فيما يروى عن الأنبياء عليهم السلام','5','0','0');
+INSERT INTO [t6952] VALUES (5,'القسم الثاني','9','0','0');
+INSERT INTO [t6952] VALUES (6,'فيما يروى عن الصحابة','9','0','0');
+INSERT INTO [t6952] VALUES (7,'القسم الثالث','18','0','0');
+INSERT INTO [t6952] VALUES (8,'فيما يروى عن العلماء والحكماء','18','0','0');
+INSERT INTO [t6952] VALUES (9,'القسم الرابع','67','0','0');
+INSERT INTO [t6952] VALUES (10,'فيما يروى من ذلك عن العرب','67','0','0');
+INSERT INTO [t6952] VALUES (11,'القسم الخامس','76','0','0');
+INSERT INTO [t6952] VALUES (12,'ما يروي عن العوام','76','0','0');
+INSERT INTO [t6952] VALUES (13,'الباب الثاني','101','0','0');
+INSERT INTO [t6952] VALUES (14,'فيما يذكر عن النساء من ذلك','101','0','0');
+INSERT INTO [t6952] VALUES (15,'الباب الثالث','113','0','0');
+INSERT INTO [t6952] VALUES (16,'فيما ذكر عن الصبيان من ذلك','113','0','0');
