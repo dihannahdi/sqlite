2e57d633b93578d70c6c0e11f3b2d805c19004a7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:22 +0700

    Upload SQL chunk: t30877_chunk_0000.sql

---
+++ sql_chunks/t30877_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t30877
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t30877" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30877
+INSERT INTO [t30877] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t30877] VALUES (2,'طريقا تعلم العقيدة','2','0','0');
+INSERT INTO [t30877] VALUES (3,'الطريق الأول هو الخبر المجرد','2','2','0');
+INSERT INTO [t30877] VALUES (4,'الطريق الثاني تعليم العقيدة من خلال الشبهات','3','2','0');
+INSERT INTO [t30877] VALUES (5,'القضايا الكلية للاعتقاد في الكتاب والسنة','4','0','0');
+INSERT INTO [t30877] VALUES (6,'أولا الإيمان','5','5','0');
+INSERT INTO [t30877] VALUES (7,'حكمة الخلق','6','6','0');
+INSERT INTO [t30877] VALUES (8,'توحيد الألوهية القصد والطلب','7','6','0');
+INSERT INTO [t30877] VALUES (9,'الإيمان بالملائكة','8','6','0');
+INSERT INTO [t30877] VALUES (10,'الإيمان بكتب الله','9','6','0');
+INSERT INTO [t30877] VALUES (11,'الإيمان برسل الله','10','6','0');
+INSERT INTO [t30877] VALUES (12,'الإيمان باليوم الآخر','11','6','0');
+INSERT INTO [t30877] VALUES (13,'ثانيا الأمة الإسلامية','12','5','0');
+INSERT INTO [t30877] VALUES (14,'ثالثا أصول الدعوة إلى الله سبحانه وتعالى','13','5','0');
+INSERT INTO [t30877] VALUES (15,'رابعا الموقف من غير المسلمين','14','5','0');
+INSERT INTO [t30877] VALUES (16,'خامسا أصول الفقه','15','5','0');
