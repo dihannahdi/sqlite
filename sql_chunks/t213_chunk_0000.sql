e6563925b26e5538d4aa9ffe8f57036d545afb0e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:20 +0700

    Upload SQL chunk: t213_chunk_0000.sql

---
+++ sql_chunks/t213_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t213
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t213" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t213
+INSERT INTO [t213] VALUES (1,'بسم الله الرحمن الرحيم وحسبنا الله ونعم الوكيل','1','0','0');
+INSERT INTO [t213] VALUES (2,'الكلام على السبع الأول المشهورة','1','0','0');
+INSERT INTO [t213] VALUES (3,'الكلام على السبعة الثانية','1','0','0');
+INSERT INTO [t213] VALUES (4,'الكلام على السبعتين اللتين زادهما الحافظ أبو الفضل ابن حجر رحمه الله تعالى، وفي أحاديثها','2','0','0');
+INSERT INTO [t213] VALUES (5,'الكلام على الخصال التي زدتها','3','0','0');
+INSERT INTO [t213] VALUES (6,'ثم وجدت خصلات أخرى','4','0','0');
+INSERT INTO [t213] VALUES (7,'ثم وجدت سبعة أخرى. فكملت سبعين','5','0','0');
