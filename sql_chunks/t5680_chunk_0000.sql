e26a033521bab61cc75cb70060e547614da18ab0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:25 +0700

    Upload SQL chunk: t5680_chunk_0000.sql

---
+++ sql_chunks/t5680_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t5680
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t5680" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5680
+INSERT INTO [t5680] VALUES (1,'حكم سباب المسلم وقتاله','5','0','0');
+INSERT INTO [t5680] VALUES (2,'أثقل ما يوضع في الميزان','8','0','0');
+INSERT INTO [t5680] VALUES (3,'عدم مشروعية الصدقة للنبي وأهله','10','0','0');
+INSERT INTO [t5680] VALUES (4,'رجال ملعونون','12','0','0');
+INSERT INTO [t5680] VALUES (5,'فضل الصبر على شدة المدينة','15','0','0');
+INSERT INTO [t5680] VALUES (6,'يحرم من الرضاعة ما يحرم من النسب','18','0','0');
+INSERT INTO [t5680] VALUES (7,'ما يقطع الصلاة','21','0','0');
+INSERT INTO [t5680] VALUES (8,'من مناقب الرسول صلى الله عليه وسلم','24','0','0');
+INSERT INTO [t5680] VALUES (9,'ما يمنع قراءة القرآن','26','0','0');
+INSERT INTO [t5680] VALUES (10,'دعاء الصباح والمساء','27','0','0');
+INSERT INTO [t5680] VALUES (11,'من شعر ابنة الدولابي','30','0','0');
