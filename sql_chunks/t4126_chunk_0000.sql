7ee6a5668f0fbe6beef21f7c281cacd2ba9a9909
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:05 +0700

    Upload SQL chunk: t4126_chunk_0000.sql

---
+++ sql_chunks/t4126_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t4126
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t4126" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4126
+INSERT INTO [t4126] VALUES (1,'المراح في المزاح','1','0','0');
+INSERT INTO [t4126] VALUES (2,'أنهاه تسويدا جامعه','83','0','0');
