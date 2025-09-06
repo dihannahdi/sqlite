7af5eb71405c852aa2ff5b12076dc8bdf16ac1a9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:25 +0700

    Upload SQL chunk: t31227_chunk_0000.sql

---
+++ sql_chunks/t31227_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t31227
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t31227" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31227
+INSERT INTO [t31227] VALUES (1,'إسباغ الوضوء','1','0','0');
+INSERT INTO [t31227] VALUES (2,'التوجه إلى القبلة','4','0','0');
+INSERT INTO [t31227] VALUES (3,'تكبيرة الإحرام ورفع اليدين عند التكبير ووضع اليدين على الصدر','5','0','0');
+INSERT INTO [t31227] VALUES (4,'دعاء الاستفتاح','5','0','0');
+INSERT INTO [t31227] VALUES (5,'الركوع والرفع منه وما يشتمل عليه','7','0','0');
+INSERT INTO [t31227] VALUES (6,'السجود والرفع منه وما يشتمل عليه','10','0','0');
+INSERT INTO [t31227] VALUES (7,'الجلوس بين السجدتين وكيفيته','12','0','0');
+INSERT INTO [t31227] VALUES (8,'الجلوس للتشهد في الصلاة الثنائية وكيفيته','13','0','0');
+INSERT INTO [t31227] VALUES (9,'الجلوس للتشهد في الصلاة الثلاثية أو الرباعية وكيفيته','17','0','0');
