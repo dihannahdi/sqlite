92aef2bb03106861284d84c6784130864c251a10
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:24:42 +0700

    Upload SQL chunk: t151182_chunk_0000.sql

---
+++ sql_chunks/t151182_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t151182
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t151182" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t151182
+INSERT INTO [t151182] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t151182] VALUES (2,'الصراط ومعانيه في القرآن الكريم','4','0','0');
+INSERT INTO [t151182] VALUES (3,'أ - تعريف الصراط وصفته','5','0','0');
+INSERT INTO [t151182] VALUES (4,'ب - ذكر بعض الأحاديث التي ورد فيها ذِكر الصراط وأوصافه، وأحوال المارين عليه','5','0','0');
+INSERT INTO [t151182] VALUES (5,'ت - حكم الإيمان بالصراط عند أهل السنة والجماعة','7','0','0');
+INSERT INTO [t151182] VALUES (6,'الخلاصة','9','0','0');
