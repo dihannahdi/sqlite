2919e1421479bc1621cbbf29db9692ef828661a8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:22 +0700

    Upload SQL chunk: t30310_chunk_0000.sql

---
+++ sql_chunks/t30310_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30310
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30310" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30310
+INSERT INTO [t30310] VALUES (1,'نص جزء يزيد بن أبي حبيب:','1','0','0');
