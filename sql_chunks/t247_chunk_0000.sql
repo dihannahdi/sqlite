fda3f02fd2a9d19bf1d0fddd22390fbff875b351
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:26 +0700

    Upload SQL chunk: t247_chunk_0000.sql

---
+++ sql_chunks/t247_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t247
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t247" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t247
+INSERT INTO [t247] VALUES (1,'الآيات البينات','1','0','0');
