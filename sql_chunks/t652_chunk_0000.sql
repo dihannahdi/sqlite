b78d7c635c124443ff902a2938f801490528db81
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:23 +0700

    Upload SQL chunk: t652_chunk_0000.sql

---
+++ sql_chunks/t652_chunk_0000.sql
@@ -0,0 +1,52 @@
+-- Table: t652
+-- Chunk: 1
+-- Rows: 45
+
+CREATE TABLE "t652" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t652
+INSERT INTO [t652] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t652] VALUES (2,'باب العقل والهوى','1','0','0');
+INSERT INTO [t652] VALUES (3,'أعوان العقل خمسون','1','2','0');
+INSERT INTO [t652] VALUES (4,'تفسير العقل وضده الهوى','1','0','0');
+INSERT INTO [t652] VALUES (5,'تفسير العقل','1','0','0');
+INSERT INTO [t652] VALUES (6,'تفسير الفهم وضده الوهم','1','0','0');
+INSERT INTO [t652] VALUES (7,'باب تفسير الفهم','1','0','0');
+INSERT INTO [t652] VALUES (8,'تفسير البصر وضده العصيان','1','0','0');
+INSERT INTO [t652] VALUES (9,'باب تفسير البصر','1','0','0');
+INSERT INTO [t652] VALUES (10,'تفسير المعرفة','2','0','0');
+INSERT INTO [t652] VALUES (11,'باب تفسير المعرفة','2','0','0');
+INSERT INTO [t652] VALUES (12,'تفسير اليقين وضده الشك والتحير','2','0','0');
+INSERT INTO [t652] VALUES (13,'باب تفسير اليقين','2','0','0');
+INSERT INTO [t652] VALUES (14,'علامات المؤمن ثلاث','2','0','0');
+INSERT INTO [t652] VALUES (15,'تفسير الفقه وضده التحير','2','0','0');
+INSERT INTO [t652] VALUES (16,'باب تفسير الفقه','2','0','0');
+INSERT INTO [t652] VALUES (17,'تفسير الوقف وضده التعجيل','2','0','0');
+INSERT INTO [t652] VALUES (18,'باب تفسير الوقف','2','0','0');
+INSERT INTO [t652] VALUES (19,'علامات الواقف ثلاث','2','0','0');
+INSERT INTO [t652] VALUES (20,'تفسير الحلم وضده الحدة','3','0','0');
+INSERT INTO [t652] VALUES (21,'باب تفسير الحلم','3','0','0');
+INSERT INTO [t652] VALUES (22,'تفسير الإلهام وضده الوسوسة','3','0','0');
+INSERT INTO [t652] VALUES (23,'تفسير الإلهام','3','0','0');
+INSERT INTO [t652] VALUES (24,'تفسير الإخلاص وضده الرياء','3','0','0');
+INSERT INTO [t652] VALUES (25,'تفسير الإخلاص','3','0','0');
+INSERT INTO [t652] VALUES (26,'تفسير التواضع وضده التكبر والبطر','4','0','0');
+INSERT INTO [t652] VALUES (27,'تفسير التواضع','4','0','0');
+INSERT INTO [t652] VALUES (28,'تفسير السخاوة وضدها البخل','4','0','0');
+INSERT INTO [t652] VALUES (29,'تفسير السخاوة','4','0','0');
+INSERT INTO [t652] VALUES (30,'تفسير الصواب وضده الخطأ','5','0','0');
+INSERT INTO [t652] VALUES (31,'تفسير الصواب','5','0','0');
+INSERT INTO [t652] VALUES (32,'تفسير النصيحة وضدها الحسد','5','0','0');
+INSERT INTO [t652] VALUES (33,'تفسير النصيحة','5','0','0');
+INSERT INTO [t652] VALUES (34,'تفسير الحسبة وضدها الاستغناء عن البر','5','0','0');
+INSERT INTO [t652] VALUES (35,'تفسير الحسبة','5','0','0');
+INSERT INTO [t652] VALUES (36,'تفسير النية فضد نية الخير نية الشر','6','0','0');
+INSERT INTO [t652] VALUES (37,'تفسير النية','6','0','0');
+INSERT INTO [t652] VALUES (38,'تفسير الشفقة وضدها العداوة','6','0','0');
+INSERT INTO [t652] VALUES (39,'تفسير الشفقة','6','0','0');
+INSERT INTO [t652] VALUES (40,'تفسير المداراة وضدها المداهنة','6','0','0');
+INSERT INTO [t652] VALUES (41,'تفسير المداراة','6','0','0');
+INSERT INTO [t652] VALUES (42,'تفسير الورع وضده الإقبال','6','0','0');
+INSERT INTO [t652] VALUES (43,'تفسير الورع','6','0','0');
+INSERT INTO [t652] VALUES (44,'تفسير الشكر وضده كفر النعمة','7','0','0');
+INSERT INTO [t652] VALUES (45,'تفسير الشكر','7','0','0');
