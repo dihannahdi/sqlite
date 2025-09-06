ab6d06e9efffe1c6f3fdffe0708e88a71024ead2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:30 +0700

    Upload SQL chunk: t11291_chunk_0000.sql

---
+++ sql_chunks/t11291_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t11291
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t11291" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11291
+INSERT INTO [t11291] VALUES (1,'أدلة من الكتاب والسنة على فضل الصحابة وعظم منزلتهم','1','0','0');
+INSERT INTO [t11291] VALUES (2,'الصحابة كلهم عدول','11','0','0');
+INSERT INTO [t11291] VALUES (3,'ملخص عقيدة أهل السنة والجماعة في الصحابة رضي الله عنهم:','14','0','0');
+INSERT INTO [t11291] VALUES (4,'من أقوال أئمة السلف في الصحابة','16','0','0');
+INSERT INTO [t11291] VALUES (5,'القدح في الصحابة قدح في الكتاب والسنة','23','0','0');
+INSERT INTO [t11291] VALUES (6,'القدح في الصحابة لا يضرهم','24','0','0');
