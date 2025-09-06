a33f5f55d3ebd084fe1028b0ceda5479743bb0c8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:18 +0700

    Upload SQL chunk: t21691_chunk_0000.sql

---
+++ sql_chunks/t21691_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21691
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21691" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21691
+INSERT INTO [t21691] VALUES (1,'من حديث الفقيه أبي القاسم الشهرزوري عن شيوخه','2','0','0');
