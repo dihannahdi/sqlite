80a8915ab0a2368d222775f2285ae73ee9fe88b8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:51 +0700

    Upload SQL chunk: t5837_chunk_0000.sql

---
+++ sql_chunks/t5837_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5837
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5837" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5837
+INSERT INTO [t5837] VALUES (1,'المنفردات للنسائي','230','0','0');
+INSERT INTO [t5837] VALUES (2,'تسمية من لم يرو عنه غير رجل واحد للنسائي','230','0','0');
