3ca623931ea65834926c73845c9a01a287b9d6e6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:12 +0700

    Upload SQL chunk: t1920_chunk_0000.sql

---
+++ sql_chunks/t1920_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t1920
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t1920" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1920
+INSERT INTO [t1920] VALUES (1,'مفهوم القضاء في الإسلام','1','0','0');
+INSERT INTO [t1920] VALUES (2,'مشروعية القضاء','2','0','0');
+INSERT INTO [t1920] VALUES (3,'الحكمة من مشروعية القضاء','5','0','0');
+INSERT INTO [t1920] VALUES (4,'حكم القضاء','6','0','0');
+INSERT INTO [t1920] VALUES (5,'الشروط الواجب توفرها في القاضي','9','0','0');
+INSERT INTO [t1920] VALUES (6,'أولا البلوغ','9','5','0');
+INSERT INTO [t1920] VALUES (7,'ثانيا العقل','10','5','0');
+INSERT INTO [t1920] VALUES (8,'ثالثا الحرية','11','5','0');
+INSERT INTO [t1920] VALUES (9,'رابعا الإسلام','12','5','0');
+INSERT INTO [t1920] VALUES (10,'خامسا الذكورة','14','5','0');
+INSERT INTO [t1920] VALUES (11,'سادسا العدالة','17','5','0');
+INSERT INTO [t1920] VALUES (12,'سابعا الاجتهاد','18','5','0');
+INSERT INTO [t1920] VALUES (13,'ثامنا سلامة الحواس','19','5','0');
+INSERT INTO [t1920] VALUES (14,'أعوان القاضي استقلال القضاء','21','0','0');
+INSERT INTO [t1920] VALUES (15,'استقلال القاضي عن السلطة التنفيذية','25','0','0');
+INSERT INTO [t1920] VALUES (16,'انتهاء ولاية القاضي وأسبابها','34','0','0');
+INSERT INTO [t1920] VALUES (17,'الدعوى والإثبات في الإسلام','36','0','0');
+INSERT INTO [t1920] VALUES (18,'تمهيد','36','17','0');
+INSERT INTO [t1920] VALUES (19,'الدعوى وشروط قبولها','39','17','0');
+INSERT INTO [t1920] VALUES (20,'مجلس القضاء وآدابه','40','17','0');
+INSERT INTO [t1920] VALUES (21,'شروط قبول الدعوى','43','17','0');
+INSERT INTO [t1920] VALUES (22,'المحاكمة','46','17','0');
+INSERT INTO [t1920] VALUES (23,'أولا الإقرار','48','17','0');
+INSERT INTO [t1920] VALUES (24,'ثانيا الشهادة','50','17','0');
+INSERT INTO [t1920] VALUES (25,'ثالثا علم القاضي','52','17','0');
+INSERT INTO [t1920] VALUES (26,'رابعا القرائن','53','17','0');
+INSERT INTO [t1920] VALUES (27,'الحكم في الدعوى وقواعده','54','17','0');
+INSERT INTO [t1920] VALUES (28,'نقض الحكم وقواعده','58','17','0');
+INSERT INTO [t1920] VALUES (29,'تنفيذ الحكم ووسائله','61','17','0');
+INSERT INTO [t1920] VALUES (30,'خاتمة','65','0','0');
+INSERT INTO [t1920] VALUES (31,'المصادر والمراجع','69','0','0');
