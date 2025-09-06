ba169ff0f59541e15c57a1e1408a8c100cd752bb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:59 +0700

    Upload SQL chunk: t22657_chunk_0000.sql

---
+++ sql_chunks/t22657_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t22657
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t22657" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22657
+INSERT INTO [t22657] VALUES (1,'فأما الخيل','63','0','0');
+INSERT INTO [t22657] VALUES (2,'وأما دوابه صلى الله عليه وسلم من البغال والحمير والإبل','70','0','0');
+INSERT INTO [t22657] VALUES (3,'وأما سلاحه صلى الله عليه وسلم','75','0','0');
+INSERT INTO [t22657] VALUES (4,'وأما كسوته صلى الله عليه وسلم','83','0','0');
+INSERT INTO [t22657] VALUES (5,'وأما سريره الذي كان ينام عليه','86','0','0');
+INSERT INTO [t22657] VALUES (6,'وأما منائحه صلى الله عليه وسلم','89','0','0');
+INSERT INTO [t22657] VALUES (7,'وأما لقاحه صلى الله عليه وسلم','94','0','0');
+INSERT INTO [t22657] VALUES (8,'وأما خدمة رسول الله صلى الله عليه وسلم','100','0','0');
