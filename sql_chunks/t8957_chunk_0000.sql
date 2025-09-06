44a53fe77cb9b0f4aa535bc65f6d00cc140e4493
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:57 +0700

    Upload SQL chunk: t8957_chunk_0000.sql

---
+++ sql_chunks/t8957_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t8957
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t8957" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8957
+INSERT INTO [t8957] VALUES (1,'المنهج بين الماضي والحاضر','1','0','0');
+INSERT INTO [t8957] VALUES (2,'التعليم','4','0','0');
+INSERT INTO [t8957] VALUES (3,'منطلق التعليم','6','0','0');
+INSERT INTO [t8957] VALUES (4,'واقع البشرية قبل المنهج الأقوم','8','0','0');
+INSERT INTO [t8957] VALUES (5,'وأصبحت الذبابة نحلة','10','0','0');
+INSERT INTO [t8957] VALUES (6,'أنواع العلوم وحكمها من حيث المنهج','13','0','0');
+INSERT INTO [t8957] VALUES (7,'واقع المسلمين اليوم','17','0','0');
+INSERT INTO [t8957] VALUES (8,'ثم ماذا..','18','0','0');
