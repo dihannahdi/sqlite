3b7445e37d54aff07c3bc87373474d050f6a0851
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:20 +0700

    Upload SQL chunk: t7063_chunk_0000.sql

---
+++ sql_chunks/t7063_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t7063
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t7063" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7063
+INSERT INTO [t7063] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t7063] VALUES (2,'القسم الثاني','77','0','0');
+INSERT INTO [t7063] VALUES (3,'بسم الله الرحمن الرحيم','79','0','0');
+INSERT INTO [t7063] VALUES (4,'حاشيتان','123','0','0');
+INSERT INTO [t7063] VALUES (5,'على موضعين في القسم الأول من هذه الرسالة','123','0','0');
+INSERT INTO [t7063] VALUES (6,'ثلاث حواش','126','0','0');
+INSERT INTO [t7063] VALUES (7,'على ما كتبه الأستاذ العلامة لليازجي عن أغلاط اللسان في مجلة الضياء','126','0','0');
+INSERT INTO [t7063] VALUES (8,'الخاتمة','130','0','0');
