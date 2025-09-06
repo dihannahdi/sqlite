d0684dd58e4ef2e85255ad0c63af198628993151
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:35 +0700

    Upload SQL chunk: t10392_chunk_0000.sql

---
+++ sql_chunks/t10392_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t10392
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t10392" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10392
+INSERT INTO [t10392] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10392] VALUES (2,'كتب الكنى','3','0','0');
+INSERT INTO [t10392] VALUES (3,'أنواع الكنى','9','0','0');
+INSERT INTO [t10392] VALUES (4,'سبب تكني عبيد الله بن عبد الكريم الرازي بكنية أبي زرعة','13','0','0');
+INSERT INTO [t10392] VALUES (5,'تمييز الأئمة والحفاظ بين أبي زرعة الرازي والدمشقي','15','0','0');
+INSERT INTO [t10392] VALUES (6,'أسماء الرواة الذين كنوا بأبي زرعة','17','0','0');
+INSERT INTO [t10392] VALUES (7,'مصادر ومراجع','42','0','0');
