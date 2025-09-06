7964b0ce8eafb7806500eecd98c23f0cb012d11c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:08 +0700

    Upload SQL chunk: t6951_chunk_0000.sql

---
+++ sql_chunks/t6951_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t6951
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t6951" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6951
+INSERT INTO [t6951] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6951] VALUES (2,'شواهد المقدمة','7','0','0');
+INSERT INTO [t6951] VALUES (3,'شواهد الفن الأول وهو علم المعاني','70','0','0');
+INSERT INTO [t6951] VALUES (4,'شواهد المسند إليه','98','0','0');
+INSERT INTO [t6951] VALUES (5,'شواهد المسند','184','0','0');
+INSERT INTO [t6951] VALUES (6,'شواهد أحوال متعلقات الفعل','230','0','0');
+INSERT INTO [t6951] VALUES (7,'شواهد القصر','255','0','0');
+INSERT INTO [t6951] VALUES (8,'شواهد الإنشاء','258','0','0');
+INSERT INTO [t6951] VALUES (9,'شواهد الوصل والفصل','263','0','0');
+INSERT INTO [t6951] VALUES (10,'شواهد الإيجاز والإطناب والمساواة','300','0','0');
+INSERT INTO [t6951] VALUES (11,'شواهد الفن الثاني وهو علم البيان','385','0','0');
+INSERT INTO [t6951] VALUES (12,'شواهد الاستعارة','492','0','0');
+INSERT INTO [t6951] VALUES (13,'شواهد الفن الثالث وهو علم البديع','556','0','0');
