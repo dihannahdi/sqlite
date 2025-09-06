c72069de4c5e826fbb7f9fcdb743d44db6472475
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:40 +0700

    Upload SQL chunk: t10279_chunk_0000.sql

---
+++ sql_chunks/t10279_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10279
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10279" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10279
+INSERT INTO [t10279] VALUES (1,'جزء في أخبار وحكايات من حديث محمد بن القاسم بن معروف (ت٣٤٧) هـ','1','0','0');
