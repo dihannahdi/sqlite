6531a9c2563e07ee7f6f79db170171cf9a8cb7bb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:27 +0700

    Upload SQL chunk: t30889_chunk_0000.sql

---
+++ sql_chunks/t30889_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t30889
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t30889" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30889
+INSERT INTO [t30889] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t30889] VALUES (2,'الأصول المعتبرة في إثبات الأحكام','3','0','0');
+INSERT INTO [t30889] VALUES (3,'الأصل الأول كتاب الله العزيز','3','2','0');
+INSERT INTO [t30889] VALUES (4,'الأصل الثاني ما صح عن رسول الله صلى الله عليه وسلم وأصحابه ومن بعدهم','6','2','0');
+INSERT INTO [t30889] VALUES (5,'مما ورد في ذلك من الآيات','6','4','0');
+INSERT INTO [t30889] VALUES (6,'مما ورد في ذلك من الأحاديث عن رسول الله صلى الله عليه وسلم','11','4','0');
+INSERT INTO [t30889] VALUES (7,'مما ورد في ذلك عن الصحابة والتابعين ومن بعدهم من أهل العلم','16','4','0');
