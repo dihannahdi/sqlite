9cd80b2f9c707962f9eed4809d2ce3f80baa251d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:50 +0700

    Upload SQL chunk: t6457_chunk_0000.sql

---
+++ sql_chunks/t6457_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t6457
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t6457" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6457
+INSERT INTO [t6457] VALUES (1,'الفصل الأول','3','0','0');
+INSERT INTO [t6457] VALUES (2,'تتمة في التلقين بعد الدفن','18','0','0');
+INSERT INTO [t6457] VALUES (3,'الفصل الثاني','22','0','0');
+INSERT INTO [t6457] VALUES (4,'الفصل الثالث','37','0','0');
+INSERT INTO [t6457] VALUES (5,'حياة الأنبياء البرزخية','37','0','0');
+INSERT INTO [t6457] VALUES (6,'النعيم والعذاب في القبر للروح والبدن','42','0','0');
+INSERT INTO [t6457] VALUES (7,'الفصل الثالث في عذاب القبر ومسألة منكر ونكير','53','0','0');
+INSERT INTO [t6457] VALUES (8,'عذاب القبر وأدلة من يقول بنفيه','57','0','0');
+INSERT INTO [t6457] VALUES (9,'زيارة القبور','59','0','0');
+INSERT INTO [t6457] VALUES (10,'فصل في زيارة القبور','59','0','0');
