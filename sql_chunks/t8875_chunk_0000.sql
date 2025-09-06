caa29b7914db59a206b8c28e7d491121ef6ae5df
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:29 +0700

    Upload SQL chunk: t8875_chunk_0000.sql

---
+++ sql_chunks/t8875_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t8875
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t8875" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8875
+INSERT INTO [t8875] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8875] VALUES (2,'الإرسال بين اللغة والاصطلاح','4','0','0');
+INSERT INTO [t8875] VALUES (3,'مذاهب العلماء في قبول الحديث المرسل والعمل به أو رده','8','0','0');
+INSERT INTO [t8875] VALUES (4,'رواة المراسيل والموارنة بينهم','14','0','0');
+INSERT INTO [t8875] VALUES (5,'خاتمة','15','0','0');
+INSERT INTO [t8875] VALUES (6,'مصادر ومراجع','16','0','0');
