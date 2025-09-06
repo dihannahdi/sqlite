99dcc0bc25f096ceba9fadb6f704db926cc20395
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:45 +0700

    Upload SQL chunk: t5722_chunk_0000.sql

---
+++ sql_chunks/t5722_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5722
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5722" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5722
+INSERT INTO [t5722] VALUES (1,'فضائل مكة','1','0','0');
+INSERT INTO [t5722] VALUES (2,'تم الجزء','30','0','0');
