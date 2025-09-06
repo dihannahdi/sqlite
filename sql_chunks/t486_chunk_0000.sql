f5179dda5ee0c2033300006c3983b36d4fd016de
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:10 +0700

    Upload SQL chunk: t486_chunk_0000.sql

---
+++ sql_chunks/t486_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t486
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t486" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t486
+INSERT INTO [t486] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t486] VALUES (2,'الفصل الأول في معنى قولهم: «أصول الفقه»','4','0','0');
+INSERT INTO [t486] VALUES (3,'المبحث الأول: «العلم» في أصل الوضع العربي هو المعرفة القطعية','4','2','0');
+INSERT INTO [t486] VALUES (4,'فوائد','5','3','0');
+INSERT INTO [t486] VALUES (5,'المبحث الثاني: «الأصول»','7','2','0');
+INSERT INTO [t486] VALUES (6,'المبحث الثالث: «الفقه»','7','2','0');
+INSERT INTO [t486] VALUES (7,'المبحث الرابع: «أصول الفقه»','8','2','0');
+INSERT INTO [t486] VALUES (8,'المبحث الخامس: «علم أصول الفقه»','9','2','0');
+INSERT INTO [t486] VALUES (9,'الفصل الثاني في أن أصول الفقه لا بد أن تكون قطعية','10','0','0');
+INSERT INTO [t486] VALUES (10,'الفصل الثاني في الأحكام','14','0','0');
+INSERT INTO [t486] VALUES (11,'الضرب الأول: التكليفي','14','10','0');
