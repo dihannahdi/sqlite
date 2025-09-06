f4cd3aaa983a70d709e6c0299d893dd23b3cf9b8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:04 +0700

    Upload SQL chunk: t10008_chunk_0000.sql

---
+++ sql_chunks/t10008_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t10008
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t10008" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10008
+INSERT INTO [t10008] VALUES (1,'مقدمات','1','0','0');
+INSERT INTO [t10008] VALUES (2,'إهداء المترجم','1','1','0');
+INSERT INTO [t10008] VALUES (3,'دواعي نشر كتاب المبادئ','1','2','0');
+INSERT INTO [t10008] VALUES (4,'ماهية الفلسفة ومقصدها عند ديكارت','5','2','0');
+INSERT INTO [t10008] VALUES (5,'الجزء الأول من مبادئ الفلسفة','11','2','0');
+INSERT INTO [t10008] VALUES (6,'إهداء المؤلف','17','1','0');
+INSERT INTO [t10008] VALUES (7,'مدخل','17','6','0');
+INSERT INTO [t10008] VALUES (8,'رسالة من المؤلف إلى مترجم الكتاب','22','6','0');
+INSERT INTO [t10008] VALUES (9,'في مبادئ المعرفة البشرية','41','0','0');
+INSERT INTO [t10008] VALUES (10,'فهرس الكتاب','89','0','0');
