8b127517f988f7da6d1223cbcad6176552d00f2d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:19 +0700

    Upload SQL chunk: t9853_chunk_0000.sql

---
+++ sql_chunks/t9853_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t9853
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t9853" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9853
+INSERT INTO [t9853] VALUES (1,'أحاديث مسندة في أبواب القضاء لأبي نعيم الأصفهاني - مخطوط','2','0','0');
