82eea66ed7551349231ca6bd88852251fd22197c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:38 +0700

    Upload SQL chunk: t36381_chunk_0000.sql

---
+++ sql_chunks/t36381_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t36381
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t36381" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36381
+INSERT INTO [t36381] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t36381] VALUES (2,'* القرآن ثقيل .. القرآن هدى *','2','0','0');
+INSERT INTO [t36381] VALUES (3,'* أخي .. القرآن ميزان! فهل وزنت نفسك؟ ! *','4','0','0');
+INSERT INTO [t36381] VALUES (4,'* أخي .. كيف أنت مع تلاوة كتاب الله تعالى؟ ! *','5','0','0');
+INSERT INTO [t36381] VALUES (5,'* أخي .. احذر هجر القرآن! *','13','0','0');
+INSERT INTO [t36381] VALUES (6,'* أخي .. هل أنت من المتدبرين لكتاب الله تعالى؟ ! *','16','0','0');
+INSERT INTO [t36381] VALUES (7,'* أخي .. هل أنت من الباكين إذا قرءوا كتاب ربهم تبارك وتعالى؟ ! *','19','0','0');
+INSERT INTO [t36381] VALUES (8,'* أخي: أين أنت من هذه الكنوز؟ ! *','22','0','0');
+INSERT INTO [t36381] VALUES (9,'* أخي .. كيف تكون قريبا من كتاب الله تعالى؟ ! *','24','0','0');
+INSERT INTO [t36381] VALUES (10,'خاتمة','26','0','0');
