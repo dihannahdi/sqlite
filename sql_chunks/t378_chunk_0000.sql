ed55beb95c3db1fc43cb68d704cb5a6b4d756848
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:16:37 +0700

    Upload SQL chunk: t378_chunk_0000.sql

---
+++ sql_chunks/t378_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t378
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t378" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t378
+INSERT INTO [t378] VALUES (1,'{ولا تسرفوا} في الآية راجع إلى الأكل','2','0','0');
+INSERT INTO [t378] VALUES (2,'فائدة (من) في قوله تعالى: {ومما رزقناهم ينفقون}','6','0','0');
+INSERT INTO [t378] VALUES (3,'شرط مفهوم المخالفة','6','0','0');
+INSERT INTO [t378] VALUES (4,'لحن الخطاب وفحوى الخطاب','7','0','0');
