71c8840b8a8bb9862a0782057d9184c331e68834
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:02 +0700

    Upload SQL chunk: t11252_chunk_0000.sql

---
+++ sql_chunks/t11252_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t11252
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t11252" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11252
+INSERT INTO [t11252] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11252] VALUES (2,'مقدمة شيخ الإسلام محمد بن عبد الوهاب','2','0','0');
+INSERT INTO [t11252] VALUES (3,'الحنيفية ملة إبراهيم','7','0','0');
+INSERT INTO [t11252] VALUES (4,'العبادة لاتسمى عبادة إلا مع التوحيد','9','0','0');
+INSERT INTO [t11252] VALUES (5,'الشرك: أهم مايجب على العبد معرفته','12','0','0');
+INSERT INTO [t11252] VALUES (6,'مدخل','12','5','0');
+INSERT INTO [t11252] VALUES (7,'القاعده الأولى','13','5','0');
+INSERT INTO [t11252] VALUES (8,'القاعدة الثانية','14','5','0');
+INSERT INTO [t11252] VALUES (9,'القاعدة الثالثة','17','5','0');
+INSERT INTO [t11252] VALUES (10,'القاعدة الرابعة','28','5','0');
