f5c9c7b59a7cf02f614b55ee060979d98405a4ba
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:05 +0700

    Upload SQL chunk: t7271_chunk_0000.sql

---
+++ sql_chunks/t7271_chunk_0000.sql
@@ -0,0 +1,49 @@
+-- Table: t7271
+-- Chunk: 1
+-- Rows: 42
+
+CREATE TABLE "t7271" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7271
+INSERT INTO [t7271] VALUES (1,'الجزء الأول','1','0','0');
+INSERT INTO [t7271] VALUES (2,'مقدمة','1','1','0');
+INSERT INTO [t7271] VALUES (3,'الباب الأول','14','1','0');
+INSERT INTO [t7271] VALUES (4,'الفصل الأول: عناية المحدثين بالكنى','14','3','0');
+INSERT INTO [t7271] VALUES (5,'الفصل الثاني: في بيان المصنفات في الكنى','17','3','0');
+INSERT INTO [t7271] VALUES (6,'الباب الثاني','29','1','0');
+INSERT INTO [t7271] VALUES (7,'وصف النسخ المخطوطة','29','6','0');
+INSERT INTO [t7271] VALUES (8,'عملي في التحقيق','33','6','0');
+INSERT INTO [t7271] VALUES (9,'نص الكتاب','36','1','0');
+INSERT INTO [t7271] VALUES (10,'أبو القاسم','36','9','0');
+INSERT INTO [t7271] VALUES (11,'حرف الألف','46','9','0');
+INSERT INTO [t7271] VALUES (12,'حرف الباء','91','9','0');
+INSERT INTO [t7271] VALUES (13,'حرف التاء','122','9','0');
+INSERT INTO [t7271] VALUES (14,'حرف الثاء','125','9','0');
+INSERT INTO [t7271] VALUES (15,'حرف الجيم','130','9','0');
+INSERT INTO [t7271] VALUES (16,'حرف الحاء','147','9','0');
+INSERT INTO [t7271] VALUES (17,'حرف الخاء','197','9','0');
+INSERT INTO [t7271] VALUES (18,'حرف الدال','212','9','0');
+INSERT INTO [t7271] VALUES (19,'حرف الذال','218','9','0');
+INSERT INTO [t7271] VALUES (20,'حرف الراء','220','9','0');
+INSERT INTO [t7271] VALUES (21,'حرف الزاي','233','9','0');
+INSERT INTO [t7271] VALUES (22,'حرف السين','246','9','0');
+INSERT INTO [t7271] VALUES (23,'حرف الشين','291','9','0');
+INSERT INTO [t7271] VALUES (24,'حرف الصاد','300','9','0');
+INSERT INTO [t7271] VALUES (25,'حرف الضاد','312','9','0');
+INSERT INTO [t7271] VALUES (26,'حرف الطاء','314','9','0');
+INSERT INTO [t7271] VALUES (27,'حرف الظاء','322','9','0');
+INSERT INTO [t7271] VALUES (28,'حرف العين','323','9','0');
+INSERT INTO [t7271] VALUES (29,'الجزء الثاني','437','0','0');
+INSERT INTO [t7271] VALUES (30,'نص الكتاب','437','29','0');
+INSERT INTO [t7271] VALUES (31,'حرف الغين','437','30','0');
+INSERT INTO [t7271] VALUES (32,'حرف الفاء','443','30','0');
+INSERT INTO [t7271] VALUES (33,'حرف القاف','454','30','0');
+INSERT INTO [t7271] VALUES (34,'حرف الكاف','462','30','0');
+INSERT INTO [t7271] VALUES (35,'حرف اللام','469','30','0');
+INSERT INTO [t7271] VALUES (36,'حرف الميم','473','30','0');
+INSERT INTO [t7271] VALUES (37,'حرف النون','543','30','0');
+INSERT INTO [t7271] VALUES (38,'حرف الهاء','554','30','0');
+INSERT INTO [t7271] VALUES (39,'حرف الواو','567','30','0');
+INSERT INTO [t7271] VALUES (40,'حرف الياء','576','30','0');
+INSERT INTO [t7271] VALUES (41,'من كنية النساء','601','30','0');
+INSERT INTO [t7271] VALUES (42,'مصادر ومراجع','607','29','0');
