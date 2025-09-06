191a17233ced228e5491130b0860713c8d5e96d5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:35 +0700

    Upload SQL chunk: t26567_chunk_0000.sql

---
+++ sql_chunks/t26567_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26567
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26567" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26567
+INSERT INTO [t26567] VALUES (1,'أحاديث من المسند الصحيح لأبي حامد ابن الشرقي','2','0','0');
