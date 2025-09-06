cb49ff4b54fa7411b7780ad5d48fb4a519cdcdbf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:55 +0700

    Upload SQL chunk: t10302_chunk_0000.sql

---
+++ sql_chunks/t10302_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t10302
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t10302" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10302
+INSERT INTO [t10302] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10302] VALUES (2,'نبذة عن حياة الإمام أحمد بن حنبل:','2','0','0');
+INSERT INTO [t10302] VALUES (3,'مؤلفاته','6','0','0');
+INSERT INTO [t10302] VALUES (4,'تلاميذه','8','0','0');
+INSERT INTO [t10302] VALUES (5,'وفاته','9','0','0');
+INSERT INTO [t10302] VALUES (6,'أولاده','11','0','0');
+INSERT INTO [t10302] VALUES (7,'محنة الإمام أحمد بن حنبل','12','0','0');
+INSERT INTO [t10302] VALUES (8,'الإمام أحمد مكبلا في الأغلال','13','0','0');
+INSERT INTO [t10302] VALUES (9,'الترغيب والترهيب والتعذيب في عهد المعتصم','15','0','0');
+INSERT INTO [t10302] VALUES (10,'تحديد إقامة الإمام أحمد في عهد الواثق','18','0','0');
+INSERT INTO [t10302] VALUES (11,'انتصار أهل السنة واندحار أهل البدعة','19','0','0');
+INSERT INTO [t10302] VALUES (12,'مشهدان من محنة الإمام أحمد بن حنبل','22','0','0');
