83f2e0b61be004162095ff2149efe011a831e9db
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:15 +0700

    Upload SQL chunk: t26906_chunk_0000.sql

---
+++ sql_chunks/t26906_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t26906
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t26906" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26906
+INSERT INTO [t26906] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26906] VALUES (2,'باب الألف','3','0','0');
+INSERT INTO [t26906] VALUES (3,'باب الباء','6','0','0');
+INSERT INTO [t26906] VALUES (4,'باب التاء','7','0','0');
+INSERT INTO [t26906] VALUES (5,'باب الثاء','7','0','0');
+INSERT INTO [t26906] VALUES (6,'باب الجيم','8','0','0');
+INSERT INTO [t26906] VALUES (7,'باب الحاء','8','0','0');
+INSERT INTO [t26906] VALUES (8,'حرف الخاء','14','0','0');
+INSERT INTO [t26906] VALUES (9,'باب الدال','15','0','0');
+INSERT INTO [t26906] VALUES (10,'باب الراء','17','0','0');
+INSERT INTO [t26906] VALUES (11,'حرف الزاي','18','0','0');
+INSERT INTO [t26906] VALUES (12,'باب السين','19','0','0');
+INSERT INTO [t26906] VALUES (13,'باب الشين','24','0','0');
+INSERT INTO [t26906] VALUES (14,'باب الصاد','24','0','0');
+INSERT INTO [t26906] VALUES (15,'باب الضاد','25','0','0');
+INSERT INTO [t26906] VALUES (16,'باب الطاء','25','0','0');
+INSERT INTO [t26906] VALUES (17,'باب العين','25','0','0');
+INSERT INTO [t26906] VALUES (18,'باب الفاء','31','0','0');
+INSERT INTO [t26906] VALUES (19,'باب القاف','31','0','0');
+INSERT INTO [t26906] VALUES (20,'باب الكاف','32','0','0');
+INSERT INTO [t26906] VALUES (21,'باب اللام','33','0','0');
+INSERT INTO [t26906] VALUES (22,'باب الميم','34','0','0');
+INSERT INTO [t26906] VALUES (23,'باب النون','38','0','0');
+INSERT INTO [t26906] VALUES (24,'باب الواو','39','0','0');
+INSERT INTO [t26906] VALUES (25,'باب الهاء','40','0','0');
+INSERT INTO [t26906] VALUES (26,'باب الياء','44','0','0');
