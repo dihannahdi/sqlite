7e8fc546b376dd9f0ad08fe0307f949e7c569ad9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:10 +0700

    Upload SQL chunk: t95681_chunk_0000.sql

---
+++ sql_chunks/t95681_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t95681
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t95681" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95681
+INSERT INTO [t95681] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95681] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t95681] VALUES (3,'آفات اللسان','6','0','0');
+INSERT INTO [t95681] VALUES (4,'ومن آفات اللسان ما يلي','6','3','0');
+INSERT INTO [t95681] VALUES (5,'الآفة الأولى','6','3','0');
+INSERT INTO [t95681] VALUES (6,'الآفة الثانية','6','3','0');
+INSERT INTO [t95681] VALUES (7,'الآفة الثالثة','6','3','0');
+INSERT INTO [t95681] VALUES (8,'الآفة الرابعة','6','3','0');
+INSERT INTO [t95681] VALUES (9,'الآفة الخامسة','7','3','0');
+INSERT INTO [t95681] VALUES (10,'الآفة السادسة','7','3','0');
+INSERT INTO [t95681] VALUES (11,'الآفة السابعة','7','3','0');
+INSERT INTO [t95681] VALUES (12,'الآفة الثامنة','7','3','0');
+INSERT INTO [t95681] VALUES (13,'الآفة التاسعة','7','3','0');
+INSERT INTO [t95681] VALUES (14,'الآفة الأولى الغيبة','31','0','0');
+INSERT INTO [t95681] VALUES (15,'الغيبة','32','0','0');
+INSERT INTO [t95681] VALUES (16,'بواعث الغيبة','36','0','0');
+INSERT INTO [t95681] VALUES (17,'بيان العلاج الذي يمنع اللسان عن الغيبة','53','0','0');
+INSERT INTO [t95681] VALUES (18,'باب ما يباح من الغيبة','54','0','0');
+INSERT INTO [t95681] VALUES (19,'كفارة الغيبة','63','0','0');
+INSERT INTO [t95681] VALUES (20,'النميمة','64','0','0');
+INSERT INTO [t95681] VALUES (21,'النميمة','65','0','0');
+INSERT INTO [t95681] VALUES (22,'أعظم من الغيبة والنميمة','74','0','0');
+INSERT INTO [t95681] VALUES (23,'الكذب','76','0','0');
+INSERT INTO [t95681] VALUES (24,'الاستهزاء','84','0','0');
+INSERT INTO [t95681] VALUES (25,'كيف كان لسانه - صلى الله عليه وسلم -','91','0','0');
+INSERT INTO [t95681] VALUES (26,'المصادر','95','0','0');
