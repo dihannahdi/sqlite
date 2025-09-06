2d8b42c4d8bbdf440e7a65ed63be0fb880dd06ac
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:56 +0700

    Upload SQL chunk: t6470_chunk_0000.sql

---
+++ sql_chunks/t6470_chunk_0000.sql
@@ -0,0 +1,58 @@
+-- Table: t6470
+-- Chunk: 1
+-- Rows: 51
+
+CREATE TABLE "t6470" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6470
+INSERT INTO [t6470] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6470] VALUES (2,'وصلى الله على سيدنا محمد وآله','1','0','0');
+INSERT INTO [t6470] VALUES (3,'رب يسر ولا تعسر','1','0','0');
+INSERT INTO [t6470] VALUES (4,'ذكر ما اختلقوه في قصة داوود عليه السلام','5','0','0');
+INSERT INTO [t6470] VALUES (5,'فصل','6','0','0');
+INSERT INTO [t6470] VALUES (6,'فصل','6','0','0');
+INSERT INTO [t6470] VALUES (7,'فصل','9','0','0');
+INSERT INTO [t6470] VALUES (8,'فصل','10','0','0');
+INSERT INTO [t6470] VALUES (9,'فصل','13','0','0');
+INSERT INTO [t6470] VALUES (10,'شرح قصة سليمان عليه السلام','15','0','0');
+INSERT INTO [t6470] VALUES (11,'شرح قصة يوسف عليه السلام','22','0','0');
+INSERT INTO [t6470] VALUES (12,'فصل','24','0','0');
+INSERT INTO [t6470] VALUES (13,'شرح قصة نبينا عليه الصلاة والسلام','28','0','0');
+INSERT INTO [t6470] VALUES (14,'فصل','31','0','0');
+INSERT INTO [t6470] VALUES (15,'فصل','42','0','0');
+INSERT INTO [t6470] VALUES (16,'شرح قصة آدم عليه السلام','44','0','0');
+INSERT INTO [t6470] VALUES (17,'فصل','45','0','0');
+INSERT INTO [t6470] VALUES (18,'فصل','45','0','0');
+INSERT INTO [t6470] VALUES (19,'فصل','49','0','0');
+INSERT INTO [t6470] VALUES (20,'شرح قصة نوح عليه السلام','56','0','0');
+INSERT INTO [t6470] VALUES (21,'فصل','60','0','0');
+INSERT INTO [t6470] VALUES (22,'فصل','62','0','0');
+INSERT INTO [t6470] VALUES (23,'شرح قصة إبراهيم عليه السلام','64','0','0');
+INSERT INTO [t6470] VALUES (24,'فصل','67','0','0');
+INSERT INTO [t6470] VALUES (25,'فصل','70','0','0');
+INSERT INTO [t6470] VALUES (26,'فصل','73','0','0');
+INSERT INTO [t6470] VALUES (27,'فصل','74','0','0');
+INSERT INTO [t6470] VALUES (28,'فصل','75','0','0');
+INSERT INTO [t6470] VALUES (29,'فصل','77','0','0');
+INSERT INTO [t6470] VALUES (30,'شرح قصة عزير عليه السلام','81','0','0');
+INSERT INTO [t6470] VALUES (31,'فصل','82','0','0');
+INSERT INTO [t6470] VALUES (32,'شرح قصة موسى عليه السلام','86','0','0');
+INSERT INTO [t6470] VALUES (33,'فصل','86','0','0');
+INSERT INTO [t6470] VALUES (34,'شرح قصة يونس عليه السلام','93','0','0');
+INSERT INTO [t6470] VALUES (35,'فصل','94','0','0');
+INSERT INTO [t6470] VALUES (36,'شرح قصة أيوب عليه السلام','99','0','0');
+INSERT INTO [t6470] VALUES (37,'فصل','102','0','0');
+INSERT INTO [t6470] VALUES (38,'فصل','116','0','0');
+INSERT INTO [t6470] VALUES (39,'فصل','118','0','0');
+INSERT INTO [t6470] VALUES (40,'فصل','119','0','0');
+INSERT INTO [t6470] VALUES (41,'مجموع نكت من بعض ما خص به نبينا صلى الله عليه وسلم','125','0','0');
+INSERT INTO [t6470] VALUES (42,'فصل','133','0','0');
+INSERT INTO [t6470] VALUES (43,'فصل','133','0','0');
+INSERT INTO [t6470] VALUES (44,'فصل','135','0','0');
+INSERT INTO [t6470] VALUES (45,'فصل','136','0','0');
+INSERT INTO [t6470] VALUES (46,'فصل','137','0','0');
+INSERT INTO [t6470] VALUES (47,'فصل','138','0','0');
+INSERT INTO [t6470] VALUES (48,'فصل','139','0','0');
+INSERT INTO [t6470] VALUES (49,'فصل','141','0','0');
+INSERT INTO [t6470] VALUES (50,'فصل','141','0','0');
+INSERT INTO [t6470] VALUES (51,'فصل','142','0','0');
