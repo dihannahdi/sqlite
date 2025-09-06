db995ca2ac2b0c81660763b470d997de5d41373a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:49 +0700

    Upload SQL chunk: t11382_chunk_0000.sql

---
+++ sql_chunks/t11382_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11382
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11382" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11382
+INSERT INTO [t11382] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11382] VALUES (2,'محمد بن عبد الوهاب عقيدته السلفية ودعوته الإصلاحية وثناء العلماء عليه','12','0','0');
