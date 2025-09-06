6956c9ab7be425edc951768ef6fe56097593c089
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:59 +0700

    Upload SQL chunk: t10136_chunk_0000.sql

---
+++ sql_chunks/t10136_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10136
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10136" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10136
+INSERT INTO [t10136] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10136] VALUES (2,'حديث المصراة رواية ودراية','5','0','0');
+INSERT INTO [t10136] VALUES (3,'مصادر ومراجع','27','0','0');
