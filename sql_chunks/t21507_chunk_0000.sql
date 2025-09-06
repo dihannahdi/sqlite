6f8563213934516ab5a14ad133ec2dd3f857aedc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:52 +0700

    Upload SQL chunk: t21507_chunk_0000.sql

---
+++ sql_chunks/t21507_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t21507
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t21507" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21507
+INSERT INTO [t21507] VALUES (1,'مقدمة (الزوائد على مسائل الجاهلية)','1','0','0');
+INSERT INTO [t21507] VALUES (2,'فهرس قسم العقيدة بالأرقام','3','0','0');
+INSERT INTO [t21507] VALUES (3,'فهرس قسم الفقه بالأرقام','3','0','0');
+INSERT INTO [t21507] VALUES (4,'بسم الله الرحمن الرحيم','5','0','0');
+INSERT INTO [t21507] VALUES (5,'الزوائد على مسائل الجاهلية','7','0','0');
