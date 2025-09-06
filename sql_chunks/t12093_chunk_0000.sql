086876a8599bc826000d88259b45977760d4e4c5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:48 +0700

    Upload SQL chunk: t12093_chunk_0000.sql

---
+++ sql_chunks/t12093_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t12093
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t12093" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12093
+INSERT INTO [t12093] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t12093] VALUES (2,'باب مخارج الحروف','3','0','0');
+INSERT INTO [t12093] VALUES (3,'باب صفات الحروف','5','0','0');
+INSERT INTO [t12093] VALUES (4,'باب التجويد','6','0','0');
+INSERT INTO [t12093] VALUES (5,'باب الترقيق','7','0','0');
+INSERT INTO [t12093] VALUES (6,'باب استعمال الحروف','8','0','0');
+INSERT INTO [t12093] VALUES (7,'باب الراءات','9','0','0');
+INSERT INTO [t12093] VALUES (8,'باب اللامات','10','0','0');
+INSERT INTO [t12093] VALUES (9,'باب الضاد والظاء','12','0','0');
+INSERT INTO [t12093] VALUES (10,'باب التحذيرات','14','0','0');
+INSERT INTO [t12093] VALUES (11,'باب النون والميم المشددتين والميم الساكنة','15','0','0');
+INSERT INTO [t12093] VALUES (12,'باب أحكام النون الساكنة والتنوين','16','0','0');
+INSERT INTO [t12093] VALUES (13,'باب المد','17','0','0');
+INSERT INTO [t12093] VALUES (14,'باب معرفة الوقف والابتداء','19','0','0');
+INSERT INTO [t12093] VALUES (15,'باب المقطوع والموصول','20','0','0');
+INSERT INTO [t12093] VALUES (16,'باب التاءات','22','0','0');
+INSERT INTO [t12093] VALUES (17,'باب همز الوصل','23','0','0');
+INSERT INTO [t12093] VALUES (18,'باب الوقف على أواخر الكلم','24','0','0');
