b1959a444da7b3f2cceab1ede097fccc3b1921f4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:49 +0700

    Upload SQL chunk: t30482_chunk_0000.sql

---
+++ sql_chunks/t30482_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30482
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30482" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30482
+INSERT INTO [t30482] VALUES (1,'الجزء الثالث من الفوائد المنتقاة الحسان والغرائب','1','0','0');
