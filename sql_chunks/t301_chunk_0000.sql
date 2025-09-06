449d250b3a61243dbd5eae989319a3f5637208fe
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:58 +0700

    Upload SQL chunk: t301_chunk_0000.sql

---
+++ sql_chunks/t301_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t301
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t301" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t301
+INSERT INTO [t301] VALUES (1,'الاحتجاج بالقدر','1','0','0');
