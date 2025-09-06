40d70597bbf358029761ba7fd743f6ade4d4a63b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:02 +0700

    Upload SQL chunk: t22002_chunk_0000.sql

---
+++ sql_chunks/t22002_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t22002
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t22002" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22002
+INSERT INTO [t22002] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t22002] VALUES (2,'الحديث الأول','3','0','0');
+INSERT INTO [t22002] VALUES (3,'الحديث الثاني','5','0','0');
+INSERT INTO [t22002] VALUES (4,'الحديث الثالث','7','0','0');
+INSERT INTO [t22002] VALUES (5,'الحديث الرابع','9','0','0');
+INSERT INTO [t22002] VALUES (6,'الحديث الخامس','11','0','0');
+INSERT INTO [t22002] VALUES (7,'الحديث السادس','13','0','0');
+INSERT INTO [t22002] VALUES (8,'الحديث السابع','15','0','0');
+INSERT INTO [t22002] VALUES (9,'الحديث الثامن','16','0','0');
+INSERT INTO [t22002] VALUES (10,'الحديث التاسع','19','0','0');
+INSERT INTO [t22002] VALUES (11,'الحديث العاشر','20','0','0');
+INSERT INTO [t22002] VALUES (12,'الحديث الحادي عشر','21','0','0');
+INSERT INTO [t22002] VALUES (13,'الحديث الثاني عشر','22','0','0');
+INSERT INTO [t22002] VALUES (14,'الحديث الثالث عشر','24','0','0');
+INSERT INTO [t22002] VALUES (15,'الحديث الرابع عشر','26','0','0');
+INSERT INTO [t22002] VALUES (16,'الحديث الخامس عشر','27','0','0');
+INSERT INTO [t22002] VALUES (17,'الحديث السادس عشر','30','0','0');
+INSERT INTO [t22002] VALUES (18,'الحديث السابع عشر','31','0','0');
+INSERT INTO [t22002] VALUES (19,'الحديث الثامن عشر','32','0','0');
+INSERT INTO [t22002] VALUES (20,'الحديث التاسع عشر','33','0','0');
+INSERT INTO [t22002] VALUES (21,'الحديث العشرون','34','0','0');
+INSERT INTO [t22002] VALUES (22,'الحديث الحادي والعشرون','36','0','0');
