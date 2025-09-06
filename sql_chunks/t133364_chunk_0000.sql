035d7aafb54b2022a9b4d23d9b4cf1b4d2c9b2fa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:13:45 +0700

    Upload SQL chunk: t133364_chunk_0000.sql

---
+++ sql_chunks/t133364_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t133364
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t133364" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t133364
+INSERT INTO [t133364] VALUES (1,'[مقدمة التحقيق]','1','0','0');
+INSERT INTO [t133364] VALUES (2,'وصف الرسالة','5','1','0');
+INSERT INTO [t133364] VALUES (3,'فصل في تزكية النفس وكيف تزكو بترك المحرمات مع فعل المأمورات','9','0','0');
+INSERT INTO [t133364] VALUES (4,'تعريف التزكية','9','0','0');
+INSERT INTO [t133364] VALUES (5,'حمل الكلام على الأظهر','13','0','0');
+INSERT INTO [t133364] VALUES (6,'أثر الحسنة على النفس','16','0','0');
+INSERT INTO [t133364] VALUES (7,'حال النفس المطمئنة واللوامة','21','0','0');
+INSERT INTO [t133364] VALUES (8,'الزكاة تستلزم الطهارة','26','0','0');
+INSERT INTO [t133364] VALUES (9,'أنواع أمراض القلب','28','0','0');
+INSERT INTO [t133364] VALUES (10,'القلب لا يمرض بالشبهات والشهوات إلا لنقصان الإيمان','33','0','0');
+INSERT INTO [t133364] VALUES (11,'مذهب أهل السنة على خلاف مذهب الخوارج','34','0','0');
+INSERT INTO [t133364] VALUES (12,'السيئات تحبط بعض الحسنات','39','0','0');
+INSERT INTO [t133364] VALUES (13,'وزن الحسنات والسيئات','44','0','0');
+INSERT INTO [t133364] VALUES (14,'الوزن على وجهين','53','0','0');
