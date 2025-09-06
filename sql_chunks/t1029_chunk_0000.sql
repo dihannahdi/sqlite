288c317a7d965cc13ac1e3757be299f2aa03a60d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:52 +0700

    Upload SQL chunk: t1029_chunk_0000.sql

---
+++ sql_chunks/t1029_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t1029
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t1029" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1029
+INSERT INTO [t1029] VALUES (1,'أخبار وتراجم أندلسية مستخرجة من معجم السفر للسلفي','1','0','0');
+INSERT INTO [t1029] VALUES (2,'تعريف موجز بالمؤلف والكتاب','1','0','0');
+INSERT INTO [t1029] VALUES (3,'معجم السفر','9','0','0');
