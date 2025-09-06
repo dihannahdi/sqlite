365b7ecde7c2b37ed8765fcaa0731854a0b5ee34
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:19 +0700

    Upload SQL chunk: t589_chunk_0000.sql

---
+++ sql_chunks/t589_chunk_0000.sql
@@ -0,0 +1,64 @@
+-- Table: t589
+-- Chunk: 1
+-- Rows: 57
+
+CREATE TABLE "t589" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t589
+INSERT INTO [t589] VALUES (1,'دير درمالس','1','0','0');
+INSERT INTO [t589] VALUES (2,'دير سمالو','3','0','0');
+INSERT INTO [t589] VALUES (3,'دير الثعالب','5','0','0');
+INSERT INTO [t589] VALUES (4,'دير الجاثليق','5','0','0');
+INSERT INTO [t589] VALUES (5,'دير مديان','7','0','0');
+INSERT INTO [t589] VALUES (6,'دير أشموني','10','0','0');
+INSERT INTO [t589] VALUES (7,'دير سابر','12','0','0');
+INSERT INTO [t589] VALUES (8,'دير قوطا','13','0','0');
+INSERT INTO [t589] VALUES (9,'دير مرجرجس','15','0','0');
+INSERT INTO [t589] VALUES (10,'دير باشهرا','18','0','0');
+INSERT INTO [t589] VALUES (11,'دير الخوات','21','0','0');
+INSERT INTO [t589] VALUES (12,'دير العلث','21','0','0');
+INSERT INTO [t589] VALUES (13,'دير العذارى','24','0','0');
+INSERT INTO [t589] VALUES (14,'دير السوسي','36','0','0');
+INSERT INTO [t589] VALUES (15,'دير مرمار','38','0','0');
+INSERT INTO [t589] VALUES (16,'دير مريحنا','40','0','0');
+INSERT INTO [t589] VALUES (17,'دير صباعي','41','0','0');
+INSERT INTO [t589] VALUES (18,'دير الأعلى','41','0','0');
+INSERT INTO [t589] VALUES (19,'دير يونس بن متى','43','0','0');
+INSERT INTO [t589] VALUES (20,'دير الشياطين','44','0','0');
+INSERT INTO [t589] VALUES (21,'عمر الزعفران','45','0','0');
+INSERT INTO [t589] VALUES (22,'عمر أحويشا','47','0','0');
+INSERT INTO [t589] VALUES (23,'دير فيق','49','0','0');
+INSERT INTO [t589] VALUES (24,'دير الطور','49','0','0');
+INSERT INTO [t589] VALUES (25,'دير البخت','51','0','0');
+INSERT INTO [t589] VALUES (26,'دير زكى','53','0','0');
+INSERT INTO [t589] VALUES (27,'دير ماسرجيس','56','0','0');
+INSERT INTO [t589] VALUES (28,'دير ابن مزعوق','56','0','0');
+INSERT INTO [t589] VALUES (29,'دير سرجس','57','0','0');
+INSERT INTO [t589] VALUES (30,'ديارات الاساقف','57','0','0');
+INSERT INTO [t589] VALUES (31,'قبة الشتيق','58','0','0');
+INSERT INTO [t589] VALUES (32,'دير هند بنت النعمان بن المنذر','59','0','0');
+INSERT INTO [t589] VALUES (33,'دير زرارة','59','0','0');
+INSERT INTO [t589] VALUES (34,'عمر مر يونان','62','0','0');
+INSERT INTO [t589] VALUES (35,'دير قنى','64','0','0');
+INSERT INTO [t589] VALUES (36,'ويعرف أيضا بدير مر ماري السليح','64','0','0');
+INSERT INTO [t589] VALUES (37,'عمر كسكر','66','0','0');
+INSERT INTO [t589] VALUES (38,'ديارات مصر التي تقصد للشرب فيها والتنزه بها فمنها: دير القصير','69','0','0');
+INSERT INTO [t589] VALUES (39,'دير القصير','69','0','0');
+INSERT INTO [t589] VALUES (40,'دير مر حنا','70','0','0');
+INSERT INTO [t589] VALUES (41,'دير نهيا','71','0','0');
+INSERT INTO [t589] VALUES (42,'دير طمويه','73','0','0');
+INSERT INTO [t589] VALUES (43,'الديارات المعروفة بالعجائب، على ما ذكره أهلها ووصفوه عنها. فمنها:','73','0','0');
+INSERT INTO [t589] VALUES (44,'دير الخنافس','73','0','0');
+INSERT INTO [t589] VALUES (45,'دير الكلب','73','0','0');
+INSERT INTO [t589] VALUES (46,'دير القيارة','73','0','0');
+INSERT INTO [t589] VALUES (47,'دير برقوما','73','0','0');
+INSERT INTO [t589] VALUES (48,'دير باطا','73','0','0');
+INSERT INTO [t589] VALUES (49,'دير مار شمعون بنواحي السن','74','0','0');
+INSERT INTO [t589] VALUES (50,'دير العجاج','74','0','0');
+INSERT INTO [t589] VALUES (51,'دير الجودي','74','0','0');
+INSERT INTO [t589] VALUES (52,'كنيسة الطور','74','0','0');
+INSERT INTO [t589] VALUES (53,'بيعة أبى هور','74','0','0');
+INSERT INTO [t589] VALUES (54,'دير يحنس','74','0','0');
+INSERT INTO [t589] VALUES (55,'بيعة اتريب','74','0','0');
+INSERT INTO [t589] VALUES (56,'وبنواحي اخميم','74','0','0');
+INSERT INTO [t589] VALUES (57,'خاتمة المخطوط','74','0','0');
