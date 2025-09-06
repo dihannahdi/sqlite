1e2bf760cad677229479dd62a893b015db8c9d54
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:11:58 +0700

    Upload SQL chunk: t37057_chunk_0000.sql

---
+++ sql_chunks/t37057_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t37057
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t37057" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37057
+INSERT INTO [t37057] VALUES (1,'الجزء الأول المآخذ على شرح ابن جني الموسوم بالفسر','1','0','0');
+INSERT INTO [t37057] VALUES (2,'الجزء الثاني المآخذ على شرح أبي العلاء المعري الموسوم باللامع العزيزي','303','0','0');
+INSERT INTO [t37057] VALUES (3,'الجزء الثالث المآخذ على شرح التبريزي الموسوم بالموضح','538','0','0');
+INSERT INTO [t37057] VALUES (4,'الجزء الرابع المآخذ على شرح الكندي الموسوم بالصفوة','704','0','0');
+INSERT INTO [t37057] VALUES (5,'الجزء الخامس المآخذ على شرح الواحدي','792','0','0');
