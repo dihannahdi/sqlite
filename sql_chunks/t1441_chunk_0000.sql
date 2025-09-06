8f62f8a3a17f65d32c0d28e4ba811343cc2dc126
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:18:44 +0700

    Upload SQL chunk: t1441_chunk_0000.sql

---
+++ sql_chunks/t1441_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t1441
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t1441" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1441
+INSERT INTO [t1441] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t1441] VALUES (2,'أخبار الدجال لعبد الغني المقدسي','2','0','0');
