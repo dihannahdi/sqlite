f4693d2ce1db73781e2fb4acdbfef1a03336eb85
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:25 +0700

    Upload SQL chunk: t9667_chunk_0000.sql

---
+++ sql_chunks/t9667_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t9667
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t9667" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9667
+INSERT INTO [t9667] VALUES (1,'الفصل الأول أدلة وجود الخالق جل وعلا','3','0','0');
+INSERT INTO [t9667] VALUES (2,'الفصل الثاني التعريف بالله وربط القلوب به','31','0','0');
+INSERT INTO [t9667] VALUES (3,'الفصل الثالث توحيد الله','189','0','0');
+INSERT INTO [t9667] VALUES (4,'الفصل الرابع نظرة في تاريخ العقيدة','201','0','0');
+INSERT INTO [t9667] VALUES (5,'الفصل الخامس تصورات الأمم الضالة للمعبود','211','0','0');
