ea58e23c88935dfaef73d89254ab4f20219b591f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:22 +0700

    Upload SQL chunk: t21474_chunk_0000.sql

---
+++ sql_chunks/t21474_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t21474
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t21474" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21474
+INSERT INTO [t21474] VALUES (1,'الإلزامات والتتبع للدارقطني','1','0','0');
+INSERT INTO [t21474] VALUES (2,'وقال أبو بكر الخوارزمي: هذا آخر ما وجدته من هذا التعليق بخط أبي الحسن الدارقطني','302','0','0');
