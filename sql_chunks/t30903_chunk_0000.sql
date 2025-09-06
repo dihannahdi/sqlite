1c6ae5cdc3b499a26aa4ca876b1f524c2ff7d084
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:33 +0700

    Upload SQL chunk: t30903_chunk_0000.sql

---
+++ sql_chunks/t30903_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t30903
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t30903" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30903
+INSERT INTO [t30903] VALUES (1,'الأمر بالمعروف والنهي عن المنكر من الدين','1','0','0');
+INSERT INTO [t30903] VALUES (2,'مراتب الأمر بالمعروف والنهي عن المنكر','7','0','0');
+INSERT INTO [t30903] VALUES (3,'الشروط الواجب توافرها في الآمر والناهي','15','0','0');
+INSERT INTO [t30903] VALUES (4,'المعاصي سبب المصائب','18','0','0');
+INSERT INTO [t30903] VALUES (5,'أقسام الذنوب','25','0','0');
+INSERT INTO [t30903] VALUES (6,'أقسام الأنفس','28','0','0');
+INSERT INTO [t30903] VALUES (7,'ذم البخل والجبن','34','0','0');
+INSERT INTO [t30903] VALUES (8,'الصبر على المصائب','39','0','0');
+INSERT INTO [t30903] VALUES (9,'أصناف الناس في الشجاعة','44','0','0');
+INSERT INTO [t30903] VALUES (10,'أقسام الناس في الأمر بالمعروف والنهي عن المنكر','47','0','0');
+INSERT INTO [t30903] VALUES (11,'من هم أولو الأمر','50','0','0');
+INSERT INTO [t30903] VALUES (12,'فصل في الإخلاص والصواب','51','0','0');
+INSERT INTO [t30903] VALUES (13,'منزلة الإخلاص في الأمر بالمعروف والنهي عن المنكر','51','12','0');
+INSERT INTO [t30903] VALUES (14,'إسلام الوجه لله والإحسان هما أصلا الشريعة','55','12','0');
