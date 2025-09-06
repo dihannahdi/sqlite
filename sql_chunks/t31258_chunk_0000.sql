ca311850199be48fa060e743822fc782dc06f82c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:43 +0700

    Upload SQL chunk: t31258_chunk_0000.sql

---
+++ sql_chunks/t31258_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t31258
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t31258" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31258
+INSERT INTO [t31258] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t31258] VALUES (2,'حكم شرب الدخان','2','0','0');
+INSERT INTO [t31258] VALUES (3,'الدلائل القرآنية على تحريم شرب الدخان','3','0','0');
+INSERT INTO [t31258] VALUES (4,'إمامة شارب الدخان وغيره من العصاة في الصلاة','4','0','0');
+INSERT INTO [t31258] VALUES (5,'تقرير هام عن التدخين','5','0','0');
