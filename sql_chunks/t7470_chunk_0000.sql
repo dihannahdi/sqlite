477c5b2dc8abcd968b60c4b292339a33267d63da
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:40 +0700

    Upload SQL chunk: t7470_chunk_0000.sql

---
+++ sql_chunks/t7470_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t7470
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t7470" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7470
+INSERT INTO [t7470] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7470] VALUES (2,'من أحكام الحيض في الصلاة والصيام','2','0','0');
+INSERT INTO [t7470] VALUES (3,'من أحكام الطهارة في الصلاة','20','0','0');
+INSERT INTO [t7470] VALUES (4,'من أحكام الحيض في الحج والاعتمار','25','0','0');
