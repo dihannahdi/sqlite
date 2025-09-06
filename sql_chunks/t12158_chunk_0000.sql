fe65c855281ebc4cb658d1caff9988bd01a9d31e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:38 +0700

    Upload SQL chunk: t12158_chunk_0000.sql

---
+++ sql_chunks/t12158_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t12158
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t12158" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12158
+INSERT INTO [t12158] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12158] VALUES (2,'المبحث الأول: ترجمة ابن سفيان','4','0','0');
+INSERT INTO [t12158] VALUES (3,'المبحث الثاني: روايته لصحيح مسلم','13','0','0');
+INSERT INTO [t12158] VALUES (4,'المبحث الثالث: زياداته على صحيح مسلم','23','0','0');
+INSERT INTO [t12158] VALUES (5,'المبحث الرابع: تعليقاته على صحيح مسلم','55','0','0');
+INSERT INTO [t12158] VALUES (6,'الخاتمة','69','0','0');
+INSERT INTO [t12158] VALUES (7,'فهرس المراجع','70','0','0');
