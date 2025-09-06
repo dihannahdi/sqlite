58ee0c1cd4f71b81fe1d19b3a98377e46bc0f2b5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:56 +0700

    Upload SQL chunk: t6342_chunk_0000.sql

---
+++ sql_chunks/t6342_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t6342
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t6342" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6342
+INSERT INTO [t6342] VALUES (1,'زغل العلم','1','0','0');
+INSERT INTO [t6342] VALUES (2,'المقدمة','4','1','0');
+INSERT INTO [t6342] VALUES (3,'ترجمة المؤلف','7','1','0');
+INSERT INTO [t6342] VALUES (4,'نسخ الكتاب','15','1','0');
+INSERT INTO [t6342] VALUES (5,'علم القراءة والتجويد','21','1','0');
+INSERT INTO [t6342] VALUES (6,'علم الحديث','23','1','0');
+INSERT INTO [t6342] VALUES (7,'المالكية','29','1','0');
+INSERT INTO [t6342] VALUES (8,'الحنفية','30','1','0');
+INSERT INTO [t6342] VALUES (9,'الشافعية','32','1','0');
+INSERT INTO [t6342] VALUES (10,'الحنابلة','35','1','0');
+INSERT INTO [t6342] VALUES (11,'علم النحو','35','1','0');
+INSERT INTO [t6342] VALUES (12,'علم اللغة','36','1','0');
+INSERT INTO [t6342] VALUES (13,'علم التفسير','36','1','0');
+INSERT INTO [t6342] VALUES (14,'أصول الفقه','37','1','0');
+INSERT INTO [t6342] VALUES (15,'علم أصول الدين','37','1','0');
+INSERT INTO [t6342] VALUES (16,'علم المنطق','39','1','0');
+INSERT INTO [t6342] VALUES (17,'علم الحكمة','40','1','0');
+INSERT INTO [t6342] VALUES (18,'الفرائض','41','1','0');
+INSERT INTO [t6342] VALUES (19,'علم الإنشاء','42','1','0');
+INSERT INTO [t6342] VALUES (20,'علم الشعر','43','1','0');
+INSERT INTO [t6342] VALUES (21,'علم الحسا ب','44','1','0');
+INSERT INTO [t6342] VALUES (22,'علم الشروط','45','1','0');
+INSERT INTO [t6342] VALUES (23,'علم الوعظ','45','1','0');
+INSERT INTO [t6342] VALUES (24,'فهرس الأحاديث','48','1','0');
