1e3955b9177ee6806693cb570f38f6b0062f63a5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:17 +0700

    Upload SQL chunk: t446_chunk_0000.sql

---
+++ sql_chunks/t446_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t446
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t446" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t446
+INSERT INTO [t446] VALUES (1,'الحاجة إلى توسعة المسعى','2','0','0');
+INSERT INTO [t446] VALUES (2,'القول في توسعة المسعى كالقول في توسعة المطاف','4','0','0');
+INSERT INTO [t446] VALUES (3,'استطراد في حكم تغيير موضع مقام إبراهيم','6','0','0');
+INSERT INTO [t446] VALUES (4,'خلاصة رأي المؤلف','12','0','0');
