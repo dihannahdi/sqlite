bb6cba0465b012d7d44bae258c12b94bbebe8c02
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:47 +0700

    Upload SQL chunk: t6056_chunk_0000.sql

---
+++ sql_chunks/t6056_chunk_0000.sql
@@ -0,0 +1,58 @@
+-- Table: t6056
+-- Chunk: 1
+-- Rows: 51
+
+CREATE TABLE "t6056" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6056
+INSERT INTO [t6056] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6056] VALUES (2,'فصل','38','0','0');
+INSERT INTO [t6056] VALUES (3,'فصل','44','0','0');
+INSERT INTO [t6056] VALUES (4,'فصل','45','0','0');
+INSERT INTO [t6056] VALUES (5,'فصل','48','0','0');
+INSERT INTO [t6056] VALUES (6,'فصل','53','0','0');
+INSERT INTO [t6056] VALUES (7,'فصل','61','0','0');
+INSERT INTO [t6056] VALUES (8,'فصل','63','0','0');
+INSERT INTO [t6056] VALUES (9,'فصل','73','0','0');
+INSERT INTO [t6056] VALUES (10,'حرف النون','367','0','0');
+INSERT INTO [t6056] VALUES (11,'فصل','398','0','0');
+INSERT INTO [t6056] VALUES (12,'فصل','402','0','0');
+INSERT INTO [t6056] VALUES (13,'فصل','416','0','0');
+INSERT INTO [t6056] VALUES (14,'فصل','424','0','0');
+INSERT INTO [t6056] VALUES (15,'فصل','428','0','0');
+INSERT INTO [t6056] VALUES (16,'فصل','432','0','0');
+INSERT INTO [t6056] VALUES (17,'فصل','432','0','0');
+INSERT INTO [t6056] VALUES (18,'فصل','433','0','0');
+INSERT INTO [t6056] VALUES (19,'فصل','435','0','0');
+INSERT INTO [t6056] VALUES (20,'فصل','438','0','0');
+INSERT INTO [t6056] VALUES (21,'فصل','438','0','0');
+INSERT INTO [t6056] VALUES (22,'فصل','441','0','0');
+INSERT INTO [t6056] VALUES (23,'فصل','443','0','0');
+INSERT INTO [t6056] VALUES (24,'فصل','445','0','0');
+INSERT INTO [t6056] VALUES (25,'فصل','450','0','0');
+INSERT INTO [t6056] VALUES (26,'فصل','454','0','0');
+INSERT INTO [t6056] VALUES (27,'فصل','457','0','0');
+INSERT INTO [t6056] VALUES (28,'فصل','459','0','0');
+INSERT INTO [t6056] VALUES (29,'فصل','460','0','0');
+INSERT INTO [t6056] VALUES (30,'فصل','462','0','0');
+INSERT INTO [t6056] VALUES (31,'فصل','462','0','0');
+INSERT INTO [t6056] VALUES (32,'فصل','463','0','0');
+INSERT INTO [t6056] VALUES (33,'فصل','466','0','0');
+INSERT INTO [t6056] VALUES (34,'في ذكر جوامع وضوابط كلية في هذا الباب','466','0','0');
+INSERT INTO [t6056] VALUES (35,'فصل','468','0','0');
+INSERT INTO [t6056] VALUES (36,'فصل','469','0','0');
+INSERT INTO [t6056] VALUES (37,'فصل','469','0','0');
+INSERT INTO [t6056] VALUES (38,'فصل','472','0','0');
+INSERT INTO [t6056] VALUES (39,'فصل','473','0','0');
+INSERT INTO [t6056] VALUES (40,'فصل','474','0','0');
+INSERT INTO [t6056] VALUES (41,'فصل','477','0','0');
+INSERT INTO [t6056] VALUES (42,'فصل','479','0','0');
+INSERT INTO [t6056] VALUES (43,'فصل','480','0','0');
+INSERT INTO [t6056] VALUES (44,'فصل','481','0','0');
+INSERT INTO [t6056] VALUES (45,'فصل','484','0','0');
+INSERT INTO [t6056] VALUES (46,'فصل','484','0','0');
+INSERT INTO [t6056] VALUES (47,'فصل','487','0','0');
+INSERT INTO [t6056] VALUES (48,'فصل','488','0','0');
+INSERT INTO [t6056] VALUES (49,'فصل','490','0','0');
+INSERT INTO [t6056] VALUES (50,'فصل','493','0','0');
+INSERT INTO [t6056] VALUES (51,'فصل','494','0','0');
