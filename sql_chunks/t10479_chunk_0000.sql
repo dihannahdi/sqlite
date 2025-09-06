a1d3e5116348f140b37e4734386352ad8dc4f9a7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:12 +0700

    Upload SQL chunk: t10479_chunk_0000.sql

---
+++ sql_chunks/t10479_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10479
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10479" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10479
+INSERT INTO [t10479] VALUES (1,'أمالي الزبيدي','1','0','0');
