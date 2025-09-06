4d5ebb9e4ed922ad3d3591847fb2c916f723e5eb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:16 +0700

    Upload SQL chunk: t372_chunk_0000.sql

---
+++ sql_chunks/t372_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t372
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t372" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t372
+INSERT INTO [t372] VALUES (1,'{الحمد لله}','2','0','0');
+INSERT INTO [t372] VALUES (2,'استشكال ثبوت «قل» في أول الإخلاص والمعوذتين','2','1','0');
+INSERT INTO [t372] VALUES (3,'الألف واللام في «الحمد»','3','1','0');
+INSERT INTO [t372] VALUES (4,'معنى الحمد','4','1','0');
+INSERT INTO [t372] VALUES (5,'اللام في «لله» للاستحقاق','6','1','0');
+INSERT INTO [t372] VALUES (6,'{رب العالمين}','6','0','0');
+INSERT INTO [t372] VALUES (7,'معنى «الرب»','6','6','0');
+INSERT INTO [t372] VALUES (8,'معنى «العالمين»','7','6','0');
+INSERT INTO [t372] VALUES (9,'إشكالات في الثناء على المخلوق، والجواب عنها','7','6','0');
+INSERT INTO [t372] VALUES (10,'{رب العالمين} رد على المشركين الزاعمين أن لمعبوداتهم تدبيرا غيبيا فوضه الله تعالى إليها','15','6','0');
+INSERT INTO [t372] VALUES (11,'{الرحمن الرحيم}','19','0','0');
+INSERT INTO [t372] VALUES (12,'{مالك يوم الدين}','21','0','0');
+INSERT INTO [t372] VALUES (13,'من فوائد هذه الصفة','21','12','0');
+INSERT INTO [t372] VALUES (14,'تحقيق معنى «العبادة»','23','12','0');
+INSERT INTO [t372] VALUES (15,'أمور يجب استحضارها','30','12','0');
+INSERT INTO [t372] VALUES (16,'{وإياك نستعين}','37','0','0');
+INSERT INTO [t372] VALUES (17,'{اهدنا الصراط المستقيم}','40','0','0');
+INSERT INTO [t372] VALUES (18,'معنى الهداية ووجوهها','41','17','0');
+INSERT INTO [t372] VALUES (19,'الصراط المستقيم','44','17','0');
+INSERT INTO [t372] VALUES (20,'إعراب «صراط الذين»','48','17','0');
+INSERT INTO [t372] VALUES (21,'من المنعم عليهم؟','49','17','0');
+INSERT INTO [t372] VALUES (22,'{غير المغضوب عليهم ولا الضالين}','51','0','0');
+INSERT INTO [t372] VALUES (23,'مسألة: صفة الغضب لله','56','22','0');
