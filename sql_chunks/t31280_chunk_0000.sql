84ac886d88500f419eb2cbb786a4b22f21edb381
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:52 +0700

    Upload SQL chunk: t31280_chunk_0000.sql

---
+++ sql_chunks/t31280_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t31280
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t31280" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31280
+INSERT INTO [t31280] VALUES (1,'في أحكام صلاة الاستسقاء','1','0','0');
+INSERT INTO [t31280] VALUES (2,'تعريف ومشروعية الاستسقاء','1','1','0');
+INSERT INTO [t31280] VALUES (3,'حكم صلاة الاستسقاء','2','1','0');
+INSERT INTO [t31280] VALUES (4,'صفة صلاة الاستسقاء وأحكامها','3','1','0');
+INSERT INTO [t31280] VALUES (5,'تذكير الناس بعوامل وأسباب قبول صلاة الاستسقاء','4','1','0');
+INSERT INTO [t31280] VALUES (6,'في أحكام الجنائز','5','0','0');
+INSERT INTO [t31280] VALUES (7,'أولا أحكام المريض والمحتضر','6','6','0');
+INSERT INTO [t31280] VALUES (8,'ثانيا أحكام الوفاة','7','6','0');
+INSERT INTO [t31280] VALUES (9,'ثالثا تغسيل الميت','8','6','0');
+INSERT INTO [t31280] VALUES (10,'رابعا أحكام التكفين','9','6','0');
+INSERT INTO [t31280] VALUES (11,'خامسا أحكام الصلاة على الميت','10','6','0');
+INSERT INTO [t31280] VALUES (12,'سادسا حمل الميت ودفنه','11','6','0');
+INSERT INTO [t31280] VALUES (13,'سابعا: أحكام التعزية وزيارة القبور','12','6','0');
