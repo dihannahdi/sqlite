9df15157e00b22c3ccd634909adfa8b8f44ee311
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:47 +0700

    Upload SQL chunk: t5827_chunk_0000.sql

---
+++ sql_chunks/t5827_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t5827
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t5827" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5827
+INSERT INTO [t5827] VALUES (1,'[من مقدمة المستدرك]','1','0','0');
+INSERT INTO [t5827] VALUES (2,'أحمد بن محمد بن الصلت أبو العباس','3','0','0');
+INSERT INTO [t5827] VALUES (3,'حصين والد داود بن الحصين','4','0','0');
+INSERT INTO [t5827] VALUES (4,'سلمة بن تمام الشقري','5','0','0');
+INSERT INTO [t5827] VALUES (5,'سلمة بن سليمان الموصلي الأزدي','9','0','0');
+INSERT INTO [t5827] VALUES (6,'سلمة بن وهرام','11','0','0');
+INSERT INTO [t5827] VALUES (7,'سلمة بن الفضل أبو عبد الله الأبرش','15','0','0');
+INSERT INTO [t5827] VALUES (8,'سالم بن عبد الأعلى','18','0','0');
+INSERT INTO [t5827] VALUES (9,'سالم بن أبى حفصة العجلى','23','0','0');
+INSERT INTO [t5827] VALUES (10,'سالم بن عبد الله الخياط','25','0','0');
+INSERT INTO [t5827] VALUES (11,'شعبة ويقال محمد ويقال سالم ويقال اسمه أبو بكر بن عياش الكوفى مولى واصل بن حيان','28','0','0');
+INSERT INTO [t5827] VALUES (12,'معلى بن عبد الرحمن الواسطى','40','0','0');
