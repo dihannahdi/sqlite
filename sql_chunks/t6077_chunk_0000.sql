02188d54a13e6c25549f13dc95dfb4076257042a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:03 +0700

    Upload SQL chunk: t6077_chunk_0000.sql

---
+++ sql_chunks/t6077_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t6077
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t6077" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6077
+INSERT INTO [t6077] VALUES (1,'باب الهمزة','3','0','0');
+INSERT INTO [t6077] VALUES (2,'باب الباء','38','0','0');
+INSERT INTO [t6077] VALUES (3,'باب التاء','42','0','0');
+INSERT INTO [t6077] VALUES (4,'باب الثاء','46','0','0');
+INSERT INTO [t6077] VALUES (5,'باب الجيم','48','0','0');
+INSERT INTO [t6077] VALUES (6,'باب الحاء','50','0','0');
+INSERT INTO [t6077] VALUES (7,'باب الخاء','56','0','0');
+INSERT INTO [t6077] VALUES (8,'باب الدال','62','0','0');
+INSERT INTO [t6077] VALUES (9,'باب الذال المعجمة','69','0','0');
+INSERT INTO [t6077] VALUES (10,'باب الراء','71','0','0');
+INSERT INTO [t6077] VALUES (11,'باب الزاي','75','0','0');
+INSERT INTO [t6077] VALUES (12,'باب السين المهملة','78','0','0');
+INSERT INTO [t6077] VALUES (13,'باب الشين المعجمة','84','0','0');
+INSERT INTO [t6077] VALUES (14,'باب الصاد المهملة','89','0','0');
+INSERT INTO [t6077] VALUES (15,'باب الضاد المعجمة','96','0','0');
+INSERT INTO [t6077] VALUES (16,'باب الطاء المهملة','98','0','0');
+INSERT INTO [t6077] VALUES (17,'باب الظاء المعجمة','102','0','0');
+INSERT INTO [t6077] VALUES (18,'باب العين المهملة','106','0','0');
+INSERT INTO [t6077] VALUES (19,'باب الغين المعجمة','117','0','0');
+INSERT INTO [t6077] VALUES (20,'باب الفاء','118','0','0');
+INSERT INTO [t6077] VALUES (21,'باب القاف','123','0','0');
+INSERT INTO [t6077] VALUES (22,'باب الكاف','128','0','0');
+INSERT INTO [t6077] VALUES (23,'باب اللام','142','0','0');
+INSERT INTO [t6077] VALUES (24,'باب الميم','152','0','0');
+INSERT INTO [t6077] VALUES (25,'باب النون','202','0','0');
+INSERT INTO [t6077] VALUES (26,'باب الهاء','208','0','0');
+INSERT INTO [t6077] VALUES (27,'باب الواو','210','0','0');
+INSERT INTO [t6077] VALUES (28,'باب لام ألف','212','0','0');
+INSERT INTO [t6077] VALUES (29,'باب الياء','219','0','0');
+INSERT INTO [t6077] VALUES (30,'فائدة','225','0','0');
+INSERT INTO [t6077] VALUES (31,'خاتمة','226','0','0');
