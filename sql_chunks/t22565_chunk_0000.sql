8f503a9bf708366e96cf6ca967a38b447a246ff7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:39 +0700

    Upload SQL chunk: t22565_chunk_0000.sql

---
+++ sql_chunks/t22565_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t22565
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t22565" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22565
+INSERT INTO [t22565] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t22565] VALUES (2,'الأمر الأول: بيان حكم الدعوة إلى الله عز وجل وبيان فضلها:','13','0','0');
+INSERT INTO [t22565] VALUES (3,'فضل الدعوة إلى الله عز وجل','19','2','0');
+INSERT INTO [t22565] VALUES (4,'الأمر الثاني: كيفية أدائها وأساليبها:','24','0','0');
+INSERT INTO [t22565] VALUES (5,'الأمر الثالث: بيان الأمر الذي يدعى إليه:','29','0','0');
+INSERT INTO [t22565] VALUES (6,'الأمر الرابع: بيان الأخلاق والصفات التي ينبغي للدعاة أن يتخلقوا بها وأن يسيروا عليها:','42','0','0');
