badde3240eb9912b10c9db78f237fe9e903fd661
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:30:40 +0700

    Upload SQL chunk: t17662_chunk_0000.sql

---
+++ sql_chunks/t17662_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t17662
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t17662" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17662
+INSERT INTO [t17662] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t17662] VALUES (2,'مدخل اصطلاحي في مفهوم السيماء','19','0','0');
+INSERT INTO [t17662] VALUES (3,'الفصل الأول: المرأة وسيماء النفس','23','0','0');
+INSERT INTO [t17662] VALUES (4,'المبحث الأول: المرأة والنفس الواحدة','23','3','0');
+INSERT INTO [t17662] VALUES (5,'المبحث الثاني: السيماء التربوية لنفسية المرأة','37','3','0');
+INSERT INTO [t17662] VALUES (6,'الفصل الثاني: المرأة وسيماء الصورة','47','0','0');
+INSERT INTO [t17662] VALUES (7,'المبحث الأول: سيماء الصورة في التدافع الحضاري','47','6','0');
+INSERT INTO [t17662] VALUES (8,'المبحث الثاني: التأصيل الفقهي لسيماء الصورة في الإسلام','61','6','0');
+INSERT INTO [t17662] VALUES (9,'الخاتمة: نهاية فبداية','90','0','0');
