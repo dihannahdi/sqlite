2f14f958bc651602c7bf3b850d62f1361cb17d40
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:52 +0700

    Upload SQL chunk: t5838_chunk_0000.sql

---
+++ sql_chunks/t5838_chunk_0000.sql
@@ -0,0 +1,63 @@
+-- Table: t5838
+-- Chunk: 1
+-- Rows: 56
+
+CREATE TABLE "t5838" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5838
+INSERT INTO [t5838] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5838] VALUES (2,'حرف الألف','2','0','0');
+INSERT INTO [t5838] VALUES (3,'حرف الباء','25','0','0');
+INSERT INTO [t5838] VALUES (4,'حرف التاء','30','0','0');
+INSERT INTO [t5838] VALUES (5,'حرف الثاء','31','0','0');
+INSERT INTO [t5838] VALUES (6,'حرف الجيم','36','0','0');
+INSERT INTO [t5838] VALUES (7,'حرف الحاء','45','0','0');
+INSERT INTO [t5838] VALUES (8,'حرف الخاء','78','0','0');
+INSERT INTO [t5838] VALUES (9,'حرف الدال','88','0','0');
+INSERT INTO [t5838] VALUES (10,'حرف الذال','90','0','0');
+INSERT INTO [t5838] VALUES (11,'حرف الراء','91','0','0');
+INSERT INTO [t5838] VALUES (12,'حرف الزاي','98','0','0');
+INSERT INTO [t5838] VALUES (13,'حرف السين','109','0','0');
+INSERT INTO [t5838] VALUES (14,'حرف الشين','133','0','0');
+INSERT INTO [t5838] VALUES (15,'حرف الصاد','140','0','0');
+INSERT INTO [t5838] VALUES (16,'حرف الضاد','143','0','0');
+INSERT INTO [t5838] VALUES (17,'حرف الطاء','146','0','0');
+INSERT INTO [t5838] VALUES (18,'حرف الظاء','150','0','0');
+INSERT INTO [t5838] VALUES (19,'حرف العين','151','0','0');
+INSERT INTO [t5838] VALUES (20,'حرف الغين','244','0','0');
+INSERT INTO [t5838] VALUES (21,'حرف الفاء','246','0','0');
+INSERT INTO [t5838] VALUES (22,'حرف القاف','248','0','0');
+INSERT INTO [t5838] VALUES (23,'حرف الكاف','258','0','0');
+INSERT INTO [t5838] VALUES (24,'حرف اللام','261','0','0');
+INSERT INTO [t5838] VALUES (25,'حرف الميم','262','0','0');
+INSERT INTO [t5838] VALUES (26,'حرف النون','313','0','0');
+INSERT INTO [t5838] VALUES (27,'حرف الهاء','320','0','0');
+INSERT INTO [t5838] VALUES (28,'حرف الواو','325','0','0');
+INSERT INTO [t5838] VALUES (29,'حرف اللام ألف','329','0','0');
+INSERT INTO [t5838] VALUES (30,'حرف الياء','330','0','0');
+INSERT INTO [t5838] VALUES (31,'الكنى','346','0','0');
+INSERT INTO [t5838] VALUES (32,'أ','346','0','0');
+INSERT INTO [t5838] VALUES (33,'ب','346','0','0');
+INSERT INTO [t5838] VALUES (34,'ت','348','0','0');
+INSERT INTO [t5838] VALUES (35,'ج','348','0','0');
+INSERT INTO [t5838] VALUES (36,'ح','349','0','0');
+INSERT INTO [t5838] VALUES (37,'خ','351','0','0');
+INSERT INTO [t5838] VALUES (38,'د','353','0','0');
+INSERT INTO [t5838] VALUES (39,'ر','353','0','0');
+INSERT INTO [t5838] VALUES (40,'ز','353','0','0');
+INSERT INTO [t5838] VALUES (41,'س','354','0','0');
+INSERT INTO [t5838] VALUES (42,'ش','356','0','0');
+INSERT INTO [t5838] VALUES (43,'ص','356','0','0');
+INSERT INTO [t5838] VALUES (44,'ض','357','0','0');
+INSERT INTO [t5838] VALUES (45,'ط','357','0','0');
+INSERT INTO [t5838] VALUES (46,'ظ','357','0','0');
+INSERT INTO [t5838] VALUES (47,'ع','357','0','0');
+INSERT INTO [t5838] VALUES (48,'ف','360','0','0');
+INSERT INTO [t5838] VALUES (49,'ق','361','0','0');
+INSERT INTO [t5838] VALUES (50,'م','361','0','0');
+INSERT INTO [t5838] VALUES (51,'ن','364','0','0');
+INSERT INTO [t5838] VALUES (52,'هـ','364','0','0');
+INSERT INTO [t5838] VALUES (53,'و','364','0','0');
+INSERT INTO [t5838] VALUES (54,'ي','365','0','0');
+INSERT INTO [t5838] VALUES (55,'باب ذكر النساء المرسلات','366','0','0');
+INSERT INTO [t5838] VALUES (56,'باب المبهمات','369','0','0');
