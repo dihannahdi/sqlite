73d50c3c18f1c1e098733f960f18096668bd86e6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:18 +0700

    Upload SQL chunk: t1245_chunk_0000.sql

---
+++ sql_chunks/t1245_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t1245
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t1245" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1245
+INSERT INTO [t1245] VALUES (1,'مفهوم الوسطية الإسلامية','1','0','0');
+INSERT INTO [t1245] VALUES (2,'مضامين الوسطية الإسلامية','7','0','0');
+INSERT INTO [t1245] VALUES (3,'وسطية الإسلام تستلزم الابتعاد عن الإفراط والتفريط','7','2','0');
+INSERT INTO [t1245] VALUES (4,'مفهوم التطرف الديني ومظاهره','10','2','0');
+INSERT INTO [t1245] VALUES (5,'صور الوسطية الإسلامية','12','2','0');
+INSERT INTO [t1245] VALUES (6,'الوسطية الإسلامية منهج اجتماعي','17','0','0');
