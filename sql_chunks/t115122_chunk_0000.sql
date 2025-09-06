ec6ac29888098a28ef8ff13a9f7a6e180859c939
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:56 +0700

    Upload SQL chunk: t115122_chunk_0000.sql

---
+++ sql_chunks/t115122_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t115122
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t115122" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t115122
+INSERT INTO [t115122] VALUES (1,'الإهداء','3','0','0');
+INSERT INTO [t115122] VALUES (2,'كلمة للقارئ','4','0','0');
+INSERT INTO [t115122] VALUES (3,'توطئة','6','0','0');
+INSERT INTO [t115122] VALUES (4,'نشأة العلم الإسلامي','9','0','0');
+INSERT INTO [t115122] VALUES (5,'البعثة الدينية','10','0','0');
+INSERT INTO [t115122] VALUES (6,'التابعون الداخلون إفريقية والرواية عنهم','13','0','0');
+INSERT INTO [t115122] VALUES (7,'مشاركة الإفريقيين في العلم','14','0','0');
+INSERT INTO [t115122] VALUES (8,'تتابع الطبقات','17','0','0');
+INSERT INTO [t115122] VALUES (9,'كيف دخلت الحنفية إفريقية؟','19','0','0');
+INSERT INTO [t115122] VALUES (10,'المدرسة المالكية','23','0','0');
+INSERT INTO [t115122] VALUES (11,'تفرد المالكية بإفريقية','29','0','0');
+INSERT INTO [t115122] VALUES (12,'انفصال إفريقية عن الشرق','33','0','0');
+INSERT INTO [t115122] VALUES (13,'الإمام المازري، نشأته وتعلمه','48','0','0');
+INSERT INTO [t115122] VALUES (14,'ثناء العلماء عليه','54','0','0');
+INSERT INTO [t115122] VALUES (15,'آراء العلماء فيه','56','0','0');
+INSERT INTO [t115122] VALUES (16,'آثاره العلمية','58','0','0');
+INSERT INTO [t115122] VALUES (17,'نماذج من تحاريره','74','0','0');
+INSERT INTO [t115122] VALUES (18,'هجرة الصقليين إلى إفريقية','89','0','0');
+INSERT INTO [t115122] VALUES (19,'رفع التباس','91','0','0');
+INSERT INTO [t115122] VALUES (20,'المازريون','92','0','0');
+INSERT INTO [t115122] VALUES (21,'وفاته','94','0','0');
+INSERT INTO [t115122] VALUES (22,'ضريحه','94','0','0');
+INSERT INTO [t115122] VALUES (23,'إصلاح غلط','97','0','0');
