7d7c23eeb80736c0a590824fd9795f3e4e6cdfb1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:45 +0700

    Upload SQL chunk: t1414_chunk_0000.sql

---
+++ sql_chunks/t1414_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t1414
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t1414" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1414
+INSERT INTO [t1414] VALUES (1,'شبهات حول القرآن','1','0','0');
+INSERT INTO [t1414] VALUES (2,'تقديم','2','0','0');
+INSERT INTO [t1414] VALUES (3,'الشبهة الأولى','3','0','0');
+INSERT INTO [t1414] VALUES (4,'الشبهة الثانية','4','0','0');
+INSERT INTO [t1414] VALUES (5,'الشبهة الثالثة','5','0','0');
+INSERT INTO [t1414] VALUES (6,'الشبهة الرابعة','6','0','0');
+INSERT INTO [t1414] VALUES (7,'الشبهة الخامسة','7','0','0');
+INSERT INTO [t1414] VALUES (8,'الشبهة السادسة','8','0','0');
+INSERT INTO [t1414] VALUES (9,'الشبهة السابعة','9','0','0');
+INSERT INTO [t1414] VALUES (10,'الشبهة الثامنة','10','0','0');
+INSERT INTO [t1414] VALUES (11,'الشبهة التاسعة','11','0','0');
+INSERT INTO [t1414] VALUES (12,'الشبهة العاشرة','12','0','0');
+INSERT INTO [t1414] VALUES (13,'خاتمة','13','0','0');
+INSERT INTO [t1414] VALUES (14,'المصادر والمراجع','14','0','0');
