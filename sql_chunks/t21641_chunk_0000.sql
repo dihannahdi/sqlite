d5fe1a5ee0fa13e64ad6afce7f49de9ba9ab3bf8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:52 +0700

    Upload SQL chunk: t21641_chunk_0000.sql

---
+++ sql_chunks/t21641_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21641
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21641" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21641
+INSERT INTO [t21641] VALUES (1,'شيوخ الشريف ابي','2','0','0');
