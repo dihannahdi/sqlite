202fe44f1d75f6c7c65d99f3f1151eeac546145f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:22 +0700

    Upload SQL chunk: t36330_chunk_0000.sql

---
+++ sql_chunks/t36330_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t36330
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t36330" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36330
+INSERT INTO [t36330] VALUES (1,'الحمد لله تعالى الرزاق ذي القوة المتين','1','0','0');
+INSERT INTO [t36330] VALUES (2,'وإليك علامات هذا الطريق؛ حتى تتقيه .. وحتى تحاسب نفسك: هل أنت من سالكيه','5','0','0');
+INSERT INTO [t36330] VALUES (3,'أولا: حب الدنيا','5','2','0');
+INSERT INTO [t36330] VALUES (4,'ثانيا: طول الأمل','5','2','0');
+INSERT INTO [t36330] VALUES (5,'ثالثا المعاصي: المعاصي تصد عن الطاعات','6','2','0');
+INSERT INTO [t36330] VALUES (6,'* منها: التهاون بالذنب','6','5','0');
+INSERT INTO [t36330] VALUES (7,'* ومنها تسويف التوبة','7','5','0');
+INSERT INTO [t36330] VALUES (8,'* ومنها قسوة القلب','7','5','0');
+INSERT INTO [t36330] VALUES (9,'* الإكثار من فعل الطاعات','9','5','0');
+INSERT INTO [t36330] VALUES (10,'* الإكثار من ذكر الله تعالى','9','5','0');
+INSERT INTO [t36330] VALUES (11,'* تذكير القبر','9','5','0');
