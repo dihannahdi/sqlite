d74b295901557e262449853de441ac122a30ba17
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:16 +0700

    Upload SQL chunk: t13147_chunk_0000.sql

---
+++ sql_chunks/t13147_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t13147
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t13147" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13147
+INSERT INTO [t13147] VALUES (1,'نسخة زين بن شعيب الإسكندراني','869','0','0');
+INSERT INTO [t13147] VALUES (2,'نسخة نافع بن أبي نعيم القارئ','1030','0','0');
+INSERT INTO [t13147] VALUES (3,'ومن أحاديث جناح بن عباد مولى الوليد بن عبد الملك رواية حماد بن صالح مولى بني أمية عنه','1115','0','0');
+INSERT INTO [t13147] VALUES (4,'أحاديث جميع بن ثوب الرحبي','1557','0','0');
