d9840903da7d2c031c5ee309f7cc8bfed9e8ff8b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:46 +0700

    Upload SQL chunk: t23078_chunk_0000.sql

---
+++ sql_chunks/t23078_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t23078
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t23078" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23078
+INSERT INTO [t23078] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t23078] VALUES (2,'مقدمة الغزوة','3','0','0');
+INSERT INTO [t23078] VALUES (3,'سبب الغزوة، ووصف المعركة، وفضل الله على المسلمين فيها','9','0','0');
+INSERT INTO [t23078] VALUES (4,'رابعا: أسباب النصر وعوامله','24','0','0');
+INSERT INTO [t23078] VALUES (5,'مدخل','24','4','0');
+INSERT INTO [t23078] VALUES (6,'أولا: الثبات عند لقاء العدو','26','4','0');
+INSERT INTO [t23078] VALUES (7,'ثانيا: الإكثار من ذكر الله','28','4','0');
+INSERT INTO [t23078] VALUES (8,'ثالثا: طاعة الله ورسوله','29','4','0');
+INSERT INTO [t23078] VALUES (9,'رابعا: اتقاء النزاع','30','4','0');
+INSERT INTO [t23078] VALUES (10,'خامسا: الصبر عند لقاء العدو','31','4','0');
+INSERT INTO [t23078] VALUES (11,'سابعا: اتقاء البطر والرياء','32','4','0');
+INSERT INTO [t23078] VALUES (12,'سابعا: التوكل على الله','33','4','0');
+INSERT INTO [t23078] VALUES (13,'خامسا: الأسرى وموقف الرسول منهم','34','0','0');
+INSERT INTO [t23078] VALUES (14,'خامسا: الأسرى وموقف الرسول منهم','34','13','0');
+INSERT INTO [t23078] VALUES (15,'سادسا: الخاتمة','35','0','0');
+INSERT INTO [t23078] VALUES (16,'الخاتمة','35','15','0');
