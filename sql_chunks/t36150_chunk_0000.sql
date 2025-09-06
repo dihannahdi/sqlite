2ba27b3863ae463b96600725cb828a6a2bed5dda
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:47 +0700

    Upload SQL chunk: t36150_chunk_0000.sql

---
+++ sql_chunks/t36150_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t36150
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t36150" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36150
+INSERT INTO [t36150] VALUES (1,'الأعيان المدفونون بسبتة','2','0','0');
+INSERT INTO [t36150] VALUES (2,'المساجد','17','0','0');
+INSERT INTO [t36150] VALUES (3,'الخزائن العلمية','19','0','0');
+INSERT INTO [t36150] VALUES (4,'الربط والزوايا','20','0','0');
+INSERT INTO [t36150] VALUES (5,'المحارس','22','0','0');
+INSERT INTO [t36150] VALUES (6,'الأزقة','23','0','0');
+INSERT INTO [t36150] VALUES (7,'الحمامات','24','0','0');
+INSERT INTO [t36150] VALUES (8,'الأسواق','26','0','0');
+INSERT INTO [t36150] VALUES (9,'الحوانيت','26','0','0');
+INSERT INTO [t36150] VALUES (10,'التربيعات','27','0','0');
+INSERT INTO [t36150] VALUES (11,'المنجرات','27','0','0');
+INSERT INTO [t36150] VALUES (12,'الفنادق','28','0','0');
+INSERT INTO [t36150] VALUES (13,'الأفران','29','0','0');
+INSERT INTO [t36150] VALUES (14,'السقايات','29','0','0');
+INSERT INTO [t36150] VALUES (15,'الميضآت','30','0','0');
+INSERT INTO [t36150] VALUES (16,'ديار الإشراف','31','0','0');
+INSERT INTO [t36150] VALUES (17,'المطامير','32','0','0');
+INSERT INTO [t36150] VALUES (18,'الطواحين','33','0','0');
+INSERT INTO [t36150] VALUES (19,'الأرباض','33','0','0');
+INSERT INTO [t36150] VALUES (20,'الأبواب','34','0','0');
+INSERT INTO [t36150] VALUES (21,'الحفائر','36','0','0');
+INSERT INTO [t36150] VALUES (22,'المصليات','36','0','0');
+INSERT INTO [t36150] VALUES (23,'المرامي','37','0','0');
+INSERT INTO [t36150] VALUES (24,'المقاصر','39','0','0');
+INSERT INTO [t36150] VALUES (25,'المقبرات','40','0','0');
+INSERT INTO [t36150] VALUES (26,'المراسي','40','0','0');
+INSERT INTO [t36150] VALUES (27,'المضارب والمصايد','41','0','0');
+INSERT INTO [t36150] VALUES (28,'قرية بنيونش','42','0','0');
