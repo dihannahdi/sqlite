b2a4fa19be974d3b0883e5c24d53c0a21e08b436
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:31 +0700

    Upload SQL chunk: t10052_chunk_0000.sql

---
+++ sql_chunks/t10052_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10052
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10052" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10052
+INSERT INTO [t10052] VALUES (1,'تحية المسجد','1','0','0');
+INSERT INTO [t10052] VALUES (2,'للدكتور محمد ضياء الرحمن الأعظمي','1','0','0');
