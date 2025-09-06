66baa1884ca90249353e71da6ea68baaa7dcc4ee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:31 +0700

    Upload SQL chunk: t37488_chunk_0000.sql

---
+++ sql_chunks/t37488_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t37488
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t37488" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37488
+INSERT INTO [t37488] VALUES (1,'معلقة امرئ القيس','1','0','0');
+INSERT INTO [t37488] VALUES (2,'معلقة طرفة بن العبد البكري','54','0','0');
+INSERT INTO [t37488] VALUES (3,'معلقة زهير بن أبي سلمى المزني','100','0','0');
+INSERT INTO [t37488] VALUES (4,'معلقة لبيد بن ربيعة العامري','128','0','0');
+INSERT INTO [t37488] VALUES (5,'معلقة عنترة بن شداد العبسي','175','0','0');
+INSERT INTO [t37488] VALUES (6,'معلقة عمرو بن كلثوم التغلبي','214','0','0');
+INSERT INTO [t37488] VALUES (7,'معلقة الحارث بن حلزة اليشكري','248','0','0');
+INSERT INTO [t37488] VALUES (8,'قصيدة الأعشى ميمون بن قيس','286','0','0');
+INSERT INTO [t37488] VALUES (9,'قصيدة النابغة الذبياني','306','0','0');
+INSERT INTO [t37488] VALUES (10,'قصيدة عبيد بن الأبرص الأسدي','322','0','0');
