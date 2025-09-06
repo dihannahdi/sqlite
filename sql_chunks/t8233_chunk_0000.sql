f1838cfa0da3213f947a84a17d69a37f44e8ee51
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:44 +0700

    Upload SQL chunk: t8233_chunk_0000.sql

---
+++ sql_chunks/t8233_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t8233
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t8233" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8233
+INSERT INTO [t8233] VALUES (1,'المجلس الأول','1','0','0');
+INSERT INTO [t8233] VALUES (2,'المجلس الثاني بعده','19','0','0');
+INSERT INTO [t8233] VALUES (3,'المجلس الثالث','34','0','0');
+INSERT INTO [t8233] VALUES (4,'المجلس الرابع','50','0','0');
+INSERT INTO [t8233] VALUES (5,'المجلس الخامس','65','0','0');
+INSERT INTO [t8233] VALUES (6,'المجلس السادس','75','0','0');
+INSERT INTO [t8233] VALUES (7,'المجلس السابع','86','0','0');
