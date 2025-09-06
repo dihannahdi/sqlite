0011fb7c412e2f1626ba3b9394c25d5d9d655b26
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:24 +0700

    Upload SQL chunk: t26097_chunk_0000.sql

---
+++ sql_chunks/t26097_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t26097
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t26097" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26097
+INSERT INTO [t26097] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t26097] VALUES (2,'مقدمة','1','0','0');
+INSERT INTO [t26097] VALUES (3,'مذهب أبي عبيد','9','0','0');
+INSERT INTO [t26097] VALUES (4,'حرف الهمزة','9','0','0');
+INSERT INTO [t26097] VALUES (5,'حرف الباء','21','0','0');
+INSERT INTO [t26097] VALUES (6,'حرف التاء','23','0','0');
+INSERT INTO [t26097] VALUES (7,'حرف الجيم','24','0','0');
+INSERT INTO [t26097] VALUES (8,'حرف الحاء','26','0','0');
+INSERT INTO [t26097] VALUES (9,'حرف الدال','30','0','0');
+INSERT INTO [t26097] VALUES (10,'حرف الراء','33','0','0');
+INSERT INTO [t26097] VALUES (11,'حرف الزاي','38','0','0');
+INSERT INTO [t26097] VALUES (12,'حرف السين','39','0','0');
+INSERT INTO [t26097] VALUES (13,'حرف الشين','47','0','0');
+INSERT INTO [t26097] VALUES (14,'حرف الطاء','54','0','0');
+INSERT INTO [t26097] VALUES (15,'حرف العين','60','0','0');
+INSERT INTO [t26097] VALUES (16,'حرف الغين','62','0','0');
+INSERT INTO [t26097] VALUES (17,'حرف الفاء','64','0','0');
+INSERT INTO [t26097] VALUES (18,'حرف القاف','67','0','0');
+INSERT INTO [t26097] VALUES (19,'حرف الكاف','77','0','0');
+INSERT INTO [t26097] VALUES (20,'حرف اللام','82','0','0');
+INSERT INTO [t26097] VALUES (21,'حرف النون','94','0','0');
+INSERT INTO [t26097] VALUES (22,'حرف الهاء','95','0','0');
+INSERT INTO [t26097] VALUES (23,'حرف الواو','100','0','0');
