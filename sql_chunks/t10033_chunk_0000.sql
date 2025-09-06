32301a1694851789690f0ee406a929276ca203ab
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:22 +0700

    Upload SQL chunk: t10033_chunk_0000.sql

---
+++ sql_chunks/t10033_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t10033
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t10033" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10033
+INSERT INTO [t10033] VALUES (1,'جغرافيتها من حيث الموقع والحدود والعاصمة والمدن الهامة والأماكن السياحية','1','0','0');
+INSERT INTO [t10033] VALUES (2,'تاريخ دخول الإسلام في الفلبين','3','0','0');
+INSERT INTO [t10033] VALUES (3,'بداية الصراع','5','0','0');
+INSERT INTO [t10033] VALUES (4,'نشاط المسلمين في نصر دينهم','7','0','0');
+INSERT INTO [t10033] VALUES (5,'نشاط أعداء الإسلام في نشر المبادئ الهدامة','10','0','0');
+INSERT INTO [t10033] VALUES (6,'نظام البلاد في الماضي والحاضر','12','0','0');
+INSERT INTO [t10033] VALUES (7,'حالة مسلمي الفلبين الحاضرة','14','0','0');
+INSERT INTO [t10033] VALUES (8,'بعض الحوادث التي وقعت على المسلمين','16','0','0');
+INSERT INTO [t10033] VALUES (9,'أسباب مأساة مسلمي الفلبين','19','0','0');
+INSERT INTO [t10033] VALUES (10,'بعض الأحداث التي حدثت على المسلمين من قريب','25','0','0');
