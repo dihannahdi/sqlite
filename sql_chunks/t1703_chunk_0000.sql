1d16360fe4a202bf28279a93e1b78d5f9aad3c14
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:28:05 +0700

    Upload SQL chunk: t1703_chunk_0000.sql

---
+++ sql_chunks/t1703_chunk_0000.sql
@@ -0,0 +1,64 @@
+-- Table: t1703
+-- Chunk: 1
+-- Rows: 57
+
+CREATE TABLE "t1703" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1703
+INSERT INTO [t1703] VALUES (1,'مقدمة التحقيق','2','0','0');
+INSERT INTO [t1703] VALUES (2,'القول في الجزء الأول من هذا الكتاب','7','0','0');
+INSERT INTO [t1703] VALUES (3,'القسم الأول','9','2','0');
+INSERT INTO [t1703] VALUES (4,'القول في القسم الثاني من الجزء الأول','12','2','0');
+INSERT INTO [t1703] VALUES (5,'فصل:','12','4','0');
+INSERT INTO [t1703] VALUES (6,'فصل:','14','4','0');
+INSERT INTO [t1703] VALUES (7,'مسألة أولى:','15','6','0');
+INSERT INTO [t1703] VALUES (8,'مسألة ثانية:','15','6','0');
+INSERT INTO [t1703] VALUES (9,'مسألة ثالثة:','16','6','0');
+INSERT INTO [t1703] VALUES (10,'مسألة رابعة:','18','6','0');
+INSERT INTO [t1703] VALUES (11,'القول في القسم الثالث من الجزء الأول','19','2','0');
+INSERT INTO [t1703] VALUES (12,'القول في القسم الرابع:','26','2','0');
+INSERT INTO [t1703] VALUES (13,'الفصل الأول:','26','12','0');
+INSERT INTO [t1703] VALUES (14,'الفصل الثاني:','26','12','0');
+INSERT INTO [t1703] VALUES (15,'الفصل الثالث:','27','12','0');
+INSERT INTO [t1703] VALUES (16,'الفصل الرابع:','28','12','0');
+INSERT INTO [t1703] VALUES (17,'القول في الجزء الثاني من هذا الكتاب','29','0','0');
+INSERT INTO [t1703] VALUES (18,'الأصل الأول','31','17','0');
+INSERT INTO [t1703] VALUES (19,'القول في الأصل الثاني وهو السنة','34','17','0');
+INSERT INTO [t1703] VALUES (20,'الفصل الأول:','39','19','0');
+INSERT INTO [t1703] VALUES (21,'الفصل الثاني:','40','19','0');
+INSERT INTO [t1703] VALUES (22,'الفصل الثالث:','43','19','0');
+INSERT INTO [t1703] VALUES (23,'القول في الناسخ والمنسوخ','52','19','0');
+INSERT INTO [t1703] VALUES (24,'مسألة:','53','23','0');
+INSERT INTO [t1703] VALUES (25,'مسألة:','53','23','0');
+INSERT INTO [t1703] VALUES (26,'مسألة:','53','23','0');
+INSERT INTO [t1703] VALUES (27,'مسألة:','54','23','0');
+INSERT INTO [t1703] VALUES (28,'مسألة:','54','23','0');
+INSERT INTO [t1703] VALUES (29,'مسألة:','54','23','0');
+INSERT INTO [t1703] VALUES (30,'مسألة:','56','23','0');
+INSERT INTO [t1703] VALUES (31,'مسألة:','56','23','0');
+INSERT INTO [t1703] VALUES (32,'مسألة:','57','23','0');
+INSERT INTO [t1703] VALUES (33,'القول في الأصل الثالث من أصول الأدلة وهو الإجماع','58','17','0');
+INSERT INTO [t1703] VALUES (34,'القول في الأصل الرابع','64','17','0');
+INSERT INTO [t1703] VALUES (35,'القول في الجزء الثالث من المختصر','67','0','0');
+INSERT INTO [t1703] VALUES (36,'القول في النص والمجمل من جهة الصيغة','69','35','0');
+INSERT INTO [t1703] VALUES (37,'القول في الظاهر والمؤول من جهة الصيغة','75','35','0');
+INSERT INTO [t1703] VALUES (38,'مسألة:','79','37','0');
+INSERT INTO [t1703] VALUES (39,'مسألة:','79','37','0');
+INSERT INTO [t1703] VALUES (40,'مسألة:','79','37','0');
+INSERT INTO [t1703] VALUES (41,'مسألة:','79','37','0');
+INSERT INTO [t1703] VALUES (42,'مسألة:','80','37','0');
+INSERT INTO [t1703] VALUES (43,'مسألة:','84','37','0');
+INSERT INTO [t1703] VALUES (44,'القول في الألفاظ الخاصة','85','35','0');
+INSERT INTO [t1703] VALUES (45,'القول في دلالات الألفاظ بمفهومها','86','35','0');
+INSERT INTO [t1703] VALUES (46,'القول في الأوامر والنواهي','88','35','0');
+INSERT INTO [t1703] VALUES (47,'مسألة:','90','46','0');
+INSERT INTO [t1703] VALUES (48,'مسألة:','91','46','0');
+INSERT INTO [t1703] VALUES (49,'مسألة:','91','46','0');
+INSERT INTO [t1703] VALUES (50,'القول في القياس','92','35','0');
+INSERT INTO [t1703] VALUES (51,'القول في الإقرار','101','35','0');
+INSERT INTO [t1703] VALUES (52,'القول في الفعل','101','35','0');
+INSERT INTO [t1703] VALUES (53,'القول في الجزء الرابع','103','0','0');
+INSERT INTO [t1703] VALUES (54,'الفصل الأول: في الاجتهاد','105','53','0');
+INSERT INTO [t1703] VALUES (55,'الفصل الثاني: القول في التقليد','111','53','0');
+INSERT INTO [t1703] VALUES (56,'فصل:','112','55','0');
+INSERT INTO [t1703] VALUES (57,'الفصل الثالث: في ترجيح طرق النقل','114','53','0');
