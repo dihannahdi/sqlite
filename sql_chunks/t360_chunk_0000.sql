72e30e9dd647dbe216bb4aacf9df20fb525c01d2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:21 +0700

    Upload SQL chunk: t360_chunk_0000.sql

---
+++ sql_chunks/t360_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t360
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t360" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t360
+INSERT INTO [t360] VALUES (1,'كتاب الطهارة','1','0','0');
+INSERT INTO [t360] VALUES (2,'كتاب الصلاة','139','0','0');
+INSERT INTO [t360] VALUES (3,'كتاب الأذان','198','0','0');
+INSERT INTO [t360] VALUES (4,'كتاب المساجد','219','0','0');
+INSERT INTO [t360] VALUES (5,'كتاب القبلة','241','0','0');
+INSERT INTO [t360] VALUES (6,'كتاب الإمامة','249','0','0');
+INSERT INTO [t360] VALUES (7,'كتاب الافتتاح','270','0','0');
+INSERT INTO [t360] VALUES (8,'كتاب الجمعة','372','0','0');
+INSERT INTO [t360] VALUES (9,'كتاب تقصير الصلاة في السفر','392','0','0');
+INSERT INTO [t360] VALUES (10,'كتاب الكسوف','394','0','0');
+INSERT INTO [t360] VALUES (11,'كتاب الاستسقاء','418','0','0');
+INSERT INTO [t360] VALUES (12,'كتاب العيدين','429','0','0');
+INSERT INTO [t360] VALUES (13,'كتاب قيام الليل وتطوع النهار','440','0','0');
+INSERT INTO [t360] VALUES (14,'كتاب الصوم','548','0','0');
+INSERT INTO [t360] VALUES (15,'كتاب الزكاة','595','0','0');
+INSERT INTO [t360] VALUES (16,'كتاب مناسك الحج','684','0','0');
+INSERT INTO [t360] VALUES (17,'كتاب الجهاد','771','0','0');
+INSERT INTO [t360] VALUES (18,'كتاب النكاح','790','0','0');
+INSERT INTO [t360] VALUES (19,'كتاب الطلاق','836','0','0');
+INSERT INTO [t360] VALUES (20,'كتاب الخيل','877','0','0');
+INSERT INTO [t360] VALUES (21,'كتاب الأحباس','884','0','0');
+INSERT INTO [t360] VALUES (22,'كتاب الوصايا','885','0','0');
+INSERT INTO [t360] VALUES (23,'كتاب الإيمان والنذور','896','0','0');
+INSERT INTO [t360] VALUES (24,'كتاب المزارعة','899','0','0');
+INSERT INTO [t360] VALUES (25,'كتاب عشرة النساء','901','0','0');
+INSERT INTO [t360] VALUES (26,'كتاب تحريم الدم','910','0','0');
+INSERT INTO [t360] VALUES (27,'كتاب البيعة','930','0','0');
+INSERT INTO [t360] VALUES (28,'كتاب العقيقة','945','0','0');
+INSERT INTO [t360] VALUES (29,'كتاب البيوع','973','0','0');
+INSERT INTO [t360] VALUES (30,'كتاب الإيمان وشرائعه','1027','0','0');
+INSERT INTO [t360] VALUES (31,'كتاب الزينة','1056','0','0');
+INSERT INTO [t360] VALUES (32,'كتاب آداب القضاة','1093','0','0');
+INSERT INTO [t360] VALUES (33,'كتاب اللاستعاذة','1107','0','0');
+INSERT INTO [t360] VALUES (34,'كتاب الأشربة','1119','0','0');
