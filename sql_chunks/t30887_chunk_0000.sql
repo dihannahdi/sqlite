3910c9d214bf321ea917d4137ee77a3dcb502e62
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:25 +0700

    Upload SQL chunk: t30887_chunk_0000.sql

---
+++ sql_chunks/t30887_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t30887
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t30887" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30887
+INSERT INTO [t30887] VALUES (1,'سنة ولادته صلى الله عليه وسلم','1','0','0');
+INSERT INTO [t30887] VALUES (2,'الشهر الذي ولد فيه النبي صلى الله عليه وسلم','2','0','0');
+INSERT INTO [t30887] VALUES (3,'يوم ولادته صلى الله عليه وسلم','2','0','0');
+INSERT INTO [t30887] VALUES (4,'وقت ولادته صلى الله عليه وسلم','3','0','0');
+INSERT INTO [t30887] VALUES (5,'حكم الاحتفال بمولده صلى الله عليه وسلم','3','0','0');
