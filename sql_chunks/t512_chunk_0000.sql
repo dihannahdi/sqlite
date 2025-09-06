18e61bcda25fc5504bfde6b5291dfb6d52868bb4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:46 +0700

    Upload SQL chunk: t512_chunk_0000.sql

---
+++ sql_chunks/t512_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t512
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t512" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t512
+INSERT INTO [t512] VALUES (1,'رياض الصالحين','1','0','0');
