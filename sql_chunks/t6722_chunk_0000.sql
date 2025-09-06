77ad1ed426147c3502fa67d6eccd624cda6adf56
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:14 +0700

    Upload SQL chunk: t6722_chunk_0000.sql

---
+++ sql_chunks/t6722_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t6722
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t6722" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6722
+INSERT INTO [t6722] VALUES (1,'للحافظ العسكري','1','0','0');
+INSERT INTO [t6722] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t6722] VALUES (3,'حدثنا الشيخ الإمام العالم الزاهد','2','0','0');
+INSERT INTO [t6722] VALUES (4,'آخر الكتاب','36','0','0');
