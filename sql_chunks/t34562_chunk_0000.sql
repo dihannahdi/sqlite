2e4d7d78b103e1380f16f1ceed161782ac0da368
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:39 +0700

    Upload SQL chunk: t34562_chunk_0000.sql

---
+++ sql_chunks/t34562_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t34562
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t34562" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t34562
+INSERT INTO [t34562] VALUES (1,'المقدمة','3','0','0');
+INSERT INTO [t34562] VALUES (2,'فضل الدعاء','6','0','0');
+INSERT INTO [t34562] VALUES (3,'آداب الدعاء','8','0','0');
+INSERT INTO [t34562] VALUES (4,'من يستجيب الله عز وجل لهم','10','0','0');
+INSERT INTO [t34562] VALUES (5,'موانع الإجابة','12','0','0');
+INSERT INTO [t34562] VALUES (6,'أوقات الإجابة','14','0','0');
+INSERT INTO [t34562] VALUES (7,'أماكن الإجابة','16','0','0');
+INSERT INTO [t34562] VALUES (8,'الدعاء','17','0','0');
+INSERT INTO [t34562] VALUES (9,'المراجع','113','0','0');
+INSERT INTO [t34562] VALUES (10,'الفهرس','114','0','0');
