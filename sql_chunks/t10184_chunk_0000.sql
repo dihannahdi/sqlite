07eaacaf078e9d0a78a259c938c5b3cb21093abc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:11 +0700

    Upload SQL chunk: t10184_chunk_0000.sql

---
+++ sql_chunks/t10184_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t10184
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t10184" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10184
+INSERT INTO [t10184] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10184] VALUES (2,'النص النبوي الشريف','2','0','0');
+INSERT INTO [t10184] VALUES (3,'سند الحديث','4','0','0');
+INSERT INTO [t10184] VALUES (4,'شرح الحديث','6','0','0');
+INSERT INTO [t10184] VALUES (5,'المسألة الأولى: مسألة فقهية','9','0','0');
+INSERT INTO [t10184] VALUES (6,'المسألة الثانية: مسألة طبية','11','0','0');
+INSERT INTO [t10184] VALUES (7,'أقوال الأطباء المسلمون','13','0','0');
