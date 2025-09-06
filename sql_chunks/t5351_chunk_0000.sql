7b06c0ec27a2d6b3651a906c65c7102ad51e5660
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:12 +0700

    Upload SQL chunk: t5351_chunk_0000.sql

---
+++ sql_chunks/t5351_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t5351
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t5351" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5351
+INSERT INTO [t5351] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5351] VALUES (2,'جملة من النظم والنثر فقي وصف النور والزهر','14','0','0');
+INSERT INTO [t5351] VALUES (3,'فقر للنبيذيين','45','0','0');
+INSERT INTO [t5351] VALUES (4,'فصل لأبي بكر الخوارزمي','47','0','0');
