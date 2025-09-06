fe245a68fc4cf314fe292d0661212a3374e282ca
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:32 +0700

    Upload SQL chunk: t37489_chunk_0000.sql

---
+++ sql_chunks/t37489_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t37489
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t37489" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37489
+INSERT INTO [t37489] VALUES (1,'ذكر ما يحتمله الشعر','2','0','0');
+INSERT INTO [t37489] VALUES (2,'ذكر أنواع الضرائر','5','0','0');
+INSERT INTO [t37489] VALUES (3,'فصل الزيادة','5','2','0');
+INSERT INTO [t37489] VALUES (4,'فصل النقص','72','2','0');
+INSERT INTO [t37489] VALUES (5,'فصل التقديم والتأخير','175','2','0');
+INSERT INTO [t37489] VALUES (6,'فصل البدل','204','2','0');
