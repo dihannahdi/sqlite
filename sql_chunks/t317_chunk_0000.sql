93150ddfd3d0386145a49495f9681f33ab61adfd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:43 +0700

    Upload SQL chunk: t317_chunk_0000.sql

---
+++ sql_chunks/t317_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t317
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t317" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t317
+INSERT INTO [t317] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t317] VALUES (2,'اللهم صل على سيدنا محمد وآله وسلم','1','0','0');
+INSERT INTO [t317] VALUES (3,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t317] VALUES (4,'نسبه ونسبته','1','0','0');
+INSERT INTO [t317] VALUES (5,'مولده ونشأته','2','0','0');
+INSERT INTO [t317] VALUES (6,'انتقاله إلى دمشق','2','0','0');
+INSERT INTO [t317] VALUES (7,'سفره للحج','3','0','0');
+INSERT INTO [t317] VALUES (8,'اشتغاله بالعلم','3','0','0');
+INSERT INTO [t317] VALUES (9,'شيوخه','4','0','0');
+INSERT INTO [t317] VALUES (10,'مسموعاته','7','0','0');
+INSERT INTO [t317] VALUES (11,'تصانيفه ومؤلفاته','7','0','0');
+INSERT INTO [t317] VALUES (12,'نبذة من كلام العلماء في كتبه','10','0','0');
+INSERT INTO [t317] VALUES (13,'الوظائف التي تولاها','18','0','0');
+INSERT INTO [t317] VALUES (14,'تلاميذه','19','0','0');
+INSERT INTO [t317] VALUES (15,'الوظائف التي تولاها','21','0','0');
+INSERT INTO [t317] VALUES (16,'من شعره','22','0','0');
+INSERT INTO [t317] VALUES (17,'تلاميذه','23','0','0');
+INSERT INTO [t317] VALUES (18,'صفاته ومناقبه','25','0','0');
+INSERT INTO [t317] VALUES (19,'تقدمه في العلم وكثرة عبادته','27','0','0');
+INSERT INTO [t317] VALUES (20,'ورعه وخشونة عيشه','28','0','0');
+INSERT INTO [t317] VALUES (21,'مناصحته لحكام عصره','30','0','0');
+INSERT INTO [t317] VALUES (22,'من أفرده بالترجمة','39','0','0');
+INSERT INTO [t317] VALUES (23,'من ترجمه مع غيره','41','0','0');
+INSERT INTO [t317] VALUES (24,'سلسلته في الفقه وأهمية معرفتها','44','0','0');
+INSERT INTO [t317] VALUES (25,'من رواة كتبه','45','0','0');
+INSERT INTO [t317] VALUES (26,'مرويات السخاوي عنه','47','0','0');
