4dade7db750b3863010aa45a635c342de74b920e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:05 +0700

    Upload SQL chunk: t36079_chunk_0000.sql

---
+++ sql_chunks/t36079_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t36079
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t36079" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36079
+INSERT INTO [t36079] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t36079] VALUES (2,'انتهت الرسالة','25','0','0');
