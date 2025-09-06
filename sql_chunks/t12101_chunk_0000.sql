ee43fa3c8bee71c7dbbacc32f6cbd460e0ecebc1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:55 +0700

    Upload SQL chunk: t12101_chunk_0000.sql

---
+++ sql_chunks/t12101_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t12101
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t12101" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12101
+INSERT INTO [t12101] VALUES (1,'منهج شيخ الإسلام محمد بن عبد الوهاب في التأليف','1','0','0');
+INSERT INTO [t12101] VALUES (2,'إعداد: عبد المحسن بن حمد العباد البدر','1','0','0');
