dffc014d04dc5ec0bf2196e87807ff3b1474c244
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:15 +0700

    Upload SQL chunk: t37468_chunk_0000.sql

---
+++ sql_chunks/t37468_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t37468
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t37468" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37468
+INSERT INTO [t37468] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t37468] VALUES (2,'الجزء الأول أول الكتاب','7','0','0');
+INSERT INTO [t37468] VALUES (3,'الجزء الثاني','50','0','0');
+INSERT INTO [t37468] VALUES (4,'الجزء الثالث','101','0','0');
+INSERT INTO [t37468] VALUES (5,'الجزء الرابع','128','0','0');
