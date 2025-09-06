279886c8f84fb7a272fd566ed64ef540abe540a2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:05 +0700

    Upload SQL chunk: t6080_chunk_0000.sql

---
+++ sql_chunks/t6080_chunk_0000.sql
@@ -0,0 +1,58 @@
+-- Table: t6080
+-- Chunk: 1
+-- Rows: 51
+
+CREATE TABLE "t6080" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6080
+INSERT INTO [t6080] VALUES (1,'حرف الألف','2','0','0');
+INSERT INTO [t6080] VALUES (2,'حرف الباء','55','0','0');
+INSERT INTO [t6080] VALUES (3,'حرف التاء','57','0','0');
+INSERT INTO [t6080] VALUES (4,'حرف الثاء','60','0','0');
+INSERT INTO [t6080] VALUES (5,'حرف الجيم','62','0','0');
+INSERT INTO [t6080] VALUES (6,'حرف الحاء','64','0','0');
+INSERT INTO [t6080] VALUES (7,'حرف الخاء','65','0','0');
+INSERT INTO [t6080] VALUES (8,'حرف الدال','68','0','0');
+INSERT INTO [t6080] VALUES (9,'حرف الذال','71','0','0');
+INSERT INTO [t6080] VALUES (10,'حرف الراء','72','0','0');
+INSERT INTO [t6080] VALUES (11,'حرف الزاي','75','0','0');
+INSERT INTO [t6080] VALUES (12,'حرف السين','76','0','0');
+INSERT INTO [t6080] VALUES (13,'حرف الشين','79','0','0');
+INSERT INTO [t6080] VALUES (14,'حرف الصاد','81','0','0');
+INSERT INTO [t6080] VALUES (15,'حرف الضاد','83','0','0');
+INSERT INTO [t6080] VALUES (16,'حرف الطاء','84','0','0');
+INSERT INTO [t6080] VALUES (17,'حرف العين','85','0','0');
+INSERT INTO [t6080] VALUES (18,'حرف الغين','87','0','0');
+INSERT INTO [t6080] VALUES (19,'حرف الفاء','88','0','0');
+INSERT INTO [t6080] VALUES (20,'حرف القاف','90','0','0');
+INSERT INTO [t6080] VALUES (21,'حرف الكاف','92','0','0');
+INSERT INTO [t6080] VALUES (22,'حرف اللام','102','0','0');
+INSERT INTO [t6080] VALUES (23,'حرف الباء مع من','124','0','0');
+INSERT INTO [t6080] VALUES (24,'حرف التاء مع من','126','0','0');
+INSERT INTO [t6080] VALUES (25,'حرف الجيم مع من','128','0','0');
+INSERT INTO [t6080] VALUES (26,'حرف الحاء مع من','129','0','0');
+INSERT INTO [t6080] VALUES (27,'حرف الخاء مع من','131','0','0');
+INSERT INTO [t6080] VALUES (28,'حرف الدال مع من','132','0','0');
+INSERT INTO [t6080] VALUES (29,'حرف الراء مع من','133','0','0');
+INSERT INTO [t6080] VALUES (30,'حرف الزاي مع من','134','0','0');
+INSERT INTO [t6080] VALUES (31,'حرف السين مع من','135','0','0');
+INSERT INTO [t6080] VALUES (32,'حرف الشين مع من','136','0','0');
+INSERT INTO [t6080] VALUES (33,'حرف الصاد مع من','138','0','0');
+INSERT INTO [t6080] VALUES (34,'حرف الضاد مع من','141','0','0');
+INSERT INTO [t6080] VALUES (35,'حرف الطاء مع من','142','0','0');
+INSERT INTO [t6080] VALUES (36,'حرف العين مع من','143','0','0');
+INSERT INTO [t6080] VALUES (37,'حرف الغين مع من','144','0','0');
+INSERT INTO [t6080] VALUES (38,'حرف الفاء مع من','145','0','0');
+INSERT INTO [t6080] VALUES (39,'حرف القاف مع من','146','0','0');
+INSERT INTO [t6080] VALUES (40,'حرف الكاف مع من','148','0','0');
+INSERT INTO [t6080] VALUES (41,'حرف اللام مع من','151','0','0');
+INSERT INTO [t6080] VALUES (42,'حرف الميم مع من','152','0','0');
+INSERT INTO [t6080] VALUES (43,'حرف النون مع من','154','0','0');
+INSERT INTO [t6080] VALUES (44,'حرف الواو مع من','156','0','0');
+INSERT INTO [t6080] VALUES (45,'حرف الياء مع من','157','0','0');
+INSERT INTO [t6080] VALUES (46,'حرف النون','158','0','0');
+INSERT INTO [t6080] VALUES (47,'حرف الهاء','162','0','0');
+INSERT INTO [t6080] VALUES (48,'حرف الواو','163','0','0');
+INSERT INTO [t6080] VALUES (49,'حرف لا','165','0','0');
+INSERT INTO [t6080] VALUES (50,'حرف الياء','175','0','0');
+INSERT INTO [t6080] VALUES (51,'حرف ما دخله الألف واللام على ترتيب حروف المعجم','180','0','0');
