e58a904e5beaa4145b93987913baef86cbe965bf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:55 +0700

    Upload SQL chunk: t5184_chunk_0000.sql

---
+++ sql_chunks/t5184_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t5184
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t5184" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5184
+INSERT INTO [t5184] VALUES (1,'أتوعد سنات الرسول بمحوها','1','0','0');
+INSERT INTO [t5184] VALUES (2,'أحد الرؤساء من أهل المغرب كان يحارب توحيد الله واتباع سنة رسوله','1','0','0');
+INSERT INTO [t5184] VALUES (3,'أتوعد سنات الرسول بمحوها','2','0','0');
