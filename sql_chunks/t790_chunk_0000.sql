4e10477141e485363f572fd21edcfb0d497921e5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:38 +0700

    Upload SQL chunk: t790_chunk_0000.sql

---
+++ sql_chunks/t790_chunk_0000.sql
@@ -0,0 +1,50 @@
+-- Table: t790
+-- Chunk: 1
+-- Rows: 43
+
+CREATE TABLE "t790" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t790
+INSERT INTO [t790] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t790] VALUES (2,'أسماء الطير في الفرس','11','0','0');
+INSERT INTO [t790] VALUES (3,'دعاء الخيل','11','0','0');
+INSERT INTO [t790] VALUES (4,'عيوب الخيل','12','0','0');
+INSERT INTO [t790] VALUES (5,'مما يكون خلقه','12','4','0');
+INSERT INTO [t790] VALUES (6,'ومن عيوب الخيل الحادثة التي ليست من خلقتها','12','4','0');
+INSERT INTO [t790] VALUES (7,'ومما يستدل به جودة الفرس وجودة خلقة هو مجلل بما ظهر من جلالة','13','4','0');
+INSERT INTO [t790] VALUES (8,'ومما يستدل به على عتق الفرس وهو مجلل بما ظهر منه من جلالة','13','4','0');
+INSERT INTO [t790] VALUES (9,'ومما يستدل به على جودة الفرس وهو معنق','13','4','0');
+INSERT INTO [t790] VALUES (10,'ومما يستدل به على جودة الفرس وهو محضر','13','4','0');
+INSERT INTO [t790] VALUES (11,'صفة ما يستدل به على ذراعة الفرس إذا كان محضرا','14','4','0');
+INSERT INTO [t790] VALUES (12,'صفة العتق','15','4','0');
+INSERT INTO [t790] VALUES (13,'صفة ما يخالف الذكر فيه الأنثى','16','0','0');
+INSERT INTO [t790] VALUES (14,'صفة ما يحضر من الخيل من غير ضمر','17','0','0');
+INSERT INTO [t790] VALUES (15,'أسماء الخيل','17','0','0');
+INSERT INTO [t790] VALUES (16,'ما تستحب العرب في الخيل','18','0','0');
+INSERT INTO [t790] VALUES (17,'ألوان الخيل','26','0','0');
+INSERT INTO [t790] VALUES (18,'الدهمة','26','17','0');
+INSERT INTO [t790] VALUES (19,'الخضرة','26','17','0');
+INSERT INTO [t790] VALUES (20,'الحوة','26','17','0');
+INSERT INTO [t790] VALUES (21,'الكمتة','27','17','0');
+INSERT INTO [t790] VALUES (22,'الصفرة','27','17','0');
+INSERT INTO [t790] VALUES (23,'الوردة','27','17','0');
+INSERT INTO [t790] VALUES (24,'الشقرة','27','17','0');
+INSERT INTO [t790] VALUES (25,'الشهبة','27','17','0');
+INSERT INTO [t790] VALUES (26,'الشية في الفرس','27','17','0');
+INSERT INTO [t790] VALUES (27,'فمن الغرر','28','17','0');
+INSERT INTO [t790] VALUES (28,'القرحة','28','17','0');
+INSERT INTO [t790] VALUES (29,'الرثم','28','17','0');
+INSERT INTO [t790] VALUES (30,'اليعسوب','28','17','0');
+INSERT INTO [t790] VALUES (31,'اللمظة','28','17','0');
+INSERT INTO [t790] VALUES (32,'تسمية وضح القوائم','28','0','0');
+INSERT INTO [t790] VALUES (33,'شية الذنب','29','32','0');
+INSERT INTO [t790] VALUES (34,'أسماء الدوائر التي تكون في الخيل','29','0','0');
+INSERT INTO [t790] VALUES (35,'الخيل وصفاتها','29','0','0');
+INSERT INTO [t790] VALUES (36,'قيام الخيل','31','0','0');
+INSERT INTO [t790] VALUES (37,'مشي الخيل','31','0','0');
+INSERT INTO [t790] VALUES (38,'أصناف الحضر','32','0','0');
+INSERT INTO [t790] VALUES (39,'عيوب الخيل في جريها','32','0','0');
+INSERT INTO [t790] VALUES (40,'النشاط','32','0','0');
+INSERT INTO [t790] VALUES (41,'الصهيل','33','0','0');
+INSERT INTO [t790] VALUES (42,'ما قالت العرب في أشعارها من صفة الخيل','33','0','0');
+INSERT INTO [t790] VALUES (43,'تمت الكلمة','34','42','0');
