4c80b1a32643e4c65069163991e69e02e0028b1a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:38 +0700

    Upload SQL chunk: t26574_chunk_0000.sql

---
+++ sql_chunks/t26574_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t26574
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t26574" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26574
+INSERT INTO [t26574] VALUES (1,'باب الألف','6','0','0');
+INSERT INTO [t26574] VALUES (2,'باب الباء','23','0','0');
+INSERT INTO [t26574] VALUES (3,'باب التاء','47','0','0');
+INSERT INTO [t26574] VALUES (4,'باب الثاء','54','0','0');
+INSERT INTO [t26574] VALUES (5,'باب الجيم','62','0','0');
+INSERT INTO [t26574] VALUES (6,'باب الحاء','110','0','0');
+INSERT INTO [t26574] VALUES (7,'باب الخاء','176','0','0');
+INSERT INTO [t26574] VALUES (8,'باب الدال','202','0','0');
+INSERT INTO [t26574] VALUES (9,'باب الذال','227','0','0');
+INSERT INTO [t26574] VALUES (10,'باب الراء','233','0','0');
+INSERT INTO [t26574] VALUES (11,'باب الزاي','275','0','0');
+INSERT INTO [t26574] VALUES (12,'باب السين','298','0','0');
+INSERT INTO [t26574] VALUES (13,'باب الشين','353','0','0');
+INSERT INTO [t26574] VALUES (14,'حرف الصاد','393','0','0');
+INSERT INTO [t26574] VALUES (15,'باب الضاد','420','0','0');
+INSERT INTO [t26574] VALUES (16,'باب الطاء','427','0','0');
+INSERT INTO [t26574] VALUES (17,'باب الظاء','440','0','0');
+INSERT INTO [t26574] VALUES (18,'باب العين','444','0','0');
+INSERT INTO [t26574] VALUES (19,'باب الغين','547','0','0');
+INSERT INTO [t26574] VALUES (20,'باب الفاء','566','0','0');
+INSERT INTO [t26574] VALUES (21,'باب القاف','592','0','0');
+INSERT INTO [t26574] VALUES (22,'باب الكاف','626','0','0');
+INSERT INTO [t26574] VALUES (23,'باب اللام','639','0','0');
+INSERT INTO [t26574] VALUES (24,'باب الميم','648','0','0');
+INSERT INTO [t26574] VALUES (25,'باب النون','782','0','0');
+INSERT INTO [t26574] VALUES (26,'باب الواو','800','0','0');
+INSERT INTO [t26574] VALUES (27,'باب الهاء','813','0','0');
+INSERT INTO [t26574] VALUES (28,'باب الياء','838','0','0');
+INSERT INTO [t26574] VALUES (29,'خاتمة الكتاب','844','0','0');
