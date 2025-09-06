1e16e023b18cee002d156c903680299e896b8972
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:59 +0700

    Upload SQL chunk: t30240_chunk_0000.sql

---
+++ sql_chunks/t30240_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t30240
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t30240" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30240
+INSERT INTO [t30240] VALUES (1,'كتاب الإيمان','50','0','0');
+INSERT INTO [t30240] VALUES (2,'من كتاب الصيد والذبائح','63','0','0');
+INSERT INTO [t30240] VALUES (3,'من كتاب القدر','70','0','0');
+INSERT INTO [t30240] VALUES (4,'من كتاب الذكر','73','0','0');
+INSERT INTO [t30240] VALUES (5,'النكاح','80','0','0');
