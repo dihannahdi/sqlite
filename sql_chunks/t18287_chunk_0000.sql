ccff0f8dc16660bdb5c10da2e85e7427cb6236d5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:33:51 +0700

    Upload SQL chunk: t18287_chunk_0000.sql

---
+++ sql_chunks/t18287_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t18287
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t18287" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t18287
+INSERT INTO [t18287] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t18287] VALUES (2,'باب الطهارة','3','0','0');
+INSERT INTO [t18287] VALUES (3,'باب الصلاة','12','0','0');
+INSERT INTO [t18287] VALUES (4,'باب الزكاة','20','0','0');
+INSERT INTO [t18287] VALUES (5,'باب زكاة الفضة','21','0','0');
+INSERT INTO [t18287] VALUES (6,'باب الصيام والاعتكاف','25','0','0');
+INSERT INTO [t18287] VALUES (7,'باب المناسك','33','0','0');
+INSERT INTO [t18287] VALUES (8,'باب الصيد والذبائح والضحايا','45','0','0');
+INSERT INTO [t18287] VALUES (9,'باب النكاح','53','0','0');
+INSERT INTO [t18287] VALUES (10,'باب الطلاق','65','0','0');
+INSERT INTO [t18287] VALUES (11,'باب العتاق والتدبير والمكاتبة وأمهات الأولاد','86','0','0');
+INSERT INTO [t18287] VALUES (12,'باب الأيمان والكفارة','100','0','0');
+INSERT INTO [t18287] VALUES (13,'باب الفرائض','110','0','0');
+INSERT INTO [t18287] VALUES (14,'باب في الوصايا','118','0','0');
+INSERT INTO [t18287] VALUES (15,'باب الجهاد','131','0','0');
+INSERT INTO [t18287] VALUES (16,'باب الحدود','150','0','0');
+INSERT INTO [t18287] VALUES (17,'باب الجنايات والديات','173','0','0');
+INSERT INTO [t18287] VALUES (18,'باب الصرف','188','0','0');
+INSERT INTO [t18287] VALUES (19,'باب السلم','196','0','0');
+INSERT INTO [t18287] VALUES (20,'باب البيوع','202','0','0');
+INSERT INTO [t18287] VALUES (21,'باب الشفعة','216','0','0');
+INSERT INTO [t18287] VALUES (22,'باب الإجارات','219','0','0');
+INSERT INTO [t18287] VALUES (23,'باب المضارب','231','0','0');
+INSERT INTO [t18287] VALUES (24,'باب الشركة','236','0','0');
+INSERT INTO [t18287] VALUES (25,'باب الوكالات','238','0','0');
+INSERT INTO [t18287] VALUES (26,'باب الرهون','241','0','0');
+INSERT INTO [t18287] VALUES (27,'باب الكفالة','244','0','0');
+INSERT INTO [t18287] VALUES (28,'باب الغصب','248','0','0');
+INSERT INTO [t18287] VALUES (29,'باب المأذون','251','0','0');
+INSERT INTO [t18287] VALUES (30,'باب الهبات','253','0','0');
+INSERT INTO [t18287] VALUES (31,'باب الصلح','256','0','0');
+INSERT INTO [t18287] VALUES (32,'باب اللقطة','258','0','0');
+INSERT INTO [t18287] VALUES (33,'باب اللباس وغيره','261','0','0');
+INSERT INTO [t18287] VALUES (34,'باب القضاء','264','0','0');
