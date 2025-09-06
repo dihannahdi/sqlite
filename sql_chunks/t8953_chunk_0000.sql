f4520f4552d4de21cfea65c73596be3fd67b06f7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:54 +0700

    Upload SQL chunk: t8953_chunk_0000.sql

---
+++ sql_chunks/t8953_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t8953
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t8953" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8953
+INSERT INTO [t8953] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8953] VALUES (2,'مدخل','6','0','0');
+INSERT INTO [t8953] VALUES (3,'تعدي الفعل ولزومه','6','2','0');
+INSERT INTO [t8953] VALUES (4,'علامات تعدي الفعل ولزومه','8','2','0');
+INSERT INTO [t8953] VALUES (5,'تعليق الجار والمجرور','11','2','0');
+INSERT INTO [t8953] VALUES (6,'دراسة نزع الخافض','13','0','0');
+INSERT INTO [t8953] VALUES (7,'تعريف المصطلح','13','6','0');
+INSERT INTO [t8953] VALUES (8,'أحوال نزع الخافض في العربية','15','6','0');
+INSERT INTO [t8953] VALUES (9,'حكم منزوع الخافض','25','6','0');
+INSERT INTO [t8953] VALUES (10,'المنصوب بعد (ما) الحجازية','26','6','0');
+INSERT INTO [t8953] VALUES (11,'نزع الخافض والتضمين','28','6','0');
+INSERT INTO [t8953] VALUES (12,'المنصوب علي نزع الخافض في القرآن','34','0','0');
+INSERT INTO [t8953] VALUES (13,'نزع الخافض قياسا في القرآن','34','12','0');
+INSERT INTO [t8953] VALUES (14,'دراسة أن وأن بعد نزع الخافض','34','13','0');
+INSERT INTO [t8953] VALUES (15,'نزع الخافض من أن وأن في القرآن','37','13','0');
+INSERT INTO [t8953] VALUES (16,'دراسة كي في اللغة','40','13','0');
+INSERT INTO [t8953] VALUES (17,'كي في القرآن','44','13','0');
+INSERT INTO [t8953] VALUES (18,'نزع الخافض سماعا في القرآن','47','12','0');
+INSERT INTO [t8953] VALUES (19,'نزع الخافض من المفرد','47','18','0');
+INSERT INTO [t8953] VALUES (20,'دراسة التعليق والجمل التي بعده','64','18','0');
+INSERT INTO [t8953] VALUES (21,'نزع الخافض من الجمل المعلقة في القرآن','67','18','0');
+INSERT INTO [t8953] VALUES (22,'المفعول الثاني منصوب على نزع الخافض والفعل معلق عنه','72','18','0');
+INSERT INTO [t8953] VALUES (23,'مصادر ومراجع','78','0','0');
