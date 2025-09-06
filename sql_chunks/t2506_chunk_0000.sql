6d28601289a445fe41357b38b7a394502be97ceb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:33 +0700

    Upload SQL chunk: t2506_chunk_0000.sql

---
+++ sql_chunks/t2506_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t2506
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t2506" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2506
+INSERT INTO [t2506] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t2506] VALUES (2,'الفصل الأول','10','0','0');
+INSERT INTO [t2506] VALUES (3,'الفصل الثاني','17','0','0');
+INSERT INTO [t2506] VALUES (4,'الفصل الثالث','21','0','0');
+INSERT INTO [t2506] VALUES (5,'الفصل الرابع','24','0','0');
+INSERT INTO [t2506] VALUES (6,'الفصل الخامس','27','0','0');
+INSERT INTO [t2506] VALUES (7,'الفصل السادس','30','0','0');
+INSERT INTO [t2506] VALUES (8,'الفصل السابع','34','0','0');
+INSERT INTO [t2506] VALUES (9,'الفصل الثامن','39','0','0');
+INSERT INTO [t2506] VALUES (10,'الفصل التاسع','43','0','0');
+INSERT INTO [t2506] VALUES (11,'الفصل العاشر','47','0','0');
+INSERT INTO [t2506] VALUES (12,'الفصل الحادي عشر','50','0','0');
+INSERT INTO [t2506] VALUES (13,'الفصل الثاني عشر','54','0','0');
+INSERT INTO [t2506] VALUES (14,'الفصل الثالث عشر','56','0','0');
+INSERT INTO [t2506] VALUES (15,'الفصل الرابع عشر','61','0','0');
+INSERT INTO [t2506] VALUES (16,'الفصل الخامس عشر','66','0','0');
+INSERT INTO [t2506] VALUES (17,'الفصل السادس عشر','71','0','0');
+INSERT INTO [t2506] VALUES (18,'الفصل السابع عشر','75','0','0');
+INSERT INTO [t2506] VALUES (19,'الفصل الثامن عشر','77','0','0');
+INSERT INTO [t2506] VALUES (20,'الفصل التاسع عشر','81','0','0');
+INSERT INTO [t2506] VALUES (21,'الفصل الموفى عشرين','85','0','0');
+INSERT INTO [t2506] VALUES (22,'الفصل الحادي والعشرون','89','0','0');
+INSERT INTO [t2506] VALUES (23,'الفصل الثاني والعشرون','94','0','0');
+INSERT INTO [t2506] VALUES (24,'الفصل الثالث والعشرون','99','0','0');
+INSERT INTO [t2506] VALUES (25,'الفصل الرابع والعشرون','103','0','0');
+INSERT INTO [t2506] VALUES (26,'الفصل الخامس والعشرون','106','0','0');
+INSERT INTO [t2506] VALUES (27,'الفصل السادس والعشرون','111','0','0');
+INSERT INTO [t2506] VALUES (28,'الفصل السابع والعشرون','115','0','0');
+INSERT INTO [t2506] VALUES (29,'الفصل الثامن والعشرون','121','0','0');
+INSERT INTO [t2506] VALUES (30,'الفصل التاسع والعشرون','126','0','0');
+INSERT INTO [t2506] VALUES (31,'الفصل الموفى ثلاثين','131','0','0');
+INSERT INTO [t2506] VALUES (32,'الفصل الحادي والثلاثون','135','0','0');
+INSERT INTO [t2506] VALUES (33,'الفصل الثاني والثلاثون','140','0','0');
