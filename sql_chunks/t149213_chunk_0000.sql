bdc5cad5ca7c1d7f1535111e601b9488cf3384d9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:47 +0700

    Upload SQL chunk: t149213_chunk_0000.sql

---
+++ sql_chunks/t149213_chunk_0000.sql
@@ -0,0 +1,69 @@
+-- Table: t149213
+-- Chunk: 1
+-- Rows: 62
+
+CREATE TABLE "t149213" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t149213
+INSERT INTO [t149213] VALUES (1,'مقدمة الناشر','4','0','0');
+INSERT INTO [t149213] VALUES (2,'مقدمة التحقيق','6','0','0');
+INSERT INTO [t149213] VALUES (3,'مقدمة المصنف','96','0','0');
+INSERT INTO [t149213] VALUES (4,'حرف الألف','113','0','0');
+INSERT INTO [t149213] VALUES (5,'حرف الباء','440','0','0');
+INSERT INTO [t149213] VALUES (6,'حرف التاء','509','0','0');
+INSERT INTO [t149213] VALUES (7,'حرف الثاء','518','0','0');
+INSERT INTO [t149213] VALUES (8,'حرف الجيم','539','0','0');
+INSERT INTO [t149213] VALUES (9,'حرف الحاء','602','0','0');
+INSERT INTO [t149213] VALUES (10,'حرف الخاء','934','0','0');
+INSERT INTO [t149213] VALUES (11,'حرف الدال','1021','0','0');
+INSERT INTO [t149213] VALUES (12,'حرف الذال','1056','0','0');
+INSERT INTO [t149213] VALUES (13,'حرف الراء','1061','0','0');
+INSERT INTO [t149213] VALUES (14,'حرف الزاى','1128','0','0');
+INSERT INTO [t149213] VALUES (15,'حرف السين','1231','0','0');
+INSERT INTO [t149213] VALUES (16,'حرف الشين','1556','0','0');
+INSERT INTO [t149213] VALUES (17,'حرف الصاد','1624','0','0');
+INSERT INTO [t149213] VALUES (18,'حرف الضاد','1677','0','0');
+INSERT INTO [t149213] VALUES (19,'حرف الطاء','1694','0','0');
+INSERT INTO [t149213] VALUES (20,'حرف الظاء','1728','0','0');
+INSERT INTO [t149213] VALUES (21,'حرف العين','1732','0','0');
+INSERT INTO [t149213] VALUES (22,'حرف الغين','2934','0','0');
+INSERT INTO [t149213] VALUES (23,'حرف الفاء','2942','0','0');
+INSERT INTO [t149213] VALUES (24,'حرف القاف','2983','0','0');
+INSERT INTO [t149213] VALUES (25,'حرف الكاف','3062','0','0');
+INSERT INTO [t149213] VALUES (26,'حرف اللام','3091','0','0');
+INSERT INTO [t149213] VALUES (27,'حرف الميم','3105','0','0');
+INSERT INTO [t149213] VALUES (28,'حرف النون','3729','0','0');
+INSERT INTO [t149213] VALUES (29,'حرف الهاء','3806','0','0');
+INSERT INTO [t149213] VALUES (30,'حرف الواو','3884','0','0');
+INSERT INTO [t149213] VALUES (31,'حرف اللام ألف','3953','0','0');
+INSERT INTO [t149213] VALUES (32,'حرف الياء','3955','0','0');
+INSERT INTO [t149213] VALUES (33,'باب الكنى','4184','0','0');
+INSERT INTO [t149213] VALUES (34,'حرف الألف','4184','33','0');
+INSERT INTO [t149213] VALUES (35,'حرف الباء','4199','33','0');
+INSERT INTO [t149213] VALUES (36,'حرف التاء.','4229','33','0');
+INSERT INTO [t149213] VALUES (37,'حرف الثاء.','4230','33','0');
+INSERT INTO [t149213] VALUES (38,'حرف الجيم','4232','33','0');
+INSERT INTO [t149213] VALUES (39,'حرف الحاء','4242','33','0');
+INSERT INTO [t149213] VALUES (40,'حرف الخاء','4257','33','0');
+INSERT INTO [t149213] VALUES (41,'حرف الدال','4264','33','0');
+INSERT INTO [t149213] VALUES (42,'حرف الذال','4265','33','0');
+INSERT INTO [t149213] VALUES (43,'حرف الراء','4268','33','0');
+INSERT INTO [t149213] VALUES (44,'حرف الزاي','4275','33','0');
+INSERT INTO [t149213] VALUES (45,'حرف السين','4280','33','0');
+INSERT INTO [t149213] VALUES (46,'حرف الشين','4299','33','0');
+INSERT INTO [t149213] VALUES (47,'حرف الصاد','4303','33','0');
+INSERT INTO [t149213] VALUES (48,'حرف الضاد','4312','33','0');
+INSERT INTO [t149213] VALUES (49,'حرف الطاء','4313','33','0');
+INSERT INTO [t149213] VALUES (50,'حرف الظاء','4317','33','0');
+INSERT INTO [t149213] VALUES (51,'حرف العين.','4319','33','0');
+INSERT INTO [t149213] VALUES (52,'حرف الغين','4368','33','0');
+INSERT INTO [t149213] VALUES (53,'حرف الفاء','4372','33','0');
+INSERT INTO [t149213] VALUES (54,'حرف القاف','4375','33','0');
+INSERT INTO [t149213] VALUES (55,'حرف الكاف','4381','33','0');
+INSERT INTO [t149213] VALUES (56,'حرف اللام','4386','33','0');
+INSERT INTO [t149213] VALUES (57,'حرف الميم','4389','33','0');
+INSERT INTO [t149213] VALUES (58,'حرف النون','4424','33','0');
+INSERT INTO [t149213] VALUES (59,'حرف الهاء','4431','33','0');
+INSERT INTO [t149213] VALUES (60,'حرف الواو','4440','33','0');
+INSERT INTO [t149213] VALUES (61,'حرف اللام ألف','4445','33','0');
+INSERT INTO [t149213] VALUES (62,'حرف الياء','4446','33','0');
