b671541dda0250b7578eb9f4d2c76d444eff2019
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:31 +0700

    Upload SQL chunk: t4_chunk_0000.sql

---
+++ sql_chunks/t4_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t4
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t4" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4
+INSERT INTO [t4] VALUES (1,'الطريق المستقيم في نظم علامات الترقيم','1','0','0');
+INSERT INTO [t4] VALUES (2,'مقدمة النظم','1','0','0');
+INSERT INTO [t4] VALUES (3,'علامات الترقيم','2','0','0');
+INSERT INTO [t4] VALUES (4,'الفصلة (،)','3','0','0');
+INSERT INTO [t4] VALUES (5,'تنبيه','4','0','0');
+INSERT INTO [t4] VALUES (6,'الفصلة المنقوطة (؛)','5','0','0');
+INSERT INTO [t4] VALUES (7,'الوقفة أو النقطة (.)','6','0','0');
+INSERT INTO [t4] VALUES (8,'الوصلة أو الشرطة (ــ)','7','0','0');
+INSERT INTO [t4] VALUES (9,'القوسان ()','8','0','0');
+INSERT INTO [t4] VALUES (10,'علامة التنصيص ("")','9','0','0');
+INSERT INTO [t4] VALUES (11,'النقطتان الرأسيتان (:)','10','0','0');
+INSERT INTO [t4] VALUES (12,'تنبيه','11','0','0');
+INSERT INTO [t4] VALUES (13,'علامة الاستفهام (؟)','12','0','0');
+INSERT INTO [t4] VALUES (14,'علامة الانفعال أو التعجب (!)','13','0','0');
+INSERT INTO [t4] VALUES (15,'الاستفهام التعجبي (؟!)','14','0','0');
+INSERT INTO [t4] VALUES (16,'علامة الحذف (....)','15','0','0');
+INSERT INTO [t4] VALUES (17,'الخاتمة','16','0','0');
