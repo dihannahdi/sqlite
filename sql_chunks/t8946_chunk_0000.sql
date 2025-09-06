ab9e7679be434c7338fab9adb5ea4ac078b76596
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:48 +0700

    Upload SQL chunk: t8946_chunk_0000.sql

---
+++ sql_chunks/t8946_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8946
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8946" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8946
+INSERT INTO [t8946] VALUES (1,'المفهوم الصحيح للتوسل','1','0','0');
+INSERT INTO [t8946] VALUES (2,'لفضيلة الأستاذ حماد بن محمد الأنصاري','1','0','0');
