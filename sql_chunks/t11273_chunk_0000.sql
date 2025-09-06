821dc860902d7760b2564f9052954d768cd42af0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:18 +0700

    Upload SQL chunk: t11273_chunk_0000.sql

---
+++ sql_chunks/t11273_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t11273
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t11273" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11273
+INSERT INTO [t11273] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11273] VALUES (2,'مواظبته صلى الله عليه وسلم على صلاة العيد في المصلى والأحاديث في ذلك','13','0','0');
+INSERT INTO [t11273] VALUES (3,'دلالة الأحاديث على أن السنة الصلاة في المصلى','18','0','0');
+INSERT INTO [t11273] VALUES (4,'رد تعليل الصلاة في المصلى بعلة ضيق المسجد','20','0','0');
+INSERT INTO [t11273] VALUES (5,'حكمة الصلاة في المصلى','35','0','0');
+INSERT INTO [t11273] VALUES (6,'شبه وجوابها','41','0','0');
