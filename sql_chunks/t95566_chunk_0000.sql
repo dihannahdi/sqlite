aebfc164f954b6254f9488ce7102ac991baf6db5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:27 +0700

    Upload SQL chunk: t95566_chunk_0000.sql

---
+++ sql_chunks/t95566_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t95566
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t95566" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95566
+INSERT INTO [t95566] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t95566] VALUES (2,'تعريف الكفر والردة','5','0','0');
+INSERT INTO [t95566] VALUES (3,'التحذير من التكفير','8','0','0');
+INSERT INTO [t95566] VALUES (4,'التكفير حكم شرعي','11','0','0');
+INSERT INTO [t95566] VALUES (5,'أقوال العلماء في التحذير من التكفير','14','0','0');
+INSERT INTO [t95566] VALUES (6,'أسباب الوقوع في التكفير','20','0','0');
+INSERT INTO [t95566] VALUES (7,'التكفير والفهم الخاطئ للنصوص الشرعية','25','0','0');
+INSERT INTO [t95566] VALUES (8,'التفريق بين فعل الكفر والحكم بكفر الفاعل','38','0','0');
+INSERT INTO [t95566] VALUES (9,'الحكم بالظاهر والإعراض عن السرائر','44','0','0');
+INSERT INTO [t95566] VALUES (10,'لازم القول ليس بقول','50','0','0');
+INSERT INTO [t95566] VALUES (11,'العذر بالجهل','53','0','0');
+INSERT INTO [t95566] VALUES (12,'العذر بالخطأ مع سلامة القصد','64','0','0');
+INSERT INTO [t95566] VALUES (13,'العذر بالتأويل','69','0','0');
+INSERT INTO [t95566] VALUES (14,'العذر بالإكراه','81','0','0');
+INSERT INTO [t95566] VALUES (15,'التفريق بين مسائل العقيدة ومسائل الأحكام في مسألة التكفير وموانعه','86','0','0');
+INSERT INTO [t95566] VALUES (16,'الحكم بغير ما أنزل الله','90','0','0');
+INSERT INTO [t95566] VALUES (17,'الخاتمة','98','0','0');
+INSERT INTO [t95566] VALUES (18,'المصادر والمراجع','101','0','0');
