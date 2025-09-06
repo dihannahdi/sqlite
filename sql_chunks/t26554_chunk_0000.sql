f0f75f7fb9f1182c58d4d8e9c363ba14b45ab10c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:24 +0700

    Upload SQL chunk: t26554_chunk_0000.sql

---
+++ sql_chunks/t26554_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t26554
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t26554" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26554
+INSERT INTO [t26554] VALUES (1,'من حديث أبي نصر العكبري','2','0','0');
+INSERT INTO [t26554] VALUES (2,'المنتقى من حديث أحمد النصيبي','19','0','0');
+INSERT INTO [t26554] VALUES (3,'من حديث خيثمة','38','0','0');
+INSERT INTO [t26554] VALUES (4,'في ذكر صفة النبي صلى الله عليه وسلم وصفة أخلاقه وسيرته وأدبه وخفض جناحه','50','0','0');
+INSERT INTO [t26554] VALUES (5,'من حديث عنبسة بن سعيد الرازي','59','0','0');
