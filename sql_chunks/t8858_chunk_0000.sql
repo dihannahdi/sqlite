cbdb05f910677459b570058680e414e58be43736
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:26 +0700

    Upload SQL chunk: t8858_chunk_0000.sql

---
+++ sql_chunks/t8858_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t8858
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t8858" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8858
+INSERT INTO [t8858] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8858] VALUES (2,'رحلاته العلمية','2','0','0');
+INSERT INTO [t8858] VALUES (3,'أساتذته','3','0','0');
+INSERT INTO [t8858] VALUES (4,'مصنفاته','5','0','0');
+INSERT INTO [t8858] VALUES (5,'الشفا بتعريف حقوق المصطفى','7','0','0');
