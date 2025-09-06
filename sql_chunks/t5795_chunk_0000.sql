336f0352db09aa2f84ffb1f83207c080d41ec468
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:23 +0700

    Upload SQL chunk: t5795_chunk_0000.sql

---
+++ sql_chunks/t5795_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t5795
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t5795" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5795
+INSERT INTO [t5795] VALUES (1,'ومن حرف الحاء ممن اسمه الحسن أو الحسين','270','0','0');
+INSERT INTO [t5795] VALUES (2,'ومن اسمه حماد','285','0','0');
+INSERT INTO [t5795] VALUES (3,'ومن حرف العين من إسمه عبد الله','287','0','0');
+INSERT INTO [t5795] VALUES (4,'ومن اسمه عبد الرحمان وعبد الرحيم','305','0','0');
+INSERT INTO [t5795] VALUES (5,'ومن اسمه علي','313','0','0');
+INSERT INTO [t5795] VALUES (6,'ومن حرف الميم من إسمه محمد','329','0','0');
+INSERT INTO [t5795] VALUES (7,'ومن اسمه يوسف','489','0','0');
+INSERT INTO [t5795] VALUES (8,'ومن اسمه يعقوب','491','0','0');
