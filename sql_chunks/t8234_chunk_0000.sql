8cc9d632b0a5bd6bee84fcf5c57f8c2f1d7e44d4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:45 +0700

    Upload SQL chunk: t8234_chunk_0000.sql

---
+++ sql_chunks/t8234_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t8234
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t8234" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8234
+INSERT INTO [t8234] VALUES (1,'دعوة إلى التشمير والاستعداد للجنة','1','0','0');
+INSERT INTO [t8234] VALUES (2,'صفة الجنة','5','0','0');
+INSERT INTO [t8234] VALUES (3,'صفة شجر الجنة','42','0','0');
+INSERT INTO [t8234] VALUES (4,'باب شجرة طوبى','54','0','0');
+INSERT INTO [t8234] VALUES (5,'باب طعام أهل الجنة','101','0','0');
+INSERT INTO [t8234] VALUES (6,'باب شراب أهل الجنة','120','0','0');
+INSERT INTO [t8234] VALUES (7,'باب لباس أهل الجنة','148','0','0');
+INSERT INTO [t8234] VALUES (8,'باب فراش أهل الجنة','157','0','0');
+INSERT INTO [t8234] VALUES (9,'باب قصور الجنة','175','0','0');
+INSERT INTO [t8234] VALUES (10,'باب درجات أهل الجنة','187','0','0');
+INSERT INTO [t8234] VALUES (11,'باب ملك أهل الجنة','203','0','0');
+INSERT INTO [t8234] VALUES (12,'باب خدم أهل الجنة','212','0','0');
+INSERT INTO [t8234] VALUES (13,'باب لسان أهل الجنة','219','0','0');
+INSERT INTO [t8234] VALUES (14,'باب حلي أهل الجنة','225','0','0');
+INSERT INTO [t8234] VALUES (15,'باب أبواب الجنة','230','0','0');
+INSERT INTO [t8234] VALUES (16,'باب تزاور أهل الجنة ومتنزهاتهم','249','0','0');
+INSERT INTO [t8234] VALUES (17,'باب سوق أهل الجنة','260','0','0');
+INSERT INTO [t8234] VALUES (18,'باب غناء أهل الجنة','266','0','0');
+INSERT INTO [t8234] VALUES (19,'باب جماع أهل الجنة','277','0','0');
+INSERT INTO [t8234] VALUES (20,'باب الحور العين','298','0','0');
+INSERT INTO [t8234] VALUES (21,'باب صفة الحور العين','307','0','0');
+INSERT INTO [t8234] VALUES (22,'باب جامع من ذكر الجنة','373','0','0');
