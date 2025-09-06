f0525b50ea85615da900eca585a29f905b5f5151
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:32 +0700

    Upload SQL chunk: t10387_chunk_0000.sql

---
+++ sql_chunks/t10387_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t10387
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t10387" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10387
+INSERT INTO [t10387] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10387] VALUES (2,'الرشوة لغة','2','0','0');
+INSERT INTO [t10387] VALUES (3,'ألفاظ مرادفة للرشوة: منها:','3','0','0');
+INSERT INTO [t10387] VALUES (4,'مناقشة تلك الأقوال','5','0','0');
+INSERT INTO [t10387] VALUES (5,'الرشوة في الإصطلاح','7','0','0');
+INSERT INTO [t10387] VALUES (6,'الصلة بين المعنى اللغوي والمعنى الاصطلاحي','9','0','0');
+INSERT INTO [t10387] VALUES (7,'الرشوة في القرآن الكريم','12','0','0');
+INSERT INTO [t10387] VALUES (8,'دراسة النص الأول','14','0','0');
+INSERT INTO [t10387] VALUES (9,'الموطن الثالث','18','0','0');
+INSERT INTO [t10387] VALUES (10,'الفرق بين الرشوة والسحت','21','0','0');
+INSERT INTO [t10387] VALUES (11,'الرشوة في نصوص الحديث','22','0','0');
+INSERT INTO [t10387] VALUES (12,'دراسة تلك النصوص','25','0','0');
+INSERT INTO [t10387] VALUES (13,'عوامل تفشي الرشوة والبيئة التي تساعد على إنتشارها','27','0','0');
+INSERT INTO [t10387] VALUES (14,'مضار الرشوة','28','0','0');
+INSERT INTO [t10387] VALUES (15,'تأثيرها على الفرد','32','0','0');
+INSERT INTO [t10387] VALUES (16,'طريقة علاجها','33','0','0');
+INSERT INTO [t10387] VALUES (17,'الفرق بين الهدية والرشوة','35','0','0');
+INSERT INTO [t10387] VALUES (18,'أقوال العلماء فيمن صانع بماله عند اضطراره','40','0','0');
+INSERT INTO [t10387] VALUES (19,'كلام الفقهاء','43','0','0');
