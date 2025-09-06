d426986d54dacf61cf8144ca8fe6536b8bcc3d86
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:08:08 +0700

    Upload SQL chunk: t12864_chunk_0000.sql

---
+++ sql_chunks/t12864_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t12864
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t12864" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12864
+INSERT INTO [t12864] VALUES (1,'ذكر من يسمى عطاء من رواة الحديث','1','0','0');
+INSERT INTO [t12864] VALUES (2,'عطاء الشيبي','1','0','0');
+INSERT INTO [t12864] VALUES (3,'عطاء بن يسار','4','0','0');
+INSERT INTO [t12864] VALUES (4,'عطاء بن يزيد الليثي','6','0','0');
+INSERT INTO [t12864] VALUES (5,'عطاء الشامي','6','0','0');
+INSERT INTO [t12864] VALUES (6,'عطاء العطار','7','0','0');
+INSERT INTO [t12864] VALUES (7,'عطاء بن فروخ','8','0','0');
+INSERT INTO [t12864] VALUES (8,'عطاء بن أبي ميمونة','9','0','0');
+INSERT INTO [t12864] VALUES (9,'عطاء بن بخت','10','0','0');
+INSERT INTO [t12864] VALUES (10,'عطاء بن أبي مروان','11','0','0');
+INSERT INTO [t12864] VALUES (11,'عطاء بن مسعود الكعبي','12','0','0');
+INSERT INTO [t12864] VALUES (12,'عطاء بن خباب','13','0','0');
+INSERT INTO [t12864] VALUES (13,'عطاء بن دينار الهذلي','15','0','0');
+INSERT INTO [t12864] VALUES (14,'عطاء بن قرة','16','0','0');
+INSERT INTO [t12864] VALUES (15,'عطاء بن نافع الكيخاراني','17','0','0');
+INSERT INTO [t12864] VALUES (16,'عطاء أبو يعلى الطائفي','18','0','0');
+INSERT INTO [t12864] VALUES (17,'عطاء بن السايب','19','0','0');
+INSERT INTO [t12864] VALUES (18,'عطاء البزاز','20','0','0');
+INSERT INTO [t12864] VALUES (19,'عطاء أبو محمد','20','0','0');
+INSERT INTO [t12864] VALUES (20,'عطاء بن مسلم الصنعاني','21','0','0');
+INSERT INTO [t12864] VALUES (21,'عطاء بن مسلم الحلبي الخفاف','22','0','0');
+INSERT INTO [t12864] VALUES (22,'عطاء بن جبلة القراوي','23','0','0');
+INSERT INTO [t12864] VALUES (23,'عطاء بن عجلان','24','0','0');
+INSERT INTO [t12864] VALUES (24,'عطاء السليمي','25','0','0');
+INSERT INTO [t12864] VALUES (25,'عطاء مولى ابن سباع','26','0','0');
+INSERT INTO [t12864] VALUES (26,'عطاء بن زهير','26','0','0');
