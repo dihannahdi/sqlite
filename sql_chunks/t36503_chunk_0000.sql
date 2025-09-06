5f7cbbd81c51afd9578c8b99125925cce64329e2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:35 +0700

    Upload SQL chunk: t36503_chunk_0000.sql

---
+++ sql_chunks/t36503_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t36503
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t36503" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36503
+INSERT INTO [t36503] VALUES (1,'مقدمة الطبعة الرابعة','1','0','0');
+INSERT INTO [t36503] VALUES (2,'المقدمة','2','0','0');
+INSERT INTO [t36503] VALUES (3,'تعريف التوبة','5','0','0');
+INSERT INTO [t36503] VALUES (4,'حقيقة التوبة','6','0','0');
+INSERT INTO [t36503] VALUES (5,'من معاني التوبة في القرآن الكريم','8','0','0');
+INSERT INTO [t36503] VALUES (6,'فضل التوبة إلى الله','9','0','0');
+INSERT INTO [t36503] VALUES (7,'وجوب التوبة على الفور','13','0','0');
+INSERT INTO [t36503] VALUES (8,'تأخير التوبة ذنب تجب التوبة منه','15','0','0');
+INSERT INTO [t36503] VALUES (9,'شروط التوبة','16','0','0');
+INSERT INTO [t36503] VALUES (10,'وقت التوبة ونهاية وقتها','21','0','0');
+INSERT INTO [t36503] VALUES (11,'إمكان التوبة من جميع الذنوب','26','0','0');
+INSERT INTO [t36503] VALUES (12,'التوبة من ترك الحسنات','30','0','0');
+INSERT INTO [t36503] VALUES (13,'الذنوب والمعاصي التي تجب التوبة منها','33','0','0');
+INSERT INTO [t36503] VALUES (14,'وسائل إزالة تعلق القلب بالذنب','40','0','0');
+INSERT INTO [t36503] VALUES (15,'حكم توبة العاجز عن المعصية','46','0','0');
+INSERT INTO [t36503] VALUES (16,'الوسائل المعينة على التوبة','47','0','0');
+INSERT INTO [t36503] VALUES (17,'الأسباب الصارفة عن التوبة','51','0','0');
+INSERT INTO [t36503] VALUES (18,'علامات صدق التائب','53','0','0');
+INSERT INTO [t36503] VALUES (19,'التوبة العامة والخاصة','56','0','0');
+INSERT INTO [t36503] VALUES (20,'التوبة التامة','58','0','0');
+INSERT INTO [t36503] VALUES (21,'ما ينقض التوبة','60','0','0');
+INSERT INTO [t36503] VALUES (22,'هل العودة إلى الذنب مفسد للتوبة','62','0','0');
+INSERT INTO [t36503] VALUES (23,'طبقات التائبين','65','0','0');
