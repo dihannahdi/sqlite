821ef08ef671115142d9ceac3dfa160171047f21
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:18:41 +0700

    Upload SQL chunk: t1440_chunk_0000.sql

---
+++ sql_chunks/t1440_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t1440
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t1440" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1440
+INSERT INTO [t1440] VALUES (1,'حرف الألف','13','0','0');
+INSERT INTO [t1440] VALUES (2,'حرف الباء الموحدة','52','0','0');
+INSERT INTO [t1440] VALUES (3,'حرف التاء','72','0','0');
+INSERT INTO [t1440] VALUES (4,'حرف الثاء','76','0','0');
+INSERT INTO [t1440] VALUES (5,'حرف الجيم','82','0','0');
+INSERT INTO [t1440] VALUES (6,'حرف الحاء','113','0','0');
+INSERT INTO [t1440] VALUES (7,'حرف الخاء','156','0','0');
+INSERT INTO [t1440] VALUES (8,'حرف الدال','166','0','0');
+INSERT INTO [t1440] VALUES (9,'حرف الذال','171','0','0');
+INSERT INTO [t1440] VALUES (10,'حرف: الراء','181','0','0');
+INSERT INTO [t1440] VALUES (11,'حرف الزاي','195','0','0');
+INSERT INTO [t1440] VALUES (12,'حرف السين','213','0','0');
+INSERT INTO [t1440] VALUES (13,'حرف الشين.','258','0','0');
+INSERT INTO [t1440] VALUES (14,'حرف الصاد.','264','0','0');
+INSERT INTO [t1440] VALUES (15,'حرف الضاد','275','0','0');
+INSERT INTO [t1440] VALUES (16,'حرف الطاء','279','0','0');
+INSERT INTO [t1440] VALUES (17,'حرف الظاء','284','0','0');
+INSERT INTO [t1440] VALUES (18,'حرف العين','285','0','0');
+INSERT INTO [t1440] VALUES (19,'حرف الغين','460','0','0');
+INSERT INTO [t1440] VALUES (20,'حرف الفاء','463','0','0');
+INSERT INTO [t1440] VALUES (21,'حرف القاف','468','0','0');
+INSERT INTO [t1440] VALUES (22,'حرف الكاف','480','0','0');
+INSERT INTO [t1440] VALUES (23,'حرف الام','490','0','0');
+INSERT INTO [t1440] VALUES (24,'حرف اللام','490','0','0');
+INSERT INTO [t1440] VALUES (25,'حرف الميم','499','0','0');
+INSERT INTO [t1440] VALUES (26,'حرف النون','532','0','0');
+INSERT INTO [t1440] VALUES (27,'حرف الواو','552','0','0');
+INSERT INTO [t1440] VALUES (28,'حرف الهاء','558','0','0');
+INSERT INTO [t1440] VALUES (29,'حرف الياء','562','0','0');
