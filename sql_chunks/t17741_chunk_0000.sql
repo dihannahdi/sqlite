5cb02b828e9622a52099b2eb92dbec300ed13c40
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:15 +0700

    Upload SQL chunk: t17741_chunk_0000.sql

---
+++ sql_chunks/t17741_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t17741
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t17741" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17741
+INSERT INTO [t17741] VALUES (1,'[مقدمة المحقق]','5','0','0');
+INSERT INTO [t17741] VALUES (2,'ترجمة المصنف','7','1','0');
+INSERT INTO [t17741] VALUES (3,'تعليق على آخر حديث في رياض الصالحين','14','0','0');
+INSERT INTO [t17741] VALUES (4,'من آثار المحقق','42','0','0');
