2c830ab260a0c3c8b27cce34bb2f4eab5923809b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:35 +0700

    Upload SQL chunk: t6429_chunk_0000.sql

---
+++ sql_chunks/t6429_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t6429
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t6429" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6429
+INSERT INTO [t6429] VALUES (1,'فيه فتيا وجوابها أجاب عنها الإمام الحافظ الناقد شيخ الإسلام وأوحد الأنام','1','0','0');
+INSERT INTO [t6429] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t6429] VALUES (3,'نص الفتيا','2','0','0');
+INSERT INTO [t6429] VALUES (4,'نص الجواب','8','0','0');
+INSERT INTO [t6429] VALUES (5,'ومما جاء في اختلاف التأويل وتنافر القلوب','25','0','0');
+INSERT INTO [t6429] VALUES (6,'فصل في ذم الاهواء المردية والآراء المغوية','30','0','0');
+INSERT INTO [t6429] VALUES (7,'فصل في الاستواء','38','0','0');
