1da41b7f4eedf62567094bb87a202477e6c910bb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:14 +0700

    Upload SQL chunk: t17740_chunk_0000.sql

---
+++ sql_chunks/t17740_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t17740
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t17740" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17740
+INSERT INTO [t17740] VALUES (1,'مقدمة المحقق','4','0','0');
+INSERT INTO [t17740] VALUES (2,'ترجمة مؤلف الرسالة','5','1','0');
+INSERT INTO [t17740] VALUES (3,'اسمه ونسبه','5','2','0');
+INSERT INTO [t17740] VALUES (4,'ولادته ووفاته','5','2','0');
+INSERT INTO [t17740] VALUES (5,'ذكر بعض شيوخه','5','2','0');
+INSERT INTO [t17740] VALUES (6,'بعض مناصبه','5','2','0');
+INSERT INTO [t17740] VALUES (7,'بعض فضائله ومناقبه وثناء العلماء عليه','6','2','0');
+INSERT INTO [t17740] VALUES (8,'مصنفاته','6','2','0');
+INSERT INTO [t17740] VALUES (9,'وصف النسخ الخطية','8','1','0');
+INSERT INTO [t17740] VALUES (10,'منهج التحقيق','9','1','0');
+INSERT INTO [t17740] VALUES (11,'[مقدمة المصنف]','14','0','0');
+INSERT INTO [t17740] VALUES (12,'ذكر عدد من المنكرات والبدع بالمسجد الحرام','15','0','0');
+INSERT INTO [t17740] VALUES (13,'الأدلة من القرآن الكريم على منع رفع الصوت بالدعاء بالمسجد الحرام','20','0','0');
+INSERT INTO [t17740] VALUES (14,'الصحابة والتابعون الوارد عنهم المنع','21','0','0');
+INSERT INTO [t17740] VALUES (15,'المفسرون الوارد عنهم ذلك أيضا','24','0','0');
+INSERT INTO [t17740] VALUES (16,'أقوال الأئمة الأربعة الوارد عنهم ذلك أيضا','24','0','0');
+INSERT INTO [t17740] VALUES (17,'الحنفية','25','16','0');
+INSERT INTO [t17740] VALUES (18,'المالكية','28','16','0');
+INSERT INTO [t17740] VALUES (19,'الشافعية','30','16','0');
+INSERT INTO [t17740] VALUES (20,'الحنابلة','32','16','0');
+INSERT INTO [t17740] VALUES (21,'آخر الرسالة','33','0','0');
