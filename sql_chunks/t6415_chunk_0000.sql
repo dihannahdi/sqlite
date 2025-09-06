b8f0948f0c939122360ecace99390efa99f9b574
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:31 +0700

    Upload SQL chunk: t6415_chunk_0000.sql

---
+++ sql_chunks/t6415_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6415
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6415" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6415
+INSERT INTO [t6415] VALUES (1,'كتاب الرد على من يقول القرآن مخلوق','1','0','0');
+INSERT INTO [t6415] VALUES (2,'وآخر الجزء','42','0','0');
