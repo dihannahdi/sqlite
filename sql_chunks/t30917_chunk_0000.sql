42ef9566e14928300d285ffd28f923f56c564d19
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:41 +0700

    Upload SQL chunk: t30917_chunk_0000.sql

---
+++ sql_chunks/t30917_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t30917
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t30917" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30917
+INSERT INTO [t30917] VALUES (1,'الرسالة الأولى حكم الاحتفال بالمولد النبوي','1','0','0');
+INSERT INTO [t30917] VALUES (2,'الرسالة الثانية حكم الاحتفال بليلة الإسراء والمعراج','18','0','0');
+INSERT INTO [t30917] VALUES (3,'الرسالة الثالثة حكم الاحتفال بليلة النصف من شعبان','27','0','0');
+INSERT INTO [t30917] VALUES (4,'الرسالة الرابعة تكذيب الرؤيا المزعومة من خادم الحجرة النبوية','48','0','0');
