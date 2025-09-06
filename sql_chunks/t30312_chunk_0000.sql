3210d5ff9feeef7a49295060417b54f41f80b6f1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:22 +0700

    Upload SQL chunk: t30312_chunk_0000.sql

---
+++ sql_chunks/t30312_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t30312
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t30312" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30312
+INSERT INTO [t30312] VALUES (1,'فصل فيما ورد في ذم البدع والكلام ومن تعصب لبدعة أو مبتدع، أو قام معه ومدح في رد ذلك','2','0','0');
+INSERT INTO [t30312] VALUES (2,'فصل','122','0','0');
+INSERT INTO [t30312] VALUES (3,'فصل','129','0','0');
+INSERT INTO [t30312] VALUES (4,'فصل','140','0','0');
+INSERT INTO [t30312] VALUES (5,'باب ما ذكر في اجتهاد أبي الحسن في العبادة , ونقل عنه في التقلل والزهادة','143','0','0');
+INSERT INTO [t30312] VALUES (6,'باب ما يسر له في النعمة من كونه في خير قرون هذه الأمة','144','0','0');
+INSERT INTO [t30312] VALUES (7,'فصل','150','0','0');
