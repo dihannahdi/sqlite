d8145102337817b2aac4259248b6bf53b33ef906
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:47:51 +0700

    Upload SQL chunk: t23640_chunk_0000.sql

---
+++ sql_chunks/t23640_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t23640
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t23640" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23640
+INSERT INTO [t23640] VALUES (1,'صور من المخطوطة','2','0','0');
+INSERT INTO [t23640] VALUES (2,'صورة عنوان النسخة الأولى من " التوضيح الأبهر "','2','1','0');
+INSERT INTO [t23640] VALUES (3,'صورة الورقة الآخيرة من النسخة الأولى من " التوضيح الأبهر "','3','1','0');
+INSERT INTO [t23640] VALUES (4,'صورة عنوان النسخة الثانية من " التوضيح الأبهر "','4','1','0');
+INSERT INTO [t23640] VALUES (5,'صورة الورقة الأخيرة من النسخة الثانية من " التوضيح الأبهر "','5','1','0');
+INSERT INTO [t23640] VALUES (6,'مقدمة','6','0','0');
+INSERT INTO [t23640] VALUES (7,'أنواع علم الحديث','7','0','0');
+INSERT INTO [t23640] VALUES (8,'آخر ((التذكرة))','20','0','0');
