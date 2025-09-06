5791110dac501e9f7799cab0fdddc8419c01004f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:35 +0700

    Upload SQL chunk: t858_chunk_0000.sql

---
+++ sql_chunks/t858_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t858
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t858" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t858
+INSERT INTO [t858] VALUES (1,'الباب الأول: حقيقة الإيمان','17','0','0');
+INSERT INTO [t858] VALUES (2,'الباب الثاني: زيادة الإيمان ونقصانه','61','0','0');
+INSERT INTO [t858] VALUES (3,'الباب الثالث: الكفر وضوابط التكفير','76','0','0');
+INSERT INTO [t858] VALUES (4,'الباب الرابع: نواقض الإيمان','145','0','0');
