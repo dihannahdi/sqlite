03882ee5c90e7d9fe77d0609a8131f5ba35e60fe
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:36 +0700

    Upload SQL chunk: t30095_chunk_0000.sql

---
+++ sql_chunks/t30095_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t30095
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t30095" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30095
+INSERT INTO [t30095] VALUES (1,'الجزء الثامن عشر من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30095] VALUES (2,'من حديث أبي علي بن الصواف','2','0','0');
+INSERT INTO [t30095] VALUES (3,'من فوائد أبي محمد الخلال','8','0','0');
+INSERT INTO [t30095] VALUES (4,'من حديث أبي القاسم البغوي','10','0','0');
+INSERT INTO [t30095] VALUES (5,'من فوائد أبي بكر محمد بن الحسين النقاش المقرئ','17','0','0');
+INSERT INTO [t30095] VALUES (6,'من حديث علي بن عمر السكري','18','0','0');
+INSERT INTO [t30095] VALUES (7,'من حديث أبي الحسين الأصبهاني','28','0','0');
+INSERT INTO [t30095] VALUES (8,'حكاية واحدة','29','0','0');
