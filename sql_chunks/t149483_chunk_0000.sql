95913b80023b97c4b487c7dda338b9f045e743c9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:49 +0700

    Upload SQL chunk: t149483_chunk_0000.sql

---
+++ sql_chunks/t149483_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t149483
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t149483" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t149483
+INSERT INTO [t149483] VALUES (1,'الديوان','1','0','0');
+INSERT INTO [t149483] VALUES (2,'الأشعار المنسوبة إلى سلامة مما لم تثبته أصول ديوانه المخطوطة','45','0','0');
