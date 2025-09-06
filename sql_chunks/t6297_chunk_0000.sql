2f4a348f474ed2ca391d4a028fe8f04ab14cca86
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:30 +0700

    Upload SQL chunk: t6297_chunk_0000.sql

---
+++ sql_chunks/t6297_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t6297
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t6297" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6297
+INSERT INTO [t6297] VALUES (1,'معنى أصول الفقه','1','0','0');
+INSERT INTO [t6297] VALUES (2,'أنواع الحكم','2','0','0');
+INSERT INTO [t6297] VALUES (3,'الفرق بين الفقه والعلم والظن والشك','3','0','0');
+INSERT INTO [t6297] VALUES (4,'أبواب أصول الفقه','4','0','0');
+INSERT INTO [t6297] VALUES (5,'أقسام الكلام','5','0','0');
+INSERT INTO [t6297] VALUES (6,'الأمر','7','0','0');
+INSERT INTO [t6297] VALUES (7,'النهي','9','0','0');
+INSERT INTO [t6297] VALUES (8,'العام والخاص','10','0','0');
+INSERT INTO [t6297] VALUES (9,'المجمل والمبين','12','0','0');
+INSERT INTO [t6297] VALUES (10,'الظاهر والمؤول','13','0','0');
+INSERT INTO [t6297] VALUES (11,'الأفعال','14','0','0');
+INSERT INTO [t6297] VALUES (12,'النسخ','15','0','0');
+INSERT INTO [t6297] VALUES (13,'الإجماع','18','0','0');
+INSERT INTO [t6297] VALUES (14,'الأخبار','19','0','0');
+INSERT INTO [t6297] VALUES (15,'القياس','20','0','0');
+INSERT INTO [t6297] VALUES (16,'الحظر والإباحة','21','0','0');
+INSERT INTO [t6297] VALUES (17,'ترتيب الأدلة','22','0','0');
+INSERT INTO [t6297] VALUES (18,'شروط المفتي','23','0','0');
+INSERT INTO [t6297] VALUES (19,'شروط المستفتي','24','0','0');
+INSERT INTO [t6297] VALUES (20,'الاجتهاد','25','0','0');
