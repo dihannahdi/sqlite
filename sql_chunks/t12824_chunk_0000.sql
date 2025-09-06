eeb63978642b0c667bc24c6c793201d488f9de0c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:36 +0700

    Upload SQL chunk: t12824_chunk_0000.sql

---
+++ sql_chunks/t12824_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t12824
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t12824" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12824
+INSERT INTO [t12824] VALUES (1,'الباب الأول في الخصائص التي اختص بها عن جميع الأنبياء ولم يؤتها نبي قبله','5','0','0');
+INSERT INTO [t12824] VALUES (2,'الفصل الأول','6','1','0');
+INSERT INTO [t12824] VALUES (3,'الفصل الثاني','64','1','0');
+INSERT INTO [t12824] VALUES (4,'الفصل الثالث','110','1','0');
+INSERT INTO [t12824] VALUES (5,'الفصل الرابع','120','1','0');
+INSERT INTO [t12824] VALUES (6,'الباب الثاني في الخصائص التي اختص بهاصلى الله عليه وآله وسلم عن أمته ومنها ما علم مشاركة الأنبياء له فيه ومنها ما لم يعلم','125','0','0');
+INSERT INTO [t12824] VALUES (7,'الفصل الأول','125','6','0');
+INSERT INTO [t12824] VALUES (8,'الفصل الثاني','139','6','0');
+INSERT INTO [t12824] VALUES (9,'الفصل الثالث','151','6','0');
+INSERT INTO [t12824] VALUES (10,'الفصل الرابع','170','6','0');
