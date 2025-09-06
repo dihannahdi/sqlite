edd576e79520080277f84523feb65ba1440a2043
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:17 +0700

    Upload SQL chunk: t6972_chunk_0000.sql

---
+++ sql_chunks/t6972_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t6972
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t6972" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6972
+INSERT INTO [t6972] VALUES (1,'في تفسير المفردات وذكر آحكامها','6','0','0');
+INSERT INTO [t6972] VALUES (2,'فصل','13','0','0');
+INSERT INTO [t6972] VALUES (3,'إن المكسورة الخفيفة','22','0','0');
+INSERT INTO [t6972] VALUES (4,'أن المفتوحة الهمزة الساكنة النون','30','0','0');
+INSERT INTO [t6972] VALUES (5,'ألا بفتح الهمزة والتخفيف','84','0','0');
+INSERT INTO [t6972] VALUES (6,'إلا بالكسر والتشديد','87','0','0');
+INSERT INTO [t6972] VALUES (7,'ألا بالفتح والتشديد','91','0','0');
+INSERT INTO [t6972] VALUES (8,'إي بالكسر والسكون','94','0','0');
+INSERT INTO [t6972] VALUES (9,'الفصل الأول','117','0','0');
+INSERT INTO [t6972] VALUES (10,'في خروجها عن الظرفية','117','0','0');
+INSERT INTO [t6972] VALUES (11,'الفصل الثاني','118','0','0');
+INSERT INTO [t6972] VALUES (12,'في خروجها عن الاستقبال','118','0','0');
+INSERT INTO [t6972] VALUES (13,'الفصل الثالث','124','0','0');
+INSERT INTO [t6972] VALUES (14,'في خروج إذا عن الشرطية','124','0','0');
