3bc907699577247db0d0d4606b5eb8c0467560b4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:41 +0700

    Upload SQL chunk: t1598_chunk_0000.sql

---
+++ sql_chunks/t1598_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t1598
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t1598" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1598
+INSERT INTO [t1598] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t1598] VALUES (2,'وصلى الله على سيدنا محمد وآله وسلم تسليما','1','0','0');
+INSERT INTO [t1598] VALUES (3,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t1598] VALUES (4,'أقسام الغناء','4','0','0');
+INSERT INTO [t1598] VALUES (5,'القسم الأول','4','4','0');
+INSERT INTO [t1598] VALUES (6,'بغير آلة ملحن بألحان','4','4','0');
+INSERT INTO [t1598] VALUES (7,'القسم الثاني','11','4','0');
+INSERT INTO [t1598] VALUES (8,'في الغناء المقارن للدف والشبابة','11','4','0');
+INSERT INTO [t1598] VALUES (9,'القسم الثالث','14','4','0');
+INSERT INTO [t1598] VALUES (10,'وهو سماع الغناء بالأوتار وسائر المزامير','14','4','0');
+INSERT INTO [t1598] VALUES (11,'فصل في الرقص','20','0','0');
+INSERT INTO [t1598] VALUES (12,'فصل في من حضر السماع','25','0','0');
+INSERT INTO [t1598] VALUES (13,'حكاية','33','12','0');
+INSERT INTO [t1598] VALUES (14,'خاتمة','35','0','0');
+INSERT INTO [t1598] VALUES (15,'مسألة','36','14','0');
+INSERT INTO [t1598] VALUES (16,'توضيح وبيان','38','14','0');
+INSERT INTO [t1598] VALUES (17,'تتميم وتكميل','41','14','0');
+INSERT INTO [t1598] VALUES (18,'الملاحق','44','14','0');
+INSERT INTO [t1598] VALUES (19,'رقم واحد','44','14','0');
+INSERT INTO [t1598] VALUES (20,'ما جاء في الوليمة وما يكره من السماع فيها','44','14','0');
+INSERT INTO [t1598] VALUES (21,'رقم اثنين','47','14','0');
+INSERT INTO [t1598] VALUES (22,'رأي ابن حزم في اللهو وآلاته','47','14','0');
+INSERT INTO [t1598] VALUES (23,'رقم ثلاثة','52','14','0');
+INSERT INTO [t1598] VALUES (24,'ذكر ما ورد في الغناء من الحظر والإباحة','52','14','0');
+INSERT INTO [t1598] VALUES (25,'رقم أربعة','56','14','0');
+INSERT INTO [t1598] VALUES (26,'حكم الرقص والسماع','56','14','0');
