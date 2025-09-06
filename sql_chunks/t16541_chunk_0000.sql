4904e49f3fd161f846ef431dd8d1398435de972a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:26:18 +0700

    Upload SQL chunk: t16541_chunk_0000.sql

---
+++ sql_chunks/t16541_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t16541
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t16541" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t16541
+INSERT INTO [t16541] VALUES (1,'مقدمة المؤلف','5','0','0');
+INSERT INTO [t16541] VALUES (2,'باب الألف','9','0','0');
+INSERT INTO [t16541] VALUES (3,'باب الباء','29','0','0');
+INSERT INTO [t16541] VALUES (4,'باب التاء','35','0','0');
+INSERT INTO [t16541] VALUES (5,'باب الثاء','39','0','0');
+INSERT INTO [t16541] VALUES (6,'باب الجيم','40','0','0');
+INSERT INTO [t16541] VALUES (7,'باب الحاء','44','0','0');
+INSERT INTO [t16541] VALUES (8,'باب الخاء','51','0','0');
+INSERT INTO [t16541] VALUES (9,'باب الدال','54','0','0');
+INSERT INTO [t16541] VALUES (10,'باب الذال','58','0','0');
+INSERT INTO [t16541] VALUES (11,'باب الراء','60','0','0');
+INSERT INTO [t16541] VALUES (12,'باب الزاء','64','0','0');
+INSERT INTO [t16541] VALUES (13,'باب السين','67','0','0');
+INSERT INTO [t16541] VALUES (14,'باب الشين','74','0','0');
+INSERT INTO [t16541] VALUES (15,'باب الصاد','79','0','0');
+INSERT INTO [t16541] VALUES (16,'باب الضاد','81','0','0');
+INSERT INTO [t16541] VALUES (17,'باب الطاء','82','0','0');
+INSERT INTO [t16541] VALUES (18,'باب الظاء','84','0','0');
+INSERT INTO [t16541] VALUES (19,'باب العين','86','0','0');
+INSERT INTO [t16541] VALUES (20,'باب الغين','93','0','0');
+INSERT INTO [t16541] VALUES (21,'باب الفاء','94','0','0');
+INSERT INTO [t16541] VALUES (22,'باب القاف','98','0','0');
+INSERT INTO [t16541] VALUES (23,'باب الكاف','104','0','0');
+INSERT INTO [t16541] VALUES (24,'باب اللام','109','0','0');
+INSERT INTO [t16541] VALUES (25,'باب الميم','112','0','0');
+INSERT INTO [t16541] VALUES (26,'باب النون','128','0','0');
+INSERT INTO [t16541] VALUES (27,'باب الواو','132','0','0');
+INSERT INTO [t16541] VALUES (28,'باب الهاء','134','0','0');
+INSERT INTO [t16541] VALUES (29,'باب الياء','137','0','0');
