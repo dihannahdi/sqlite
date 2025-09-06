d795ea820996819113f3f99185b7f103f6ae68e3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:41 +0700

    Upload SQL chunk: t11056_chunk_0000.sql

---
+++ sql_chunks/t11056_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t11056
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t11056" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11056
+INSERT INTO [t11056] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t11056] VALUES (2,'غناء جارية ولحنها بين يدي الواثق','2','0','0');
+INSERT INTO [t11056] VALUES (3,'ترجمة المعتزلة','12','0','0');
+INSERT INTO [t11056] VALUES (4,'معن بن زائدة','129','0','0');
+INSERT INTO [t11056] VALUES (5,'يزيد بن المهلب','129','0','0');
+INSERT INTO [t11056] VALUES (6,'يزيد بن حاتم','129','0','0');
+INSERT INTO [t11056] VALUES (7,'أبو دلف','130','0','0');
+INSERT INTO [t11056] VALUES (8,'خالد بن عبد الله القسري','130','0','0');
+INSERT INTO [t11056] VALUES (9,'عدي بن حاتم','130','0','0');
+INSERT INTO [t11056] VALUES (10,'عيون المها','138','0','0');
+INSERT INTO [t11056] VALUES (11,'ذكاء المنصور','139','0','0');
+INSERT INTO [t11056] VALUES (12,'المهدي والقاضي شريك','140','0','0');
+INSERT INTO [t11056] VALUES (13,'التاجر الذي أنكر العقد','141','0','0');
+INSERT INTO [t11056] VALUES (14,'ذكاء إياس','142','0','0');
+INSERT INTO [t11056] VALUES (15,'ذكاء في القضاء','142','0','0');
+INSERT INTO [t11056] VALUES (16,'يحيى بن أكثم القاضي','144','0','0');
+INSERT INTO [t11056] VALUES (17,'فتوى أبي حنيفة','144','0','0');
+INSERT INTO [t11056] VALUES (18,'الإفراط في ذكاء العرب','148','0','0');
+INSERT INTO [t11056] VALUES (19,'الرشيد وأجر وصفة الشيخ','150','0','0');
+INSERT INTO [t11056] VALUES (20,'ذكاء طبيب','151','0','0');
+INSERT INTO [t11056] VALUES (21,'ذكاء متطفل','152','0','0');
+INSERT INTO [t11056] VALUES (22,'ذكاء متلصص','154','0','0');
+INSERT INTO [t11056] VALUES (23,'ذكاء صبي','155','0','0');
+INSERT INTO [t11056] VALUES (24,'من أذكياء النساء','155','0','0');
+INSERT INTO [t11056] VALUES (25,'ذكاء الحيوان','156','0','0');
+INSERT INTO [t11056] VALUES (26,'من أخبار الحمقى','157','0','0');
+INSERT INTO [t11056] VALUES (27,'أبو عيشان','159','0','0');
+INSERT INTO [t11056] VALUES (28,'ربيعة البكاء','160','0','0');
+INSERT INTO [t11056] VALUES (29,'حمزة بن بيض','160','0','0');
+INSERT INTO [t11056] VALUES (30,'ابن الجصاص','161','0','0');
+INSERT INTO [t11056] VALUES (31,'يسوق الحمير ويعدها','169','0','0');
+INSERT INTO [t11056] VALUES (32,'أخبار وطرف','170','0','0');
+INSERT INTO [t11056] VALUES (33,'معلمي الصبيان','171','0','0');
+INSERT INTO [t11056] VALUES (34,'الوليد ومسامر الخلفاء','172','0','0');
+INSERT INTO [t11056] VALUES (35,'بسم الله الرحمن الرحيم','388','0','0');
+INSERT INTO [t11056] VALUES (36,'بسم الله الرحمن الرحيم','459','0','0');
+INSERT INTO [t11056] VALUES (37,'أمنية أحمقين','480','0','0');
+INSERT INTO [t11056] VALUES (38,'بكاء زين العابدين','480','0','0');
+INSERT INTO [t11056] VALUES (39,'قصة أحمد اليتيم','498','0','0');
