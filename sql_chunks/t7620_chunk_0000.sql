694d5d1a2ee6f714b55b6b9e4bd29e139ff4f1ab
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:44 +0700

    Upload SQL chunk: t7620_chunk_0000.sql

---
+++ sql_chunks/t7620_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7620
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7620" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7620
+INSERT INTO [t7620] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7620] VALUES (2,'الرد على البردة','9','0','0');
