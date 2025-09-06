caa8c703caf24b671d5fa03aff5c841981fe3bfc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:24 +0700

    Upload SQL chunk: t21540_chunk_0000.sql

---
+++ sql_chunks/t21540_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t21540
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t21540" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21540
+INSERT INTO [t21540] VALUES (1,'فصل في آداب الطالب','3','0','0');
+INSERT INTO [t21540] VALUES (2,'فصل في ألقاب المتن','5','0','0');
+INSERT INTO [t21540] VALUES (3,'فصل في ألقاب الرواة','7','0','0');
+INSERT INTO [t21540] VALUES (4,'فصل في أنواع الحديث المقبول','10','0','0');
+INSERT INTO [t21540] VALUES (5,'فصل في أنواع الحديث المردود','11','0','0');
+INSERT INTO [t21540] VALUES (6,'فصل في أنواع الحديث الغلط','13','0','0');
+INSERT INTO [t21540] VALUES (7,'فصل','14','0','0');
+INSERT INTO [t21540] VALUES (8,'فصل في أنواع التحمل وصيغ الأداء','17','0','0');
+INSERT INTO [t21540] VALUES (9,'فصل في أسباب التوثيق للرواة','19','0','0');
+INSERT INTO [t21540] VALUES (10,'فصل في أسباب الجرح','21','0','0');
+INSERT INTO [t21540] VALUES (11,'فصل في قواعد الجرح والتعديل','23','0','0');
+INSERT INTO [t21540] VALUES (12,'فصل في أنواع كتب الحديث','24','0','0');
+INSERT INTO [t21540] VALUES (13,'فصل:','26','0','0');
