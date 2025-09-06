008ef0bc17c3b40c5b80711a16c5def3a18ae866
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:07 +0700

    Upload SQL chunk: t147065_chunk_0000.sql

---
+++ sql_chunks/t147065_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t147065
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t147065" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t147065
+INSERT INTO [t147065] VALUES (1,'مقدمة الناشر','6','0','0');
+INSERT INTO [t147065] VALUES (2,'ترجمة مرعي الكرمي','6','1','0');
+INSERT INTO [t147065] VALUES (3,'اسمه ونسبه','6','2','0');
+INSERT INTO [t147065] VALUES (4,'مولده ونشأته','6','2','0');
+INSERT INTO [t147065] VALUES (5,'مذهبه وعلمه، وثناء العلماء عليه','7','2','0');
+INSERT INTO [t147065] VALUES (6,'شيوخه','8','2','0');
+INSERT INTO [t147065] VALUES (7,'إقراؤه وتدريسه','8','2','0');
+INSERT INTO [t147065] VALUES (8,'مصنفاته','8','2','0');
+INSERT INTO [t147065] VALUES (9,'أ - المطبوعة','9','8','0');
+INSERT INTO [t147065] VALUES (10,'ب - المخطوطة','10','8','0');
+INSERT INTO [t147065] VALUES (11,'شعره','18','2','0');
+INSERT INTO [t147065] VALUES (12,'وفاته','20','2','0');
+INSERT INTO [t147065] VALUES (13,'مصادر ترجمته','20','2','0');
+INSERT INTO [t147065] VALUES (14,'هذا الكتاب','21','1','0');
+INSERT INTO [t147065] VALUES (15,'هذه الطبعة','23','1','0');
+INSERT INTO [t147065] VALUES (16,'مقدمة المصنف','25','0','0');
+INSERT INTO [t147065] VALUES (17,'فصل [في] [نفع المزاح أحيانا في مقام الشدائد]','42','0','0');
+INSERT INTO [t147065] VALUES (18,'فصل [في] [ذم المزح]','44','0','0');
