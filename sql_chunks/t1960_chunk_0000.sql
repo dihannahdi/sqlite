a3f0262f5510b982e5bd3680e0209138d9d22978
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:38 +0700

    Upload SQL chunk: t1960_chunk_0000.sql

---
+++ sql_chunks/t1960_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t1960
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t1960" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1960
+INSERT INTO [t1960] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1960] VALUES (2,'القسم الأول الفرص الدعوية','4','0','0');
+INSERT INTO [t1960] VALUES (3,'المساجد','4','2','0');
+INSERT INTO [t1960] VALUES (4,'المقابر ومغاسل الموتى','8','2','0');
+INSERT INTO [t1960] VALUES (5,'الحدائق والمنتزهات','9','2','0');
+INSERT INTO [t1960] VALUES (6,'المستشفيات','10','2','0');
+INSERT INTO [t1960] VALUES (7,'الفنادق','12','2','0');
+INSERT INTO [t1960] VALUES (8,'التسجيلات ودور النشر','13','2','0');
+INSERT INTO [t1960] VALUES (9,'المدارس','14','2','0');
+INSERT INTO [t1960] VALUES (10,'أماكن العمل','19','2','0');
+INSERT INTO [t1960] VALUES (11,'وسائل المواصلات','20','2','0');
+INSERT INTO [t1960] VALUES (12,'السجون ودور الملاحظة','22','2','0');
+INSERT INTO [t1960] VALUES (13,'رمضان','23','2','0');
+INSERT INTO [t1960] VALUES (14,'الإجازات الموسمية','26','2','0');
+INSERT INTO [t1960] VALUES (15,'الدعوة داخل أوساط الأسرة','27','2','0');
+INSERT INTO [t1960] VALUES (16,'الدعوة مع البرامج الإغاثية','29','2','0');
+INSERT INTO [t1960] VALUES (17,'الدعوة الفردية','30','2','0');
+INSERT INTO [t1960] VALUES (18,'الدعوة عبر الوسائل الحديثة','31','2','0');
+INSERT INTO [t1960] VALUES (19,'القسم الثاني الوسائل الدعوية','32','0','0');
+INSERT INTO [t1960] VALUES (20,'هدية اللباس','32','19','0');
+INSERT INTO [t1960] VALUES (21,'هدية سيارات الأجرة','33','19','0');
+INSERT INTO [t1960] VALUES (22,'هدية سيارات التأجير','34','19','0');
+INSERT INTO [t1960] VALUES (23,'هدية المرضى','35','19','0');
+INSERT INTO [t1960] VALUES (24,'هدية الحلاقين والمشاغل النسائية','36','19','0');
+INSERT INTO [t1960] VALUES (25,'المكتبة الدعوية','37','19','0');
+INSERT INTO [t1960] VALUES (26,'حامل خذ نسختك','38','19','0');
+INSERT INTO [t1960] VALUES (27,'العبارات الدعوية على الأكياس','39','19','0');
+INSERT INTO [t1960] VALUES (28,'المجلة الحائطية','40','19','0');
+INSERT INTO [t1960] VALUES (29,'اللوحات الإرشادية','41','19','0');
+INSERT INTO [t1960] VALUES (30,'تحف الأذكار','42','19','0');
+INSERT INTO [t1960] VALUES (31,'الكفيل الداعية','43','19','0');
+INSERT INTO [t1960] VALUES (32,'الحقيبة الدعوية','44','19','0');
+INSERT INTO [t1960] VALUES (33,'حقيبة الانتظار','45','19','0');
+INSERT INTO [t1960] VALUES (34,'حقيبة المسافر','46','19','0');
+INSERT INTO [t1960] VALUES (35,'المصليات المتنقلة','47','19','0');
+INSERT INTO [t1960] VALUES (36,'الرحلات السياحية الدعوية للجاليات','48','19','0');
+INSERT INTO [t1960] VALUES (37,'خاتمة','49','0','0');
+INSERT INTO [t1960] VALUES (38,'المراجع','50','0','0');
