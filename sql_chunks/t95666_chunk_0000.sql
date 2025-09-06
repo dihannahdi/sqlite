da70e4051b88a09c2cf92899540bcdd4f0e352fe
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:53 +0700

    Upload SQL chunk: t95666_chunk_0000.sql

---
+++ sql_chunks/t95666_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t95666
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t95666" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95666
+INSERT INTO [t95666] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95666] VALUES (2,'وأحسنوا إن الله يحب المحسنين','2','0','0');
+INSERT INTO [t95666] VALUES (3,'جزاء الإحسان','4','0','0');
+INSERT INTO [t95666] VALUES (4,'من صور الإحسان إلى الناس','7','0','0');
+INSERT INTO [t95666] VALUES (5,'وقفة','24','0','0');
+INSERT INTO [t95666] VALUES (6,'الأجور العظيمة','25','0','0');
+INSERT INTO [t95666] VALUES (7,'متطلبات الإحسان','31','0','0');
