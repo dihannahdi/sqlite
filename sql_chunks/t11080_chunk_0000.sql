68ded97128d77e1c60828f7e4b92fe1064a40292
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:59 +0700

    Upload SQL chunk: t11080_chunk_0000.sql

---
+++ sql_chunks/t11080_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t11080
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t11080" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11080
+INSERT INTO [t11080] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11080] VALUES (2,'الإعجاز لغة واصطلاحا','3','0','0');
+INSERT INTO [t11080] VALUES (3,'معجزة رسول الله محمد صلى الله عليه وسلم','7','0','0');
+INSERT INTO [t11080] VALUES (4,'سر الإعجاز في القرآن الكريم','16','0','0');
+INSERT INTO [t11080] VALUES (5,'وجوه الإعجاز في القرآن الكريم','19','0','0');
+INSERT INTO [t11080] VALUES (6,'مدخل','19','5','0');
+INSERT INTO [t11080] VALUES (7,'الوجه الأول: كونه عربيا','21','5','0');
+INSERT INTO [t11080] VALUES (8,'الوجه الثاني: أنه حق','24','5','0');
+INSERT INTO [t11080] VALUES (9,'الوجه الثالث: "كون القرآن هدى"','30','5','0');
+INSERT INTO [t11080] VALUES (10,'الوجه الرابع: أن القرآن الكريم ذو شرف ويشرف من نسب إليه','34','5','0');
+INSERT INTO [t11080] VALUES (11,'وجوه الإعجاز في السنة النبوية الشريفة','37','0','0');
+INSERT INTO [t11080] VALUES (12,'ضوابط القول بالإعجاز','43','0','0');
+INSERT INTO [t11080] VALUES (13,'الضابط الأول','43','12','0');
+INSERT INTO [t11080] VALUES (14,'الضابط الثاني','45','12','0');
+INSERT INTO [t11080] VALUES (15,'الضابط الثالث','47','12','0');
+INSERT INTO [t11080] VALUES (16,'الضابط الرابع','48','12','0');
+INSERT INTO [t11080] VALUES (17,'مصادر ومراجع','51','0','0');
