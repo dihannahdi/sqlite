7aefc415bd10bbb6cb4b10ff5ddb83a416041397
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:49 +0700

    Upload SQL chunk: t10531_chunk_0000.sql

---
+++ sql_chunks/t10531_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t10531
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t10531" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10531
+INSERT INTO [t10531] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10531] VALUES (2,'باب فضل حملة القرآن','14','0','0');
+INSERT INTO [t10531] VALUES (3,'باب فضل من تعلم القرآن وعلمه','23','0','0');
+INSERT INTO [t10531] VALUES (4,'باب فضل الاجتماع في المسجد لدرس القرآن','28','0','0');
+INSERT INTO [t10531] VALUES (5,'باب ذكر أخلاق أهل القرآن','32','0','0');
+INSERT INTO [t10531] VALUES (6,'باب أخلاق من قرأ القرآن لا يريد به الله عز وجل','41','0','0');
+INSERT INTO [t10531] VALUES (7,'باب أخلاق المقرئ إذا جلس يقرئ ويلقن لله عز وجل ماذا ينبغي له أن يتخلق به','59','0','0');
+INSERT INTO [t10531] VALUES (8,'باب ذكر أخلاق من يقرأ على المقرئ','78','0','0');
+INSERT INTO [t10531] VALUES (9,'باب أدب القراء عند تلاوتهم القرآن مما لا ينبغي لهم جهله','87','0','0');
+INSERT INTO [t10531] VALUES (10,'باب في حسن الصوت بالقرآن','100','0','0');
+INSERT INTO [t10531] VALUES (11,'ملحق بهذا الكتاب','111','0','0');
