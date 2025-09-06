04613085bcff9973b5ee2863cd3782ddd69e5a9a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:28 +0700

    Upload SQL chunk: t95567_chunk_0000.sql

---
+++ sql_chunks/t95567_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t95567
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t95567" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95567
+INSERT INTO [t95567] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t95567] VALUES (2,'التعريفات','4','0','0');
+INSERT INTO [t95567] VALUES (3,'حتمية الخلاف','9','0','0');
+INSERT INTO [t95567] VALUES (4,'تاريخ الحوار','14','0','0');
+INSERT INTO [t95567] VALUES (5,'أنواع الحوار ومشروعيتها','20','0','0');
+INSERT INTO [t95567] VALUES (6,'أ. حوار الدعوة','20','5','0');
+INSERT INTO [t95567] VALUES (7,'ب. حوار التعامل','26','5','0');
+INSERT INTO [t95567] VALUES (8,'ج. حوار الوحدة','31','5','0');
+INSERT INTO [t95567] VALUES (9,'وحدة الدين','37','0','0');
+INSERT INTO [t95567] VALUES (10,'آداب الحوار','40','0','0');
+INSERT INTO [t95567] VALUES (11,'هل آيات الأمر بالدعوة والجدال والحوار منسوخة بآية السيف؟','56','0','0');
+INSERT INTO [t95567] VALUES (12,'محظورات في الحوار','65','0','0');
+INSERT INTO [t95567] VALUES (13,'خاتمة','75','0','0');
+INSERT INTO [t95567] VALUES (14,'المصادر والمراجع','76','0','0');
