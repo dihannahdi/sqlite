e86f5b7eaa3cb3e2cfb30b9b1381dd6273138b57
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:51 +0700

    Upload SQL chunk: t30928_chunk_0000.sql

---
+++ sql_chunks/t30928_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t30928
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t30928" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30928
+INSERT INTO [t30928] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t30928] VALUES (2,'المحور الأول الخطبة','2','0','0');
+INSERT INTO [t30928] VALUES (3,'الغرض من الخطبة','2','2','0');
+INSERT INTO [t30928] VALUES (4,'صفة الخطبة وسياقها','4','2','0');
+INSERT INTO [t30928] VALUES (5,'قواعد وضوابط إعداد الخطبة','5','2','0');
+INSERT INTO [t30928] VALUES (6,'المحور الثاني الخطيب ومسؤولياته','18','0','0');
+INSERT INTO [t30928] VALUES (7,'صفات الخطيب','18','6','0');
+INSERT INTO [t30928] VALUES (8,'مسؤوليات وواجبات الخطيب','20','6','0');
+INSERT INTO [t30928] VALUES (9,'أخطاء يقع فيها بعض الخطباء','21','6','0');
