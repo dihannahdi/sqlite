0b763ea6a72349adc0e449bdd03d48513f407fc3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:14 +0700

    Upload SQL chunk: t8560_chunk_0000.sql

---
+++ sql_chunks/t8560_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t8560
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t8560" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8560
+INSERT INTO [t8560] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8560] VALUES (2,'هدفنا من صنع هذا المعجم','4','0','0');
+INSERT INTO [t8560] VALUES (3,'ما نعنيه بالمعجم الموسوعي','6','0','0');
+INSERT INTO [t8560] VALUES (4,'ملاحظات على أعمال سابقة','8','0','0');
+INSERT INTO [t8560] VALUES (5,'لماذا الاهتمام بالقراءات القرآنية؟','16','0','0');
+INSERT INTO [t8560] VALUES (6,'منهج المعجم','29','0','0');
+INSERT INTO [t8560] VALUES (7,'قواعد عامة','55','0','0');
+INSERT INTO [t8560] VALUES (8,'النسخة الإلكترونية','57','0','0');
+INSERT INTO [t8560] VALUES (9,'المرفقات','59','0','0');
