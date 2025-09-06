e629528bd35636c32ffd7fbabc1163d6f65c5e14
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:07 +0700

    Upload SQL chunk: t13644_chunk_0000.sql

---
+++ sql_chunks/t13644_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t13644
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t13644" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13644
+INSERT INTO [t13644] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t13644] VALUES (2,'القضية الحاملة على هذا التأليف','3','0','0');
+INSERT INTO [t13644] VALUES (3,'الحكم الذي أبديناه في "يا عديم الدين"','4','0','0');
+INSERT INTO [t13644] VALUES (4,'رد كلام المعترضين','23','0','0');
+INSERT INTO [t13644] VALUES (5,'الكلام على بقية الألفاظ والأفعال التي توقع في الكفر','29','0','0');
+INSERT INTO [t13644] VALUES (6,'تعليق المؤلف على أحد كتب الأحناف في هذا الباب وفيه زيادات','90','0','0');
+INSERT INTO [t13644] VALUES (7,'تتمات وفوائد','174','0','0');
+INSERT INTO [t13644] VALUES (8,'١','174','7','0');
+INSERT INTO [t13644] VALUES (9,'٢','192','7','0');
+INSERT INTO [t13644] VALUES (10,'٣','193','7','0');
+INSERT INTO [t13644] VALUES (11,'٤','203','7','0');
+INSERT INTO [t13644] VALUES (12,'٥','204','7','0');
+INSERT INTO [t13644] VALUES (13,'٦','205','7','0');
+INSERT INTO [t13644] VALUES (14,'٧','207','7','0');
+INSERT INTO [t13644] VALUES (15,'٨','207','7','0');
+INSERT INTO [t13644] VALUES (16,'٩','208','7','0');
+INSERT INTO [t13644] VALUES (17,'١٠','210','7','0');
+INSERT INTO [t13644] VALUES (18,'١١','212','7','0');
+INSERT INTO [t13644] VALUES (19,'١٢','220','7','0');
+INSERT INTO [t13644] VALUES (20,'١٣','220','7','0');
+INSERT INTO [t13644] VALUES (21,'١٤','220','7','0');
+INSERT INTO [t13644] VALUES (22,'الخاتمة','221','0','0');
