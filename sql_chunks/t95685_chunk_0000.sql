7769edddfdef41f8f6bf0999e98ccba8e4fd8c11
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:13 +0700

    Upload SQL chunk: t95685_chunk_0000.sql

---
+++ sql_chunks/t95685_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t95685
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t95685" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95685
+INSERT INTO [t95685] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95685] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t95685] VALUES (3,'المحبة في الله','35','0','0');
+INSERT INTO [t95685] VALUES (4,'المصادر','57','0','0');
