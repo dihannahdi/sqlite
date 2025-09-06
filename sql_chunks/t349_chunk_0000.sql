47a19fd8836f86879d80b292d1dec888622b538a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:45 +0700

    Upload SQL chunk: t349_chunk_0000.sql

---
+++ sql_chunks/t349_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t349
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t349" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t349
+INSERT INTO [t349] VALUES (1,'تحذير الساجد من اتخاذ القبور مساجد','1','0','0');
+INSERT INTO [t349] VALUES (2,'مقدمة الطبعة الثانية','2','0','0');
+INSERT INTO [t349] VALUES (3,'الفصل الأول','8','0','0');
+INSERT INTO [t349] VALUES (4,'الفصل الثاني','27','0','0');
+INSERT INTO [t349] VALUES (5,'الفصل الثالث','40','0','0');
+INSERT INTO [t349] VALUES (6,'الفصل الرابع','53','0','0');
+INSERT INTO [t349] VALUES (7,'الفصل الخامس','95','0','0');
+INSERT INTO [t349] VALUES (8,'الفصل السادس','108','0','0');
+INSERT INTO [t349] VALUES (9,'الفصل السابع','110','0','0');
