c25cc611545eaed6a9dedb91a260bcc8635bcff5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:51 +0700

    Upload SQL chunk: t30124_chunk_0000.sql

---
+++ sql_chunks/t30124_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t30124
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t30124" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30124
+INSERT INTO [t30124] VALUES (1,'الجزء السابع والعشرون من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30124] VALUES (2,'من حديث أبي محمد الخلال','2','0','0');
+INSERT INTO [t30124] VALUES (3,'من فوائد عبد العزيز الأزجي','6','0','0');
+INSERT INTO [t30124] VALUES (4,'من حديث أبي طالب الشاري','11','0','0');
+INSERT INTO [t30124] VALUES (5,'من حديث أبي الحسن الطريثيثي','21','0','0');
+INSERT INTO [t30124] VALUES (6,'من فوائد علي بن عمر الحربي','25','0','0');
+INSERT INTO [t30124] VALUES (7,'من حديث أبي الأحوص سلام بن سليم','28','0','0');
+INSERT INTO [t30124] VALUES (8,'من حديث الشريف أبي العباس بن الزوال','32','0','0');
+INSERT INTO [t30124] VALUES (9,'حديث واحد من حديث الخلال وما فيه من حديث الجوهري عن ابن حيويه','36','0','0');
