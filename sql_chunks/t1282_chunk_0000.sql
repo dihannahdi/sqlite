ff1a87bea5ffec24f79f66b9038a84fec5a4efe2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:43 +0700

    Upload SQL chunk: t1282_chunk_0000.sql

---
+++ sql_chunks/t1282_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t1282
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t1282" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1282
+INSERT INTO [t1282] VALUES (1,'مفهوم الوسطية','1','0','0');
+INSERT INTO [t1282] VALUES (2,'الوسطية في القرآن والسنة','3','0','0');
+INSERT INTO [t1282] VALUES (3,'الوسطية في العقيدة','10','0','0');
+INSERT INTO [t1282] VALUES (4,'الوسطية في التشريع','11','0','0');
+INSERT INTO [t1282] VALUES (5,'الوسطية في التكاليف والعبادات','12','0','0');
+INSERT INTO [t1282] VALUES (6,'أهمية موضوع وسطية الإسلام وسماحته','16','0','0');
+INSERT INTO [t1282] VALUES (7,'الوسطية في الدعوة','22','0','0');
+INSERT INTO [t1282] VALUES (8,'وسطية أهل السنة','26','0','0');
+INSERT INTO [t1282] VALUES (9,'صور من وسطية الإسلام','28','0','0');
+INSERT INTO [t1282] VALUES (10,'الدعوة الإسلامية واستخدام الحوار','37','0','0');
+INSERT INTO [t1282] VALUES (11,'مجالات الوسطية في الإسلام','43','0','0');
+INSERT INTO [t1282] VALUES (12,'شمولية الوسطية في الإسلام لكل قضايا الإنسان','47','0','0');
+INSERT INTO [t1282] VALUES (13,'الوسطية في الحقوق','50','0','0');
+INSERT INTO [t1282] VALUES (14,'وسطية الإسلام في إنفاق المال','53','0','0');
+INSERT INTO [t1282] VALUES (15,'الوسطية في النفس','56','0','0');
