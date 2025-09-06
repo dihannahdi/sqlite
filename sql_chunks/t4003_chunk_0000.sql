fdc774e052d65c80af5a0b850c35a9f812e90a42
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:41 +0700

    Upload SQL chunk: t4003_chunk_0000.sql

---
+++ sql_chunks/t4003_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t4003
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t4003" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4003
+INSERT INTO [t4003] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t4003] VALUES (2,'الجزء الأول','2','0','0');
+INSERT INTO [t4003] VALUES (3,'أول الجزء الثاني','57','0','0');
+INSERT INTO [t4003] VALUES (4,'أول الجزء الثالث','112','0','0');
