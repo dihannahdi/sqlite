a3ebe980b36c19147ddbf42957282890d6c1480d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:26 +0700

    Upload SQL chunk: t10373_chunk_0000.sql

---
+++ sql_chunks/t10373_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10373
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10373" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10373
+INSERT INTO [t10373] VALUES (1,'الذبائح في مناسك الحج مصادرها ومصارفها','1','0','0');
+INSERT INTO [t10373] VALUES (2,'بم يثبت الهدى','5','0','0');
+INSERT INTO [t10373] VALUES (3,'مكان النحر وزمانه','7','0','0');
