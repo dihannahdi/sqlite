74d10f22ca1987157e328890c0c6d0169a227c91
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:36 +0700

    Upload SQL chunk: t11369_chunk_0000.sql

---
+++ sql_chunks/t11369_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11369
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11369" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11369
+INSERT INTO [t11369] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11369] VALUES (2,'مبحث الاجتهاد والخلاف','3','0','0');
