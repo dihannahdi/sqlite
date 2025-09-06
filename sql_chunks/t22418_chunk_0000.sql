ad5e9c8c834801b83865eac48a41300e6ddbc424
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:28 +0700

    Upload SQL chunk: t22418_chunk_0000.sql

---
+++ sql_chunks/t22418_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t22418
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t22418" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22418
+INSERT INTO [t22418] VALUES (1,'فوائد ابن شاهين','1','0','0');
