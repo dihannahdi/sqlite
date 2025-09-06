eb3a76e334aafd0502ad4dbd7b11947557e27a90
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:10 +0700

    Upload SQL chunk: t95682_chunk_0000.sql

---
+++ sql_chunks/t95682_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t95682
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t95682" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95682
+INSERT INTO [t95682] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95682] VALUES (2,'يقول ابن القيم -رحمه الله-','4','0','0');
+INSERT INTO [t95682] VALUES (3,'فوائد الحرص على إقامة صلاة الأبناء في المسجد','4','0','0');
+INSERT INTO [t95682] VALUES (4,'ومن الأسباب المعينة على ذلك','5','3','0');
+INSERT INTO [t95682] VALUES (5,'أيها الأب','9','0','0');
