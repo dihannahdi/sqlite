cd75ee4c6bed8b6f329851278434794dd0edfe58
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:06 +0700

    Upload SQL chunk: t31324_chunk_0000.sql

---
+++ sql_chunks/t31324_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t31324
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t31324" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31324
+INSERT INTO [t31324] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t31324] VALUES (2,'التبرج','4','0','0');
+INSERT INTO [t31324] VALUES (3,'وجوب الحجاب والنهي عن التبرج','4','2','0');
+INSERT INTO [t31324] VALUES (4,'خطر مشاركة المرأة للرجل في ميدان عمله','20','0','0');
+INSERT INTO [t31324] VALUES (5,'العواقب الوخيمة لخروج المرأة للعمل','20','4','0');
+INSERT INTO [t31324] VALUES (6,'تحريم الاختلاط وتحريم جميع الوسائل المؤديه إليه','24','4','0');
+INSERT INTO [t31324] VALUES (7,'اعتراف رجال الغرب والشرق بمضار الاختلاط ومفاسده','34','4','0');
