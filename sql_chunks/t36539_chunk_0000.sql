1e4b2180bf91745bd7e9be2768a4b29b2dee8c91
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:55 +0700

    Upload SQL chunk: t36539_chunk_0000.sql

---
+++ sql_chunks/t36539_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t36539
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t36539" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36539
+INSERT INTO [t36539] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36539] VALUES (2,'معنى الوقف والسكت والقطع','5','0','0');
+INSERT INTO [t36539] VALUES (3,'أهمية علم الوقف والابتداء','8','0','0');
+INSERT INTO [t36539] VALUES (4,'عبارات منقولة عن السلف في الوقف','23','0','0');
+INSERT INTO [t36539] VALUES (5,'تخريج حديث أم سلمة','53','0','0');
+INSERT INTO [t36539] VALUES (6,'سند الحديث والحكم عليه','56','0','0');
+INSERT INTO [t36539] VALUES (7,'معنى الحديث','69','0','0');
+INSERT INTO [t36539] VALUES (8,'حكم الوقف على رؤوس الآي عند علماء الوقف وغيرهم','76','0','0');
