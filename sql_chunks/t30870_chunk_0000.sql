5fce63980ebaf8486fd2e93430e2885415733ee8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:18 +0700

    Upload SQL chunk: t30870_chunk_0000.sql

---
+++ sql_chunks/t30870_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t30870
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t30870" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30870
+INSERT INTO [t30870] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t30870] VALUES (2,'التحاكم إلى الله من مقتضى شهادة أن لا إله إلا الله وأن محمدا عبده ورسوله','3','0','0');
+INSERT INTO [t30870] VALUES (3,'التحاكم إلى الطواغيت والرؤساء والعرافين ونحوهم ينافي الإيمان','7','0','0');
+INSERT INTO [t30870] VALUES (4,'التحاكم بين العباد بشرعه ووحيه لأنه سبحانه المنزه عما يصيب البشر وهو العليم بأحوال عباده وما يصلحهم','12','0','0');
+INSERT INTO [t30870] VALUES (5,'خاتمة','14','0','0');
