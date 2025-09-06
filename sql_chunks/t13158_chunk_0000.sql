aa4ebca999988d0a1b82c517cd8f827decfe6a12
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:26 +0700

    Upload SQL chunk: t13158_chunk_0000.sql

---
+++ sql_chunks/t13158_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t13158
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t13158" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13158
+INSERT INTO [t13158] VALUES (1,'ما رواه ابن عباس عن أسامة بن زيد','15','0','0');
+INSERT INTO [t13158] VALUES (2,'عبد الله بن عمر، عن أسامة','33','0','0');
+INSERT INTO [t13158] VALUES (3,'أبو هريرة عن أسامة بن زيد','50','0','0');
+INSERT INTO [t13158] VALUES (4,'جابر بن عبد الله، عن أسامة','53','0','0');
+INSERT INTO [t13158] VALUES (5,'أبو وائل شقيق بن سلمة وأدرك النبي صلى الله عليه وعلى آله وسلم عن أسامة بن زيد','56','0','0');
