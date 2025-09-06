ce537038eac1034615da05551ce7cf2cdfca6b5c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:54 +0700

    Upload SQL chunk: t8624_chunk_0000.sql

---
+++ sql_chunks/t8624_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t8624
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t8624" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8624
+INSERT INTO [t8624] VALUES (1,'النكاح','1','0','0');
+INSERT INTO [t8624] VALUES (2,'أداب النكاح وسننة','4','0','0');
+INSERT INTO [t8624] VALUES (3,'الشروط في النكاح','7','0','0');
+INSERT INTO [t8624] VALUES (4,'الخيار في بقاء النكاح أو فسخه','8','0','0');
+INSERT INTO [t8624] VALUES (5,'حقوق الزوجين','9','0','0');
+INSERT INTO [t8624] VALUES (6,'حقوق الزوج','11','0','0');
+INSERT INTO [t8624] VALUES (7,'الأنكحة المحرمة','13','0','0');
+INSERT INTO [t8624] VALUES (8,'المحرمات من النساء تحريما أبديا','15','0','0');
+INSERT INTO [t8624] VALUES (9,'الطلاق واحكامه','17','0','0');
+INSERT INTO [t8624] VALUES (10,'الطلاق السني','18','0','0');
+INSERT INTO [t8624] VALUES (11,'صيغة الطلاق السني الشرعي','20','0','0');
+INSERT INTO [t8624] VALUES (12,'الطلاق البدعي المحرم','22','0','0');
+INSERT INTO [t8624] VALUES (13,'صور الطلاق البدعي','24','0','0');
+INSERT INTO [t8624] VALUES (14,'تفاصيل هذه الطلاق البدعي هي:','25','0','0');
+INSERT INTO [t8624] VALUES (15,'أحكام الطلاق','27','0','0');
+INSERT INTO [t8624] VALUES (16,'الخاتمة','29','0','0');
