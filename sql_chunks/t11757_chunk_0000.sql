b5e3d5afbf85d4ad7b320d83c016f90ca75b84a4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:25 +0700

    Upload SQL chunk: t11757_chunk_0000.sql

---
+++ sql_chunks/t11757_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11757
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11757" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11757
+INSERT INTO [t11757] VALUES (1,'ذكر من له الآيات ومن تكلم بعد الموت للنجاد مخطوط','1','0','0');
