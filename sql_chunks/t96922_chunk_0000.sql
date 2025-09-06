81fbe555c922d1c8a90c9b4552483bb14120923b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:00:16 +0700

    Upload SQL chunk: t96922_chunk_0000.sql

---
+++ sql_chunks/t96922_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t96922
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t96922" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96922
+INSERT INTO [t96922] VALUES (1,'كلمة الناشر','3','0','0');
+INSERT INTO [t96922] VALUES (2,'تقديم','6','0','0');
+INSERT INTO [t96922] VALUES (3,'إنكار المالكية للبدع','16','0','0');
+INSERT INTO [t96922] VALUES (4,'المغاربة والتصوف','52','0','0');
+INSERT INTO [t96922] VALUES (5,'علماء المغرب والقبورية','98','0','0');
+INSERT INTO [t96922] VALUES (6,'المغاربة والاحتفال بمواسم الأضرحة','116','0','0');
+INSERT INTO [t96922] VALUES (7,'علماء المالكية والاحتفال بالمولد النبوي','126','0','0');
+INSERT INTO [t96922] VALUES (8,'المالكية والمذهب الأشعري','136','0','0');
+INSERT INTO [t96922] VALUES (9,'علماء المغرب والدعوة الوهابية','158','0','0');
