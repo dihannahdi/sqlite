02b05b8992e583e306473e24c5776373543b1498
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:46:47 +0700

    Upload SQL chunk: t8524_chunk_0000.sql

---
+++ sql_chunks/t8524_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t8524
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t8524" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8524
+INSERT INTO [t8524] VALUES (1,'القاعدة الأولى:','2','0','0');
+INSERT INTO [t8524] VALUES (2,'القاعدة الثانية:','2','0','0');
+INSERT INTO [t8524] VALUES (3,'والقاعدة الثالثة:','3','0','0');
+INSERT INTO [t8524] VALUES (4,'القاعدة الرابعة:','4','0','0');
