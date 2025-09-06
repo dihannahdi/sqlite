47ccfdb090c01fcc184ca2abcac808c0e5bff7b6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:09 +0700

    Upload SQL chunk: t6245_chunk_0000.sql

---
+++ sql_chunks/t6245_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t6245
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t6245" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6245
+INSERT INTO [t6245] VALUES (1,'خطبة الكتاب','1','0','0');
+INSERT INTO [t6245] VALUES (2,'تنبيه','22','0','0');
+INSERT INTO [t6245] VALUES (3,'تنبيه','23','0','0');
+INSERT INTO [t6245] VALUES (4,'الفصل الأول في التصرف الذي يمكن أن يقع لله تعالى ولغيره','27','0','0');
+INSERT INTO [t6245] VALUES (5,'تنبيه','31','0','0');
+INSERT INTO [t6245] VALUES (6,'الفصل الثاني فيما لا يمكن أن يقع من التصرف إلا لله تعالى','32','0','0');
+INSERT INTO [t6245] VALUES (7,'الفصل الثالث فيما لا يمكن أن يفعل لله تعالى','33','0','0');
+INSERT INTO [t6245] VALUES (8,'تنبيه','44','0','0');
