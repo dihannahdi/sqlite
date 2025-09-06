84512c0df7de95f7232d71dafc89639b31d951fd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:41 +0700

    Upload SQL chunk: t96789_chunk_0000.sql

---
+++ sql_chunks/t96789_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t96789
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t96789" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96789
+INSERT INTO [t96789] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t96789] VALUES (2,'مقابلة في شأن الجهاد الأفغاني','3','0','0');
+INSERT INTO [t96789] VALUES (3,'مقتل الشيخ جميل الرحمن','29','0','0');
+INSERT INTO [t96789] VALUES (4,'بعض المعلومات التي وصلت من بعض إخواننا في الله','62','0','0');
+INSERT INTO [t96789] VALUES (5,'حول كلمة وهابي','71','0','0');
