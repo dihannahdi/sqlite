cb2695b32358b16f2050048838c704d29107468a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:42 +0700

    Upload SQL chunk: t18098_chunk_0000.sql

---
+++ sql_chunks/t18098_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t18098
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t18098" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t18098
+INSERT INTO [t18098] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t18098] VALUES (2,'الإيمان بما وصف الله به نفسه في كتابه','2','0','0');
+INSERT INTO [t18098] VALUES (3,'الإيمان بما وصف الرسول صلى الله عليه وسلم به ربه','11','0','0');
+INSERT INTO [t18098] VALUES (4,'الإيمان بأنه فوق عرشه','17','0','0');
+INSERT INTO [t18098] VALUES (5,'الإيمان بأنه قريب مجيب','17','0','0');
+INSERT INTO [t18098] VALUES (6,'الإيمان بأن القرآن كلام الله منزل غير مخلوق','18','0','0');
+INSERT INTO [t18098] VALUES (7,'الإيمان برؤية المؤمنين لربهم','18','0','0');
+INSERT INTO [t18098] VALUES (8,'الإيمان بما بعد الموت','19','0','0');
+INSERT INTO [t18098] VALUES (9,'الإيمان بالقدر خيره وشره','21','0','0');
+INSERT INTO [t18098] VALUES (10,'الإيمان قول وعمل يزيد وينقص','23','0','0');
+INSERT INTO [t18098] VALUES (11,'وجوب حب الصحابة وأهل البيت','24','0','0');
+INSERT INTO [t18098] VALUES (12,'وجوب التصديق بكرامات الأولياء','27','0','0');
+INSERT INTO [t18098] VALUES (13,'من علامات أهل السنة الإتباع وليس الابتداع','28','0','0');
+INSERT INTO [t18098] VALUES (14,'متفرقات والخاتمة','29','0','0');
+INSERT INTO [t18098] VALUES (15,'خاتمة الطبع','31','0','0');
