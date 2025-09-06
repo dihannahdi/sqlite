e15e1893809d0814661f2a29e6a2da9c206c99d5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:25 +0700

    Upload SQL chunk: t449_chunk_0000.sql

---
+++ sql_chunks/t449_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t449
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t449" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t449
+INSERT INTO [t449] VALUES (1,'كلام الشافعي وأتباعه في المسألة','2','0','0');
+INSERT INTO [t449] VALUES (2,'حجج الشافعية في المسألة','4','0','0');
+INSERT INTO [t449] VALUES (3,'شبه يتمسك بها القائل بجواز توكيل غير المجبر قبل إذن موليته','7','0','0');
+INSERT INTO [t449] VALUES (4,'الشبهة الأولى والجواب عنها','7','3','0');
+INSERT INTO [t449] VALUES (5,'الشبهة الثانية والجواب عنها','10','3','0');
+INSERT INTO [t449] VALUES (6,'الشبهة الثالثة والجواب عنها','11','3','0');
+INSERT INTO [t449] VALUES (7,'الشبهة الرابعة والجواب عنها','14','3','0');
+INSERT INTO [t449] VALUES (8,'الشبهة الخامسة والجواب عنها','17','3','0');
+INSERT INTO [t449] VALUES (9,'الشبهة السادسة والجواب عنها','19','3','0');
+INSERT INTO [t449] VALUES (10,'الشبهة السابعة والجواب عنها','22','3','0');
+INSERT INTO [t449] VALUES (11,'الشبهة الثامنة والجواب عنها','23','3','0');
+INSERT INTO [t449] VALUES (12,'الشبهة التاسعة والجواب عنها','24','3','0');
+INSERT INTO [t449] VALUES (13,'الشبهة العاشرة والجواب عنها','34','3','0');
