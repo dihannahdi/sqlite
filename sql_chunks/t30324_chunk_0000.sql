57f85d7f3d3f86e703790d367f0d85a920031cb4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:26 +0700

    Upload SQL chunk: t30324_chunk_0000.sql

---
+++ sql_chunks/t30324_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30324
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30324" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30324
+INSERT INTO [t30324] VALUES (1,'مجلس من أمالي الحافظ أبي موسى محمد بن أبي بكر المديني','1','0','0');
