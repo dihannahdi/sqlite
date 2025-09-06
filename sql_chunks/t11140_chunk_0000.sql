24a6dd46d21218b9536237476fc91e82b932231e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:39 +0700

    Upload SQL chunk: t11140_chunk_0000.sql

---
+++ sql_chunks/t11140_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t11140
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t11140" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11140
+INSERT INTO [t11140] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11140] VALUES (2,'تمهيد','5','0','0');
+INSERT INTO [t11140] VALUES (3,'المبحث الأول: تعريف العلل لغة واصطلاحا','5','2','0');
+INSERT INTO [t11140] VALUES (4,'المبحث الثاني: أهمية علم العلل وشرفه وعزته، وأسباب ذلك','9','2','0');
+INSERT INTO [t11140] VALUES (5,'الفصل الأول: ذكر أئمة العلل والمصنفات فيه من بداية القرن الثاني إلى نهاية القرن التاسع','18','0','0');
+INSERT INTO [t11140] VALUES (6,'مدخل','18','5','0');
+INSERT INTO [t11140] VALUES (7,'أولا: أئمة علم العلل','19','5','0');
+INSERT INTO [t11140] VALUES (8,'ثانيا: المصنفات في العلل','42','5','0');
+INSERT INTO [t11140] VALUES (9,'الفصل الثاني: أمثلة من دقائق تعليل النقاد للأحاديث','55','0','0');
+INSERT INTO [t11140] VALUES (10,'أمثلة من دقائق تعليل النقاد للأحاديث','55','9','0');
+INSERT INTO [t11140] VALUES (11,'الخاتمة','63','0','0');
+INSERT INTO [t11140] VALUES (12,'التوصيات','67','0','0');
+INSERT INTO [t11140] VALUES (13,'مصادر ومراجع','69','0','0');
