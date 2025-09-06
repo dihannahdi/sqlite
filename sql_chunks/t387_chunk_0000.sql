a9322132cc08553e4d4a120984d0b3b6eaa48100
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:09 +0700

    Upload SQL chunk: t387_chunk_0000.sql

---
+++ sql_chunks/t387_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t387
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t387" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t387
+INSERT INTO [t387] VALUES (1,'تفسير قوله تعالى: {إن الظن لا يغني من الحق شيئا}','5','0','0');
