606973455d2b924e4fde0db037215044a68eb4ef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:01:36 +0700

    Upload SQL chunk: t97802_chunk_0000.sql

---
+++ sql_chunks/t97802_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t97802
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t97802" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t97802
+INSERT INTO [t97802] VALUES (1,'إهداء','2','0','0');
+INSERT INTO [t97802] VALUES (2,'المقدمة','3','0','0');
+INSERT INTO [t97802] VALUES (3,'فلسفتي في الحياة','14','0','0');
+INSERT INTO [t97802] VALUES (4,'ملحمة حر','16','0','0');
+INSERT INTO [t97802] VALUES (5,'قيدي يحدثني','21','0','0');
+INSERT INTO [t97802] VALUES (6,'شعري','24','0','0');
+INSERT INTO [t97802] VALUES (7,'عمري لحظة','27','0','0');
+INSERT INTO [t97802] VALUES (8,'هل ينتهي ليلي؟','29','0','0');
+INSERT INTO [t97802] VALUES (9,'عندما بكى وبكيته الكتاب','32','0','0');
+INSERT INTO [t97802] VALUES (10,'عيد أمة هانت','36','0','0');
+INSERT INTO [t97802] VALUES (11,'مسلمة تتحدث عن نفسها','40','0','0');
+INSERT INTO [t97802] VALUES (12,'رثاء الشيخ أحمد ياسين','43','0','0');
+INSERT INTO [t97802] VALUES (13,'حوار بين ليفني ومبارك بشأن غزة','46','0','0');
+INSERT INTO [t97802] VALUES (14,'غزة .. لا تخر مع الساجدين','48','0','0');
+INSERT INTO [t97802] VALUES (15,'يا قدس صبرا','51','0','0');
+INSERT INTO [t97802] VALUES (16,'أنا شاهد بين الورى: النفط أغلى من دمك','58','0','0');
+INSERT INTO [t97802] VALUES (17,'طريق العلم','69','0','0');
+INSERT INTO [t97802] VALUES (18,'رسالة إلى أسير','72','0','0');
+INSERT INTO [t97802] VALUES (19,'رثاء وأمل في اللقاء','75','0','0');
+INSERT INTO [t97802] VALUES (20,'فلتفخري مصر','79','0','0');
+INSERT INTO [t97802] VALUES (21,'كاميليا والخائنون','82','0','0');
+INSERT INTO [t97802] VALUES (22,'اخسأ ولا تطغى أيا سلمي','85','0','0');
+INSERT INTO [t97802] VALUES (23,'درتي','87','0','0');
+INSERT INTO [t97802] VALUES (24,'لا مثل','90','0','0');
+INSERT INTO [t97802] VALUES (25,'سلام','91','0','0');
+INSERT INTO [t97802] VALUES (26,'هديتي','93','0','0');
+INSERT INTO [t97802] VALUES (27,'شذا قلبي وكلماتي','94','0','0');
+INSERT INTO [t97802] VALUES (28,'عيون','96','0','0');
+INSERT INTO [t97802] VALUES (29,'حلم العاشقين','98','0','0');
+INSERT INTO [t97802] VALUES (30,'لو تصعدين إلى القمر','100','0','0');
+INSERT INTO [t97802] VALUES (31,'السيرة الذاتية','102','0','0');
