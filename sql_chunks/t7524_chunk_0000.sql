dc732fdde15221c9a401761ff27957b96c0fa7b6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:24 +0700

    Upload SQL chunk: t7524_chunk_0000.sql

---
+++ sql_chunks/t7524_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t7524
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t7524" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7524
+INSERT INTO [t7524] VALUES (1,'الجواب عن الأسئلة','1','0','0');
+INSERT INTO [t7524] VALUES (2,'مدخل','1','1','0');
+INSERT INTO [t7524] VALUES (3,'الجواب عن الفقرة الثانية','11','1','0');
+INSERT INTO [t7524] VALUES (4,'الجواب عن الفقرة الثالثة','12','1','0');
+INSERT INTO [t7524] VALUES (5,'الجواب عن الفقرة الرابعة','21','1','0');
+INSERT INTO [t7524] VALUES (6,'أحكام الجمعة','56','0','0');
+INSERT INTO [t7524] VALUES (7,'مدخل','56','6','0');
+INSERT INTO [t7524] VALUES (8,'حكم صلاة الجمعة','58','6','0');
+INSERT INTO [t7524] VALUES (9,'بدع الجمعة','92','6','0');
