39ce858a3a6aba4478c9d1ae0b70e6802127c4f2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:24 +0700

    Upload SQL chunk: t28114_chunk_0000.sql

---
+++ sql_chunks/t28114_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t28114
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t28114" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t28114
+INSERT INTO [t28114] VALUES (1,'الله يدعونا إليه','1','0','0');
+INSERT INTO [t28114] VALUES (2,'والرسول يرشدنا إلى الطريق','5','0','0');
+INSERT INTO [t28114] VALUES (3,'التوبة وحقيقتها','14','0','0');
+INSERT INTO [t28114] VALUES (4,'الخوف والرجاء','21','0','0');
+INSERT INTO [t28114] VALUES (5,'ما يكفر الذنوب ما تقدم منها وما تاخر','23','0','0');
+INSERT INTO [t28114] VALUES (6,'ما يكفر ما تقدم من الذنوب','26','0','0');
+INSERT INTO [t28114] VALUES (7,'ما يخرج الإنسان من الذنوب كيوم ولدته أمه','29','0','0');
+INSERT INTO [t28114] VALUES (8,'ما يكفر الذنوب وإن كانت مثل زبد البحر','32','0','0');
+INSERT INTO [t28114] VALUES (9,'ما يغفر الذنوب وإن كان قد فر من الزحف','39','0','0');
+INSERT INTO [t28114] VALUES (10,'ما يغفر الذنوب بوجه عام','41','0','0');
+INSERT INTO [t28114] VALUES (11,'ما يوجب الجنة','55','0','0');
+INSERT INTO [t28114] VALUES (12,'سعة رحمة الله تعالى','63','0','0');
+INSERT INTO [t28114] VALUES (13,'احذر نفسك أن تحبط عملك','79','0','0');
+INSERT INTO [t28114] VALUES (14,'الاستكثار من طلب الثواب','85','0','0');
