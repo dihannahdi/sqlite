01c9a673d898f789d932dc54ed56c8e587052f15
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:31 +0700

    Upload SQL chunk: t23415_chunk_0000.sql

---
+++ sql_chunks/t23415_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t23415
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t23415" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23415
+INSERT INTO [t23415] VALUES (1,'مقدمة','6','0','0');
+INSERT INTO [t23415] VALUES (2,'أوليات','7','0','0');
+INSERT INTO [t23415] VALUES (3,'النوع والمجتمع','13','0','0');
+INSERT INTO [t23415] VALUES (4,'الآراء المختلفة في تفسير الحركة التاريخية','18','0','0');
+INSERT INTO [t23415] VALUES (5,'التاريخ والعلاقات الاجتماعية','24','0','0');
+INSERT INTO [t23415] VALUES (6,'أصل العلاقات الاجتماعية','26','0','0');
+INSERT INTO [t23415] VALUES (7,'طبيعة العلاقات','29','0','0');
+INSERT INTO [t23415] VALUES (8,'الثروة الاجتماعية','35','0','0');
+INSERT INTO [t23415] VALUES (9,'المرض الاجتماعي','40','0','0');
+INSERT INTO [t23415] VALUES (10,'المجتمع والقيمة الخلقية','46','0','0');
+INSERT INTO [t23415] VALUES (11,'الدين والعلاقات الاجتماعية','52','0','0');
+INSERT INTO [t23415] VALUES (12,'شبكة العلاقات والجفرافيا','57','0','0');
+INSERT INTO [t23415] VALUES (13,'العلاقات الاجتماعية وعلم النفس','63','0','0');
+INSERT INTO [t23415] VALUES (14,'فكرة التربية الاجتماعية','73','0','0');
+INSERT INTO [t23415] VALUES (15,'شبكة العلاقات الاجتماعية والاستعمار','80','0','0');
+INSERT INTO [t23415] VALUES (16,'دفاع عن شبكة العلاقات الاجتماعية','92','0','0');
+INSERT INTO [t23415] VALUES (17,'الشروط الأولية للتربية الاجتماعية','100','0','0');
+INSERT INTO [t23415] VALUES (18,'المسارد','114','0','0');
