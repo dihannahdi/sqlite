71c9a410108bf487e3597f6be60d4f762bf10403
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:36:59 +0700

    Upload SQL chunk: t20771_chunk_0000.sql

---
+++ sql_chunks/t20771_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t20771
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t20771" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t20771
+INSERT INTO [t20771] VALUES (1,'تسألون فيه عن حكم الشرع في اختلاط الجنسين في الدراسة الجامعية','2','0','0');
+INSERT INTO [t20771] VALUES (2,'والجواب عما سألتم عنه وفقنا الله وإياكم','2','0','0');
+INSERT INTO [t20771] VALUES (3,'أما القرآن العظيم','2','2','0');
+INSERT INTO [t20771] VALUES (4,'وأما أدلة السنة','10','2','0');
+INSERT INTO [t20771] VALUES (5,'فاعلموا أن سد الذريعة الموصلة إلى فاحشة الزنا واجب بإجماع المسلمين','14','2','0');
