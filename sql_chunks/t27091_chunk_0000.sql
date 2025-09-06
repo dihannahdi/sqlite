b5709290e3ad9005b1c9d809cd811ea10a81f4f7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:39 +0700

    Upload SQL chunk: t27091_chunk_0000.sql

---
+++ sql_chunks/t27091_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t27091
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t27091" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t27091
+INSERT INTO [t27091] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t27091] VALUES (2,'أحاديث ذيلها عبد الوهاب','26','0','0');
