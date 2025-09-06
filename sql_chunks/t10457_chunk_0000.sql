ce1ac751fa43813d2481bf82fb81a928730a2e21
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:03 +0700

    Upload SQL chunk: t10457_chunk_0000.sql

---
+++ sql_chunks/t10457_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t10457
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t10457" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10457
+INSERT INTO [t10457] VALUES (1,'حرف الألف','7','0','0');
+INSERT INTO [t10457] VALUES (2,'حرف الباء','11','0','0');
+INSERT INTO [t10457] VALUES (3,'حرف التاء','11','0','0');
+INSERT INTO [t10457] VALUES (4,'حرف الجيم','14','0','0');
+INSERT INTO [t10457] VALUES (5,'حرف الحاء','16','0','0');
+INSERT INTO [t10457] VALUES (6,'حرف الخاء','17','0','0');
+INSERT INTO [t10457] VALUES (7,'حرف الدال','17','0','0');
+INSERT INTO [t10457] VALUES (8,'حرف الذال','18','0','0');
+INSERT INTO [t10457] VALUES (9,'حرف الراء','18','0','0');
+INSERT INTO [t10457] VALUES (10,'حرف السين','21','0','0');
+INSERT INTO [t10457] VALUES (11,'حرف الشين','22','0','0');
+INSERT INTO [t10457] VALUES (12,'حرف الصاد','23','0','0');
+INSERT INTO [t10457] VALUES (13,'حرف الضاد','24','0','0');
+INSERT INTO [t10457] VALUES (14,'حرف العين','24','0','0');
+INSERT INTO [t10457] VALUES (15,'حرف الفاء','24','0','0');
+INSERT INTO [t10457] VALUES (16,'حرف القاف','30','0','0');
+INSERT INTO [t10457] VALUES (17,'حرف الكاف','35','0','0');
+INSERT INTO [t10457] VALUES (18,'حرف اللام','35','0','0');
+INSERT INTO [t10457] VALUES (19,'حرف الميم','36','0','0');
+INSERT INTO [t10457] VALUES (20,'حرف النون','42','0','0');
+INSERT INTO [t10457] VALUES (21,'حرف الهاء','43','0','0');
+INSERT INTO [t10457] VALUES (22,'حرف الواو','44','0','0');
