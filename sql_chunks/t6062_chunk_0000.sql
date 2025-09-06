be413b53712f681a27fd34190fe45b0b1c908d02
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:51 +0700

    Upload SQL chunk: t6062_chunk_0000.sql

---
+++ sql_chunks/t6062_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t6062
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t6062" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6062
+INSERT INTO [t6062] VALUES (1,'كتاب التوحيد','3','0','0');
+INSERT INTO [t6062] VALUES (2,'كتاب الإيمان','29','0','0');
+INSERT INTO [t6062] VALUES (3,'كتاب المبتدأ','41','0','0');
+INSERT INTO [t6062] VALUES (4,'كتاب الأنبياء والقدماء','141','0','0');
+INSERT INTO [t6062] VALUES (5,'كتاب العلم','167','0','0');
+INSERT INTO [t6062] VALUES (6,'كتاب السنة','219','0','0');
+INSERT INTO [t6062] VALUES (7,'كتاب المناقب','235','0','0');
+INSERT INTO [t6062] VALUES (8,'كتاب الطهارة','435','0','0');
+INSERT INTO [t6062] VALUES (9,'كتاب الصلاة','442','0','0');
+INSERT INTO [t6062] VALUES (10,'كتاب الصيام','514','0','0');
+INSERT INTO [t6062] VALUES (11,'كتاب الحج','531','0','0');
+INSERT INTO [t6062] VALUES (12,'كتاب الجهاد','543','0','0');
+INSERT INTO [t6062] VALUES (13,'كتاب المعاملات','551','0','0');
+INSERT INTO [t6062] VALUES (14,'كتاب النكاح','566','0','0');
+INSERT INTO [t6062] VALUES (15,'كتاب الأحكام والحدود','586','0','0');
+INSERT INTO [t6062] VALUES (16,'كتاب الأطعمة','608','0','0');
+INSERT INTO [t6062] VALUES (17,'كتاب اللباس','652','0','0');
+INSERT INTO [t6062] VALUES (18,'كتاب الأدب والزهد','668','0','0');
+INSERT INTO [t6062] VALUES (19,'كتاب الذكر والدعاء','719','0','0');
+INSERT INTO [t6062] VALUES (20,'كتاب المواعظ والوصايا','732','0','0');
+INSERT INTO [t6062] VALUES (21,'كتاب الفتن','752','0','0');
+INSERT INTO [t6062] VALUES (22,'كتاب المرض والطب','761','0','0');
+INSERT INTO [t6062] VALUES (23,'كتاب الموت والقبور','776','0','0');
+INSERT INTO [t6062] VALUES (24,'كتاب المواريث','799','0','0');
+INSERT INTO [t6062] VALUES (25,'كتاب البعث','800','0','0');
