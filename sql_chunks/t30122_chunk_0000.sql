5d2b941d1c4e9cead33bf41da6a25739ba6c2220
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:49 +0700

    Upload SQL chunk: t30122_chunk_0000.sql

---
+++ sql_chunks/t30122_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t30122
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t30122" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30122
+INSERT INTO [t30122] VALUES (1,'الجزء الرابع والعشرون من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30122] VALUES (2,'من حديث أبي محمد الخلال','2','0','0');
+INSERT INTO [t30122] VALUES (3,'من حديثه أيضا','8','0','0');
+INSERT INTO [t30122] VALUES (4,'من فوائد أبي عبد الله الصوري الحافظ','14','0','0');
+INSERT INTO [t30122] VALUES (5,'من حديث ابن شاهين عن شيوخه','17','0','0');
+INSERT INTO [t30122] VALUES (6,'من فوائد أبي الحسين بن بشران','50','0','0');
+INSERT INTO [t30122] VALUES (7,'من فوائد أبي الرجاء الحداد الأصبهاني','51','0','0');
+INSERT INTO [t30122] VALUES (8,'من فوائد أبي غالب عن شيوخه بإجازته عنهم','56','0','0');
+INSERT INTO [t30122] VALUES (9,'أبيات من رواية هناد النسفي','57','0','0');
+INSERT INTO [t30122] VALUES (10,'من فوائد أبي بكر الخطيب','58','0','0');
+INSERT INTO [t30122] VALUES (11,'حكاية واحدة','59','0','0');
+INSERT INTO [t30122] VALUES (12,'فائدة أخرى','60','0','0');
+INSERT INTO [t30122] VALUES (13,'من حديث أبي بكر الشافعي عن معاذ بن المثنى عن عبد الله بن محمد بن أسماء','61','0','0');
+INSERT INTO [t30122] VALUES (14,'فائدة واحدة','64','0','0');
+INSERT INTO [t30122] VALUES (15,'حديث واحد','65','0','0');
+INSERT INTO [t30122] VALUES (16,'من فوائد أبي محمد الخلال','67','0','0');
