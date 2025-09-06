611bf3fec23d4e918b6427a72e30e2be6e9c9e2d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:43 +0700

    Upload SQL chunk: t23075_chunk_0000.sql

---
+++ sql_chunks/t23075_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t23075
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t23075" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23075
+INSERT INTO [t23075] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t23075] VALUES (2,'مدخل','1','1','0');
+INSERT INTO [t23075] VALUES (3,'صيغة فعلان ومعانيها','3','0','0');
+INSERT INTO [t23075] VALUES (4,'فعلان المصدر','3','3','0');
+INSERT INTO [t23075] VALUES (5,'فعلان العلم','4','3','0');
+INSERT INTO [t23075] VALUES (6,'فعلان الصفة','5','3','0');
+INSERT INTO [t23075] VALUES (7,'جمع فعلان','8','0','0');
+INSERT INTO [t23075] VALUES (8,'جمع فعلان','8','7','0');
+INSERT INTO [t23075] VALUES (9,'فعال','10','7','0');
+INSERT INTO [t23075] VALUES (10,'فعلى وجمع فعلان على فعلى','11','7','0');
+INSERT INTO [t23075] VALUES (11,'(فعالى) وجمع (فعلان) على فعالى','12','7','0');
+INSERT INTO [t23075] VALUES (12,'جمع فعلان اسم الجنس والعلم','14','7','0');
+INSERT INTO [t23075] VALUES (13,'(فعلان) الممنوع من الصرف','16','0','0');
+INSERT INTO [t23075] VALUES (14,'صيغة فعلان اذا كانت صفة','16','13','0');
+INSERT INTO [t23075] VALUES (15,'صيغة فعلان اذا كانت علم','17','13','0');
+INSERT INTO [t23075] VALUES (16,'هل المؤثر انتفاء التاء أم وجود فعلى؟','19','0','0');
+INSERT INTO [t23075] VALUES (17,'هل المؤثر انتفاء التاء أم وجود فعلى؟','19','16','0');
+INSERT INTO [t23075] VALUES (18,'فعلان في باب التصغير','22','0','0');
+INSERT INTO [t23075] VALUES (19,'فعلان، وفعلان الجمع','27','0','0');
+INSERT INTO [t23075] VALUES (20,'فعلان: بفتح الفاء والعين','32','0','0');
