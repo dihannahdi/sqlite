c6d8cff37b812be48fd2ea30573c86a5709b23fa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:39 +0700

    Upload SQL chunk: t23069_chunk_0000.sql

---
+++ sql_chunks/t23069_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t23069
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t23069" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23069
+INSERT INTO [t23069] VALUES (1,'تعريفه','1','0','0');
+INSERT INTO [t23069] VALUES (2,'عند اليونان','3','0','0');
+INSERT INTO [t23069] VALUES (3,'عند الهنود','4','0','0');
+INSERT INTO [t23069] VALUES (4,'لدى العرب','5','0','0');
+INSERT INTO [t23069] VALUES (5,'من أين أتت الخدعة','8','0','0');
+INSERT INTO [t23069] VALUES (6,'رأينا','10','0','0');
+INSERT INTO [t23069] VALUES (7,'يقول أبان في مزدوجة الفرائض شارحا أحكام الصوم والزكاة','15','0','0');
+INSERT INTO [t23069] VALUES (8,'هذا نموذج من نظمه لكليلة ودمنة','17','0','0');
+INSERT INTO [t23069] VALUES (9,'ففي ميدان الصراع العقدي والمذهبي','19','0','0');
+INSERT INTO [t23069] VALUES (10,'في ميدان الموعظة والحكمة','23','0','0');
+INSERT INTO [t23069] VALUES (11,'في ميدان النحو والفلك','26','0','0');
+INSERT INTO [t23069] VALUES (12,'في ميدان اللهو والمجون','28','0','0');
+INSERT INTO [t23069] VALUES (13,'في ميدان السير والتاريخ','31','0','0');
+INSERT INTO [t23069] VALUES (14,'القيمة الفنية للشعر التعليمي','36','0','0');
