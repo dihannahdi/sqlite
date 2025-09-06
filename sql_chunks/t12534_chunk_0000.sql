4ba39c212209be0d78bd0aa75c90aa872b8b6e89
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:36 +0700

    Upload SQL chunk: t12534_chunk_0000.sql

---
+++ sql_chunks/t12534_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t12534
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t12534" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12534
+INSERT INTO [t12534] VALUES (1,'من كتاب الأشربة','4','0','0');
+INSERT INTO [t12534] VALUES (2,'من كتاب المناسك','23','0','0');
+INSERT INTO [t12534] VALUES (3,'من كتاب الزكاة','43','0','0');
+INSERT INTO [t12534] VALUES (4,'من كتاب الصلاة','48','0','0');
+INSERT INTO [t12534] VALUES (5,'من كتاب النكاح','55','0','0');
+INSERT INTO [t12534] VALUES (6,'من كتاب الصوم','66','0','0');
+INSERT INTO [t12534] VALUES (7,'كتاب القسامة والعقول والديات','113','0','0');
