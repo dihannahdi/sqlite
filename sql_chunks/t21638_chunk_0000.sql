a58d1670ed0187840c6919ddb1aedb41ae523f9f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:47 +0700

    Upload SQL chunk: t21638_chunk_0000.sql

---
+++ sql_chunks/t21638_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21638
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21638" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21638
+INSERT INTO [t21638] VALUES (1,'حديث_شعبة','1','0','0');
