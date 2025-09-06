dd8731dcd853af0412b4230ce69a4b9f0235fa65
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:08 +0700

    Upload SQL chunk: t21673_chunk_0000.sql

---
+++ sql_chunks/t21673_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21673
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21673" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21673
+INSERT INTO [t21673] VALUES (1,'من حديث أبي محمد ابن الأكفاني - مخطوط','2','0','0');
