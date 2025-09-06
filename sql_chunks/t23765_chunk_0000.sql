0197986bea9a1bbb630ced1bd3baff38e5008608
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:48:49 +0700

    Upload SQL chunk: t23765_chunk_0000.sql

---
+++ sql_chunks/t23765_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t23765
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t23765" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23765
+INSERT INTO [t23765] VALUES (1,'[الحلقة الأولى]','2','0','0');
+INSERT INTO [t23765] VALUES (2,'[الحلقة الثانية]','42','0','0');
+INSERT INTO [t23765] VALUES (3,'[الحلقة الثالثة]','79','0','0');
+INSERT INTO [t23765] VALUES (4,'[الحلقة الرابعة]','109','0','0');
+INSERT INTO [t23765] VALUES (5,'[الحلقة الخامسة والأخيرة]','143','0','0');
