05b92578e8c14f726b7f4e8b852d829bfe67c12a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:30 +0700

    Upload SQL chunk: t13784_chunk_0000.sql

---
+++ sql_chunks/t13784_chunk_0000.sql
@@ -0,0 +1,69 @@
+-- Table: t13784
+-- Chunk: 1
+-- Rows: 62
+
+CREATE TABLE "t13784" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13784
+INSERT INTO [t13784] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t13784] VALUES (2,'كتاب الطهارة','14','0','0');
+INSERT INTO [t13784] VALUES (3,'فصل في الوضوء والغسل','29','2','0');
+INSERT INTO [t13784] VALUES (4,'فصل في مسح الخف','46','2','0');
+INSERT INTO [t13784] VALUES (5,'فصل في التيمم','51','2','0');
+INSERT INTO [t13784] VALUES (6,'فصل في إزالة النجاسة','57','2','0');
+INSERT INTO [t13784] VALUES (7,'فصل في البئر','62','2','0');
+INSERT INTO [t13784] VALUES (8,'فصل في الاستنجاء','65','2','0');
+INSERT INTO [t13784] VALUES (9,'كتاب الصلاة','68','0','0');
+INSERT INTO [t13784] VALUES (10,'فصل','70','9','0');
+INSERT INTO [t13784] VALUES (11,'فصل','76','9','0');
+INSERT INTO [t13784] VALUES (12,'فصل في السنن الرواتب وغيرها','121','9','0');
+INSERT INTO [t13784] VALUES (13,'فصل في التراويح','126','9','0');
+INSERT INTO [t13784] VALUES (14,'فصل في الوتر','129','9','0');
+INSERT INTO [t13784] VALUES (15,'فصل','133','9','0');
+INSERT INTO [t13784] VALUES (16,'فصل في الجماعة','141','9','0');
+INSERT INTO [t13784] VALUES (17,'فصل في الجمعة','148','9','0');
+INSERT INTO [t13784] VALUES (18,'فصل في العيدين','154','9','0');
+INSERT INTO [t13784] VALUES (19,'فصل في المسافر','159','9','0');
+INSERT INTO [t13784] VALUES (20,'فصل في المريض','166','9','0');
+INSERT INTO [t13784] VALUES (21,'فصل في الفائتة أي في بيان الصلوات الفائتة','170','9','0');
+INSERT INTO [t13784] VALUES (22,'فصل','172','9','0');
+INSERT INTO [t13784] VALUES (23,'فصل في السهو','176','9','0');
+INSERT INTO [t13784] VALUES (24,'فصل في سجود التلاوة','180','9','0');
+INSERT INTO [t13784] VALUES (25,'فصل في الميت','185','9','0');
+INSERT INTO [t13784] VALUES (26,'كتاب الزكاة','194','0','0');
+INSERT INTO [t13784] VALUES (27,'كتاب الصوم','229','0','0');
+INSERT INTO [t13784] VALUES (28,'فصل','236','27','0');
+INSERT INTO [t13784] VALUES (29,'فصل','246','27','0');
+INSERT INTO [t13784] VALUES (30,'كتاب الحج','259','0','0');
+INSERT INTO [t13784] VALUES (31,'فصل','269','30','0');
+INSERT INTO [t13784] VALUES (32,'فصل','291','30','0');
+INSERT INTO [t13784] VALUES (33,'فصل','294','30','0');
+INSERT INTO [t13784] VALUES (34,'فصل','300','30','0');
+INSERT INTO [t13784] VALUES (35,'فصل','305','30','0');
+INSERT INTO [t13784] VALUES (36,'كتاب الجهاد','312','0','0');
+INSERT INTO [t13784] VALUES (37,'فصل','317','36','0');
+INSERT INTO [t13784] VALUES (38,'فصل','328','36','0');
+INSERT INTO [t13784] VALUES (39,'فصل','332','36','0');
+INSERT INTO [t13784] VALUES (40,'فصل','336','36','0');
+INSERT INTO [t13784] VALUES (41,'كتاب الصيد والذبائح','341','0','0');
+INSERT INTO [t13784] VALUES (42,'فصل','349','41','0');
+INSERT INTO [t13784] VALUES (43,'فصل','359','41','0');
+INSERT INTO [t13784] VALUES (44,'فصل','364','41','0');
+INSERT INTO [t13784] VALUES (45,'كتاب الكراهية','373','0','0');
+INSERT INTO [t13784] VALUES (46,'فصل','380','45','0');
+INSERT INTO [t13784] VALUES (47,'فصل','390','45','0');
+INSERT INTO [t13784] VALUES (48,'فروع','395','45','0');
+INSERT INTO [t13784] VALUES (49,'كتاب الفرائض','404','0','0');
+INSERT INTO [t13784] VALUES (50,'فصل','410','49','0');
+INSERT INTO [t13784] VALUES (51,'فصل','414','49','0');
+INSERT INTO [t13784] VALUES (52,'فصل','421','49','0');
+INSERT INTO [t13784] VALUES (53,'فصل','429','49','0');
+INSERT INTO [t13784] VALUES (54,'فصل','430','49','0');
+INSERT INTO [t13784] VALUES (55,'فصل','431','49','0');
+INSERT INTO [t13784] VALUES (56,'فصل','432','49','0');
+INSERT INTO [t13784] VALUES (57,'فصل','434','49','0');
+INSERT INTO [t13784] VALUES (58,'كتاب الكسب والأدب','437','0','0');
+INSERT INTO [t13784] VALUES (59,'فصل','442','58','0');
+INSERT INTO [t13784] VALUES (60,'فصل','451','58','0');
+INSERT INTO [t13784] VALUES (61,'فصل','454','58','0');
+INSERT INTO [t13784] VALUES (62,'فصل','458','58','0');
