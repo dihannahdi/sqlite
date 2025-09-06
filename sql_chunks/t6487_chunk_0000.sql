1059d89a1590cc2329960c26a7c6fedae5bbf5db
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:02 +0700

    Upload SQL chunk: t6487_chunk_0000.sql

---
+++ sql_chunks/t6487_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6487
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6487" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6487
+INSERT INTO [t6487] VALUES (1,'الأولى مسألة العلو وهو ان الله عز وجل كان ولا مكان ولا عرش ولا ماء ولا فضاء ولا هواء ولا خلاء ولا ملاء وأنه كان منفردا في قدمه وأزليته متوحدا في فردانيته سبحانه وتعالى في تلك الفردانية لا يوصف بأنه فوق كذا إذ لا شيء غيره','12','0','0');
+INSERT INTO [t6487] VALUES (2,'تمت الرسالة','24','0','0');
