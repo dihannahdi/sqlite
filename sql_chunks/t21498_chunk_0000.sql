a23e664bc18bf4218fd911241493102e2cc4cb15
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:43 +0700

    Upload SQL chunk: t21498_chunk_0000.sql

---
+++ sql_chunks/t21498_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t21498
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t21498" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21498
+INSERT INTO [t21498] VALUES (1,'نص الكتاب','1','0','0');
+INSERT INTO [t21498] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
+INSERT INTO [t21498] VALUES (3,'بسم الله الرحمن الرحيم','4','1','0');
+INSERT INTO [t21498] VALUES (4,'مقدمة المؤلف','4','0','0');
+INSERT INTO [t21498] VALUES (5,'أخبار المتنبي','4','0','0');
+INSERT INTO [t21498] VALUES (6,'عداوة الشعراء بئس المقتنى','546','5','0');
+INSERT INTO [t21498] VALUES (7,'خاتمة','934','0','0');
