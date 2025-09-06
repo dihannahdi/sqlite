e86147264a182e94b137244eec80708f8511db65
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:45 +0700

    Upload SQL chunk: t21500_chunk_0000.sql

---
+++ sql_chunks/t21500_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t21500
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t21500" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21500
+INSERT INTO [t21500] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t21500] VALUES (2,'الحمد لله والسلام على رسول الله','75','0','0');
