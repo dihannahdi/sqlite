a8f9c2703ad803c6ed38b9d0b9af8192467f8f57
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:52:43 +0700

    Upload SQL chunk: t9314_chunk_0000.sql

---
+++ sql_chunks/t9314_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t9314
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t9314" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9314
+INSERT INTO [t9314] VALUES (1,'الحسين بن علي الجعفي','1','0','0');
+INSERT INTO [t9314] VALUES (2,'يحيى بن آدم','19','0','0');
+INSERT INTO [t9314] VALUES (3,'أبو أسامة حماد بن أسامة','25','0','0');
+INSERT INTO [t9314] VALUES (4,'أبو يحيى عبد الحميد بن عبد الرحمن الحماني','33','0','0');
+INSERT INTO [t9314] VALUES (5,'زيد بن الحباب','39','0','0');
+INSERT INTO [t9314] VALUES (6,'محمد بن بشر','43','0','0');
+INSERT INTO [t9314] VALUES (7,'روح بن عبادة','47','0','0');
+INSERT INTO [t9314] VALUES (8,'شبابة بن سوار','51','0','0');
+INSERT INTO [t9314] VALUES (9,'أبو سفيان صالح بن مهران','55','0','0');
+INSERT INTO [t9314] VALUES (10,'أبو سفيان صالح بن مهران ومحمد بن المغيرة','58','0','0');
+INSERT INTO [t9314] VALUES (11,'أبو عبد الرحمن عبد الله بن يزيد المقرئ','65','0','0');
+INSERT INTO [t9314] VALUES (12,'سفيان بن عيينة','67','0','0');
