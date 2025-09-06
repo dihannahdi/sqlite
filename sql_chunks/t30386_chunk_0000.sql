579b751aff1fa5d6927caf9ba944c2eedbf27931
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:37 +0700

    Upload SQL chunk: t30386_chunk_0000.sql

---
+++ sql_chunks/t30386_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t30386
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t30386" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30386
+INSERT INTO [t30386] VALUES (1,'كتاب في علم الحديث','1','0','0');
+INSERT INTO [t30386] VALUES (2,'باب ذكر بيان المسانيد من الآثار وتقسيمها','2','0','0');
+INSERT INTO [t30386] VALUES (3,'فصل','5','0','0');
+INSERT INTO [t30386] VALUES (4,'فصل','9','0','0');
+INSERT INTO [t30386] VALUES (5,'فصل','10','0','0');
+INSERT INTO [t30386] VALUES (6,'فصل','12','0','0');
+INSERT INTO [t30386] VALUES (7,'فصل','16','0','0');
+INSERT INTO [t30386] VALUES (8,'فصل','17','0','0');
+INSERT INTO [t30386] VALUES (9,'فصل','20','0','0');
+INSERT INTO [t30386] VALUES (10,'باب ذكر بيان المرسل من الآثار وتفصيله','22','0','0');
+INSERT INTO [t30386] VALUES (11,'فصل','25','0','0');
+INSERT INTO [t30386] VALUES (12,'فصل','27','0','0');
+INSERT INTO [t30386] VALUES (13,'باب ذكر بيان الموقوف في الآثار وأنواعه','28','0','0');
+INSERT INTO [t30386] VALUES (14,'فصل','30','0','0');
+INSERT INTO [t30386] VALUES (15,'فصل','32','0','0');
+INSERT INTO [t30386] VALUES (16,'فصل','35','0','0');
+INSERT INTO [t30386] VALUES (17,'فصل','38','0','0');
+INSERT INTO [t30386] VALUES (18,'باب ذكر بيان المقطوع من الآثار وتمثيله','42','0','0');
+INSERT INTO [t30386] VALUES (19,'فصل','45','0','0');
+INSERT INTO [t30386] VALUES (20,'فصل','47','0','0');
+INSERT INTO [t30386] VALUES (21,'باب ذكر أحوال المدلسين من أصحاب الحديث وتقسيم طباقتهم وشرح مذاهبهم','49','0','0');
