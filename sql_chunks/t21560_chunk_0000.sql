3f6260cdd4e03b2df9c49c0935e7edfca0aac5ee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:41 +0700

    Upload SQL chunk: t21560_chunk_0000.sql

---
+++ sql_chunks/t21560_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t21560
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t21560" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21560
+INSERT INTO [t21560] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t21560] VALUES (2,'مقدمة في الكلام على حال الحديث','30','0','0');
+INSERT INTO [t21560] VALUES (3,'الفصل الأول','31','0','0');
+INSERT INTO [t21560] VALUES (4,'الفصل الثاني','37','0','0');
+INSERT INTO [t21560] VALUES (5,'نتيجة فيما يستفاد منه للعمل','37','0','0');
+INSERT INTO [t21560] VALUES (6,'وهو المقصود بالسؤال','37','0','0');
+INSERT INTO [t21560] VALUES (7,'الفصل الثالث','39','0','0');
+INSERT INTO [t21560] VALUES (8,'خاتمة في التنبيه على الراجح منذلك','39','0','0');
