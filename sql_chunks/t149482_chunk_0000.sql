1346a6baa88f2f027e1b9414d1201889675c2600
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:49 +0700

    Upload SQL chunk: t149482_chunk_0000.sql

---
+++ sql_chunks/t149482_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t149482
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t149482" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t149482
+INSERT INTO [t149482] VALUES (1,'قافية الهمزة','2','0','0');
+INSERT INTO [t149482] VALUES (2,'قافية الباء','5','0','0');
+INSERT INTO [t149482] VALUES (3,'قافية التاء','10','0','0');
+INSERT INTO [t149482] VALUES (4,'قافية الحاء','11','0','0');
+INSERT INTO [t149482] VALUES (5,'قافية الدال','13','0','0');
+INSERT INTO [t149482] VALUES (6,'قافية الراء','17','0','0');
+INSERT INTO [t149482] VALUES (7,'قافية الزاي','33','0','0');
+INSERT INTO [t149482] VALUES (8,'قافية السين','36','0','0');
+INSERT INTO [t149482] VALUES (9,'قافية الضاد','38','0','0');
+INSERT INTO [t149482] VALUES (10,'قافية العين','39','0','0');
+INSERT INTO [t149482] VALUES (11,'قافية الفاء','43','0','0');
+INSERT INTO [t149482] VALUES (12,'قافية القاف','45','0','0');
+INSERT INTO [t149482] VALUES (13,'قافية الكاف','48','0','0');
+INSERT INTO [t149482] VALUES (14,'قافية اللام','52','0','0');
+INSERT INTO [t149482] VALUES (15,'قافية الميم','67','0','0');
+INSERT INTO [t149482] VALUES (16,'قافية النون','75','0','0');
+INSERT INTO [t149482] VALUES (17,'الشعر المنسوب لمعاوية مما أنشده أو تمثل به أو لشبهة عرضت','78','0','0');
+INSERT INTO [t149482] VALUES (18,'تخريج أشعار معاوية بن أبي سفيان','90','0','0');
