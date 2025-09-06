92315c9ff6dfaec13de27d7a78bb53faedc4cd6a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:40 +0700

    Upload SQL chunk: t8228_chunk_0000.sql

---
+++ sql_chunks/t8228_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t8228
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t8228" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8228
+INSERT INTO [t8228] VALUES (1,'عقوبة قاطع الرحم والبغي','1','0','0');
+INSERT INTO [t8228] VALUES (2,'ما هو داء الأمم؟','3','0','0');
+INSERT INTO [t8228] VALUES (3,'إياك والبغي','5','0','0');
+INSERT INTO [t8228] VALUES (4,'التواضع من خلق المسلم','7','0','0');
+INSERT INTO [t8228] VALUES (5,'احذروا البغي','9','0','0');
+INSERT INTO [t8228] VALUES (6,'نهاية الباغي','12','0','0');
+INSERT INTO [t8228] VALUES (7,'مواعظ وحكم','16','0','0');
+INSERT INTO [t8228] VALUES (8,'من قصص أهل البغي','18','0','0');
+INSERT INTO [t8228] VALUES (9,'من الباغي الأول؟','20','0','0');
+INSERT INTO [t8228] VALUES (10,'البغي أساس الذل','29','0','0');
+INSERT INTO [t8228] VALUES (11,'موعظة بليغة','34','0','0');
+INSERT INTO [t8228] VALUES (12,'حديث خرافة','37','0','0');
+INSERT INTO [t8228] VALUES (13,'عقوبة الباغي','43','0','0');
+INSERT INTO [t8228] VALUES (14,'احذروا البغي الخفي','45','0','0');
+INSERT INTO [t8228] VALUES (15,'البلاء موكل بالمنطق','47','0','0');
+INSERT INTO [t8228] VALUES (16,'فضل إن شاء الله','49','0','0');
+INSERT INTO [t8228] VALUES (17,'احذر ثلاث خصال','51','0','0');
+INSERT INTO [t8228] VALUES (18,'وصية أعرابي','53','0','0');
