fc18c9ab62b952c2e9d37b29b558f12691d22609
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:19 +0700

    Upload SQL chunk: t10025_chunk_0000.sql

---
+++ sql_chunks/t10025_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t10025
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t10025" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10025
+INSERT INTO [t10025] VALUES (1,'١','2','0','0');
+INSERT INTO [t10025] VALUES (2,'٢','3','0','0');
+INSERT INTO [t10025] VALUES (3,'٣','4','0','0');
+INSERT INTO [t10025] VALUES (4,'٤','4','0','0');
+INSERT INTO [t10025] VALUES (5,'٥','5','0','0');
+INSERT INTO [t10025] VALUES (6,'٦','5','0','0');
+INSERT INTO [t10025] VALUES (7,'٧','5','0','0');
+INSERT INTO [t10025] VALUES (8,'٨','6','0','0');
+INSERT INTO [t10025] VALUES (9,'٩','6','0','0');
+INSERT INTO [t10025] VALUES (10,'١٠','6','0','0');
+INSERT INTO [t10025] VALUES (11,'١١','7','0','0');
+INSERT INTO [t10025] VALUES (12,'١٢','7','0','0');
+INSERT INTO [t10025] VALUES (13,'١٣','8','0','0');
+INSERT INTO [t10025] VALUES (14,'١٤','8','0','0');
+INSERT INTO [t10025] VALUES (15,'١٥','8','0','0');
+INSERT INTO [t10025] VALUES (16,'١٦','8','0','0');
+INSERT INTO [t10025] VALUES (17,'١٧','8','0','0');
+INSERT INTO [t10025] VALUES (18,'١٨','9','0','0');
+INSERT INTO [t10025] VALUES (19,'١٩','9','0','0');
+INSERT INTO [t10025] VALUES (20,'٢٠','9','0','0');
+INSERT INTO [t10025] VALUES (21,'٢١','9','0','0');
+INSERT INTO [t10025] VALUES (22,'٢٢','9','0','0');
+INSERT INTO [t10025] VALUES (23,'٢٣','10','0','0');
+INSERT INTO [t10025] VALUES (24,'٢٤','10','0','0');
+INSERT INTO [t10025] VALUES (25,'٢٥','10','0','0');
+INSERT INTO [t10025] VALUES (26,'٢٦','10','0','0');
+INSERT INTO [t10025] VALUES (27,'٢٧','10','0','0');
+INSERT INTO [t10025] VALUES (28,'٢٨','11','0','0');
+INSERT INTO [t10025] VALUES (29,'٢٩','11','0','0');
+INSERT INTO [t10025] VALUES (30,'٣٠','11','0','0');
+INSERT INTO [t10025] VALUES (31,'٣١','11','0','0');
