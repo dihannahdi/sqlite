ff4b79c8d924fa0cd8bdd098e91645be207d3f99
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:14 +0700

    Upload SQL chunk: t7056_chunk_0000.sql

---
+++ sql_chunks/t7056_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7056
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7056" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7056
+INSERT INTO [t7056] VALUES (1,'هذا كتاب ما خالف فيه الإنسان ذوات الأربع من البهائم والسباع والطير','1','0','0');
+INSERT INTO [t7056] VALUES (2,'تم كتاب الفرق بحمد الله وعونه','35','0','0');
