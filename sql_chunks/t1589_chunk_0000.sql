d0e31d2ce05bbee3099c3bbe9bb778e814fa47da
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:33 +0700

    Upload SQL chunk: t1589_chunk_0000.sql

---
+++ sql_chunks/t1589_chunk_0000.sql
@@ -0,0 +1,54 @@
+-- Table: t1589
+-- Chunk: 1
+-- Rows: 47
+
+CREATE TABLE "t1589" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1589
+INSERT INTO [t1589] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t1589] VALUES (2,'اللهم يسر وأعن','1','1','0');
+INSERT INTO [t1589] VALUES (3,'وصول الرسالة','3','0','0');
+INSERT INTO [t1589] VALUES (4,'شجرة طيبة','4','0','0');
+INSERT INTO [t1589] VALUES (5,'خمر الجنة','7','0','0');
+INSERT INTO [t1589] VALUES (6,'ايمان الأعشى','17','0','0');
+INSERT INTO [t1589] VALUES (7,'النابغتان','27','0','0');
+INSERT INTO [t1589] VALUES (8,'لبيد','32','0','0');
+INSERT INTO [t1589] VALUES (9,'ميمية المخبل السعدي','36','0','0');
+INSERT INTO [t1589] VALUES (10,'رباب','38','0','0');
+INSERT INTO [t1589] VALUES (11,'حسان بن ثابت','42','0','0');
+INSERT INTO [t1589] VALUES (12,'عوران قيس','43','0','0');
+INSERT INTO [t1589] VALUES (13,'قصيدة الشماخ','43','0','0');
+INSERT INTO [t1589] VALUES (14,'تميم بن أبي','48','0','0');
+INSERT INTO [t1589] VALUES (15,'مدح رضوان','49','0','0');
+INSERT INTO [t1589] VALUES (16,'مدح زفر','50','0','0');
+INSERT INTO [t1589] VALUES (17,'حمزة بن عبد المطلب','51','0','0');
+INSERT INTO [t1589] VALUES (18,'قاضي حلب','52','0','0');
+INSERT INTO [t1589] VALUES (19,'توبة علي بن منصور','53','0','0');
+INSERT INTO [t1589] VALUES (20,'فاطمة بنت محمد','53','0','0');
+INSERT INTO [t1589] VALUES (21,'حميد بن ثور','56','0','0');
+INSERT INTO [t1589] VALUES (22,'جران العود','62','0','0');
+INSERT INTO [t1589] VALUES (23,'آداب الجن','70','0','0');
+INSERT INTO [t1589] VALUES (24,'الخنساء السلمية','79','0','0');
+INSERT INTO [t1589] VALUES (25,'قول عدي بن زيد','81','0','0');
+INSERT INTO [t1589] VALUES (26,'عنترة العبسي','86','0','0');
+INSERT INTO [t1589] VALUES (27,'عمرو بن كلثوم','90','0','0');
+INSERT INTO [t1589] VALUES (28,'طرفة بن العبد','92','0','0');
+INSERT INTO [t1589] VALUES (29,'الندامى','100','0','0');
+INSERT INTO [t1589] VALUES (30,'آدم كان ينطق العربية في الجنة','104','0','0');
+INSERT INTO [t1589] VALUES (31,'ذات الصفا','105','0','0');
+INSERT INTO [t1589] VALUES (32,'العجاج','112','0','0');
+INSERT INTO [t1589] VALUES (33,'سيبويه','126','0','0');
+INSERT INTO [t1589] VALUES (34,'دين دعبل','129','0','0');
+INSERT INTO [t1589] VALUES (35,'شاتم الدهر','133','0','0');
+INSERT INTO [t1589] VALUES (36,'صالح بن عبد القدوس','137','0','0');
+INSERT INTO [t1589] VALUES (37,'الحلاج','147','0','0');
+INSERT INTO [t1589] VALUES (38,'ابن الرومي','156','0','0');
+INSERT INTO [t1589] VALUES (39,'الإفشين','161','0','0');
+INSERT INTO [t1589] VALUES (40,'بابك','161','0','0');
+INSERT INTO [t1589] VALUES (41,'الكيسانية','162','0','0');
+INSERT INTO [t1589] VALUES (42,'ابن الراوندي','163','0','0');
+INSERT INTO [t1589] VALUES (43,'عمر بن عبد العزيز','170','0','0');
+INSERT INTO [t1589] VALUES (44,'عمر بن الخطاب','170','0','0');
+INSERT INTO [t1589] VALUES (45,'أبي الهذيل العلاف','178','0','0');
+INSERT INTO [t1589] VALUES (46,'الفرزدق','195','0','0');
+INSERT INTO [t1589] VALUES (47,'مذهب الحلول','201','0','0');
