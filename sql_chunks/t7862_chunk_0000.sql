d398cf54c6794fcca8cdf4490e34b58a99023a90
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:29 +0700

    Upload SQL chunk: t7862_chunk_0000.sql

---
+++ sql_chunks/t7862_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t7862
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t7862" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7862
+INSERT INTO [t7862] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7862] VALUES (2,'السبب الباعث على اختيار الموضوع:','3','0','0');
+INSERT INTO [t7862] VALUES (3,'التمهيد','3','0','0');
+INSERT INTO [t7862] VALUES (4,'تعريف البكاء','3','0','0');
+INSERT INTO [t7862] VALUES (5,'الكتب المؤلفة في الموضوع','4','0','0');
+INSERT INTO [t7862] VALUES (6,'الباب الأول: البكاء الممدوح','6','0','0');
+INSERT INTO [t7862] VALUES (7,'١. البكاء عند قراءة القرآن','6','6','0');
+INSERT INTO [t7862] VALUES (8,'٢. البكاء في الصلاة','16','6','0');
+INSERT INTO [t7862] VALUES (9,'٣. البكاء من خشية الله','18','6','0');
+INSERT INTO [t7862] VALUES (10,'٤. البكاء عند العجز عن تحصيل شيء من مراضي الله','33','6','0');
+INSERT INTO [t7862] VALUES (11,'٥. بكاء التوبة:','40','6','0');
+INSERT INTO [t7862] VALUES (12,'٦. بكاء استشعار مقام النبي صلى الله عليه وسلم','55','6','0');
+INSERT INTO [t7862] VALUES (13,'الباب الثاني: البكاء المذموم','60','0','0');
+INSERT INTO [t7862] VALUES (14,'١- بكاء النياحة','60','13','0');
+INSERT INTO [t7862] VALUES (15,'٢- بكاء التصنع والكذب','64','13','0');
+INSERT INTO [t7862] VALUES (16,'الباب الثالث: البكاء المباح','64','0','0');
+INSERT INTO [t7862] VALUES (17,'- بكاء الحزن','65','16','0');
+INSERT INTO [t7862] VALUES (18,'أقوال العلماء في البكاء على الميت','74','16','0');
+INSERT INTO [t7862] VALUES (19,'- بكاء الألم','82','16','0');
+INSERT INTO [t7862] VALUES (20,'- بكاء الشوق','85','16','0');
+INSERT INTO [t7862] VALUES (21,'- بكاء الرحمة','86','16','0');
+INSERT INTO [t7862] VALUES (22,'- بكاء الموافقة','89','16','0');
+INSERT INTO [t7862] VALUES (23,'- بكاء العتب','95','16','0');
+INSERT INTO [t7862] VALUES (24,'- بكاء الفرح','96','16','0');
+INSERT INTO [t7862] VALUES (25,'أنواع أخرى من البكاء','98','0','0');
+INSERT INTO [t7862] VALUES (26,'- بكاء الشياطين','98','25','0');
+INSERT INTO [t7862] VALUES (27,'- بكاء الجماد','98','25','0');
+INSERT INTO [t7862] VALUES (28,'خاتمة','101','0','0');
+INSERT INTO [t7862] VALUES (29,'المصادر والمراجع:','102','0','0');
