75598e5db63121c1777d98a382a764b802443afd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:40 +0700

    Upload SQL chunk: t7616_chunk_0000.sql

---
+++ sql_chunks/t7616_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7616
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7616" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7616
+INSERT INTO [t7616] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7616] VALUES (2,'الدعوة إلى التمسك بالقرآن الكريم وأثره في حياة المسلم','6','0','0');
