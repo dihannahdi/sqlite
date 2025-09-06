a42c55b16062df17b9e7ca03f84711937fe0e482
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:40 +0700

    Upload SQL chunk: t36383_chunk_0000.sql

---
+++ sql_chunks/t36383_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t36383
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t36383" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36383
+INSERT INTO [t36383] VALUES (1,'أيها المذنب! أين أنت من هذا البصر؟ !','1','0','0');
+INSERT INTO [t36383] VALUES (2,'أيها المذنب! أتدري من عصيت؟ !','2','0','0');
+INSERT INTO [t36383] VALUES (3,'أيها المذنب! لا تنس أن الله يراك!','3','0','0');
+INSERT INTO [t36383] VALUES (4,'أيها المذنب! الله تعالى أحق من استحييت منه ..','5','0','0');
+INSERT INTO [t36383] VALUES (5,'أيها المذنب! فهل استحضرت الحياء من الله تعالى؟ !','6','0','0');
+INSERT INTO [t36383] VALUES (6,'أيها المذنب! الحياء .. الحياء!','6','0','0');
+INSERT INTO [t36383] VALUES (7,'أيها المذنب! الله قريب منك!','7','0','0');
+INSERT INTO [t36383] VALUES (8,'أيها المذنب! الأنس بالله تعالى غنيمة أهل الطاعات!','8','0','0');
+INSERT INTO [t36383] VALUES (9,'أيها المذنب!','11','0','0');
