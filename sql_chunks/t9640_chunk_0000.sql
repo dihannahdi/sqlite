f6d7719d5fe5cb9c7ace6f9db9c1672a04919a90
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:57:16 +0700

    Upload SQL chunk: t9640_chunk_0000.sql

---
+++ sql_chunks/t9640_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9640
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9640" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9640
+INSERT INTO [t9640] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t9640] VALUES (2,'كرم المرء تقواه ومروءته خلقه وحسبه دينه','2','0','0');
