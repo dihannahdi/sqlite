1b076c04caefe59100725ba22297bafd28a20af0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:37 +0700

    Upload SQL chunk: t10396_chunk_0000.sql

---
+++ sql_chunks/t10396_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t10396
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t10396" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10396
+INSERT INTO [t10396] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10396] VALUES (2,'أهميةالسؤال التربوي','3','0','0');
+INSERT INTO [t10396] VALUES (3,'أولا: قدم طريقة السؤال','3','2','0');
+INSERT INTO [t10396] VALUES (4,'ثانيا: أهمية السؤال في التربية الحديثة','6','2','0');
+INSERT INTO [t10396] VALUES (5,'ثالثا: أهمية السؤال عند المربيين المسلمين المتقدمين','12','2','0');
+INSERT INTO [t10396] VALUES (6,'رابعا: أهمية السؤال في السنة','16','2','0');
+INSERT INTO [t10396] VALUES (7,'مكانة السؤال في القرآن الكريم','19','0','0');
+INSERT INTO [t10396] VALUES (8,'أولا: الحث على السؤال والترغيب فيه','19','7','0');
+INSERT INTO [t10396] VALUES (9,'ثانيا: السؤال في قصة موسى والخضر عليهما السلام','21','7','0');
+INSERT INTO [t10396] VALUES (10,'آداب السؤال عند المتعلمين','24','0','0');
+INSERT INTO [t10396] VALUES (11,'آداب السؤال عند المعلمين','34','0','0');
+INSERT INTO [t10396] VALUES (12,'خصائص السؤال في القرآن الكريم','41','0','0');
+INSERT INTO [t10396] VALUES (13,'خصائص الإجابات القرآنية','46','0','0');
+INSERT INTO [t10396] VALUES (14,'مصادر ومراجع','56','0','0');
