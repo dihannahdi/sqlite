824d1c4e3906b9aff8b54e599c3ae15a1dfbaff2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:40 +0700

    Upload SQL chunk: t30103_chunk_0000.sql

---
+++ sql_chunks/t30103_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t30103
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t30103" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30103
+INSERT INTO [t30103] VALUES (1,'الجزء الثلاثون من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30103] VALUES (2,'من فوائد أبي عمر بن حيوة','2','0','0');
+INSERT INTO [t30103] VALUES (3,'من فوائد ابن الفضل القطان','4','0','0');
+INSERT INTO [t30103] VALUES (4,'من فوائد البرجلاني','6','0','0');
+INSERT INTO [t30103] VALUES (5,'من فوائد هناد','9','0','0');
+INSERT INTO [t30103] VALUES (6,'من فوائد أبي عمر بن حيويه','29','0','0');
