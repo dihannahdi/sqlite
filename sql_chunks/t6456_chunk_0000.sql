fb9d23ac84015230184e83a4b5d22e3e998ba220
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:49 +0700

    Upload SQL chunk: t6456_chunk_0000.sql

---
+++ sql_chunks/t6456_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t6456
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t6456" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6456
+INSERT INTO [t6456] VALUES (1,'هذا ما سنح للفقير محمد بن إسماعيل الأمير','40','0','0');
