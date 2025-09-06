f49b22ef1dd0300dc2c2fe4c1c75106e6ad367a7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:07 +0700

    Upload SQL chunk: t8793_chunk_0000.sql

---
+++ sql_chunks/t8793_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t8793
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t8793" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8793
+INSERT INTO [t8793] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8793] VALUES (2,'البديع','4','0','0');
+INSERT INTO [t8793] VALUES (3,'المقابلة','6','0','0');
+INSERT INTO [t8793] VALUES (4,'التورية','9','0','0');
+INSERT INTO [t8793] VALUES (5,'الجناس','14','0','0');
+INSERT INTO [t8793] VALUES (6,'الجناس المزدوج','17','0','0');
+INSERT INTO [t8793] VALUES (7,'القلب','20','0','0');
+INSERT INTO [t8793] VALUES (8,'عكس الكلمات','22','0','0');
+INSERT INTO [t8793] VALUES (9,'الرسالة الرقطاء','28','0','0');
+INSERT INTO [t8793] VALUES (10,'الملحق البديع','30','0','0');
