3fa2a137e783a10f095de2e9f4b983a2ffb07bab
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:10 +0700

    Upload SQL chunk: t5650_chunk_0000.sql

---
+++ sql_chunks/t5650_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5650
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5650" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5650
+INSERT INTO [t5650] VALUES (1,'تم الجزء بحمد الله ومنه','44','0','0');
+INSERT INTO [t5650] VALUES (2,'والحمد لله على عونه أولا وآخرا','44','0','0');
