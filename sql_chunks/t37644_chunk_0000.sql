65f250b9273d42b74dace73ed7c85106ded8e852
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:14:51 +0700

    Upload SQL chunk: t37644_chunk_0000.sql

---
+++ sql_chunks/t37644_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t37644
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t37644" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37644
+INSERT INTO [t37644] VALUES (1,'المقدمة: والفرق بين علماء السلف واهل الوقت','2','0','0');
+INSERT INTO [t37644] VALUES (2,'ميزان الإرادة في طلب العلم الشرعي','14','0','0');
+INSERT INTO [t37644] VALUES (3,'بعض الآثار','18','0','0');
+INSERT INTO [t37644] VALUES (4,'بعض ما قيل عن علوم الوقت','24','0','0');
+INSERT INTO [t37644] VALUES (5,'ميزان الاصطلاحات الحادثة في المدح والذم','27','0','0');
+INSERT INTO [t37644] VALUES (6,'هل ما ظهر بعد الصحابة فضيلة','30','0','0');
+INSERT INTO [t37644] VALUES (7,'اعتياد سماع الباطل','32','0','0');
+INSERT INTO [t37644] VALUES (8,'رطانة الاعاجم','33','0','0');
