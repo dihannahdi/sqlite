f3b754dc9bf4fd83b550ce642d0cc4928724aca5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:23 +0700

    Upload SQL chunk: t6979_chunk_0000.sql

---
+++ sql_chunks/t6979_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t6979
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t6979" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6979
+INSERT INTO [t6979] VALUES (1,'حرف الألف','1','0','0');
+INSERT INTO [t6979] VALUES (2,'حرف الباء','1','0','0');
+INSERT INTO [t6979] VALUES (3,'حرف التاء','3','0','0');
+INSERT INTO [t6979] VALUES (4,'حرف الثاء','3','0','0');
+INSERT INTO [t6979] VALUES (5,'حرف الجيم','3','0','0');
+INSERT INTO [t6979] VALUES (6,'حرف الحاء','4','0','0');
+INSERT INTO [t6979] VALUES (7,'حرف الخاء','5','0','0');
+INSERT INTO [t6979] VALUES (8,'حرف الدال','5','0','0');
+INSERT INTO [t6979] VALUES (9,'حرف الذال','6','0','0');
+INSERT INTO [t6979] VALUES (10,'حرف الراء','6','0','0');
+INSERT INTO [t6979] VALUES (11,'حرف الزاي','7','0','0');
+INSERT INTO [t6979] VALUES (12,'حرف السين','7','0','0');
+INSERT INTO [t6979] VALUES (13,'حرف الشين','8','0','0');
+INSERT INTO [t6979] VALUES (14,'حرف الصاد','8','0','0');
+INSERT INTO [t6979] VALUES (15,'حرف الضاد','9','0','0');
+INSERT INTO [t6979] VALUES (16,'حرف الطاء','9','0','0');
+INSERT INTO [t6979] VALUES (17,'حرف الظاء','9','0','0');
+INSERT INTO [t6979] VALUES (18,'حرف العين','10','0','0');
+INSERT INTO [t6979] VALUES (19,'حرف الغين','10','0','0');
+INSERT INTO [t6979] VALUES (20,'حرف الفاء','10','0','0');
+INSERT INTO [t6979] VALUES (21,'حرف القاف','11','0','0');
+INSERT INTO [t6979] VALUES (22,'حرف الكاف','12','0','0');
+INSERT INTO [t6979] VALUES (23,'حرف اللام','13','0','0');
+INSERT INTO [t6979] VALUES (24,'حرف النون','14','0','0');
+INSERT INTO [t6979] VALUES (25,'حرف الهاء','15','0','0');
+INSERT INTO [t6979] VALUES (26,'حرف الواو','17','0','0');
+INSERT INTO [t6979] VALUES (27,'حرف الياء','17','0','0');
+INSERT INTO [t6979] VALUES (28,'فصل','18','0','0');
+INSERT INTO [t6979] VALUES (29,'فصل من المقاييس','26','0','0');
