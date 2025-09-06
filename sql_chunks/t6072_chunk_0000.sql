70e053cdc03fb944c8fa3160481186d4f48c7c62
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:58 +0700

    Upload SQL chunk: t6072_chunk_0000.sql

---
+++ sql_chunks/t6072_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t6072
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t6072" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6072
+INSERT INTO [t6072] VALUES (1,'باب الألف','5','0','0');
+INSERT INTO [t6072] VALUES (2,'باب التاء','535','0','0');
+INSERT INTO [t6072] VALUES (3,'باب الثاء','576','0','0');
+INSERT INTO [t6072] VALUES (4,'- باب الجيم','600','0','0');
+INSERT INTO [t6072] VALUES (5,'- باب الحاء','620','0','0');
+INSERT INTO [t6072] VALUES (6,'باب الخاء','656','0','0');
+INSERT INTO [t6072] VALUES (7,'- باب الدال','702','0','0');
+INSERT INTO [t6072] VALUES (8,'- باب الذال','732','0','0');
+INSERT INTO [t6072] VALUES (9,'باب الراء','744','0','0');
+INSERT INTO [t6072] VALUES (10,'باب','781','0','0');
+INSERT INTO [t6072] VALUES (11,'باب السين','795','0','0');
+INSERT INTO [t6072] VALUES (12,'باب الشين','841','0','0');
+INSERT INTO [t6072] VALUES (13,'- باب الصاد','873','0','0');
+INSERT INTO [t6072] VALUES (14,'- باب الضاد','915','0','0');
+INSERT INTO [t6072] VALUES (15,'- باب الطاء','928','0','0');
+INSERT INTO [t6072] VALUES (16,'- باب الظاء','959','0','0');
+INSERT INTO [t6072] VALUES (17,'- باب الغين','1048','0','0');
+INSERT INTO [t6072] VALUES (18,'- باب الفاء','1071','0','0');
+INSERT INTO [t6072] VALUES (19,'- باب القاف','1113','0','0');
+INSERT INTO [t6072] VALUES (20,'- باب الكاف','1188','0','0');
+INSERT INTO [t6072] VALUES (21,'- باب اللام','1264','0','0');
+INSERT INTO [t6072] VALUES (22,'- باب الميم','1417','0','0');
+INSERT INTO [t6072] VALUES (23,'- باب الهاء','1898','0','0');
+INSERT INTO [t6072] VALUES (24,'باب الواو','1930','0','0');
+INSERT INTO [t6072] VALUES (25,'- باب لام ألف','2017','0','0');
+INSERT INTO [t6072] VALUES (26,'- باب الياء','2236','0','0');
