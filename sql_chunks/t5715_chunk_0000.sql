e9a4bc2f997fdbd466590e10f4ef0301347019ef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:41 +0700

    Upload SQL chunk: t5715_chunk_0000.sql

---
+++ sql_chunks/t5715_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t5715
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t5715" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5715
+INSERT INTO [t5715] VALUES (1,'الحديث الاول','5','0','0');
+INSERT INTO [t5715] VALUES (2,'الحديث الثاني','8','0','0');
+INSERT INTO [t5715] VALUES (3,'الحديث الثالث','10','0','0');
+INSERT INTO [t5715] VALUES (4,'الحديث الرابع','12','0','0');
+INSERT INTO [t5715] VALUES (5,'الحديث الخامس','15','0','0');
+INSERT INTO [t5715] VALUES (6,'الحديث السادس','17','0','0');
+INSERT INTO [t5715] VALUES (7,'الحديث السابع','19','0','0');
+INSERT INTO [t5715] VALUES (8,'الحديث الثامن','22','0','0');
+INSERT INTO [t5715] VALUES (9,'الحديث التاسع','24','0','0');
+INSERT INTO [t5715] VALUES (10,'الحديث العاشر','26','0','0');
+INSERT INTO [t5715] VALUES (11,'الحديث الحادي عشر','29','0','0');
+INSERT INTO [t5715] VALUES (12,'الحديث الثاني عشر','31','0','0');
+INSERT INTO [t5715] VALUES (13,'الحديث الثالث عشر','33','0','0');
+INSERT INTO [t5715] VALUES (14,'الحديث الرابع عشر','37','0','0');
+INSERT INTO [t5715] VALUES (15,'الحديث الخامس عشر','39','0','0');
+INSERT INTO [t5715] VALUES (16,'الحديث السادس عشر','43','0','0');
+INSERT INTO [t5715] VALUES (17,'الحديث السابع عشر','44','0','0');
+INSERT INTO [t5715] VALUES (18,'الحديث الثامن عشر','46','0','0');
+INSERT INTO [t5715] VALUES (19,'الحديث التاسع عشر','52','0','0');
+INSERT INTO [t5715] VALUES (20,'الحديث العشرون','54','0','0');
+INSERT INTO [t5715] VALUES (21,'الحديث الحادي والعشرون','57','0','0');
+INSERT INTO [t5715] VALUES (22,'الحديث الثاني والعشرون','58','0','0');
+INSERT INTO [t5715] VALUES (23,'الحديث الثالث والعشرون','60','0','0');
+INSERT INTO [t5715] VALUES (24,'الحديث الرابع والعشرون','62','0','0');
+INSERT INTO [t5715] VALUES (25,'الحديث الخامس والعشرون','64','0','0');
+INSERT INTO [t5715] VALUES (26,'الحديث السادس والعشرون','65','0','0');
+INSERT INTO [t5715] VALUES (27,'الحديث السابع والعشرون','67','0','0');
+INSERT INTO [t5715] VALUES (28,'الحديث الثامن والعشرون','70','0','0');
+INSERT INTO [t5715] VALUES (29,'الحديث التاسع والعشرون','72','0','0');
+INSERT INTO [t5715] VALUES (30,'الحديث الثلاثون','74','0','0');
+INSERT INTO [t5715] VALUES (31,'الحديث الثاني والثلاثون','79','0','0');
+INSERT INTO [t5715] VALUES (32,'الحديث الثالث والثلاثون','81','0','0');
+INSERT INTO [t5715] VALUES (33,'الحديث الرابع والثلاثون','83','0','0');
+INSERT INTO [t5715] VALUES (34,'الحديث الخامس والثلاثون','84','0','0');
+INSERT INTO [t5715] VALUES (35,'الحديث السادس والثلاثون','86','0','0');
+INSERT INTO [t5715] VALUES (36,'الحديث السابع والثلاثون','88','0','0');
+INSERT INTO [t5715] VALUES (37,'الحديث الثامن والثلاثون','90','0','0');
+INSERT INTO [t5715] VALUES (38,'الحديث التاسع والثلاثون','92','0','0');
+INSERT INTO [t5715] VALUES (39,'الحديث الأربعون','94','0','0');
