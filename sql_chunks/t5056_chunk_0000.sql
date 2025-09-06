a6f1d81366ac8eede2188d0e26a9023d9460ca8f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:38 +0700

    Upload SQL chunk: t5056_chunk_0000.sql

---
+++ sql_chunks/t5056_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t5056
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t5056" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5056
+INSERT INTO [t5056] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t5056] VALUES (2,'مكانة لا إله إلا الله في الحياة','4','0','0');
+INSERT INTO [t5056] VALUES (3,'فضل لا إله إلا الله','8','0','0');
+INSERT INTO [t5056] VALUES (4,'إعرابها وأركانها وشروطها','11','0','0');
+INSERT INTO [t5056] VALUES (5,'معنى الكلمة ومقتضاها','16','0','0');
+INSERT INTO [t5056] VALUES (6,'متى ينفع الإنسان قول لا إله إلا الله ومتى لا ينفعه ذلك','28','0','0');
+INSERT INTO [t5056] VALUES (7,'آثار لا إله إلا الله','38','0','0');
