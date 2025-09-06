344a1739171c614a1ea1086d160e9d868c26f9a7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:26 +0700

    Upload SQL chunk: t30622_chunk_0000.sql

---
+++ sql_chunks/t30622_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t30622
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t30622" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30622
+INSERT INTO [t30622] VALUES (1,'من حديث الآدمي','3','0','0');
+INSERT INTO [t30622] VALUES (2,'من حديث أبي عبد الرحمن المقرئ','8','0','0');
+INSERT INTO [t30622] VALUES (3,'من حديث أبي القاسم بن بشران','14','0','0');
+INSERT INTO [t30622] VALUES (4,'من حديث عفان','17','0','0');
+INSERT INTO [t30622] VALUES (5,'من حديث أبي محمد بن حيان المعروف بأبي الشيخ','21','0','0');
+INSERT INTO [t30622] VALUES (6,'من حديث أبي نصر السجزي , عن أبي العباس الإشبيلي','24','0','0');
+INSERT INTO [t30622] VALUES (7,'من حديث أبي القاسم القاضي','44','0','0');
+INSERT INTO [t30622] VALUES (8,'من حديث عبد الله بن أحمد بن حنبل , عن أبيه','56','0','0');
+INSERT INTO [t30622] VALUES (9,'من حديث أبي القاسم بن بشران','59','0','0');
+INSERT INTO [t30622] VALUES (10,'منام شريح بن يونس رحمه الله','64','0','0');
+INSERT INTO [t30622] VALUES (11,'من حديث ابن المقرئ','65','0','0');
+INSERT INTO [t30622] VALUES (12,'من فوائد التنوخي','69','0','0');
