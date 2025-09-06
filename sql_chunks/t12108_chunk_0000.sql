1ed98ee797d721d5762d9089b7c7b1c271323860
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:03 +0700

    Upload SQL chunk: t12108_chunk_0000.sql

---
+++ sql_chunks/t12108_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t12108
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t12108" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12108
+INSERT INTO [t12108] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12108] VALUES (2,'مفهوم المعجم التاريخي للمصطلحات القرآنية المعرفة','2','0','0');
+INSERT INTO [t12108] VALUES (3,'مفهوم المعجم التاريخي','2','2','0');
+INSERT INTO [t12108] VALUES (4,'مفهوم المصطلحات القرآنية','3','2','0');
+INSERT INTO [t12108] VALUES (5,'مفهوم المصطلحات المعرفة','6','2','0');
+INSERT INTO [t12108] VALUES (6,'مفهوم المعجم التاريخي للمصطلحات القرآنية المعرفة','7','2','0');
+INSERT INTO [t12108] VALUES (7,'أهداف المعجم','8','0','0');
+INSERT INTO [t12108] VALUES (8,'مصادر مادة المعجم','10','0','0');
+INSERT INTO [t12108] VALUES (9,'ترتيب مادة المعجم','12','0','0');
+INSERT INTO [t12108] VALUES (10,'مراحل إنجاز المعجم','13','0','0');
+INSERT INTO [t12108] VALUES (11,'عرض أولي لصنف من مادة مصطلح من المعجم','14','0','0');
+INSERT INTO [t12108] VALUES (12,'مدخل','14','11','0');
+INSERT INTO [t12108] VALUES (13,'التغيير','16','11','0');
+INSERT INTO [t12108] VALUES (14,'تغيير خلق الله','19','11','0');
+INSERT INTO [t12108] VALUES (15,'تغيير نعمة الله','27','11','0');
+INSERT INTO [t12108] VALUES (16,'تغيير ما بالأنفس','30','11','0');
+INSERT INTO [t12108] VALUES (17,'تغيير ما بقوم','34','11','0');
+INSERT INTO [t12108] VALUES (18,'مصادر ومراجع','36','0','0');
