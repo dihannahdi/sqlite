7fc5fb0474266ca16d2af0d504d3e439ef6c7954
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:16 +0700

    Upload SQL chunk: t26541_chunk_0000.sql

---
+++ sql_chunks/t26541_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t26541
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t26541" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26541
+INSERT INTO [t26541] VALUES (1,'صور من المخطوطات التي اعتمدناها في تحقيق الكتاب','1','0','0');
+INSERT INTO [t26541] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t26541] VALUES (3,'مقدمة','1','0','0');
+INSERT INTO [t26541] VALUES (4,'خاتمة','40','0','0');
