cd5e473816bfe7cc1bc56a22d4583e62795ed22e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:46 +0700

    Upload SQL chunk: t30116_chunk_0000.sql

---
+++ sql_chunks/t30116_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t30116
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t30116" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30116
+INSERT INTO [t30116] VALUES (1,'الجزء الخامس والعشرون من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30116] VALUES (2,'من حديث أبي الفضل الزهري عن شيوخه','3','0','0');
+INSERT INTO [t30116] VALUES (3,'من فوائد أبي الفتح سليم بن أيوب الرازي','12','0','0');
+INSERT INTO [t30116] VALUES (4,'من حديث أبي بكر الشافعي','16','0','0');
+INSERT INTO [t30116] VALUES (5,'من فوائد ابن بطة العكبري','19','0','0');
+INSERT INTO [t30116] VALUES (6,'من فوائد ابن الطوسي','20','0','0');
+INSERT INTO [t30116] VALUES (7,'من حديث ابن الجندي','21','0','0');
+INSERT INTO [t30116] VALUES (8,'من فوائد الجوهري','25','0','0');
+INSERT INTO [t30116] VALUES (9,'من حديث أبي علي الكوكبي','27','0','0');
+INSERT INTO [t30116] VALUES (10,'من فوائد أبي الفتح نصر بن إبراهيم المقدسي الفقيه','31','0','0');
+INSERT INTO [t30116] VALUES (11,'من حديث أبي جعفر بن المسلمة','34','0','0');
+INSERT INTO [t30116] VALUES (12,'من فوائد أبي بكر بن دريد','39','0','0');
+INSERT INTO [t30116] VALUES (13,'من فوائد أبي زكريا البخاري','48','0','0');
