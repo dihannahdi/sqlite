6cf00a8cb7779a1d697399c5336b67f0ecbf312a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:50:08 +0700

    Upload SQL chunk: t1060_chunk_0000.sql

---
+++ sql_chunks/t1060_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t1060
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t1060" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1060
+INSERT INTO [t1060] VALUES (1,'تقديم','1','0','0');
+INSERT INTO [t1060] VALUES (2,'[بين يدى الكتاب]','2','0','0');
+INSERT INTO [t1060] VALUES (3,'دراسة السؤالات','4','0','0');
+INSERT INTO [t1060] VALUES (4,'[ترجمة الإمام الدارقطني]','7','0','0');
+INSERT INTO [t1060] VALUES (5,'[التعريف بالإمام أبى بكر البرقانى]','18','0','0');
+INSERT INTO [t1060] VALUES (6,'[وصف مخطوطات الكتاب]','22','0','0');
+INSERT INTO [t1060] VALUES (7,'[عملي في الكتاب]','25','0','0');
+INSERT INTO [t1060] VALUES (8,'الجزء فيه سؤالات أبى بكر البرقانى','28','0','0');
+INSERT INTO [t1060] VALUES (9,'بسم الله الرحمن الرحيم','31','0','0');
+INSERT INTO [t1060] VALUES (10,'[آخر الجزء]','79','0','0');
