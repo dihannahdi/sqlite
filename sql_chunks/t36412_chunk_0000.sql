c4bded85bb3f9034159ef618d6995835cab941f2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:06 +0700

    Upload SQL chunk: t36412_chunk_0000.sql

---
+++ sql_chunks/t36412_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t36412
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t36412" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36412
+INSERT INTO [t36412] VALUES (1,'وماذا بعد .. ؟','1','0','0');
+INSERT INTO [t36412] VALUES (2,'أحداث غزة','1','0','0');
+INSERT INTO [t36412] VALUES (3,'{ولو شاء ربك ما فعلوه}','2','0','0');
+INSERT INTO [t36412] VALUES (4,'لا إكراه على الإيمان','2','3','0');
+INSERT INTO [t36412] VALUES (5,'بنو إسرائيل والرسالة','3','0','0');
+INSERT INTO [t36412] VALUES (6,'الإستبدال الخطير','3','5','0');
+INSERT INTO [t36412] VALUES (7,'ماذا حدث بعد ذلك؟!','3','5','0');
+INSERT INTO [t36412] VALUES (8,'النعمة العظمى لأمة الإسلام','3','0','0');
+INSERT INTO [t36412] VALUES (9,'الخير المخبوء','4','8','0');
+INSERT INTO [t36412] VALUES (10,'أهمية الجهاد','4','0','0');
+INSERT INTO [t36412] VALUES (11,'أمة الإسلام أمة جهاد','4','10','0');
+INSERT INTO [t36412] VALUES (12,'ماذا لو فرطنا؟!','5','10','0');
+INSERT INTO [t36412] VALUES (13,'لماذا نعاقب؟!','5','0','0');
+INSERT INTO [t36412] VALUES (14,'إصلاح الداخل أولا','6','13','0');
+INSERT INTO [t36412] VALUES (15,'والتغيير المنشود يشمل كيان الإنسان بمحاوره الأربعة','6','13','0');
+INSERT INTO [t36412] VALUES (16,'لا بديل عن التربية','7','0','0');
+INSERT INTO [t36412] VALUES (17,'ضرورة المعية والصحبة','7','16','0');
+INSERT INTO [t36412] VALUES (18,'مهمة طلائع الأمة في تربيتها','8','16','0');
+INSERT INTO [t36412] VALUES (19,'هل من الضروري تربية الأمة جميعا؟','8','16','0');
+INSERT INTO [t36412] VALUES (20,'الجمرة المشتعلة','8','0','0');
+INSERT INTO [t36412] VALUES (21,'الفرد المتوهج أولا','9','20','0');
+INSERT INTO [t36412] VALUES (22,'كلمة أخيرة عن معركتنا مع اليهود','9','0','0');
+INSERT INTO [t36412] VALUES (23,'حقيقة المعركة مع اليهود','9','22','0');
+INSERT INTO [t36412] VALUES (24,'هيا ابدأ بإصلاح نفسك وتربية غيرك','10','22','0');
+INSERT INTO [t36412] VALUES (25,'المراجع','11','0','0');
