bceda23f45d30e74d4368add4670f12258f263fd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:09 +0700

    Upload SQL chunk: t6244_chunk_0000.sql

---
+++ sql_chunks/t6244_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t6244
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t6244" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6244
+INSERT INTO [t6244] VALUES (1,'فصل','2','0','0');
+INSERT INTO [t6244] VALUES (2,'فصل','3','0','0');
+INSERT INTO [t6244] VALUES (3,'فصل','12','0','0');
+INSERT INTO [t6244] VALUES (4,'فصل','17','0','0');
+INSERT INTO [t6244] VALUES (5,'فصل','21','0','0');
+INSERT INTO [t6244] VALUES (6,'فصل','22','0','0');
+INSERT INTO [t6244] VALUES (7,'فصل','25','0','0');
+INSERT INTO [t6244] VALUES (8,'فصل','39','0','0');
+INSERT INTO [t6244] VALUES (9,'فصل','42','0','0');
+INSERT INTO [t6244] VALUES (10,'فصل','49','0','0');
+INSERT INTO [t6244] VALUES (11,'فصل','51','0','0');
+INSERT INTO [t6244] VALUES (12,'فصل','53','0','0');
+INSERT INTO [t6244] VALUES (13,'فصل','63','0','0');
+INSERT INTO [t6244] VALUES (14,'فصل','66','0','0');
+INSERT INTO [t6244] VALUES (15,'فصل','68','0','0');
+INSERT INTO [t6244] VALUES (16,'فصل','71','0','0');
+INSERT INTO [t6244] VALUES (17,'فصل','73','0','0');
+INSERT INTO [t6244] VALUES (18,'فصل','75','0','0');
