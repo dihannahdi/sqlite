e61641f2bd839acea0e2ae71c9584c7eb2e84fe0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:07 +0700

    Upload SQL chunk: t10169_chunk_0000.sql

---
+++ sql_chunks/t10169_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t10169
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t10169" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10169
+INSERT INTO [t10169] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10169] VALUES (2,'تمهيد','5','0','0');
+INSERT INTO [t10169] VALUES (3,'أقوال المفسرين','9','0','0');
+INSERT INTO [t10169] VALUES (4,'أقوال أئمة الحديث','14','0','0');
+INSERT INTO [t10169] VALUES (5,'أقوال أئمة المذاهب الأربعة','17','0','0');
+INSERT INTO [t10169] VALUES (6,'كلام علماء الأصول','20','0','0');
