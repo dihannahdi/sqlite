5bf1428869a370ef8b8a7654443fa9157647fcbf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:39 +0700

    Upload SQL chunk: t12082_chunk_0000.sql

---
+++ sql_chunks/t12082_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t12082
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t12082" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12082
+INSERT INTO [t12082] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12082] VALUES (2,'كمال الشريعة الإسلامية من ناحية الأخلاق','3','0','0');
+INSERT INTO [t12082] VALUES (3,'الأخلاق بين الطبع والتطبع','5','0','0');
+INSERT INTO [t12082] VALUES (4,'من الأفضل','6','0','0');
+INSERT INTO [t12082] VALUES (5,'مجالات حسن الخلق','8','0','0');
+INSERT INTO [t12082] VALUES (6,'كيفية اكتساب مكارم الأخلاق','24','0','0');
+INSERT INTO [t12082] VALUES (7,'صور من مكارم الأخلاق','28','0','0');
+INSERT INTO [t12082] VALUES (8,'أخلاق غير المسلمين','42','0','0');
+INSERT INTO [t12082] VALUES (9,'كمال خلق النبي صلى الله علية وسلم','45','0','0');
