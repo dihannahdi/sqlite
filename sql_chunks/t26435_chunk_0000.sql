9db5a3b5cac8b543458eb61a8334b7d1eebf618f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:26 +0700

    Upload SQL chunk: t26435_chunk_0000.sql

---
+++ sql_chunks/t26435_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t26435
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t26435" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26435
+INSERT INTO [t26435] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26435] VALUES (2,'إن إبراهيم كان أمة قانتا','2','0','0');
+INSERT INTO [t26435] VALUES (3,'[النبي الأمة]','3','0','0');
+INSERT INTO [t26435] VALUES (4,'[القانت]','5','0','0');
+INSERT INTO [t26435] VALUES (5,'[الحنيف]','6','0','0');
+INSERT INTO [t26435] VALUES (6,'[الشاكر المضياف]','11','0','0');
+INSERT INTO [t26435] VALUES (7,'[الخليل المجتبى]','15','0','0');
+INSERT INTO [t26435] VALUES (8,'[الصالح]','18','0','0');
+INSERT INTO [t26435] VALUES (9,'[صاحب الملة الحنيفية]','20','0','0');
+INSERT INTO [t26435] VALUES (10,'[وصفه]','25','0','0');
+INSERT INTO [t26435] VALUES (11,'[أول من اختتن]','28','0','0');
+INSERT INTO [t26435] VALUES (12,'[الاطمئنان]','32','0','0');
+INSERT INTO [t26435] VALUES (13,'[الصحف]','37','0','0');
+INSERT INTO [t26435] VALUES (14,'[أول من يكسى يوم القيامة]','40','0','0');
+INSERT INTO [t26435] VALUES (15,'[ومع أبيه آزر]','42','0','0');
+INSERT INTO [t26435] VALUES (16,'[قتل الوزغ]','45','0','0');
+INSERT INTO [t26435] VALUES (17,'[حسبنا الله]','46','0','0');
+INSERT INTO [t26435] VALUES (18,'[أنبياء مكرمون]','47','0','0');
+INSERT INTO [t26435] VALUES (19,'[نبينا أفضل البشر]','48','0','0');
+INSERT INTO [t26435] VALUES (20,'[الكذبات وتأويلها]','50','0','0');
+INSERT INTO [t26435] VALUES (21,'[زيارة إسماعيل]','54','0','0');
+INSERT INTO [t26435] VALUES (22,'[لمحات من حياته]','56','0','0');
+INSERT INTO [t26435] VALUES (23,'[نصيحة لأمة محمد صلى الله عليه وسلم]','58','0','0');
+INSERT INTO [t26435] VALUES (24,'[الثناء عليه نظما]','60','0','0');
