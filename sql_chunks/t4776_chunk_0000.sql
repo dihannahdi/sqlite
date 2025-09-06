24e179fd5d361ec3c4521479c532abba5bdc60a9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:58 +0700

    Upload SQL chunk: t4776_chunk_0000.sql

---
+++ sql_chunks/t4776_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t4776
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t4776" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4776
+INSERT INTO [t4776] VALUES (1,'القسم الأول','1','0','0');
+INSERT INTO [t4776] VALUES (2,'موارد البيهقي في السنن الكبرى','1','1','0');
+INSERT INTO [t4776] VALUES (3,'أولا: علوم القرآن','3','1','0');
+INSERT INTO [t4776] VALUES (4,'ثانيا: الحديث وعلومه:','4','1','0');
+INSERT INTO [t4776] VALUES (5,'ثالثا: كتب الفقه وأصوله:','12','1','0');
+INSERT INTO [t4776] VALUES (6,'رابعا: كتب التواريخ والأنساب والمناقب والجرح والتعديل','16','1','0');
+INSERT INTO [t4776] VALUES (7,'خامسا: كتب الرقاق واللغة','20','1','0');
+INSERT INTO [t4776] VALUES (8,'سادسا: بعض المصادر التي أستعملها البيهقي ولم أتمكن من تحديدها:','22','1','0');
+INSERT INTO [t4776] VALUES (9,'سابعا: وقد أعتمد','23','1','0');
+INSERT INTO [t4776] VALUES (10,'القسم الثاني','24','0','0');
+INSERT INTO [t4776] VALUES (11,'دراسة نقدية لمنهج البيهقي في"السنن الكبرى"','24','10','0');
+INSERT INTO [t4776] VALUES (12,'مصادر ومراجع','34','0','0');
