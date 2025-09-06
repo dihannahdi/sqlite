c13ef35ad02beb5425a604985aff1c0f0d1279d7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:37 +0700

    Upload SQL chunk: t30696_chunk_0000.sql

---
+++ sql_chunks/t30696_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30696
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30696" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30696
+INSERT INTO [t30696] VALUES (1,'الجزء الثامن والعشرون من المنتقى من سماعات محمد بن عبد الرحيم بن عبد الواحد بن أحمد المقدسي','1','0','0');
