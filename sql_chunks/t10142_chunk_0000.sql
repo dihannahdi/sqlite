81ed15578134fd095af12af6e0b8c62414278358
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:01 +0700

    Upload SQL chunk: t10142_chunk_0000.sql

---
+++ sql_chunks/t10142_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t10142
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t10142" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10142
+INSERT INTO [t10142] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10142] VALUES (2,'حروف المضارعة','5','0','0');
+INSERT INTO [t10142] VALUES (3,'ما يكسر من حروف المضارعة وما يمتنع كسره','11','0','0');
+INSERT INTO [t10142] VALUES (4,'كسر حرف المضارعة ليعلم أن ثاني الفعل الماضي مكسور','20','0','0');
+INSERT INTO [t10142] VALUES (5,'الأجوف','25','0','0');
+INSERT INTO [t10142] VALUES (6,'الناقص','27','0','0');
+INSERT INTO [t10142] VALUES (7,'المبدوء بهمزة وصل','29','0','0');
+INSERT INTO [t10142] VALUES (8,'المبدوء بتاء زائدة معتادة','31','0','0');
+INSERT INTO [t10142] VALUES (9,'ماجاء على (فعل) ولم يكسر منه حرف المضارعة','35','0','0');
+INSERT INTO [t10142] VALUES (10,'حكم ضم حرف المضارعة في غير رباعي الماضي','37','0','0');
+INSERT INTO [t10142] VALUES (11,'الكسر في اللهجات المعاصرة','40','0','0');
+INSERT INTO [t10142] VALUES (12,'نتائج البحث','42','0','0');
+INSERT INTO [t10142] VALUES (13,'مصادر ومراجع','43','0','0');
