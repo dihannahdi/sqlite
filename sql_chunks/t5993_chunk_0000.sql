51ec9e81a46defbaf3fcc8d35850c20e221e77e8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:09 +0700

    Upload SQL chunk: t5993_chunk_0000.sql

---
+++ sql_chunks/t5993_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t5993
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t5993" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5993
+INSERT INTO [t5993] VALUES (1,'من كتاب الطهارة','11','0','0');
+INSERT INTO [t5993] VALUES (2,'من كتاب الجمعة','31','0','0');
+INSERT INTO [t5993] VALUES (3,'من كتاب الكسوف','35','0','0');
+INSERT INTO [t5993] VALUES (4,'من كتاب الجنائز','36','0','0');
+INSERT INTO [t5993] VALUES (5,'من كتاب الزكاة','39','0','0');
+INSERT INTO [t5993] VALUES (6,'من كتاب الصيام','41','0','0');
+INSERT INTO [t5993] VALUES (7,'من كتاب الحج','42','0','0');
+INSERT INTO [t5993] VALUES (8,'من كتاب الرضاع','52','0','0');
+INSERT INTO [t5993] VALUES (9,'من كتاب الطلاق','53','0','0');
+INSERT INTO [t5993] VALUES (10,'من كتاب البيوع','55','0','0');
+INSERT INTO [t5993] VALUES (11,'من كتاب الهبات من الوصايا والعمرى','61','0','0');
+INSERT INTO [t5993] VALUES (12,'من كتاب الحدود','65','0','0');
+INSERT INTO [t5993] VALUES (13,'من كتاب القضاء والشهادات','67','0','0');
+INSERT INTO [t5993] VALUES (14,'من كتاب اللقطة','68','0','0');
+INSERT INTO [t5993] VALUES (15,'من كتاب الجهاد','69','0','0');
+INSERT INTO [t5993] VALUES (16,'من كتاب الإمارة','73','0','0');
+INSERT INTO [t5993] VALUES (17,'من كتاب الصيد والذبائح','78','0','0');
+INSERT INTO [t5993] VALUES (18,'من كتاب الأضاحي','81','0','0');
+INSERT INTO [t5993] VALUES (19,'من كتابي الأشربة والأطعمة','82','0','0');
+INSERT INTO [t5993] VALUES (20,'من كتاب اللباس والزينة','84','0','0');
+INSERT INTO [t5993] VALUES (21,'من كتاب الأدب','88','0','0');
+INSERT INTO [t5993] VALUES (22,'من كتاب السلام','89','0','0');
+INSERT INTO [t5993] VALUES (23,'من كتاب التعبير','91','0','0');
+INSERT INTO [t5993] VALUES (24,'من كتاب المناقب','92','0','0');
+INSERT INTO [t5993] VALUES (25,'من كتاب الأدب والبر والصلة','102','0','0');
+INSERT INTO [t5993] VALUES (26,'من كتاب القدر','105','0','0');
+INSERT INTO [t5993] VALUES (27,'من كتاب الذكر','107','0','0');
+INSERT INTO [t5993] VALUES (28,'من كتاب الفتن','108','0','0');
+INSERT INTO [t5993] VALUES (29,'من كتاب الرقاق','115','0','0');
