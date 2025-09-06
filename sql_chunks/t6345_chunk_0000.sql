1eef1cf9e9a2c9962114a73fa213609529638d70
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:57 +0700

    Upload SQL chunk: t6345_chunk_0000.sql

---
+++ sql_chunks/t6345_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t6345
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t6345" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6345
+INSERT INTO [t6345] VALUES (1,'مقدمة في أهمية الإفتاء وعظم خطره وفضله','1','0','0');
+INSERT INTO [t6345] VALUES (2,'فصل في معرفة من يصلح للفتوى','5','0','0');
+INSERT INTO [t6345] VALUES (3,'فصل في وجوب ورع المفتي وديانته','6','0','0');
+INSERT INTO [t6345] VALUES (4,'فصل في شروط المفتي','7','0','0');
+INSERT INTO [t6345] VALUES (5,'فصل في أقسام المفتين','10','0','0');
+INSERT INTO [t6345] VALUES (6,'فصل في بعض مسائل أهلية المفتي','19','0','0');
+INSERT INTO [t6345] VALUES (7,'فصل في أحكام المفتين','23','0','0');
+INSERT INTO [t6345] VALUES (8,'فصل في آداب الفتوى','32','0','0');
+INSERT INTO [t6345] VALUES (9,'فصل في آداب المستفتي وصفته وأحكامه','59','0','0');
