a92ffe8368d6aa333967e254d473db47ca235f8f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:12 +0700

    Upload SQL chunk: t21681_chunk_0000.sql

---
+++ sql_chunks/t21681_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21681
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21681" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21681
+INSERT INTO [t21681] VALUES (1,'مشيخة دانيال بن منكلى بن صرفا الكركي','1','0','0');
