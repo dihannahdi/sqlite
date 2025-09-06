631fa7b6543f5894bb58b0031f7248b7a30fc025
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:53:10 +0700

    Upload SQL chunk: t9378_chunk_0000.sql

---
+++ sql_chunks/t9378_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9378
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9378" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9378
+INSERT INTO [t9378] VALUES (1,'«إذا أردت حاجة، فاقرأ بفاتحة الكتاب حتى تختمها، تقضى إن شاء الله»','1','0','0');
+INSERT INTO [t9378] VALUES (2,'قسمت الصلاة بيني وبين عبدي، فإذا قال العبد: {الحمد لله رب العالمين} [الفاتحة: ٢] ، قال','2','0','0');
