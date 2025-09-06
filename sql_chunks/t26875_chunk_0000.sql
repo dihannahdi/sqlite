6f7bf2d55b4ea65d21f2c7f6bf7a8b185477d28d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:00 +0700

    Upload SQL chunk: t26875_chunk_0000.sql

---
+++ sql_chunks/t26875_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t26875
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t26875" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26875
+INSERT INTO [t26875] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26875] VALUES (2,'الجزء الأول من المنتقى من أخبار الأصمعي','2','0','0');
+INSERT INTO [t26875] VALUES (3,'[الجزء الثاني]','75','0','0');
+INSERT INTO [t26875] VALUES (4,'[من مناقب] عبد الله بن جعفر بن أبي طالب','95','0','0');
