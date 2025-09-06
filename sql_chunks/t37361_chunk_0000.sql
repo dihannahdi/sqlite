3850534ce16fce4a19a5d16278ed437414059c04
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:35 +0700

    Upload SQL chunk: t37361_chunk_0000.sql

---
+++ sql_chunks/t37361_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t37361
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t37361" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37361
+INSERT INTO [t37361] VALUES (1,'الجزء الأول','2','0','0');
+INSERT INTO [t37361] VALUES (2,'الغلاف','2','1','0');
+INSERT INTO [t37361] VALUES (3,'الجزء الأول من التفسير تفسير القرآن من جامع عبد الله بن وهب','3','1','0');
+INSERT INTO [t37361] VALUES (4,'بسم الله الرحمن الرحيم','4','1','0');
+INSERT INTO [t37361] VALUES (5,'تفسير ما بين المغرب والعشاء','73','1','0');
+INSERT INTO [t37361] VALUES (6,'النهي عن المسألة عن القرآن','221','1','0');
+INSERT INTO [t37361] VALUES (7,'الجزء الثاني','360','0','0');
+INSERT INTO [t37361] VALUES (8,'الجزء الثالث','724','0','0');
+INSERT INTO [t37361] VALUES (9,'الغلاف','724','8','0');
+INSERT INTO [t37361] VALUES (10,'ترغيب القرآن','744','8','0');
+INSERT INTO [t37361] VALUES (11,'في العربية بالقرآن','791','8','0');
+INSERT INTO [t37361] VALUES (12,'في اختلاف حروف القرآن','811','8','0');
+INSERT INTO [t37361] VALUES (13,'باب الناسخ: وهذا كتاب الناسخ والمنسوخ','871','8','0');
+INSERT INTO [t37361] VALUES (14,'الناسخ من القرآن','907','8','0');
+INSERT INTO [t37361] VALUES (15,'السجود','916','8','0');
+INSERT INTO [t37361] VALUES (16,'في سجود القرآن','939','8','0');
