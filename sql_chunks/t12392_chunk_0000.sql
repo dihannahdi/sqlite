01df702620d6c98c32a74759fbc64a5b8602c86b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:04:41 +0700

    Upload SQL chunk: t12392_chunk_0000.sql

---
+++ sql_chunks/t12392_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t12392
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t12392" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12392
+INSERT INTO [t12392] VALUES (1,'مقدمة المصنف','1','0','0');
+INSERT INTO [t12392] VALUES (2,'فصل في ذكر اشتقاق " العرب "','27','0','0');
+INSERT INTO [t12392] VALUES (3,'فصل في اشتقاق اللغة','56','0','0');
+INSERT INTO [t12392] VALUES (4,'في ذكر الشعراء المراقسة','66','0','0');
