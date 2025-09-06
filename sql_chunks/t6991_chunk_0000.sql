211ebb101c54710f85f9c68bc064fdb516e9ebc4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:34 +0700

    Upload SQL chunk: t6991_chunk_0000.sql

---
+++ sql_chunks/t6991_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6991
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6991" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6991
+INSERT INTO [t6991] VALUES (1,'المسألة الأولى','1','0','0');
+INSERT INTO [t6991] VALUES (2,'المسألة الثانية','3','0','0');
+INSERT INTO [t6991] VALUES (3,'المسألة الثالثة','5','0','0');
