17dfc0c2023eab40830bbd590596212d63ad1f1e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:14 +0700

    Upload SQL chunk: t8649_chunk_0000.sql

---
+++ sql_chunks/t8649_chunk_0000.sql
@@ -0,0 +1,53 @@
+-- Table: t8649
+-- Chunk: 1
+-- Rows: 46
+
+CREATE TABLE "t8649" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8649
+INSERT INTO [t8649] VALUES (1,'الفرائد الحسان في عد آى القرآن','1','0','0');
+INSERT INTO [t8649] VALUES (2,'نفائس البيان-شرح الفرائد الحسان','15','0','0');
+INSERT INTO [t8649] VALUES (3,'مدخل','15','2','0');
+INSERT INTO [t8649] VALUES (4,'سورة الفاتحة','21','2','0');
+INSERT INTO [t8649] VALUES (5,'سورة البقرة','22','2','0');
+INSERT INTO [t8649] VALUES (6,'سورة آل عمران','26','2','0');
+INSERT INTO [t8649] VALUES (7,'سورة النساء','28','2','0');
+INSERT INTO [t8649] VALUES (8,'سورة المائدة','29','2','0');
+INSERT INTO [t8649] VALUES (9,'سورة الأنعام والأعراف','30','2','0');
+INSERT INTO [t8649] VALUES (10,'سورة الأنفال والتوبة','33','2','0');
+INSERT INTO [t8649] VALUES (11,'سورة يونس عليه السلام','35','2','0');
+INSERT INTO [t8649] VALUES (12,'سورة هود','36','2','0');
+INSERT INTO [t8649] VALUES (13,'سورة الرعد','38','2','0');
+INSERT INTO [t8649] VALUES (14,'سورة إبراهيم','39','2','0');
+INSERT INTO [t8649] VALUES (15,'سورة الإسراء والكهف','40','2','0');
+INSERT INTO [t8649] VALUES (16,'سورة مريم','42','2','0');
+INSERT INTO [t8649] VALUES (17,'سورة طه','43','2','0');
+INSERT INTO [t8649] VALUES (18,'سورة الأنبياء والحج','47','2','0');
+INSERT INTO [t8649] VALUES (19,'سورة المؤمنين والنور','49','2','0');
+INSERT INTO [t8649] VALUES (20,'سورة الشعراء','50','2','0');
+INSERT INTO [t8649] VALUES (21,'سورة النمل والقصص','51','2','0');
+INSERT INTO [t8649] VALUES (22,'سورة العنكبوت','52','2','0');
+INSERT INTO [t8649] VALUES (23,'سورة الروم','53','2','0');
+INSERT INTO [t8649] VALUES (24,'سورة لقمان والسجدة','55','2','0');
+INSERT INTO [t8649] VALUES (25,'سورة سبأ وفاطر','56','2','0');
+INSERT INTO [t8649] VALUES (26,'سورة الصافات وص','58','2','0');
+INSERT INTO [t8649] VALUES (27,'سورة الزمر','60','2','0');
+INSERT INTO [t8649] VALUES (28,'سورة غافر وفصلت والشورى','61','2','0');
+INSERT INTO [t8649] VALUES (29,'سورة الزخرف والدخان','63','2','0');
+INSERT INTO [t8649] VALUES (30,'سورة القتال','65','2','0');
+INSERT INTO [t8649] VALUES (31,'سورة الطور والنجم','66','2','0');
+INSERT INTO [t8649] VALUES (32,'سورة الرحمن','67','2','0');
+INSERT INTO [t8649] VALUES (33,'سورة الواقعة','68','2','0');
+INSERT INTO [t8649] VALUES (34,'سورة الحديد والمجادلة','71','2','0');
+INSERT INTO [t8649] VALUES (35,'سورة الطلاق والتحريم والملك','72','2','0');
+INSERT INTO [t8649] VALUES (36,'سورة الحاقة والمعارج','73','2','0');
+INSERT INTO [t8649] VALUES (37,'سورة نوح والجن','74','2','0');
+INSERT INTO [t8649] VALUES (38,'سورة المزمل والمدثر','76','2','0');
+INSERT INTO [t8649] VALUES (39,'سورة القيامة والنبأ','78','2','0');
+INSERT INTO [t8649] VALUES (40,'سورة النازعات وعبس','79','2','0');
+INSERT INTO [t8649] VALUES (41,'سورة التكوير والانشقاق والطارق','80','2','0');
+INSERT INTO [t8649] VALUES (42,'سورة الفجر','81','2','0');
+INSERT INTO [t8649] VALUES (43,'سورة الشمس والعلق والقدر','82','2','0');
+INSERT INTO [t8649] VALUES (44,'سورة البينة والزلزلة','83','2','0');
+INSERT INTO [t8649] VALUES (45,'سورة القارعة','84','2','0');
+INSERT INTO [t8649] VALUES (46,'من سورة والعصر إلى آخر القرآن الكريم','85','2','0');
