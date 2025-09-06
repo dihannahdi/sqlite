92ef2147828f65c0c093b62ea9d72f75ad0f6988
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:24 +0700

    Upload SQL chunk: t6013_chunk_0000.sql

---
+++ sql_chunks/t6013_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t6013
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t6013" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6013
+INSERT INTO [t6013] VALUES (1,'اختياره أحد الحديثين الصحيحين لقدم حفظ صاحبه','3','0','0');
+INSERT INTO [t6013] VALUES (2,'قلة أحاديث الأبواب','3','0','0');
+INSERT INTO [t6013] VALUES (3,'إعادة الحديث','3','0','0');
+INSERT INTO [t6013] VALUES (4,'اختصار الحديث','4','0','0');
+INSERT INTO [t6013] VALUES (5,'المرسل والاحتجاج به','4','0','0');
+INSERT INTO [t6013] VALUES (6,'ليس في الكتاب حديث عن متروك','5','0','0');
+INSERT INTO [t6013] VALUES (7,'يبين المنكر','5','0','0');
+INSERT INTO [t6013] VALUES (8,'موازنة بينه وبين كتب ابن المبارك ووكيع ومالك وحماد','5','0','0');
+INSERT INTO [t6013] VALUES (9,'جمعه السنن واستقصاؤه','6','0','0');
+INSERT INTO [t6013] VALUES (10,'يبين ما فيه وهن شديد','7','0','0');
+INSERT INTO [t6013] VALUES (11,'المسكوت عنه صالح','7','0','0');
+INSERT INTO [t6013] VALUES (12,'استقصاؤه','7','0','0');
+INSERT INTO [t6013] VALUES (13,'قيمته ومقداره','8','0','0');
+INSERT INTO [t6013] VALUES (14,'أحاديث كتابه أصول المسائل الفقهية','8','0','0');
+INSERT INTO [t6013] VALUES (15,'آراء الصحابة','8','0','0');
+INSERT INTO [t6013] VALUES (16,'جامع سفيان','8','0','0');
+INSERT INTO [t6013] VALUES (17,'أحاديث السنن مشاهير ولا يحتج بالغريب','9','0','0');
+INSERT INTO [t6013] VALUES (18,'قد يوجد المرسل والمدلس عند عدم وجود الصحاح','10','0','0');
+INSERT INTO [t6013] VALUES (19,'عدد أجزائها','12','0','0');
+INSERT INTO [t6013] VALUES (20,'عدد أحاديث كتابه','12','0','0');
+INSERT INTO [t6013] VALUES (21,'منهجه في الاختيار','12','0','0');
+INSERT INTO [t6013] VALUES (22,'اقتصاره على الأحكام','14','0','0');
