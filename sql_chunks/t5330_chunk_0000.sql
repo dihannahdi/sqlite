6efd229b61e009f1f5f848ad444bfa141ca104e1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:11 +0700

    Upload SQL chunk: t5330_chunk_0000.sql

---
+++ sql_chunks/t5330_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t5330
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t5330" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5330
+INSERT INTO [t5330] VALUES (1,'نبذة موجزة عن نشأة علم المكي والمدني','1','0','0');
+INSERT INTO [t5330] VALUES (2,'تعريفات للمكي والمدني','2','0','0');
+INSERT INTO [t5330] VALUES (3,'أسباب الاختلاف في تعيين المكي والمدني','3','0','0');
+INSERT INTO [t5330] VALUES (4,'فوائد معرفة المكي والمدني','4','0','0');
+INSERT INTO [t5330] VALUES (5,'ذكر بعض المؤلفات التي تتضمن المكي والمدني','5','0','0');
+INSERT INTO [t5330] VALUES (6,'الروايات التي حددت السور المكية والمدنية','6','0','0');
+INSERT INTO [t5330] VALUES (7,'القواعد التي يقوم عليها علم المكي والمدني','7','0','0');
+INSERT INTO [t5330] VALUES (8,'الضوابط التي يعرف بها المكي والمدني','8','0','0');
+INSERT INTO [t5330] VALUES (9,'ضوابط السور المدنية وخصائصها','9','0','0');
+INSERT INTO [t5330] VALUES (10,'بيان القول الراجح في السور المختلف فيها','10','0','0');
