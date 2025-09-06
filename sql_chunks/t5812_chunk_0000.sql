49838ec53b24e783c7f90ff0c51038cbe8439fde
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:39 +0700

    Upload SQL chunk: t5812_chunk_0000.sql

---
+++ sql_chunks/t5812_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t5812
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t5812" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5812
+INSERT INTO [t5812] VALUES (1,'حرف الباء','204','0','0');
+INSERT INTO [t5812] VALUES (2,'حرف التاء','601','0','0');
+INSERT INTO [t5812] VALUES (3,'حرف الثاء','679','0','0');
+INSERT INTO [t5812] VALUES (4,'حرف الجيم','713','0','0');
+INSERT INTO [t5812] VALUES (5,'حرف الخاء','1567','0','0');
+INSERT INTO [t5812] VALUES (6,'حرف الدال','1669','0','0');
+INSERT INTO [t5812] VALUES (7,'حرف الذال','1736','0','0');
+INSERT INTO [t5812] VALUES (8,'حرف الراء','1748','0','0');
+INSERT INTO [t5812] VALUES (9,'حرف الزاي','1919','0','0');
+INSERT INTO [t5812] VALUES (10,'حرف السين','2004','0','0');
+INSERT INTO [t5812] VALUES (11,'حرف الشين','2256','0','0');
+INSERT INTO [t5812] VALUES (12,'حرف الصاد','2393','0','0');
+INSERT INTO [t5812] VALUES (13,'حرف الضاد','2448','0','0');
+INSERT INTO [t5812] VALUES (14,'حرف الطاء','2460','0','0');
+INSERT INTO [t5812] VALUES (15,'حرف الظاء','2499','0','0');
+INSERT INTO [t5812] VALUES (16,'حرف العين','2510','0','0');
+INSERT INTO [t5812] VALUES (17,'حرف الغين','2858','0','0');
+INSERT INTO [t5812] VALUES (18,'حرف الفاء حرف الفاء','2904','0','0');
+INSERT INTO [t5812] VALUES (19,'حرف القاف','3045','0','0');
+INSERT INTO [t5812] VALUES (20,'حرف الكاف','3162','0','0');
+INSERT INTO [t5812] VALUES (21,'حرف اللام','3251','0','0');
+INSERT INTO [t5812] VALUES (22,'حرف الميم','3281','0','0');
+INSERT INTO [t5812] VALUES (23,'حرف النون','3601','0','0');
+INSERT INTO [t5812] VALUES (24,'حرف الهاء','3731','0','0');
