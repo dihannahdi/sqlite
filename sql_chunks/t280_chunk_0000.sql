f2c7b95c5a6c32903e1702a66ca4b75af6d82480
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:18 +0700

    Upload SQL chunk: t280_chunk_0000.sql

---
+++ sql_chunks/t280_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t280
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t280" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t280
+INSERT INTO [t280] VALUES (1,'الرسالة الأولى البيت السعيد','1','0','0');
+INSERT INTO [t280] VALUES (2,'مقدمة','1','1','0');
+INSERT INTO [t280] VALUES (3,'دعائم بناء الأسرة المسلمة','6','1','0');
+INSERT INTO [t280] VALUES (4,'الإيمان بالله وتقواه','6','3','0');
+INSERT INTO [t280] VALUES (5,'المعاشرة بالمعروف','8','3','0');
+INSERT INTO [t280] VALUES (6,'دور الزوج في الحفاظ على بيت الزوجية والمعاشرة بالمعروف','8','5','0');
+INSERT INTO [t280] VALUES (7,'دور الزوجة في الحفاظ على بيت الزوجية والمعاشرة بالمعروف','11','5','0');
+INSERT INTO [t280] VALUES (8,'خاتمة','14','1','0');
+INSERT INTO [t280] VALUES (9,'الرسالة الثانية خلاف الزوجين','16','0','0');
+INSERT INTO [t280] VALUES (10,'مقدمة','16','9','0');
+INSERT INTO [t280] VALUES (11,'من أسباب الخلاف بين الزوجين','18','9','0');
+INSERT INTO [t280] VALUES (12,'من وسائل علاج الاختلاف بين الزوجين','21','9','0');
+INSERT INTO [t280] VALUES (13,'الوسيلة الأخيرة في معالجة الاختلاف','26','9','0');
