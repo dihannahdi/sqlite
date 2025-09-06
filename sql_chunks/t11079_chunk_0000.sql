40dd50b9023d7195cb4ad9fef2710a8b669b42fc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:58 +0700

    Upload SQL chunk: t11079_chunk_0000.sql

---
+++ sql_chunks/t11079_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11079
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11079" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11079
+INSERT INTO [t11079] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11079] VALUES (2,'تأسيس التقديس في كشف شبهات داود بن جرجيس','9','0','0');
