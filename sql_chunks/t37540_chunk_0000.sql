de1675f3a3ca541aa9e63306278527540f7aca5c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:44 +0700

    Upload SQL chunk: t37540_chunk_0000.sql

---
+++ sql_chunks/t37540_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t37540
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t37540" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37540
+INSERT INTO [t37540] VALUES (1,'مقدمة المحقق','3','0','0');
+INSERT INTO [t37540] VALUES (2,'ترجمة ابن المقفع','12','0','0');
+INSERT INTO [t37540] VALUES (3,'الأدب الصغير','16','0','0');
