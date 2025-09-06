761f949ec5866eca88698df74151a779230862ee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:28 +0700

    Upload SQL chunk: t4991_chunk_0000.sql

---
+++ sql_chunks/t4991_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t4991
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t4991" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4991
+INSERT INTO [t4991] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t4991] VALUES (2,'الفصل الأول','9','0','0');
+INSERT INTO [t4991] VALUES (3,'الفصل الثاني','18','0','0');
