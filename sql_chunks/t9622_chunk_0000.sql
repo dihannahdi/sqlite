06354eebae2cecb91d42e0f6664b0b66f28844f1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:16 +0700

    Upload SQL chunk: t9622_chunk_0000.sql

---
+++ sql_chunks/t9622_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t9622
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t9622" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9622
+INSERT INTO [t9622] VALUES (1,'الألف','1','0','0');
+INSERT INTO [t9622] VALUES (2,'الباء','2','0','0');
+INSERT INTO [t9622] VALUES (3,'التاء','3','0','0');
+INSERT INTO [t9622] VALUES (4,'الثاء','5','0','0');
+INSERT INTO [t9622] VALUES (5,'الجيم','5','0','0');
+INSERT INTO [t9622] VALUES (6,'الحاء','7','0','0');
+INSERT INTO [t9622] VALUES (7,'الخاء','8','0','0');
+INSERT INTO [t9622] VALUES (8,'الدال','9','0','0');
+INSERT INTO [t9622] VALUES (9,'الذال','10','0','0');
+INSERT INTO [t9622] VALUES (10,'الراء','10','0','0');
+INSERT INTO [t9622] VALUES (11,'الزاي','12','0','0');
+INSERT INTO [t9622] VALUES (12,'الشين','13','0','0');
+INSERT INTO [t9622] VALUES (13,'الصاد','14','0','0');
+INSERT INTO [t9622] VALUES (14,'الضاد','15','0','0');
+INSERT INTO [t9622] VALUES (15,'الطاء','16','0','0');
+INSERT INTO [t9622] VALUES (16,'الظاء','17','0','0');
+INSERT INTO [t9622] VALUES (17,'العين','18','0','0');
+INSERT INTO [t9622] VALUES (18,'الغين','19','0','0');
+INSERT INTO [t9622] VALUES (19,'الفاء','20','0','0');
+INSERT INTO [t9622] VALUES (20,'القاف','21','0','0');
+INSERT INTO [t9622] VALUES (21,'الكاف','23','0','0');
+INSERT INTO [t9622] VALUES (22,'اللام','24','0','0');
+INSERT INTO [t9622] VALUES (23,'الميم','25','0','0');
+INSERT INTO [t9622] VALUES (24,'النون','25','0','0');
+INSERT INTO [t9622] VALUES (25,'الواو','26','0','0');
+INSERT INTO [t9622] VALUES (26,'الهاء','27','0','0');
+INSERT INTO [t9622] VALUES (27,'اللام ألف','28','0','0');
+INSERT INTO [t9622] VALUES (28,'الياء','29','0','0');
