bfcf890d782f3f468057928cd3c4ddb20bd92356
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:01 +0700

    Upload SQL chunk: t26876_chunk_0000.sql

---
+++ sql_chunks/t26876_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26876
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26876" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26876
+INSERT INTO [t26876] VALUES (1,'الغلاف','1','0','0');
