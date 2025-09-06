1a848a73370907a0fddd187028af01248abda59b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:25 +0700

    Upload SQL chunk: t21542_chunk_0000.sql

---
+++ sql_chunks/t21542_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t21542
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t21542" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21542
+INSERT INTO [t21542] VALUES (1,'- حرف الهمزة.','3','0','0');
+INSERT INTO [t21542] VALUES (2,'- حرف الباء.','50','0','0');
+INSERT INTO [t21542] VALUES (3,'- حرف التاء.','55','0','0');
+INSERT INTO [t21542] VALUES (4,'- حرف الجيم.','60','0','0');
+INSERT INTO [t21542] VALUES (5,'- حرف الحاء.','63','0','0');
+INSERT INTO [t21542] VALUES (6,'- حرف الخاء.','69','0','0');
+INSERT INTO [t21542] VALUES (7,'- حرف الدال.','75','0','0');
+INSERT INTO [t21542] VALUES (8,'- حرف الذال.','79','0','0');
+INSERT INTO [t21542] VALUES (9,'- حرف الراء.','80','0','0');
+INSERT INTO [t21542] VALUES (10,'- حرف الزاي.','84','0','0');
+INSERT INTO [t21542] VALUES (11,'- حرف السين.','86','0','0');
+INSERT INTO [t21542] VALUES (12,'- حرف الشين.','90','0','0');
+INSERT INTO [t21542] VALUES (13,'- حرف الصاد.','94','0','0');
+INSERT INTO [t21542] VALUES (14,'- حرف الطاء.','97','0','0');
+INSERT INTO [t21542] VALUES (15,'- حرف الظاء.','100','0','0');
+INSERT INTO [t21542] VALUES (16,'- حرف العين.','102','0','0');
+INSERT INTO [t21542] VALUES (17,'- حرف الغين.','108','0','0');
+INSERT INTO [t21542] VALUES (18,'- حرف الفاء.','110','0','0');
+INSERT INTO [t21542] VALUES (19,'- حرف القاف.','112','0','0');
+INSERT INTO [t21542] VALUES (20,'- حرف الكاف.','115','0','0');
+INSERT INTO [t21542] VALUES (21,'- حرف اللام.','121','0','0');
+INSERT INTO [t21542] VALUES (22,'- حرف الميم.','126','0','0');
+INSERT INTO [t21542] VALUES (23,'- حرف النون.','147','0','0');
+INSERT INTO [t21542] VALUES (24,'- حرف الهاء.','151','0','0');
+INSERT INTO [t21542] VALUES (25,'- حرف الواو.','152','0','0');
+INSERT INTO [t21542] VALUES (26,'- حرف لا.','156','0','0');
+INSERT INTO [t21542] VALUES (27,'- حرف الياء.','162','0','0');
+INSERT INTO [t21542] VALUES (28,'- فصل في أشياء لم تدخل في الحروف.','164','0','0');
+INSERT INTO [t21542] VALUES (29,'- خاتمة.','176','0','0');
+INSERT INTO [t21542] VALUES (30,'خاتمة المؤلف [لكتاب الدرر المنتثرة]','176','0','0');
