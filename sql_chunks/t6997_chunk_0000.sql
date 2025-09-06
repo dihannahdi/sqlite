e194700ac0c281b2c821398a1cef3617c2f2958f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:39 +0700

    Upload SQL chunk: t6997_chunk_0000.sql

---
+++ sql_chunks/t6997_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t6997
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t6997" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6997
+INSERT INTO [t6997] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6997] VALUES (2,'اللامات','1','0','0');
+INSERT INTO [t6997] VALUES (3,'الألفات','3','0','0');
+INSERT INTO [t6997] VALUES (4,'الهاءات','5','0','0');
+INSERT INTO [t6997] VALUES (5,'الياءات','7','0','0');
+INSERT INTO [t6997] VALUES (6,'النونات','10','0','0');
+INSERT INTO [t6997] VALUES (7,'التاءات','12','0','0');
+INSERT INTO [t6997] VALUES (8,'وجوه ما','15','0','0');
+INSERT INTO [t6997] VALUES (9,'من','20','0','0');
+INSERT INTO [t6997] VALUES (10,'أي','23','0','0');
+INSERT INTO [t6997] VALUES (11,'أن المخففة','25','0','0');
+INSERT INTO [t6997] VALUES (12,'إن','27','0','0');
+INSERT INTO [t6997] VALUES (13,'حتى','28','0','0');
+INSERT INTO [t6997] VALUES (14,'من','29','0','0');
+INSERT INTO [t6997] VALUES (15,'لام الإضافة','30','0','0');
+INSERT INTO [t6997] VALUES (16,'رويد','31','0','0');
+INSERT INTO [t6997] VALUES (17,'تصرف الحروف','32','0','0');
+INSERT INTO [t6997] VALUES (18,'الخبر','33','0','0');
+INSERT INTO [t6997] VALUES (19,'الأسماء التي تعمل عمل الفعل','34','0','0');
+INSERT INTO [t6997] VALUES (20,'حروف الزيادة','35','0','0');
+INSERT INTO [t6997] VALUES (21,'الفرق بين أما وإما','36','0','0');
+INSERT INTO [t6997] VALUES (22,'الفرق بين إن وأن','37','0','0');
+INSERT INTO [t6997] VALUES (23,'الفرق بين أم وأو','38','0','0');
+INSERT INTO [t6997] VALUES (24,'الفرق بين لو وإن','39','0','0');
+INSERT INTO [t6997] VALUES (25,'الفرق بين إن وأن','40','0','0');
+INSERT INTO [t6997] VALUES (26,'الحدود','41','0','0');
+INSERT INTO [t6997] VALUES (27,'بسم الله الرحمن الرحيم','42','0','0');
+INSERT INTO [t6997] VALUES (28,'باب الحدود','43','0','0');
+INSERT INTO [t6997] VALUES (29,'باب حدود الموصولات','52','0','0');
