68b6eeb90cbd862a32e0fbfa405c1d6ed3778662
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:07 +0700

    Upload SQL chunk: t29413_chunk_0000.sql

---
+++ sql_chunks/t29413_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t29413
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t29413" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29413
+INSERT INTO [t29413] VALUES (1,'الحديث الأول','2','0','0');
+INSERT INTO [t29413] VALUES (2,'الحديث الثاني','4','0','0');
+INSERT INTO [t29413] VALUES (3,'الحديث الثالث','6','0','0');
+INSERT INTO [t29413] VALUES (4,'الحديث الرابع','8','0','0');
