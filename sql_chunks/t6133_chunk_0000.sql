8f283c04b5b8ef412ac80829de73f0b0de2c8d10
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:25 +0700

    Upload SQL chunk: t6133_chunk_0000.sql

---
+++ sql_chunks/t6133_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t6133
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t6133" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6133
+INSERT INTO [t6133] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t6133] VALUES (2,'فصل في اللباس','89','0','0');
+INSERT INTO [t6133] VALUES (3,'فصل زكاة الأموال الظاهرة والباطنة','105','0','0');
+INSERT INTO [t6133] VALUES (4,'فصل في الخصائص','311','0','0');
+INSERT INTO [t6133] VALUES (5,'فصل يستحب للمرأة خضب يديها بالحناء','313','0','0');
+INSERT INTO [t6133] VALUES (6,'فصل يستحب قبول الخطبة للمحرم والمحرمة','313','0','0');
+INSERT INTO [t6133] VALUES (7,'فوائد','470','0','0');
