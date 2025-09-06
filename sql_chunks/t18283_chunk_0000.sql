345253db8dd8db4b24d3561ca1659552262f0485
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:33:48 +0700

    Upload SQL chunk: t18283_chunk_0000.sql

---
+++ sql_chunks/t18283_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t18283
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t18283" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t18283
+INSERT INTO [t18283] VALUES (1,'هذا باب ذكر فيه ما فات سيبويه من أبنية كلام العرب','1','0','0');
+INSERT INTO [t18283] VALUES (2,'وذكر أبو إسحاق إبراهيم بن السري الزجاج','1','0','0');
+INSERT INTO [t18283] VALUES (3,'ثم ذكر أبو بكر محمد بن السري','2','0','0');
+INSERT INTO [t18283] VALUES (4,'أكثر ما ذكره أبو بكر غير داخل على سبيويه','6','0','0');
