117c5a197fb068014ad5ef1c802904569a9c8fd3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:22 +0700

    Upload SQL chunk: t13152_chunk_0000.sql

---
+++ sql_chunks/t13152_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t13152
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t13152" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13152
+INSERT INTO [t13152] VALUES (1,'باب مداراة الناس والصبر على أذاهم','1','0','0');
+INSERT INTO [t13152] VALUES (2,'باب التودد إلى الناس','32','0','0');
+INSERT INTO [t13152] VALUES (3,'باب المداراة بطلاقة الوجه وحسن البشر','56','0','0');
+INSERT INTO [t13152] VALUES (4,'باب جميل المعاشرة بحسن الخلق','78','0','0');
+INSERT INTO [t13152] VALUES (5,'باب ذم سوء الخلق','95','0','0');
+INSERT INTO [t13152] VALUES (6,'باب المداراة بلين الجانب وطيب الكلام','102','0','0');
+INSERT INTO [t13152] VALUES (7,'باب الحذر من الناس اتقاء شرهم والمداراة لهم','119','0','0');
+INSERT INTO [t13152] VALUES (8,'باب اعتزال الشر وأهله','144','0','0');
+INSERT INTO [t13152] VALUES (9,'باب الإصلاح بين الناس','155','0','0');
+INSERT INTO [t13152] VALUES (10,'باب مداراة الرجل زوجته وحسن معاشرته إياها','161','0','0');
+INSERT INTO [t13152] VALUES (11,'باب مداراة المرأة لزوجها وحسن معاشرتها إياه','183','0','0');
