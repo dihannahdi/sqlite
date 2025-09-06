56c30f011117b90a30bc8ff0038c1348b605a132
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:57 +0700

    Upload SQL chunk: t21647_chunk_0000.sql

---
+++ sql_chunks/t21647_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21647
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21647" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21647
+INSERT INTO [t21647] VALUES (1,'فضائل معاوية','2','0','0');
