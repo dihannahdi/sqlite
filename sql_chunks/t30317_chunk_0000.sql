a46f7983f4959885a59e97eb01e0eb7e744b1f4f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:25 +0700

    Upload SQL chunk: t30317_chunk_0000.sql

---
+++ sql_chunks/t30317_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30317
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30317" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30317
+INSERT INTO [t30317] VALUES (1,'جزء فيه فضل يوم التروية وعرفة','1','0','0');
