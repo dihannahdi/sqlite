d50ab378eaddb1cda7ec3c9f15925aeaf5256f68
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:24 +0700

    Upload SQL chunk: t741_chunk_0000.sql

---
+++ sql_chunks/t741_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t741
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t741" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t741
+INSERT INTO [t741] VALUES (1,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t741] VALUES (2,'وصية هود النبي عليه السلام','2','0','0');
+INSERT INTO [t741] VALUES (3,'وصية قحطان بن هود','2','0','0');
+INSERT INTO [t741] VALUES (4,'وصية يعرب بن قحطان','3','0','0');
+INSERT INTO [t741] VALUES (5,'وصية يشجب بن يعرب','3','0','0');
+INSERT INTO [t741] VALUES (6,'وصية حمير بن سبأ','5','0','0');
+INSERT INTO [t741] VALUES (7,'وصية زهير بن أيمن','6','0','0');
+INSERT INTO [t741] VALUES (8,'وصية عريب بن زهير','7','0','0');
+INSERT INTO [t741] VALUES (9,'وصية تبع بن عمرو','10','0','0');
+INSERT INTO [t741] VALUES (10,'وصية يزيد بن هاشم','15','0','0');
+INSERT INTO [t741] VALUES (11,'وصية كهلان بن سبأ','18','0','0');
+INSERT INTO [t741] VALUES (12,'وصية زيد بن كهلان','20','0','0');
+INSERT INTO [t741] VALUES (13,'وصية مازن بن الأزد','22','0','0');
+INSERT INTO [t741] VALUES (14,'وصية ثعلبة بن مازن','22','0','0');
+INSERT INTO [t741] VALUES (15,'وصية ماء السماء','24','0','0');
+INSERT INTO [t741] VALUES (16,'وصية عمرو بن عامر','29','0','0');
+INSERT INTO [t741] VALUES (17,'وصية أفصى بن حارثة','31','0','0');
+INSERT INTO [t741] VALUES (18,'وصية عمرو بن لحي الخزاعي','32','0','0');
+INSERT INTO [t741] VALUES (19,'وصية جفنة بن ثعلبة','32','0','0');
+INSERT INTO [t741] VALUES (20,'وصية الحارث بن ثعلبة بن جفنة','33','0','0');
+INSERT INTO [t741] VALUES (21,'وصية عمرو بن الحارث','34','0','0');
+INSERT INTO [t741] VALUES (22,'وصية كندة','36','0','0');
+INSERT INTO [t741] VALUES (23,'وصية واثلة بن كندة','37','0','0');
+INSERT INTO [t741] VALUES (24,'وصية معاوية الأكرمين','37','0','0');
+INSERT INTO [t741] VALUES (25,'وصية عمرو المفضور','37','0','0');
+INSERT INTO [t741] VALUES (26,'وصية معديكرب','38','0','0');
+INSERT INTO [t741] VALUES (27,'وصية همدان بن أوسلة','40','0','0');
+INSERT INTO [t741] VALUES (28,'وصية جشم بن حبران','40','0','0');
+INSERT INTO [t741] VALUES (29,'وصية أدد بن مالك','41','0','0');
+INSERT INTO [t741] VALUES (30,'وصية أود بن مالك','43','0','0');
+INSERT INTO [t741] VALUES (31,'وصية مراد بن سعد','43','0','0');
+INSERT INTO [t741] VALUES (32,'وصية الحارث بن كعب','43','0','0');
