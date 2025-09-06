4f2f7e0af9f0e51ba6d587af7a4702bab44670cf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:17 +0700

    Upload SQL chunk: t9015_chunk_0000.sql

---
+++ sql_chunks/t9015_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t9015
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t9015" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9015
+INSERT INTO [t9015] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t9015] VALUES (2,'القرآن يخاطب الجن كما يخاطب الإنس','5','0','0');
+INSERT INTO [t9015] VALUES (3,'السنة الصحيحة تثبت وجود الجن','9','0','0');
+INSERT INTO [t9015] VALUES (4,'شطط يقود إلى شطط','11','0','0');
