1608c03f8ca63ee596377dea721a450d32eead16
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:54 +0700

    Upload SQL chunk: t12704_chunk_0000.sql

---
+++ sql_chunks/t12704_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t12704
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t12704" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12704
+INSERT INTO [t12704] VALUES (1,'ذكر تقريب يوم القيامة','1','0','0');
+INSERT INTO [t12704] VALUES (2,'ذكر الصور','47','0','0');
+INSERT INTO [t12704] VALUES (3,'ذكر تبديل الأرض غير الأرض','65','0','0');
+INSERT INTO [t12704] VALUES (4,'ذكر البعث والنشور','73','0','0');
+INSERT INTO [t12704] VALUES (5,'ذكر الحساب والعرض والقصاص','178','0','0');
+INSERT INTO [t12704] VALUES (6,'ذكر الموقف','196','0','0');
+INSERT INTO [t12704] VALUES (7,'ذكر الحشر','229','0','0');
