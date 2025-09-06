34d1af68a832f3354dda30e8380198f565d7c664
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:30:39 +0700

    Upload SQL chunk: t17660_chunk_0000.sql

---
+++ sql_chunks/t17660_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t17660
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t17660" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17660
+INSERT INTO [t17660] VALUES (1,'مقدمة المحقق','5','0','0');
+INSERT INTO [t17660] VALUES (2,'وصف المخطوطة','6','1','0');
+INSERT INTO [t17660] VALUES (3,'ترجمة الناظم الشيخ تقي الدين السبكي','8','1','0');
+INSERT INTO [t17660] VALUES (4,'ترجمة ابن الناظم محمد بن علي بن عبد الكافي السبكي','9','1','0');
+INSERT INTO [t17660] VALUES (5,'أول الوصية المنظومة','13','0','0');
