8f5d04c949054a853fb08d88836bbf2346d518c4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:50 +0700

    Upload SQL chunk: t5955_chunk_0000.sql

---
+++ sql_chunks/t5955_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5955
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5955" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5955
+INSERT INTO [t5955] VALUES (1,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t5955] VALUES (2,'المدرج إلى المدرج','2','0','0');
