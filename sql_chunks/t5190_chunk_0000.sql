b84a30241247117274ed646370e4388d5e105d3f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:57 +0700

    Upload SQL chunk: t5190_chunk_0000.sql

---
+++ sql_chunks/t5190_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t5190
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t5190" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5190
+INSERT INTO [t5190] VALUES (1,'موسوعات الفقه الأسلامي','1','0','0');
+INSERT INTO [t5190] VALUES (2,'المعاجم','5','0','0');
+INSERT INTO [t5190] VALUES (3,'معاجم الفقه','6','0','0');
+INSERT INTO [t5190] VALUES (4,'معجم الحديث','7','0','0');
+INSERT INTO [t5190] VALUES (5,'التعليق على محاضرة السيد المنتصر لسماحة الشيخ عبد العزيز بن باز','10','0','0');
