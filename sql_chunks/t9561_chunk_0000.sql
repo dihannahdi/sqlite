45725540bd4027e4e4c45c0a9d728866f5b242a3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:45 +0700

    Upload SQL chunk: t9561_chunk_0000.sql

---
+++ sql_chunks/t9561_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t9561
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t9561" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9561
+INSERT INTO [t9561] VALUES (1,'فضائل شهر رمضان','1','0','0');
+INSERT INTO [t9561] VALUES (2,'في السحور','40','0','0');
+INSERT INTO [t9561] VALUES (3,'في ليلة القدر','44','0','0');
