168448a81fa1ccf097ffa71c6f28de197b3784b5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:40 +0700

    Upload SQL chunk: t6311_chunk_0000.sql

---
+++ sql_chunks/t6311_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t6311
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t6311" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6311
+INSERT INTO [t6311] VALUES (1,'فصل','11','0','0');
+INSERT INTO [t6311] VALUES (2,'الفصل الأول','13','0','0');
+INSERT INTO [t6311] VALUES (3,'الفصل الثاني في سنة رسول الله','23','0','0');
+INSERT INTO [t6311] VALUES (4,'الفصل الثالث','26','0','0');
+INSERT INTO [t6311] VALUES (5,'الفصل الرابع','29','0','0');
