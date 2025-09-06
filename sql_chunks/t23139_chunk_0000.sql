4fa2158f3e223116b0776abc2c2d02f036f39329
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:03 +0700

    Upload SQL chunk: t23139_chunk_0000.sql

---
+++ sql_chunks/t23139_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t23139
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t23139" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23139
+INSERT INTO [t23139] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t23139] VALUES (2,'أدب الإسلام للطفل فطرة وعبادة','4','0','0');
+INSERT INTO [t23139] VALUES (3,'مدخل','4','2','0');
+INSERT INTO [t23139] VALUES (4,'تلاوة الأطفال للقرآن وسماعه تأديب وتربية وتعليم','11','2','0');
+INSERT INTO [t23139] VALUES (5,'وصية الرسول صلى الله عليه وسلم للغلام عبد الله بن عباس','15','2','0');
+INSERT INTO [t23139] VALUES (6,'وصية لقمان لابنه','17','2','0');
+INSERT INTO [t23139] VALUES (7,'أدب الإسلام في استئذان الأطفال','19','2','0');
+INSERT INTO [t23139] VALUES (8,'أدب القصة القرآنية والنبوية للأطفال','21','2','0');
+INSERT INTO [t23139] VALUES (9,'مراحل الطفولة في الأدب القرآني والنبوي','27','0','0');
+INSERT INTO [t23139] VALUES (10,'أثر القرآن والسنة في تأديب الطفل شعرا ونثرا','33','0','0');
+INSERT INTO [t23139] VALUES (11,'من المصادر والمراجع','38','0','0');
