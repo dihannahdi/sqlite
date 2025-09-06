9253a248031de6b0ec5065fec4ae0dfd28680388
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:24 +0700

    Upload SQL chunk: t9970_chunk_0000.sql

---
+++ sql_chunks/t9970_chunk_0000.sql
@@ -0,0 +1,55 @@
+-- Table: t9970
+-- Chunk: 1
+-- Rows: 48
+
+CREATE TABLE "t9970" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9970
+INSERT INTO [t9970] VALUES (1,'مقدمات','1','0','0');
+INSERT INTO [t9970] VALUES (2,'ملابسات هذا البحث','1','1','0');
+INSERT INTO [t9970] VALUES (3,'تقديم','3','1','0');
+INSERT INTO [t9970] VALUES (4,'مقدمة','7','1','0');
+INSERT INTO [t9970] VALUES (5,'الكلام واللغة','24','0','0');
+INSERT INTO [t9970] VALUES (6,'الأصوات','36','0','0');
+INSERT INTO [t9970] VALUES (7,'النظام الصوتي: علم الصوتيات','54','0','0');
+INSERT INTO [t9970] VALUES (8,'النظام الصرفي','67','0','0');
+INSERT INTO [t9970] VALUES (9,'أولا: كيف يتألف هذا النظام','67','8','0');
+INSERT INTO [t9970] VALUES (10,'ثانيا: أقسام الكلم','71','8','0');
+INSERT INTO [t9970] VALUES (11,'مدخل','71','10','0');
+INSERT INTO [t9970] VALUES (12,'الاسم','76','10','0');
+INSERT INTO [t9970] VALUES (13,'الصفة','85','10','0');
+INSERT INTO [t9970] VALUES (14,'الفعل','90','10','0');
+INSERT INTO [t9970] VALUES (15,'الضمير','95','10','0');
+INSERT INTO [t9970] VALUES (16,'الخوالف','100','10','0');
+INSERT INTO [t9970] VALUES (17,'الظرف','106','10','0');
+INSERT INTO [t9970] VALUES (18,'الأداة','110','10','0');
+INSERT INTO [t9970] VALUES (19,'ثالثا: المبنى','118','8','0');
+INSERT INTO [t9970] VALUES (20,'مدخل','118','19','0');
+INSERT INTO [t9970] VALUES (21,'الصيغة','122','19','0');
+INSERT INTO [t9970] VALUES (22,'الإلصاق','140','19','0');
+INSERT INTO [t9970] VALUES (23,'الزيادة','144','19','0');
+INSERT INTO [t9970] VALUES (24,'تعدد المعنى الوظيفي للمبنى','147','19','0');
+INSERT INTO [t9970] VALUES (25,'الاشتقاق','151','19','0');
+INSERT INTO [t9970] VALUES (26,'النبر','155','19','0');
+INSERT INTO [t9970] VALUES (27,'النظام النحوي','161','0','0');
+INSERT INTO [t9970] VALUES (28,'مدخل','161','27','0');
+INSERT INTO [t9970] VALUES (29,'قرائن التعليق','173','27','0');
+INSERT INTO [t9970] VALUES (30,'الزمن والجهة','220','27','0');
+INSERT INTO [t9970] VALUES (31,'الظواهر السياقية','240','0','0');
+INSERT INTO [t9970] VALUES (32,'مدخل','240','31','0');
+INSERT INTO [t9970] VALUES (33,'ظاهرة التأليف','244','31','0');
+INSERT INTO [t9970] VALUES (34,'ظاهرة الوقف','250','31','0');
+INSERT INTO [t9970] VALUES (35,'المناسبة','253','31','0');
+INSERT INTO [t9970] VALUES (36,'الإعلال والإبدال','255','31','0');
+INSERT INTO [t9970] VALUES (37,'التوصل','258','31','0');
+INSERT INTO [t9970] VALUES (38,'الإدغام','261','31','0');
+INSERT INTO [t9970] VALUES (39,'التخلص','277','31','0');
+INSERT INTO [t9970] VALUES (40,'الحذف','280','31','0');
+INSERT INTO [t9970] VALUES (41,'الإسكان','282','31','0');
+INSERT INTO [t9970] VALUES (42,'الكمية','284','31','0');
+INSERT INTO [t9970] VALUES (43,'الإشباع والإضعاف','286','31','0');
+INSERT INTO [t9970] VALUES (44,'النبر','289','31','0');
+INSERT INTO [t9970] VALUES (45,'التنغيم','294','31','0');
+INSERT INTO [t9970] VALUES (46,'المعجم','297','0','0');
+INSERT INTO [t9970] VALUES (47,'الدلالة','320','0','0');
+INSERT INTO [t9970] VALUES (48,'الفهرس','358','0','0');
