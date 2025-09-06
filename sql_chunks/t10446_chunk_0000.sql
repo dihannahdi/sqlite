bc12182b32840377e2b5f13b383ed299281b529a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:58 +0700

    Upload SQL chunk: t10446_chunk_0000.sql

---
+++ sql_chunks/t10446_chunk_0000.sql
@@ -0,0 +1,59 @@
+-- Table: t10446
+-- Chunk: 1
+-- Rows: 52
+
+CREATE TABLE "t10446" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10446
+INSERT INTO [t10446] VALUES (1,'تقديم العلامة الشيخ ابن جبرين حفظه الله','2','0','0');
+INSERT INTO [t10446] VALUES (2,'المقدمة','4','0','0');
+INSERT INTO [t10446] VALUES (3,'إرشادات عامة تراعى عند الرقية:','9','0','0');
+INSERT INTO [t10446] VALUES (4,'الرقية القرآنية من العين والحسد والسحر','11','0','0');
+INSERT INTO [t10446] VALUES (5,'سورة الفاتحة:','12','4','0');
+INSERT INTO [t10446] VALUES (6,'من سورة البقرة:','12','4','0');
+INSERT INTO [t10446] VALUES (7,'من سورة آل عمران:','22','4','0');
+INSERT INTO [t10446] VALUES (8,'من سورة النساء:','28','4','0');
+INSERT INTO [t10446] VALUES (9,'من سورة الأنعام:','30','4','0');
+INSERT INTO [t10446] VALUES (10,'من سورة الأعراف:','34','4','0');
+INSERT INTO [t10446] VALUES (11,'من سورة الأنفال:','36','4','0');
+INSERT INTO [t10446] VALUES (12,'من سورة التوبة:','37','4','0');
+INSERT INTO [t10446] VALUES (13,'من سورة يونس عليه السلام:','39','4','0');
+INSERT INTO [t10446] VALUES (14,'من سورة يوسف عليه السلام:','41','4','0');
+INSERT INTO [t10446] VALUES (15,'من سورة الرعد:','41','4','0');
+INSERT INTO [t10446] VALUES (16,'من سورة الحجر:','42','4','0');
+INSERT INTO [t10446] VALUES (17,'من سورة النحل:','44','4','0');
+INSERT INTO [t10446] VALUES (18,'من سورة الإسراء:','45','4','0');
+INSERT INTO [t10446] VALUES (19,'من سورة الكهف:','48','4','0');
+INSERT INTO [t10446] VALUES (20,'من سورة طه:','52','4','0');
+INSERT INTO [t10446] VALUES (21,'من سورة الأنبياء:','54','4','0');
+INSERT INTO [t10446] VALUES (22,'من سورة المؤمنون:','55','4','0');
+INSERT INTO [t10446] VALUES (23,'من سورة النور:','56','4','0');
+INSERT INTO [t10446] VALUES (24,'من سورة الفرقان:','58','4','0');
+INSERT INTO [t10446] VALUES (25,'من سورة الشعراء:','58','4','0');
+INSERT INTO [t10446] VALUES (26,'من سورة النمل:','60','4','0');
+INSERT INTO [t10446] VALUES (27,'من سورة القصص:','60','4','0');
+INSERT INTO [t10446] VALUES (28,'من سورة سبأ:','61','4','0');
+INSERT INTO [t10446] VALUES (29,'من سورة فاطر:','62','4','0');
+INSERT INTO [t10446] VALUES (30,'من سورة يس:','63','4','0');
+INSERT INTO [t10446] VALUES (31,'من سورة الصافات:','64','4','0');
+INSERT INTO [t10446] VALUES (32,'من سورة الزمر:','66','4','0');
+INSERT INTO [t10446] VALUES (33,'من سورة غافر:','68','4','0');
+INSERT INTO [t10446] VALUES (34,'من سورة فصلت:','68','4','0');
+INSERT INTO [t10446] VALUES (35,'من سورة الأحقاف:','69','4','0');
+INSERT INTO [t10446] VALUES (36,'من سورة الفتح:','70','4','0');
+INSERT INTO [t10446] VALUES (37,'من سورة القمر:','72','4','0');
+INSERT INTO [t10446] VALUES (38,'من سورة الحشر:','73','4','0');
+INSERT INTO [t10446] VALUES (39,'من سورة الطلاق:','75','4','0');
+INSERT INTO [t10446] VALUES (40,'من سورة الملك:','75','4','0');
+INSERT INTO [t10446] VALUES (41,'من سورة القلم:','76','4','0');
+INSERT INTO [t10446] VALUES (42,'من سورة الحاقة:','76','4','0');
+INSERT INTO [t10446] VALUES (43,'من سورة الجن:','77','4','0');
+INSERT INTO [t10446] VALUES (44,'من سورة القيامة:','79','4','0');
+INSERT INTO [t10446] VALUES (45,'سورة الشرح:','79','4','0');
+INSERT INTO [t10446] VALUES (46,'سورة العصر:','80','4','0');
+INSERT INTO [t10446] VALUES (47,'سورة قريش:','80','4','0');
+INSERT INTO [t10446] VALUES (48,'سورة الكافرون:','81','4','0');
+INSERT INTO [t10446] VALUES (49,'سورة الإخلاص:','81','4','0');
+INSERT INTO [t10446] VALUES (50,'سورة الفلق:','82','4','0');
+INSERT INTO [t10446] VALUES (51,'سورة الناس:','82','4','0');
+INSERT INTO [t10446] VALUES (52,'خاتمة','84','0','0');
