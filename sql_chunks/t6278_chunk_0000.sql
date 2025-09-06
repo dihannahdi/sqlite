c921d0a65a79643a253d82945a2c0b404205365d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:23 +0700

    Upload SQL chunk: t6278_chunk_0000.sql

---
+++ sql_chunks/t6278_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t6278
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t6278" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6278
+INSERT INTO [t6278] VALUES (1,'الفصل الأول','10','0','0');
+INSERT INTO [t6278] VALUES (2,'الفصل الثاني','12','0','0');
+INSERT INTO [t6278] VALUES (3,'الفصل الثالث','30','0','0');
+INSERT INTO [t6278] VALUES (4,'الفصل الرابع','51','0','0');
+INSERT INTO [t6278] VALUES (5,'الفصل الخامس','63','0','0');
+INSERT INTO [t6278] VALUES (6,'الفصل السادس','67','0','0');
+INSERT INTO [t6278] VALUES (7,'الفصل السابع','82','0','0');
+INSERT INTO [t6278] VALUES (8,'الفصل الثامن','140','0','0');
+INSERT INTO [t6278] VALUES (9,'الفصل التاسع','158','0','0');
+INSERT INTO [t6278] VALUES (10,'الفصل العاشر','197','0','0');
