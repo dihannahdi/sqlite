464e67bd3d039fa74406d5a58ac946ea3de859d4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:33 +0700

    Upload SQL chunk: t6752_chunk_0000.sql

---
+++ sql_chunks/t6752_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6752
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6752" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6752
+INSERT INTO [t6752] VALUES (1,'التنبيهات الواردة على الجزء الأول','2','0','0');
+INSERT INTO [t6752] VALUES (2,'التنبيهات الواردة على الجزء الثاني','73','0','0');
