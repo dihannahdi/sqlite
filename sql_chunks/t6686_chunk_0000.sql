6a24c92bb3dd33e11eb0c991149db1dcf2ce2d59
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:55 +0700

    Upload SQL chunk: t6686_chunk_0000.sql

---
+++ sql_chunks/t6686_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t6686
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t6686" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6686
+INSERT INTO [t6686] VALUES (1,'طبقات الصوفية','1','0','0');
+INSERT INTO [t6686] VALUES (2,'الطبقة الأولى','3','1','0');
+INSERT INTO [t6686] VALUES (3,'الطبقة الثانية من أئمة الصوفية','110','1','0');
+INSERT INTO [t6686] VALUES (4,'الطبقة الثالثة من أئمة الصوفية','184','1','0');
+INSERT INTO [t6686] VALUES (5,'الطبقة الرابعة من أئمة الصوفية','238','1','0');
+INSERT INTO [t6686] VALUES (6,'الطبقة الخامسة من ائمة الصوفية','301','1','0');
+INSERT INTO [t6686] VALUES (7,'ذكر النسوة المتعبدات الصوفيات','367','0','0');
