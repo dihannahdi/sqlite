ec2329f8a6ba8f27305f8c99f48f6e822de9d515
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:00:33 +0700

    Upload SQL chunk: t96966_chunk_0000.sql

---
+++ sql_chunks/t96966_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t96966
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t96966" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96966
+INSERT INTO [t96966] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t96966] VALUES (2,'باب الهمزة','7','0','0');
+INSERT INTO [t96966] VALUES (3,'باب الباء','20','0','0');
+INSERT INTO [t96966] VALUES (4,'باب التاء','28','0','0');
+INSERT INTO [t96966] VALUES (5,'باب الثاء','32','0','0');
+INSERT INTO [t96966] VALUES (6,'باب الجيم','35','0','0');
+INSERT INTO [t96966] VALUES (7,'باب الحاء','43','0','0');
+INSERT INTO [t96966] VALUES (8,'باب الخاء','51','0','0');
+INSERT INTO [t96966] VALUES (9,'باب الدال','56','0','0');
+INSERT INTO [t96966] VALUES (10,'باب الذال','60','0','0');
+INSERT INTO [t96966] VALUES (11,'باب الراء','62','0','0');
+INSERT INTO [t96966] VALUES (12,'باب الزاي','66','0','0');
+INSERT INTO [t96966] VALUES (13,'باب السين','69','0','0');
+INSERT INTO [t96966] VALUES (14,'باب الشين','76','0','0');
+INSERT INTO [t96966] VALUES (15,'باب الصاد','78','0','0');
+INSERT INTO [t96966] VALUES (16,'باب الضاد','80','0','0');
+INSERT INTO [t96966] VALUES (17,'باب الطاء','82','0','0');
+INSERT INTO [t96966] VALUES (18,'باب الظاء','84','0','0');
+INSERT INTO [t96966] VALUES (19,'باب العين','85','0','0');
+INSERT INTO [t96966] VALUES (20,'حرف الغين','94','0','0');
+INSERT INTO [t96966] VALUES (21,'حرف الفاء','98','0','0');
+INSERT INTO [t96966] VALUES (22,'حرف القاف','100','0','0');
+INSERT INTO [t96966] VALUES (23,'باب الكاف','105','0','0');
+INSERT INTO [t96966] VALUES (24,'باب اللام','108','0','0');
+INSERT INTO [t96966] VALUES (25,'باب الميم','109','0','0');
+INSERT INTO [t96966] VALUES (26,'باب النون','116','0','0');
+INSERT INTO [t96966] VALUES (27,'باب الواو','120','0','0');
+INSERT INTO [t96966] VALUES (28,'باب الهاء','122','0','0');
+INSERT INTO [t96966] VALUES (29,'باب الياء','124','0','0');
