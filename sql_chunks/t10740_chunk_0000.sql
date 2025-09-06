d01b9200ce74f280bd1e6e5186dabd834da96788
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:06 +0700

    Upload SQL chunk: t10740_chunk_0000.sql

---
+++ sql_chunks/t10740_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t10740
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t10740" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10740
+INSERT INTO [t10740] VALUES (1,'بسم الله الرحمن الرحيم صلى الله على سيدنا محمد وعلى آله وسلم تسليما','1','0','0');
+INSERT INTO [t10740] VALUES (2,'ومن النساء المجيزات','133','0','0');
+INSERT INTO [t10740] VALUES (3,'تمت والحمد لله رب العالمين والصلاة التامة على سيدنا محمد خاتم النبيين وعلى آله وصحبه أجمعين','137','0','0');
+INSERT INTO [t10740] VALUES (4,'الجزء الثاني من برنامج الوادي آشي','138','0','0');
+INSERT INTO [t10740] VALUES (5,'بسم الله الرحمن الرحيم صلى الله علي سيدنا محمد وعلى آله وسلم تسليما','139','0','0');
+INSERT INTO [t10740] VALUES (6,'هذا ذكر ما حضرني ذكره مما شرح في أول التأليف سره','139','0','0');
+INSERT INTO [t10740] VALUES (7,'انتهى والحمد لله عز وجل وصلاته وسلامه على سيدنا ومولانا محمد وعلى آله وصحبه وسلم تسليما','285','0','0');
+INSERT INTO [t10740] VALUES (8,'فهرس أسماء الكتب','286','0','0');
+INSERT INTO [t10740] VALUES (9,'أ','286','0','0');
+INSERT INTO [t10740] VALUES (10,'ب','290','0','0');
+INSERT INTO [t10740] VALUES (11,'ت','291','0','0');
+INSERT INTO [t10740] VALUES (12,'ث','292','0','0');
+INSERT INTO [t10740] VALUES (13,'ج','293','0','0');
+INSERT INTO [t10740] VALUES (14,'ح','294','0','0');
+INSERT INTO [t10740] VALUES (15,'خ','295','0','0');
+INSERT INTO [t10740] VALUES (16,'د','295','0','0');
+INSERT INTO [t10740] VALUES (17,'ذ','296','0','0');
+INSERT INTO [t10740] VALUES (18,'ر','296','0','0');
+INSERT INTO [t10740] VALUES (19,'ز','296','0','0');
+INSERT INTO [t10740] VALUES (20,'س','297','0','0');
+INSERT INTO [t10740] VALUES (21,'ش','297','0','0');
+INSERT INTO [t10740] VALUES (22,'ص','298','0','0');
+INSERT INTO [t10740] VALUES (23,'ض','298','0','0');
+INSERT INTO [t10740] VALUES (24,'ط','298','0','0');
+INSERT INTO [t10740] VALUES (25,'ع','298','0','0');
+INSERT INTO [t10740] VALUES (26,'غ','299','0','0');
+INSERT INTO [t10740] VALUES (27,'ف','299','0','0');
+INSERT INTO [t10740] VALUES (28,'ق','300','0','0');
+INSERT INTO [t10740] VALUES (29,'ك','301','0','0');
+INSERT INTO [t10740] VALUES (30,'م','301','0','0');
+INSERT INTO [t10740] VALUES (31,'ن','307','0','0');
+INSERT INTO [t10740] VALUES (32,'و','307','0','0');
