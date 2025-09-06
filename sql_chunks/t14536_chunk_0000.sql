56b86d70b283ced96a69457ac8e0544c052bf686
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:19:49 +0700

    Upload SQL chunk: t14536_chunk_0000.sql

---
+++ sql_chunks/t14536_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t14536
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t14536" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14536
+INSERT INTO [t14536] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t14536] VALUES (2,'تفسير سورة الفاتحة','4','0','0');
+INSERT INTO [t14536] VALUES (3,'تفسير سورة عم','10','0','0');
+INSERT INTO [t14536] VALUES (4,'تفسير سورة النازعات','22','0','0');
+INSERT INTO [t14536] VALUES (5,'تفسير سورة عبس','32','0','0');
+INSERT INTO [t14536] VALUES (6,'تفسير سورة التكوير','41','0','0');
+INSERT INTO [t14536] VALUES (7,'تفسير سورة الانفطار','48','0','0');
+INSERT INTO [t14536] VALUES (8,'تفسير سورة المطففين','53','0','0');
+INSERT INTO [t14536] VALUES (9,'تفسير سورة الانشقاق','62','0','0');
+INSERT INTO [t14536] VALUES (10,'تفسير سورة سورة البروج','68','0','0');
+INSERT INTO [t14536] VALUES (11,'تفسير سورة الطارق','75','0','0');
+INSERT INTO [t14536] VALUES (12,'تفسير سورة الأعلى','79','0','0');
+INSERT INTO [t14536] VALUES (13,'تفسير سورة الغاشية','84','0','0');
+INSERT INTO [t14536] VALUES (14,'تفسير سورة الفجر','90','0','0');
+INSERT INTO [t14536] VALUES (15,'تفسير سورة البلد','98','0','0');
+INSERT INTO [t14536] VALUES (16,'تفسير سورة الشمس','104','0','0');
+INSERT INTO [t14536] VALUES (17,'تفسير سورة الليل','108','0','0');
+INSERT INTO [t14536] VALUES (18,'تفسير سورة الضحى','113','0','0');
+INSERT INTO [t14536] VALUES (19,'تفسير سورة الشرح','117','0','0');
+INSERT INTO [t14536] VALUES (20,'تفسير سورة التين','119','0','0');
+INSERT INTO [t14536] VALUES (21,'تفسير سورة العلق','122','0','0');
+INSERT INTO [t14536] VALUES (22,'تفسير سورة القدر','126','0','0');
+INSERT INTO [t14536] VALUES (23,'تفسير سورة البينة','128','0','0');
+INSERT INTO [t14536] VALUES (24,'تفسير سورة الزلزلة','133','0','0');
+INSERT INTO [t14536] VALUES (25,'تفسير سورة العاديات','135','0','0');
+INSERT INTO [t14536] VALUES (26,'تفسير سورة القارعة','138','0','0');
+INSERT INTO [t14536] VALUES (27,'تفسير سورة التكاثر','141','0','0');
+INSERT INTO [t14536] VALUES (28,'تفسير سورة العصر','143','0','0');
+INSERT INTO [t14536] VALUES (29,'تفسير سورة الهمزة','145','0','0');
+INSERT INTO [t14536] VALUES (30,'تفسير سورة الفيل','148','0','0');
+INSERT INTO [t14536] VALUES (31,'تفسير سورة قريش','150','0','0');
+INSERT INTO [t14536] VALUES (32,'تفسير سورة الماعون','152','0','0');
+INSERT INTO [t14536] VALUES (33,'تفسير سورة الكوثر','154','0','0');
+INSERT INTO [t14536] VALUES (34,'تفسير سورة الكافرون','156','0','0');
+INSERT INTO [t14536] VALUES (35,'تفسير سورة النصر','158','0','0');
+INSERT INTO [t14536] VALUES (36,'تفسير سورة المسد','160','0','0');
+INSERT INTO [t14536] VALUES (37,'تفسير سورة الإخلاص','162','0','0');
+INSERT INTO [t14536] VALUES (38,'تفسير سورة الفلق','164','0','0');
+INSERT INTO [t14536] VALUES (39,'تفسير سورة الناس','167','0','0');
