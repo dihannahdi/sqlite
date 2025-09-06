57cf3c3623dd63bff839da64b4b21da6b0a6b33f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:56 +0700

    Upload SQL chunk: t1418_chunk_0000.sql

---
+++ sql_chunks/t1418_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t1418
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t1418" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1418
+INSERT INTO [t1418] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t1418] VALUES (2,'* هذه مقدمة إيضاحية كان لابد منها *','4','0','0');
+INSERT INTO [t1418] VALUES (3,'* شاهد من أهلها *','5','0','0');
+INSERT INTO [t1418] VALUES (4,'* أولا: أسفار العهد القديم:','6','0','0');
+INSERT INTO [t1418] VALUES (5,'* ثانيا: أسفار العهد الجديد:','19','0','0');
+INSERT INTO [t1418] VALUES (6,'* التناقض المقدس ... !!! *','24','0','0');
+INSERT INTO [t1418] VALUES (7,'* الفداء *','30','0','0');
+INSERT INTO [t1418] VALUES (8,'* يا رجال الفاتيكان هذا نداؤنا *','31','0','0');
