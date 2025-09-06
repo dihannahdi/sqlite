7f793988f7622ee5816fc669033ebf8e04cdb1ae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:56 +0700

    Upload SQL chunk: t518_chunk_0000.sql

---
+++ sql_chunks/t518_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t518
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t518" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t518
+INSERT INTO [t518] VALUES (1,'القسم الأول: أخطاء تصحيحية ونحوها','2','0','0');
+INSERT INTO [t518] VALUES (2,'القسم الثاني: أوهام للشارح تتعلق بضبط الكلمات أو إعرابها أو تفسيرها أو غير ذلك','6','0','0');
+INSERT INTO [t518] VALUES (3,'القسم الثالث: تعليقات','10','0','0');
