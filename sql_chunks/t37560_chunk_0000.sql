bc827bee24f2ee368963bc717e7b9ae36779cbdd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:14:00 +0700

    Upload SQL chunk: t37560_chunk_0000.sql

---
+++ sql_chunks/t37560_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t37560
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t37560" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37560
+INSERT INTO [t37560] VALUES (1,'بسم الله الرحمن الرحيم','5','0','0');
+INSERT INTO [t37560] VALUES (2,'نصنف أسماءهم في شبه ما يسمى ((طبقات))','5','0','0');
+INSERT INTO [t37560] VALUES (3,'أثر في تطور أفكار المجتمع الاسلامي منذ قرن','7','0','0');
