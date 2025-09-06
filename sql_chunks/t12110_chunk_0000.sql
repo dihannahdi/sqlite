c23db6fbdafe20ddd702fc4553f88dab4c4b9e39
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:05 +0700

    Upload SQL chunk: t12110_chunk_0000.sql

---
+++ sql_chunks/t12110_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t12110
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t12110" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12110
+INSERT INTO [t12110] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12110] VALUES (2,'ملخص بحث: نزول القرآن الكريم','2','0','0');
+INSERT INTO [t12110] VALUES (3,'النزول في اللغة:','4','0','0');
+INSERT INTO [t12110] VALUES (4,'النزول في القرآن الكريم:','5','0','0');
+INSERT INTO [t12110] VALUES (5,'الفرق بين الإنزال والتنزيل:','9','0','0');
+INSERT INTO [t12110] VALUES (6,'تنزلات القرآن الكريم:','13','0','0');
+INSERT INTO [t12110] VALUES (7,'القول الأول:','13','6','0');
+INSERT INTO [t12110] VALUES (8,'القول الثاني:','19','6','0');
+INSERT INTO [t12110] VALUES (9,'القول الثالث:','22','6','0');
+INSERT INTO [t12110] VALUES (10,'القول الرابع:','24','6','0');
+INSERT INTO [t12110] VALUES (11,'الترجيح:','27','6','0');
+INSERT INTO [t12110] VALUES (12,'حكمة نزول القرآن الكريم جملة:','36','0','0');
+INSERT INTO [t12110] VALUES (13,'وقت نزول القرآن الكريم:','38','0','0');
+INSERT INTO [t12110] VALUES (14,'مدة نزول القرآن الكريم:','41','0','0');
+INSERT INTO [t12110] VALUES (15,'يوم إنزال القرآن:','46','0','0');
+INSERT INTO [t12110] VALUES (16,'شهر إنزال القرآن الكريم:','47','0','0');
+INSERT INTO [t12110] VALUES (17,'مقدار التنزيل:','54','0','0');
