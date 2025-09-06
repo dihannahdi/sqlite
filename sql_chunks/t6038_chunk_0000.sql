9e778ed0c35ca8685040d7cc27fa93612f780e1d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:38 +0700

    Upload SQL chunk: t6038_chunk_0000.sql

---
+++ sql_chunks/t6038_chunk_0000.sql
@@ -0,0 +1,51 @@
+-- Table: t6038
+-- Chunk: 1
+-- Rows: 44
+
+CREATE TABLE "t6038" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6038
+INSERT INTO [t6038] VALUES (1,'كتاب العلل','1','0','0');
+INSERT INTO [t6038] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t6038] VALUES (3,'من روى عن زيد بن ثابت','13','0','0');
+INSERT INTO [t6038] VALUES (4,'قيس بن أبي حازم','15','0','0');
+INSERT INTO [t6038] VALUES (5,'الحسن البصري','17','0','0');
+INSERT INTO [t6038] VALUES (6,'محمد بن سيرين','26','0','0');
+INSERT INTO [t6038] VALUES (7,'إبراهيم النخعي','26','0','0');
+INSERT INTO [t6038] VALUES (8,'همام بن الحارث','27','0','0');
+INSERT INTO [t6038] VALUES (9,'مسروق','27','0','0');
+INSERT INTO [t6038] VALUES (10,'زياد بن علاقة','28','0','0');
+INSERT INTO [t6038] VALUES (11,'القاسم بن عبد الرحمن','29','0','0');
+INSERT INTO [t6038] VALUES (12,'سالم بن أبي الجعد','29','0','0');
+INSERT INTO [t6038] VALUES (13,'ابو عثمان النهدي','30','0','0');
+INSERT INTO [t6038] VALUES (14,'عطاء بن أبي رباح','32','0','0');
+INSERT INTO [t6038] VALUES (15,'حبيب بن ثابت','32','0','0');
+INSERT INTO [t6038] VALUES (16,'أبو راشد','32','0','0');
+INSERT INTO [t6038] VALUES (17,'ابو العباس','33','0','0');
+INSERT INTO [t6038] VALUES (18,'زياد بن علاقة','33','0','0');
+INSERT INTO [t6038] VALUES (19,'أبو رزين','33','0','0');
+INSERT INTO [t6038] VALUES (20,'أبو مريم الاسدي','33','0','0');
+INSERT INTO [t6038] VALUES (21,'نعيم بن حكيم','33','0','0');
+INSERT INTO [t6038] VALUES (22,'عطاء الشامي','34','0','0');
+INSERT INTO [t6038] VALUES (23,'يسير بن عمرو','34','0','0');
+INSERT INTO [t6038] VALUES (24,'القاسم بن ربيعة','35','0','0');
+INSERT INTO [t6038] VALUES (25,'أبو المتوكل','35','0','0');
+INSERT INTO [t6038] VALUES (26,'محمد ين قيس','35','0','0');
+INSERT INTO [t6038] VALUES (27,'عبد الله بن الحارث','36','0','0');
+INSERT INTO [t6038] VALUES (28,'حطان بن عبد الله الرقاشي','36','0','0');
+INSERT INTO [t6038] VALUES (29,'محمد بن زيد بن مهاجر','36','0','0');
+INSERT INTO [t6038] VALUES (30,'نعيم بن أبي الهند','36','0','0');
+INSERT INTO [t6038] VALUES (31,'يعقوب بن زيد بن طلحة','37','0','0');
+INSERT INTO [t6038] VALUES (32,'يزيد بن هرمز','37','0','0');
+INSERT INTO [t6038] VALUES (33,'سليمان بن قنة','37','0','0');
+INSERT INTO [t6038] VALUES (34,'اصحاب ثابت','38','0','0');
+INSERT INTO [t6038] VALUES (35,'سالم بن أبي الجعد','38','0','0');
+INSERT INTO [t6038] VALUES (36,'علل حديث من جعل على القضاء','39','0','0');
+INSERT INTO [t6038] VALUES (37,'وفيات بعض المحدثين','40','0','0');
+INSERT INTO [t6038] VALUES (38,'علل حديث إذا اكل أحدكم فليأكل بيمينه','41','0','0');
+INSERT INTO [t6038] VALUES (39,'علل حديث من صلى على جنازة','42','0','0');
+INSERT INTO [t6038] VALUES (40,'علل حديث منزلنا غدا إن شاء الله بخيف بني كنانة','42','0','0');
+INSERT INTO [t6038] VALUES (41,'علل حديث أن النبي رأى رجلا يدعو رافعا يديه','43','0','0');
+INSERT INTO [t6038] VALUES (42,'علل حديث أمرت أن أقاتل الناس حتى يقولوا لا إله إلا الله','45','0','0');
+INSERT INTO [t6038] VALUES (43,'علل حديث إذا زنت أمة أحدكم','47','0','0');
+INSERT INTO [t6038] VALUES (44,'علل حديث لا يحرم من الرضاعة المصة والمصتان','47','0','0');
