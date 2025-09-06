8b35f062b99dcd4d90fdd2a0e6c99ab17e369bc9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:35 +0700

    Upload SQL chunk: t5706_chunk_0000.sql

---
+++ sql_chunks/t5706_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t5706
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t5706" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5706
+INSERT INTO [t5706] VALUES (1,'المجلس الأول','1','0','0');
+INSERT INTO [t5706] VALUES (2,'المجلس الثاني','18','0','0');
+INSERT INTO [t5706] VALUES (3,'المجلس الثالث في جامع ثغر سلماس','33','0','0');
+INSERT INTO [t5706] VALUES (4,'المجلس الرابع','52','0','0');
+INSERT INTO [t5706] VALUES (5,'المجلس الخامس','70','0','0');
