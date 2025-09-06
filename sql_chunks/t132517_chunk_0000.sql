22c076bb6f086eb15005cd0a380eb37594f2c0e9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:12:48 +0700

    Upload SQL chunk: t132517_chunk_0000.sql

---
+++ sql_chunks/t132517_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t132517
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t132517" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t132517
+INSERT INTO [t132517] VALUES (1,'تمهيد','3','0','0');
+INSERT INTO [t132517] VALUES (2,'تعريفات في مصطلح الحديث','4','0','0');
+INSERT INTO [t132517] VALUES (3,'الخبر باعتبار النقل','4','0','0');
+INSERT INTO [t132517] VALUES (4,'فصل','5','0','0');
+INSERT INTO [t132517] VALUES (5,'الحديث منقطع السند','5','0','0');
+INSERT INTO [t132517] VALUES (6,'التدليس','6','0','0');
+INSERT INTO [t132517] VALUES (7,'المضطرب','6','0','0');
+INSERT INTO [t132517] VALUES (8,'الإدراج في المتن','6','0','0');
+INSERT INTO [t132517] VALUES (9,'الزيادة في الحديث','7','0','0');
+INSERT INTO [t132517] VALUES (10,'اختصار الحديث','7','0','0');
+INSERT INTO [t132517] VALUES (11,'رواية الحديث بالمعنى','7','0','0');
+INSERT INTO [t132517] VALUES (12,'الموضوع','8','0','0');
+INSERT INTO [t132517] VALUES (13,'الجرح','8','0','0');
+INSERT INTO [t132517] VALUES (14,'التعديل','9','0','0');
+INSERT INTO [t132517] VALUES (15,'تعارض الجرح والتعديل','10','0','0');
+INSERT INTO [t132517] VALUES (16,'أقسام الخبر باعتبار الإضافة','10','0','0');
+INSERT INTO [t132517] VALUES (17,'الصحابي','11','0','0');
+INSERT INTO [t132517] VALUES (18,'المخضرم','11','0','0');
+INSERT INTO [t132517] VALUES (19,'التابعي','11','0','0');
+INSERT INTO [t132517] VALUES (20,'الإسناد','12','0','0');
+INSERT INTO [t132517] VALUES (21,'المسلسل','12','0','0');
+INSERT INTO [t132517] VALUES (22,'تحمل الحديث','12','0','0');
+INSERT INTO [t132517] VALUES (23,'أداء الحديث','13','0','0');
+INSERT INTO [t132517] VALUES (24,'آداب العالم والمتعلم','14','0','0');
