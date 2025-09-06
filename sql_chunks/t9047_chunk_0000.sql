1d6e5d191e0e88cb3efec1fca76ab1eb97321ddc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:22 +0700

    Upload SQL chunk: t9047_chunk_0000.sql

---
+++ sql_chunks/t9047_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t9047
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t9047" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9047
+INSERT INTO [t9047] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t9047] VALUES (2,'المبحث الأول: حكم إجابة الدعوة','3','0','0');
+INSERT INTO [t9047] VALUES (3,'المبحث الثاني: الأكل لمن دعي إذا حضر','39','0','0');
+INSERT INTO [t9047] VALUES (4,'الخاتمة','52','0','0');
+INSERT INTO [t9047] VALUES (5,'مصادر ومراجع','53','0','0');
