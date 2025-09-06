fd12ee1e152f880866d088f66c90149985efd061
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:44 +0700

    Upload SQL chunk: t9597_chunk_0000.sql

---
+++ sql_chunks/t9597_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t9597
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t9597" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9597
+INSERT INTO [t9597] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t9597] VALUES (2,'فصل في قوله أنزل الله القرآن على سبعة أحرف','3','0','0');
+INSERT INTO [t9597] VALUES (3,'مسألة','13','2','0');
+INSERT INTO [t9597] VALUES (4,'فصل','15','0','0');
+INSERT INTO [t9597] VALUES (5,'الاختلاف بين القراء','16','0','0');
+INSERT INTO [t9597] VALUES (6,'اختلف فيمن جمع القرآن في عهد رسول الله صلى الله عليه وسلم','17','0','0');
+INSERT INTO [t9597] VALUES (7,'باب الأصول الدائرة في استعمال القراءة','22','0','0');
