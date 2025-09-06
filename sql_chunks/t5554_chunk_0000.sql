e530bacb0f26c0ebee96cf5d5b226c1d81100d6b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:23:39 +0700

    Upload SQL chunk: t5554_chunk_0000.sql

---
+++ sql_chunks/t5554_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5554
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5554" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5554
+INSERT INTO [t5554] VALUES (1,'تكرر سؤال بعض أصحابنا عن حكاية مناظرة جرت بيني وبين بعض أهل البدعة','1','0','0');
+INSERT INTO [t5554] VALUES (2,'آخره والحمد لله رب العالمين','45','0','0');
