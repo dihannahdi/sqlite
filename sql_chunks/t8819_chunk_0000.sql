dd55a0f86d03faaec180524ae2460fd7aca59a86
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:15 +0700

    Upload SQL chunk: t8819_chunk_0000.sql

---
+++ sql_chunks/t8819_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t8819
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t8819" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8819
+INSERT INTO [t8819] VALUES (1,'من هو ابن رشد','1','0','0');
+INSERT INTO [t8819] VALUES (2,'بين يدي البحث','3','0','0');
+INSERT INTO [t8819] VALUES (3,'العقل الصريح لا يخالف النقل الصحيح','5','0','0');
+INSERT INTO [t8819] VALUES (4,'تعريف العقل','6','0','0');
+INSERT INTO [t8819] VALUES (5,'تعريف النقل','7','0','0');
+INSERT INTO [t8819] VALUES (6,'العقل الصريح لا يخالف النقل الصحيح','9','0','0');
+INSERT INTO [t8819] VALUES (7,'فلسفة ابن رشد','10','0','0');
+INSERT INTO [t8819] VALUES (8,'وجود الله عند ابن رشد','15','0','0');
+INSERT INTO [t8819] VALUES (9,'الوحدانية عند ابن رشد','17','0','0');
+INSERT INTO [t8819] VALUES (10,'العلم عند ابن رشد','19','0','0');
+INSERT INTO [t8819] VALUES (11,'محاولة ابن رشد الحل الوسط في قضية قدم العالم','20','0','0');
+INSERT INTO [t8819] VALUES (12,'صفة الحياة والقدم والإرادة عند الفيلسوف ابن رشد','22','0','0');
+INSERT INTO [t8819] VALUES (13,'الكلام عند ابن رشد','24','0','0');
+INSERT INTO [t8819] VALUES (14,'مناقشة ابن رشد في رأيه','27','0','0');
+INSERT INTO [t8819] VALUES (15,'موقف ابن رشد من مثبتة الصفات','29','0','0');
+INSERT INTO [t8819] VALUES (16,'ابن رشد يثبت المعاد بالأدلة العقلية والنقلية','32','0','0');
+INSERT INTO [t8819] VALUES (17,'الخلاصة','35','0','0');
+INSERT INTO [t8819] VALUES (18,'القضاء والقدر عند ابن رشد','36','0','0');
+INSERT INTO [t8819] VALUES (19,'معنى القدر والقضاء وأيهما أسبق','37','0','0');
+INSERT INTO [t8819] VALUES (20,'ما ينتج من هذا القول','39','0','0');
+INSERT INTO [t8819] VALUES (21,'وجهة نظرهم','41','0','0');
+INSERT INTO [t8819] VALUES (22,'كسب الأشعري','42','0','0');
+INSERT INTO [t8819] VALUES (23,'خلاصة رأي ابن رشد','44','0','0');
+INSERT INTO [t8819] VALUES (24,'سر القدر','47','0','0');
+INSERT INTO [t8819] VALUES (25,'من ثمرات الإيمان القدر','49','0','0');
+INSERT INTO [t8819] VALUES (26,'الخاتمة','50','0','0');
