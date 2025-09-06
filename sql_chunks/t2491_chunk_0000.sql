f6b956a04b5a3db779ef182f54e4b3f00e788d6b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:29 +0700

    Upload SQL chunk: t2491_chunk_0000.sql

---
+++ sql_chunks/t2491_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t2491
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t2491" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2491
+INSERT INTO [t2491] VALUES (1,'مقدمة التحقيق','2','0','0');
+INSERT INTO [t2491] VALUES (2,'التعريف بالامام الحافظ ابي الحسن علي بن عمر الدارقطني','6','1','0');
+INSERT INTO [t2491] VALUES (3,'[حمزة السهمي]','39','1','0');
+INSERT INTO [t2491] VALUES (4,'سؤالات حمزة بن يوسف السهمي للدارقطني وغيره','48','1','0');
+INSERT INTO [t2491] VALUES (5,'منهجي في التحقيق والتعليق','57','1','0');
+INSERT INTO [t2491] VALUES (6,'ترجمة رواة سؤالات حمزة بن يوسف السهمي','64','1','0');
+INSERT INTO [t2491] VALUES (7,'باب محمد','70','0','0');
+INSERT INTO [t2491] VALUES (8,'باب الألف من اسمه أحمد','132','0','0');
+INSERT INTO [t2491] VALUES (9,'من اسمه إبراهيم','164','0','0');
+INSERT INTO [t2491] VALUES (10,'من اسمه إسحاق','169','0','0');
+INSERT INTO [t2491] VALUES (11,'بقية الألف','173','0','0');
+INSERT INTO [t2491] VALUES (12,'باب الباء','177','0','0');
+INSERT INTO [t2491] VALUES (13,'باب الثاء','185','0','0');
+INSERT INTO [t2491] VALUES (14,'باب الجيم','185','0','0');
+INSERT INTO [t2491] VALUES (15,'باب الحاء','192','0','0');
+INSERT INTO [t2491] VALUES (16,'باب الخاء','211','0','0');
+INSERT INTO [t2491] VALUES (17,'باب الدال','212','0','0');
+INSERT INTO [t2491] VALUES (18,'باب الزاي','213','0','0');
+INSERT INTO [t2491] VALUES (19,'باب السين','214','0','0');
+INSERT INTO [t2491] VALUES (20,'باب الصاد','217','0','0');
+INSERT INTO [t2491] VALUES (21,'باب الطاء','218','0','0');
+INSERT INTO [t2491] VALUES (22,'باب العين','219','0','0');
+INSERT INTO [t2491] VALUES (23,'باب القاف','247','0','0');
+INSERT INTO [t2491] VALUES (24,'باب الكاف','250','0','0');
+INSERT INTO [t2491] VALUES (25,'باب اللام','250','0','0');
+INSERT INTO [t2491] VALUES (26,'باب الميم','250','0','0');
+INSERT INTO [t2491] VALUES (27,'باب الهاء','251','0','0');
+INSERT INTO [t2491] VALUES (28,'باب الواو','254','0','0');
+INSERT INTO [t2491] VALUES (29,'باب الياء','256','0','0');
