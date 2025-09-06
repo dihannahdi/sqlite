3ab084138ea1da840e52b364c2736b5385fb5fe5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:30 +0700

    Upload SQL chunk: t10376_chunk_0000.sql

---
+++ sql_chunks/t10376_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t10376
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t10376" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10376
+INSERT INTO [t10376] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10376] VALUES (2,'جهود في اللغة','3','0','0');
+INSERT INTO [t10376] VALUES (3,'آثاره اللفوية','3','2','0');
+INSERT INTO [t10376] VALUES (4,'خطة بحث','6','2','0');
+INSERT INTO [t10376] VALUES (5,'قضايا لغوية','15','2','0');
+INSERT INTO [t10376] VALUES (6,'الترادف','15','5','0');
+INSERT INTO [t10376] VALUES (7,'دافع عن القوالب الأدبية','18','5','0');
+INSERT INTO [t10376] VALUES (8,'الإشتقاق','21','5','0');
