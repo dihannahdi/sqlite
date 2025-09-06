49f87fb8831ee9facdd41ae7cbefd72bfc5b642e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:16 +0700

    Upload SQL chunk: t7059_chunk_0000.sql

---
+++ sql_chunks/t7059_chunk_0000.sql
@@ -0,0 +1,56 @@
+-- Table: t7059
+-- Chunk: 1
+-- Rows: 49
+
+CREATE TABLE "t7059" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7059
+INSERT INTO [t7059] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t7059] VALUES (2,'خطبة الكتاب','1','0','0');
+INSERT INTO [t7059] VALUES (3,'المقصد الأول','2','0','0');
+INSERT INTO [t7059] VALUES (4,'المقصد الثاني','2','0','0');
+INSERT INTO [t7059] VALUES (5,'الخاتمة','3','0','0');
+INSERT INTO [t7059] VALUES (6,'مقدمة مشتملة على فصول','3','0','0');
+INSERT INTO [t7059] VALUES (7,'فصل','3','0','0');
+INSERT INTO [t7059] VALUES (8,'فصل','4','0','0');
+INSERT INTO [t7059] VALUES (9,'فصل','6','0','0');
+INSERT INTO [t7059] VALUES (10,'فصل','9','0','0');
+INSERT INTO [t7059] VALUES (11,'فصل','14','0','0');
+INSERT INTO [t7059] VALUES (12,'فصل','16','0','0');
+INSERT INTO [t7059] VALUES (13,'فصل','17','0','0');
+INSERT INTO [t7059] VALUES (14,'المقصد الأول','20','0','0');
+INSERT INTO [t7059] VALUES (15,'فصل','21','0','0');
+INSERT INTO [t7059] VALUES (16,'فصل','22','0','0');
+INSERT INTO [t7059] VALUES (17,'فصل','23','0','0');
+INSERT INTO [t7059] VALUES (18,'فصل','24','0','0');
+INSERT INTO [t7059] VALUES (19,'فصل','24','0','0');
+INSERT INTO [t7059] VALUES (20,'فصل','25','0','0');
+INSERT INTO [t7059] VALUES (21,'المقصد الثاني','26','0','0');
+INSERT INTO [t7059] VALUES (22,'فصل','26','0','0');
+INSERT INTO [t7059] VALUES (23,'فصل','27','0','0');
+INSERT INTO [t7059] VALUES (24,'خاتمة','32','0','0');
+INSERT INTO [t7059] VALUES (25,'فصل','32','0','0');
+INSERT INTO [t7059] VALUES (26,'فصل','35','0','0');
+INSERT INTO [t7059] VALUES (27,'المقصد الأول','37','0','0');
+INSERT INTO [t7059] VALUES (28,'فصل','37','0','0');
+INSERT INTO [t7059] VALUES (29,'فصل','39','0','0');
+INSERT INTO [t7059] VALUES (30,'تنبيه','40','0','0');
+INSERT INTO [t7059] VALUES (31,'فائدة','41','0','0');
+INSERT INTO [t7059] VALUES (32,'فائدة','41','0','0');
+INSERT INTO [t7059] VALUES (33,'فصل','44','0','0');
+INSERT INTO [t7059] VALUES (34,'فصل','45','0','0');
+INSERT INTO [t7059] VALUES (35,'النوع الأول','46','0','0');
+INSERT INTO [t7059] VALUES (36,'الأسماء الجامدة','46','0','0');
+INSERT INTO [t7059] VALUES (37,'فرع في أوزان الاسم الثلاثي المجرد','46','0','0');
+INSERT INTO [t7059] VALUES (38,'فرع في أوزان الاسم الرباعي المجرد','47','0','0');
+INSERT INTO [t7059] VALUES (39,'فرع في أوزان الخماسي المجرد','48','0','0');
+INSERT INTO [t7059] VALUES (40,'فرع','48','0','0');
+INSERT INTO [t7059] VALUES (41,'فائدة','51','0','0');
+INSERT INTO [t7059] VALUES (42,'مزيد الرباعي','56','0','0');
+INSERT INTO [t7059] VALUES (43,'مزيد الخماسي','57','0','0');
+INSERT INTO [t7059] VALUES (44,'فرع','57','0','0');
+INSERT INTO [t7059] VALUES (45,'القسم الثاني في مضعف اللام','59','0','0');
+INSERT INTO [t7059] VALUES (46,'النوع الثاني','60','0','0');
+INSERT INTO [t7059] VALUES (47,'في المصادر','60','0','0');
+INSERT INTO [t7059] VALUES (48,'النوع الثالث','62','0','0');
+INSERT INTO [t7059] VALUES (49,'فيما يحتاج إلى ذكره من الأسماء المشتقة','62','0','0');
