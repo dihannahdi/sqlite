22a70b0fe8bfa0ad00e9e806fcbde355cdd2e011
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:23:21 +0700

    Upload SQL chunk: t5509_chunk_0000.sql

---
+++ sql_chunks/t5509_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t5509
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t5509" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5509
+INSERT INTO [t5509] VALUES (1,'النص','1','0','0');
+INSERT INTO [t5509] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5509] VALUES (3,'سبب نزول الآية','1','0','0');
+INSERT INTO [t5509] VALUES (4,'تفسير آية {المال والبنون}','4','0','0');
+INSERT INTO [t5509] VALUES (5,'اختلاف المفسرين في المراد بالباقيات الصالحات','5','0','0');
+INSERT INTO [t5509] VALUES (6,'ترجيح المؤلف الرأي الثاني','8','0','0');
+INSERT INTO [t5509] VALUES (7,'ذكر الأحاديث في فضل التسبيح والتحميد والتهليل والتكبير','14','0','0');
+INSERT INTO [t5509] VALUES (8,'فصل في بيان سر فضل هذه الكلمات الأربعة','26','0','0');
+INSERT INTO [t5509] VALUES (9,'إضافة الحوقلة إلى الباقيات الصالحات','28','0','0');
+INSERT INTO [t5509] VALUES (10,'عظم موقع الباقيات الصالحات','30','0','0');
