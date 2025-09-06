a0045fc141f3b9dec69fe052d1f5aaed19ca9409
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:48 +0700

    Upload SQL chunk: t149481_chunk_0000.sql

---
+++ sql_chunks/t149481_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t149481
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t149481" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t149481
+INSERT INTO [t149481] VALUES (1,'الديوان','1','0','0');
+INSERT INTO [t149481] VALUES (2,'ذيل الديوان','46','0','0');
