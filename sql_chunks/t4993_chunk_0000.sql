6b92aa85eee40d370e5c3bf8b93ad2e89a49761f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:29 +0700

    Upload SQL chunk: t4993_chunk_0000.sql

---
+++ sql_chunks/t4993_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t4993
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t4993" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4993
+INSERT INTO [t4993] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t4993] VALUES (2,'إلى رحمة الله وحسن جواره','4','0','0');
+INSERT INTO [t4993] VALUES (3,'وهذه ترجمته رحمه الله كما سمعتها منه مباشرة','11','0','0');
+INSERT INTO [t4993] VALUES (4,'منهجه العلمي في الدراسة','17','0','0');
+INSERT INTO [t4993] VALUES (5,'الحياة الدراسية','19','0','0');
+INSERT INTO [t4993] VALUES (6,'أعماله في البلاد','23','0','0');
+INSERT INTO [t4993] VALUES (7,'طريقته في القضاء','24','0','0');
+INSERT INTO [t4993] VALUES (8,'قضاء الدماء','25','0','0');
+INSERT INTO [t4993] VALUES (9,'خروجه من بلاده رحمه الله','26','0','0');
+INSERT INTO [t4993] VALUES (10,'أولا في المسجد النبوي','27','0','0');
+INSERT INTO [t4993] VALUES (11,'منهجه في درسه','28','0','0');
+INSERT INTO [t4993] VALUES (12,'آثاره في الرياض','29','0','0');
+INSERT INTO [t4993] VALUES (13,'المنهج العلمي','31','0','0');
+INSERT INTO [t4993] VALUES (14,'في مسجد الشيخ','33','0','0');
+INSERT INTO [t4993] VALUES (15,'في بيته رحمه الله','34','0','0');
+INSERT INTO [t4993] VALUES (16,'تقدير المسؤولين له','35','0','0');
+INSERT INTO [t4993] VALUES (17,'دوره رحمه الله في الجامعة الإسلامية','36','0','0');
+INSERT INTO [t4993] VALUES (18,'امتداد نشاطه خارج المملكة','38','0','0');
+INSERT INTO [t4993] VALUES (19,'في هيئة كبار العلماء','40','0','0');
+INSERT INTO [t4993] VALUES (20,'في الرابطة','41','0','0');
+INSERT INTO [t4993] VALUES (21,'مؤلفاته رحمه الله','42','0','0');
+INSERT INTO [t4993] VALUES (22,'أما مؤلفاته هنا فهي','44','0','0');
+INSERT INTO [t4993] VALUES (23,'مواقفه مع الحق','47','0','0');
