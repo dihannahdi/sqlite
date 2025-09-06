d47fb986052c7fbe45dee370c22e925b55443746
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:42 +0700

    Upload SQL chunk: t11148_chunk_0000.sql

---
+++ sql_chunks/t11148_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11148
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11148" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11148
+INSERT INTO [t11148] VALUES (1,'مقدمة المصحح','1','0','0');
+INSERT INTO [t11148] VALUES (2,'مقدمة الشارح','6','0','0');
+INSERT INTO [t11148] VALUES (3,'القواعد التي يجب على كل مسلم معرفتها','8','0','0');
