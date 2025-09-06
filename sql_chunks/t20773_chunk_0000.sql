323dedf32d0998a164b981c6f7e0dff19b2dbe4f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:37:00 +0700

    Upload SQL chunk: t20773_chunk_0000.sql

---
+++ sql_chunks/t20773_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t20773
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t20773" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t20773
+INSERT INTO [t20773] VALUES (1,'"المنهج" في اللغة العربية','2','0','0');
+INSERT INTO [t20773] VALUES (2,'والإسلام في الإصطلاح الشرعي','2','0','0');
+INSERT INTO [t20773] VALUES (3,'و"التشريع" هو وضع الشرع','3','0','0');
+INSERT INTO [t20773] VALUES (4,'أما الاعتقاد فقد دل استقراء القرآن أنه في حق الله تعالى ثلاثة أنواع','3','0','0');
+INSERT INTO [t20773] VALUES (5,'والأركان الكبار التي بني عليها التشريع السماوي خمس وهي','6','0','0');
+INSERT INTO [t20773] VALUES (6,'اعلم أن طريق تشريع الله دينه لخلقه فيها من الحكم والأسرار من جهات شتى ما لا يحيط بعلمه إلا الله جل وعلا وحده','10','0','0');
