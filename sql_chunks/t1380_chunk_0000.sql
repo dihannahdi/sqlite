dfcf0856228580e1388653cc30027be889de4af4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:37 +0700

    Upload SQL chunk: t1380_chunk_0000.sql

---
+++ sql_chunks/t1380_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t1380
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t1380" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1380
+INSERT INTO [t1380] VALUES (1,'باب النسب','15','0','0');
+INSERT INTO [t1380] VALUES (2,'باب الأسماء','52','0','0');
+INSERT INTO [t1380] VALUES (3,'البر','139','0','0');
+INSERT INTO [t1380] VALUES (4,'الإخاء في الله','155','0','0');
+INSERT INTO [t1380] VALUES (5,'الإخاء في الله','186','0','0');
+INSERT INTO [t1380] VALUES (6,'الإخاء','230','0','0');
+INSERT INTO [t1380] VALUES (7,'في هجرة الرجل أخاه','260','0','0');
+INSERT INTO [t1380] VALUES (8,'البغي','277','0','0');
+INSERT INTO [t1380] VALUES (9,'هذا كتاب الصمت من جامع عبد الله بن وهب بن مسلم القرشي','298','0','0');
+INSERT INTO [t1380] VALUES (10,'في الكلام لما لا ينبغي ولا يحسن','299','0','0');
+INSERT INTO [t1380] VALUES (11,'باب العزلة','417','0','0');
+INSERT INTO [t1380] VALUES (12,'في الجلوس إلى القاص','560','0','0');
+INSERT INTO [t1380] VALUES (13,'في ربط الأسنان بالذهب','600','0','0');
+INSERT INTO [t1380] VALUES (14,'في الطيرة والعدوى والهام والصفر والغول','620','0','0');
+INSERT INTO [t1380] VALUES (15,'في التمائم، والتول، والنفس','656','0','0');
+INSERT INTO [t1380] VALUES (16,'في الرقية','691','0','0');
