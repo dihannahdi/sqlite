f3a5897997828b95ed470fe80094d8c5791905a2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:25 +0700

    Upload SQL chunk: t26556_chunk_0000.sql

---
+++ sql_chunks/t26556_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26556
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26556" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26556
+INSERT INTO [t26556] VALUES (1,'نسخة نبيط بن شريط','1','0','0');
