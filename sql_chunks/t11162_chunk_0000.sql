a89f8f0b4ac33e640200692f472afce69cbd9080
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:50 +0700

    Upload SQL chunk: t11162_chunk_0000.sql

---
+++ sql_chunks/t11162_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t11162
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t11162" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11162
+INSERT INTO [t11162] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11162] VALUES (2,'الصراع بين ابن تيمية وخصومه','7','0','0');
+INSERT INTO [t11162] VALUES (3,'صور المخطوطات','12','0','0');
+INSERT INTO [t11162] VALUES (4,'أول كتاب حقوق آل البيت','13','0','0');
+INSERT INTO [t11162] VALUES (5,'وحدة المسلمين بالكتاب والسنة','15','0','0');
+INSERT INTO [t11162] VALUES (6,'أهل البيت وخصائصهم','19','0','0');
+INSERT INTO [t11162] VALUES (7,'صفات أهل الفيء','25','0','0');
+INSERT INTO [t11162] VALUES (8,'سب الصحابة ... حرام على آل البيت وغيرهم','27','0','0');
+INSERT INTO [t11162] VALUES (9,'جهل الشيعة بمذهب الإمام علي','30','0','0');
+INSERT INTO [t11162] VALUES (10,'عوامل الضلال','33','0','0');
+INSERT INTO [t11162] VALUES (11,'أهل الاستقامة ... عند المصيبة','40','0','0');
+INSERT INTO [t11162] VALUES (12,'بدع وضلالات','46','0','0');
