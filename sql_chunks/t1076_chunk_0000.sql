060e1cc3138bf6299e276ccad7091429064b0b7c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:21 +0700

    Upload SQL chunk: t1076_chunk_0000.sql

---
+++ sql_chunks/t1076_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t1076
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t1076" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1076
+INSERT INTO [t1076] VALUES (1,'(مقدمة المصحح)','2','0','0');
+INSERT INTO [t1076] VALUES (2,'طريقتنا في تحقيق الكتاب','8','0','0');
+INSERT INTO [t1076] VALUES (3,'كتاب بيان خطإ محمد بن إسماعيل البخاري في تاريخه','9','0','0');
