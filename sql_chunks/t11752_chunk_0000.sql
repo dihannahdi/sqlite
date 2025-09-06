fcd18a50c9ace2163b8ae3bf89d8b1e6422f7dab
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:21 +0700

    Upload SQL chunk: t11752_chunk_0000.sql

---
+++ sql_chunks/t11752_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t11752
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t11752" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11752
+INSERT INTO [t11752] VALUES (1,'توطئة','1','0','0');
+INSERT INTO [t11752] VALUES (2,'مدخل تاريخي','3','0','0');
+INSERT INTO [t11752] VALUES (3,'حقوق الإنسان بين الخصوصية والكونية','6','0','0');
+INSERT INTO [t11752] VALUES (4,'حقوق الإنسان في الإسلام','8','0','0');
+INSERT INTO [t11752] VALUES (5,'الحقوق الأساسية','8','4','0');
+INSERT INTO [t11752] VALUES (6,'مدخل','8','5','0');
+INSERT INTO [t11752] VALUES (7,'حق الحياة','10','5','0');
+INSERT INTO [t11752] VALUES (8,'حق الحرية','12','5','0');
+INSERT INTO [t11752] VALUES (9,'حق المساواة والعدالة','15','5','0');
+INSERT INTO [t11752] VALUES (10,'الحقوق الاجتماعية','17','4','0');
+INSERT INTO [t11752] VALUES (11,'مدخل','17','10','0');
+INSERT INTO [t11752] VALUES (12,'حقوق المرأة في الإسلام','20','10','0');
+INSERT INTO [t11752] VALUES (13,'حقوق الطفل في الإسلام','25','10','0');
+INSERT INTO [t11752] VALUES (14,'حق الإنسان في محيط نظيف وبيئة طبيعية سليمة','30','4','0');
+INSERT INTO [t11752] VALUES (15,'خاتمة البحث','33','0','0');
+INSERT INTO [t11752] VALUES (16,'خلاصة البحث','36','0','0');
+INSERT INTO [t11752] VALUES (17,'تمهيد','36','16','0');
+INSERT INTO [t11752] VALUES (18,'حقوق الإنسان في الإسلام','37','16','0');
+INSERT INTO [t11752] VALUES (19,'الحقوق الأساسية','37','18','0');
+INSERT INTO [t11752] VALUES (20,'الحقوق الاجتماعية','40','18','0');
+INSERT INTO [t11752] VALUES (21,'الحق في محيط نظيف وبيئة طبيعية سليمة','43','16','0');
+INSERT INTO [t11752] VALUES (22,'خاتمة','44','16','0');
