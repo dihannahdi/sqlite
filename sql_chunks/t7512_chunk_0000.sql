5568377a83841c7cfe59e4252af276108829b2aa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:16 +0700

    Upload SQL chunk: t7512_chunk_0000.sql

---
+++ sql_chunks/t7512_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t7512
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t7512" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7512
+INSERT INTO [t7512] VALUES (1,'الأستاذ الدكتور فاروق حمادة','1','0','0');
+INSERT INTO [t7512] VALUES (2,'مقدمة','2','0','0');
+INSERT INTO [t7512] VALUES (3,'الدور الأول','6','0','0');
+INSERT INTO [t7512] VALUES (4,'الدور الثاني','16','0','0');
+INSERT INTO [t7512] VALUES (5,'الدور الثالث','36','0','0');
+INSERT INTO [t7512] VALUES (6,'الدور الرابع','43','0','0');
+INSERT INTO [t7512] VALUES (7,'الخلاصة','64','0','0');
+INSERT INTO [t7512] VALUES (8,'مصادر ومراجع','66','0','0');
