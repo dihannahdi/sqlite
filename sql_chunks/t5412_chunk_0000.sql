bccf94d0f6c2fb851223f51ff6038a1e520bfd43
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:44 +0700

    Upload SQL chunk: t5412_chunk_0000.sql

---
+++ sql_chunks/t5412_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t5412
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t5412" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5412
+INSERT INTO [t5412] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t5412] VALUES (2,'سحر الزوجة على الزوج','18','0','0');
+INSERT INTO [t5412] VALUES (3,'سؤال السحرة والمشعوذين','22','0','0');
+INSERT INTO [t5412] VALUES (4,'العلاج عند طبيب شعبي يستخدم الجن','27','0','0');
+INSERT INTO [t5412] VALUES (5,'الدواء الشرعي للسحر','30','0','0');
+INSERT INTO [t5412] VALUES (6,'إيضاح الحق في دخول الجن في الإنس والرد على من أنكر ذلك','36','0','0');
+INSERT INTO [t5412] VALUES (7,'تنبيه','56','0','0');
