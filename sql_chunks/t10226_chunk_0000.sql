3c8b2928a5b37b0ae52bfba2868b080c2a7b8964
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:22 +0700

    Upload SQL chunk: t10226_chunk_0000.sql

---
+++ sql_chunks/t10226_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t10226
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t10226" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10226
+INSERT INTO [t10226] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10226] VALUES (2,'تعريف الجريمة','2','0','0');
+INSERT INTO [t10226] VALUES (3,'نظرة الإسلام إلى الجريمة','5','0','0');
+INSERT INTO [t10226] VALUES (4,'العقوبة ضرورة اجتماعية','8','0','0');
+INSERT INTO [t10226] VALUES (5,'الحكمة من العقوبة','10','0','0');
+INSERT INTO [t10226] VALUES (6,'هل العقوبات الإسلامية قاسية','12','0','0');
