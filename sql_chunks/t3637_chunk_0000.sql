42a674824d9b54f263bf6578d9b373ae44555f05
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:37 +0700

    Upload SQL chunk: t3637_chunk_0000.sql

---
+++ sql_chunks/t3637_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t3637
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t3637" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3637
+INSERT INTO [t3637] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t3637] VALUES (2,'باب: فرض الجهاد:','14','0','0');
+INSERT INTO [t3637] VALUES (3,'فصل: في حكم الجهاد','14','2','0');
+INSERT INTO [t3637] VALUES (4,'فصل: في الأعذار','27','2','0');
+INSERT INTO [t3637] VALUES (5,'فصل: في الإمام إذا بعث سرية','39','2','0');
+INSERT INTO [t3637] VALUES (6,'باب: جامع السير:','59','0','0');
+INSERT INTO [t3637] VALUES (7,'فصل: إذا التقى الصفان ولكن بمقابلة كل مسلم مشركان','59','6','0');
+INSERT INTO [t3637] VALUES (8,'فصل: في الأمان','79','6','0');
+INSERT INTO [t3637] VALUES (9,'فصل: ولو أن علجا كافرا دل الإمام على قلعة','88','6','0');
+INSERT INTO [t3637] VALUES (10,'فصل: من ارتكب من المسلمين في دار الحرب جريمة','93','6','0');
+INSERT INTO [t3637] VALUES (11,'باب: المبارزة:','95','0','0');
+INSERT INTO [t3637] VALUES (12,'فصل: إذا أسر الكفار مسلما ثم أطلقوه من غير شرط','95','11','0');
+INSERT INTO [t3637] VALUES (13,'فصل: إذا حاصر الإمام قلعة فترك أهلها على حكم الحاكم','105','11','0');
+INSERT INTO [t3637] VALUES (14,'باب: السواد:','108','0','0');
