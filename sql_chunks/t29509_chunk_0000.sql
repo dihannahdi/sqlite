86c65d8b80dda4d20d9ff1940b1bfa5a0b5f6293
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:14 +0700

    Upload SQL chunk: t29509_chunk_0000.sql

---
+++ sql_chunks/t29509_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t29509
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t29509" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29509
+INSERT INTO [t29509] VALUES (1,'حديث جابر رضي الله عنه','3','0','0');
+INSERT INTO [t29509] VALUES (2,'حديث ابن مسعود رضي الله عنه','6','0','0');
+INSERT INTO [t29509] VALUES (3,'ذكر طريق آخر للحديث','9','0','0');
+INSERT INTO [t29509] VALUES (4,'حديث أبي هريرة رضي الله عنه','11','0','0');
+INSERT INTO [t29509] VALUES (5,'ذكر طريق آخر للحديث','13','0','0');
+INSERT INTO [t29509] VALUES (6,'حديث أبي سعيد الخدري رضي الله عنه','15','0','0');
+INSERT INTO [t29509] VALUES (7,'ذكر طريق آخر للحديث','17','0','0');
+INSERT INTO [t29509] VALUES (8,'حديث ابن عمر رضي الله عنهما','18','0','0');
+INSERT INTO [t29509] VALUES (9,'ذكر طريق آخر للحديث','21','0','0');
+INSERT INTO [t29509] VALUES (10,'ذكر قول ابن المنتشر رحمه الله تعالى','24','0','0');
