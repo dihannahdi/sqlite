057b6ab345298c99315db66ecbba80b60d8e0b1c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:41 +0700

    Upload SQL chunk: t11057_chunk_0000.sql

---
+++ sql_chunks/t11057_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11057
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11057" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11057
+INSERT INTO [t11057] VALUES (1,'الكلام على حديث امرأتي لا ترد يد لامس لابن حجر العسقلاني - مخطوط','2','0','0');
