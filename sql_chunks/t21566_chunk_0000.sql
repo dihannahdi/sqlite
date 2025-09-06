6f083f03f39de4354c604175e4940c23ca1a4ee8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:47 +0700

    Upload SQL chunk: t21566_chunk_0000.sql

---
+++ sql_chunks/t21566_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t21566
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t21566" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21566
+INSERT INTO [t21566] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t21566] VALUES (2,'المقدمة','2','0','0');
+INSERT INTO [t21566] VALUES (3,'المبحث الأول مدخل لقضية المفاهيم والمصطلحات','3','0','0');
+INSERT INTO [t21566] VALUES (4,'أولا: تجديد المشكلة وتصورها','4','3','0');
+INSERT INTO [t21566] VALUES (5,'ثانيا: المدخل النظري','9','3','0');
+INSERT INTO [t21566] VALUES (6,'المبحث الثاني الاصطلاح','19','0','0');
+INSERT INTO [t21566] VALUES (7,'المبحث الثالث التطبيق على شرح مصطلح القياس','34','0','0');
+INSERT INTO [t21566] VALUES (8,'خاتمة','53','0','0');
