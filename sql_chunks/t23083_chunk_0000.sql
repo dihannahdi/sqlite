e66f7afeb5e31ca8fd1e1bab436980b9531cf8ec
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:49 +0700

    Upload SQL chunk: t23083_chunk_0000.sql

---
+++ sql_chunks/t23083_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t23083
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t23083" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23083
+INSERT INTO [t23083] VALUES (1,'معنى الرق ونشأته','1','0','0');
+INSERT INTO [t23083] VALUES (2,'الرق قبل الإسلام','4','0','0');
+INSERT INTO [t23083] VALUES (3,'الرق في الإسلام','11','0','0');
+INSERT INTO [t23083] VALUES (4,'هل في الإسلام رق','13','0','0');
+INSERT INTO [t23083] VALUES (5,'الإسلام يدعو لتحرير الرقيق','16','0','0');
+INSERT INTO [t23083] VALUES (6,'أثر الرق في الإسلام','22','0','0');
