12300de7516deaa5dad7a782732fd3459435f865
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:21 +0700

    Upload SQL chunk: t30874_chunk_0000.sql

---
+++ sql_chunks/t30874_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t30874
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t30874" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30874
+INSERT INTO [t30874] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t30874] VALUES (2,'مكانة المسجد','2','0','0');
+INSERT INTO [t30874] VALUES (3,'إمام المسجد','5','0','0');
+INSERT INTO [t30874] VALUES (4,'مقومات الإمام العلمية','7','0','0');
+INSERT INTO [t30874] VALUES (5,'المقومات الخلقية','21','0','0');
+INSERT INTO [t30874] VALUES (6,'الخلاصة','26','0','0');
+INSERT INTO [t30874] VALUES (7,'التوصيات','27','0','0');
