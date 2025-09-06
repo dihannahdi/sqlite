213d3c28b4336ad0fffb5c99b81652aaa10b8e35
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:08 +0700

    Upload SQL chunk: t9539_chunk_0000.sql

---
+++ sql_chunks/t9539_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9539
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9539" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9539
+INSERT INTO [t9539] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t9539] VALUES (2,'ومن الأخبار التي فيها يذكر من اتخذ للنبي صلى الله عليه وسلم المنبر','8','0','0');
