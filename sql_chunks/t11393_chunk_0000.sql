ccd9f551ba990472326a436c8684fdde7ba3ee8d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:57 +0700

    Upload SQL chunk: t11393_chunk_0000.sql

---
+++ sql_chunks/t11393_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t11393
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t11393" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11393
+INSERT INTO [t11393] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11393] VALUES (2,'عناية السلف بالسنة نقدا وتدوينا','2','0','0');
+INSERT INTO [t11393] VALUES (3,'شروط قبول الرواية عند المحدثين','8','0','0');
+INSERT INTO [t11393] VALUES (4,'أسباب تفاوت شروط الرواية بين المحدثين وكتاب التأريخ والسير','14','0','0');
+INSERT INTO [t11393] VALUES (5,'المعايير المقترحة لنقد المرويات في السيرة','18','0','0');
+INSERT INTO [t11393] VALUES (6,'ملامح المعايير المقترحة','24','0','0');
+INSERT INTO [t11393] VALUES (7,'درجات الأخبار ومراتبها حسب المعايير المقترحة','28','0','0');
+INSERT INTO [t11393] VALUES (8,'الخاتمة','35','0','0');
