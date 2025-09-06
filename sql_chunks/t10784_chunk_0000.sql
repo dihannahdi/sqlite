7e4f482edd2fa6831794ae6350365bd246b7c165
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:28 +0700

    Upload SQL chunk: t10784_chunk_0000.sql

---
+++ sql_chunks/t10784_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t10784
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t10784" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10784
+INSERT INTO [t10784] VALUES (1,'سئل شيخ الإسلام','2','0','0');
+INSERT INTO [t10784] VALUES (2,'فأجاب رحمه الله تعالى','2','0','0');
+INSERT INTO [t10784] VALUES (3,'أحدها:','3','2','0');
+INSERT INTO [t10784] VALUES (4,'المقام الثاني:','15','2','0');
+INSERT INTO [t10784] VALUES (5,'المقام الثالث','20','2','0');
