e6dabeff670fa337847293e90e42b413ab6eb646
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:12 +0700

    Upload SQL chunk: t36085_chunk_0000.sql

---
+++ sql_chunks/t36085_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t36085
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t36085" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36085
+INSERT INTO [t36085] VALUES (1,'فاتحة الكتاب','2','0','0');
+INSERT INTO [t36085] VALUES (2,'مقدمة','6','0','0');
+INSERT INTO [t36085] VALUES (3,'ما هو الاستبداد','10','0','0');
+INSERT INTO [t36085] VALUES (4,'الاستبداد والدين','19','0','0');
+INSERT INTO [t36085] VALUES (5,'الاستبداد والعلم','39','0','0');
+INSERT INTO [t36085] VALUES (6,'الاستبداد والمجد','48','0','0');
+INSERT INTO [t36085] VALUES (7,'الاستبداد والمال','66','0','0');
+INSERT INTO [t36085] VALUES (8,'الاستبداد والأخلاق','85','0','0');
+INSERT INTO [t36085] VALUES (9,'الاستبداد والتربية','105','0','0');
+INSERT INTO [t36085] VALUES (10,'الاستبداد والترقي','121','0','0');
+INSERT INTO [t36085] VALUES (11,'الاستبداد والتخلص منه','155','0','0');
