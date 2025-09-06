1bcf49692b87276f6dd1cbb762afa6235a2f44f5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:46 +0700

    Upload SQL chunk: t287_chunk_0000.sql

---
+++ sql_chunks/t287_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t287
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t287" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t287
+INSERT INTO [t287] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t287] VALUES (2,'الفتوى الأولى هل العالم مخلوق ومرزوق من بركة النبي - صلى الله عليه وسلم - أو ذاك له أسباب أخرى؟','23','0','0');
+INSERT INTO [t287] VALUES (3,'الفتوى الثانية','39','0','0');
+INSERT INTO [t287] VALUES (4,'الفتوى الثالثة التعليل بالحكمة','64','0','0');
+INSERT INTO [t287] VALUES (5,'الفتوى الرابعة الإجابة الصادرة على صحة الصلاة في الطائرة','73','0','0');
+INSERT INTO [t287] VALUES (6,'الفتوى الخامسة وجهة نظر في حكم السعي فوق سقف المسعى','86','0','0');
+INSERT INTO [t287] VALUES (7,'ملحق','97','6','0');
+INSERT INTO [t287] VALUES (8,'الخلاصة','99','6','0');
