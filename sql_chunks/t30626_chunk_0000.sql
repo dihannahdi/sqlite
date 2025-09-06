679825edb847e2a7bfa871d1a5ca3739c8d507a5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:29 +0700

    Upload SQL chunk: t30626_chunk_0000.sql

---
+++ sql_chunks/t30626_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t30626
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t30626" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30626
+INSERT INTO [t30626] VALUES (1,'حديث واحد عن ابن حيان','8','0','0');
+INSERT INTO [t30626] VALUES (2,'حديث الشافعي','10','0','0');
+INSERT INTO [t30626] VALUES (3,'من حديث صدقة الإسكاف','14','0','0');
+INSERT INTO [t30626] VALUES (4,'من حديث ابن حنبل','17','0','0');
+INSERT INTO [t30626] VALUES (5,'من حديث أبي طاهر مسدد الجنزي','21','0','0');
+INSERT INTO [t30626] VALUES (6,'حديث واحد عن القاسم العقلي','24','0','0');
+INSERT INTO [t30626] VALUES (7,'من حديث أبي محمد الجوهري.','26','0','0');
+INSERT INTO [t30626] VALUES (8,'من حديث بركة المعلم السقلاطوني','30','0','0');
+INSERT INTO [t30626] VALUES (9,'من حديث أبي الحسن الهمداني','35','0','0');
+INSERT INTO [t30626] VALUES (10,'من حديث أبي بكر الخطيب','38','0','0');
