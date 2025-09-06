a1b763994b0187189337087d60b7d23e8c38a027
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:44 +0700

    Upload SQL chunk: t148_chunk_0000.sql

---
+++ sql_chunks/t148_chunk_0000.sql
@@ -0,0 +1,44 @@
+-- Table: t148
+-- Chunk: 1
+-- Rows: 37
+
+CREATE TABLE "t148" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t148
+INSERT INTO [t148] VALUES (1,'أصحاب قتادة','9','0','0');
+INSERT INTO [t148] VALUES (2,'أصحاب الأعمش','11','0','0');
+INSERT INTO [t148] VALUES (3,'أصحاب أيوب','14','0','0');
+INSERT INTO [t148] VALUES (4,'أصحاب عمرو بن دينار','15','0','0');
+INSERT INTO [t148] VALUES (5,'أصحاب الشعبي','16','0','0');
+INSERT INTO [t148] VALUES (6,'أصحاب إبراهيم','17','0','0');
+INSERT INTO [t148] VALUES (7,'أصحاب أبي إسحاق السبيعي','19','0','0');
+INSERT INTO [t148] VALUES (8,'أصحاب منصور','20','0','0');
+INSERT INTO [t148] VALUES (9,'أصحاب سفيان','21','0','0');
+INSERT INTO [t148] VALUES (10,'أصحاب شعبة','24','0','0');
+INSERT INTO [t148] VALUES (11,'باب الألف','25','0','0');
+INSERT INTO [t148] VALUES (12,'باب الباء','38','0','0');
+INSERT INTO [t148] VALUES (13,'باب الثاء','43','0','0');
+INSERT INTO [t148] VALUES (14,'باب الجيم','44','0','0');
+INSERT INTO [t148] VALUES (15,'باب الحاء','48','0','0');
+INSERT INTO [t148] VALUES (16,'باب الخاء','63','0','0');
+INSERT INTO [t148] VALUES (17,'باب الدال','67','0','0');
+INSERT INTO [t148] VALUES (18,'باب الذال','69','0','0');
+INSERT INTO [t148] VALUES (19,'باب الراء','69','0','0');
+INSERT INTO [t148] VALUES (20,'باب الزاي','72','0','0');
+INSERT INTO [t148] VALUES (21,'باب السين','75','0','0');
+INSERT INTO [t148] VALUES (22,'باب الشين','90','0','0');
+INSERT INTO [t148] VALUES (23,'باب الصاد','93','0','0');
+INSERT INTO [t148] VALUES (24,'باب الضاد','95','0','0');
+INSERT INTO [t148] VALUES (25,'باب الطاء','96','0','0');
+INSERT INTO [t148] VALUES (26,'باب العين','97','0','0');
+INSERT INTO [t148] VALUES (27,'باب الغين','149','0','0');
+INSERT INTO [t148] VALUES (28,'باب الفاء','150','0','0');
+INSERT INTO [t148] VALUES (29,'باب القاف','151','0','0');
+INSERT INTO [t148] VALUES (30,'باب الكاف','155','0','0');
+INSERT INTO [t148] VALUES (31,'باب اللام','156','0','0');
+INSERT INTO [t148] VALUES (32,'باب الميم','157','0','0');
+INSERT INTO [t148] VALUES (33,'باب النون','177','0','0');
+INSERT INTO [t148] VALUES (34,'باب الواو','179','0','0');
+INSERT INTO [t148] VALUES (35,'باب الهاء','181','0','0');
+INSERT INTO [t148] VALUES (36,'باب الياء','184','0','0');
+INSERT INTO [t148] VALUES (37,'باب من يعرف بالكنى وغير ذلك','194','0','0');
