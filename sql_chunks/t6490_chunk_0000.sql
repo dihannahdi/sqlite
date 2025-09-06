9ee519a7e8ea99d1262f62cca4913ac3db3e52a8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:03 +0700

    Upload SQL chunk: t6490_chunk_0000.sql

---
+++ sql_chunks/t6490_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6490
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6490" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6490
+INSERT INTO [t6490] VALUES (1,'كان بعض العارفين يقول أليس عجبا أن أكون بين أظهركم وفي قلبي من الإشتياق إلى ربي مثل الشعل التي لا تنطفئ ولم أر مثل نار الحب نارا تزيد ببعد موقدها اتقادا','37','0','0');
+INSERT INTO [t6490] VALUES (2,'اجتهدوا اليوم في تحقيق التوحيد','65','0','0');
