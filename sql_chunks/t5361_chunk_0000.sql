815411d05140e8424a3e2167d95325ca0c62999b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:20 +0700

    Upload SQL chunk: t5361_chunk_0000.sql

---
+++ sql_chunks/t5361_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t5361
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t5361" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5361
+INSERT INTO [t5361] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5361] VALUES (2,'تمهيد','1','0','0');
+INSERT INTO [t5361] VALUES (3,'القسم الأول','1','0','0');
+INSERT INTO [t5361] VALUES (4,'القسم الثاني','3','0','0');
+INSERT INTO [t5361] VALUES (5,'القسم الثالث','10','0','0');
+INSERT INTO [t5361] VALUES (6,'القسم الرابع','12','0','0');
+INSERT INTO [t5361] VALUES (7,'القسم الخامس','19','0','0');
+INSERT INTO [t5361] VALUES (8,'القسم السادس','23','0','0');
+INSERT INTO [t5361] VALUES (9,'القسم السابع','26','0','0');
