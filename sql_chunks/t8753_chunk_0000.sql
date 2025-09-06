0f4a3a40dffffb5d8a1f0e737f65c72f3009dd87
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:57 +0700

    Upload SQL chunk: t8753_chunk_0000.sql

---
+++ sql_chunks/t8753_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8753
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8753" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8753
+INSERT INTO [t8753] VALUES (1,'الأسماء والصفات نقلا وعقلا','1','0','0');
+INSERT INTO [t8753] VALUES (2,'محاضرة لفضيلة الشيخ محمد الأمين الشنقيطي المدرس بكلية الشريعة بالجامعة','1','0','0');
