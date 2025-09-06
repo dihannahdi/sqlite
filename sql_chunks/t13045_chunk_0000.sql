a12e702a92be2d8a92f605b4458e38befd2dc6f9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:09:36 +0700

    Upload SQL chunk: t13045_chunk_0000.sql

---
+++ sql_chunks/t13045_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t13045
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t13045" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13045
+INSERT INTO [t13045] VALUES (1,'ذكر الغرباء من المؤمنين وأوصافهم في الدنيا، وعلى أي حال هم فيها','1','0','0');
+INSERT INTO [t13045] VALUES (2,'باب الحث على بلوغ مراتب الغرباء','13','0','0');
+INSERT INTO [t13045] VALUES (3,'باب صفة الغريب الذي لو أقسم على الله عز وجل لأبر قسمه','21','0','0');
+INSERT INTO [t13045] VALUES (4,'باب ذكر من كان يحب الغربة ويخفي نفسه وينتقل من موضع إلى موضع','33','0','0');
+INSERT INTO [t13045] VALUES (5,'باب في موت الغريب','42','0','0');
