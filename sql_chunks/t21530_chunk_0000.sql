6975cdfc00f1c8db788b7a21c19188a9513226d1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:18 +0700

    Upload SQL chunk: t21530_chunk_0000.sql

---
+++ sql_chunks/t21530_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t21530
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t21530" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21530
+INSERT INTO [t21530] VALUES (1,'القول الأصدق في بيان ما خالف فيه الأصبهاني الأزرق','1','0','0');
+INSERT INTO [t21530] VALUES (2,'القول في البسملة والمد والقصر','6','0','0');
+INSERT INTO [t21530] VALUES (3,'القول في هاء الكناية','9','0','0');
+INSERT INTO [t21530] VALUES (4,'القول في الهمزتين من كلمة','10','0','0');
+INSERT INTO [t21530] VALUES (5,'تتمة','11','4','0');
+INSERT INTO [t21530] VALUES (6,'القول قي الهمزتين من كلمتين','12','0','0');
+INSERT INTO [t21530] VALUES (7,'تتمة','12','6','0');
+INSERT INTO [t21530] VALUES (8,'القول في الهمز المفرد','13','0','0');
+INSERT INTO [t21530] VALUES (9,'القول في نقل حركة الهمزة إلى الساكن قبلها','19','0','0');
+INSERT INTO [t21530] VALUES (10,'تتمة','20','9','0');
+INSERT INTO [t21530] VALUES (11,'القول في الإظهار والإدغام','20','0','0');
+INSERT INTO [t21530] VALUES (12,'القول في النون الساكنة والتنوين عند اللام والراء','23','0','0');
+INSERT INTO [t21530] VALUES (13,'القول في الفتح والإمالة وبين اللفظين','24','0','0');
+INSERT INTO [t21530] VALUES (14,'القول في الراءات واللامات','26','0','0');
+INSERT INTO [t21530] VALUES (15,'القول في ياءات الإضافة','26','0','0');
+INSERT INTO [t21530] VALUES (16,'القول في ياءات الزوائد','26','0','0');
+INSERT INTO [t21530] VALUES (17,'خاتمة نسأل الله حسنها','26','0','0');
+INSERT INTO [t21530] VALUES (18,'تتمة','27','17','0');
