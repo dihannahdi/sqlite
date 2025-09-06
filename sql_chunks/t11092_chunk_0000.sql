67228391602bf52b5a2699e468081e7463586f46
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:09 +0700

    Upload SQL chunk: t11092_chunk_0000.sql

---
+++ sql_chunks/t11092_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t11092
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t11092" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11092
+INSERT INTO [t11092] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11092] VALUES (2,'أولا: دراسة حياة المصنف','3','0','0');
+INSERT INTO [t11092] VALUES (3,'ثانيا: دراسة الكتاب','9','0','0');
+INSERT INTO [t11092] VALUES (4,'ثالثا: النص المحقق','13','0','0');
+INSERT INTO [t11092] VALUES (5,'مصادر ومراجع','64','0','0');
