80c5986d8c7e01a5a4cfea164fa398efc0048e66
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:14:13 +0700

    Upload SQL chunk: t133417_chunk_0000.sql

---
+++ sql_chunks/t133417_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t133417
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t133417" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t133417
+INSERT INTO [t133417] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t133417] VALUES (2,'باب شعر','3','0','0');
+INSERT INTO [t133417] VALUES (3,'باب رجز','21','0','0');
+INSERT INTO [t133417] VALUES (4,'باب رجز','98','0','0');
+INSERT INTO [t133417] VALUES (5,'باب رجز سماع أبي زيد من العرب','116','0','0');
+INSERT INTO [t133417] VALUES (6,'باب نوادر','164','0','0');
+INSERT INTO [t133417] VALUES (7,'باب رجز','180','0','0');
+INSERT INTO [t133417] VALUES (8,'باب نوادر','184','0','0');
+INSERT INTO [t133417] VALUES (9,'باب رجز','199','0','0');
+INSERT INTO [t133417] VALUES (10,'باب نوادر','254','0','0');
+INSERT INTO [t133417] VALUES (11,'باب شعر','267','0','0');
+INSERT INTO [t133417] VALUES (12,'باب رجز','311','0','0');
+INSERT INTO [t133417] VALUES (13,'[باب نوادر]','324','0','0');
+INSERT INTO [t133417] VALUES (14,'باب رجز','330','0','0');
+INSERT INTO [t133417] VALUES (15,'باب نوادر','355','0','0');
+INSERT INTO [t133417] VALUES (16,'باب نوادر','419','0','0');
