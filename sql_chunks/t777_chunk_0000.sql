a4e1ba60d46b55a8aabf2e3f2c2cf791dcf626cc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:03 +0700

    Upload SQL chunk: t777_chunk_0000.sql

---
+++ sql_chunks/t777_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t777
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t777" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t777
+INSERT INTO [t777] VALUES (1,'نص الكتاب','1','0','0');
+INSERT INTO [t777] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
+INSERT INTO [t777] VALUES (3,'الملك الحق المبين','1','1','0');
