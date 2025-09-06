7af2ade74bf285031f6038d6b60eb81fb4ee7884
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:35 +0700

    Upload SQL chunk: t6032_chunk_0000.sql

---
+++ sql_chunks/t6032_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6032
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6032" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6032
+INSERT INTO [t6032] VALUES (1,'وبه قال ابن عساكر أخبرنا أبو بكر وجيه بن طاهر بن محمد الشحامي قال أنبأنا أبو أحمد بن الحسن العدل قال أنبأنا أبو محمد','8','0','0');
+INSERT INTO [t6032] VALUES (2,'والحمد لله وحده وصلى الله على محمد وآله وصحبه وسلم كثيرا وحسبنا الله ونعم الوكيل تم الكتاب بحمد الله','28','0','0');
