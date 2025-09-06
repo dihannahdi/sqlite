11d5cbdb64e885ae0f2b3d166724a6855382e27e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:24 +0700

    Upload SQL chunk: t567_chunk_0000.sql

---
+++ sql_chunks/t567_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t567
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t567" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t567
+INSERT INTO [t567] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t567] VALUES (2,'وهو حسبي','1','1','0');
+INSERT INTO [t567] VALUES (3,'الفن الأول والثاني','1','1','0');
+INSERT INTO [t567] VALUES (4,'الزجل والمواليا','1','0','0');
+INSERT INTO [t567] VALUES (5,'الفن الثالث','22','4','0');
+INSERT INTO [t567] VALUES (6,'الكان وكان','22','0','0');
+INSERT INTO [t567] VALUES (7,'الفن الرابع','23','6','0');
+INSERT INTO [t567] VALUES (8,'القوما','23','0','0');
