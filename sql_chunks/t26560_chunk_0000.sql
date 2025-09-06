fc77abffbc019989bed102cc2458a5e0f81c8635
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:29 +0700

    Upload SQL chunk: t26560_chunk_0000.sql

---
+++ sql_chunks/t26560_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t26560
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t26560" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26560
+INSERT INTO [t26560] VALUES (1,'كتاب سلوك طريق السلف','1','0','0');
+INSERT INTO [t26560] VALUES (2,'الشيخ الأول','3','0','0');
+INSERT INTO [t26560] VALUES (3,'شيخ آخر','4','0','0');
+INSERT INTO [t26560] VALUES (4,'شيخ آخر','5','0','0');
+INSERT INTO [t26560] VALUES (5,'شيخ آخر','7','0','0');
+INSERT INTO [t26560] VALUES (6,'شيخ آخر','8','0','0');
+INSERT INTO [t26560] VALUES (7,'شيخ آخر','9','0','0');
+INSERT INTO [t26560] VALUES (8,'شيخ آخر','10','0','0');
+INSERT INTO [t26560] VALUES (9,'شيخ آخر','11','0','0');
+INSERT INTO [t26560] VALUES (10,'شيخ آخر','13','0','0');
+INSERT INTO [t26560] VALUES (11,'شيخ آخر','14','0','0');
+INSERT INTO [t26560] VALUES (12,'شيخ آخر','15','0','0');
+INSERT INTO [t26560] VALUES (13,'شيخ آخر','16','0','0');
