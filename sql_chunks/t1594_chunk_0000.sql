fd9e56ae32a282c2cbe3cf95b5ff5747987035d1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:38 +0700

    Upload SQL chunk: t1594_chunk_0000.sql

---
+++ sql_chunks/t1594_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t1594
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t1594" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1594
+INSERT INTO [t1594] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1594] VALUES (2,'باب الهمزة','7','0','0');
+INSERT INTO [t1594] VALUES (3,'فصل إذا كانت الهمزة أول الكلمة','7','2','0');
+INSERT INTO [t1594] VALUES (4,'فصل إذا وقعت الهمزة آخر الكلمة','8','2','0');
+INSERT INTO [t1594] VALUES (5,'فصل إذا وقعت الهمزة وسطا في الكلمة المفردة أو المركبة','17','2','0');
+INSERT INTO [t1594] VALUES (6,'باب الألف','26','0','0');
+INSERT INTO [t1594] VALUES (7,'فصل في الألف الزائدة','26','6','0');
+INSERT INTO [t1594] VALUES (8,'فصل في الألف الناقص من الخط','36','6','0');
+INSERT INTO [t1594] VALUES (9,'فصل في الألف المنقلبة عن الياء أو الواو','47','6','0');
+INSERT INTO [t1594] VALUES (10,'باب الواو','58','0','0');
+INSERT INTO [t1594] VALUES (11,'فصل في الواو الزائد في الخط','58','10','0');
+INSERT INTO [t1594] VALUES (12,'فصل في الواو الناقصة من الخط','59','10','0');
+INSERT INTO [t1594] VALUES (13,'باب الياء','61','0','0');
+INSERT INTO [t1594] VALUES (14,'فصل في الياء الزائدة','61','13','0');
+INSERT INTO [t1594] VALUES (15,'فصل في الياء الناقصة في الخط','63','13','0');
+INSERT INTO [t1594] VALUES (16,'باب مد التاءات وقبضها','79','0','0');
+INSERT INTO [t1594] VALUES (17,'باب الوصل والحجز','89','0','0');
