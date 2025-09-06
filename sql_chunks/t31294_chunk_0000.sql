10e663ce0781bed069e95abadcd13c3fcaebb781
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:58 +0700

    Upload SQL chunk: t31294_chunk_0000.sql

---
+++ sql_chunks/t31294_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t31294
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t31294" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31294
+INSERT INTO [t31294] VALUES (1,'فتوى سماحة الشيخ الإمام عبد العزيز بن باز رحمه الله','1','0','0');
+INSERT INTO [t31294] VALUES (2,'فتوى فضيلة الشيخ العلامة صالح الفوزان حفظه الله','3','0','0');
