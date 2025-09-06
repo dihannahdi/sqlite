a5227ec24b0b10bf5e3d14f2a019fc7f7883b6a6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:37 +0700

    Upload SQL chunk: t96785_chunk_0000.sql

---
+++ sql_chunks/t96785_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t96785
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t96785" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96785
+INSERT INTO [t96785] VALUES (1,'مقدمة الطبعة الثانية','2','0','0');
+INSERT INTO [t96785] VALUES (2,'المقدمة','4','0','0');
+INSERT INTO [t96785] VALUES (3,'المبتدعة ينكرون حقيقة السحر','6','0','0');
+INSERT INTO [t96785] VALUES (4,'رجال زائغون سنوا للناس سنة سيئة','9','0','0');
+INSERT INTO [t96785] VALUES (5,'شبهة وجوابها','45','0','0');
+INSERT INTO [t96785] VALUES (6,'الطاعنون في الحديث','51','0','0');
+INSERT INTO [t96785] VALUES (7,'من يقبل منه الجرح والتعديل','58','0','0');
+INSERT INTO [t96785] VALUES (8,'بعض أدلة الجرح والتعديل','64','0','0');
+INSERT INTO [t96785] VALUES (9,'الإنكار على من رد السنن بالرأي والاستحسان','70','0','0');
+INSERT INTO [t96785] VALUES (10,'حديث السحر','85','0','0');
+INSERT INTO [t96785] VALUES (11,'الرواة لحديث السحر عن هشام بن عروة','88','0','0');
+INSERT INTO [t96785] VALUES (12,'شاهد لحديث السحر','93','0','0');
+INSERT INTO [t96785] VALUES (13,'شرح الحديث','95','0','0');
+INSERT INTO [t96785] VALUES (14,'رد أهل العلم على الطاعنين في الحديث','111','0','0');
+INSERT INTO [t96785] VALUES (15,'وللشيخ الفاضل أحمد شاكر رحمه الله كلام حسن','131','0','0');
+INSERT INTO [t96785] VALUES (16,'مسألة','138','0','0');
+INSERT INTO [t96785] VALUES (17,'الخاتمة','139','0','0');
