af83d96a2f6066439e541f6ee1c06329e8b63f71
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:55 +0700

    Upload SQL chunk: t117137_chunk_0000.sql

---
+++ sql_chunks/t117137_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t117137
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t117137" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t117137
+INSERT INTO [t117137] VALUES (1,'أما حديث أنس بن مالك','1','0','0');
+INSERT INTO [t117137] VALUES (2,'أولا- عبد العزيز بن صهيب','1','1','0');
+INSERT INTO [t117137] VALUES (3,'ثانيا- وروي عن أنس بزيادة','1','1','0');
+INSERT INTO [t117137] VALUES (4,'أما حديث زيد بن أرقم','1','0','0');
+INSERT INTO [t117137] VALUES (5,'وأما حديث ابن عباس','5','0','0');
+INSERT INTO [t117137] VALUES (6,'والخلاصة','5','0','0');
