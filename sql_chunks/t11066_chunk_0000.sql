e2539888faf5f2ffc3c57d2959859f328a46cd3c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:45 +0700

    Upload SQL chunk: t11066_chunk_0000.sql

---
+++ sql_chunks/t11066_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11066
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11066" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11066
+INSERT INTO [t11066] VALUES (1,'بأي عقل ودين يكون التفجير والتدمير جهادا؟! ويحكم ... أفيقوا يا شباب!!','1','0','0');
+INSERT INTO [t11066] VALUES (2,'إعداد: عبد المحسن بن حمد العباد البدر','1','0','0');
