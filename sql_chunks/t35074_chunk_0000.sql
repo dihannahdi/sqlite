1aba16528b63d510accb2045b4b4cd4db45709ce
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:47 +0700

    Upload SQL chunk: t35074_chunk_0000.sql

---
+++ sql_chunks/t35074_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t35074
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t35074" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t35074
+INSERT INTO [t35074] VALUES (1,'الحمد لله تعالى الذي جعل التقوى زينة للمتقين','1','0','0');
+INSERT INTO [t35074] VALUES (2,'أخي المسلم: إن طهارة القلب ..','1','1','0');
+INSERT INTO [t35074] VALUES (3,'أخي في الله','6','1','0');
+INSERT INTO [t35074] VALUES (4,'أخي في الله','8','1','0');
+INSERT INTO [t35074] VALUES (5,'أخي في الله','11','1','0');
