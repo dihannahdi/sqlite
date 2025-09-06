61bd071fb5dc732d72b11095e1c1f54715c4d8f0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:47 +0700

    Upload SQL chunk: t6454_chunk_0000.sql

---
+++ sql_chunks/t6454_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t6454
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t6454" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6454
+INSERT INTO [t6454] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6454] VALUES (2,'أقاويل الثقات في تأويل الأسماء والصفات','3','0','0');
+INSERT INTO [t6454] VALUES (3,'مقدمة','3','0','0');
+INSERT INTO [t6454] VALUES (4,'نصيحة','66','0','0');
+INSERT INTO [t6454] VALUES (5,'تنبيه','88','0','0');
+INSERT INTO [t6454] VALUES (6,'تنبيه','102','0','0');
+INSERT INTO [t6454] VALUES (7,'تنبيه','109','0','0');
+INSERT INTO [t6454] VALUES (8,'تنبيه','112','0','0');
+INSERT INTO [t6454] VALUES (9,'الدبا الجراد قبل أن يطير','136','0','0');
+INSERT INTO [t6454] VALUES (10,'أي في حاجته أو حقه','139','0','0');
+INSERT INTO [t6454] VALUES (11,'تنبيه','144','0','0');
+INSERT INTO [t6454] VALUES (12,'تنبيه','152','0','0');
+INSERT INTO [t6454] VALUES (13,'تنبيه','161','0','0');
+INSERT INTO [t6454] VALUES (14,'خاتمة','165','0','0');
+INSERT INTO [t6454] VALUES (15,'فضحك','166','0','0');
