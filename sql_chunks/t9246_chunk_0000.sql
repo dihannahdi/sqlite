2e56f01cdc0b8fe0ad8e783d7d98ca76b70980f3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:51:57 +0700

    Upload SQL chunk: t9246_chunk_0000.sql

---
+++ sql_chunks/t9246_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t9246
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t9246" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9246
+INSERT INTO [t9246] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t9246] VALUES (2,'مدخل السفسطة وجحد العلوم','7','0','0');
+INSERT INTO [t9246] VALUES (3,'الفلسفة','13','0','0');
+INSERT INTO [t9246] VALUES (4,'علم الكلام مقصوده وحاصله','13','0','0');
+INSERT INTO [t9246] VALUES (5,'أصناف الفلاسفة وشمول وصمة الكفر كافتهم','20','0','0');
+INSERT INTO [t9246] VALUES (6,'الدهريون','20','5','0');
+INSERT INTO [t9246] VALUES (7,'الطبيعيون','20','5','0');
+INSERT INTO [t9246] VALUES (8,'الإلهيون','20','5','0');
+INSERT INTO [t9246] VALUES (9,'الصنف الأول','20','5','0');
+INSERT INTO [t9246] VALUES (10,'والصنف الثاني','23','5','0');
+INSERT INTO [t9246] VALUES (11,'والصنف الثالث','24','5','0');
+INSERT INTO [t9246] VALUES (12,'أقسام علومهم','27','0','0');
+INSERT INTO [t9246] VALUES (13,'مذهب التعليم وغائلته','44','0','0');
+INSERT INTO [t9246] VALUES (14,'طرق الصوفية','56','0','0');
+INSERT INTO [t9246] VALUES (15,'حقيقة النبوة واضطرار كافة الخلق إليها','67','0','0');
+INSERT INTO [t9246] VALUES (16,'سبب نشر العلم بعد الإعراض عنه','74','0','0');
