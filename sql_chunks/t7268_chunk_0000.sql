0e8cce85cd4305dbc0b50d6553f201669c21cb74
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:03 +0700

    Upload SQL chunk: t7268_chunk_0000.sql

---
+++ sql_chunks/t7268_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t7268
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t7268" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7268
+INSERT INTO [t7268] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t7268] VALUES (2,'((ذكر الأحاديث الصريحة في أن الله تعالى في السماء))','5','0','0');
+INSERT INTO [t7268] VALUES (3,'((ذكر الأخبار الواردة بأن الله تعالى فوق عرشه))','29','0','0');
+INSERT INTO [t7268] VALUES (4,'((ذكر أخبار دالة على ذلك في الجملة))','56','0','0');
+INSERT INTO [t7268] VALUES (5,'((ذكر أخبار واردة في هذا عن الأنبياء المتقدمين عليهم السلام))','78','0','0');
+INSERT INTO [t7268] VALUES (6,'((أقوال الصحابة رضي الله عنهم))','84','0','0');
+INSERT INTO [t7268] VALUES (7,'((أقوال التابعين رحمهم الله))','99','0','0');
+INSERT INTO [t7268] VALUES (8,'((أقوال الأئمة رضي الله عنهم))','105','0','0');
+INSERT INTO [t7268] VALUES (9,'مصادر ومراجع','132','0','0');
