f549562ce94a60cbc122d51afb0c879dcdf66753
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:16 +0700

    Upload SQL chunk: t8820_chunk_0000.sql

---
+++ sql_chunks/t8820_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8820
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8820" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8820
+INSERT INTO [t8820] VALUES (1,'العقيدة الصحيحة وما يضادها','1','0','0');
+INSERT INTO [t8820] VALUES (2,'العقيدة الصحيحة هي أصل دين الإسلام','1','0','0');
