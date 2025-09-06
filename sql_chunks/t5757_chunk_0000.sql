664cc70ce71ee8640da5251e9184b650253db856
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:06 +0700

    Upload SQL chunk: t5757_chunk_0000.sql

---
+++ sql_chunks/t5757_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t5757
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t5757" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5757
+INSERT INTO [t5757] VALUES (1,'الجزء ١','1','0','0');
+INSERT INTO [t5757] VALUES (2,'باب وقوت الصلاة','1','1','0');
+INSERT INTO [t5757] VALUES (3,'كتاب الجنائز','148','1','0');
+INSERT INTO [t5757] VALUES (4,'كتاب الزكاة','163','1','0');
+INSERT INTO [t5757] VALUES (5,'كتاب الصيام','177','1','0');
+INSERT INTO [t5757] VALUES (6,'كتاب الحج','195','1','0');
+INSERT INTO [t5757] VALUES (7,'كتاب الجهاد','232','1','0');
+INSERT INTO [t5757] VALUES (8,'كتاب النذور والأيمان','248','1','0');
+INSERT INTO [t5757] VALUES (9,'كتاب الذبائح','257','1','0');
+INSERT INTO [t5757] VALUES (10,'كتاب الصيد','259','1','0');
+INSERT INTO [t5757] VALUES (11,'كتاب العقيقة','261','1','0');
+INSERT INTO [t5757] VALUES (12,'كتاب الفرائض','263','1','0');
+INSERT INTO [t5757] VALUES (13,'الجزء ٢','266','0','0');
+INSERT INTO [t5757] VALUES (14,'كتاب النكاح','266','13','0');
+INSERT INTO [t5757] VALUES (15,'كتاب الرضاع','289','13','0');
+INSERT INTO [t5757] VALUES (16,'كتاب البيوع','293','13','0');
+INSERT INTO [t5757] VALUES (17,'كتاب المساقاة','313','13','0');
+INSERT INTO [t5757] VALUES (18,'كتاب الشفعة','315','13','0');
+INSERT INTO [t5757] VALUES (19,'كتاب الأقضية','316','13','0');
+INSERT INTO [t5757] VALUES (20,'كتاب الحدود','338','13','0');
+INSERT INTO [t5757] VALUES (21,'كتاب الأشربة','346','13','0');
+INSERT INTO [t5757] VALUES (22,'كتاب العقول','349','13','0');
+INSERT INTO [t5757] VALUES (23,'كتاب الجامع قال بن العري في التفسير هذا كتاب اخترعه مالك في التصنيف لفائدتين إحاداهما أنه خارج عن رسم التكليف','353','13','0');
