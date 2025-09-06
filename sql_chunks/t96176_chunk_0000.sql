ee75db1f2dcdfa6bba9b8da9af15cdab9a8e336c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:02 +0700

    Upload SQL chunk: t96176_chunk_0000.sql

---
+++ sql_chunks/t96176_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t96176
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t96176" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96176
+INSERT INTO [t96176] VALUES (1,'تدوين السنة:','5','0','0');
+INSERT INTO [t96176] VALUES (2,'[أمية النبي - صلى الله عليه وسلم -]:','6','0','0');
+INSERT INTO [t96176] VALUES (3,'الصحف التي دونت في العهد النبوي:','10','0','0');
+INSERT INTO [t96176] VALUES (4,'موقف المستشرقين من تدوين الحديث:','13','0','0');
+INSERT INTO [t96176] VALUES (5,'الرد على جولدتسيهر:','14','0','0');
+INSERT INTO [t96176] VALUES (6,'الرد على سوفاجيه:','14','0','0');
+INSERT INTO [t96176] VALUES (7,'الرد على دوزي:','15','0','0');
+INSERT INTO [t96176] VALUES (8,'الضلال العلمي:','17','0','0');
+INSERT INTO [t96176] VALUES (9,'ملامح التجديد في المنهجية الحديثية عند الدكتور الشهيد:','20','0','0');
+INSERT INTO [t96176] VALUES (10,'ملامح منهجية النقد عند الدكتور صبحي الصالح:','22','0','0');
+INSERT INTO [t96176] VALUES (11,'ملامح الشخصية العلمية عند الدكتور صبحي الصالح (في مواجهة المستشرقين):','23','0','0');
+INSERT INTO [t96176] VALUES (12,'شبهة وردها:','25','0','0');
+INSERT INTO [t96176] VALUES (13,'جريدة المصادر والمراجع:','28','0','0');
