13ce1da1ca340401dce98b8d91f5a700709989e0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:46 +0700

    Upload SQL chunk: t7624_chunk_0000.sql

---
+++ sql_chunks/t7624_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7624
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7624" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7624
+INSERT INTO [t7624] VALUES (1,'الرد على الرفاعي والبوطي في كذبهما على أهل السنة ودعوتهما إلى البدع والضلال','1','0','0');
+INSERT INTO [t7624] VALUES (2,'إعداد: عبد المحسن بن حمد العباد البدر','1','0','0');
