09aefb480c0cbaa8353cd23b5fa37cdf53a21b8c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:22 +0700

    Upload SQL chunk: t22381_chunk_0000.sql

---
+++ sql_chunks/t22381_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t22381
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t22381" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22381
+INSERT INTO [t22381] VALUES (1,'فوائد أبي بكر الأبهري','1','0','0');
