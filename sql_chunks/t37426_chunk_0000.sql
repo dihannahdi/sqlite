69176a0c4cf52ff58181255d24771d64c0999626
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:54 +0700

    Upload SQL chunk: t37426_chunk_0000.sql

---
+++ sql_chunks/t37426_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t37426
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t37426" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37426
+INSERT INTO [t37426] VALUES (1,'جمعية الترتيل للخدمات الثقافية والدينية','1','0','0');
+INSERT INTO [t37426] VALUES (2,'تقديم فضيلة الأستاذ الدكتور / محمد نعيم محمد هاني الساعي','2','0','0');
+INSERT INTO [t37426] VALUES (3,'مقدمة اللجنة العلمية','4','0','0');
+INSERT INTO [t37426] VALUES (4,'العلمانية','7','0','0');
+INSERT INTO [t37426] VALUES (5,'الليبرالية','16','0','0');
+INSERT INTO [t37426] VALUES (6,'الديمقراطية','21','0','0');
+INSERT INTO [t37426] VALUES (7,'الدولة المدنية','28','0','0');
+INSERT INTO [t37426] VALUES (8,'وجوب التحاكم بالكتاب والسنة','33','0','0');
+INSERT INTO [t37426] VALUES (9,'فواجب الوقت الآن','48','0','0');
