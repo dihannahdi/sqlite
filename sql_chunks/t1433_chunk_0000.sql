80a14ac2fd4c37b4bb715e0172303aa2c753cf4f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:18:25 +0700

    Upload SQL chunk: t1433_chunk_0000.sql

---
+++ sql_chunks/t1433_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t1433
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t1433" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1433
+INSERT INTO [t1433] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t1433] VALUES (2,'بداية عصر النهضة والتقدم عند الأروبيين','2','0','0');
+INSERT INTO [t1433] VALUES (3,'التقدم والرجعية','21','0','0');
+INSERT INTO [t1433] VALUES (4,'الرجعية في نظر الدول التي لا تدين بدين','21','3','0');
+INSERT INTO [t1433] VALUES (5,'الرجعية عند الشعوب العربية في العصور الأخيرة','24','3','0');
+INSERT INTO [t1433] VALUES (6,'استدراك','32','3','0');
