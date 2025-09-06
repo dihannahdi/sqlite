8dd0ec77bd68fe8570b14d7f7010505d2c0b9213
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:31 +0700

    Upload SQL chunk: t396_chunk_0000.sql

---
+++ sql_chunks/t396_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t396
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t396" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t396
+INSERT INTO [t396] VALUES (1,'نص الإشكال','2','0','0');
+INSERT INTO [t396] VALUES (2,'إن كانت ظاهرة في السماع، كان المدلس كاذبا لعدم نصبه قرينة صارفة','2','1','0');
+INSERT INTO [t396] VALUES (3,'إن كانت غير ظاهرة في السماع، كيف يصح حملها على السماع في حق من لم يوصف بالتدليس ولا بعدمه؟','4','1','0');
+INSERT INTO [t396] VALUES (4,'الجواب: (بيان أنها غير ظاهرة في السماع)','5','0','0');
+INSERT INTO [t396] VALUES (5,'فائدة مغفول عنها حول تكرار «عن» في ثاني حديث في البخاري','6','0','0');
+INSERT INTO [t396] VALUES (6,'فائدة: (قد ترد «عن» ولا يقصد بها الرواية)','8','0','0');
+INSERT INTO [t396] VALUES (7,'الفرق بين الإرسال الخفي والتدليس','9','0','0');
