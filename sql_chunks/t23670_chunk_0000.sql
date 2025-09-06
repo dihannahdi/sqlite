e1c14f4c313b0498c5d207cbe7f5046f57ab1b19
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:48:09 +0700

    Upload SQL chunk: t23670_chunk_0000.sql

---
+++ sql_chunks/t23670_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t23670
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t23670" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23670
+INSERT INTO [t23670] VALUES (1,'مقدمات التحقيق','1','0','0');
+INSERT INTO [t23670] VALUES (2,'الإهداء','1','1','0');
+INSERT INTO [t23670] VALUES (3,'- تقديم - آفة الشعر عند ما يكون مدحا لغير رسول الله صلى الله عليه وسلم','2','1','0');
+INSERT INTO [t23670] VALUES (4,'مقدمة','5','1','0');
+INSERT INTO [t23670] VALUES (5,'محمد الإنسان الكبير:','5','4','0');
+INSERT INTO [t23670] VALUES (6,'محمد والشعر صلى الله عليه وسلم','8','4','0');
+INSERT INTO [t23670] VALUES (7,'نبي يرى ما لا ترون','10','0','0');
+INSERT INTO [t23670] VALUES (8,'«بانت سعاد»','11','0','0');
+INSERT INTO [t23670] VALUES (9,'أغر عليه خاتم النبوة','14','0','0');
+INSERT INTO [t23670] VALUES (10,'البردة','15','0','0');
+INSERT INTO [t23670] VALUES (11,'ابن الساعاتي','22','0','0');
+INSERT INTO [t23670] VALUES (12,'الهمزية النبوية','23','0','0');
+INSERT INTO [t23670] VALUES (13,'رسالة محمد ... رسالة الله','31','0','0');
+INSERT INTO [t23670] VALUES (14,'ذكرى سيد الوجود محمد صلى الله عليه وسلم','33','0','0');
+INSERT INTO [t23670] VALUES (15,'«المولد»','35','0','0');
+INSERT INTO [t23670] VALUES (16,'عيد البرية','37','0','0');
+INSERT INTO [t23670] VALUES (17,'ذكرى محمد صلى الله عليه وسلم في عيد ميلاد','38','0','0');
+INSERT INTO [t23670] VALUES (18,'نور على الأفق','41','0','0');
+INSERT INTO [t23670] VALUES (19,'الكعبة الزهراء','43','0','0');
+INSERT INTO [t23670] VALUES (20,'يوم أحمد','53','0','0');
+INSERT INTO [t23670] VALUES (21,'(بشائر النعمى)','53','20','0');
+INSERT INTO [t23670] VALUES (22,'"فيض النور"','53','20','0');
+INSERT INTO [t23670] VALUES (23,'"حلم الصحراء"','54','20','0');
+INSERT INTO [t23670] VALUES (24,'"صبح الحق"','55','20','0');
+INSERT INTO [t23670] VALUES (25,'"محمد" صلى الله عليه وسلم','55','20','0');
+INSERT INTO [t23670] VALUES (26,'"شكوى"','56','20','0');
