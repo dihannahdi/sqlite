cb8477b2acdd7956251501dcbd6982dda5b0b2fc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:08 +0700

    Upload SQL chunk: t26533_chunk_0000.sql

---
+++ sql_chunks/t26533_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26533
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26533" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26533
+INSERT INTO [t26533] VALUES (1,'فوائد أبي القاسم الحرفي رواية الثقفي','2','0','0');
