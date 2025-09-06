9bdacd6c0138c7d5705fb5bfb4e350f6313cddd6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:17 +0700

    Upload SQL chunk: t6260_chunk_0000.sql

---
+++ sql_chunks/t6260_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t6260
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t6260" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6260
+INSERT INTO [t6260] VALUES (1,'مسألة','1','0','0');
+INSERT INTO [t6260] VALUES (2,'المسلك الأول','2','0','0');
+INSERT INTO [t6260] VALUES (3,'المسلك الثاني','4','0','0');
+INSERT INTO [t6260] VALUES (4,'المسلك الثالث','4','0','0');
+INSERT INTO [t6260] VALUES (5,'المسلك الرابع','4','0','0');
+INSERT INTO [t6260] VALUES (6,'المسلك الخامس','5','0','0');
+INSERT INTO [t6260] VALUES (7,'المسلك السادس','5','0','0');
+INSERT INTO [t6260] VALUES (8,'المسلك السابع','5','0','0');
+INSERT INTO [t6260] VALUES (9,'المسلك الثامن','6','0','0');
+INSERT INTO [t6260] VALUES (10,'المسلك التاسع','6','0','0');
