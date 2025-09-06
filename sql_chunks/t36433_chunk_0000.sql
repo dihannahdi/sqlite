4c59ebf61094de178ba6b1a97569b655b34888c8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:19 +0700

    Upload SQL chunk: t36433_chunk_0000.sql

---
+++ sql_chunks/t36433_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t36433
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t36433" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36433
+INSERT INTO [t36433] VALUES (1,'الحمد لله المتفضل على عباده بأنواع النعم','1','0','0');
+INSERT INTO [t36433] VALUES (2,'من أنت','2','0','0');
+INSERT INTO [t36433] VALUES (3,'أنت المكرمة بالإسلام','3','0','0');
+INSERT INTO [t36433] VALUES (4,'احذري أعداء المرأة!','5','0','0');
+INSERT INTO [t36433] VALUES (5,'أختاه .. احذري الذئاب! !','7','0','0');
+INSERT INTO [t36433] VALUES (6,'أختاه .. احذري الاختلاط','11','0','0');
+INSERT INTO [t36433] VALUES (7,'أختاه .. احذري الزينة الكاذبة!','13','0','0');
+INSERT INTO [t36433] VALUES (8,'أختاه .. الحجاب .. الحجاب ..','14','0','0');
+INSERT INTO [t36433] VALUES (9,'بشرى لك','18','0','0');
+INSERT INTO [t36433] VALUES (10,'أختاه .. الخلوة شر!','19','0','0');
+INSERT INTO [t36433] VALUES (11,'طاعة الأزواج طريق إلى الجنة','20','0','0');
+INSERT INTO [t36433] VALUES (12,'أختاه .. وأخيرا أهدي إليك هذه الدرر الغاليات','21','0','0');
