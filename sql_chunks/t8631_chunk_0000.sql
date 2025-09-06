724970af6e28eec732f0435f2ec9f89417b5154c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:56 +0700

    Upload SQL chunk: t8631_chunk_0000.sql

---
+++ sql_chunks/t8631_chunk_0000.sql
@@ -0,0 +1,61 @@
+-- Table: t8631
+-- Chunk: 1
+-- Rows: 54
+
+CREATE TABLE "t8631" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8631
+INSERT INTO [t8631] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8631] VALUES (2,'الموضوعات','2','0','0');
+INSERT INTO [t8631] VALUES (3,'الزواج','8','0','0');
+INSERT INTO [t8631] VALUES (4,'مدخل','8','3','0');
+INSERT INTO [t8631] VALUES (5,'مقدمات الزواج','12','3','0');
+INSERT INTO [t8631] VALUES (6,'أركان الزواج','16','3','0');
+INSERT INTO [t8631] VALUES (7,'شروط الزواج','19','3','0');
+INSERT INTO [t8631] VALUES (8,'صيغة الزواج من حيث اطلاقها وتقييدها','32','3','0');
+INSERT INTO [t8631] VALUES (9,'حكم الزواج','35','3','0');
+INSERT INTO [t8631] VALUES (10,'المحرمات من النساء','41','3','0');
+INSERT INTO [t8631] VALUES (11,'الولاية على الزواج','55','3','0');
+INSERT INTO [t8631] VALUES (12,'الوكالة بالزواج','63','3','0');
+INSERT INTO [t8631] VALUES (13,'الكفاءة في الزواج','67','3','0');
+INSERT INTO [t8631] VALUES (14,'حقوق الزوجة على زوجها','72','3','0');
+INSERT INTO [t8631] VALUES (15,'مدخل','72','14','0');
+INSERT INTO [t8631] VALUES (16,'المهر','73','14','0');
+INSERT INTO [t8631] VALUES (17,'نفقة الزوجة','104','14','0');
+INSERT INTO [t8631] VALUES (18,'عدم الاضرار بالزوجة','119','14','0');
+INSERT INTO [t8631] VALUES (19,'العدل بين الزوجات','120','14','0');
+INSERT INTO [t8631] VALUES (20,'حقوق الزوج على زوجته','122','3','0');
+INSERT INTO [t8631] VALUES (21,'مدخل','122','20','0');
+INSERT INTO [t8631] VALUES (22,'الطاعة','123','20','0');
+INSERT INTO [t8631] VALUES (23,'ولاية التأديب','124','20','0');
+INSERT INTO [t8631] VALUES (24,'الحقوق المشتركة بين الزوجين','125','3','0');
+INSERT INTO [t8631] VALUES (25,'زوج المسلم بالكتابيات','126','3','0');
+INSERT INTO [t8631] VALUES (26,'زواج غير المسلمين بعضهم ببعض','130','3','0');
+INSERT INTO [t8631] VALUES (27,'الطلاق','134','0','0');
+INSERT INTO [t8631] VALUES (28,'مدخل','134','27','0');
+INSERT INTO [t8631] VALUES (29,'أحوال صيغة الطلاق','144','27','0');
+INSERT INTO [t8631] VALUES (30,'أنواع الطلاق','147','27','0');
+INSERT INTO [t8631] VALUES (31,'حكم الطلاق','149','27','0');
+INSERT INTO [t8631] VALUES (32,'طلاق المريض مرض الموت','155','27','0');
+INSERT INTO [t8631] VALUES (33,'تفويض الطلاق إلى الزوجة','158','27','0');
+INSERT INTO [t8631] VALUES (34,'الخلع','162','27','0');
+INSERT INTO [t8631] VALUES (35,'الحالات التى يطلق فيها القاضى','170','27','0');
+INSERT INTO [t8631] VALUES (36,'فسخ الزواج','178','27','0');
+INSERT INTO [t8631] VALUES (37,'العدة','181','0','0');
+INSERT INTO [t8631] VALUES (38,'ثبوت النسب','192','0','0');
+INSERT INTO [t8631] VALUES (39,'ثبوت النسب بالفراش','192','38','0');
+INSERT INTO [t8631] VALUES (40,'ثبات النسب بالإقرار','200','38','0');
+INSERT INTO [t8631] VALUES (41,'ثبوت النسب بالبينة','203','38','0');
+INSERT INTO [t8631] VALUES (42,'اللقيط','207','38','0');
+INSERT INTO [t8631] VALUES (43,'الرضاعة','210','0','0');
+INSERT INTO [t8631] VALUES (44,'الحضانة والنفقات','215','0','0');
+INSERT INTO [t8631] VALUES (45,'الحضانة','215','44','0');
+INSERT INTO [t8631] VALUES (46,'نفقة الأقارب','225','44','0');
+INSERT INTO [t8631] VALUES (47,'مقارنة بين أنواع النفقات بعضها وبعض','237','44','0');
+INSERT INTO [t8631] VALUES (48,'الحجر','242','0','0');
+INSERT INTO [t8631] VALUES (49,'الولاية على المال','251','0','0');
+INSERT INTO [t8631] VALUES (50,'المفقود','267','0','0');
+INSERT INTO [t8631] VALUES (51,'المريض مرض الموت','271','0','0');
+INSERT INTO [t8631] VALUES (52,'مدخل','271','51','0');
+INSERT INTO [t8631] VALUES (53,'الهبة','275','51','0');
+INSERT INTO [t8631] VALUES (54,'الوصية','289','51','0');
