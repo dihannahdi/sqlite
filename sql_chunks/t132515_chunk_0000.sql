c172473864c9871e8a65f1d81b50cf5403abba63
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:12:46 +0700

    Upload SQL chunk: t132515_chunk_0000.sql

---
+++ sql_chunks/t132515_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t132515
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t132515" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t132515
+INSERT INTO [t132515] VALUES (1,'تمهيد','2','0','0');
+INSERT INTO [t132515] VALUES (2,'أنواع الكلمة','3','0','0');
+INSERT INTO [t132515] VALUES (3,'الإعراب والبناء','3','0','0');
+INSERT INTO [t132515] VALUES (4,'أحوال إعراب الأسماء','3','0','0');
+INSERT INTO [t132515] VALUES (5,'أحوال إعراب الفعل المضارع','4','0','0');
+INSERT INTO [t132515] VALUES (6,'نواصب الفعل المضارع','4','0','0');
+INSERT INTO [t132515] VALUES (7,'جوازم الفعل المضارع','5','0','0');
+INSERT INTO [t132515] VALUES (8,'جر الأسماء بالحروف','5','0','0');
+INSERT INTO [t132515] VALUES (9,'جر الأسماء بالإضافة (المضاف والمضاف إليه)','5','0','0');
+INSERT INTO [t132515] VALUES (10,'النكرة والمعرفة','5','0','0');
+INSERT INTO [t132515] VALUES (11,'الفاعل والمفعول به','6','0','0');
+INSERT INTO [t132515] VALUES (12,'نائب الفاعل','6','0','0');
+INSERT INTO [t132515] VALUES (13,'المبتدأ والخبر','6','0','0');
+INSERT INTO [t132515] VALUES (14,'كان وأخواتها','7','0','0');
+INSERT INTO [t132515] VALUES (15,'إن وأخواتها','7','0','0');
+INSERT INTO [t132515] VALUES (16,'ظن وأخواتها','7','0','0');
+INSERT INTO [t132515] VALUES (17,'المفعول المطلق','8','0','0');
+INSERT INTO [t132515] VALUES (18,'المفعول لأجله (أو له)','8','0','0');
+INSERT INTO [t132515] VALUES (19,'المفعول فيه (ظرف الزمان والمكان)','8','0','0');
+INSERT INTO [t132515] VALUES (20,'المفعول معه','9','0','0');
+INSERT INTO [t132515] VALUES (21,'الصفة (أو النعت)','9','0','0');
+INSERT INTO [t132515] VALUES (22,'التوكيد','9','0','0');
+INSERT INTO [t132515] VALUES (23,'البدل','9','0','0');
+INSERT INTO [t132515] VALUES (24,'العطف','10','0','0');
+INSERT INTO [t132515] VALUES (25,'الحال','10','0','0');
+INSERT INTO [t132515] VALUES (26,'التمييز','11','0','0');
+INSERT INTO [t132515] VALUES (27,'النداء','11','0','0');
+INSERT INTO [t132515] VALUES (28,'الاستثناء','11','0','0');
