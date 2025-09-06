531e1c7127b9f6e40fd406f479ab769630fc432e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:31 +0700

    Upload SQL chunk: t30900_chunk_0000.sql

---
+++ sql_chunks/t30900_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t30900
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t30900" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30900
+INSERT INTO [t30900] VALUES (1,'المقدمة','3','0','0');
+INSERT INTO [t30900] VALUES (2,'فضل الدعاء','5','0','0');
+INSERT INTO [t30900] VALUES (3,'آداب الدعاء وأسباب الإجابة','7','0','0');
+INSERT INTO [t30900] VALUES (4,'أوقات وأحوال وأماكن يستجاب فيها الدعاء','9','0','0');
+INSERT INTO [t30900] VALUES (5,'الدعاء من الكتاب والسنة','14','0','0');
