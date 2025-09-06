857e318a17ebd0a20410776b225e380595af1b1c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:37:26 +0700

    Upload SQL chunk: t2087_chunk_0000.sql

---
+++ sql_chunks/t2087_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t2087
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t2087" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2087
+INSERT INTO [t2087] VALUES (1,'من بلاغة القرآن فى التعبير بالغدو والأصال والعشى والإبكار','1','0','0');
+INSERT INTO [t2087] VALUES (2,'المقدمة','1','0','0');
+INSERT INTO [t2087] VALUES (3,'المبحث الأول: مدلولات الألفاظ الواردة في معنىطرفي النهار وسر تنوعها وتخصيصها بالذكر','3','0','0');
+INSERT INTO [t2087] VALUES (4,'المبحث الثاني: التسبيح بالغدو والآصال بين القائلين بالحقيقة والقائلين فيه بالمجاز','40','0','0');
