9d3c776ef7bd42b13c7b18884e6816d68fabf439
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:24 +0700

    Upload SQL chunk: t30049_chunk_0000.sql

---
+++ sql_chunks/t30049_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t30049
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t30049" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30049
+INSERT INTO [t30049] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t30049] VALUES (2,'منهج البحث:','2','1','0');
+INSERT INTO [t30049] VALUES (3,'باب الألف','7','0','0');
+INSERT INTO [t30049] VALUES (4,'باب الباء','33','0','0');
+INSERT INTO [t30049] VALUES (5,'باب التاء','36','0','0');
+INSERT INTO [t30049] VALUES (6,'باب الثاء','46','0','0');
+INSERT INTO [t30049] VALUES (7,'باب الجيم','47','0','0');
+INSERT INTO [t30049] VALUES (8,'باب الحاء','49','0','0');
+INSERT INTO [t30049] VALUES (9,'باب الخاء','55','0','0');
+INSERT INTO [t30049] VALUES (10,'باب الذال','58','0','0');
+INSERT INTO [t30049] VALUES (11,'باب الراء','59','0','0');
+INSERT INTO [t30049] VALUES (12,'باب السين','63','0','0');
+INSERT INTO [t30049] VALUES (13,'باب الشين','66','0','0');
+INSERT INTO [t30049] VALUES (14,'باب الصاد','68','0','0');
+INSERT INTO [t30049] VALUES (15,'باب الضاد','70','0','0');
+INSERT INTO [t30049] VALUES (16,'باب الطاء','71','0','0');
+INSERT INTO [t30049] VALUES (17,'باب العين','72','0','0');
+INSERT INTO [t30049] VALUES (18,'باب الفاء','74','0','0');
+INSERT INTO [t30049] VALUES (19,'باب القاف','77','0','0');
+INSERT INTO [t30049] VALUES (20,'باب الكاف','88','0','0');
+INSERT INTO [t30049] VALUES (21,'باب اللام','90','0','0');
+INSERT INTO [t30049] VALUES (22,'باب الميم','91','0','0');
+INSERT INTO [t30049] VALUES (23,'باب النون','110','0','0');
+INSERT INTO [t30049] VALUES (24,'باب الهاء','112','0','0');
+INSERT INTO [t30049] VALUES (25,'باب الواو','116','0','0');
+INSERT INTO [t30049] VALUES (26,'باب الياء','127','0','0');
