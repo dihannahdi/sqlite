efa48e8d27678e20106d100fff83cf2b03e50a4c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:57 +0700

    Upload SQL chunk: t2916_chunk_0000.sql

---
+++ sql_chunks/t2916_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t2916
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t2916" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2916
+INSERT INTO [t2916] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t2916] VALUES (2,'التعريف بالمد العارض للسكون: حده، أقسامه، أحكام المد به عند القراء','9','0','0');
+INSERT INTO [t2916] VALUES (3,'الأوجه الجائزة في المد العارض للسكون','12','0','0');
+INSERT INTO [t2916] VALUES (4,'طريقة الإدخال واستخراج النتائج','25','0','0');
+INSERT INTO [t2916] VALUES (5,'مصادر ومراجع','65','0','0');
