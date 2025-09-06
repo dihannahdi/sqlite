d140e91b99b832983c7490febefae9e820d80a59
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:10 +0700

    Upload SQL chunk: t12120_chunk_0000.sql

---
+++ sql_chunks/t12120_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t12120
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t12120" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12120
+INSERT INTO [t12120] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12120] VALUES (2,'الباب الأول: التوسل وحكمه','10','0','0');
+INSERT INTO [t12120] VALUES (3,'الباب الثاني: مسائل في توحيد الربوبية والألوهية','90','0','0');
+INSERT INTO [t12120] VALUES (4,'الباب الثالث: الشفاعة','127','0','0');
+INSERT INTO [t12120] VALUES (5,'الباب الرابع: التكفير','171','0','0');
+INSERT INTO [t12120] VALUES (6,'الباب الخامس: التبرك','199','0','0');
+INSERT INTO [t12120] VALUES (7,'الباب السادس: ملابسات محمد المالكي','221','0','0');
+INSERT INTO [t12120] VALUES (8,'الخاتمة','238','0','0');
+INSERT INTO [t12120] VALUES (9,'مصادر ومراجع','239','0','0');
