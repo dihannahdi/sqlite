c2d100d85122f2cadc038e17d02955384acb8acd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:37 +0700

    Upload SQL chunk: t11780_chunk_0000.sql

---
+++ sql_chunks/t11780_chunk_0000.sql
@@ -0,0 +1,42 @@
+-- Table: t11780
+-- Chunk: 1
+-- Rows: 35
+
+CREATE TABLE "t11780" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11780
+INSERT INTO [t11780] VALUES (1,'الجزء الاول','7','0','0');
+INSERT INTO [t11780] VALUES (2,'لابن حوقل النصيبى كتاب صورة الأرض','7','1','0');
+INSERT INTO [t11780] VALUES (3,'مقدمة القسم الأول','7','1','0');
+INSERT INTO [t11780] VALUES (4,'المقدمة','8','1','0');
+INSERT INTO [t11780] VALUES (5,'فهرس القسم الأول','11','1','0');
+INSERT INTO [t11780] VALUES (6,'[صورة الأرض]','11','1','0');
+INSERT INTO [t11780] VALUES (7,'[ديار العرب]','24','1','0');
+INSERT INTO [t11780] VALUES (8,'وما فى بطن هذه الصفحة فهو صورة ديار العرب، [٧ ب]','26','7','0');
+INSERT INTO [t11780] VALUES (9,'[بحر فارس]','47','1','0');
+INSERT INTO [t11780] VALUES (10,'(٢) وهذه صورة بحر فارس، [١٤ ظ]','48','9','0');
+INSERT INTO [t11780] VALUES (11,'[المغرب]','64','1','0');
+INSERT INTO [t11780] VALUES (12,'(٩) وهذه صورة المغرب، [١٩ ظ]','66','11','0');
+INSERT INTO [t11780] VALUES (13,'[الاندلس]','112','1','0');
+INSERT INTO [t11780] VALUES (14,'[صقليه]','122','1','0');
+INSERT INTO [t11780] VALUES (15,'[مصر]','136','1','0');
+INSERT INTO [t11780] VALUES (16,'[الشأم]','169','1','0');
+INSERT INTO [t11780] VALUES (17,'[بحر الروم]','193','1','0');
+INSERT INTO [t11780] VALUES (18,'[الجزيرة]','208','1','0');
+INSERT INTO [t11780] VALUES (19,'وهذه الصورة شكل الجزيرة، [٦١ ظ]','208','18','0');
+INSERT INTO [t11780] VALUES (20,'[العراق]','232','1','0');
+INSERT INTO [t11780] VALUES (21,'الجزء الثانى','248','0','0');
+INSERT INTO [t11780] VALUES (22,'[خوزستان]','248','21','0');
+INSERT INTO [t11780] VALUES (23,'[فارس]','258','21','0');
+INSERT INTO [t11780] VALUES (24,'وما فى بطن هذه الصفحة صورة فارس [٧٥ ب]','258','23','0');
+INSERT INTO [t11780] VALUES (25,'[كرمان]','302','21','0');
+INSERT INTO [t11780] VALUES (26,'[السند]','312','21','0');
+INSERT INTO [t11780] VALUES (27,'[ارمينيه واذربيجان والران]','326','21','0');
+INSERT INTO [t11780] VALUES (28,'[الجبال]','350','21','0');
+INSERT INTO [t11780] VALUES (29,'[الديلم وطبرستان]','367','21','0');
+INSERT INTO [t11780] VALUES (30,'[بحر الخزر]','378','21','0');
+INSERT INTO [t11780] VALUES (31,'[مفازة خراسان وفارس]','390','21','0');
+INSERT INTO [t11780] VALUES (32,'[سجستان]','401','21','0');
+INSERT INTO [t11780] VALUES (33,'[خراسان]','415','21','0');
+INSERT INTO [t11780] VALUES (34,'[ما وراء النهر]','448','21','0');
+INSERT INTO [t11780] VALUES (35,'[خاتمة الكتاب]','515','21','0');
