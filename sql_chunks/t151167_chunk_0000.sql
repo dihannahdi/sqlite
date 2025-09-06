ac63aa962fbdf66731050c336b14c04b2cb2c06b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:24:31 +0700

    Upload SQL chunk: t151167_chunk_0000.sql

---
+++ sql_chunks/t151167_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t151167
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t151167" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t151167
+INSERT INTO [t151167] VALUES (1,'تفسير سورة الطلاق','1','0','0');
+INSERT INTO [t151167] VALUES (2,'تفسير سورة المعارج','6','0','0');
+INSERT INTO [t151167] VALUES (3,'تفسير سورة نوح','14','0','0');
+INSERT INTO [t151167] VALUES (4,'تفسير سورة الإنسان','18','0','0');
