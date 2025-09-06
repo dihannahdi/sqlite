54cf9a3c79dae594405d41ff91345e6e55a525e8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:12 +0700

    Upload SQL chunk: t4144_chunk_0000.sql

---
+++ sql_chunks/t4144_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t4144
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t4144" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4144
+INSERT INTO [t4144] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t4144] VALUES (2,'المبحث الأول: مشروعية صلاة التوبة وسببها','4','0','0');
+INSERT INTO [t4144] VALUES (3,'المسألة الأولى: مشروعية صلاة التوبة','4','2','0');
+INSERT INTO [t4144] VALUES (4,'المسألة الثانية: سبب صلاة التوبة','9','2','0');
+INSERT INTO [t4144] VALUES (5,'المبحث الثاني: وقت صلاة التوبة','10','0','0');
+INSERT INTO [t4144] VALUES (6,'المبحث الثالث: محل صلاة التوبة','13','0','0');
+INSERT INTO [t4144] VALUES (7,'المبحث الرابع: صفة صلاة التوبة','17','0','0');
+INSERT INTO [t4144] VALUES (8,'الخاتمة','24','0','0');
