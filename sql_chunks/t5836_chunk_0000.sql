5f5cfdf7cf52e7b1b218f6d49fdc784e28120feb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:51 +0700

    Upload SQL chunk: t5836_chunk_0000.sql

---
+++ sql_chunks/t5836_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t5836
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t5836" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5836
+INSERT INTO [t5836] VALUES (1,'حرف الألف','4','0','0');
+INSERT INTO [t5836] VALUES (2,'حرف الباء','98','0','0');
+INSERT INTO [t5836] VALUES (3,'حرف التاء','116','0','0');
+INSERT INTO [t5836] VALUES (4,'حرف الثاء','118','0','0');
+INSERT INTO [t5836] VALUES (5,'حرف الجيم','123','0','0');
+INSERT INTO [t5836] VALUES (6,'حرف الحاء','137','0','0');
+INSERT INTO [t5836] VALUES (7,'حرف الخاء','198','0','0');
+INSERT INTO [t5836] VALUES (8,'حرف الدال','214','0','0');
+INSERT INTO [t5836] VALUES (9,'حرف الذال','223','0','0');
+INSERT INTO [t5836] VALUES (10,'حرف الراء','224','0','0');
+INSERT INTO [t5836] VALUES (11,'حرف الزاي','234','0','0');
+INSERT INTO [t5836] VALUES (12,'حرف السين','248','0','0');
+INSERT INTO [t5836] VALUES (13,'حرف الشين','291','0','0');
+INSERT INTO [t5836] VALUES (14,'حرف الصاد','299','0','0');
+INSERT INTO [t5836] VALUES (15,'حرف الضاد','308','0','0');
+INSERT INTO [t5836] VALUES (16,'حرف الطاء','311','0','0');
+INSERT INTO [t5836] VALUES (17,'حرف الظاء','316','0','0');
+INSERT INTO [t5836] VALUES (18,'حرف العين','317','0','0');
+INSERT INTO [t5836] VALUES (19,'حرف الغين','495','0','0');
+INSERT INTO [t5836] VALUES (20,'حرف الفاء','499','0','0');
+INSERT INTO [t5836] VALUES (21,'حرف القاف','508','0','0');
+INSERT INTO [t5836] VALUES (22,'حرف الكاف','520','0','0');
+INSERT INTO [t5836] VALUES (23,'حرف اللام','526','0','0');
+INSERT INTO [t5836] VALUES (24,'حرف الميم','528','0','0');
+INSERT INTO [t5836] VALUES (25,'حرف النون','682','0','0');
+INSERT INTO [t5836] VALUES (26,'حرف الهاء','694','0','0');
+INSERT INTO [t5836] VALUES (27,'حرف الواو','708','0','0');
+INSERT INTO [t5836] VALUES (28,'حرف اللام ألف','718','0','0');
+INSERT INTO [t5836] VALUES (29,'حرف الياء','719','0','0');
+INSERT INTO [t5836] VALUES (30,'باب الكنى','758','0','0');
+INSERT INTO [t5836] VALUES (31,'من عرف بأبيه','807','0','0');
