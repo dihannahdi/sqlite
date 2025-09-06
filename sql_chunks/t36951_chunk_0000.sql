081725f91b335b94f0eeb058e4eb74739046f6a4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:44 +0700

    Upload SQL chunk: t36951_chunk_0000.sql

---
+++ sql_chunks/t36951_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t36951
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t36951" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36951
+INSERT INTO [t36951] VALUES (1,'إهداء','2','0','0');
+INSERT INTO [t36951] VALUES (2,'المقدمة','3','0','0');
+INSERT INTO [t36951] VALUES (3,'الدراسة','7','0','0');
+INSERT INTO [t36951] VALUES (4,'السيدة فاطمة رضي الله عنها','17','3','0');
+INSERT INTO [t36951] VALUES (5,'حث العلماء على التزام الصمت والتحلي بحسن السمت','20','3','0');
+INSERT INTO [t36951] VALUES (6,'منهج السيوطي','28','3','0');
+INSERT INTO [t36951] VALUES (7,'ترجمة الإمام: ابن أبي الدنيا','31','3','0');
+INSERT INTO [t36951] VALUES (8,'مولده','31','7','0');
+INSERT INTO [t36951] VALUES (9,'آراء العلماء فيه وثناؤهم عليه','32','7','0');
+INSERT INTO [t36951] VALUES (10,'مصنفاته','33','7','0');
+INSERT INTO [t36951] VALUES (11,'وفاته','33','7','0');
+INSERT INTO [t36951] VALUES (12,'سمت بأسماء الكتب التي تم نشرها لابن أبي الدنيا','33','7','0');
+INSERT INTO [t36951] VALUES (13,'ترجمة جلال الدين السيوطي','39','3','0');
+INSERT INTO [t36951] VALUES (14,'مولده','39','13','0');
+INSERT INTO [t36951] VALUES (15,'نشأته','39','13','0');
+INSERT INTO [t36951] VALUES (16,'نشأته','39','13','0');
+INSERT INTO [t36951] VALUES (17,'شيوخه','40','13','0');
+INSERT INTO [t36951] VALUES (18,'تلاميذه','42','13','0');
+INSERT INTO [t36951] VALUES (19,'مصنفاته','43','13','0');
+INSERT INTO [t36951] VALUES (20,'تحقيق نسبة الكتاب للمصنف ورموز النسخ','46','13','0');
+INSERT INTO [t36951] VALUES (21,'كتاب حسن السمت في الصمت','55','0','0');
+INSERT INTO [t36951] VALUES (22,'المراجع والمصادر','112','0','0');
