cab124bd7c2e87e1b3b8b006f0a0a365cf92e69f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:23 +0700

    Upload SQL chunk: t12743_chunk_0000.sql

---
+++ sql_chunks/t12743_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t12743
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t12743" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12743
+INSERT INTO [t12743] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t12743] VALUES (2,'فضائل الشام للسمعاني','2','0','0');
