529c8b2afaf43663edbd7ed1dc05792fdfcbb5ef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:17 +0700

    Upload SQL chunk: t26033_chunk_0000.sql

---
+++ sql_chunks/t26033_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t26033
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t26033" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26033
+INSERT INTO [t26033] VALUES (1,'حرف الهمزة','48','0','0');
+INSERT INTO [t26033] VALUES (2,'حرف الباء الموحدة.','118','0','0');
+INSERT INTO [t26033] VALUES (3,'حرف التاء.','127','0','0');
+INSERT INTO [t26033] VALUES (4,'حرف الثاء','172','0','0');
+INSERT INTO [t26033] VALUES (5,'حرف الجيم','177','0','0');
+INSERT INTO [t26033] VALUES (6,'حرف الهاء المهملة.','192','0','0');
+INSERT INTO [t26033] VALUES (7,'حرف الخاء المعجمة','206','0','0');
+INSERT INTO [t26033] VALUES (8,'حرف الدال المهملة.','209','0','0');
+INSERT INTO [t26033] VALUES (9,'حرف الذال المعجمة','216','0','0');
+INSERT INTO [t26033] VALUES (10,'حرف الراء','221','0','0');
+INSERT INTO [t26033] VALUES (11,'حرف الزاي','231','0','0');
+INSERT INTO [t26033] VALUES (12,'حرف السين المهملة','235','0','0');
+INSERT INTO [t26033] VALUES (13,'حرف الشين المعجمة','246','0','0');
+INSERT INTO [t26033] VALUES (14,'حرف الصاد المهملة','255','0','0');
+INSERT INTO [t26033] VALUES (15,'حرف الضاد المعجمة','260','0','0');
+INSERT INTO [t26033] VALUES (16,'حرف الطاء المهملة','261','0','0');
+INSERT INTO [t26033] VALUES (17,'حرف الظاء المعجمة','266','0','0');
+INSERT INTO [t26033] VALUES (18,'حرف الغين المعجمة','277','0','0');
+INSERT INTO [t26033] VALUES (19,'حرف الفاء','281','0','0');
+INSERT INTO [t26033] VALUES (20,'حرف القاف','303','0','0');
+INSERT INTO [t26033] VALUES (21,'حرف الكاف','309','0','0');
+INSERT INTO [t26033] VALUES (22,'حرف اللام','315','0','0');
+INSERT INTO [t26033] VALUES (23,'حرف الميم','317','0','0');
+INSERT INTO [t26033] VALUES (24,'حرف النون','395','0','0');
+INSERT INTO [t26033] VALUES (25,'حرف الهاء','407','0','0');
+INSERT INTO [t26033] VALUES (26,'حرف الواو','410','0','0');
+INSERT INTO [t26033] VALUES (27,'حرف اللام الف','414','0','0');
+INSERT INTO [t26033] VALUES (28,'حرف الياء','415','0','0');
