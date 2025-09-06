72dbfbe6b77c4fea5036dae5bcbbe878eb85b0d2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:51 +0700

    Upload SQL chunk: t12183_chunk_0000.sql

---
+++ sql_chunks/t12183_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t12183
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t12183" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12183
+INSERT INTO [t12183] VALUES (1,'أخلاق الشباب المسلم','1','0','0');
+INSERT INTO [t12183] VALUES (2,'شروح وإيضاح','5','0','0');
+INSERT INTO [t12183] VALUES (3,'توضيحات','12','0','0');
