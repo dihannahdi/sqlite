079f90dc95d1f600661d3a079a26d65c4504457f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:56 +0700

    Upload SQL chunk: t6070_chunk_0000.sql

---
+++ sql_chunks/t6070_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6070
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6070" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6070
+INSERT INTO [t6070] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6070] VALUES (2,'وبه نستعين','1','0','0');
+INSERT INTO [t6070] VALUES (3,'أسامي الضعفاء والمتروكين عند أئمة الحديث','58','0','0');
