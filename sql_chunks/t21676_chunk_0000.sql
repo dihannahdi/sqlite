1770480b4bbff192d251cfa758fc35216c4dff64
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:08 +0700

    Upload SQL chunk: t21676_chunk_0000.sql

---
+++ sql_chunks/t21676_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21676
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21676" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21676
+INSERT INTO [t21676] VALUES (1,'عوالي مسند عبد بن حميد ٢٤٩ - مخطوط','2','0','0');
