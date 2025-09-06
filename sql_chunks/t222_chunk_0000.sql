016a45d64874ea2d746d7928fe85a479a159b36a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:12 +0700

    Upload SQL chunk: t222_chunk_0000.sql

---
+++ sql_chunks/t222_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t222
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t222" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t222
+INSERT INTO [t222] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t222] VALUES (2,'ذكر السبعة الأول المشهورة','1','0','0');
+INSERT INTO [t222] VALUES (3,'ذكر شواهد هذه الخصال السبعة','2','0','0');
+INSERT INTO [t222] VALUES (4,'ذكر السبعة الثانية التي أوردها شيخ الإسلام','3','0','0');
+INSERT INTO [t222] VALUES (5,'الحديث الأول','3','4','0');
+INSERT INTO [t222] VALUES (6,'ذكر السبعتين اللتين أوردهما شيخ الإسلام','5','0','0');
+INSERT INTO [t222] VALUES (7,'الحديث الأول','5','6','0');
+INSERT INTO [t222] VALUES (8,'الحديث الثاني','5','6','0');
+INSERT INTO [t222] VALUES (9,'الحديث الثالث','5','6','0');
+INSERT INTO [t222] VALUES (10,'الحديث الرابع','6','6','0');
+INSERT INTO [t222] VALUES (11,'الحديث الخامس','6','6','0');
+INSERT INTO [t222] VALUES (12,'الحديث الخامس','7','6','0');
+INSERT INTO [t222] VALUES (13,'الحديث السابع','7','6','0');
+INSERT INTO [t222] VALUES (14,'الحديث الثامن','7','6','0');
+INSERT INTO [t222] VALUES (15,'الحديث التاسع','7','6','0');
+INSERT INTO [t222] VALUES (16,'الحديث العاشر','7','6','0');
+INSERT INTO [t222] VALUES (17,'ذكر الخصال التي وقعت لي','8','0','0');
+INSERT INTO [t222] VALUES (18,'ثم تتبعت لأجد سبعة أخرى، فتكمل سبعين','14','0','0');
