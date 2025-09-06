e790f4d1526bc077e26cfd4c889103de47df9369
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:40 +0700

    Upload SQL chunk: t1597_chunk_0000.sql

---
+++ sql_chunks/t1597_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t1597
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t1597" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1597
+INSERT INTO [t1597] VALUES (1,'رواية ابن دريد عن أبي حاتم عن الأصمعي','1','0','0');
+INSERT INTO [t1597] VALUES (2,'تم والله أعلم','12','0','0');
