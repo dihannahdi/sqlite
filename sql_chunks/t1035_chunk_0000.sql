3eee092fc6d57f760f189f913e16acacb6dcaa87
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:23 +0700

    Upload SQL chunk: t1035_chunk_0000.sql

---
+++ sql_chunks/t1035_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t1035
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t1035" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1035
+INSERT INTO [t1035] VALUES (1,'الجزء ١','1','0','0');
+INSERT INTO [t1035] VALUES (2,'الجزء ٢','566','0','0');
+INSERT INTO [t1035] VALUES (3,'الجزء ٣','939','0','0');
+INSERT INTO [t1035] VALUES (4,'الجزء ٤','1459','0','0');
+INSERT INTO [t1035] VALUES (5,'الجزء ٥','1785','0','0');
+INSERT INTO [t1035] VALUES (6,'الجزء ٦','2313','0','0');
+INSERT INTO [t1035] VALUES (7,'الجزء ٧','2687','0','0');
+INSERT INTO [t1035] VALUES (8,'الجزء ٨','3084','0','0');
