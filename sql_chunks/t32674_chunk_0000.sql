9fd2ad372fd471fe799d456623bead5c87406ec2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:10 +0700

    Upload SQL chunk: t32674_chunk_0000.sql

---
+++ sql_chunks/t32674_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t32674
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t32674" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t32674
+INSERT INTO [t32674] VALUES (1,'هذا الكتاب','7','0','0');
+INSERT INTO [t32674] VALUES (2,'مقدمة الطبعة الفرنسية','11','0','0');
+INSERT INTO [t32674] VALUES (3,'الإهداء','18','0','0');
+INSERT INTO [t32674] VALUES (4,'مقدمة','19','0','0');
+INSERT INTO [t32674] VALUES (5,'توطئة','24','0','0');
+INSERT INTO [t32674] VALUES (6,'مشكلة الحضارة','26','0','0');
+INSERT INTO [t32674] VALUES (7,'مشكلة الثقافة','60','0','0');
+INSERT INTO [t32674] VALUES (8,'مشكلة المفهومية','84','0','0');
+INSERT INTO [t32674] VALUES (9,'الديمقراطية في الإسلام','122','0','0');
+INSERT INTO [t32674] VALUES (10,'إنتاج المستشرقين','155','0','0');
