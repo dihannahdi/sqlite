e76355beacbdc746a574dafbb7a945517ab9acf3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:57:06 +0700

    Upload SQL chunk: t9632_chunk_0000.sql

---
+++ sql_chunks/t9632_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t9632
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t9632" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9632
+INSERT INTO [t9632] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t9632] VALUES (2,'أحكام النون الساكنة والتنوين','2','0','0');
+INSERT INTO [t9632] VALUES (3,'أحكام النون والميم المشددتين','4','0','0');
+INSERT INTO [t9632] VALUES (4,'أحكام الميم الساكنة','4','0','0');
+INSERT INTO [t9632] VALUES (5,'حكم لام أل ولام الفعل','5','0','0');
+INSERT INTO [t9632] VALUES (6,'في المثلين والمتقاربين والمتجانسين','5','0','0');
+INSERT INTO [t9632] VALUES (7,'أقسام المد','6','0','0');
+INSERT INTO [t9632] VALUES (8,'أحكام المد','7','0','0');
+INSERT INTO [t9632] VALUES (9,'أقسام المد اللازم','7','0','0');
+INSERT INTO [t9632] VALUES (10,'الخاتمة','8','0','0');
