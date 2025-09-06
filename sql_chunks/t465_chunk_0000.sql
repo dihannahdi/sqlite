b03436b82de0d36f6080389263b22f850d39fdc5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:44 +0700

    Upload SQL chunk: t465_chunk_0000.sql

---
+++ sql_chunks/t465_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t465
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t465" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t465
+INSERT INTO [t465] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t465] VALUES (2,'تسمية حلب','1','0','0');
+INSERT INTO [t465] VALUES (3,'بناء مدينة حلب','2','0','0');
+INSERT INTO [t465] VALUES (4,'ملوك حلب','2','0','0');
+INSERT INTO [t465] VALUES (5,'فتح حلب','4','0','0');
+INSERT INTO [t465] VALUES (6,'دخول سيف الدولة حلب','18','0','0');
