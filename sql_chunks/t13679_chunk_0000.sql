8d67b1a1352017044c01aff041d030ba7478fb5b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:16 +0700

    Upload SQL chunk: t13679_chunk_0000.sql

---
+++ sql_chunks/t13679_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t13679
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t13679" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13679
+INSERT INTO [t13679] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t13679] VALUES (2,'باب أقسام العربية','3','0','0');
+INSERT INTO [t13679] VALUES (3,'باب الإعراب','9','0','0');
+INSERT INTO [t13679] VALUES (4,'باب رفع الاثنين والجمع','16','0','0');
+INSERT INTO [t13679] VALUES (5,'باب أقسام الأفعال','27','0','0');
+INSERT INTO [t13679] VALUES (6,'باب الفاعل والمفعول به','34','0','0');
+INSERT INTO [t13679] VALUES (7,'باب الابتداء','39','0','0');
+INSERT INTO [t13679] VALUES (8,'باب حروف الخفض','42','0','0');
+INSERT INTO [t13679] VALUES (9,'باب الحروف التي تنصب الأسماء وترفع الأخبار','46','0','0');
+INSERT INTO [t13679] VALUES (10,'باب الحروف التي ترفع الأسماء وتنصب الأخبار','49','0','0');
+INSERT INTO [t13679] VALUES (11,'باب الحروف التي تنصب الأفعال المستقبلة','51','0','0');
+INSERT INTO [t13679] VALUES (12,'باب الجواب بالفاء','55','0','0');
+INSERT INTO [t13679] VALUES (13,'باب الحروف التي تجزم الأفعال المستقبلة','61','0','0');
+INSERT INTO [t13679] VALUES (14,'باب حروف الرفع','65','0','0');
+INSERT INTO [t13679] VALUES (15,'باب المفعول الذي لم يسم فاعله','68','0','0');
+INSERT INTO [t13679] VALUES (16,'باب المعرفة والنكرة','71','0','0');
+INSERT INTO [t13679] VALUES (17,'باب ما يتبع الاسم في إعرابه','77','0','0');
+INSERT INTO [t13679] VALUES (18,'باب النعت','78','0','0');
+INSERT INTO [t13679] VALUES (19,'باب حروف العطف','83','0','0');
+INSERT INTO [t13679] VALUES (20,'باب التوكيد','86','0','0');
+INSERT INTO [t13679] VALUES (21,'باب البدل','91','0','0');
+INSERT INTO [t13679] VALUES (22,'باب الحال','94','0','0');
+INSERT INTO [t13679] VALUES (23,'باب الظروف','98','0','0');
+INSERT INTO [t13679] VALUES (24,'باب الإغراء والتحذير','101','0','0');
+INSERT INTO [t13679] VALUES (25,'باب التفسير','104','0','0');
+INSERT INTO [t13679] VALUES (26,'باب التعجب','106','0','0');
+INSERT INTO [t13679] VALUES (27,'باب النداء','108','0','0');
+INSERT INTO [t13679] VALUES (28,'باب العدد','111','0','0');
+INSERT INTO [t13679] VALUES (29,'حروف الاستثناء','114','0','0');
+INSERT INTO [t13679] VALUES (30,'باب علامات التانيث','119','0','0');
+INSERT INTO [t13679] VALUES (31,'باب ألفات الوصل في أوائل الأسماء','122','0','0');
+INSERT INTO [t13679] VALUES (32,'باب الأسماء التي لا تنصرف','123','0','0');
