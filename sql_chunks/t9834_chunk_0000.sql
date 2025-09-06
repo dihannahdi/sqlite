84940d47c5158656396bea4bf5fb955559579714
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:05 +0700

    Upload SQL chunk: t9834_chunk_0000.sql

---
+++ sql_chunks/t9834_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9834
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9834" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9834
+INSERT INTO [t9834] VALUES (1,'الباب الأول القيامة الصغرى','1','0','0');
+INSERT INTO [t9834] VALUES (2,'الباب الثاني أشراط الساعة','100','0','0');
