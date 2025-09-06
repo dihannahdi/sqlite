2064238861045bf39493656c7b13a88233405ca9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:23 +0700

    Upload SQL chunk: t7523_chunk_0000.sql

---
+++ sql_chunks/t7523_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t7523
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t7523" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7523
+INSERT INTO [t7523] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7523] VALUES (2,'المطلب الأول: آيات المعية','5','0','0');
+INSERT INTO [t7523] VALUES (3,'المطلب الثاني: أقوال العلماء في تفسير آيات المعية العامة','9','0','0');
+INSERT INTO [t7523] VALUES (4,'المطلب الثالث: الآثار والأقوال عن العلماء في الجمع بين صفتي العلو والمعية','27','0','0');
+INSERT INTO [t7523] VALUES (5,'المطلب الرابع: أقوال الناس في صفة المعية','47','0','0');
