96d76a89c57555002cd6ac0ae5d5f41e6f0f2d88
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:24 +0700

    Upload SQL chunk: t26735_chunk_0000.sql

---
+++ sql_chunks/t26735_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26735
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26735" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26735
+INSERT INTO [t26735] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26735] VALUES (2,'فضل أم المؤمنين عائشة','2','0','0');
