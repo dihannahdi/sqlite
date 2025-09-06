cd49ffb23a4a0b0d70c77a145d7f24a237134e8d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:44 +0700

    Upload SQL chunk: t6321_chunk_0000.sql

---
+++ sql_chunks/t6321_chunk_0000.sql
@@ -0,0 +1,59 @@
+-- Table: t6321
+-- Chunk: 1
+-- Rows: 52
+
+CREATE TABLE "t6321" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6321
+INSERT INTO [t6321] VALUES (1,'أما بعد','1','0','0');
+INSERT INTO [t6321] VALUES (2,'فصل','1','0','0');
+INSERT INTO [t6321] VALUES (3,'الكلام في الإجماع وما هو','4','0','0');
+INSERT INTO [t6321] VALUES (4,'فصل','12','0','0');
+INSERT INTO [t6321] VALUES (5,'فصل','13','0','0');
+INSERT INTO [t6321] VALUES (6,'فصل في نوعين من الاجماع','13','0','0');
+INSERT INTO [t6321] VALUES (7,'فصل في الكلام في حكم الاختلاف','14','0','0');
+INSERT INTO [t6321] VALUES (8,'فصل في النقل المتواتر','15','0','0');
+INSERT INTO [t6321] VALUES (9,'فصل في خبر الواحد وأنواعه','15','0','0');
+INSERT INTO [t6321] VALUES (10,'فصل العدل السيء الحفظ لا يجوز ان تقبل روايته','19','0','0');
+INSERT INTO [t6321] VALUES (11,'فصل','20','0','0');
+INSERT INTO [t6321] VALUES (12,'فصل','21','0','0');
+INSERT INTO [t6321] VALUES (13,'فصل','22','0','0');
+INSERT INTO [t6321] VALUES (14,'فصل','22','0','0');
+INSERT INTO [t6321] VALUES (15,'فصل','24','0','0');
+INSERT INTO [t6321] VALUES (16,'فصل','25','0','0');
+INSERT INTO [t6321] VALUES (17,'فصل','27','0','0');
+INSERT INTO [t6321] VALUES (18,'فصل','28','0','0');
+INSERT INTO [t6321] VALUES (19,'فصل','28','0','0');
+INSERT INTO [t6321] VALUES (20,'فصل','29','0','0');
+INSERT INTO [t6321] VALUES (21,'فصل','30','0','0');
+INSERT INTO [t6321] VALUES (22,'فصل','30','0','0');
+INSERT INTO [t6321] VALUES (23,'فصل آخر','35','0','0');
+INSERT INTO [t6321] VALUES (24,'فصل','35','0','0');
+INSERT INTO [t6321] VALUES (25,'فصل','36','0','0');
+INSERT INTO [t6321] VALUES (26,'فصل','36','0','0');
+INSERT INTO [t6321] VALUES (27,'فصل وكل عمل في الشريعة فهو اما معلق بوقت محدود الطرفين أو بوقت محدود المبدأ غير محدود الآخر فما كان معلقا بوقت محدود الطرفين لم يجز ان يوفي به في غير وقته ولا قبل وقته ولا بعده الا بنص او اجماع بالمجيء به في غير وقته فيوقف عنده والا فلا كالصلاة وصيام','37','0','0');
+INSERT INTO [t6321] VALUES (28,'فصل','37','0','0');
+INSERT INTO [t6321] VALUES (29,'فصل','38','0','0');
+INSERT INTO [t6321] VALUES (30,'فصل','38','0','0');
+INSERT INTO [t6321] VALUES (31,'فصل وكل من روى عن صاحب ولم يسمه فان كان ذلك الراوي ممن لا يجهل صحة قول مدعي الصحبه من بطلانه فهو خبر مسند تقوم به حجة لأن جميع الصحابة عدول قال الله تعالى للفقراء المهاجرين الذين أخرجوا من ديارهم وأموالهم يبتغون فضلا من الله ورضوانا وينصرون الله','39','0','0');
+INSERT INTO [t6321] VALUES (32,'فصل','39','0','0');
+INSERT INTO [t6321] VALUES (33,'فصل','41','0','0');
+INSERT INTO [t6321] VALUES (34,'فصل','41','0','0');
+INSERT INTO [t6321] VALUES (35,'فصل','42','0','0');
+INSERT INTO [t6321] VALUES (36,'فصل','42','0','0');
+INSERT INTO [t6321] VALUES (37,'فصل','43','0','0');
+INSERT INTO [t6321] VALUES (38,'فصل','43','0','0');
+INSERT INTO [t6321] VALUES (39,'فصل','45','0','0');
+INSERT INTO [t6321] VALUES (40,'فصل في الرأي','45','0','0');
+INSERT INTO [t6321] VALUES (41,'فصل','47','0','0');
+INSERT INTO [t6321] VALUES (42,'فصل في القياس','48','0','0');
+INSERT INTO [t6321] VALUES (43,'فصل','53','0','0');
+INSERT INTO [t6321] VALUES (44,'فصل','55','0','0');
+INSERT INTO [t6321] VALUES (45,'فصل في دليل الخطاب والخصوص','55','0','0');
+INSERT INTO [t6321] VALUES (46,'فصل','57','0','0');
+INSERT INTO [t6321] VALUES (47,'فصل في التقليد','57','0','0');
+INSERT INTO [t6321] VALUES (48,'فصل','58','0','0');
+INSERT INTO [t6321] VALUES (49,'فصل','60','0','0');
+INSERT INTO [t6321] VALUES (50,'فصل','60','0','0');
+INSERT INTO [t6321] VALUES (51,'فصل','61','0','0');
+INSERT INTO [t6321] VALUES (52,'فصل','61','0','0');
