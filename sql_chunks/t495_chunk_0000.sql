8b85930d9c6c6d02b21e16de2b98405fc9fba2d4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:24 +0700

    Upload SQL chunk: t495_chunk_0000.sql

---
+++ sql_chunks/t495_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t495
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t495" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t495
+INSERT INTO [t495] VALUES (1,'الباب الأول في الجمل وفيه أربع مسائل','2','0','0');
+INSERT INTO [t495] VALUES (2,'الجمل التي لها محل من الإعراب','3','1','0');
+INSERT INTO [t495] VALUES (3,'الجمل التي لا محل لها من الإعراب','6','1','0');
+INSERT INTO [t495] VALUES (4,'الباب الثاني: في الظرف والجار والمجرور وفيه أربع مسائل','11','0','0');
+INSERT INTO [t495] VALUES (5,'الباب الثالث: فيما يقال عند ذكر أدوات يكثر دورها في الكلام','19','0','0');
