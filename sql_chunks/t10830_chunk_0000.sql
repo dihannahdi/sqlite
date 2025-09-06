159b3d0a91a5ff12f7ef6846bc638d2b8cbf4065
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:47 +0700

    Upload SQL chunk: t10830_chunk_0000.sql

---
+++ sql_chunks/t10830_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10830
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10830" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10830
+INSERT INTO [t10830] VALUES (1,'الضوء الساري في معرفة خبر تميم الداري للمقريزي - مخطوط','1','0','0');
