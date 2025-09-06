3d681ed74f5787a136e244337181b7905fe1cb2e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:20 +0700

    Upload SQL chunk: t36098_chunk_0000.sql

---
+++ sql_chunks/t36098_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t36098
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t36098" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36098
+INSERT INTO [t36098] VALUES (1,'١','2','0','0');
+INSERT INTO [t36098] VALUES (2,'عناصر الدرس','2','1','0');
+INSERT INTO [t36098] VALUES (3,'٢','24','0','0');
+INSERT INTO [t36098] VALUES (4,'عناصر الدرس','24','3','0');
+INSERT INTO [t36098] VALUES (5,'٣','40','0','0');
+INSERT INTO [t36098] VALUES (6,'عناصر الدرس','40','5','0');
+INSERT INTO [t36098] VALUES (7,'٤','59','0','0');
+INSERT INTO [t36098] VALUES (8,'عناصر الدرس','59','7','0');
+INSERT INTO [t36098] VALUES (9,'٥','81','0','0');
+INSERT INTO [t36098] VALUES (10,'عناصر الدرس','81','9','0');
+INSERT INTO [t36098] VALUES (11,'٦','96','0','0');
+INSERT INTO [t36098] VALUES (12,'عناصر الدرس','96','11','0');
+INSERT INTO [t36098] VALUES (13,'٧','116','0','0');
+INSERT INTO [t36098] VALUES (14,'عناصر الدرس','116','13','0');
+INSERT INTO [t36098] VALUES (15,'٨','139','0','0');
+INSERT INTO [t36098] VALUES (16,'عناصر الدرس','139','15','0');
+INSERT INTO [t36098] VALUES (17,'٩','158','0','0');
+INSERT INTO [t36098] VALUES (18,'عناصر الدرس','158','17','0');
+INSERT INTO [t36098] VALUES (19,'١٠','175','0','0');
+INSERT INTO [t36098] VALUES (20,'عناصر الدرس','175','19','0');
+INSERT INTO [t36098] VALUES (21,'١١','198','0','0');
+INSERT INTO [t36098] VALUES (22,'عناصر الدرس','198','21','0');
+INSERT INTO [t36098] VALUES (23,'١٢','219','0','0');
+INSERT INTO [t36098] VALUES (24,'عناصر الدرس','219','23','0');
+INSERT INTO [t36098] VALUES (25,'١٣','240','0','0');
+INSERT INTO [t36098] VALUES (26,'عناصر الدرس','240','25','0');
+INSERT INTO [t36098] VALUES (27,'١٤','256','0','0');
+INSERT INTO [t36098] VALUES (28,'عناصر الدرس','256','27','0');
+INSERT INTO [t36098] VALUES (29,'١٥','284','0','0');
+INSERT INTO [t36098] VALUES (30,'عناصر الدرس','284','29','0');
