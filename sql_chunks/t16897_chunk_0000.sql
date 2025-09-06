92ca942c76a4b6e5d97203e48c51df38a03e238b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:27:28 +0700

    Upload SQL chunk: t16897_chunk_0000.sql

---
+++ sql_chunks/t16897_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t16897
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t16897" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t16897
+INSERT INTO [t16897] VALUES (1,'[مقدمة المحقق]','3','0','0');
+INSERT INTO [t16897] VALUES (2,'عملى فى الكتاب','4','1','0');
+INSERT INTO [t16897] VALUES (3,'ترجمة المصنف','5','1','0');
+INSERT INTO [t16897] VALUES (4,'١ - نسبه ونشأته العلمية','5','3','0');
+INSERT INTO [t16897] VALUES (5,'٢ - شيوخه الذين تلقى عنهم','5','3','0');
+INSERT INTO [t16897] VALUES (6,'٣ - تلاميذه الذين أخذوا عنه','5','3','0');
+INSERT INTO [t16897] VALUES (7,'٤ - مناصبه العلمية','6','3','0');
+INSERT INTO [t16897] VALUES (8,'٥ - شعره','6','3','0');
+INSERT INTO [t16897] VALUES (9,'٦ - ثناء العلماء عليه','6','3','0');
+INSERT INTO [t16897] VALUES (10,'٧ - مآخذ العلماء عليه','7','3','0');
+INSERT INTO [t16897] VALUES (11,'٨ - مصنفاته','7','3','0');
+INSERT INTO [t16897] VALUES (12,'٩ - وفاته','8','3','0');
+INSERT INTO [t16897] VALUES (13,'وصف مخطوط الكتاب','12','1','0');
+INSERT INTO [t16897] VALUES (14,'بداية الكتاب','15','0','0');
