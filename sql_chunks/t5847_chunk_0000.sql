9063cd58e4841ca29a4d8dc7362765781f877972
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:55 +0700

    Upload SQL chunk: t5847_chunk_0000.sql

---
+++ sql_chunks/t5847_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t5847
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t5847" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5847
+INSERT INTO [t5847] VALUES (1,'حرف الألف','1','0','0');
+INSERT INTO [t5847] VALUES (2,'حرف الباء','53','0','0');
+INSERT INTO [t5847] VALUES (3,'حرف الثاء المثلثة','57','0','0');
+INSERT INTO [t5847] VALUES (4,'حرف الثاء المثلثة','58','0','0');
+INSERT INTO [t5847] VALUES (5,'حرف الجيم','59','0','0');
+INSERT INTO [t5847] VALUES (6,'حرف الجيم','60','0','0');
+INSERT INTO [t5847] VALUES (7,'حرف الحاء المهملة','64','0','0');
+INSERT INTO [t5847] VALUES (8,'حرف الخاء المعجمة','78','0','0');
+INSERT INTO [t5847] VALUES (9,'حرف الدال','83','0','0');
+INSERT INTO [t5847] VALUES (10,'حرف الدال','84','0','0');
+INSERT INTO [t5847] VALUES (11,'حرف الذال المعجمة','89','0','0');
+INSERT INTO [t5847] VALUES (12,'حرف الراء المهملة','90','0','0');
+INSERT INTO [t5847] VALUES (13,'حرف الزاي','96','0','0');
+INSERT INTO [t5847] VALUES (14,'حرف السين المهملة','104','0','0');
+INSERT INTO [t5847] VALUES (15,'حرف الشين','114','0','0');
+INSERT INTO [t5847] VALUES (16,'حرف الصاد المهملة','115','0','0');
+INSERT INTO [t5847] VALUES (17,'حرف الضاد المعجمة','118','0','0');
+INSERT INTO [t5847] VALUES (18,'حرف الطاء المهملة','119','0','0');
+INSERT INTO [t5847] VALUES (19,'حرف العين','121','0','0');
+INSERT INTO [t5847] VALUES (20,'حرف الفاء','160','0','0');
+INSERT INTO [t5847] VALUES (21,'حرف القاف','163','0','0');
+INSERT INTO [t5847] VALUES (22,'حرف الكاف','166','0','0');
+INSERT INTO [t5847] VALUES (23,'حرف الميم','168','0','0');
+INSERT INTO [t5847] VALUES (24,'حرف الدال','175','0','0');
+INSERT INTO [t5847] VALUES (25,'حرف النون','190','0','0');
+INSERT INTO [t5847] VALUES (26,'حرف الهاء','193','0','0');
+INSERT INTO [t5847] VALUES (27,'حرف الواو','195','0','0');
+INSERT INTO [t5847] VALUES (28,'حرف الياء','197','0','0');
+INSERT INTO [t5847] VALUES (29,'باب الكنى','203','0','0');
+INSERT INTO [t5847] VALUES (30,'فصل في الأنساب','214','0','0');
+INSERT INTO [t5847] VALUES (31,'آخر الكتاب','215','0','0');
