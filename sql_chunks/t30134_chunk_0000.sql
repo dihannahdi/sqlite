a85ad62e2ecca66a88ebdecea3d3fce5e05cbceb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:55 +0700

    Upload SQL chunk: t30134_chunk_0000.sql

---
+++ sql_chunks/t30134_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t30134
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t30134" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30134
+INSERT INTO [t30134] VALUES (1,'محمد بن بشر','2','0','0');
+INSERT INTO [t30134] VALUES (2,'محمد بن سابق','4','0','0');
+INSERT INTO [t30134] VALUES (3,'ومما زيد في حديثه أي: محمد بن سابق:','7','0','0');
+INSERT INTO [t30134] VALUES (4,'محمد بن الصباح','16','0','0');
+INSERT INTO [t30134] VALUES (5,'محمد بن عبيد الطنافسي','24','0','0');
+INSERT INTO [t30134] VALUES (6,'محمد بن مصعب القرقساني','28','0','0');
+INSERT INTO [t30134] VALUES (7,'أبو أحمد محمد بن عبد الله بن الزبير الكوفي الزبيري مولى بني أسد','36','0','0');
+INSERT INTO [t30134] VALUES (8,'محمد بن عبد الله الأنصاري','43','0','0');
+INSERT INTO [t30134] VALUES (9,'عارم بن الفضل السدوسي','69','0','0');
+INSERT INTO [t30134] VALUES (10,'محمد بن كثير العبدي','78','0','0');
+INSERT INTO [t30134] VALUES (11,'محمد بن كناسة','82','0','0');
