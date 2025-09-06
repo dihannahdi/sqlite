47603df097a8063fbdd4688c339e2ab8779bf7b5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:09 +0700

    Upload SQL chunk: t6718_chunk_0000.sql

---
+++ sql_chunks/t6718_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t6718
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t6718" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6718
+INSERT INTO [t6718] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6718] VALUES (2,'الوافدون على معاوية من طريق الحافظ ابن عساكر','21','0','0');
+INSERT INTO [t6718] VALUES (3,'خالد بن المعمر السدوسي','26','0','0');
+INSERT INTO [t6718] VALUES (4,'شريك الأعور','29','0','0');
+INSERT INTO [t6718] VALUES (5,'صعصعة بن صوحان العبدي','32','0','0');
+INSERT INTO [t6718] VALUES (6,'أبو الطفيل عامر بن واثلة الكناني','35','0','0');
