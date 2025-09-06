25f0021e241c25979255ba2d605f871d68e67089
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:58 +0700

    Upload SQL chunk: t1478_chunk_0000.sql

---
+++ sql_chunks/t1478_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t1478
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t1478" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1478
+INSERT INTO [t1478] VALUES (1,'وحدة النص أو الوحدة الفنية تنشطر إلى قسمين','3','0','0');
+INSERT INTO [t1478] VALUES (2,'السور الكريمة تخضع للأنماط الآتية من البناء الفني من حيث الأشكال','3','0','0');
+INSERT INTO [t1478] VALUES (3,'السورة القرآنية الكريمة تخضع لواحد من الأبنية الآتية لأفكارها وموضوعاتها','4','0','0');
+INSERT INTO [t1478] VALUES (4,'هذه السببية تلحظ على أنماط متنوعة','5','0','0');
