dc1315249812bc40bf0f0702c2d9c81b02bc7544
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:41 +0700

    Upload SQL chunk: t10409_chunk_0000.sql

---
+++ sql_chunks/t10409_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t10409
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t10409" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10409
+INSERT INTO [t10409] VALUES (1,'خطبة الكتاب','1','0','0');
+INSERT INTO [t10409] VALUES (2,'تعريف الشعر','3','0','0');
+INSERT INTO [t10409] VALUES (3,'مكانة الشعر العربي','5','0','0');
+INSERT INTO [t10409] VALUES (4,'ما ورد في تفسير آيات الشعراء','11','0','0');
+INSERT INTO [t10409] VALUES (5,'أضواء على الآيات وفوائد تتعلق بها','14','0','0');
+INSERT INTO [t10409] VALUES (6,'القرآن الكريم وسر إعجازه','17','0','0');
+INSERT INTO [t10409] VALUES (7,'الأحاديث والأمر بهجاء المشركين','19','0','0');
+INSERT INTO [t10409] VALUES (8,'نبذة عن شعر حسان','22','0','0');
+INSERT INTO [t10409] VALUES (9,'استنشاده صلى الله عليه وسلم واستماعه وما تمثل به منه وبيان أنه بمنزلة الكلام','25','0','0');
+INSERT INTO [t10409] VALUES (10,'كراهة الشعر لمن غلب عليه','30','0','0');
+INSERT INTO [t10409] VALUES (11,'مصادر ومراجع','32','0','0');
