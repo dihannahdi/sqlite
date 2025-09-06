31c35fb64cd6e60a3f9857ad86bc3f41c250a492
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:50 +0700

    Upload SQL chunk: t3508_chunk_0000.sql

---
+++ sql_chunks/t3508_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t3508
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t3508" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3508
+INSERT INTO [t3508] VALUES (1,'التابعين ومن بعدهم من أهل مكة','58','0','0');
+INSERT INTO [t3508] VALUES (2,'الأول من التابعين ومن بعدهم من أهل المدينة','151','0','0');
+INSERT INTO [t3508] VALUES (3,'أهل الكوفة','256','0','0');
+INSERT INTO [t3508] VALUES (4,'الأول من البصريين','633','0','0');
+INSERT INTO [t3508] VALUES (5,'تسمية الخراسانيين','905','0','0');
+INSERT INTO [t3508] VALUES (6,'تسمية أهل واسط والسواد وأهل المدائن وبغداد ومن يليهم','926','0','0');
+INSERT INTO [t3508] VALUES (7,'تسمية الشاميين وأهل مصر والجزيرة','962','0','0');
+INSERT INTO [t3508] VALUES (8,'فيه تسمية التابعين ومن بعدهم من الشاميين وأهل مصر وأهل الجزيرة وأهل الرقة وما يليهم','1022','0','0');
+INSERT INTO [t3508] VALUES (9,'رسالة مالك إلى الليث بن سعد','1046','0','0');
