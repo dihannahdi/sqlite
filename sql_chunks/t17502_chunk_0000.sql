dd239785b830451a06da2c0c962075021af4da6a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:30:17 +0700

    Upload SQL chunk: t17502_chunk_0000.sql

---
+++ sql_chunks/t17502_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t17502
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t17502" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17502
+INSERT INTO [t17502] VALUES (1,'لذي تواجهه أمة محمد الآن أشد من كل ما واجهت في سالف الأيام','4','0','0');
+INSERT INTO [t17502] VALUES (2,'زعم اليهود أنهم مظلومون','8','0','0');
+INSERT INTO [t17502] VALUES (3,'وبعد، فأنا رجل معتزل','29','0','0');
