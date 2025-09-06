bc6f2051581b82d0ed594373947bc2c4c5df597f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:59 +0700

    Upload SQL chunk: t21580_chunk_0000.sql

---
+++ sql_chunks/t21580_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t21580
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t21580" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21580
+INSERT INTO [t21580] VALUES (1,'بطاقة الكتاب','1','0','0');
+INSERT INTO [t21580] VALUES (2,'مقدمة الطبعة الثانية','2','0','0');
+INSERT INTO [t21580] VALUES (3,'سند المؤلف لمتن الجزرية','4','0','0');
+INSERT INTO [t21580] VALUES (4,'شكر وتقدير','4','0','0');
+INSERT INTO [t21580] VALUES (5,'مقدمة الناظم','6','0','0');
+INSERT INTO [t21580] VALUES (6,'باب مخارج الحروف','8','0','0');
+INSERT INTO [t21580] VALUES (7,'باب صفات الحروف','13','0','0');
+INSERT INTO [t21580] VALUES (8,'باب التجويد','24','0','0');
+INSERT INTO [t21580] VALUES (9,'باب ذكر بعض التنبيهات','27','0','0');
+INSERT INTO [t21580] VALUES (10,'باب الراءات','29','0','0');
+INSERT INTO [t21580] VALUES (11,'باب اللامات وأحكام متفرقة','31','0','0');
+INSERT INTO [t21580] VALUES (12,'الضاد والظاء','36','0','0');
+INSERT INTO [t21580] VALUES (13,'النون والميم المشددتين والميم الساكنة','41','0','0');
+INSERT INTO [t21580] VALUES (14,'باب أحكام النون الساكنة والتنوين','43','0','0');
+INSERT INTO [t21580] VALUES (15,'المدود','48','0','0');
+INSERT INTO [t21580] VALUES (16,'الوقف والابتداء','55','0','0');
+INSERT INTO [t21580] VALUES (17,'المقطوع والموصول','61','0','0');
+INSERT INTO [t21580] VALUES (18,'التاءات','72','0','0');
+INSERT INTO [t21580] VALUES (19,'همز الوصل','81','0','0');
+INSERT INTO [t21580] VALUES (20,'باب الوقف على آخر الكلم','83','0','0');
+INSERT INTO [t21580] VALUES (21,'فوائد متفرقة','86','0','0');
+INSERT INTO [t21580] VALUES (22,'الخاتمة','106','0','0');
