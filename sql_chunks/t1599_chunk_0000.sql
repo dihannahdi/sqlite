7027c68c4277b11033b9485bd5d76144d40b4481
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:42 +0700

    Upload SQL chunk: t1599_chunk_0000.sql

---
+++ sql_chunks/t1599_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t1599
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t1599" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1599
+INSERT INTO [t1599] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t1599] VALUES (2,'الباب الأول','1','0','0');
+INSERT INTO [t1599] VALUES (3,'الباب الثاني','4','0','0');
+INSERT INTO [t1599] VALUES (4,'الباب الثالث','6','0','0');
+INSERT INTO [t1599] VALUES (5,'الباب الرابع','15','0','0');
+INSERT INTO [t1599] VALUES (6,'الباب الخامس','18','0','0');
+INSERT INTO [t1599] VALUES (7,'الباب السادس','19','0','0');
+INSERT INTO [t1599] VALUES (8,'الباب السابع','22','0','0');
+INSERT INTO [t1599] VALUES (9,'الباب الثامن','25','0','0');
+INSERT INTO [t1599] VALUES (10,'الباب التاسع','27','0','0');
+INSERT INTO [t1599] VALUES (11,'الباب العاشر','30','0','0');
+INSERT INTO [t1599] VALUES (12,'الباب الحادي عشر','31','0','0');
+INSERT INTO [t1599] VALUES (13,'الباب الثاني عشر','33','0','0');
+INSERT INTO [t1599] VALUES (14,'الباب الثالث عشر','44','0','0');
+INSERT INTO [t1599] VALUES (15,'الباب الرابع عشر','46','0','0');
+INSERT INTO [t1599] VALUES (16,'الباب الخامس عشر','48','0','0');
+INSERT INTO [t1599] VALUES (17,'الباب السادس عشر','50','0','0');
+INSERT INTO [t1599] VALUES (18,'الباب السابع عشر','56','0','0');
+INSERT INTO [t1599] VALUES (19,'الباب التاسع عشر','63','0','0');
+INSERT INTO [t1599] VALUES (20,'الباب العشرون','66','0','0');
+INSERT INTO [t1599] VALUES (21,'الباب الحادي والعشرون','70','0','0');
+INSERT INTO [t1599] VALUES (22,'الباب الثاني والعشرون','73','0','0');
+INSERT INTO [t1599] VALUES (23,'الباب الثالث والعشرون','76','0','0');
+INSERT INTO [t1599] VALUES (24,'الباب الرابع والعشرون','77','0','0');
+INSERT INTO [t1599] VALUES (25,'الباب الخامس والعشرون','79','0','0');
