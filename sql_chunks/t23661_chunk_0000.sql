f13ab58175b78a1d5d6369c022535d7f41895365
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:48:04 +0700

    Upload SQL chunk: t23661_chunk_0000.sql

---
+++ sql_chunks/t23661_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t23661
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t23661" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23661
+INSERT INTO [t23661] VALUES (1,'[المدخل] (بسم الله الرحمن الرحيم)','1','0','0');
+INSERT INTO [t23661] VALUES (2,'«ولادته ونسبه الشريف»','2','0','0');
+INSERT INTO [t23661] VALUES (3,'(شق جبريل لصدره في الرابعة من عمره)','4','0','0');
+INSERT INTO [t23661] VALUES (4,'سفره الى الشام مع عمه ابي طالب ورجوعه الى مكة عند وصوله بصرى','4','0','0');
+INSERT INTO [t23661] VALUES (5,'كيف ومتى جاءه الوحي؟','6','0','0');
+INSERT INTO [t23661] VALUES (6,'- الاسراء والمعراج-','8','0','0');
+INSERT INTO [t23661] VALUES (7,'هجرته- صلى الله عليه وسلم-','10','0','0');
+INSERT INTO [t23661] VALUES (8,'بناء المسجد','12','0','0');
+INSERT INTO [t23661] VALUES (9,'الاذن في الجهاد','13','0','0');
