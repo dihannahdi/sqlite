47b25b3b56103ac158fe52d1b994bc1f39570054
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:01 +0700

    Upload SQL chunk: t410_chunk_0000.sql

---
+++ sql_chunks/t410_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t410
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t410" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t410
+INSERT INTO [t410] VALUES (1,'الفصل الأول','1','0','0');
+INSERT INTO [t410] VALUES (2,'أولا: في ذكر قبائل اليمن وبيانهم','1','1','0');
+INSERT INTO [t410] VALUES (3,'ثانيا: فصل في ذكر قبائل صنعاء','1','1','0');
+INSERT INTO [t410] VALUES (4,'الفصل الثاني','3','0','0');
+INSERT INTO [t410] VALUES (5,'أولا: في معرفة عرب تهامة','3','4','0');
+INSERT INTO [t410] VALUES (6,'ثانيا: في ذكر قبائل الحجاز','7','4','0');
+INSERT INTO [t410] VALUES (7,'الفصل الثالث في ذكر نجد','9','0','0');
+INSERT INTO [t410] VALUES (8,'الفصل الرابع في قبائل نجد','10','0','0');
+INSERT INTO [t410] VALUES (9,'الفصل الخامس في ذكر عمان وسواحلها','11','0','0');
+INSERT INTO [t410] VALUES (10,'الفصل السادس الإحساء','11','0','0');
+INSERT INTO [t410] VALUES (11,'الفصل السابع في قبائل العراق','13','0','0');
+INSERT INTO [t410] VALUES (12,'الفصل الثامن في قبائل حلب من عنزة وغيرهم','17','0','0');
