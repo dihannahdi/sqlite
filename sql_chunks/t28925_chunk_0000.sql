7f25f8d1caac2f2ce53c962bb49472e9a01c25c7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:49 +0700

    Upload SQL chunk: t28925_chunk_0000.sql

---
+++ sql_chunks/t28925_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t28925
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t28925" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t28925
+INSERT INTO [t28925] VALUES (1,'من تعبد؟ ، قال: الذي في السماء عرشه، وفي الأرض سلطانه، وفي البحر سبيله، وفي الجنة رحمته،','1','0','0');
