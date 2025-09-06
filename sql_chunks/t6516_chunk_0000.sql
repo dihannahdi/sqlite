159c7e45047086baa89bfd9324c9f504f86866a3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:15 +0700

    Upload SQL chunk: t6516_chunk_0000.sql

---
+++ sql_chunks/t6516_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t6516
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t6516" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6516
+INSERT INTO [t6516] VALUES (1,'الفصل الأول','1','0','0');
+INSERT INTO [t6516] VALUES (2,'الفصل الثاني','2','0','0');
+INSERT INTO [t6516] VALUES (3,'الفصل الثاني','3','0','0');
+INSERT INTO [t6516] VALUES (4,'في شرح فرق الخوارج','9','0','0');
+INSERT INTO [t6516] VALUES (5,'الروافض','15','0','0');
+INSERT INTO [t6516] VALUES (6,'فصل','29','0','0');
+INSERT INTO [t6516] VALUES (7,'في فرق الكرامية','30','0','0');
+INSERT INTO [t6516] VALUES (8,'في فرق الجبرية','31','0','0');
+INSERT INTO [t6516] VALUES (9,'في المرجئية','33','0','0');
+INSERT INTO [t6516] VALUES (10,'في أحوال الصوفيه','35','0','0');
+INSERT INTO [t6516] VALUES (11,'ذكر بعض فرق الأسلامية','37','0','0');
+INSERT INTO [t6516] VALUES (12,'في الذين يتظاهرون بالإسلام وإن لم يكونوا مسلمين','39','0','0');
+INSERT INTO [t6516] VALUES (13,'في شرح الفرق الذين هم خارجون على الإسلام بالحقيقة وبالإسم','45','0','0');
+INSERT INTO [t6516] VALUES (14,'الفصل الأول','45','0','0');
+INSERT INTO [t6516] VALUES (15,'الفصل الثاني','47','0','0');
+INSERT INTO [t6516] VALUES (16,'الفصل الثالث','49','0','0');
+INSERT INTO [t6516] VALUES (17,'فصل في الثنوية','51','0','0');
+INSERT INTO [t6516] VALUES (18,'الفصل الخامس','53','0','0');
+INSERT INTO [t6516] VALUES (19,'الفصل السادس','54','0','0');
