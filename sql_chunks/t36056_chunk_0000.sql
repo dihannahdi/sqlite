ee1d124aff74f8951fe5e51feb4e6f3fe7c85610
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:57 +0700

    Upload SQL chunk: t36056_chunk_0000.sql

---
+++ sql_chunks/t36056_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t36056
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t36056" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36056
+INSERT INTO [t36056] VALUES (1,'١','2','0','0');
+INSERT INTO [t36056] VALUES (2,'عناصر الدرس','2','1','0');
+INSERT INTO [t36056] VALUES (3,'٢','29','0','0');
+INSERT INTO [t36056] VALUES (4,'عناصر الدرس','29','3','0');
+INSERT INTO [t36056] VALUES (5,'٣','57','0','0');
+INSERT INTO [t36056] VALUES (6,'عناصر الدرس','57','5','0');
+INSERT INTO [t36056] VALUES (7,'٤','81','0','0');
+INSERT INTO [t36056] VALUES (8,'عناصر الدرس','81','7','0');
+INSERT INTO [t36056] VALUES (9,'٥','114','0','0');
+INSERT INTO [t36056] VALUES (10,'عناصر الدرس','114','9','0');
