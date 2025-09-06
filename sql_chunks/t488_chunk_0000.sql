b458016138eff97e45e296d67bd8d9d6c3846490
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:14 +0700

    Upload SQL chunk: t488_chunk_0000.sql

---
+++ sql_chunks/t488_chunk_0000.sql
@@ -0,0 +1,51 @@
+-- Table: t488
+-- Chunk: 1
+-- Rows: 44
+
+CREATE TABLE "t488" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t488
+INSERT INTO [t488] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t488] VALUES (2,'تعريف النحو','3','1','0');
+INSERT INTO [t488] VALUES (3,'تعريف الإعراب','3','1','0');
+INSERT INTO [t488] VALUES (4,'تعريف البناء','3','1','0');
+INSERT INTO [t488] VALUES (5,'فائدة النحو','4','1','0');
+INSERT INTO [t488] VALUES (6,'الكلام وأقسامه','4','1','0');
+INSERT INTO [t488] VALUES (7,'الفعل الماضي','4','1','0');
+INSERT INTO [t488] VALUES (8,'الفعل المضارع','4','1','0');
+INSERT INTO [t488] VALUES (9,'فعل الأمر','6','1','0');
+INSERT INTO [t488] VALUES (10,'الاسم وعلامته','6','1','0');
+INSERT INTO [t488] VALUES (11,'المبنيات','7','1','0');
+INSERT INTO [t488] VALUES (12,'أنواع البناء','9','1','0');
+INSERT INTO [t488] VALUES (13,'أسباب الفتح والكسر والضم','10','1','0');
+INSERT INTO [t488] VALUES (14,'باب الممنوع','11','1','0');
+INSERT INTO [t488] VALUES (15,'خصائص الاسم','14','1','0');
+INSERT INTO [t488] VALUES (16,'إعراب الأسماء المنصرفة','15','1','0');
+INSERT INTO [t488] VALUES (17,'ما تقدر عليه الحركة','15','1','0');
+INSERT INTO [t488] VALUES (18,'المثنى وجمع المذكر السالم والأسماء الخمسة','15','1','0');
+INSERT INTO [t488] VALUES (19,'الفاعل ونائبه','16','1','0');
+INSERT INTO [t488] VALUES (20,'مواضع وجوب تقديم الفاعل على المفعول','16','1','0');
+INSERT INTO [t488] VALUES (21,'المبتدأ','17','1','0');
+INSERT INTO [t488] VALUES (22,'الخبر ومواضع وجوب تقديم المبتدأ','17','1','0');
+INSERT INTO [t488] VALUES (23,'مواضع وجوب تقديم الخبر','17','1','0');
+INSERT INTO [t488] VALUES (24,'تعدد الخبر ومجيء المبتدأ نكرة والخبر جملة','17','1','0');
+INSERT INTO [t488] VALUES (25,'وجوب حذف المبتدأ','17','1','0');
+INSERT INTO [t488] VALUES (26,'وجوب حذف الخبر','18','1','0');
+INSERT INTO [t488] VALUES (27,'اسم الأفعال الناقصة','18','1','0');
+INSERT INTO [t488] VALUES (28,'مسائل تتعلق باسم كان وخبرها','18','1','0');
+INSERT INTO [t488] VALUES (29,'خبر إن وأخواتها','19','1','0');
+INSERT INTO [t488] VALUES (30,'المنصوبات عشرة','20','1','0');
+INSERT INTO [t488] VALUES (31,'ما يصلح أن يكون ظرف مكان','21','1','0');
+INSERT INTO [t488] VALUES (32,'المجرورات','24','1','0');
+INSERT INTO [t488] VALUES (33,'صيغتا التعجب','24','1','0');
+INSERT INTO [t488] VALUES (34,'اسم الفعل','24','1','0');
+INSERT INTO [t488] VALUES (35,'اسم التفضيل','24','1','0');
+INSERT INTO [t488] VALUES (36,'المصدر وعمله','25','1','0');
+INSERT INTO [t488] VALUES (37,'اسم الفاعل','25','1','0');
+INSERT INTO [t488] VALUES (38,'أمثلة المبالغة واسم المفعول والصفة المشبهة','25','1','0');
+INSERT INTO [t488] VALUES (39,'التنازع في العمل','25','1','0');
+INSERT INTO [t488] VALUES (40,'الاشتغال','25','1','0');
+INSERT INTO [t488] VALUES (41,'التوابع','26','1','0');
+INSERT INTO [t488] VALUES (42,'خاتمة في الجمل','27','1','0');
+INSERT INTO [t488] VALUES (43,'الجمل التي لها محل من الإعراب','27','42','0');
+INSERT INTO [t488] VALUES (44,'الجمل التي لا محل لها من الإعراب','27','42','0');
