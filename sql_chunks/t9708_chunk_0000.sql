e74fc1eef67f872611ea0debddfb76b020e9cff8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:00:41 +0700

    Upload SQL chunk: t9708_chunk_0000.sql

---
+++ sql_chunks/t9708_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9708
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9708" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9708
+INSERT INTO [t9708] VALUES (1,'نص الكتاب','1','0','0');
+INSERT INTO [t9708] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
