5b9865e32da0a098ae943ffe1a3cea99e52d7c24
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:31 +0700

    Upload SQL chunk: t1093_chunk_0000.sql

---
+++ sql_chunks/t1093_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t1093
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t1093" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1093
+INSERT INTO [t1093] VALUES (1,'أحاديث منتخبة من أجزاء أبي منصور الخوجاني للسلفي','2','0','0');
