11a5864f500d84c013576371d523d9232ce7b8d7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:13 +0700

    Upload SQL chunk: t12730_chunk_0000.sql

---
+++ sql_chunks/t12730_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t12730
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t12730" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12730
+INSERT INTO [t12730] VALUES (1,'القاعدة الأولى','2','0','0');
+INSERT INTO [t12730] VALUES (2,'محذورات القاعدة الأولى','5','1','0');
+INSERT INTO [t12730] VALUES (3,'القاعدة الثانية','8','0','0');
+INSERT INTO [t12730] VALUES (4,'محذورات القاعدة الثانية','12','3','0');
+INSERT INTO [t12730] VALUES (5,'القاعدة الثالثة','17','0','0');
+INSERT INTO [t12730] VALUES (6,'محذورات القاعدة الثالثة','22','5','0');
+INSERT INTO [t12730] VALUES (7,'ترجمة الطوائف الواردة فى الكتاب','33','0','0');
