0b3fc1f990c08951ec61fe8e049f84c58ae711d2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:02 +0700

    Upload SQL chunk: t11180_chunk_0000.sql

---
+++ sql_chunks/t11180_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t11180
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t11180" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11180
+INSERT INTO [t11180] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11180] VALUES (2,'المبحث الاول','5','0','0');
+INSERT INTO [t11180] VALUES (3,'مدخل','5','2','0');
+INSERT INTO [t11180] VALUES (4,'المطلب الاول: تعريف الإستشراق','8','2','0');
+INSERT INTO [t11180] VALUES (5,'المطلب الثاني: أقوال المستشرقين','9','2','0');
+INSERT INTO [t11180] VALUES (6,'المبحث الثاني','12','0','0');
+INSERT INTO [t11180] VALUES (7,'مدخل','12','6','0');
+INSERT INTO [t11180] VALUES (8,'المطلب الاول','20','6','0');
+INSERT INTO [t11180] VALUES (9,'المطلب الثاني','47','6','0');
+INSERT INTO [t11180] VALUES (10,'الخاتمة','56','0','0');
+INSERT INTO [t11180] VALUES (11,'مصادر ومراجع','58','0','0');
