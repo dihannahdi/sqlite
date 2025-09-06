57f29b3fb66df08b31fcf712b263c06f7c7e3185
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:30 +0700

    Upload SQL chunk: t30082_chunk_0000.sql

---
+++ sql_chunks/t30082_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t30082
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t30082" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30082
+INSERT INTO [t30082] VALUES (1,'الجزء التاسع من المشيخة البغدادية لأبي طاهر السلفي','1','0','0');
+INSERT INTO [t30082] VALUES (2,'من حديث أبي محمد الخلال الحافظ عن شيوخه','2','0','0');
+INSERT INTO [t30082] VALUES (3,'من فوائد أبي محمد الخلال','69','0','0');
+INSERT INTO [t30082] VALUES (4,'من فوائد أبي علي بن شاذان','122','0','0');
+INSERT INTO [t30082] VALUES (5,'من فوائد القاضي أبي العلاء الواسطي','129','0','0');
+INSERT INTO [t30082] VALUES (6,'من شعر الصمدي','145','0','0');
+INSERT INTO [t30082] VALUES (7,'من حديث العتيقي','147','0','0');
+INSERT INTO [t30082] VALUES (8,'من فوائد ابن الطيوري','151','0','0');
+INSERT INTO [t30082] VALUES (9,'من فوائد أبي محمد بن السراج','156','0','0');
