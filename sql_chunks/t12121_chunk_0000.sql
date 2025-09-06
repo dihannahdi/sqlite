196b48f4349b99be1ff56f4701be50b64250b5e5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:11 +0700

    Upload SQL chunk: t12121_chunk_0000.sql

---
+++ sql_chunks/t12121_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t12121
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t12121" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12121
+INSERT INTO [t12121] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12121] VALUES (2,'الترجمات الإنجليزية في الميزان:','7','0','0');
+INSERT INTO [t12121] VALUES (3,'الترجمات الإنجليزية الاستشراقية','7','2','0');
+INSERT INTO [t12121] VALUES (4,'الترجمات الإنجليزية الإسلامية','14','2','0');
+INSERT INTO [t12121] VALUES (5,'الترجمات الإنجليزية القاديانية','25','2','0');
+INSERT INTO [t12121] VALUES (6,'من مشكلات الترجمة:','27','0','0');
+INSERT INTO [t12121] VALUES (7,'أولا: الأخطاء الدلالية','27','6','0');
+INSERT INTO [t12121] VALUES (8,'ثانيا: الأخطاء النحوية','37','6','0');
+INSERT INTO [t12121] VALUES (9,'الخلاصة:','41','0','0');
