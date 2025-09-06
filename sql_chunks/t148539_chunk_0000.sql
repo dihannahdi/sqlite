af2317cf5de1f55ce3846aaebb11d4a80b19b6eb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:36 +0700

    Upload SQL chunk: t148539_chunk_0000.sql

---
+++ sql_chunks/t148539_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t148539
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t148539" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t148539
+INSERT INTO [t148539] VALUES (1,'شعر أبي الأسود الدؤلي صنع السكري','1','0','0');
+INSERT INTO [t148539] VALUES (2,'التخريج','150','0','0');
+INSERT INTO [t148539] VALUES (3,'شعر أبي الأسود الدؤلي رواية ابن جني','151','0','0');
+INSERT INTO [t148539] VALUES (4,'مستدرك الديوان','241','0','0');
+INSERT INTO [t148539] VALUES (5,'أ - الشعر الثابت النسبة','242','4','0');
+INSERT INTO [t148539] VALUES (6,'ب - الشعر المشكوك','297','4','0');
+INSERT INTO [t148539] VALUES (7,'ملاحق الديوان','321','0','0');
+INSERT INTO [t148539] VALUES (8,'ما بقي من شعر أبي الأسود الدؤلي صنعه الأصمعي','322','7','0');
+INSERT INTO [t148539] VALUES (9,'ما بقي من أخبار أبي الأسود الدؤلي رواية أبي عبيدة','326','7','0');
+INSERT INTO [t148539] VALUES (10,'ما بقي من شعر أبي الأسود تأليف المدائني','327','7','0');
