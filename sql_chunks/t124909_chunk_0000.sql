d5a446ff8c16484b310baa5019a64e8ed74004c4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:23 +0700

    Upload SQL chunk: t124909_chunk_0000.sql

---
+++ sql_chunks/t124909_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t124909
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t124909" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t124909
+INSERT INTO [t124909] VALUES (1,'المقدمة','4','0','0');
+INSERT INTO [t124909] VALUES (2,'منهج أهل السنة والجماعة في الاعتقاد والأحكام العامة للشريعة','9','0','0');
+INSERT INTO [t124909] VALUES (3,'التوحيد','18','0','0');
+INSERT INTO [t124909] VALUES (4,'الإيمان بأسماء الله وصفاته كلها من غير تحريف ولا تعطيل ومن غير تكييف ولا تمثيل','25','0','0');
+INSERT INTO [t124909] VALUES (5,'أصول الإسلام والإيمان','31','0','0');
+INSERT INTO [t124909] VALUES (6,'أصول عامة','39','0','0');
+INSERT INTO [t124909] VALUES (7,'اتباع السنة والجماعة والتحذير من البدعة والفرقة','53','0','0');
+INSERT INTO [t124909] VALUES (8,'الطهارة','56','0','0');
+INSERT INTO [t124909] VALUES (9,'الصلاة','59','0','0');
+INSERT INTO [t124909] VALUES (10,'الزكاة والصدقة','62','0','0');
+INSERT INTO [t124909] VALUES (11,'الصوم','64','0','0');
+INSERT INTO [t124909] VALUES (12,'الحج','65','0','0');
+INSERT INTO [t124909] VALUES (13,'البيوع والأطعمة والأشربة','66','0','0');
+INSERT INTO [t124909] VALUES (14,'القضاء والحكم','70','0','0');
+INSERT INTO [t124909] VALUES (15,'المواريث','71','0','0');
+INSERT INTO [t124909] VALUES (16,'الرضاعة','72','0','0');
+INSERT INTO [t124909] VALUES (17,'الآداب والأخلاق','73','0','0');
+INSERT INTO [t124909] VALUES (18,'الذكر','83','0','0');
+INSERT INTO [t124909] VALUES (19,'أفعال الخير','86','0','0');
+INSERT INTO [t124909] VALUES (20,'الزهد','95','0','0');
+INSERT INTO [t124909] VALUES (21,'الجنائز','98','0','0');
+INSERT INTO [t124909] VALUES (22,'القصيدة الحائية للإمام أبي بكر ابن أبي داود السجستاني في أصول السنة','100','0','0');
