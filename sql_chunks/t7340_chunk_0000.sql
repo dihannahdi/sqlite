e379bcf475dee56f7ca88dfa03dc1c2cdb179124
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:46 +0700

    Upload SQL chunk: t7340_chunk_0000.sql

---
+++ sql_chunks/t7340_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t7340
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t7340" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7340
+INSERT INTO [t7340] VALUES (1,'المجلد الأول','1','0','0');
+INSERT INTO [t7340] VALUES (2,'مقدمة المحقق','1','1','0');
+INSERT INTO [t7340] VALUES (3,'حرف الألف','27','1','0');
+INSERT INTO [t7340] VALUES (4,'حرف الباء','107','1','0');
+INSERT INTO [t7340] VALUES (5,'حرف التاء','155','1','0');
+INSERT INTO [t7340] VALUES (6,'حرف الثاء','159','1','0');
+INSERT INTO [t7340] VALUES (7,'حرف الجيم','167','1','0');
+INSERT INTO [t7340] VALUES (8,'حرف الحاء','194','1','0');
+INSERT INTO [t7340] VALUES (9,'حرف الخاء','271','1','0');
+INSERT INTO [t7340] VALUES (10,'حرف الدال','294','1','0');
+INSERT INTO [t7340] VALUES (11,'حرف الذال','302','1','0');
+INSERT INTO [t7340] VALUES (12,'حرف الراء','305','1','0');
+INSERT INTO [t7340] VALUES (13,'حرف الزاي','325','1','0');
+INSERT INTO [t7340] VALUES (14,'حرف السين','347','1','0');
+INSERT INTO [t7340] VALUES (15,'حرف الشين','414','1','0');
+INSERT INTO [t7340] VALUES (16,'حرف الصاد','428','1','0');
+INSERT INTO [t7340] VALUES (17,'حرف الضاد','446','1','0');
+INSERT INTO [t7340] VALUES (18,'حرف الطاء','450','1','0');
+INSERT INTO [t7340] VALUES (19,'حرف الظاء','457','1','0');
+INSERT INTO [t7340] VALUES (20,'حرف العين','459','1','0');
+INSERT INTO [t7340] VALUES (21,'المجلد الثاني','657','0','0');
+INSERT INTO [t7340] VALUES (22,'حرف الغين','657','21','0');
+INSERT INTO [t7340] VALUES (23,'حرف الفاء','667','21','0');
+INSERT INTO [t7340] VALUES (24,'حرف القاف','680','21','0');
+INSERT INTO [t7340] VALUES (25,'حرف الكاف','696','21','0');
+INSERT INTO [t7340] VALUES (26,'حرف اللام','706','21','0');
+INSERT INTO [t7340] VALUES (27,'حرف الميم','711','21','0');
+INSERT INTO [t7340] VALUES (28,'حرف النون','830','21','0');
+INSERT INTO [t7340] VALUES (29,'حرف الواو','850','21','0');
+INSERT INTO [t7340] VALUES (30,'حرف الهاء','866','21','0');
+INSERT INTO [t7340] VALUES (31,'حرف الياء','892','21','0');
+INSERT INTO [t7340] VALUES (32,'مصادر ومراجع','928','21','0');
