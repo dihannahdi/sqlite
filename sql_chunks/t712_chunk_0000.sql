bdbd4af9ae539e533e08533736f1f1b1ff6b716d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:24 +0700

    Upload SQL chunk: t712_chunk_0000.sql

---
+++ sql_chunks/t712_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t712
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t712" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t712
+INSERT INTO [t712] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t712] VALUES (2,'أولا','4','0','0');
+INSERT INTO [t712] VALUES (3,'ثانيا','7','0','0');
+INSERT INTO [t712] VALUES (4,'ثالثا','7','0','0');
+INSERT INTO [t712] VALUES (5,'رابعا','8','0','0');
+INSERT INTO [t712] VALUES (6,'خامسا','11','0','0');
+INSERT INTO [t712] VALUES (7,'سادسا','12','0','0');
+INSERT INTO [t712] VALUES (8,'سابعا','13','0','0');
+INSERT INTO [t712] VALUES (9,'ثامنا','18','0','0');
+INSERT INTO [t712] VALUES (10,'فهرس المراجع','19','0','0');
