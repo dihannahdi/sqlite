2d7cdbc9a92ef6933aaeccc0bf24583fb50e3f66
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:17:55 +0700

    Upload SQL chunk: t38212_chunk_0000.sql

---
+++ sql_chunks/t38212_chunk_0000.sql
@@ -0,0 +1,51 @@
+-- Table: t38212
+-- Chunk: 1
+-- Rows: 44
+
+CREATE TABLE "t38212" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t38212
+INSERT INTO [t38212] VALUES (1,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t38212] VALUES (2,'الباب الأول','4','0','0');
+INSERT INTO [t38212] VALUES (3,'الهمزة','4','2','0');
+INSERT INTO [t38212] VALUES (4,'الهمزة أول الكلمة','4','2','0');
+INSERT INTO [t38212] VALUES (5,'الهمزة آخر الكلمة','9','2','0');
+INSERT INTO [t38212] VALUES (6,'الهمزة وسط الكلمة','10','2','0');
+INSERT INTO [t38212] VALUES (7,'الباب الثاني','19','0','0');
+INSERT INTO [t38212] VALUES (8,'الألف اللينة','19','7','0');
+INSERT INTO [t38212] VALUES (9,'الألف اللينة وسطا','19','8','0');
+INSERT INTO [t38212] VALUES (10,'الألف اللينة طرفا','20','8','0');
+INSERT INTO [t38212] VALUES (11,'معرفة الواوي واليائي','28','7','0');
+INSERT INTO [t38212] VALUES (12,'الألف المبدلة من ياء المتكلم','30','7','0');
+INSERT INTO [t38212] VALUES (13,'الألف المبدلة من نون التوكيد الخفيفة','30','7','0');
+INSERT INTO [t38212] VALUES (14,'الألف المبدلة من نون إذن','31','7','0');
+INSERT INTO [t38212] VALUES (15,'الباب الثالث','32','0','0');
+INSERT INTO [t38212] VALUES (16,'الحروف التي تزاد','32','15','0');
+INSERT INTO [t38212] VALUES (17,'زيادة الألف','32','15','0');
+INSERT INTO [t38212] VALUES (18,'زيادة الواو','34','15','0');
+INSERT INTO [t38212] VALUES (19,'الباب الرابع','36','0','0');
+INSERT INTO [t38212] VALUES (20,'الحروف التي تنقص','36','19','0');
+INSERT INTO [t38212] VALUES (21,'نقص الألف أولا','36','19','0');
+INSERT INTO [t38212] VALUES (22,'نقص الألف وسطا','40','19','0');
+INSERT INTO [t38212] VALUES (23,'نقص الألف آخر','41','19','0');
+INSERT INTO [t38212] VALUES (24,'نقص أل','43','19','0');
+INSERT INTO [t38212] VALUES (25,'نقص الواو','44','19','0');
+INSERT INTO [t38212] VALUES (26,'نقص الياء','44','19','0');
+INSERT INTO [t38212] VALUES (27,'نقص النون','45','19','0');
+INSERT INTO [t38212] VALUES (28,'النقص للرمز','47','19','0');
+INSERT INTO [t38212] VALUES (29,'الباب الخامس','51','0','0');
+INSERT INTO [t38212] VALUES (30,'الفصل والوصل','51','29','0');
+INSERT INTO [t38212] VALUES (31,'الوصل','51','30','0');
+INSERT INTO [t38212] VALUES (32,'وصل (من) بما قبلها','54','30','0');
+INSERT INTO [t38212] VALUES (33,'وصل (ما) بما قبلها','55','30','0');
+INSERT INTO [t38212] VALUES (34,'وصل (لا) بما قبلها','58','30','0');
+INSERT INTO [t38212] VALUES (35,'فصل','59','29','0');
+INSERT INTO [t38212] VALUES (36,'في هاء التأنيث','59','35','0');
+INSERT INTO [t38212] VALUES (37,'نماذج وتعليلات لرسم الهمزة والألف','63','0','0');
+INSERT INTO [t38212] VALUES (38,'أولا: الهمزة','63','37','0');
+INSERT INTO [t38212] VALUES (39,'الهمزة أول الكلمة: حقيقة أو حكما','63','38','0');
+INSERT INTO [t38212] VALUES (40,'الهمزة وسط الكلمة','64','38','0');
+INSERT INTO [t38212] VALUES (41,'الهمزة آخر الكلمة','71','38','0');
+INSERT INTO [t38212] VALUES (42,'ثانيا: الألف اللينة','72','37','0');
+INSERT INTO [t38212] VALUES (43,'الألف المتوسطة','72','42','0');
+INSERT INTO [t38212] VALUES (44,'الألف المتطرفة','73','42','0');
