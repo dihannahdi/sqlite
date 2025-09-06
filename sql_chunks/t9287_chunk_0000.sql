ab18fe554c1d1594c2aef81f1bba4fcca981cf03
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:52:23 +0700

    Upload SQL chunk: t9287_chunk_0000.sql

---
+++ sql_chunks/t9287_chunk_0000.sql
@@ -0,0 +1,48 @@
+-- Table: t9287
+-- Chunk: 1
+-- Rows: 41
+
+CREATE TABLE "t9287" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9287
+INSERT INTO [t9287] VALUES (1,'مقدمة الكتاب','1','0','0');
+INSERT INTO [t9287] VALUES (2,'حياة الأموي','8','0','0');
+INSERT INTO [t9287] VALUES (3,'جولة في الأموي','12','0','0');
+INSERT INTO [t9287] VALUES (4,'السور والدهاليز','12','3','0');
+INSERT INTO [t9287] VALUES (5,'مداخل الأموي','12','3','0');
+INSERT INTO [t9287] VALUES (6,'النوفرة','13','3','0');
+INSERT INTO [t9287] VALUES (7,'أبواب الأموي','14','3','0');
+INSERT INTO [t9287] VALUES (8,'خلع النعال','15','3','0');
+INSERT INTO [t9287] VALUES (9,'في صحن الأموي','17','0','0');
+INSERT INTO [t9287] VALUES (10,'القباب','18','9','0');
+INSERT INTO [t9287] VALUES (11,'البلاط','19','9','0');
+INSERT INTO [t9287] VALUES (12,'في الحرم','21','0','0');
+INSERT INTO [t9287] VALUES (13,'القبر','22','12','0');
+INSERT INTO [t9287] VALUES (14,'عمارة الأموي','25','0','0');
+INSERT INTO [t9287] VALUES (15,'هندسة الأموي','28','14','0');
+INSERT INTO [t9287] VALUES (16,'بناء القبة','29','14','0');
+INSERT INTO [t9287] VALUES (17,'هدية اليهودية','29','14','0');
+INSERT INTO [t9287] VALUES (18,'الأروقة والفسيفساء','30','14','0');
+INSERT INTO [t9287] VALUES (19,'القناديل','31','14','0');
+INSERT INTO [t9287] VALUES (20,'نفقات البناء','31','14','0');
+INSERT INTO [t9287] VALUES (21,'صفائح التاريخ','32','14','0');
+INSERT INTO [t9287] VALUES (22,'النصارى والأموي','32','14','0');
+INSERT INTO [t9287] VALUES (23,'عمر وزخارف الأموي','33','14','0');
+INSERT INTO [t9287] VALUES (24,'أطوار الأموي وأحداثه','36','0','0');
+INSERT INTO [t9287] VALUES (25,'الحرائق والزلازل','36','24','0');
+INSERT INTO [t9287] VALUES (26,'إصلاحات في الأموي','38','24','0');
+INSERT INTO [t9287] VALUES (27,'القبة','38','24','0');
+INSERT INTO [t9287] VALUES (28,'المآذن','39','24','0');
+INSERT INTO [t9287] VALUES (29,'المشاهد','40','24','0');
+INSERT INTO [t9287] VALUES (30,'الرخام والفسيفساء','40','24','0');
+INSERT INTO [t9287] VALUES (31,'من أخبار الأموي','42','0','0');
+INSERT INTO [t9287] VALUES (32,'الأموي في أواخر القرن السادس الهجري','44','0','0');
+INSERT INTO [t9287] VALUES (33,'صعوده إلى القبة','53','32','0');
+INSERT INTO [t9287] VALUES (34,'الأموي في القرن الثامن','55','32','0');
+INSERT INTO [t9287] VALUES (35,'الحريق الأخير','56','0','0');
+INSERT INTO [t9287] VALUES (36,'الإصلاحات الجديدة','62','0','0');
+INSERT INTO [t9287] VALUES (37,'المرحلة الأولى','62','36','0');
+INSERT INTO [t9287] VALUES (38,'المرحلة الثانية','63','36','0');
+INSERT INTO [t9287] VALUES (39,'المرحلة الثالثة','64','36','0');
+INSERT INTO [t9287] VALUES (40,'خاتمة','69','0','0');
+INSERT INTO [t9287] VALUES (41,'من آثار المؤلف','72','0','0');
