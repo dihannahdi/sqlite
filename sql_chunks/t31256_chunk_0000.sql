2211dd572c4e76615cc0db152ef6f4ecdfc88f73
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:42 +0700

    Upload SQL chunk: t31256_chunk_0000.sql

---
+++ sql_chunks/t31256_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t31256
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t31256" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31256
+INSERT INTO [t31256] VALUES (1,'الرسالة الأولى: كيفية صلاة النبي صلى الله عليه وسلم','1','0','0');
+INSERT INTO [t31256] VALUES (2,'الرسالة الثانية في وجوب أداء الصلاة في الجماعة','13','0','0');
+INSERT INTO [t31256] VALUES (3,'الرسالة الثالثة أين يضع المصلي يديه بعد الرفع من الركوع','20','0','0');
