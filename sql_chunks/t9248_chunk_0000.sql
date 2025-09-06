3c2839706cd95fd0484adf21db68e76997a63926
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:51:58 +0700

    Upload SQL chunk: t9248_chunk_0000.sql

---
+++ sql_chunks/t9248_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t9248
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t9248" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9248
+INSERT INTO [t9248] VALUES (1,'النكت العصرية','1','0','0');
+INSERT INTO [t9248] VALUES (2,'في أخبار الوزراء المصرية','1','0','0');
+INSERT INTO [t9248] VALUES (3,'تأليف','1','0','0');
+INSERT INTO [t9248] VALUES (4,'القاضي الفقيه الأرشد','1','0','0');
+INSERT INTO [t9248] VALUES (5,'نجم الدين أبي محمد عمارة بن أبي الحسن الحكمي','1','0','0');
+INSERT INTO [t9248] VALUES (6,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t9248] VALUES (7,'أخبار ورد الصالحى','147','0','0');
+INSERT INTO [t9248] VALUES (8,'مختار من','151','0','0');
+INSERT INTO [t9248] VALUES (9,'ديوان العلامة الأديب','151','0','0');
+INSERT INTO [t9248] VALUES (10,'الأوحد الناظم الناثر الفقيه','151','0','0');
+INSERT INTO [t9248] VALUES (11,'عمارة اليمنى رحمه الله تعالى','151','0','0');
+INSERT INTO [t9248] VALUES (12,'قافية التاء','179','0','0');
+INSERT INTO [t9248] VALUES (13,'قافية الجيم','186','0','0');
+INSERT INTO [t9248] VALUES (14,'قافية الحاء','186','0','0');
+INSERT INTO [t9248] VALUES (15,'قافية الخاء','191','0','0');
+INSERT INTO [t9248] VALUES (16,'قافية الدال','192','0','0');
+INSERT INTO [t9248] VALUES (17,'قافية الراء','218','0','0');
+INSERT INTO [t9248] VALUES (18,'قافية الزاي','276','0','0');
+INSERT INTO [t9248] VALUES (19,'قافية السين','277','0','0');
+INSERT INTO [t9248] VALUES (20,'قافية الشين','278','0','0');
+INSERT INTO [t9248] VALUES (21,'قافية الضاد','279','0','0');
+INSERT INTO [t9248] VALUES (22,'قافية العين','279','0','0');
+INSERT INTO [t9248] VALUES (23,'قافية القاف','290','0','0');
+INSERT INTO [t9248] VALUES (24,'قافية الكاف','298','0','0');
+INSERT INTO [t9248] VALUES (25,'قافية اللام','298','0','0');
+INSERT INTO [t9248] VALUES (26,'قافية النون','358','0','0');
+INSERT INTO [t9248] VALUES (27,'من','385','0','0');
+INSERT INTO [t9248] VALUES (28,'ذيل','388','0','0');
+INSERT INTO [t9248] VALUES (29,'ديوان عمارة اليمني','388','0','0');
+INSERT INTO [t9248] VALUES (30,'نبذة','391','0','0');
+INSERT INTO [t9248] VALUES (31,'خريدة القصر وجريدة العصر','391','0','0');
+INSERT INTO [t9248] VALUES (32,'لعماد الدين كاتب','391','0','0');
