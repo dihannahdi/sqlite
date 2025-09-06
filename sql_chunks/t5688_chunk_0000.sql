9750f0f9c950143bfbc91da51286671c4f8506e4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:27 +0700

    Upload SQL chunk: t5688_chunk_0000.sql

---
+++ sql_chunks/t5688_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t5688
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t5688" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5688
+INSERT INTO [t5688] VALUES (1,'تم الجزء','59','0','0');
