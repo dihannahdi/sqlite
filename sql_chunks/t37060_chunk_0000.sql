8f37677cf2994ec3124147df4fb322058d1117eb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:00 +0700

    Upload SQL chunk: t37060_chunk_0000.sql

---
+++ sql_chunks/t37060_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t37060
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t37060" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37060
+INSERT INTO [t37060] VALUES (1,'الخروج من دمشق','11','0','0');
+INSERT INTO [t37060] VALUES (2,'بعلبك','19','0','0');
+INSERT INTO [t37060] VALUES (3,'حمص','23','0','0');
+INSERT INTO [t37060] VALUES (4,'حماة','29','0','0');
+INSERT INTO [t37060] VALUES (5,'حلب','40','0','0');
+INSERT INTO [t37060] VALUES (6,'ذكر إرجاع ابن الفرفور وما حدث بعد ذلك من الأمور','50','0','0');
+INSERT INTO [t37060] VALUES (7,'أدنة','70','0','0');
+INSERT INTO [t37060] VALUES (8,'قونية','81','0','0');
+INSERT INTO [t37060] VALUES (9,'القسطنطينية','100','0','0');
+INSERT INTO [t37060] VALUES (10,'ذكر الرحلة الأزنكمودية','190','0','0');
+INSERT INTO [t37060] VALUES (11,'ذكر العود إلى القسطنطينية وما جرى بعد ذلك من الأمور المرضية','234','0','0');
+INSERT INTO [t37060] VALUES (12,'ذكر الرجوع الى الوطن والأوبة بعد طول مدة هذه الغيبة','260','0','0');
