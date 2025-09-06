c3577fc3d54a7c0568a67b70a7d53e777018443e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:29 +0700

    Upload SQL chunk: t28173_chunk_0000.sql

---
+++ sql_chunks/t28173_chunk_0000.sql
@@ -0,0 +1,62 @@
+-- Table: t28173
+-- Chunk: 1
+-- Rows: 55
+
+CREATE TABLE "t28173" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t28173
+INSERT INTO [t28173] VALUES (1,'نشوة السكران','1','0','0');
+INSERT INTO [t28173] VALUES (2,'مقدمة في ذكر العشق واسمه وما جاء في حده ورسمه','2','0','0');
+INSERT INTO [t28173] VALUES (3,'مبحث في أسباب العشق وعلاماته','7','0','0');
+INSERT INTO [t28173] VALUES (4,'مبحث في مراتب العشق وأسمائه وصفاته','9','0','0');
+INSERT INTO [t28173] VALUES (5,'مبحث في العشق وذمه وترياقه وسمه','13','0','0');
+INSERT INTO [t28173] VALUES (6,'مبحث في أن العشق اضطراري أو اختياري','16','0','0');
+INSERT INTO [t28173] VALUES (7,'مبحث في ذكر الحسن والجمال','19','0','0');
+INSERT INTO [t28173] VALUES (8,'مبحث في ذكر الغزلان','30','0','0');
+INSERT INTO [t28173] VALUES (9,'مبحث في قسمة العشق مخاطباته','36','0','0');
+INSERT INTO [t28173] VALUES (10,'مبحث في أقسام النسوان وجلوة عدة من سرب الغزلان','40','0','0');
+INSERT INTO [t28173] VALUES (11,'مبحث في التقسيم باعتبار السن','44','0','0');
+INSERT INTO [t28173] VALUES (12,'فصل في أقسام الغزلان التي هي من مستخرجات أزاد رحمه الله تعالى','60','0','0');
+INSERT INTO [t28173] VALUES (13,'مبحث في ذكر من كلف وهو غير مكلف','94','0','0');
+INSERT INTO [t28173] VALUES (14,'فصل في أحوال العشاق','101','0','0');
+INSERT INTO [t28173] VALUES (15,'خاتمة','107','0','0');
+INSERT INTO [t28173] VALUES (16,'مطلق الحسن','112','0','0');
+INSERT INTO [t28173] VALUES (17,'الضفيرة','113','0','0');
+INSERT INTO [t28173] VALUES (18,'الجبهة','113','0','0');
+INSERT INTO [t28173] VALUES (19,'الحاجب','113','0','0');
+INSERT INTO [t28173] VALUES (20,'العين','113','0','0');
+INSERT INTO [t28173] VALUES (21,'الهدب','113','0','0');
+INSERT INTO [t28173] VALUES (22,'اللحظ','113','0','0');
+INSERT INTO [t28173] VALUES (23,'الكحل','113','0','0');
+INSERT INTO [t28173] VALUES (24,'الأنف','114','0','0');
+INSERT INTO [t28173] VALUES (25,'الفم','114','0','0');
+INSERT INTO [t28173] VALUES (26,'الشفة','114','0','0');
+INSERT INTO [t28173] VALUES (27,'المسي','114','0','0');
+INSERT INTO [t28173] VALUES (28,'الثغر','114','0','0');
+INSERT INTO [t28173] VALUES (29,'التبسم','114','0','0');
+INSERT INTO [t28173] VALUES (30,'اللسان','114','0','0');
+INSERT INTO [t28173] VALUES (31,'الحديث','115','0','0');
+INSERT INTO [t28173] VALUES (32,'الرضاب','115','0','0');
+INSERT INTO [t28173] VALUES (33,'الخد','115','0','0');
+INSERT INTO [t28173] VALUES (34,'العرق','115','0','0');
+INSERT INTO [t28173] VALUES (35,'الخال','115','0','0');
+INSERT INTO [t28173] VALUES (36,'الذقن','115','0','0');
+INSERT INTO [t28173] VALUES (37,'القرط','116','0','0');
+INSERT INTO [t28173] VALUES (38,'الجيد','116','0','0');
+INSERT INTO [t28173] VALUES (39,'الطوق','116','0','0');
+INSERT INTO [t28173] VALUES (40,'الثدي','116','0','0');
+INSERT INTO [t28173] VALUES (41,'الوشاح','116','0','0');
+INSERT INTO [t28173] VALUES (42,'القلب','116','0','0');
+INSERT INTO [t28173] VALUES (43,'الساعد','117','0','0');
+INSERT INTO [t28173] VALUES (44,'السوار','117','0','0');
+INSERT INTO [t28173] VALUES (45,'اليد','117','0','0');
+INSERT INTO [t28173] VALUES (46,'الظفر','117','0','0');
+INSERT INTO [t28173] VALUES (47,'الحناء','117','0','0');
+INSERT INTO [t28173] VALUES (48,'الخصر','117','0','0');
+INSERT INTO [t28173] VALUES (49,'السرة','118','0','0');
+INSERT INTO [t28173] VALUES (50,'ما تحت السرة','118','0','0');
+INSERT INTO [t28173] VALUES (51,'الردف','118','0','0');
+INSERT INTO [t28173] VALUES (52,'الساق','118','0','0');
+INSERT INTO [t28173] VALUES (53,'الرجل','118','0','0');
+INSERT INTO [t28173] VALUES (54,'الخلخال','118','0','0');
+INSERT INTO [t28173] VALUES (55,'الخاتمة','120','0','0');
