f9540ee7a7cd7891d3a93c87474f6b6e7cc5f05b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:48 +0700

    Upload SQL chunk: t10096_chunk_0000.sql

---
+++ sql_chunks/t10096_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t10096
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t10096" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10096
+INSERT INTO [t10096] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10096] VALUES (2,'التعريف بالمؤلف','4','0','0');
+INSERT INTO [t10096] VALUES (3,'أسمه ونسبه','4','2','0');
+INSERT INTO [t10096] VALUES (4,'حياته','5','2','0');
+INSERT INTO [t10096] VALUES (5,'مؤلفاته','8','2','0');
+INSERT INTO [t10096] VALUES (6,'التعريف بالرسالة','14','0','0');
+INSERT INTO [t10096] VALUES (7,'عنوانها','14','6','0');
+INSERT INTO [t10096] VALUES (8,'توثيق نسبتها إلى المؤلف','15','6','0');
+INSERT INTO [t10096] VALUES (9,'محتوى الرسالة','18','6','0');
+INSERT INTO [t10096] VALUES (10,'قيمتها العلمية','23','6','0');
+INSERT INTO [t10096] VALUES (11,'المآخذ عليها','31','6','0');
+INSERT INTO [t10096] VALUES (12,'وصف النسخ','37','6','0');
+INSERT INTO [t10096] VALUES (13,'نماذج منها','38','6','0');
+INSERT INTO [t10096] VALUES (14,'نص الرسالة','43','0','0');
+INSERT INTO [t10096] VALUES (15,'مصادر ومراجع','88','0','0');
