9f752e65c404aee851db3550d16d17ecb33c944a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:01 +0700

    Upload SQL chunk: t21761_chunk_0000.sql

---
+++ sql_chunks/t21761_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t21761
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t21761" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21761
+INSERT INTO [t21761] VALUES (1,'كلمة سماحة المفتي','1','0','0');
+INSERT INTO [t21761] VALUES (2,'مقدمة','10','0','0');
+INSERT INTO [t21761] VALUES (3,'خطبة عام ١٤٠٢ الهجري','15','0','0');
+INSERT INTO [t21761] VALUES (4,'خطبة عام ١٤٠٣ الهجري','42','0','0');
+INSERT INTO [t21761] VALUES (5,'خطبة عام ١٤٠٤ الهجري','53','0','0');
+INSERT INTO [t21761] VALUES (6,'خطبة عام ١٤٠٥ الهجري','69','0','0');
+INSERT INTO [t21761] VALUES (7,'خطبة عام ١٤٠٦ الهجري','87','0','0');
+INSERT INTO [t21761] VALUES (8,'خطبة عام ١٤٠٧ الهجري','104','0','0');
+INSERT INTO [t21761] VALUES (9,'خطبة عام ١٤٠٨ الهجري','120','0','0');
+INSERT INTO [t21761] VALUES (10,'خطبة عام ١٤٠٩ الهجري','137','0','0');
+INSERT INTO [t21761] VALUES (11,'خطبة عام ١٤١٠ الهجري','157','0','0');
+INSERT INTO [t21761] VALUES (12,'خطبة عام ١٤١١ الهجري','178','0','0');
