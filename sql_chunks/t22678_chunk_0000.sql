0dd6ac513c314c2674132cc9c77b97540e39d18d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:10 +0700

    Upload SQL chunk: t22678_chunk_0000.sql

---
+++ sql_chunks/t22678_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t22678
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t22678" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22678
+INSERT INTO [t22678] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t22678] VALUES (2,'دوافع المسلمين للاهتمام بسيرة الرسول صلى الله عليه وسلم','10','0','0');
+INSERT INTO [t22678] VALUES (3,'مدخل','10','2','0');
+INSERT INTO [t22678] VALUES (4,'بداية التأليف في السيرة النبوية','11','2','0');
+INSERT INTO [t22678] VALUES (5,'طبقات كتاب السيرة','15','0','0');
+INSERT INTO [t22678] VALUES (6,'رجال الطبقة الأولى من كتاب المغازي والسير','15','5','0');
+INSERT INTO [t22678] VALUES (7,'أبان بن عثمان','15','6','0');
+INSERT INTO [t22678] VALUES (8,'عروة بن الزبير','17','6','0');
+INSERT INTO [t22678] VALUES (9,'شرحبيل بن سعد','21','6','0');
+INSERT INTO [t22678] VALUES (10,'وهب بن منبه','22','6','0');
+INSERT INTO [t22678] VALUES (11,'رجال الطبقة الثانية من كتاب المغازى والسير','24','5','0');
+INSERT INTO [t22678] VALUES (12,'مدخل','24','11','0');
+INSERT INTO [t22678] VALUES (13,'محمد بن مسلم بن شهاب الزهرى','26','11','0');
+INSERT INTO [t22678] VALUES (14,'عبد الله بن أبي بكر بن محمد بن عمرو بن حزم الأنصاري','29','11','0');
+INSERT INTO [t22678] VALUES (15,'عاصم بن عمر بن قتادة','32','11','0');
+INSERT INTO [t22678] VALUES (16,'رجال الطبقة الثالثة من كتاب المغازي والسير','33','5','0');
+INSERT INTO [t22678] VALUES (17,'موسى بن عقبة','33','16','0');
+INSERT INTO [t22678] VALUES (18,'معمر بن راشد','35','16','0');
+INSERT INTO [t22678] VALUES (19,'محمد بن إسحاق المطلبي','36','16','0');
+INSERT INTO [t22678] VALUES (20,'الواقدي','71','16','0');
+INSERT INTO [t22678] VALUES (21,'محمد بن سعد','78','16','0');
+INSERT INTO [t22678] VALUES (22,'محتويات الكتاب','83','0','0');
