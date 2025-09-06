5c380930a6fe8300fd0833b4ecdc43ee66d4f4ed
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:04:39 +0700

    Upload SQL chunk: t12375_chunk_0000.sql

---
+++ sql_chunks/t12375_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t12375
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t12375" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12375
+INSERT INTO [t12375] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t12375] VALUES (2,'حاجتنا إلى الاجتهاد','4','0','0');
+INSERT INTO [t12375] VALUES (3,'* سياسيا:','7','2','0');
+INSERT INTO [t12375] VALUES (4,'* إقتصاديا:','7','2','0');
+INSERT INTO [t12375] VALUES (5,'كيف نجتهد','9','0','0');
+INSERT INTO [t12375] VALUES (6,'هل يوجد المجتهد المطلق؟','14','0','0');
+INSERT INTO [t12375] VALUES (7,'أولا: المعرفة الكلية','16','6','0');
+INSERT INTO [t12375] VALUES (8,'ثانيا: المعرفة الجزئية','16','6','0');
+INSERT INTO [t12375] VALUES (9,'الثابت والمتغير في الدين','18','0','0');
+INSERT INTO [t12375] VALUES (10,'أولا:','19','9','0');
+INSERT INTO [t12375] VALUES (11,'ثانيا:','20','9','0');
+INSERT INTO [t12375] VALUES (12,'ثالثا:','21','9','0');
+INSERT INTO [t12375] VALUES (13,'رابعا:','23','9','0');
+INSERT INTO [t12375] VALUES (14,'مشكلات تعترض طريقنا','26','0','0');
+INSERT INTO [t12375] VALUES (15,'أولا:','26','14','0');
+INSERT INTO [t12375] VALUES (16,'ثانيا:','29','14','0');
+INSERT INTO [t12375] VALUES (17,'من الأئمة الأربعة؟ وما موقفنا منهم؟','32','0','0');
+INSERT INTO [t12375] VALUES (18,'أولا:','41','17','0');
+INSERT INTO [t12375] VALUES (19,'ثانيا:','41','17','0');
+INSERT INTO [t12375] VALUES (20,'ثالثا:','41','17','0');
+INSERT INTO [t12375] VALUES (21,'رابعا:','41','17','0');
+INSERT INTO [t12375] VALUES (22,'خامسا:','42','17','0');
+INSERT INTO [t12375] VALUES (23,'سادسا:','42','17','0');
+INSERT INTO [t12375] VALUES (24,'سابعا:','42','17','0');
+INSERT INTO [t12375] VALUES (25,'ثامنا:','43','17','0');
+INSERT INTO [t12375] VALUES (26,'تاسعا:','43','17','0');
+INSERT INTO [t12375] VALUES (27,'عاشرا:','43','17','0');
+INSERT INTO [t12375] VALUES (28,'السلفيون والأئمة الأربعة','45','0','0');
