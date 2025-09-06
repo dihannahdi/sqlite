54e933d5a4403d64a152f82fe66a0a7348af8248
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:34 +0700

    Upload SQL chunk: t30908_chunk_0000.sql

---
+++ sql_chunks/t30908_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t30908
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t30908" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30908
+INSERT INTO [t30908] VALUES (1,'فتوى الشيخ صالح بن غصون رحمه الله حول المظاهرات والاغتيالات والمسيرات','1','0','0');
+INSERT INTO [t30908] VALUES (2,'بعض الدعاة يدندن حول مسألة وسائل الدعوة، وإنكار المنكر، ويدخلون فيها المظاهرات والاغتيالات','2','0','0');
