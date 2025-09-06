3d1ceec1698e72a1c3f274a4ce15fe739ea0cefe
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:59 +0700

    Upload SQL chunk: t8959_chunk_0000.sql

---
+++ sql_chunks/t8959_chunk_0000.sql
@@ -0,0 +1,56 @@
+-- Table: t8959
+-- Chunk: 1
+-- Rows: 49
+
+CREATE TABLE "t8959" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8959
+INSERT INTO [t8959] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8959] VALUES (2,'القسم الأول: الدراسة','5','0','0');
+INSERT INTO [t8959] VALUES (3,'الفصل الأول: المصنف','5','2','0');
+INSERT INTO [t8959] VALUES (4,'المبحث الأول: أسمه ونسبه ومولده ووفاته','5','3','0');
+INSERT INTO [t8959] VALUES (5,'المبحث الثاني: شيوخه وتلاميذه','9','3','0');
+INSERT INTO [t8959] VALUES (6,'أولا: شيوخه','9','5','0');
+INSERT INTO [t8959] VALUES (7,'ثانيا: تلاميذه','13','5','0');
+INSERT INTO [t8959] VALUES (8,'المبحث الثالث: آثاره العلمية وشعره','14','3','0');
+INSERT INTO [t8959] VALUES (9,'أولا: أثاره العلمية','14','8','0');
+INSERT INTO [t8959] VALUES (10,'الفصل الثاني: كتاب المنهل المأهول','18','2','0');
+INSERT INTO [t8959] VALUES (11,'المبحث الأول: تحقيق اسم الكتاب وتوثيق نسبته','18','10','0');
+INSERT INTO [t8959] VALUES (12,'المبحث الثاني: موضوع الكتاب ومادته ومنهجه','23','10','0');
+INSERT INTO [t8959] VALUES (13,'المبحث الثالث: مصادره وشواهده','24','10','0');
+INSERT INTO [t8959] VALUES (14,'أولا: مصادره','24','13','0');
+INSERT INTO [t8959] VALUES (15,'ثانيا: شواهده','24','13','0');
+INSERT INTO [t8959] VALUES (16,'القسم الثاني: التحقيق','24','0','0');
+INSERT INTO [t8959] VALUES (17,'أولا: وصف النسخ','24','16','0');
+INSERT INTO [t8959] VALUES (18,'ثانيا: منهج التحقيق','25','16','0');
+INSERT INTO [t8959] VALUES (19,'النص المحقق','25','18','0');
+INSERT INTO [t8959] VALUES (20,'مقدمة المؤلف','26','18','0');
+INSERT INTO [t8959] VALUES (21,'باب الهمزة','26','18','0');
+INSERT INTO [t8959] VALUES (22,'باب الباء الموحدة','26','18','0');
+INSERT INTO [t8959] VALUES (23,'باب التاء المثناة','26','18','0');
+INSERT INTO [t8959] VALUES (24,'باب الثاء المثلثة','26','18','0');
+INSERT INTO [t8959] VALUES (25,'باب الجيم','26','18','0');
+INSERT INTO [t8959] VALUES (26,'باب الحاء المهملة','26','18','0');
+INSERT INTO [t8959] VALUES (27,'باب الخاء المعجمة','26','18','0');
+INSERT INTO [t8959] VALUES (28,'باب الدال المهملة','26','18','0');
+INSERT INTO [t8959] VALUES (29,'باب الذال المعجمة','26','18','0');
+INSERT INTO [t8959] VALUES (30,'باب الراء','26','18','0');
+INSERT INTO [t8959] VALUES (31,'باب الزاي','26','18','0');
+INSERT INTO [t8959] VALUES (32,'باب السين المهملة','26','18','0');
+INSERT INTO [t8959] VALUES (33,'باب الشين المعجمة','26','18','0');
+INSERT INTO [t8959] VALUES (34,'باب الضاد المعجمة','26','18','0');
+INSERT INTO [t8959] VALUES (35,'باب الطاء المهملة','26','18','0');
+INSERT INTO [t8959] VALUES (36,'باب الظاء المهملة','26','18','0');
+INSERT INTO [t8959] VALUES (37,'باب العين المهملة','26','18','0');
+INSERT INTO [t8959] VALUES (38,'باب الغين المعجمة','26','18','0');
+INSERT INTO [t8959] VALUES (39,'باب الفاء','26','18','0');
+INSERT INTO [t8959] VALUES (40,'باب القاف','26','18','0');
+INSERT INTO [t8959] VALUES (41,'باب الكاف','26','18','0');
+INSERT INTO [t8959] VALUES (42,'باب اللام','26','18','0');
+INSERT INTO [t8959] VALUES (43,'باب الميم','26','18','0');
+INSERT INTO [t8959] VALUES (44,'باب النون','26','18','0');
+INSERT INTO [t8959] VALUES (45,'باب الهاء','26','18','0');
+INSERT INTO [t8959] VALUES (46,'باب الواو','26','18','0');
+INSERT INTO [t8959] VALUES (47,'باب الياء','26','18','0');
+INSERT INTO [t8959] VALUES (48,'الخاتمة','26','0','0');
+INSERT INTO [t8959] VALUES (49,'مصادر ومراجع','27','0','0');
