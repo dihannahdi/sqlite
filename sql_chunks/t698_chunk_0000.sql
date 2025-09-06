7fe6e76967d76f6c27416ca58491a2eb58908100
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:32 +0700

    Upload SQL chunk: t698_chunk_0000.sql

---
+++ sql_chunks/t698_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t698
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t698" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t698
+INSERT INTO [t698] VALUES (1,'تلخيص المقالة الأولى من الخطابة','1','0','0');
+INSERT INTO [t698] VALUES (2,'القول في المدح والذم','29','1','0');
+INSERT INTO [t698] VALUES (3,'القول في الشكاية والاعتذار','33','1','0');
+INSERT INTO [t698] VALUES (4,'فصل','47','1','0');
+INSERT INTO [t698] VALUES (5,'القول في الشهود','51','1','0');
+INSERT INTO [t698] VALUES (6,'القول في العقود','52','1','0');
+INSERT INTO [t698] VALUES (7,'القول في العذاب','53','1','0');
+INSERT INTO [t698] VALUES (8,'القول في الأيمان','53','1','0');
+INSERT INTO [t698] VALUES (9,'المقالة الثانية من الخطابة','54','0','0');
+INSERT INTO [t698] VALUES (10,'القول في المسكنات للغضب','58','9','0');
+INSERT INTO [t698] VALUES (11,'القول في الصداقة والمحبة','61','9','0');
+INSERT INTO [t698] VALUES (12,'القول في الخوف','64','9','0');
+INSERT INTO [t698] VALUES (13,'القول في الشجاعة','66','9','0');
+INSERT INTO [t698] VALUES (14,'القول في الحياء والخجل','67','9','0');
+INSERT INTO [t698] VALUES (15,'القول في إثبات المنة','71','9','0');
+INSERT INTO [t698] VALUES (16,'وشكرها وفي إنكارها وكفرها','71','9','0');
+INSERT INTO [t698] VALUES (17,'القول في الاهتمام','72','9','0');
+INSERT INTO [t698] VALUES (18,'القول في النفاسة','75','9','0');
+INSERT INTO [t698] VALUES (19,'القول في الحسد','76','9','0');
+INSERT INTO [t698] VALUES (20,'القول في الغبطة','77','9','0');
+INSERT INTO [t698] VALUES (21,'القول في الأسى والأسف','78','9','0');
+INSERT INTO [t698] VALUES (22,'القول في الخلقيات','79','9','0');
+INSERT INTO [t698] VALUES (23,'القول في أخلاق الشباب','79','9','0');
+INSERT INTO [t698] VALUES (24,'في أخلاق المشايخ','80','9','0');
+INSERT INTO [t698] VALUES (25,'القول في سن الكهول','82','9','0');
+INSERT INTO [t698] VALUES (26,'فصل','82','9','0');
+INSERT INTO [t698] VALUES (27,'فصل','82','9','0');
+INSERT INTO [t698] VALUES (28,'القول في مواضع التوبيخ','95','9','0');
+INSERT INTO [t698] VALUES (29,'المقالة الثالثة من كتاب الخطابة','101','0','0');
+INSERT INTO [t698] VALUES (30,'القول في الألفاظ المفردة','103','29','0');
+INSERT INTO [t698] VALUES (31,'القول في أجزاء الخطب','125','29','0');
