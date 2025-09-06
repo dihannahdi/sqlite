a99d63b89601236872c9f11807a7df247b7604c3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:03:52 +0700

    Upload SQL chunk: t12277_chunk_0000.sql

---
+++ sql_chunks/t12277_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t12277
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t12277" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12277
+INSERT INTO [t12277] VALUES (1,'المدخل','1','0','0');
+INSERT INTO [t12277] VALUES (2,'طرسوس في عهد الخلافة الراشدة والدولة الأموية ١١-١٣٢ هـ:','3','0','0');
+INSERT INTO [t12277] VALUES (3,'طرسوس في عهد قوة الدولة العباسية ٣٣ ١- ٢٤٨ هـ:','5','0','0');
+INSERT INTO [t12277] VALUES (4,'طرسوس في أوج قوتها- عهد النفوذ المكي ٢٤٨- ٣٣٤ هـ:','13','0','0');
+INSERT INTO [t12277] VALUES (5,'طرسوس ومأساتها ٣٣٤- ٣٥٥ هـ:','25','0','0');
+INSERT INTO [t12277] VALUES (6,'طرسوس بلد العلم والرباط:','35','0','0');
+INSERT INTO [t12277] VALUES (7,'مصادر ومراجع','41','0','0');
