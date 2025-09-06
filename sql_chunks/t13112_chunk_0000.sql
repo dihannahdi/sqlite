c9a420904668ef9a81f149edffabc481517ebf84
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:10:45 +0700

    Upload SQL chunk: t13112_chunk_0000.sql

---
+++ sql_chunks/t13112_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t13112
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t13112" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13112
+INSERT INTO [t13112] VALUES (1,'أحاديث يونس بن يزيد الأيلي','63','0','0');
+INSERT INTO [t13112] VALUES (2,'أحاديث محمد بن عمرو بن علقمة','87','0','0');
