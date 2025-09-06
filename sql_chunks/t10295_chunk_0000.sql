e1aa6a603d28c93291c3664154c15276963bbf31
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:51 +0700

    Upload SQL chunk: t10295_chunk_0000.sql

---
+++ sql_chunks/t10295_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t10295
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t10295" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10295
+INSERT INTO [t10295] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t10295] VALUES (2,'حكمة انبعاث الإسلام من الجزيرة العربية','4','0','0');
+INSERT INTO [t10295] VALUES (3,'النواحي التي تناولها التشريع الإسلامي','6','0','0');
+INSERT INTO [t10295] VALUES (4,'النزعة الجماعية في التشريع الإسلامي','9','0','0');
+INSERT INTO [t10295] VALUES (5,'مميزات التشريع الإسلامي','11','0','0');
+INSERT INTO [t10295] VALUES (6,'التشريع الإسلامي يحفظ المقاصد التي يقوم عليها أمر الدين والدنيا','12','0','0');
+INSERT INTO [t10295] VALUES (7,'السنة توضح القرآن','15','0','0');
+INSERT INTO [t10295] VALUES (8,'التشريع الإسلامي ملائم لكل البيئات','17','0','0');
+INSERT INTO [t10295] VALUES (9,'أهم المبادئ التي يدعو إليها الإسلام','19','0','0');
