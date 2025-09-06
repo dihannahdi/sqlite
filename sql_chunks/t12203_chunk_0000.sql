a11eed099864ed8de7fa1f1721ec656fefa64c8b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:55 +0700

    Upload SQL chunk: t12203_chunk_0000.sql

---
+++ sql_chunks/t12203_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t12203
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t12203" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12203
+INSERT INTO [t12203] VALUES (1,'أصح البشائر في مبعث سيد الأوائل والأواخر','1','0','0');
+INSERT INTO [t12203] VALUES (2,'تذييل','15','0','0');
