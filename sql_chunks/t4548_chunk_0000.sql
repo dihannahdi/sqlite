c438e2693beb22c2350ee95c6c57f0f4349e4bc8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:32 +0700

    Upload SQL chunk: t4548_chunk_0000.sql

---
+++ sql_chunks/t4548_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t4548
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t4548" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4548
+INSERT INTO [t4548] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t4548] VALUES (2,'المصادر التي اعتمدها','3','0','0');
+INSERT INTO [t4548] VALUES (3,'ماذا نعني بمصطلح معسكر؟','9','0','0');
+INSERT INTO [t4548] VALUES (4,'الروح الصليبية ونشأتها','21','0','0');
+INSERT INTO [t4548] VALUES (5,'الثأر القديم','27','0','0');
+INSERT INTO [t4548] VALUES (6,'المصالح الآنية','31','0','0');
+INSERT INTO [t4548] VALUES (7,'مخاوف مستقبله','37','0','0');
