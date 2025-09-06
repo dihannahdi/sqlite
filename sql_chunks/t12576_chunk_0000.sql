5123a8852f1ab3958cb59116f4d20dfed4270736
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:44 +0700

    Upload SQL chunk: t12576_chunk_0000.sql

---
+++ sql_chunks/t12576_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t12576
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t12576" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12576
+INSERT INTO [t12576] VALUES (1,'الإباضية..وهل هم خوارج؟','2','0','0');
+INSERT INTO [t12576] VALUES (2,'١- لماذا الإباضية؟','2','0','0');
+INSERT INTO [t12576] VALUES (3,'٢- من الإباضية؟','4','0','0');
+INSERT INTO [t12576] VALUES (4,'٣- مصدر التلقي عندهم:','5','0','0');
+INSERT INTO [t12576] VALUES (5,'٤- هل الإباضية من الخوارج؟','10','0','0');
+INSERT INTO [t12576] VALUES (6,'نصيحة للطائفة الإباضية:','13','0','0');
