224e22019c89eaf64f33cc1a8e8e37a5171ec015
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:17 +0700

    Upload SQL chunk: t30869_chunk_0000.sql

---
+++ sql_chunks/t30869_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t30869
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t30869" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30869
+INSERT INTO [t30869] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t30869] VALUES (2,'نواقض الإسلام العشرة','2','0','0');
