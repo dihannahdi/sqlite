5b85cff903002f3ede82e952afd4893ddf595e91
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:03 +0700

    Upload SQL chunk: t4119_chunk_0000.sql

---
+++ sql_chunks/t4119_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t4119
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t4119" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4119
+INSERT INTO [t4119] VALUES (1,'وحدة الكون وسر الحياة','1','0','0');
+INSERT INTO [t4119] VALUES (2,'نشأة الكون','4','0','0');
+INSERT INTO [t4119] VALUES (3,'تمدد الكون وسعته','6','0','0');
+INSERT INTO [t4119] VALUES (4,'نقص الأوكسجين في الارتفاعات. أو (الضغط الجوي)','8','0','0');
+INSERT INTO [t4119] VALUES (5,'تفجير الذرة','11','0','0');
+INSERT INTO [t4119] VALUES (6,'الزوجية في كل شيء','13','0','0');
+INSERT INTO [t4119] VALUES (7,'الأمواج الداخلية والسطحية','15','0','0');
