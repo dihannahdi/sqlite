ace5bb6181de84cdbfaff7897c7fba420321d12e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:25 +0700

    Upload SQL chunk: t11206_chunk_0000.sql

---
+++ sql_chunks/t11206_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t11206
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t11206" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11206
+INSERT INTO [t11206] VALUES (1,'القرآن ومعناه','1','0','0');
+INSERT INTO [t11206] VALUES (2,'القرآن المعجزة','4','0','0');
+INSERT INTO [t11206] VALUES (3,'موطن الإعجاز في القرآن','11','0','0');
+INSERT INTO [t11206] VALUES (4,'الجاحظ ورأيه في الإعجاز','14','0','0');
