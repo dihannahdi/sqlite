90de825969b11280ec7cafa3c74139e203abc128
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:01 +0700

    Upload SQL chunk: t1912_chunk_0000.sql

---
+++ sql_chunks/t1912_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t1912
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t1912" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1912
+INSERT INTO [t1912] VALUES (1,'مفهوم السعادة في الإسلام','1','0','0');
+INSERT INTO [t1912] VALUES (2,'شواهد على السعادة من الكتاب والسنة','1','1','0');
+INSERT INTO [t1912] VALUES (3,'السعادة ليست في الماديات فقط','2','1','0');
+INSERT INTO [t1912] VALUES (4,'الإسلام يحقق السعادة الأبدية للإنسان','3','0','0');
+INSERT INTO [t1912] VALUES (5,'السعادة الدنيوية','3','4','0');
+INSERT INTO [t1912] VALUES (6,'السعادة الأخروية','4','4','0');
+INSERT INTO [t1912] VALUES (7,'أسباب تحصيل السعادة','7','0','0');
+INSERT INTO [t1912] VALUES (8,'الإيمان والعمل الصالح','7','7','0');
+INSERT INTO [t1912] VALUES (9,'التحلي بالأخلاق الفاضلة','9','7','0');
+INSERT INTO [t1912] VALUES (10,'الإكثار من ذكر الله','11','7','0');
+INSERT INTO [t1912] VALUES (11,'العناية الصحية','13','7','0');
+INSERT INTO [t1912] VALUES (12,'السعي لتحقيق القدر المادي اللازم للسعادة','17','7','0');
+INSERT INTO [t1912] VALUES (13,'تنظيم الوقت','18','7','0');
