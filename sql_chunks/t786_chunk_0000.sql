843163b877d52f3d55ee1a2c1ff4807079f3a1e3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:31 +0700

    Upload SQL chunk: t786_chunk_0000.sql

---
+++ sql_chunks/t786_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t786
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t786" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t786
+INSERT INTO [t786] VALUES (1,'نص الكتاب','1','0','0');
+INSERT INTO [t786] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t786] VALUES (3,'رب أعن برحمتك','1','2','0');
