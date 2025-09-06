e30e6359f9475bd37a5735f9a9a20de3542dad83
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:51 +0700

    Upload SQL chunk: t36041_chunk_0000.sql

---
+++ sql_chunks/t36041_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t36041
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t36041" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36041
+INSERT INTO [t36041] VALUES (1,'سند المصنف','3','0','0');
+INSERT INTO [t36041] VALUES (2,'الأحاديث الواردة في ذم المكس','3','0','0');
+INSERT INTO [t36041] VALUES (3,'صاحب المكس في النار','4','0','0');
+INSERT INTO [t36041] VALUES (4,'ساعة تفتح فيها أبواب السماء','6','0','0');
+INSERT INTO [t36041] VALUES (5,'الساحر لا يستجاب له','7','0','0');
+INSERT INTO [t36041] VALUES (6,'من دلائل النبوة','8','0','0');
+INSERT INTO [t36041] VALUES (7,'هل كان سهيل عشارا','9','0','0');
