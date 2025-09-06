ed9cb09ea8ccef91de93059f651632c12031f930
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:32 +0700

    Upload SQL chunk: t23422_chunk_0000.sql

---
+++ sql_chunks/t23422_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t23422
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t23422" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23422
+INSERT INTO [t23422] VALUES (1,'كتاب الحج (١)','2','0','0');
+INSERT INTO [t23422] VALUES (2,'كتاب الحج (٢)','35','0','0');
+INSERT INTO [t23422] VALUES (3,'كتاب الحج (٣)','67','0','0');
+INSERT INTO [t23422] VALUES (4,'كتاب الحج (٤)','107','0','0');
+INSERT INTO [t23422] VALUES (5,'كتاب الحج (٥)','140','0','0');
+INSERT INTO [t23422] VALUES (6,'كتاب الحج (٦)','182','0','0');
+INSERT INTO [t23422] VALUES (7,'كتاب الحج (٧)','209','0','0');
+INSERT INTO [t23422] VALUES (8,'كتاب الحج (٨)','232','0','0');
+INSERT INTO [t23422] VALUES (9,'كتاب الحج (٩)','263','0','0');
+INSERT INTO [t23422] VALUES (10,'كتاب الحج (١٠)','284','0','0');
+INSERT INTO [t23422] VALUES (11,'كتاب الحج (١١)','308','0','0');
+INSERT INTO [t23422] VALUES (12,'كتاب الحج (١٢)','332','0','0');
+INSERT INTO [t23422] VALUES (13,'كتاب الحج (١٣)','357','0','0');
+INSERT INTO [t23422] VALUES (14,'كتاب الحج (١٤)','390','0','0');
+INSERT INTO [t23422] VALUES (15,'كتاب الحج (١٥)','422','0','0');
+INSERT INTO [t23422] VALUES (16,'كتاب الحج (١٦)','453','0','0');
+INSERT INTO [t23422] VALUES (17,'كتاب الحج (١٧)','487','0','0');
+INSERT INTO [t23422] VALUES (18,'كتاب الحج (١٨)','515','0','0');
+INSERT INTO [t23422] VALUES (19,'كتاب الحج (١٩)','538','0','0');
+INSERT INTO [t23422] VALUES (20,'كتاب الحج (٢٠)','561','0','0');
+INSERT INTO [t23422] VALUES (21,'كتاب الحج (٢١)','590','0','0');
+INSERT INTO [t23422] VALUES (22,'كتاب الحج (٢٢)','618','0','0');
+INSERT INTO [t23422] VALUES (23,'كتاب الحج (٢٣)','649','0','0');
+INSERT INTO [t23422] VALUES (24,'كتاب الحج (٢٤)','684','0','0');
+INSERT INTO [t23422] VALUES (25,'كتاب الحج (٢٥)','717','0','0');
+INSERT INTO [t23422] VALUES (26,'كتاب الحج (٢٦)','748','0','0');
+INSERT INTO [t23422] VALUES (27,'كتاب الحج (٢٧)','779','0','0');
+INSERT INTO [t23422] VALUES (28,'كتاب الحج (٢٨)','815','0','0');
+INSERT INTO [t23422] VALUES (29,'كتاب الحج (٢٩)','844','0','0');
