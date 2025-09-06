10f2eafb4de59e4d291d005265bbe23dfdc5c9f8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:23:12 +0700

    Upload SQL chunk: t546_chunk_0000.sql

---
+++ sql_chunks/t546_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t546
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t546" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t546
+INSERT INTO [t546] VALUES (1,'أولا التكفير حكم شرعي مرده إلى الله ورسوله','1','0','0');
+INSERT INTO [t546] VALUES (2,'ثانيا استباحة الدماء وانتهاك الأعراض وأمثالها من الأعمال محرمة','5','0','0');
+INSERT INTO [t546] VALUES (3,'ثالثا الإسلام بريء من هذا المعتقد الخاطئ','7','0','0');
