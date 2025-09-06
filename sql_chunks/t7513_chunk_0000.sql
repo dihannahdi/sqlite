efad310228c09fc48110f7537c7d9bffca105d69
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:17 +0700

    Upload SQL chunk: t7513_chunk_0000.sql

---
+++ sql_chunks/t7513_chunk_0000.sql
@@ -0,0 +1,44 @@
+-- Table: t7513
+-- Chunk: 1
+-- Rows: 37
+
+CREATE TABLE "t7513" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7513
+INSERT INTO [t7513] VALUES (1,'أما الكتاب فمن وجوه','6','0','0');
+INSERT INTO [t7513] VALUES (2,'أحدها: قوله تعالى','6','1','0');
+INSERT INTO [t7513] VALUES (3,'الوجه الثاني من دلالة الكتاب','8','1','0');
+INSERT INTO [t7513] VALUES (4,'الوجه الثالث','10','1','0');
+INSERT INTO [t7513] VALUES (5,'الوجه الرابع','10','1','0');
+INSERT INTO [t7513] VALUES (6,'الوجه الخامس','11','1','0');
+INSERT INTO [t7513] VALUES (7,'فصل فاما دلالة السنة فمن وجوه','12','0','0');
+INSERT INTO [t7513] VALUES (8,'أحدها','12','7','0');
+INSERT INTO [t7513] VALUES (9,'الثاني','15','7','0');
+INSERT INTO [t7513] VALUES (10,'الثالث','18','7','0');
+INSERT INTO [t7513] VALUES (11,'وأما آثار الصحابة فمن وجوه','18','0','0');
+INSERT INTO [t7513] VALUES (12,'أحدها','18','11','0');
+INSERT INTO [t7513] VALUES (13,'الوجه الثاني','18','11','0');
+INSERT INTO [t7513] VALUES (14,'واما الاعتبار وأصول الشريعة','21','0','0');
+INSERT INTO [t7513] VALUES (15,'الأول','21','14','0');
+INSERT INTO [t7513] VALUES (16,'الوجه الثاني','22','14','0');
+INSERT INTO [t7513] VALUES (17,'الوجه الثالث','22','14','0');
+INSERT INTO [t7513] VALUES (18,'الوجه الرابع','22','14','0');
+INSERT INTO [t7513] VALUES (19,'الوجه الخامس','23','14','0');
+INSERT INTO [t7513] VALUES (20,'الوجه السادس','23','14','0');
+INSERT INTO [t7513] VALUES (21,'الوجه السابع','24','14','0');
+INSERT INTO [t7513] VALUES (22,'الوجه الثامن','26','14','0');
+INSERT INTO [t7513] VALUES (23,'الوجه التاسع','27','14','0');
+INSERT INTO [t7513] VALUES (24,'الوجه العاشر','28','14','0');
+INSERT INTO [t7513] VALUES (25,'الوجه الحادي عشر','29','14','0');
+INSERT INTO [t7513] VALUES (26,'الوجه الثاني عشر','30','14','0');
+INSERT INTO [t7513] VALUES (27,'الوجه الثالث عشر','32','14','0');
+INSERT INTO [t7513] VALUES (28,'الوجه الرابع عشر','33','14','0');
+INSERT INTO [t7513] VALUES (29,'الوجه الخامس عشر','35','14','0');
+INSERT INTO [t7513] VALUES (30,'الوجه السادس عشر','35','14','0');
+INSERT INTO [t7513] VALUES (31,'الوجه السابع عشر','36','14','0');
+INSERT INTO [t7513] VALUES (32,'الوجه الثامن عشر','36','14','0');
+INSERT INTO [t7513] VALUES (33,'الوجه التاسع عشر','39','14','0');
+INSERT INTO [t7513] VALUES (34,'الوجه العشرون','40','14','0');
+INSERT INTO [t7513] VALUES (35,'الوجه الحادي والعشرون','41','14','0');
+INSERT INTO [t7513] VALUES (36,'الوجه الرابع والعشرون','42','14','0');
+INSERT INTO [t7513] VALUES (37,'الوجه الخامس والعشرون','43','14','0');
