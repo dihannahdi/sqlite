1f593beec6c6796e0198e38b660daf9379ecd40a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:35 +0700

    Upload SQL chunk: t13165_chunk_0000.sql

---
+++ sql_chunks/t13165_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t13165
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t13165" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13165
+INSERT INTO [t13165] VALUES (1,'حديث أبي عبد الله بلال بن رباح','1','0','0');
+INSERT INTO [t13165] VALUES (2,'باب المسح على الخفين','8','0','0');
+INSERT INTO [t13165] VALUES (3,'باب أفطر الحاجم والمحجوم','17','0','0');
+INSERT INTO [t13165] VALUES (4,'باب الأذان','19','0','0');
