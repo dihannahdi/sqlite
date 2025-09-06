2ac4695cb91582280133cc0ff3a0ffa3d6fe352d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:26 +0700

    Upload SQL chunk: t21799_chunk_0000.sql

---
+++ sql_chunks/t21799_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t21799
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t21799" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21799
+INSERT INTO [t21799] VALUES (1,'كتاب التوهم','1','0','0');
+INSERT INTO [t21799] VALUES (2,'بسم الله الرحمن الرحيم','7','0','0');
+INSERT INTO [t21799] VALUES (3,'تم كتاب (التوهم) بحمد الله','82','0','0');
