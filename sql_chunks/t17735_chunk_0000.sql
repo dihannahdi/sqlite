8796437acfa3592ed2c75cc39cda709802c1b935
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:10 +0700

    Upload SQL chunk: t17735_chunk_0000.sql

---
+++ sql_chunks/t17735_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t17735
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t17735" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17735
+INSERT INTO [t17735] VALUES (1,'تصدير','4','0','0');
+INSERT INTO [t17735] VALUES (2,'مقدمة','10','0','0');
+INSERT INTO [t17735] VALUES (3,'ترجمة مؤلف "مفتاح طريق الأولياء"','13','0','0');
+INSERT INTO [t17735] VALUES (4,'ترجمة مؤلف "نبذة لطيفة ونصيحة شريفة"','17','0','0');
+INSERT INTO [t17735] VALUES (5,'فصل','25','0','0');
+INSERT INTO [t17735] VALUES (6,'فصل','25','0','0');
+INSERT INTO [t17735] VALUES (7,'فصل','26','0','0');
+INSERT INTO [t17735] VALUES (8,'فصل','26','0','0');
