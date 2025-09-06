17422968c5c5822ccb7c8a3054a406d794c86c2a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:38 +0700

    Upload SQL chunk: t26768_chunk_0000.sql

---
+++ sql_chunks/t26768_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t26768
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t26768" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26768
+INSERT INTO [t26768] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26768] VALUES (2,'مخطوطة جار الله ولي الدين أفندي بتركيا','2','0','0');
+INSERT INTO [t26768] VALUES (3,'مخطوطة مكتبة ألماي بتركيا','36','0','0');
