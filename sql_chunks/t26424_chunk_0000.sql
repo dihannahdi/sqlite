92e4052988f37ceab84110b4866d2610b9f2992d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:19 +0700

    Upload SQL chunk: t26424_chunk_0000.sql

---
+++ sql_chunks/t26424_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26424
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26424" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26424
+INSERT INTO [t26424] VALUES (1,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t26424] VALUES (2,'آخر حديث المحاملي','53','0','0');
