5d8a18cafe1def4c738e5bfdb4b8565af4b42e21
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:19 +0700

    Upload SQL chunk: t11350_chunk_0000.sql

---
+++ sql_chunks/t11350_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11350
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11350" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11350
+INSERT INTO [t11350] VALUES (1,'كتب غريب القرآن','1','0','0');
+INSERT INTO [t11350] VALUES (2,'المراجع','18','0','0');
+INSERT INTO [t11350] VALUES (3,'الدراسات في غريب القرآن','19','0','0');
