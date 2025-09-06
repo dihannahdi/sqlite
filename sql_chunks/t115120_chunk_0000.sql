b1dc53977bdc7454a06f759b947c3196090dceb9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:55 +0700

    Upload SQL chunk: t115120_chunk_0000.sql

---
+++ sql_chunks/t115120_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t115120
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t115120" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t115120
+INSERT INTO [t115120] VALUES (1,'ماذا يراد بالإخاء الديني؟','4','0','0');
+INSERT INTO [t115120] VALUES (2,'القرآن له منزلة الفصل في الكتب السماوية السابقة','8','0','0');
+INSERT INTO [t115120] VALUES (3,'هل الإخاء الديني جماعة سياسية؟','12','0','0');
+INSERT INTO [t115120] VALUES (4,'وادي الراحة - والبديل عن القدس','17','0','0');
