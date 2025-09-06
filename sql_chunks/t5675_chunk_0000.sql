89a44f31762a41cb649eaa574e26395cee0004d5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:16 +0700

    Upload SQL chunk: t5675_chunk_0000.sql

---
+++ sql_chunks/t5675_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t5675
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t5675" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5675
+INSERT INTO [t5675] VALUES (1,'خطبة الكتاب','1','0','0');
+INSERT INTO [t5675] VALUES (2,'آية في الوفاء بالوعد','3','0','0');
+INSERT INTO [t5675] VALUES (3,'فأولها سبب النزول','4','0','0');
+INSERT INTO [t5675] VALUES (4,'وثانيها أن هذا وعد من الله عز وجل لرسوله','10','0','0');
+INSERT INTO [t5675] VALUES (5,'وثالثها في حكم الوفاء بالوعد','25','0','0');
+INSERT INTO [t5675] VALUES (6,'ورابعها في الاحاديث والآثار الواردة في الوفاء بالوعد','38','0','0');
+INSERT INTO [t5675] VALUES (7,'وخامسها في شيء من الأشعار في ذلك','68','0','0');
