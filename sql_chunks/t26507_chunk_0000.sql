a8a63ada1a03cd6e28689de8911e4ef39b9356da
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:58 +0700

    Upload SQL chunk: t26507_chunk_0000.sql

---
+++ sql_chunks/t26507_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t26507
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t26507" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26507
+INSERT INTO [t26507] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26507] VALUES (2,'باب','2','0','0');
+INSERT INTO [t26507] VALUES (3,'باب','6','0','0');
+INSERT INTO [t26507] VALUES (4,'باب','9','0','0');
+INSERT INTO [t26507] VALUES (5,'باب','15','0','0');
+INSERT INTO [t26507] VALUES (6,'باب','18','0','0');
+INSERT INTO [t26507] VALUES (7,'باب','21','0','0');
+INSERT INTO [t26507] VALUES (8,'باب','23','0','0');
+INSERT INTO [t26507] VALUES (9,'باب','26','0','0');
+INSERT INTO [t26507] VALUES (10,'باب','28','0','0');
+INSERT INTO [t26507] VALUES (11,'باب','30','0','0');
+INSERT INTO [t26507] VALUES (12,'باب','32','0','0');
+INSERT INTO [t26507] VALUES (13,'باب','34','0','0');
+INSERT INTO [t26507] VALUES (14,'باب','42','0','0');
+INSERT INTO [t26507] VALUES (15,'باب','45','0','0');
+INSERT INTO [t26507] VALUES (16,'باب','47','0','0');
+INSERT INTO [t26507] VALUES (17,'باب','49','0','0');
+INSERT INTO [t26507] VALUES (18,'باب','51','0','0');
+INSERT INTO [t26507] VALUES (19,'باب','55','0','0');
+INSERT INTO [t26507] VALUES (20,'باب','56','0','0');
+INSERT INTO [t26507] VALUES (21,'باب','58','0','0');
+INSERT INTO [t26507] VALUES (22,'باب','60','0','0');
+INSERT INTO [t26507] VALUES (23,'باب','62','0','0');
+INSERT INTO [t26507] VALUES (24,'باب','64','0','0');
+INSERT INTO [t26507] VALUES (25,'باب','66','0','0');
+INSERT INTO [t26507] VALUES (26,'باب','68','0','0');
+INSERT INTO [t26507] VALUES (27,'باب','70','0','0');
