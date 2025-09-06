65495d314e94c3b99cfed67ca5e042611dac31c6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:33 +0700

    Upload SQL chunk: t7609_chunk_0000.sql

---
+++ sql_chunks/t7609_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7609
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7609" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7609
+INSERT INTO [t7609] VALUES (1,'الحسنة والسيئة','1','0','0');
+INSERT INTO [t7609] VALUES (2,'المؤلف: شيخ الإسلام ابن تيمية','1','0','0');
