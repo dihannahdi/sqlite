222eabf348fccade606d2415a1ea606e6652ea58
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:55 +0700

    Upload SQL chunk: t149873_chunk_0000.sql

---
+++ sql_chunks/t149873_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t149873
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t149873" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t149873
+INSERT INTO [t149873] VALUES (1,'ديوان قيس بن الخطيم','1','0','0');
