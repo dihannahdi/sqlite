fde9061304e9f6c759ff075ebf87a44c150cd355
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:11 +0700

    Upload SQL chunk: t11262_chunk_0000.sql

---
+++ sql_chunks/t11262_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t11262
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t11262" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11262
+INSERT INTO [t11262] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11262] VALUES (2,'شروط الصلاة','2','0','0');
+INSERT INTO [t11262] VALUES (3,'شروط الوضوء','7','0','0');
+INSERT INTO [t11262] VALUES (4,'فروض الوضوء','11','0','0');
+INSERT INTO [t11262] VALUES (5,'حكم التسمية في الوضوء','20','0','0');
+INSERT INTO [t11262] VALUES (6,'نواقض الوضوء','22','0','0');
+INSERT INTO [t11262] VALUES (7,'عوده إلى بقية شروط الصلاة','31','0','0');
+INSERT INTO [t11262] VALUES (8,'أركان الصلاة','45','0','0');
+INSERT INTO [t11262] VALUES (9,'واجبات الصلاة','92','0','0');
+INSERT INTO [t11262] VALUES (10,'المحتويات','97','0','0');
