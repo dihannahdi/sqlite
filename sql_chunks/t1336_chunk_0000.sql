1c8c09beb43e55e10092337fc29f0e12b411849e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:14:14 +0700

    Upload SQL chunk: t1336_chunk_0000.sql

---
+++ sql_chunks/t1336_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t1336
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t1336" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1336
+INSERT INTO [t1336] VALUES (1,'ترجمة المؤلف، مختصرة من تاريخ ابن خلكان','1','0','0');
+INSERT INTO [t1336] VALUES (2,'الكتاب','2','0','0');
+INSERT INTO [t1336] VALUES (3,'ملحق','217','0','0');
+INSERT INTO [t1336] VALUES (4,'القسم الأول: ما نص على أنه من الأمالي الصغرى فقط أو من الأمالي الصغرى وغيرها','217','3','0');
+INSERT INTO [t1336] VALUES (5,'القسم الثاني ما نص على أنه من الأمالي الوسطى فقط','226','3','0');
+INSERT INTO [t1336] VALUES (6,'القسم الثالث ما نص على أنه من الأمالي الكبرى فقط','234','3','0');
+INSERT INTO [t1336] VALUES (7,'القسم الرابع ما ورد منسوبا إلى الأمالي للزجاجي، هملا بدون قيد','235','3','0');
