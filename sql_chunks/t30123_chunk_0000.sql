0b0cce217445cedc5673d966cf06217f73510f4f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:50 +0700

    Upload SQL chunk: t30123_chunk_0000.sql

---
+++ sql_chunks/t30123_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t30123
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t30123" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30123
+INSERT INTO [t30123] VALUES (1,'الجزء السابع عشر من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30123] VALUES (2,'من فوائد النسفي','8','0','0');
+INSERT INTO [t30123] VALUES (3,'من فوائد أبي بكر الطريثيثي','9','0','0');
+INSERT INTO [t30123] VALUES (4,'من حديث عبد الله بن أحمد بن حنبل','10','0','0');
+INSERT INTO [t30123] VALUES (5,'من حديث ابن بكير الحافظ','16','0','0');
+INSERT INTO [t30123] VALUES (6,'من فوائد جعفر السراج','23','0','0');
+INSERT INTO [t30123] VALUES (7,'من فوائد عبد العزيز الأزجي','24','0','0');
+INSERT INTO [t30123] VALUES (8,'من حديث عبد الله بن أحمد بن حنبل','27','0','0');
+INSERT INTO [t30123] VALUES (9,'من حديث عبد الله بن أحمد أيضا','32','0','0');
+INSERT INTO [t30123] VALUES (10,'من حديث الحسين بن جعفر السلماسي','52','0','0');
+INSERT INTO [t30123] VALUES (11,'من حديث أبي بكر الشافعي','56','0','0');
+INSERT INTO [t30123] VALUES (12,'من حديث الشافعي أيضا','59','0','0');
+INSERT INTO [t30123] VALUES (13,'من حديثه أيضا','62','0','0');
+INSERT INTO [t30123] VALUES (14,'من حديثه أيضا','65','0','0');
+INSERT INTO [t30123] VALUES (15,'من حديث الشافعي أيضا','68','0','0');
+INSERT INTO [t30123] VALUES (16,'من حديث أبي زكريا البخاري','72','0','0');
