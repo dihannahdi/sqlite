a5054f54e07bcaf623cebd7e9cf3f6b7a08ae513
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:05 +0700

    Upload SQL chunk: t11088_chunk_0000.sql

---
+++ sql_chunks/t11088_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t11088
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t11088" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11088
+INSERT INTO [t11088] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11088] VALUES (2,'المسألة الأولى: في تعريف الصحابي','23','0','0');
+INSERT INTO [t11088] VALUES (3,'المسألة الثانية: في طرق إثبات الصحبة','45','0','0');
+INSERT INTO [t11088] VALUES (4,'المسألة الثالثة: في تقرير عدالة الصحابة','55','0','0');
+INSERT INTO [t11088] VALUES (5,'مصادر ومراجع','102','0','0');
