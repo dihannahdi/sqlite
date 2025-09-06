0370c17fb4066bc3566a830545380ff37ba08c81
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:20 +0700

    Upload SQL chunk: t11431_chunk_0000.sql

---
+++ sql_chunks/t11431_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11431
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11431" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11431
+INSERT INTO [t11431] VALUES (1,'جزء فيه ثلاثة مجالس من أمالي أبي القاسم بن بشران ٤٣٠ - مخطوط','2','0','0');
