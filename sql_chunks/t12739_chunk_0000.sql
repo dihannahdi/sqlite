93e43c0a8b135d4b2a603ba4aa5e303f595e4637
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:20 +0700

    Upload SQL chunk: t12739_chunk_0000.sql

---
+++ sql_chunks/t12739_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t12739
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t12739" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12739
+INSERT INTO [t12739] VALUES (1,'باب ذم النفس','22','0','0');
+INSERT INTO [t12739] VALUES (2,'باب معاقبة النفس','49','0','0');
+INSERT INTO [t12739] VALUES (3,'باب جهاد النفس ومنعها من شهواتها','63','0','0');
+INSERT INTO [t12739] VALUES (4,'باب الحذر على النفس مخافة سوء المنقلب والمقت','91','0','0');
+INSERT INTO [t12739] VALUES (5,'باب إجهاز النفس في الأعمال طلب الراحة يوم المعاد','104','0','0');
