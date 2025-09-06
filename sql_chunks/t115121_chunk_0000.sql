9bbdb78d5eb9e8f0c1ecf1fcd96adcdc00085408
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:56 +0700

    Upload SQL chunk: t115121_chunk_0000.sql

---
+++ sql_chunks/t115121_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t115121
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t115121" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t115121
+INSERT INTO [t115121] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t115121] VALUES (2,'أليست هذه مذاهب تخفي وراءها مصالح خاصة؟','4','0','0');
+INSERT INTO [t115121] VALUES (3,'أهذه مذاهب فكرية؟','9','0','0');
+INSERT INTO [t115121] VALUES (4,'من المفهوم .. إلى التطبيق','14','0','0');
+INSERT INTO [t115121] VALUES (5,'• في مفهوم العلمانية .. وفي تطبيقها','15','4','0');
+INSERT INTO [t115121] VALUES (6,'• في مفهوم الماسونية','19','4','0');
+INSERT INTO [t115121] VALUES (7,'• في مفهوم الصليبية الدولية','22','4','0');
+INSERT INTO [t115121] VALUES (8,'• في مفهوم الإلحاد العلمي','26','4','0');
+INSERT INTO [t115121] VALUES (9,'• في مفهوم الاستشراق','30','4','0');
+INSERT INTO [t115121] VALUES (10,'• في مفهوم العلم .. والدين','33','4','0');
+INSERT INTO [t115121] VALUES (11,'المواجهة هي السبيل','37','0','0');
