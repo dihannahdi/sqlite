f1b3e31e503323c647a66150b950edd5ec671d6d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:18 +0700

    Upload SQL chunk: t36096_chunk_0000.sql

---
+++ sql_chunks/t36096_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t36096
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t36096" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36096
+INSERT INTO [t36096] VALUES (1,'فضائل العفاف','3','0','0');
+INSERT INTO [t36096] VALUES (2,'١ - العفة من سبل الفلاح:','3','1','0');
+INSERT INTO [t36096] VALUES (3,'٢ - العفة عنوان الصلاح:','4','1','0');
+INSERT INTO [t36096] VALUES (4,'٣ - العفة سبب في المغفرة:','4','1','0');
+INSERT INTO [t36096] VALUES (5,'٤ - العفة تلبية لنداء الرحمن:','5','1','0');
+INSERT INTO [t36096] VALUES (6,'٥ - العفة مفتاح الفرج:','7','1','0');
+INSERT INTO [t36096] VALUES (7,'٦ - بالعفة كملت مريم:','8','1','0');
+INSERT INTO [t36096] VALUES (8,'٧ - العفة مخالفة لطريق الهالكين:','8','1','0');
+INSERT INTO [t36096] VALUES (9,'٨ - العفة مفتاح الجنة:','10','1','0');
+INSERT INTO [t36096] VALUES (10,'٩ - العفة تاج:','12','1','0');
+INSERT INTO [t36096] VALUES (11,'عقبات في طريق العفاف','13','0','0');
+INSERT INTO [t36096] VALUES (12,'١ - الموضة والأزياء:','13','11','0');
+INSERT INTO [t36096] VALUES (13,'٢ - القنوات الفضائية:','13','11','0');
+INSERT INTO [t36096] VALUES (14,'٣ - الإنترنت:','13','11','0');
+INSERT INTO [t36096] VALUES (15,'٤ - المجلات الخليعة:','13','11','0');
+INSERT INTO [t36096] VALUES (16,'٥ - الخلوة والاختلاط:','14','11','0');
+INSERT INTO [t36096] VALUES (17,'٦ - صديقات السوء:','14','11','0');
+INSERT INTO [t36096] VALUES (18,'٧ - تأخير الزواج:','14','11','0');
+INSERT INTO [t36096] VALUES (19,'٨ - التساهل مع الرجال الأجانب:','14','11','0');
+INSERT INTO [t36096] VALUES (20,'٩ - إطلاق البصر:','14','11','0');
