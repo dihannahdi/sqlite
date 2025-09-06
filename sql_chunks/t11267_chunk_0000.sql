e55357498e80be2621107cf5b2ba9134025729f6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:16 +0700

    Upload SQL chunk: t11267_chunk_0000.sql

---
+++ sql_chunks/t11267_chunk_0000.sql
@@ -0,0 +1,49 @@
+-- Table: t11267
+-- Chunk: 1
+-- Rows: 42
+
+CREATE TABLE "t11267" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11267
+INSERT INTO [t11267] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11267] VALUES (2,'نعمة بلوغ رمضان','3','0','0');
+INSERT INTO [t11267] VALUES (3,'ابتداء صيام رمضان','5','0','0');
+INSERT INTO [t11267] VALUES (4,'البشرى بقدوم رمضان','7','0','0');
+INSERT INTO [t11267] VALUES (5,'معنى رمضان والصيام','9','0','0');
+INSERT INTO [t11267] VALUES (6,'من فضائل الصيام','11','0','0');
+INSERT INTO [t11267] VALUES (7,'صلاة التراويح','13','0','0');
+INSERT INTO [t11267] VALUES (8,'تدريب الأطفال على الصيام','15','0','0');
+INSERT INTO [t11267] VALUES (9,'فوائد الإفطار على الرطب','17','0','0');
+INSERT INTO [t11267] VALUES (10,'السواك للصائم','19','0','0');
+INSERT INTO [t11267] VALUES (11,'بركة السحور','21','0','0');
+INSERT INTO [t11267] VALUES (12,'الإفراط في الشبع','23','0','0');
+INSERT INTO [t11267] VALUES (13,'رمضان والمدخنون','25','0','0');
+INSERT INTO [t11267] VALUES (14,'الإسراف في رمضان','27','0','0');
+INSERT INTO [t11267] VALUES (15,'العمرة في رمضان','29','0','0');
+INSERT INTO [t11267] VALUES (16,'فضائل الدعاء','31','0','0');
+INSERT INTO [t11267] VALUES (17,'حفظ اللسان','33','0','0');
+INSERT INTO [t11267] VALUES (18,'ذم التوسل','35','0','0');
+INSERT INTO [t11267] VALUES (19,'فوائد الصيام الصحية','37','0','0');
+INSERT INTO [t11267] VALUES (20,'الزكاة المفروضة','41','0','0');
+INSERT INTO [t11267] VALUES (21,'صدقة التطوع','43','0','0');
+INSERT INTO [t11267] VALUES (22,'قراءة القرآن','45','0','0');
+INSERT INTO [t11267] VALUES (23,'البكاء عند قراءة القرآن','47','0','0');
+INSERT INTO [t11267] VALUES (24,'التعاون على البر في رمضان','49','0','0');
+INSERT INTO [t11267] VALUES (25,'كفارة الجماع','51','0','0');
+INSERT INTO [t11267] VALUES (26,'السفر والمرض','53','0','0');
+INSERT INTO [t11267] VALUES (27,'سؤال الله الجنة','55','0','0');
+INSERT INTO [t11267] VALUES (28,'شهر الانتصارات','57','0','0');
+INSERT INTO [t11267] VALUES (29,'معركة بدر الكبرى','59','0','0');
+INSERT INTO [t11267] VALUES (30,'فتح مكة المكرمة','61','0','0');
+INSERT INTO [t11267] VALUES (31,'العشر الأواخر','63','0','0');
+INSERT INTO [t11267] VALUES (32,'الاعتكاف','65','0','0');
+INSERT INTO [t11267] VALUES (33,'فضل قيام الليل','67','0','0');
+INSERT INTO [t11267] VALUES (34,'المسارعة إلى فعل الخيرات','69','0','0');
+INSERT INTO [t11267] VALUES (35,'التماس ليلة القدر','71','0','0');
+INSERT INTO [t11267] VALUES (36,'التوبة','73','0','0');
+INSERT INTO [t11267] VALUES (37,'الاستغفار','75','0','0');
+INSERT INTO [t11267] VALUES (38,'وداع رمضان','77','0','0');
+INSERT INTO [t11267] VALUES (39,'صدقة الفطر','79','0','0');
+INSERT INTO [t11267] VALUES (40,'ختام الشهر','81','0','0');
+INSERT INTO [t11267] VALUES (41,'عيد الفطر','83','0','0');
+INSERT INTO [t11267] VALUES (42,'حال المسلم بعد رمضان','85','0','0');
