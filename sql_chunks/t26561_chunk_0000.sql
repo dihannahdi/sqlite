a18190edc00ce742612d0264e81915e0dfed3728
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:29 +0700

    Upload SQL chunk: t26561_chunk_0000.sql

---
+++ sql_chunks/t26561_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26561
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26561" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26561
+INSERT INTO [t26561] VALUES (1,'عوالي هشام بن عروة وغيره لأبي الحجاج الدمشقي','2','0','0');
