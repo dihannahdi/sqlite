eec4402c7289c120feace8f9e9789e25d91c5287
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:26 +0700

    Upload SQL chunk: t95829_chunk_0000.sql

---
+++ sql_chunks/t95829_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t95829
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t95829" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95829
+INSERT INTO [t95829] VALUES (1,'مقدمة المحقق','5','0','0');
+INSERT INTO [t95829] VALUES (2,'ترجمة القاضي عياض','9','1','0');
+INSERT INTO [t95829] VALUES (3,'اسمه ومولده:','9','2','0');
+INSERT INTO [t95829] VALUES (4,'مكانته العلمية:','9','2','0');
+INSERT INTO [t95829] VALUES (5,'مشائخه:','9','2','0');
+INSERT INTO [t95829] VALUES (6,'حياته وآثاره:','10','2','0');
+INSERT INTO [t95829] VALUES (7,'وفاته:','11','2','0');
+INSERT INTO [t95829] VALUES (8,'تواليفه:','11','2','0');
+INSERT INTO [t95829] VALUES (9,'شعره:','13','2','0');
+INSERT INTO [t95829] VALUES (10,'مصادر الترجمة.','13','2','0');
+INSERT INTO [t95829] VALUES (11,'ترجمة السيوطي','14','1','0');
+INSERT INTO [t95829] VALUES (12,'اسمه ومولده:','14','11','0');
+INSERT INTO [t95829] VALUES (13,'مشايخه:','14','11','0');
+INSERT INTO [t95829] VALUES (14,'مكانته العلمية:','15','11','0');
+INSERT INTO [t95829] VALUES (15,'زهده وعبادته:','16','11','0');
+INSERT INTO [t95829] VALUES (16,'شعره:','17','11','0');
+INSERT INTO [t95829] VALUES (17,'منهج العمل','24','1','0');
+INSERT INTO [t95829] VALUES (18,'وصف النسخة الخطية','24','1','0');
+INSERT INTO [t95829] VALUES (19,'مناهل الصفا','25','0','0');
+INSERT INTO [t95829] VALUES (20,'فهرس المصادر','245','0','0');
+INSERT INTO [t95829] VALUES (21,'من أعمال مركز الخدمات والأبحاث الثقافية','248','0','0');
