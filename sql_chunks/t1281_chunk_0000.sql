8b8b2a39dedd3760d46604e68068fc4c88749c39
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:30 +0700

    Upload SQL chunk: t1281_chunk_0000.sql

---
+++ sql_chunks/t1281_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t1281
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t1281" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1281
+INSERT INTO [t1281] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1281] VALUES (2,'وسطية الإسلام','4','0','0');
+INSERT INTO [t1281] VALUES (3,'مفهوم الوسطية ومنشؤها','4','2','0');
+INSERT INTO [t1281] VALUES (4,'آفاق الوسطية','8','2','0');
+INSERT INTO [t1281] VALUES (5,'سماحة الإسلام','38','0','0');
+INSERT INTO [t1281] VALUES (6,'نصوص من الكتاب والسنة','38','5','0');
+INSERT INTO [t1281] VALUES (7,'من مظاهر اليسر والسماحة وآلياته في أصول الشريعة','42','5','0');
+INSERT INTO [t1281] VALUES (8,'دعوة الإسلام للحوار','52','0','0');
