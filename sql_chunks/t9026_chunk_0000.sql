0e680a6e7b5106f03cb9c6b299522b5933867aaf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:20 +0700

    Upload SQL chunk: t9026_chunk_0000.sql

---
+++ sql_chunks/t9026_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9026
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9026" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9026
+INSERT INTO [t9026] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t9026] VALUES (2,'أهمية الدعوة','3','0','0');
