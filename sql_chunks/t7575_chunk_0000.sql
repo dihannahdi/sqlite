0a0939e8bd800e2b530e73e831bf023809bd6e65
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:08 +0700

    Upload SQL chunk: t7575_chunk_0000.sql

---
+++ sql_chunks/t7575_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t7575
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t7575" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7575
+INSERT INTO [t7575] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7575] VALUES (2,'نص المنظومة','7','0','0');
+INSERT INTO [t7575] VALUES (3,'الاعتصام بالكتاب والسنة ومجانبة البدع','9','0','0');
+INSERT INTO [t7575] VALUES (4,'صفة الكلام','16','0','0');
+INSERT INTO [t7575] VALUES (5,'إثبات رؤية الله تعالى','25','0','0');
+INSERT INTO [t7575] VALUES (6,'إثبات صفة النزول لله تعالى','42','0','0');
+INSERT INTO [t7575] VALUES (7,'عقيدة أهل السنة في الصحابة','53','0','0');
+INSERT INTO [t7575] VALUES (8,'الإيمان بالقدر','67','0','0');
+INSERT INTO [t7575] VALUES (9,'الإيمان باليوم الآخر','76','0','0');
+INSERT INTO [t7575] VALUES (10,'حكم مرتكب الكبيرة والتحذير من مذهبي الخوارج والمرجئة','91','0','0');
+INSERT INTO [t7575] VALUES (11,'تعريف الإيمان وزيادته ونقصانه','101','0','0');
+INSERT INTO [t7575] VALUES (12,'التحذير من الرأي، ومن القدح في الحديث وأهله','108','0','0');
