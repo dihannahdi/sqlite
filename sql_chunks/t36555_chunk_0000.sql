2b80cbc91f97336156b275bd5a1d20bf2c303523
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:09 +0700

    Upload SQL chunk: t36555_chunk_0000.sql

---
+++ sql_chunks/t36555_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t36555
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t36555" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36555
+INSERT INTO [t36555] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36555] VALUES (2,'مدخل','3','0','0');
+INSERT INTO [t36555] VALUES (3,'وسارعوا','3','0','0');
+INSERT INTO [t36555] VALUES (4,'قبل البداية','8','0','0');
+INSERT INTO [t36555] VALUES (5,'الانطلاقة','11','0','0');
+INSERT INTO [t36555] VALUES (6,'خطوة خطوة','14','0','0');
+INSERT INTO [t36555] VALUES (7,'لله الأمر','16','0','0');
+INSERT INTO [t36555] VALUES (8,'ما بال أقوام','18','0','0');
+INSERT INTO [t36555] VALUES (9,'مفتاح القلوب','22','0','0');
+INSERT INTO [t36555] VALUES (10,'استثمار الفرص','25','0','0');
+INSERT INTO [t36555] VALUES (11,'تهادوا تحابوا','30','0','0');
+INSERT INTO [t36555] VALUES (12,'أبواب مشرعة','32','0','0');
+INSERT INTO [t36555] VALUES (13,'يا أبت','37','0','0');
+INSERT INTO [t36555] VALUES (14,'العدو','39','0','0');
+INSERT INTO [t36555] VALUES (15,'وقفات','40','0','0');
+INSERT INTO [t36555] VALUES (16,'لا تزل قدمك','43','0','0');
+INSERT INTO [t36555] VALUES (17,'بحر الجود','46','0','0');
+INSERT INTO [t36555] VALUES (18,'أسهل الوسائل','48','0','0');
+INSERT INTO [t36555] VALUES (19,'يا بني','50','0','0');
+INSERT INTO [t36555] VALUES (20,'أصحاب الهمم','52','0','0');
+INSERT INTO [t36555] VALUES (21,'تقدموا وتأخرنا','54','0','0');
+INSERT INTO [t36555] VALUES (22,'أنت تحت المجهر','56','0','0');
+INSERT INTO [t36555] VALUES (23,'الصدقة الجارية','58','0','0');
+INSERT INTO [t36555] VALUES (24,'تنفيس الكرب','60','0','0');
+INSERT INTO [t36555] VALUES (25,'الصحبة','62','0','0');
+INSERT INTO [t36555] VALUES (26,'الجليس الصامت','65','0','0');
+INSERT INTO [t36555] VALUES (27,'أصغ بقلبك','68','0','0');
+INSERT INTO [t36555] VALUES (28,'لمسات حانية','71','0','0');
+INSERT INTO [t36555] VALUES (29,'المجاهدة','76','0','0');
+INSERT INTO [t36555] VALUES (30,'آخر الدواء الكي','79','0','0');
+INSERT INTO [t36555] VALUES (31,'أنت أم هم؟','82','0','0');
