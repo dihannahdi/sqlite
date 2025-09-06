804903f3f2670e515c1b544268d1f6218e1c59d6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:25 +0700

    Upload SQL chunk: t352_chunk_0000.sql

---
+++ sql_chunks/t352_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t352
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t352" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t352
+INSERT INTO [t352] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t352] VALUES (2,'أولاد كهلان بن سبأ','1','0','0');
+INSERT INTO [t352] VALUES (3,'بنو عريب بن زيد بن كهلان','1','0','0');
+INSERT INTO [t352] VALUES (4,'بنو غالب ومالك ابني زيد بن كهلان','1','0','0');
+INSERT INTO [t352] VALUES (5,'نسب همدان','1','0','0');
+INSERT INTO [t352] VALUES (6,'بنو عمرو بن همدان','1','0','0');
+INSERT INTO [t352] VALUES (7,'بنو نوف بن همدان','4','0','0');
+INSERT INTO [t352] VALUES (8,'نسب حاشد','4','0','0');
+INSERT INTO [t352] VALUES (9,'بنو يريم بن جشم بن حاشد','4','0','0');
+INSERT INTO [t352] VALUES (10,'بنو ضمام بن جشم بن حاشد','4','0','0');
+INSERT INTO [t352] VALUES (11,'بنو مرثد بن جشم بن حاشد','4','0','0');
+INSERT INTO [t352] VALUES (12,'سائر ولد مرثد بن جشم بن حاشد','7','0','0');
+INSERT INTO [t352] VALUES (13,'بنو مالك بن جشم بن حاشد','8','0','0');
+INSERT INTO [t352] VALUES (14,'نسب آل مرب ملوك حاشد','8','0','0');
+INSERT INTO [t352] VALUES (15,'بسم الله الرحمن الرحيم','10','0','0');
+INSERT INTO [t352] VALUES (16,'نسب آل ذي كبار','11','0','0');
+INSERT INTO [t352] VALUES (17,'بقية بني السبع','11','0','0');
+INSERT INTO [t352] VALUES (18,'بنو عمرو بن كثير بن مالك بن جشم','11','0','0');
+INSERT INTO [t352] VALUES (19,'يعوق الصنم','12','0','0');
+INSERT INTO [t352] VALUES (20,'بنو قابض أخي خيوان','12','0','0');
+INSERT INTO [t352] VALUES (21,'بنو عامر بن مالك','12','0','0');
+INSERT INTO [t352] VALUES (22,'بن جشم بن حاشد','12','0','0');
+INSERT INTO [t352] VALUES (23,'بنو ذي بارق بن مالك','13','0','0');
+INSERT INTO [t352] VALUES (24,'بن جشم بن حاشد','13','0','0');
+INSERT INTO [t352] VALUES (25,'بن جشم بن حاشد','13','0','0');
+INSERT INTO [t352] VALUES (26,'بنو دافع بن مالك','13','0','0');
+INSERT INTO [t352] VALUES (27,'نسب المعيديين','14','0','0');
+INSERT INTO [t352] VALUES (28,'نسب وادعة','17','0','0');
+INSERT INTO [t352] VALUES (29,'معدي كرب بن جشم بن حاشد','21','0','0');
+INSERT INTO [t352] VALUES (30,'بنو زيد بن جشم بن حاشد','21','0','0');
+INSERT INTO [t352] VALUES (31,'بنو عمرو بن جشم بن حاشد','22','0','0');
+INSERT INTO [t352] VALUES (32,'بنو عريب بن جشم بن حاشد','22','0','0');
+INSERT INTO [t352] VALUES (33,'أنساب بكيل بن جشم بن حبران','25','0','0');
+INSERT INTO [t352] VALUES (34,'بنو صعب بن دومان','29','0','0');
+INSERT INTO [t352] VALUES (35,'بنو سفيان بن أرحب','43','0','0');
+INSERT INTO [t352] VALUES (36,'نسب بني صبارة بن سفيان','45','0','0');
