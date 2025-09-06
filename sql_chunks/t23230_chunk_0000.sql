5c0f76c3da378ac1894d5f555345294db760a240
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:17 +0700

    Upload SQL chunk: t23230_chunk_0000.sql

---
+++ sql_chunks/t23230_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t23230
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t23230" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23230
+INSERT INTO [t23230] VALUES (1,'نسبها','1','0','0');
+INSERT INTO [t23230] VALUES (2,'نشأتها','1','0','0');
+INSERT INTO [t23230] VALUES (3,'زينب وبناء الكعبة','1','0','0');
+INSERT INTO [t23230] VALUES (4,'زينب والإسلام','2','0','0');
+INSERT INTO [t23230] VALUES (5,'زواجها','3','0','0');
+INSERT INTO [t23230] VALUES (6,'الهجرة','4','0','0');
+INSERT INTO [t23230] VALUES (7,'يوم الفرقان','5','0','0');
+INSERT INTO [t23230] VALUES (8,'من يبارز؟','6','0','0');
+INSERT INTO [t23230] VALUES (9,'الزواج وأم المؤمنين','7','0','0');
+INSERT INTO [t23230] VALUES (10,'أم المساكين','8','0','0');
+INSERT INTO [t23230] VALUES (11,'الوفاة','8','0','0');
