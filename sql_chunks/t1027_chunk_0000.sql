4b75f5e0a46387581c9b052259d826116013fe12
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:40 +0700

    Upload SQL chunk: t1027_chunk_0000.sql

---
+++ sql_chunks/t1027_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t1027
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t1027" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1027
+INSERT INTO [t1027] VALUES (1,'الكتيبة الكامنة في من لقيناه بالأندلس من شعراء المائة الثامنة','1','0','0');
+INSERT INTO [t1027] VALUES (2,'تمهيد','1','0','0');
+INSERT INTO [t1027] VALUES (3,'مقدمة','3','0','0');
+INSERT INTO [t1027] VALUES (4,'مراجع التحقيق','19','0','0');
+INSERT INTO [t1027] VALUES (5,'بسم الله الرحمن الرحيم','20','0','0');
