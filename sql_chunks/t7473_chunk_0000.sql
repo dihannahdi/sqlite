f7852802af585cfc5ddcbb4993f2ace1607e4a74
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:42 +0700

    Upload SQL chunk: t7473_chunk_0000.sql

---
+++ sql_chunks/t7473_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t7473
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t7473" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7473
+INSERT INTO [t7473] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7473] VALUES (2,'القسم الأول: أبيات في العقيدة','3','0','0');
+INSERT INTO [t7473] VALUES (3,'القسم الثاني: أبيات متفرقة وتشمل: نصائح، مواعظ، وصايا، حكم، أمثال، أدب','36','0','0');
+INSERT INTO [t7473] VALUES (4,'مصادر ومراجع','84','0','0');
