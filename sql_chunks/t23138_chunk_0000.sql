365855546d941a63b72277fa6c4a59f44462a0e1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:02 +0700

    Upload SQL chunk: t23138_chunk_0000.sql

---
+++ sql_chunks/t23138_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t23138
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t23138" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23138
+INSERT INTO [t23138] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t23138] VALUES (2,'الفصل الأول: الجغرافية التاريخية','4','0','0');
+INSERT INTO [t23138] VALUES (3,'وصف خراسان','4','2','0');
+INSERT INTO [t23138] VALUES (4,'ولاة خراسان','14','2','0');
+INSERT INTO [t23138] VALUES (5,'القبائل العربية بخراسان','32','2','0');
+INSERT INTO [t23138] VALUES (6,'الفصل الثاني: موضوعات الشعر وخصائصه','71','0','0');
+INSERT INTO [t23138] VALUES (7,'تصوير الأحوال السياسية','71','6','0');
+INSERT INTO [t23138] VALUES (8,'تسجيل الغزوات الحربية','120','6','0');
+INSERT INTO [t23138] VALUES (9,'موضوعات أخرى تقليدية','154','6','0');
+INSERT INTO [t23138] VALUES (10,'خصائص موضوعية وفنية ولفظية','201','6','0');
+INSERT INTO [t23138] VALUES (11,'الفصل الثالث: شعراء القبائل بخراسان','207','0','0');
+INSERT INTO [t23138] VALUES (12,'شاعر أزدي متعصب موتور','207','11','0');
+INSERT INTO [t23138] VALUES (13,'شاعر أزدي مفتون متهور','229','11','0');
+INSERT INTO [t23138] VALUES (14,'شاعر بكري مضطرب متحزب','247','11','0');
+INSERT INTO [t23138] VALUES (15,'شاعر تميمي سوي متبصر','257','11','0');
+INSERT INTO [t23138] VALUES (16,'شاعر تميمي منافق متقلب','273','11','0');
+INSERT INTO [t23138] VALUES (17,'شاعر مولى جوال متكسب','282','11','0');
+INSERT INTO [t23138] VALUES (18,'خاتمة','298','0','0');
+INSERT INTO [t23138] VALUES (19,'المصادر والمراجع','302','0','0');
+INSERT INTO [t23138] VALUES (20,'محتويات الكتاب','310','0','0');
