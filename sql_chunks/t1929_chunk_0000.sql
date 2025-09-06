c24b62c840a014d6eaa49d44e2054a4bf39fc897
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:25 +0700

    Upload SQL chunk: t1929_chunk_0000.sql

---
+++ sql_chunks/t1929_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t1929
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t1929" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1929
+INSERT INTO [t1929] VALUES (1,'صالح بن فوزان الفوزان','2','0','0');
+INSERT INTO [t1929] VALUES (2,'عبد الله بن سليمان المنيع','3','0','0');
+INSERT INTO [t1929] VALUES (3,'عبد الله بن محمد الغنيمان','4','0','0');
+INSERT INTO [t1929] VALUES (4,'المقدمة','5','0','0');
+INSERT INTO [t1929] VALUES (5,'أهمية مسألة الإيمان','7','0','0');
+INSERT INTO [t1929] VALUES (6,'مسألة تعريف الإيمان ومسماه:','8','0','0');
+INSERT INTO [t1929] VALUES (7,'بيان فساد أقوال الطوائف إجمالا','11','0','0');
+INSERT INTO [t1929] VALUES (8,'معنى الإيمان لغة:','17','0','0');
+INSERT INTO [t1929] VALUES (9,'العلاقة بين الإسلام والإيمان:','19','0','0');
+INSERT INTO [t1929] VALUES (10,'زيادة الإيمان ونقصانه:','24','0','0');
+INSERT INTO [t1929] VALUES (11,'الأدلة من السنة على زيادة الإيمان ونقصانه:','28','0','0');
+INSERT INTO [t1929] VALUES (12,'الآثار السلفية عن الصحابة ومن بعدهم','30','0','0');
+INSERT INTO [t1929] VALUES (13,'أسباب زيادة الإيمان ونقصانه:','31','0','0');
+INSERT INTO [t1929] VALUES (14,'مسألة الإيمان المطلق ومطلق الإيمان:','34','0','0');
+INSERT INTO [t1929] VALUES (15,'الكفر عند أهل السنة والجماعة ومخالفيهم','38','0','0');
+INSERT INTO [t1929] VALUES (16,'أهمية الموضوع:','39','0','0');
+INSERT INTO [t1929] VALUES (17,'الكفر ومعناه:','42','0','0');
+INSERT INTO [t1929] VALUES (18,'قواعد مهمة في معرفة أنواع الكفر','45','0','0');
+INSERT INTO [t1929] VALUES (19,'ملاحظة وتنبيه:','48','0','0');
+INSERT INTO [t1929] VALUES (20,'فهرس المحتوى','52','0','0');
