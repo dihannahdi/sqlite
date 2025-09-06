3e35e72c0850ff6a9100eaac560a12cdf8dd69ca
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:14 +0700

    Upload SQL chunk: t8202_chunk_0000.sql

---
+++ sql_chunks/t8202_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t8202
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t8202" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8202
+INSERT INTO [t8202] VALUES (1,'مجلس أول','1','0','0');
+INSERT INTO [t8202] VALUES (2,'مجلس ثان للباغندي','28','0','0');
+INSERT INTO [t8202] VALUES (3,'مجلس ثالث للباغندي','41','0','0');
+INSERT INTO [t8202] VALUES (4,'مجلس رابع للباغندي','51','0','0');
+INSERT INTO [t8202] VALUES (5,'مجلس خامس للباغندي','65','0','0');
