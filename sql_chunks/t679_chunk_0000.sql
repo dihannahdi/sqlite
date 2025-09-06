133469f0a522cce578685d037fe1b8b6029f6451
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:47 +0700

    Upload SQL chunk: t679_chunk_0000.sql

---
+++ sql_chunks/t679_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t679
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t679" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t679
+INSERT INTO [t679] VALUES (1,'صفة الفتوى والمفتي والمستفتي','1','0','0');
