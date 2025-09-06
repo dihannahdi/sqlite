4f683f2584a547fe2d1185fb918613987055748e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:49 +0700

    Upload SQL chunk: t8725_chunk_0000.sql

---
+++ sql_chunks/t8725_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t8725
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t8725" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8725
+INSERT INTO [t8725] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8725] VALUES (2,'اضاعة الصحة','2','0','0');
+INSERT INTO [t8725] VALUES (3,'اتلاف المال','3','0','0');
+INSERT INTO [t8725] VALUES (4,'الرائحة المنتنة','6','0','0');
+INSERT INTO [t8725] VALUES (5,'تخدير الجسم بمادة النيكوتين','8','0','0');
+INSERT INTO [t8725] VALUES (6,'تعريض الجسم للتهلكة بسرطان الرئة','12','0','0');
+INSERT INTO [t8725] VALUES (7,'لماذا يشيع التدخين بين الشباب؟','15','0','0');
+INSERT INTO [t8725] VALUES (8,'كيف توقف التدخين؟','17','0','0');
+INSERT INTO [t8725] VALUES (9,'نداء إلى شباب الإسلام..!','20','0','0');
