478284f06bfbc9c8e96ee18071caf7fa62530010
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:29:23 +0700

    Upload SQL chunk: t17328_chunk_0000.sql

---
+++ sql_chunks/t17328_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t17328
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t17328" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17328
+INSERT INTO [t17328] VALUES (1,'تقديم الشيخ عبد العزيز بن عبد الله بن باز','2','0','0');
+INSERT INTO [t17328] VALUES (2,'تقديم الشيخ عبد الله بن محمد بن حميد','4','0','0');
+INSERT INTO [t17328] VALUES (3,'المقدمة','5','0','0');
+INSERT INTO [t17328] VALUES (4,'الخاتمة','23','0','0');
