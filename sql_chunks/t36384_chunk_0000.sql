cb7989cff722c47128ebfd395deacf33cc37dd56
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:41 +0700

    Upload SQL chunk: t36384_chunk_0000.sql

---
+++ sql_chunks/t36384_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t36384
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t36384" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36384
+INSERT INTO [t36384] VALUES (1,'الحمد لله تعالى بصر بالحق فهدى','1','0','0');
+INSERT INTO [t36384] VALUES (2,'* فماذا أعددت له أيها المذنب','2','1','0');
+INSERT INTO [t36384] VALUES (3,'* أيها المذنب! احذر فظاعة المثوى','2','1','0');
+INSERT INTO [t36384] VALUES (4,'* أيها المذنب! تذكر بيت الوحدة','4','1','0');
+INSERT INTO [t36384] VALUES (5,'* أيها المذنب! إنه بيت الظلمة','7','1','0');
+INSERT INTO [t36384] VALUES (6,'* تذكر ضمة القبر','8','1','0');
+INSERT INTO [t36384] VALUES (7,'* أيها المذنب! كيف أنت إذا تفرق عنك الأهل والأصحاب','9','1','0');
+INSERT INTO [t36384] VALUES (8,'* أيها المذنب! القبر حاجز بينك وبين الصالحات','10','1','0');
