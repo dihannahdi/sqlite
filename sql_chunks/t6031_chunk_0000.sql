134ea6e92d787d4da7f585f912ed0028479bcdec
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:34 +0700

    Upload SQL chunk: t6031_chunk_0000.sql

---
+++ sql_chunks/t6031_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6031
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6031" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6031
+INSERT INTO [t6031] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6031] VALUES (2,'عونك يا رب وتيسيرك','1','0','0');
