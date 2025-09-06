04bd76c6b98f7ba204c33c430ddcf31e43c567b3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:18:22 +0700

    Upload SQL chunk: t14333_chunk_0000.sql

---
+++ sql_chunks/t14333_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t14333
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t14333" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14333
+INSERT INTO [t14333] VALUES (1,'الدعاء باب مغلق يحتاج إلي مفتاح! !','3','0','0');
+INSERT INTO [t14333] VALUES (2,'أسرار حول إجابة الدعاء! !','3','0','0');
+INSERT INTO [t14333] VALUES (3,'طريق إجابة الدعاء','5','0','0');
+INSERT INTO [t14333] VALUES (4,'أمور تعين على إجابة الدعاء','8','0','0');
+INSERT INTO [t14333] VALUES (5,'الشرط الأكبر للدعاء المستجاب','10','0','0');
+INSERT INTO [t14333] VALUES (6,'روح الدعاء المستجاب .. التذلل والافتقار!','12','0','0');
+INSERT INTO [t14333] VALUES (7,'الدعاء المستجاب .. أوقات وأماكن','14','0','0');
+INSERT INTO [t14333] VALUES (8,'المفتاح العجيب! ! (آداب الدعاء)','19','0','0');
+INSERT INTO [t14333] VALUES (9,'كلمات أخيرة .. لكل طارق باب ربه تبارك وتعالى ..','22','0','0');
