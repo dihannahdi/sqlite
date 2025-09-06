103f83e38f8e0dbc674fe0b527d860dba1e2d84b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:15 +0700

    Upload SQL chunk: t5893_chunk_0000.sql

---
+++ sql_chunks/t5893_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t5893
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t5893" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5893
+INSERT INTO [t5893] VALUES (1,'باب تسمية من سمع من النبي','20','0','0');
+INSERT INTO [t5893] VALUES (2,'باب تسمية الإخوة','25','0','0');
+INSERT INTO [t5893] VALUES (3,'باب من روى عنه ممن اسمه هشام','68','0','0');
+INSERT INTO [t5893] VALUES (4,'باب من روى عنه ممن اسمه سالم','70','0','0');
+INSERT INTO [t5893] VALUES (5,'باب من روى عنه ممن اسمه قيس','73','0','0');
+INSERT INTO [t5893] VALUES (6,'باب من روي عنه ممن اسمه معبد','76','0','0');
+INSERT INTO [t5893] VALUES (7,'باب من روى عنه ممن اسمه المغيرة','77','0','0');
+INSERT INTO [t5893] VALUES (8,'باب من روى عنه ممن اسمه مسلم','80','0','0');
+INSERT INTO [t5893] VALUES (9,'باب من روى عنه ممن اسمه إياس','84','0','0');
+INSERT INTO [t5893] VALUES (10,'باب من روى عنه ممن اسمه الأشعث من أهل البصره','85','0','0');
+INSERT INTO [t5893] VALUES (11,'ومن أهل الكوفة','85','0','0');
+INSERT INTO [t5893] VALUES (12,'باب من روى عنه ممن اسمه وهب','87','0','0');
+INSERT INTO [t5893] VALUES (13,'باب من روى عنه ممن اسمه يونس','89','0','0');
+INSERT INTO [t5893] VALUES (14,'باب ممن اسمه أيوب','91','0','0');
