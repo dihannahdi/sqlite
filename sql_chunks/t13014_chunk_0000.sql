729c27baa9dbd4143439d90f2a772d8ea2f09bec
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:09:06 +0700

    Upload SQL chunk: t13014_chunk_0000.sql

---
+++ sql_chunks/t13014_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t13014
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t13014" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13014
+INSERT INTO [t13014] VALUES (1,'طلب العلم والصبر عليه','2','0','0');
+INSERT INTO [t13014] VALUES (2,'طلب الرفعة عند الله بالحلم','5','0','0');
+INSERT INTO [t13014] VALUES (3,'شرف الحلم في يوم القيامة','9','0','0');
+INSERT INTO [t13014] VALUES (4,'الحليم وأعوانه','15','0','0');
+INSERT INTO [t13014] VALUES (5,'خصلتان يحبهما الله الحلم والأناة','25','0','0');
+INSERT INTO [t13014] VALUES (6,'فضيلة السكوت عن السفيه','28','0','0');
+INSERT INTO [t13014] VALUES (7,'الناس رجلان. . . . عاقل وجاهل','44','0','0');
+INSERT INTO [t13014] VALUES (8,'أشعر الناس','48','0','0');
+INSERT INTO [t13014] VALUES (9,'أجمل الحكم والأشعار في ذكر الحلم','54','0','0');
+INSERT INTO [t13014] VALUES (10,'من حكم وهب بن منبه','61','0','0');
+INSERT INTO [t13014] VALUES (11,'وصية لقمان لابنه عن الحلم','63','0','0');
+INSERT INTO [t13014] VALUES (12,'محبة الله للحليم','68','0','0');
+INSERT INTO [t13014] VALUES (13,'فضل أهل الحلم يوم القيامة','71','0','0');
+INSERT INTO [t13014] VALUES (14,'فضل مجالسة الحلماء','74','0','0');
+INSERT INTO [t13014] VALUES (15,'وصية أبي الدرداء بالحلم','77','0','0');
+INSERT INTO [t13014] VALUES (16,'الصبر على المكروه من علامة الحلماء','91','0','0');
+INSERT INTO [t13014] VALUES (17,'زينة المرء في الحلم','99','0','0');
+INSERT INTO [t13014] VALUES (18,'من وصايا لقمان','114','0','0');
+INSERT INTO [t13014] VALUES (19,'السيادة لأهل الحلم','122','0','0');
+INSERT INTO [t13014] VALUES (20,'الناس معادن','132','0','0');
+INSERT INTO [t13014] VALUES (21,'ستر زلة الكريم من الحلم','139','0','0');
+INSERT INTO [t13014] VALUES (22,'ثواب الحلم يوم القيامة','145','0','0');
