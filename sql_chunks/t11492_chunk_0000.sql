2135a7a585d2b1bb010f4ed5e328b490b790f67d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:48 +0700

    Upload SQL chunk: t11492_chunk_0000.sql

---
+++ sql_chunks/t11492_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11492
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11492" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11492
+INSERT INTO [t11492] VALUES (1,'حديث عيسى وحديث الطير وحديث الضب','2','0','0');
