4e9fca1aa3c6d53fa0151137cbbd4b733a9561bc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:40 +0700

    Upload SQL chunk: t26467_chunk_0000.sql

---
+++ sql_chunks/t26467_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26467
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26467" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26467
+INSERT INTO [t26467] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26467] VALUES (2,'ذم ذي الوجهين واللسانين','2','0','0');
