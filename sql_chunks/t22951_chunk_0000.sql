70309ff140c4214e76c508e22889bdd3e13243c8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:25 +0700

    Upload SQL chunk: t22951_chunk_0000.sql

---
+++ sql_chunks/t22951_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t22951
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t22951" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22951
+INSERT INTO [t22951] VALUES (1,'الرد على من كذب بالأحاديث الصحيحة الواردة في المهدي','1','0','0');
+INSERT INTO [t22951] VALUES (2,'الشبهة الأولى:','18','0','0');
+INSERT INTO [t22951] VALUES (3,'الشبهة الثانية:','18','0','0');
+INSERT INTO [t22951] VALUES (4,'الشبهة الثالثة:','19','0','0');
+INSERT INTO [t22951] VALUES (5,'الشبهة الرابعة:','19','0','0');
