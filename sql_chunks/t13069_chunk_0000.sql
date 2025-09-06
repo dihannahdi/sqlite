fd20a15a9c42f1dd6a29bdf38bb638daffb51892
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:09:58 +0700

    Upload SQL chunk: t13069_chunk_0000.sql

---
+++ sql_chunks/t13069_chunk_0000.sql
@@ -0,0 +1,63 @@
+-- Table: t13069
+-- Chunk: 1
+-- Rows: 56
+
+CREATE TABLE "t13069" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13069
+INSERT INTO [t13069] VALUES (1,'معاذ بن أنس الجهني','1','0','0');
+INSERT INTO [t13069] VALUES (2,'عرفجة بن أسعد','15','0','0');
+INSERT INTO [t13069] VALUES (3,'أبو العشراء الدارمي','18','0','0');
+INSERT INTO [t13069] VALUES (4,'عتبان بن مالك','20','0','0');
+INSERT INTO [t13069] VALUES (5,'عمرو بن خارجة','23','0','0');
+INSERT INTO [t13069] VALUES (6,'عمارة بن أوس','25','0','0');
+INSERT INTO [t13069] VALUES (7,'سعد بن الأطول','27','0','0');
+INSERT INTO [t13069] VALUES (8,'أبو مرثد الغنوي','31','0','0');
+INSERT INTO [t13069] VALUES (9,'عبد الله بن عبد الرحمن الأنصاري','33','0','0');
+INSERT INTO [t13069] VALUES (10,'عبد الرحمن بن سمرة','35','0','0');
+INSERT INTO [t13069] VALUES (11,'المقداد بن عمرو الكندي','37','0','0');
+INSERT INTO [t13069] VALUES (12,'عبد الرحمن بن شبل الأنصاري','39','0','0');
+INSERT INTO [t13069] VALUES (13,'جندب بن عبد الله البجلي','41','0','0');
+INSERT INTO [t13069] VALUES (14,'ثابت بن الضحاك الأنصاري','58','0','0');
+INSERT INTO [t13069] VALUES (15,'حمزة الأسلمي','60','0','0');
+INSERT INTO [t13069] VALUES (16,'يزيد بن ركانة','62','0','0');
+INSERT INTO [t13069] VALUES (17,'الجارود','65','0','0');
+INSERT INTO [t13069] VALUES (18,'عبد الله بن الحارث بن جزء الزبيدي','67','0','0');
+INSERT INTO [t13069] VALUES (19,'هبيب بن مغفل','70','0','0');
+INSERT INTO [t13069] VALUES (20,'أبو شهم','72','0','0');
+INSERT INTO [t13069] VALUES (21,'رافع بن مكيث','74','0','0');
+INSERT INTO [t13069] VALUES (22,'رياح بن ربيع','77','0','0');
+INSERT INTO [t13069] VALUES (23,'عفيف الكندي','79','0','0');
+INSERT INTO [t13069] VALUES (24,'قتادة بن النعمان','81','0','0');
+INSERT INTO [t13069] VALUES (25,'معن بن يزيد','85','0','0');
+INSERT INTO [t13069] VALUES (26,'أحمر صاحب النبي صلى الله عليه وسلم','87','0','0');
+INSERT INTO [t13069] VALUES (27,'هشام بن عامر','89','0','0');
+INSERT INTO [t13069] VALUES (28,'أبو جمعة','96','0','0');
+INSERT INTO [t13069] VALUES (29,'عبد خير','100','0','0');
+INSERT INTO [t13069] VALUES (30,'عبد الله بن سرجس','102','0','0');
+INSERT INTO [t13069] VALUES (31,'عمرو بن مرة','105','0','0');
+INSERT INTO [t13069] VALUES (32,'مخول البهزي','109','0','0');
+INSERT INTO [t13069] VALUES (33,'عم أبي حرة الرقاشي','111','0','0');
+INSERT INTO [t13069] VALUES (34,'الحارث الأشعري','114','0','0');
+INSERT INTO [t13069] VALUES (35,'أبو هبيرة الأنصاري','116','0','0');
+INSERT INTO [t13069] VALUES (36,'سعد مولى أبي بكر الصديق','118','0','0');
+INSERT INTO [t13069] VALUES (37,'دغفل','121','0','0');
+INSERT INTO [t13069] VALUES (38,'عبيد مولى النبي صلى الله عليه وسلم','123','0','0');
+INSERT INTO [t13069] VALUES (39,'أبو مالك الأشعري','125','0','0');
+INSERT INTO [t13069] VALUES (40,'العباس بن مرداس السلمي','127','0','0');
+INSERT INTO [t13069] VALUES (41,'حابس التميمي','129','0','0');
+INSERT INTO [t13069] VALUES (42,'الحكم بن ميناء','131','0','0');
+INSERT INTO [t13069] VALUES (43,'عمير بن سعد','133','0','0');
+INSERT INTO [t13069] VALUES (44,'الحارث بن وقيش','135','0','0');
+INSERT INTO [t13069] VALUES (45,'الفلتان بن عاصم','137','0','0');
+INSERT INTO [t13069] VALUES (46,'معن بن نضلة','139','0','0');
+INSERT INTO [t13069] VALUES (47,'وابصة بن معبد','141','0','0');
+INSERT INTO [t13069] VALUES (48,'ثابت بن شماس','146','0','0');
+INSERT INTO [t13069] VALUES (49,'سفينة','148','0','0');
+INSERT INTO [t13069] VALUES (50,'قيس بن عاصم المنقري','154','0','0');
+INSERT INTO [t13069] VALUES (51,'رجل يقال له: طارق','156','0','0');
+INSERT INTO [t13069] VALUES (52,'حنظلة بن حنيفة','158','0','0');
+INSERT INTO [t13069] VALUES (53,'أبو رزين العقيلي','160','0','0');
+INSERT INTO [t13069] VALUES (54,'رجل من الأنصار','162','0','0');
+INSERT INTO [t13069] VALUES (55,'خالد الخزاعي','164','0','0');
+INSERT INTO [t13069] VALUES (56,'عتبة بن غزوان','166','0','0');
