cc2053c65712a4d23491060020967de4cece9944
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:50:16 +0700

    Upload SQL chunk: t10628_chunk_0000.sql

---
+++ sql_chunks/t10628_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t10628
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t10628" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10628
+INSERT INTO [t10628] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t10628] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t10628] VALUES (3,'من هتف من المقبرة بموعظة','13','0','0');
+INSERT INTO [t10628] VALUES (4,'باب الموعظة بالجنازة والاعتبار بها','26','0','0');
+INSERT INTO [t10628] VALUES (5,'باب في النشور','70','0','0');
+INSERT INTO [t10628] VALUES (6,'جامع ذكر القبور','83','0','0');
+INSERT INTO [t10628] VALUES (7,'باب ما قرئ من الكتاب على القبور','201','0','0');
