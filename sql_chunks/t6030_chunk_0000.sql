f9d41d456eeb950d322b91faeedbd6517b6c5ba8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:33 +0700

    Upload SQL chunk: t6030_chunk_0000.sql

---
+++ sql_chunks/t6030_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t6030
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t6030" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6030
+INSERT INTO [t6030] VALUES (1,'بسم الله الرحمن الرحيم وصلى الله على سيدنا محمد وعلى آله وسلم','1','0','0');
+INSERT INTO [t6030] VALUES (2,'الضرب الأول السماع من لفظ الشيخ','67','0','0');
+INSERT INTO [t6030] VALUES (3,'الضرب الثانى القراءة على الشيخ','68','0','0');
+INSERT INTO [t6030] VALUES (4,'الضرب الثالث المناولة','77','0','0');
+INSERT INTO [t6030] VALUES (5,'نوع آخر','80','0','0');
+INSERT INTO [t6030] VALUES (6,'الضرب الرابع الكتابة','81','0','0');
+INSERT INTO [t6030] VALUES (7,'الضرب الخامس الإجازة','86','0','0');
+INSERT INTO [t6030] VALUES (8,'الوجه الثانى','89','0','0');
+INSERT INTO [t6030] VALUES (9,'الوجه الثالث','95','0','0');
+INSERT INTO [t6030] VALUES (10,'الوجه الرابع','99','0','0');
+INSERT INTO [t6030] VALUES (11,'الوجه الخامس','102','0','0');
+INSERT INTO [t6030] VALUES (12,'الوجه السادس','103','0','0');
+INSERT INTO [t6030] VALUES (13,'الضرب السادس','105','0','0');
+INSERT INTO [t6030] VALUES (14,'الضرب السابع الوصية بالكتب','113','0','0');
+INSERT INTO [t6030] VALUES (15,'الضرب الثامن الخط','114','0','0');
