ae130599a9e88c6a700f3d729b00b357d7ae2e4d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:33 +0700

    Upload SQL chunk: t96770_chunk_0000.sql

---
+++ sql_chunks/t96770_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t96770
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t96770" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96770
+INSERT INTO [t96770] VALUES (1,'باب','2','0','0');
+INSERT INTO [t96770] VALUES (2,'فصل','4','0','0');
+INSERT INTO [t96770] VALUES (3,'باب','5','0','0');
+INSERT INTO [t96770] VALUES (4,'باب','8','0','0');
+INSERT INTO [t96770] VALUES (5,'باب','10','0','0');
+INSERT INTO [t96770] VALUES (6,'باب','14','0','0');
+INSERT INTO [t96770] VALUES (7,'باب','21','0','0');
+INSERT INTO [t96770] VALUES (8,'باب','22','0','0');
+INSERT INTO [t96770] VALUES (9,'باب في عمل الفعل','24','0','0');
+INSERT INTO [t96770] VALUES (10,'باب','26','0','0');
+INSERT INTO [t96770] VALUES (11,'باب','29','0','0');
+INSERT INTO [t96770] VALUES (12,'باب','29','0','0');
+INSERT INTO [t96770] VALUES (13,'باب','30','0','0');
+INSERT INTO [t96770] VALUES (14,'فصل','33','0','0');
+INSERT INTO [t96770] VALUES (15,'باب','33','0','0');
+INSERT INTO [t96770] VALUES (16,'باب العدد','34','0','0');
