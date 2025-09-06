c7b3c26b109a8dc5a320a0f0304e9e103a5d74e2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:40 +0700

    Upload SQL chunk: t12549_chunk_0000.sql

---
+++ sql_chunks/t12549_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t12549
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t12549" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12549
+INSERT INTO [t12549] VALUES (1,'مجلس من إملاء أبو الفوارس الزينبي - مخطوط','2','0','0');
