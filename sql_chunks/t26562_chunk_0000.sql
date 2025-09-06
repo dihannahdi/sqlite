562d63bbd2d39a4fdf63c0e1bff03c8e47a2ae5d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:30 +0700

    Upload SQL chunk: t26562_chunk_0000.sql

---
+++ sql_chunks/t26562_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26562
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26562" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26562
+INSERT INTO [t26562] VALUES (1,'مجلسان من أمالي أبي الحسين بن بشران','2','0','0');
