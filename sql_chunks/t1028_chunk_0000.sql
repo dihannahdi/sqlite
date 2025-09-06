0eb66fff9915617d84b42271c7a57882ad0feeaa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:48 +0700

    Upload SQL chunk: t1028_chunk_0000.sql

---
+++ sql_chunks/t1028_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t1028
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t1028" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1028
+INSERT INTO [t1028] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1028] VALUES (2,'التقريب لحد المنطق','14','0','0');
