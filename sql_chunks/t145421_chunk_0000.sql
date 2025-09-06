1e5f962530c031a416a6f49212da37cc37c4818c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:19:58 +0700

    Upload SQL chunk: t145421_chunk_0000.sql

---
+++ sql_chunks/t145421_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t145421
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t145421" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t145421
+INSERT INTO [t145421] VALUES (1,'تفيد المعاجم اللغوية','1','0','0');
+INSERT INTO [t145421] VALUES (2,'ورد حديثان موضوعان = مكذوبان، فيهما كلمة «شاطر» ضمن معنى سيئ، وهو','9','0','0');
+INSERT INTO [t145421] VALUES (3,'أيهما أكثر دهاء: شطار مصر، أم شطار بغداد؟ !','12','0','0');
+INSERT INTO [t145421] VALUES (4,'للشطار ملاحة! !','13','0','0');
+INSERT INTO [t145421] VALUES (5,'فوائد','16','0','0');
+INSERT INTO [t145421] VALUES (6,'الخلاصة','17','0','0');
