050dac8434833099d23cb49ced7a28050fe1de30
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:09 +0700

    Upload SQL chunk: t26889_chunk_0000.sql

---
+++ sql_chunks/t26889_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t26889
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t26889" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26889
+INSERT INTO [t26889] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26889] VALUES (2,'باب الألف','2','0','0');
+INSERT INTO [t26889] VALUES (3,'من اسمه إسماعيل','4','0','0');
+INSERT INTO [t26889] VALUES (4,'من اسمه إسحاق','4','0','0');
+INSERT INTO [t26889] VALUES (5,'من اسمه أحمد','7','0','0');
+INSERT INTO [t26889] VALUES (6,'من اسمه أزهر','15','0','0');
+INSERT INTO [t26889] VALUES (7,'باب الباء','16','0','0');
+INSERT INTO [t26889] VALUES (8,'باب التاء','18','0','0');
+INSERT INTO [t26889] VALUES (9,'باب الثاء','18','0','0');
+INSERT INTO [t26889] VALUES (10,'باب الجيم','18','0','0');
+INSERT INTO [t26889] VALUES (11,'باب الحاء','19','0','0');
+INSERT INTO [t26889] VALUES (12,'من اسمه الحسين','22','0','0');
+INSERT INTO [t26889] VALUES (13,'من اسمه الحكم','23','0','0');
+INSERT INTO [t26889] VALUES (14,'باب الخاء','25','0','0');
+INSERT INTO [t26889] VALUES (15,'باب الدال','26','0','0');
+INSERT INTO [t26889] VALUES (16,'من اسمه داود','26','0','0');
+INSERT INTO [t26889] VALUES (17,'باب الذال','27','0','0');
+INSERT INTO [t26889] VALUES (18,'باب الراء','27','0','0');
+INSERT INTO [t26889] VALUES (19,'باب الزاي','27','0','0');
+INSERT INTO [t26889] VALUES (20,'من اسمه زهير','28','0','0');
+INSERT INTO [t26889] VALUES (21,'باب السين','28','0','0');
+INSERT INTO [t26889] VALUES (22,'باب الشين، والصاد، والضاد، والطاء','31','0','0');
+INSERT INTO [t26889] VALUES (23,'باب العين','32','0','0');
+INSERT INTO [t26889] VALUES (24,'باب الفاء','44','0','0');
+INSERT INTO [t26889] VALUES (25,'باب القاف','44','0','0');
+INSERT INTO [t26889] VALUES (26,'باب الميم','45','0','0');
+INSERT INTO [t26889] VALUES (27,'باب النون','56','0','0');
+INSERT INTO [t26889] VALUES (28,'باب الواو والهاء','57','0','0');
+INSERT INTO [t26889] VALUES (29,'باب الياء','58','0','0');
