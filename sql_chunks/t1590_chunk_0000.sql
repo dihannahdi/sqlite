119146b17222b01a3c905785022e4fc85e08d316
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:34 +0700

    Upload SQL chunk: t1590_chunk_0000.sql

---
+++ sql_chunks/t1590_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t1590
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t1590" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1590
+INSERT INTO [t1590] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t1590] VALUES (2,'قال الشيخ هذا آخر ما كان بخط ابن بكير','100','0','0');
