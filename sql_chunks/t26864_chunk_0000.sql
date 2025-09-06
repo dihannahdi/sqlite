9565f97fef4fc841396fa4fbc30a7d727dcd2603
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:54 +0700

    Upload SQL chunk: t26864_chunk_0000.sql

---
+++ sql_chunks/t26864_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t26864
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t26864" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26864
+INSERT INTO [t26864] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26864] VALUES (2,'الجزء الأول من كتاب الفتوة','2','0','0');
+INSERT INTO [t26864] VALUES (3,'ذكر بيان بعض مكارم الأخلاق من السنة','8','0','0');
+INSERT INTO [t26864] VALUES (4,'الجزء الثاني من الكتاب الفتوة','24','0','0');
+INSERT INTO [t26864] VALUES (5,'الجزء الثالث من الكتاب الفتوة','42','0','0');
+INSERT INTO [t26864] VALUES (6,'الجزء الرابع من الكتاب الفتوة','60','0','0');
+INSERT INTO [t26864] VALUES (7,'الجزء الخامس من الكتاب الفتوة','77','0','0');
