c814ef8fb81424ef365120ecba423914ad62ace6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:37 +0700

    Upload SQL chunk: t30096_chunk_0000.sql

---
+++ sql_chunks/t30096_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t30096
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t30096" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30096
+INSERT INTO [t30096] VALUES (1,'من حديث أبي القاسم البغوي','5','0','0');
+INSERT INTO [t30096] VALUES (2,'حديث واحد','9','0','0');
+INSERT INTO [t30096] VALUES (3,'حديث واحد','11','0','0');
+INSERT INTO [t30096] VALUES (4,'من حديث أبي القاسم البغوي','13','0','0');
+INSERT INTO [t30096] VALUES (5,'من حديث أبي القاسم البغوي أيضا','14','0','0');
+INSERT INTO [t30096] VALUES (6,'ومن حديثه أيضا','15','0','0');
+INSERT INTO [t30096] VALUES (7,'من حديث أبي جعفر شيخ الصوفية','18','0','0');
