91fad832a47374eedcd55b1c2eb81618983be2f8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:26 +0700

    Upload SQL chunk: t10045_chunk_0000.sql

---
+++ sql_chunks/t10045_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10045
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10045" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10045
+INSERT INTO [t10045] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10045] VALUES (2,'تجريد التوحيد المفيد','2','0','0');
