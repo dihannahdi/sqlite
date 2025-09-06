88b8a541120435c4f623f86f437ee0044de0d4da
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:38 +0700

    Upload SQL chunk: t10397_chunk_0000.sql

---
+++ sql_chunks/t10397_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t10397
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t10397" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10397
+INSERT INTO [t10397] VALUES (1,'باب: المبحث الأول: مدخل الدراسة','1','0','0');
+INSERT INTO [t10397] VALUES (2,'مقدمة','1','1','0');
+INSERT INTO [t10397] VALUES (3,'أهمية الدراسة','3','1','0');
+INSERT INTO [t10397] VALUES (4,'مشكلة الدراسة','4','1','0');
+INSERT INTO [t10397] VALUES (5,'أسئلة الدراسة','6','1','0');
+INSERT INTO [t10397] VALUES (6,'حدود الدراسة','7','1','0');
+INSERT INTO [t10397] VALUES (7,'منهج الدراسة','8','1','0');
+INSERT INTO [t10397] VALUES (8,'باب: المبحث الثاني: مفهوم السبق التربوي ومنهجه','9','0','0');
+INSERT INTO [t10397] VALUES (9,'باب: المبحث الثالث: معالم السبق التربوي','19','0','0');
+INSERT INTO [t10397] VALUES (10,'باب: المبحث الرابع: مقارنة التربية الإسلامية بمتناقضات التربيات الأخرى','42','0','0');
+INSERT INTO [t10397] VALUES (11,'باب: التعقيب','52','0','0');
+INSERT INTO [t10397] VALUES (12,'باب: النتائج والتوصيات','55','0','0');
+INSERT INTO [t10397] VALUES (13,'أولا: النتائج','55','12','0');
+INSERT INTO [t10397] VALUES (14,'ثانيا: التوصيات','57','12','0');
