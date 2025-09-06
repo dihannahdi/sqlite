fa108a00dfdd72828c70e163dcd52d8a4f5441a6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:56 +0700

    Upload SQL chunk: t3128_chunk_0000.sql

---
+++ sql_chunks/t3128_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t3128
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t3128" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3128
+INSERT INTO [t3128] VALUES (1,'الزيادات على كتاب الكبائر للبرديجي للضياء المقدسي','1','0','0');
