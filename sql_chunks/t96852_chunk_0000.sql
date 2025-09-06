5752efa6164cc207c3cb26f7bb29bbd2cd53fcdb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:52 +0700

    Upload SQL chunk: t96852_chunk_0000.sql

---
+++ sql_chunks/t96852_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t96852
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t96852" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96852
+INSERT INTO [t96852] VALUES (1,'[مقدمة المحقق]','2','0','0');
+INSERT INTO [t96852] VALUES (2,'عملي في المخطوط','2','1','0');
+INSERT INTO [t96852] VALUES (3,'حول الرسالة','2','1','0');
+INSERT INTO [t96852] VALUES (4,'وصف النسخ الخطية','2','1','0');
+INSERT INTO [t96852] VALUES (5,'توثيق نسبتها للمؤلف','6','1','0');
+INSERT INTO [t96852] VALUES (6,'ترجمة المؤلف','7','1','0');
+INSERT INTO [t96852] VALUES (7,'تصانيفه','8','6','0');
+INSERT INTO [t96852] VALUES (8,'[تجدد المسرات بالقسم بين الزوجات]','8','0','0');
