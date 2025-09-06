3c12468d25e60ec4ca004e7ff5132050182ac8be
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:15 +0700

    Upload SQL chunk: t11266_chunk_0000.sql

---
+++ sql_chunks/t11266_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11266
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11266" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11266
+INSERT INTO [t11266] VALUES (1,'شروط الصلاة تسعة','1','0','0');
+INSERT INTO [t11266] VALUES (2,'وأركان الصلاة أربعة عشر','4','0','0');
+INSERT INTO [t11266] VALUES (3,'والواجبات ثمانية','10','0','0');
