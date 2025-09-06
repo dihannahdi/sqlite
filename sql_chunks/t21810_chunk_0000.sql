2bba993f38caac361e3179470c97d35fad926e82
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:39 +0700

    Upload SQL chunk: t21810_chunk_0000.sql

---
+++ sql_chunks/t21810_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t21810
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t21810" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21810
+INSERT INTO [t21810] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t21810] VALUES (2,'أدلة وجوب الحجاب','4','0','0');
+INSERT INTO [t21810] VALUES (3,'أولا: أدلة القرآن','4','2','0');
+INSERT INTO [t21810] VALUES (4,'ثانيا: أدلة السنة','12','2','0');
+INSERT INTO [t21810] VALUES (5,'ثالثا: أدلة القياس','17','2','0');
+INSERT INTO [t21810] VALUES (6,'رابعا أدلة المبيحين لكشف الوجه','22','2','0');
+INSERT INTO [t21810] VALUES (7,'خامسا: الجواب عن هذه الأدلة','24','2','0');
