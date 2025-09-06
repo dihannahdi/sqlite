b3a5a70d13a50d2f615134b325bfc06c27f9068d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:29 +0700

    Upload SQL chunk: t30898_chunk_0000.sql

---
+++ sql_chunks/t30898_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t30898
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t30898" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30898
+INSERT INTO [t30898] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t30898] VALUES (2,'تمهيد الإيمان بالقضاء والقدر','4','0','0');
+INSERT INTO [t30898] VALUES (3,'ما يؤمن به المسلم','4','2','0');
+INSERT INTO [t30898] VALUES (4,'أسباب الابتلاء وأنواعه','4','2','0');
+INSERT INTO [t30898] VALUES (5,'فوائد الابتلاء','7','2','0');
+INSERT INTO [t30898] VALUES (6,'تحقيق الشهادتين','8','0','0');
+INSERT INTO [t30898] VALUES (7,'عقيدة الولاء والبراء','8','6','0');
+INSERT INTO [t30898] VALUES (8,'حكم إزهاق الأرواح','13','6','0');
+INSERT INTO [t30898] VALUES (9,'الثقة بوعد الله جل وعلا','15','6','0');
+INSERT INTO [t30898] VALUES (10,'العلماء والدعاة قدوة هذه الأمة','17','0','0');
+INSERT INTO [t30898] VALUES (11,'الواجب معرفة منهج السلف','17','10','0');
+INSERT INTO [t30898] VALUES (12,'اليقظة عند الأراجيف والشائعات','18','10','0');
+INSERT INTO [t30898] VALUES (13,'إلقاء الكلام من دون نظر فيه','20','10','0');
+INSERT INTO [t30898] VALUES (14,'الحذر من جر المعركة إلى داخل الأمة','21','10','0');
+INSERT INTO [t30898] VALUES (15,'تفويت الفرصة على الأعداء نباهة','22','10','0');
+INSERT INTO [t30898] VALUES (16,'عدم شحن النفوس','23','10','0');
+INSERT INTO [t30898] VALUES (17,'القدوة الحسنة','26','10','0');
+INSERT INTO [t30898] VALUES (18,'الوسطية أصل من أصول أهل السنة والجماعة','27','0','0');
+INSERT INTO [t30898] VALUES (19,'الجهاد صفة هذه الأمة','30','0','0');
+INSERT INTO [t30898] VALUES (20,'الاجتماع على هدي السلف عند ظهور الفتن','35','0','0');
+INSERT INTO [t30898] VALUES (21,'الخاتمة','39','0','0');
