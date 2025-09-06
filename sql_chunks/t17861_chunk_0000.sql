35b4fb53132b1fdcd85ca647dbe9c07f199d9891
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:08 +0700

    Upload SQL chunk: t17861_chunk_0000.sql

---
+++ sql_chunks/t17861_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t17861
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t17861" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17861
+INSERT INTO [t17861] VALUES (1,'تصدير المجموعة الرابعة','3','0','0');
+INSERT INTO [t17861] VALUES (2,'مقدمة التحقيق','8','0','0');
+INSERT INTO [t17861] VALUES (3,'مقدمة المعتني بها','8','2','0');
+INSERT INTO [t17861] VALUES (4,'ترجمة صاحب القصيدة وتوثيقها وتخميسها','9','2','0');
+INSERT INTO [t17861] VALUES (5,'أولا: ترجمة الناظم','9','4','0');
+INSERT INTO [t17861] VALUES (6,'ثانيا: توثيق القصيدة وصحة نسبتها لأبي عمران موسى بن بهيج الأندلسي','11','4','0');
+INSERT INTO [t17861] VALUES (7,'ثالثا: تخميسها','18','4','0');
+INSERT INTO [t17861] VALUES (8,'وصف النسختين المخطوطتين','29','2','0');
+INSERT INTO [t17861] VALUES (9,'عملي في النص','32','2','0');
+INSERT INTO [t17861] VALUES (10,'نص سماع القصيدة على فضيلة الشيخ العلامة شيخ الحنابلة عبد الله بن عبد العزيز العقيل حفظه المولى ورعاه','34','2','0');
+INSERT INTO [t17861] VALUES (11,'مقدمة وسند القصيدة إلى ابن بهيج أبي عمران','36','0','0');
+INSERT INTO [t17861] VALUES (12,'أول القصيدة','37','0','0');
+INSERT INTO [t17861] VALUES (13,'الخاتمة','40','0','0');
