3d1f26e0217765d0938c03f621db4f6ee37dda4d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:07 +0700

    Upload SQL chunk: t8979_chunk_0000.sql

---
+++ sql_chunks/t8979_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t8979
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t8979" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8979
+INSERT INTO [t8979] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8979] VALUES (2,'مكة البلد الأمين','3','0','0');
+INSERT INTO [t8979] VALUES (3,'الهجرة سبيل الأنبياء','7','0','0');
+INSERT INTO [t8979] VALUES (4,'ذكريات','9','0','0');
+INSERT INTO [t8979] VALUES (5,'المؤامرة','10','0','0');
+INSERT INTO [t8979] VALUES (6,'إلا تنصروه فقد نصره الله','15','0','0');
+INSERT INTO [t8979] VALUES (7,'إلى المدينة','17','0','0');
