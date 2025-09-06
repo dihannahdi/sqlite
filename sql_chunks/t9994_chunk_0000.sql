dc98696b41b8b221e179f2bad01f1df40a307bbf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:45 +0700

    Upload SQL chunk: t9994_chunk_0000.sql

---
+++ sql_chunks/t9994_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t9994
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t9994" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9994
+INSERT INTO [t9994] VALUES (1,'باب المقدمات','1','0','0');
+INSERT INTO [t9994] VALUES (2,'مقدمة الطبعة الجديدة','1','1','0');
+INSERT INTO [t9994] VALUES (3,'مقدمة الطبعة الثانية','22','1','0');
+INSERT INTO [t9994] VALUES (4,'مقدمة الطبعة الأولى','31','1','0');
+INSERT INTO [t9994] VALUES (5,'الشرط الأول: استيعاب جميع البدن إلا ما استثنى','33','0','0');
+INSERT INTO [t9994] VALUES (6,'الشرط الثاني: أن لا يكون زينة في نفسه','112','0','0');
+INSERT INTO [t9994] VALUES (7,'الشرط الثالث: أن يكون صفيقا لا يشف','117','0','0');
+INSERT INTO [t9994] VALUES (8,'الشرط الرابع: أن يكون فضفاضا غير ضيق فيصف شيئا من جسمها','122','0','0');
+INSERT INTO [t9994] VALUES (9,'الشرط الخامس: أن لا يكون مبخرا مطيبا','128','0','0');
+INSERT INTO [t9994] VALUES (10,'الشرط السادس: أن لايشبه لباس الرجل','132','0','0');
+INSERT INTO [t9994] VALUES (11,'الشرط السابع: أن لا يشبه لباس الكافرات','151','0','0');
+INSERT INTO [t9994] VALUES (12,'الشرط الثامن: أن لا يكون لباس شهرة','203','0','0');
+INSERT INTO [t9994] VALUES (13,'الفهارس','207','0','0');
