4f0437f4db704f588f1bab0120c11e55879e1c65
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:56 +0700

    Upload SQL chunk: t13187_chunk_0000.sql

---
+++ sql_chunks/t13187_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t13187
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t13187" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13187
+INSERT INTO [t13187] VALUES (1,'باب أخذ المال من حقه','1','0','0');
+INSERT INTO [t13187] VALUES (2,'باب فضل المال','43','0','0');
+INSERT INTO [t13187] VALUES (3,'باب إصلاح المال','114','0','0');
+INSERT INTO [t13187] VALUES (4,'باب الرفق في المعيشة وحسن التدبير','160','0','0');
+INSERT INTO [t13187] VALUES (5,'باب الاحتراف','197','0','0');
+INSERT INTO [t13187] VALUES (6,'باب أفاضل التجارات','236','0','0');
+INSERT INTO [t13187] VALUES (7,'باب المذموم من التجارة','246','0','0');
+INSERT INTO [t13187] VALUES (8,'باب المماكسة في الابتياع','275','0','0');
+INSERT INTO [t13187] VALUES (9,'باب العقارات','282','0','0');
+INSERT INTO [t13187] VALUES (10,'باب الضياع','290','0','0');
+INSERT INTO [t13187] VALUES (11,'باب عمل اليد','303','0','0');
+INSERT INTO [t13187] VALUES (12,'باب القصد في المال','319','0','0');
+INSERT INTO [t13187] VALUES (13,'باب القصد في المطعم','343','0','0');
+INSERT INTO [t13187] VALUES (14,'باب القصد في اللباس','372','0','0');
+INSERT INTO [t13187] VALUES (15,'باب التركات','412','0','0');
+INSERT INTO [t13187] VALUES (16,'باب في كثرة المال','425','0','0');
+INSERT INTO [t13187] VALUES (17,'باب الفقر','435','0','0');
