b52f365fef6b88da9247d0cdcd53aa45f65aa662
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:19 +0700

    Upload SQL chunk: t1561_chunk_0000.sql

---
+++ sql_chunks/t1561_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t1561
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t1561" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1561
+INSERT INTO [t1561] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1561] VALUES (2,'وجاء النبي المنتظر','6','0','0');
+INSERT INTO [t1561] VALUES (3,'شروط صحة الكتاب المقدس','7','0','0');
+INSERT INTO [t1561] VALUES (4,'الكتاب المقدس لدى النصارى','11','0','0');
+INSERT INTO [t1561] VALUES (5,'بشارات موسى بمحمد صلى الله عليه وسلم (البشارة الأولى)','24','0','0');
+INSERT INTO [t1561] VALUES (6,'البشارة الثانية','44','0','0');
+INSERT INTO [t1561] VALUES (7,'البشارة الثالثة','47','0','0');
+INSERT INTO [t1561] VALUES (8,'البشارة الرابعة','52','0','0');
+INSERT INTO [t1561] VALUES (9,'البشارة الخامسة','54','0','0');
+INSERT INTO [t1561] VALUES (10,'البشارة السادسة','57','0','0');
+INSERT INTO [t1561] VALUES (11,'البشارة السابعة','59','0','0');
+INSERT INTO [t1561] VALUES (12,'البشارة الثامنة','62','0','0');
+INSERT INTO [t1561] VALUES (13,'البشارة التاسعة','65','0','0');
+INSERT INTO [t1561] VALUES (14,'بشارات عيسى بمحمد صلى الله عليه وسلم (البشارة الأولى)','70','0','0');
+INSERT INTO [t1561] VALUES (15,'البشارة الثانية','90','0','0');
+INSERT INTO [t1561] VALUES (16,'البشارة الثالثة','96','0','0');
+INSERT INTO [t1561] VALUES (17,'بقايا أهل الكتاب ينتظرون النبي','103','0','0');
