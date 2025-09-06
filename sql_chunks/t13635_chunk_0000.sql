2041124dd51b6434c138699414420b55bc8cfc8c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:04 +0700

    Upload SQL chunk: t13635_chunk_0000.sql

---
+++ sql_chunks/t13635_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t13635
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t13635" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13635
+INSERT INTO [t13635] VALUES (1,'أحسن كما أحسن الله إليك','4','0','0');
+INSERT INTO [t13635] VALUES (2,'حديث عن دمشق','12','0','0');
+INSERT INTO [t13635] VALUES (3,'رمضان','20','0','0');
+INSERT INTO [t13635] VALUES (4,'مزعجات رمضان','27','0','0');
+INSERT INTO [t13635] VALUES (5,'أين أرباب الأقلام؟','33','0','0');
+INSERT INTO [t13635] VALUES (6,'نحن المذنبون','41','0','0');
+INSERT INTO [t13635] VALUES (7,'كل شيء للناس!','47','0','0');
+INSERT INTO [t13635] VALUES (8,'لا تؤجل','53','0','0');
+INSERT INTO [t13635] VALUES (9,'الحب والزواج','61','0','0');
+INSERT INTO [t13635] VALUES (10,'السن المناسبة للزواج','66','0','0');
+INSERT INTO [t13635] VALUES (11,'طريق السعادة','70','0','0');
+INSERT INTO [t13635] VALUES (12,'لصوص الوقت','79','0','0');
+INSERT INTO [t13635] VALUES (13,'الوظيفة والموظفون','85','0','0');
+INSERT INTO [t13635] VALUES (14,'الوعد الشرقي','90','0','0');
+INSERT INTO [t13635] VALUES (15,'شغلوا الطلاب في عطلة الصيف','98','0','0');
+INSERT INTO [t13635] VALUES (16,'مشكلة الزواج','104','0','0');
+INSERT INTO [t13635] VALUES (17,'أسباب المشكلة','111','0','0');
+INSERT INTO [t13635] VALUES (18,'إبراهيم بك هنانو قال لي!','119','0','0');
+INSERT INTO [t13635] VALUES (19,'من حديث المزعجات','127','0','0');
+INSERT INTO [t13635] VALUES (20,'في الفندق','134','0','0');
+INSERT INTO [t13635] VALUES (21,'بين المعلم والتلميذ','139','0','0');
+INSERT INTO [t13635] VALUES (22,'إلى الطلاب','143','0','0');
+INSERT INTO [t13635] VALUES (23,'الوصايا','150','0','0');
+INSERT INTO [t13635] VALUES (24,'نساؤنا ونساء الإفرنج','157','0','0');
+INSERT INTO [t13635] VALUES (25,'صناعة «المشيخة»','165','0','0');
+INSERT INTO [t13635] VALUES (26,'هذا نذير للناس','171','0','0');
+INSERT INTO [t13635] VALUES (27,'هذا هو الدواء','182','0','0');
+INSERT INTO [t13635] VALUES (28,'الإذاعات العربية','193','0','0');
+INSERT INTO [t13635] VALUES (29,'صور دمشقية سوداء من ربع قرن','200','0','0');
+INSERT INTO [t13635] VALUES (30,'رسالة','206','0','0');
+INSERT INTO [t13635] VALUES (31,'صور من تاريخنا العلمي','214','0','0');
+INSERT INTO [t13635] VALUES (32,'الطلاب والعطلة','228','0','0');
+INSERT INTO [t13635] VALUES (33,'في الزواج','236','0','0');
+INSERT INTO [t13635] VALUES (34,'حديث العيد','243','0','0');
+INSERT INTO [t13635] VALUES (35,'مجنون','250','0','0');
+INSERT INTO [t13635] VALUES (36,'موضوع إنشاء','257','0','0');
+INSERT INTO [t13635] VALUES (37,'من عبث التلاميذ','261','0','0');
+INSERT INTO [t13635] VALUES (38,'إلى لبنان','265','0','0');
