f21fd0b6c1190faa08c764e9c7fd3eca0c3100ac
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:46 +0700

    Upload SQL chunk: t7479_chunk_0000.sql

---
+++ sql_chunks/t7479_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t7479
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t7479" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7479
+INSERT INTO [t7479] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7479] VALUES (2,'مدخل','4','0','0');
+INSERT INTO [t7479] VALUES (3,'شيخ الإسلام محمد بن عبد الوهاب','34','0','0');
+INSERT INTO [t7479] VALUES (4,'أثر آل سعود الكرام في خدمة الإسلام وإحياء سنة النبي صلى الله عليه وسلم','65','0','0');
+INSERT INTO [t7479] VALUES (5,'النجديون في الحجاز في المرة الأولى','88','0','0');
+INSERT INTO [t7479] VALUES (6,'حال الحجاز في العهد السعودي وقبله وبعده','92','0','0');
+INSERT INTO [t7479] VALUES (7,'صقر الجزيرة وبطل الإسلام','109','0','0');
