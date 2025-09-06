b108ab1ad75f2a63995b0dd6330900ae08c42948
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:55 +0700

    Upload SQL chunk: t26865_chunk_0000.sql

---
+++ sql_chunks/t26865_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t26865
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t26865" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26865
+INSERT INTO [t26865] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26865] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t26865] VALUES (3,'من كتاب الأشربة','16','0','0');
+INSERT INTO [t26865] VALUES (4,'[في الحج]','83','0','0');
+INSERT INTO [t26865] VALUES (5,'من كتاب الزكاة','180','0','0');
+INSERT INTO [t26865] VALUES (6,'من كتاب الصلاة','201','0','0');
+INSERT INTO [t26865] VALUES (7,'من كتاب النكاح','230','0','0');
+INSERT INTO [t26865] VALUES (8,'من كتاب الصوم','272','0','0');
+INSERT INTO [t26865] VALUES (9,'[من كتاب الصلاة]','318','0','0');
+INSERT INTO [t26865] VALUES (10,'كتاب القسامة والعقول والديات','468','0','0');
