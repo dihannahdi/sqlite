0564de345c3bbf9dacfd910ad7737f956d2cf4eb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:17 +0700

    Upload SQL chunk: t3924_chunk_0000.sql

---
+++ sql_chunks/t3924_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t3924
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t3924" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3924
+INSERT INTO [t3924] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t3924] VALUES (2,'خصائص مدرسة الدعوة','2','0','0');
+INSERT INTO [t3924] VALUES (3,'الصحابة في ميدان الدعوة','8','0','0');
+INSERT INTO [t3924] VALUES (4,'الدعوة تواجه التيارات المضادة','13','0','0');
+INSERT INTO [t3924] VALUES (5,'العادات الموروثة','35','0','0');
+INSERT INTO [t3924] VALUES (6,'الخداع العقلي','36','0','0');
+INSERT INTO [t3924] VALUES (7,'واقع المسلمين المتخلف','37','0','0');
