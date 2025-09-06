9dc1a74e5f8176160731a13575d878e97a17b4e6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:08 +0700

    Upload SQL chunk: t8983_chunk_0000.sql

---
+++ sql_chunks/t8983_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t8983
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t8983" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8983
+INSERT INTO [t8983] VALUES (1,'الرسل واسطة تبليغ','1','0','0');
+INSERT INTO [t8983] VALUES (2,'الرسل لا يجلبون النفع','6','0','0');
+INSERT INTO [t8983] VALUES (3,'العلماء ورثة الأنبياء','11','0','0');
+INSERT INTO [t8983] VALUES (4,'أنواع الوسائط المردودة','13','0','0');
+INSERT INTO [t8983] VALUES (5,'الشفاعة الباطلة والصحيحة','18','0','0');
+INSERT INTO [t8983] VALUES (6,'مقدار الأسباب','22','0','0');
+INSERT INTO [t8983] VALUES (7,'الرسول يحقق التوحيد','31','0','0');
+INSERT INTO [t8983] VALUES (8,'الأسباب المشروعة وغير المشروعة','33','0','0');
