e90c5ba6fbab706f9099b35b78d3c484cc65a82f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:40 +0700

    Upload SQL chunk: t254_chunk_0000.sql

---
+++ sql_chunks/t254_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t254
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t254" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t254
+INSERT INTO [t254] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t254] VALUES (2,'باب الكلمة والكلام','4','0','0');
+INSERT INTO [t254] VALUES (3,'باب الاسم والفعل والحرف','6','0','0');
+INSERT INTO [t254] VALUES (4,'باب الإعراب والبناء','9','0','0');
+INSERT INTO [t254] VALUES (5,'باب المعرب والمبني','11','0','0');
+INSERT INTO [t254] VALUES (6,'باب علامات الإعراب','14','0','0');
+INSERT INTO [t254] VALUES (7,'باب أحكام الفعل المضارع','17','0','0');
+INSERT INTO [t254] VALUES (8,'باب النكرة والمعرفة','22','0','0');
+INSERT INTO [t254] VALUES (9,'باب أحكام الأسماء','27','0','0');
+INSERT INTO [t254] VALUES (10,'باب الفاعل','28','0','0');
+INSERT INTO [t254] VALUES (11,'باب نائب الفاعل','30','0','0');
+INSERT INTO [t254] VALUES (12,'باب المبتدأ والخبر','31','0','0');
+INSERT INTO [t254] VALUES (13,'باب النواسخ لحكم المبتدأ والخبر','33','0','0');
+INSERT INTO [t254] VALUES (14,'باب التابع','37','0','0');
+INSERT INTO [t254] VALUES (15,'باب المنصوبات','43','0','0');
+INSERT INTO [t254] VALUES (16,'باب المفعول به','44','0','0');
+INSERT INTO [t254] VALUES (17,'باب المفعول المطلق','45','0','0');
+INSERT INTO [t254] VALUES (18,'باب المفعول لأجله','46','0','0');
+INSERT INTO [t254] VALUES (19,'باب المفعول فيه','47','0','0');
+INSERT INTO [t254] VALUES (20,'باب المفعول معه','48','0','0');
+INSERT INTO [t254] VALUES (21,'باب الحال','49','0','0');
+INSERT INTO [t254] VALUES (22,'باب التمييز','53','0','0');
+INSERT INTO [t254] VALUES (23,'باب المستثنى','55','0','0');
+INSERT INTO [t254] VALUES (24,'باب اسم "لا"','57','0','0');
+INSERT INTO [t254] VALUES (25,'باب المنادى','59','0','0');
+INSERT INTO [t254] VALUES (26,'باب المجرورات','61','0','0');
+INSERT INTO [t254] VALUES (27,'باب العامل','64','0','0');
+INSERT INTO [t254] VALUES (28,'باب في ألفاظ متفقة بمعان مختلفة','75','0','0');
+INSERT INTO [t254] VALUES (29,'باب في الجمل','81','0','0');
+INSERT INTO [t254] VALUES (30,'باب في الخط','84','0','0');
