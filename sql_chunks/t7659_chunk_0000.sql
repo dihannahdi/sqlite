76c2d581f0a93f8341b4963b661fb2fde4549f62
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:41:13 +0700

    Upload SQL chunk: t7659_chunk_0000.sql

---
+++ sql_chunks/t7659_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7659
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7659" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7659
+INSERT INTO [t7659] VALUES (1,'الشيخ محمد بن عثيمين من العلماء الربانيين','1','0','0');
+INSERT INTO [t7659] VALUES (2,'محاضرة ألقاها: عبد المحسن بن حمد العبادر البدر','1','0','0');
