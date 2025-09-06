9835c1a47e6f4d629f50ff61cf4f5753c2bc47e2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:11 +0700

    Upload SQL chunk: t7374_chunk_0000.sql

---
+++ sql_chunks/t7374_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t7374
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t7374" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7374
+INSERT INTO [t7374] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t7374] VALUES (2,'فضل شهر رمضان','3','0','0');
+INSERT INTO [t7374] VALUES (3,'فصل في فضل صوم شهر رمضان','9','0','0');
+INSERT INTO [t7374] VALUES (4,'فصل في فضل الجود في رمضان وتلاوة القرآن','27','0','0');
+INSERT INTO [t7374] VALUES (5,'فصل التراويح سنة','36','0','0');
+INSERT INTO [t7374] VALUES (6,'فصل في قيام رمضان','43','0','0');
+INSERT INTO [t7374] VALUES (7,'فصل في العشر الوسط','47','0','0');
+INSERT INTO [t7374] VALUES (8,'فصل في فضل العشر الأواخر من رمضان','50','0','0');
+INSERT INTO [t7374] VALUES (9,'فصل في السبع الأواخر','59','0','0');
+INSERT INTO [t7374] VALUES (10,'فصل في أرجى ليلة لها','62','0','0');
+INSERT INTO [t7374] VALUES (11,'فصل في العمل في ليلة القدر','64','0','0');
+INSERT INTO [t7374] VALUES (12,'فصل في وداع رمضان','66','0','0');
+INSERT INTO [t7374] VALUES (13,'تتمة: في صيام ست من شوال','79','0','0');
