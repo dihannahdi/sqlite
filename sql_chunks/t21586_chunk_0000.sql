c2b598d4368fac45ff9c3c52a9b580967df54d20
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:05 +0700

    Upload SQL chunk: t21586_chunk_0000.sql

---
+++ sql_chunks/t21586_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t21586
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t21586" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21586
+INSERT INTO [t21586] VALUES (1,'الناس في الصيام','2','0','0');
+INSERT INTO [t21586] VALUES (2,'مفطرات الصائم','3','0','0');
+INSERT INTO [t21586] VALUES (3,'* المفطرات ثمانية وهي:','4','2','0');
+INSERT INTO [t21586] VALUES (4,'فوائد','5','0','0');
