e5eb67a10b529fd08d218c85f565d3c2a2c4d117
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:50 +0700

    Upload SQL chunk: t404_chunk_0000.sql

---
+++ sql_chunks/t404_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t404
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t404" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t404
+INSERT INTO [t404] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t404] VALUES (2,'سنة ١١٥٤','1','0','0');
+INSERT INTO [t404] VALUES (3,'سنة ١١٥٥','2','0','0');
+INSERT INTO [t404] VALUES (4,'سنة ١١٥٦','6','0','0');
+INSERT INTO [t404] VALUES (5,'سنة ١١٥٧','12','0','0');
+INSERT INTO [t404] VALUES (6,'سنة ١١٥٨','14','0','0');
+INSERT INTO [t404] VALUES (7,'سنة ١١٥٩','15','0','0');
+INSERT INTO [t404] VALUES (8,'سنة ١١٦٠','20','0','0');
+INSERT INTO [t404] VALUES (9,'سنة ١١٦١','25','0','0');
+INSERT INTO [t404] VALUES (10,'سنة ١١٦٢','30','0','0');
+INSERT INTO [t404] VALUES (11,'سنة ١١٦٣','34','0','0');
+INSERT INTO [t404] VALUES (12,'سنة ١١٦٤','38','0','0');
+INSERT INTO [t404] VALUES (13,'سنة ١١٦٥','41','0','0');
+INSERT INTO [t404] VALUES (14,'سنة ١١٦٦','43','0','0');
+INSERT INTO [t404] VALUES (15,'سنة ١١٦٧','45','0','0');
+INSERT INTO [t404] VALUES (16,'سنة ١١٦٨','46','0','0');
+INSERT INTO [t404] VALUES (17,'سنة ١١٦٩','47','0','0');
+INSERT INTO [t404] VALUES (18,'سنة ١١٧٠','48','0','0');
+INSERT INTO [t404] VALUES (19,'سنة ١١٧١','52','0','0');
+INSERT INTO [t404] VALUES (20,'سنة ١١٧٢','55','0','0');
+INSERT INTO [t404] VALUES (21,'سنة ١١٧٣','58','0','0');
+INSERT INTO [t404] VALUES (22,'سنة ١١٧٤','58','0','0');
+INSERT INTO [t404] VALUES (23,'سنة ١١٧٥','59','0','0');
