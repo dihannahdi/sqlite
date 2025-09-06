9c23ff3e7413e3b39029ba01c7684d7382e8996b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:24:54 +0700

    Upload SQL chunk: t1519_chunk_0000.sql

---
+++ sql_chunks/t1519_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t1519
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t1519" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1519
+INSERT INTO [t1519] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t1519] VALUES (2,'موضوع الكتاب:','3','0','0');
+INSERT INTO [t1519] VALUES (3,'المؤلف:','4','0','0');
+INSERT INTO [t1519] VALUES (4,'الأصل المخطوط:','5','0','0');
+INSERT INTO [t1519] VALUES (5,'التوثيق:','5','0','0');
+INSERT INTO [t1519] VALUES (6,'منهج التحقيق:','5','0','0');
+INSERT INTO [t1519] VALUES (7,'نص الكتاب','7','0','0');
