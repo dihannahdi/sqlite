15c05115d7782f998742dbb1f1c5e3641699c230
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:51 +0700

    Upload SQL chunk: t7558_chunk_0000.sql

---
+++ sql_chunks/t7558_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t7558
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t7558" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7558
+INSERT INTO [t7558] VALUES (1,'الفصل الأول: مؤلفات المسلمين وأنواعها','1','0','0');
+INSERT INTO [t7558] VALUES (2,'الفصل الثاني: مؤلفات الإنجليز والناطقين باللغة الإنجليزية من غير المسلمين','10','0','0');
+INSERT INTO [t7558] VALUES (3,'مدخل','10','2','0');
+INSERT INTO [t7558] VALUES (4,'المبحث الأول: أنواع مصنفاتهم','11','2','0');
+INSERT INTO [t7558] VALUES (5,'المبحث الثاني: مراحل التأليف عن السيرة باللغة الإنجليزية','17','2','0');
+INSERT INTO [t7558] VALUES (6,'المبحث الثالث: كتاب ميوير عن السيرة','24','2','0');
+INSERT INTO [t7558] VALUES (7,'المبحث الرابع: كتاب مرغوليوث','34','2','0');
+INSERT INTO [t7558] VALUES (8,'المبحث الخامس: مؤلفات وات في السيرة','40','2','0');
+INSERT INTO [t7558] VALUES (9,'الخاتمة','50','0','0');
+INSERT INTO [t7558] VALUES (10,'مصادر ومراجع','52','0','0');
