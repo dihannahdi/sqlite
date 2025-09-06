ff5e5c076967c6e45cf2e1fef6275de43bfa9dc8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:56 +0700

    Upload SQL chunk: t26866_chunk_0000.sql

---
+++ sql_chunks/t26866_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t26866
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t26866" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26866
+INSERT INTO [t26866] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26866] VALUES (2,'أخبار لحفظ القرآن','2','0','0');
+INSERT INTO [t26866] VALUES (3,'في الحجامة وفضلها','7','0','0');
+INSERT INTO [t26866] VALUES (4,'سبب النسيان','8','0','0');
