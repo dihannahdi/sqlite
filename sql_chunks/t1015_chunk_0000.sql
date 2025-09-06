b3d2174e13ff28be9570f9566139904f6516dcaa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:06 +0700

    Upload SQL chunk: t1015_chunk_0000.sql

---
+++ sql_chunks/t1015_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t1015
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t1015" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1015
+INSERT INTO [t1015] VALUES (1,'تاريخ الأدب الأندلسي (عصر الطوائف والمرابطين)','1','0','0');
+INSERT INTO [t1015] VALUES (2,'تصدير','1','0','0');
+INSERT INTO [t1015] VALUES (3,'مقدمات تاريخية','3','0','0');
+INSERT INTO [t1015] VALUES (4,'بعض المظاهر الإجتماعية','28','0','0');
+INSERT INTO [t1015] VALUES (5,'الدراسات العلمية والفلسفية','50','0','0');
+INSERT INTO [t1015] VALUES (6,'الدولة وتشجيع الحياة الأدبية','65','0','0');
+INSERT INTO [t1015] VALUES (7,'الحال الاجتماعية للشاعر','75','0','0');
+INSERT INTO [t1015] VALUES (8,'النقد الأدبي','87','0','0');
+INSERT INTO [t1015] VALUES (9,'دراسة مظاهر التطور الأدبي','101','0','0');
+INSERT INTO [t1015] VALUES (10,'خاتمة','321','0','0');
