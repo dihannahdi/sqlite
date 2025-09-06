34ee9ae5181239bf32fee3d60352d764b7445e49
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:48 +0700

    Upload SQL chunk: t11238_chunk_0000.sql

---
+++ sql_chunks/t11238_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t11238
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t11238" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11238
+INSERT INTO [t11238] VALUES (1,'فصل في سجود القرآن','1','0','0');
+INSERT INTO [t11238] VALUES (2,'حكم سجود التلاوة','8','0','0');
+INSERT INTO [t11238] VALUES (3,'فصل آيات الله توجب فهمها، وعبادة الله','21','0','0');
+INSERT INTO [t11238] VALUES (4,'فصل سجود القرآن لا يشرع فيه تحريم ولا تحليل','53','0','0');
+INSERT INTO [t11238] VALUES (5,'فصل سجود التلاوة قائما أفضل من قاعدا','75','0','0');
+INSERT INTO [t11238] VALUES (6,'هل يجوز سجود التلاوة بغير وضوء؟','81','0','0');
