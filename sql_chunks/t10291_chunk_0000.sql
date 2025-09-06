788125a4a1d892cafe8ce6b74f95302d85659b5e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:50 +0700

    Upload SQL chunk: t10291_chunk_0000.sql

---
+++ sql_chunks/t10291_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10291
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10291" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10291
+INSERT INTO [t10291] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10291] VALUES (2,'التربية في العصر الجاهلي','11','0','0');
+INSERT INTO [t10291] VALUES (3,'التطور المنهجي للتربية الإسلامية','12','0','0');
