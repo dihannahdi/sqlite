849743f274e95c3b47028ddd5da2f252cb4501b5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:43 +0700

    Upload SQL chunk: t36387_chunk_0000.sql

---
+++ sql_chunks/t36387_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t36387
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t36387" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36387
+INSERT INTO [t36387] VALUES (1,'الحمد لله المتفرد بالثناء وله الدين الخالص','1','0','0');
+INSERT INTO [t36387] VALUES (2,'أخي المسلم: إخلاصك ..','2','0','0');
+INSERT INTO [t36387] VALUES (3,'أخي المسلم: ما أسعد أهل النيات الصادقة','3','0','0');
+INSERT INTO [t36387] VALUES (4,'أخي المسلم: أما يستحي المرائى','5','0','0');
+INSERT INTO [t36387] VALUES (5,'أخي في الله: كم بين المرائي والمخلص من درجات ..','7','0','0');
+INSERT INTO [t36387] VALUES (6,'أخي المسلم: ما أقذر الرياء ...','8','0','0');
+INSERT INTO [t36387] VALUES (7,'وهذا الفقيه الرباني .. متين الإيمان','10','0','0');
