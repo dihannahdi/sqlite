01b93ffbc6226822bb0674083b7f7c0a93d361cd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:32 +0700

    Upload SQL chunk: t295_chunk_0000.sql

---
+++ sql_chunks/t295_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t295
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t295" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t295
+INSERT INTO [t295] VALUES (1,'همام بن يحيى يحذر من اللحن','5','0','0');
+INSERT INTO [t295] VALUES (2,'شيخ الإسلام حماد بن سلمة يحذر من اللحن','6','0','0');
+INSERT INTO [t295] VALUES (3,'أول من وضع النحو','8','0','0');
+INSERT INTO [t295] VALUES (4,'أول من وضع العربية','9','0','0');
+INSERT INTO [t295] VALUES (5,'كذابان في أهل الكوفة','10','0','0');
+INSERT INTO [t295] VALUES (6,'التوصية بإصلاح اللسان','11','0','0');
+INSERT INTO [t295] VALUES (7,'من مناقب أبي عمرو بن العلاء','12','0','0');
+INSERT INTO [t295] VALUES (8,'ابن عمر يغضب من أهل اللحن','13','0','0');
+INSERT INTO [t295] VALUES (9,'أمير المؤمنين عمر بن الخطاب يوصى بتعلم العربية','14','0','0');
+INSERT INTO [t295] VALUES (10,'أبو بكر يخشى من اللحن','16','0','0');
+INSERT INTO [t295] VALUES (11,'تربية الأولاد على عدم اللحن','18','0','0');
+INSERT INTO [t295] VALUES (12,'العلم بإعراب القرآن أفضل من حفظ حروفه','21','0','0');
+INSERT INTO [t295] VALUES (13,'من طرائف أهل اللغة','23','0','0');
+INSERT INTO [t295] VALUES (14,'شاب شعره من صعود المنبر','27','0','0');
+INSERT INTO [t295] VALUES (15,'يستغفر ربه عند اللحن','28','0','0');
+INSERT INTO [t295] VALUES (16,'من مناقب الأوزاعي','29','0','0');
+INSERT INTO [t295] VALUES (17,'من طرائف تلميذ للأعمش','30','0','0');
+INSERT INTO [t295] VALUES (18,'افضل قصيدة في مدح النحو وأهله','31','0','0');
