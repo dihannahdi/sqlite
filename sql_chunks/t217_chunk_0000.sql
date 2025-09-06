f97ca83e0625cbb30b99d40ba4eb231c6c5d2b76
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:27 +0700

    Upload SQL chunk: t217_chunk_0000.sql

---
+++ sql_chunks/t217_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t217
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t217" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t217
+INSERT INTO [t217] VALUES (1,'لا ذكر للهلال في نصوص الوحيين، إلا الهلال الذي هو مواقيت للناس في الحج والصيام','2','0','0');
+INSERT INTO [t217] VALUES (2,'كلام عبد الحي الكتاني في «التراتيب الإدارية»','3','0','0');
+INSERT INTO [t217] VALUES (3,'لم يرد في رايات النبي -صلى الله عليه وسلم- وألويته ذكر للهلال، إلا ما عقده لسعد بن مالك','3','0','0');
+INSERT INTO [t217] VALUES (4,'كان الهلال يرفع فوق بعض قصور الملوك قبل العثمانيين','4','0','0');
+INSERT INTO [t217] VALUES (5,'كلام أحمد تيمور باشا عن البيرق النبوي','5','0','0');
+INSERT INTO [t217] VALUES (6,'خلاصة الأمر وزبدته','6','0','0');
