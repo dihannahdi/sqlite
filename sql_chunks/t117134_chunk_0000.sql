3410f761ccc9111a0f6333bbfa5af58760d7a1f0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:54 +0700

    Upload SQL chunk: t117134_chunk_0000.sql

---
+++ sql_chunks/t117134_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t117134
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t117134" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t117134
+INSERT INTO [t117134] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t117134] VALUES (2,'ويمكن عزو أسباب ما قدمناه إلى أسباب مختلفة','2','0','0');
+INSERT INTO [t117134] VALUES (3,'الأول: عدم تسمية الكتاب من قبل المصنف نفسه','2','2','0');
+INSERT INTO [t117134] VALUES (4,'ثانيا: اختلاف النساخ أو أصحاب الخزانات','7','2','0');
+INSERT INTO [t117134] VALUES (5,'ثبت أهم المصادر والمراجع','14','0','0');
