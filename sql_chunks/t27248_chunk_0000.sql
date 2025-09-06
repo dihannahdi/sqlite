b920ab659137bc9c8a2f4fc30edd70a0ff97816d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:02 +0700

    Upload SQL chunk: t27248_chunk_0000.sql

---
+++ sql_chunks/t27248_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t27248
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t27248" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t27248
+INSERT INTO [t27248] VALUES (1,'لامية شيخ الإسلام (١)','2','0','0');
+INSERT INTO [t27248] VALUES (2,'لامية شيخ الإسلام (٢)','35','0','0');
+INSERT INTO [t27248] VALUES (3,'لامية شيخ الإسلام (٣)','67','0','0');
