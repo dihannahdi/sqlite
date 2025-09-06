2a28692302fa33883e6557a40fa7dc1c526fb119
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:03 +0700

    Upload SQL chunk: t563_chunk_0000.sql

---
+++ sql_chunks/t563_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t563
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t563" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t563
+INSERT INTO [t563] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t563] VALUES (2,'وصلى الله على سيدنا محمد وآله وسلم تسليما','1','0','0');
+INSERT INTO [t563] VALUES (3,'قال أبو العلاء أحمد بن عبد الله بن سليمان','1','0','0');
+INSERT INTO [t563] VALUES (4,'رحمه الله','1','0','0');
+INSERT INTO [t563] VALUES (5,'قال بعض الرعاة','1','0','0');
+INSERT INTO [t563] VALUES (6,'فيقول الشاحج','3','0','0');
+INSERT INTO [t563] VALUES (7,'فيقول الشاحج','3','0','0');
+INSERT INTO [t563] VALUES (8,'فيقول الشاحج','3','0','0');
+INSERT INTO [t563] VALUES (9,'فيقول الشاحج','3','0','0');
+INSERT INTO [t563] VALUES (10,'فيقول الشاحج','3','0','0');
+INSERT INTO [t563] VALUES (11,'فيقول الصاهل','3','0','0');
+INSERT INTO [t563] VALUES (12,'فيقول الصاهل','3','0','0');
+INSERT INTO [t563] VALUES (13,'فيقول الصاهل','3','0','0');
+INSERT INTO [t563] VALUES (14,'فيقول الصاهل','3','0','0');
+INSERT INTO [t563] VALUES (15,'فيقول الشاحج','30','0','0');
+INSERT INTO [t563] VALUES (16,'فيقول الشاحج','73','0','0');
+INSERT INTO [t563] VALUES (17,'فيقول الشاحج','74','0','0');
