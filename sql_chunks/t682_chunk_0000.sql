8f2b60a1c704593c92048a5b8a22c3626d16b738
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:53 +0700

    Upload SQL chunk: t682_chunk_0000.sql

---
+++ sql_chunks/t682_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t682
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t682" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t682
+INSERT INTO [t682] VALUES (1,'أول الكتاب','1','0','0');
