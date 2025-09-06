4a823b7ad8a48aacc1aa15f006b87ff326164d12
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:59 +0700

    Upload SQL chunk: t7567_chunk_0000.sql

---
+++ sql_chunks/t7567_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t7567
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t7567" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7567
+INSERT INTO [t7567] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7567] VALUES (2,'مقدمة','12','0','0');
+INSERT INTO [t7567] VALUES (3,'الفصل الأول: بيان منهج تحقيق الرسالة','12','2','0');
+INSERT INTO [t7567] VALUES (4,'الفصل الثاني: ترجمة المؤلف','23','2','0');
+INSERT INTO [t7567] VALUES (5,'الفصل الثالث: ذكر مؤلفاته','25','2','0');
+INSERT INTO [t7567] VALUES (6,'الفصل الرابع: تلاميذه الذين تلقوا العلم عنه','26','2','0');
+INSERT INTO [t7567] VALUES (7,'الفصل الخامس: شيوخه الذين ثقفوه بالثقافة العلمية واللغوية والدينية','28','2','0');
+INSERT INTO [t7567] VALUES (8,'نص الكتاب','35','0','0');
+INSERT INTO [t7567] VALUES (9,'تنبيه','56','0','0');
+INSERT INTO [t7567] VALUES (10,'الخاتمة','115','0','0');
+INSERT INTO [t7567] VALUES (11,'مصادر ومراجع','117','0','0');
