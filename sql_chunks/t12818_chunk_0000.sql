41eab89f326dc65081b07de7ecb66eb9ab96545e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:29 +0700

    Upload SQL chunk: t12818_chunk_0000.sql

---
+++ sql_chunks/t12818_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t12818
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t12818" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12818
+INSERT INTO [t12818] VALUES (1,'أحاديث من أمالي أبي بكر محمد بن يوسف بن محمد بن دوست - مخطوط','2','0','0');
