eca2cde6af70d5eefea39f9828296060932cf638
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:45 +0700

    Upload SQL chunk: t3197_chunk_0000.sql

---
+++ sql_chunks/t3197_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t3197
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t3197" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3197
+INSERT INTO [t3197] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t3197] VALUES (2,'باب حكاية قول الطائفة التي ردت الأخبار كلها','2','0','0');
+INSERT INTO [t3197] VALUES (3,'باب حكاية قول من رد خبر الخاصة','18','0','0');
+INSERT INTO [t3197] VALUES (4,'بيان فرائض الله تبارك وتعالى','46','0','0');
+INSERT INTO [t3197] VALUES (5,'باب الصوم','49','0','0');
+INSERT INTO [t3197] VALUES (6,'صفة نهي النبي صلى الله عليه وسلم','56','0','0');
