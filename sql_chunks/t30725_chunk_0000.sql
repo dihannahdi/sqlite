0630ece6e37d2f956a0dbb1926d1ecc530e0c7e7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:41 +0700

    Upload SQL chunk: t30725_chunk_0000.sql

---
+++ sql_chunks/t30725_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t30725
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t30725" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30725
+INSERT INTO [t30725] VALUES (1,'مجلس يوم الجمعة بعد الصلاة التاسع والعشرين من جمادى الأول سنة اثنتين وتسعين وأربعمائة','2','0','0');
+INSERT INTO [t30725] VALUES (2,'أول المجلس الثاني:','12','0','0');
+INSERT INTO [t30725] VALUES (3,'أول المجلس الثالث:','22','0','0');
+INSERT INTO [t30725] VALUES (4,'أول الرابع:','33','0','0');
+INSERT INTO [t30725] VALUES (5,'أول المجلس الخامس:','44','0','0');
+INSERT INTO [t30725] VALUES (6,'وأول المجلس السادس:','57','0','0');
+INSERT INTO [t30725] VALUES (7,'مجلس في يوم الجمعة بعد الصلاة، الثالث من رجب من سنة ثلاث وسبعين وأربع مائة.','58','0','0');
+INSERT INTO [t30725] VALUES (8,'أول السابع:','67','0','0');
+INSERT INTO [t30725] VALUES (9,'مجلس يوم الجمعة الثامن من شعبان من سنة ثلاث وسبعين وأربع مائة','68','0','0');
+INSERT INTO [t30725] VALUES (10,'أول المجلس الثامن:','78','0','0');
+INSERT INTO [t30725] VALUES (11,'أول المجلس التاسع:','89','0','0');
