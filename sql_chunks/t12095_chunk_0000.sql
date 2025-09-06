288a20c232270aed43c7f46a4dd97429c0cea606
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:50 +0700

    Upload SQL chunk: t12095_chunk_0000.sql

---
+++ sql_chunks/t12095_chunk_0000.sql
@@ -0,0 +1,48 @@
+-- Table: t12095
+-- Chunk: 1
+-- Rows: 41
+
+CREATE TABLE "t12095" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12095
+INSERT INTO [t12095] VALUES (1,'تمهيد','1','0','0');
+INSERT INTO [t12095] VALUES (2,'توطئة','1','1','0');
+INSERT INTO [t12095] VALUES (3,'صحة نسبة هذه الرسالة للإمام الذهبي','6','1','0');
+INSERT INTO [t12095] VALUES (4,'موضوع الرسالة','8','1','0');
+INSERT INTO [t12095] VALUES (5,'منهج الذهبي في الرسالة','10','1','0');
+INSERT INTO [t12095] VALUES (6,'الفرق بين هذه الرسالة ورسالة الثقات','12','1','0');
+INSERT INTO [t12095] VALUES (7,'النسخ المطبوعة من الرسالة','15','1','0');
+INSERT INTO [t12095] VALUES (8,'عنوان الرسالة','25','1','0');
+INSERT INTO [t12095] VALUES (9,'عملي في الرسالة','26','1','0');
+INSERT INTO [t12095] VALUES (10,'هل يرد كل حديث فيه راو متكلم فيه','33','1','0');
+INSERT INTO [t12095] VALUES (11,'نبذة مختصرة عن المؤلف','43','1','0');
+INSERT INTO [t12095] VALUES (12,'النص المحقق','53','0','0');
+INSERT INTO [t12095] VALUES (13,'مدخل','53','12','0');
+INSERT INTO [t12095] VALUES (14,'الاسماء','56','12','0');
+INSERT INTO [t12095] VALUES (15,'حرف الألف','56','14','0');
+INSERT INTO [t12095] VALUES (16,'حرف الباء','122','14','0');
+INSERT INTO [t12095] VALUES (17,'حرف الثاء','135','14','0');
+INSERT INTO [t12095] VALUES (18,'حرف الجيم','139','14','0');
+INSERT INTO [t12095] VALUES (19,'حرف الحاء','149','14','0');
+INSERT INTO [t12095] VALUES (20,'حرف الخاء','182','14','0');
+INSERT INTO [t12095] VALUES (21,'حرف الدال','191','14','0');
+INSERT INTO [t12095] VALUES (22,'حرف الراء','198','14','0');
+INSERT INTO [t12095] VALUES (23,'حرف الزاي','205','14','0');
+INSERT INTO [t12095] VALUES (24,'حرف السين','212','14','0');
+INSERT INTO [t12095] VALUES (25,'حرف الشين','258','14','0');
+INSERT INTO [t12095] VALUES (26,'حرف الصاد','269','14','0');
+INSERT INTO [t12095] VALUES (27,'حرف الضاد','272','14','0');
+INSERT INTO [t12095] VALUES (28,'حرف الطاء','274','14','0');
+INSERT INTO [t12095] VALUES (29,'حرف العين','281','14','0');
+INSERT INTO [t12095] VALUES (30,'حرف الغين','425','14','0');
+INSERT INTO [t12095] VALUES (31,'حرف الفاء','427','14','0');
+INSERT INTO [t12095] VALUES (32,'حرف القاف','434','14','0');
+INSERT INTO [t12095] VALUES (33,'حرف الكاف','444','14','0');
+INSERT INTO [t12095] VALUES (34,'حرف الميم','448','14','0');
+INSERT INTO [t12095] VALUES (35,'حرف النون','525','14','0');
+INSERT INTO [t12095] VALUES (36,'حرف الهاء','529','14','0');
+INSERT INTO [t12095] VALUES (37,'حرف الواو','540','14','0');
+INSERT INTO [t12095] VALUES (38,'حرف الياء','549','14','0');
+INSERT INTO [t12095] VALUES (39,'الكنى','578','12','0');
+INSERT INTO [t12095] VALUES (40,'حرف الألف','578','39','0');
+INSERT INTO [t12095] VALUES (41,'مصادر ومراجع','589','0','0');
