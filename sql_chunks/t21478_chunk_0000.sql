da4d8400e1528348daf93b01ed9da2abd843603e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:27 +0700

    Upload SQL chunk: t21478_chunk_0000.sql

---
+++ sql_chunks/t21478_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t21478
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t21478" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21478
+INSERT INTO [t21478] VALUES (1,'حديث الهميان','1','0','0');
+INSERT INTO [t21478] VALUES (2,'آخر القصة','7','0','0');
