a612918dc227a247e7ce34671640add497991b64
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:37 +0700

    Upload SQL chunk: t11223_chunk_0000.sql

---
+++ sql_chunks/t11223_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11223
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11223" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11223
+INSERT INTO [t11223] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11223] VALUES (2,'الأمة الإسلامية انقسمت في القدر إلى ثلاثة أقسام','5','0','0');
+INSERT INTO [t11223] VALUES (3,'مراتب القضاء والقدر عند أهل السنة والجماعة','21','0','0');
