f3118c9383761411b3356f4dc8c2770c02fd9787
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:41 +0700

    Upload SQL chunk: t9593_chunk_0000.sql

---
+++ sql_chunks/t9593_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9593
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9593" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9593
+INSERT INTO [t9593] VALUES (1,'يصوم حتى نقول لا يفطر، ويفطر حتى نقول لا يصوم، وما رأيت رسول الله صلى الله عليه وسلم','4','0','0');
+INSERT INTO [t9593] VALUES (2,'ليلة النصف من شعبان، لله في هذه الليلة عتقاء من النار بعدد شعر غنم كلب، قالت: قلت: وما','26','0','0');
