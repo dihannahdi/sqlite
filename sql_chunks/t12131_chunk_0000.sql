b4d8dbb9f0765d1a951eef99de1419d110f05933
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:13 +0700

    Upload SQL chunk: t12131_chunk_0000.sql

---
+++ sql_chunks/t12131_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t12131
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t12131" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12131
+INSERT INTO [t12131] VALUES (1,'مسلسلات ابن الجوزي','2','0','0');
