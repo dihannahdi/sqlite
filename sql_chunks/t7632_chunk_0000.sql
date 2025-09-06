40eccc1cafb8d30df6c69ec9dc26b084bd1a333f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:55 +0700

    Upload SQL chunk: t7632_chunk_0000.sql

---
+++ sql_chunks/t7632_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t7632
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t7632" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7632
+INSERT INTO [t7632] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7632] VALUES (2,'فصل ما بين العبد وربه','10','0','0');
+INSERT INTO [t7632] VALUES (3,'فصل في الهجرة إلى الله ورسوله','11','0','0');
+INSERT INTO [t7632] VALUES (4,'فصل الهجرة بين القوة والضعف','16','0','0');
+INSERT INTO [t7632] VALUES (5,'فصل في الهجرة الى الله ورسوله صلى الله عليه وسلم','17','0','0');
+INSERT INTO [t7632] VALUES (6,'فصل معركة الأتباع والمتبوعين','45','0','0');
+INSERT INTO [t7632] VALUES (7,'فصل الأتباع السعداء','48','0','0');
+INSERT INTO [t7632] VALUES (8,'فصل أطفال المؤمنين','52','0','0');
+INSERT INTO [t7632] VALUES (9,'فصل سفر الهجرة','54','0','0');
+INSERT INTO [t7632] VALUES (10,'فصل التدبر والتفكير في آلاء الله','57','0','0');
+INSERT INTO [t7632] VALUES (11,'فصل أفلا يتدبرون القرآن','58','0','0');
+INSERT INTO [t7632] VALUES (12,'فصل الرفيق والطريق','68','0','0');
