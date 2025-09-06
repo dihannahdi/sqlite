57fc5342c813d559b00d50e23908a783f4b4058d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:28 +0700

    Upload SQL chunk: t1025_chunk_0000.sql

---
+++ sql_chunks/t1025_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t1025
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t1025" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1025
+INSERT INTO [t1025] VALUES (1,'مقدمة التحقيق','1','0','0');
+INSERT INTO [t1025] VALUES (2,'تحقيق الكتاب','10','0','0');
+INSERT INTO [t1025] VALUES (3,'١ - نسخ الكتاب:','10','2','0');
+INSERT INTO [t1025] VALUES (4,'٢ - منهجنا في التحقيق:','12','2','0');
+INSERT INTO [t1025] VALUES (5,'فصل المقال في شرح كتاب الأمثال','15','0','0');
+INSERT INTO [t1025] VALUES (6,'الباب الأول','33','0','0');
+INSERT INTO [t1025] VALUES (7,'الباب الثاني','101','0','0');
+INSERT INTO [t1025] VALUES (8,'الباب الثالث','135','0','0');
+INSERT INTO [t1025] VALUES (9,'الباب الرابع','221','0','0');
+INSERT INTO [t1025] VALUES (10,'الباب الخامس','241','0','0');
+INSERT INTO [t1025] VALUES (11,'الباب السادس','257','0','0');
+INSERT INTO [t1025] VALUES (12,'الباب السابع','269','0','0');
+INSERT INTO [t1025] VALUES (13,'الباب الثامن','289','0','0');
+INSERT INTO [t1025] VALUES (14,'الباب التاسع','307','0','0');
+INSERT INTO [t1025] VALUES (15,'الباب العاشر','321','0','0');
+INSERT INTO [t1025] VALUES (16,'الباب الحادي عشر','343','0','0');
+INSERT INTO [t1025] VALUES (17,'الباب الثاني عشر','385','0','0');
+INSERT INTO [t1025] VALUES (18,'الباب الثالث عشر','403','0','0');
+INSERT INTO [t1025] VALUES (19,'الباب الرابع عشر','425','0','0');
+INSERT INTO [t1025] VALUES (20,'الباب الخامس عشر','441','0','0');
+INSERT INTO [t1025] VALUES (21,'الباب السادس عشر','451','0','0');
+INSERT INTO [t1025] VALUES (22,'الباب السابع عشر','465','0','0');
+INSERT INTO [t1025] VALUES (23,'الباب الثامن عشر','489','0','0');
+INSERT INTO [t1025] VALUES (24,'الباب التاسع عشر','503','0','0');
+INSERT INTO [t1025] VALUES (25,'الباب العشرون','519','0','0');
