dc8741e04b4489b0000f40377bc36d61518bce09
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:54 +0700

    Upload SQL chunk: t4089_chunk_0000.sql

---
+++ sql_chunks/t4089_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t4089
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t4089" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4089
+INSERT INTO [t4089] VALUES (1,'مقدمة الطبعة الثانية','1','0','0');
+INSERT INTO [t4089] VALUES (2,'المقدمة','1','0','0');
+INSERT INTO [t4089] VALUES (3,'المبحث الأول: تسمية من كتب في هذه النازلة: (البطاقة الائتمانية)','3','0','0');
+INSERT INTO [t4089] VALUES (4,'المبحث الثاني: تعريفها','3','0','0');
+INSERT INTO [t4089] VALUES (5,'المبحث الثالث: تاريخها','4','0','0');
+INSERT INTO [t4089] VALUES (6,'المبحث الرابع: معرفة من له حق إصدار البطاقة','7','0','0');
+INSERT INTO [t4089] VALUES (7,'المبحث الخامس: أنواعها وصورها','7','0','0');
+INSERT INTO [t4089] VALUES (8,'المبحث السادس: أطرافها والعلاقة التعاقدية بينهم','11','0','0');
+INSERT INTO [t4089] VALUES (9,'المبحث السابع: منافعها','13','0','0');
+INSERT INTO [t4089] VALUES (10,'المبحث الثامن: مضارها وسوالبها','18','0','0');
+INSERT INTO [t4089] VALUES (11,'المبحث التاسع: حكمها','19','0','0');
+INSERT INTO [t4089] VALUES (12,'ملاحق البحث','25','0','0');
