ba75ba56770fa37549e7f6550505a61c6b38e733
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:03 +0700

    Upload SQL chunk: t1916_chunk_0000.sql

---
+++ sql_chunks/t1916_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t1916
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t1916" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1916
+INSERT INTO [t1916] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1916] VALUES (2,'مفهوم الفقه الإسلامي','2','0','0');
+INSERT INTO [t1916] VALUES (3,'تعريف الفقه','2','2','0');
+INSERT INTO [t1916] VALUES (4,'موضوعات الفقه الإسلامي','6','0','0');
+INSERT INTO [t1916] VALUES (5,'خصائص الفقه الإسلامي','11','0','0');
+INSERT INTO [t1916] VALUES (6,'أولا سمو الغاية والأهداف','11','5','0');
+INSERT INTO [t1916] VALUES (7,'ثانيا أحكام الفقه الإسلامي وحي إلهي','14','5','0');
+INSERT INTO [t1916] VALUES (8,'ثالثا تطبيق أحكام الفقه الإسلامي يعد طاعة لله تعالى','15','5','0');
+INSERT INTO [t1916] VALUES (9,'رابعا يمتاز الفقه الإسلامي بالشمولية والعموم','16','5','0');
+INSERT INTO [t1916] VALUES (10,'خامسا الثبات في القواعد والمرونة في التطبيق','19','5','0');
+INSERT INTO [t1916] VALUES (11,'سادسا عدم الحرج وقلة التكليف','22','5','0');
+INSERT INTO [t1916] VALUES (12,'سابعا الثراء والغنية','24','5','0');
+INSERT INTO [t1916] VALUES (13,'أطوار الفقه الإسلامي','26','0','0');
+INSERT INTO [t1916] VALUES (14,'مصادر الفقه الإسلامي','30','0','0');
+INSERT INTO [t1916] VALUES (15,'أولا الكتاب','30','14','0');
+INSERT INTO [t1916] VALUES (16,'ثانيا السنة','34','14','0');
+INSERT INTO [t1916] VALUES (17,'ثالثا الإجماع','37','14','0');
+INSERT INTO [t1916] VALUES (18,'رابعا القياس','38','14','0');
+INSERT INTO [t1916] VALUES (19,'أولا الاستصلاح','41','14','0');
+INSERT INTO [t1916] VALUES (20,'ثانيا العرف','43','14','0');
+INSERT INTO [t1916] VALUES (21,'المراجع والمصادر','47','0','0');
