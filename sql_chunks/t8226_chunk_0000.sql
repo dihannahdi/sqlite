031c3abd15e75eaf81c7224038037426ce6630be
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:38 +0700

    Upload SQL chunk: t8226_chunk_0000.sql

---
+++ sql_chunks/t8226_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8226
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8226" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8226
+INSERT INTO [t8226] VALUES (1,'فضيلة للعباس بن عبد المطلب رضي الله عنه','10','0','0');
+INSERT INTO [t8226] VALUES (2,'مجلس آخر','31','0','0');
