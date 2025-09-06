a77cb514a1b5456b3f21a73fa59900dcfd0802d0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-06 20:44:29 +0700

    Upload SQL chunk: b25868_chunk_0000.sql

---
+++ sql_chunks/b25868_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: b25868
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "b25868"(id INTEGER PRIMARY KEY,content TEXT, part TEXT, page TEXT, number TEXT, services TEXT, is_deleted TEXT);
+
+-- Insert data for b25868
+INSERT INTO [b25868] VALUES (1,'قال الشيخ أبو الحسن: قرأت بخط أبي بكر الحداد، عن أبي عبد الرحمن النسائي قال:
+<span data-type="title" id=toc-1>ذكر المدلسين</span>
+<span data-type="title" id=toc-2>١. الحسن</span>',NULL,'121',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (2,'<span data-type="title" id=toc-3>٢. وقتادة</span>',NULL,'121',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (3,'<span data-type="title" id=toc-4>٣. وحميد الطويل</span>',NULL,'121',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (4,'<span data-type="title" id=toc-5>٤. ويحيى بن أبي كثير</span>',NULL,'121',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (5,'<span data-type="title" id=toc-6>٥. والتيمي</span>',NULL,'122',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (6,'<span data-type="title" id=toc-7>٦. ويونس بن عبيد</span>',NULL,'122',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (7,'<span data-type="title" id=toc-8>٧. وابن أبي عروبة</span>',NULL,'122',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (8,'<span data-type="title" id=toc-9>٨. وهشيم</span>',NULL,'122',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (9,'<span data-type="title" id=toc-10>٩. وأبو إسحاق السبيعي</span>',NULL,'122',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (10,'<span data-type="title" id=toc-11>١٠. وإسماعيل بن أبي خالد</span>',NULL,'122',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (11,'<span data-type="title" id=toc-12>١١. والحكم</span>',NULL,'123',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (12,'<span data-type="title" id=toc-13>١٢. والحجاج بن أرطاة</span>',NULL,'123',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (13,'<span data-type="title" id=toc-14>١٣. ومغيرة</span>',NULL,'123',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (14,'<span data-type="title" id=toc-15>١٤. والثوري</span>',NULL,'123',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (15,'<span data-type="title" id=toc-16>١٥. وأبو الزبير المكي</span>',NULL,'123',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (16,'<span data-type="title" id=toc-17>١٦. وابن أبي نجيح</span>',NULL,'123',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (17,'<span data-type="title" id=toc-18>١٧. [وابن جريج]</span>',NULL,'124',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (18,'<span data-type="title" id=toc-19>١٨. وابن عيينة</span>',NULL,'124',NULL,NULL,'0');
+INSERT INTO [b25868] VALUES (19,'<span data-type="title" id=toc-20>ملحق</span>
+الرواة الذين نقل عن النسائي أنه وصفهم بالتدليس ولم يذكرهم في جزئه عنهم
+وهم أربعة رواة فقط، نقل الحافظ ابن حجر عن النسائي أنه وصفهم بالتدليس، وهاكهم:
+<span data-type="title" id=toc-21>١. سليمان بن مهران الأعمش [الطبقة الثانية] </span>.
+<span data-type="title" id=toc-22>٢. وعباد بن منصور الناجي البصري [الطبقة الرابعة]</span>
+<span data-type="title" id=toc-23>٣. وميمون بن موسى المرئي [الطبقة الثالثة]</span>
+٤.<span data-type="title" id=toc-24> ويحيى بن أبي حية الكلبي [الطبقة الخامسة]</span>',NULL,'125',NULL,NULL,'0');
