e322a5e4f375b60abf893d140f0cbaf2fd877244
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:41 +0700

    Upload SQL chunk: t10410_chunk_0000.sql

---
+++ sql_chunks/t10410_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t10410
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t10410" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10410
+INSERT INTO [t10410] VALUES (1,'الشمس في نصف الليل','1','0','0');
+INSERT INTO [t10410] VALUES (2,'أوقات الصلاة في الاراضي القطبية','3','0','0');
+INSERT INTO [t10410] VALUES (3,'هل في الاراضي القطبية فصول','4','0','0');
+INSERT INTO [t10410] VALUES (4,'من وحي الأندلس قصيدة من بحر جديد','7','0','0');
+INSERT INTO [t10410] VALUES (5,'وصف جامع قرطبة','10','0','0');
