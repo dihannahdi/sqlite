896ccc9ef51041d6063a23b994d6a2ae178241ee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:15 +0700

    Upload SQL chunk: t35143_chunk_0000.sql

---
+++ sql_chunks/t35143_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t35143
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t35143" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t35143
+INSERT INTO [t35143] VALUES (1,'الحمد لله تعالى هدى إلى الرشاد','1','0','0');
+INSERT INTO [t35143] VALUES (2,'* فما هو الإخلاص','2','0','0');
+INSERT INTO [t35143] VALUES (3,'* كنز عجيب','3','0','0');
+INSERT INTO [t35143] VALUES (4,'* أول عتبات الإخلاص','4','0','0');
+INSERT INTO [t35143] VALUES (5,'* الإخلاص قبل العمل','5','0','0');
+INSERT INTO [t35143] VALUES (6,'* كنوز الإخلاص','6','0','0');
+INSERT INTO [t35143] VALUES (7,'* الإخلاص نجاة','6','0','0');
+INSERT INTO [t35143] VALUES (8,'* ارتفاع أهل الإخلاص','7','0','0');
+INSERT INTO [t35143] VALUES (9,'* في الإخلاص عون الله تعالى ووقايته','7','0','0');
+INSERT INTO [t35143] VALUES (10,'* الإخلاص صفاء للأعمال','7','0','0');
+INSERT INTO [t35143] VALUES (11,'* في الإخلاص صلاح الأمور','7','0','0');
+INSERT INTO [t35143] VALUES (12,'* الإخلاص يحث على أعمال البر كلها','8','0','0');
+INSERT INTO [t35143] VALUES (13,'* نماذج رائعة لأهل الإخلاص','8','0','0');
+INSERT INTO [t35143] VALUES (14,'* كان لعمر بن عبد العزيز دراعة من شعر وغل','8','0','0');
+INSERT INTO [t35143] VALUES (15,'* وتقول زوجة حسان بن أبي سنان','8','0','0');
+INSERT INTO [t35143] VALUES (16,'* وكان علي بن الحسين (زين العابدين)','8','0','0');
+INSERT INTO [t35143] VALUES (17,'* وعن محمد بن عيسى قال','9','0','0');
+INSERT INTO [t35143] VALUES (18,'* وعن عاصم','9','0','0');
+INSERT INTO [t35143] VALUES (19,'* وقال محمد بن واسع','9','0','0');
+INSERT INTO [t35143] VALUES (20,'* وكان محمد بن يوسف الأصبهاني','10','0','0');
+INSERT INTO [t35143] VALUES (21,'* ودخل ابن محيريز حانوتا','10','0','0');
+INSERT INTO [t35143] VALUES (22,'* ومر الحسن البصري على طاوس','10','0','0');
