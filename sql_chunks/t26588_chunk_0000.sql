54eb39748327f7bbb4990840166f6e3eb379ea24
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:51 +0700

    Upload SQL chunk: t26588_chunk_0000.sql

---
+++ sql_chunks/t26588_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26588
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26588" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26588
+INSERT INTO [t26588] VALUES (1,'فوائد الكوفيين لأبي الغنائم لنرسي','2','0','0');
