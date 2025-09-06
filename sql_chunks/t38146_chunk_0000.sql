96dbfc979b34554535fbbc819db3e456a61be9e2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:17:28 +0700

    Upload SQL chunk: t38146_chunk_0000.sql

---
+++ sql_chunks/t38146_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t38146
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t38146" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t38146
+INSERT INTO [t38146] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t38146] VALUES (2,'المبحث الأول: الاستعمار','4','0','0');
+INSERT INTO [t38146] VALUES (3,'مصطلح الاستعمار','4','2','0');
+INSERT INTO [t38146] VALUES (4,'رؤية تاريخية للاستعمار','5','2','0');
+INSERT INTO [t38146] VALUES (5,'دوافع الاستعمار الأوربي','7','2','0');
+INSERT INTO [t38146] VALUES (6,'أعمال الاستعمار','10','2','0');
+INSERT INTO [t38146] VALUES (7,'المبحث الثاني: التبشير','15','0','0');
+INSERT INTO [t38146] VALUES (8,'المبحث الثالث: العلاقة بين التبشير والاستعمار','18','0','0');
+INSERT INTO [t38146] VALUES (9,'قائمة أهم المصادر والمراجع','28','0','0');
