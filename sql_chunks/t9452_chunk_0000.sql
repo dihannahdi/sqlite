9eda400484e9cde5ba22f9627d64aafbadab9195
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:53:50 +0700

    Upload SQL chunk: t9452_chunk_0000.sql

---
+++ sql_chunks/t9452_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9452
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9452" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9452
+INSERT INTO [t9452] VALUES (1,'صافحت رسول الله صلى الله عليه وسلم','1','0','0');
+INSERT INTO [t9452] VALUES (2,'قال ثابت البناني: أنا صافحت أنس بن مالك.','2','0','0');
