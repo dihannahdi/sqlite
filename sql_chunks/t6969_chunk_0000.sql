dbbc83943e3e9c5889837c7bc6bff9c7e466c199
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:15 +0700

    Upload SQL chunk: t6969_chunk_0000.sql

---
+++ sql_chunks/t6969_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t6969
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t6969" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6969
+INSERT INTO [t6969] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6969] VALUES (2,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t6969] VALUES (3,'الكلمة وأقسامها','2','0','0');
+INSERT INTO [t6969] VALUES (4,'تعريف الكلمة','2','0','0');
+INSERT INTO [t6969] VALUES (5,'الإعراب','28','0','0');
+INSERT INTO [t6969] VALUES (6,'النكرة والمعرفة','148','0','0');
+INSERT INTO [t6969] VALUES (7,'المصدر','464','0','0');
