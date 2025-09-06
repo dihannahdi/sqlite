47473915d2ea69138680d17122d8598c38a46cde
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:27 +0700

    Upload SQL chunk: t11285_chunk_0000.sql

---
+++ sql_chunks/t11285_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t11285
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t11285" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11285
+INSERT INTO [t11285] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11285] VALUES (2,'فصل في افتقار الإنسان إلى اختيار الله وتقديره','22','0','0');
+INSERT INTO [t11285] VALUES (3,'فصل وهو مثل المقدمة لهذا الذي أمامه','23','0','0');
+INSERT INTO [t11285] VALUES (4,'فصل في الفاتحة','28','0','0');
+INSERT INTO [t11285] VALUES (5,'فصل في الفاتحة','30','0','0');
+INSERT INTO [t11285] VALUES (6,'فصل في معنى الحمد لله رب العالمين','40','0','0');
+INSERT INTO [t11285] VALUES (7,'فصل في إقرار الناس بتوحيد الربوبية أسبق وأكثر من الإقرار بتوحيد الإلهية','42','0','0');
+INSERT INTO [t11285] VALUES (8,'فصل الإنسان وجميع المخلوقات عبيد الله','44','0','0');
+INSERT INTO [t11285] VALUES (9,'فصل العبد وكل مخلوق فقير إلى الله','60','0','0');
+INSERT INTO [t11285] VALUES (10,'فصل العبد فقير إلى الله فيما يصلحه ويقصده','67','0','0');
