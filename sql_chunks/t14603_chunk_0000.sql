5f39e7d887c872eff07938668f79ef2a70f53629
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:20:47 +0700

    Upload SQL chunk: t14603_chunk_0000.sql

---
+++ sql_chunks/t14603_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t14603
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t14603" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14603
+INSERT INTO [t14603] VALUES (1,'باب ما كتب بالضاد','2','0','0');
+INSERT INTO [t14603] VALUES (2,'باب الألف','2','1','0');
+INSERT INTO [t14603] VALUES (3,'الباء','4','1','0');
+INSERT INTO [t14603] VALUES (4,'التاء','5','1','0');
+INSERT INTO [t14603] VALUES (5,'الجيم','6','1','0');
+INSERT INTO [t14603] VALUES (6,'الحاء','6','1','0');
+INSERT INTO [t14603] VALUES (7,'الخاء','8','1','0');
+INSERT INTO [t14603] VALUES (8,'الدال','9','1','0');
+INSERT INTO [t14603] VALUES (9,'الراء','9','1','0');
+INSERT INTO [t14603] VALUES (10,'الضاد','10','1','0');
+INSERT INTO [t14603] VALUES (11,'العين','15','1','0');
+INSERT INTO [t14603] VALUES (12,'الغين','16','1','0');
+INSERT INTO [t14603] VALUES (13,'الفاء','18','1','0');
+INSERT INTO [t14603] VALUES (14,'القاف','19','1','0');
+INSERT INTO [t14603] VALUES (15,'الميم','20','1','0');
+INSERT INTO [t14603] VALUES (16,'النون','21','1','0');
+INSERT INTO [t14603] VALUES (17,'الهاء','23','1','0');
+INSERT INTO [t14603] VALUES (18,'الواو','23','1','0');
+INSERT INTO [t14603] VALUES (19,'باب الظاء','25','0','0');
+INSERT INTO [t14603] VALUES (20,'الألف','25','19','0');
+INSERT INTO [t14603] VALUES (21,'الباء','25','19','0');
+INSERT INTO [t14603] VALUES (22,'التاء','25','19','0');
+INSERT INTO [t14603] VALUES (23,'الجيم','26','19','0');
+INSERT INTO [t14603] VALUES (24,'الحاء','26','19','0');
+INSERT INTO [t14603] VALUES (25,'الشين','27','19','0');
+INSERT INTO [t14603] VALUES (26,'الظاء','28','19','0');
+INSERT INTO [t14603] VALUES (27,'العين','30','19','0');
+INSERT INTO [t14603] VALUES (28,'الغين','31','19','0');
+INSERT INTO [t14603] VALUES (29,'الفاء','31','19','0');
+INSERT INTO [t14603] VALUES (30,'القاف','32','19','0');
+INSERT INTO [t14603] VALUES (31,'الكاف','32','19','0');
+INSERT INTO [t14603] VALUES (32,'اللام','33','19','0');
+INSERT INTO [t14603] VALUES (33,'الميم','33','19','0');
+INSERT INTO [t14603] VALUES (34,'النون','33','19','0');
+INSERT INTO [t14603] VALUES (35,'الواو','35','19','0');
+INSERT INTO [t14603] VALUES (36,'الياء','35','19','0');
