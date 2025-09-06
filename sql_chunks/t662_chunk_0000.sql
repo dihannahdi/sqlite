3fb44c63d90f9984970d2c7aa6e2ed32ebc1e834
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:14 +0700

    Upload SQL chunk: t662_chunk_0000.sql

---
+++ sql_chunks/t662_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t662
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t662" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t662
+INSERT INTO [t662] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t662] VALUES (2,'الفصل الأول','1','0','0');
+INSERT INTO [t662] VALUES (3,'في ذكر الإختلاف في جواز التكلم فيها وعدمه.','1','0','0');
+INSERT INTO [t662] VALUES (4,'الفصل الثاني','2','0','0');
+INSERT INTO [t662] VALUES (5,'في حقيقة الروح وما قيل فيها','2','0','0');
+INSERT INTO [t662] VALUES (6,'الفصل الثالث','8','0','0');
+INSERT INTO [t662] VALUES (7,'في بقائها بعد الموت وتجردها مستقلة','8','0','0');
+INSERT INTO [t662] VALUES (8,'وما في ذلك من الخلاف','8','0','0');
