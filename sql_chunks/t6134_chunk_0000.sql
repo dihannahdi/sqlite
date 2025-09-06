2a111aada2250abffdba21dfc9465f3723c28c79
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:26 +0700

    Upload SQL chunk: t6134_chunk_0000.sql

---
+++ sql_chunks/t6134_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t6134
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t6134" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6134
+INSERT INTO [t6134] VALUES (1,'كتاب الطهارة','7','0','0');
+INSERT INTO [t6134] VALUES (2,'كتاب الصلاة','16','0','0');
+INSERT INTO [t6134] VALUES (3,'كتاب الزكاة','27','0','0');
+INSERT INTO [t6134] VALUES (4,'كتاب البيع','33','0','0');
+INSERT INTO [t6134] VALUES (5,'كتاب النكاح','41','0','0');
+INSERT INTO [t6134] VALUES (6,'كتاب الجراح','47','0','0');
