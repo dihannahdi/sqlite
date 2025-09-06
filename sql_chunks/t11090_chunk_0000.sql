8840167c7f6b50d98605ee38fd752ad6f7c5f033
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:07 +0700

    Upload SQL chunk: t11090_chunk_0000.sql

---
+++ sql_chunks/t11090_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t11090
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t11090" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11090
+INSERT INTO [t11090] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t11090] VALUES (2,'تحديد المصطلحات','2','0','0');
+INSERT INTO [t11090] VALUES (3,'تدوين السنة النبوية في القرن الثاني الهجري','8','0','0');
+INSERT INTO [t11090] VALUES (4,'تدوين السنة النبوية في القرن الثالث الهجري','24','0','0');
+INSERT INTO [t11090] VALUES (5,'تدوين السنة النبوية في القرن الرابع الهجري','44','0','0');
+INSERT INTO [t11090] VALUES (6,'الخاتمة','55','0','0');
+INSERT INTO [t11090] VALUES (7,'مصادر ومراجع','56','0','0');
