1c18922ea3622f94e541c6147a2d69cc96f13105
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:23 +0700

    Upload SQL chunk: t11277_chunk_0000.sql

---
+++ sql_chunks/t11277_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t11277
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t11277" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11277
+INSERT INTO [t11277] VALUES (1,'تمهيد','1','0','0');
+INSERT INTO [t11277] VALUES (2,'العظة الاولى','2','0','0');
+INSERT INTO [t11277] VALUES (3,'مدخل','2','2','0');
+INSERT INTO [t11277] VALUES (4,'العبرة الاولى','5','2','0');
+INSERT INTO [t11277] VALUES (5,'العظة الثانية','7','0','0');
+INSERT INTO [t11277] VALUES (6,'مدخل','7','5','0');
+INSERT INTO [t11277] VALUES (7,'العبرة الثانية','9','5','0');
+INSERT INTO [t11277] VALUES (8,'العظة الثالثة','12','0','0');
+INSERT INTO [t11277] VALUES (9,'مدخل','12','8','0');
+INSERT INTO [t11277] VALUES (10,'العبرة الثالثة','15','8','0');
+INSERT INTO [t11277] VALUES (11,'العظة الرابعة','18','0','0');
+INSERT INTO [t11277] VALUES (12,'مدخل','18','11','0');
+INSERT INTO [t11277] VALUES (13,'العبرة الرابعة','20','11','0');
+INSERT INTO [t11277] VALUES (14,'العظة الخامسة','22','0','0');
+INSERT INTO [t11277] VALUES (15,'مدخل','22','14','0');
+INSERT INTO [t11277] VALUES (16,'العبرة الخامسة','24','14','0');
+INSERT INTO [t11277] VALUES (17,'العظة السادسة','26','0','0');
+INSERT INTO [t11277] VALUES (18,'مدخل','26','17','0');
+INSERT INTO [t11277] VALUES (19,'العبرة السادسة','28','17','0');
+INSERT INTO [t11277] VALUES (20,'العظة السابعة','31','0','0');
+INSERT INTO [t11277] VALUES (21,'مدخل','31','20','0');
+INSERT INTO [t11277] VALUES (22,'العبرة السابعة','34','20','0');
+INSERT INTO [t11277] VALUES (23,'العظة الثامنة','37','0','0');
+INSERT INTO [t11277] VALUES (24,'مدخل','37','23','0');
+INSERT INTO [t11277] VALUES (25,'العبرة الثامنة','40','23','0');
+INSERT INTO [t11277] VALUES (26,'العظة التاسعة','42','0','0');
+INSERT INTO [t11277] VALUES (27,'مدخل','42','26','0');
+INSERT INTO [t11277] VALUES (28,'العبرة التاسعة','44','26','0');
+INSERT INTO [t11277] VALUES (29,'العظة العاشرة','46','0','0');
+INSERT INTO [t11277] VALUES (30,'مدخل','46','29','0');
+INSERT INTO [t11277] VALUES (31,'العبرة العاشرة','49','29','0');
+INSERT INTO [t11277] VALUES (32,'الخاتمة','52','0','0');
