04c52161fb1c98cab28867c17c9b5ad7e535609f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:37 +0700

    Upload SQL chunk: t26569_chunk_0000.sql

---
+++ sql_chunks/t26569_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t26569
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t26569" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26569
+INSERT INTO [t26569] VALUES (1,'ما جاء في أشراط الساعة من ذهاب الأخيار','1','0','0');
+INSERT INTO [t26569] VALUES (2,'باب ما جاء في الأشراط التي تقوم عليها الساعة أعاذنا الله من ذلك','13','0','0');
+INSERT INTO [t26569] VALUES (3,'باب ما جاء في خروج الدابة من الأرض التي تكلم الناس وهم على غفلة.','24','0','0');
+INSERT INTO [t26569] VALUES (4,'باب ما جاء في نزول عيسى بن مريم , عليهما السلام , وخروج الدجال لعنه الله','32','0','0');
