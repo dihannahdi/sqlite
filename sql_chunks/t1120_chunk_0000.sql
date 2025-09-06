4010ce45b235768e34ffab997496545da654835d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:27 +0700

    Upload SQL chunk: t1120_chunk_0000.sql

---
+++ sql_chunks/t1120_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t1120
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t1120" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1120
+INSERT INTO [t1120] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1120] VALUES (2,'معجزة القرآن','4','0','0');
+INSERT INTO [t1120] VALUES (3,'وشهدوا للقرآن وهم كافرون','22','0','0');
+INSERT INTO [t1120] VALUES (4,'آيات الله في الارض','33','0','0');
+INSERT INTO [t1120] VALUES (5,'القرآن والعلم','36','0','0');
+INSERT INTO [t1120] VALUES (6,'والأرض مددنا وهي كرة','43','0','0');
