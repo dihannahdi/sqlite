82cd205bdd0850d4e5409e959801c9a86ec257ee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:28 +0700

    Upload SQL chunk: t752_chunk_0000.sql

---
+++ sql_chunks/t752_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t752
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t752" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t752
+INSERT INTO [t752] VALUES (1,'باب','1','0','0');
+INSERT INTO [t752] VALUES (2,'الكبر والشيب','1','0','0');
+INSERT INTO [t752] VALUES (3,'باب','12','2','0');
+INSERT INTO [t752] VALUES (4,'النسيب والملاهي','12','0','0');
+INSERT INTO [t752] VALUES (5,'باب','23','4','0');
+INSERT INTO [t752] VALUES (6,'الهجاء','23','0','0');
+INSERT INTO [t752] VALUES (7,'باب','34','6','0');
+INSERT INTO [t752] VALUES (8,'المديح','34','0','0');
+INSERT INTO [t752] VALUES (9,'الاستعطاف والاعتذار','39','0','0');
