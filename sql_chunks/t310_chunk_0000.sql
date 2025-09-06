d8b4355e4b219914d1102ba8e17f0f8ec4a41def
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:14 +0700

    Upload SQL chunk: t310_chunk_0000.sql

---
+++ sql_chunks/t310_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t310
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t310" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t310
+INSERT INTO [t310] VALUES (1,'حرف الألف','1','0','0');
+INSERT INTO [t310] VALUES (2,'حرف الباء','9','0','0');
+INSERT INTO [t310] VALUES (3,'حرف التاء','11','0','0');
+INSERT INTO [t310] VALUES (4,'حرف الثاء','13','0','0');
+INSERT INTO [t310] VALUES (5,'حرف الجيم','13','0','0');
+INSERT INTO [t310] VALUES (6,'حرف الحاء المهملة','14','0','0');
+INSERT INTO [t310] VALUES (7,'حرف الخاء المعجمة','15','0','0');
+INSERT INTO [t310] VALUES (8,'حرف الدال','16','0','0');
+INSERT INTO [t310] VALUES (9,'حرف الذال المعجمة','17','0','0');
+INSERT INTO [t310] VALUES (10,'حرف الراء','17','0','0');
+INSERT INTO [t310] VALUES (11,'حرف الزين','18','0','0');
+INSERT INTO [t310] VALUES (12,'حرف السين المهملة','20','0','0');
+INSERT INTO [t310] VALUES (13,'حرف الشين المعجمة','23','0','0');
+INSERT INTO [t310] VALUES (14,'حرف الطاء المهملة','25','0','0');
+INSERT INTO [t310] VALUES (15,'حرف العين المهملة','25','0','0');
+INSERT INTO [t310] VALUES (16,'حرف الغين المعجمة','27','0','0');
+INSERT INTO [t310] VALUES (17,'حرف الفاء','28','0','0');
+INSERT INTO [t310] VALUES (18,'حرف القاف','30','0','0');
+INSERT INTO [t310] VALUES (19,'حرف الكاف','32','0','0');
+INSERT INTO [t310] VALUES (20,'حرف اللام','34','0','0');
+INSERT INTO [t310] VALUES (21,'حرف الميم','34','0','0');
+INSERT INTO [t310] VALUES (22,'حرف النون','44','0','0');
+INSERT INTO [t310] VALUES (23,'حرف الواو','46','0','0');
+INSERT INTO [t310] VALUES (24,'حرف الهاء','46','0','0');
+INSERT INTO [t310] VALUES (25,'حرف الياء','47','0','0');
