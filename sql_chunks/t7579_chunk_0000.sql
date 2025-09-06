8b2168a85b88830ec344399f59848eaf337c8f88
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:09 +0700

    Upload SQL chunk: t7579_chunk_0000.sql

---
+++ sql_chunks/t7579_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t7579
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t7579" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7579
+INSERT INTO [t7579] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7579] VALUES (2,'مدخل','5','0','0');
+INSERT INTO [t7579] VALUES (3,'المبحث الأول: التصنيف في علوم الحديث','13','0','0');
+INSERT INTO [t7579] VALUES (4,'المبحث الثاني: التصنيف في الحديث النبوي','29','0','0');
+INSERT INTO [t7579] VALUES (5,'المبحث الثالث: تصنيف الأمالي، والمجالس، والأجزاء، والفوائد الحديثية','48','0','0');
+INSERT INTO [t7579] VALUES (6,'المبحث الرابع: التصنيف في التواريخ والتراجم والسير','65','0','0');
+INSERT INTO [t7579] VALUES (7,'المبحث الخامس: مصنفات المحدثين العامة','85','0','0');
+INSERT INTO [t7579] VALUES (8,'الخاتمة','87','0','0');
+INSERT INTO [t7579] VALUES (9,'مصادر ومراجع','88','0','0');
