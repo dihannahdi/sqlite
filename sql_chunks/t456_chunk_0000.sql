dff55ce00c864e1a55563cb1ff3aebd11000068f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:34 +0700

    Upload SQL chunk: t456_chunk_0000.sql

---
+++ sql_chunks/t456_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t456
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t456" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t456
+INSERT INTO [t456] VALUES (1,'خلاصة الدعوى وما عليها من انتقادات','2','0','0');
+INSERT INTO [t456] VALUES (2,'جواب المؤلف على الدعوى وتحرير الواقعة وحكمها','5','0','0');
