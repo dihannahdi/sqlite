7f636deeb8b3b4d05a7e596c29276782dcaae27e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:50 +0700

    Upload SQL chunk: t515_chunk_0000.sql

---
+++ sql_chunks/t515_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t515
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t515" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t515
+INSERT INTO [t515] VALUES (1,'رسالة الصولي إلى مزاحم بن فاتك','1','0','0');
+INSERT INTO [t515] VALUES (2,'ما جاء في تفضيل أبي تمام','1','0','0');
+INSERT INTO [t515] VALUES (3,'نسبه','1','2','0');
+INSERT INTO [t515] VALUES (4,'فضله','1','2','0');
+INSERT INTO [t515] VALUES (5,'أخبار أبي تمام مع أحمد بن أبي دؤاد','16','0','0');
+INSERT INTO [t515] VALUES (6,'أخبار أبي تمام مع خالد بن يزيد الشيباني','19','0','0');
+INSERT INTO [t515] VALUES (7,'أخبار أبو تمام مع الحسن بن رجاء','20','0','0');
+INSERT INTO [t515] VALUES (8,'أخبار أبي تمام مع الحسن بن وهب','24','0','0');
+INSERT INTO [t515] VALUES (9,'أخبار أبي تمام مع آل طاهر بن الحسين','29','0','0');
+INSERT INTO [t515] VALUES (10,'أخبار أبي تمام مع أبي سعيد الثغري','33','0','0');
+INSERT INTO [t515] VALUES (11,'أبي سعيد محمد بن يوسف الثغري','33','10','0');
+INSERT INTO [t515] VALUES (12,'الطائي الحميدي','33','10','0');
+INSERT INTO [t515] VALUES (13,'أخبار أبي تمام مع أحمد بن المعتصم','33','0','0');
+INSERT INTO [t515] VALUES (14,'أخبار أبي تمام مع مخلد بن بكار الموصلي','34','0','0');
+INSERT INTO [t515] VALUES (15,'ما روي من معائب أبي تمام','36','0','0');
+INSERT INTO [t515] VALUES (16,'ما رواه أبو تمام','37','0','0');
+INSERT INTO [t515] VALUES (17,'صفة أبي تمام وأخبار أهله','39','0','0');
+INSERT INTO [t515] VALUES (18,'أخبار لأبي تمام متفرقة','40','0','0');
+INSERT INTO [t515] VALUES (19,'وفاة أبي تمام','42','0','0');
+INSERT INTO [t515] VALUES (20,'ومبلغ سنه','42','19','0');
+INSERT INTO [t515] VALUES (21,'مراثي أبي تمام','42','0','0');
