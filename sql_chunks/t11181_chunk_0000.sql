2bdb271e2699b385966694d9d21ca9078d766cdb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:03 +0700

    Upload SQL chunk: t11181_chunk_0000.sql

---
+++ sql_chunks/t11181_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t11181
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t11181" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11181
+INSERT INTO [t11181] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11181] VALUES (2,'ترجمة المؤلف','16','0','0');
+INSERT INTO [t11181] VALUES (3,'صيغة الشبهة التي يوردها بعض أهل الأهواء','19','0','0');
+INSERT INTO [t11181] VALUES (4,'الجواب عن حديث (إن الشيطان يئس ... )','26','0','0');
+INSERT INTO [t11181] VALUES (5,'فصل في الجواب عن الحديث (ياعباد الله احبسوا) وبيان ضعفه','38','0','0');
+INSERT INTO [t11181] VALUES (6,'فصل في قتال من قال (لا إله إلا الله) إذا أتى بما يناقضها','40','0','0');
+INSERT INTO [t11181] VALUES (7,'مصادر ومراجع','52','0','0');
