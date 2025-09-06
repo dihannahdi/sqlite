eeea7329c110bf71ac18f3abfa0bb63d2219050a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:53:45 +0700

    Upload SQL chunk: t943_chunk_0000.sql

---
+++ sql_chunks/t943_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t943
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t943" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t943
+INSERT INTO [t943] VALUES (1,'تمهيد','2','0','0');
+INSERT INTO [t943] VALUES (2,'المبحث الأول: كلام الشوكاني عن المناسبات','3','0','0');
+INSERT INTO [t943] VALUES (3,'المبحث الثاني ردود تفصيلية','11','0','0');
+INSERT INTO [t943] VALUES (4,'المطلب الأول: زعمه أن المناسبات علم بلا فائدة','11','3','0');
+INSERT INTO [t943] VALUES (5,'المطلب الثاني زعمه أنه لا يطلب للآيات مناسبة لأنها نزلت متفرقة حسب الحوادث والنوازل','25','3','0');
+INSERT INTO [t943] VALUES (6,'المطلب الثالث: زعمه أن لا مناسبة بين الحديث عن بني إسرائيل في سورة البقرة وبين ما سبقه','36','3','0');
+INSERT INTO [t943] VALUES (7,'المطلب الرابع: إيراده الكثير من المناسبات في تفسيره!','57','3','0');
+INSERT INTO [t943] VALUES (8,'الخاتمة','84','0','0');
+INSERT INTO [t943] VALUES (9,'مراجع البحث','88','0','0');
