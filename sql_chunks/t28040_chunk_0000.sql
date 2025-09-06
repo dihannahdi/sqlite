2a8ee88ec01faa39be875f84bb52e58a00215b5d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:17 +0700

    Upload SQL chunk: t28040_chunk_0000.sql

---
+++ sql_chunks/t28040_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t28040
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t28040" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t28040
+INSERT INTO [t28040] VALUES (1,'مقدمة:','4','0','0');
+INSERT INTO [t28040] VALUES (2,'الفصل الأول: الرسول - صلى الله عليه وسلم - وسنته الشريفة:','11','0','0');
+INSERT INTO [t28040] VALUES (3,'مكانة السنة من القرآن:','24','2','0');
+INSERT INTO [t28040] VALUES (4,'مكانة السنة من التشريع:','28','2','0');
+INSERT INTO [t28040] VALUES (5,'الفصل الثاني: تدوين السنة:','33','0','0');
+INSERT INTO [t28040] VALUES (6,'الفصل الثالث: المحدثون في جهادهم:','59','0','0');
+INSERT INTO [t28040] VALUES (7,'الفصل الرابع: الوضاعون في العصر الحاضر:','81','0','0');
+INSERT INTO [t28040] VALUES (8,'فهرس الكتاب:','95','0','0');
