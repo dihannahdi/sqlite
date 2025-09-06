f4e99c75c7ba94d9549c8ab97f4d759f98653182
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:15 +0700

    Upload SQL chunk: t9953_chunk_0000.sql

---
+++ sql_chunks/t9953_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t9953
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t9953" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9953
+INSERT INTO [t9953] VALUES (1,'باب في ذكر مشايخ هذه الطريقة وما يدل من سيرهم وأقوالهم على تعظيم الشريعة','20','0','0');
+INSERT INTO [t9953] VALUES (2,'باب التوبة','193','0','0');
+INSERT INTO [t9953] VALUES (3,'باب الورع','219','0','0');
+INSERT INTO [t9953] VALUES (4,'باب الزهد','225','0','0');
+INSERT INTO [t9953] VALUES (5,'باب الصمت','231','0','0');
+INSERT INTO [t9953] VALUES (6,'باب الشكر','297','0','0');
+INSERT INTO [t9953] VALUES (7,'باب المراقبة','315','0','0');
+INSERT INTO [t9953] VALUES (8,'باب الرضا','319','0','0');
+INSERT INTO [t9953] VALUES (9,'باب الفتوة','357','0','0');
+INSERT INTO [t9953] VALUES (10,'باب الأدب','422','0','0');
+INSERT INTO [t9953] VALUES (11,'باب المعرفة بالله','454','0','0');
