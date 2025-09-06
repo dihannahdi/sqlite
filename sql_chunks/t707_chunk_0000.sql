a57f0d19190d2201be50d094b85f35fbc7869291
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:21 +0700

    Upload SQL chunk: t707_chunk_0000.sql

---
+++ sql_chunks/t707_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t707
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t707" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t707
+INSERT INTO [t707] VALUES (1,'فتنة التكفير','1','0','0');
