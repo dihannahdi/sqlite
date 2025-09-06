6b27df5fc7fb44f84fe73f7def6e1e10df39d40b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:39 +0700

    Upload SQL chunk: t10514_chunk_0000.sql

---
+++ sql_chunks/t10514_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10514
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10514" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10514
+INSERT INTO [t10514] VALUES (1,'الفوائد العوالي المنتقاة للرئيس القاسم بن الفضل الثقفي - الجزء ٤ - مخطوط','1','0','0');
