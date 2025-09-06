2d85b4402b2c2851f03d4faa1a9c11aba88dfa4c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:42 +0700

    Upload SQL chunk: t10412_chunk_0000.sql

---
+++ sql_chunks/t10412_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t10412
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t10412" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10412
+INSERT INTO [t10412] VALUES (1,'الإنسان متدين بطبعه','1','0','0');
+INSERT INTO [t10412] VALUES (2,'تبديل وتغيير','3','0','0');
+INSERT INTO [t10412] VALUES (3,'إنكار وافتراء','5','0','0');
+INSERT INTO [t10412] VALUES (4,'تدليل باطل','6','0','0');
+INSERT INTO [t10412] VALUES (5,'نقاش وإبطال','8','0','0');
+INSERT INTO [t10412] VALUES (6,'تعصب ممقوت','9','0','0');
+INSERT INTO [t10412] VALUES (7,'زيادة إيضاح','11','0','0');
+INSERT INTO [t10412] VALUES (8,'لا تصح المقارنة','17','0','0');
