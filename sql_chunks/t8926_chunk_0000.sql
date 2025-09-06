d03ff21fef3b18845011e72747679809abab9d6d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:40 +0700

    Upload SQL chunk: t8926_chunk_0000.sql

---
+++ sql_chunks/t8926_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t8926
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t8926" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8926
+INSERT INTO [t8926] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8926] VALUES (2,'دراسات المستشرقين للإسلام والعربية','6','0','0');
+INSERT INTO [t8926] VALUES (3,'المستشرقون في ميزان الإسلام','15','0','0');
+INSERT INTO [t8926] VALUES (4,'أهداف الإستشراق والمستشرقين','22','0','0');
+INSERT INTO [t8926] VALUES (5,'أسلوب جديد في محاربة الإسلام','25','0','0');
+INSERT INTO [t8926] VALUES (6,'من أساليب المستشرقين الفكرية','31','0','0');
