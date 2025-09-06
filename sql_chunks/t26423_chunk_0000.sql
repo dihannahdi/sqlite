1f6178a4e34e405af4ab2c4042f162232b57118b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:19 +0700

    Upload SQL chunk: t26423_chunk_0000.sql

---
+++ sql_chunks/t26423_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t26423
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t26423" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26423
+INSERT INTO [t26423] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t26423] VALUES (2,'وبه ثقتي','1','1','0');
+INSERT INTO [t26423] VALUES (3,'الجزء الأول','1','0','0');
+INSERT INTO [t26423] VALUES (4,'بسم الله الرحمن الرحيم','248','3','0');
+INSERT INTO [t26423] VALUES (5,'الجزء الثاني','248','0','0');
+INSERT INTO [t26423] VALUES (6,'رب أعني بمنك','487','5','0');
+INSERT INTO [t26423] VALUES (7,'الجزء الثالث','487','0','0');
+INSERT INTO [t26423] VALUES (8,'بسم الله الرحمن الرحيم','669','7','0');
+INSERT INTO [t26423] VALUES (9,'ربي أعن بمنك','669','7','0');
+INSERT INTO [t26423] VALUES (10,'الجزء الرابع','669','0','0');
+INSERT INTO [t26423] VALUES (11,'رب عونك بمنك','912','10','0');
+INSERT INTO [t26423] VALUES (12,'الجزء الخامس','912','0','0');
+INSERT INTO [t26423] VALUES (13,'بسم الله الرحمن الرحيم','1136','12','0');
+INSERT INTO [t26423] VALUES (14,'رب أعن برحمتك','1136','12','0');
+INSERT INTO [t26423] VALUES (15,'الجزء السادس','1136','0','0');
+INSERT INTO [t26423] VALUES (16,'رب أعن','1383','15','0');
+INSERT INTO [t26423] VALUES (17,'الجزء السابع','1383','0','0');
+INSERT INTO [t26423] VALUES (18,'بسم الله الرحمن الرحيم','1671','17','0');
+INSERT INTO [t26423] VALUES (19,'رب أعن برحمتك','1671','17','0');
+INSERT INTO [t26423] VALUES (20,'الجزء الثامن','1671','0','0');
+INSERT INTO [t26423] VALUES (21,'بسم الله الرحمن الرحيم','1874','20','0');
+INSERT INTO [t26423] VALUES (22,'رب أعن','1877','20','0');
