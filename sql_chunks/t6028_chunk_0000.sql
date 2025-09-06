df7bb7e4d95fa57cc9fb93457e7d46d625db8cfa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:33 +0700

    Upload SQL chunk: t6028_chunk_0000.sql

---
+++ sql_chunks/t6028_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t6028
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t6028" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6028
+INSERT INTO [t6028] VALUES (1,'رب يسر وأعن','1','0','0');
+INSERT INTO [t6028] VALUES (2,'باب ذكر أحوال رواة الجامع','14','0','0');
+INSERT INTO [t6028] VALUES (3,'ذكر الرواة عن الجراحي لجامع الترمذي','17','0','0');
+INSERT INTO [t6028] VALUES (4,'ذكر الراوي عن المشايخ الأربعه','21','0','0');
+INSERT INTO [t6028] VALUES (5,'ذكر من روى لنا الكروخي سماعا وإجازة','23','0','0');
