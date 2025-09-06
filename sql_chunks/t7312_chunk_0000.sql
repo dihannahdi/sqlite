022bb1b62c7482270afdfed88fda4d526bd33119
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:23 +0700

    Upload SQL chunk: t7312_chunk_0000.sql

---
+++ sql_chunks/t7312_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t7312
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t7312" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7312
+INSERT INTO [t7312] VALUES (1,'المجلد الأول','1','0','0');
+INSERT INTO [t7312] VALUES (2,'مقدمة','1','1','0');
+INSERT INTO [t7312] VALUES (3,'باب: الألف','2','1','0');
+INSERT INTO [t7312] VALUES (4,'باب: الباء','37','1','0');
+INSERT INTO [t7312] VALUES (5,'باب: التاء','45','1','0');
+INSERT INTO [t7312] VALUES (6,'باب: الثاء','67','1','0');
+INSERT INTO [t7312] VALUES (7,'باب: الجيم','68','1','0');
+INSERT INTO [t7312] VALUES (8,'باب: الحاء','76','1','0');
+INSERT INTO [t7312] VALUES (9,'باب: الخاء','90','1','0');
+INSERT INTO [t7312] VALUES (10,'باب: الدال','98','1','0');
+INSERT INTO [t7312] VALUES (11,'باب: الذال','102','1','0');
+INSERT INTO [t7312] VALUES (12,'باب: الراء','104','1','0');
+INSERT INTO [t7312] VALUES (13,'باب: الزاي','109','1','0');
+INSERT INTO [t7312] VALUES (14,'باب: السين','111','1','0');
+INSERT INTO [t7312] VALUES (15,'باب: الشين','119','1','0');
+INSERT INTO [t7312] VALUES (16,'باب: الصاد','126','1','0');
+INSERT INTO [t7312] VALUES (17,'باب: الضاد','132','1','0');
+INSERT INTO [t7312] VALUES (18,'باب: الطاء','135','1','0');
+INSERT INTO [t7312] VALUES (19,'باب: الظاء','138','1','0');
+INSERT INTO [t7312] VALUES (20,'باب: العين','140','1','0');
+INSERT INTO [t7312] VALUES (21,'باب: الغين','156','1','0');
+INSERT INTO [t7312] VALUES (22,'باب: الفاء','159','1','0');
+INSERT INTO [t7312] VALUES (23,'باب: القاف','166','1','0');
+INSERT INTO [t7312] VALUES (24,'باب: الكاف','178','1','0');
+INSERT INTO [t7312] VALUES (25,'باب: اللام','185','1','0');
+INSERT INTO [t7312] VALUES (26,'باب: الميم','190','1','0');
+INSERT INTO [t7312] VALUES (27,'باب: النون','234','1','0');
+INSERT INTO [t7312] VALUES (28,'باب: الواو','244','1','0');
+INSERT INTO [t7312] VALUES (29,'باب: الهاء','251','1','0');
+INSERT INTO [t7312] VALUES (30,'باب: الياء','253','1','0');
+INSERT INTO [t7312] VALUES (31,'باب: الفهارس','256','1','0');
