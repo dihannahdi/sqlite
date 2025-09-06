78832130a84e276b844a672c1479e31663514961
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:06 +0700

    Upload SQL chunk: t21670_chunk_0000.sql

---
+++ sql_chunks/t21670_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21670
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21670" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21670
+INSERT INTO [t21670] VALUES (1,'مشيخة يعقوب بن سفيان الفسوي','1','0','0');
