7e4d146ed7b577e4198bd0aaa7b7a86c8f700447
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:54 +0700

    Upload SQL chunk: t26339_chunk_0000.sql

---
+++ sql_chunks/t26339_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26339
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26339" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26339
+INSERT INTO [t26339] VALUES (1,'منتقى حديث أبي الحسن العبدويي','1','0','0');
