eb1d1c94bbfc350cda009ab035b6ad3cae24fb52
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:25 +0700

    Upload SQL chunk: t10372_chunk_0000.sql

---
+++ sql_chunks/t10372_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t10372
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t10372" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10372
+INSERT INTO [t10372] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10372] VALUES (2,'نبذة من التصوف الهندي الوثني','8','0','0');
+INSERT INTO [t10372] VALUES (3,'اقتفاء الجهلة المتصوفة آثار الهنادكة في تعذيب أنفسهم','11','0','0');
+INSERT INTO [t10372] VALUES (4,'الحكاية الأولى','12','0','0');
+INSERT INTO [t10372] VALUES (5,'الحكاية الثانية','14','0','0');
+INSERT INTO [t10372] VALUES (6,'الحكاية الثالثة','16','0','0');
+INSERT INTO [t10372] VALUES (7,'تعذيب أنفسهم بالجوع','18','0','0');
