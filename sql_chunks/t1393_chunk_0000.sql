6f1137d4b4f6aa15a2d1fd513ee6a718d088d068
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:04 +0700

    Upload SQL chunk: t1393_chunk_0000.sql

---
+++ sql_chunks/t1393_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t1393
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t1393" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1393
+INSERT INTO [t1393] VALUES (1,'المستشرقون والمبشرون في العالم العربي والإسلامي','3','0','0');
+INSERT INTO [t1393] VALUES (2,'تمهيد','3','0','0');
+INSERT INTO [t1393] VALUES (3,'أولا: لماذا أسلمت؟','4','0','0');
+INSERT INTO [t1393] VALUES (4,'الجهاز الكنيسي البروتستانتى لسنة ١٩٦٠','22','0','0');
+INSERT INTO [t1393] VALUES (5,'المجمع الأمريكي اختصاصاته، والمؤسسات التي يديرها:','25','0','0');
+INSERT INTO [t1393] VALUES (6,'أشهر المؤسسات التعليمة في الشرق العربي','37','0','0');
+INSERT INTO [t1393] VALUES (7,'منشور الجامعة الأمريكية','38','0','0');
+INSERT INTO [t1393] VALUES (8,'ما مدى نشاط هذا العمل التبشيري:','45','0','0');
+INSERT INTO [t1393] VALUES (9,'السياسة العامة للتبشير والاستشراق:','66','0','0');
+INSERT INTO [t1393] VALUES (10,'السياسة التوجيهية العامة','73','0','0');
+INSERT INTO [t1393] VALUES (11,'كمال التشريع الإسلامي (ولو كره الكافرون)','84','0','0');
+INSERT INTO [t1393] VALUES (12,'الشريعة الإسلامية','89','0','0');
+INSERT INTO [t1393] VALUES (13,'كلمة أخيرة ذات معنى','93','0','0');
+INSERT INTO [t1393] VALUES (14,'العلاج','96','0','0');
