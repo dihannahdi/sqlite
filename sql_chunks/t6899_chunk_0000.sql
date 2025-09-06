8131ff49b4b85a3ea7429ea6d9adb4e55f7bd12a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:30 +0700

    Upload SQL chunk: t6899_chunk_0000.sql

---
+++ sql_chunks/t6899_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t6899
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t6899" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6899
+INSERT INTO [t6899] VALUES (1,'باب المبادرة بالعمل','107','0','0');
+INSERT INTO [t6899] VALUES (2,'ذم التسويف','197','0','0');
+INSERT INTO [t6899] VALUES (3,'باب البناء وما ذموا منه','230','0','0');
+INSERT INTO [t6899] VALUES (4,'باب البناء وذمه','236','0','0');
