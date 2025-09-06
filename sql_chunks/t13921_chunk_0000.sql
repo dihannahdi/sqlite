4bf837020933f0dfa16e1e92642205253be68d67
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:59 +0700

    Upload SQL chunk: t13921_chunk_0000.sql

---
+++ sql_chunks/t13921_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t13921
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t13921" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13921
+INSERT INTO [t13921] VALUES (1,'تقديم بقلم الدكتور أحمد بن نعمان','5','0','0');
+INSERT INTO [t13921] VALUES (2,'كلمة المترجم','15','0','0');
+INSERT INTO [t13921] VALUES (3,'تصدير بقلم عبد الرحمان بن عمارة','17','0','0');
+INSERT INTO [t13921] VALUES (4,'توطئة','21','0','0');
+INSERT INTO [t13921] VALUES (5,'مقدمة','23','0','0');
+INSERT INTO [t13921] VALUES (6,'المرحلة الأولى الطالب','28','0','0');
+INSERT INTO [t13921] VALUES (7,'العنكبوت','29','6','0');
+INSERT INTO [t13921] VALUES (8,'أول الضحايا','45','6','0');
+INSERT INTO [t13921] VALUES (9,'رحيل والدتي','75','6','0');
+INSERT INTO [t13921] VALUES (10,'الخونة - الأبطال على الدرب','93','6','0');
+INSERT INTO [t13921] VALUES (11,'المرحلة الثانية المنبوذ','127','0','0');
+INSERT INTO [t13921] VALUES (12,'الفوضى','153','11','0');
+INSERT INTO [t13921] VALUES (13,'الحرب','191','11','0');
+INSERT INTO [t13921] VALUES (14,'فهرس الأسماء','195','0','0');
