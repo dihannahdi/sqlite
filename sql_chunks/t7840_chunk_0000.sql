a992b926b1bdca26ad5ab46575a57c41d9cfc930
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:21 +0700

    Upload SQL chunk: t7840_chunk_0000.sql

---
+++ sql_chunks/t7840_chunk_0000.sql
@@ -0,0 +1,67 @@
+-- Table: t7840
+-- Chunk: 1
+-- Rows: 60
+
+CREATE TABLE "t7840" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7840
+INSERT INTO [t7840] VALUES (1,'مقدمه الناظم','1','0','0');
+INSERT INTO [t7840] VALUES (2,'باب تعريف الكلام','2','0','0');
+INSERT INTO [t7840] VALUES (3,'باب معرفه الاسم','3','0','0');
+INSERT INTO [t7840] VALUES (4,'باب معرفه الفعل','3','0','0');
+INSERT INTO [t7840] VALUES (5,'باب معرفه الحرف','4','0','0');
+INSERT INTO [t7840] VALUES (6,'باب النكره والمعرفه','4','0','0');
+INSERT INTO [t7840] VALUES (7,'باب قسمه الافعال','6','0','0');
+INSERT INTO [t7840] VALUES (8,'باب معرفه الامر','7','0','0');
+INSERT INTO [t7840] VALUES (9,'باب احرف المضارعه','9','0','0');
+INSERT INTO [t7840] VALUES (10,'باب معرفه الاعراب','10','0','0');
+INSERT INTO [t7840] VALUES (11,'باب','12','0','0');
+INSERT INTO [t7840] VALUES (12,'فصل في الاسماء السته المعتله المضافه','13','0','0');
+INSERT INTO [t7840] VALUES (13,'فصل في الاسماء المنقوصه','14','0','0');
+INSERT INTO [t7840] VALUES (14,'فصل في الاسماء المقصور','15','0','0');
+INSERT INTO [t7840] VALUES (15,'فصل في التثنيه','16','0','0');
+INSERT INTO [t7840] VALUES (16,'باب جمع المذكر السالم','17','0','0');
+INSERT INTO [t7840] VALUES (17,'فصل في جمع التانيث','19','0','0');
+INSERT INTO [t7840] VALUES (18,'باب جمع التكسير','19','0','0');
+INSERT INTO [t7840] VALUES (19,'باب حروف الجر','20','0','0');
+INSERT INTO [t7840] VALUES (20,'فصل في القسم','21','0','0');
+INSERT INTO [t7840] VALUES (21,'فصل في الاضافه','22','0','0');
+INSERT INTO [t7840] VALUES (22,'باب كم الخبريه','23','0','0');
+INSERT INTO [t7840] VALUES (23,'باب المبتدا والخبر','24','0','0');
+INSERT INTO [t7840] VALUES (24,'باب الاشتغال','26','0','0');
+INSERT INTO [t7840] VALUES (25,'باب الفاعل','26','0','0');
+INSERT INTO [t7840] VALUES (26,'فصل في افراد الفعل مع الفاعل وتذكيره وتانيثه','27','0','0');
+INSERT INTO [t7840] VALUES (27,'باب المفعول الذي لم يسم فاعله','28','0','0');
+INSERT INTO [t7840] VALUES (28,'باب المفعول به','29','0','0');
+INSERT INTO [t7840] VALUES (29,'باب الافعال المتعديه وهي ظننت واخواتها','30','0','0');
+INSERT INTO [t7840] VALUES (30,'باب اسم الفاعل المنون','31','0','0');
+INSERT INTO [t7840] VALUES (31,'باب المصدر','32','0','0');
+INSERT INTO [t7840] VALUES (32,'باب المفعول له','34','0','0');
+INSERT INTO [t7840] VALUES (33,'باب المفعول معه','35','0','0');
+INSERT INTO [t7840] VALUES (34,'باب الحال','35','0','0');
+INSERT INTO [t7840] VALUES (35,'باب التمييز','37','0','0');
+INSERT INTO [t7840] VALUES (36,'اساليب المدح والذم','38','0','0');
+INSERT INTO [t7840] VALUES (37,'باب كم الاستفهاميه','39','0','0');
+INSERT INTO [t7840] VALUES (38,'باب الظروف','39','0','0');
+INSERT INTO [t7840] VALUES (39,'باب الاستثناء','41','0','0');
+INSERT INTO [t7840] VALUES (40,'باب لا في النفي','43','0','0');
+INSERT INTO [t7840] VALUES (41,'باب التعجب','44','0','0');
+INSERT INTO [t7840] VALUES (42,'باب الاغراء','45','0','0');
+INSERT INTO [t7840] VALUES (43,'باب التحذير','46','0','0');
+INSERT INTO [t7840] VALUES (44,'باب ان واخواتها','46','0','0');
+INSERT INTO [t7840] VALUES (45,'باب كان واخواتها','49','0','0');
+INSERT INTO [t7840] VALUES (46,'باب ما النافيه','51','0','0');
+INSERT INTO [t7840] VALUES (47,'باب النداء','52','0','0');
+INSERT INTO [t7840] VALUES (48,'باب الترخيم','54','0','0');
+INSERT INTO [t7840] VALUES (49,'باب التصغير','56','0','0');
+INSERT INTO [t7840] VALUES (50,'باب الحروف الزوائد','59','0','0');
+INSERT INTO [t7840] VALUES (51,'باب النسب','61','0','0');
+INSERT INTO [t7840] VALUES (52,'باب التوابع','63','0','0');
+INSERT INTO [t7840] VALUES (53,'باب العطف','64','0','0');
+INSERT INTO [t7840] VALUES (54,'باب ما لا ينصرف','65','0','0');
+INSERT INTO [t7840] VALUES (55,'باب العدد','71','0','0');
+INSERT INTO [t7840] VALUES (56,'باب نواصب الفعل المستقبل','72','0','0');
+INSERT INTO [t7840] VALUES (57,'فصل الافعال الخمسه','76','0','0');
+INSERT INTO [t7840] VALUES (58,'باب ما يجزم الفعل المضارع','78','0','0');
+INSERT INTO [t7840] VALUES (59,'باب الشرط والجزاء','80','0','0');
+INSERT INTO [t7840] VALUES (60,'باب المبنيات','82','0','0');
