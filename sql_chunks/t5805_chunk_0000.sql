3c6f119eb5c0fdb19a6196514c73cf50e521cdab
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:35 +0700

    Upload SQL chunk: t5805_chunk_0000.sql

---
+++ sql_chunks/t5805_chunk_0000.sql
@@ -0,0 +1,58 @@
+-- Table: t5805
+-- Chunk: 1
+-- Rows: 51
+
+CREATE TABLE "t5805" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5805
+INSERT INTO [t5805] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5805] VALUES (2,'حرف الألف','1','0','0');
+INSERT INTO [t5805] VALUES (3,'من اسمه أحمد','1','0','0');
+INSERT INTO [t5805] VALUES (4,'حرف الباء','51','0','0');
+INSERT INTO [t5805] VALUES (5,'حرف التاء','53','0','0');
+INSERT INTO [t5805] VALUES (6,'حرف الجيم','54','0','0');
+INSERT INTO [t5805] VALUES (7,'حرف الحاء','56','0','0');
+INSERT INTO [t5805] VALUES (8,'من اسمه الحسين','57','0','0');
+INSERT INTO [t5805] VALUES (9,'تم الفصل من اسمه الحسين','61','0','0');
+INSERT INTO [t5805] VALUES (10,'حرف الخاء','63','0','0');
+INSERT INTO [t5805] VALUES (11,'من اسمه الخليل','63','0','0');
+INSERT INTO [t5805] VALUES (12,'خميس بن علي الحوزي الحافظ','68','0','0');
+INSERT INTO [t5805] VALUES (13,'حرف الدال','72','0','0');
+INSERT INTO [t5805] VALUES (14,'حرف الذال','74','0','0');
+INSERT INTO [t5805] VALUES (15,'حرف الراء','76','0','0');
+INSERT INTO [t5805] VALUES (16,'حرف الزاي','87','0','0');
+INSERT INTO [t5805] VALUES (17,'حرف السين','90','0','0');
+INSERT INTO [t5805] VALUES (18,'حرف الشين','98','0','0');
+INSERT INTO [t5805] VALUES (19,'حرف الصاد','107','0','0');
+INSERT INTO [t5805] VALUES (20,'حرف الضاد','114','0','0');
+INSERT INTO [t5805] VALUES (21,'حرف الطاء','117','0','0');
+INSERT INTO [t5805] VALUES (22,'حرف الظاء','122','0','0');
+INSERT INTO [t5805] VALUES (23,'حرف العين','126','0','0');
+INSERT INTO [t5805] VALUES (24,'من اسمه عبد الله','126','0','0');
+INSERT INTO [t5805] VALUES (25,'من اسمه عبد الرحمن','158','0','0');
+INSERT INTO [t5805] VALUES (26,'من اسمه عبد الواحد','169','0','0');
+INSERT INTO [t5805] VALUES (27,'من اسمه عبد الكريم','175','0','0');
+INSERT INTO [t5805] VALUES (28,'فصل آخر في الأسماء التي تبتدئ بعبد','185','0','0');
+INSERT INTO [t5805] VALUES (29,'من اسمه عبد الحق','188','0','0');
+INSERT INTO [t5805] VALUES (30,'أسماء أخرى تبتدئ بعبد','190','0','0');
+INSERT INTO [t5805] VALUES (31,'من أسمه عبد الملك','200','0','0');
+INSERT INTO [t5805] VALUES (32,'فصل آخر في الأسماء التي تبتدئ بعبد','202','0','0');
+INSERT INTO [t5805] VALUES (33,'من اسمه عبيد الله','212','0','0');
+INSERT INTO [t5805] VALUES (34,'فصل آخر في الأسماء التي تبتدئ بعبد','214','0','0');
+INSERT INTO [t5805] VALUES (35,'من اسمه عمر','216','0','0');
+INSERT INTO [t5805] VALUES (36,'من اسمه عثمان','232','0','0');
+INSERT INTO [t5805] VALUES (37,'من اسمه علي','235','0','0');
+INSERT INTO [t5805] VALUES (38,'من اسمه عامر','290','0','0');
+INSERT INTO [t5805] VALUES (39,'من اسمه عتيق','291','0','0');
+INSERT INTO [t5805] VALUES (40,'من اسمه عيسى','294','0','0');
+INSERT INTO [t5805] VALUES (41,'فصل في بقية الأسماء التي تبتدئ بعين','296','0','0');
+INSERT INTO [t5805] VALUES (42,'حرف الغين','310','0','0');
+INSERT INTO [t5805] VALUES (43,'حرف الفاء','316','0','0');
+INSERT INTO [t5805] VALUES (44,'حرف القاف','326','0','0');
+INSERT INTO [t5805] VALUES (45,'حرف الكاف','328','0','0');
+INSERT INTO [t5805] VALUES (46,'حرف اللام','334','0','0');
+INSERT INTO [t5805] VALUES (47,'حرف الميم','335','0','0');
+INSERT INTO [t5805] VALUES (48,'حرف النون','378','0','0');
+INSERT INTO [t5805] VALUES (49,'حرف الهاء','398','0','0');
+INSERT INTO [t5805] VALUES (50,'حرف الواو','418','0','0');
+INSERT INTO [t5805] VALUES (51,'حرف الياء','425','0','0');
