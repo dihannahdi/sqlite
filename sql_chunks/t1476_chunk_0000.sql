77e28b855facdc73a937aa94642093d0ddf06e8d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:54 +0700

    Upload SQL chunk: t1476_chunk_0000.sql

---
+++ sql_chunks/t1476_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t1476
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t1476" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1476
+INSERT INTO [t1476] VALUES (1,'القرآن الكريم منهج متكامل','1','0','0');
+INSERT INTO [t1476] VALUES (2,'منهج متكامل لإصلاح المجتمع','1','0','0');
+INSERT INTO [t1476] VALUES (3,'المحور الأول: (الهدف العام من إنزال القرآن الكريم)','1','0','0');
+INSERT INTO [t1476] VALUES (4,'المحور الثاني: (علاقة الإنسان بربه)','4','0','0');
+INSERT INTO [t1476] VALUES (5,'المحور الثالث: علاقة الإنسان بمجتمعه','8','0','0');
+INSERT INTO [t1476] VALUES (6,'الخاتمة','16','0','0');
+INSERT INTO [t1476] VALUES (7,'المصادر والمراجع','16','0','0');
