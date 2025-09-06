61cb480f4c8ce48a773f6580a4a4a96efa016c19
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:56 +0700

    Upload SQL chunk: t95670_chunk_0000.sql

---
+++ sql_chunks/t95670_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t95670
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t95670" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95670
+INSERT INTO [t95670] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95670] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t95670] VALUES (3,'من منازل إياك نعبد','3','0','0');
+INSERT INTO [t95670] VALUES (4,'من سيرته - صلى الله عليه وسلم -','5','0','0');
+INSERT INTO [t95670] VALUES (5,'ما هو التواضع','5','0','0');
+INSERT INTO [t95670] VALUES (6,'رأس التواضع ثلاث','7','0','0');
+INSERT INTO [t95670] VALUES (7,'العجب','11','0','0');
+INSERT INTO [t95670] VALUES (8,'الفرق بين التواضع والمهانة','12','0','0');
+INSERT INTO [t95670] VALUES (9,'أين نحن من هؤلاء؟','18','0','0');
+INSERT INTO [t95670] VALUES (10,'التواضع نوعان','19','0','0');
+INSERT INTO [t95670] VALUES (11,'ابن تيمية المتواضع','20','0','0');
+INSERT INTO [t95670] VALUES (12,'أول ذنب عصي الله به','21','0','0');
+INSERT INTO [t95670] VALUES (13,'وصايا','22','0','0');
+INSERT INTO [t95670] VALUES (14,'التكبر على الخالق','25','0','0');
+INSERT INTO [t95670] VALUES (15,'مواقف عظيمة','29','0','0');
+INSERT INTO [t95670] VALUES (16,'الكبر من أخلاق الكفار','33','0','0');
+INSERT INTO [t95670] VALUES (17,'من أنواع الكبر','34','0','0');
+INSERT INTO [t95670] VALUES (18,'من خصال المتكبرين','38','0','0');
+INSERT INTO [t95670] VALUES (19,'أصحاب المنامات','42','0','0');
+INSERT INTO [t95670] VALUES (20,'العجب يدعو إلى الكبر','44','0','0');
+INSERT INTO [t95670] VALUES (21,'مواقف جميلة','47','0','0');
+INSERT INTO [t95670] VALUES (22,'الحال مع الفقراء','49','0','0');
