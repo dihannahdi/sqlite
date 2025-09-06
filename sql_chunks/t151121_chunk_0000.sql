b4e56c51ecedb81bf488c5aadafc081f9e866e7a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:23:56 +0700

    Upload SQL chunk: t151121_chunk_0000.sql

---
+++ sql_chunks/t151121_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t151121
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t151121" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t151121
+INSERT INTO [t151121] VALUES (1,'البشارة العظمى للمؤمن بأن حظه من النار الحمى','1','0','0');
