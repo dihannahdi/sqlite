75eb3ec42406fec67868fc5c89e74566aac1620d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:20:01 +0700

    Upload SQL chunk: t145424_chunk_0000.sql

---
+++ sql_chunks/t145424_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t145424
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t145424" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t145424
+INSERT INTO [t145424] VALUES (1,'مقدمة النشرة الرابعة','2','0','0');
+INSERT INTO [t145424] VALUES (2,'مقدمة النشرة الثالثة','4','0','0');
+INSERT INTO [t145424] VALUES (3,'هذه الأكلة الرمضانية العجيبة «الفارسية»','11','0','0');
+INSERT INTO [t145424] VALUES (4,'اسم السمبوسة','11','0','0');
+INSERT INTO [t145424] VALUES (5,'تعريف السمبوسة، وطريقة عملها','13','0','0');
+INSERT INTO [t145424] VALUES (6,'أطعمة العرب','18','0','0');
+INSERT INTO [t145424] VALUES (7,'دخول الأطعمة الفارسية والرومية البلاد العربية','20','0','0');
+INSERT INTO [t145424] VALUES (8,'كُتب الطبخ عند العرب','22','0','0');
