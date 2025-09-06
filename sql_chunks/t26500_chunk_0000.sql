557c2dd90786b69699cab271e3299312cf1d3b3a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:55 +0700

    Upload SQL chunk: t26500_chunk_0000.sql

---
+++ sql_chunks/t26500_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26500
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26500" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26500
+INSERT INTO [t26500] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26500] VALUES (2,'جزء فيه: المنتخب من غرائب أحاديث مالك بن أنس','2','0','0');
