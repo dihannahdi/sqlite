4021846effd830953cda36fc041a0456dca48161
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:17 +0700

    Upload SQL chunk: t14014_chunk_0000.sql

---
+++ sql_chunks/t14014_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t14014
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t14014" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14014
+INSERT INTO [t14014] VALUES (1,'تصدير المجموعة الثانية','3','0','0');
+INSERT INTO [t14014] VALUES (2,'مقدمة المعتني وفيها وصف النسخ المعتمدة في التحقيق','6','0','0');
+INSERT INTO [t14014] VALUES (3,'السيد جمال الدين القاسمي','9','0','0');
+INSERT INTO [t14014] VALUES (4,'[مقدمة المصنف]','23','0','0');
+INSERT INTO [t14014] VALUES (5,'سياق الأحاديث الواردة في الإستغفار عقب الصلوات','24','0','0');
+INSERT INTO [t14014] VALUES (6,'كلام ابن القيم في سر الاستغفار عقب العبادات','26','0','0');
+INSERT INTO [t14014] VALUES (7,'خاتمة الرسالة','32','0','0');
