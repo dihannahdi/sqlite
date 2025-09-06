e713902851e117c3826a13969b95a9a75742005e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:09 +0700

    Upload SQL chunk: t11737_chunk_0000.sql

---
+++ sql_chunks/t11737_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t11737
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t11737" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11737
+INSERT INTO [t11737] VALUES (1,'مقدمات','1','0','0');
+INSERT INTO [t11737] VALUES (2,'مقدمة الطبعة الجديدة','1','1','0');
+INSERT INTO [t11737] VALUES (3,'مقدمة الطبعة الأولى','3','1','0');
+INSERT INTO [t11737] VALUES (4,'مدخل','5','1','0');
+INSERT INTO [t11737] VALUES (5,'الحديث الأول','7','0','0');
+INSERT INTO [t11737] VALUES (6,'الحديث الثاني','8','0','0');
+INSERT INTO [t11737] VALUES (7,'الحديث الثالث','10','0','0');
+INSERT INTO [t11737] VALUES (8,'الحديث الرابع','12','0','0');
+INSERT INTO [t11737] VALUES (9,'الحديث الخامس','15','0','0');
+INSERT INTO [t11737] VALUES (10,'الحديث السادس','17','0','0');
+INSERT INTO [t11737] VALUES (11,'الحديث السابع','20','0','0');
+INSERT INTO [t11737] VALUES (12,'الحديث الثامن','21','0','0');
+INSERT INTO [t11737] VALUES (13,'الحديث التاسع','25','0','0');
+INSERT INTO [t11737] VALUES (14,'الحديث العاشر','29','0','0');
+INSERT INTO [t11737] VALUES (15,'الحديث الحادي عشر','30','0','0');
+INSERT INTO [t11737] VALUES (16,'الحديث الثاني عشر','31','0','0');
+INSERT INTO [t11737] VALUES (17,'الحديث الثالث عشر','34','0','0');
+INSERT INTO [t11737] VALUES (18,'الحديث الرابع عشر','35','0','0');
+INSERT INTO [t11737] VALUES (19,'الحديث الخامس عشر','36','0','0');
+INSERT INTO [t11737] VALUES (20,'الحديث السادس عشر','37','0','0');
+INSERT INTO [t11737] VALUES (21,'الحديث السابع عشر','39','0','0');
+INSERT INTO [t11737] VALUES (22,'الحديث الثامن عشر','40','0','0');
+INSERT INTO [t11737] VALUES (23,'الحديث التاسع عشر','44','0','0');
+INSERT INTO [t11737] VALUES (24,'الحديث العشرون','46','0','0');
+INSERT INTO [t11737] VALUES (25,'الحديث الواحد والعشرون','47','0','0');
+INSERT INTO [t11737] VALUES (26,'الحديث الثاني والعشرون','56','0','0');
+INSERT INTO [t11737] VALUES (27,'الحديث الثالث والعشرون','57','0','0');
+INSERT INTO [t11737] VALUES (28,'الحديث الرابع والعشرون','58','0','0');
+INSERT INTO [t11737] VALUES (29,'الحديث الخامس والعشرون','59','0','0');
+INSERT INTO [t11737] VALUES (30,'الحديث السادس والعشرون','60','0','0');
+INSERT INTO [t11737] VALUES (31,'الحديث السابع والعشرون','61','0','0');
+INSERT INTO [t11737] VALUES (32,'الحديث الثامن والعشرون','63','0','0');
+INSERT INTO [t11737] VALUES (33,'الحديث التاسع والعشرون','65','0','0');
+INSERT INTO [t11737] VALUES (34,'الحديث الثلاثون','68','0','0');
+INSERT INTO [t11737] VALUES (35,'كلمة عامة حول بعض أبواب الكتاب','70','0','0');
+INSERT INTO [t11737] VALUES (36,'الفهارس','73','0','0');
+INSERT INTO [t11737] VALUES (37,'فهرس الكتاب','73','36','0');
+INSERT INTO [t11737] VALUES (38,'فهرس الأحاديث والآثار مرتبة على الحروف','81','36','0');
