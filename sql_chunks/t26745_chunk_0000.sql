7721113785cf981f1ece0ed8774074fac4eaa880
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:29 +0700

    Upload SQL chunk: t26745_chunk_0000.sql

---
+++ sql_chunks/t26745_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t26745
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t26745" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26745
+INSERT INTO [t26745] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26745] VALUES (2,'الجزء الثاني من حديث أبي عبيدة مجاعة بن الزبير العتكي','3','0','0');
+INSERT INTO [t26745] VALUES (3,'[من حديث أبي الحسين عبد الباقي بن قانع البغدادي]','58','0','0');
+INSERT INTO [t26745] VALUES (4,'الجزء فيه من حديث أبي الحسين عبد الصمد بن علي بن مكرم الطستي','74','0','0');
+INSERT INTO [t26745] VALUES (5,'نسخة السري بن سهل الجنديسابوري عن عبد الله بن رشيد عن أبي عبيدة مجاعة بن الزبير مما لم يرد في القسمين الأولين','94','0','0');
