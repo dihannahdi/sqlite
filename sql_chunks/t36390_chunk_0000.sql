7554d54abaa82121adb8422911c9b6b60df01e27
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:45 +0700

    Upload SQL chunk: t36390_chunk_0000.sql

---
+++ sql_chunks/t36390_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t36390
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t36390" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36390
+INSERT INTO [t36390] VALUES (1,'الحمد لله الذي أكرم أولياءه بجنات النعيم','1','0','0');
+INSERT INTO [t36390] VALUES (2,'أخي في الله','2','0','0');
+INSERT INTO [t36390] VALUES (3,'أخي في الله','7','0','0');
+INSERT INTO [t36390] VALUES (4,'أخي في الله','10','0','0');
+INSERT INTO [t36390] VALUES (5,'أخي في الله','15','0','0');
+INSERT INTO [t36390] VALUES (6,'أخي المسلم','15','0','0');
