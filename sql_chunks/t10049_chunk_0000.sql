e8a53961e65e906d90780b09af14b680e39c303e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:29 +0700

    Upload SQL chunk: t10049_chunk_0000.sql

---
+++ sql_chunks/t10049_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t10049
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t10049" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10049
+INSERT INTO [t10049] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10049] VALUES (2,'مدخل','23','0','0');
+INSERT INTO [t10049] VALUES (3,'من باب الزكاة','24','0','0');
+INSERT INTO [t10049] VALUES (4,'من باب بيع الأصول والثمار','27','0','0');
+INSERT INTO [t10049] VALUES (5,'من باب الرهن','29','0','0');
+INSERT INTO [t10049] VALUES (6,'من باب الفلس','32','0','0');
+INSERT INTO [t10049] VALUES (7,'من باب الحجر','35','0','0');
+INSERT INTO [t10049] VALUES (8,'من باب الوكالة','39','0','0');
+INSERT INTO [t10049] VALUES (9,'من باب الإقرار','40','0','0');
+INSERT INTO [t10049] VALUES (10,'من باب العارية','41','0','0');
+INSERT INTO [t10049] VALUES (11,'من باب الوقف','43','0','0');
+INSERT INTO [t10049] VALUES (12,'من باب الشهادات','44','0','0');
+INSERT INTO [t10049] VALUES (13,'من باب إحياء الموات','46','0','0');
+INSERT INTO [t10049] VALUES (14,'من كتاب الوصية','47','0','0');
+INSERT INTO [t10049] VALUES (15,'من كتاب النكاح','48','0','0');
+INSERT INTO [t10049] VALUES (16,'من باب الصداق','51','0','0');
+INSERT INTO [t10049] VALUES (17,'من باب الاستبراء','53','0','0');
+INSERT INTO [t10049] VALUES (18,'من باب الحضانة','54','0','0');
+INSERT INTO [t10049] VALUES (19,'من باب الجنايات','56','0','0');
+INSERT INTO [t10049] VALUES (20,'من باب القضاء','58','0','0');
+INSERT INTO [t10049] VALUES (21,'من باب الدعوى','59','0','0');
+INSERT INTO [t10049] VALUES (22,'من باب الشهادة','60','0','0');
+INSERT INTO [t10049] VALUES (23,'من باب الجزية','62','0','0');
+INSERT INTO [t10049] VALUES (24,'من باب العتق','64','0','0');
+INSERT INTO [t10049] VALUES (25,'من باب أمية الولد','66','0','0');
+INSERT INTO [t10049] VALUES (26,'مصادر ومراجع','67','0','0');
