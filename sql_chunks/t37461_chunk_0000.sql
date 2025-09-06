5d19fdbdde7cb54e556913fe7cb6f4b9cc8b9650
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:14 +0700

    Upload SQL chunk: t37461_chunk_0000.sql

---
+++ sql_chunks/t37461_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t37461
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t37461" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37461
+INSERT INTO [t37461] VALUES (1,'فصل','1','0','0');
+INSERT INTO [t37461] VALUES (2,'فصل','5','0','0');
+INSERT INTO [t37461] VALUES (3,'فصل','8','0','0');
+INSERT INTO [t37461] VALUES (4,'فصل','17','0','0');
+INSERT INTO [t37461] VALUES (5,'فصل','18','0','0');
+INSERT INTO [t37461] VALUES (6,'فصل أيضا منه','20','0','0');
