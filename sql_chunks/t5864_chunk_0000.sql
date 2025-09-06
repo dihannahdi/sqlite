3956a936b6e800809cd726bbd14157e26c31443e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:06 +0700

    Upload SQL chunk: t5864_chunk_0000.sql

---
+++ sql_chunks/t5864_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t5864
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t5864" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5864
+INSERT INTO [t5864] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5864] VALUES (2,'مقدمة','1','0','0');
+INSERT INTO [t5864] VALUES (3,'بلال المؤذن','10','0','0');
+INSERT INTO [t5864] VALUES (4,'الحديث الرابع','55','0','0');
+INSERT INTO [t5864] VALUES (5,'الحديث الخامس','56','0','0');
