4a7b66d8c515c65dc786804ed6d1dd959ba977ca
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:48 +0700

    Upload SQL chunk: t12179_chunk_0000.sql

---
+++ sql_chunks/t12179_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t12179
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t12179" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12179
+INSERT INTO [t12179] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12179] VALUES (2,'التمهيد','7','0','0');
+INSERT INTO [t12179] VALUES (3,'الفصل الأول: في لمس العورة','14','0','0');
+INSERT INTO [t12179] VALUES (4,'المبحث الأول: في لمس الفرج','14','3','0');
+INSERT INTO [t12179] VALUES (5,'المطلب الأول: في لمس الرجل فرجه','14','4','0');
+INSERT INTO [t12179] VALUES (6,'المطلب الثاني: في لمس الرجل فرج غيره','25','4','0');
+INSERT INTO [t12179] VALUES (7,'المطلب الثالث: في لمس المرأة فرجها','27','4','0');
+INSERT INTO [t12179] VALUES (8,'المبحث الثاني: في لمس غير الفرج من العورة','30','3','0');
+INSERT INTO [t12179] VALUES (9,'المطلب الأول: في لمس الدبر','30','8','0');
+INSERT INTO [t12179] VALUES (10,'المطلب الثاني: في لمس الأنثيين والألية والعانة','34','8','0');
+INSERT INTO [t12179] VALUES (11,'المطلب الثالث: في لمس فرج البهيمة','36','8','0');
