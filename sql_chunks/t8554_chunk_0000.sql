4b47e2eba1fa8c067fcee181e9c2507d98872091
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:10 +0700

    Upload SQL chunk: t8554_chunk_0000.sql

---
+++ sql_chunks/t8554_chunk_0000.sql
@@ -0,0 +1,56 @@
+-- Table: t8554
+-- Chunk: 1
+-- Rows: 49
+
+CREATE TABLE "t8554" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8554
+INSERT INTO [t8554] VALUES (1,'باب ذكر فصول تكون كالمقدمة لهذا الكتاب','1','0','0');
+INSERT INTO [t8554] VALUES (2,'باب ذكر آي في سورة البقرة في ذلك','4','0','0');
+INSERT INTO [t8554] VALUES (3,'سورة آل عمران','12','0','0');
+INSERT INTO [t8554] VALUES (4,'سورة النساء','13','0','0');
+INSERT INTO [t8554] VALUES (5,'سورة المائدة','16','0','0');
+INSERT INTO [t8554] VALUES (6,'سورة [الأنعام]','20','0','0');
+INSERT INTO [t8554] VALUES (7,'سورة الأعراف','26','0','0');
+INSERT INTO [t8554] VALUES (8,'سورة الأنفال','26','0','0');
+INSERT INTO [t8554] VALUES (9,'سورة التوبة','28','0','0');
+INSERT INTO [t8554] VALUES (10,'سورة يونس','28','0','0');
+INSERT INTO [t8554] VALUES (11,'سورة هود عليه السلام','29','0','0');
+INSERT INTO [t8554] VALUES (12,'سورة الرعد','30','0','0');
+INSERT INTO [t8554] VALUES (13,'سورة [الحجر]','31','0','0');
+INSERT INTO [t8554] VALUES (14,'سورة النحل','31','0','0');
+INSERT INTO [t8554] VALUES (15,'سورة الإسراء','33','0','0');
+INSERT INTO [t8554] VALUES (16,'سورة طه','33','0','0');
+INSERT INTO [t8554] VALUES (17,'سورة [الحج]','34','0','0');
+INSERT INTO [t8554] VALUES (18,'سورة المؤمنون','34','0','0');
+INSERT INTO [t8554] VALUES (19,'سورة النور','35','0','0');
+INSERT INTO [t8554] VALUES (20,'سورة الفرقان','36','0','0');
+INSERT INTO [t8554] VALUES (21,'سورة النمل','36','0','0');
+INSERT INTO [t8554] VALUES (22,'سورة القصص','36','0','0');
+INSERT INTO [t8554] VALUES (23,'سورة العنكبوت','37','0','0');
+INSERT INTO [t8554] VALUES (24,'سورة السجدة','37','0','0');
+INSERT INTO [t8554] VALUES (25,'سورة الأحزاب','37','0','0');
+INSERT INTO [t8554] VALUES (26,'سورة سبأ','38','0','0');
+INSERT INTO [t8554] VALUES (27,'سورة الصافات','38','0','0');
+INSERT INTO [t8554] VALUES (28,'سورة الزمر','39','0','0');
+INSERT INTO [t8554] VALUES (29,'سورة المؤمن','39','0','0');
+INSERT INTO [t8554] VALUES (30,'سورة السجدة','40','0','0');
+INSERT INTO [t8554] VALUES (31,'سورة حم عسق','40','0','0');
+INSERT INTO [t8554] VALUES (32,'سورة الزخرف','42','0','0');
+INSERT INTO [t8554] VALUES (33,'سورة الدخان','42','0','0');
+INSERT INTO [t8554] VALUES (34,'سورة الجاثية','42','0','0');
+INSERT INTO [t8554] VALUES (35,'سورة الأحقاف','43','0','0');
+INSERT INTO [t8554] VALUES (36,'سورة محمد صلي الله عليه وسلم','43','0','0');
+INSERT INTO [t8554] VALUES (37,'سورة ق','44','0','0');
+INSERT INTO [t8554] VALUES (38,'سورة الذاريات','44','0','0');
+INSERT INTO [t8554] VALUES (39,'سورة الطور','44','0','0');
+INSERT INTO [t8554] VALUES (40,'سورة النجم','45','0','0');
+INSERT INTO [t8554] VALUES (41,'سورة المجادلة','45','0','0');
+INSERT INTO [t8554] VALUES (42,'سورة الحشر','46','0','0');
+INSERT INTO [t8554] VALUES (43,'سورة الممتحنة','46','0','0');
+INSERT INTO [t8554] VALUES (44,'سورة التغابن','47','0','0');
+INSERT INTO [t8554] VALUES (45,'سورة ن','47','0','0');
+INSERT INTO [t8554] VALUES (46,'سورة المعارج','48','0','0');
+INSERT INTO [t8554] VALUES (47,'سورة المزمل','48','0','0');
+INSERT INTO [t8554] VALUES (48,'سورة الغاشية','49','0','0');
+INSERT INTO [t8554] VALUES (49,'سورة الكافرون','49','0','0');
