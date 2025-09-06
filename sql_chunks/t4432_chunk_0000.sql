c8a1b74c64c01d8384db81b81921767a40dc5149
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:10 +0700

    Upload SQL chunk: t4432_chunk_0000.sql

---
+++ sql_chunks/t4432_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t4432
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t4432" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4432
+INSERT INTO [t4432] VALUES (1,'بين يدي القارئ','1','0','0');
+INSERT INTO [t4432] VALUES (2,'تمهيد:','2','0','0');
+INSERT INTO [t4432] VALUES (3,'نص ابن الصلاح في تصحيح الأحاديث وتحسينها:','5','0','0');
+INSERT INTO [t4432] VALUES (4,'تحليل النص:','6','0','0');
+INSERT INTO [t4432] VALUES (5,'مستجدات عصره وأثرها في معرفة الصحيح والحسن','7','0','0');
+INSERT INTO [t4432] VALUES (6,'كتب الأجزاء وغيرها','11','0','0');
+INSERT INTO [t4432] VALUES (7,'المتأخرون وأسانيدهم وأمثلة روايتهم العالية:','13','0','0');
+INSERT INTO [t4432] VALUES (8,'المثال الأول وتحليله:','14','0','0');
+INSERT INTO [t4432] VALUES (9,'المثال الثاني وتحليله:','17','0','0');
+INSERT INTO [t4432] VALUES (10,'المثال الثالث وتحليله:','18','0','0');
+INSERT INTO [t4432] VALUES (11,'مقصود ابن الصلاح بالأجزاء وغيرها:','20','0','0');
+INSERT INTO [t4432] VALUES (12,'توجيه رأي ابن الصلاح وتحقيقه:','20','0','0');
+INSERT INTO [t4432] VALUES (13,'مواقف المعارضين له:','25','0','0');
+INSERT INTO [t4432] VALUES (14,'الإمام النووي ورأيه:','26','0','0');
+INSERT INTO [t4432] VALUES (15,'الحافظ العراقي ورأيه:','26','0','0');
+INSERT INTO [t4432] VALUES (16,'الحافظ ابن حجر ورأيه:','26','0','0');
+INSERT INTO [t4432] VALUES (17,'الحافظ السيوطي ومحاولة الاستدراك عليهم:','29','0','0');
+INSERT INTO [t4432] VALUES (18,'مناقشة رأي السيوطي:','30','0','0');
+INSERT INTO [t4432] VALUES (19,'الخاتمة:','36','0','0');
