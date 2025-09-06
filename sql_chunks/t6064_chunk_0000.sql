22247487caf8420e87acc10291449e1806c6f09c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:53 +0700

    Upload SQL chunk: t6064_chunk_0000.sql

---
+++ sql_chunks/t6064_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t6064
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t6064" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6064
+INSERT INTO [t6064] VALUES (1,'حرف الهمزة','3','0','0');
+INSERT INTO [t6064] VALUES (2,'حرف الباء الموحدة','29','0','0');
+INSERT INTO [t6064] VALUES (3,'حرف التاء المثناة من فوق','35','0','0');
+INSERT INTO [t6064] VALUES (4,'حرف الثاء المثلثة','42','0','0');
+INSERT INTO [t6064] VALUES (5,'حرف الجيم','42','0','0');
+INSERT INTO [t6064] VALUES (6,'حرف الحاء المهملة','43','0','0');
+INSERT INTO [t6064] VALUES (7,'حرف الخاء المعجمة','51','0','0');
+INSERT INTO [t6064] VALUES (8,'حرف الدال المهملة','53','0','0');
+INSERT INTO [t6064] VALUES (9,'حرف الراء المهملة','56','0','0');
+INSERT INTO [t6064] VALUES (10,'حرف الزاي المعجمة','60','0','0');
+INSERT INTO [t6064] VALUES (11,'حرف السين المهملة','63','0','0');
+INSERT INTO [t6064] VALUES (12,'حرف الشين المعجمة','66','0','0');
+INSERT INTO [t6064] VALUES (13,'حرف الصاد المهملة','68','0','0');
+INSERT INTO [t6064] VALUES (14,'حرف الضاد المعجمة','72','0','0');
+INSERT INTO [t6064] VALUES (15,'حرف الطاء المهملة','73','0','0');
+INSERT INTO [t6064] VALUES (16,'حرف الظاء المعجمة','74','0','0');
+INSERT INTO [t6064] VALUES (17,'حرف العين المهملة','74','0','0');
+INSERT INTO [t6064] VALUES (18,'حرف الغين المعجمة','78','0','0');
+INSERT INTO [t6064] VALUES (19,'حرف الفاء','79','0','0');
+INSERT INTO [t6064] VALUES (20,'حرف القاف','81','0','0');
+INSERT INTO [t6064] VALUES (21,'حرف الكاف','84','0','0');
+INSERT INTO [t6064] VALUES (22,'حرف اللام','95','0','0');
+INSERT INTO [t6064] VALUES (23,'حرف الميم','102','0','0');
+INSERT INTO [t6064] VALUES (24,'حرف النون','147','0','0');
+INSERT INTO [t6064] VALUES (25,'حرف الهاء','152','0','0');
+INSERT INTO [t6064] VALUES (26,'حرف الواو','152','0','0');
+INSERT INTO [t6064] VALUES (27,'حرف اللام ألف','154','0','0');
+INSERT INTO [t6064] VALUES (28,'حرف الياء الأخيرة','158','0','0');
+INSERT INTO [t6064] VALUES (29,'كلمات للأئمة حول بعض الأخبار الموضوعة','167','0','0');
+INSERT INTO [t6064] VALUES (30,'ومما لا أصل له من القبور','174','0','0');
+INSERT INTO [t6064] VALUES (31,'لما أراد أن يبني مسجد المدينة أتاه جبريل عليه السلام فقال ابنه سبعة أذرع طولا في السماء غير مزخرفة ولا','204','0','0');
