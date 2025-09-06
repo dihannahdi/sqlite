5161f217a457eec2c4ef24a662bd9583fc9fe235
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:57 +0700

    Upload SQL chunk: t436_chunk_0000.sql

---
+++ sql_chunks/t436_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t436
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t436" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t436
+INSERT INTO [t436] VALUES (1,'شكوى قوم معاذ كانت للتأخير والتطويل','2','0','0');
+INSERT INTO [t436] VALUES (2,'أحاديث النهي عن الإعادة والجواب عنها','4','0','0');
+INSERT INTO [t436] VALUES (3,'فصل (في ذهاب بعض أئمة الحنفية في الهند إلى أن معاذا إنما كان يصلي المغرب مع النبي - صلى الله عليه وسلم - ثم العشاء بقومه)','7','0','0');
+INSERT INTO [t436] VALUES (4,'فصل (الجواب عن أدلة القائلين بمنع أداء الفريضة خلف من قد أداها)','11','0','0');
