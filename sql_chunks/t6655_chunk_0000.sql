c03be091bafb1596fb45382ac6c1b2fbb6a3bcfa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:31 +0700

    Upload SQL chunk: t6655_chunk_0000.sql

---
+++ sql_chunks/t6655_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t6655
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t6655" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6655
+INSERT INTO [t6655] VALUES (1,'تاريخ طلب أبي عبد الله الحديث','3','0','0');
+INSERT INTO [t6655] VALUES (2,'ما ذكر في زهد أبي عبد الله رضي الله عنه','16','0','0');
+INSERT INTO [t6655] VALUES (3,'ما ذكر من ورود كتاب المأمون في المحنة من طرسوس وبأشخاص أبي رحمه الله ومحمد بن نوح رضي الله عنهما','21','0','0');
+INSERT INTO [t6655] VALUES (4,'ذكر محنة أبي إسحاق المعتصم لأبي رحمة الله','24','0','0');
+INSERT INTO [t6655] VALUES (5,'الجزء الثاني','59','0','0');
+INSERT INTO [t6655] VALUES (6,'بسم الله الرحمن الرحيم','74','0','0');
+INSERT INTO [t6655] VALUES (7,'بسم الله الرحمن الرحيم','75','0','0');
+INSERT INTO [t6655] VALUES (8,'بسم الله الرحمن الرحيم','83','0','0');
+INSERT INTO [t6655] VALUES (9,'بعد عودة من المعسكر','90','0','0');
