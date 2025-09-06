bd65735628275dc7b9e73a99432a42ed0125d772
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:16:36 +0700

    Upload SQL chunk: t3785_chunk_0000.sql

---
+++ sql_chunks/t3785_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t3785
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t3785" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3785
+INSERT INTO [t3785] VALUES (1,'ملاحظات حول كتاب عقيدة السلف والخلف','1','0','0');
+INSERT INTO [t3785] VALUES (2,'الجعد بن درهم','7','0','0');
+INSERT INTO [t3785] VALUES (3,'أبو محرز جهم بن صفوان','10','0','0');
+INSERT INTO [t3785] VALUES (4,'واصل بن عطاء البصري','14','0','0');
+INSERT INTO [t3785] VALUES (5,'عمرو بن عبيد بن باب','17','0','0');
