0f8772d11b729f03480819e9fbef99e624b01bd1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:56 +0700

    Upload SQL chunk: t9628_chunk_0000.sql

---
+++ sql_chunks/t9628_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t9628
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t9628" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9628
+INSERT INTO [t9628] VALUES (1,'حكيم بن حزام','1','0','0');
+INSERT INTO [t9628] VALUES (2,'عاصم بن عدي','7','0','0');
+INSERT INTO [t9628] VALUES (3,'حويطب بن عبد العزى بن أبي قيس بن عبد ود بن نصر بن مالك بن حسل','14','0','0');
+INSERT INTO [t9628] VALUES (4,'سعد بن إياس','18','0','0');
+INSERT INTO [t9628] VALUES (5,'مخرمة بن نوفل','19','0','0');
+INSERT INTO [t9628] VALUES (6,'سعيد بن يربوع','23','0','0');
+INSERT INTO [t9628] VALUES (7,'سعد بن جنادة','25','0','0');
+INSERT INTO [t9628] VALUES (8,'حسان بن ثابت','26','0','0');
+INSERT INTO [t9628] VALUES (9,'عبد خير','31','0','0');
+INSERT INTO [t9628] VALUES (10,'حمنن','34','0','0');
+INSERT INTO [t9628] VALUES (11,'نتجع جد ناجية','34','0','0');
+INSERT INTO [t9628] VALUES (12,'نافع أبو سليمان العبدي','35','0','0');
+INSERT INTO [t9628] VALUES (13,'اللجلاج','37','0','0');
+INSERT INTO [t9628] VALUES (14,'أبو شداد','38','0','0');
