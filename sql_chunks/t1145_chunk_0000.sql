ef8e9cdb5741473ff1678a18e6d0b73dedf51a50
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:35 +0700

    Upload SQL chunk: t1145_chunk_0000.sql

---
+++ sql_chunks/t1145_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t1145
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t1145" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1145
+INSERT INTO [t1145] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1145] VALUES (2,'تمهيد','5','0','0');
+INSERT INTO [t1145] VALUES (3,'المبحث الأول في تحريم الاعتداء على النفس','20','0','0');
+INSERT INTO [t1145] VALUES (4,'المطلب الأول في تحريم اعتداء الإنسان على نفسه','20','3','0');
+INSERT INTO [t1145] VALUES (5,'المطلب الثاني في تحريم اعتداء الإنسان على غيره','29','3','0');
+INSERT INTO [t1145] VALUES (6,'المبحث الثاني في تحريم إنكار المنكر إذا كان يستلزم ما هو أنكر منه','41','0','0');
+INSERT INTO [t1145] VALUES (7,'خاتمة','53','0','0');
