f533e7fe7fb524b1e14211f22afb08b6447b975a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:09 +0700

    Upload SQL chunk: t8797_chunk_0000.sql

---
+++ sql_chunks/t8797_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t8797
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t8797" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8797
+INSERT INTO [t8797] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8797] VALUES (2,'كيف تم تكوين هذه العصابة','2','0','0');
+INSERT INTO [t8797] VALUES (3,'مؤتمر بدشت ١٢٦٤ هـ','7','0','0');
+INSERT INTO [t8797] VALUES (4,'من البابية إلى البهائية','10','0','0');
+INSERT INTO [t8797] VALUES (5,'النحلة الجديدة','12','0','0');
+INSERT INTO [t8797] VALUES (6,'هلاك حسين المازندراني','14','0','0');
+INSERT INTO [t8797] VALUES (7,'تنازع الأخوة على الرئاسة','15','0','0');
+INSERT INTO [t8797] VALUES (8,'من أقوال الرسول الكريم','19','0','0');
