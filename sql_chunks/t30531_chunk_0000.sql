c5d7366fe3c1941af417cc33ba0302e1ca4a2c68
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:08 +0700

    Upload SQL chunk: t30531_chunk_0000.sql

---
+++ sql_chunks/t30531_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t30531
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t30531" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30531
+INSERT INTO [t30531] VALUES (1,'خطبة الكتاب','2','0','0');
+INSERT INTO [t30531] VALUES (2,'تعريف المتن','4','1','0');
+INSERT INTO [t30531] VALUES (3,'تعريف الحديث','5','1','0');
+INSERT INTO [t30531] VALUES (4,'تعريف السند والإسناد والخبر المتواتر','6','1','0');
+INSERT INTO [t30531] VALUES (5,'تعريف الآحاد','10','1','0');
+INSERT INTO [t30531] VALUES (6,'الباب الأول في أقسام الحديث وأنواعه','13','0','0');
+INSERT INTO [t30531] VALUES (7,'الفصل الأول في الصحيح','13','6','0');
+INSERT INTO [t30531] VALUES (8,'الفصل الثاني في الحسن','18','6','0');
+INSERT INTO [t30531] VALUES (9,'الفصل الثالث في الضعيف','23','6','0');
+INSERT INTO [t30531] VALUES (10,'المسند','25','9','0');
+INSERT INTO [t30531] VALUES (11,'المتصل','25','9','0');
+INSERT INTO [t30531] VALUES (12,'المرفوع','26','9','0');
+INSERT INTO [t30531] VALUES (13,'المعنعن','26','9','0');
+INSERT INTO [t30531] VALUES (14,'المعلق','27','9','0');
+INSERT INTO [t30531] VALUES (15,'الإفراد','27','9','0');
+INSERT INTO [t30531] VALUES (16,'المدرج','28','9','0');
+INSERT INTO [t30531] VALUES (17,'المشهور','29','9','0');
+INSERT INTO [t30531] VALUES (18,'الغريب','29','9','0');
+INSERT INTO [t30531] VALUES (19,'المصحف','31','9','0');
+INSERT INTO [t30531] VALUES (20,'المسلسل','31','9','0');
+INSERT INTO [t30531] VALUES (21,'الاعتبار','32','9','0');
+INSERT INTO [t30531] VALUES (22,'الموقوف','33','9','0');
+INSERT INTO [t30531] VALUES (23,'المقطوع','34','9','0');
+INSERT INTO [t30531] VALUES (24,'المرسل','34','9','0');
+INSERT INTO [t30531] VALUES (25,'المنقطع','35','9','0');
+INSERT INTO [t30531] VALUES (26,'المعضل','35','9','0');
+INSERT INTO [t30531] VALUES (27,'الشاذ والمنكر','36','9','0');
+INSERT INTO [t30531] VALUES (28,'المعلل','37','9','0');
+INSERT INTO [t30531] VALUES (29,'المدلس','38','9','0');
+INSERT INTO [t30531] VALUES (30,'المضطرب','40','9','0');
+INSERT INTO [t30531] VALUES (31,'المقلوب','40','9','0');
+INSERT INTO [t30531] VALUES (32,'الموضوع','41','9','0');
+INSERT INTO [t30531] VALUES (33,'الباب الثاني في معرفة أوصاف الرواة ومن تقبل روايته ومن لا تقبل','46','0','0');
+INSERT INTO [t30531] VALUES (34,'الفصل الأول في العدالة والضبط','47','33','0');
+INSERT INTO [t30531] VALUES (35,'الفصل الثاني ولا تقبل رواية من عرف بالتساهل','49','33','0');
+INSERT INTO [t30531] VALUES (36,'تذييل للفصل الثاني','50','35','0');
+INSERT INTO [t30531] VALUES (37,'الباب الثالث في تحمل الحديث','51','0','0');
+INSERT INTO [t30531] VALUES (38,'طرق تحمل الحديث','51','37','0');
+INSERT INTO [t30531] VALUES (39,'الباب الرابع في أسماء الرجال','57','0','0');
