28c706dc32b14d174d27f24b0e51e87ab4de57ac
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:42 +0700

    Upload SQL chunk: t30429_chunk_0000.sql

---
+++ sql_chunks/t30429_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30429
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30429" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30429
+INSERT INTO [t30429] VALUES (1,'الجزء من حديث أبي الحسن محمد بن أحمد بن رزقويه','1','0','0');
