606e93426683a23d5e6aaca530ddd239e37f5a52
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:51 +0700

    Upload SQL chunk: t99_chunk_0000.sql

---
+++ sql_chunks/t99_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t99
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t99" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t99
+INSERT INTO [t99] VALUES (1,'بسم الله الرحمن الرحيم:','1','0','0');
+INSERT INTO [t99] VALUES (2,'باب ما يروى رواية من المؤنث','1','0','0');
+INSERT INTO [t99] VALUES (3,'باب الألف','2','0','0');
+INSERT INTO [t99] VALUES (4,'باب الباء','3','0','0');
+INSERT INTO [t99] VALUES (5,'باب التاء','3','0','0');
+INSERT INTO [t99] VALUES (6,'باب الثاء','3','0','0');
+INSERT INTO [t99] VALUES (7,'باب الجيم','3','0','0');
+INSERT INTO [t99] VALUES (8,'باب الحاء','4','0','0');
+INSERT INTO [t99] VALUES (9,'باب الخاء','4','0','0');
+INSERT INTO [t99] VALUES (10,'باب الدال','4','0','0');
+INSERT INTO [t99] VALUES (11,'باب الذال','4','0','0');
+INSERT INTO [t99] VALUES (12,'باب الراء','4','0','0');
+INSERT INTO [t99] VALUES (13,'باب الزاي','5','0','0');
+INSERT INTO [t99] VALUES (14,'باب السين','5','0','0');
+INSERT INTO [t99] VALUES (15,'باب الشين','5','0','0');
+INSERT INTO [t99] VALUES (16,'باب الصاد','6','0','0');
+INSERT INTO [t99] VALUES (17,'باب الضاد','6','0','0');
+INSERT INTO [t99] VALUES (18,'باب الطاء','6','0','0');
+INSERT INTO [t99] VALUES (19,'باب الظاء','6','0','0');
+INSERT INTO [t99] VALUES (20,'باب العين','6','0','0');
+INSERT INTO [t99] VALUES (21,'باب الغين','6','0','0');
+INSERT INTO [t99] VALUES (22,'باب الفاء','6','0','0');
+INSERT INTO [t99] VALUES (23,'باب القاف','7','0','0');
+INSERT INTO [t99] VALUES (24,'باب الكاف','7','0','0');
+INSERT INTO [t99] VALUES (25,'باب اللام','7','0','0');
+INSERT INTO [t99] VALUES (26,'باب الميم','7','0','0');
+INSERT INTO [t99] VALUES (27,'باب النون','8','0','0');
+INSERT INTO [t99] VALUES (28,'باب الهاء','8','0','0');
+INSERT INTO [t99] VALUES (29,'باب الواو','8','0','0');
+INSERT INTO [t99] VALUES (30,'باب الياء','8','0','0');
