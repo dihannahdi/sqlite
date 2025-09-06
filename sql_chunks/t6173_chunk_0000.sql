ef39dd4d6d387bca2063856243453eb32218e185
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:41 +0700

    Upload SQL chunk: t6173_chunk_0000.sql

---
+++ sql_chunks/t6173_chunk_0000.sql
@@ -0,0 +1,69 @@
+-- Table: t6173
+-- Chunk: 1
+-- Rows: 62
+
+CREATE TABLE "t6173" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6173
+INSERT INTO [t6173] VALUES (1,'فصل في الوضوء والغسل','6','0','0');
+INSERT INTO [t6173] VALUES (2,'نواقض الوضوء','11','0','0');
+INSERT INTO [t6173] VALUES (3,'فصل في مسح الخف','13','0','0');
+INSERT INTO [t6173] VALUES (4,'فصل في التيمم','16','0','0');
+INSERT INTO [t6173] VALUES (5,'فصل في إزالة النجاسة','20','0','0');
+INSERT INTO [t6173] VALUES (6,'فصل في البئر','23','0','0');
+INSERT INTO [t6173] VALUES (7,'فصل في الاستنجاء','26','0','0');
+INSERT INTO [t6173] VALUES (8,'فصل في الأذان','28','0','0');
+INSERT INTO [t6173] VALUES (9,'فصل وشروط الصلاة ستة','32','0','0');
+INSERT INTO [t6173] VALUES (10,'الشرط الأول الوقت','35','0','0');
+INSERT INTO [t6173] VALUES (11,'الثاني الطهارة','39','0','0');
+INSERT INTO [t6173] VALUES (12,'الثالث ستر العورة','42','0','0');
+INSERT INTO [t6173] VALUES (13,'الرابع استقبال القبلة','44','0','0');
+INSERT INTO [t6173] VALUES (14,'الخامس النية','45','0','0');
+INSERT INTO [t6173] VALUES (15,'السادس تكبيرة الإحرام','46','0','0');
+INSERT INTO [t6173] VALUES (16,'فصل الأركان أولها القيام','48','0','0');
+INSERT INTO [t6173] VALUES (17,'الثاني القراءة','49','0','0');
+INSERT INTO [t6173] VALUES (18,'الثالث الركوع','52','0','0');
+INSERT INTO [t6173] VALUES (19,'الرابع السجود','53','0','0');
+INSERT INTO [t6173] VALUES (20,'الخامس الانتقال من ركن الى ركن','54','0','0');
+INSERT INTO [t6173] VALUES (21,'السادس القعدة الأخيرة','54','0','0');
+INSERT INTO [t6173] VALUES (22,'فصل في سنن الرواتب وغيرها','56','0','0');
+INSERT INTO [t6173] VALUES (23,'فصل في التراويح','59','0','0');
+INSERT INTO [t6173] VALUES (24,'فصل في الوتر','61','0','0');
+INSERT INTO [t6173] VALUES (25,'فصل','63','0','0');
+INSERT INTO [t6173] VALUES (26,'فصل في الجماعة','67','0','0');
+INSERT INTO [t6173] VALUES (27,'فصل في الجمعة','70','0','0');
+INSERT INTO [t6173] VALUES (28,'فصل في العيد','73','0','0');
+INSERT INTO [t6173] VALUES (29,'فصل في المسافر','76','0','0');
+INSERT INTO [t6173] VALUES (30,'فصل في المريض','80','0','0');
+INSERT INTO [t6173] VALUES (31,'فصل في الفائتة','82','0','0');
+INSERT INTO [t6173] VALUES (32,'فصل','83','0','0');
+INSERT INTO [t6173] VALUES (33,'فصل في السهو','86','0','0');
+INSERT INTO [t6173] VALUES (34,'فصل في سجدة التلاوة','89','0','0');
+INSERT INTO [t6173] VALUES (35,'فصل في الميت','92','0','0');
+INSERT INTO [t6173] VALUES (36,'فصل','118','0','0');
+INSERT INTO [t6173] VALUES (37,'فصل','124','0','0');
+INSERT INTO [t6173] VALUES (38,'فصل','135','0','0');
+INSERT INTO [t6173] VALUES (39,'فصل','142','0','0');
+INSERT INTO [t6173] VALUES (40,'فصل','144','0','0');
+INSERT INTO [t6173] VALUES (41,'فصل','149','0','0');
+INSERT INTO [t6173] VALUES (42,'فصل','152','0','0');
+INSERT INTO [t6173] VALUES (43,'فصل','160','0','0');
+INSERT INTO [t6173] VALUES (44,'فصل','167','0','0');
+INSERT INTO [t6173] VALUES (45,'فصل','170','0','0');
+INSERT INTO [t6173] VALUES (46,'فصل','173','0','0');
+INSERT INTO [t6173] VALUES (47,'فصل','182','0','0');
+INSERT INTO [t6173] VALUES (48,'فصل','190','0','0');
+INSERT INTO [t6173] VALUES (49,'فصل','193','0','0');
+INSERT INTO [t6173] VALUES (50,'فصل','205','0','0');
+INSERT INTO [t6173] VALUES (51,'فصل','212','0','0');
+INSERT INTO [t6173] VALUES (52,'فصل','227','0','0');
+INSERT INTO [t6173] VALUES (53,'فصل','231','0','0');
+INSERT INTO [t6173] VALUES (54,'فصل','234','0','0');
+INSERT INTO [t6173] VALUES (55,'فصل','237','0','0');
+INSERT INTO [t6173] VALUES (56,'فصل','239','0','0');
+INSERT INTO [t6173] VALUES (57,'فصل','240','0','0');
+INSERT INTO [t6173] VALUES (58,'فصل','241','0','0');
+INSERT INTO [t6173] VALUES (59,'فصل','242','0','0');
+INSERT INTO [t6173] VALUES (60,'فصل','247','0','0');
+INSERT INTO [t6173] VALUES (61,'فصل','253','0','0');
+INSERT INTO [t6173] VALUES (62,'فصل','258','0','0');
