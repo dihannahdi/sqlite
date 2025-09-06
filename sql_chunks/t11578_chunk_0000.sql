8a547fe4c61e4ecd2fdb495246f3cbaaffe4734c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:41 +0700

    Upload SQL chunk: t11578_chunk_0000.sql

---
+++ sql_chunks/t11578_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t11578
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t11578" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11578
+INSERT INTO [t11578] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11578] VALUES (2,'النص المحقق للكتاب','9','0','0');
+INSERT INTO [t11578] VALUES (3,'أدلة من قال بكراهة المجاورة','15','0','0');
+INSERT INTO [t11578] VALUES (4,'الأدلة على استحباب المجاورة بمكة','20','0','0');
+INSERT INTO [t11578] VALUES (5,'الأدلة على أن جنس المرابطة أفضل من جنس المجاورة','27','0','0');
+INSERT INTO [t11578] VALUES (6,'طريقتين للسلف فى الرباط','45','0','0');
+INSERT INTO [t11578] VALUES (7,'فضائل الرباط فى السنة','55','0','0');
+INSERT INTO [t11578] VALUES (8,'الأدلة على من يقصد البيت المقدس للتعريف فى وقت الحج','60','0','0');
+INSERT INTO [t11578] VALUES (9,'الوجه الأول','60','8','0');
+INSERT INTO [t11578] VALUES (10,'الوجه الثانى من الأدلة','68','8','0');
+INSERT INTO [t11578] VALUES (11,'الوجه الثالث من الأدلة','70','8','0');
