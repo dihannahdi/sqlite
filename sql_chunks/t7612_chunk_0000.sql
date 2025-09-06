7e1e382fab9a8a75c7a2a5b5fbb56aeba87e7546
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:37 +0700

    Upload SQL chunk: t7612_chunk_0000.sql

---
+++ sql_chunks/t7612_chunk_0000.sql
@@ -0,0 +1,42 @@
+-- Table: t7612
+-- Chunk: 1
+-- Rows: 35
+
+CREATE TABLE "t7612" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7612
+INSERT INTO [t7612] VALUES (1,'الخطبة الأولى لشيخ الإسلام محمد بن عبد الوهاب','1','0','0');
+INSERT INTO [t7612] VALUES (2,'خطبة له أيضا','2','0','0');
+INSERT INTO [t7612] VALUES (3,'خطبة له أيضا','4','0','0');
+INSERT INTO [t7612] VALUES (4,'خطبة له أيضا','6','0','0');
+INSERT INTO [t7612] VALUES (5,'خطبة له أيضا','7','0','0');
+INSERT INTO [t7612] VALUES (6,'خطبة له أيضا','8','0','0');
+INSERT INTO [t7612] VALUES (7,'خطبة له أيضا','10','0','0');
+INSERT INTO [t7612] VALUES (8,'خطبة له أيضا','11','0','0');
+INSERT INTO [t7612] VALUES (9,'خطبة له أيضا','13','0','0');
+INSERT INTO [t7612] VALUES (10,'خطبة له أيضا','14','0','0');
+INSERT INTO [t7612] VALUES (11,'خطبة له أيضا','15','0','0');
+INSERT INTO [t7612] VALUES (12,'خطبة له أيضا','17','0','0');
+INSERT INTO [t7612] VALUES (13,'خطبة له أيضا','18','0','0');
+INSERT INTO [t7612] VALUES (14,'خطبة عند دخول رمضان له أيضا','19','0','0');
+INSERT INTO [t7612] VALUES (15,'الخطبة التي بعدها في رمضان','21','0','0');
+INSERT INTO [t7612] VALUES (16,'خطبة بعدها لرمضان','24','0','0');
+INSERT INTO [t7612] VALUES (17,'خطبة بعدها آخر رمضان','26','0','0');
+INSERT INTO [t7612] VALUES (18,'خطبة عيد الفطر','28','0','0');
+INSERT INTO [t7612] VALUES (19,'خطبة في الحث على الحج','32','0','0');
+INSERT INTO [t7612] VALUES (20,'خطبة عيد النحر','34','0','0');
+INSERT INTO [t7612] VALUES (21,'خطبة في الأيام العشر من شهر ذي الحجة','39','0','0');
+INSERT INTO [t7612] VALUES (22,'خطبة أيضا','40','0','0');
+INSERT INTO [t7612] VALUES (23,'خطبة أيضا','42','0','0');
+INSERT INTO [t7612] VALUES (24,'خطبة أيضا','43','0','0');
+INSERT INTO [t7612] VALUES (25,'خطبة أيضا','44','0','0');
+INSERT INTO [t7612] VALUES (26,'خطبة أيضا','46','0','0');
+INSERT INTO [t7612] VALUES (27,'خطبة أيضا','47','0','0');
+INSERT INTO [t7612] VALUES (28,'خطبة أيضا','49','0','0');
+INSERT INTO [t7612] VALUES (29,'خطبة أيضا','50','0','0');
+INSERT INTO [t7612] VALUES (30,'خطبة أيضا','53','0','0');
+INSERT INTO [t7612] VALUES (31,'خطبة أيضا','54','0','0');
+INSERT INTO [t7612] VALUES (32,'خطبة أيضا','56','0','0');
+INSERT INTO [t7612] VALUES (33,'خطبة أيضا','57','0','0');
+INSERT INTO [t7612] VALUES (34,'خطبة أيضا','59','0','0');
+INSERT INTO [t7612] VALUES (35,'خطبة أيضا','60','0','0');
