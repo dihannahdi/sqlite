7f1abbcf6b1df12493f5b00c1cf4742606924b88
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:27 +0700

    Upload SQL chunk: t6285_chunk_0000.sql

---
+++ sql_chunks/t6285_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t6285
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t6285" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6285
+INSERT INTO [t6285] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t6285] VALUES (2,'مسألة','3','0','0');
+INSERT INTO [t6285] VALUES (3,'مسألة','6','0','0');
+INSERT INTO [t6285] VALUES (4,'مسألة','8','0','0');
+INSERT INTO [t6285] VALUES (5,'مسألة','9','0','0');
+INSERT INTO [t6285] VALUES (6,'مسألة','11','0','0');
+INSERT INTO [t6285] VALUES (7,'مسألة','13','0','0');
+INSERT INTO [t6285] VALUES (8,'مسألة','17','0','0');
+INSERT INTO [t6285] VALUES (9,'مسألة','20','0','0');
+INSERT INTO [t6285] VALUES (10,'مسألة','25','0','0');
