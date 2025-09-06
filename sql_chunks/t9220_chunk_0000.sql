3576915b86911c38fa85f279755bbec581262cd4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:51:40 +0700

    Upload SQL chunk: t9220_chunk_0000.sql

---
+++ sql_chunks/t9220_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9220
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9220" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9220
+INSERT INTO [t9220] VALUES (1,'الإكليل في المتشابه والتأويل','1','0','0');
+INSERT INTO [t9220] VALUES (2,'شيخ الإسلام ابن تيمية','1','0','0');
