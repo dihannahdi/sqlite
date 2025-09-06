39f63caf11e43be4bab31c850f99b09a9750eb93
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:43 +0700

    Upload SQL chunk: t1216_chunk_0000.sql

---
+++ sql_chunks/t1216_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t1216
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t1216" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1216
+INSERT INTO [t1216] VALUES (1,'ضعيف سنن الترمذي','1','0','0');
