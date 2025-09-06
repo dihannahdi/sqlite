a4dc75e9f7c70a48b23c9c7f1bd78baed0edd587
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:27 +0700

    Upload SQL chunk: t11360_chunk_0000.sql

---
+++ sql_chunks/t11360_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t11360
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t11360" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11360
+INSERT INTO [t11360] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11360] VALUES (2,'صحيح البخاري','8','0','0');
+INSERT INTO [t11360] VALUES (3,'صحيح مسلم','15','0','0');
+INSERT INTO [t11360] VALUES (4,'سنن أبي داود','18','0','0');
+INSERT INTO [t11360] VALUES (5,'سنن النسائي','22','0','0');
+INSERT INTO [t11360] VALUES (6,'سنن الترمذي','25','0','0');
+INSERT INTO [t11360] VALUES (7,'سنن ابن ماجه','27','0','0');
