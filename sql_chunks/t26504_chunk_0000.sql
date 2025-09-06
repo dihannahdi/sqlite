3302908668719f3b00ced44a8d83a45928207ea5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:56 +0700

    Upload SQL chunk: t26504_chunk_0000.sql

---
+++ sql_chunks/t26504_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26504
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26504" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26504
+INSERT INTO [t26504] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26504] VALUES (2,'أحاديث منتقاة من جزء ابن الفرات انتقاء العلائي','2','0','0');
