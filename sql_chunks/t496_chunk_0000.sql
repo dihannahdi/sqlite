d6b3e73715b906c54354c2700fbedfd767dfe4bf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:25 +0700

    Upload SQL chunk: t496_chunk_0000.sql

---
+++ sql_chunks/t496_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t496
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t496" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t496
+INSERT INTO [t496] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t496] VALUES (2,'ولد عبد مناف بن قصي','1','0','0');
+INSERT INTO [t496] VALUES (3,'الخلفاء','3','0','0');
+INSERT INTO [t496] VALUES (4,'من لم يسلم من ولد عبد المطلب','3','0','0');
+INSERT INTO [t496] VALUES (5,'من ولد أبي لهب بن عبد المطلب','4','0','0');
+INSERT INTO [t496] VALUES (6,'حلفاء بني هاشم','5','0','0');
+INSERT INTO [t496] VALUES (7,'من خلفاء بني عبد هاشم','6','6','0');
+INSERT INTO [t496] VALUES (8,'ولد عبد شمس بن عبد مناف','6','0','0');
+INSERT INTO [t496] VALUES (9,'ولد عبد العزى بن قصي','10','0','0');
+INSERT INTO [t496] VALUES (10,'من حلفاء بني عبد العزى بن قصي','11','0','0');
+INSERT INTO [t496] VALUES (11,'من بني عبد بن قصي','11','0','0');
+INSERT INTO [t496] VALUES (12,'ولد زهرة بن كلاب','12','0','0');
+INSERT INTO [t496] VALUES (13,'فمن بني زهرة بن كلاب:','12','12','0');
+INSERT INTO [t496] VALUES (14,'أخو كلاب بن مرة','12','0','0');
+INSERT INTO [t496] VALUES (15,'من بني مخزوم','14','0','0');
+INSERT INTO [t496] VALUES (16,'أخوا مرة بن كعب','15','0','0');
