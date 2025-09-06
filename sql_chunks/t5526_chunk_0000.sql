0c893dda78e3f33d88088609397777f4305c821f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:23:26 +0700

    Upload SQL chunk: t5526_chunk_0000.sql

---
+++ sql_chunks/t5526_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t5526
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t5526" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5526
+INSERT INTO [t5526] VALUES (1,'الأحرف السبعة لأبي عمرو الداني','1','0','0');
+INSERT INTO [t5526] VALUES (2,'معنى الأحرف السبعة','16','0','0');
+INSERT INTO [t5526] VALUES (3,'حكمة إنزال القرآن على سبعة أحرف','20','0','0');
+INSERT INTO [t5526] VALUES (4,'أوجه اختلاف الأحرف السبعة','22','0','0');
+INSERT INTO [t5526] VALUES (5,'أصل اختلاف القراءات','34','0','0');
+INSERT INTO [t5526] VALUES (6,'اختلاف المعاني تبعا لاختلاف الألفاظ في الأحرف السبعة','35','0','0');
+INSERT INTO [t5526] VALUES (7,'الأحرف السبعة لا تجمعها رواية ولا قراءة واحدة','40','0','0');
+INSERT INTO [t5526] VALUES (8,'الأحرف السبعة كلها صواب','41','0','0');
+INSERT INTO [t5526] VALUES (9,'خبر نزول القرآن على سبعة أبواب وبيان معناه','45','0','0');
+INSERT INTO [t5526] VALUES (10,'ما ينبغي اعتقاده في الأحرف والقراءات وتاريخ المصحف','48','0','0');
