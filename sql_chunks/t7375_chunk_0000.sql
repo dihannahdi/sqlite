7d68256937e00549ef0167a67cd7dea1826a79a1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:12 +0700

    Upload SQL chunk: t7375_chunk_0000.sql

---
+++ sql_chunks/t7375_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t7375
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t7375" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7375
+INSERT INTO [t7375] VALUES (1,'مقدمات','1','0','0');
+INSERT INTO [t7375] VALUES (2,'مقدمة الكتاب','1','1','0');
+INSERT INTO [t7375] VALUES (3,'المخطوطات','12','1','0');
+INSERT INTO [t7375] VALUES (4,'الشيخ الأول','13','0','0');
+INSERT INTO [t7375] VALUES (5,'الشيخ الثاني','29','0','0');
+INSERT INTO [t7375] VALUES (6,'الشيخ الثالث','34','0','0');
+INSERT INTO [t7375] VALUES (7,'الشيخ الرابع','37','0','0');
+INSERT INTO [t7375] VALUES (8,'الشيخ الخامس','49','0','0');
+INSERT INTO [t7375] VALUES (9,'الشيخ السادس','55','0','0');
+INSERT INTO [t7375] VALUES (10,'الشيخ السابع','61','0','0');
+INSERT INTO [t7375] VALUES (11,'الشيخ الثامن','67','0','0');
+INSERT INTO [t7375] VALUES (12,'الشيخ التاسع','75','0','0');
+INSERT INTO [t7375] VALUES (13,'الشيخ العاشر','85','0','0');
+INSERT INTO [t7375] VALUES (14,'الشيخ الحادي عشر','103','0','0');
+INSERT INTO [t7375] VALUES (15,'الشيخ الثاني عشر','109','0','0');
+INSERT INTO [t7375] VALUES (16,'الشيخ الثالث عشر','112','0','0');
+INSERT INTO [t7375] VALUES (17,'الشيخ الرابع عشر','117','0','0');
+INSERT INTO [t7375] VALUES (18,'الشيخ الخامس عشر','119','0','0');
+INSERT INTO [t7375] VALUES (19,'الشيخ السادس عشر','125','0','0');
+INSERT INTO [t7375] VALUES (20,'الشيخ السابع عشر','127','0','0');
+INSERT INTO [t7375] VALUES (21,'الشيخ الثامن عشر','131','0','0');
+INSERT INTO [t7375] VALUES (22,'الشيخ التاسع عشر','134','0','0');
+INSERT INTO [t7375] VALUES (23,'الشيخ العشرون','140','0','0');
+INSERT INTO [t7375] VALUES (24,'الشيخ الحادي والعشرون','142','0','0');
+INSERT INTO [t7375] VALUES (25,'الشيخ الثاني والعشرون','146','0','0');
+INSERT INTO [t7375] VALUES (26,'الشيخ الثالث والعشرون','149','0','0');
+INSERT INTO [t7375] VALUES (27,'الشيخ الرابع والعشرون','151','0','0');
+INSERT INTO [t7375] VALUES (28,'الشيخ الخامس والسادس والعشرون','153','0','0');
+INSERT INTO [t7375] VALUES (29,'الشيخ السابع والعشرون','154','0','0');
+INSERT INTO [t7375] VALUES (30,'الشيخ الثامن والعشرون','156','0','0');
+INSERT INTO [t7375] VALUES (31,'الفهارس','160','0','0');
+INSERT INTO [t7375] VALUES (32,'فهرس الأحاديث والآثار','160','31','0');
+INSERT INTO [t7375] VALUES (33,'فهرس شيوخ شهدة','164','31','0');
+INSERT INTO [t7375] VALUES (34,'قائمة مصادر التحقيق','166','31','0');
