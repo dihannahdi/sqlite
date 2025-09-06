17acc72d096645758654011983c37b60b1b38788
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:28 +0700

    Upload SQL chunk: t11760_chunk_0000.sql

---
+++ sql_chunks/t11760_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11760
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11760" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11760
+INSERT INTO [t11760] VALUES (1,'رسالة في بيان مالم يثبت فيه حديث صحيح من الأبواب للإمام الع','1','0','0');
