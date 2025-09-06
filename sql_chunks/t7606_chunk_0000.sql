e40ef7602915f2b193fc23ba1eeea4a747f70f6c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:30 +0700

    Upload SQL chunk: t7606_chunk_0000.sql

---
+++ sql_chunks/t7606_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t7606
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t7606" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7606
+INSERT INTO [t7606] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7606] VALUES (2,'القسم الثالث: زيارة المسجد النبوي وما فيها من أحكام وآداب','9','0','0');
+INSERT INTO [t7606] VALUES (3,'القسم الرابع: تنبيهات على أخطاء ومخالفات تحصل من بعض الحجاج','19','0','0');
+INSERT INTO [t7606] VALUES (4,'القسم الخامس: الفتاوى','100','0','0');
+INSERT INTO [t7606] VALUES (5,'القسم الأول: وصايا, نصائح, فوائد, آداب, منافع, توجيهات, تحذيرات','235','0','0');
+INSERT INTO [t7606] VALUES (6,'القسم الثاني: مناسك الحج والعمرة وصفتهما','260','0','0');
+INSERT INTO [t7606] VALUES (7,'مصادر ومراجع','287','0','0');
