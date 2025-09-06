99cd991f47f22b7a9f7b1ddd71d2832e25d7b621
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:14 +0700

    Upload SQL chunk: t13995_chunk_0000.sql

---
+++ sql_chunks/t13995_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t13995
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t13995" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13995
+INSERT INTO [t13995] VALUES (1,'مقدمة التحقيق','5','0','0');
+INSERT INTO [t13995] VALUES (2,'ترجمة المؤلف','7','1','0');
+INSERT INTO [t13995] VALUES (3,'تمهيد','14','0','0');
+INSERT INTO [t13995] VALUES (4,'المقدمة','15','0','0');
+INSERT INTO [t13995] VALUES (5,'واختلف في اشتقاق العاتكة من النساء على أقوال','18','4','0');
+INSERT INTO [t13995] VALUES (6,'[المهمة]','21','0','0');
+INSERT INTO [t13995] VALUES (7,'المطلب الأول في بيان الحديث الذي ورد فيه هذا اللفظ','21','6','0');
+INSERT INTO [t13995] VALUES (8,'المطلب الثاني في تأويل هذا الحديث وبيان نسب بني سليم','25','6','0');
+INSERT INTO [t13995] VALUES (9,'المطلب الثالث في تفصيل أسمائهن','28','6','0');
+INSERT INTO [t13995] VALUES (10,'خاتمة في بيان العواتك من الصحابيات','39','0','0');
