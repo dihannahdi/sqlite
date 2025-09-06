82d205d2a71ab675355ea9ad4fff275bf516f898
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:34 +0700

    Upload SQL chunk: t11026_chunk_0000.sql

---
+++ sql_chunks/t11026_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11026
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11026" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11026
+INSERT INTO [t11026] VALUES (1,'الفوائد المنتقاة المسماة بمشيخة الموصل لعبد الرحمان بن أبي الفهم ٦٥٥ - مخطوط','2','0','0');
