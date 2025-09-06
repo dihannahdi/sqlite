4533c3cb3c3034453313792e1a3d15c064fad059
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:34 +0700

    Upload SQL chunk: t13164_chunk_0000.sql

---
+++ sql_chunks/t13164_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t13164
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t13164" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13164
+INSERT INTO [t13164] VALUES (1,'من حديث يوسف بن الحكم الثقفي أبي الحجاج','1','0','0');
+INSERT INTO [t13164] VALUES (2,'ومن حديث الحجاج بن يوسف','4','0','0');
+INSERT INTO [t13164] VALUES (3,'من حديث أبي مسلم صاحب الدولة','10','0','0');
+INSERT INTO [t13164] VALUES (4,'ومن حديث نصر بن سيار أمير خراسان','14','0','0');
+INSERT INTO [t13164] VALUES (5,'ومن حديث أبي خالد','18','0','0');
+INSERT INTO [t13164] VALUES (6,'ومن حديث الفضل بن الربيع صاحب هارون الرشيد','21','0','0');
