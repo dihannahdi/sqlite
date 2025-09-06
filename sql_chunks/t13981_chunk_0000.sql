14695f12f7a757ecf43994bc4f04dbc0c20567f3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:13 +0700

    Upload SQL chunk: t13981_chunk_0000.sql

---
+++ sql_chunks/t13981_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t13981
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t13981" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13981
+INSERT INTO [t13981] VALUES (1,'باب الإيجاز','3','0','0');
+INSERT INTO [t13981] VALUES (2,'باب التشبيه','7','0','0');
+INSERT INTO [t13981] VALUES (3,'باب الاستعارة','12','0','0');
+INSERT INTO [t13981] VALUES (4,'باب التلاؤم','21','0','0');
+INSERT INTO [t13981] VALUES (5,'باب الفواصل','24','0','0');
+INSERT INTO [t13981] VALUES (6,'باب التجانس','26','0','0');
+INSERT INTO [t13981] VALUES (7,'باب التصريف','28','0','0');
+INSERT INTO [t13981] VALUES (8,'باب التضمين','29','0','0');
+INSERT INTO [t13981] VALUES (9,'باب المبالغة','31','0','0');
+INSERT INTO [t13981] VALUES (10,'باب البيان','33','0','0');
