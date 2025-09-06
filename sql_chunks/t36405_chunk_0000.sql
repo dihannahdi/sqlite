ab77870387d5933d99a587dfa4acd268ec1ccc23
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:01 +0700

    Upload SQL chunk: t36405_chunk_0000.sql

---
+++ sql_chunks/t36405_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t36405
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t36405" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36405
+INSERT INTO [t36405] VALUES (1,'إهداء','2','0','0');
+INSERT INTO [t36405] VALUES (2,'مقدمة','3','0','0');
+INSERT INTO [t36405] VALUES (3,'من ذاق عرف','5','0','0');
