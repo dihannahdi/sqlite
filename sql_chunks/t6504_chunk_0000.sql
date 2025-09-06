7ee9386f6c33c8e5930219cedec399bf460d876d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:11 +0700

    Upload SQL chunk: t6504_chunk_0000.sql

---
+++ sql_chunks/t6504_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t6504
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t6504" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6504
+INSERT INTO [t6504] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6504] VALUES (2,'فصل','24','0','0');
+INSERT INTO [t6504] VALUES (3,'فصل','36','0','0');
+INSERT INTO [t6504] VALUES (4,'فصل','39','0','0');
+INSERT INTO [t6504] VALUES (5,'فصل','44','0','0');
+INSERT INTO [t6504] VALUES (6,'باب القول في المذهب','47','0','0');
+INSERT INTO [t6504] VALUES (7,'فصل','50','0','0');
+INSERT INTO [t6504] VALUES (8,'فصل','61','0','0');
+INSERT INTO [t6504] VALUES (9,'فصل','64','0','0');
+INSERT INTO [t6504] VALUES (10,'فصل','68','0','0');
+INSERT INTO [t6504] VALUES (11,'فصل','81','0','0');
+INSERT INTO [t6504] VALUES (12,'فصل','85','0','0');
+INSERT INTO [t6504] VALUES (13,'تمييز الصدق من المين في محاورة الرجلين','94','0','0');
