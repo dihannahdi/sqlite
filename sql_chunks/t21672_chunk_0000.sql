e1a51943d3d338956860fbb948ae9161f0da4a7c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:07 +0700

    Upload SQL chunk: t21672_chunk_0000.sql

---
+++ sql_chunks/t21672_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21672
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21672" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21672
+INSERT INTO [t21672] VALUES (1,'من حديث اللحياني عن شيوخه - مخطوط','1','0','0');
