c471b464a6d10f88aded06fdf189c54559f2d9f8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:08 +0700

    Upload SQL chunk: t7287_chunk_0000.sql

---
+++ sql_chunks/t7287_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t7287
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t7287" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7287
+INSERT INTO [t7287] VALUES (1,'ما في أوله همزة','1','0','0');
+INSERT INTO [t7287] VALUES (2,'ما في أوله الباء','14','0','0');
+INSERT INTO [t7287] VALUES (3,'ما في اوله التاء','38','0','0');
+INSERT INTO [t7287] VALUES (4,'ما في أوله الثاء','45','0','0');
+INSERT INTO [t7287] VALUES (5,'ما في أوله الجيم','54','0','0');
+INSERT INTO [t7287] VALUES (6,'ما في اوله الحاء','69','0','0');
+INSERT INTO [t7287] VALUES (7,'ما في أوله الخاء','90','0','0');
+INSERT INTO [t7287] VALUES (8,'ما في أوله الدال','100','0','0');
+INSERT INTO [t7287] VALUES (9,'ما في أوله الراء','121','0','0');
+INSERT INTO [t7287] VALUES (10,'ما في أوله الزاء','135','0','0');
+INSERT INTO [t7287] VALUES (11,'ما في اوله السين','141','0','0');
+INSERT INTO [t7287] VALUES (12,'ما في أوله الشين','158','0','0');
+INSERT INTO [t7287] VALUES (13,'ما في أوله الصاد','170','0','0');
+INSERT INTO [t7287] VALUES (14,'ما في أوله الضاد','177','0','0');
+INSERT INTO [t7287] VALUES (15,'ما في اوله الطاء','182','0','0');
+INSERT INTO [t7287] VALUES (16,'ما في أوله الظاء','186','0','0');
+INSERT INTO [t7287] VALUES (17,'ما في أوله العين','188','0','0');
+INSERT INTO [t7287] VALUES (18,'ما في اوله الغين','215','0','0');
+INSERT INTO [t7287] VALUES (19,'ما في أوله الفاء','223','0','0');
+INSERT INTO [t7287] VALUES (20,'ما في أوله القاف','231','0','0');
+INSERT INTO [t7287] VALUES (21,'ما في أوله الكاف','248','0','0');
+INSERT INTO [t7287] VALUES (22,'ما في اوله اللام','257','0','0');
+INSERT INTO [t7287] VALUES (23,'ما في أوله الميم','263','0','0');
+INSERT INTO [t7287] VALUES (24,'ما في أوله نون','272','0','0');
+INSERT INTO [t7287] VALUES (25,'ما في أوله الواو','286','0','0');
+INSERT INTO [t7287] VALUES (26,'ما في أوله الهاء','296','0','0');
+INSERT INTO [t7287] VALUES (27,'ما في أوله الياء','302','0','0');
+INSERT INTO [t7287] VALUES (28,'أسامي الجبال الكبيرة الشاخصة بين ينبع وبين مكة','307','0','0');
+INSERT INTO [t7287] VALUES (29,'والجبال الصغار بينهما','307','0','0');
+INSERT INTO [t7287] VALUES (30,'الأودية بين مكة وينبع','308','0','0');
+INSERT INTO [t7287] VALUES (31,'المياه بين مكة وينبع','310','0','0');
