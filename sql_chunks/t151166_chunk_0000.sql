d5375fe3d7514c0cc85903f186def5a30db53bc1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:24:30 +0700

    Upload SQL chunk: t151166_chunk_0000.sql

---
+++ sql_chunks/t151166_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t151166
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t151166" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t151166
+INSERT INTO [t151166] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t151166] VALUES (2,'تفسير سورة الحجرات','2','0','0');
+INSERT INTO [t151166] VALUES (3,'تفسير سورة (ق)','65','0','0');
+INSERT INTO [t151166] VALUES (4,'تفسير سورة الذاريات','108','0','0');
+INSERT INTO [t151166] VALUES (5,'تفسير سورة الطور','165','0','0');
+INSERT INTO [t151166] VALUES (6,'تفسير سورة النجم','197','0','0');
+INSERT INTO [t151166] VALUES (7,'تفسير سورة القمر','252','0','0');
+INSERT INTO [t151166] VALUES (8,'تفسير سورة الرحمن','291','0','0');
+INSERT INTO [t151166] VALUES (9,'تفسير سورة الواقعة','317','0','0');
+INSERT INTO [t151166] VALUES (10,'تفسير سورة الحديد','347','0','0');
