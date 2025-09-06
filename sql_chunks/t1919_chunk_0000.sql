c1813c597043411ff8257c945d5ee347a5ef9828
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:10 +0700

    Upload SQL chunk: t1919_chunk_0000.sql

---
+++ sql_chunks/t1919_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t1919
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t1919" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1919
+INSERT INTO [t1919] VALUES (1,'واقع المرأة قبل الإسلام','1','0','0');
+INSERT INTO [t1919] VALUES (2,'المساواة الإسلامية بين الرجل والمرأة','2','0','0');
+INSERT INTO [t1919] VALUES (3,'المساواة في أصل النشأة','2','2','0');
+INSERT INTO [t1919] VALUES (4,'المساواة في المصير','3','2','0');
+INSERT INTO [t1919] VALUES (5,'المساواة في أهلية الخطاب الشرعي','4','2','0');
+INSERT INTO [t1919] VALUES (6,'المساواة أمام القضاء','6','2','0');
+INSERT INTO [t1919] VALUES (7,'المساواة داخل الأسرة بين الحقوق التي للمرأة والواجبات التي عليها','8','2','0');
+INSERT INTO [t1919] VALUES (8,'حقوق المرأة في الإسلام','9','0','0');
+INSERT INTO [t1919] VALUES (9,'حقوق المرأة الفكرية والدينية','9','8','0');
