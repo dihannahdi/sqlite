c17aa0a17787c21c2e5822ac9b0d06a0d5ca59dc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:23 +0700

    Upload SQL chunk: t6392_chunk_0000.sql

---
+++ sql_chunks/t6392_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t6392
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t6392" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6392
+INSERT INTO [t6392] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6392] VALUES (2,'مقدمات','3','0','0');
+INSERT INTO [t6392] VALUES (3,'أقسام المعلوم','12','0','0');
+INSERT INTO [t6392] VALUES (4,'حكم المستحيل','12','0','0');
+INSERT INTO [t6392] VALUES (5,'أحكام الممكن','13','0','0');
+INSERT INTO [t6392] VALUES (6,'الممكن موجود قطعا','14','0','0');
+INSERT INTO [t6392] VALUES (7,'وجود الممكن يقتضى بالضرورة وجود الواجب','15','0','0');
+INSERT INTO [t6392] VALUES (8,'أحكام الواجب','15','0','0');
+INSERT INTO [t6392] VALUES (9,'القدم والبقاء ونفى التركيب','15','0','0');
+INSERT INTO [t6392] VALUES (10,'الحياة','16','0','0');
+INSERT INTO [t6392] VALUES (11,'العلم','17','0','0');
+INSERT INTO [t6392] VALUES (12,'الإرادة','19','0','0');
+INSERT INTO [t6392] VALUES (13,'القدرة','20','0','0');
+INSERT INTO [t6392] VALUES (14,'الإختيار','20','0','0');
+INSERT INTO [t6392] VALUES (15,'الوحدة','21','0','0');
+INSERT INTO [t6392] VALUES (16,'الصفات السمعية التى يجب الإعتقاد بها','22','0','0');
+INSERT INTO [t6392] VALUES (17,'كلمات في الصفات إجمالا','23','0','0');
+INSERT INTO [t6392] VALUES (18,'أفعال الله جل شأنه','26','0','0');
+INSERT INTO [t6392] VALUES (19,'أفعال العباد','29','0','0');
+INSERT INTO [t6392] VALUES (20,'حسن الأفعال وقبحها','33','0','0');
+INSERT INTO [t6392] VALUES (21,'الرسالة العامة','42','0','0');
+INSERT INTO [t6392] VALUES (22,'حاجة البشر إلى الرسالة','44','0','0');
+INSERT INTO [t6392] VALUES (23,'امكان الوحى','55','0','0');
+INSERT INTO [t6392] VALUES (24,'وقوع الوحي والرسالة','59','0','0');
+INSERT INTO [t6392] VALUES (25,'وظيفة الرسل عليهم السلام','60','0','0');
+INSERT INTO [t6392] VALUES (26,'اعتراض مشهود','63','0','0');
+INSERT INTO [t6392] VALUES (27,'رسالة محمد صلى الله عليه وسلم','66','0','0');
+INSERT INTO [t6392] VALUES (28,'القرآن','74','0','0');
+INSERT INTO [t6392] VALUES (29,'الإسلام أو الدين الإسلامى','77','0','0');
+INSERT INTO [t6392] VALUES (30,'انتشار الإسلام','92','0','0');
+INSERT INTO [t6392] VALUES (31,'بسرعة لم يعهد لها نظير فى التاريخ','92','0','0');
+INSERT INTO [t6392] VALUES (32,'ايراد سهل الايراد','99','0','0');
+INSERT INTO [t6392] VALUES (33,'الجواب','101','0','0');
+INSERT INTO [t6392] VALUES (34,'التصديق بما جاء به محمد صلى الله عليه وسلم','102','0','0');
+INSERT INTO [t6392] VALUES (35,'خاتمة','105','0','0');
+INSERT INTO [t6392] VALUES (36,'بسم الله الرحمن الرحيم','105','0','0');
