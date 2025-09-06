de065debd6b49a042aad23bb51ffca726cb9be14
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:54 +0700

    Upload SQL chunk: t1389_chunk_0000.sql

---
+++ sql_chunks/t1389_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t1389
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t1389" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1389
+INSERT INTO [t1389] VALUES (1,'مقدمة الطبعة الأولى','2','0','0');
+INSERT INTO [t1389] VALUES (2,'سورة الضحى','9','0','0');
+INSERT INTO [t1389] VALUES (3,'سورة الشرح','42','0','0');
+INSERT INTO [t1389] VALUES (4,'سورة الزلزلة','63','0','0');
+INSERT INTO [t1389] VALUES (5,'سورة العاديات','86','0','0');
+INSERT INTO [t1389] VALUES (6,'سورة النازعات','105','0','0');
+INSERT INTO [t1389] VALUES (7,'سورة البلد','147','0','0');
+INSERT INTO [t1389] VALUES (8,'سورة التكاثر','175','0','0');
+INSERT INTO [t1389] VALUES (9,'مقدمة','199','0','0');
+INSERT INTO [t1389] VALUES (10,'التفسير البياني الجزء الثاني','199','0','0');
+INSERT INTO [t1389] VALUES (11,'سورة العلق','202','0','0');
+INSERT INTO [t1389] VALUES (12,'سورة القلم','226','0','0');
+INSERT INTO [t1389] VALUES (13,'سورة العصر','261','0','0');
+INSERT INTO [t1389] VALUES (14,'سورة الليل','281','0','0');
+INSERT INTO [t1389] VALUES (15,'سورة الفجر','307','0','0');
+INSERT INTO [t1389] VALUES (16,'سورة الهمزة','348','0','0');
+INSERT INTO [t1389] VALUES (17,'سورة الماعون','363','0','0');
