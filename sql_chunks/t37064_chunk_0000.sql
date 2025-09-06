21124ea41f81e60a9a26bfe48459b9b5c92465f8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:03 +0700

    Upload SQL chunk: t37064_chunk_0000.sql

---
+++ sql_chunks/t37064_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t37064
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t37064" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37064
+INSERT INTO [t37064] VALUES (1,'مبادىء علم الفقه','1','0','0');
+INSERT INTO [t37064] VALUES (2,'باب نواقض الوضوء','2','0','0');
+INSERT INTO [t37064] VALUES (3,'باب أقسام المياه وأحكامها','4','0','0');
+INSERT INTO [t37064] VALUES (4,'باب الوضوء وشروطه وفرائضه وسننه وفضائله ومكروهاته','5','0','0');
+INSERT INTO [t37064] VALUES (5,'باب الغسل','9','0','0');
+INSERT INTO [t37064] VALUES (6,'باب التيمم','11','0','0');
+INSERT INTO [t37064] VALUES (7,'باب الصلاة','14','0','0');
+INSERT INTO [t37064] VALUES (8,'باب سجود السهو','18','0','0');
+INSERT INTO [t37064] VALUES (9,'باب الإمامة','19','0','0');
+INSERT INTO [t37064] VALUES (10,'باب شروط الجمعة وآدابها وأعذارها','21','0','0');
+INSERT INTO [t37064] VALUES (11,'باب صلاة الجنازة','22','0','0');
+INSERT INTO [t37064] VALUES (12,'باب الصوم','24','0','0');
+INSERT INTO [t37064] VALUES (13,'أحكام عامة','26','12','0');
+INSERT INTO [t37064] VALUES (14,'باب الاعتكاف وشروطه ومبطلاته ومكروهاته','29','0','0');
+INSERT INTO [t37064] VALUES (15,'باب زكاة الفطر','30','0','0');
+INSERT INTO [t37064] VALUES (16,'باب الزكاة','32','0','0');
+INSERT INTO [t37064] VALUES (17,'باب زكاة العين','34','16','0');
+INSERT INTO [t37064] VALUES (18,'باب زكاة الماشية','34','17','0');
+INSERT INTO [t37064] VALUES (19,'مصرف الزكاة','35','17','0');
+INSERT INTO [t37064] VALUES (20,'باب الذكاة والأضحية','36','0','0');
+INSERT INTO [t37064] VALUES (21,'الأضحية','37','20','0');
+INSERT INTO [t37064] VALUES (22,'باب الحج','39','0','0');
+INSERT INTO [t37064] VALUES (23,'فصل محرمات الإحرام','45','22','0');
+INSERT INTO [t37064] VALUES (24,'فصل في مفسدات الحج','47','23','0');
