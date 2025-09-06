dc7219abb4c13419fdf90858c5861eba16f09752
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:00 +0700

    Upload SQL chunk: t4109_chunk_0000.sql

---
+++ sql_chunks/t4109_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t4109
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t4109" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4109
+INSERT INTO [t4109] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t4109] VALUES (2,'هرقل ينصح قومه','3','0','0');
+INSERT INTO [t4109] VALUES (3,'عدد الجيش','4','0','0');
+INSERT INTO [t4109] VALUES (4,'الخليفة يدرس أسباب تأخر النصر','5','0','0');
+INSERT INTO [t4109] VALUES (5,'خالد يتوجه إلى الشام','6','0','0');
+INSERT INTO [t4109] VALUES (6,'خالد يدعو إلى توحيد القيادة','7','0','0');
+INSERT INTO [t4109] VALUES (7,'خطة خالد لمواجهة الروم','9','0','0');
+INSERT INTO [t4109] VALUES (8,'خالد يأمر بالهجوم','11','0','0');
+INSERT INTO [t4109] VALUES (9,'خالد يستقبل بريد الخليفة','13','0','0');
+INSERT INTO [t4109] VALUES (10,'إسلام قائد من قواد الروم','14','0','0');
+INSERT INTO [t4109] VALUES (11,'موقف عكرمة من هجوم الروم','15','0','0');
+INSERT INTO [t4109] VALUES (12,'خالد يدير المعركة','16','0','0');
+INSERT INTO [t4109] VALUES (13,'دفع الشبهة','18','0','0');
+INSERT INTO [t4109] VALUES (14,'هزيمة الروم','20','0','0');
+INSERT INTO [t4109] VALUES (15,'وفاة الصديق','22','0','0');
