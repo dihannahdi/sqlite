156f2d12707380c5d35758684cbd0b329ccffed4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:58 +0700

    Upload SQL chunk: t7639_chunk_0000.sql

---
+++ sql_chunks/t7639_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t7639
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t7639" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7639
+INSERT INTO [t7639] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7639] VALUES (2,'الفصل الأول: في معنى النكاح لغة وشرعا','10','0','0');
+INSERT INTO [t7639] VALUES (3,'الفصل الثاني: في حكم النكاح','11','0','0');
+INSERT INTO [t7639] VALUES (4,'الفصل الثالث: في شروط النكاح','13','0','0');
+INSERT INTO [t7639] VALUES (5,'الفصل الرابع: في صفة المرأة التي ينبغي نكاحها','17','0','0');
+INSERT INTO [t7639] VALUES (6,'الفصل الخامس: في المحرمات بالنكاح','19','0','0');
+INSERT INTO [t7639] VALUES (7,'الفصل السادس: في العدد المباح في النكاح','26','0','0');
+INSERT INTO [t7639] VALUES (8,'الفصل السابع: في حكمة النكاح','29','0','0');
+INSERT INTO [t7639] VALUES (9,'الفصل الثامن: في الآثار المترتبة على النكاح','34','0','0');
+INSERT INTO [t7639] VALUES (10,'الفصل التاسع: في حكم الطلاق وما يراعى فيه','39','0','0');
+INSERT INTO [t7639] VALUES (11,'الفصل العاشر: فيما يترتب علي الطلاق','42','0','0');
