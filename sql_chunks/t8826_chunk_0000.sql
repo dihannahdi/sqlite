80203ffa5a5e2dfa80ec9abf796dc1638f630de8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:18 +0700

    Upload SQL chunk: t8826_chunk_0000.sql

---
+++ sql_chunks/t8826_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t8826
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t8826" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8826
+INSERT INTO [t8826] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8826] VALUES (2,'باب حمل الغنم ونتاجها','3','0','0');
+INSERT INTO [t8826] VALUES (3,'باب أسماء أولادها','8','0','0');
+INSERT INTO [t8826] VALUES (4,'باب أسماء أولادها','8','0','0');
+INSERT INTO [t8826] VALUES (5,'باب أسماء أولادها','8','0','0');
+INSERT INTO [t8826] VALUES (6,'باب من نعوتها في ولادتها','10','0','0');
+INSERT INTO [t8826] VALUES (7,'باب نعوتها من قبل أسنانها','14','0','0');
+INSERT INTO [t8826] VALUES (8,'باب نعوتها من قبل ألبانها','16','0','0');
+INSERT INTO [t8826] VALUES (9,'باب ضرع الشاة وعيوبه','21','0','0');
+INSERT INTO [t8826] VALUES (10,'باب نعوتها من قبل هزالها','26','0','0');
+INSERT INTO [t8826] VALUES (11,'باب نعوتها من قبل أمراضها وعيوبها','28','0','0');
+INSERT INTO [t8826] VALUES (12,'باب نعوتها من قبل أخلاقها','32','0','0');
+INSERT INTO [t8826] VALUES (13,'باب من عيوبها','34','0','0');
+INSERT INTO [t8826] VALUES (14,'باب نعوتها من قبل أسنانها','35','0','0');
+INSERT INTO [t8826] VALUES (15,'باب نعوتها من قبل قرونها','38','0','0');
+INSERT INTO [t8826] VALUES (16,'باب نعوتها من قبل علفها','39','0','0');
+INSERT INTO [t8826] VALUES (17,'باب نعوتها من قبل أخلاقها','40','0','0');
+INSERT INTO [t8826] VALUES (18,'باب نعوتها من قبل جماعاتها','41','0','0');
+INSERT INTO [t8826] VALUES (19,'باب من أسمائها','42','0','0');
