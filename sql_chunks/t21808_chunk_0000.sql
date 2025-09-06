27efc58997b706cebd358491d0364f3c27aa45ee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:34 +0700

    Upload SQL chunk: t21808_chunk_0000.sql

---
+++ sql_chunks/t21808_chunk_0000.sql
@@ -0,0 +1,58 @@
+-- Table: t21808
+-- Chunk: 1
+-- Rows: 51
+
+CREATE TABLE "t21808" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21808
+INSERT INTO [t21808] VALUES (1,'تقديم','1','0','0');
+INSERT INTO [t21808] VALUES (2,'الآية (١)','5','0','0');
+INSERT INTO [t21808] VALUES (3,'الآية (٢)','8','0','0');
+INSERT INTO [t21808] VALUES (4,'الآية (٣)','10','0','0');
+INSERT INTO [t21808] VALUES (5,'الآيتان (٤، ٥)','12','0','0');
+INSERT INTO [t21808] VALUES (6,'الآية (٦)','26','0','0');
+INSERT INTO [t21808] VALUES (7,'الآية (٧)','29','0','0');
+INSERT INTO [t21808] VALUES (8,'الآية (٨)','33','0','0');
+INSERT INTO [t21808] VALUES (9,'الآية (٩)','43','0','0');
+INSERT INTO [t21808] VALUES (10,'الآية (١٠)','57','0','0');
+INSERT INTO [t21808] VALUES (11,'الآية (١١)','62','0','0');
+INSERT INTO [t21808] VALUES (12,'الآية (١٢)','66','0','0');
+INSERT INTO [t21808] VALUES (13,'الآية (١٣)','68','0','0');
+INSERT INTO [t21808] VALUES (14,'الآيتان (١٤، ١٥)','71','0','0');
+INSERT INTO [t21808] VALUES (15,'الآية (١٦)','75','0','0');
+INSERT INTO [t21808] VALUES (16,'الآيتان (١٧، ١٨)','82','0','0');
+INSERT INTO [t21808] VALUES (17,'الآية (١٩)','89','0','0');
+INSERT INTO [t21808] VALUES (18,'الآية (٢٠)','94','0','0');
+INSERT INTO [t21808] VALUES (19,'الآية (٢١)','101','0','0');
+INSERT INTO [t21808] VALUES (20,'الآية (٢٢)','111','0','0');
+INSERT INTO [t21808] VALUES (21,'الآية (٢٣)','118','0','0');
+INSERT INTO [t21808] VALUES (22,'الآية (٢٤)','124','0','0');
+INSERT INTO [t21808] VALUES (23,'الآية (٢٥)','131','0','0');
+INSERT INTO [t21808] VALUES (24,'الآية (٢٦)','135','0','0');
+INSERT INTO [t21808] VALUES (25,'الآية (٢٧)','139','0','0');
+INSERT INTO [t21808] VALUES (26,'الآية (٢٨)','151','0','0');
+INSERT INTO [t21808] VALUES (27,'الآية (٢٩)','159','0','0');
+INSERT INTO [t21808] VALUES (28,'الآية (٣٠)','167','0','0');
+INSERT INTO [t21808] VALUES (29,'الآيتان (٣١، ٣٢)','180','0','0');
+INSERT INTO [t21808] VALUES (30,'الآية (٣٣)','192','0','0');
+INSERT INTO [t21808] VALUES (31,'الآيتان (٣٤, ٣٥)','199','0','0');
+INSERT INTO [t21808] VALUES (32,'الآية (٣٦)','206','0','0');
+INSERT INTO [t21808] VALUES (33,'الآية (٣٧)','213','0','0');
+INSERT INTO [t21808] VALUES (34,'الآية (٣٨)','217','0','0');
+INSERT INTO [t21808] VALUES (35,'الآية (٣٩)','226','0','0');
+INSERT INTO [t21808] VALUES (36,'الآية (٤٠)','236','0','0');
+INSERT INTO [t21808] VALUES (37,'الآية (٤١)','247','0','0');
+INSERT INTO [t21808] VALUES (38,'الآية (٤٢)','255','0','0');
+INSERT INTO [t21808] VALUES (39,'الآية (٤٣)','261','0','0');
+INSERT INTO [t21808] VALUES (40,'الآية (٤٤)','267','0','0');
+INSERT INTO [t21808] VALUES (41,'الآية (٤٥)','272','0','0');
+INSERT INTO [t21808] VALUES (42,'الآية (٤٦)','281','0','0');
+INSERT INTO [t21808] VALUES (43,'الآية (٤٧)','290','0','0');
+INSERT INTO [t21808] VALUES (44,'الآيتان (٤٨، ٤٩)','302','0','0');
+INSERT INTO [t21808] VALUES (45,'الآية (٥٠)','311','0','0');
+INSERT INTO [t21808] VALUES (46,'الآية (٥١)','320','0','0');
+INSERT INTO [t21808] VALUES (47,'الآية (٥٢)','322','0','0');
+INSERT INTO [t21808] VALUES (48,'الآية (٥٣)','325','0','0');
+INSERT INTO [t21808] VALUES (49,'الآية (٥٤)','329','0','0');
+INSERT INTO [t21808] VALUES (50,'الآيات (٥٥ - ٥٩)','331','0','0');
+INSERT INTO [t21808] VALUES (51,'الآية (٦٠)','333','0','0');
