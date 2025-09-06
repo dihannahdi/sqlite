b24dd8d5fbb031aec3744178fbd2506859df63b1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:40 +0700

    Upload SQL chunk: t677_chunk_0000.sql

---
+++ sql_chunks/t677_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t677
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t677" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t677
+INSERT INTO [t677] VALUES (1,'بسم الله الرحمن الرحيم','3','0','0');
+INSERT INTO [t677] VALUES (2,'- ربيعه','5','1','0');
+INSERT INTO [t677] VALUES (3,'- ربيعة','21','1','0');
+INSERT INTO [t677] VALUES (4,'- اليمن','21','1','0');
+INSERT INTO [t677] VALUES (5,'الإسلاميون إلى آخر أيام بني أمية','23','0','0');
+INSERT INTO [t677] VALUES (6,'- ربيعة','26','5','0');
+INSERT INTO [t677] VALUES (7,'المحدثون في أيام بني العباس','29','0','0');
+INSERT INTO [t677] VALUES (8,'- ربيعة','32','7','0');
+INSERT INTO [t677] VALUES (9,'- اليمن','33','7','0');
+INSERT INTO [t677] VALUES (10,'خاتمة الكتاب','35','0','0');
