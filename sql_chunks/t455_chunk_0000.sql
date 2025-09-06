b4100beef799565033a0f2e84e81d3cd552032c9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:33 +0700

    Upload SQL chunk: t455_chunk_0000.sql

---
+++ sql_chunks/t455_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t455
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t455" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t455
+INSERT INTO [t455] VALUES (1,'الإسلام والتسعير ونحوه','2','0','0');
+INSERT INTO [t455] VALUES (2,'حول أجور العقار','6','0','0');
