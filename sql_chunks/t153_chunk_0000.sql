8ccd5a1c46db62ded146172362d4aff7b4727976
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:08 +0700

    Upload SQL chunk: t153_chunk_0000.sql

---
+++ sql_chunks/t153_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t153
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t153" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t153
+INSERT INTO [t153] VALUES (1,'مقدمة التحقيق','1','0','0');
+INSERT INTO [t153] VALUES (2,'ترجمة عبد الله بن وهب','5','1','0');
+INSERT INTO [t153] VALUES (3,'اسمه ونسبه ومولده','5','2','0');
+INSERT INTO [t153] VALUES (4,'شيوخه','5','2','0');
+INSERT INTO [t153] VALUES (5,'تلامذته','6','2','0');
+INSERT INTO [t153] VALUES (6,'أقوال العلماء فيه جرحا وتعديلا','6','2','0');
+INSERT INTO [t153] VALUES (7,'مؤلفاته','9','2','0');
+INSERT INTO [t153] VALUES (8,'من ترجم لابن وهب','10','2','0');
+INSERT INTO [t153] VALUES (9,'١ - الإمام ابن قيم الجوزية','10','8','0');
+INSERT INTO [t153] VALUES (10,'٢ - الحافظ المزي','11','8','0');
+INSERT INTO [t153] VALUES (11,'٣ - الحافظ ابن حجر','11','8','0');
+INSERT INTO [t153] VALUES (12,'٤ - الحافظ العيني','12','8','0');
+INSERT INTO [t153] VALUES (13,'٥ - الإمام أبو الطيب التقي الفاسي','12','8','0');
+INSERT INTO [t153] VALUES (14,'٦ - الإمام ابن بطة','12','8','0');
+INSERT INTO [t153] VALUES (15,'دراسة إسناد النسخة','13','1','0');
+INSERT INTO [t153] VALUES (16,'وصف المخطوطات المعتمدة في التحقيق','16','1','0');
+INSERT INTO [t153] VALUES (17,'١ - نسخة الأسكوريال (الأصل)','16','16','0');
+INSERT INTO [t153] VALUES (18,'٢ - نسخة الظاهرية','17','16','0');
+INSERT INTO [t153] VALUES (19,'٣ - النسخة الهندية','17','16','0');
+INSERT INTO [t153] VALUES (20,'نماذج من المخطوطات المعتمدة','19','1','0');
+INSERT INTO [t153] VALUES (21,'الجزء الأول','26','0','0');
+INSERT INTO [t153] VALUES (22,'الجزء الثاني','51','0','0');
