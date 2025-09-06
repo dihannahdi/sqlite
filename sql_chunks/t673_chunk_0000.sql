08d0383f8bfd07cf15834963b1785704081b4040
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:26 +0700

    Upload SQL chunk: t673_chunk_0000.sql

---
+++ sql_chunks/t673_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t673
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t673" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t673
+INSERT INTO [t673] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t673] VALUES (2,'زيادات في النسخة الخطية','9','0','0');
