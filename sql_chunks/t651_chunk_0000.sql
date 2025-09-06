4e54c9d922dcca4156c63fc0ebe7daf1209bc64a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:19 +0700

    Upload SQL chunk: t651_chunk_0000.sql

---
+++ sql_chunks/t651_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t651
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t651" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t651
+INSERT INTO [t651] VALUES (1,'القسم المجموع','2','0','0');
+INSERT INTO [t651] VALUES (2,'" نقد الشعر "','2','1','0');
+INSERT INTO [t651] VALUES (3,'" أخبار الشعراء "','3','1','0');
+INSERT INTO [t651] VALUES (4,'" الراعي النميري "','3','1','0');
+INSERT INTO [t651] VALUES (5,'" ذو الرمة "','3','1','0');
+INSERT INTO [t651] VALUES (6,'" القطامي "','3','1','0');
+INSERT INTO [t651] VALUES (7,'" محمد بن مناذر "','3','1','0');
+INSERT INTO [t651] VALUES (8,'" العباس بن الأحنف "','4','1','0');
+INSERT INTO [t651] VALUES (9,'" العرجي "','4','1','0');
+INSERT INTO [t651] VALUES (10,'" عبد الله بن الزبير الأسدي "','4','1','0');
+INSERT INTO [t651] VALUES (11,'" قطري بن الفجاءة "','4','1','0');
+INSERT INTO [t651] VALUES (12,'" عبد الله بن محمد "','5','1','0');
+INSERT INTO [t651] VALUES (13,'" البحتري "','5','1','0');
+INSERT INTO [t651] VALUES (14,'" مروان بن أبي حفصة "','6','1','0');
+INSERT INTO [t651] VALUES (15,'" دعبل الخزاعي "','6','1','0');
+INSERT INTO [t651] VALUES (16,'" العتابي "','6','1','0');
+INSERT INTO [t651] VALUES (17,'" عمرو بن زعبل "','6','1','0');
+INSERT INTO [t651] VALUES (18,'" أبو العتاهية "','6','1','0');
+INSERT INTO [t651] VALUES (19,'" الجمل المصري "','7','1','0');
+INSERT INTO [t651] VALUES (20,'" إبراهيم بن المهدي "','7','1','0');
+INSERT INTO [t651] VALUES (21,'" الجماز "','8','1','0');
+INSERT INTO [t651] VALUES (22,'" أبو نؤاس "','9','1','0');
+INSERT INTO [t651] VALUES (23,'" عيسى بن الفاسي "','9','1','0');
+INSERT INTO [t651] VALUES (24,'" أبو النجم العجلي "','9','1','0');
+INSERT INTO [t651] VALUES (25,'" ابن الحصني "','10','1','0');
+INSERT INTO [t651] VALUES (26,'" ليلى الأخيلية "','10','1','0');
+INSERT INTO [t651] VALUES (27,'" امرأة "','10','1','0');
+INSERT INTO [t651] VALUES (28,'" جارية "','10','1','0');
+INSERT INTO [t651] VALUES (29,'" منصور النمري "','11','1','0');
+INSERT INTO [t651] VALUES (30,'" الملح والنوادر "','11','1','0');
