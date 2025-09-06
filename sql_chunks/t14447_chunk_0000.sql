1e37abe00dcc4e9d533d45b82b7623b9b5922b6d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:18:53 +0700

    Upload SQL chunk: t14447_chunk_0000.sql

---
+++ sql_chunks/t14447_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t14447
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t14447" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14447
+INSERT INTO [t14447] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t14447] VALUES (2,'فضل العلم والعلماء','2','0','0');
+INSERT INTO [t14447] VALUES (3,'فصل [طلب العلم من أحسن العبادات]','7','0','0');
+INSERT INTO [t14447] VALUES (4,'فصل [الفقه شعار الصحابة]','18','0','0');
+INSERT INTO [t14447] VALUES (5,'فصل [بعض الصحابة أشهر من بعض]','21','0','0');
+INSERT INTO [t14447] VALUES (6,'فصل [تفاوت المجتهدين]','25','0','0');
+INSERT INTO [t14447] VALUES (7,'فصل [أصحاب الشافعي]','36','0','0');
+INSERT INTO [t14447] VALUES (8,'فصل في صفة أهل العلم','39','0','0');
+INSERT INTO [t14447] VALUES (9,'فصل [إن الله لا يقبض العلم انتزاعا]','47','0','0');
+INSERT INTO [t14447] VALUES (10,'فصل [الكلام في المسائل التي لم تقع]','50','0','0');
+INSERT INTO [t14447] VALUES (11,'فصل [جمع كتاب شامل في الشريعة]','61','0','0');
+INSERT INTO [t14447] VALUES (12,'فصل [خلل التصنيف]','70','0','0');
+INSERT INTO [t14447] VALUES (13,'فصل [معنى التعصب]','81','0','0');
+INSERT INTO [t14447] VALUES (14,'فصل [مختصر المزني]','91','0','0');
+INSERT INTO [t14447] VALUES (15,'فصل [مذهب الشافعي]','100','0','0');
+INSERT INTO [t14447] VALUES (16,'فصل [كلام الغزالي في نصح أهل العلم]','105','0','0');
