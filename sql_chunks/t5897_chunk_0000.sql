b4346dff76795f6086bd72aefe0b356bd86bc37d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:17 +0700

    Upload SQL chunk: t5897_chunk_0000.sql

---
+++ sql_chunks/t5897_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t5897
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t5897" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5897
+INSERT INTO [t5897] VALUES (1,'سند أقوال سفيان الثوري','1','0','0');
+INSERT INTO [t5897] VALUES (2,'سند أقوال مالك بن أنس','1','0','0');
+INSERT INTO [t5897] VALUES (3,'سند أقوال بن المبارك','1','0','0');
+INSERT INTO [t5897] VALUES (4,'سند أقوال الشافعي','2','0','0');
+INSERT INTO [t5897] VALUES (5,'سند أقوال أحمد بن حنبل وإسحاق بن إبراهيم','2','0','0');
+INSERT INTO [t5897] VALUES (6,'مصادر ذكر العلل في الأحاديث والرجال','3','0','0');
+INSERT INTO [t5897] VALUES (7,'جواز الحكم على الرجال والأسانيد','4','0','0');
+INSERT INTO [t5897] VALUES (8,'التضعيف من جهة الحفظ','5','0','0');
+INSERT INTO [t5897] VALUES (9,'الرواية بالمعنى','6','0','0');
+INSERT INTO [t5897] VALUES (10,'تفاضل أهل العلم في الحفظ','7','0','0');
+INSERT INTO [t5897] VALUES (11,'القراءة على العالم','8','0','0');
+INSERT INTO [t5897] VALUES (12,'المناولة','9','0','0');
+INSERT INTO [t5897] VALUES (13,'الفرق بين أخبرنا وحدثنا','9','0','0');
+INSERT INTO [t5897] VALUES (14,'الإجازة','9','0','0');
+INSERT INTO [t5897] VALUES (15,'كتابة الحديث','10','0','0');
+INSERT INTO [t5897] VALUES (16,'الحديث المرسل','11','0','0');
+INSERT INTO [t5897] VALUES (17,'الاختلاف في تضعيف الرجال','12','0','0');
+INSERT INTO [t5897] VALUES (18,'معاني الاصطلاح للترمذي','13','0','0');
+INSERT INTO [t5897] VALUES (19,'غرابة الإسناد','14','0','0');
