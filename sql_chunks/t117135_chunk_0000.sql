63b07ecf6afa8c65580e5a06f7cb761445305d9c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:54 +0700

    Upload SQL chunk: t117135_chunk_0000.sql

---
+++ sql_chunks/t117135_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t117135
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t117135" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t117135
+INSERT INTO [t117135] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t117135] VALUES (2,'إن بعض الأئمة المتقدمين تعرض لتفسير بعض من المصطلحات الحديثية','3','0','0');
+INSERT INTO [t117135] VALUES (3,'وإطلاق الصيادلة على أهل الحديث تشنيع بهم','4','0','0');
+INSERT INTO [t117135] VALUES (4,'من أهم أسباب ظهور تواليف علم مصطلح الحديث هو الدفاع عن أهل الحديث','6','0','0');
+INSERT INTO [t117135] VALUES (5,'سرد تاريخي سريع لأهم مصنفات المصطلح','9','0','0');
+INSERT INTO [t117135] VALUES (6,'خلاصة','13','0','0');
+INSERT INTO [t117135] VALUES (7,'قائمة بأهم المصادر والمراجع','15','0','0');
