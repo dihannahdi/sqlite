949c8481cf122246c3054a396a618219e8a7ec39
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:39 +0700

    Upload SQL chunk: t1596_chunk_0000.sql

---
+++ sql_chunks/t1596_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t1596
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t1596" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1596
+INSERT INTO [t1596] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t1596] VALUES (2,'ملحق رقم (١)','39','0','0');
+INSERT INTO [t1596] VALUES (3,'تخريج الأحاديث الواردة فى وضع اليمين على الشمال','39','0','0');
+INSERT INTO [t1596] VALUES (4,'حديث سهل بن سعد','39','3','0');
+INSERT INTO [t1596] VALUES (5,'وحديث وائل بن حجر','39','3','0');
+INSERT INTO [t1596] VALUES (6,'وحديث الحارث بن غطيف بن الحارث','40','3','0');
+INSERT INTO [t1596] VALUES (7,'وحديث عبد الله بن مسعود','41','3','0');
+INSERT INTO [t1596] VALUES (8,'وحديث جابر','41','3','0');
+INSERT INTO [t1596] VALUES (9,'وحديث ابن عباس','42','3','0');
+INSERT INTO [t1596] VALUES (10,'وحديث أبى هريرة','43','3','0');
+INSERT INTO [t1596] VALUES (11,'وحديث عائشة','44','3','0');
+INSERT INTO [t1596] VALUES (12,'وحديث ابن عمر','44','3','0');
+INSERT INTO [t1596] VALUES (13,'وحديث شداد بن شرحبيل','45','3','0');
+INSERT INTO [t1596] VALUES (14,'وحديث يعلى بن مرة','46','3','0');
+INSERT INTO [t1596] VALUES (15,'وحديث أبى الدرداء','46','3','0');
+INSERT INTO [t1596] VALUES (16,'وحديث عبد الله بن جابر الباضى','47','3','0');
+INSERT INTO [t1596] VALUES (17,'وحديث أبى حميد الساعدى','47','3','0');
+INSERT INTO [t1596] VALUES (18,'رضى الله عنه','47','3','0');
+INSERT INTO [t1596] VALUES (19,'وحديث معاذ بن جبل','48','3','0');
+INSERT INTO [t1596] VALUES (20,'ملحق رقم (٢)','51','0','0');
+INSERT INTO [t1596] VALUES (21,'مذهب العلماء في الوضع والإرسال','51','0','0');
+INSERT INTO [t1596] VALUES (22,'وحديث سهل بن سعد','54','21','0');
