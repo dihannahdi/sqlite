5efce9a2edf506678d7bb88d66f58c8673d910f0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:43 +0700

    Upload SQL chunk: t5818_chunk_0000.sql

---
+++ sql_chunks/t5818_chunk_0000.sql
@@ -0,0 +1,62 @@
+-- Table: t5818
+-- Chunk: 1
+-- Rows: 55
+
+CREATE TABLE "t5818" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5818
+INSERT INTO [t5818] VALUES (1,'باب الألف','2','0','0');
+INSERT INTO [t5818] VALUES (2,'من اسمه إسماعيل','2','1','0');
+INSERT INTO [t5818] VALUES (3,'من اسمه أشعث','12','1','0');
+INSERT INTO [t5818] VALUES (4,'من اسمه إسرائيل','13','1','0');
+INSERT INTO [t5818] VALUES (5,'من اسمه أصبغ','14','1','0');
+INSERT INTO [t5818] VALUES (6,'من اسمه أسامة','14','1','0');
+INSERT INTO [t5818] VALUES (7,'من اسمه أبان','14','1','0');
+INSERT INTO [t5818] VALUES (8,'ذكر من اسمه الأخضر','16','1','0');
+INSERT INTO [t5818] VALUES (9,'من اسمه أزهر','16','1','0');
+INSERT INTO [t5818] VALUES (10,'من اسمه أسلم','17','1','0');
+INSERT INTO [t5818] VALUES (11,'من اسمه آدم','17','1','0');
+INSERT INTO [t5818] VALUES (12,'من اسمه أيمن','18','1','0');
+INSERT INTO [t5818] VALUES (13,'من اسمه أحمد','18','1','0');
+INSERT INTO [t5818] VALUES (14,'من اسمه إدريس','18','1','0');
+INSERT INTO [t5818] VALUES (15,'من اسمه أسباط','19','1','0');
+INSERT INTO [t5818] VALUES (16,'من اسمه أنس','19','1','0');
+INSERT INTO [t5818] VALUES (17,'من اسمه أسيد','19','1','0');
+INSERT INTO [t5818] VALUES (18,'من اسمه أسد','20','1','0');
+INSERT INTO [t5818] VALUES (19,'باب الباء','22','0','0');
+INSERT INTO [t5818] VALUES (20,'باب التاء','27','0','0');
+INSERT INTO [t5818] VALUES (21,'باب الثاء المثلثة','28','0','0');
+INSERT INTO [t5818] VALUES (22,'باب الجيم','30','0','0');
+INSERT INTO [t5818] VALUES (23,'باب الحاء','35','0','0');
+INSERT INTO [t5818] VALUES (24,'من اسمه الحسين','38','23','0');
+INSERT INTO [t5818] VALUES (25,'من اسمه الحكم','38','23','0');
+INSERT INTO [t5818] VALUES (26,'من اسمه حنظلة','39','23','0');
+INSERT INTO [t5818] VALUES (27,'من اسمه حبيب','40','23','0');
+INSERT INTO [t5818] VALUES (28,'من اسمه حبيب','41','23','0');
+INSERT INTO [t5818] VALUES (29,'من اسمه حصين','41','23','0');
+INSERT INTO [t5818] VALUES (30,'من اسمه حماد','42','23','0');
+INSERT INTO [t5818] VALUES (31,'من اسمه حجاج','43','23','0');
+INSERT INTO [t5818] VALUES (32,'من اسمه حوشب','46','23','0');
+INSERT INTO [t5818] VALUES (33,'باب الخاء','52','0','0');
+INSERT INTO [t5818] VALUES (34,'باب الدال','57','0','0');
+INSERT INTO [t5818] VALUES (35,'باب الذال','60','0','0');
+INSERT INTO [t5818] VALUES (36,'باب الراء','61','0','0');
+INSERT INTO [t5818] VALUES (37,'باب الزاي','66','0','0');
+INSERT INTO [t5818] VALUES (38,'باب السين','72','0','0');
+INSERT INTO [t5818] VALUES (39,'باب الشين','87','0','0');
+INSERT INTO [t5818] VALUES (40,'باب الصاد','92','0','0');
+INSERT INTO [t5818] VALUES (41,'باب الضاد','96','0','0');
+INSERT INTO [t5818] VALUES (42,'باب الطاء','97','0','0');
+INSERT INTO [t5818] VALUES (43,'باب الضاد','99','0','0');
+INSERT INTO [t5818] VALUES (44,'باب العين','100','0','0');
+INSERT INTO [t5818] VALUES (45,'ذكر من اسمه عثمان','114','44','0');
+INSERT INTO [t5818] VALUES (46,'باب الغين','159','0','0');
+INSERT INTO [t5818] VALUES (47,'باب الفاء','161','0','0');
+INSERT INTO [t5818] VALUES (48,'باب القاف','165','0','0');
+INSERT INTO [t5818] VALUES (49,'باب الكاف','170','0','0');
+INSERT INTO [t5818] VALUES (50,'باب اللام','172','0','0');
+INSERT INTO [t5818] VALUES (51,'باب الميم','173','0','0');
+INSERT INTO [t5818] VALUES (52,'من اسمه محرر','212','51','0');
+INSERT INTO [t5818] VALUES (53,'باب النون','216','0','0');
+INSERT INTO [t5818] VALUES (54,'باب الواو','221','0','0');
+INSERT INTO [t5818] VALUES (55,'باب الهاء','225','0','0');
