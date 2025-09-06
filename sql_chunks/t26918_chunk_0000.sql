e14e2417ca3e1f8342148ab4741a16bb1e56df64
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:16 +0700

    Upload SQL chunk: t26918_chunk_0000.sql

---
+++ sql_chunks/t26918_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t26918
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t26918" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26918
+INSERT INTO [t26918] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26918] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t26918] VALUES (3,'باب الألف','3','0','0');
+INSERT INTO [t26918] VALUES (4,'باب الباء','17','0','0');
+INSERT INTO [t26918] VALUES (5,'باب التاء','29','0','0');
+INSERT INTO [t26918] VALUES (6,'باب الثاء','31','0','0');
+INSERT INTO [t26918] VALUES (7,'باب الجيم','36','0','0');
+INSERT INTO [t26918] VALUES (8,'باب الحاء','51','0','0');
+INSERT INTO [t26918] VALUES (9,'باب الخاء','72','0','0');
+INSERT INTO [t26918] VALUES (10,'باب الدال','80','0','0');
+INSERT INTO [t26918] VALUES (11,'باب الذال','90','0','0');
+INSERT INTO [t26918] VALUES (12,'باب الراء','98','0','0');
+INSERT INTO [t26918] VALUES (13,'باب الزاي','107','0','0');
+INSERT INTO [t26918] VALUES (14,'باب السين','115','0','0');
+INSERT INTO [t26918] VALUES (15,'باب الشين','129','0','0');
+INSERT INTO [t26918] VALUES (16,'باب الصاد','142','0','0');
+INSERT INTO [t26918] VALUES (17,'باب الضاد','148','0','0');
+INSERT INTO [t26918] VALUES (18,'باب الطاء','152','0','0');
+INSERT INTO [t26918] VALUES (19,'باب الظاء','155','0','0');
+INSERT INTO [t26918] VALUES (20,'باب العين','157','0','0');
+INSERT INTO [t26918] VALUES (21,'باب الغين','179','0','0');
+INSERT INTO [t26918] VALUES (22,'باب الفاء','186','0','0');
+INSERT INTO [t26918] VALUES (23,'باب القاف','194','0','0');
+INSERT INTO [t26918] VALUES (24,'باب الكاف','206','0','0');
+INSERT INTO [t26918] VALUES (25,'باب اللام','213','0','0');
+INSERT INTO [t26918] VALUES (26,'باب الميم','220','0','0');
+INSERT INTO [t26918] VALUES (27,'باب النون','255','0','0');
+INSERT INTO [t26918] VALUES (28,'باب الواو','273','0','0');
+INSERT INTO [t26918] VALUES (29,'باب الهاء','282','0','0');
+INSERT INTO [t26918] VALUES (30,'باب الياء','290','0','0');
