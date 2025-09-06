0220d603ccdc945e40f89fde1f6cc5ec2570d949
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:58 +0700

    Upload SQL chunk: t11173_chunk_0000.sql

---
+++ sql_chunks/t11173_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11173
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11173" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11173
+INSERT INTO [t11173] VALUES (1,'خصائص التفكير الفقهي عند الشيخ محمد بن عبد الوهاب','1','0','0');
+INSERT INTO [t11173] VALUES (2,'مصادر ومراجع','36','0','0');
