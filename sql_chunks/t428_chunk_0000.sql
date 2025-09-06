6ee0aa2608ad25bf9c452b2062c1fd67aaa45f32
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:41 +0700

    Upload SQL chunk: t428_chunk_0000.sql

---
+++ sql_chunks/t428_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t428
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t428" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t428
+INSERT INTO [t428] VALUES (1,'سرد طرق الحديث','2','0','0');
+INSERT INTO [t428] VALUES (2,'تحرير الألفاظ المختلفة في طرق الحديث','5','0','0');
+INSERT INTO [t428] VALUES (3,'الموضع الأول في الاختلاف','5','2','0');
+INSERT INTO [t428] VALUES (4,'الموضع الثاني في الاختلاف','8','2','0');
+INSERT INTO [t428] VALUES (5,'مناقشة صاحب «فيض الباري» في تقدير لفظة في الحديث','10','0','0');
