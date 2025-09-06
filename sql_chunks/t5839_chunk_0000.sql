7a82da8eaf0f84464d5f5ee3979248f7def17400
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:53 +0700

    Upload SQL chunk: t5839_chunk_0000.sql

---
+++ sql_chunks/t5839_chunk_0000.sql
@@ -0,0 +1,62 @@
+-- Table: t5839
+-- Chunk: 1
+-- Rows: 55
+
+CREATE TABLE "t5839" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5839
+INSERT INTO [t5839] VALUES (1,'ذكر من حدث عنه بن أبي عروبة ولم يسمع منه','5','0','0');
+INSERT INTO [t5839] VALUES (2,'والكذابون المعروفون بوضع الحديث على رسول الله صلى الله عليه وسلم أربعة','6','0','0');
+INSERT INTO [t5839] VALUES (3,'تسمية فقهاء الأمصار من أصحاب رسول الله صلى الله عليه وسلم ومن بعدهم','9','0','0');
+INSERT INTO [t5839] VALUES (4,'ومن التابعين','9','0','0');
+INSERT INTO [t5839] VALUES (5,'ومن بعد التابعين','10','0','0');
+INSERT INTO [t5839] VALUES (6,'وبعد هؤلاء','10','0','0');
+INSERT INTO [t5839] VALUES (7,'وأصحاب مالك من أهل المدينة','10','0','0');
+INSERT INTO [t5839] VALUES (8,'ومن أهل مصر','10','0','0');
+INSERT INTO [t5839] VALUES (9,'وأصحاب عبد الله بن عباس من أهل مكة','10','0','0');
+INSERT INTO [t5839] VALUES (10,'وبعد هؤلاء','10','0','0');
+INSERT INTO [t5839] VALUES (11,'وبعد هؤلاء','10','0','0');
+INSERT INTO [t5839] VALUES (12,'وأصحاب الشافعي','11','0','0');
+INSERT INTO [t5839] VALUES (13,'والفقهاء من أهل الكوفة','11','0','0');
+INSERT INTO [t5839] VALUES (14,'ومن فقهاء التابعين','11','0','0');
+INSERT INTO [t5839] VALUES (15,'وبعد هؤلاء','11','0','0');
+INSERT INTO [t5839] VALUES (16,'وبعد هذين','11','0','0');
+INSERT INTO [t5839] VALUES (17,'وبعد هؤلاء','11','0','0');
+INSERT INTO [t5839] VALUES (18,'وبعد هؤلاء','11','0','0');
+INSERT INTO [t5839] VALUES (19,'وأصحاب الحسن بن حي','11','0','0');
+INSERT INTO [t5839] VALUES (20,'ومن فقهاء أهل البصرة','11','0','0');
+INSERT INTO [t5839] VALUES (21,'ومن التابعين','12','0','0');
+INSERT INTO [t5839] VALUES (22,'وبعد هؤلاء','12','0','0');
+INSERT INTO [t5839] VALUES (23,'وبعد هؤلاء','12','0','0');
+INSERT INTO [t5839] VALUES (24,'وبعد هؤلاء','12','0','0');
+INSERT INTO [t5839] VALUES (25,'وبعد هؤلاء','12','0','0');
+INSERT INTO [t5839] VALUES (26,'وبعد هؤلاء','12','0','0');
+INSERT INTO [t5839] VALUES (27,'ومن فقهاء الشام','12','0','0');
+INSERT INTO [t5839] VALUES (28,'وبعد هؤلاء','12','0','0');
+INSERT INTO [t5839] VALUES (29,'وبعده','12','0','0');
+INSERT INTO [t5839] VALUES (30,'ومن فقهاء أهل مصر','12','0','0');
+INSERT INTO [t5839] VALUES (31,'وبعد هؤلاء','12','0','0');
+INSERT INTO [t5839] VALUES (32,'وبعد هؤلاء','12','0','0');
+INSERT INTO [t5839] VALUES (33,'ومن فقهاء أهل خراسان','12','0','0');
+INSERT INTO [t5839] VALUES (34,'وبعد هؤلاء','12','0','0');
+INSERT INTO [t5839] VALUES (35,'وأثبت أصحاب الأوزاعي','12','0','0');
+INSERT INTO [t5839] VALUES (36,'وأثبت أصحاب أيوب','12','0','0');
+INSERT INTO [t5839] VALUES (37,'وأثبت أصحاب حماد بن سلمة','12','0','0');
+INSERT INTO [t5839] VALUES (38,'وأثبت أصحاب سعيد بن أبي عروبة','12','0','0');
+INSERT INTO [t5839] VALUES (39,'الطبقات','12','0','0');
+INSERT INTO [t5839] VALUES (40,'الطبقة الثانية','12','0','0');
+INSERT INTO [t5839] VALUES (41,'الطبقة الثالثة','12','0','0');
+INSERT INTO [t5839] VALUES (42,'الطبقة الرابعة','12','0','0');
+INSERT INTO [t5839] VALUES (43,'الطبقة الخامسة','12','0','0');
+INSERT INTO [t5839] VALUES (44,'الطبقة السادسة','12','0','0');
+INSERT INTO [t5839] VALUES (45,'الطبقة السابعة','12','0','0');
+INSERT INTO [t5839] VALUES (46,'الطبقة الثامنة','12','0','0');
+INSERT INTO [t5839] VALUES (47,'الطبقة التاسعة وهم الضعفاء','12','0','0');
+INSERT INTO [t5839] VALUES (48,'الطبقة المتروك حديثهم','12','0','0');
+INSERT INTO [t5839] VALUES (49,'طبقات أصحاب الأعمش','12','0','0');
+INSERT INTO [t5839] VALUES (50,'الطبقة الثانية','12','0','0');
+INSERT INTO [t5839] VALUES (51,'الطبقة الثالثة','12','0','0');
+INSERT INTO [t5839] VALUES (52,'الطبقة الرابعة','12','0','0');
+INSERT INTO [t5839] VALUES (53,'الطبقة الخامسة','12','0','0');
+INSERT INTO [t5839] VALUES (54,'الطبقة السادسة','12','0','0');
+INSERT INTO [t5839] VALUES (55,'الطبقة السابعة','12','0','0');
