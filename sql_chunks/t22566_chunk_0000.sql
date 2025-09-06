55d4ef5f71bafe89813d36f13ac145fb3e68ada2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:40 +0700

    Upload SQL chunk: t22566_chunk_0000.sql

---
+++ sql_chunks/t22566_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t22566
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t22566" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22566
+INSERT INTO [t22566] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t22566] VALUES (2,'فصل في أنواع النصيحة','9','0','0');
+INSERT INTO [t22566] VALUES (3,'فصل في كيفية النصيحة','11','0','0');
+INSERT INTO [t22566] VALUES (4,'فصل في العقوبة','15','0','0');
+INSERT INTO [t22566] VALUES (5,'فصل في التعيير','17','0','0');
+INSERT INTO [t22566] VALUES (6,'فصل في العلاج','21','0','0');
