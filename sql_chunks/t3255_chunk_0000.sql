86129de95240095b5369af4b935f6477ec7c26b4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:07 +0700

    Upload SQL chunk: t3255_chunk_0000.sql

---
+++ sql_chunks/t3255_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t3255
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t3255" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3255
+INSERT INTO [t3255] VALUES (1,'مقدمة الكتاب','1','0','0');
+INSERT INTO [t3255] VALUES (2,'بيان قرب الساعة','2','0','0');
+INSERT INTO [t3255] VALUES (3,'بيان الساعتين','3','0','0');
+INSERT INTO [t3255] VALUES (4,'ساعة القيامة','4','0','0');
