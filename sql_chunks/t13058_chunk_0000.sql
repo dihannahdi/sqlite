353758e074be18438383ded4ca1c69fac4875ae4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:09:48 +0700

    Upload SQL chunk: t13058_chunk_0000.sql

---
+++ sql_chunks/t13058_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t13058
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t13058" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13058
+INSERT INTO [t13058] VALUES (1,'أحاديث وآثار في الاحتضار','1','0','0');
+INSERT INTO [t13058] VALUES (2,'باب حسن الظن بالله عند نزول الموت','17','0','0');
+INSERT INTO [t13058] VALUES (3,'ذكر قول رسول الله صلى الله عليه وسلم عند الموت','30','0','0');
+INSERT INTO [t13058] VALUES (4,'مقالة الخلفاء عند حضور الموت','39','0','0');
+INSERT INTO [t13058] VALUES (5,'ما قالت الأمراء والملوك عند نزول الموت بها','101','0','0');
+INSERT INTO [t13058] VALUES (6,'باب تعزية النفس عند الاحتضار بالصبر والاحتساب','123','0','0');
+INSERT INTO [t13058] VALUES (7,'باب الجزع عند الموت مخافة سوء المرد','164','0','0');
+INSERT INTO [t13058] VALUES (8,'باب من تمثل بشعر عند الموت','252','0','0');
+INSERT INTO [t13058] VALUES (9,'باب في أقوال وأحوال شتى','275','0','0');
