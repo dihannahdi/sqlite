f7b6049fa35c968580041b54b3e22341922bc7bb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:18:58 +0700

    Upload SQL chunk: t1445_chunk_0000.sql

---
+++ sql_chunks/t1445_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t1445
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t1445" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1445
+INSERT INTO [t1445] VALUES (1,'تقديم','1','0','0');
+INSERT INTO [t1445] VALUES (2,'التعبير القرآني','2','0','0');
+INSERT INTO [t1445] VALUES (3,'البنية في التعبير القرآني','3','0','0');
+INSERT INTO [t1445] VALUES (4,'التقديم والتأخير','4','0','0');
+INSERT INTO [t1445] VALUES (5,'الذكر والحذف','5','0','0');
+INSERT INTO [t1445] VALUES (6,'التوكيد في القرآن الكريم','6','0','0');
+INSERT INTO [t1445] VALUES (7,'التشابه والاختلاف','7','0','0');
+INSERT INTO [t1445] VALUES (8,'فواصل الآي','8','0','0');
+INSERT INTO [t1445] VALUES (9,'السمة التعبيرية للسياق','9','0','0');
+INSERT INTO [t1445] VALUES (10,'الحشد الفني','10','0','0');
+INSERT INTO [t1445] VALUES (11,'الحشد الفني في القصص القرآني','11','0','0');
+INSERT INTO [t1445] VALUES (12,'قصة سيدنا آدم عليه السلام','12','0','0');
+INSERT INTO [t1445] VALUES (13,'قصة سيدنا آدم في سورتي البقرة والاعراف','12','12','0');
+INSERT INTO [t1445] VALUES (14,'قصة سيدنا آدم في سورتي الاعراف و (ص)','13','12','0');
+INSERT INTO [t1445] VALUES (15,'قصة سيدنا آدم في سورة الحجر و (ص)','14','12','0');
+INSERT INTO [t1445] VALUES (16,'قصة سيدنا موسى عليه السلام','15','0','0');
+INSERT INTO [t1445] VALUES (17,'في البقرة والأعراف','15','16','0');
+INSERT INTO [t1445] VALUES (18,'في الأعراف والشعراء','16','16','0');
+INSERT INTO [t1445] VALUES (19,'تفسير سورة التين','17','0','0');
+INSERT INTO [t1445] VALUES (20,'المراجع','18','0','0');
