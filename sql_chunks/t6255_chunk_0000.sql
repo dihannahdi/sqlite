fb47099f2edc62e65c764352662704c56f0e7f89
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:15 +0700

    Upload SQL chunk: t6255_chunk_0000.sql

---
+++ sql_chunks/t6255_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t6255
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t6255" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6255
+INSERT INTO [t6255] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t6255] VALUES (2,'مسائل الصلاة','2','0','0');
+INSERT INTO [t6255] VALUES (3,'مسائل الطهارة','35','0','0');
+INSERT INTO [t6255] VALUES (4,'مسائل الصيام','53','0','0');
+INSERT INTO [t6255] VALUES (5,'مسائل الزكاة','61','0','0');
+INSERT INTO [t6255] VALUES (6,'مسائل الذبائح والأضحية والأيمان','63','0','0');
+INSERT INTO [t6255] VALUES (7,'مسائل النكاح','66','0','0');
+INSERT INTO [t6255] VALUES (8,'مسائل البيوع','71','0','0');
+INSERT INTO [t6255] VALUES (9,'مسائل مختلفة','87','0','0');
