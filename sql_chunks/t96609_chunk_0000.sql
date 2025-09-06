6869cc2647d865dcc9f3d76837663fd90167509a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:12 +0700

    Upload SQL chunk: t96609_chunk_0000.sql

---
+++ sql_chunks/t96609_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t96609
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t96609" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96609
+INSERT INTO [t96609] VALUES (1,'ثمانون عاما على نشأة الجماعة (١٩٢٨ - ٢٠٠٨)','5','0','0');
+INSERT INTO [t96609] VALUES (2,'زهد وبساطة','17','0','0');
+INSERT INTO [t96609] VALUES (3,'ثقافة حسن البنا','20','0','0');
+INSERT INTO [t96609] VALUES (4,'السماحة والتقشف والتنظيم','22','0','0');
+INSERT INTO [t96609] VALUES (5,'نقد الحضارة الغربية','23','0','0');
+INSERT INTO [t96609] VALUES (6,'كان لا يخاف الموت','24','0','0');
+INSERT INTO [t96609] VALUES (7,'أغراه الإنجليز فرفض','27','0','0');
