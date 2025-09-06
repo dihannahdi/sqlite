3583c408606447a51268505f5072056a7354a627
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:28:39 +0700

    Upload SQL chunk: t17091_chunk_0000.sql

---
+++ sql_chunks/t17091_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t17091
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t17091" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17091
+INSERT INTO [t17091] VALUES (1,'بين يدي الكتاب','3','0','0');
+INSERT INTO [t17091] VALUES (2,'ترجمة العلامة الفقيه المحدث عمر بن علي بن يحيى الطحلاوي','7','0','0');
+INSERT INTO [t17091] VALUES (3,'اسمه ونسبه','7','2','0');
+INSERT INTO [t17091] VALUES (4,'شيوخه','7','2','0');
+INSERT INTO [t17091] VALUES (5,'فضله وعلمه','8','2','0');
+INSERT INTO [t17091] VALUES (6,'وفاته','9','2','0');
+INSERT INTO [t17091] VALUES (7,'ترجمة العلامة البحر المحقق محمد بن محمد بن أحمد السناوي','10','0','0');
+INSERT INTO [t17091] VALUES (8,'اسمه ونسبه','10','7','0');
+INSERT INTO [t17091] VALUES (9,'ولادته ونشأته','11','7','0');
+INSERT INTO [t17091] VALUES (10,'شيوخه ومقروءاته','12','7','0');
+INSERT INTO [t17091] VALUES (11,'مكانة العلامة الأمير العلمية','17','7','0');
+INSERT INTO [t17091] VALUES (12,'مسلكه التربوي والأخلاقي','19','7','0');
+INSERT INTO [t17091] VALUES (13,'تلامذته','21','7','0');
+INSERT INTO [t17091] VALUES (14,'مؤلفات العلامة الأمير','24','7','0');
+INSERT INTO [t17091] VALUES (15,'وفاته','27','7','0');
+INSERT INTO [t17091] VALUES (16,'نسبة الكتاب لمؤلفه، وتاريخ تأليفه','28','0','0');
+INSERT INTO [t17091] VALUES (17,'موضوع الكتاب','29','0','0');
+INSERT INTO [t17091] VALUES (18,'أهمية الكتاب ومزاياه','33','0','0');
+INSERT INTO [t17091] VALUES (19,'وصف النسخ الخطية','37','0','0');
+INSERT INTO [t17091] VALUES (20,'منهج العمل في الكتاب','39','0','0');
+INSERT INTO [t17091] VALUES (21,'غاية الإحكام في آداب الفهم والإفهام','49','0','0');
+INSERT INTO [t17091] VALUES (22,'[خطبة الكتاب]','50','21','0');
+INSERT INTO [t17091] VALUES (23,'المقدمة','54','21','0');
+INSERT INTO [t17091] VALUES (24,'باب الآداب ونحوها','56','21','0');
+INSERT INTO [t17091] VALUES (25,'ثمر الثمام','65','0','0');
+INSERT INTO [t17091] VALUES (26,'[خطبة الكتاب]','66','25','0');
+INSERT INTO [t17091] VALUES (27,'المقدمة','92','25','0');
+INSERT INTO [t17091] VALUES (28,'باب الآداب ونحوها','99','25','0');
+INSERT INTO [t17091] VALUES (29,'أهم مصادر ومراجع التحقيق','131','0','0');
