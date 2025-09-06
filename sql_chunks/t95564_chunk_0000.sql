1dc77e7218a6c31b14fac38fecf35ed540d86bb4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:25 +0700

    Upload SQL chunk: t95564_chunk_0000.sql

---
+++ sql_chunks/t95564_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t95564
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t95564" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95564
+INSERT INTO [t95564] VALUES (1,'مقدمة','5','0','0');
+INSERT INTO [t95564] VALUES (2,'كتاب الطهارة','9','0','0');
+INSERT INTO [t95564] VALUES (3,'كتاب الصلاة','40','0','0');
+INSERT INTO [t95564] VALUES (4,'كتاب الزكاة','91','0','0');
+INSERT INTO [t95564] VALUES (5,'كتاب الصوم','101','0','0');
+INSERT INTO [t95564] VALUES (6,'كتاب الحج','109','0','0');
+INSERT INTO [t95564] VALUES (7,'كتاب البيوع وغيرها من المعاملات','124','0','0');
+INSERT INTO [t95564] VALUES (8,'كتاب الفرائض والوصايا','151','0','0');
+INSERT INTO [t95564] VALUES (9,'كتاب النكاح','158','0','0');
+INSERT INTO [t95564] VALUES (10,'كتاب الجنايات','192','0','0');
+INSERT INTO [t95564] VALUES (11,'كتاب الحدود','204','0','0');
+INSERT INTO [t95564] VALUES (12,'كتاب الجهاد','225','0','0');
+INSERT INTO [t95564] VALUES (13,'كتاب الصيد والذبائح','235','0','0');
+INSERT INTO [t95564] VALUES (14,'كتاب السبق والرمي','248','0','0');
+INSERT INTO [t95564] VALUES (15,'كتاب الأيمان والنذور','251','0','0');
+INSERT INTO [t95564] VALUES (16,'كتاب الأقضية والشهادات','257','0','0');
+INSERT INTO [t95564] VALUES (17,'كتاب العتق','273','0','0');
+INSERT INTO [t95564] VALUES (18,'بيان بالمراجع المشار اليها وببعض المصطلحات','280','0','0');
+INSERT INTO [t95564] VALUES (19,'مراجع الحديث المعتمدة','280','18','0');
