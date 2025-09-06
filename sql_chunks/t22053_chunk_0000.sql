dbca33490db97d47841e39b787278cbd31589a66
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:03 +0700

    Upload SQL chunk: t22053_chunk_0000.sql

---
+++ sql_chunks/t22053_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t22053
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t22053" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22053
+INSERT INTO [t22053] VALUES (1,'جزء في فضل رجب','1','0','0');
