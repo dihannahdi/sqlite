862fa544b45aebd6b180f3742daaa31b5a5f91c8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:01:12 +0700

    Upload SQL chunk: t976_chunk_0000.sql

---
+++ sql_chunks/t976_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t976
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t976" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t976
+INSERT INTO [t976] VALUES (1,'ترجمة المؤلف','3','0','0');
+INSERT INTO [t976] VALUES (2,'بسم الله الرحمن الرحيم','28','0','0');
+INSERT INTO [t976] VALUES (3,'باب الطهارة','30','0','0');
+INSERT INTO [t976] VALUES (4,'باب الصلاة','36','0','0');
+INSERT INTO [t976] VALUES (5,'باب الجنائز','44','0','0');
+INSERT INTO [t976] VALUES (6,'باب الصيام','49','0','0');
+INSERT INTO [t976] VALUES (7,'باب الحج','53','0','0');
+INSERT INTO [t976] VALUES (8,'باب البيع','55','0','0');
+INSERT INTO [t976] VALUES (9,'باب النكاح','59','0','0');
+INSERT INTO [t976] VALUES (10,'باب الجنايات','61','0','0');
+INSERT INTO [t976] VALUES (11,'باب الأضحية','63','0','0');
+INSERT INTO [t976] VALUES (12,'باب الأطعمة','64','0','0');
+INSERT INTO [t976] VALUES (13,'باب الأدب','65','0','0');
