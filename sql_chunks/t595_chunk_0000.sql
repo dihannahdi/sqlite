0e4890ec1aea72665219e6da254fb5cc3f84cb48
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:52 +0700

    Upload SQL chunk: t595_chunk_0000.sql

---
+++ sql_chunks/t595_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t595
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t595" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t595
+INSERT INTO [t595] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t595] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
+INSERT INTO [t595] VALUES (3,'وهو حسبي','1','1','0');
+INSERT INTO [t595] VALUES (4,'الباب الأول','1','1','0');
+INSERT INTO [t595] VALUES (5,'لطائف الصحابة والتابعين','1','0','0');
+INSERT INTO [t595] VALUES (6,'الباب الثاني','2','5','0');
+INSERT INTO [t595] VALUES (7,'لطائف الملوك المتقدمين','2','0','0');
+INSERT INTO [t595] VALUES (8,'الباب الثالث','2','7','0');
+INSERT INTO [t595] VALUES (9,'لطائف ملوك الإسلام','2','0','0');
+INSERT INTO [t595] VALUES (10,'الباب الرابع','6','9','0');
+INSERT INTO [t595] VALUES (11,'لطائف الوزراء والكبراء','6','0','0');
+INSERT INTO [t595] VALUES (12,'الباب الخامس','9','11','0');
+INSERT INTO [t595] VALUES (13,'لطائف الأدباء والبلغاء','9','0','0');
+INSERT INTO [t595] VALUES (14,'الباب السادس','10','13','0');
+INSERT INTO [t595] VALUES (15,'لطائف القضاة والعلماء','10','0','0');
+INSERT INTO [t595] VALUES (16,'الباب السابع','11','15','0');
+INSERT INTO [t595] VALUES (17,'لطائف الفلاسفة والأطباء','11','0','0');
+INSERT INTO [t595] VALUES (18,'الباب الثامن','12','17','0');
+INSERT INTO [t595] VALUES (19,'لطائف الجواري والنساء','12','0','0');
+INSERT INTO [t595] VALUES (20,'فصل من كتاب الصاحب في ذكر الحنظلية الشاعرة','13','19','0');
+INSERT INTO [t595] VALUES (21,'الباب التاسع','14','19','0');
+INSERT INTO [t595] VALUES (22,'المغنين والمطربين','14','19','0');
+INSERT INTO [t595] VALUES (23,'فصل في لطائف غرر','14','19','0');
+INSERT INTO [t595] VALUES (24,'فصل في تفضيل المطرب على الطبيب','14','19','0');
+INSERT INTO [t595] VALUES (25,'الباب العاشر','14','19','0');
+INSERT INTO [t595] VALUES (26,'لطائف الفضلاء والظرفاء من كل فن وطبقة','14','0','0');
+INSERT INTO [t595] VALUES (27,'الباب الحادي عشر','16','26','0');
+INSERT INTO [t595] VALUES (28,'لطائف الشعراء نثرا','16','0','0');
+INSERT INTO [t595] VALUES (29,'فصل في نهاية الظرف','16','28','0');
+INSERT INTO [t595] VALUES (30,'أختم به هذا الكتاب','16','28','0');
+INSERT INTO [t595] VALUES (31,'الباب الثاني عشر','17','28','0');
+INSERT INTO [t595] VALUES (32,'لطائف الشعراء نظما','17','0','0');
+INSERT INTO [t595] VALUES (33,'فصل في الذين حصروا أشعارهم على الغزل','17','32','0');
+INSERT INTO [t595] VALUES (34,'وجعلوه في نهاية الظرف لا يشوبونه بغيره','17','32','0');
