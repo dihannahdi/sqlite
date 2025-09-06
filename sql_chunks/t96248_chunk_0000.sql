b95f845bdcf82abd0a329700115a0b0e97868dfd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:34 +0700

    Upload SQL chunk: t96248_chunk_0000.sql

---
+++ sql_chunks/t96248_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t96248
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t96248" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96248
+INSERT INTO [t96248] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t96248] VALUES (2,'حكم المعازف والموسيقى وآلات الملاهي','1','0','0');
+INSERT INTO [t96248] VALUES (3,'الحكمة من تحريم الموسيقى','2','0','0');
+INSERT INTO [t96248] VALUES (4,'حكم البرامج النافعة التي يصاحبها صوت العزف','2','0','0');
+INSERT INTO [t96248] VALUES (5,'حكم المؤثرات الصوتية','3','0','0');
+INSERT INTO [t96248] VALUES (6,'حكم تلحين الصوت البشري بدون الاستعانة بالكمبيوتر ونحوه','9','0','0');
+INSERT INTO [t96248] VALUES (7,'حكم تجويد القرآن وتحسين الصوت به','10','0','0');
+INSERT INTO [t96248] VALUES (8,'شبهات وإشكالات','10','0','0');
+INSERT INTO [t96248] VALUES (9,'الإشكال الأول','10','8','0');
+INSERT INTO [t96248] VALUES (10,'الإشكال الثاني','12','8','0');
+INSERT INTO [t96248] VALUES (11,'مناقشة العلماء المبيحين للغناء المصحوب بالموسيقى والمعازف','13','0','0');
+INSERT INTO [t96248] VALUES (12,'خلاصة حكم الغناء','16','0','0');
