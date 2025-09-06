2b646569f7c1535aea44ab13f03ea70d5db8c45e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:24 +0700

    Upload SQL chunk: t26430_chunk_0000.sql

---
+++ sql_chunks/t26430_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t26430
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t26430" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26430
+INSERT INTO [t26430] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26430] VALUES (2,'صفة النبي صلى الله عليه وسلم وصفة أخلاقه وسيرته وأدبه وخفض جناحه','2','0','0');
+INSERT INTO [t26430] VALUES (3,'حديث هند بن أبي هالة','2','2','0');
+INSERT INTO [t26430] VALUES (4,'حديث أبي هريرة وعلي','10','2','0');
+INSERT INTO [t26430] VALUES (5,'حديث أم معبد في صفة النبي صلى الله عليه وسلم','12','2','0');
