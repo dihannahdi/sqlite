22cad243aa2afd060d37bb1591ce86a8361e9dc4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:55 +0700

    Upload SQL chunk: t26641_chunk_0000.sql

---
+++ sql_chunks/t26641_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t26641
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t26641" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26641
+INSERT INTO [t26641] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26641] VALUES (2,'جزء فيه خمسة أحاديث عن الأئمة الخمسة','2','0','0');
+INSERT INTO [t26641] VALUES (3,'الحديث الأول','7','0','0');
+INSERT INTO [t26641] VALUES (4,'الحديث الثاني','14','0','0');
+INSERT INTO [t26641] VALUES (5,'الحديث الثالث','20','0','0');
+INSERT INTO [t26641] VALUES (6,'الحديث الرابع','25','0','0');
+INSERT INTO [t26641] VALUES (7,'الحديث الخامس','32','0','0');
