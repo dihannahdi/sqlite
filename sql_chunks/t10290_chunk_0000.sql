388e1ac759839cc7d8a80be749083725dc8be539
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:49 +0700

    Upload SQL chunk: t10290_chunk_0000.sql

---
+++ sql_chunks/t10290_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t10290
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t10290" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10290
+INSERT INTO [t10290] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10290] VALUES (2,'مرحلة ماقبل الولادة','2','0','0');
+INSERT INTO [t10290] VALUES (3,'الطفولة الأولى','6','0','0');
+INSERT INTO [t10290] VALUES (4,'الطفولة الثانية','10','0','0');
+INSERT INTO [t10290] VALUES (5,'مرحلة البلوغ','13','0','0');
+INSERT INTO [t10290] VALUES (6,'التربية الجنسية','16','0','0');
+INSERT INTO [t10290] VALUES (7,'مرحلة الرشد','24','0','0');
