2a5b19225e2134c4df378f054e84ad85efa2f442
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:53 +0700

    Upload SQL chunk: t26338_chunk_0000.sql

---
+++ sql_chunks/t26338_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t26338
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t26338" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26338
+INSERT INTO [t26338] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t26338] VALUES (2,'ذكر أخبار أبي زيد','33','0','0');
+INSERT INTO [t26338] VALUES (3,'ذكر أخبار الأصمعي','37','0','0');
+INSERT INTO [t26338] VALUES (4,'ذكر أخبار أبي عبيدة','44','0','0');
+INSERT INTO [t26338] VALUES (5,'أخبار أبي عمر الجرمي','47','0','0');
+INSERT INTO [t26338] VALUES (6,'أخبار أبي عثمان المازني','49','0','0');
+INSERT INTO [t26338] VALUES (7,'أخبار التوزي','57','0','0');
+INSERT INTO [t26338] VALUES (8,'أخبار الزيادي','59','0','0');
+INSERT INTO [t26338] VALUES (9,'أخبار الرياشي','60','0','0');
+INSERT INTO [t26338] VALUES (10,'أخبار أبي حاتم السجستاني','62','0','0');
+INSERT INTO [t26338] VALUES (11,'أخبار أبي العباس محمد بن يزيد الأزدي','64','0','0');
