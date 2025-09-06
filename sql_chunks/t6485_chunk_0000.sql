3b6cb113fddf2e2ea7dab3fa12f2db8d738c04ec
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:01 +0700

    Upload SQL chunk: t6485_chunk_0000.sql

---
+++ sql_chunks/t6485_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t6485
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t6485" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6485
+INSERT INTO [t6485] VALUES (1,'بسم الله الرحمن الرحيم رب أنعمت فزد','1','0','0');
+INSERT INTO [t6485] VALUES (2,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t6485] VALUES (3,'فصل','49','0','0');
+INSERT INTO [t6485] VALUES (4,'فصل','85','0','0');
+INSERT INTO [t6485] VALUES (5,'فصل فيه فوائد','134','0','0');
+INSERT INTO [t6485] VALUES (6,'فصل','256','0','0');
+INSERT INTO [t6485] VALUES (7,'فصل','258','0','0');
+INSERT INTO [t6485] VALUES (8,'في نبذ من أخبار من رأى الموتى في منامه وسألهم عن حالهم فأخبروه','258','0','0');
+INSERT INTO [t6485] VALUES (9,'فصل','295','0','0');
+INSERT INTO [t6485] VALUES (10,'خاتمة','300','0','0');
+INSERT INTO [t6485] VALUES (11,'في فوائد تتعلق بالروح','300','0','0');
