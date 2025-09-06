19ff2c87d62b15568b2b8cc0c6ce762e369053e6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:00 +0700

    Upload SQL chunk: t95672_chunk_0000.sql

---
+++ sql_chunks/t95672_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t95672
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t95672" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95672
+INSERT INTO [t95672] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95672] VALUES (2,'من الثمار','2','0','0');
+INSERT INTO [t95672] VALUES (3,'الثمرة الأولى','3','2','0');
+INSERT INTO [t95672] VALUES (4,'الثمرة الثانية','3','2','0');
+INSERT INTO [t95672] VALUES (5,'الثمرة الثالثة','4','2','0');
+INSERT INTO [t95672] VALUES (6,'الثمرة الرابعة','4','2','0');
+INSERT INTO [t95672] VALUES (7,'الثمرة الخامسة','4','2','0');
+INSERT INTO [t95672] VALUES (8,'الثمرة السادسة','5','2','0');
+INSERT INTO [t95672] VALUES (9,'الثمرة السابعة','6','2','0');
+INSERT INTO [t95672] VALUES (10,'الثمرة الثامنة','7','2','0');
+INSERT INTO [t95672] VALUES (11,'الثمرة التاسعة','7','2','0');
+INSERT INTO [t95672] VALUES (12,'الثمرة العاشرة','8','2','0');
+INSERT INTO [t95672] VALUES (13,'الثمرة الحادية عشرة','9','2','0');
+INSERT INTO [t95672] VALUES (14,'الثمرة الثانية عشرة','10','2','0');
+INSERT INTO [t95672] VALUES (15,'الثمرة الثالثة عشر','10','2','0');
+INSERT INTO [t95672] VALUES (16,'الثمرة الرابعة عشر','10','2','0');
+INSERT INTO [t95672] VALUES (17,'الثمرة الخامسة عشر','11','2','0');
+INSERT INTO [t95672] VALUES (18,'الثمرة السادسة عشر','12','2','0');
+INSERT INTO [t95672] VALUES (19,'الثمرة السابعة عشر','12','2','0');
+INSERT INTO [t95672] VALUES (20,'الثمرة الثامنة عشر','12','2','0');
+INSERT INTO [t95672] VALUES (21,'الثمرة التاسعة عشر','13','2','0');
+INSERT INTO [t95672] VALUES (22,'الثمرة العشرون','13','2','0');
+INSERT INTO [t95672] VALUES (23,'الثمرة الحادية والعشرون','14','2','0');
+INSERT INTO [t95672] VALUES (24,'الثمرة الثانية والعشرون','15','2','0');
+INSERT INTO [t95672] VALUES (25,'الثمرة الثالثة والعشرون','15','2','0');
+INSERT INTO [t95672] VALUES (26,'الثمرة الرابعة والعشرون','16','2','0');
+INSERT INTO [t95672] VALUES (27,'الثمرة الخامسة والعشرون','17','2','0');
+INSERT INTO [t95672] VALUES (28,'الثمرة السادسة والعشرون','17','2','0');
+INSERT INTO [t95672] VALUES (29,'الثمرة السابعة والعشرون','17','2','0');
+INSERT INTO [t95672] VALUES (30,'الثمرة الثامنة والعشرون','18','2','0');
+INSERT INTO [t95672] VALUES (31,'الثمرة التاسعة والعشرون','18','2','0');
+INSERT INTO [t95672] VALUES (32,'الثمرة الثلاثون','19','2','0');
+INSERT INTO [t95672] VALUES (33,'وماذا بعد الثمرات','20','0','0');
