e5ff615474b9789bfb30877d6ea825523a85180d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:05 +0700

    Upload SQL chunk: t8548_chunk_0000.sql

---
+++ sql_chunks/t8548_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8548
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8548" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8548
+INSERT INTO [t8548] VALUES (1,'المرأة في حياة إمام الدعوة الشيخ محمد بن عبد الوهاب','1','0','0');
+INSERT INTO [t8548] VALUES (2,'تأليف: فضيلة الشيخ: حمد الجاسر','1','0','0');
