08f4c4d246325341f8a124f4b26b7de28c8dfa15
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:36 +0700

    Upload SQL chunk: t10802_chunk_0000.sql

---
+++ sql_chunks/t10802_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t10802
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t10802" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10802
+INSERT INTO [t10802] VALUES (1,'الإمام الدارقطني','1','0','0');
+INSERT INTO [t10802] VALUES (2,'كتاب الضعفاء:','2','0','0');
+INSERT INTO [t10802] VALUES (3,'جزء فيه الضعفاء والمتروكون:','2','0','0');
+INSERT INTO [t10802] VALUES (4,'أبواب الألف','3','0','0');
+INSERT INTO [t10802] VALUES (5,'باب إبراهيم','3','4','0');
+INSERT INTO [t10802] VALUES (6,'باب أحمد','6','4','0');
+INSERT INTO [t10802] VALUES (7,'باب إسماعيل','9','4','0');
+INSERT INTO [t10802] VALUES (8,'باب إسحاق','11','4','0');
+INSERT INTO [t10802] VALUES (9,'باب أبان','12','4','0');
+INSERT INTO [t10802] VALUES (10,'باب يجمع جماعة','12','4','0');
+INSERT INTO [t10802] VALUES (11,'حرف الباء','13','0','0');
+INSERT INTO [t10802] VALUES (12,'حرف التاء والثاء','14','0','0');
+INSERT INTO [t10802] VALUES (13,'حرف الجيم','15','0','0');
+INSERT INTO [t10802] VALUES (14,'حرف الحاء','18','0','0');
+INSERT INTO [t10802] VALUES (15,'باب الخاء','21','0','0');
+INSERT INTO [t10802] VALUES (16,'حرف الدال والذال','22','0','0');
+INSERT INTO [t10802] VALUES (17,'حرف الراء','22','0','0');
+INSERT INTO [t10802] VALUES (18,'حرف الزاي','23','0','0');
+INSERT INTO [t10802] VALUES (19,'حرف السين','24','0','0');
+INSERT INTO [t10802] VALUES (20,'حرف الشين','28','0','0');
+INSERT INTO [t10802] VALUES (21,'حرف الصاد','28','0','0');
+INSERT INTO [t10802] VALUES (22,'باب الضاد والطاء','29','0','0');
+INSERT INTO [t10802] VALUES (23,'حرف العين','29','0','0');
+INSERT INTO [t10802] VALUES (24,'حرف الغين','38','0','0');
+INSERT INTO [t10802] VALUES (25,'حرف الفاء','38','0','0');
+INSERT INTO [t10802] VALUES (26,'حرف القاف','39','0','0');
+INSERT INTO [t10802] VALUES (27,'حرف الكاف','39','0','0');
+INSERT INTO [t10802] VALUES (28,'حرف اللام','39','0','0');
+INSERT INTO [t10802] VALUES (29,'حرف الميم','40','0','0');
+INSERT INTO [t10802] VALUES (30,'حرف النون','45','0','0');
+INSERT INTO [t10802] VALUES (31,'حرف الواو','46','0','0');
+INSERT INTO [t10802] VALUES (32,'حرف الهاء','46','0','0');
+INSERT INTO [t10802] VALUES (33,'حرف الياء','47','0','0');
