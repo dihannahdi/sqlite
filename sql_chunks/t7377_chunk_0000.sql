ac8866663d20c407ab4b8fd1218b486cfa214cb7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:13 +0700

    Upload SQL chunk: t7377_chunk_0000.sql

---
+++ sql_chunks/t7377_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t7377
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t7377" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7377
+INSERT INTO [t7377] VALUES (1,'المقدمات','1','0','0');
+INSERT INTO [t7377] VALUES (2,'مقدمة المؤلف','1','1','0');
+INSERT INTO [t7377] VALUES (3,'مقدمة الكتاب','2','1','0');
+INSERT INTO [t7377] VALUES (4,'في البيع','5','0','0');
+INSERT INTO [t7377] VALUES (5,'مدخل','5','4','0');
+INSERT INTO [t7377] VALUES (6,'أركان البيع','9','4','0');
+INSERT INTO [t7377] VALUES (7,'مدخل','9','6','0');
+INSERT INTO [t7377] VALUES (8,'الركن الأول','10','6','0');
+INSERT INTO [t7377] VALUES (9,'مدخل','10','8','0');
+INSERT INTO [t7377] VALUES (10,'الصريح والكناية','12','8','0');
+INSERT INTO [t7377] VALUES (11,'شروط الصيغة','13','8','0');
+INSERT INTO [t7377] VALUES (12,'انعقاد البيع بالمعاطاة','16','8','0');
+INSERT INTO [t7377] VALUES (13,'الركن الثاني: العاقد','19','6','0');
+INSERT INTO [t7377] VALUES (14,'الركن الثالث: المعقود عليه','26','6','0');
+INSERT INTO [t7377] VALUES (15,'الربا: تعريفه وأقسامه','67','0','0');
+INSERT INTO [t7377] VALUES (16,'مدخل','67','15','0');
+INSERT INTO [t7377] VALUES (17,'الأشاء التي يكون فيها الربا محرما','77','15','0');
+INSERT INTO [t7377] VALUES (18,'المبحث الثاني: بيع الحبوب بأجناسها وبغير أجناسها','80','15','0');
+INSERT INTO [t7377] VALUES (19,'المبحث الثالث: بيع الفاكهة بجنسها وما يتعلق بها.','83','15','0');
+INSERT INTO [t7377] VALUES (20,'المبحث الرابع: بيع اللحم بجنسه وما يتعلق به.','85','15','0');
+INSERT INTO [t7377] VALUES (21,'المبحث الخامس: بيع المائعات بأجناسها وبيعها بما تخرج عنه','87','15','0');
+INSERT INTO [t7377] VALUES (22,'المبحث السادس: الصرف','90','15','0');
+INSERT INTO [t7377] VALUES (23,'في القراض وما يتعلق به من أحكام','91','0','0');
+INSERT INTO [t7377] VALUES (24,'اللقطة والأحكام المتعلقة بها','108','0','0');
+INSERT INTO [t7377] VALUES (25,'مدخل','108','24','0');
+INSERT INTO [t7377] VALUES (26,'حكم اللقطة','110','24','0');
+INSERT INTO [t7377] VALUES (27,'اللقيط وأحكامه','140','24','0');
+INSERT INTO [t7377] VALUES (28,'أحكام الجعالة','156','0','0');
+INSERT INTO [t7377] VALUES (29,'إحياء الموات','168','0','0');
+INSERT INTO [t7377] VALUES (30,'الوقف وما يتعلق به من أحكام','209','0','0');
+INSERT INTO [t7377] VALUES (31,'الهبة وأحكامها','241','0','0');
+INSERT INTO [t7377] VALUES (32,'الفهارس','263','0','0');
