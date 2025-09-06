d8d4a3cdf43e95670c021a1a06acfa2c8d4462e8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:58 +0700

    Upload SQL chunk: t10133_chunk_0000.sql

---
+++ sql_chunks/t10133_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10133
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10133" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10133
+INSERT INTO [t10133] VALUES (1,'حجية السنة ودحض الشبهات التي تثار حولها','1','0','0');
+INSERT INTO [t10133] VALUES (2,'بقلم الدكتور: محمود أحمد طحان','1','0','0');
