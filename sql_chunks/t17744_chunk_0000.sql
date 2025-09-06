1ff7e970a21abaaa62bd546dc6b4ff4e953b361f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:16 +0700

    Upload SQL chunk: t17744_chunk_0000.sql

---
+++ sql_chunks/t17744_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t17744
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t17744" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17744
+INSERT INTO [t17744] VALUES (1,'تقدمة المعتني بالكتاب','3','0','0');
+INSERT INTO [t17744] VALUES (2,'سبب تأليفه للرسالة ومجمل مواضيعها','4','1','0');
+INSERT INTO [t17744] VALUES (3,'كلمة عن أصول الرسالة وعملي فيها','7','1','0');
+INSERT INTO [t17744] VALUES (4,'[مقدمة المصنف]','11','0','0');
+INSERT INTO [t17744] VALUES (5,'اعلم أنهم قد أحدثوا في آخر جمعة شهر رمضان أمورا مما لا أصل لها','12','0','0');
+INSERT INTO [t17744] VALUES (6,'فهرس المصادر','79','0','0');
