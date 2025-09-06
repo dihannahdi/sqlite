4a379a7b60351c0bc487da2aad58e5e8a51fcbe3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:11:57 +0700

    Upload SQL chunk: t37054_chunk_0000.sql

---
+++ sql_chunks/t37054_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t37054
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t37054" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37054
+INSERT INTO [t37054] VALUES (1,'رياضة النفس','24','0','0');
+INSERT INTO [t37054] VALUES (2,'اليقين','25','0','0');
+INSERT INTO [t37054] VALUES (3,'صفة القلب','42','0','0');
+INSERT INTO [t37054] VALUES (4,'صفة الموقن','61','0','0');
+INSERT INTO [t37054] VALUES (5,'النصرة','94','0','0');
+INSERT INTO [t37054] VALUES (6,'المجاهدة','97','0','0');
+INSERT INTO [t37054] VALUES (7,'الهوى','104','0','0');
+INSERT INTO [t37054] VALUES (8,'ثمرة الهوى','107','0','0');
