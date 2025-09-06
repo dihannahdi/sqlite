baee91d4036a72dd8dcd12404e9d21466f36ae93
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:06 +0700

    Upload SQL chunk: t10165_chunk_0000.sql

---
+++ sql_chunks/t10165_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t10165
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t10165" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10165
+INSERT INTO [t10165] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10165] VALUES (2,'بيان الأدلة على كفر من طعن في القرآن وفي الرسول صلى الله عليه وسلم','10','0','0');
+INSERT INTO [t10165] VALUES (3,'ذكر كلام العلماء فيمن طعن في القرآن والرسول','13','0','0');
+INSERT INTO [t10165] VALUES (4,'كشف الشبه المذكورة في الخطاب المنسوب إلى أبو رقيبة','16','0','0');
+INSERT INTO [t10165] VALUES (5,'ماذا يجب على رؤساء الدول الإسلامية إزاء ما نسب إلى الرئيس أبي رقيبة','44','0','0');
