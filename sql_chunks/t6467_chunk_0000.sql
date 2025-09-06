ac3a6605ae45fd67fc9837b2f2790f4786a87a90
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:54 +0700

    Upload SQL chunk: t6467_chunk_0000.sql

---
+++ sql_chunks/t6467_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t6467
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t6467" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6467
+INSERT INTO [t6467] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6467] VALUES (2,'فصل','13','0','0');
+INSERT INTO [t6467] VALUES (3,'فصل','27','0','0');
+INSERT INTO [t6467] VALUES (4,'فصل','30','0','0');
+INSERT INTO [t6467] VALUES (5,'فصل','31','0','0');
+INSERT INTO [t6467] VALUES (6,'فصل','31','0','0');
+INSERT INTO [t6467] VALUES (7,'فصل','37','0','0');
+INSERT INTO [t6467] VALUES (8,'فصل','38','0','0');
