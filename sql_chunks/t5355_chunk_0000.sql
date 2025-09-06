7392deba0daf4053be4a317a822bc2b43033bac0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:16 +0700

    Upload SQL chunk: t5355_chunk_0000.sql

---
+++ sql_chunks/t5355_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t5355
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t5355" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5355
+INSERT INTO [t5355] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5355] VALUES (2,'وبه العون والثقة','1','0','0');
+INSERT INTO [t5355] VALUES (3,'حتى أتى على آخرها، فضحك، ثم قال يعطى سبعين ألف درهم فقبضتها وانصرفت محمولا، ثم','26','0','0');
+INSERT INTO [t5355] VALUES (4,'تذكرة من التاريخ','40','0','0');
+INSERT INTO [t5355] VALUES (5,'نبذة في الهدية','47','0','0');
+INSERT INTO [t5355] VALUES (6,'من كتاب النساء في عيون الأخبار','56','0','0');
+INSERT INTO [t5355] VALUES (7,'في تقبيل اليد','61','0','0');
+INSERT INTO [t5355] VALUES (8,'في تقبيل الرأس','62','0','0');
+INSERT INTO [t5355] VALUES (9,'فألقى السيف في النطع، وقال أف لك، أما والله لو كان من سيوف الأجداد ما نبا، فغضب','84','0','0');
+INSERT INTO [t5355] VALUES (10,'فائدة وحديث','88','0','0');
