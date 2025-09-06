b1c437f56119ab4cacc6483b5d8bbf4000dca682
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:30:31 +0700

    Upload SQL chunk: t1756_chunk_0000.sql

---
+++ sql_chunks/t1756_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t1756
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t1756" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1756
+INSERT INTO [t1756] VALUES (1,'مقدمة المحقق','1','0','0');
+INSERT INTO [t1756] VALUES (2,'بسم الله الرحمن الرحيم','49','0','0');
