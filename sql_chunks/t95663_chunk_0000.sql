02138b22f85d9d2eaa40b54de50493c3896ae104
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:51 +0700

    Upload SQL chunk: t95663_chunk_0000.sql

---
+++ sql_chunks/t95663_chunk_0000.sql
@@ -0,0 +1,42 @@
+-- Table: t95663
+-- Chunk: 1
+-- Rows: 35
+
+CREATE TABLE "t95663" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95663
+INSERT INTO [t95663] VALUES (1,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t95663] VALUES (2,'أبدأ بالحمد .....','2','1','0');
+INSERT INTO [t95663] VALUES (3,'وذي من اقسام الحديث عده ....','2','1','0');
+INSERT INTO [t95663] VALUES (4,'المصطلحات','2','0','0');
+INSERT INTO [t95663] VALUES (5,'الحديث الصحيح','3','4','0');
+INSERT INTO [t95663] VALUES (6,'الحديث الحسن','4','4','0');
+INSERT INTO [t95663] VALUES (7,'الحديث الضعيف','5','4','0');
+INSERT INTO [t95663] VALUES (8,'الحديث المرفوع والمقطوع','6','4','0');
+INSERT INTO [t95663] VALUES (9,'الحديث المسند','7','4','0');
+INSERT INTO [t95663] VALUES (10,'الحديث المتصل','8','4','0');
+INSERT INTO [t95663] VALUES (11,'الحديث المسلسل','9','4','0');
+INSERT INTO [t95663] VALUES (12,'الحديث العزيز','10','4','0');
+INSERT INTO [t95663] VALUES (13,'الحديث المشهور','10','4','0');
+INSERT INTO [t95663] VALUES (14,'الحديث المعنعن','11','4','0');
+INSERT INTO [t95663] VALUES (15,'الحديث المبهم','11','4','0');
+INSERT INTO [t95663] VALUES (16,'الحديث العالي والنازل','12','4','0');
+INSERT INTO [t95663] VALUES (17,'الحديث الموقوف','13','4','0');
+INSERT INTO [t95663] VALUES (18,'الحديث المرسل','14','4','0');
+INSERT INTO [t95663] VALUES (19,'الحديث الغريب','15','4','0');
+INSERT INTO [t95663] VALUES (20,'الحديث المنقطع','16','4','0');
+INSERT INTO [t95663] VALUES (21,'الحديث المعضل','17','4','0');
+INSERT INTO [t95663] VALUES (22,'الحديث المدلس','18','4','0');
+INSERT INTO [t95663] VALUES (23,'الحديث الشاذ','19','4','0');
+INSERT INTO [t95663] VALUES (24,'الحديث المقلوب','19','4','0');
+INSERT INTO [t95663] VALUES (25,'الحديث الفرد','20','4','0');
+INSERT INTO [t95663] VALUES (26,'الحديث المعلل','21','4','0');
+INSERT INTO [t95663] VALUES (27,'الحديث المضطرب','22','4','0');
+INSERT INTO [t95663] VALUES (28,'الحديث المدرج','23','4','0');
+INSERT INTO [t95663] VALUES (29,'الحديث المدبج','24','4','0');
+INSERT INTO [t95663] VALUES (30,'الحديث المتفق والمفترق','25','4','0');
+INSERT INTO [t95663] VALUES (31,'المؤتلف والمختلف','26','4','0');
+INSERT INTO [t95663] VALUES (32,'الحديث المنكر','27','4','0');
+INSERT INTO [t95663] VALUES (33,'الحديث المتروك','28','4','0');
+INSERT INTO [t95663] VALUES (34,'الحديث الموضوع','29','4','0');
+INSERT INTO [t95663] VALUES (35,'الخاتمة','30','0','0');
