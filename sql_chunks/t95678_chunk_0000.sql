cfcbc4b10dd5757391c368a4be181a1e91fb0528
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:06 +0700

    Upload SQL chunk: t95678_chunk_0000.sql

---
+++ sql_chunks/t95678_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t95678
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t95678" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95678
+INSERT INTO [t95678] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95678] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t95678] VALUES (3,'أكل الصديقين','6','0','0');
+INSERT INTO [t95678] VALUES (4,'الذكر والشكر','6','0','0');
+INSERT INTO [t95678] VALUES (5,'وكلوا واشربوا','8','0','0');
+INSERT INTO [t95678] VALUES (6,'الإسراف والتبذير','9','0','0');
+INSERT INTO [t95678] VALUES (7,'فضول الطعام','12','0','0');
+INSERT INTO [t95678] VALUES (8,'ما شبع ستة عشر عاما','16','0','0');
+INSERT INTO [t95678] VALUES (9,'لا يفرحون بشيء','18','0','0');
+INSERT INTO [t95678] VALUES (10,'النعم ثلاثة','21','0','0');
+INSERT INTO [t95678] VALUES (11,'الدنيا أعيان موجودة','24','0','0');
+INSERT INTO [t95678] VALUES (12,'أعظم المهلكات','26','0','0');
+INSERT INTO [t95678] VALUES (13,'الدنيا أجل حاضر','29','0','0');
+INSERT INTO [t95678] VALUES (14,'نماذج مضيئة','31','0','0');
+INSERT INTO [t95678] VALUES (15,'مطية لحسن العبادة','33','0','0');
+INSERT INTO [t95678] VALUES (16,'العيش عيش واحد','35','0','0');
+INSERT INTO [t95678] VALUES (17,'من سنن المرسلين','37','0','0');
+INSERT INTO [t95678] VALUES (18,'ما أبالي ما فتني','38','0','0');
+INSERT INTO [t95678] VALUES (19,'فوائد الجوع','40','0','0');
+INSERT INTO [t95678] VALUES (20,'رقة القلب','41','0','0');
+INSERT INTO [t95678] VALUES (21,'الحمية رأس الدواء','52','0','0');
+INSERT INTO [t95678] VALUES (22,'ما سبب آجالكم؟','52','0','0');
+INSERT INTO [t95678] VALUES (23,'تفصيل دقيق لابن تيمية','55','0','0');
+INSERT INTO [t95678] VALUES (24,'بيت النبوة','60','0','0');
