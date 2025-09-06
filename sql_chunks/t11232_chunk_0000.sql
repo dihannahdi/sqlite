b7df948d30ad31450a37df671418a57b6d581e59
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:43 +0700

    Upload SQL chunk: t11232_chunk_0000.sql

---
+++ sql_chunks/t11232_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11232
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11232" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11232
+INSERT INTO [t11232] VALUES (1,'فضل المدينة','1','0','0');
+INSERT INTO [t11232] VALUES (2,'آداب سكناها وزيارتها','22','0','0');
