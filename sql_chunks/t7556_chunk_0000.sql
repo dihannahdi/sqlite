450fbee35a40bff71434b5497737d9c2f8c1cf97
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:49 +0700

    Upload SQL chunk: t7556_chunk_0000.sql

---
+++ sql_chunks/t7556_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t7556
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t7556" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7556
+INSERT INTO [t7556] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7556] VALUES (2,'النص المحقق','7','0','0');
+INSERT INTO [t7556] VALUES (3,'مصادر ومراجع','86','0','0');
