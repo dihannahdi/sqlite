85cd9f054ca1055c04d0c92dbd81d961a11dee65
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:00 +0700

    Upload SQL chunk: t8541_chunk_0000.sql

---
+++ sql_chunks/t8541_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8541
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8541" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8541
+INSERT INTO [t8541] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8541] VALUES (2,'المحاضرة الدفاعية عن السنة المحمدية','3','0','0');
