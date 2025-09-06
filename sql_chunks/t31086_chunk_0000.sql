c917e048c3cdff23a7c52bb9a132a2ec77e0126e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:12 +0700

    Upload SQL chunk: t31086_chunk_0000.sql

---
+++ sql_chunks/t31086_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t31086
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t31086" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31086
+INSERT INTO [t31086] VALUES (1,'تحريم خطف الطائرات','1','0','0');
+INSERT INTO [t31086] VALUES (2,'التحاكم إلى الكتاب والسنة','1','1','0');
+INSERT INTO [t31086] VALUES (3,'الواجب على الدولة التي يقع في يدها الخاطفون','4','1','0');
+INSERT INTO [t31086] VALUES (4,'لا يجوز لأهل الإسلام التحاكم إلى القوانين الوضعية','6','1','0');
+INSERT INTO [t31086] VALUES (5,'كل حكم يخالف شرع الله فهو من حكم الجاهلية','8','1','0');
