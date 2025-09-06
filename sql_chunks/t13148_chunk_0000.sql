b3dbc571fdc6154e44ec6576753e41a84445a4be
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:16 +0700

    Upload SQL chunk: t13148_chunk_0000.sql

---
+++ sql_chunks/t13148_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t13148
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t13148" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13148
+INSERT INTO [t13148] VALUES (1,'يا أبا عمير، ما فعل النغير','3','0','0');
+INSERT INTO [t13148] VALUES (2,'يا أبا عمير، ما فعل النغير','4','0','0');
+INSERT INTO [t13148] VALUES (3,'يا أبا عمير، ما فعل النغير','5','0','0');
+INSERT INTO [t13148] VALUES (4,'أبا عمير، ما فعل النغير؟ قال أنس: وما مسست شيئا قط - خزة، ولا حريرة - ألين من كف رسول','6','0','0');
+INSERT INTO [t13148] VALUES (5,'كان يأتي أم سليم، وكان إذا مشى يتوكأ، فكان ينام على فراشها - ثم ذكر الحديث بطوله - قال','7','0','0');
