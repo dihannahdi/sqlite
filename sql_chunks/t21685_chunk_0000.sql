87053e59c14a8576fa0287a6d57a0f0624a1032a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:14 +0700

    Upload SQL chunk: t21685_chunk_0000.sql

---
+++ sql_chunks/t21685_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21685
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21685" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21685
+INSERT INTO [t21685] VALUES (1,'من حديث أبي بكر أحمد بن علي بن لال - مخطوط','1','0','0');
