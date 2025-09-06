130586d903361fd8331d197e8816311460ef027f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:48 +0700

    Upload SQL chunk: t6588_chunk_0000.sql

---
+++ sql_chunks/t6588_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t6588
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t6588" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6588
+INSERT INTO [t6588] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6588] VALUES (2,'الواحبات','6','0','0');
+INSERT INTO [t6588] VALUES (3,'الواحبات','7','0','0');
+INSERT INTO [t6588] VALUES (4,'نقد المصنف لهذا الحديث','10','0','0');
+INSERT INTO [t6588] VALUES (5,'الواجب المتعلق بالنكاح','43','0','0');
+INSERT INTO [t6588] VALUES (6,'الأول المحرمات في غير النكاح','59','0','0');
+INSERT INTO [t6588] VALUES (7,'المحرمات المتعلقة بالنكاح','80','0','0');
+INSERT INTO [t6588] VALUES (8,'ما اختص به من المباحات والتخفيفات','88','0','0');
+INSERT INTO [t6588] VALUES (9,'المباحات له في غير النكاح','89','0','0');
+INSERT INTO [t6588] VALUES (10,'التخفيفات المتعلقة بالنكاح','121','0','0');
+INSERT INTO [t6588] VALUES (11,'القسم الأول','156','0','0');
+INSERT INTO [t6588] VALUES (12,'كراماته في غير النكاح','189','0','0');
