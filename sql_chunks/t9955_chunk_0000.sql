8b0828b02342a42121cb02bde954cb4fab85e33d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:16 +0700

    Upload SQL chunk: t9955_chunk_0000.sql

---
+++ sql_chunks/t9955_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t9955
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t9955" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9955
+INSERT INTO [t9955] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t9955] VALUES (2,'الدوافع على الغزوة','3','0','0');
+INSERT INTO [t9955] VALUES (3,'بقي تحديد المكان','6','0','0');
+INSERT INTO [t9955] VALUES (4,'جها لوجه','7','0','0');
+INSERT INTO [t9955] VALUES (5,'قبل بدء المعركة','10','0','0');
+INSERT INTO [t9955] VALUES (6,'وتفصيل تلك التعليمات','11','0','0');
+INSERT INTO [t9955] VALUES (7,'تصفية المعركة','17','0','0');
