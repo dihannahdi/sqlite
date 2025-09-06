bbfb26170bf155797786d39688ce9914e8cd7a3a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:57 +0700

    Upload SQL chunk: t29710_chunk_0000.sql

---
+++ sql_chunks/t29710_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t29710
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t29710" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29710
+INSERT INTO [t29710] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t29710] VALUES (2,'أذكار الصباح','4','0','0');
+INSERT INTO [t29710] VALUES (3,'تنبيه','19','2','0');
+INSERT INTO [t29710] VALUES (4,'أذكار المساء','21','0','0');
+INSERT INTO [t29710] VALUES (5,'من آداب الصباح والمساء','28','0','0');
+INSERT INTO [t29710] VALUES (6,'الأول','28','5','0');
+INSERT INTO [t29710] VALUES (7,'الثاني','29','5','0');
+INSERT INTO [t29710] VALUES (8,'الثالث','29','5','0');
+INSERT INTO [t29710] VALUES (9,'الرابع','30','5','0');
+INSERT INTO [t29710] VALUES (10,'الخامس','31','5','0');
+INSERT INTO [t29710] VALUES (11,'السادس','31','5','0');
+INSERT INTO [t29710] VALUES (12,'السابع','35','5','0');
+INSERT INTO [t29710] VALUES (13,'الثامن','35','5','0');
+INSERT INTO [t29710] VALUES (14,'التاسع','35','5','0');
+INSERT INTO [t29710] VALUES (15,'العاشر','36','5','0');
