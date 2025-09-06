be2ce5cb33b98208b573be2615e02615cb60d19d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:47 +0700

    Upload SQL chunk: t147668_chunk_0000.sql

---
+++ sql_chunks/t147668_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t147668
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t147668" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t147668
+INSERT INTO [t147668] VALUES (1,'مقدمة المحقق','5','0','0');
+INSERT INTO [t147668] VALUES (2,'* ترجمة المؤلف.','8','1','0');
+INSERT INTO [t147668] VALUES (3,'* دراسته للحديث.','9','1','0');
+INSERT INTO [t147668] VALUES (4,'* نسبة الكتاب إلى مؤلفه.','9','1','0');
+INSERT INTO [t147668] VALUES (5,'نقل العلماء من المؤلف: -','9','1','0');
+INSERT INTO [t147668] VALUES (6,'عملي في التحقيق','15','1','0');
+INSERT INTO [t147668] VALUES (7,'طبعات الكتاب','17','1','0');
+INSERT INTO [t147668] VALUES (8,'وصف المخطوط','19','1','0');
+INSERT INTO [t147668] VALUES (9,'مقدمة المصنف','20','0','0');
+INSERT INTO [t147668] VALUES (10,'فصل','25','9','0');
+INSERT INTO [t147668] VALUES (11,'الفصل الأول','26','0','0');
+INSERT INTO [t147668] VALUES (12,'الفصل الثاني في سنة رسول الله - صلى الله عليه وسلم -','30','0','0');
+INSERT INTO [t147668] VALUES (13,'الفصل الثالث','33','0','0');
+INSERT INTO [t147668] VALUES (14,'الفصل الرابع','35','0','0');
+INSERT INTO [t147668] VALUES (15,'[أقسام القياس]','36','14','0');
+INSERT INTO [t147668] VALUES (16,'[تعريف الفقه وأصول الفقه]','38','14','0');
+INSERT INTO [t147668] VALUES (17,'[دليل الخطاب]','45','14','0');
+INSERT INTO [t147668] VALUES (18,'[مفهوم العدد]','47','14','0');
+INSERT INTO [t147668] VALUES (19,'[مفهوم الغاية]','47','14','0');
+INSERT INTO [t147668] VALUES (20,'[مفهوم الشرط]','48','14','0');
+INSERT INTO [t147668] VALUES (21,'[مفهوم الخطاب]','51','14','0');
+INSERT INTO [t147668] VALUES (22,'[هل يسمى مفهوم الخطاب قياسا]','53','14','0');
+INSERT INTO [t147668] VALUES (23,'[هل يجتهد السائل في عين المفتي أو يأخذ قول أي مفتي]','70','14','0');
+INSERT INTO [t147668] VALUES (24,'الفصل الخامس','72','0','0');
+INSERT INTO [t147668] VALUES (25,'الفصل السادس','75','0','0');
+INSERT INTO [t147668] VALUES (26,'فصل','77','25','0');
