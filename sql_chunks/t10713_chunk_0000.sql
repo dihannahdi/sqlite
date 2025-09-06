140a24b15cf74c5a28b347f2908288e0053c0879
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:50:50 +0700

    Upload SQL chunk: t10713_chunk_0000.sql

---
+++ sql_chunks/t10713_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t10713
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t10713" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10713
+INSERT INTO [t10713] VALUES (1,'فمما رأيته في التفسير','1','0','0');
+INSERT INTO [t10713] VALUES (2,'ففي سورة البقرة','1','0','0');
+INSERT INTO [t10713] VALUES (3,'وفي سورة آل عمران','3','0','0');
+INSERT INTO [t10713] VALUES (4,'وفي سورة النساء','3','0','0');
+INSERT INTO [t10713] VALUES (5,'وفي سورة المائدة','3','0','0');
+INSERT INTO [t10713] VALUES (6,'وفي سورة الأنعام','4','0','0');
+INSERT INTO [t10713] VALUES (7,'وفي سورة الأعراف','4','0','0');
+INSERT INTO [t10713] VALUES (8,'وفي سورة الأنفال','4','0','0');
+INSERT INTO [t10713] VALUES (9,'وفي سورة براءة','4','0','0');
+INSERT INTO [t10713] VALUES (10,'وفي سورة يونس','5','0','0');
+INSERT INTO [t10713] VALUES (11,'وفي سورة يوسف','5','0','0');
+INSERT INTO [t10713] VALUES (12,'وفي سورة الرعد','5','0','0');
+INSERT INTO [t10713] VALUES (13,'وفي سورة الحجر','6','0','0');
+INSERT INTO [t10713] VALUES (14,'وفي سورة النحل','6','0','0');
+INSERT INTO [t10713] VALUES (15,'وفي سورة الأنبياء','6','0','0');
+INSERT INTO [t10713] VALUES (16,'وفي سورة الحج','6','0','0');
+INSERT INTO [t10713] VALUES (17,'وفي سورة النور','7','0','0');
+INSERT INTO [t10713] VALUES (18,'وفي سورة القصص','7','0','0');
+INSERT INTO [t10713] VALUES (19,'وفي سورة العنكبوت','7','0','0');
+INSERT INTO [t10713] VALUES (20,'وفي سورة لقمان','7','0','0');
+INSERT INTO [t10713] VALUES (21,'وفي سورة آلم السجدة','8','0','0');
+INSERT INTO [t10713] VALUES (22,'وفي سورة الأحزاب','8','0','0');
+INSERT INTO [t10713] VALUES (23,'وفي سورة سبأ','8','0','0');
+INSERT INTO [t10713] VALUES (24,'وفي سورة فاطر','8','0','0');
+INSERT INTO [t10713] VALUES (25,'وفي سورة غافر','9','0','0');
+INSERT INTO [t10713] VALUES (26,'وفي سورة الشورى','9','0','0');
+INSERT INTO [t10713] VALUES (27,'وفي سورة الزخرف','9','0','0');
+INSERT INTO [t10713] VALUES (28,'وفي سورة الدخان','9','0','0');
+INSERT INTO [t10713] VALUES (29,'وفي سورة الجاثية','9','0','0');
+INSERT INTO [t10713] VALUES (30,'وفي سورة الذاريات','9','0','0');
+INSERT INTO [t10713] VALUES (31,'وفي سورة الواقعة','10','0','0');
+INSERT INTO [t10713] VALUES (32,'وفي سورة المجادلة','10','0','0');
+INSERT INTO [t10713] VALUES (33,'وفي سورة الممتحنة','10','0','0');
+INSERT INTO [t10713] VALUES (34,'ومما صنفه في الأصول مبتدئا أو مجيبا لمعترض أو سائل','12','0','0');
+INSERT INTO [t10713] VALUES (35,'قواعد وفتاوى','13','0','0');
+INSERT INTO [t10713] VALUES (36,'الكتب الفقهية','20','0','0');
+INSERT INTO [t10713] VALUES (37,'وله وصايا منها','22','0','0');
+INSERT INTO [t10713] VALUES (38,'وله إجازات منها','22','0','0');
+INSERT INTO [t10713] VALUES (39,'وله رسائل تتضمن علوما','23','0','0');
