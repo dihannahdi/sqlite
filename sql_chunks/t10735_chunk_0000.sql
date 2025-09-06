120b50145134a125f26be6dafb818d44889c0ac9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:01 +0700

    Upload SQL chunk: t10735_chunk_0000.sql

---
+++ sql_chunks/t10735_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10735
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10735" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10735
+INSERT INTO [t10735] VALUES (1,'إجازة المؤلف برنامجه لأبي القاسم البياض وولديه','1','0','0');
+INSERT INTO [t10735] VALUES (2,'مقدمة المؤلف','2','0','0');
