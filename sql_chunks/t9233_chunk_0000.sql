cffcb0b4505e8367e0973b456f1e1b51c8cbfe55
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:51:50 +0700

    Upload SQL chunk: t9233_chunk_0000.sql

---
+++ sql_chunks/t9233_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t9233
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t9233" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9233
+INSERT INTO [t9233] VALUES (1,'الباب الأول','1','0','0');
+INSERT INTO [t9233] VALUES (2,'ذكر انتساب الملوك بني الرسول','1','0','0');
+INSERT INTO [t9233] VALUES (3,'وكيف كان السبب في دخولهم اليمن واستقلالهم بالملك فيها.','1','2','0');
+INSERT INTO [t9233] VALUES (4,'فصل','5','2','0');
+INSERT INTO [t9233] VALUES (5,'فصل','6','2','0');
+INSERT INTO [t9233] VALUES (6,'ذكر خراب السد','6','2','0');
+INSERT INTO [t9233] VALUES (7,'فصل','8','2','0');
+INSERT INTO [t9233] VALUES (8,'فصل','10','2','0');
+INSERT INTO [t9233] VALUES (9,'ذكر ملوك الشام الجاهلية من غسان','11','2','0');
+INSERT INTO [t9233] VALUES (10,'فصل','14','2','0');
+INSERT INTO [t9233] VALUES (11,'فصل','14','2','0');
+INSERT INTO [t9233] VALUES (12,'فصل','20','2','0');
+INSERT INTO [t9233] VALUES (13,'ذكر بني رسول ملوك اليمن في الإسلام','20','2','0');
+INSERT INTO [t9233] VALUES (14,'الباب الثاني','34','2','0');
+INSERT INTO [t9233] VALUES (15,'ذكر قيام الدولة المنصورية','34','0','0');
+INSERT INTO [t9233] VALUES (16,'وأسبابها','34','15','0');
+INSERT INTO [t9233] VALUES (17,'فصل','35','15','0');
+INSERT INTO [t9233] VALUES (18,'الباب الثالث','69','15','0');
+INSERT INTO [t9233] VALUES (19,'أخبار الدولة المظفرية وفتوحها','69','0','0');
+INSERT INTO [t9233] VALUES (20,'الباب الرابع','221','19','0');
+INSERT INTO [t9233] VALUES (21,'ذكر قيام الدولة الأشرفية الصغرى','221','0','0');
+INSERT INTO [t9233] VALUES (22,'الباب الخامس','232','21','0');
+INSERT INTO [t9233] VALUES (23,'ذكر أخبار الدولة المؤيدية','232','0','0');
+INSERT INTO [t9233] VALUES (24,'وما كان فيها','232','23','0');
+INSERT INTO [t9233] VALUES (25,'الباب السادس','341','23','0');
+INSERT INTO [t9233] VALUES (26,'ذكر أخبار الدولة المجاهدية','342','0','0');
+INSERT INTO [t9233] VALUES (27,'الباب السابع','437','26','0');
+INSERT INTO [t9233] VALUES (28,'ذكر قيام الدولة الأفضلية','438','0','0');
+INSERT INTO [t9233] VALUES (29,'ووقائعها','438','28','0');
+INSERT INTO [t9233] VALUES (30,'الباب الثامن','466','28','0');
+INSERT INTO [t9233] VALUES (31,'ذكر قيام الدولة الأشرفية الكبرى','467','0','0');
+INSERT INTO [t9233] VALUES (32,'وبعض أيامها','467','31','0');
