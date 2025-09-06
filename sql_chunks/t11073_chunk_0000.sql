33c6206b7fc6af07bd8ee2d2a43c0b80e131f03f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:53 +0700

    Upload SQL chunk: t11073_chunk_0000.sql

---
+++ sql_chunks/t11073_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11073
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11073" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11073
+INSERT INTO [t11073] VALUES (1,'بيان المحجة في الرد على اللجة','1','0','0');
+INSERT INTO [t11073] VALUES (2,'نفي ما في البردة من شرك نشأ عن جهل وفساد تصور','3','0','0');
+INSERT INTO [t11073] VALUES (3,'وبه كمل الجواب، والحمد لله الذي هدانا لدينه الذي رضيه لعباده','61','0','0');
