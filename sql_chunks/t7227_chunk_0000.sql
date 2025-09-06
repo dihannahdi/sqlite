f48a7ad9e99d82369cd70172200708f336628080
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:31 +0700

    Upload SQL chunk: t7227_chunk_0000.sql

---
+++ sql_chunks/t7227_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t7227
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t7227" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7227
+INSERT INTO [t7227] VALUES (1,'اختلاف أقدار الناس وتفاوت أحوالهم سبب بقائهم','2','0','0');
+INSERT INTO [t7227] VALUES (2,'وجوب السياسة','3','0','0');
+INSERT INTO [t7227] VALUES (3,'الحاجة إلى السياسة','4','0','0');
+INSERT INTO [t7227] VALUES (4,'العقل أساس الإصلاح','7','0','0');
+INSERT INTO [t7227] VALUES (5,'معرفة أوجه الفساد وعلاجها','7','0','0');
+INSERT INTO [t7227] VALUES (6,'ضرورة الصديق لكشف عيوب النفس','8','0','0');
+INSERT INTO [t7227] VALUES (7,'ضرورة من يكشف للرؤساء عيوبهم','8','0','0');
+INSERT INTO [t7227] VALUES (8,'قرناء السوء وأثرهم','9','0','0');
+INSERT INTO [t7227] VALUES (9,'أساس الصحبة','10','0','0');
+INSERT INTO [t7227] VALUES (10,'موقف الرؤساء من النصح','11','0','0');
+INSERT INTO [t7227] VALUES (11,'لكي تأتي الشورة ثمارها','11','0','0');
+INSERT INTO [t7227] VALUES (12,'أخلاق الرئيس من أخلاق الناس','12','0','0');
+INSERT INTO [t7227] VALUES (13,'الثواب والعقاب للنفس','12','0','0');
+INSERT INTO [t7227] VALUES (14,'الحاجة إلى الأقوات','13','0','0');
+INSERT INTO [t7227] VALUES (15,'أصناف الناس في الكسب','13','0','0');
+INSERT INTO [t7227] VALUES (16,'أنواع صناعات ذوي المروءة','13','0','0');
+INSERT INTO [t7227] VALUES (17,'السيرة العادلة','15','0','0');
+INSERT INTO [t7227] VALUES (18,'الزكوات','15','0','0');
+INSERT INTO [t7227] VALUES (19,'شروط المعروف','15','0','0');
+INSERT INTO [t7227] VALUES (20,'النفقات','16','0','0');
+INSERT INTO [t7227] VALUES (21,'الادخار','16','0','0');
+INSERT INTO [t7227] VALUES (22,'المرأة الصالحة وصفاتها','18','0','0');
+INSERT INTO [t7227] VALUES (23,'أسس سياسة الرجل لأهله','18','0','0');
+INSERT INTO [t7227] VALUES (24,'الهيبة','18','0','0');
+INSERT INTO [t7227] VALUES (25,'الكرامة التامة','19','0','0');
+INSERT INTO [t7227] VALUES (26,'شغل الخاطر بالمهم','20','0','0');
+INSERT INTO [t7227] VALUES (27,'حق الولد على والديه','21','0','0');
+INSERT INTO [t7227] VALUES (28,'بداية تربية الطفل بعد الفطم','21','0','0');
+INSERT INTO [t7227] VALUES (29,'في دور التمييز','22','0','0');
+INSERT INTO [t7227] VALUES (30,'مؤدب الصبي','22','0','0');
+INSERT INTO [t7227] VALUES (31,'رفيق الصبي','23','0','0');
+INSERT INTO [t7227] VALUES (32,'إعداد الصبي للمهنة وفقا لطبعه','24','0','0');
+INSERT INTO [t7227] VALUES (33,'مراعاة ميل الصبي للصناعة','25','0','0');
+INSERT INTO [t7227] VALUES (34,'كسب الصبي من الصناعة وفوائده','25','0','0');
+INSERT INTO [t7227] VALUES (35,'الحاجة إلى الخدم','27','0','0');
+INSERT INTO [t7227] VALUES (36,'واجب السيد نحو الخدم','28','0','0');
+INSERT INTO [t7227] VALUES (37,'طريق اتخاذ الخدم','28','0','0');
+INSERT INTO [t7227] VALUES (38,'اختيار ما يصلح الخادم','29','0','0');
+INSERT INTO [t7227] VALUES (39,'الخادم شريك سيده في النعمة','29','0','0');
