969d8641dd27a5e2f0def16c10eb8c77cd9a9ab8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:43 +0700

    Upload SQL chunk: t26470_chunk_0000.sql

---
+++ sql_chunks/t26470_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t26470
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t26470" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26470
+INSERT INTO [t26470] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26470] VALUES (2,'الجزء فيه تفسير القرآن برواية أبي جعفر الترمذي','3','0','0');
+INSERT INTO [t26470] VALUES (3,'تفسير يحيى بن يمان','3','2','0');
+INSERT INTO [t26470] VALUES (4,'تفسير آخر لنافع بن أبي نعيم','32','2','0');
+INSERT INTO [t26470] VALUES (5,'تفسير مسلم بن خالد الزنجي','42','2','0');
+INSERT INTO [t26470] VALUES (6,'تفسير عطاء الخراساني','204','2','0');
+INSERT INTO [t26470] VALUES (7,'[أحاديث من رواية محمد بن يونس المقرئ]','382','2','0');
