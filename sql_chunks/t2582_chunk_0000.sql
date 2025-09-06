5f82de4188a1f0f1a18b22d341704deadfee8c17
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:58 +0700

    Upload SQL chunk: t2582_chunk_0000.sql

---
+++ sql_chunks/t2582_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t2582
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t2582" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2582
+INSERT INTO [t2582] VALUES (1,'مقدمة الإعداد','1','0','0');
+INSERT INTO [t2582] VALUES (2,'مقدمة المحقق','2','0','0');
+INSERT INTO [t2582] VALUES (3,'طرق حديث من كنت مولاه','4','0','0');
+INSERT INTO [t2582] VALUES (4,'تمت والحمد لله رب العالمين','89','0','0');
