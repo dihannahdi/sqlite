6a7294f8344f212382a92949650620b5da0747ef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:46:26 +0700

    Upload SQL chunk: t8491_chunk_0000.sql

---
+++ sql_chunks/t8491_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t8491
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t8491" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8491
+INSERT INTO [t8491] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8491] VALUES (2,'أولا: معنى النسخ','1','1','0');
+INSERT INTO [t8491] VALUES (3,'ثانيا: أين يقع النسخ','2','1','0');
+INSERT INTO [t8491] VALUES (4,'ثالثا: الفرق بين النسخ والبداء','3','1','0');
+INSERT INTO [t8491] VALUES (5,'رابعا: الفرق بين النسخ والتخصيص','4','1','0');
+INSERT INTO [t8491] VALUES (6,'خامسا: فضل هذا العلم','4','1','0');
+INSERT INTO [t8491] VALUES (7,'المصنفون في النسخ في القرآن','6','1','0');
+INSERT INTO [t8491] VALUES (8,'قتادة بن دعامة وكتابه المؤلف','14','1','0');
+INSERT INTO [t8491] VALUES (9,'كتاب الناسخ والمنسوخ','18','1','0');
+INSERT INTO [t8491] VALUES (10,'أولا: توثيقه','18','1','0');
+INSERT INTO [t8491] VALUES (11,'ثانيا: مخطوطة الكتاب','20','1','0');
+INSERT INTO [t8491] VALUES (12,'سورة البقرة','26','0','0');
+INSERT INTO [t8491] VALUES (13,'سورة آل عمران','33','0','0');
+INSERT INTO [t8491] VALUES (14,'سورة النساء','33','0','0');
+INSERT INTO [t8491] VALUES (15,'سورة المائدة','35','0','0');
+INSERT INTO [t8491] VALUES (16,'سورة الأنعام','37','0','0');
+INSERT INTO [t8491] VALUES (17,'سورة الأنفال','37','0','0');
+INSERT INTO [t8491] VALUES (18,'سورة التوبة','38','0','0');
+INSERT INTO [t8491] VALUES (19,'سورة النحل','39','0','0');
+INSERT INTO [t8491] VALUES (20,'سورة الإسراء','39','0','0');
+INSERT INTO [t8491] VALUES (21,'سورة العنكبوت','40','0','0');
+INSERT INTO [t8491] VALUES (22,'سورة الجاثية','40','0','0');
+INSERT INTO [t8491] VALUES (23,'سورة الأحقاف','41','0','0');
+INSERT INTO [t8491] VALUES (24,'سورة محمد','42','0','0');
+INSERT INTO [t8491] VALUES (25,'سورة المجادلة','42','0','0');
+INSERT INTO [t8491] VALUES (26,'سورة الحشر','43','0','0');
+INSERT INTO [t8491] VALUES (27,'سورة الممتحنة','43','0','0');
+INSERT INTO [t8491] VALUES (28,'سورة المزمل','45','0','0');
+INSERT INTO [t8491] VALUES (29,'ذكر المدني من القرآن','47','0','0');
+INSERT INTO [t8491] VALUES (30,'فهرس المصادر والمراجع','49','0','0');
