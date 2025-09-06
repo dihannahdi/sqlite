10c3c73ca86d84466f619b4a5703adb7c195f715
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:36 +0700

    Upload SQL chunk: t10062_chunk_0000.sql

---
+++ sql_chunks/t10062_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10062
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10062" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10062
+INSERT INTO [t10062] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10062] VALUES (2,'تعريف القرآن','3','0','0');
+INSERT INTO [t10062] VALUES (3,'ترجمة معاني القرآن','10','0','0');
