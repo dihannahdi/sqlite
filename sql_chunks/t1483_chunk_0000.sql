cf8c9bc576c55eae1c1cb21a66edefb0902323d1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:22 +0700

    Upload SQL chunk: t1483_chunk_0000.sql

---
+++ sql_chunks/t1483_chunk_0000.sql
@@ -0,0 +1,44 @@
+-- Table: t1483
+-- Chunk: 1
+-- Rows: 37
+
+CREATE TABLE "t1483" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1483
+INSERT INTO [t1483] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t1483] VALUES (2,'الجزء فيه مجرد أسماء الرواة عن الإمام أبي عبد الله مالك بن أنس الأصبحي','2','0','0');
+INSERT INTO [t1483] VALUES (3,'باب الألف','2','2','0');
+INSERT INTO [t1483] VALUES (4,'باب الباء','28','2','0');
+INSERT INTO [t1483] VALUES (5,'باب الثاء','34','2','0');
+INSERT INTO [t1483] VALUES (6,'باب الجيم','34','2','0');
+INSERT INTO [t1483] VALUES (7,'باب الحاء','35','2','0');
+INSERT INTO [t1483] VALUES (8,'باب الخاء','48','2','0');
+INSERT INTO [t1483] VALUES (9,'باب الدال','52','2','0');
+INSERT INTO [t1483] VALUES (10,'باب الذال','56','2','0');
+INSERT INTO [t1483] VALUES (11,'باب الراء','56','2','0');
+INSERT INTO [t1483] VALUES (12,'باب الزاي','59','2','0');
+INSERT INTO [t1483] VALUES (13,'باب السين','63','2','0');
+INSERT INTO [t1483] VALUES (14,'باب الشين','75','2','0');
+INSERT INTO [t1483] VALUES (15,'باب الصاد','77','2','0');
+INSERT INTO [t1483] VALUES (16,'باب الضاد','79','2','0');
+INSERT INTO [t1483] VALUES (17,'باب الطاء','80','2','0');
+INSERT INTO [t1483] VALUES (18,'باب العين','80','2','0');
+INSERT INTO [t1483] VALUES (19,'باب الغين','130','2','0');
+INSERT INTO [t1483] VALUES (20,'باب الفاء','131','2','0');
+INSERT INTO [t1483] VALUES (21,'باب القاف','135','2','0');
+INSERT INTO [t1483] VALUES (22,'باب الكاف','136','2','0');
+INSERT INTO [t1483] VALUES (23,'باب اللام','136','2','0');
+INSERT INTO [t1483] VALUES (24,'باب الميم','138','2','0');
+INSERT INTO [t1483] VALUES (25,'باب النون','178','2','0');
+INSERT INTO [t1483] VALUES (26,'باب الواو','182','2','0');
+INSERT INTO [t1483] VALUES (27,'باب الهاء','184','2','0');
+INSERT INTO [t1483] VALUES (28,'باب الياء','189','2','0');
+INSERT INTO [t1483] VALUES (29,'باب من روى عنه ممن لا يعرف اسمه أصحاب الكنى','208','2','0');
+INSERT INTO [t1483] VALUES (30,'ومن النساء','212','2','0');
+INSERT INTO [t1483] VALUES (31,'ما استدركه العطار على الخطيب','213','0','0');
+INSERT INTO [t1483] VALUES (32,'الأسماء التي سقطت من الأصل ونبه العلماء أنها عند الخطيب','224','0','0');
+INSERT INTO [t1483] VALUES (33,'المستدرك على الخطيب والعطار وهو جمهرة الرواة عن مالك.','236','0','0');
+INSERT INTO [t1483] VALUES (34,'الكنى','388','33','0');
+INSERT INTO [t1483] VALUES (35,'الأبناء','395','33','0');
+INSERT INTO [t1483] VALUES (36,'من لم يسم','397','33','0');
+INSERT INTO [t1483] VALUES (37,'النساء','398','33','0');
