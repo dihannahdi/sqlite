e3d9eab04c937b6349fc1fbb105e5e6a04004f84
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:04 +0700

    Upload SQL chunk: t7500_chunk_0000.sql

---
+++ sql_chunks/t7500_chunk_0000.sql
@@ -0,0 +1,44 @@
+-- Table: t7500
+-- Chunk: 1
+-- Rows: 37
+
+CREATE TABLE "t7500" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7500
+INSERT INTO [t7500] VALUES (1,'المجلد الأول','1','0','0');
+INSERT INTO [t7500] VALUES (2,'مقدمة','1','1','0');
+INSERT INTO [t7500] VALUES (3,'أزواج بالكذب','3','1','0');
+INSERT INTO [t7500] VALUES (4,'وصف المشكلة','3','3','0');
+INSERT INTO [t7500] VALUES (5,'*','3','4','0');
+INSERT INTO [t7500] VALUES (6,'آيات قرآنية وأحاديث نبوية','14','4','0');
+INSERT INTO [t7500] VALUES (7,'من الواجب على الآخرين','22','4','0');
+INSERT INTO [t7500] VALUES (8,'حل المشكلة','23','3','0');
+INSERT INTO [t7500] VALUES (9,'*','23','8','0');
+INSERT INTO [t7500] VALUES (10,'- أولا: الوقاية قبل العلاج','24','8','0');
+INSERT INTO [t7500] VALUES (11,'- ثانيا: حل المشكلة بمبادرة من الزوج','26','8','0');
+INSERT INTO [t7500] VALUES (12,'- ثالثا: الحل إذا لم يبادر الزوج','29','8','0');
+INSERT INTO [t7500] VALUES (13,'وقفة مع الآباء أيضا','36','3','0');
+INSERT INTO [t7500] VALUES (14,'مخاوف في غير موضعها!','41','3','0');
+INSERT INTO [t7500] VALUES (15,'تساؤلات تحتاج إجابة!','44','3','0');
+INSERT INTO [t7500] VALUES (16,'قصص من الواقع في هذا الموضوع','48','3','0');
+INSERT INTO [t7500] VALUES (17,'*','48','16','0');
+INSERT INTO [t7500] VALUES (18,'القصة الأولى','49','16','0');
+INSERT INTO [t7500] VALUES (19,'القصة الثانية','51','16','0');
+INSERT INTO [t7500] VALUES (20,'قصة ثالثة','53','16','0');
+INSERT INTO [t7500] VALUES (21,'قصة رابعة','55','16','0');
+INSERT INTO [t7500] VALUES (22,'قصة خامسة','58','16','0');
+INSERT INTO [t7500] VALUES (23,'قصة سادسة','59','16','0');
+INSERT INTO [t7500] VALUES (24,'قصة سابعة','60','16','0');
+INSERT INTO [t7500] VALUES (25,'قصة ثامنة','62','16','0');
+INSERT INTO [t7500] VALUES (26,'رأي في الموضوع','63','3','0');
+INSERT INTO [t7500] VALUES (27,'*','63','26','0');
+INSERT INTO [t7500] VALUES (28,'رأي ثان','66','26','0');
+INSERT INTO [t7500] VALUES (29,'رأي ثالث','69','26','0');
+INSERT INTO [t7500] VALUES (30,'رأي رابع','71','26','0');
+INSERT INTO [t7500] VALUES (31,'رأي خامس','72','26','0');
+INSERT INTO [t7500] VALUES (32,'رأي سادس','74','26','0');
+INSERT INTO [t7500] VALUES (33,'رأي سابع','76','26','0');
+INSERT INTO [t7500] VALUES (34,'تعقيب','77','26','0');
+INSERT INTO [t7500] VALUES (35,'أيها الداعون إلى الصبر دائما','79','3','0');
+INSERT INTO [t7500] VALUES (36,'كلمة أخيرة في الموضوع','81','3','0');
+INSERT INTO [t7500] VALUES (37,'الخاتمة','84','3','0');
