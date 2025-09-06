0f8a02512f46c259fb5c4d4dee626be821eee8b7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:26 +0700

    Upload SQL chunk: t8571_chunk_0000.sql

---
+++ sql_chunks/t8571_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8571
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8571" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8571
+INSERT INTO [t8571] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8571] VALUES (2,'المورد العذب الزلال في كشف شبه أهل الضلال','8','0','0');
