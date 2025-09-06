90c4f09d179f83a90e4c6ba81e53a42884016e47
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:41:02 +0700

    Upload SQL chunk: t7645_chunk_0000.sql

---
+++ sql_chunks/t7645_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t7645
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t7645" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7645
+INSERT INTO [t7645] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7645] VALUES (2,'ترجمة الهيثمي','4','0','0');
+INSERT INTO [t7645] VALUES (3,'تعريف بكتاب مجمع الزوائد، وبيان قيمته، ومنهج المؤلف فيه','11','0','0');
+INSERT INTO [t7645] VALUES (4,'مادة السيرة النبوية في الكتاب، ومنهج المؤلف في تدوينها','15','0','0');
+INSERT INTO [t7645] VALUES (5,'معالم من منهج الهيثمي في نقد الرجال والأحاديث','29','0','0');
+INSERT INTO [t7645] VALUES (6,'علماء استفادوا من كتاب مجمع الزوائد للهيثمي','35','0','0');
+INSERT INTO [t7645] VALUES (7,'نماذج من مرويات السيرة النبوية من مجمع الزوائد للهيثمي','37','0','0');
+INSERT INTO [t7645] VALUES (8,'الخاتمة','50','0','0');
+INSERT INTO [t7645] VALUES (9,'مصادر ومراجع','51','0','0');
