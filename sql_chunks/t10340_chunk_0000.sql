231466d3b3d0728b3c89de507f27f9465af7e5db
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:12 +0700

    Upload SQL chunk: t10340_chunk_0000.sql

---
+++ sql_chunks/t10340_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t10340
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t10340" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10340
+INSERT INTO [t10340] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10340] VALUES (2,'ترجمة الأبذي','2','0','0');
+INSERT INTO [t10340] VALUES (3,'تلاميذه','12','0','0');
+INSERT INTO [t10340] VALUES (4,'صفاته','15','0','0');
+INSERT INTO [t10340] VALUES (5,'مؤلفاته','17','0','0');
+INSERT INTO [t10340] VALUES (6,'مكانته العلمية','18','0','0');
+INSERT INTO [t10340] VALUES (7,'وفاته','21','0','0');
+INSERT INTO [t10340] VALUES (8,'نسخ التحقيق','23','0','0');
+INSERT INTO [t10340] VALUES (9,'منهج الكتاب','26','0','0');
+INSERT INTO [t10340] VALUES (10,'مذهب المؤلف النحوي','28','0','0');
+INSERT INTO [t10340] VALUES (11,'بسم الله الرحمن الرحيم','31','0','0');
+INSERT INTO [t10340] VALUES (12,'مصادر ومراجع','77','0','0');
