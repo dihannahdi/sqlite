13f56209ae768691c6a386245dafdee9428707d3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:10:25 +0700

    Upload SQL chunk: t130963_chunk_0000.sql

---
+++ sql_chunks/t130963_chunk_0000.sql
@@ -0,0 +1,51 @@
+-- Table: t130963
+-- Chunk: 1
+-- Rows: 44
+
+CREATE TABLE "t130963" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t130963
+INSERT INTO [t130963] VALUES (1,'مقدمة التحقيق','4','0','0');
+INSERT INTO [t130963] VALUES (2,'ترجمة المصنف','5','1','0');
+INSERT INTO [t130963] VALUES (3,'الكتاب','8','1','0');
+INSERT INTO [t130963] VALUES (4,'مخطوط الكتاب','8','1','0');
+INSERT INTO [t130963] VALUES (5,'(باب السين)','9','1','0');
+INSERT INTO [t130963] VALUES (6,'تداول العلماء الكتاب','9','1','0');
+INSERT INTO [t130963] VALUES (7,'منهج المصنف في الكتاب','10','1','0');
+INSERT INTO [t130963] VALUES (8,'عناصر الترجمة','11','1','0');
+INSERT INTO [t130963] VALUES (9,'تحقيق الكتاب','12','1','0');
+INSERT INTO [t130963] VALUES (10,'باب إبراهيم','14','0','0');
+INSERT INTO [t130963] VALUES (11,'باب أحمد','17','0','0');
+INSERT INTO [t130963] VALUES (12,'باب إسماعيل','29','0','0');
+INSERT INTO [t130963] VALUES (13,'باب إسحاق','30','0','0');
+INSERT INTO [t130963] VALUES (14,'باب الأفراد','31','0','0');
+INSERT INTO [t130963] VALUES (15,'باب الباء','34','0','0');
+INSERT INTO [t130963] VALUES (16,'باب التاء','36','0','0');
+INSERT INTO [t130963] VALUES (17,'باب الثاء','37','0','0');
+INSERT INTO [t130963] VALUES (18,'باب الجيم','39','0','0');
+INSERT INTO [t130963] VALUES (19,'باب الحسن','42','0','0');
+INSERT INTO [t130963] VALUES (20,'باب الحسين','47','0','0');
+INSERT INTO [t130963] VALUES (21,'باب الخاء','51','0','0');
+INSERT INTO [t130963] VALUES (22,'باب الدال','52','0','0');
+INSERT INTO [t130963] VALUES (23,'باب الذال','55','0','0');
+INSERT INTO [t130963] VALUES (24,'باب الراء','58','0','0');
+INSERT INTO [t130963] VALUES (25,'باب الزاي','60','0','0');
+INSERT INTO [t130963] VALUES (26,'باب السين','61','0','0');
+INSERT INTO [t130963] VALUES (27,'باب الشين','64','0','0');
+INSERT INTO [t130963] VALUES (28,'باب الصاد','65','0','0');
+INSERT INTO [t130963] VALUES (29,'باب الضاد','66','0','0');
+INSERT INTO [t130963] VALUES (30,'باب الطاء','67','0','0');
+INSERT INTO [t130963] VALUES (31,'باب الظاء','67','0','0');
+INSERT INTO [t130963] VALUES (32,'باب العين','68','0','0');
+INSERT INTO [t130963] VALUES (33,'باب عمر','73','32','0');
+INSERT INTO [t130963] VALUES (34,'باب عثمان','76','32','0');
+INSERT INTO [t130963] VALUES (35,'باب علي','78','32','0');
+INSERT INTO [t130963] VALUES (36,'باب عمرو','83','32','0');
+INSERT INTO [t130963] VALUES (37,'باب الغين','94','0','0');
+INSERT INTO [t130963] VALUES (38,'باب الفاء','95','0','0');
+INSERT INTO [t130963] VALUES (39,'باب القاف','96','0','0');
+INSERT INTO [t130963] VALUES (40,'باب الكاف','102','0','0');
+INSERT INTO [t130963] VALUES (41,'باب اللام','103','0','0');
+INSERT INTO [t130963] VALUES (42,'باب الميم','104','0','0');
+INSERT INTO [t130963] VALUES (43,'فهرس الكتب','106','0','0');
+INSERT INTO [t130963] VALUES (44,'فهرس كتب للمحقق','106','0','0');
