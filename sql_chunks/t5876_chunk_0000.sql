208fd2f86349352cf6114916e772e7a2a63ddaf1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:07 +0700

    Upload SQL chunk: t5876_chunk_0000.sql

---
+++ sql_chunks/t5876_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t5876
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t5876" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5876
+INSERT INTO [t5876] VALUES (1,'أخبار المكيين','1','0','0');
+INSERT INTO [t5876] VALUES (2,'ولادة النبي صلى الله عليه وسلم','51','0','0');
+INSERT INTO [t5876] VALUES (3,'علي بن أبي طالب رضي الله عنه','65','0','0');
+INSERT INTO [t5876] VALUES (4,'تسمية من نزل مكة من أصحاب رسول الله صلى الله عليه وسلم','126','0','0');
