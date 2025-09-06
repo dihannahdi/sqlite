e5d15ceeb602cda13211707ce4f1a5528a9218bd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:19 +0700

    Upload SQL chunk: t1119_chunk_0000.sql

---
+++ sql_chunks/t1119_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t1119
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t1119" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1119
+INSERT INTO [t1119] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1119] VALUES (2,'من تجليات وسطية الإسلام','10','0','0');
+INSERT INTO [t1119] VALUES (3,'تكريم الإنسان','10','2','0');
+INSERT INTO [t1119] VALUES (4,'جعل تحيته السلام وعلاقاته مع الآخرين السلام','19','2','0');
+INSERT INTO [t1119] VALUES (5,'ربط العلم والتقنية بالقيم الخلقية','25','0','0');
+INSERT INTO [t1119] VALUES (6,'الأدعية والأذكار المشروعة تزكي القيم الروحية والخلقية','27','0','0');
+INSERT INTO [t1119] VALUES (7,'حد الأخلاق الإسلامية وضابطها','28','0','0');
