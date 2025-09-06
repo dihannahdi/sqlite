d955e4f2796c3689488d90e9d098abe86db1ebdf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:01 +0700

    Upload SQL chunk: t8184_chunk_0000.sql

---
+++ sql_chunks/t8184_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t8184
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t8184" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8184
+INSERT INTO [t8184] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t8184] VALUES (2,'تفسير الأبيات','40','0','0');
+INSERT INTO [t8184] VALUES (3,'سنة ثلاث وعشرين وثلاثمائة','61','0','0');
+INSERT INTO [t8184] VALUES (4,'سنة أربع وعشرين وثلاثمائة','70','0','0');
+INSERT INTO [t8184] VALUES (5,'سنة خمس وعشرين وثلاثمائة','86','0','0');
+INSERT INTO [t8184] VALUES (6,'سنة ست وعشرين وثلاثمائة','90','0','0');
+INSERT INTO [t8184] VALUES (7,'سنة سبع وعشرين وثلاثمائة','108','0','0');
+INSERT INTO [t8184] VALUES (8,'سنة ثمان وعشرين وثلاثمائة','138','0','0');
+INSERT INTO [t8184] VALUES (9,'سنة تسع وعشرين وثلاثمائة','145','0','0');
+INSERT INTO [t8184] VALUES (10,'أشعار الراضي بالله','154','9','0');
+INSERT INTO [t8184] VALUES (11,'قافية الباء','154','9','0');
+INSERT INTO [t8184] VALUES (12,'وقال على','163','9','0');
+INSERT INTO [t8184] VALUES (13,'وقال على','163','9','0');
+INSERT INTO [t8184] VALUES (14,'قافية التاء','163','9','0');
+INSERT INTO [t8184] VALUES (15,'قافية الجيم','164','9','0');
+INSERT INTO [t8184] VALUES (16,'قافية الحاء','165','9','0');
+INSERT INTO [t8184] VALUES (17,'قافية الدال','166','9','0');
+INSERT INTO [t8184] VALUES (18,'قافية الراء','168','9','0');
+INSERT INTO [t8184] VALUES (19,'قافية السين','175','9','0');
+INSERT INTO [t8184] VALUES (20,'قافية الضاد','176','9','0');
+INSERT INTO [t8184] VALUES (21,'قافية العين','177','9','0');
+INSERT INTO [t8184] VALUES (22,'قافية القاف','177','9','0');
+INSERT INTO [t8184] VALUES (23,'قافية الكاف','178','9','0');
+INSERT INTO [t8184] VALUES (24,'قافية اللام','179','9','0');
+INSERT INTO [t8184] VALUES (25,'قافية الميم','180','9','0');
+INSERT INTO [t8184] VALUES (26,'قافية النون','182','9','0');
+INSERT INTO [t8184] VALUES (27,'قافية الهاء','182','9','0');
+INSERT INTO [t8184] VALUES (28,'وفاة الراضي','183','9','0');
+INSERT INTO [t8184] VALUES (29,'بسم الله الرحمن الرحيم','186','0','0');
+INSERT INTO [t8184] VALUES (30,'أخبار المتقي لله','186','29','0');
+INSERT INTO [t8184] VALUES (31,'ثم دخلت سنة ثلاثين وثلاثمائة','213','0','0');
+INSERT INTO [t8184] VALUES (32,'ثم دخلت سنة إحدى وثلاثين وثلاثمائة','231','0','0');
+INSERT INTO [t8184] VALUES (33,'ثم دخلت سنة اثنتين وثلاثين وثلاثمائة','245','0','0');
+INSERT INTO [t8184] VALUES (34,'وفاة البريدي','259','33','0');
+INSERT INTO [t8184] VALUES (35,'ذكر قتل الترجمان','260','33','0');
+INSERT INTO [t8184] VALUES (36,'ذكر رجوع الأمير أبي الوفاء توزون','267','33','0');
+INSERT INTO [t8184] VALUES (37,'سنة ثلاث وثلاثين وثلاثمائة','276','0','0');
+INSERT INTO [t8184] VALUES (38,'أخر أمر المتقي لله','282','37','0');
+INSERT INTO [t8184] VALUES (39,'ذكر عمال المتقي لله وقت زوال أمره','284','37','0');
