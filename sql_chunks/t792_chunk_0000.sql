8afff97fb4328c25cb4c77d581b1f3851a434998
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:41 +0700

    Upload SQL chunk: t792_chunk_0000.sql

---
+++ sql_chunks/t792_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t792
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t792" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t792
+INSERT INTO [t792] VALUES (1,'القصيدة التائية في القدر','1','0','0');
