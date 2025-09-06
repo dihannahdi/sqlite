340f8318ee1452c76045a84a54f057c726a0304a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:42 +0700

    Upload SQL chunk: t8230_chunk_0000.sql

---
+++ sql_chunks/t8230_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t8230
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t8230" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8230
+INSERT INTO [t8230] VALUES (1,'باب: في المزمار','62','0','0');
+INSERT INTO [t8230] VALUES (2,'باب في الشطرنج','88','0','0');
+INSERT INTO [t8230] VALUES (3,'باب في الشهاردة','102','0','0');
+INSERT INTO [t8230] VALUES (4,'باب في السدر','107','0','0');
+INSERT INTO [t8230] VALUES (5,'باب في المراجيح','109','0','0');
+INSERT INTO [t8230] VALUES (6,'باب في القمار','113','0','0');
+INSERT INTO [t8230] VALUES (7,'باب في اللعب بالحمام','122','0','0');
+INSERT INTO [t8230] VALUES (8,'باب: في عمل قوم لوط','128','0','0');
+INSERT INTO [t8230] VALUES (9,'باب اللوطية في النساء','150','0','0');
+INSERT INTO [t8230] VALUES (10,'ومن ذكر اللوطيين في الرجال','162','0','0');
+INSERT INTO [t8230] VALUES (11,'باب: في المخنثين','168','0','0');
