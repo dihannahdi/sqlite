d69ffe3f317cf46ed5816a01646760f8dd0f46e2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:23:58 +0700

    Upload SQL chunk: t151126_chunk_0000.sql

---
+++ sql_chunks/t151126_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t151126
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t151126" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t151126
+INSERT INTO [t151126] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t151126] VALUES (2,'القسم الأول:','3','0','0');
+INSERT INTO [t151126] VALUES (3,'القسم الثاني:','20','0','0');
