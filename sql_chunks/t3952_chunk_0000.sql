909926967fc34a6a0fa243dc35e881c7718eb3bc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:28 +0700

    Upload SQL chunk: t3952_chunk_0000.sql

---
+++ sql_chunks/t3952_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t3952
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t3952" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3952
+INSERT INTO [t3952] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t3952] VALUES (2,'مقدمة الكاتب','2','0','0');
+INSERT INTO [t3952] VALUES (3,'عبد الله بن سبأ','8','0','0');
+INSERT INTO [t3952] VALUES (4,'الحقيقة في انتساب الشيعة لأهل البيت','13','0','0');
+INSERT INTO [t3952] VALUES (5,'المتعة وما يتعلق بها','32','0','0');
+INSERT INTO [t3952] VALUES (6,'الخمس','55','0','0');
+INSERT INTO [t3952] VALUES (7,'ملخص تطور نظرية الخمس','64','6','0');
+INSERT INTO [t3952] VALUES (8,'الكتب السماوية','73','0','0');
+INSERT INTO [t3952] VALUES (9,'١ - الجامعة:','73','8','0');
+INSERT INTO [t3952] VALUES (10,'٢ - صحيفة الناموس:','74','8','0');
+INSERT INTO [t3952] VALUES (11,'٣ - صحيفة العبيطة:','75','8','0');
+INSERT INTO [t3952] VALUES (12,'٤ - صحيفة ذؤابة السيف:','75','8','0');
+INSERT INTO [t3952] VALUES (13,'٥ - صحيفة علي وهي صحيفة أخرى وجدت في ذؤابة السيف:','76','8','0');
+INSERT INTO [t3952] VALUES (14,'٦ - الجفر: وهو نوعان: الجفر الأبيض والجفر الأحمر:','76','8','0');
+INSERT INTO [t3952] VALUES (15,'٧ - مصحف فاطمة:','77','8','0');
+INSERT INTO [t3952] VALUES (16,'٨ - التوراة والإنجيل والزبور:','78','8','0');
+INSERT INTO [t3952] VALUES (17,'٩ - القرآن:','78','8','0');
+INSERT INTO [t3952] VALUES (18,'نظرة الشيعة إلى أهل السنة','82','0','0');
+INSERT INTO [t3952] VALUES (19,'أثر العناصر الأجنبية في صنع التشيع','92','0','0');
+INSERT INTO [t3952] VALUES (20,'الخاتمة','114','0','0');
