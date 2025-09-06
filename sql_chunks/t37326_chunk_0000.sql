3e43ff5c513ab734b63983a24abe4bf618f0e341
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:19 +0700

    Upload SQL chunk: t37326_chunk_0000.sql

---
+++ sql_chunks/t37326_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t37326
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t37326" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37326
+INSERT INTO [t37326] VALUES (1,'باب نواقض الوضوء','1','0','0');
+INSERT INTO [t37326] VALUES (2,'باب أقسام المياه التي يجوز منها الوضوء','2','0','0');
+INSERT INTO [t37326] VALUES (3,'باب فرائض الوضوء وسننه وفضائله','3','0','0');
+INSERT INTO [t37326] VALUES (4,'باب فرائض الغسل وسننه وفضائله','3','0','0');
+INSERT INTO [t37326] VALUES (5,'باب التيمم','4','0','0');
+INSERT INTO [t37326] VALUES (6,'باب شروط الصلاة','4','0','0');
+INSERT INTO [t37326] VALUES (7,'باب فرائض الصلاة وفضائلها ومكروهاتها','4','0','0');
+INSERT INTO [t37326] VALUES (8,'باب مندوبات الصلاة','7','0','0');
+INSERT INTO [t37326] VALUES (9,'باب مفسدات الصلاة','8','0','0');
+INSERT INTO [t37326] VALUES (10,'باب سجود السهو','8','0','0');
+INSERT INTO [t37326] VALUES (11,'باب في الإمامة','9','0','0');
+INSERT INTO [t37326] VALUES (12,'باب صلاة الجمعة','11','0','0');
+INSERT INTO [t37326] VALUES (13,'باب صلاة الجنازة','13','0','0');
+INSERT INTO [t37326] VALUES (14,'باب الصيام','15','0','0');
