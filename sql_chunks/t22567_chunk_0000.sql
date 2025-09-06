8afa05476709be3f53680cad4ba55314770c528f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:41 +0700

    Upload SQL chunk: t22567_chunk_0000.sql

---
+++ sql_chunks/t22567_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t22567
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t22567" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22567
+INSERT INTO [t22567] VALUES (1,'الديانة الهندوسية','15','0','0');
+INSERT INTO [t22567] VALUES (2,'عقيدة التناسخ','15','1','0');
+INSERT INTO [t22567] VALUES (3,'مسألة القيامة','16','1','0');
+INSERT INTO [t22567] VALUES (4,'الكلام الأخير','20','1','0');
+INSERT INTO [t22567] VALUES (5,'الديانة الجينية','21','0','0');
+INSERT INTO [t22567] VALUES (6,'الجينية "جين دهرم"','21','5','0');
+INSERT INTO [t22567] VALUES (7,'موجز تاريخها','22','5','0');
+INSERT INTO [t22567] VALUES (8,'الفرق والمذهب','23','5','0');
+INSERT INTO [t22567] VALUES (9,'ترتيب تعليمات مها بيرسوامي','24','5','0');
+INSERT INTO [t22567] VALUES (10,'النقد التاريخي','25','5','0');
+INSERT INTO [t22567] VALUES (11,'الاختلاف الأساسي بين الفرقة الدكمبرية السونبابرية','27','5','0');
+INSERT INTO [t22567] VALUES (12,'العقائد الجينية','28','0','0');
+INSERT INTO [t22567] VALUES (13,'الألوهية عند الجينيين','29','0','0');
+INSERT INTO [t22567] VALUES (14,'طبقات الرجال','31','0','0');
+INSERT INTO [t22567] VALUES (15,'فلسفة عبادة الأصنام','32','0','0');
+INSERT INTO [t22567] VALUES (16,'مدخل','32','15','0');
+INSERT INTO [t22567] VALUES (17,'أثر جين دهرم على ويدك دهرم وبدها درهم','34','15','0');
