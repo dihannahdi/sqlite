8753715a764432d3dadec6603b8bc092f847cac6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:36 +0700

    Upload SQL chunk: t12152_chunk_0000.sql

---
+++ sql_chunks/t12152_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t12152
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t12152" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12152
+INSERT INTO [t12152] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12152] VALUES (2,'المبحث الأول: تعريف النكاح وأدلة مشروعيته','6','0','0');
+INSERT INTO [t12152] VALUES (3,'المبحث الثاني: حكم النكاح','12','0','0');
+INSERT INTO [t12152] VALUES (4,'المبحث الثالث: الأسس التي وضعها لاختيار الزوجين','18','0','0');
+INSERT INTO [t12152] VALUES (5,'المبحث الرابع: في الخطبة والخطبة','24','0','0');
+INSERT INTO [t12152] VALUES (6,'المبحث الخامس: الإذن في النكاح','43','0','0');
+INSERT INTO [t12152] VALUES (7,'المبحث السادس: أركان عقد النكاح','53','0','0');
+INSERT INTO [t12152] VALUES (8,'المبحث السابع: الولاية في النكاح','61','0','0');
+INSERT INTO [t12152] VALUES (9,'المبحث الثامن: الكفاءة في النكاح','85','0','0');
+INSERT INTO [t12152] VALUES (10,'المبحث التاسع: الشهادة في النكاح','98','0','0');
+INSERT INTO [t12152] VALUES (11,'مصادر ومراجع','114','0','0');
