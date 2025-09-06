fb5a374d5fcc11d64ed98c4a40a811e26baefd03
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:37 +0700

    Upload SQL chunk: t21554_chunk_0000.sql

---
+++ sql_chunks/t21554_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t21554
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t21554" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21554
+INSERT INTO [t21554] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t21554] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t21554] VALUES (3,'الحمد لله رب العالمين','1','0','0');
+INSERT INTO [t21554] VALUES (4,'باب من التعازي','4','0','0');
+INSERT INTO [t21554] VALUES (5,'باب من الشعر','12','0','0');
+INSERT INTO [t21554] VALUES (6,'باب من التعازي والمواعظ','38','0','0');
+INSERT INTO [t21554] VALUES (7,'باب من التعازي والتعزي في الأشعار','67','0','0');
+INSERT INTO [t21554] VALUES (8,'باب وصايا','97','0','0');
+INSERT INTO [t21554] VALUES (9,'وصية أبي بكر الصديق','97','8','0');
+INSERT INTO [t21554] VALUES (10,'رحمه الله','97','8','0');
+INSERT INTO [t21554] VALUES (11,'رحمه الله','97','8','0');
+INSERT INTO [t21554] VALUES (12,'رحمه الله','97','8','0');
+INSERT INTO [t21554] VALUES (13,'رحمه الله','97','8','0');
+INSERT INTO [t21554] VALUES (14,'وصية عمر بن الخطاب','98','8','0');
+INSERT INTO [t21554] VALUES (15,'وصية علي بن أبي طالب','99','8','0');
+INSERT INTO [t21554] VALUES (16,'وصية معاوية بن أبي سفيان','100','8','0');
+INSERT INTO [t21554] VALUES (17,'وصية أبي عبيدة بن الجراح','103','8','0');
+INSERT INTO [t21554] VALUES (18,'رحمه الله','103','8','0');
+INSERT INTO [t21554] VALUES (19,'وصية عبد الملك بن مروان','104','8','0');
+INSERT INTO [t21554] VALUES (20,'وصية الربيع بن خثيم','108','8','0');
+INSERT INTO [t21554] VALUES (21,'وصية جندب بن عبد الله البجلي','109','8','0');
+INSERT INTO [t21554] VALUES (22,'وصية المهلب بن أبي صفرة الأزدي','111','8','0');
+INSERT INTO [t21554] VALUES (23,'باب مراث من أشعار المحدثين','129','0','0');
+INSERT INTO [t21554] VALUES (24,'باب مواعظ وتعاز وأشعار','153','23','0');
+INSERT INTO [t21554] VALUES (25,'باب الجفاة عند الموت','200','23','0');
+INSERT INTO [t21554] VALUES (26,'باب من تكلم في مرضه','215','23','0');
+INSERT INTO [t21554] VALUES (27,'بشيء حكي عنه','215','23','0');
