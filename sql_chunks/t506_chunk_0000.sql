f85110f5d648b95e6add00af042c69a9cf536621
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:40 +0700

    Upload SQL chunk: t506_chunk_0000.sql

---
+++ sql_chunks/t506_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t506
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t506" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t506
+INSERT INTO [t506] VALUES (1,'لا تثبت لأحد من الأئمة العصمة عن الخطأ والغلط والزلل','2','0','0');
+INSERT INTO [t506] VALUES (2,'لم يشعر الأئمة أنها ستنشأ لهم مذاهب على هذا النحو المشاهد','3','0','0');
+INSERT INTO [t506] VALUES (3,'لم تطبق الأمة على هجر الكتاب والسنة','4','0','0');
+INSERT INTO [t506] VALUES (4,'تعصب المحامين عن المذهب وحيفهم على الأدلة','5','0','0');
