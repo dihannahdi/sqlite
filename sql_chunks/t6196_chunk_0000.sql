9bbfc6fe36c298aa7fe06e87a42319b56691ce63
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:48 +0700

    Upload SQL chunk: t6196_chunk_0000.sql

---
+++ sql_chunks/t6196_chunk_0000.sql
@@ -0,0 +1,52 @@
+-- Table: t6196
+-- Chunk: 1
+-- Rows: 45
+
+CREATE TABLE "t6196" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6196
+INSERT INTO [t6196] VALUES (1,'الوضوء','15','0','0');
+INSERT INTO [t6196] VALUES (2,'الاستنجاء','23','0','0');
+INSERT INTO [t6196] VALUES (3,'نواقض الوضوء','26','0','0');
+INSERT INTO [t6196] VALUES (4,'الغسل','31','0','0');
+INSERT INTO [t6196] VALUES (5,'التيمم','36','0','0');
+INSERT INTO [t6196] VALUES (6,'المسح على الخفين','42','0','0');
+INSERT INTO [t6196] VALUES (7,'الحيض','46','0','0');
+INSERT INTO [t6196] VALUES (8,'النفاس','50','0','0');
+INSERT INTO [t6196] VALUES (9,'كتاب الصلاة','51','0','0');
+INSERT INTO [t6196] VALUES (10,'الأذان','57','0','0');
+INSERT INTO [t6196] VALUES (11,'وللصلاة شروط وفرائض وسنن وفضائل','60','0','0');
+INSERT INTO [t6196] VALUES (12,'صلاة الجماعة','78','0','0');
+INSERT INTO [t6196] VALUES (13,'القصر','87','0','0');
+INSERT INTO [t6196] VALUES (14,'الجمع','91','0','0');
+INSERT INTO [t6196] VALUES (15,'الجمعة','93','0','0');
+INSERT INTO [t6196] VALUES (16,'صلاة الخوف','97','0','0');
+INSERT INTO [t6196] VALUES (17,'صلاة العيدين','99','0','0');
+INSERT INTO [t6196] VALUES (18,'صلاة الكسوف','102','0','0');
+INSERT INTO [t6196] VALUES (19,'وصلاة الخسوف','103','0','0');
+INSERT INTO [t6196] VALUES (20,'صلاة الاستسقاء','103','0','0');
+INSERT INTO [t6196] VALUES (21,'صلاة التطوع','104','0','0');
+INSERT INTO [t6196] VALUES (22,'الجنائز','108','0','0');
+INSERT INTO [t6196] VALUES (23,'والمقدم','110','0','0');
+INSERT INTO [t6196] VALUES (24,'الزكاة','115','0','0');
+INSERT INTO [t6196] VALUES (25,'المعدن والركاز','124','0','0');
+INSERT INTO [t6196] VALUES (26,'ومصرف الزكاة','135','0','0');
+INSERT INTO [t6196] VALUES (27,'صدقة الفطر','138','0','0');
+INSERT INTO [t6196] VALUES (28,'الصيام','140','0','0');
+INSERT INTO [t6196] VALUES (29,'الاعتكاف','151','0','0');
+INSERT INTO [t6196] VALUES (30,'الحج','154','0','0');
+INSERT INTO [t6196] VALUES (31,'الصيد','190','0','0');
+INSERT INTO [t6196] VALUES (32,'الذبائح','194','0','0');
+INSERT INTO [t6196] VALUES (33,'كتاب الأضحية','199','0','0');
+INSERT INTO [t6196] VALUES (34,'الجهاد','214','0','0');
+INSERT INTO [t6196] VALUES (35,'الجزية','219','0','0');
+INSERT INTO [t6196] VALUES (36,'كتاب النكاح','226','0','0');
+INSERT INTO [t6196] VALUES (37,'الوليمة','256','0','0');
+INSERT INTO [t6196] VALUES (38,'الإيلاء','277','0','0');
+INSERT INTO [t6196] VALUES (39,'الرضاع','300','0','0');
+INSERT INTO [t6196] VALUES (40,'والحضانة','306','0','0');
+INSERT INTO [t6196] VALUES (41,'كتاب البيوع','308','0','0');
+INSERT INTO [t6196] VALUES (42,'بيوع الآجال','323','0','0');
+INSERT INTO [t6196] VALUES (43,'الخيار','327','0','0');
+INSERT INTO [t6196] VALUES (44,'كتاب السلم','341','0','0');
+INSERT INTO [t6196] VALUES (45,'القرض','345','0','0');
