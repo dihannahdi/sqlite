51920714de450f3a2482bea9566c3b5c05c5bbb5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:10 +0700

    Upload SQL chunk: t29464_chunk_0000.sql

---
+++ sql_chunks/t29464_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t29464
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t29464" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29464
+INSERT INTO [t29464] VALUES (1,': «المتبايعان كل واحد منهما على صاحبه بالخيار ما لم يتفرقا إلا بيع الخيار» ، أخرجه البخاري، عن','1','0','0');
+INSERT INTO [t29464] VALUES (2,': أي الإسنادين أحب إليكم؟ الأعمش عن أبي وائل، عن عبد الله، أو سفيان، عن منصور، عن إبراهيم،','3','0','0');
