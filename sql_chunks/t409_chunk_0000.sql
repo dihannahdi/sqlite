3ee180e5f72e731ab5fe56bfc3fb5d938495b8ef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:58 +0700

    Upload SQL chunk: t409_chunk_0000.sql

---
+++ sql_chunks/t409_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t409
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t409" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t409
+INSERT INTO [t409] VALUES (1,'معنى «إذا» في الحديث','2','0','0');
+INSERT INTO [t409] VALUES (2,'معنى حديثي أبي هريرة وابن عمرو واحد','3','0','0');
+INSERT INTO [t409] VALUES (3,'توجيه ما رواه مقاتل عن الحسن مرسلا','4','0','0');
+INSERT INTO [t409] VALUES (4,'خلاصة البحث','6','0','0');
+INSERT INTO [t409] VALUES (5,'ما ورد في وصف كل واحدة من الخصال الثلاث بأنها كفر، وأن نقائضها من شعب الإيمان','7','4','0');
