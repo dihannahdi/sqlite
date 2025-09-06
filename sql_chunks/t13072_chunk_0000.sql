3c4d26db7b87942ff0a3e775aa9672c47bdbab12
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:10:01 +0700

    Upload SQL chunk: t13072_chunk_0000.sql

---
+++ sql_chunks/t13072_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t13072
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t13072" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13072
+INSERT INTO [t13072] VALUES (1,'بشر بن سحيم','1','0','0');
+INSERT INTO [t13072] VALUES (2,'عطية السعدي','3','0','0');
+INSERT INTO [t13072] VALUES (3,'أبو عبيد','6','0','0');
+INSERT INTO [t13072] VALUES (4,'الأسلع','8','0','0');
+INSERT INTO [t13072] VALUES (5,'عبد الله بن أبي الحمساء','10','0','0');
+INSERT INTO [t13072] VALUES (6,'أبو عامر - أو أبو مالك - الأشعري','13','0','0');
+INSERT INTO [t13072] VALUES (7,'أبو زهير الأنماري','15','0','0');
+INSERT INTO [t13072] VALUES (8,'حصين بن مشمت','17','0','0');
+INSERT INTO [t13072] VALUES (9,'عتبة بن فرقد','19','0','0');
+INSERT INTO [t13072] VALUES (10,'الشريد بن سويد','21','0','0');
+INSERT INTO [t13072] VALUES (11,'بديل بن ورقاء','25','0','0');
+INSERT INTO [t13072] VALUES (12,'أبو لاس الخزاعي','28','0','0');
+INSERT INTO [t13072] VALUES (13,'حمزة بن عمرو الأسلمي','30','0','0');
+INSERT INTO [t13072] VALUES (14,'ربيعة بن كعب الأسلمي','32','0','0');
+INSERT INTO [t13072] VALUES (15,'نبيشة الهذلي','34','0','0');
