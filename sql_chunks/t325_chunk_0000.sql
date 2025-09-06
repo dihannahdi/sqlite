2171d7898c79287b061b1bb6f55acdb01550ef90
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:09 +0700

    Upload SQL chunk: t325_chunk_0000.sql

---
+++ sql_chunks/t325_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t325
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t325" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t325
+INSERT INTO [t325] VALUES (1,'نظم الأسماء المؤنثة السماعية','2','0','0');
+INSERT INTO [t325] VALUES (2,'نظم جموع (عبد)','4','0','0');
+INSERT INTO [t325] VALUES (3,'نظم جموع شيخ','5','0','0');
