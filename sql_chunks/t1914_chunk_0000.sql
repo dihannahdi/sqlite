75c13754ce20880112b3ec39651a41bc07a92f1f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:02 +0700

    Upload SQL chunk: t1914_chunk_0000.sql

---
+++ sql_chunks/t1914_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t1914
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t1914" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1914
+INSERT INTO [t1914] VALUES (1,'مفهوم العبادة في الإسلام','1','0','0');
+INSERT INTO [t1914] VALUES (2,'شمول العبادة','2','0','0');
+INSERT INTO [t1914] VALUES (3,'الغاية من العبادة','4','0','0');
+INSERT INTO [t1914] VALUES (4,'ثمرات العبادة','5','0','0');
+INSERT INTO [t1914] VALUES (5,'أولا تربية الروح وتغذيتها','5','4','0');
+INSERT INTO [t1914] VALUES (6,'ثانيا تحقيق حرية الإنسان','6','4','0');
+INSERT INTO [t1914] VALUES (7,'ثالثا تمحيص المؤمن بابتلائه بالعبادة','7','4','0');
+INSERT INTO [t1914] VALUES (8,'رابعا العبادة سبيل لصلاح المجتمع','8','4','0');
+INSERT INTO [t1914] VALUES (9,'الإصلاح الإسلامي لعموم البشر وأهم مبادئه','9','0','0');
+INSERT INTO [t1914] VALUES (10,'أولا أنه لا معبود بحق إلا الله','9','9','0');
+INSERT INTO [t1914] VALUES (11,'ثانيا تحرير العبادة من سلطان الكهنوت','11','9','0');
+INSERT INTO [t1914] VALUES (12,'ثالثا التوازن بين الروح والجسد','14','9','0');
+INSERT INTO [t1914] VALUES (13,'رابعا التيسير ودفع الحرج في العبادات','16','9','0');
+INSERT INTO [t1914] VALUES (14,'شعائر العبادة في الإسلام','17','0','0');
+INSERT INTO [t1914] VALUES (15,'آثار العبادات الأربع','17','14','0');
