34190063826a30a5afde505512ce4b2b0dcc9314
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:13 +0700

    Upload SQL chunk: t37460_chunk_0000.sql

---
+++ sql_chunks/t37460_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t37460
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t37460" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37460
+INSERT INTO [t37460] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t37460] VALUES (2,'قال المتنبي','3','0','0');
+INSERT INTO [t37460] VALUES (3,'نهاية الكتاب','58','0','0');
