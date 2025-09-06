17e952545b20eb738258b9955e5de47639a04203
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:53 +0700

    Upload SQL chunk: t147675_chunk_0000.sql

---
+++ sql_chunks/t147675_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t147675
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t147675" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t147675
+INSERT INTO [t147675] VALUES (1,'[المقدمة]','2','0','0');
+INSERT INTO [t147675] VALUES (2,'[الاختيارات التي ذكرها ابن عبد الهادي في العقود الدرية]','4','0','0');
+INSERT INTO [t147675] VALUES (3,'[الاختيارات التي زادها ابن رجب في ذيل الطبقات]','8','0','0');
