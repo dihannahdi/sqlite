d41eefb569dc886546bc435048aabdca6bef013e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:40 +0700

    Upload SQL chunk: t22823_chunk_0000.sql

---
+++ sql_chunks/t22823_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t22823
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t22823" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22823
+INSERT INTO [t22823] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t22823] VALUES (2,'طريق الخلاص','3','0','0');
+INSERT INTO [t22823] VALUES (3,'كمال هذا الدين','4','0','0');
+INSERT INTO [t22823] VALUES (4,'سبيل المؤمنين','5','0','0');
+INSERT INTO [t22823] VALUES (5,'الكتاب والسنة مصدر كل سعادة','7','0','0');
+INSERT INTO [t22823] VALUES (6,'ذم التفرق وبيان أسبابه','8','0','0');
+INSERT INTO [t22823] VALUES (7,'حفظ هذا الدين وبقاؤه','10','0','0');
+INSERT INTO [t22823] VALUES (8,'وجوب الاعتصام بالكتاب والسنة','11','0','0');
+INSERT INTO [t22823] VALUES (9,'المرجع عند التنازع','13','0','0');
+INSERT INTO [t22823] VALUES (10,'حكم الإحداث في الدين','15','0','0');
+INSERT INTO [t22823] VALUES (11,'حرص الصحابة على لزوم الكتاب والسنة والبعد عن البدع','18','0','0');
+INSERT INTO [t22823] VALUES (12,'من أقوال السلف بعد الصحابة','20','0','0');
+INSERT INTO [t22823] VALUES (13,'السنة والبدعة','22','0','0');
+INSERT INTO [t22823] VALUES (14,'أمثلة من البدع الشائعة بين المسلمين','24','0','0');
+INSERT INTO [t22823] VALUES (15,'أصل بدعة المولود','26','0','0');
+INSERT INTO [t22823] VALUES (16,'دفع توهم','31','0','0');
+INSERT INTO [t22823] VALUES (17,'الحق لا يعرف بكثرة الإتباع','35','0','0');
