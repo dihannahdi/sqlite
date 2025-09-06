69bd7266a6deac00108af1c866b289745083a1bb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:09 +0700

    Upload SQL chunk: t10336_chunk_0000.sql

---
+++ sql_chunks/t10336_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t10336
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t10336" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10336
+INSERT INTO [t10336] VALUES (1,'الحج عرفة','1','0','0');
+INSERT INTO [t10336] VALUES (2,'حدود الموقف','3','0','0');
+INSERT INTO [t10336] VALUES (3,'أسماء جبل الرحمة','5','0','0');
+INSERT INTO [t10336] VALUES (4,'الصلاة في الجبل','7','0','0');
+INSERT INTO [t10336] VALUES (5,'النصح للحجاج','9','0','0');
+INSERT INTO [t10336] VALUES (6,'زمن الوقوف','11','0','0');
+INSERT INTO [t10336] VALUES (7,'الأحكام الفقهية','15','0','0');
