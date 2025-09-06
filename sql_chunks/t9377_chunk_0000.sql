4dec61683b540b71cecb319d0389cc75480fa243
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:53:09 +0700

    Upload SQL chunk: t9377_chunk_0000.sql

---
+++ sql_chunks/t9377_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t9377
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t9377" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9377
+INSERT INTO [t9377] VALUES (1,'الحديث الأول','1','0','0');
+INSERT INTO [t9377] VALUES (2,'الحديث الثاني','3','0','0');
+INSERT INTO [t9377] VALUES (3,'الحديث الثالث','5','0','0');
+INSERT INTO [t9377] VALUES (4,'الحديث الرابع','6','0','0');
+INSERT INTO [t9377] VALUES (5,'الأثر الخامس','7','0','0');
+INSERT INTO [t9377] VALUES (6,'المجلس السادس','9','0','0');
+INSERT INTO [t9377] VALUES (7,'الحديث السادس','9','0','0');
+INSERT INTO [t9377] VALUES (8,'الحديث السابع','11','0','0');
+INSERT INTO [t9377] VALUES (9,'الحديث الثامن','12','0','0');
+INSERT INTO [t9377] VALUES (10,'الحديث التاسع','14','0','0');
+INSERT INTO [t9377] VALUES (11,'المجلس السابع','18','0','0');
+INSERT INTO [t9377] VALUES (12,'الحديث العاشر','18','0','0');
+INSERT INTO [t9377] VALUES (13,'الحديث الحادي عشر','21','0','0');
+INSERT INTO [t9377] VALUES (14,'الحديث الثاني عشر','23','0','0');
+INSERT INTO [t9377] VALUES (15,'المجلس الثامن','26','0','0');
+INSERT INTO [t9377] VALUES (16,'الحديث الثالث عشر','26','0','0');
+INSERT INTO [t9377] VALUES (17,'الحديث الرابع عشر','27','0','0');
+INSERT INTO [t9377] VALUES (18,'الحديث الخامس عشر','30','0','0');
+INSERT INTO [t9377] VALUES (19,'الحديث السادس عشر','31','0','0');
