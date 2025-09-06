af1646d1e4b681b4560b49065289bde2d4551f64
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:29 +0700

    Upload SQL chunk: t425_chunk_0000.sql

---
+++ sql_chunks/t425_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t425
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t425" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t425
+INSERT INTO [t425] VALUES (1,'سبب تأليف الرسالة','2','0','0');
+INSERT INTO [t425] VALUES (2,'دعوى البطلان إما أن تكون لتغيير نظم القرآن أو للإتيان بكلم ليست منه','3','0','0');
+INSERT INTO [t425] VALUES (3,'الرد على الاحتمال الأول','3','0','0');
+INSERT INTO [t425] VALUES (4,'الرد على الاحتمال الثاني','5','0','0');
+INSERT INTO [t425] VALUES (5,'هل يبطل الكلام الصلاة ولو سهوا أو خطأ؟','8','0','0');
+INSERT INTO [t425] VALUES (6,'مقدار الكلام الذي يبطل الصلاة','9','0','0');
+INSERT INTO [t425] VALUES (7,'استطراد في مسألة مدة الإقامة التي يجوز فيها القصر','9','0','0');
+INSERT INTO [t425] VALUES (8,'تحرير حكم القراءة خلف الإمام في الصلاة الجهرية','12','0','0');
+INSERT INTO [t425] VALUES (9,'الدعاء حال قراءة الإمام في الصلاة الجهرية','17','0','0');
