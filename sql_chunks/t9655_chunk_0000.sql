7e33fa274d1d49e3b62233759e36f67551917da2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:04 +0700

    Upload SQL chunk: t9655_chunk_0000.sql

---
+++ sql_chunks/t9655_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9655
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9655" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9655
+INSERT INTO [t9655] VALUES (1,'الباب الأول الرسل والأنبياء','1','0','0');
+INSERT INTO [t9655] VALUES (2,'الباب الثاني الرسالات السماوية','193','0','0');
