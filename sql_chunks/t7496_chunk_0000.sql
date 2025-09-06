45460efa2f7c67ac04b8054ef6f619bade20159e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:59 +0700

    Upload SQL chunk: t7496_chunk_0000.sql

---
+++ sql_chunks/t7496_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t7496
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t7496" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7496
+INSERT INTO [t7496] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7496] VALUES (2,'تعريف الموعظة','4','0','0');
+INSERT INTO [t7496] VALUES (3,'ورود الموعظة في القرآن الكريم','5','0','0');
+INSERT INTO [t7496] VALUES (4,'مقاصد الموعظة وحكمها','7','0','0');
+INSERT INTO [t7496] VALUES (5,'أدب الموعظة','9','0','0');
