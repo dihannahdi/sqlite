c0cba57d283d7084e0e8c5c82e05b65e982c3f99
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:29 +0700

    Upload SQL chunk: t3542_chunk_0000.sql

---
+++ sql_chunks/t3542_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t3542
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t3542" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3542
+INSERT INTO [t3542] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t3542] VALUES (2,'تمهيد','3','0','0');
+INSERT INTO [t3542] VALUES (3,'مدخل','9','0','0');
+INSERT INTO [t3542] VALUES (4,'الشرط الأول: العلم','11','0','0');
+INSERT INTO [t3542] VALUES (5,'الشرط الثاني: القين.','17','0','0');
+INSERT INTO [t3542] VALUES (6,'الشرط الثالث: الإخلاص.','21','0','0');
+INSERT INTO [t3542] VALUES (7,'الشرط الرابع: الصدق.','27','0','0');
+INSERT INTO [t3542] VALUES (8,'الشرط الخامس: المحبة.','32','0','0');
+INSERT INTO [t3542] VALUES (9,'الشرط السادس: الإنقياد.','37','0','0');
+INSERT INTO [t3542] VALUES (10,'الشرط السابع: القبول.','41','0','0');
+INSERT INTO [t3542] VALUES (11,'الخاتمة','45','0','0');
+INSERT INTO [t3542] VALUES (12,'مصادر ومراجع','48','0','0');
