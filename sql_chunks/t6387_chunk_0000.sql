e9448ca2bf3e36534b5664a38c8f5dcaec4d01e9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:19 +0700

    Upload SQL chunk: t6387_chunk_0000.sql

---
+++ sql_chunks/t6387_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t6387
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t6387" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6387
+INSERT INTO [t6387] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6387] VALUES (2,'من أصول أهل السنة والجماعة','1','0','0');
+INSERT INTO [t6387] VALUES (3,'أفضل الفقه وتعريف الإيمان وأركانه','4','0','0');
+INSERT INTO [t6387] VALUES (4,'حكم من كذب بالخلق او انكر معلوما من الدين بالضرورة','11','0','0');
+INSERT INTO [t6387] VALUES (5,'كلامه عن الاستطاعة','15','0','0');
+INSERT INTO [t6387] VALUES (6,'الرد على من زعم ان الله لم يخلق الشر','16','0','0');
+INSERT INTO [t6387] VALUES (7,'القول فيمن يشك في إيمانه','24','0','0');
+INSERT INTO [t6387] VALUES (8,'المؤمن قد يعذب بذنوبه','27','0','0');
+INSERT INTO [t6387] VALUES (9,'الكفار يؤمنون عند المعاينة','29','0','0');
+INSERT INTO [t6387] VALUES (10,'أثر معاذ','32','0','0');
+INSERT INTO [t6387] VALUES (11,'وجوب الهجرة الى الله','35','0','0');
+INSERT INTO [t6387] VALUES (12,'إثبات العلو','36','0','0');
+INSERT INTO [t6387] VALUES (13,'اثبات عذاب القبر','37','0','0');
+INSERT INTO [t6387] VALUES (14,'تحريم التألي على الله','38','0','0');
+INSERT INTO [t6387] VALUES (15,'وجوب لزوم القرآن','40','0','0');
+INSERT INTO [t6387] VALUES (16,'الاستثناء في الإيمان','48','0','0');
