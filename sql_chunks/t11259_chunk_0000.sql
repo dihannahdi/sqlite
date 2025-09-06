e56ae2ce441f667277d2eb5e61c62383a807dd07
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:09 +0700

    Upload SQL chunk: t11259_chunk_0000.sql

---
+++ sql_chunks/t11259_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11259
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11259" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11259
+INSERT INTO [t11259] VALUES (1,'شرح حديث جبريل في تعليم الدين','1','0','0');
+INSERT INTO [t11259] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
