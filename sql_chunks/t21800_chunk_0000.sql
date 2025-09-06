d06e9983d92a87dfed5911cc06aeb0f56c6570ce
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:28 +0700

    Upload SQL chunk: t21800_chunk_0000.sql

---
+++ sql_chunks/t21800_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t21800
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t21800" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21800
+INSERT INTO [t21800] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t21800] VALUES (2,'الأذكار التي تقال صباحا ومساء','4','0','0');
+INSERT INTO [t21800] VALUES (3,'تفصيل الأذكار التي تقال صباحا ومساء','9','0','0');
+INSERT INTO [t21800] VALUES (4,'بعض الأدعية','35','0','0');
