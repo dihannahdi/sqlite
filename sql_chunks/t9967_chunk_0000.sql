d898f3248120b944867d103d0e9ebd3440f2b41c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:20 +0700

    Upload SQL chunk: t9967_chunk_0000.sql

---
+++ sql_chunks/t9967_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t9967
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t9967" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9967
+INSERT INTO [t9967] VALUES (1,'الفصل الأول صفاتهم وقدراتهم','2','0','0');
+INSERT INTO [t9967] VALUES (2,'الفصل الثاني عبادة الملائكة','20','0','0');
+INSERT INTO [t9967] VALUES (3,'الفصل الثالث الملائكة والإنسان','25','0','0');
+INSERT INTO [t9967] VALUES (4,'الفصل الرابع الملائكة وبقية المخلوقات','63','0','0');
+INSERT INTO [t9967] VALUES (5,'الفصل الخامس المفاضلة بين الملائكة وبني البشر','66','0','0');
