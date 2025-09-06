01b4fcd70ef8643b3f59f5f94ea34986c21d15a9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:21 +0700

    Upload SQL chunk: t14041_chunk_0000.sql

---
+++ sql_chunks/t14041_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t14041
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t14041" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14041
+INSERT INTO [t14041] VALUES (1,'باب الألف الممدودة','5','0','0');
+INSERT INTO [t14041] VALUES (2,'باب الألف المهموزة','50','0','0');
+INSERT INTO [t14041] VALUES (3,'باب الألف المهموزة في التسكين','58','0','0');
+INSERT INTO [t14041] VALUES (4,'باب الألف المقصورة','66','0','0');
+INSERT INTO [t14041] VALUES (5,'باب الباء','94','0','0');
+INSERT INTO [t14041] VALUES (6,'باب التاء','177','0','0');
+INSERT INTO [t14041] VALUES (7,'باب الثاء','192','0','0');
+INSERT INTO [t14041] VALUES (8,'باب الجيم','201','0','0');
+INSERT INTO [t14041] VALUES (9,'باب الحاء','229','0','0');
+INSERT INTO [t14041] VALUES (10,'باب الخاء','258','0','0');
+INSERT INTO [t14041] VALUES (11,'باب الدال','265','0','0');
+INSERT INTO [t14041] VALUES (12,'باب الذال','305','0','0');
+INSERT INTO [t14041] VALUES (13,'باب الراء','312','0','0');
+INSERT INTO [t14041] VALUES (14,'باب الزاي','402','0','0');
+INSERT INTO [t14041] VALUES (15,'باب السين','416','0','0');
+INSERT INTO [t14041] VALUES (16,'باب الشين','443','0','0');
+INSERT INTO [t14041] VALUES (17,'باب الصاد','449','0','0');
+INSERT INTO [t14041] VALUES (18,'باب الضاد','458','0','0');
+INSERT INTO [t14041] VALUES (19,'باب الطاء','476','0','0');
+INSERT INTO [t14041] VALUES (20,'باب الظاء','493','0','0');
+INSERT INTO [t14041] VALUES (21,'باب العين','497','0','0');
+INSERT INTO [t14041] VALUES (22,'باب الغين','538','0','0');
+INSERT INTO [t14041] VALUES (23,'باب الفاء','541','0','0');
+INSERT INTO [t14041] VALUES (24,'باب القاف','565','0','0');
+INSERT INTO [t14041] VALUES (25,'باب الكاف','579','0','0');
+INSERT INTO [t14041] VALUES (26,'باب اللام','584','0','0');
+INSERT INTO [t14041] VALUES (27,'باب الميم','597','0','0');
+INSERT INTO [t14041] VALUES (28,'باب النون','613','0','0');
+INSERT INTO [t14041] VALUES (29,'باب الهاء','630','0','0');
+INSERT INTO [t14041] VALUES (30,'باب الواو','640','0','0');
+INSERT INTO [t14041] VALUES (31,'باب الياء','654','0','0');
