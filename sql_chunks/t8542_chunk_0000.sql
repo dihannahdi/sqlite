512764fb1ebaf3b693b476b9ecfa262ae20cee44
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:01 +0700

    Upload SQL chunk: t8542_chunk_0000.sql

---
+++ sql_chunks/t8542_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t8542
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t8542" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8542
+INSERT INTO [t8542] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8542] VALUES (2,'المختلف فيهم','13','0','0');
+INSERT INTO [t8542] VALUES (3,'مصادر ومراجع','74','0','0');
