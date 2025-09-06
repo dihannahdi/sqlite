2e96310741a9a3bca59aef56816aa6cd52708877
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:46:22 +0700

    Upload SQL chunk: t8477_chunk_0000.sql

---
+++ sql_chunks/t8477_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t8477
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t8477" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8477
+INSERT INTO [t8477] VALUES (1,'وفي سنة إحدى','290','0','0');
+INSERT INTO [t8477] VALUES (2,'ثم دخلت سنة ثنتين من تاريخ مقدم رسول الله المدينة','291','0','0');
+INSERT INTO [t8477] VALUES (3,'ثم دخلت سنة ثلاث','307','0','0');
+INSERT INTO [t8477] VALUES (4,'ثم دخلت سنة أربع','310','0','0');
+INSERT INTO [t8477] VALUES (5,'ثم دخلت سنة خمس من التاريخ','312','0','0');
+INSERT INTO [t8477] VALUES (6,'ثم دخلت سنة ست','314','0','0');
+INSERT INTO [t8477] VALUES (7,'ثم دخلت سنة سبع','318','0','0');
+INSERT INTO [t8477] VALUES (8,'ثم دخلت سنة ثمان','322','0','0');
+INSERT INTO [t8477] VALUES (9,'ثم دخلت سنة تسع من التاريخ','330','0','0');
+INSERT INTO [t8477] VALUES (10,'ثم دخلت سنةعشر','340','0','0');
+INSERT INTO [t8477] VALUES (11,'تسمية من نزل بالكوفة من أصحاب النبي صلى الله عليه وسلم','699','0','0');
