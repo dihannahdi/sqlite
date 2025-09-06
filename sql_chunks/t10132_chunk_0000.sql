db66df1b28e28c831ab9d9377fd217e1d4ea0e63
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:57 +0700

    Upload SQL chunk: t10132_chunk_0000.sql

---
+++ sql_chunks/t10132_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t10132
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t10132" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10132
+INSERT INTO [t10132] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10132] VALUES (2,'معنى السنة لغة واصطلاحا','2','0','0');
+INSERT INTO [t10132] VALUES (3,'معنى السنة اصطلاحا','6','0','0');
+INSERT INTO [t10132] VALUES (4,'مكانة السنة ومنزلتها من التشريع الإسلامي','8','0','0');
+INSERT INTO [t10132] VALUES (5,'جهود العلماء في حفظ السنة','19','0','0');
