3a516d3ac77f7bceea2c6b442b2d6137ea92602d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:19 +0700

    Upload SQL chunk: t10914_chunk_0000.sql

---
+++ sql_chunks/t10914_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t10914
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t10914" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10914
+INSERT INTO [t10914] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t10914] VALUES (2,'ابن زيدون','19','0','0');
+INSERT INTO [t10914] VALUES (3,'وقال أبو عامر بن شهيد','46','0','0');
+INSERT INTO [t10914] VALUES (4,'وقال ابن خفاجة','51','0','0');
