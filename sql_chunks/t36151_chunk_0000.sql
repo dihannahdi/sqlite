c31c2deca3b69adaf08de89cb4794f5c041da294
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:47 +0700

    Upload SQL chunk: t36151_chunk_0000.sql

---
+++ sql_chunks/t36151_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t36151
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t36151" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36151
+INSERT INTO [t36151] VALUES (1,'التعريف بالإمام البخاري:','4','0','0');
+INSERT INTO [t36151] VALUES (2,'طلبه للحديث ورحلته:','4','1','0');
+INSERT INTO [t36151] VALUES (3,'شيوخ البخاري:','4','1','0');
+INSERT INTO [t36151] VALUES (4,'تفقهه:','5','1','0');
+INSERT INTO [t36151] VALUES (5,'نبوغه:','5','1','0');
+INSERT INTO [t36151] VALUES (6,'الرواة عنه:','6','1','0');
+INSERT INTO [t36151] VALUES (7,'صفاته وخلقه:','6','1','0');
+INSERT INTO [t36151] VALUES (8,'حفظه وقوة ذاكرته:','7','1','0');
+INSERT INTO [t36151] VALUES (9,'إمامته في الحديث:','7','1','0');
+INSERT INTO [t36151] VALUES (10,'أثره في علم الحديث:','8','1','0');
+INSERT INTO [t36151] VALUES (11,'وفاته:','8','1','0');
+INSERT INTO [t36151] VALUES (12,'" الجامع الصحيح " وطريقة البخاري فيه:','9','0','0');
+INSERT INTO [t36151] VALUES (13,'أهمية التبويب:','10','12','0');
+INSERT INTO [t36151] VALUES (14,'اعتناء العلماء بدارسة تراجم البخاري:','11','12','0');
+INSERT INTO [t36151] VALUES (15,'أنواع التراجم في " صحيح البخاري ":','12','12','0');
+INSERT INTO [t36151] VALUES (16,'أولا - التراجم الظاهرة:','15','15','0');
+INSERT INTO [t36151] VALUES (17,'ثانيا - التراجم الاستنباطية:','22','15','0');
+INSERT INTO [t36151] VALUES (18,'ثالثا - التراجم المرسلة:','26','15','0');
+INSERT INTO [t36151] VALUES (19,'رابعا - التراجم المفردة:','28','15','0');
+INSERT INTO [t36151] VALUES (20,'ثبت المراجع:','30','0','0');
