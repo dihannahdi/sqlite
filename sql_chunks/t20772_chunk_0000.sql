93acbad428b7418bfe94c975d39347ca20f4003a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:36:59 +0700

    Upload SQL chunk: t20772_chunk_0000.sql

---
+++ sql_chunks/t20772_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t20772
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t20772" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t20772
+INSERT INTO [t20772] VALUES (1,'[الكلام على الصفات السلبية عند المتكلمين]','8','0','0');
+INSERT INTO [t20772] VALUES (2,'[الكلام عن الصفات السبع]','11','0','0');
+INSERT INTO [t20772] VALUES (3,'[الصفات الجامعة]','13','0','0');
+INSERT INTO [t20772] VALUES (4,'[الصفات التي اختلف فيها المتكلمون]','17','0','0');
+INSERT INTO [t20772] VALUES (5,'[مناقشة المتكلمين بمقتضى قواعدهم]','29','0','0');
+INSERT INTO [t20772] VALUES (6,'[مقارنة بين ما سموه مذهب السلف ومذهب الخلف]','34','0','0');
