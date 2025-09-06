5835dbf9c5f83a0f21c506a41b712baad8396fce
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:04 +0700

    Upload SQL chunk: t74_chunk_0000.sql

---
+++ sql_chunks/t74_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t74
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t74" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t74
+INSERT INTO [t74] VALUES (1,'تمهيد','2','0','0');
+INSERT INTO [t74] VALUES (2,'المقدمة','5','0','0');
+INSERT INTO [t74] VALUES (3,'تعريف المحبة لغة واصطلاحاً','5','2','0');
+INSERT INTO [t74] VALUES (4,'المحبة لغة','5','3','0');
+INSERT INTO [t74] VALUES (5,'المحبة اصطلاحاً','5','3','0');
+INSERT INTO [t74] VALUES (6,'الفصل الأول: مذهب أهل السنة والجماعة في صفة المحبة لله عز وجل','6','0','0');
+INSERT INTO [t74] VALUES (7,'الفصل الثاني: مذهب المعطلة في صفة المحبة لله عز وجل','9','0','0');
+INSERT INTO [t74] VALUES (8,'الخاتمة','21','0','0');
+INSERT INTO [t74] VALUES (9,'فهرس المراجع','22','0','0');
