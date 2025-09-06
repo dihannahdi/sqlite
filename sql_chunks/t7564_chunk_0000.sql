50dcef7f2cb025d38ead0df6991fd1ca6d5e6511
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:56 +0700

    Upload SQL chunk: t7564_chunk_0000.sql

---
+++ sql_chunks/t7564_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7564
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7564" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7564
+INSERT INTO [t7564] VALUES (1,'الإيمان','1','0','0');
+INSERT INTO [t7564] VALUES (2,'فهرس الموضوعات','353','0','0');
