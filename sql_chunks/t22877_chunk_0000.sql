6da1a8ffe3739ce92e467fffb599e8f7d1f6603b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:57 +0700

    Upload SQL chunk: t22877_chunk_0000.sql

---
+++ sql_chunks/t22877_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t22877
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t22877" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22877
+INSERT INTO [t22877] VALUES (1,'[خطبة المؤلف]','2','0','0');
+INSERT INTO [t22877] VALUES (2,'فصول في الإمامة','2','0','0');
+INSERT INTO [t22877] VALUES (3,'فصل فأما ولاية القضاة','43','0','0');
+INSERT INTO [t22877] VALUES (4,'فصل في الولايات على إمامة الصلوات','77','0','0');
+INSERT INTO [t22877] VALUES (5,'فصل: في ولاية الحج','91','0','0');
+INSERT INTO [t22877] VALUES (6,'فصل: في ولايات الصدقات','98','0','0');
+INSERT INTO [t22877] VALUES (7,'فصل في قسمة الفيء والغنيمة','119','0','0');
+INSERT INTO [t22877] VALUES (8,'فصل في وضع الخراج والجزية','136','0','0');
+INSERT INTO [t22877] VALUES (9,'فصل فيم تختلف أحكامه من البلاد','168','0','0');
+INSERT INTO [t22877] VALUES (10,'فصل في إحياء الموات، واستخراج المياه','190','0','0');
+INSERT INTO [t22877] VALUES (11,'فصل في الحمي والإرفاق وحمي الموات:','203','0','0');
+INSERT INTO [t22877] VALUES (12,'فصل في أحكام القطائع','208','0','0');
+INSERT INTO [t22877] VALUES (13,'فصل في وضع الديوان، وذكر أحكامه','217','0','0');
