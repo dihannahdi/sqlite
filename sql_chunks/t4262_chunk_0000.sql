64af7b5add155b103c4f76e6e204a46fb3d8c91e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:32 +0700

    Upload SQL chunk: t4262_chunk_0000.sql

---
+++ sql_chunks/t4262_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t4262
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t4262" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4262
+INSERT INTO [t4262] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t4262] VALUES (2,'الفصل الأول: الدراسة','5','0','0');
+INSERT INTO [t4262] VALUES (3,'المبحث الأول: التعريف بعبد الله بن أحمد.','5','2','0');
+INSERT INTO [t4262] VALUES (4,'المبحث الثاني: التعريف بالقطيعي.','8','2','0');
+INSERT INTO [t4262] VALUES (5,'المبحث الثالث: التعريف بمسند الإمام أحمد.','18','2','0');
+INSERT INTO [t4262] VALUES (6,'المبحث الرابع: التعريف بزيادات الرواة.','25','2','0');
+INSERT INTO [t4262] VALUES (7,'المطلب الأول: معناها','25','6','0');
+INSERT INTO [t4262] VALUES (8,'المطلب الثاني: أنواعها','26','6','0');
+INSERT INTO [t4262] VALUES (9,'المبحث الخامس: التعريف بزيادات القطيعي.','32','2','0');
+INSERT INTO [t4262] VALUES (10,'الفصل الثاني: تخريج زيادات القطيعي','35','0','0');
+INSERT INTO [t4262] VALUES (11,'المبحث الأول: الحديث الأول','35','10','0');
+INSERT INTO [t4262] VALUES (12,'المبحث الثاني: الحديث الثاني','42','10','0');
+INSERT INTO [t4262] VALUES (13,'المبحث الثالث: الحديث الثالث','51','10','0');
+INSERT INTO [t4262] VALUES (14,'المبحث الرابع: الحديث الرابع','58','10','0');
+INSERT INTO [t4262] VALUES (15,'الخاتمة','65','0','0');
+INSERT INTO [t4262] VALUES (16,'مصادر ومراجع','67','0','0');
