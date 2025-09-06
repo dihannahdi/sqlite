8720beea9900f1479a2435ee49e15d75f220f9a4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:33 +0700

    Upload SQL chunk: t26565_chunk_0000.sql

---
+++ sql_chunks/t26565_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t26565
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t26565" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26565
+INSERT INTO [t26565] VALUES (1,'جزء في مصافحة الإمام مسلم والإمام النسائي','1','0','0');
+INSERT INTO [t26565] VALUES (2,'حديث مصافحة لمسلم','4','0','0');
+INSERT INTO [t26565] VALUES (3,'حديث مصافحة للنسائي رحمه الله','18','0','0');
+INSERT INTO [t26565] VALUES (4,'حديث مصافحة النسائي أيضا','23','0','0');
+INSERT INTO [t26565] VALUES (5,'حديث مصافحة النسائي أيضا','25','0','0');
+INSERT INTO [t26565] VALUES (6,'حديث مصافحة للنسائي أيضا','29','0','0');
+INSERT INTO [t26565] VALUES (7,'حديث سابع مصافحة لمسلم وأبي داود ألحقه به المخرج','36','0','0');
+INSERT INTO [t26565] VALUES (8,'حديث ثامن مصاحفة ألحقه به أيضا','40','0','0');
