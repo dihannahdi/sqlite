13136a05e3bf76f68edd3cf4a980b304992c9167
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:23 +0700

    Upload SQL chunk: t1372_chunk_0000.sql

---
+++ sql_chunks/t1372_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t1372
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t1372" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1372
+INSERT INTO [t1372] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t1372] VALUES (2,'مبادئ وسطية الإسلام وسماحته','5','0','0');
+INSERT INTO [t1372] VALUES (3,'أولا وسطية الإسلام','5','2','0');
+INSERT INTO [t1372] VALUES (4,'ثانيا في دلالة سماحة الإسلام','13','2','0');
+INSERT INTO [t1372] VALUES (5,'ثالثا دعوة الإسلام إلى الحوار','17','2','0');
+INSERT INTO [t1372] VALUES (6,'منطلقات الحوار وموضوعاته وأهدافه','23','0','0');
+INSERT INTO [t1372] VALUES (7,'أولا منطلقات الحوار من منظور إسلامي','23','6','0');
+INSERT INTO [t1372] VALUES (8,'ثانيا موضوعات الحوار','28','6','0');
+INSERT INTO [t1372] VALUES (9,'ثالثا أهداف الحوار','33','6','0');
+INSERT INTO [t1372] VALUES (10,'رابعا رسالة الوسطية والسماحة والحوار','35','0','0');
