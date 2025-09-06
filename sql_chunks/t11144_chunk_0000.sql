13d71258c838c8c5f176372d2983f04af66b50aa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:40 +0700

    Upload SQL chunk: t11144_chunk_0000.sql

---
+++ sql_chunks/t11144_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t11144
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t11144" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11144
+INSERT INTO [t11144] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11144] VALUES (2,'معرفة الصحيح والضعيف','13','0','0');
+INSERT INTO [t11144] VALUES (3,'اكتشاف كذب الرواة','15','0','0');
+INSERT INTO [t11144] VALUES (4,'ترتيب مصادر الأخبار بحسب قوتها','18','0','0');
+INSERT INTO [t11144] VALUES (5,'النقد الداخلي للأخبار','26','0','0');
+INSERT INTO [t11144] VALUES (6,'مراعاة أحوال الخبر واستحضار لوازمه وما يحتف به من قرائن وملابسات','26','5','0');
+INSERT INTO [t11144] VALUES (7,'استحضار معارض الخبر','35','5','0');
+INSERT INTO [t11144] VALUES (8,'تحكيم قواعد العادة وسنن الكون والحياة','42','5','0');
+INSERT INTO [t11144] VALUES (9,'استحضار الكليات وتحكيم الأصول القطعيات','44','5','0');
+INSERT INTO [t11144] VALUES (10,'الخاتمة','47','0','0');
+INSERT INTO [t11144] VALUES (11,'مصادر ومراجع','48','0','0');
