ef85f035e0180c6f2147e70b84d6165197b83a63
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:25 +0700

    Upload SQL chunk: t10492_chunk_0000.sql

---
+++ sql_chunks/t10492_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10492
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10492" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10492
+INSERT INTO [t10492] VALUES (1,'فوائد أبي علي الصواف','2','0','0');
