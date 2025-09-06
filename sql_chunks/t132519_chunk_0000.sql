4958a3442ce9882a98bf5b804423d979d45f81ca
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:12:48 +0700

    Upload SQL chunk: t132519_chunk_0000.sql

---
+++ sql_chunks/t132519_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t132519
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t132519" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t132519
+INSERT INTO [t132519] VALUES (1,'تمهيد','3','0','0');
+INSERT INTO [t132519] VALUES (2,'تعريف القرآن والسورة والآية، وأسماؤه','4','0','0');
+INSERT INTO [t132519] VALUES (3,'نزول القرآن','4','0','0');
+INSERT INTO [t132519] VALUES (4,'أسباب النزول','4','0','0');
+INSERT INTO [t132519] VALUES (5,'المكي والمدني','5','0','0');
+INSERT INTO [t132519] VALUES (6,'الأحرف السبعة','5','0','0');
+INSERT INTO [t132519] VALUES (7,'جمع القرآن وحفظه','5','0','0');
+INSERT INTO [t132519] VALUES (8,'ترتيب القرآن، آياته وسوره','6','0','0');
+INSERT INTO [t132519] VALUES (9,'مسائل في ترتيب القرآن','6','0','0');
+INSERT INTO [t132519] VALUES (10,'الرسم العثماني','7','0','0');
+INSERT INTO [t132519] VALUES (11,'القراءات','7','0','0');
+INSERT INTO [t132519] VALUES (12,'النسخ في القرآن','7','0','0');
+INSERT INTO [t132519] VALUES (13,'تفسير القرآن','8','0','0');
+INSERT INTO [t132519] VALUES (14,'الإسرائيليات','9','0','0');
+INSERT INTO [t132519] VALUES (15,'تجويد تلاوة القرآن','9','0','0');
+INSERT INTO [t132519] VALUES (16,'الوقف والابتداء','9','0','0');
