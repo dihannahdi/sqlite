c64c02c22a26668fe6c860de7edda664b8c5a0f2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:03 +0700

    Upload SQL chunk: t5984_chunk_0000.sql

---
+++ sql_chunks/t5984_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t5984
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t5984" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5984
+INSERT INTO [t5984] VALUES (1,'الباب الأول في أقسام الحديث وأنواعه','4','0','0');
+INSERT INTO [t5984] VALUES (2,'الفصل الأول في الصحيح','4','1','0');
+INSERT INTO [t5984] VALUES (3,'الفصل الثاني في الحسن','9','1','0');
+INSERT INTO [t5984] VALUES (4,'الفصل الثالث في الضعيف','13','1','0');
+INSERT INTO [t5984] VALUES (5,'الباب الثاني في الجرح والتعديل','33','0','0');
+INSERT INTO [t5984] VALUES (6,'الباب الثالث في تحمل الحديث','35','0','0');
+INSERT INTO [t5984] VALUES (7,'الباب الرابع في أسماء الرجال','39','0','0');
