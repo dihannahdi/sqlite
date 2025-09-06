0677e934b4d70d5cb3b6e9e72bcfcd1ff19a9d38
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:11 +0700

    Upload SQL chunk: t21678_chunk_0000.sql

---
+++ sql_chunks/t21678_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21678
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21678" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21678
+INSERT INTO [t21678] VALUES (1,'الجزء من فوائد أبي القاسم علي بن عبد الرحمن بن الحسين بن عليك النيسابوري','2','0','0');
