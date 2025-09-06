f38dd8a28db7fec755b828ff84188a8bddcfad9d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:46 +0700

    Upload SQL chunk: t9891_chunk_0000.sql

---
+++ sql_chunks/t9891_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t9891
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t9891" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9891
+INSERT INTO [t9891] VALUES (1,'أولا: السيرة الذاتية لابن كمال باشا','1','0','0');
+INSERT INTO [t9891] VALUES (2,'ثانيا: القراءة التقويمية','8','0','0');
+INSERT INTO [t9891] VALUES (3,'ثالثا: تحقيق الرسالة','14','0','0');
