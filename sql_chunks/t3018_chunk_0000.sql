0700abd62531fe966dea907328338e30288f5af8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:58 +0700

    Upload SQL chunk: t3018_chunk_0000.sql

---
+++ sql_chunks/t3018_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t3018
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t3018" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3018
+INSERT INTO [t3018] VALUES (1,'الأمر','1','0','0');
+INSERT INTO [t3018] VALUES (2,'النهي','2','0','0');
+INSERT INTO [t3018] VALUES (3,'الخبر','2','0','0');
+INSERT INTO [t3018] VALUES (4,'الاستخبار','3','0','0');
+INSERT INTO [t3018] VALUES (5,'المدح','3','0','0');
+INSERT INTO [t3018] VALUES (6,'الهجاء','4','0','0');
+INSERT INTO [t3018] VALUES (7,'المرثية','4','0','0');
+INSERT INTO [t3018] VALUES (8,'الاعتذار','5','0','0');
+INSERT INTO [t3018] VALUES (9,'التشبيه','5','0','0');
+INSERT INTO [t3018] VALUES (10,'التشبيب','5','0','0');
+INSERT INTO [t3018] VALUES (11,'اقتصاص الأخبار','6','0','0');
+INSERT INTO [t3018] VALUES (12,'التشبيه الخارج عن التعدي والتقصير','6','0','0');
+INSERT INTO [t3018] VALUES (13,'الإفراط في الإغراق','15','0','0');
+INSERT INTO [t3018] VALUES (14,'الاستعارة','23','0','0');
+INSERT INTO [t3018] VALUES (15,'حسن الخروج','26','0','0');
+INSERT INTO [t3018] VALUES (16,'مجاورة الأضداد','28','0','0');
+INSERT INTO [t3018] VALUES (17,'المطابق','30','0','0');
+INSERT INTO [t3018] VALUES (18,'جزالة اللفظ','33','0','0');
+INSERT INTO [t3018] VALUES (19,'اتساق النظم','33','0','0');
+INSERT INTO [t3018] VALUES (20,'السناد','33','0','0');
+INSERT INTO [t3018] VALUES (21,'الإقواء','33','0','0');
+INSERT INTO [t3018] VALUES (22,'الإكفاء','33','0','0');
+INSERT INTO [t3018] VALUES (23,'الإجازة','33','0','0');
+INSERT INTO [t3018] VALUES (24,'الإيطاء','33','0','0');
+INSERT INTO [t3018] VALUES (25,'المعدل من أبيات الشعر','36','0','0');
+INSERT INTO [t3018] VALUES (26,'الأبيات الغر','42','0','0');
+INSERT INTO [t3018] VALUES (27,'الأبيات المحجلة','46','0','0');
+INSERT INTO [t3018] VALUES (28,'الأبيات الموضحة','51','0','0');
+INSERT INTO [t3018] VALUES (29,'الأبيات المرجلة','54','0','0');
