ff2955e5b62fab2968240079b159a6add40a7fb2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:04 +0700

    Upload SQL chunk: t17848_chunk_0000.sql

---
+++ sql_chunks/t17848_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t17848
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t17848" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17848
+INSERT INTO [t17848] VALUES (1,'مقدمة التحقيق','4','0','0');
+INSERT INTO [t17848] VALUES (2,'ترجمة المؤلف من كتاب "البدر الطالع"، للشوكاني','7','1','0');
+INSERT INTO [t17848] VALUES (3,'مقدمة المصنف','20','0','0');
+INSERT INTO [t17848] VALUES (4,'تخريج الحديث وطرقه','20','0','0');
+INSERT INTO [t17848] VALUES (5,'حكم العمل بالحديث','23','0','0');
+INSERT INTO [t17848] VALUES (6,'كلام الخطابي حول معنى الحديث والرد عليه','26','0','0');
+INSERT INTO [t17848] VALUES (7,'هل مال الولد لأبيه على الإطلاق أم ماذا؟','30','0','0');
+INSERT INTO [t17848] VALUES (8,'هل الخبر منسوخ أم لا؟','31','0','0');
+INSERT INTO [t17848] VALUES (9,'الجمع بين الأدلة في الإثبات والنسخ','31','0','0');
+INSERT INTO [t17848] VALUES (10,'هل الأم كالأب في ملكها مال ابنها؟','36','0','0');
