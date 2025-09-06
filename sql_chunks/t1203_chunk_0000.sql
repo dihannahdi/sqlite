75349dfbf26501f902db1ae4e47b59ce24b24c35
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:05 +0700

    Upload SQL chunk: t1203_chunk_0000.sql

---
+++ sql_chunks/t1203_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t1203
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t1203" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1203
+INSERT INTO [t1203] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1203] VALUES (2,'الشطر الأول مفاهيم متعلقة بالدعوة الإسلامية','3','0','0');
+INSERT INTO [t1203] VALUES (3,'المقصود بالدعوة الإسلامية','3','2','0');
+INSERT INTO [t1203] VALUES (4,'ما يقتضيه المقصود بتجديد الدعوة على الحكمة والوسطية والاعتدال','10','2','0');
+INSERT INTO [t1203] VALUES (5,'الشطر الثاني النموذج المطلوب في الدعوة الإسلامية المعاصرة بالديار الأندلسية والمناقض للأسس الفكرية والجذور الإيديولوجية لظاهرة الإرهاب','20','0','0');
+INSERT INTO [t1203] VALUES (6,'خاتمة','36','0','0');
