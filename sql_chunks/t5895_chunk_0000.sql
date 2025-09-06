15809909335388c1ec07206a3064d1236b06fea7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:16 +0700

    Upload SQL chunk: t5895_chunk_0000.sql

---
+++ sql_chunks/t5895_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t5895
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t5895" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5895
+INSERT INTO [t5895] VALUES (1,'مقدمة المصنف','1','0','0');
+INSERT INTO [t5895] VALUES (2,'ق اب','1','1','0');
+INSERT INTO [t5895] VALUES (3,'ما جاء في تأويل قول الله عز وجل','2','1','0');
+INSERT INTO [t5895] VALUES (4,'باب ما يجوز من ذكر الناس نحو قولهم الطويل والقصير لا يراد به شين الرجل','8','1','0');
+INSERT INTO [t5895] VALUES (5,'في حرف الألف','17','0','0');
+INSERT INTO [t5895] VALUES (6,'فمن الصحابة','17','5','0');
+INSERT INTO [t5895] VALUES (7,'التابعون ومن بعدهم','23','5','0');
+INSERT INTO [t5895] VALUES (8,'حرف الباء','34','0','0');
+INSERT INTO [t5895] VALUES (9,'من الصحابة','34','8','0');
+INSERT INTO [t5895] VALUES (10,'التابعون ومن بعدهم','35','8','0');
+INSERT INTO [t5895] VALUES (11,'حرف التاء','38','0','0');
+INSERT INTO [t5895] VALUES (12,'حرف الجيم','39','0','0');
+INSERT INTO [t5895] VALUES (13,'حرف الحاء','40','0','0');
+INSERT INTO [t5895] VALUES (14,'حرف الخاء','42','0','0');
+INSERT INTO [t5895] VALUES (15,'حرف الدال','43','0','0');
+INSERT INTO [t5895] VALUES (16,'حرف الذال','45','0','0');
+INSERT INTO [t5895] VALUES (17,'حرف الراء','46','0','0');
+INSERT INTO [t5895] VALUES (18,'حرف الزاي','48','0','0');
+INSERT INTO [t5895] VALUES (19,'حرف السين','51','0','0');
+INSERT INTO [t5895] VALUES (20,'حرف الشين','55','0','0');
+INSERT INTO [t5895] VALUES (21,'حرف الصاد','56','0','0');
+INSERT INTO [t5895] VALUES (22,'حرف الضاد','58','0','0');
+INSERT INTO [t5895] VALUES (23,'حرف الطاء','59','0','0');
+INSERT INTO [t5895] VALUES (24,'حرف العين','62','0','0');
+INSERT INTO [t5895] VALUES (25,'حرف الغين','68','0','0');
+INSERT INTO [t5895] VALUES (26,'حرف الفاء','69','0','0');
+INSERT INTO [t5895] VALUES (27,'حرف القاف','71','0','0');
+INSERT INTO [t5895] VALUES (28,'حرف الكاف','75','0','0');
+INSERT INTO [t5895] VALUES (29,'حرف اللام','76','0','0');
+INSERT INTO [t5895] VALUES (30,'حرف الميم','77','0','0');
+INSERT INTO [t5895] VALUES (31,'حرف النون','85','0','0');
+INSERT INTO [t5895] VALUES (32,'حرف الهاء','87','0','0');
+INSERT INTO [t5895] VALUES (33,'حرف الواو','88','0','0');
+INSERT INTO [t5895] VALUES (34,'حرف الياء','89','0','0');
