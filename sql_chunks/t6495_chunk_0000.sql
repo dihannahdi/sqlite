0ad9f6cae9571bd2a9eb2278cb4219ebfa0b1856
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:06 +0700

    Upload SQL chunk: t6495_chunk_0000.sql

---
+++ sql_chunks/t6495_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6495
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6495" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6495
+INSERT INTO [t6495] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6495] VALUES (2,'وصلى الله على سيدنا ومولانا محمد','97','0','0');
