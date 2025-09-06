3e0802a48005486e68ddea2bc2c08e78b99634ae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:28 +0700

    Upload SQL chunk: t11210_chunk_0000.sql

---
+++ sql_chunks/t11210_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11210
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11210" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11210
+INSERT INTO [t11210] VALUES (1,'رسالة الاجتهاد والتقليد','1','0','0');
+INSERT INTO [t11210] VALUES (2,'معرفة الدليل ولو للمتعلم','9','0','0');
+INSERT INTO [t11210] VALUES (3,'تقليد الأئمة الأربعة','16','0','0');
