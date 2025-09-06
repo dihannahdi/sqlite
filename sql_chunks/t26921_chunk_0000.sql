df07ef28bcea14442e59a1471b45fcbea17713dd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:18 +0700

    Upload SQL chunk: t26921_chunk_0000.sql

---
+++ sql_chunks/t26921_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t26921
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t26921" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26921
+INSERT INTO [t26921] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26921] VALUES (2,'الجزء الأول من الفوائد المنتخبة العوالي من المزكيات انتقاء الحافظ الدارقطني','2','0','0');
+INSERT INTO [t26921] VALUES (3,'الجزء الثاني من الفوائد المنتخبة العوالي من المزكيات انتقاء الحافظ الدارقطني','89','0','0');
+INSERT INTO [t26921] VALUES (4,'[وهذا مجلس من حديثه أملاه علينا]','157','0','0');
+INSERT INTO [t26921] VALUES (5,'ومن حديث أبي بكر بن مالك','171','0','0');
