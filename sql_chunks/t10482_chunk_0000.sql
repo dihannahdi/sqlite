fd5c35389eafed9a6d736b826c8168b94fa2b83f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:15 +0700

    Upload SQL chunk: t10482_chunk_0000.sql

---
+++ sql_chunks/t10482_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10482
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10482" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10482
+INSERT INTO [t10482] VALUES (1,'التوكل وسؤال الله عز وجل لعبد الغني المقدسي - مخطوط','2','0','0');
