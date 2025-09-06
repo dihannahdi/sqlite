7041fcb7f1643ea9ddd63f04b3e4e593595ed9a2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:39 +0700

    Upload SQL chunk: t7540_chunk_0000.sql

---
+++ sql_chunks/t7540_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t7540
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t7540" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7540
+INSERT INTO [t7540] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7540] VALUES (2,'حديث أبي هريرة','4','0','0');
+INSERT INTO [t7540] VALUES (3,'حديث أنس بن مالك','6','0','0');
+INSERT INTO [t7540] VALUES (4,'حديث أبي بن كعب','53','0','0');
+INSERT INTO [t7540] VALUES (5,'حديث بريدة بن الحصيب الأسلمي','55','0','0');
+INSERT INTO [t7540] VALUES (6,'حديث جابر بن عبد الله الأنصاري','57','0','0');
+INSERT INTO [t7540] VALUES (7,'حديث حذيفة بن اليمان','59','0','0');
+INSERT INTO [t7540] VALUES (8,'حديث شداد بن أوس','62','0','0');
+INSERT INTO [t7540] VALUES (9,'حديث صهيب','67','0','0');
+INSERT INTO [t7540] VALUES (10,'حديث عبد الرحمن بن قرط','68','0','0');
+INSERT INTO [t7540] VALUES (11,'حديث عبد الله بن عباس','70','0','0');
+INSERT INTO [t7540] VALUES (12,'حديث عبد الله بن عمر','85','0','0');
+INSERT INTO [t7540] VALUES (13,'حديث عبد الله بن مسعود','86','0','0');
+INSERT INTO [t7540] VALUES (14,'حديث علي','101','0','0');
+INSERT INTO [t7540] VALUES (15,'حديث عمر بن الخطاب','103','0','0');
+INSERT INTO [t7540] VALUES (16,'حديث مالك بن صعصعة','104','0','0');
+INSERT INTO [t7540] VALUES (17,'حديث أبي أيوب الأنصاري','105','0','0');
+INSERT INTO [t7540] VALUES (18,'حديث أبي ذر','107','0','0');
