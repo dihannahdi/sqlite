186008c4a2b324d7035099608e523c21bc5a012e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:19:11 +0700

    Upload SQL chunk: t1447_chunk_0000.sql

---
+++ sql_chunks/t1447_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t1447
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t1447" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1447
+INSERT INTO [t1447] VALUES (1,'الوحدة الموضعية في القرآن الكريم','2','0','0');
+INSERT INTO [t1447] VALUES (2,'كلنا في خدمة النبي صل يا رب عليه وآله وبارك وسلم كما تحبه','8','0','0');
+INSERT INTO [t1447] VALUES (3,'التكرار في القرآن','20','0','0');
+INSERT INTO [t1447] VALUES (4,'{وقدر فيها أقواتها}','23','0','0');
+INSERT INTO [t1447] VALUES (5,'قصة القرآن الكريم','24','0','0');
+INSERT INTO [t1447] VALUES (6,'إبراهيم ـ عليه السلام ـ والكواكب','28','0','0');
+INSERT INTO [t1447] VALUES (7,'عصمة الأنبياء','31','0','0');
+INSERT INTO [t1447] VALUES (8,'الأمانة','34','0','0');
+INSERT INTO [t1447] VALUES (9,'زواج النبي - صلى الله عليه وسلم -','36','0','0');
+INSERT INTO [t1447] VALUES (10,'لستن كأحد من النساء','48','0','0');
+INSERT INTO [t1447] VALUES (11,'داوود عليه الصلاة والسلام','52','0','0');
+INSERT INTO [t1447] VALUES (12,'** محمد ** صل يارب عليه وعلى آله وبارك وسلم كما تحبه وترضاه آمين *','56','0','0');
+INSERT INTO [t1447] VALUES (13,'** حول معنى الهداية **','59','0','0');
+INSERT INTO [t1447] VALUES (14,'روائع الكلمة العربية في الآيات القرآنية','63','0','0');
