05a1b8956bd24534789b12a2925650f3831f6b13
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:04 +0700

    Upload SQL chunk: t1479_chunk_0000.sql

---
+++ sql_chunks/t1479_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t1479
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t1479" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1479
+INSERT INTO [t1479] VALUES (1,'قالوا عن القرآن','1','0','0');
+INSERT INTO [t1479] VALUES (2,'خليل أحمد','2','0','0');
+INSERT INTO [t1479] VALUES (3,'ارنولد','3','0','0');
+INSERT INTO [t1479] VALUES (4,'ايرفنج','4','0','0');
+INSERT INTO [t1479] VALUES (5,'بروز','5','0','0');
+INSERT INTO [t1479] VALUES (6,'بلاشير','6','0','0');
+INSERT INTO [t1479] VALUES (7,'بوازار','7','0','0');
+INSERT INTO [t1479] VALUES (8,'بوتر','8','0','0');
+INSERT INTO [t1479] VALUES (9,'بوكاي','9','0','0');
+INSERT INTO [t1479] VALUES (10,'بيكارد','10','0','0');
+INSERT INTO [t1479] VALUES (11,'حتي','11','0','0');
+INSERT INTO [t1479] VALUES (12,'حنا','12','0','0');
+INSERT INTO [t1479] VALUES (13,'داود','13','0','0');
+INSERT INTO [t1479] VALUES (14,'درمنغم','14','0','0');
+INSERT INTO [t1479] VALUES (15,'دي كاستري','15','0','0');
+INSERT INTO [t1479] VALUES (16,'دينيه','16','0','0');
+INSERT INTO [t1479] VALUES (17,'ديورانت','17','0','0');
+INSERT INTO [t1479] VALUES (18,'روزنثال','18','0','0');
+INSERT INTO [t1479] VALUES (19,'ريسلر','19','0','0');
+INSERT INTO [t1479] VALUES (20,'سارتون','20','0','0');
+INSERT INTO [t1479] VALUES (21,'ستشيجفسكا','21','0','0');
+INSERT INTO [t1479] VALUES (22,'ستيفنز','22','0','0');
+INSERT INTO [t1479] VALUES (23,'سلهب','23','0','0');
+INSERT INTO [t1479] VALUES (24,'سوسه','24','0','0');
+INSERT INTO [t1479] VALUES (25,'سيديو','25','0','0');
+INSERT INTO [t1479] VALUES (26,'سيرويا','26','0','0');
+INSERT INTO [t1479] VALUES (27,'شاد','27','0','0');
+INSERT INTO [t1479] VALUES (28,'فاغليري','28','0','0');
+INSERT INTO [t1479] VALUES (29,'فايس','29','0','0');
+INSERT INTO [t1479] VALUES (30,'فيشر','30','0','0');
+INSERT INTO [t1479] VALUES (31,'جب','31','0','0');
+INSERT INTO [t1479] VALUES (32,'كويليام','32','0','0');
+INSERT INTO [t1479] VALUES (33,'لاندو','33','0','0');
+INSERT INTO [t1479] VALUES (34,'لوبون','34','0','0');
+INSERT INTO [t1479] VALUES (35,'ليختنستادتر','35','0','0');
+INSERT INTO [t1479] VALUES (36,'مونتاي','36','0','0');
+INSERT INTO [t1479] VALUES (37,'هوني','37','0','0');
+INSERT INTO [t1479] VALUES (38,'وات','38','0','0');
