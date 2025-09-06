5bd58e90f08e21dfc88aebf2e670df1155033dcd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:44 +0700

    Upload SQL chunk: t10418_chunk_0000.sql

---
+++ sql_chunks/t10418_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t10418
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t10418" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10418
+INSERT INTO [t10418] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10418] VALUES (2,'معنى الصلاة عليه صلى الله عليه وسلم وبيان كيفيتها','4','0','0');
+INSERT INTO [t10418] VALUES (3,'فضل الصلاة على النبي صلى الله عليه وسلم','10','0','0');
+INSERT INTO [t10418] VALUES (4,'مما ألف في الصلاة على النبي صلى الله عليه وسلم','13','0','0');
+INSERT INTO [t10418] VALUES (5,'نماذج مما فيه من الاحاديث الموضوعة','16','0','0');
