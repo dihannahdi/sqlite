90453a0215fd47899ae6195c80374140087469c8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:53:33 +0700

    Upload SQL chunk: t940_chunk_0000.sql

---
+++ sql_chunks/t940_chunk_0000.sql
@@ -0,0 +1,67 @@
+-- Table: t940
+-- Chunk: 1
+-- Rows: 60
+
+CREATE TABLE "t940" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t940
+INSERT INTO [t940] VALUES (1,'تقديم','6','0','0');
+INSERT INTO [t940] VALUES (2,'مقدمة المحقق','8','1','0');
+INSERT INTO [t940] VALUES (3,'نسبه ولقبه','11','1','0');
+INSERT INTO [t940] VALUES (4,'الحياة العلمية في عصر النحاس','12','1','0');
+INSERT INTO [t940] VALUES (5,'نشأته العلمية','13','1','0');
+INSERT INTO [t940] VALUES (6,'شيوخ النحاس','14','1','0');
+INSERT INTO [t940] VALUES (7,'تلامذة النحاس','16','1','0');
+INSERT INTO [t940] VALUES (8,'النحاس إمام محقق','18','1','0');
+INSERT INTO [t940] VALUES (9,'مؤلفات النحاس','24','1','0');
+INSERT INTO [t940] VALUES (10,'وفاة الامام النحاس','26','1','0');
+INSERT INTO [t940] VALUES (11,'المخطوطة وحيدة','28','1','0');
+INSERT INTO [t940] VALUES (12,'عملنا في هذه المخطوطة','29','1','0');
+INSERT INTO [t940] VALUES (13,'مراجع ترجمة النحاس','32','1','0');
+INSERT INTO [t940] VALUES (14,'مقدمة','41','0','0');
+INSERT INTO [t940] VALUES (15,'تفسير سورة الفاتحة','44','0','0');
+INSERT INTO [t940] VALUES (16,'تفسير سورة البقرة','68','0','0');
+INSERT INTO [t940] VALUES (17,'تفسير سورة آل عمران','332','0','0');
+INSERT INTO [t940] VALUES (18,'تفسير سورة النساء','532','0','0');
+INSERT INTO [t940] VALUES (19,'تفسير سورة المائدة','771','0','0');
+INSERT INTO [t940] VALUES (20,'تفسير سورة الأنعام','920','0','0');
+INSERT INTO [t940] VALUES (21,'تفسير سورة الأعراف','1057','0','0');
+INSERT INTO [t940] VALUES (22,'تفسير سورة الأنفال','1175','0','0');
+INSERT INTO [t940] VALUES (23,'تفسير سورة التوبة','1225','0','0');
+INSERT INTO [t940] VALUES (24,'تفسير سورة يونس','1320','0','0');
+INSERT INTO [t940] VALUES (25,'تفسير سورة هود','1371','0','0');
+INSERT INTO [t940] VALUES (26,'تفسير سورة يوسف','1438','0','0');
+INSERT INTO [t940] VALUES (27,'تفسير سورة الرعد','1509','0','0');
+INSERT INTO [t940] VALUES (28,'تفسير سورة إبراهيم','1553','0','0');
+INSERT INTO [t940] VALUES (29,'تفسير سورة الحجر','1594','0','0');
+INSERT INTO [t940] VALUES (30,'تفسير سورة النحل','1637','0','0');
+INSERT INTO [t940] VALUES (31,'تفسير سورة الإسراء','1702','0','0');
+INSERT INTO [t940] VALUES (32,'تفسير سورة الكهف','1795','0','0');
+INSERT INTO [t940] VALUES (33,'تفسير سورة مريم','1889','0','0');
+INSERT INTO [t940] VALUES (34,'تفسير سورة الحج','1950','0','0');
+INSERT INTO [t940] VALUES (35,'تفسير سورة المؤمنون','2018','0','0');
+INSERT INTO [t940] VALUES (36,'تفسير سورة النور','2069','0','0');
+INSERT INTO [t940] VALUES (37,'تفسير سورة الفرقان','2150','0','0');
+INSERT INTO [t940] VALUES (38,'تفسير سورة الشعراء','2203','0','0');
+INSERT INTO [t940] VALUES (39,'تفسير سورة النمل','2254','0','0');
+INSERT INTO [t940] VALUES (40,'تفسير سورة القصص','2294','0','0');
+INSERT INTO [t940] VALUES (41,'تفسير سورة العنكبوت','2349','0','0');
+INSERT INTO [t940] VALUES (42,'تفسير سورة الروم','2377','0','0');
+INSERT INTO [t940] VALUES (43,'تفسير سورة لقمان','2411','0','0');
+INSERT INTO [t940] VALUES (44,'تفسير سورة السجدة','2429','0','0');
+INSERT INTO [t940] VALUES (45,'تفسير سورة الأحزاب','2448','0','0');
+INSERT INTO [t940] VALUES (46,'تفسير سورة سبأ','2520','0','0');
+INSERT INTO [t940] VALUES (47,'تفسير سورة فاطر','2562','0','0');
+INSERT INTO [t940] VALUES (48,'تفسير سورة يس','2597','0','0');
+INSERT INTO [t940] VALUES (49,'تفسير سورة الصافات','2656','0','0');
+INSERT INTO [t940] VALUES (50,'تفسير سورة ص','2721','0','0');
+INSERT INTO [t940] VALUES (51,'تفسير سورة الزمر','2792','0','0');
+INSERT INTO [t940] VALUES (52,'تفسير سورة غافر','2845','0','0');
+INSERT INTO [t940] VALUES (53,'تفسير سورة فصلت','2883','0','0');
+INSERT INTO [t940] VALUES (54,'تفسير سورة الشورى','2931','0','0');
+INSERT INTO [t940] VALUES (55,'تفسير سورة الزخرف','2971','0','0');
+INSERT INTO [t940] VALUES (56,'تفسير سورة الدخان','3031','0','0');
+INSERT INTO [t940] VALUES (57,'تفسير سورة الجاثية','3056','0','0');
+INSERT INTO [t940] VALUES (58,'تفسير سورة الأحقاف','3071','0','0');
+INSERT INTO [t940] VALUES (59,'تفسير سورة محمد','3091','0','0');
+INSERT INTO [t940] VALUES (60,'تفسير سورة الفتح','3122','0','0');
