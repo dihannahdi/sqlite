88ca414f27d442396452f105be5e874c5653f411
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:10:41 +0700

    Upload SQL chunk: t13106_chunk_0000.sql

---
+++ sql_chunks/t13106_chunk_0000.sql
@@ -0,0 +1,56 @@
+-- Table: t13106
+-- Chunk: 1
+-- Rows: 49
+
+CREATE TABLE "t13106" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13106
+INSERT INTO [t13106] VALUES (1,'من سورة أم الكتاب','1','0','0');
+INSERT INTO [t13106] VALUES (2,'ومن سورة البقرة','14','0','0');
+INSERT INTO [t13106] VALUES (3,'ومن سورة آل عمران','28','0','0');
+INSERT INTO [t13106] VALUES (4,'ومن سورة النساء','32','0','0');
+INSERT INTO [t13106] VALUES (5,'ومن سورة المائدة','39','0','0');
+INSERT INTO [t13106] VALUES (6,'من سورة الأنعام','46','0','0');
+INSERT INTO [t13106] VALUES (7,'ومن سورة الأعراف','52','0','0');
+INSERT INTO [t13106] VALUES (8,'ومن سورة الأنفال','59','0','0');
+INSERT INTO [t13106] VALUES (9,'ومن سورة براءة','62','0','0');
+INSERT INTO [t13106] VALUES (10,'ومن سورة يونس','65','0','0');
+INSERT INTO [t13106] VALUES (11,'ومن سورة هود','69','0','0');
+INSERT INTO [t13106] VALUES (12,'ومن سورة يوسف','75','0','0');
+INSERT INTO [t13106] VALUES (13,'ومن سورة الرعد','80','0','0');
+INSERT INTO [t13106] VALUES (14,'ومن سورة إبراهيم','84','0','0');
+INSERT INTO [t13106] VALUES (15,'ومن سورة بني إسرائيل','86','0','0');
+INSERT INTO [t13106] VALUES (16,'ومن سورة الكهف','89','0','0');
+INSERT INTO [t13106] VALUES (17,'ومن سورة مريم','95','0','0');
+INSERT INTO [t13106] VALUES (18,'ومن سورة طه','97','0','0');
+INSERT INTO [t13106] VALUES (19,'ومن سورة الحج','99','0','0');
+INSERT INTO [t13106] VALUES (20,'ومن سورة المؤمنين','102','0','0');
+INSERT INTO [t13106] VALUES (21,'ومن سورة النور','105','0','0');
+INSERT INTO [t13106] VALUES (22,'ومن سورة الفرقان','108','0','0');
+INSERT INTO [t13106] VALUES (23,'ومن سورة العنكبوت','110','0','0');
+INSERT INTO [t13106] VALUES (24,'ومن سورة الروم','112','0','0');
+INSERT INTO [t13106] VALUES (25,'ومن سورة الأحزاب','114','0','0');
+INSERT INTO [t13106] VALUES (26,'ومن سورة سبإ','116','0','0');
+INSERT INTO [t13106] VALUES (27,'ومن سورة يس','118','0','0');
+INSERT INTO [t13106] VALUES (28,'ومن سورة الزمر','122','0','0');
+INSERT INTO [t13106] VALUES (29,'ومن سورة الزخرف','127','0','0');
+INSERT INTO [t13106] VALUES (30,'ومن سورة الأحقاف','130','0','0');
+INSERT INTO [t13106] VALUES (31,'ومن سورة محمد','132','0','0');
+INSERT INTO [t13106] VALUES (32,'ومن سورة الحجرات','134','0','0');
+INSERT INTO [t13106] VALUES (33,'ومن سورة ق','136','0','0');
+INSERT INTO [t13106] VALUES (34,'ومن سورة الذاريات','138','0','0');
+INSERT INTO [t13106] VALUES (35,'ومن سورة النجم','140','0','0');
+INSERT INTO [t13106] VALUES (36,'ومن سورة الساعة','142','0','0');
+INSERT INTO [t13106] VALUES (37,'ومن سورة الرحمن عز وجل','147','0','0');
+INSERT INTO [t13106] VALUES (38,'ومن سورة الواقعة','149','0','0');
+INSERT INTO [t13106] VALUES (39,'ومن سورة الطلاق','153','0','0');
+INSERT INTO [t13106] VALUES (40,'ومن سورة الحاقة','155','0','0');
+INSERT INTO [t13106] VALUES (41,'ومن سورة الجن','157','0','0');
+INSERT INTO [t13106] VALUES (42,'ومن سورة المدثر','159','0','0');
+INSERT INTO [t13106] VALUES (43,'ومن سورة إذا الشمس كورت','161','0','0');
+INSERT INTO [t13106] VALUES (44,'ومن سورة إذا السماء انفطرت','163','0','0');
+INSERT INTO [t13106] VALUES (45,'ومن سورة الفجر','165','0','0');
+INSERT INTO [t13106] VALUES (46,'ومن سورة البلد','168','0','0');
+INSERT INTO [t13106] VALUES (47,'ومن سورة والشمس','170','0','0');
+INSERT INTO [t13106] VALUES (48,'ومن سورة والليل','172','0','0');
+INSERT INTO [t13106] VALUES (49,'ومن سورة قريش','175','0','0');
