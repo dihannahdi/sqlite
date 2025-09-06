dcd6355195695a4728999cf0a92e94b5321a565c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:49 +0700

    Upload SQL chunk: t7481_chunk_0000.sql

---
+++ sql_chunks/t7481_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t7481
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t7481" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7481
+INSERT INTO [t7481] VALUES (1,'أثر العبادات في حياة المسلم','1','0','0');
+INSERT INTO [t7481] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t7481] VALUES (3,'والحمد لله رب العالمين','29','0','0');
