409a39492c436050b010db53c9fbdc64acbd40ef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:28 +0700

    Upload SQL chunk: t26559_chunk_0000.sql

---
+++ sql_chunks/t26559_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26559
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26559" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26559
+INSERT INTO [t26559] VALUES (1,'حديث أبي الطيب الحوراني','2','0','0');
