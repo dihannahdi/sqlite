78d3f09a14264b213e1bba2cd11e7d7ed11f6b94
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:46 +0700

    Upload SQL chunk: t11485_chunk_0000.sql

---
+++ sql_chunks/t11485_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t11485
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t11485" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11485
+INSERT INTO [t11485] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t11485] VALUES (2,'بسم الله الرحمن الرحيم','54','0','0');
+INSERT INTO [t11485] VALUES (3,'بسم الله الرحمن الرحيم','54','0','0');
+INSERT INTO [t11485] VALUES (4,'بسم الله الرحمن الرحيم','56','0','0');
+INSERT INTO [t11485] VALUES (5,'بسم الله الرحمن الرحيم','56','0','0');
+INSERT INTO [t11485] VALUES (6,'بسم الله الرحمن الرحيم','61','0','0');
+INSERT INTO [t11485] VALUES (7,'بسم الله الرحمن الرحيم','81','0','0');
+INSERT INTO [t11485] VALUES (8,'بسم الله الرحمن الرحيم','114','0','0');
+INSERT INTO [t11485] VALUES (9,'بسم الله الرحمن الرحيم','126','0','0');
+INSERT INTO [t11485] VALUES (10,'بسم الله الرحمن الرحيم','126','0','0');
+INSERT INTO [t11485] VALUES (11,'بسم الله الرحمن الرحيم','197','0','0');
+INSERT INTO [t11485] VALUES (12,'بسم الله الرحمن الرحيم','208','0','0');
+INSERT INTO [t11485] VALUES (13,'بسم الله الرحمن الرحيم','208','0','0');
+INSERT INTO [t11485] VALUES (14,'بسم الله الرحمن الرحيم','209','0','0');
+INSERT INTO [t11485] VALUES (15,'بسم الله الرحمن الرحيم','209','0','0');
+INSERT INTO [t11485] VALUES (16,'بسم الله الرحمن الرحيم','209','0','0');
+INSERT INTO [t11485] VALUES (17,'بسم الله الرحمن الرحيم','259','0','0');
