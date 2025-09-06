0e2ed723942aab58a2eba3479309ab1eacb0c5d3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:53 +0700

    Upload SQL chunk: t7630_chunk_0000.sql

---
+++ sql_chunks/t7630_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t7630
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t7630" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7630
+INSERT INTO [t7630] VALUES (1,'الرسائل الشخصية للشيخ محمد بن عبد الوهاب','1','0','0');
+INSERT INTO [t7630] VALUES (2,'الرسائل والظروف المحيطة بالدعوة','9','0','0');
+INSERT INTO [t7630] VALUES (3,'الحالة الدينية في نجد عند ظهور دعوة الشيخ','9','0','0');
+INSERT INTO [t7630] VALUES (4,'حالة البادية:','12','0','0');
+INSERT INTO [t7630] VALUES (5,'بدء الدعوة في نجد:','13','0','0');
+INSERT INTO [t7630] VALUES (6,'أسلوب الدعوة:','15','0','0');
+INSERT INTO [t7630] VALUES (7,'المعارضة النجدية:','16','0','0');
+INSERT INTO [t7630] VALUES (8,'علماء الأحساء والدعوة:','21','0','0');
+INSERT INTO [t7630] VALUES (9,'الأشراف والدعوة:','22','0','0');
+INSERT INTO [t7630] VALUES (10,'أيها السادة:','23','0','0');
