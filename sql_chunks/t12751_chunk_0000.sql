ba0f1b600bc28846347750470c6f52f88a476240
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:32 +0700

    Upload SQL chunk: t12751_chunk_0000.sql

---
+++ sql_chunks/t12751_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t12751
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t12751" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12751
+INSERT INTO [t12751] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t12751] VALUES (2,'[فضائله، ومقتله، رضي الله عنه]','2','0','0');
+INSERT INTO [t12751] VALUES (3,'وصية علي بن أبي طالب رحمه الله','41','0','0');
+INSERT INTO [t12751] VALUES (4,'موت علي بن أبي طالب رحمة الله عليه','52','0','0');
+INSERT INTO [t12751] VALUES (5,'سن علي بن أبي طالب رحمه الله','59','0','0');
+INSERT INTO [t12751] VALUES (6,'صفة علي رحمة الله عليه','68','0','0');
+INSERT INTO [t12751] VALUES (7,'غسل علي وتكفينه والصلاة عليه ودفنه رضوان الله عليه','78','0','0');
+INSERT INTO [t12751] VALUES (8,'موضع دفن علي رحمة الله عليه','81','0','0');
+INSERT INTO [t12751] VALUES (9,'أمر ابن ملجم وقتله','87','0','0');
+INSERT INTO [t12751] VALUES (10,'ندب علي ومراثيه صلوات الله عليه','105','0','0');
+INSERT INTO [t12751] VALUES (11,'ولد علي بن أبي طالب عليه وعليهم السلام','122','0','0');
+INSERT INTO [t12751] VALUES (12,'آخر كتاب مقتل أمير المؤمنين علي بن أبي طالب عليه السلام','140','0','0');
+INSERT INTO [t12751] VALUES (13,'ولد علي بن أبي طالب','143','0','0');
