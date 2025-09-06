8194950b6fd17699db76ae2222bb9f903951c94b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:44 +0700

    Upload SQL chunk: t256_chunk_0000.sql

---
+++ sql_chunks/t256_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t256
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t256" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t256
+INSERT INTO [t256] VALUES (1,'بنو المصطلق','1','0','0');
+INSERT INTO [t256] VALUES (2,'القائد المظفر الناجح - صلى الله عليه وسلم -','1','0','0');
+INSERT INTO [t256] VALUES (3,'المعركة','2','0','0');
+INSERT INTO [t256] VALUES (4,'برة الأسيرة','3','0','0');
+INSERT INTO [t256] VALUES (5,'حديث عائشة - رضي الله عنها -','3','0','0');
+INSERT INTO [t256] VALUES (6,'الخير العظيم','5','0','0');
+INSERT INTO [t256] VALUES (7,'إسلام الحارث','5','0','0');
+INSERT INTO [t256] VALUES (8,'من برة إلى جويرية','5','0','0');
+INSERT INTO [t256] VALUES (9,'الصوامة القوامة في بيت النبوة','6','0','0');
+INSERT INTO [t256] VALUES (10,'بعد رسول الله - صلى الله عليه وسلم -','8','0','0');
+INSERT INTO [t256] VALUES (11,'ومرت الأعوام','9','0','0');
+INSERT INTO [t256] VALUES (12,'موقفها من الفتن','9','0','0');
+INSERT INTO [t256] VALUES (13,'وفاتها','10','0','0');
