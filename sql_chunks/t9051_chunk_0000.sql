16e028d7431ea8d7b55b335fdbc15c714fe1b272
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:23 +0700

    Upload SQL chunk: t9051_chunk_0000.sql

---
+++ sql_chunks/t9051_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t9051
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t9051" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9051
+INSERT INTO [t9051] VALUES (1,'سعيد بن عامر','1','0','0');
+INSERT INTO [t9051] VALUES (2,'معاذ بن هشام','6','0','0');
+INSERT INTO [t9051] VALUES (3,'أبو داود الطيالسي','10','0','0');
