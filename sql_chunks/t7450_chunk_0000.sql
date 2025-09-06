be271cfcd4341c546cfdd0aa536365e41b731b56
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:32 +0700

    Upload SQL chunk: t7450_chunk_0000.sql

---
+++ sql_chunks/t7450_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t7450
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t7450" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7450
+INSERT INTO [t7450] VALUES (1,'المجلد: الأول','1','0','0');
+INSERT INTO [t7450] VALUES (2,'المتن','1','1','0');
+INSERT INTO [t7450] VALUES (3,'الفهارس','295','1','0');
+INSERT INTO [t7450] VALUES (4,'المجلد: الثاني','307','0','0');
+INSERT INTO [t7450] VALUES (5,'المتن','307','4','0');
+INSERT INTO [t7450] VALUES (6,'الفهارس','589','4','0');
