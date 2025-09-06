c227f44a35a9bc0ca17cc8d0d1e26aa67828102f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:36 +0700

    Upload SQL chunk: t5046_chunk_0000.sql

---
+++ sql_chunks/t5046_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t5046
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t5046" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5046
+INSERT INTO [t5046] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t5046] VALUES (2,'اختلاف العلماء في هذه المسألة','3','0','0');
+INSERT INTO [t5046] VALUES (3,'المخرجون لأحاديث اللعن إجمالا','5','0','0');
+INSERT INTO [t5046] VALUES (4,'تفصيل روايات المخرجين','6','0','0');
+INSERT INTO [t5046] VALUES (5,'سند حديث "زائرات" بطريقيه','7','0','0');
+INSERT INTO [t5046] VALUES (6,'الكلام على سندي هذا الحديث','8','0','0');
+INSERT INTO [t5046] VALUES (7,'ضبط زاى زوارات','10','0','0');
+INSERT INTO [t5046] VALUES (8,'تفصيل أدلة المنع','12','0','0');
+INSERT INTO [t5046] VALUES (9,'أدلة المجيز ين والجواب عنها','14','0','0');
+INSERT INTO [t5046] VALUES (10,'نقل جملة من كلام أئمة التحقيق في هذه المسألة','23','0','0');
