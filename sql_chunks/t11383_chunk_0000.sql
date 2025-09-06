6132d5fe016ef02bdf051d3437101f5d988705bc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:50 +0700

    Upload SQL chunk: t11383_chunk_0000.sql

---
+++ sql_chunks/t11383_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11383
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11383" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11383
+INSERT INTO [t11383] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11383] VALUES (2,'مختصر تفسير سورة الأنفال','3','0','0');
+INSERT INTO [t11383] VALUES (3,'مصادر ومراجع','25','0','0');
