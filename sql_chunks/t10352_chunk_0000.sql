9adc0199096fc8e3cca2c90330257af74c6a88e3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:19 +0700

    Upload SQL chunk: t10352_chunk_0000.sql

---
+++ sql_chunks/t10352_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t10352
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t10352" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10352
+INSERT INTO [t10352] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10352] VALUES (2,'المبحث الأول: مفهوم الحوقلة:','4','0','0');
+INSERT INTO [t10352] VALUES (3,'أولا: المراد بالحوقلة:','4','2','0');
+INSERT INTO [t10352] VALUES (4,'ثانيا: معنى ((لا حول ولا قوة إلا بالله)) :','6','2','0');
+INSERT INTO [t10352] VALUES (5,'ثالثا: إعراب ((لا حول ولا قوة إلا بالله)) :','8','2','0');
+INSERT INTO [t10352] VALUES (6,'المبحث الثاني: فضائل ((لاحول ولا قوة إلا بالله)) :','10','0','0');
+INSERT INTO [t10352] VALUES (7,'المبحث الثالث: دلائل ((لا حول ولا قوة إلا بالله)) :','16','0','0');
+INSERT INTO [t10352] VALUES (8,'المبحث الرابع: في التنبيه على بعض المفاهيم الخاطئة حول ((لا حول ولا قوة إلا بالله)) :','26','0','0');
