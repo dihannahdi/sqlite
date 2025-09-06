6510b36f8f1e2550211720bda174a8a04a3d1a3b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:38 +0700

    Upload SQL chunk: t12546_chunk_0000.sql

---
+++ sql_chunks/t12546_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t12546
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t12546" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12546
+INSERT INTO [t12546] VALUES (1,'ذكر من لم يكن عنده إلا حديث واحد للخلال','2','0','0');
