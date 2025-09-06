9bac174d366dc97980b6db318273c7ba9e520ed4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:09 +0700

    Upload SQL chunk: t6861_chunk_0000.sql

---
+++ sql_chunks/t6861_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6861
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6861" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6861
+INSERT INTO [t6861] VALUES (1,'كتب الشيخ أبو هلال الحسن بن عبد الله','1','0','0');
+INSERT INTO [t6861] VALUES (2,'والسلام عليكم ورحمة الله وبركاته','51','0','0');
