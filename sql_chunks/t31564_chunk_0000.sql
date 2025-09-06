20a3d8c4a95b8abcd44b40b698e33734f68138e5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:40 +0700

    Upload SQL chunk: t31564_chunk_0000.sql

---
+++ sql_chunks/t31564_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t31564
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t31564" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31564
+INSERT INTO [t31564] VALUES (1,'تقديم','1','0','0');
+INSERT INTO [t31564] VALUES (2,'ضيوف القضية','3','0','0');
+INSERT INTO [t31564] VALUES (3,'بين يدي الكتاب','4','0','0');
+INSERT INTO [t31564] VALUES (4,'المحور الأول النشأة والأسباب','8','0','0');
+INSERT INTO [t31564] VALUES (5,'بين مكة والمدينة','8','4','0');
+INSERT INTO [t31564] VALUES (6,'الجهل والخوارج','11','4','0');
+INSERT INTO [t31564] VALUES (7,'ثقافة العنف','14','4','0');
+INSERT INTO [t31564] VALUES (8,'أولا الأسباب العامة','14','7','0');
+INSERT INTO [t31564] VALUES (9,'ثانيا الأسباب الخاصة بمجتمعاتنا','18','7','0');
+INSERT INTO [t31564] VALUES (10,'المحور الثاني كيف نغير المنكر','27','0','0');
+INSERT INTO [t31564] VALUES (11,'قصور في الفهم','27','10','0');
+INSERT INTO [t31564] VALUES (12,'آثار مدمرة','31','10','0');
+INSERT INTO [t31564] VALUES (13,'مقومات الإنكار باللسان','32','10','0');
+INSERT INTO [t31564] VALUES (14,'حقوق المعاهدين','33','10','0');
+INSERT INTO [t31564] VALUES (15,'تشويه صورة الإسلام','36','10','0');
+INSERT INTO [t31564] VALUES (16,'المحور الثالث الأحزاب والجماعات','36','0','0');
+INSERT INTO [t31564] VALUES (17,'بين القول والفعل','36','16','0');
+INSERT INTO [t31564] VALUES (18,'مراعاة الظروف','40','16','0');
+INSERT INTO [t31564] VALUES (19,'المعيار السليم','42','16','0');
+INSERT INTO [t31564] VALUES (20,'المحور الرابع فتنة التكفير','45','0','0');
+INSERT INTO [t31564] VALUES (21,'أي نفاق','45','20','0');
+INSERT INTO [t31564] VALUES (22,'منهج السلف','47','20','0');
+INSERT INTO [t31564] VALUES (23,'وقاية وعلاج','48','20','0');
+INSERT INTO [t31564] VALUES (24,'المحور الخامس كيف تطبق الشريعة الإسلامية في دول العالم الإسلامي','49','0','0');
+INSERT INTO [t31564] VALUES (25,'تنوع السبل','49','24','0');
+INSERT INTO [t31564] VALUES (26,'الحاكم والهيئات العلمية','51','24','0');
+INSERT INTO [t31564] VALUES (27,'التربية والدعوة','58','24','0');
+INSERT INTO [t31564] VALUES (28,'العمل السلمي','59','24','0');
+INSERT INTO [t31564] VALUES (29,'المحور السادس الحصاد المر','61','0','0');
+INSERT INTO [t31564] VALUES (30,'حسن النية لا يكفي','61','29','0');
+INSERT INTO [t31564] VALUES (31,'نشر الأوهام','64','29','0');
