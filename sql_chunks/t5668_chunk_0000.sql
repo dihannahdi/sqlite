7c550b0fcdb72bea432d4a33c321c51c01e269aa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:13 +0700

    Upload SQL chunk: t5668_chunk_0000.sql

---
+++ sql_chunks/t5668_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5668
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5668" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5668
+INSERT INTO [t5668] VALUES (1,'كتاب بيبي بنت عبد الصمد الهروية الهرثمية','1','0','0');
+INSERT INTO [t5668] VALUES (2,'عفوك اللهم','1','0','0');
