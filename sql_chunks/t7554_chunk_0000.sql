203010109e2f11fa820aab7b4018ca59d5b296fe
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:48 +0700

    Upload SQL chunk: t7554_chunk_0000.sql

---
+++ sql_chunks/t7554_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t7554
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t7554" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7554
+INSERT INTO [t7554] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7554] VALUES (2,'نص الكتاب','11','0','0');
+INSERT INTO [t7554] VALUES (3,'مدخل','11','2','0');
+INSERT INTO [t7554] VALUES (4,'فصل (اعتراض)','27','2','0');
+INSERT INTO [t7554] VALUES (5,'فصل (مما يتعين الاعتناء به)','38','2','0');
+INSERT INTO [t7554] VALUES (6,'فصل (دعوى أن الشرك هو الصلاة والسجود لغير الله فقط)','44','2','0');
+INSERT INTO [t7554] VALUES (7,'فصل (الفرق بين الشرك الأكبر والبدع)','47','2','0');
+INSERT INTO [t7554] VALUES (8,'فصل (سد باب الفتنة مقصد شرعي)','75','2','0');
+INSERT INTO [t7554] VALUES (9,'فصل (ما يتعين على من نصح نفسه)','79','2','0');
