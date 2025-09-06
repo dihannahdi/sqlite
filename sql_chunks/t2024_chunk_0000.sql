7032392a2ee79822393929b5841b5790732ab665
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:36:08 +0700

    Upload SQL chunk: t2024_chunk_0000.sql

---
+++ sql_chunks/t2024_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t2024
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t2024" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2024
+INSERT INTO [t2024] VALUES (1,'تصدير','1','0','0');
+INSERT INTO [t2024] VALUES (2,'المقدمة','2','0','0');
+INSERT INTO [t2024] VALUES (3,'الإسلام والإيمان','8','0','0');
+INSERT INTO [t2024] VALUES (4,'الإيمان قول وعمل يزيد وينقص','14','0','0');
+INSERT INTO [t2024] VALUES (5,'الخطأ في فهم معنى الكفر العملي','40','0','0');
+INSERT INTO [t2024] VALUES (6,'أولا: في الاعتقاد','49','0','0');
+INSERT INTO [t2024] VALUES (7,'في الرد على المرجئة','77','0','0');
+INSERT INTO [t2024] VALUES (8,'أحاديث الشفاعة وحقيقة الشهادتين','81','0','0');
+INSERT INTO [t2024] VALUES (9,'مرجئة العصر الحديث','98','0','0');
