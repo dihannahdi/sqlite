dbbbedf51247aee14bc9fdbb14940052a4e0fbd6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:15 +0700

    Upload SQL chunk: t10348_chunk_0000.sql

---
+++ sql_chunks/t10348_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t10348
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t10348" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10348
+INSERT INTO [t10348] VALUES (1,'الحكم على الشيء فرع عن تصوره','1','0','0');
+INSERT INTO [t10348] VALUES (2,'مبحث الأسماء والصفات','11','0','0');
+INSERT INTO [t10348] VALUES (3,'إسلام الخميني ليس بإسلام','19','0','0');
+INSERT INTO [t10348] VALUES (4,'يا سبحان الله!','25','0','0');
