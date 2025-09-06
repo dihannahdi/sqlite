8f591c3521b46ef375076c4977da78f5878c58ac
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:43 +0700

    Upload SQL chunk: t180_chunk_0000.sql

---
+++ sql_chunks/t180_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t180
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t180" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t180
+INSERT INTO [t180] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t180] VALUES (2,'الحمد لله الذي أحيا أموات النبات','1','0','0');
