3452e76054803602c8cbf1254c2e8198d7cdd51f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:02 +0700

    Upload SQL chunk: t17845_chunk_0000.sql

---
+++ sql_chunks/t17845_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t17845
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t17845" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17845
+INSERT INTO [t17845] VALUES (1,'مقدمة التحقيق','3','0','0');
+INSERT INTO [t17845] VALUES (2,'ترجمة المؤلف','6','1','0');
+INSERT INTO [t17845] VALUES (3,'نسبه ومولده','6','2','0');
+INSERT INTO [t17845] VALUES (4,'شيوخه ومسموعاته وتلاميذه','6','2','0');
+INSERT INTO [t17845] VALUES (5,'علمه ومنزلته','7','2','0');
+INSERT INTO [t17845] VALUES (6,'مصنفاته','8','2','0');
+INSERT INTO [t17845] VALUES (7,'وفاته','11','2','0');
+INSERT INTO [t17845] VALUES (8,'وصف النسخة المعتمدة','12','1','0');
+INSERT INTO [t17845] VALUES (9,'مسألة إتيان المأموم بالتسميع والتحميد وأقوال العلماء في ذلك','16','0','0');
+INSERT INTO [t17845] VALUES (10,'اقتصار المأموم على التحميد والجواب عليه','27','0','0');
+INSERT INTO [t17845] VALUES (11,'بقية مسألة الجهر بالتسميع','32','0','0');
