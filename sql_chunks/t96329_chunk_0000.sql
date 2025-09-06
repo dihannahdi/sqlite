67a191f7770a9da1a2a87436511e53d0f9008615
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:57:06 +0700

    Upload SQL chunk: t96329_chunk_0000.sql

---
+++ sql_chunks/t96329_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t96329
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t96329" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96329
+INSERT INTO [t96329] VALUES (1,'بحث','1','0','0');
