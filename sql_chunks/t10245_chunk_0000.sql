579657b0d5974e938e2ce42f6cc87e14fe2a1cdd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:25 +0700

    Upload SQL chunk: t10245_chunk_0000.sql

---
+++ sql_chunks/t10245_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t10245
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t10245" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10245
+INSERT INTO [t10245] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10245] VALUES (2,'الإعلام سلاح في يد الأعداء','3','0','0');
+INSERT INTO [t10245] VALUES (3,'الإعلام الطباعي','5','0','0');
+INSERT INTO [t10245] VALUES (4,'الإعلام الصهيوني','7','0','0');
+INSERT INTO [t10245] VALUES (5,'الإذاعات التنصيرية','9','0','0');
+INSERT INTO [t10245] VALUES (6,'الفيلم وقضايا المسلمين','12','0','0');
+INSERT INTO [t10245] VALUES (7,'مسؤولية الإعلام عن بث روح التضامن','13','0','0');
+INSERT INTO [t10245] VALUES (8,'الإعلام وأصالة التضامن الإسلامي','16','0','0');
+INSERT INTO [t10245] VALUES (9,'الإعلام وتحقيق التضامن الإسلامي','17','0','0');
+INSERT INTO [t10245] VALUES (10,'أساليب الإعلام والدعوة الإسلامية','20','0','0');
+INSERT INTO [t10245] VALUES (11,'الإعلام قوة حضارية إسلامية','21','0','0');
+INSERT INTO [t10245] VALUES (12,'الإعلام وإشاعة روح التضامن','23','0','0');
+INSERT INTO [t10245] VALUES (13,'مسؤولية الإعلام ومستقبله','25','0','0');
