34a7c36a5afc03108529a47bd0b80f5225c9eb44
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:41 +0700

    Upload SQL chunk: t5405_chunk_0000.sql

---
+++ sql_chunks/t5405_chunk_0000.sql
@@ -0,0 +1,44 @@
+-- Table: t5405
+-- Chunk: 1
+-- Rows: 37
+
+CREATE TABLE "t5405" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5405
+INSERT INTO [t5405] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5405] VALUES (2,'ما يذكر من حمل المرأة وولادتها والمولود','1','0','0');
+INSERT INTO [t5405] VALUES (3,'ما يذكر من تقلب أحوال الإنسان','1','0','0');
+INSERT INTO [t5405] VALUES (4,'هذا ما تسمي العرب من جماعة خلق الانسان','2','0','0');
+INSERT INTO [t5405] VALUES (5,'ثم الرأس','3','0','0');
+INSERT INTO [t5405] VALUES (6,'ثم الفروة','3','0','0');
+INSERT INTO [t5405] VALUES (7,'ومن ألوان الشعر','6','0','0');
+INSERT INTO [t5405] VALUES (8,'من اللحى','6','0','0');
+INSERT INTO [t5405] VALUES (9,'ثم الوجه','7','0','0');
+INSERT INTO [t5405] VALUES (10,'ثم الجبهة','7','0','0');
+INSERT INTO [t5405] VALUES (11,'ثم الحجاجان','7','0','0');
+INSERT INTO [t5405] VALUES (12,'ثم العين','8','0','0');
+INSERT INTO [t5405] VALUES (13,'ثم الأنف','10','0','0');
+INSERT INTO [t5405] VALUES (14,'ثم الفم','11','0','0');
+INSERT INTO [t5405] VALUES (15,'ثم اللثة','12','0','0');
+INSERT INTO [t5405] VALUES (16,'ثم اللسان','13','0','0');
+INSERT INTO [t5405] VALUES (17,'ثم الغلصمة','13','0','0');
+INSERT INTO [t5405] VALUES (18,'ثم الحلقوم','13','0','0');
+INSERT INTO [t5405] VALUES (19,'ثم العنق','13','0','0');
+INSERT INTO [t5405] VALUES (20,'ثم المنكب','15','0','0');
+INSERT INTO [t5405] VALUES (21,'ثم الكتف','15','0','0');
+INSERT INTO [t5405] VALUES (22,'ثم الذراع','15','0','0');
+INSERT INTO [t5405] VALUES (23,'ثم الكف','16','0','0');
+INSERT INTO [t5405] VALUES (24,'ثم الظهر','17','0','0');
+INSERT INTO [t5405] VALUES (25,'ثم الجنبان','18','0','0');
+INSERT INTO [t5405] VALUES (26,'ثم الصدر','18','0','0');
+INSERT INTO [t5405] VALUES (27,'ثم الجوف','19','0','0');
+INSERT INTO [t5405] VALUES (28,'ثم الخلب','19','0','0');
+INSERT INTO [t5405] VALUES (29,'ثم البطن','19','0','0');
+INSERT INTO [t5405] VALUES (30,'ثم الذكر','21','0','0');
+INSERT INTO [t5405] VALUES (31,'ثم الوركان','21','0','0');
+INSERT INTO [t5405] VALUES (32,'ثم الفخذان','21','0','0');
+INSERT INTO [t5405] VALUES (33,'ثم الركبة','22','0','0');
+INSERT INTO [t5405] VALUES (34,'ثم الساق','22','0','0');
+INSERT INTO [t5405] VALUES (35,'ثم القدم','22','0','0');
+INSERT INTO [t5405] VALUES (36,'ومما يكون في النساء دون الرجال','23','0','0');
+INSERT INTO [t5405] VALUES (37,'آخر الكتاب','23','0','0');
