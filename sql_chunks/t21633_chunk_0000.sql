8fbf6c486dd965cf16b1323b2c9fb3e04f29e2c3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:42 +0700

    Upload SQL chunk: t21633_chunk_0000.sql

---
+++ sql_chunks/t21633_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21633
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21633" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21633
+INSERT INTO [t21633] VALUES (1,'الكلام على حديث الإستلقاء لأبي موسى المديني - مخطوط','2','0','0');
