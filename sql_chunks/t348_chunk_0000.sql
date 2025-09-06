8958235310fae0bef56f483d67dcc7d0845eaefb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:44 +0700

    Upload SQL chunk: t348_chunk_0000.sql

---
+++ sql_chunks/t348_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t348
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t348" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t348
+INSERT INTO [t348] VALUES (1,'بداية السول','1','0','0');
