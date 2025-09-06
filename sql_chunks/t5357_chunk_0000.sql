651008ac5ab11afc71e74ebbf0553182d56e9249
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:17 +0700

    Upload SQL chunk: t5357_chunk_0000.sql

---
+++ sql_chunks/t5357_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t5357
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t5357" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5357
+INSERT INTO [t5357] VALUES (1,'نص الرسالة','1','0','0');
+INSERT INTO [t5357] VALUES (2,'البري ووجوهه','1','0','0');
+INSERT INTO [t5357] VALUES (3,'الدواة','1','0','0');
+INSERT INTO [t5357] VALUES (4,'الليقة','2','0','0');
+INSERT INTO [t5357] VALUES (5,'المداد','2','0','0');
+INSERT INTO [t5357] VALUES (6,'الحبر','3','0','0');
+INSERT INTO [t5357] VALUES (7,'الكتاب','3','0','0');
+INSERT INTO [t5357] VALUES (8,'المط','3','0','0');
+INSERT INTO [t5357] VALUES (9,'التطليس','4','0','0');
+INSERT INTO [t5357] VALUES (10,'القرطاس','4','0','0');
+INSERT INTO [t5357] VALUES (11,'السحاة','4','0','0');
+INSERT INTO [t5357] VALUES (12,'التراب','4','0','0');
+INSERT INTO [t5357] VALUES (13,'العنوان','4','0','0');
+INSERT INTO [t5357] VALUES (14,'الطين','4','0','0');
+INSERT INTO [t5357] VALUES (15,'الخاتم','4','0','0');
+INSERT INTO [t5357] VALUES (16,'القراءات ووجوهها','4','0','0');
+INSERT INTO [t5357] VALUES (17,'الديوان','5','0','0');
+INSERT INTO [t5357] VALUES (18,'التاريخ','5','0','0');
