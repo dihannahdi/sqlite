6a91043c1f8f24a0aa231d3cad229ca8013e6b7b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:04 +0700

    Upload SQL chunk: t23140_chunk_0000.sql

---
+++ sql_chunks/t23140_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t23140
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t23140" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23140
+INSERT INTO [t23140] VALUES (1,'خطبة الكتاب','1','0','0');
+INSERT INTO [t23140] VALUES (2,'الباب الأول: في بيان بناء الصحيح','5','0','0');
+INSERT INTO [t23140] VALUES (3,'فصل في بيان أمثلة الماضي','23','2','0');
+INSERT INTO [t23140] VALUES (4,'فصل وتدخل المضمرات في الماضي وأخواته','33','2','0');
+INSERT INTO [t23140] VALUES (5,'فصل في المستقبل','44','2','0');
+INSERT INTO [t23140] VALUES (6,'"فصل: في الأمر والنهي:','52','2','0');
+INSERT INTO [t23140] VALUES (7,'فصل في اسم الفاعل','65','2','0');
+INSERT INTO [t23140] VALUES (8,'فصل: في اسم المفعول','74','2','0');
+INSERT INTO [t23140] VALUES (9,'"فصل: في اسمي الزمان والمكان":','76','2','0');
+INSERT INTO [t23140] VALUES (10,'"فصل: في اسم الآلة:','79','2','0');
+INSERT INTO [t23140] VALUES (11,'الباب الثاني: في المضاعف','81','0','0');
+INSERT INTO [t23140] VALUES (12,'الباب الثالث: في المهموز','99','0','0');
+INSERT INTO [t23140] VALUES (13,'الباب الرابع في المعتل','116','0','0');
+INSERT INTO [t23140] VALUES (14,'الباب الخامس في الأجوف','120','0','0');
+INSERT INTO [t23140] VALUES (15,'الباب السادس في الناقص','138','0','0');
+INSERT INTO [t23140] VALUES (16,'الباب السابع في اللفيف','154','0','0');
+INSERT INTO [t23140] VALUES (17,'الفهرست','158','0','0');
