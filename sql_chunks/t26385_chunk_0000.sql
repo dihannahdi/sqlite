458d3f54c29878d6912012071f796a339d57ccab
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:10 +0700

    Upload SQL chunk: t26385_chunk_0000.sql

---
+++ sql_chunks/t26385_chunk_0000.sql
@@ -0,0 +1,69 @@
+-- Table: t26385
+-- Chunk: 1
+-- Rows: 62
+
+CREATE TABLE "t26385" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26385
+INSERT INTO [t26385] VALUES (1,'مقدمة الطبعة الثالثة','2','0','0');
+INSERT INTO [t26385] VALUES (2,'المبحث الأول: في عظم منزلة حفظ اللسان في الإسلام','18','1','0');
+INSERT INTO [t26385] VALUES (3,'المبحث الثاني في كتب الملكين كل ما يلفظ به اللسان من الكلام','26','1','0');
+INSERT INTO [t26385] VALUES (4,'المبحث الثالث: في كفارة من فاه بلفظ منهي عنه','27','1','0');
+INSERT INTO [t26385] VALUES (5,'المبحث الرابع: وسائل حفظ المنطق','29','1','0');
+INSERT INTO [t26385] VALUES (6,'المبحث الخامس: المؤلفات المفردة في المناهي اللفظية','42','1','0');
+INSERT INTO [t26385] VALUES (7,'معجم المناهي اللفظية','47','0','0');
+INSERT INTO [t26385] VALUES (8,'(حرف الألف)','47','7','0');
+INSERT INTO [t26385] VALUES (9,'(حرف الباء)','166','7','0');
+INSERT INTO [t26385] VALUES (10,'(حرف التاء)','180','7','0');
+INSERT INTO [t26385] VALUES (11,'(حرف الثاء)','203','7','0');
+INSERT INTO [t26385] VALUES (12,'(حرف الجيم)','204','7','0');
+INSERT INTO [t26385] VALUES (13,'(حرف الحاء)','215','7','0');
+INSERT INTO [t26385] VALUES (14,'(حرف الخاء)','236','7','0');
+INSERT INTO [t26385] VALUES (15,'(حرف الدال)','248','7','0');
+INSERT INTO [t26385] VALUES (16,'(حرف الذال)','259','7','0');
+INSERT INTO [t26385] VALUES (17,'(حرف الراء)','260','7','0');
+INSERT INTO [t26385] VALUES (18,'(حرف الزاي)','277','7','0');
+INSERT INTO [t26385] VALUES (19,'(حرف السين)','281','7','0');
+INSERT INTO [t26385] VALUES (20,'(حرف الشين)','301','7','0');
+INSERT INTO [t26385] VALUES (21,'(حرف الصاد)','314','7','0');
+INSERT INTO [t26385] VALUES (22,'(حرف الضاد)','341','7','0');
+INSERT INTO [t26385] VALUES (23,'(حرف الطاء)','343','7','0');
+INSERT INTO [t26385] VALUES (24,'(حرف الظاء)','350','7','0');
+INSERT INTO [t26385] VALUES (25,'(حرف العين)','351','7','0');
+INSERT INTO [t26385] VALUES (26,'(حرف الغين)','386','7','0');
+INSERT INTO [t26385] VALUES (27,'(حرف الفاء)','390','7','0');
+INSERT INTO [t26385] VALUES (28,'(حرف القاف)','416','7','0');
+INSERT INTO [t26385] VALUES (29,'(حرف الكاف)','431','7','0');
+INSERT INTO [t26385] VALUES (30,'(حرف اللام)','450','7','0');
+INSERT INTO [t26385] VALUES (31,'(حرف الميم)','465','7','0');
+INSERT INTO [t26385] VALUES (32,'(حرف النون)','516','7','0');
+INSERT INTO [t26385] VALUES (33,'(حرف الهاء)','523','7','0');
+INSERT INTO [t26385] VALUES (34,'(حرف الواو)','528','7','0');
+INSERT INTO [t26385] VALUES (35,'(حرف الياء)','550','7','0');
+INSERT INTO [t26385] VALUES (36,'فوائد في الألفاظ','568','0','0');
+INSERT INTO [t26385] VALUES (37,'حرف الألف','568','36','0');
+INSERT INTO [t26385] VALUES (38,'حرف الباء','590','36','0');
+INSERT INTO [t26385] VALUES (39,'حرف التاء','603','36','0');
+INSERT INTO [t26385] VALUES (40,'حرف الجيم','607','36','0');
+INSERT INTO [t26385] VALUES (41,'حرف الحاء','610','36','0');
+INSERT INTO [t26385] VALUES (42,'حرف الخاء','615','36','0');
+INSERT INTO [t26385] VALUES (43,'حرف الدال','616','36','0');
+INSERT INTO [t26385] VALUES (44,'حرف الذال','617','36','0');
+INSERT INTO [t26385] VALUES (45,'حرف الراء','618','36','0');
+INSERT INTO [t26385] VALUES (46,'حرف الزاي','621','36','0');
+INSERT INTO [t26385] VALUES (47,'حرف السين','624','36','0');
+INSERT INTO [t26385] VALUES (48,'حرف الشين','629','36','0');
+INSERT INTO [t26385] VALUES (49,'حرف الصاد','631','36','0');
+INSERT INTO [t26385] VALUES (50,'حرف الضاد','634','36','0');
+INSERT INTO [t26385] VALUES (51,'حرف العين','635','36','0');
+INSERT INTO [t26385] VALUES (52,'حرف الغين','644','36','0');
+INSERT INTO [t26385] VALUES (53,'حرف الفاء','645','36','0');
+INSERT INTO [t26385] VALUES (54,'حرف القاف','648','36','0');
+INSERT INTO [t26385] VALUES (55,'حرف الكاف','652','36','0');
+INSERT INTO [t26385] VALUES (56,'حرف اللام','654','36','0');
+INSERT INTO [t26385] VALUES (57,'حرف الميم','656','36','0');
+INSERT INTO [t26385] VALUES (58,'حرف النون','663','36','0');
+INSERT INTO [t26385] VALUES (59,'حرف الهاء','665','36','0');
+INSERT INTO [t26385] VALUES (60,'حرف الواو','667','36','0');
+INSERT INTO [t26385] VALUES (61,'حرف اللام ألف','671','36','0');
+INSERT INTO [t26385] VALUES (62,'حرف الياء','674','36','0');
