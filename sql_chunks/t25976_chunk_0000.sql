fdf97920157fd6a6ea398ba7c82973597252f63b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:13 +0700

    Upload SQL chunk: t25976_chunk_0000.sql

---
+++ sql_chunks/t25976_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t25976
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t25976" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t25976
+INSERT INTO [t25976] VALUES (1,'كيف يستفيد طالب العلم من كتب السنة (١)','2','0','0');
+INSERT INTO [t25976] VALUES (2,'كيف يستفيد طالب العلم من كتب السنة (٢)','20','0','0');
