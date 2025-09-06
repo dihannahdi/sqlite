be9b9ddcee4915b51395565543bc07f30b3afb0d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:40 +0700

    Upload SQL chunk: t11576_chunk_0000.sql

---
+++ sql_chunks/t11576_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t11576
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t11576" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11576
+INSERT INTO [t11576] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11576] VALUES (2,'القسم الأول: مصادر أصلية','4','0','0');
+INSERT INTO [t11576] VALUES (3,'القرآن الكريم','4','2','0');
+INSERT INTO [t11576] VALUES (4,'كتب الحديث النبوي','9','2','0');
+INSERT INTO [t11576] VALUES (5,'كتب السيرة المختصة','12','2','0');
+INSERT INTO [t11576] VALUES (6,'كتب الشمائل والدلائل','54','2','0');
+INSERT INTO [t11576] VALUES (7,'القسم الثاني: مصادر تكميلية','61','0','0');
+INSERT INTO [t11576] VALUES (8,'مدخل','61','7','0');
+INSERT INTO [t11576] VALUES (9,'أولا: المصادر التي اهتمت بتاريخ مكة المكرمة والمدينة المنورة','62','7','0');
+INSERT INTO [t11576] VALUES (10,'ثانيا: كتب الأدب والشعر','64','7','0');
+INSERT INTO [t11576] VALUES (11,'ثالثا: كتب التراجم','66','7','0');
+INSERT INTO [t11576] VALUES (12,'رابعا: كتب البلدان','68','7','0');
+INSERT INTO [t11576] VALUES (13,'الخاتمة','69','0','0');
+INSERT INTO [t11576] VALUES (14,'مصادر ومراجع','70','0','0');
