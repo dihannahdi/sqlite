067bd36de150fb09fd38238af8f95c553b3d9eae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:26 +0700

    Upload SQL chunk: t26740_chunk_0000.sql

---
+++ sql_chunks/t26740_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t26740
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t26740" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26740
+INSERT INTO [t26740] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26740] VALUES (2,'الجزء فيه حديث الشيخ الإمام الحافظ أبي إسحاق إبراهيم بن الحسين بن علي المعروف بابن ديزيل','2','0','0');
+INSERT INTO [t26740] VALUES (3,'إسلام كعب بن زهير','15','0','0');
+INSERT INTO [t26740] VALUES (4,'حديث أم زرع','18','0','0');
+INSERT INTO [t26740] VALUES (5,'تفسير الحديث','20','0','0');
