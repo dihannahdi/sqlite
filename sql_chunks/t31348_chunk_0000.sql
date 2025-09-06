ea8fb1f92197b246046ff0bc7f208dba8ec3162d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:08 +0700

    Upload SQL chunk: t31348_chunk_0000.sql

---
+++ sql_chunks/t31348_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t31348
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t31348" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31348
+INSERT INTO [t31348] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t31348] VALUES (2,'تعريف العقوق','7','0','0');
+INSERT INTO [t31348] VALUES (3,'من مظاهر عقوق الوالدين','8','0','0');
+INSERT INTO [t31348] VALUES (4,'نماذج من قصص العقوق','20','0','0');
+INSERT INTO [t31348] VALUES (5,'أسباب العقوق','24','0','0');
+INSERT INTO [t31348] VALUES (6,'سبل العلاج','28','0','0');
+INSERT INTO [t31348] VALUES (7,'تعريف البر بالوالدين','28','0','0');
+INSERT INTO [t31348] VALUES (8,'الآداب التي تراعى مع الوالدين','29','0','0');
+INSERT INTO [t31348] VALUES (9,'الأمور المعينة على البر','37','0','0');
+INSERT INTO [t31348] VALUES (10,'بين الزوجة والوالدين','41','0','0');
+INSERT INTO [t31348] VALUES (11,'نماذج من قصص البر','52','0','0');
