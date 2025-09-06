104621ee2fb3f4205d10c32a14c7e7ff5f431226
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:57:04 +0700

    Upload SQL chunk: t29715_chunk_0000.sql

---
+++ sql_chunks/t29715_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t29715
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t29715" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29715
+INSERT INTO [t29715] VALUES (1,'أمرني رسول الله صلى الله عليه وسلم أن أردف عائشة، فأعمرها من التنعيم.','2','0','0');
