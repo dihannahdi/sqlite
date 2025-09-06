8adccc5b833e155c5f10786bba352178e788a939
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:03:12 +0700

    Upload SQL chunk: t12222_chunk_0000.sql

---
+++ sql_chunks/t12222_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t12222
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t12222" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12222
+INSERT INTO [t12222] VALUES (1,'[مقدمة]','1','0','0');
+INSERT INTO [t12222] VALUES (2,'المؤلف','4','0','0');
+INSERT INTO [t12222] VALUES (3,'كتبه','8','0','0');
+INSERT INTO [t12222] VALUES (4,'كتاب الردة','11','0','0');
+INSERT INTO [t12222] VALUES (5,'من ألف في الردة','13','0','0');
+INSERT INTO [t12222] VALUES (6,'المخطوطة','15','0','0');
+INSERT INTO [t12222] VALUES (7,'منهج التحقيق','17','0','0');
+INSERT INTO [t12222] VALUES (8,'[اضطراب امر الناس عند وفاة النبي صلى الله عليه وآله وسلم]','20','0','0');
+INSERT INTO [t12222] VALUES (9,'أخبار سقيفة بني ساعدة','25','0','0');
+INSERT INTO [t12222] VALUES (10,'ذكر أخبار الردة','41','0','0');
+INSERT INTO [t12222] VALUES (11,'ذكر خروج أسامة بن زيد','47','0','0');
+INSERT INTO [t12222] VALUES (12,'ذكر فجاءة بن عبد ياليل','68','0','0');
+INSERT INTO [t12222] VALUES (13,'خبر مالك بن نويرة ومسيلمة الكذاب','96','0','0');
+INSERT INTO [t12222] VALUES (14,'ذكر ردة أهل البحرين','140','0','0');
+INSERT INTO [t12222] VALUES (15,'ذكر ارتداد أهل حضرموت من كندة وغيرها','160','0','0');
+INSERT INTO [t12222] VALUES (16,'نبذة في ذكر المثنى بن حارثة الشيباني، وهو أول الفتوح بعد قتال أهل الردة، وهو أيضا من رواية ابن الأعثم الكوفي','208','0','0');
