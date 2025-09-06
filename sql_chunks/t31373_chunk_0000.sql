cb71cb417f1bdd1a0acf55eda76097e71921b2e2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:19 +0700

    Upload SQL chunk: t31373_chunk_0000.sql

---
+++ sql_chunks/t31373_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t31373
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t31373" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31373
+INSERT INTO [t31373] VALUES (1,'نص الكتاب','1','0','0');
+INSERT INTO [t31373] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
+INSERT INTO [t31373] VALUES (3,'الحمد لله وبه نستعين','1','1','0');
