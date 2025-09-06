d656476f11e557b5c6e6ddaa9a8b61c3294a7432
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:18:57 +0700

    Upload SQL chunk: t14456_chunk_0000.sql

---
+++ sql_chunks/t14456_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t14456
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t14456" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14456
+INSERT INTO [t14456] VALUES (1,'لغتكم يا أيها العرب','4','0','0');
+INSERT INTO [t14456] VALUES (2,'آفة اللغة هذا النحو','10','0','0');
+INSERT INTO [t14456] VALUES (3,'بين العلم والأدب','17','0','0');
+INSERT INTO [t14456] VALUES (4,'العقيدة بين العقل والعاطفة','22','0','0');
+INSERT INTO [t14456] VALUES (5,'من غزل الفقهاء','28','0','0');
+INSERT INTO [t14456] VALUES (6,'مقالة في التحليل الأدبي','39','0','0');
+INSERT INTO [t14456] VALUES (7,'الملكة والثقافة','55','0','0');
+INSERT INTO [t14456] VALUES (8,'بحث في الوظيفة والموظفين','58','0','0');
+INSERT INTO [t14456] VALUES (9,'الحلقة المفقودة','67','0','0');
+INSERT INTO [t14456] VALUES (10,'من شوارد الشواهد','75','0','0');
+INSERT INTO [t14456] VALUES (11,'القضاء في الإسلام','99','0','0');
+INSERT INTO [t14456] VALUES (12,'الحجاب','119','0','0');
+INSERT INTO [t14456] VALUES (13,'التشجيع','125','0','0');
+INSERT INTO [t14456] VALUES (14,'الفتح الإسلامي','132','0','0');
+INSERT INTO [t14456] VALUES (15,'كيف تكون كاتبا','139','0','0');
+INSERT INTO [t14456] VALUES (16,'في النقد','144','0','0');
+INSERT INTO [t14456] VALUES (17,'الأدب العربي في مدارس العراق','147','0','0');
+INSERT INTO [t14456] VALUES (18,'أدب إقليمي','156','0','0');
+INSERT INTO [t14456] VALUES (19,'الحياة الأدبية في دمشق','161','0','0');
+INSERT INTO [t14456] VALUES (20,'الترجمة والتأليف','167','0','0');
+INSERT INTO [t14456] VALUES (21,'النفقات والتكافل الاجتماعي','171','0','0');
+INSERT INTO [t14456] VALUES (22,'تعبير الرؤيا لابن قتيبة','178','0','0');
+INSERT INTO [t14456] VALUES (23,'الأبيوردي','190','0','0');
+INSERT INTO [t14456] VALUES (24,'كلمة لا بد منها','202','0','0');
+INSERT INTO [t14456] VALUES (25,'سؤال','206','0','0');
