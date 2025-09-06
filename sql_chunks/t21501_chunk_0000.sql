2fe67da0becec58da9d9138cdf3ab2b564d63780
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:45 +0700

    Upload SQL chunk: t21501_chunk_0000.sql

---
+++ sql_chunks/t21501_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t21501
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t21501" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21501
+INSERT INTO [t21501] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t21501] VALUES (2,'الباب الأول','4','0','0');
+INSERT INTO [t21501] VALUES (3,'الباب الثاني','45','0','0');
+INSERT INTO [t21501] VALUES (4,'الباب الثالث','73','0','0');
+INSERT INTO [t21501] VALUES (5,'الباب الرابع','92','0','0');
