ed1714e8d5e22968a7957263a4b8a620c431e775
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:29 +0700

    Upload SQL chunk: t30628_chunk_0000.sql

---
+++ sql_chunks/t30628_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t30628
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t30628" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30628
+INSERT INTO [t30628] VALUES (1,'من حديث أبي العباس البصير الرازي','2','0','0');
+INSERT INTO [t30628] VALUES (2,'حديثان عن أبي الفضل عبد الله بن محمد بن الحارث، عن ابن كردي','5','0','0');
+INSERT INTO [t30628] VALUES (3,'من حديث ابن الوكيل','8','0','0');
+INSERT INTO [t30628] VALUES (4,'من حديث ابن مالك القطيعي','10','0','0');
+INSERT INTO [t30628] VALUES (5,'حديث مسلسل','14','0','0');
+INSERT INTO [t30628] VALUES (6,'من حديث ابن الطيوري','16','0','0');
+INSERT INTO [t30628] VALUES (7,'من حديث أبي منصور المقرب','19','0','0');
+INSERT INTO [t30628] VALUES (8,'من حديث ابن الفراء','29','0','0');
+INSERT INTO [t30628] VALUES (9,'من حديث الكتاني','38','0','0');
+INSERT INTO [t30628] VALUES (10,'من حديث الشريف العلوي','43','0','0');
+INSERT INTO [t30628] VALUES (11,'من حديث ابن البرذاني، عن جماعة كتبوا إليه من مصر','47','0','0');
+INSERT INTO [t30628] VALUES (12,'حديث واحد','49','0','0');
+INSERT INTO [t30628] VALUES (13,'من فوائد ابن الطيوري','51','0','0');
+INSERT INTO [t30628] VALUES (14,'من حديث الخلال','53','0','0');
+INSERT INTO [t30628] VALUES (15,'حديث واحد','56','0','0');
+INSERT INTO [t30628] VALUES (16,'حديث واحد','58','0','0');
+INSERT INTO [t30628] VALUES (17,'من حديث.... الملك أبو منصور..... الدواب','60','0','0');
+INSERT INTO [t30628] VALUES (18,'من حديث عبد الله بن عمر مشكرانة','64','0','0');
+INSERT INTO [t30628] VALUES (19,'من حديث أبي العباس الرازي','67','0','0');
+INSERT INTO [t30628] VALUES (20,'من حديث أبي بكر الخطيب','71','0','0');
