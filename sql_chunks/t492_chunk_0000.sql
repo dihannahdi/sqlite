344c7ec064184f579b67925df297b303b5f6b095
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:21 +0700

    Upload SQL chunk: t492_chunk_0000.sql

---
+++ sql_chunks/t492_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t492
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t492" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t492
+INSERT INTO [t492] VALUES (1,'الهمزة','1','0','0');
+INSERT INTO [t492] VALUES (2,'الباء','5','1','0');
+INSERT INTO [t492] VALUES (3,'التاء','6','1','0');
+INSERT INTO [t492] VALUES (4,'الجيم','7','1','0');
+INSERT INTO [t492] VALUES (5,'الحاء','8','1','0');
+INSERT INTO [t492] VALUES (6,'الخاء','14','1','0');
+INSERT INTO [t492] VALUES (7,'الدال','15','1','0');
+INSERT INTO [t492] VALUES (8,'الذال','16','1','0');
+INSERT INTO [t492] VALUES (9,'الراء','17','1','0');
+INSERT INTO [t492] VALUES (10,'الزاي','17','1','0');
+INSERT INTO [t492] VALUES (11,'السين','20','1','0');
+INSERT INTO [t492] VALUES (12,'الشين','26','1','0');
+INSERT INTO [t492] VALUES (13,'الصاد','27','1','0');
+INSERT INTO [t492] VALUES (14,'الضاد','27','1','0');
+INSERT INTO [t492] VALUES (15,'العين','28','1','0');
+INSERT INTO [t492] VALUES (16,'الغين','33','1','0');
+INSERT INTO [t492] VALUES (17,'الفاء','34','1','0');
+INSERT INTO [t492] VALUES (18,'القاف','34','1','0');
+INSERT INTO [t492] VALUES (19,'الكاف','34','1','0');
+INSERT INTO [t492] VALUES (20,'اللام','35','1','0');
+INSERT INTO [t492] VALUES (21,'الميم','35','1','0');
+INSERT INTO [t492] VALUES (22,'النون','37','1','0');
+INSERT INTO [t492] VALUES (23,'الواو','38','1','0');
+INSERT INTO [t492] VALUES (24,'الهاء','38','1','0');
+INSERT INTO [t492] VALUES (25,'الياء','40','1','0');
