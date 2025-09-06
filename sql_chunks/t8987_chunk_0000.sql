5e88eef9f71dc945921bc5c07d045247eb6b42f2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:09 +0700

    Upload SQL chunk: t8987_chunk_0000.sql

---
+++ sql_chunks/t8987_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t8987
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t8987" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8987
+INSERT INTO [t8987] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8987] VALUES (2,'الهدف','3','0','0');
+INSERT INTO [t8987] VALUES (3,'واقع العالم الإسلامي','5','0','0');
+INSERT INTO [t8987] VALUES (4,'الأقليات الدينية والقومية','11','0','0');
+INSERT INTO [t8987] VALUES (5,'طريقة الوحدة وإزالة العقبات','14','0','0');
