bcf35c6ed87c73ae599ecbc06c92478f2500d8e0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:32 +0700

    Upload SQL chunk: t5000_chunk_0000.sql

---
+++ sql_chunks/t5000_chunk_0000.sql
@@ -0,0 +1,51 @@
+-- Table: t5000
+-- Chunk: 1
+-- Rows: 44
+
+CREATE TABLE "t5000" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5000
+INSERT INTO [t5000] VALUES (1,'محاسن الشريعة','1','0','0');
+INSERT INTO [t5000] VALUES (2,'أسس الإحسان','4','0','0');
+INSERT INTO [t5000] VALUES (3,'التطبيق الفعلي لكمال الشريعة','5','0','0');
+INSERT INTO [t5000] VALUES (4,'مدخل','5','3','0');
+INSERT INTO [t5000] VALUES (5,'جلب المنافع','6','3','0');
+INSERT INTO [t5000] VALUES (6,'أمادفع المضار','7','3','0');
+INSERT INTO [t5000] VALUES (7,'أما الأديان','8','3','0');
+INSERT INTO [t5000] VALUES (8,'وأما حفظ العقل','9','3','0');
+INSERT INTO [t5000] VALUES (9,'وفي النسب','10','3','0');
+INSERT INTO [t5000] VALUES (10,'وفي العرض','11','3','0');
+INSERT INTO [t5000] VALUES (11,'والمال','12','3','0');
+INSERT INTO [t5000] VALUES (12,'أما مكارم الأخلاق','13','3','0');
+INSERT INTO [t5000] VALUES (13,'وفي وقعة','14','3','0');
+INSERT INTO [t5000] VALUES (14,'ومن المعاملات','16','3','0');
+INSERT INTO [t5000] VALUES (15,'أما الشمول','18','3','0');
+INSERT INTO [t5000] VALUES (16,'أما السماحة في الشريعة','20','3','0');
+INSERT INTO [t5000] VALUES (17,'ومن قواعدها','21','3','0');
+INSERT INTO [t5000] VALUES (18,'ومن السماحة','22','3','0');
+INSERT INTO [t5000] VALUES (19,'الكمال في القضاء الإسلامي','23','0','0');
+INSERT INTO [t5000] VALUES (20,'مدخل','23','19','0');
+INSERT INTO [t5000] VALUES (21,'أما النوع والمرتبة','24','19','0');
+INSERT INTO [t5000] VALUES (22,'سلم المحاكم','25','19','0');
+INSERT INTO [t5000] VALUES (23,'ماكان الوضع زمن رسول الله صلى الله عليه وسلم','28','19','0');
+INSERT INTO [t5000] VALUES (24,'وفي الزوجية','30','19','0');
+INSERT INTO [t5000] VALUES (25,'تطور الأمر','31','19','0');
+INSERT INTO [t5000] VALUES (26,'الكاتب والضبط','32','19','0');
+INSERT INTO [t5000] VALUES (27,'الكمال في منهج القضاء الإسلامي','33','19','0');
+INSERT INTO [t5000] VALUES (28,'المنهج القضائي في القرآن والسنة','34','0','0');
+INSERT INTO [t5000] VALUES (29,'مدخل','34','28','0');
+INSERT INTO [t5000] VALUES (30,'تطبيق ذلك عمليا','35','28','0');
+INSERT INTO [t5000] VALUES (31,'أما الشمول في الحكم','37','28','0');
+INSERT INTO [t5000] VALUES (32,'مساوىء القانون','40','0','0');
+INSERT INTO [t5000] VALUES (33,'مدخل','40','32','0');
+INSERT INTO [t5000] VALUES (34,'نشأة القانون','42','32','0');
+INSERT INTO [t5000] VALUES (35,'تواجد القانون في البلاد الإسلامية','43','0','0');
+INSERT INTO [t5000] VALUES (36,'مدخل','43','35','0');
+INSERT INTO [t5000] VALUES (37,'عوامل تدعيم القانون وبقائه','44','35','0');
+INSERT INTO [t5000] VALUES (38,'الطعن على الشريعة','45','35','0');
+INSERT INTO [t5000] VALUES (39,'أولا في قطع يد السارق','48','35','0');
+INSERT INTO [t5000] VALUES (40,'اعتراف بفضل الشريعة','50','35','0');
+INSERT INTO [t5000] VALUES (41,'أما رجم الزاني أو جلده','52','35','0');
+INSERT INTO [t5000] VALUES (42,'قضية العين','55','35','0');
+INSERT INTO [t5000] VALUES (43,'فكان تعليقنا','57','35','0');
+INSERT INTO [t5000] VALUES (44,'موقف القانون منها','60','35','0');
