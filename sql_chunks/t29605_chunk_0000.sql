742895e48d61c373deff359e79ca6ef1ecfbb971
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:36 +0700

    Upload SQL chunk: t29605_chunk_0000.sql

---
+++ sql_chunks/t29605_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t29605
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t29605" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29605
+INSERT INTO [t29605] VALUES (1,'المقدمة','7','0','0');
+INSERT INTO [t29605] VALUES (2,'[سرد الملاحن]','11','0','0');
