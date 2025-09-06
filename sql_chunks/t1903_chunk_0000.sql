df07a0e1bb2579b92087b31b1f4bfd7a2616da9b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:53 +0700

    Upload SQL chunk: t1903_chunk_0000.sql

---
+++ sql_chunks/t1903_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t1903
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t1903" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1903
+INSERT INTO [t1903] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1903] VALUES (2,'أنواع الخلق','3','0','0');
+INSERT INTO [t1903] VALUES (3,'الأخلاق والممارسة الإيمانية','4','0','0');
+INSERT INTO [t1903] VALUES (4,'دوام الأخلاق وثباتها','5','0','0');
+INSERT INTO [t1903] VALUES (5,'طبيعة السلوك الإنساني','6','0','0');
+INSERT INTO [t1903] VALUES (6,'السلوك وأنواعه','6','5','0');
+INSERT INTO [t1903] VALUES (7,'الأخلاق بين الجبلة والاكتساب','8','0','0');
+INSERT INTO [t1903] VALUES (8,'الأخلاق الفطرية','8','7','0');
+INSERT INTO [t1903] VALUES (9,'الأخلاق المكتسبة','11','7','0');
+INSERT INTO [t1903] VALUES (10,'أهمية الأخلاق في حياة البشر','12','0','0');
+INSERT INTO [t1903] VALUES (11,'الإلزام الخلقي','15','0','0');
+INSERT INTO [t1903] VALUES (12,'مصادر الإلزام الخلقي','15','11','0');
+INSERT INTO [t1903] VALUES (13,'ثبات الأخلاق الإسلامية','18','0','0');
+INSERT INTO [t1903] VALUES (14,'آثار ثبات الأخلاق','18','13','0');
+INSERT INTO [t1903] VALUES (15,'قيم خلقية في الإسلام','19','0','0');
+INSERT INTO [t1903] VALUES (16,'العدل','19','15','0');
+INSERT INTO [t1903] VALUES (17,'الحياء','24','15','0');
+INSERT INTO [t1903] VALUES (18,'الحلم','27','15','0');
