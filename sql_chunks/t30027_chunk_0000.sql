21a6b674b110a69dede2675755c189362a1cbaf0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:16 +0700

    Upload SQL chunk: t30027_chunk_0000.sql

---
+++ sql_chunks/t30027_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30027
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30027" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30027
+INSERT INTO [t30027] VALUES (1,'مجلس آخر','11','0','0');
