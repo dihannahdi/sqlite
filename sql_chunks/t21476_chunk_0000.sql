476bbccb5bf8c2ee81dbe39828b37c6970ee705b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:24 +0700

    Upload SQL chunk: t21476_chunk_0000.sql

---
+++ sql_chunks/t21476_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t21476
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t21476" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21476
+INSERT INTO [t21476] VALUES (1,'مجلس آخر، وهو الثاني','17','0','0');
+INSERT INTO [t21476] VALUES (2,'مجلس ثالث','30','0','0');
+INSERT INTO [t21476] VALUES (3,'وأول المجلس الرابع','39','0','0');
+INSERT INTO [t21476] VALUES (4,'أول المجلس الخامس','49','0','0');
+INSERT INTO [t21476] VALUES (5,'وأول المجلس السادس','55','0','0');
+INSERT INTO [t21476] VALUES (6,'وأول المجلس السابع','62','0','0');
+INSERT INTO [t21476] VALUES (7,'أول المجلس الثامن','70','0','0');
+INSERT INTO [t21476] VALUES (8,'أول المجلس التاسع','81','0','0');
+INSERT INTO [t21476] VALUES (9,'أول المجلس العاشر','91','0','0');
+INSERT INTO [t21476] VALUES (10,'مجلس حادي عشر','101','0','0');
+INSERT INTO [t21476] VALUES (11,'وأول المجلس الثاني عشر','110','0','0');
+INSERT INTO [t21476] VALUES (12,'أول الثالث عشر','123','0','0');
+INSERT INTO [t21476] VALUES (13,'أول المجلس الرابع عشر','135','0','0');
+INSERT INTO [t21476] VALUES (14,'وأول المجلس الخامس عشر','146','0','0');
+INSERT INTO [t21476] VALUES (15,'وأول المجلس السادس عشر','159','0','0');
+INSERT INTO [t21476] VALUES (16,'أول المجلس السابع عشر','170','0','0');
+INSERT INTO [t21476] VALUES (17,'وأول المجلس الثامن عشر','181','0','0');
+INSERT INTO [t21476] VALUES (18,'وأول المجلس التاسع عشر','193','0','0');
+INSERT INTO [t21476] VALUES (19,'وأول المجلس العشرون وهو آخر مجالس إملاء ابن سمعون رحمه الله','205','0','0');
