483126e4eadb910c303ac1412171b8f4b205882d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:22 +0700

    Upload SQL chunk: t21696_chunk_0000.sql

---
+++ sql_chunks/t21696_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21696
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21696" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21696
+INSERT INTO [t21696] VALUES (1,'من حديث مالك بن أنس للأزدي - مخطوط','2','0','0');
