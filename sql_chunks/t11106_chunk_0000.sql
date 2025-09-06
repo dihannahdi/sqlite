0ca4536a0d8ce50255424fecea1835db9e174d41
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:20 +0700

    Upload SQL chunk: t11106_chunk_0000.sql

---
+++ sql_chunks/t11106_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t11106
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t11106" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11106
+INSERT INTO [t11106] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11106] VALUES (2,'تمهيد','6','0','0');
+INSERT INTO [t11106] VALUES (3,'الفصل الأول: القرآن الكريم مصدر الهداية','14','0','0');
+INSERT INTO [t11106] VALUES (4,'أولا: من فضائل القرآن الكريم','14','3','0');
+INSERT INTO [t11106] VALUES (5,'ثانيا: من آداب تلاوته','16','3','0');
+INSERT INTO [t11106] VALUES (6,'ثالثا: اشتمال القرآن والسنة المبينة له على أهم طرق التعليم التربوي','18','3','0');
+INSERT INTO [t11106] VALUES (7,'رابعا: أهداف تعليم القرآن الكريم وعلومه بمدارس التحفيظ','30','3','0');
+INSERT INTO [t11106] VALUES (8,'الفصل الثاني: تقويم طرق تعليم القرآن الكريم وعلومه بمدارس التحفيظ','33','0','0');
+INSERT INTO [t11106] VALUES (9,'حفظ القرآن','33','8','0');
+INSERT INTO [t11106] VALUES (10,'التلاوة','46','8','0');
+INSERT INTO [t11106] VALUES (11,'التجويد','51','8','0');
+INSERT INTO [t11106] VALUES (12,'القراءات القرآنية','57','8','0');
+INSERT INTO [t11106] VALUES (13,'التفسير','61','8','0');
+INSERT INTO [t11106] VALUES (14,'علوم القرآن الكريم','67','8','0');
+INSERT INTO [t11106] VALUES (15,'خاتمة وتوصية','71','0','0');
+INSERT INTO [t11106] VALUES (16,'مصادر ومراجع','72','0','0');
