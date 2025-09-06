d00fdba5cd96a5f564e38fd990ad280b23d52a63
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:22 +0700

    Upload SQL chunk: t5794_chunk_0000.sql

---
+++ sql_chunks/t5794_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t5794
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t5794" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5794
+INSERT INTO [t5794] VALUES (1,'باب الألف','3','0','0');
+INSERT INTO [t5794] VALUES (2,'باب الباء','10','0','0');
+INSERT INTO [t5794] VALUES (3,'باب التاء','18','0','0');
+INSERT INTO [t5794] VALUES (4,'باب الثاء','21','0','0');
+INSERT INTO [t5794] VALUES (5,'باب الجيم','23','0','0');
+INSERT INTO [t5794] VALUES (6,'باب الحاء','28','0','0');
+INSERT INTO [t5794] VALUES (7,'باب الخاء','37','0','0');
+INSERT INTO [t5794] VALUES (8,'باب الدال','40','0','0');
+INSERT INTO [t5794] VALUES (9,'باب الذال فارغ','45','0','0');
+INSERT INTO [t5794] VALUES (10,'باب الراء','45','0','0');
+INSERT INTO [t5794] VALUES (11,'باب الزاي','51','0','0');
+INSERT INTO [t5794] VALUES (12,'باب السين','55','0','0');
+INSERT INTO [t5794] VALUES (13,'باب الشين','62','0','0');
+INSERT INTO [t5794] VALUES (14,'باب الصاد','66','0','0');
+INSERT INTO [t5794] VALUES (15,'باب الضاد','72','0','0');
+INSERT INTO [t5794] VALUES (16,'باب الطاء','73','0','0');
+INSERT INTO [t5794] VALUES (17,'باب العين','78','0','0');
+INSERT INTO [t5794] VALUES (18,'باب الغين','87','0','0');
+INSERT INTO [t5794] VALUES (19,'باب الفاء','88','0','0');
+INSERT INTO [t5794] VALUES (20,'باب القاف','90','0','0');
+INSERT INTO [t5794] VALUES (21,'باب الكاف','97','0','0');
+INSERT INTO [t5794] VALUES (22,'باب اللام','102','0','0');
+INSERT INTO [t5794] VALUES (23,'باب الميم','103','0','0');
+INSERT INTO [t5794] VALUES (24,'باب النون','116','0','0');
+INSERT INTO [t5794] VALUES (25,'باب الواو','120','0','0');
+INSERT INTO [t5794] VALUES (26,'باب الهاء','122','0','0');
+INSERT INTO [t5794] VALUES (27,'باب الياء','123','0','0');
