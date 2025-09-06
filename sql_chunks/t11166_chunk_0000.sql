008ef0df9ab011450c4e1dddc1481a5a2bb10c48
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:52 +0700

    Upload SQL chunk: t11166_chunk_0000.sql

---
+++ sql_chunks/t11166_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11166
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11166" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11166
+INSERT INTO [t11166] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11166] VALUES (2,'الفصل الأول: حكم تارك الصلاة','2','0','0');
+INSERT INTO [t11166] VALUES (3,'الفصل الثاني: فيما يترتب على الردة بترك الصلاة أو غيره','20','0','0');
