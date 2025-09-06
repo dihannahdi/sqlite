d30633d9b29eafbdeab84f188568404df0ec2c02
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:01 +0700

    Upload SQL chunk: t11082_chunk_0000.sql

---
+++ sql_chunks/t11082_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t11082
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t11082" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11082
+INSERT INTO [t11082] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11082] VALUES (2,'الفصل الأول','30','0','0');
+INSERT INTO [t11082] VALUES (3,'الفصل الثاني','42','0','0');
+INSERT INTO [t11082] VALUES (4,'الفصل الثالث','47','0','0');
+INSERT INTO [t11082] VALUES (5,'الفصل الرابع','51','0','0');
+INSERT INTO [t11082] VALUES (6,'الفصل الخامس','57','0','0');
+INSERT INTO [t11082] VALUES (7,'الفصل السادس','68','0','0');
+INSERT INTO [t11082] VALUES (8,'الفصل السابع','78','0','0');
+INSERT INTO [t11082] VALUES (9,'الفصل الثامن','91','0','0');
+INSERT INTO [t11082] VALUES (10,'الفصل التاسع','102','0','0');
+INSERT INTO [t11082] VALUES (11,'الفصل العاشر','132','0','0');
+INSERT INTO [t11082] VALUES (12,'الفصل الحادي عشر','135','0','0');
+INSERT INTO [t11082] VALUES (13,'أطراف الحديث','143','0','0');
