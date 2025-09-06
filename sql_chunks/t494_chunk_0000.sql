638d36b90503d4ff16dee216622d47186ccb9842
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:23 +0700

    Upload SQL chunk: t494_chunk_0000.sql

---
+++ sql_chunks/t494_chunk_0000.sql
@@ -0,0 +1,58 @@
+-- Table: t494
+-- Chunk: 1
+-- Rows: 51
+
+CREATE TABLE "t494" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t494
+INSERT INTO [t494] VALUES (1,'فصل','2','0','0');
+INSERT INTO [t494] VALUES (2,'فصل','2','0','0');
+INSERT INTO [t494] VALUES (3,'في ذكر طبقات العرب','2','0','0');
+INSERT INTO [t494] VALUES (4,'الفئة الأولى من طبقات العرب، العرب العاربة','2','0','0');
+INSERT INTO [t494] VALUES (5,'الفئة الثانية من طبقات العرب وهم العرب العرباء','3','0','0');
+INSERT INTO [t494] VALUES (6,'فصل','3','0','0');
+INSERT INTO [t494] VALUES (7,'في ذكر قحطان وذكر كتاب وصايا الملوك','3','0','0');
+INSERT INTO [t494] VALUES (8,'فصل','4','0','0');
+INSERT INTO [t494] VALUES (9,'فصل','17','0','0');
+INSERT INTO [t494] VALUES (10,'في قضاعة بن مالك بن حمير بن سبأ بن يشجب','17','0','0');
+INSERT INTO [t494] VALUES (11,'فصل','20','0','0');
+INSERT INTO [t494] VALUES (12,'فصل','20','0','0');
+INSERT INTO [t494] VALUES (13,'فصل','20','0','0');
+INSERT INTO [t494] VALUES (14,'فصل','20','0','0');
+INSERT INTO [t494] VALUES (15,'في جهينة','20','0','0');
+INSERT INTO [t494] VALUES (16,'في بني نهد','20','0','0');
+INSERT INTO [t494] VALUES (17,'فصل','21','0','0');
+INSERT INTO [t494] VALUES (18,'فصل','22','0','0');
+INSERT INTO [t494] VALUES (19,'في بطون حضرموت بن قحطان','22','0','0');
+INSERT INTO [t494] VALUES (20,'فصل','23','0','0');
+INSERT INTO [t494] VALUES (21,'فصل','23','0','0');
+INSERT INTO [t494] VALUES (22,'في جرهم بن قحطان','23','0','0');
+INSERT INTO [t494] VALUES (23,'في نسب كهلان','23','0','0');
+INSERT INTO [t494] VALUES (24,'فصل','30','0','0');
+INSERT INTO [t494] VALUES (25,'فصل','33','0','0');
+INSERT INTO [t494] VALUES (26,'ومن بطون الأزد الدواسر','33','0','0');
+INSERT INTO [t494] VALUES (27,'فصل','34','0','0');
+INSERT INTO [t494] VALUES (28,'فصل','40','0','0');
+INSERT INTO [t494] VALUES (29,'في جديلة','40','0','0');
+INSERT INTO [t494] VALUES (30,'فصل','48','0','0');
+INSERT INTO [t494] VALUES (31,'فصل','50','0','0');
+INSERT INTO [t494] VALUES (32,'فصل','52','0','0');
+INSERT INTO [t494] VALUES (33,'فصل','52','0','0');
+INSERT INTO [t494] VALUES (34,'في عاملة','52','0','0');
+INSERT INTO [t494] VALUES (35,'في لخم','52','0','0');
+INSERT INTO [t494] VALUES (36,'فصل','53','0','0');
+INSERT INTO [t494] VALUES (37,'في جذام أخي لخم','53','0','0');
+INSERT INTO [t494] VALUES (38,'فصل','54','0','0');
+INSERT INTO [t494] VALUES (39,'في كندة','54','0','0');
+INSERT INTO [t494] VALUES (40,'فصل','55','0','0');
+INSERT INTO [t494] VALUES (41,'في همدان','55','0','0');
+INSERT INTO [t494] VALUES (42,'فصل','57','0','0');
+INSERT INTO [t494] VALUES (43,'القسم الثاني " العرب المستعربة "','57','0','0');
+INSERT INTO [t494] VALUES (44,'فصل','62','0','0');
+INSERT INTO [t494] VALUES (45,'في كنانة','62','0','0');
+INSERT INTO [t494] VALUES (46,'فصل','63','0','0');
+INSERT INTO [t494] VALUES (47,'فصل','66','0','0');
+INSERT INTO [t494] VALUES (48,'فصل','67','0','0');
+INSERT INTO [t494] VALUES (49,'الفائدة الأولى','68','0','0');
+INSERT INTO [t494] VALUES (50,'الفائدة الثانية','69','0','0');
+INSERT INTO [t494] VALUES (51,'الفائدة الثالثة','71','0','0');
