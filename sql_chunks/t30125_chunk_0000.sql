8c98db575a8a0e6c72a9af683d341ef3ee040f7e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:52 +0700

    Upload SQL chunk: t30125_chunk_0000.sql

---
+++ sql_chunks/t30125_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t30125
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t30125" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30125
+INSERT INTO [t30125] VALUES (1,'الجزء السادس عشر من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30125] VALUES (2,'من حديث ابن الغريق','2','0','0');
+INSERT INTO [t30125] VALUES (3,'من حديث أبي طالب العشاري','6','0','0');
+INSERT INTO [t30125] VALUES (4,'من حديث عبد الله بن سعيد الكندي وهو أبو سعيد الأشج','28','0','0');
+INSERT INTO [t30125] VALUES (5,'من حديث القاضي أبي الحسين بن الغريق','32','0','0');
