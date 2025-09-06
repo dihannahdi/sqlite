3b40bde4f0d9c2b991aa0f7d2d0e948bc8627f00
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:00:18 +0700

    Upload SQL chunk: t1196_chunk_0000.sql

---
+++ sql_chunks/t1196_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t1196
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t1196" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1196
+INSERT INTO [t1196] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1196] VALUES (2,'سماحة الإسلام في التعامل مع غير المسلمين','4','0','0');
+INSERT INTO [t1196] VALUES (3,'سماحة الإسلام في التجارة وقضاء الحق','17','0','0');
+INSERT INTO [t1196] VALUES (4,'سماحة الإسلام في درء الحدود','19','0','0');
+INSERT INTO [t1196] VALUES (5,'سماحة الإسلام في حالات الضرورة','36','0','0');
