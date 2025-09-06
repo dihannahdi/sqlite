ffe50e7e65f449ae3d601151dec52012e73307ba
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:51 +0700

    Upload SQL chunk: t405_chunk_0000.sql

---
+++ sql_chunks/t405_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t405
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t405" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t405
+INSERT INTO [t405] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t405] VALUES (2,'وبالله التوفيق والإعانة','1','1','0');
+INSERT INTO [t405] VALUES (3,'فضل مصر على غيرها','1','0','0');
+INSERT INTO [t405] VALUES (4,'ذكر ما ورد في فضل مصر','1','0','0');
+INSERT INTO [t405] VALUES (5,'دعاء الأنبياء لمصر وأهلها','2','0','0');
+INSERT INTO [t405] VALUES (6,'من صاهر القبط من الأنبياء','2','0','0');
+INSERT INTO [t405] VALUES (7,'من ذكرهم الله تعالى في كتابه من أهل مصر','2','0','0');
+INSERT INTO [t405] VALUES (8,'ذكر من أظهرته مصر من الحكماء','3','0','0');
+INSERT INTO [t405] VALUES (9,'من كان بمصر من الأنبياء','4','0','0');
+INSERT INTO [t405] VALUES (10,'من كان بمصر في الإسلام من الصحابة','4','0','0');
+INSERT INTO [t405] VALUES (11,'من كان بمصر من الفقهاء والعلماء','5','0','0');
+INSERT INTO [t405] VALUES (12,'من كان بمصر من الزهاد','5','0','0');
+INSERT INTO [t405] VALUES (13,'من ولد بمصر من الخلفاء','5','0','0');
+INSERT INTO [t405] VALUES (14,'من دخل مصر من الفقهاء وغيرهم','5','0','0');
+INSERT INTO [t405] VALUES (15,'من دخل مصر من الخلفاء','5','0','0');
+INSERT INTO [t405] VALUES (16,'من دخل مصر من الشعراء','5','0','0');
+INSERT INTO [t405] VALUES (17,'ذكر مصر وفضلها على غيرها من الأمصار','5','0','0');
+INSERT INTO [t405] VALUES (18,'كور مصر','6','0','0');
+INSERT INTO [t405] VALUES (19,'خراج مصر','8','0','0');
+INSERT INTO [t405] VALUES (20,'مناظر مصر وجمالها','9','0','0');
+INSERT INTO [t405] VALUES (21,'ذكر ما ورد في نيل مصر','9','0','0');
+INSERT INTO [t405] VALUES (22,'فضل مقبرة مصر','10','0','0');
+INSERT INTO [t405] VALUES (23,'الخواص والعجائب التي بمصر','11','0','0');
