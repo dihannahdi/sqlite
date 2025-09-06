c7b5dbca44efb0b9a903ce20f730c15ea2b9b6aa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:50:58 +0700

    Upload SQL chunk: t10732_chunk_0000.sql

---
+++ sql_chunks/t10732_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t10732
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t10732" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10732
+INSERT INTO [t10732] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t10732] VALUES (2,'وصلى الله على سيدنا محمد وآله وصحبه وسلم تسليما','1','0','0');
+INSERT INTO [t10732] VALUES (3,'ذكر رحلتي إلى مصر ومن اجتمعت به هنالك من المشايخ وما أخذته عنهم ومن أجازني منهم وما أجازني به وما يتبع ذلك','26','0','0');
+INSERT INTO [t10732] VALUES (4,'خاتمة تتضمن الاعتذار','40','0','0');
