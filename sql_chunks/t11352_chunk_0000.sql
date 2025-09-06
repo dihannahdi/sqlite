c8bb9e7f4a8c20ac650e8428f9cefb5b4a5cd545
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:20 +0700

    Upload SQL chunk: t11352_chunk_0000.sql

---
+++ sql_chunks/t11352_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t11352
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t11352" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11352
+INSERT INTO [t11352] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11352] VALUES (2,'تمهيد','4','0','0');
+INSERT INTO [t11352] VALUES (3,'فصل','11','0','0');
+INSERT INTO [t11352] VALUES (4,'فصل','20','0','0');
+INSERT INTO [t11352] VALUES (5,'فصل','23','0','0');
+INSERT INTO [t11352] VALUES (6,'فصل','79','0','0');
+INSERT INTO [t11352] VALUES (7,'فصل في الفرق بين قيام الحجة وفهم الحجة','88','0','0');
+INSERT INTO [t11352] VALUES (8,'فصل','94','0','0');
+INSERT INTO [t11352] VALUES (9,'فصل','97','0','0');
+INSERT INTO [t11352] VALUES (10,'فصل','101','0','0');
+INSERT INTO [t11352] VALUES (11,'فصل','111','0','0');
+INSERT INTO [t11352] VALUES (12,'فصل','113','0','0');
+INSERT INTO [t11352] VALUES (13,'فصل','115','0','0');
+INSERT INTO [t11352] VALUES (14,'فصل','122','0','0');
+INSERT INTO [t11352] VALUES (15,'فصل','125','0','0');
