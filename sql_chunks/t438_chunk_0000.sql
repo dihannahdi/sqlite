ecb06fb0dc4ae307b715dbb3384dc5192549de04
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:01 +0700

    Upload SQL chunk: t438_chunk_0000.sql

---
+++ sql_chunks/t438_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t438
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t438" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t438
+INSERT INTO [t438] VALUES (1,'سبب تأليف الرسالة','2','0','0');
+INSERT INTO [t438] VALUES (2,'مقدمة في حقيقة الوتر','3','0','0');
+INSERT INTO [t438] VALUES (3,'المقالة الأولى: في حقيقة الوتر','6','0','0');
+INSERT INTO [t438] VALUES (4,'مبحث في الوتر بواحدة','12','3','0');
+INSERT INTO [t438] VALUES (5,'بحث في حقيقة الوتر ومسماه في الشرع','14','3','0');
+INSERT INTO [t438] VALUES (6,'اختيار المؤلف في حقيقة الوتر','22','3','0');
+INSERT INTO [t438] VALUES (7,'مسألة الوتر بواحدة بعد العشاء','29','3','0');
+INSERT INTO [t438] VALUES (8,'الفصل والوصل بين ركعات الوتر','33','3','0');
+INSERT INTO [t438] VALUES (9,'الفصل الثاني: في الاقتصار على ثلاث','40','0','0');
+INSERT INTO [t438] VALUES (10,'تحقيق الكلام في حديث: «لا توتروا بثلاث»','44','9','0');
+INSERT INTO [t438] VALUES (11,'تنبيه (في الشفع الذي يسبق الوتر)','48','9','0');
+INSERT INTO [t438] VALUES (12,'حكم صلاة الوتر','52','0','0');
