bf57011b0e57eb4287e7c0acc7997fbff9495019
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:37 +0700

    Upload SQL chunk: t12153_chunk_0000.sql

---
+++ sql_chunks/t12153_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t12153
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t12153" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12153
+INSERT INTO [t12153] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12153] VALUES (2,'التعريف بالرسالة ووصف النسخ الخطية وعملي فيها','5','0','0');
+INSERT INTO [t12153] VALUES (3,'التحقيق','15','0','0');
+INSERT INTO [t12153] VALUES (4,'مدخل','15','3','0');
+INSERT INTO [t12153] VALUES (5,'الوجه الأول','23','3','0');
+INSERT INTO [t12153] VALUES (6,'الوجه الثاني','24','3','0');
+INSERT INTO [t12153] VALUES (7,'الوجه الثالث','26','3','0');
+INSERT INTO [t12153] VALUES (8,'الوجه الرابع','27','3','0');
+INSERT INTO [t12153] VALUES (9,'الوجه الخامس','29','3','0');
+INSERT INTO [t12153] VALUES (10,'الوجه السادس','30','3','0');
+INSERT INTO [t12153] VALUES (11,'الوجه السابع','31','3','0');
+INSERT INTO [t12153] VALUES (12,'الوجه الثامن','33','3','0');
+INSERT INTO [t12153] VALUES (13,'الوجه التاسع','34','3','0');
+INSERT INTO [t12153] VALUES (14,'الوجه العاشر','36','3','0');
+INSERT INTO [t12153] VALUES (15,'الوجه الحادي عشر','37','3','0');
+INSERT INTO [t12153] VALUES (16,'الوجه الثاني عشر','38','3','0');
+INSERT INTO [t12153] VALUES (17,'الوجه الثالث عشر','39','3','0');
+INSERT INTO [t12153] VALUES (18,'الوجه الرابع عشر','40','3','0');
+INSERT INTO [t12153] VALUES (19,'الوجه الخامس عشر','41','3','0');
+INSERT INTO [t12153] VALUES (20,'الوجه السادس عشر','43','3','0');
+INSERT INTO [t12153] VALUES (21,'الوجه السابع عشر','44','3','0');
+INSERT INTO [t12153] VALUES (22,'الوجه الثامن عشر','45','3','0');
+INSERT INTO [t12153] VALUES (23,'الوجه التاسع عشر','47','3','0');
+INSERT INTO [t12153] VALUES (24,'الوجه العشرون','48','3','0');
+INSERT INTO [t12153] VALUES (25,'مصادر ومراجع','49','0','0');
