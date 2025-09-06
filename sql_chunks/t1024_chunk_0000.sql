0d7090d37065440784e07b9f6a2ca8abb54bf59e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:26 +0700

    Upload SQL chunk: t1024_chunk_0000.sql

---
+++ sql_chunks/t1024_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t1024
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t1024" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1024
+INSERT INTO [t1024] VALUES (1,'بسم الله الرحمن الرحيم','21','0','0');
+INSERT INTO [t1024] VALUES (2,'وبه أستعين','21','0','0');
