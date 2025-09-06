52a13213bf04cc065fd48ad7d48e95bb50f05670
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:30:26 +0700

    Upload SQL chunk: t17534_chunk_0000.sql

---
+++ sql_chunks/t17534_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t17534
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t17534" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17534
+INSERT INTO [t17534] VALUES (1,'مقدمة التحقيق','5','0','0');
+INSERT INTO [t17534] VALUES (2,'ترجمة المؤلف','8','1','0');
+INSERT INTO [t17534] VALUES (3,'مقدمة المؤلف','16','0','0');
+INSERT INTO [t17534] VALUES (4,'فصل','17','3','0');
+INSERT INTO [t17534] VALUES (5,'حرف الهمزة','18','0','0');
+INSERT INTO [t17534] VALUES (6,'حرف الباء','60','0','0');
+INSERT INTO [t17534] VALUES (7,'حرف التاء','63','0','0');
+INSERT INTO [t17534] VALUES (8,'حرف الثاء','64','0','0');
+INSERT INTO [t17534] VALUES (9,'حرف الجيم','65','0','0');
+INSERT INTO [t17534] VALUES (10,'حرف الحاء','67','0','0');
+INSERT INTO [t17534] VALUES (11,'حرف الخاء','84','0','0');
+INSERT INTO [t17534] VALUES (12,'حرف الدال','89','0','0');
+INSERT INTO [t17534] VALUES (13,'حرف الذال','90','0','0');
+INSERT INTO [t17534] VALUES (14,'حرف الراء','91','0','0');
+INSERT INTO [t17534] VALUES (15,'حرف الزاي','93','0','0');
+INSERT INTO [t17534] VALUES (16,'حرف السين','98','0','0');
+INSERT INTO [t17534] VALUES (17,'حرف الشين','111','0','0');
+INSERT INTO [t17534] VALUES (18,'حرف الصاد','113','0','0');
+INSERT INTO [t17534] VALUES (19,'حرف الضاد','115','0','0');
+INSERT INTO [t17534] VALUES (20,'حرف الطاء','116','0','0');
+INSERT INTO [t17534] VALUES (21,'حرف العين','118','0','0');
+INSERT INTO [t17534] VALUES (22,'حرف الغين','184','0','0');
+INSERT INTO [t17534] VALUES (23,'حرف الفاء','185','0','0');
+INSERT INTO [t17534] VALUES (24,'حرف القاف','187','0','0');
+INSERT INTO [t17534] VALUES (25,'حرف الكاف','190','0','0');
+INSERT INTO [t17534] VALUES (26,'حرف اللام','191','0','0');
+INSERT INTO [t17534] VALUES (27,'حرف الميم','192','0','0');
+INSERT INTO [t17534] VALUES (28,'حرف النون','250','0','0');
+INSERT INTO [t17534] VALUES (29,'حرف الهاء','252','0','0');
+INSERT INTO [t17534] VALUES (30,'حرف الواو','258','0','0');
+INSERT INTO [t17534] VALUES (31,'حرف الياء','261','0','0');
+INSERT INTO [t17534] VALUES (32,'فصل في الكنى','273','0','0');
+INSERT INTO [t17534] VALUES (33,'فصل في النساء','281','0','0');
