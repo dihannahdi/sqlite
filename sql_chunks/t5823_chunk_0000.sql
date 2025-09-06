413647784f9eceb1a42f0552e97020733fb91451
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:45 +0700

    Upload SQL chunk: t5823_chunk_0000.sql

---
+++ sql_chunks/t5823_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t5823
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t5823" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5823
+INSERT INTO [t5823] VALUES (1,'الطبقة الأولى من الصحابة','3','0','0');
+INSERT INTO [t5823] VALUES (2,'الطبقة الثانية من كبار التابعين','10','0','0');
+INSERT INTO [t5823] VALUES (3,'الطبقة الثالثة الوسطى من التابعين','25','0','0');
+INSERT INTO [t5823] VALUES (4,'الطبقة الرابعة صغار التابعين','39','0','0');
+INSERT INTO [t5823] VALUES (5,'الطبقة الخامسة','67','0','0');
+INSERT INTO [t5823] VALUES (6,'الطبقة السادسة','100','0','0');
+INSERT INTO [t5823] VALUES (7,'الطبقة السابعة','134','0','0');
+INSERT INTO [t5823] VALUES (8,'الطبقة الثامنة','171','0','0');
+INSERT INTO [t5823] VALUES (9,'الطبقة التاسعة','227','0','0');
+INSERT INTO [t5823] VALUES (10,'الطبقة العاشرة','271','0','0');
+INSERT INTO [t5823] VALUES (11,'الطبقة الحادية عشرة','319','0','0');
+INSERT INTO [t5823] VALUES (12,'الطبقة الثانية عشرة','351','0','0');
+INSERT INTO [t5823] VALUES (13,'الطبقة الثالثة عشرة','387','0','0');
+INSERT INTO [t5823] VALUES (14,'الطبقة الرابعة عشرة','417','0','0');
+INSERT INTO [t5823] VALUES (15,'الطبقة الخامسة عشرة','433','0','0');
+INSERT INTO [t5823] VALUES (16,'الطبقة السادسة عشرة','457','0','0');
+INSERT INTO [t5823] VALUES (17,'الطبقة السابعة عشرة','469','0','0');
+INSERT INTO [t5823] VALUES (18,'الطبقة الثامنة عشرة','484','0','0');
+INSERT INTO [t5823] VALUES (19,'الطبقة التاسعة عشرة','497','0','0');
+INSERT INTO [t5823] VALUES (20,'الطبقة العشرون','502','0','0');
+INSERT INTO [t5823] VALUES (21,'الطبقة الحادية والعشرين','508','0','0');
+INSERT INTO [t5823] VALUES (22,'الطبقة الثانية والعشرون','522','0','0');
+INSERT INTO [t5823] VALUES (23,'الطبقة الثالثة والعشرون','530','0','0');
+INSERT INTO [t5823] VALUES (24,'الطبقة الرابعة والعشرون','537','0','0');
