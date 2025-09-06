24f368802775678b07558724f0c318e4248ffe81
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:01 +0700

    Upload SQL chunk: t8637_chunk_0000.sql

---
+++ sql_chunks/t8637_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t8637
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t8637" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8637
+INSERT INTO [t8637] VALUES (1,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t8637] VALUES (2,'الحديث الأول','2','0','0');
+INSERT INTO [t8637] VALUES (3,'الحديث الثالث','6','0','0');
+INSERT INTO [t8637] VALUES (4,'الحديث الرابع','7','0','0');
+INSERT INTO [t8637] VALUES (5,'الحديث الخامس','10','0','0');
+INSERT INTO [t8637] VALUES (6,'الحديث السادس','13','0','0');
+INSERT INTO [t8637] VALUES (7,'الحديث السابع','14','0','0');
+INSERT INTO [t8637] VALUES (8,'الحديث الثامن','15','0','0');
+INSERT INTO [t8637] VALUES (9,'الحديث التاسع','16','0','0');
+INSERT INTO [t8637] VALUES (10,'الحديث العاشر','17','0','0');
+INSERT INTO [t8637] VALUES (11,'الحديث الحادي عشر','19','0','0');
+INSERT INTO [t8637] VALUES (12,'الحديث الثاني عشر','20','0','0');
+INSERT INTO [t8637] VALUES (13,'الحديث الثالث عشر','23','0','0');
+INSERT INTO [t8637] VALUES (14,'الحديث الرابع عشر','25','0','0');
+INSERT INTO [t8637] VALUES (15,'الحديث الخامس عشر','26','0','0');
+INSERT INTO [t8637] VALUES (16,'الحديث السادس عشر','27','0','0');
+INSERT INTO [t8637] VALUES (17,'الحديث السابع عشر','28','0','0');
+INSERT INTO [t8637] VALUES (18,'الحديث الثامن عشر','33','0','0');
+INSERT INTO [t8637] VALUES (19,'الحديث التاسع عشر','36','0','0');
+INSERT INTO [t8637] VALUES (20,'الحديث العشرون','39','0','0');
+INSERT INTO [t8637] VALUES (21,'الحديث الحادي والعشرين','41','0','0');
+INSERT INTO [t8637] VALUES (22,'الحديث الثاني والعشرون','43','0','0');
