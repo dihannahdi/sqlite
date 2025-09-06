841bf8f8655858bdc40693f00f5e56531cab4900
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:32 +0700

    Upload SQL chunk: t5802_chunk_0000.sql

---
+++ sql_chunks/t5802_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t5802
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t5802" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5802
+INSERT INTO [t5802] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t5802] VALUES (2,'حسان بن ثابت','2','0','0');
+INSERT INTO [t5802] VALUES (3,'حكيم بن حزام','7','0','0');
+INSERT INTO [t5802] VALUES (4,'حمنن بن عوف','8','0','0');
+INSERT INTO [t5802] VALUES (5,'حويطب بن عبد العزى','9','0','0');
+INSERT INTO [t5802] VALUES (6,'سعد بن اياس الشيباني','13','0','0');
+INSERT INTO [t5802] VALUES (7,'سعد بن جنادة','15','0','0');
+INSERT INTO [t5802] VALUES (8,'سعيد بن يربوع','16','0','0');
+INSERT INTO [t5802] VALUES (9,'ابو شداد الذماري العماني','19','0','0');
+INSERT INTO [t5802] VALUES (10,'عاصم بن عدي','21','0','0');
+INSERT INTO [t5802] VALUES (11,'عبد خير بن يزيد','23','0','0');
+INSERT INTO [t5802] VALUES (12,'اللجلاج الغطفاني','25','0','0');
+INSERT INTO [t5802] VALUES (13,'مخرمة بن نوفل','27','0','0');
+INSERT INTO [t5802] VALUES (14,'نافع بن سليمان العبدي','29','0','0');
+INSERT INTO [t5802] VALUES (15,'النابغة الجعدي','31','0','0');
+INSERT INTO [t5802] VALUES (16,'لبيد بن ربيعة','32','0','0');
+INSERT INTO [t5802] VALUES (17,'أوس بن مغراء','33','0','0');
+INSERT INTO [t5802] VALUES (18,'عدي بن حاتم','34','0','0');
+INSERT INTO [t5802] VALUES (19,'نوفل بن معاوية','35','0','0');
