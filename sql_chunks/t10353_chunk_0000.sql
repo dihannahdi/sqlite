f849f5fb3160e41ee704518425431f3081c62c57
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:20 +0700

    Upload SQL chunk: t10353_chunk_0000.sql

---
+++ sql_chunks/t10353_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t10353
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t10353" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10353
+INSERT INTO [t10353] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10353] VALUES (2,'تمهيد','3','0','0');
+INSERT INTO [t10353] VALUES (3,'الفصل الأول','12','0','0');
+INSERT INTO [t10353] VALUES (4,'الفصل الثاني','13','0','0');
+INSERT INTO [t10353] VALUES (5,'الفصل الثالث','18','0','0');
+INSERT INTO [t10353] VALUES (6,'الفصل الرابع','19','0','0');
+INSERT INTO [t10353] VALUES (7,'الفصل الخامس','23','0','0');
+INSERT INTO [t10353] VALUES (8,'الفصل السادس','28','0','0');
+INSERT INTO [t10353] VALUES (9,'الفصل السابع','57','0','0');
