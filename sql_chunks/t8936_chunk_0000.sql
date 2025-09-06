96c642dc5e01e95d97ee2566c24ad0c6028b8e69
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:45 +0700

    Upload SQL chunk: t8936_chunk_0000.sql

---
+++ sql_chunks/t8936_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t8936
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t8936" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8936
+INSERT INTO [t8936] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8936] VALUES (2,'حفظ العقل','2','0','0');
+INSERT INTO [t8936] VALUES (3,'تحريم الخمر','4','0','0');
+INSERT INTO [t8936] VALUES (4,'التدرج التشريعي في تحريم الخمر','5','0','0');
+INSERT INTO [t8936] VALUES (5,'السنة النبوية والخمر','7','0','0');
+INSERT INTO [t8936] VALUES (6,'حد الشرب','8','0','0');
+INSERT INTO [t8936] VALUES (7,'الخمر داء','9','0','0');
+INSERT INTO [t8936] VALUES (8,'التداوي بالنجس','11','0','0');
+INSERT INTO [t8936] VALUES (9,'المنافع التي في الخمر','13','0','0');
+INSERT INTO [t8936] VALUES (10,'المخدرات','15','0','0');
+INSERT INTO [t8936] VALUES (11,'ويقول الدكتور نجيب الكلاني عن المخدرات','19','0','0');
+INSERT INTO [t8936] VALUES (12,'مصادر ومراجع','21','0','0');
