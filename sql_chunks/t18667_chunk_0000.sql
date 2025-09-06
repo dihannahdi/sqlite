d5247dcf2c7a7faef43152c103ecb44595b14eb3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:35 +0700

    Upload SQL chunk: t18667_chunk_0000.sql

---
+++ sql_chunks/t18667_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t18667
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t18667" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t18667
+INSERT INTO [t18667] VALUES (1,'تعريف العنوان','2','0','0');
+INSERT INTO [t18667] VALUES (2,'أما الأول منها: وهو التشريع','5','0','0');
+INSERT INTO [t18667] VALUES (3,'المثل العليا في أخلاق العاملين','13','0','0');
+INSERT INTO [t18667] VALUES (4,'النوع الثالث في جزاء العاملين','23','0','0');
