7daaff59e82a02320184fc81346227c23d732eee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:42 +0700

    Upload SQL chunk: t12165_chunk_0000.sql

---
+++ sql_chunks/t12165_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t12165
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t12165" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12165
+INSERT INTO [t12165] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t12165] VALUES (2,'القسم الأول: واقع الأمة الإسلامية','3','0','0');
+INSERT INTO [t12165] VALUES (3,'أولا: في العقيدة','3','2','0');
+INSERT INTO [t12165] VALUES (4,'ثانيا: في العبادة','7','2','0');
+INSERT INTO [t12165] VALUES (5,'ثالثا: في الشريعة','8','2','0');
+INSERT INTO [t12165] VALUES (6,'القسم الثاني: أسباب هذا الواقع','10','0','0');
+INSERT INTO [t12165] VALUES (7,'جهل الأمة بدينها','10','6','0');
+INSERT INTO [t12165] VALUES (8,'ثانيا: تمزيق الاستعمار لبلدان المسلمين','12','6','0');
+INSERT INTO [t12165] VALUES (9,'ثالثا: الغزو الفكري المنظم','13','6','0');
+INSERT INTO [t12165] VALUES (10,'القسم الثالث: أسس الوحدة الإسلامية','15','0','0');
+INSERT INTO [t12165] VALUES (11,'أولا: وحدة الغاية','15','10','0');
+INSERT INTO [t12165] VALUES (12,'ثانيا: وحدة العقيدة','17','10','0');
+INSERT INTO [t12165] VALUES (13,'ثالثا: وحدة القيادة','19','10','0');
+INSERT INTO [t12165] VALUES (14,'رابعا: وحدة المنهج','20','10','0');
+INSERT INTO [t12165] VALUES (15,'القسم الرابع: وسائل تحقيق الوحدة','21','0','0');
+INSERT INTO [t12165] VALUES (16,'أولا: التعليم الموجه','21','15','0');
+INSERT INTO [t12165] VALUES (17,'ثانيا: الإعلام الملتزم','23','15','0');
+INSERT INTO [t12165] VALUES (18,'ثالثا: الاقتصاد المستقل','25','15','0');
+INSERT INTO [t12165] VALUES (19,'رابعا: إيجاد مراكز علمية','26','15','0');
+INSERT INTO [t12165] VALUES (20,'الخاتمة','27','0','0');
