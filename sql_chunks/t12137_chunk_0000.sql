1b081be691171cff7903aafbd995cd9d122bd001
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:21 +0700

    Upload SQL chunk: t12137_chunk_0000.sql

---
+++ sql_chunks/t12137_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t12137
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t12137" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12137
+INSERT INTO [t12137] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12137] VALUES (2,'المبحث الأول: الأحاديث الواردة في القراءة في صلاة الفجر','4','0','0');
+INSERT INTO [t12137] VALUES (3,'المبحث الثاني: الأحاديث الواردة في القراءة في صلاة الفجر يوم الجمعة','40','0','0');
+INSERT INTO [t12137] VALUES (4,'المبحث الثالث: الأحاديث الواردة في القراءة في الفجر وغيره','49','0','0');
+INSERT INTO [t12137] VALUES (5,'مسألة في دلالة الأحاديث الواردة في هذا البحث','69','0','0');
+INSERT INTO [t12137] VALUES (6,'الخاتمة','76','0','0');
+INSERT INTO [t12137] VALUES (7,'مصادر ومراجع','77','0','0');
