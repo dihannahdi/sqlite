73c9ee0f5f4c13e0939402a62a38dbdb5eb1e45c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:24 +0700

    Upload SQL chunk: t393_chunk_0000.sql

---
+++ sql_chunks/t393_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t393
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t393" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t393
+INSERT INTO [t393] VALUES (1,'توثيق الأئمة لمن لم يدركوا من الرواة','2','0','0');
+INSERT INTO [t393] VALUES (2,'الإشكال الأول','2','1','0');
+INSERT INTO [t393] VALUES (3,'الإشكال الثاني','3','1','0');
+INSERT INTO [t393] VALUES (4,'الإشكال الثالث','3','1','0');
+INSERT INTO [t393] VALUES (5,'الإشكال الرابع','3','1','0');
