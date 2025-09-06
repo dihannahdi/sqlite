95dcd6318ca7a23532ad8926c40105054eb60ad6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:17 +0700

    Upload SQL chunk: t7300_chunk_0000.sql

---
+++ sql_chunks/t7300_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t7300
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t7300" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7300
+INSERT INTO [t7300] VALUES (1,'مقدمات','1','0','0');
+INSERT INTO [t7300] VALUES (2,'إهداء:','1','1','0');
+INSERT INTO [t7300] VALUES (3,'مقدمة الطبعة الأولى:','2','1','0');
+INSERT INTO [t7300] VALUES (4,'مقدمة الطبعة الثانية:','5','1','0');
+INSERT INTO [t7300] VALUES (5,'المتن:','6','0','0');
+INSERT INTO [t7300] VALUES (6,'كيف وصلت إلينا الثقافة العربية؟','6','5','0');
+INSERT INTO [t7300] VALUES (7,'أول نص مكتوب:','7','5','0');
+INSERT INTO [t7300] VALUES (8,'أوائل التصنيف:','10','5','0');
+INSERT INTO [t7300] VALUES (9,'الورق والوراقون:','13','5','0');
+INSERT INTO [t7300] VALUES (10,'الخطوط:','24','5','0');
+INSERT INTO [t7300] VALUES (11,'أصول النصوص:','26','5','0');
+INSERT INTO [t7300] VALUES (12,'منازل النسخ:','34','5','0');
+INSERT INTO [t7300] VALUES (13,'كيف تجمع الأصول؟','36','5','0');
+INSERT INTO [t7300] VALUES (14,'فحص النسخ:','38','5','0');
+INSERT INTO [t7300] VALUES (15,'التحقيق:','40','5','0');
+INSERT INTO [t7300] VALUES (16,'تحقيق العنوان:','42','5','0');
+INSERT INTO [t7300] VALUES (17,'تحقيق اسم المؤلف:','44','5','0');
+INSERT INTO [t7300] VALUES (18,'تحقيق نسبة الكتاب إلى مؤلفه:','46','5','0');
+INSERT INTO [t7300] VALUES (19,'تحقيق متن الكتاب:','49','5','0');
+INSERT INTO [t7300] VALUES (20,'خطر تحقيق المتن:','54','5','0');
+INSERT INTO [t7300] VALUES (21,'مقدمات تحقيق المتن:','54','5','0');
