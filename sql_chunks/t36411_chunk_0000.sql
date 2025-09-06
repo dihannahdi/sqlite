0d54cc8384f25b27c6ca09c1848d45005b04594e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:06 +0700

    Upload SQL chunk: t36411_chunk_0000.sql

---
+++ sql_chunks/t36411_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t36411
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t36411" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36411
+INSERT INTO [t36411] VALUES (1,'يا فرحتنا ... اقترب رمضان','3','0','0');
+INSERT INTO [t36411] VALUES (2,'حدد هدفك','3','0','0');
+INSERT INTO [t36411] VALUES (3,'قبل أن نبدأ','3','0','0');
+INSERT INTO [t36411] VALUES (4,'لعلكم تتقون','3','0','0');
+INSERT INTO [t36411] VALUES (5,'مظاهر النجاح','4','0','0');
+INSERT INTO [t36411] VALUES (6,'العمل الصالح','4','5','0');
+INSERT INTO [t36411] VALUES (7,'الوسائل','4','0','0');
+INSERT INTO [t36411] VALUES (8,'أعظم وسيلة','4','7','0');
+INSERT INTO [t36411] VALUES (9,'السؤال اليومي','5','7','0');
