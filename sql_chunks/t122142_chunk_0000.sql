640606d192cd63bfc309f3d7e35698113049062c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:03:01 +0700

    Upload SQL chunk: t122142_chunk_0000.sql

---
+++ sql_chunks/t122142_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t122142
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t122142" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t122142
+INSERT INTO [t122142] VALUES (1,'مقدمة الشارح','2','0','0');
+INSERT INTO [t122142] VALUES (2,'الكلام في المقدمات','23','0','0');
+INSERT INTO [t122142] VALUES (3,'الكتاب الأول: في الكتاب','210','0','0');
+INSERT INTO [t122142] VALUES (4,'باب المنطوق والمفهوم','233','3','0');
+INSERT INTO [t122142] VALUES (5,'باب الأوامر والنواهي','477','3','0');
+INSERT INTO [t122142] VALUES (6,'باب العام والخاص','546','3','0');
+INSERT INTO [t122142] VALUES (7,'باب التخصيص','620','3','0');
+INSERT INTO [t122142] VALUES (8,'البيان والمبين','751','3','0');
+INSERT INTO [t122142] VALUES (9,'الكتاب الثاني في السنة','804','0','0');
+INSERT INTO [t122142] VALUES (10,'الكلام في الأخبار','820','9','0');
+INSERT INTO [t122142] VALUES (11,'الكتاب الثالث في الإجماع','1033','0','0');
+INSERT INTO [t122142] VALUES (12,'الكتاب الرابع: في القياس','1107','0','0');
+INSERT INTO [t122142] VALUES (13,'الكتاب الخامس: في الاستدلال','1365','0','0');
+INSERT INTO [t122142] VALUES (14,'الكتاب السادس: في التعادل والتراجيح','1431','0','0');
+INSERT INTO [t122142] VALUES (15,'الكتاب السابع: في الاجتهاد','1511','0','0');
