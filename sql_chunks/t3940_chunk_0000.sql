ec91c64b42283d179bf4a1c97c855207cf1fc58f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:25 +0700

    Upload SQL chunk: t3940_chunk_0000.sql

---
+++ sql_chunks/t3940_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t3940
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t3940" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3940
+INSERT INTO [t3940] VALUES (1,'ترجمة الناظم ـ رحمه الله ـ','1','0','0');
+INSERT INTO [t3940] VALUES (2,'[كتاب الطهارة]','2','0','0');
+INSERT INTO [t3940] VALUES (3,'أحكام المياه','2','2','0');
+INSERT INTO [t3940] VALUES (4,'آداب قضاء الحاجة','3','2','0');
+INSERT INTO [t3940] VALUES (5,'نواقض الوضوء','3','2','0');
+INSERT INTO [t3940] VALUES (6,'التيمم','3','2','0');
+INSERT INTO [t3940] VALUES (7,'إزالة النجاسة','4','2','0');
+INSERT INTO [t3940] VALUES (8,'[كتاب الصلاة]','4','0','0');
+INSERT INTO [t3940] VALUES (9,'صفة الصلاة','4','8','0');
+INSERT INTO [t3940] VALUES (10,'مبطلات الصلاة','4','8','0');
+INSERT INTO [t3940] VALUES (11,'القنوت في الوتر','5','8','0');
+INSERT INTO [t3940] VALUES (12,'سجود التلاوة','5','8','0');
+INSERT INTO [t3940] VALUES (13,'أوقات النهي','5','8','0');
+INSERT INTO [t3940] VALUES (14,'صلاة الجماعة','5','8','0');
+INSERT INTO [t3940] VALUES (15,'صلاة أهل الأعذار','6','8','0');
+INSERT INTO [t3940] VALUES (16,'بدع المساجد','6','8','0');
+INSERT INTO [t3940] VALUES (17,'[كتاب الصيام]','7','0','0');
+INSERT INTO [t3940] VALUES (18,'[كتاب المواريث]','7','0','0');
+INSERT INTO [t3940] VALUES (19,'[التقليد]','8','0','0');
