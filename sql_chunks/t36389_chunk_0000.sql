1d413ef9670c8c8d48d7888ebbb4ef872895d389
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:44 +0700

    Upload SQL chunk: t36389_chunk_0000.sql

---
+++ sql_chunks/t36389_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t36389
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t36389" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36389
+INSERT INTO [t36389] VALUES (1,'الاستخارة بابك إلى السفر','2','0','0');
+INSERT INTO [t36389] VALUES (2,'السفر يوم الخميس','3','0','0');
+INSERT INTO [t36389] VALUES (3,'التزود بوصايا الصالحين','4','0','0');
+INSERT INTO [t36389] VALUES (4,'إذن الأبوين','6','0','0');
+INSERT INTO [t36389] VALUES (5,'وداع المسافر للمقيم','6','0','0');
+INSERT INTO [t36389] VALUES (6,'دعاء السفر','8','0','0');
+INSERT INTO [t36389] VALUES (7,'التأمير في السفر','9','0','0');
+INSERT INTO [t36389] VALUES (8,'سفر الوحدة','9','0','0');
+INSERT INTO [t36389] VALUES (9,'حسن الرفقة في السفر','10','0','0');
+INSERT INTO [t36389] VALUES (10,'النزول في مكان واحد','11','0','0');
+INSERT INTO [t36389] VALUES (11,'أدب النزول في السفر','12','0','0');
+INSERT INTO [t36389] VALUES (12,'تعجيل الرجوع','13','0','0');
+INSERT INTO [t36389] VALUES (13,'النهي عن طروق المسافر أهله ليلا','14','0','0');
