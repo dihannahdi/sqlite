2e1b5dc50af407e26eeab7968c3cc319f23e2eae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:57:16 +0700

    Upload SQL chunk: t963_chunk_0000.sql

---
+++ sql_chunks/t963_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t963
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t963" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t963
+INSERT INTO [t963] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t963] VALUES (2,'باب الصلاة','23','0','0');
+INSERT INTO [t963] VALUES (3,'باب الزكاة','474','0','0');
+INSERT INTO [t963] VALUES (4,'باب الصوم','548','0','0');
+INSERT INTO [t963] VALUES (5,'باب الحج','616','0','0');
+INSERT INTO [t963] VALUES (6,'باب البيع','732','0','0');
+INSERT INTO [t963] VALUES (7,'باب في الوكالة والقراض','827','0','0');
+INSERT INTO [t963] VALUES (8,'باب في الإجارة','856','0','0');
+INSERT INTO [t963] VALUES (9,'باب في العارية','878','0','0');
+INSERT INTO [t963] VALUES (10,'باب في الهبة','895','0','0');
+INSERT INTO [t963] VALUES (11,'باب في الوقف','913','0','0');
+INSERT INTO [t963] VALUES (12,'باب في الإقرار','948','0','0');
+INSERT INTO [t963] VALUES (13,'باب في الوصية','961','0','0');
+INSERT INTO [t963] VALUES (14,'باب الفرائض','988','0','0');
+INSERT INTO [t963] VALUES (15,'باب النكاح','1022','0','0');
+INSERT INTO [t963] VALUES (16,'باب الجناية','1293','0','0');
+INSERT INTO [t963] VALUES (17,'باب في الردة','1318','0','0');
+INSERT INTO [t963] VALUES (18,'باب الحدود','1330','0','0');
+INSERT INTO [t963] VALUES (19,'باب الجهاد','1374','0','0');
+INSERT INTO [t963] VALUES (20,'باب القضاء','1408','0','0');
+INSERT INTO [t963] VALUES (21,'باب الدعوى والبينات','1452','0','0');
+INSERT INTO [t963] VALUES (22,'باب في الإعتاق','1537','0','0');
