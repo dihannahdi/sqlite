16297e3ab8ba4ed9ad0c2fa5922dbd557207651d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:18 +0700

    Upload SQL chunk: t7061_chunk_0000.sql

---
+++ sql_chunks/t7061_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t7061
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t7061" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7061
+INSERT INTO [t7061] VALUES (1,'فصل في صفات البطولة','4','0','0');
+INSERT INTO [t7061] VALUES (2,'ومن صفات الرجال المذمومة','6','0','0');
+INSERT INTO [t7061] VALUES (3,'ومن مذموم صفاتهن','14','0','0');
+INSERT INTO [t7061] VALUES (4,'فصل في أسماء الزوج','16','0','0');
+INSERT INTO [t7061] VALUES (5,'معرفة حلي النساء','19','0','0');
+INSERT INTO [t7061] VALUES (6,'فصل في أطوار عمر المرأة','36','0','0');
+INSERT INTO [t7061] VALUES (7,'ومن نعوت خلق الإنسان','45','0','0');
+INSERT INTO [t7061] VALUES (8,'فصل في أطوار الإبل','49','0','0');
+INSERT INTO [t7061] VALUES (9,'ومن صفات الإبل','50','0','0');
+INSERT INTO [t7061] VALUES (10,'فصل في جماعات الإبل','56','0','0');
+INSERT INTO [t7061] VALUES (11,'ومن ضروب السير','62','0','0');
+INSERT INTO [t7061] VALUES (12,'ومن عدو الخيل','66','0','0');
+INSERT INTO [t7061] VALUES (13,'فصل في الخيل المشهورة','69','0','0');
+INSERT INTO [t7061] VALUES (14,'فصل في ألوان الخيل','70','0','0');
+INSERT INTO [t7061] VALUES (15,'أسماء الخيل في السباق','75','0','0');
+INSERT INTO [t7061] VALUES (16,'ومن صفاته المذمومة','81','0','0');
+INSERT INTO [t7061] VALUES (17,'فصل في أجزاء السيف','82','0','0');
+INSERT INTO [t7061] VALUES (18,'صفات الرماح','83','0','0');
+INSERT INTO [t7061] VALUES (19,'مسكن الأسد','90','0','0');
+INSERT INTO [t7061] VALUES (20,'أنواع من السباع','91','0','0');
+INSERT INTO [t7061] VALUES (21,'ومن أسماء الضبع','92','0','0');
+INSERT INTO [t7061] VALUES (22,'حيوانات أخر','92','0','0');
+INSERT INTO [t7061] VALUES (23,'ومن نعوت القفار','116','0','0');
+INSERT INTO [t7061] VALUES (24,'نعوت الرمال','123','0','0');
+INSERT INTO [t7061] VALUES (25,'فصل في الأبنية المجتمعة','135','0','0');
+INSERT INTO [t7061] VALUES (26,'ومن أنواع النبات','163','0','0');
+INSERT INTO [t7061] VALUES (27,'فصل في الزهر','166','0','0');
+INSERT INTO [t7061] VALUES (28,'فصل في أطوار تمر النخيل','171','0','0');
+INSERT INTO [t7061] VALUES (29,'فصل في الأكل','176','0','0');
+INSERT INTO [t7061] VALUES (30,'فصل في اللبن','180','0','0');
+INSERT INTO [t7061] VALUES (31,'فصل في العسل','183','0','0');
+INSERT INTO [t7061] VALUES (32,'فصل في أجزاء من اللباس','194','0','0');
