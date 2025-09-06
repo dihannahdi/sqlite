a2185aa591704b9ec67f0c0156ae7ec906ba26db
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:47 +0700

    Upload SQL chunk: t12092_chunk_0000.sql

---
+++ sql_chunks/t12092_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t12092
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t12092" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12092
+INSERT INTO [t12092] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12092] VALUES (2,'تعريف الحج والعمرة','11','0','0');
+INSERT INTO [t12092] VALUES (3,'تأكد فسخ الحج إلى العمرة لمن لم يسق الهدي','13','0','0');
+INSERT INTO [t12092] VALUES (4,'الإحرام','15','0','0');
+INSERT INTO [t12092] VALUES (5,'محظورات الإحرام','19','0','0');
+INSERT INTO [t12092] VALUES (6,'الفدية','21','0','0');
+INSERT INTO [t12092] VALUES (7,'دخول مكه','23','0','0');
+INSERT INTO [t12092] VALUES (8,'الطواف بالبيت','27','0','0');
+INSERT INTO [t12092] VALUES (9,'السعي بين الصفا والمروة','33','0','0');
+INSERT INTO [t12092] VALUES (10,'الإحرام بالحج يوم التروية','37','0','0');
+INSERT INTO [t12092] VALUES (11,'الوقوف بعرفة','38','0','0');
+INSERT INTO [t12092] VALUES (12,'الدفع إلى مزدلفة','42','0','0');
+INSERT INTO [t12092] VALUES (13,'الدفاع إلى منى ورمي جمرة العقبة','43','0','0');
+INSERT INTO [t12092] VALUES (14,'الإفاضة إلى البيت والتحلل','46','0','0');
+INSERT INTO [t12092] VALUES (15,'المبيت بمنى','48','0','0');
+INSERT INTO [t12092] VALUES (16,'رمي الجمرات أيام التشريق','49','0','0');
+INSERT INTO [t12092] VALUES (17,'طواف الوداع','52','0','0');
+INSERT INTO [t12092] VALUES (18,'الوقوف بالمتلزم','53','0','0');
