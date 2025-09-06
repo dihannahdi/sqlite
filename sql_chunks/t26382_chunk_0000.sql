8e9a7c214c90985bc05cc937a1ac656510595d13
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:08 +0700

    Upload SQL chunk: t26382_chunk_0000.sql

---
+++ sql_chunks/t26382_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t26382
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t26382" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26382
+INSERT INTO [t26382] VALUES (1,'اليتيم','3','0','0');
+INSERT INTO [t26382] VALUES (2,'الشهداء','17','0','0');
+INSERT INTO [t26382] VALUES (3,'الحجاب','35','0','0');
+INSERT INTO [t26382] VALUES (4,'الذكرى','51','0','0');
+INSERT INTO [t26382] VALUES (5,'الهاوية','67','0','0');
+INSERT INTO [t26382] VALUES (6,'الجزاء','80','0','0');
+INSERT INTO [t26382] VALUES (7,'العقاب','95','0','0');
+INSERT INTO [t26382] VALUES (8,'الضحية','113','0','0');
+INSERT INTO [t26382] VALUES (9,'مذكرات مرغريت','146','0','0');
