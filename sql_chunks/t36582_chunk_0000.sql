0b854c9c00bbaffe40429bb706d9ed8b011faedf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:30 +0700

    Upload SQL chunk: t36582_chunk_0000.sql

---
+++ sql_chunks/t36582_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t36582
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t36582" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36582
+INSERT INTO [t36582] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36582] VALUES (2,'الباب الأول','6','0','0');
+INSERT INTO [t36582] VALUES (3,'المبحث الأول','6','2','0');
+INSERT INTO [t36582] VALUES (4,'المبحث الثانى','14','2','0');
+INSERT INTO [t36582] VALUES (5,'المبحث الثالث','24','2','0');
+INSERT INTO [t36582] VALUES (6,'المبحث الرابع','34','2','0');
+INSERT INTO [t36582] VALUES (7,'المبحث الخامس','42','2','0');
+INSERT INTO [t36582] VALUES (8,'المبحث السادس','43','2','0');
+INSERT INTO [t36582] VALUES (9,'الباب الثانى','46','0','0');
+INSERT INTO [t36582] VALUES (10,'تمهيد','46','9','0');
+INSERT INTO [t36582] VALUES (11,'مسند {سلمة بن الأكوع}','47','9','0');
+INSERT INTO [t36582] VALUES (12,'الحديث الأول','51','11','0');
+INSERT INTO [t36582] VALUES (13,'الحديث الثانى','61','11','0');
+INSERT INTO [t36582] VALUES (14,'الحديث الثالث','66','11','0');
+INSERT INTO [t36582] VALUES (15,'الحديث الرابع','70','11','0');
+INSERT INTO [t36582] VALUES (16,'الحديث الخامس والسادس','72','11','0');
+INSERT INTO [t36582] VALUES (17,'الحديث السابع والثامن','81','11','0');
+INSERT INTO [t36582] VALUES (18,'الحديث التاسع والعاشر','87','11','0');
+INSERT INTO [t36582] VALUES (19,'الحديث الحادى عشر','92','11','0');
+INSERT INTO [t36582] VALUES (20,'الحديث الثانى عشر','100','11','0');
+INSERT INTO [t36582] VALUES (21,'الحديث الثالث عشر','104','11','0');
+INSERT INTO [t36582] VALUES (22,'الحديث الرابع والخامس عشر','109','11','0');
+INSERT INTO [t36582] VALUES (23,'الحديث السادس عشر','114','11','0');
+INSERT INTO [t36582] VALUES (24,'الحديث السابع عشر','116','11','0');
+INSERT INTO [t36582] VALUES (25,'مسند {أنس بن مالك}','120','9','0');
+INSERT INTO [t36582] VALUES (26,'الأحاديث الثلاث (الثامن عشر والتاسع عشر والعشرون)','124','25','0');
+INSERT INTO [t36582] VALUES (27,'الحديث الواحد والعشرون','130','25','0');
+INSERT INTO [t36582] VALUES (28,'مسند {عبد الله بن بسر}','139','9','0');
+INSERT INTO [t36582] VALUES (29,'الحديث الثانى والعشرون','141','28','0');
+INSERT INTO [t36582] VALUES (30,'الباب الثالث','143','0','0');
+INSERT INTO [t36582] VALUES (31,'المبحث الأول','143','30','0');
+INSERT INTO [t36582] VALUES (32,'المبحث الثانى','145','30','0');
+INSERT INTO [t36582] VALUES (33,'المبحث الثالث','152','30','0');
+INSERT INTO [t36582] VALUES (34,'خاتمة','153','0','0');
