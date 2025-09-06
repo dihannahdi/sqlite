2fb0cf49f7c44eeddeccb2e2634cac26ad0a783d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:06 +0700

    Upload SQL chunk: t21518_chunk_0000.sql

---
+++ sql_chunks/t21518_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t21518
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t21518" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21518
+INSERT INTO [t21518] VALUES (1,'الرقية','1','0','0');
+INSERT INTO [t21518] VALUES (2,'تقديم','1','0','0');
+INSERT INTO [t21518] VALUES (3,'المقدمة','3','0','0');
+INSERT INTO [t21518] VALUES (4,'إرشادات عامة تراعى عند الرقية:','6','0','0');
+INSERT INTO [t21518] VALUES (5,'القسم الأول الرقية الموجزة','8','0','0');
+INSERT INTO [t21518] VALUES (6,'القسم الثاني الرقية المتوسطة','15','0','0');
+INSERT INTO [t21518] VALUES (7,'القسم الثالث الرقية المطولة','29','0','0');
