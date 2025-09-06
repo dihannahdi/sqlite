91724c2098e16a0ef689a8b3ee575894d08bb07e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:54 +0700

    Upload SQL chunk: t115119_chunk_0000.sql

---
+++ sql_chunks/t115119_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t115119
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t115119" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t115119
+INSERT INTO [t115119] VALUES (1,'ذكر البياضات وسدها','8','0','0');
+INSERT INTO [t115119] VALUES (2,'بياضات " هدي الساري مقدمة فتح الباري "','8','1','0');
+INSERT INTO [t115119] VALUES (3,'بياضات الجزء الثالث','14','1','0');
+INSERT INTO [t115119] VALUES (4,'بياضات الجزء الرابع','16','1','0');
+INSERT INTO [t115119] VALUES (5,'بياضات الجزء الخامس','18','1','0');
+INSERT INTO [t115119] VALUES (6,'بياضات الجزء السادس','19','1','0');
+INSERT INTO [t115119] VALUES (7,'بياضات الجزء السابع','21','1','0');
+INSERT INTO [t115119] VALUES (8,'بياضات الجزء الثامن','22','1','0');
+INSERT INTO [t115119] VALUES (9,'بياضات الجزء التاسع','31','1','0');
+INSERT INTO [t115119] VALUES (10,'بياضات الجزء العاشر','34','1','0');
+INSERT INTO [t115119] VALUES (11,'بياضات الجزء الحادي عشر','41','1','0');
+INSERT INTO [t115119] VALUES (12,'الجزء الثاني عشر','42','1','0');
+INSERT INTO [t115119] VALUES (13,'بياضات الجزء الثالث عشر','43','1','0');
+INSERT INTO [t115119] VALUES (14,'المراجع','45','0','0');
