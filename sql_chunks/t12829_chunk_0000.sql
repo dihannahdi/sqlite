1115ebe4c2f53ded74b8cebfecff47b62baef031
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:42 +0700

    Upload SQL chunk: t12829_chunk_0000.sql

---
+++ sql_chunks/t12829_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t12829
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t12829" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12829
+INSERT INTO [t12829] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t12829] VALUES (2,'*تعريف العدل:','6','0','0');
+INSERT INTO [t12829] VALUES (3,'*سبب اشتراط العدالة:','7','0','0');
+INSERT INTO [t12829] VALUES (4,'*شروط المعدل والجارح:','20','0','0');
+INSERT INTO [t12829] VALUES (5,'الخاتمة','34','0','0');
