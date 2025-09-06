5d67c8516d3fba373629daf213dae1b17a2ec72f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:11 +0700

    Upload SQL chunk: t17736_chunk_0000.sql

---
+++ sql_chunks/t17736_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t17736
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t17736" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17736
+INSERT INTO [t17736] VALUES (1,'[مقدمة المحقق]','5','0','0');
+INSERT INTO [t17736] VALUES (2,'نص الحديث وتخريجه وشرحه','7','1','0');
+INSERT INTO [t17736] VALUES (3,'التخريج','7','2','0');
+INSERT INTO [t17736] VALUES (4,'الشرح','9','2','0');
+INSERT INTO [t17736] VALUES (5,'ترجمة المؤلف','11','1','0');
+INSERT INTO [t17736] VALUES (6,'مولده ونشأته','12','5','0');
+INSERT INTO [t17736] VALUES (7,'طلبه للعلم وشيوخه','12','5','0');
+INSERT INTO [t17736] VALUES (8,'هجرته إلى بيروت','13','5','0');
+INSERT INTO [t17736] VALUES (9,'تقلده القضاء والإفتاء في بيروت','13','5','0');
+INSERT INTO [t17736] VALUES (10,'انتقاله إلى دمشق','16','5','0');
+INSERT INTO [t17736] VALUES (11,'مقتطفات من شعره','17','5','0');
+INSERT INTO [t17736] VALUES (12,'شيوخه بدمشق','18','5','0');
+INSERT INTO [t17736] VALUES (13,'تلاميذه','18','5','0');
+INSERT INTO [t17736] VALUES (14,'مما قيل فيه','19','5','0');
+INSERT INTO [t17736] VALUES (15,'مؤلفاته','21','5','0');
+INSERT INTO [t17736] VALUES (16,'وفاته ورثاء تلميذه له','23','5','0');
+INSERT INTO [t17736] VALUES (17,'أول الرسالة','30','0','0');
+INSERT INTO [t17736] VALUES (18,'آخر الرسالة','53','0','0');
