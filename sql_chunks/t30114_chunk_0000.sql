cd094cd4f78808a8cef3e13eb1f191b2f36af520
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:46 +0700

    Upload SQL chunk: t30114_chunk_0000.sql

---
+++ sql_chunks/t30114_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t30114
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t30114" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30114
+INSERT INTO [t30114] VALUES (1,'الجزء الخامس والثلاثون من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30114] VALUES (2,'من فوائد عبد العزيز الأزجي','2','0','0');
+INSERT INTO [t30114] VALUES (3,'من فوائد هناد النسفي','23','0','0');
+INSERT INTO [t30114] VALUES (4,'ومن حديثه أيضا','34','0','0');
+INSERT INTO [t30114] VALUES (5,'من فوائد القاضي أبي القاسم التنوخي','42','0','0');
+INSERT INTO [t30114] VALUES (6,'من فوائد أبي القاسم الزنجاني','49','0','0');
+INSERT INTO [t30114] VALUES (7,'من فوائد البيضاوي','51','0','0');
+INSERT INTO [t30114] VALUES (8,'من فوائد عبد العزيز الأزجي','58','0','0');
