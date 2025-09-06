f54412f301d5aa94819eaa3b9ca524c688c3e9e0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:12 +0700

    Upload SQL chunk: t9844_chunk_0000.sql

---
+++ sql_chunks/t9844_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t9844
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t9844" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9844
+INSERT INTO [t9844] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t9844] VALUES (2,'المبحث الأول: ما كتب في هذه النازلة','5','0','0');
+INSERT INTO [t9844] VALUES (3,'المبحث الثاني: في حقيقة التمثيل وأسمائه','13','0','0');
+INSERT INTO [t9844] VALUES (4,'المبحث الثالث: تاريخه','16','0','0');
+INSERT INTO [t9844] VALUES (5,'تنبيه مهم: في حكم إهداء الزهور للمرضى','17','4','0');
+INSERT INTO [t9844] VALUES (6,'تحقيق نسبة البيتين المشهورين في خيال الظل','18','4','0');
+INSERT INTO [t9844] VALUES (7,'المبحث الرابع: أنواع التمثيل','22','0','0');
+INSERT INTO [t9844] VALUES (8,'المبحث الخامس: الغاية منه','23','0','0');
+INSERT INTO [t9844] VALUES (9,'المبحث السادس: المدرك الفقهي لبيان حكم نازلة التمثيل','24','0','0');
+INSERT INTO [t9844] VALUES (10,'شبهة المجيزين والجواب عنها','47','9','0');
+INSERT INTO [t9844] VALUES (11,'الخاتمة','54','0','0');
