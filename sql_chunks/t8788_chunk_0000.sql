3afd8532b8c5e800cab297f5c292f957deed8708
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:04 +0700

    Upload SQL chunk: t8788_chunk_0000.sql

---
+++ sql_chunks/t8788_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t8788
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t8788" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8788
+INSERT INTO [t8788] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8788] VALUES (2,'معني البدعة','2','0','0');
+INSERT INTO [t8788] VALUES (3,'كل بدعة ضلالة','5','0','0');
+INSERT INTO [t8788] VALUES (4,'من أثار البدع السيئة','11','0','0');
