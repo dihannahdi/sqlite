e6c82830fb7ec317db40078f87fad74cda33d485
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:13 +0700

    Upload SQL chunk: t10342_chunk_0000.sql

---
+++ sql_chunks/t10342_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t10342
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t10342" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10342
+INSERT INTO [t10342] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10342] VALUES (2,'مدخل','1','1','0');
+INSERT INTO [t10342] VALUES (3,'مميزات الحضارة الاسلامية','3','0','0');
+INSERT INTO [t10342] VALUES (4,'أولا','3','3','0');
+INSERT INTO [t10342] VALUES (5,'ثانيا','4','3','0');
+INSERT INTO [t10342] VALUES (6,'ثالثا','5','3','0');
+INSERT INTO [t10342] VALUES (7,'رابعا','6','3','0');
+INSERT INTO [t10342] VALUES (8,'خامسا','7','3','0');
+INSERT INTO [t10342] VALUES (9,'سادسا','9','3','0');
+INSERT INTO [t10342] VALUES (10,'سابعا','13','3','0');
+INSERT INTO [t10342] VALUES (11,'ثامنا','14','3','0');
+INSERT INTO [t10342] VALUES (12,'تاسعا','16','3','0');
+INSERT INTO [t10342] VALUES (13,'عاشرا','17','3','0');
