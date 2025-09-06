98252661aa0e42a41482a95760ba8dff8241935b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:45 +0700

    Upload SQL chunk: t5945_chunk_0000.sql

---
+++ sql_chunks/t5945_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t5945
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t5945" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5945
+INSERT INTO [t5945] VALUES (1,'باب التاريخ','11','0','0');
+INSERT INTO [t5945] VALUES (2,'باب في الأسماء','16','0','0');
+INSERT INTO [t5945] VALUES (3,'باب ذكر ثقات المدنيين وغيرهم','47','0','0');
+INSERT INTO [t5945] VALUES (4,'باب ذكر ثقات أهل مكة','73','0','0');
+INSERT INTO [t5945] VALUES (5,'باب أهل الطائف','84','0','0');
+INSERT INTO [t5945] VALUES (6,'باب أهل اليمن','85','0','0');
+INSERT INTO [t5945] VALUES (7,'باب أهل مصر','88','0','0');
+INSERT INTO [t5945] VALUES (8,'باب أهل الرملة','93','0','0');
+INSERT INTO [t5945] VALUES (9,'باب أهل عسقلان','95','0','0');
+INSERT INTO [t5945] VALUES (10,'باب أهل قيسارية','97','0','0');
+INSERT INTO [t5945] VALUES (11,'باب أهل بيت المقدس','98','0','0');
+INSERT INTO [t5945] VALUES (12,'باب أهل الأردن سألت أحمد عن عبادة بن نسي فقال شامي قديم','99','0','0');
+INSERT INTO [t5945] VALUES (13,'باب أهل دمشق','100','0','0');
+INSERT INTO [t5945] VALUES (14,'باب أهل حمص','105','0','0');
+INSERT INTO [t5945] VALUES (15,'باب أهل أيلة','113','0','0');
+INSERT INTO [t5945] VALUES (16,'باب أهل حلب','115','0','0');
+INSERT INTO [t5945] VALUES (17,'باب أهل حران','117','0','0');
+INSERT INTO [t5945] VALUES (18,'باب أهل الموصل','121','0','0');
+INSERT INTO [t5945] VALUES (19,'باب أهل الرقة','122','0','0');
+INSERT INTO [t5945] VALUES (20,'باب أهل المصيصة','127','0','0');
+INSERT INTO [t5945] VALUES (21,'باب أهل طرسوس','129','0','0');
+INSERT INTO [t5945] VALUES (22,'باب أهل أنطاكية','131','0','0');
+INSERT INTO [t5945] VALUES (23,'باب أهل الكوفة','132','0','0');
+INSERT INTO [t5945] VALUES (24,'باب أهل واسط','165','0','0');
+INSERT INTO [t5945] VALUES (25,'باب أهل البصرة','169','0','0');
+INSERT INTO [t5945] VALUES (26,'باب أهل اليمامة','200','0','0');
+INSERT INTO [t5945] VALUES (27,'باب أهل خرسان','203','0','0');
+INSERT INTO [t5945] VALUES (28,'باب ضعفاء أهل المدينة','206','0','0');
+INSERT INTO [t5945] VALUES (29,'ملحق الكتاب','210','0','0');
+INSERT INTO [t5945] VALUES (30,'ملحق','211','0','0');
+INSERT INTO [t5945] VALUES (31,'باب أهل الكوفة','211','0','0');
+INSERT INTO [t5945] VALUES (32,'ملحق','214','0','0');
+INSERT INTO [t5945] VALUES (33,'باب أهل بغداد','214','0','0');
