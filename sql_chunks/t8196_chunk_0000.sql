7c5502f1d256abfeb5f2753c0316ec04cd0a4a47
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:09 +0700

    Upload SQL chunk: t8196_chunk_0000.sql

---
+++ sql_chunks/t8196_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t8196
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t8196" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8196
+INSERT INTO [t8196] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t8196] VALUES (2,'ولا حول ولا قوة إلا بالله العظيم','1','0','0');
+INSERT INTO [t8196] VALUES (3,'تم الكتاب والحمد لله رب العالمين','154','0','0');
