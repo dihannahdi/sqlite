08ca56c3a3850f67b8b9d55f45f8a2a4be8c8ed7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:31 +0700

    Upload SQL chunk: t6989_chunk_0000.sql

---
+++ sql_chunks/t6989_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6989
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6989" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6989
+INSERT INTO [t6989] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6989] VALUES (2,'قال أبو القاسم عبد الرحمن بن إسحق الزجاجي','1','0','0');
+INSERT INTO [t6989] VALUES (3,'تم كتاب حروف المعاني والصفات','87','0','0');
