48c1d997ea0e1f0aaa01b94008533181dda365bd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:58 +0700

    Upload SQL chunk: t7263_chunk_0000.sql

---
+++ sql_chunks/t7263_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t7263
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t7263" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7263
+INSERT INTO [t7263] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7263] VALUES (2,'فصل: الولايات الإسلامية، الأمر بالمعروف والنهي عن المنكر','6','0','0');
+INSERT INTO [t7263] VALUES (3,'فصل: مسئولية المحتسب','9','0','0');
+INSERT INTO [t7263] VALUES (4,'فصل: الغش والتدليس في الديات','37','0','0');
+INSERT INTO [t7263] VALUES (5,'فصل: العقوبات الشرعية','39','0','0');
+INSERT INTO [t7263] VALUES (6,'فصل: التعزير المالي','42','0','0');
+INSERT INTO [t7263] VALUES (7,'فصل: الثواب والعقاب','50','0','0');
+INSERT INTO [t7263] VALUES (8,'فهرس الموضوعات','53','0','0');
