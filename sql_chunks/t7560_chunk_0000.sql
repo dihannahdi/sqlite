9c641e2ae9f88ea61355d8cf9a7ef10b775cd3b5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:53 +0700

    Upload SQL chunk: t7560_chunk_0000.sql

---
+++ sql_chunks/t7560_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t7560
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t7560" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7560
+INSERT INTO [t7560] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7560] VALUES (2,'مؤلفات المستشرقين الروس في السيرة النبوية','8','0','0');
+INSERT INTO [t7560] VALUES (3,'مصادر ومراجع','43','0','0');
