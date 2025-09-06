af64d34cb98972c28b838beee61a64f7c5199169
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:51 +0700

    Upload SQL chunk: t11240_chunk_0000.sql

---
+++ sql_chunks/t11240_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11240
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11240" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11240
+INSERT INTO [t11240] VALUES (1,'سنة الجمعة','1','0','0');
+INSERT INTO [t11240] VALUES (2,'وسئل شيخ الإسلام رحمه الله عن الصلاة بعد الأذان الأول يوم الجمعة','1','0','0');
