7e2a41427480e2a3d7c8d5053bc052160c3a0bcf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:11 +0700

    Upload SQL chunk: t26680_chunk_0000.sql

---
+++ sql_chunks/t26680_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26680
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26680" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26680
+INSERT INTO [t26680] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26680] VALUES (2,'فوائد الفوائد','2','0','0');
