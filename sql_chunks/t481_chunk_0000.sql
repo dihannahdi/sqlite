37ae36a408c2649e5bbadb20c5767ad80b6b0574
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:05 +0700

    Upload SQL chunk: t481_chunk_0000.sql

---
+++ sql_chunks/t481_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t481
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t481" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t481
+INSERT INTO [t481] VALUES (1,'الجواب أن لهذين الصبيين خمس حالات','2','0','0');
+INSERT INTO [t481] VALUES (2,'١) حكم حالة صغرهما قبل دخول الكنيسة','2','1','0');
+INSERT INTO [t481] VALUES (3,'٢) حكم حالة دخولهما الكنيسة والتلبس بالنصرانية','3','1','0');
+INSERT INTO [t481] VALUES (4,'٣) حكم حالة بلوغهما ودوامهما على التلبس بالنصرانية','3','1','0');
+INSERT INTO [t481] VALUES (5,'٤) حكم حالة عقد النكاح بينهما','4','1','0');
+INSERT INTO [t481] VALUES (6,'٥) حكم حالة عودهما إلى الإسلام','6','1','0');
