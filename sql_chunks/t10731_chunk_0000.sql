a019fa94cb76171f3893b956ca17bd10f74dd259
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:50:57 +0700

    Upload SQL chunk: t10731_chunk_0000.sql

---
+++ sql_chunks/t10731_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10731
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10731" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10731
+INSERT INTO [t10731] VALUES (1,'تتمة ترجمة أبي الحسن الأشعري','77','0','0');
+INSERT INTO [t10731] VALUES (2,'مصنفات أبي الحسن الأشعري','88','0','0');
+INSERT INTO [t10731] VALUES (3,'العودة إلى ترجمة الخسروشاهي','106','0','0');
