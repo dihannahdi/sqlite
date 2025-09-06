fd71a8e57d5c572bd28c4be334a049397de1780b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:00 +0700

    Upload SQL chunk: t9922_chunk_0000.sql

---
+++ sql_chunks/t9922_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t9922
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t9922" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9922
+INSERT INTO [t9922] VALUES (1,'(١)','1','0','0');
+INSERT INTO [t9922] VALUES (2,'(٢)','1','0','0');
+INSERT INTO [t9922] VALUES (3,'(٣)','1','0','0');
+INSERT INTO [t9922] VALUES (4,'(٤)','1','0','0');
+INSERT INTO [t9922] VALUES (5,'(٥)','2','0','0');
+INSERT INTO [t9922] VALUES (6,'(٦)','2','0','0');
+INSERT INTO [t9922] VALUES (7,'(٧)','2','0','0');
