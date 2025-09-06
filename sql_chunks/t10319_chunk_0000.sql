20b9bd28a8e446ac3f84f08e2f5d38aed1396d5b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:59 +0700

    Upload SQL chunk: t10319_chunk_0000.sql

---
+++ sql_chunks/t10319_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t10319
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t10319" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10319
+INSERT INTO [t10319] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10319] VALUES (2,'النمو في اللغة والاصطلاح','3','0','0');
+INSERT INTO [t10319] VALUES (3,'أهمية دراسة النمو','4','0','0');
+INSERT INTO [t10319] VALUES (4,'مراحل النمو الأنساني','7','0','0');
+INSERT INTO [t10319] VALUES (5,'العوامل المؤثرة في النمو الانساني','15','0','0');
+INSERT INTO [t10319] VALUES (6,'مظاهر النمو عند طلاب التعليم العالي','34','0','0');
+INSERT INTO [t10319] VALUES (7,'الخاتمة','54','0','0');
