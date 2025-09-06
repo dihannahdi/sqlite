23c2167b4112a09b53cce4e710026ee1402b61e5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:23 +0700

    Upload SQL chunk: t26553_chunk_0000.sql

---
+++ sql_chunks/t26553_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26553
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26553" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26553
+INSERT INTO [t26553] VALUES (1,'جزء أبي حامد ابن بلال','3','0','0');
