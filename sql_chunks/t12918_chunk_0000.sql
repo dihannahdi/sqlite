e2f402e8824c3711e899049e11b53c6ee9ce6be8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:08:28 +0700

    Upload SQL chunk: t12918_chunk_0000.sql

---
+++ sql_chunks/t12918_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t12918
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t12918" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12918
+INSERT INTO [t12918] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t12918] VALUES (2,'وسائل وأساليب التنصير','8','0','0');
+INSERT INTO [t12918] VALUES (3,'واجب المسلمين تجاه ذلك','14','0','0');
