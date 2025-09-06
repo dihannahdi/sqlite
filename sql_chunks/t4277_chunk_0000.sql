df6bc835dc9535dbf2f55a72e9ac30f86404fc77
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:36 +0700

    Upload SQL chunk: t4277_chunk_0000.sql

---
+++ sql_chunks/t4277_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t4277
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t4277" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4277
+INSERT INTO [t4277] VALUES (1,'محب الدين الخطيب','1','0','0');
+INSERT INTO [t4277] VALUES (2,'بقلم الشيخ: ممدوح فخري','1','0','0');
