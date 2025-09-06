dce0414443664878f9ec91e292335783a6fb97ea
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:45 +0700

    Upload SQL chunk: t8710_chunk_0000.sql

---
+++ sql_chunks/t8710_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t8710
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t8710" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8710
+INSERT INTO [t8710] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8710] VALUES (2,'الآثار السيئة للوضع','14','0','0');
+INSERT INTO [t8710] VALUES (3,'مدخل','14','2','0');
+INSERT INTO [t8710] VALUES (4,'الفصل الأول: الآثار الدينية','16','2','0');
