8a86da62621dc6215941867dc68a7c5e505bff53
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:32 +0700

    Upload SQL chunk: t1037_chunk_0000.sql

---
+++ sql_chunks/t1037_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t1037
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t1037" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1037
+INSERT INTO [t1037] VALUES (1,'مقدمة لكتاب فهرس الفهارس','1','0','0');
+INSERT INTO [t1037] VALUES (2,'بسم الله الرحمن الرحيم','44','0','0');
+INSERT INTO [t1037] VALUES (3,'حرف الألف','53','0','0');
+INSERT INTO [t1037] VALUES (4,'حرف الباء','54','0','0');
+INSERT INTO [t1037] VALUES (5,'حرف التاء','54','0','0');
+INSERT INTO [t1037] VALUES (6,'حرف الجيم','55','0','0');
+INSERT INTO [t1037] VALUES (7,'حرف الحاء','55','0','0');
+INSERT INTO [t1037] VALUES (8,'حرف الخاء','56','0','0');
+INSERT INTO [t1037] VALUES (9,'حرف الطاء','56','0','0');
+INSERT INTO [t1037] VALUES (10,'حرف اللام','56','0','0');
+INSERT INTO [t1037] VALUES (11,'حرف الميم','56','0','0');
+INSERT INTO [t1037] VALUES (12,'حرف النون','58','0','0');
+INSERT INTO [t1037] VALUES (13,'حرف الصاد','59','0','0');
+INSERT INTO [t1037] VALUES (14,'حرف العين','59','0','0');
+INSERT INTO [t1037] VALUES (15,'حرف الفاء','60','0','0');
+INSERT INTO [t1037] VALUES (16,'حرف السين','61','0','0');
+INSERT INTO [t1037] VALUES (17,'حرف الشين','61','0','0');
+INSERT INTO [t1037] VALUES (18,'حرف الهاء','61','0','0');
+INSERT INTO [t1037] VALUES (19,'حرف الياء','61','0','0');
+INSERT INTO [t1037] VALUES (20,'حرف الباء','205','0','0');
+INSERT INTO [t1037] VALUES (21,'حرف التاء والثاء','250','0','0');
+INSERT INTO [t1037] VALUES (22,'حرف الجيم','290','0','0');
+INSERT INTO [t1037] VALUES (23,'حرف الحاء','311','0','0');
+INSERT INTO [t1037] VALUES (24,'حرف الخاء','368','0','0');
+INSERT INTO [t1037] VALUES (25,'حرف الدال','384','0','0');
+INSERT INTO [t1037] VALUES (26,'حرف الذال','412','0','0');
+INSERT INTO [t1037] VALUES (27,'حرف الراء','418','0','0');
+INSERT INTO [t1037] VALUES (28,'حرف الزاي','448','0','0');
+INSERT INTO [t1037] VALUES (29,'حرف الطاء','460','0','0');
+INSERT INTO [t1037] VALUES (30,'حرف الظاء','474','0','0');
+INSERT INTO [t1037] VALUES (31,'حرف الكاف','474','0','0');
+INSERT INTO [t1037] VALUES (32,'حرف اللام','503','0','0');
+INSERT INTO [t1037] VALUES (33,'حرف الميم','508','0','0');
+INSERT INTO [t1037] VALUES (34,'حرف النون','656','0','0');
+INSERT INTO [t1037] VALUES (35,'حرف الهاء','1088','0','0');
+INSERT INTO [t1037] VALUES (36,'حرف الواو','1100','0','0');
+INSERT INTO [t1037] VALUES (37,'حرف الياء','1121','0','0');
+INSERT INTO [t1037] VALUES (38,'حرف الهمزة [الممدودة]','1156','0','0');
