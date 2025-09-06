209b6f47bdf8dd7365527f86abf5684b67d1bf4d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:14 +0700

    Upload SQL chunk: t21526_chunk_0000.sql

---
+++ sql_chunks/t21526_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t21526
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t21526" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21526
+INSERT INTO [t21526] VALUES (1,'أي الأديان أصلح للإنسانية عقيدة وشريعة','2','0','0');
+INSERT INTO [t21526] VALUES (2,'الشيوعية','4','0','0');
+INSERT INTO [t21526] VALUES (3,'البراهيمية','7','0','0');
+INSERT INTO [t21526] VALUES (4,'الجينية','13','0','0');
+INSERT INTO [t21526] VALUES (5,'البوذية','17','0','0');
+INSERT INTO [t21526] VALUES (6,'الهندوكية','27','0','0');
+INSERT INTO [t21526] VALUES (7,'ديانات الصين','30','0','0');
+INSERT INTO [t21526] VALUES (8,'الكنفوشية','31','0','0');
+INSERT INTO [t21526] VALUES (9,'الطاوية','35','0','0');
+INSERT INTO [t21526] VALUES (10,'الشنتو','38','0','0');
+INSERT INTO [t21526] VALUES (11,'بوذية الصين','39','0','0');
+INSERT INTO [t21526] VALUES (12,'ديانات فارس والعراق وسوريا','40','0','0');
+INSERT INTO [t21526] VALUES (13,'ديانة موسى','42','0','0');
+INSERT INTO [t21526] VALUES (14,'الديانة المسيحية','57','0','0');
+INSERT INTO [t21526] VALUES (15,'توحيد الإسلام وتوحيد اليهودية','65','0','0');
+INSERT INTO [t21526] VALUES (16,'الثالوث في المسيحية والديانات الوثنية','66','0','0');
+INSERT INTO [t21526] VALUES (17,'الإسلام','73','0','0');
