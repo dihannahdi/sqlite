3c4a24219d29f3b98a924b66bcd3dc4703ab4f7c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:26 +0700

    Upload SQL chunk: t35312_chunk_0000.sql

---
+++ sql_chunks/t35312_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t35312
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t35312" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t35312
+INSERT INTO [t35312] VALUES (1,'المقدمة','5','0','0');
+INSERT INTO [t35312] VALUES (2,'ترجمة الناظم','8','1','0');
+INSERT INTO [t35312] VALUES (3,'نسبه','8','2','0');
+INSERT INTO [t35312] VALUES (4,'نسبته','8','2','0');
+INSERT INTO [t35312] VALUES (5,'ولادته','9','2','0');
+INSERT INTO [t35312] VALUES (6,'سعيه في طلب العلم','9','2','0');
+INSERT INTO [t35312] VALUES (7,'من أشهر شيوخه','10','2','0');
+INSERT INTO [t35312] VALUES (8,'حالته الاجتماعية','10','2','0');
+INSERT INTO [t35312] VALUES (9,'من تلاميذه','11','2','0');
+INSERT INTO [t35312] VALUES (10,'مكانته العلمية','12','2','0');
+INSERT INTO [t35312] VALUES (11,'عقيدته','12','2','0');
+INSERT INTO [t35312] VALUES (12,'ذكر بعض صفاته','12','2','0');
+INSERT INTO [t35312] VALUES (13,'مناصبه','13','2','0');
+INSERT INTO [t35312] VALUES (14,'مؤلفاته','14','2','0');
+INSERT INTO [t35312] VALUES (15,'وفاته','15','2','0');
+INSERT INTO [t35312] VALUES (16,'[المنظومة]','16','0','0');
+INSERT INTO [t35312] VALUES (17,'شرح المنظومة','20','0','0');
+INSERT INTO [t35312] VALUES (18,'المبحث الأول المحافظة على النوافل','27','0','0');
+INSERT INTO [t35312] VALUES (19,'المبحث الثاني الخوف والرجاء','31','0','0');
+INSERT INTO [t35312] VALUES (20,'الخوف','32','19','0');
+INSERT INTO [t35312] VALUES (21,'١ - الخوف الطبعي، أو الجبلي','32','20','0');
+INSERT INTO [t35312] VALUES (22,'٢ - الخوف الشرعي: ينقسم إلى قسمين','34','20','0');
+INSERT INTO [t35312] VALUES (23,'أ - خوف شرعي مأمور به','34','22','0');
+INSERT INTO [t35312] VALUES (24,'ب - خوف شرعي منهي عنه','35','22','0');
+INSERT INTO [t35312] VALUES (25,'الرجاء','38','19','0');
+INSERT INTO [t35312] VALUES (26,'المبحث الثالث الترغيب في الطاعات','42','0','0');
+INSERT INTO [t35312] VALUES (27,'المبحث الرابع الحث على التمسك بالكتاب والسنة','44','0','0');
+INSERT INTO [t35312] VALUES (28,'المبحث الخامس صفة الكلام','51','0','0');
+INSERT INTO [t35312] VALUES (29,'المبحث السادس القول في الأسماء والصفات','58','0','0');
+INSERT INTO [t35312] VALUES (30,'المبحث السابع الاستواء على العرش','64','0','0');
+INSERT INTO [t35312] VALUES (31,'المبحث الثامن إثبات العلو والمعية','66','0','0');
+INSERT INTO [t35312] VALUES (32,'المبحث التاسع التذكير بيوم الحساب','77','0','0');
+INSERT INTO [t35312] VALUES (33,'المبحث العاشر الإيمان بالجنة والنار','81','0','0');
