1d0353eaaa1239e2a4ff4ed527369cfc3e6367d1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:41 +0700

    Upload SQL chunk: t6668_chunk_0000.sql

---
+++ sql_chunks/t6668_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6668
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6668" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6668
+INSERT INTO [t6668] VALUES (1,'الجزء ١','1','0','0');
+INSERT INTO [t6668] VALUES (2,'الجزء ٢','422','0','0');
