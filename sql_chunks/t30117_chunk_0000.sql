2ae948f5c32868f7c31344a4a2d57fd86a9918ac
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:47 +0700

    Upload SQL chunk: t30117_chunk_0000.sql

---
+++ sql_chunks/t30117_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t30117
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t30117" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30117
+INSERT INTO [t30117] VALUES (1,'الجزء الرابع عشر من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30117] VALUES (2,'من حديث أبي نصر بن البناء','2','0','0');
+INSERT INTO [t30117] VALUES (3,'من حديث أبي الحسن المقدسي خطيب الرملة','28','0','0');
+INSERT INTO [t30117] VALUES (4,'من فوائد وهرام بن محمد المستوفي','39','0','0');
+INSERT INTO [t30117] VALUES (5,'من فوائد الدهذاء الهمذاني','40','0','0');
+INSERT INTO [t30117] VALUES (6,'من حديث أبي المعالي البقال وفوائده','41','0','0');
+INSERT INTO [t30117] VALUES (7,'من فوائد وهرام أبي نصر','44','0','0');
