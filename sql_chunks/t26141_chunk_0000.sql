e7c9c138b4d2e3bc18cae00ba7a88fe979f36f32
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:33 +0700

    Upload SQL chunk: t26141_chunk_0000.sql

---
+++ sql_chunks/t26141_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t26141
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t26141" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26141
+INSERT INTO [t26141] VALUES (1,'[مقدمة]','1','0','0');
+INSERT INTO [t26141] VALUES (2,'فقوله صلى الله عليه وسلم: " بعثت بالسيف "','3','0','0');
+INSERT INTO [t26141] VALUES (3,'قوله صلى الله عليه وسلم: " بين يدي الساعة "','7','0','0');
+INSERT INTO [t26141] VALUES (4,'قوله صلى الله عليه وسلم: " حتى يعبد الله وحده لا شريك له "','10','0','0');
+INSERT INTO [t26141] VALUES (5,'قوله صلى الله عليه وسلم: " وجعل رزقي تحت ظل رمحي "','21','0','0');
+INSERT INTO [t26141] VALUES (6,'قوله صلى الله عليه وسلم: " وجعل الذلة والصغار على من خالف أمري "','28','0','0');
+INSERT INTO [t26141] VALUES (7,'قوله صلى الله عليه وسلم: " ومن تشبه بقوم فهو منهم "','39','0','0');
