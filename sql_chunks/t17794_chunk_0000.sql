0ff321352e5af0c43d7ffc0261e65eacdb5033a1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:29 +0700

    Upload SQL chunk: t17794_chunk_0000.sql

---
+++ sql_chunks/t17794_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t17794
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t17794" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17794
+INSERT INTO [t17794] VALUES (1,'مقدمة المحقق','4','0','0');
+INSERT INTO [t17794] VALUES (2,'موضوع الجزء','4','1','0');
+INSERT INTO [t17794] VALUES (3,'ترجمة الإمام المنذري صاحب الجزء','6','1','0');
+INSERT INTO [t17794] VALUES (4,'اسمه ونسبه','6','3','0');
+INSERT INTO [t17794] VALUES (5,'ولادته ووفاته','6','3','0');
+INSERT INTO [t17794] VALUES (6,'شيوخه','7','3','0');
+INSERT INTO [t17794] VALUES (7,'منزلته ومكانته','7','3','0');
+INSERT INTO [t17794] VALUES (8,'ثناء العلماء عليه','7','3','0');
+INSERT INTO [t17794] VALUES (9,'مؤلفاته','7','3','0');
+INSERT INTO [t17794] VALUES (10,'أولا- الحديث','7','9','0');
+INSERT INTO [t17794] VALUES (11,'ثانيا- الفقه','8','9','0');
+INSERT INTO [t17794] VALUES (12,'ثالثا- التاريخ','9','9','0');
+INSERT INTO [t17794] VALUES (13,'وصف النسخة وعملي فيها','9','1','0');
+INSERT INTO [t17794] VALUES (14,'[مقدمة المصنف]','12','0','0');
+INSERT INTO [t17794] VALUES (15,'[اسمه ونسبه ونشأته]','12','0','0');
+INSERT INTO [t17794] VALUES (16,'[روايته عن الصحابة، وهل هي مرسلة أو متصلة؟]','13','0','0');
+INSERT INTO [t17794] VALUES (17,'[احتجاج العلماء بحديثه]','15','0','0');
+INSERT INTO [t17794] VALUES (18,'[الرواة عنه]','18','0','0');
+INSERT INTO [t17794] VALUES (19,'[الموازنة بينه وبين تلاميذ ابن عباس الآخرين]','19','0','0');
+INSERT INTO [t17794] VALUES (20,'[توثيقه وسعة علمه]','19','0','0');
+INSERT INTO [t17794] VALUES (21,'[ذكر من تكلم فيه]','23','0','0');
+INSERT INTO [t17794] VALUES (22,'[الرد على من تكلم فيه وإثبات توثيقه]','30','0','0');
+INSERT INTO [t17794] VALUES (23,'[وفاته رحمه الله]','35','0','0');
