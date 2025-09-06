cfa9df07ebf6247472d75aa7419a6cb876d3542f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:46:18 +0700

    Upload SQL chunk: t8466_chunk_0000.sql

---
+++ sql_chunks/t8466_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t8466
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t8466" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8466
+INSERT INTO [t8466] VALUES (1,'الوجه الأول من الكلام في الحديث','7','0','0');
+INSERT INTO [t8466] VALUES (2,'الوجه الثاني','21','0','0');
+INSERT INTO [t8466] VALUES (3,'الوجه الثالث','24','0','0');
+INSERT INTO [t8466] VALUES (4,'الوجه الرابع','26','0','0');
+INSERT INTO [t8466] VALUES (5,'الوجه الخامس','28','0','0');
+INSERT INTO [t8466] VALUES (6,'الوجه السادس','30','0','0');
+INSERT INTO [t8466] VALUES (7,'الوجه السابع','31','0','0');
+INSERT INTO [t8466] VALUES (8,'الوجه الثامن','32','0','0');
+INSERT INTO [t8466] VALUES (9,'الوجه التاسع','34','0','0');
+INSERT INTO [t8466] VALUES (10,'الوجه العاشر','36','0','0');
+INSERT INTO [t8466] VALUES (11,'الوجه الحادي عشر','38','0','0');
+INSERT INTO [t8466] VALUES (12,'الوجه الثاني عشر','40','0','0');
+INSERT INTO [t8466] VALUES (13,'الوجه الثالث عشر','41','0','0');
+INSERT INTO [t8466] VALUES (14,'الوجه الرابع عشر','42','0','0');
+INSERT INTO [t8466] VALUES (15,'الوجه الخامس عشر','43','0','0');
+INSERT INTO [t8466] VALUES (16,'الوجه السادس عشر','45','0','0');
+INSERT INTO [t8466] VALUES (17,'الوجه السابع عشر','46','0','0');
+INSERT INTO [t8466] VALUES (18,'الوجه الثامن عشر','50','0','0');
+INSERT INTO [t8466] VALUES (19,'الوجه التاسع عشر','53','0','0');
+INSERT INTO [t8466] VALUES (20,'الوجه الموفي عشرين','55','0','0');
+INSERT INTO [t8466] VALUES (21,'الوجه الحادي والعشرون','56','0','0');
+INSERT INTO [t8466] VALUES (22,'الوجه الثاني والعشرون','57','0','0');
+INSERT INTO [t8466] VALUES (23,'الوجه الثالث والعشرون','59','0','0');
+INSERT INTO [t8466] VALUES (24,'الوجه الرابع والعشرون','60','0','0');
+INSERT INTO [t8466] VALUES (25,'الوجه الخامس والعشرون','60','0','0');
+INSERT INTO [t8466] VALUES (26,'الوجه السادس والعشرون','61','0','0');
+INSERT INTO [t8466] VALUES (27,'الوجه السابع والعشرون','63','0','0');
+INSERT INTO [t8466] VALUES (28,'الوجه الثامن والعشرون','65','0','0');
+INSERT INTO [t8466] VALUES (29,'الوجه التاسع والعشرون','66','0','0');
+INSERT INTO [t8466] VALUES (30,'الوجه الموفي ثلاثين','67','0','0');
+INSERT INTO [t8466] VALUES (31,'الوجه الحادي والثلاثون','68','0','0');
+INSERT INTO [t8466] VALUES (32,'الوجه الثاني والثلاثون','69','0','0');
+INSERT INTO [t8466] VALUES (33,'الوجه الثالث والثلاثون','71','0','0');
+INSERT INTO [t8466] VALUES (34,'الوجه الرابع والثلاثون','72','0','0');
+INSERT INTO [t8466] VALUES (35,'الوجه الخامس والثلاثون','72','0','0');
+INSERT INTO [t8466] VALUES (36,'الوجه السادس والثلاثون','73','0','0');
+INSERT INTO [t8466] VALUES (37,'الوجه السابع والثلاثون','73','0','0');
+INSERT INTO [t8466] VALUES (38,'الوجه الثامن والثلاثون','74','0','0');
