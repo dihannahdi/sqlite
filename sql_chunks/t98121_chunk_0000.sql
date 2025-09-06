07d2c70bfa263c0c26a6d08d21588b05760859cf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:02:37 +0700

    Upload SQL chunk: t98121_chunk_0000.sql

---
+++ sql_chunks/t98121_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t98121
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t98121" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t98121
+INSERT INTO [t98121] VALUES (1,'مقدمة','5','0','0');
+INSERT INTO [t98121] VALUES (2,'الهداية .. كيف تكون؟','8','0','0');
+INSERT INTO [t98121] VALUES (3,'الحرية في الإسلام','19','0','0');
+INSERT INTO [t98121] VALUES (4,'الهدف من الخلق','30','0','0');
+INSERT INTO [t98121] VALUES (5,'فهم القرآن','42','0','0');
+INSERT INTO [t98121] VALUES (6,'مكانة المرأة وحقوقها','54','0','0');
+INSERT INTO [t98121] VALUES (7,'ثقافة الكراهية','66','0','0');
+INSERT INTO [t98121] VALUES (8,'لله في خلقه شئون','78','0','0');
+INSERT INTO [t98121] VALUES (9,'عصمة الأنبياء','90','0','0');
+INSERT INTO [t98121] VALUES (10,'تعدد الزوجات','104','0','0');
+INSERT INTO [t98121] VALUES (11,'لا يتوقف أمر الدين على المعجزات','116','0','0');
+INSERT INTO [t98121] VALUES (12,'بين المعجزة والكرامة','126','0','0');
+INSERT INTO [t98121] VALUES (13,'العلاقة بين القدر والدعاء','138','0','0');
+INSERT INTO [t98121] VALUES (14,'الطلاق','150','0','0');
+INSERT INTO [t98121] VALUES (15,'عدم فهم التعارض الموهوم','162','0','0');
+INSERT INTO [t98121] VALUES (16,'انتشار القتل','167','0','0');
+INSERT INTO [t98121] VALUES (17,'انتشار الاغتصاب','168','0','0');
+INSERT INTO [t98121] VALUES (18,'انتشار الاكتئاب','168','0','0');
+INSERT INTO [t98121] VALUES (19,'الخاتمة','173','0','0');
