c8b4109444601bcd3a974c3568205a5eb8e95867
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:05 +0700

    Upload SQL chunk: t95677_chunk_0000.sql

---
+++ sql_chunks/t95677_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t95677
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t95677" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95677
+INSERT INTO [t95677] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95677] VALUES (2,'الصلاة','2','0','0');
+INSERT INTO [t95677] VALUES (3,'ليس الطريق سوى طريق محمد','11','0','0');
+INSERT INTO [t95677] VALUES (4,'الوقوف بين يدي الله','20','0','0');
+INSERT INTO [t95677] VALUES (5,'حكم تارك الصلاة','39','0','0');
+INSERT INTO [t95677] VALUES (6,'المصادر','44','0','0');
