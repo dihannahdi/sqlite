fe01fa7e1465e98fa2bedb8ee23b91f56a1c126b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:38 +0700

    Upload SQL chunk: t6995_chunk_0000.sql

---
+++ sql_chunks/t6995_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t6995
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t6995" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6995
+INSERT INTO [t6995] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6995] VALUES (2,'ألف التثنية','1','0','0');
+INSERT INTO [t6995] VALUES (3,'الرأي الراجح ودليله','5','0','0');
+INSERT INTO [t6995] VALUES (4,'الاعتراضات التي ترد على القول بأن الألف حرف إعراب','8','0','0');
+INSERT INTO [t6995] VALUES (5,'ثبات الألف في المثنى','11','0','0');
+INSERT INTO [t6995] VALUES (6,'لا تقدير إعراب في ألف التثنية','12','0','0');
+INSERT INTO [t6995] VALUES (7,'اعتراض على كون النون عوضا','14','0','0');
+INSERT INTO [t6995] VALUES (8,'ألف التأنيث في حبلى','16','0','0');
+INSERT INTO [t6995] VALUES (9,'دليل آخر على كون الألف في التثنية حرف إعراب','16','0','0');
+INSERT INTO [t6995] VALUES (10,'الرد على قول أبي الحسن الأخفش','17','0','0');
+INSERT INTO [t6995] VALUES (11,'الرد على قول أبي عمر الجرمي','20','0','0');
+INSERT INTO [t6995] VALUES (12,'الرد على قول الفراء وأبي إسحاق الزيادي','23','0','0');
+INSERT INTO [t6995] VALUES (13,'واو الجمع الذي على حد التثنية','24','0','0');
+INSERT INTO [t6995] VALUES (14,'لم يثنى بالألف ويجمع بالواو','24','0','0');
+INSERT INTO [t6995] VALUES (15,'تثنية المبهم','26','0','0');
+INSERT INTO [t6995] VALUES (16,'أحوال نون التثنية','34','0','0');
+INSERT INTO [t6995] VALUES (17,'تشديد نون المبهم','38','0','0');
+INSERT INTO [t6995] VALUES (18,'حركة نوني التثنية والجمع','39','0','0');
+INSERT INTO [t6995] VALUES (19,'نون الأفعال الخمسة','42','0','0');
