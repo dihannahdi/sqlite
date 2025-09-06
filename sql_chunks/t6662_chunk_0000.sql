c11716ae01638fe5e9df7a1aabb7ca2e388977df
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:37 +0700

    Upload SQL chunk: t6662_chunk_0000.sql

---
+++ sql_chunks/t6662_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t6662
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t6662" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6662
+INSERT INTO [t6662] VALUES (1,'نص الرسالة','1','0','0');
+INSERT INTO [t6662] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6662] VALUES (3,'سؤال وجوابه للعلامة ابن حجر','4','0','0');
+INSERT INTO [t6662] VALUES (4,'وصورة الجواب','5','0','0');
+INSERT INTO [t6662] VALUES (5,'كتبه صالح بن عمر البلقيني','5','0','0');
