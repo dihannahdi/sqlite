16a40d138ad21b4a8d23b57543dcac1c47a39670
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:07 +0700

    Upload SQL chunk: t26675_chunk_0000.sql

---
+++ sql_chunks/t26675_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t26675
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t26675" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26675
+INSERT INTO [t26675] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26675] VALUES (2,'مأخذ العلم','2','0','0');
+INSERT INTO [t26675] VALUES (3,'باب أداء العلم بالتحديث به نطقا','6','0','0');
+INSERT INTO [t26675] VALUES (4,'باب في الفرق بين قول المحدث (ثنا) وبين قوله (أخبرنا)','7','0','0');
+INSERT INTO [t26675] VALUES (5,'باب المناولة','9','0','0');
+INSERT INTO [t26675] VALUES (6,'باب العالم يؤتى بالكتاب يعرفه فيقال له: أنأخذ عنك ما فيه؟','10','0','0');
+INSERT INTO [t26675] VALUES (7,'باب القول في اللحن','12','0','0');
+INSERT INTO [t26675] VALUES (8,'باب الإجازة','14','0','0');
