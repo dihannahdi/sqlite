bb637269a98df5eddb594b1189dceda6754b6125
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:51 +0700

    Upload SQL chunk: t9088_chunk_0000.sql

---
+++ sql_chunks/t9088_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9088
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9088" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9088
+INSERT INTO [t9088] VALUES (1,'أحاديث نافع، عن ابن عمر','13','0','0');
+INSERT INTO [t9088] VALUES (2,'حديث سوار بن مصعب، أبي عبد الله الهمداني','80','0','0');
