8a0bba90763bd28dd87ec95a061cad1db6517e90
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:03 +0700

    Upload SQL chunk: t10322_chunk_0000.sql

---
+++ sql_chunks/t10322_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t10322
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t10322" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10322
+INSERT INTO [t10322] VALUES (1,'تمهيد','1','0','0');
+INSERT INTO [t10322] VALUES (2,'القسم الأول','4','0','0');
+INSERT INTO [t10322] VALUES (3,'الفصل الأول: ترجمة العلائي','4','2','0');
+INSERT INTO [t10322] VALUES (4,'الفصل الثاني: معلومات موجزة عن الكتب التي أورد عليها التنبيهات ومؤلفيها','22','2','0');
+INSERT INTO [t10322] VALUES (5,'القسم الثاني','30','0','0');
+INSERT INTO [t10322] VALUES (6,'الفصل الأول: بيان عملي في الكتاب','30','5','0');
+INSERT INTO [t10322] VALUES (7,'الفصل الثاني: تسمية الكتاب. ووصف نسخته الخطية','31','5','0');
+INSERT INTO [t10322] VALUES (8,'القسم الثالث','38','0','0');
+INSERT INTO [t10322] VALUES (9,'تحقيق نصوص الكتاب','38','8','0');
+INSERT INTO [t10322] VALUES (10,'الخاتمة','82','0','0');
+INSERT INTO [t10322] VALUES (11,'مصادر ومراجع','84','0','0');
