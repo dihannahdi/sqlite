4f6100ed5cdbb92e8fe978aae4a9cca086b584ee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:45 +0700

    Upload SQL chunk: t401_chunk_0000.sql

---
+++ sql_chunks/t401_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t401
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t401" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t401
+INSERT INTO [t401] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t401] VALUES (2,'النفحة النسرينية واللمحة المرينية','1','1','0');
+INSERT INTO [t401] VALUES (3,'الأبيات','2','0','0');
+INSERT INTO [t401] VALUES (4,'الشرح','6','0','0');
