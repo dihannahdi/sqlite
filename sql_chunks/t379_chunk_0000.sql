29feca0021a1f3ec032f5a81455621bf19b55629
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:16:43 +0700

    Upload SQL chunk: t379_chunk_0000.sql

---
+++ sql_chunks/t379_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t379
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t379" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t379
+INSERT INTO [t379] VALUES (1,'أجزاء الآية','2','0','0');
+INSERT INTO [t379] VALUES (2,'معنى «الفتنة»','2','0','0');
+INSERT INTO [t379] VALUES (3,'معنى «الجسد»','3','0','0');
+INSERT INTO [t379] VALUES (4,'الفرق بين الإنابة والتوبة','4','0','0');
+INSERT INTO [t379] VALUES (5,'سؤال المغفرة لا يستلزم سبق معصية','4','0','0');
+INSERT INTO [t379] VALUES (6,'ما قيل في تفسير الآية','5','0','0');
+INSERT INTO [t379] VALUES (7,'المنقول عن المتقدمين','5','0','0');
+INSERT INTO [t379] VALUES (8,'قول النقاش','6','0','0');
+INSERT INTO [t379] VALUES (9,'قول أبي مسلم الأصبهاني','6','0','0');
+INSERT INTO [t379] VALUES (10,'تمحيص','7','0','0');
+INSERT INTO [t379] VALUES (11,'تدبر','9','0','0');
+INSERT INTO [t379] VALUES (12,'من فوائد ذكر القصة','9','0','0');
+INSERT INTO [t379] VALUES (13,'من فوائد الإجمال','10','0','0');
+INSERT INTO [t379] VALUES (14,'المحصل','10','0','0');
