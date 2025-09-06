87b414f2c685f4d64a731712a89cc933c1f98f32
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:35 +0700

    Upload SQL chunk: t7610_chunk_0000.sql

---
+++ sql_chunks/t7610_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t7610
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t7610" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7610
+INSERT INTO [t7610] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7610] VALUES (2,'النص المحقق','18','0','0');
+INSERT INTO [t7610] VALUES (3,'مصادر ومراجع','86','0','0');
