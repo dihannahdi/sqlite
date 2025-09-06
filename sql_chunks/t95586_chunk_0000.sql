a9883afbf4ec8859770128e1a25a6e9e15b511d7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:42 +0700

    Upload SQL chunk: t95586_chunk_0000.sql

---
+++ sql_chunks/t95586_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t95586
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t95586" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95586
+INSERT INTO [t95586] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t95586] VALUES (2,'الفصل الأول مراد الإمام البخاري من قوله: «فيه نظر»','3','0','0');
+INSERT INTO [t95586] VALUES (3,'المبحث الأول: أنها مرتبة في الجرح الشديد','3','2','0');
+INSERT INTO [t95586] VALUES (4,'المبحث الثاني: من عدها جرحا خفيفا','5','2','0');
+INSERT INTO [t95586] VALUES (5,'المبحث الثالث: مناقشة الآراء والترجيح','6','2','0');
+INSERT INTO [t95586] VALUES (6,'الفصل الثاني الدراسة التطبيقية في التأريخ الكبير','9','0','0');
+INSERT INTO [t95586] VALUES (7,'قائمة ببعض المصادر والمراجع','25','0','0');
