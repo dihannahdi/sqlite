0bfdc89175fc5aa8c379069ce6089ddebc539d90
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:12 +0700

    Upload SQL chunk: t8810_chunk_0000.sql

---
+++ sql_chunks/t8810_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t8810
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t8810" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8810
+INSERT INTO [t8810] VALUES (1,'التوحيد','1','0','0');
+INSERT INTO [t8810] VALUES (2,'التوحيد','1','1','0');
+INSERT INTO [t8810] VALUES (3,'فضائل التوحيد','5','1','0');
+INSERT INTO [t8810] VALUES (4,'العبادات وأثرها في إصلاح القلوب وتهذيب النفوس','9','0','0');
+INSERT INTO [t8810] VALUES (5,'مدخل','9','4','0');
+INSERT INTO [t8810] VALUES (6,'الصلاة وفضائلها','10','4','0');
+INSERT INTO [t8810] VALUES (7,'الزكاة وفضائلها','16','4','0');
+INSERT INTO [t8810] VALUES (8,'الصيام وفضائله','18','4','0');
+INSERT INTO [t8810] VALUES (9,'الحج وفضائله','20','4','0');
+INSERT INTO [t8810] VALUES (10,'العبادلت بين السلف والخلف','23','4','0');
+INSERT INTO [t8810] VALUES (11,'داء ودواء','25','4','0');
