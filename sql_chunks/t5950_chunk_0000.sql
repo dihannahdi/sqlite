b53811d472281bd57fe3d7f3db0ceb38f9ef8ac8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:48 +0700

    Upload SQL chunk: t5950_chunk_0000.sql

---
+++ sql_chunks/t5950_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5950
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5950" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5950
+INSERT INTO [t5950] VALUES (1,'أخبرنا الشيخ أبو بكر عبد العزيز','1','0','0');
+INSERT INTO [t5950] VALUES (2,'حدثني أبو عبد الله الحاكم','204','0','0');
