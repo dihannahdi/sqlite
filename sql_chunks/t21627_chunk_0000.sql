7d9a3d0ea9be1926ab73773465474181e519f1db
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:39 +0700

    Upload SQL chunk: t21627_chunk_0000.sql

---
+++ sql_chunks/t21627_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t21627
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t21627" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21627
+INSERT INTO [t21627] VALUES (1,'شرح كتاب العلم لأبي خيثمة (١)','2','0','0');
+INSERT INTO [t21627] VALUES (2,'شرح كتاب العلم لأبي خيثمة (٢)','34','0','0');
+INSERT INTO [t21627] VALUES (3,'شرح كتاب العلم لأبي خيثمة (٣)','67','0','0');
+INSERT INTO [t21627] VALUES (4,'شرح كتاب العلم لأبي خيثمة (٤)','104','0','0');
+INSERT INTO [t21627] VALUES (5,'شرح كتاب العلم لأبي خيثمة (٥)','139','0','0');
+INSERT INTO [t21627] VALUES (6,'شرح كتاب العلم لأبي خيثمة (٦)','175','0','0');
+INSERT INTO [t21627] VALUES (7,'شرح كتاب العلم لأبي خيثمة (٧)','209','0','0');
