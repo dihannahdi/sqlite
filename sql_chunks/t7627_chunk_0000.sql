85b5bb0de78881f2ea3d84ccb41e3891dfb3f3a8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:51 +0700

    Upload SQL chunk: t7627_chunk_0000.sql

---
+++ sql_chunks/t7627_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t7627
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t7627" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7627
+INSERT INTO [t7627] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7627] VALUES (2,'القسم الأول: عرض لآراء المسلمين حول منهجية توثيق السنة بالاعتماد على نقد السند والمتن معا','3','0','0');
+INSERT INTO [t7627] VALUES (3,'القسم الثاني: عرض لآراء المستغربين فيما يتعلق بتوثيق السنة عبر نقد المتن والسند والرد عليهم','9','0','0');
+INSERT INTO [t7627] VALUES (4,'القسم الثالث: نظرية الإسناد عند شاخت والرد عليه','39','0','0');
+INSERT INTO [t7627] VALUES (5,'الخاتمة والتوصيات','57','0','0');
+INSERT INTO [t7627] VALUES (6,'مصادر ومراجع','61','0','0');
