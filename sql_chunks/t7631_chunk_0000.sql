ace3b43e27b149f4ecf6ea51858050c427c47067
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:54 +0700

    Upload SQL chunk: t7631_chunk_0000.sql

---
+++ sql_chunks/t7631_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7631
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7631" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7631
+INSERT INTO [t7631] VALUES (1,'الرسالة الأكملية في ما يجب لله من صفات الكمال','1','0','0');
+INSERT INTO [t7631] VALUES (2,'تأليف: شيخ الإسلام ابن تيمية','1','0','0');
