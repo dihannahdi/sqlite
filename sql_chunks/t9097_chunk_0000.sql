e6f8cdec505fb6e77ba781be3bb030c633345711
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:57 +0700

    Upload SQL chunk: t9097_chunk_0000.sql

---
+++ sql_chunks/t9097_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t9097
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t9097" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9097
+INSERT INTO [t9097] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t9097] VALUES (2,'سورة الفاتحة','7','0','0');
+INSERT INTO [t9097] VALUES (3,'من سورة المائدة','69','0','0');
+INSERT INTO [t9097] VALUES (4,'قصة إبراهيم في سورتي الحجر والذاريات','79','0','0');
+INSERT INTO [t9097] VALUES (5,'قصة موسى في سورتي النمل والقصص','86','0','0');
+INSERT INTO [t9097] VALUES (6,'من سورتي المؤمنون والزمر','110','0','0');
+INSERT INTO [t9097] VALUES (7,'من سورتي المؤمنون والمعارج','122','0','0');
+INSERT INTO [t9097] VALUES (8,'من سورتي الطور والقلم','164','0','0');
+INSERT INTO [t9097] VALUES (9,'من سورة القمر','166','0','0');
+INSERT INTO [t9097] VALUES (10,'من سورة الجمعة','171','0','0');
+INSERT INTO [t9097] VALUES (11,'من سورة المنافقون','174','0','0');
+INSERT INTO [t9097] VALUES (12,'من سورتي المعارج وعبس','189','0','0');
+INSERT INTO [t9097] VALUES (13,'من سورتي المعارج والقارعة','194','0','0');
+INSERT INTO [t9097] VALUES (14,'سورة القيامة','197','0','0');
+INSERT INTO [t9097] VALUES (15,'سورة البلد','237','0','0');
+INSERT INTO [t9097] VALUES (16,'مراجع الكتاب','281','0','0');
