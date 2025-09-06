ea8c290789035e3ba56215b16085d9a5e9067c95
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:53 +0700

    Upload SQL chunk: t35097_chunk_0000.sql

---
+++ sql_chunks/t35097_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t35097
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t35097" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t35097
+INSERT INTO [t35097] VALUES (1,'الأحاديث الواردة المقيدة بأدبار الصلوات في كتب السنة جمعا ودراسة','1','0','0');
+INSERT INTO [t35097] VALUES (2,'المقدمة','2','0','0');
+INSERT INTO [t35097] VALUES (3,'تمهيد: في معنى (دبر الصلاة) في الأحاديث','4','0','0');
+INSERT INTO [t35097] VALUES (4,'مسألة: هل يلزم الترتيب في التسبيح والتحميد والتكبير أم لا','9','3','0');
+INSERT INTO [t35097] VALUES (5,'الفصل الأول: الأحاديث المقيدة بأدبار الصلاة قبل السلام','10','0','0');
+INSERT INTO [t35097] VALUES (6,'الفصل الثاني: الأحاديث التي تقال بعد السلام','19','0','0');
+INSERT INTO [t35097] VALUES (7,'الخاتمة','39','0','0');
+INSERT INTO [t35097] VALUES (8,'فهرس المصادر والمراجع','41','0','0');
