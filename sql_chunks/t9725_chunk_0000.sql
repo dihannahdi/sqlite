38d21b7f5ff5d663c172537210bc07f13ee51a00
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:00:44 +0700

    Upload SQL chunk: t9725_chunk_0000.sql

---
+++ sql_chunks/t9725_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t9725
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t9725" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9725
+INSERT INTO [t9725] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t9725] VALUES (2,'نص المنظومة','3','0','0');
+INSERT INTO [t9725] VALUES (3,'تمهيد','13','0','0');
+INSERT INTO [t9725] VALUES (4,'أقسام التوحيد ثلاثة:','13','0','0');
+INSERT INTO [t9725] VALUES (5,'مقدمة المنظومة','83','0','0');
+INSERT INTO [t9725] VALUES (6,'الباب الأول في معرفة الله تعالى','142','0','0');
+INSERT INTO [t9725] VALUES (7,'الباب الثاني في الأفعال المخلوقة','306','0','0');
+INSERT INTO [t9725] VALUES (8,'الباب الثالث في الأحكام','353','0','0');
+INSERT INTO [t9725] VALUES (9,'الباب الرابع في أشراط الساعة','421','0','0');
+INSERT INTO [t9725] VALUES (10,'الباب الخامس في ذكر النبوة ومتعلقاتها','508','0','0');
+INSERT INTO [t9725] VALUES (11,'الباب السادس في ذكر الإمامة ومتعلقاتها','648','0','0');
+INSERT INTO [t9725] VALUES (12,'الخاتمة','700','0','0');
