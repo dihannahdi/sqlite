056271f04246ac8e5d69ec9e8ac87e30c5cf81c4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:03 +0700

    Upload SQL chunk: t480_chunk_0000.sql

---
+++ sql_chunks/t480_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t480
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t480" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t480
+INSERT INTO [t480] VALUES (1,'مذهب الشافعي أن الزواج لا يصح إلا بولاية صحيح الولاية','2','0','0');
+INSERT INTO [t480] VALUES (2,'إبطال النكاح بعد صحته عند القاضي','3','0','0');
+INSERT INTO [t480] VALUES (3,'فتوى جماعة من متأخري الشافعية بجواز إنكاح غير الأب والجد الصغيرة','5','0','0');
