0f174a01f2af6d6d0b8eac2c31d95240bc9545b0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:50:18 +0700

    Upload SQL chunk: t10632_chunk_0000.sql

---
+++ sql_chunks/t10632_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10632
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10632" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10632
+INSERT INTO [t10632] VALUES (1,'الجواهر الغوالي في ذكر الأسانيد العوالي للبديري - مخطوط','1','0','0');
