a8cca40b05ad86740353509c1ddd0a1584795b88
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:39 +0700

    Upload SQL chunk: t6908_chunk_0000.sql

---
+++ sql_chunks/t6908_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t6908
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t6908" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6908
+INSERT INTO [t6908] VALUES (1,'الجزء ١','1','0','0');
+INSERT INTO [t6908] VALUES (2,'الجزء ٢','348','0','0');
+INSERT INTO [t6908] VALUES (3,'الجزء ٣','741','0','0');
+INSERT INTO [t6908] VALUES (4,'قافية الميم','1057','3','0');
+INSERT INTO [t6908] VALUES (5,'الجزء ٤','1131','0','0');
