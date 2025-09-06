a7c90edc15777032cd9a973b7a19e837547791cd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:35 +0700

    Upload SQL chunk: t3561_chunk_0000.sql

---
+++ sql_chunks/t3561_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t3561
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t3561" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3561
+INSERT INTO [t3561] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t3561] VALUES (2,'أولا: التعريف بالبحث:','3','0','0');
+INSERT INTO [t3561] VALUES (3,'ثانيا: الدراسات السابقة:','6','0','0');
+INSERT INTO [t3561] VALUES (4,'ثالثا: الإطار النظري للبحث:','14','0','0');
+INSERT INTO [t3561] VALUES (5,'رابعا: تربية النبي - صلى الله عليه وسلم - لبناته في مرحلة الطفولة:','18','0','0');
+INSERT INTO [t3561] VALUES (6,'خامسا: رعاية النبي - صلى الله عليه وسلم- لبناته في مرحلة الصبا:','20','0','0');
+INSERT INTO [t3561] VALUES (7,'سادسا: تزويج النبي - صلى الله عليه وسلم- لبناته:','22','0','0');
+INSERT INTO [t3561] VALUES (8,'سابعا: رعاية النبي - صلى الله عليه وسلم- لبناته بعد الزواج:','32','0','0');
+INSERT INTO [t3561] VALUES (9,'نتائج البحث:','38','0','0');
+INSERT INTO [t3561] VALUES (10,'التوصيات:','39','0','0');
