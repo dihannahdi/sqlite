d62d9a3e43c825d345f417656512f5449e3c52f2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:47 +0700

    Upload SQL chunk: t4317_chunk_0000.sql

---
+++ sql_chunks/t4317_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t4317
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t4317" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4317
+INSERT INTO [t4317] VALUES (1,'{وخلق منها زوجها}','2','0','0');
+INSERT INTO [t4317] VALUES (2,'الشقيقتان بنات إبليس','4','0','0');
+INSERT INTO [t4317] VALUES (3,'الجانب العملي والفكري في سماحة الإسلام','9','0','0');
+INSERT INTO [t4317] VALUES (4,'أمة واحدة','16','0','0');
+INSERT INTO [t4317] VALUES (5,'أمة واحدة ٢','23','0','0');
+INSERT INTO [t4317] VALUES (6,'مع الزمن في القرآن الكريم','28','0','0');
+INSERT INTO [t4317] VALUES (7,'القرآن يبدأ عندما تنتهي حضارة العلماء','33','0','0');
+INSERT INTO [t4317] VALUES (8,'علاقة الحكم الشرعي بمصدره','34','0','0');
+INSERT INTO [t4317] VALUES (9,'القرض الحسن بين الأجر من الله , والمنفعة , وسلامة الدين','37','0','0');
+INSERT INTO [t4317] VALUES (10,'شهادة المرأة','44','0','0');
+INSERT INTO [t4317] VALUES (11,'المشهد الأخير من القصة القرآنية','47','0','0');
+INSERT INTO [t4317] VALUES (12,'أمم أمثالكم','56','0','0');
+INSERT INTO [t4317] VALUES (13,'معاني الأمر في القرآن وأثره على اجتهاد الأئمة','61','0','0');
