83e1ea16fdd0342a17840a60fe585b4bb81f6c6c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:29 +0700

    Upload SQL chunk: t6022_chunk_0000.sql

---
+++ sql_chunks/t6022_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t6022
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t6022" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6022
+INSERT INTO [t6022] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t6022] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t6022] VALUES (3,'باب فضل اصطناع المعروف','3','0','0');
+INSERT INTO [t6022] VALUES (4,'باب الضحك','27','0','0');
+INSERT INTO [t6022] VALUES (5,'باب اصطناع المعروف إلى من هو أهله','45','0','0');
+INSERT INTO [t6022] VALUES (6,'باب في الحوائج','77','0','0');
+INSERT INTO [t6022] VALUES (7,'الجزء الثاني بسم الله الرحمن الرحيم','91','0','0');
+INSERT INTO [t6022] VALUES (8,'باب طلب الحوائج إلى حسان الوجوه','111','0','0');
+INSERT INTO [t6022] VALUES (9,'باب في شكر الصنيعة','130','0','0');
+INSERT INTO [t6022] VALUES (10,'باب من أنظر معسرا','157','0','0');
