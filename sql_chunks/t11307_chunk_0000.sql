968e9b07b240e95024799568d2d5d2641ecdf506
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:45 +0700

    Upload SQL chunk: t11307_chunk_0000.sql

---
+++ sql_chunks/t11307_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t11307
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t11307" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11307
+INSERT INTO [t11307] VALUES (1,'مقدمة الطبعة الثانية','1','0','0');
+INSERT INTO [t11307] VALUES (2,'مقدمة الطبعة الأولى','2','0','0');
+INSERT INTO [t11307] VALUES (3,'قيام رمضان','15','0','0');
+INSERT INTO [t11307] VALUES (4,'الاعتكاف','32','0','0');
