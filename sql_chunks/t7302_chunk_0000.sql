81f34fcdabfc840a336e825a7a4b33f7c1011c0c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:19 +0700

    Upload SQL chunk: t7302_chunk_0000.sql

---
+++ sql_chunks/t7302_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t7302
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t7302" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7302
+INSERT INTO [t7302] VALUES (1,'كتاب الألف','1','0','0');
+INSERT INTO [t7302] VALUES (2,'باب الباء','25','0','0');
+INSERT INTO [t7302] VALUES (3,'باب التاء','45','0','0');
+INSERT INTO [t7302] VALUES (4,'باب الثاء','52','0','0');
+INSERT INTO [t7302] VALUES (5,'باب الجيم','59','0','0');
+INSERT INTO [t7302] VALUES (6,'باب الحاء','88','0','0');
+INSERT INTO [t7302] VALUES (7,'باب الخاء','167','0','0');
+INSERT INTO [t7302] VALUES (8,'باب الدال','190','0','0');
+INSERT INTO [t7302] VALUES (9,'باب الذال المعجمة','226','0','0');
+INSERT INTO [t7302] VALUES (10,'باب الراء','234','0','0');
+INSERT INTO [t7302] VALUES (11,'هذا باب الزاي','306','0','0');
+INSERT INTO [t7302] VALUES (12,'باب حروف السين','348','0','0');
+INSERT INTO [t7302] VALUES (13,'باب الشين','387','0','0');
+INSERT INTO [t7302] VALUES (14,'باب الصاد','427','0','0');
+INSERT INTO [t7302] VALUES (15,'باب الضاد','454','0','0');
+INSERT INTO [t7302] VALUES (16,'باب الطاء','466','0','0');
+INSERT INTO [t7302] VALUES (17,'باب الظاء','481','0','0');
+INSERT INTO [t7302] VALUES (18,'باب العين','486','0','0');
+INSERT INTO [t7302] VALUES (19,'باب من القاف','675','0','0');
+INSERT INTO [t7302] VALUES (20,'بقية باب القاف','698','0','0');
+INSERT INTO [t7302] VALUES (21,'باب الكاف','742','0','0');
+INSERT INTO [t7302] VALUES (22,'باب اللام','788','0','0');
+INSERT INTO [t7302] VALUES (23,'باب الميم','830','0','0');
+INSERT INTO [t7302] VALUES (24,'باب النون','855','0','0');
+INSERT INTO [t7302] VALUES (25,'باب الواو','889','0','0');
+INSERT INTO [t7302] VALUES (26,'باب الهاء','915','0','0');
+INSERT INTO [t7302] VALUES (27,'باب الياء','925','0','0');
