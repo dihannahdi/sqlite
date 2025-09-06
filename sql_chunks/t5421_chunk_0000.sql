9a21756f910ad4dcbc231bd49d1b6fdabb13dc0e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:49 +0700

    Upload SQL chunk: t5421_chunk_0000.sql

---
+++ sql_chunks/t5421_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t5421
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t5421" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5421
+INSERT INTO [t5421] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5421] VALUES (2,'الألقاب والأبيات','21','0','0');
+INSERT INTO [t5421] VALUES (3,'الزحاف المنفرد','23','0','0');
+INSERT INTO [t5421] VALUES (4,'الزحاف المزدوج','26','0','0');
+INSERT INTO [t5421] VALUES (5,'المعاقبة والمراقبة والمكانفة','27','0','0');
+INSERT INTO [t5421] VALUES (6,'علل الأجزاء','29','0','0');
+INSERT INTO [t5421] VALUES (7,'ما اجرى من العلل مجرى الزحاف','42','0','0');
+INSERT INTO [t5421] VALUES (8,'الطويل','48','0','0');
+INSERT INTO [t5421] VALUES (9,'المديد','52','0','0');
+INSERT INTO [t5421] VALUES (10,'إلى آخره، فحمله بعضهم على أنه من شاذ تامه، وأن القصيدة مصرعة، وبعضهم على أنه مما','53','0','0');
+INSERT INTO [t5421] VALUES (11,'البسيط','54','0','0');
+INSERT INTO [t5421] VALUES (12,'الوافر','56','0','0');
+INSERT INTO [t5421] VALUES (13,'الكامل','59','0','0');
+INSERT INTO [t5421] VALUES (14,'فقوله ((أعمالي)) هو الضرب، وزنه مفعولن. وأشار إلى هذا الشاهد بقوبله ((لم تجد))','60','0','0');
+INSERT INTO [t5421] VALUES (15,'الهرج','61','0','0');
+INSERT INTO [t5421] VALUES (16,'الرجز','62','0','0');
+INSERT INTO [t5421] VALUES (17,'الرمل','65','0','0');
+INSERT INTO [t5421] VALUES (18,'السريع','66','0','0');
+INSERT INTO [t5421] VALUES (19,'المنسرح','68','0','0');
+INSERT INTO [t5421] VALUES (20,'الخفيف','69','0','0');
+INSERT INTO [t5421] VALUES (21,'المضارع','70','0','0');
+INSERT INTO [t5421] VALUES (22,'المقتضب','70','0','0');
+INSERT INTO [t5421] VALUES (23,'المجتث','71','0','0');
+INSERT INTO [t5421] VALUES (24,'المتقارب','72','0','0');
+INSERT INTO [t5421] VALUES (25,'القوافي وعيوبها','80','0','0');
