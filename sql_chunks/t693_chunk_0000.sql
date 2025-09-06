5affdc3cb8286d1c694f3fa134002ec9b35d072e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:00 +0700

    Upload SQL chunk: t693_chunk_0000.sql

---
+++ sql_chunks/t693_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t693
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t693" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t693
+INSERT INTO [t693] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t693] VALUES (2,'القول على','1','1','0');
+INSERT INTO [t693] VALUES (3,'الياقوت','1','0','0');
+INSERT INTO [t693] VALUES (4,'القول على','2','3','0');
+INSERT INTO [t693] VALUES (5,'القول على','2','3','0');
+INSERT INTO [t693] VALUES (6,'القول على','2','3','0');
+INSERT INTO [t693] VALUES (7,'البلخش','2','0','0');
+INSERT INTO [t693] VALUES (8,'البجادي','2','0','0');
+INSERT INTO [t693] VALUES (9,'الماس','2','0','0');
+INSERT INTO [t693] VALUES (10,'القول على','3','9','0');
+INSERT INTO [t693] VALUES (11,'الدر واللؤلؤ','3','0','0');
+INSERT INTO [t693] VALUES (12,'القول في','4','11','0');
+INSERT INTO [t693] VALUES (13,'الزمرد','4','0','0');
+INSERT INTO [t693] VALUES (14,'القول على','5','13','0');
+INSERT INTO [t693] VALUES (15,'القول على','5','13','0');
+INSERT INTO [t693] VALUES (16,'القول على','5','13','0');
+INSERT INTO [t693] VALUES (17,'الزبرجد','5','0','0');
+INSERT INTO [t693] VALUES (18,'الفيروزج','5','0','0');
+INSERT INTO [t693] VALUES (19,'البلور','5','0','0');
+INSERT INTO [t693] VALUES (20,'القول على','6','19','0');
+INSERT INTO [t693] VALUES (21,'القول على','6','19','0');
+INSERT INTO [t693] VALUES (22,'القول على','6','19','0');
+INSERT INTO [t693] VALUES (23,'القول على','6','19','0');
+INSERT INTO [t693] VALUES (24,'الدهنج','6','0','0');
+INSERT INTO [t693] VALUES (25,'اليشب','6','0','0');
+INSERT INTO [t693] VALUES (26,'الفاذزهر','6','0','0');
+INSERT INTO [t693] VALUES (27,'الخرتوت','6','0','0');
