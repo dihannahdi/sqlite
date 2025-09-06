08b0efe9ec2e08de69a63b60ce2831e69ceaaa6b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:54 +0700

    Upload SQL chunk: t10847_chunk_0000.sql

---
+++ sql_chunks/t10847_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t10847
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t10847" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10847
+INSERT INTO [t10847] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10847] VALUES (2,'الهدف الأول','5','0','0');
+INSERT INTO [t10847] VALUES (3,'إيضاح عبودية المخلوق للخالق وتحقيقها','5','2','0');
+INSERT INTO [t10847] VALUES (4,'الهدف الثاني','13','0','0');
+INSERT INTO [t10847] VALUES (5,'الاهتمام بالمجتمع الإسلامي','13','4','0');
+INSERT INTO [t10847] VALUES (6,'الهدف الثالث','19','0','0');
+INSERT INTO [t10847] VALUES (7,'إقامة دين الله بين عباد الله بالطرق الموصلة إلى ذلك','19','6','0');
+INSERT INTO [t10847] VALUES (8,'فصل: في بيان عقيدة الشيخ محمد بن عبد الوهاب','25','0','0');
+INSERT INTO [t10847] VALUES (9,'*','25','8','0');
+INSERT INTO [t10847] VALUES (10,'الخاتمة','30','8','0');
+INSERT INTO [t10847] VALUES (11,'مراجع الرسالة','31','8','0');
