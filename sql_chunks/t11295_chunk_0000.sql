acf9fcd0d1fb1334fbe3954c04e7c52a923f3b61
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:34 +0700

    Upload SQL chunk: t11295_chunk_0000.sql

---
+++ sql_chunks/t11295_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t11295
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t11295" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11295
+INSERT INTO [t11295] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11295] VALUES (2,'تمهيد','4','0','0');
+INSERT INTO [t11295] VALUES (3,'الفصل الأول: طرق تخريج الحديث','21','0','0');
+INSERT INTO [t11295] VALUES (4,'مدخل','21','3','0');
+INSERT INTO [t11295] VALUES (5,'الحالة الأولى','22','3','0');
+INSERT INTO [t11295] VALUES (6,'الحالة الثانية','37','3','0');
+INSERT INTO [t11295] VALUES (7,'الحالة الثالثة','47','3','0');
+INSERT INTO [t11295] VALUES (8,'الفصل الثاني: دراسة الإسناد والحكم على الحديث','58','0','0');
+INSERT INTO [t11295] VALUES (9,'مدخل','58','8','0');
+INSERT INTO [t11295] VALUES (10,'دراسة إسناد الحديث ومتنه','61','8','0');
+INSERT INTO [t11295] VALUES (11,'الكتب التي تعين الباحث على الكشف عن الرجال','66','8','0');
