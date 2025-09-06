682d2ab6690065e8cd79d31e5033fbe9143f7395
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:41:00 +0700

    Upload SQL chunk: t7640_chunk_0000.sql

---
+++ sql_chunks/t7640_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t7640
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t7640" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7640
+INSERT INTO [t7640] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7640] VALUES (2,'مقدمة المؤلف','38','0','0');
+INSERT INTO [t7640] VALUES (3,'باب: الألف','42','0','0');
+INSERT INTO [t7640] VALUES (4,'باب: الباء','152','0','0');
+INSERT INTO [t7640] VALUES (5,'باب: الثاء','161','0','0');
+INSERT INTO [t7640] VALUES (6,'باب: الجيم','162','0','0');
+INSERT INTO [t7640] VALUES (7,'باب: الحاء','167','0','0');
+INSERT INTO [t7640] VALUES (8,'باب: الخاء','182','0','0');
+INSERT INTO [t7640] VALUES (9,'باب: الدال','185','0','0');
+INSERT INTO [t7640] VALUES (10,'باب: الراء','187','0','0');
+INSERT INTO [t7640] VALUES (11,'باب: الزاء','189','0','0');
+INSERT INTO [t7640] VALUES (12,'باب: السين','193','0','0');
+INSERT INTO [t7640] VALUES (13,'باب: الشين','219','0','0');
+INSERT INTO [t7640] VALUES (14,'باب: الصاد','224','0','0');
+INSERT INTO [t7640] VALUES (15,'باب: الضاد','225','0','0');
+INSERT INTO [t7640] VALUES (16,'باب: العين','226','0','0');
+INSERT INTO [t7640] VALUES (17,'باب: الفاء','264','0','0');
+INSERT INTO [t7640] VALUES (18,'باب: القاف','267','0','0');
+INSERT INTO [t7640] VALUES (19,'باب: الكاف','270','0','0');
+INSERT INTO [t7640] VALUES (20,'باب: اللام','271','0','0');
+INSERT INTO [t7640] VALUES (21,'باب: الميم','273','0','0');
+INSERT INTO [t7640] VALUES (22,'باب: النون','313','0','0');
+INSERT INTO [t7640] VALUES (23,'باب: الواو','315','0','0');
+INSERT INTO [t7640] VALUES (24,'باب: الهاء','318','0','0');
+INSERT INTO [t7640] VALUES (25,'باب: الياء','326','0','0');
+INSERT INTO [t7640] VALUES (26,'مصادر ومراجع','338','0','0');
