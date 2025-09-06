49b57256c28691a8ab70456fd37db483f93fd86b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:44 +0700

    Upload SQL chunk: t11150_chunk_0000.sql

---
+++ sql_chunks/t11150_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t11150
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t11150" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11150
+INSERT INTO [t11150] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11150] VALUES (2,'فصل في اللباس في الصلاة','11','0','0');
+INSERT INTO [t11150] VALUES (3,'فصل في ستر النساء والرجال','19','0','0');
+INSERT INTO [t11150] VALUES (4,'معاني الاستنباط من سورة النور','30','0','0');
