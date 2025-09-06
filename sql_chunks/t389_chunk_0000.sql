cb7d85a9efcfc25e3cddc569ab9a849cd092d2aa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:12 +0700

    Upload SQL chunk: t389_chunk_0000.sql

---
+++ sql_chunks/t389_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t389
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t389" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t389
+INSERT INTO [t389] VALUES (1,'من الحكمة في كتابة الأعمال','2','0','0');
+INSERT INTO [t389] VALUES (2,'الكلام على قوله تعالى: {وما جعلنا عدتهم إلا فتنة للذين كفروا}','3','0','0');
+INSERT INTO [t389] VALUES (3,'معنى الوقار في قوله تعالى: {ما لكم لا ترجون لله وقارا}','5','0','0');
