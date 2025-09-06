a7de97e7b558b5ff3b17d5d462ac9517e2dd2ed2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:20 +0700

    Upload SQL chunk: t10026_chunk_0000.sql

---
+++ sql_chunks/t10026_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t10026
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t10026" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10026
+INSERT INTO [t10026] VALUES (1,'أولا: الأسرة','1','0','0');
+INSERT INTO [t10026] VALUES (2,'ثانيا: المسجد','6','0','0');
+INSERT INTO [t10026] VALUES (3,'المسجد ودوره في محو الأمية وتعليم الكبار','10','0','0');
+INSERT INTO [t10026] VALUES (4,'ثالثا: المدرسة','12','0','0');
+INSERT INTO [t10026] VALUES (5,'رابعا: المجتمع','16','0','0');
+INSERT INTO [t10026] VALUES (6,'خامسا: الوراثة والبيئة','20','0','0');
