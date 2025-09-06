acc6dd565e2bfebc50fd53b4ded8e1609b2dc596
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:49 +0700

    Upload SQL chunk: t96848_chunk_0000.sql

---
+++ sql_chunks/t96848_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t96848
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t96848" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96848
+INSERT INTO [t96848] VALUES (1,'[مقدمة التحقيق]','2','0','0');
+INSERT INTO [t96848] VALUES (2,'عملي في المخطوط','2','1','0');
+INSERT INTO [t96848] VALUES (3,'حول المخطوط','2','1','0');
+INSERT INTO [t96848] VALUES (4,'تحقيق نسبة المخطوط للمؤلف','5','1','0');
+INSERT INTO [t96848] VALUES (5,'ترجمة المصنف','6','1','0');
+INSERT INTO [t96848] VALUES (6,'اسمه ونسبه ولقبه وكنيته','6','5','0');
+INSERT INTO [t96848] VALUES (7,'مولده ونشأته','6','5','0');
+INSERT INTO [t96848] VALUES (8,'طلبه للعلم وشيوخه','6','5','0');
+INSERT INTO [t96848] VALUES (9,'رحلاته','8','5','0');
+INSERT INTO [t96848] VALUES (10,'تصديه للتدريس والإفتاء، ونبذة عن تلاميذه','8','5','0');
+INSERT INTO [t96848] VALUES (11,'مكانته العلمية بين أبناء عصره','8','5','0');
+INSERT INTO [t96848] VALUES (12,'مؤلفاته','9','5','0');
+INSERT INTO [t96848] VALUES (13,'الوظائف التي تولاها','10','5','0');
+INSERT INTO [t96848] VALUES (14,'صفاته الخلقية','11','5','0');
+INSERT INTO [t96848] VALUES (15,'نظمه','11','5','0');
+INSERT INTO [t96848] VALUES (16,'مرضه ووفاته','11','5','0');
+INSERT INTO [t96848] VALUES (17,'النص المحقق','12','0','0');
