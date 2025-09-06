d23e5aaa607ea3797da2a790a3640499d490da33
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:08:17 +0700

    Upload SQL chunk: t12878_chunk_0000.sql

---
+++ sql_chunks/t12878_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t12878
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t12878" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12878
+INSERT INTO [t12878] VALUES (1,'شرح متن الورقات (١)','2','0','0');
+INSERT INTO [t12878] VALUES (2,'شرح متن الورقات (٢)','20','0','0');
+INSERT INTO [t12878] VALUES (3,'شرح متن الورقات (٣)','40','0','0');
+INSERT INTO [t12878] VALUES (4,'شرح متن الورقات (٤)','63','0','0');
+INSERT INTO [t12878] VALUES (5,'شرح متن الورقات (٥)','83','0','0');
+INSERT INTO [t12878] VALUES (6,'باب الأمر:','96','5','0');
+INSERT INTO [t12878] VALUES (7,'الذي يدخل في الأمر والنهي وما لا يدخل:','99','5','0');
+INSERT INTO [t12878] VALUES (8,'شرح متن الورقات (٦)','124','0','0');
+INSERT INTO [t12878] VALUES (9,'شرح متن الورقات (٧)','145','0','0');
+INSERT INTO [t12878] VALUES (10,'شرح متن الورقات (٨)','163','0','0');
+INSERT INTO [t12878] VALUES (11,'تخصيص الكتاب بالسنة:','169','10','0');
+INSERT INTO [t12878] VALUES (12,'تخصيص الكتاب بالإجماع:','171','10','0');
+INSERT INTO [t12878] VALUES (13,'تخصيص الكتاب بالقياس:','171','10','0');
+INSERT INTO [t12878] VALUES (14,'تخصيص السنة بالكتاب:','172','10','0');
+INSERT INTO [t12878] VALUES (15,'تخصيص السنة بالسنة:','172','10','0');
+INSERT INTO [t12878] VALUES (16,'تخصيص النطق بالقياس:','174','10','0');
+INSERT INTO [t12878] VALUES (17,'أسباب الإجمال:','184','10','0');
+INSERT INTO [t12878] VALUES (18,'الأفعال:','191','10','0');
+INSERT INTO [t12878] VALUES (19,'شرح متن الورقات (٩)','198','0','0');
+INSERT INTO [t12878] VALUES (20,'شرح متن الورقات (١٠)','225','0','0');
+INSERT INTO [t12878] VALUES (21,'شرح متن الورقات (١١)','249','0','0');
+INSERT INTO [t12878] VALUES (22,'شرح متن الورقات (١٢)','274','0','0');
+INSERT INTO [t12878] VALUES (23,'شرح متن الورقات (١٣)','301','0','0');
+INSERT INTO [t12878] VALUES (24,'شرح متن الورقات (١٤)','324','0','0');
+INSERT INTO [t12878] VALUES (25,'شرح متن الورقات (١٥)','348','0','0');
+INSERT INTO [t12878] VALUES (26,'شرح متن الورقات (١٦)','371','0','0');
