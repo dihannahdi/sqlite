42e2b1f02552b760462bd170dba88f9b51c4a113
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:14 +0700

    Upload SQL chunk: t5997_chunk_0000.sql

---
+++ sql_chunks/t5997_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5997
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5997" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5997
+INSERT INTO [t5997] VALUES (1,'ذكر معرفة أنواع الصحيح','7','0','0');
+INSERT INTO [t5997] VALUES (2,'ذكر أنواع الجرح والمجروحون على عشرة طبقات','24','0','0');
