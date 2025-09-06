da811279272d8f871d0d4711c38645f4311025be
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:25 +0700

    Upload SQL chunk: t21702_chunk_0000.sql

---
+++ sql_chunks/t21702_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21702
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21702" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21702
+INSERT INTO [t21702] VALUES (1,'من مجالس أبي سعيد النقاش - مخطوط','4','0','0');
