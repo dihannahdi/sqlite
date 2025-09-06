2bd087ebd988202129cbbe5f278de8a44cd65d80
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:40 +0700

    Upload SQL chunk: t7237_chunk_0000.sql

---
+++ sql_chunks/t7237_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7237
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7237" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7237
+INSERT INTO [t7237] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t7237] VALUES (2,'الكلام على المقدمة الحرية بالتقدمة','6','0','0');
