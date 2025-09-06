29a0ae59c140b22b472bfc848314801afac5f01c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:15 +0700

    Upload SQL chunk: t12446_chunk_0000.sql

---
+++ sql_chunks/t12446_chunk_0000.sql
@@ -0,0 +1,65 @@
+-- Table: t12446
+-- Chunk: 1
+-- Rows: 58
+
+CREATE TABLE "t12446" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12446
+INSERT INTO [t12446] VALUES (1,'كتاب الطهارة','11','0','0');
+INSERT INTO [t12446] VALUES (2,'كتاب الصلاة','19','0','0');
+INSERT INTO [t12446] VALUES (3,'كتاب الجنائز','29','0','0');
+INSERT INTO [t12446] VALUES (4,'كتاب الزكاة','29','0','0');
+INSERT INTO [t12446] VALUES (5,'الركاز','33','0','0');
+INSERT INTO [t12446] VALUES (6,'كتاب الصيام','34','0','0');
+INSERT INTO [t12446] VALUES (7,'باب الاعتكاف','36','0','0');
+INSERT INTO [t12446] VALUES (8,'كتاب الحج','36','0','0');
+INSERT INTO [t12446] VALUES (9,'كتاب الأقضية','44','0','0');
+INSERT INTO [t12446] VALUES (10,'بقية من الاقضية والدعوى والاقرار والقسمة والشهادات','47','0','0');
+INSERT INTO [t12446] VALUES (11,'كتاب التفليس','53','0','0');
+INSERT INTO [t12446] VALUES (12,'كتاب الحجر','53','0','0');
+INSERT INTO [t12446] VALUES (13,'كتاب الغصب','54','0','0');
+INSERT INTO [t12446] VALUES (14,'اللقطة والضالة','54','0','0');
+INSERT INTO [t12446] VALUES (15,'الآبق','54','0','0');
+INSERT INTO [t12446] VALUES (16,'المزارعة والمساقاة','55','0','0');
+INSERT INTO [t12446] VALUES (17,'الاجارات','55','0','0');
+INSERT INTO [t12446] VALUES (18,'اللقيط','55','0','0');
+INSERT INTO [t12446] VALUES (19,'الصلح','55','0','0');
+INSERT INTO [t12446] VALUES (20,'كتاب الرهن','55','0','0');
+INSERT INTO [t12446] VALUES (21,'الاكراه','56','0','0');
+INSERT INTO [t12446] VALUES (22,'الوديعة','56','0','0');
+INSERT INTO [t12446] VALUES (23,'الوكالة','56','0','0');
+INSERT INTO [t12446] VALUES (24,'الحوالة','57','0','0');
+INSERT INTO [t12446] VALUES (25,'الكفالة','57','0','0');
+INSERT INTO [t12446] VALUES (26,'كتاب النكاح','57','0','0');
+INSERT INTO [t12446] VALUES (27,'الايلاء','65','0','0');
+INSERT INTO [t12446] VALUES (28,'الطلاق والخلع','66','0','0');
+INSERT INTO [t12446] VALUES (29,'الرجعة','70','0','0');
+INSERT INTO [t12446] VALUES (30,'العدد','70','0','0');
+INSERT INTO [t12446] VALUES (31,'الاستبراء','73','0','0');
+INSERT INTO [t12446] VALUES (32,'بقية من العدد','74','0','0');
+INSERT INTO [t12446] VALUES (33,'كتاب الرضاع والنفقات والحضانة','74','0','0');
+INSERT INTO [t12446] VALUES (34,'اللعان','75','0','0');
+INSERT INTO [t12446] VALUES (35,'الظهار','76','0','0');
+INSERT INTO [t12446] VALUES (36,'اختلاف الزوجين في متاع البيت','77','0','0');
+INSERT INTO [t12446] VALUES (37,'كتاب البيوع','78','0','0');
+INSERT INTO [t12446] VALUES (38,'الشفعة','85','0','0');
+INSERT INTO [t12446] VALUES (39,'الشركة','86','0','0');
+INSERT INTO [t12446] VALUES (40,'القراض','86','0','0');
+INSERT INTO [t12446] VALUES (41,'القرض','89','0','0');
+INSERT INTO [t12446] VALUES (42,'العارية','89','0','0');
+INSERT INTO [t12446] VALUES (43,'احياء الموات','90','0','0');
+INSERT INTO [t12446] VALUES (44,'النفح','90','0','0');
+INSERT INTO [t12446] VALUES (45,'كتاب الفرائض','92','0','0');
+INSERT INTO [t12446] VALUES (46,'كتاب الوصايا والاوصياء','105','0','0');
+INSERT INTO [t12446] VALUES (47,'قسم الفيء والجهاد والسير','109','0','0');
+INSERT INTO [t12446] VALUES (48,'الامامة وحرب أهل الردة ودفع المرء عن نفسه وقطع الطريق','119','0','0');
+INSERT INTO [t12446] VALUES (49,'كتاب الحدود','124','0','0');
+INSERT INTO [t12446] VALUES (50,'الأشربة','131','0','0');
+INSERT INTO [t12446] VALUES (51,'الدماء','132','0','0');
+INSERT INTO [t12446] VALUES (52,'الديات ومن العقوبات','135','0','0');
+INSERT INTO [t12446] VALUES (53,'الصيد والضحايا والذبائح والعقيقة','140','0','0');
+INSERT INTO [t12446] VALUES (54,'السبق والرمي','151','0','0');
+INSERT INTO [t12446] VALUES (55,'الأيمان والنذور','152','0','0');
+INSERT INTO [t12446] VALUES (56,'العتق','156','0','0');
+INSERT INTO [t12446] VALUES (57,'باب','159','0','0');
+INSERT INTO [t12446] VALUES (58,'باب من الإجماع في الاعتقادات يكفر من خالفه باجماع','161','0','0');
