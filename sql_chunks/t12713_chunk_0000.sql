852d656c8dbfcef88c72e1a8d837b19aa737ce15
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:55 +0700

    Upload SQL chunk: t12713_chunk_0000.sql

---
+++ sql_chunks/t12713_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t12713
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t12713" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12713
+INSERT INTO [t12713] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t12713] VALUES (2,'١ ـ طريق عكرمة عن ابن عباس.','12','0','0');
+INSERT INTO [t12713] VALUES (3,'٢ ـ طريق سعيد بن جبير عن ابن عباس ـ رضي الله عنهم ـ','81','0','0');
+INSERT INTO [t12713] VALUES (4,'٣ـ طريق داود البصري عن ابن عباس:','102','0','0');
+INSERT INTO [t12713] VALUES (5,'٤ـ طريق (علي بن عبد الله بن عباس عن أبيه) ـ باختصار المتن ـ','120','0','0');
+INSERT INTO [t12713] VALUES (6,'من الألغاز:','178','0','0');
