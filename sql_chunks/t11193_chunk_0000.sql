e19262ddc1ec5486c1804ba1357790e0e7ea121e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:14 +0700

    Upload SQL chunk: t11193_chunk_0000.sql

---
+++ sql_chunks/t11193_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t11193
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t11193" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11193
+INSERT INTO [t11193] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t11193] VALUES (2,'الكلام من صفات الله تعالى','3','0','0');
+INSERT INTO [t11193] VALUES (3,'اختلاف الناس في حقيقة كلام الله تعالى','5','0','0');
+INSERT INTO [t11193] VALUES (4,'مصادر ومراجع','14','0','0');
