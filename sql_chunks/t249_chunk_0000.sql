984fcb84b4f0a74e1e97e57fc5471b5bd963788b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:30 +0700

    Upload SQL chunk: t249_chunk_0000.sql

---
+++ sql_chunks/t249_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t249
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t249" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t249
+INSERT INTO [t249] VALUES (1,'١ - الجمهور المستهدف من هذا البحث','3','0','0');
+INSERT INTO [t249] VALUES (2,'٢ - أهمية الدعوة إلى الله وفضلها','4','0','0');
+INSERT INTO [t249] VALUES (3,'٣ - ترك الدعوة موجب للهلاك','5','0','0');
+INSERT INTO [t249] VALUES (4,'٤ - مفهوم الحكمة في الدعوة','6','0','0');
+INSERT INTO [t249] VALUES (5,'٥ - مفهوم الموعظة الحسنة في الدعوة','7','0','0');
+INSERT INTO [t249] VALUES (6,'٦ - مفهوم المجادلة بالتي هي أحسن','8','0','0');
+INSERT INTO [t249] VALUES (7,'صورة حية من السيرة النبوية رسمت معنى المجادلة بالتي هي أحسن','9','6','0');
+INSERT INTO [t249] VALUES (8,'أهداف الحوار ومقاصده','10','6','0');
+INSERT INTO [t249] VALUES (9,'٧ - صفات ومقومات الداعية الناجح','11','0','0');
+INSERT INTO [t249] VALUES (10,'١ - أن يكون مخلصا صداقا في نفسه وفي دعوته','11','9','0');
+INSERT INTO [t249] VALUES (11,'٢ - أن يمتلك الزاد العلمي الضروري','11','9','0');
+INSERT INTO [t249] VALUES (12,'٣ - أن يتحلى برحابة الصدر والرفق بالمدعو','11','9','0');
+INSERT INTO [t249] VALUES (13,'٤ - أن يبدأ بالأهم فالأهم','11','9','0');
+INSERT INTO [t249] VALUES (14,'أهم مراجع البحث','12','0','0');
