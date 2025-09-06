c11a4920e4df26913ca6bdc29490d94656357e91
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:59:51 +0700

    Upload SQL chunk: t96851_chunk_0000.sql

---
+++ sql_chunks/t96851_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t96851
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t96851" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96851
+INSERT INTO [t96851] VALUES (1,'مقدمة التحقيق','2','0','0');
+INSERT INTO [t96851] VALUES (2,'حول الرسالة','4','1','0');
+INSERT INTO [t96851] VALUES (3,'سبب تأليفه للرسالة','5','1','0');
+INSERT INTO [t96851] VALUES (4,'تحقيق نسبتها للمؤلف','5','1','0');
+INSERT INTO [t96851] VALUES (5,'ترجمة المصنف','6','1','0');
+INSERT INTO [t96851] VALUES (6,'تأخير الظلامة إلى يوم القيامة','7','0','0');
