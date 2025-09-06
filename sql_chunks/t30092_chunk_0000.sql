850967f31e15c392b6d8efdf3982e21906b6a5ec
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:34 +0700

    Upload SQL chunk: t30092_chunk_0000.sql

---
+++ sql_chunks/t30092_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t30092
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t30092" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30092
+INSERT INTO [t30092] VALUES (1,'من فوائد أخي أبي جعفر الخلدي عن الحسن بن عبيد الله الأبزاري','2','0','0');
+INSERT INTO [t30092] VALUES (2,'من فوائد القاضي أبي الحسن محمد بن علي بن محمد بن صخر البصري','4','0','0');
+INSERT INTO [t30092] VALUES (3,'من حديث الصاحب إسماعيل بن عباد الوزير','8','0','0');
+INSERT INTO [t30092] VALUES (4,'من فوائد القاضي أبي العلاء الواسطي','12','0','0');
+INSERT INTO [t30092] VALUES (5,'من فوائد محمد بن مرزوق الزعفراني','16','0','0');
+INSERT INTO [t30092] VALUES (6,'حديث واحد عن الكياء الطبري','17','0','0');
+INSERT INTO [t30092] VALUES (7,'بيتا سعد من رواية المؤتمن الساجي','19','0','0');
+INSERT INTO [t30092] VALUES (8,'من حديث ابن رزين عن شيوخه','21','0','0');
