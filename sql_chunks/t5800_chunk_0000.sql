7a894a15e34d8d544a4e7a918dfecb571105d703
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:29 +0700

    Upload SQL chunk: t5800_chunk_0000.sql

---
+++ sql_chunks/t5800_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t5800
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t5800" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5800
+INSERT INTO [t5800] VALUES (1,'بسم الله الرحمن الرحيم رب يسر برحمتك','1','0','0');
+INSERT INTO [t5800] VALUES (2,'ذكر أبان بن أبي عياش','1','0','0');
+INSERT INTO [t5800] VALUES (3,'آخر ما أورده أبو حفص عمر بن شاهين من المختلف فيهم','75','0','0');
