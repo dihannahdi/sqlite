fe8a9568a8ba7dcfb6aeffe9a215f0e84befe512
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:31 +0700

    Upload SQL chunk: t17796_chunk_0000.sql

---
+++ sql_chunks/t17796_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t17796
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t17796" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17796
+INSERT INTO [t17796] VALUES (1,'مقدمة المعتني','3','0','0');
+INSERT INTO [t17796] VALUES (2,'وصف النسخة المعتمدة في إخراج هذه الرسالة','4','1','0');
+INSERT INTO [t17796] VALUES (3,'ترجمة المؤلف','5','1','0');
+INSERT INTO [t17796] VALUES (4,'[مقدمة المصنف]','18','0','0');
+INSERT INTO [t17796] VALUES (5,'مقدمة في حكم الجهاد في سبيل الله تعالى','20','0','0');
+INSERT INTO [t17796] VALUES (6,'الباب الأول في التحريض على الجهاد والحث عليه','22','0','0');
+INSERT INTO [t17796] VALUES (7,'الباب الثاني في الأسباب التي تقتضي امتداد أطماع الكفرة اللئام في نيل شيء من بلاد الإسلام','34','0','0');
+INSERT INTO [t17796] VALUES (8,'خاتمة في حكم الغنيمة','41','0','0');
