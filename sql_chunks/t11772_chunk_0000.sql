f83dcee15052c6acf57460cdbe493399d5f78f6f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:35 +0700

    Upload SQL chunk: t11772_chunk_0000.sql

---
+++ sql_chunks/t11772_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t11772
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t11772" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11772
+INSERT INTO [t11772] VALUES (1,'مشخصات كتاب','9','0','0');
+INSERT INTO [t11772] VALUES (2,'من نص الرحلة ص ١٠٨','9','0','0');
+INSERT INTO [t11772] VALUES (3,'استهلال','9','0','0');
+INSERT INTO [t11772] VALUES (4,'المقدمة','9','0','0');
+INSERT INTO [t11772] VALUES (5,'النسخة الوحيدة لمخطوطة رسالة ابن فضلان:','9','4','0');
+INSERT INTO [t11772] VALUES (6,'ياقوت الحموي يقود إلى ابن فضلان','10','4','0');
+INSERT INTO [t11772] VALUES (7,'ياقوت الحموي ليس الوحيد الذي يشير إلى ابن فضلان','12','4','0');
+INSERT INTO [t11772] VALUES (8,'هل زار ابن فضلان البلدان الإسكندنافية؟','13','4','0');
+INSERT INTO [t11772] VALUES (9,'أ: في الذهاب','16','4','0');
+INSERT INTO [t11772] VALUES (10,'ب: في الإياب (طريق العودة لبغداد)','17','4','0');
+INSERT INTO [t11772] VALUES (11,'من هو ابن فضلان؟','26','4','0');
+INSERT INTO [t11772] VALUES (12,'١ - علاقة العرب بالآخر:','27','4','0');
+INSERT INTO [t11772] VALUES (13,'٢ - هجرة الأيدي العاملة تتابع مراكز الثروة:','28','4','0');
+INSERT INTO [t11772] VALUES (14,'أي مسار اختط ابن فضلان؟','30','4','0');
+INSERT INTO [t11772] VALUES (15,'هجرة الأيدي العاملة تتابع مراكز الثروة:','30','4','0');
+INSERT INTO [t11772] VALUES (16,'مسار الرحلة','39','0','0');
+INSERT INTO [t11772] VALUES (17,'نص الرحلة','39','0','0');
+INSERT INTO [t11772] VALUES (18,'العجم والأتراك','39','17','0');
+INSERT INTO [t11772] VALUES (19,'الصقالبة','67','17','0');
+INSERT INTO [t11772] VALUES (20,'الروسية','90','17','0');
+INSERT INTO [t11772] VALUES (21,'الخزر','101','17','0');
+INSERT INTO [t11772] VALUES (22,'ملحق ابن فضلان أغناطيوس كراتشكوفسكي','110','0','0');
+INSERT INTO [t11772] VALUES (23,'كشاف حضاري وفهارس','110','0','0');
+INSERT INTO [t11772] VALUES (24,'الأعلام','110','23','0');
+INSERT INTO [t11772] VALUES (25,'المحتويات','131','23','0');
