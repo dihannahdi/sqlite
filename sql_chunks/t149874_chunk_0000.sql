7ab1a389f50adebf3ddc321a70bc340e3f0fea26
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:56 +0700

    Upload SQL chunk: t149874_chunk_0000.sql

---
+++ sql_chunks/t149874_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t149874
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t149874" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t149874
+INSERT INTO [t149874] VALUES (1,'الباء','2','0','0');
+INSERT INTO [t149874] VALUES (2,'الجيم','11','0','0');
+INSERT INTO [t149874] VALUES (3,'الجيم','12','0','0');
+INSERT INTO [t149874] VALUES (4,'الدال','14','0','0');
+INSERT INTO [t149874] VALUES (5,'الراء','15','0','0');
+INSERT INTO [t149874] VALUES (6,'العين','36','0','0');
+INSERT INTO [t149874] VALUES (7,'الفاء','38','0','0');
+INSERT INTO [t149874] VALUES (8,'القاف','43','0','0');
+INSERT INTO [t149874] VALUES (9,'اللام','44','0','0');
+INSERT INTO [t149874] VALUES (10,'الميم','58','0','0');
+INSERT INTO [t149874] VALUES (11,'النون','70','0','0');
+INSERT INTO [t149874] VALUES (12,'الهاء','71','0','0');
+INSERT INTO [t149874] VALUES (13,'الياء','74','0','0');
