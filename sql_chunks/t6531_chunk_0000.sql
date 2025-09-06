4ffac2612e22d2b3f9f50a3caff9131bce51f258
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:24 +0700

    Upload SQL chunk: t6531_chunk_0000.sql

---
+++ sql_chunks/t6531_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t6531
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t6531" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6531
+INSERT INTO [t6531] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6531] VALUES (2,'وبه نستعين','1','0','0');
+INSERT INTO [t6531] VALUES (3,'الفصل الأول في نقل المذاهب في هذه المسألة','16','0','0');
+INSERT INTO [t6531] VALUES (4,'الفصل الثاني في المذهب الواجب الإتباع','47','0','0');
+INSERT INTO [t6531] VALUES (5,'روى جدنا عن جبرئيل عن الباري','174','0','0');
+INSERT INTO [t6531] VALUES (6,'الفصل الثالث في إمامة علي رضي الله عنه','284','0','0');
+INSERT INTO [t6531] VALUES (7,'الفصل الرابع في إمامة باقي الإثني عشر','515','0','0');
+INSERT INTO [t6531] VALUES (8,'الفصل الخامس تخرصات الشيعة في إمامة الصديق والفاروق وذي النورين','518','0','0');
+INSERT INTO [t6531] VALUES (9,'الفصل السادس في الحجج علي إمامة أبي بكر','526','0','0');
