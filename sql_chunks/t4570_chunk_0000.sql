eaffc3140d3adcadc98d59d05638b8e3b40e13a8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:35 +0700

    Upload SQL chunk: t4570_chunk_0000.sql

---
+++ sql_chunks/t4570_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t4570
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t4570" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4570
+INSERT INTO [t4570] VALUES (1,'مملكة النبات كما يعرضها القرآن ويصفها','1','0','0');
+INSERT INTO [t4570] VALUES (2,'بقلم الدكتور/ حامد صادق قنيبي','1','0','0');
