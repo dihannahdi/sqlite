92dbd2f5f2b3fe94ec1c9dc00d374132399fe54a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:20:10 +0700

    Upload SQL chunk: t145433_chunk_0000.sql

---
+++ sql_chunks/t145433_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t145433
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t145433" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t145433
+INSERT INTO [t145433] VALUES (1,'التفسير وأصوله','13','0','0');
+INSERT INTO [t145433] VALUES (2,'الحديث','16','0','0');
+INSERT INTO [t145433] VALUES (3,'علوم الحديث','23','0','0');
+INSERT INTO [t145433] VALUES (4,'رجال الحديث والتراجم','25','0','0');
+INSERT INTO [t145433] VALUES (5,'العقيدة','28','0','0');
+INSERT INTO [t145433] VALUES (6,'الكتب الأصول المسندة','28','5','0');
+INSERT INTO [t145433] VALUES (7,'الكتب الأخرى','29','5','0');
+INSERT INTO [t145433] VALUES (8,'الملل والمذاهب','32','5','0');
+INSERT INTO [t145433] VALUES (9,'الفقه','34','0','0');
+INSERT INTO [t145433] VALUES (10,'أصول الفقه وقواعده وتاريخه','37','9','0');
+INSERT INTO [t145433] VALUES (11,'السيرة النبوية والتاريخ','40','0','0');
+INSERT INTO [t145433] VALUES (12,'المعاجم اللغوية','41','0','0');
+INSERT INTO [t145433] VALUES (13,'كتب المباني','41','12','0');
+INSERT INTO [t145433] VALUES (14,'كتب المعاني','42','12','0');
+INSERT INTO [t145433] VALUES (15,'النحو','42','0','0');
+INSERT INTO [t145433] VALUES (16,'الصرف','43','0','0');
+INSERT INTO [t145433] VALUES (17,'البلاغة والنقد','44','0','0');
+INSERT INTO [t145433] VALUES (18,'الإملاء','45','0','0');
+INSERT INTO [t145433] VALUES (19,'الأدب','45','0','0');
+INSERT INTO [t145433] VALUES (20,'مجاميع أدبية','46','0','0');
+INSERT INTO [t145433] VALUES (21,'الدواوين والمختارات الشعرية','49','0','0');
+INSERT INTO [t145433] VALUES (22,'مجاميع','51','0','0');
+INSERT INTO [t145433] VALUES (23,'الأخلاق والآداب','54','0','0');
+INSERT INTO [t145433] VALUES (24,'الاعتصام بالسنة والحذر من البدعة','55','0','0');
+INSERT INTO [t145433] VALUES (25,'الفتاوى','56','0','0');
+INSERT INTO [t145433] VALUES (26,'الخطب المنبرية','56','0','0');
+INSERT INTO [t145433] VALUES (27,'الفرائض','57','0','0');
