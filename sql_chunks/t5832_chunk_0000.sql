4c00852d88b492405e5cb50041606e346296b813
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:49 +0700

    Upload SQL chunk: t5832_chunk_0000.sql

---
+++ sql_chunks/t5832_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t5832
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t5832" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5832
+INSERT INTO [t5832] VALUES (1,'باب الهمزة','11','0','0');
+INSERT INTO [t5832] VALUES (2,'باب الباء','53','0','0');
+INSERT INTO [t5832] VALUES (3,'حرف التاء','58','0','0');
+INSERT INTO [t5832] VALUES (4,'حرف الثاء','59','0','0');
+INSERT INTO [t5832] VALUES (5,'حرف الجيم','60','0','0');
+INSERT INTO [t5832] VALUES (6,'باب الحاء المهملة','66','0','0');
+INSERT INTO [t5832] VALUES (7,'حرف الخاء','83','0','0');
+INSERT INTO [t5832] VALUES (8,'حرف الدال','90','0','0');
+INSERT INTO [t5832] VALUES (9,'حرف الراء','93','0','0');
+INSERT INTO [t5832] VALUES (10,'باب الزاي','97','0','0');
+INSERT INTO [t5832] VALUES (11,'حرف السين','101','0','0');
+INSERT INTO [t5832] VALUES (12,'حرف الشين','111','0','0');
+INSERT INTO [t5832] VALUES (13,'حرف الصاد','112','0','0');
+INSERT INTO [t5832] VALUES (14,'باب الضاد المعجمة','116','0','0');
+INSERT INTO [t5832] VALUES (15,'حرف الطاء','117','0','0');
+INSERT INTO [t5832] VALUES (16,'باب الظاء المعجمة','119','0','0');
+INSERT INTO [t5832] VALUES (17,'حرف العين','121','0','0');
+INSERT INTO [t5832] VALUES (18,'باب الغين المعجمة','185','0','0');
+INSERT INTO [t5832] VALUES (19,'حرف الفاء','186','0','0');
+INSERT INTO [t5832] VALUES (20,'حرف القاف','188','0','0');
+INSERT INTO [t5832] VALUES (21,'حرف الكاف','190','0','0');
+INSERT INTO [t5832] VALUES (22,'حرف الميم','191','0','0');
+INSERT INTO [t5832] VALUES (23,'حرف النون','244','0','0');
+INSERT INTO [t5832] VALUES (24,'حرف الهاء','248','0','0');
+INSERT INTO [t5832] VALUES (25,'حرف الواو','253','0','0');
+INSERT INTO [t5832] VALUES (26,'باب اللام ألف','255','0','0');
+INSERT INTO [t5832] VALUES (27,'باب الياء','256','0','0');
+INSERT INTO [t5832] VALUES (28,'الكنى','264','0','0');
+INSERT INTO [t5832] VALUES (29,'المبهمات','268','0','0');
+INSERT INTO [t5832] VALUES (30,'الأبناء','269','0','0');
