aac1027e5f3831153f144ba2f79a530a3da62cf6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:49 +0700

    Upload SQL chunk: t22862_chunk_0000.sql

---
+++ sql_chunks/t22862_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t22862
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t22862" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22862
+INSERT INTO [t22862] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t22862] VALUES (2,'الفصل الأول: مدخل لدراسة النبوة والنبيين','6','0','0');
+INSERT INTO [t22862] VALUES (3,'الفصل الثاني: أنبياء القرون الأولى','26','0','0');
+INSERT INTO [t22862] VALUES (4,'الفصل الثالث: أنبياء بني إسرائيل','36','0','0');
+INSERT INTO [t22862] VALUES (5,'الفصل الرابع: نبي العالمين محمد خاتم النبيين','103','0','0');
+INSERT INTO [t22862] VALUES (6,'مدخل','103','5','0');
+INSERT INTO [t22862] VALUES (7,'البشارات','105','5','0');
+INSERT INTO [t22862] VALUES (8,'الرسول في القرآن','160','5','0');
+INSERT INTO [t22862] VALUES (9,'قبس من سيرة الرسول','185','5','0');
+INSERT INTO [t22862] VALUES (10,'معجزات الرسول','195','5','0');
+INSERT INTO [t22862] VALUES (11,'محمد نبي الملكوت','262','5','0');
+INSERT INTO [t22862] VALUES (12,'الفهارس','284','0','0');
+INSERT INTO [t22862] VALUES (13,'محتويات الكتاب','284','12','0');
