5d6bb2872b37a32b712cd9ea33655e597222ca52
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:05 +0700

    Upload SQL chunk: t12724_chunk_0000.sql

---
+++ sql_chunks/t12724_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t12724
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t12724" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12724
+INSERT INTO [t12724] VALUES (1,'قضاة الحنفية','15','0','0');
+INSERT INTO [t12724] VALUES (2,'قضاة المالكية','17','0','0');
+INSERT INTO [t12724] VALUES (3,'قضاة الحنابلة','19','0','0');
+INSERT INTO [t12724] VALUES (4,'القضاة على ترتيب المعجم','20','0','0');
+INSERT INTO [t12724] VALUES (5,'حرف الألف','20','4','0');
+INSERT INTO [t12724] VALUES (6,'ذكر من اسمه إبراهيم','20','5','0');
+INSERT INTO [t12724] VALUES (7,'ذكر من اسمه أحمد','37','5','0');
+INSERT INTO [t12724] VALUES (8,'الأفراد','77','5','0');
+INSERT INTO [t12724] VALUES (9,'حرف الباء الموحدة','89','4','0');
+INSERT INTO [t12724] VALUES (10,'حرف التاء المثناة','107','4','0');
+INSERT INTO [t12724] VALUES (11,'حرف الثاء المثلثة','110','4','0');
+INSERT INTO [t12724] VALUES (12,'حرف الجيم','111','4','0');
+INSERT INTO [t12724] VALUES (13,'حرف الحاء المهملة','113','4','0');
+INSERT INTO [t12724] VALUES (14,'حرف الخاء المعجمة','148','4','0');
+INSERT INTO [t12724] VALUES (15,'حرف الدال المهملة','155','4','0');
+INSERT INTO [t12724] VALUES (16,'حرف الذال المعجمة','155','4','0');
+INSERT INTO [t12724] VALUES (17,'حرف الراء المهملة','155','4','0');
+INSERT INTO [t12724] VALUES (18,'حرف الزاي المعجمة','155','4','0');
+INSERT INTO [t12724] VALUES (19,'حرف السين المهملة','156','4','0');
+INSERT INTO [t12724] VALUES (20,'حرف الشين المعجمة','167','4','0');
+INSERT INTO [t12724] VALUES (21,'حرف الصاد المهملة','167','4','0');
+INSERT INTO [t12724] VALUES (22,'حرف الضاد المعجمة','170','4','0');
+INSERT INTO [t12724] VALUES (23,'حرف الطاء المهملة','170','4','0');
+INSERT INTO [t12724] VALUES (24,'حرف الظاء المعجمة','170','4','0');
+INSERT INTO [t12724] VALUES (25,'حرف العين المهملة','171','4','0');
+INSERT INTO [t12724] VALUES (26,'حرف الغين المعجمة','298','4','0');
+INSERT INTO [t12724] VALUES (27,'حرف الفاء','302','4','0');
+INSERT INTO [t12724] VALUES (28,'حرف القاف','305','4','0');
+INSERT INTO [t12724] VALUES (29,'حرف الكاف','307','4','0');
+INSERT INTO [t12724] VALUES (30,'حرف اللام','310','4','0');
+INSERT INTO [t12724] VALUES (31,'حرف الميم','314','4','0');
+INSERT INTO [t12724] VALUES (32,'ذكر من اسمه محمد','321','31','0');
+INSERT INTO [t12724] VALUES (33,'حرف الهاء','439','4','0');
+INSERT INTO [t12724] VALUES (34,'حرف النون','441','4','0');
+INSERT INTO [t12724] VALUES (35,'حرف الواو','458','4','0');
+INSERT INTO [t12724] VALUES (36,'حرف الياء المثناة من تحت','459','4','0');
+INSERT INTO [t12724] VALUES (37,'ذكر من ولي قضاء مصر ولم يباشر بها القضاء','477','0','0');
+INSERT INTO [t12724] VALUES (38,'باب الكنى للقضاة بالديار المصرية من غير استيعاب','478','0','0');
+INSERT INTO [t12724] VALUES (39,'باب الألقاب من غير ترتيب ولا استيعاب','480','0','0');
