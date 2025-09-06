27b4c4a7324a2719604f1265e73487054e67666f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:01 +0700

    Upload SQL chunk: t11177_chunk_0000.sql

---
+++ sql_chunks/t11177_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t11177
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t11177" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11177
+INSERT INTO [t11177] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11177] VALUES (2,'غاشية الليل','13','0','0');
+INSERT INTO [t11177] VALUES (3,'بزوغ الشمس','31','0','0');
+INSERT INTO [t11177] VALUES (4,'دعوة التوحيد','52','0','0');
+INSERT INTO [t11177] VALUES (5,'مسيرة الدعوة','71','0','0');
+INSERT INTO [t11177] VALUES (6,'بناء الدولة','91','0','0');
+INSERT INTO [t11177] VALUES (7,'ضمان الانتصار','109','0','0');
+INSERT INTO [t11177] VALUES (8,'مصادر ومراجع','126','0','0');
