b52416787d0f89e5a50da57e1c48690922e56471
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:12:15 +0700

    Upload SQL chunk: t132394_chunk_0000.sql

---
+++ sql_chunks/t132394_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t132394
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t132394" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t132394
+INSERT INTO [t132394] VALUES (1,'الإهداء','6','0','0');
+INSERT INTO [t132394] VALUES (2,'منهج البحث','7','0','0');
+INSERT INTO [t132394] VALUES (3,'خطة البحث','8','0','0');
+INSERT INTO [t132394] VALUES (4,'المقدمة','9','0','0');
+INSERT INTO [t132394] VALUES (5,'المبحث الأول: تعريف الجن','15','0','0');
+INSERT INTO [t132394] VALUES (6,'المبحث الثاني: أصل خلقهم','18','0','0');
+INSERT INTO [t132394] VALUES (7,'المبحث الثالث: صورهم وتشكلهم','21','0','0');
+INSERT INTO [t132394] VALUES (8,'المبحث الرابع: مشابهتهم','22','0','0');
+INSERT INTO [t132394] VALUES (9,'المبحث الخامس: حياتهم ومعاشهم','26','0','0');
+INSERT INTO [t132394] VALUES (10,'المبحث السادس أقسامهم','30','0','0');
+INSERT INTO [t132394] VALUES (11,'المبحث السابع: منافعهم وحرفهم','36','0','0');
+INSERT INTO [t132394] VALUES (12,'المبحث الثامن: مضارهم','39','0','0');
+INSERT INTO [t132394] VALUES (13,'المبحث التاسع: التعامل معهم','53','0','0');
+INSERT INTO [t132394] VALUES (14,'المبحث العاشر: تسلطهم على بني آدم؟','56','0','0');
+INSERT INTO [t132394] VALUES (15,'المبحث الحادي عشر أسباب مشاركتهم لبني آدم','62','0','0');
