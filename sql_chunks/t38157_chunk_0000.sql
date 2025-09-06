e40c4182a428544ee9d51910d7e43c7e0518a651
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:17:33 +0700

    Upload SQL chunk: t38157_chunk_0000.sql

---
+++ sql_chunks/t38157_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t38157
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t38157" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t38157
+INSERT INTO [t38157] VALUES (1,'ما يجب أن يعمله المفضل','5','0','0');
+INSERT INTO [t38157] VALUES (2,'فضائل الصديق مختصة','5','0','0');
+INSERT INTO [t38157] VALUES (3,'أصح حديث في فضل علي والرد على النواصب','12','0','0');
+INSERT INTO [t38157] VALUES (4,'أحب الناس إلى رسول الله - صلى الله عليه وسلم -','14','0','0');
+INSERT INTO [t38157] VALUES (5,'القول في تشبيه علي بهارون','15','0','0');
+INSERT INTO [t38157] VALUES (6,'تخصيص علي بنبذ العهود ليست من الخصائص','17','0','0');
+INSERT INTO [t38157] VALUES (7,'تشبيه بعض الصحابة بالأنبياء ليس مطلقا','18','0','0');
+INSERT INTO [t38157] VALUES (8,'معنى الموالاة في حديث "من كنت مولاه"','22','0','0');
+INSERT INTO [t38157] VALUES (9,'حديث غدير خم ليس من الخصائص','23','0','0');
+INSERT INTO [t38157] VALUES (10,'هل آية المباهلة من خصائص علي؟','25','0','0');
+INSERT INTO [t38157] VALUES (11,'معنى (الأنفس) في القرآن','26','0','0');
+INSERT INTO [t38157] VALUES (12,'آية المخاصمة ليست من الخصائص','27','0','0');
+INSERT INTO [t38157] VALUES (13,'سورة الإنسان ليست خاصة بهم','29','0','0');
