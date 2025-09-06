26fe40898f7108b965419948c79712e0418b1319
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:16 +0700

    Upload SQL chunk: t11749_chunk_0000.sql

---
+++ sql_chunks/t11749_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t11749
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t11749" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11749
+INSERT INTO [t11749] VALUES (1,'مقدمات','1','0','0');
+INSERT INTO [t11749] VALUES (2,'الاستفتاح','1','1','0');
+INSERT INTO [t11749] VALUES (3,'تمهيد ومقدمة','2','1','0');
+INSERT INTO [t11749] VALUES (4,'تطور فكرة حقوق الإنسان','5','0','0');
+INSERT INTO [t11749] VALUES (5,'المبادئ التي انبنت عليها حقوق الإنسان','11','0','0');
+INSERT INTO [t11749] VALUES (6,'حقوق الإنسان الثقافية','16','0','0');
+INSERT INTO [t11749] VALUES (7,'حقوق الإنسان الاجتماعية','22','0','0');
+INSERT INTO [t11749] VALUES (8,'مدخل','22','7','0');
+INSERT INTO [t11749] VALUES (9,'الرق','30','7','0');
+INSERT INTO [t11749] VALUES (10,'الطفولة والأبناء','32','7','0');
+INSERT INTO [t11749] VALUES (11,'حقوق الإنسان السياسية','34','0','0');
+INSERT INTO [t11749] VALUES (12,'حقوق الإنسان المدنية','40','0','0');
+INSERT INTO [t11749] VALUES (13,'مدخل','40','12','0');
+INSERT INTO [t11749] VALUES (14,'المساواة','42','12','0');
+INSERT INTO [t11749] VALUES (15,'حقوق الإنسان الاقتصادية','46','0','0');
+INSERT INTO [t11749] VALUES (16,'حقوق الإنسان بين النظرية والتطبيق','48','0','0');
+INSERT INTO [t11749] VALUES (17,'خلاصة وخاتمة','55','0','0');
+INSERT INTO [t11749] VALUES (18,'المراجع','58','0','0');
