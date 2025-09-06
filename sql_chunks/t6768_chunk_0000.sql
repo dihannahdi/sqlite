684bbfaff3182e92787625d620a102461bb68449
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:36 +0700

    Upload SQL chunk: t6768_chunk_0000.sql

---
+++ sql_chunks/t6768_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6768
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6768" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6768
+INSERT INTO [t6768] VALUES (1,'حدثنا أبو محمد الحسن','9','0','0');
+INSERT INTO [t6768] VALUES (2,'وأنشدني بعض مشايخي','50','0','0');
