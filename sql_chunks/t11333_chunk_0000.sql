936feb9b95c6ecca67a8d7598b707a4ac9122f6a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:08 +0700

    Upload SQL chunk: t11333_chunk_0000.sql

---
+++ sql_chunks/t11333_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t11333
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t11333" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11333
+INSERT INTO [t11333] VALUES (1,'مقدمة المحقق','1','0','0');
+INSERT INTO [t11333] VALUES (2,'التعريف بالمصنف','3','1','0');
+INSERT INTO [t11333] VALUES (3,'وصف النسختين الخطيتين للكتاب','5','1','0');
+INSERT INTO [t11333] VALUES (4,'عمل المحقق في الكتاب','10','1','0');
+INSERT INTO [t11333] VALUES (5,'نسبة كتاب القدر','12','1','0');
+INSERT INTO [t11333] VALUES (6,'الأحاديث والآثار في خلق آدم عليه السلام','16','0','0');
+INSERT INTO [t11333] VALUES (7,'باب ما روي أن النبي صلى الله عليه وسلم قال: "احتج آدم وموسى عليهما الصلاة والسلام"','78','0','0');
+INSERT INTO [t11333] VALUES (8,'باب ما روي في أولاد المشركين, وقول النبي صلى الله عليه وسلم: "الله أعلم بما كانوا عاملين"','109','0','0');
+INSERT INTO [t11333] VALUES (9,'باب ما روي في الأهواء وتكذيب أهل القدر','216','0','0');
