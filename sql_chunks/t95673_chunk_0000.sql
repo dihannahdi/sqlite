538b42c6748c05c1e9ac13c65ea52adaa599d959
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:01 +0700

    Upload SQL chunk: t95673_chunk_0000.sql

---
+++ sql_chunks/t95673_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t95673
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t95673" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95673
+INSERT INTO [t95673] VALUES (1,'الإهداء','1','0','0');
+INSERT INTO [t95673] VALUES (2,'المقدمة','2','0','0');
+INSERT INTO [t95673] VALUES (3,'الصبر','7','0','0');
+INSERT INTO [t95673] VALUES (4,'حقيقة الصبر','9','0','0');
+INSERT INTO [t95673] VALUES (5,'أقسام الصبر','12','0','0');
+INSERT INTO [t95673] VALUES (6,'علاج المصائب','14','0','0');
+INSERT INTO [t95673] VALUES (7,'آداب الصبر','17','0','0');
+INSERT INTO [t95673] VALUES (8,'الجزع لا يرد مصيبة','17','0','0');
+INSERT INTO [t95673] VALUES (9,'مكانة الصبر','28','0','0');
+INSERT INTO [t95673] VALUES (10,'من كمال الصبر','33','0','0');
+INSERT INTO [t95673] VALUES (11,'السعادة في طاعة الله','38','0','0');
+INSERT INTO [t95673] VALUES (12,'حسن التوفيق','45','0','0');
+INSERT INTO [t95673] VALUES (13,'التجزع من المصائب','47','0','0');
+INSERT INTO [t95673] VALUES (14,'مما يسلي أهل المصائب','61','0','0');
+INSERT INTO [t95673] VALUES (15,'العاقبة للمتقين','68','0','0');
+INSERT INTO [t95673] VALUES (16,'ثواب الشدة','71','0','0');
+INSERT INTO [t95673] VALUES (17,'ثواب الصابرين','72','0','0');
+INSERT INTO [t95673] VALUES (18,'نعمة الصبر','81','0','0');
+INSERT INTO [t95673] VALUES (19,'مراتب الناس حال المصائب','90','0','0');
+INSERT INTO [t95673] VALUES (20,'المصادر','93','0','0');
