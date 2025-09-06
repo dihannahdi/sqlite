09ca1d3b29302c8ad965f040af11c7f1540c8a2d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:13 +0700

    Upload SQL chunk: t1018_chunk_0000.sql

---
+++ sql_chunks/t1018_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t1018
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t1018" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1018
+INSERT INTO [t1018] VALUES (1,'اتجاهات الشعر العربي المعاصر','1','0','0');
+INSERT INTO [t1018] VALUES (2,'تمهيد','1','0','0');
+INSERT INTO [t1018] VALUES (3,'نظرة تاريخية موجزة','7','0','0');
