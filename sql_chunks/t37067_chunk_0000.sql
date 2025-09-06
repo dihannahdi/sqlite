adc3736218adf6f49dbaa6da531782b2905897d6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:05 +0700

    Upload SQL chunk: t37067_chunk_0000.sql

---
+++ sql_chunks/t37067_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t37067
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t37067" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37067
+INSERT INTO [t37067] VALUES (1,'باب أقسام الكلام','2','0','0');
+INSERT INTO [t37067] VALUES (2,'باب الأسماء واشتقاقها','2','0','0');
+INSERT INTO [t37067] VALUES (3,'فصل','3','2','0');
+INSERT INTO [t37067] VALUES (4,'باب موانع الصرف','3','0','0');
+INSERT INTO [t37067] VALUES (5,'(باب الإعراب والبناء)','4','0','0');
+INSERT INTO [t37067] VALUES (6,'فصل','4','5','0');
+INSERT INTO [t37067] VALUES (7,'فصل','4','5','0');
+INSERT INTO [t37067] VALUES (8,'فصل','5','5','0');
+INSERT INTO [t37067] VALUES (9,'فصل','5','5','0');
+INSERT INTO [t37067] VALUES (10,'فصل','5','5','0');
+INSERT INTO [t37067] VALUES (11,'فصل','5','5','0');
+INSERT INTO [t37067] VALUES (12,'فصل','6','5','0');
+INSERT INTO [t37067] VALUES (13,'باب البدل','6','0','0');
+INSERT INTO [t37067] VALUES (14,'فصل','7','13','0');
+INSERT INTO [t37067] VALUES (15,'فصل','7','13','0');
+INSERT INTO [t37067] VALUES (16,'فصل','7','13','0');
+INSERT INTO [t37067] VALUES (17,'فصل','7','13','0');
