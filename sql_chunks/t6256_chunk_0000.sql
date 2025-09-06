445c66b815af747ff0608f7a06930de45816c0e0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:15 +0700

    Upload SQL chunk: t6256_chunk_0000.sql

---
+++ sql_chunks/t6256_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t6256
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t6256" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6256
+INSERT INTO [t6256] VALUES (1,'الكتابة بالذهب في الإجازة','1','0','0');
+INSERT INTO [t6256] VALUES (2,'شراء زرع من غاصب','1','0','0');
+INSERT INTO [t6256] VALUES (3,'قراءة كتب الخرافات والشعوذة','2','0','0');
+INSERT INTO [t6256] VALUES (4,'تحريك النعل والاعتماد عليه في الصلاة','2','0','0');
+INSERT INTO [t6256] VALUES (5,'نسيان مسح الرأس','3','0','0');
+INSERT INTO [t6256] VALUES (6,'بقاء لمعة من غسل الجنابة','4','0','0');
+INSERT INTO [t6256] VALUES (7,'قتل الذمي الذي يسرق أولاد المسلمين','4','0','0');
+INSERT INTO [t6256] VALUES (8,'الذبح على كرتين','5','0','0');
+INSERT INTO [t6256] VALUES (9,'رد قلة الزيت عن قلة السمن','6','0','0');
+INSERT INTO [t6256] VALUES (10,'تحمل تكاليف التحصين','7','0','0');
+INSERT INTO [t6256] VALUES (11,'حق الزوجة في السكنى ببلد فيها قاض إذا ادعت مضارة زوجها بها','9','0','0');
+INSERT INTO [t6256] VALUES (12,'الطلاق المعلق تعليق السياق','10','0','0');
