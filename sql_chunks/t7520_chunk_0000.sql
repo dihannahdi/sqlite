a1de749c1bb4ad6b800dbe6c76a97aa12c569608
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:22 +0700

    Upload SQL chunk: t7520_chunk_0000.sql

---
+++ sql_chunks/t7520_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t7520
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t7520" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7520
+INSERT INTO [t7520] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t7520] VALUES (2,'ترجمة موجزة للمؤلف','5','1','0');
+INSERT INTO [t7520] VALUES (3,'التعريف بالنسخ الخطية','7','1','0');
+INSERT INTO [t7520] VALUES (4,'توثيق نسبة الرسالة إلى مؤلفها','9','1','0');
+INSERT INTO [t7520] VALUES (5,'عنوان الرسالة','11','1','0');
+INSERT INTO [t7520] VALUES (6,'منهجي في التحقيق','13','1','0');
+INSERT INTO [t7520] VALUES (7,'الإتحاف فى الرد على الصحاف','14','0','0');
