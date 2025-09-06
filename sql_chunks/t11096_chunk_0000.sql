61028ee5e5039c76f4484fa662cef50facb38b8c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:12 +0700

    Upload SQL chunk: t11096_chunk_0000.sql

---
+++ sql_chunks/t11096_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t11096
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t11096" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11096
+INSERT INTO [t11096] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11096] VALUES (2,'تمهيد','5','0','0');
+INSERT INTO [t11096] VALUES (3,'فضل الحج وعظيم ثوابه','13','0','0');
+INSERT INTO [t11096] VALUES (4,'فرض الحج وخطر التهاون عن أدائه','21','0','0');
+INSERT INTO [t11096] VALUES (5,'تنبيهات وآداب','29','0','0');
+INSERT INTO [t11096] VALUES (6,'المواقيت الزمانية والمكانية','51','0','0');
+INSERT INTO [t11096] VALUES (7,'الإحرام ومحظوراته وأعمال العمرة','58','0','0');
+INSERT INTO [t11096] VALUES (8,'أعمال الحج','66','0','0');
+INSERT INTO [t11096] VALUES (9,'يوم عرفة','73','0','0');
+INSERT INTO [t11096] VALUES (10,'أعمال يوم النحر','79','0','0');
+INSERT INTO [t11096] VALUES (11,'أيام التشريق','86','0','0');
+INSERT INTO [t11096] VALUES (12,'ختام أعمال الحج','94','0','0');
+INSERT INTO [t11096] VALUES (13,'زيارة مدينة النبي صلى الله عليه وسلم','101','0','0');
+INSERT INTO [t11096] VALUES (14,'مصادر ومراجع','109','0','0');
