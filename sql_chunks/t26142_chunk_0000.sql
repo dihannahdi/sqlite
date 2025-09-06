d429125ac6742ecb4d1481335adb1afd0baa6c76
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:34 +0700

    Upload SQL chunk: t26142_chunk_0000.sql

---
+++ sql_chunks/t26142_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t26142
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t26142" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26142
+INSERT INTO [t26142] VALUES (1,'العيد:','11','0','0');
+INSERT INTO [t26142] VALUES (2,'رحمة الله للأمة المحمدية بالعيدين','13','0','0');
+INSERT INTO [t26142] VALUES (3,'الإذن بسماع الدف من الجويريات','14','0','0');
+INSERT INTO [t26142] VALUES (4,'التجمل في العيد','17','0','0');
+INSERT INTO [t26142] VALUES (5,'الخروج إلى المصلى','20','0','0');
+INSERT INTO [t26142] VALUES (6,'الذهاب والإياب إلى المصلى','22','0','0');
+INSERT INTO [t26142] VALUES (7,'التكبير في العيدين','24','0','0');
+INSERT INTO [t26142] VALUES (8,'متى يأكل في العيدين؟','29','0','0');
+INSERT INTO [t26142] VALUES (9,'الغسل قبل العيد','32','0','0');
+INSERT INTO [t26142] VALUES (10,'هل يصلى قبل صلاة العيد أو بعدها؟','34','0','0');
+INSERT INTO [t26142] VALUES (11,'حكم صلاة العيدين','35','0','0');
+INSERT INTO [t26142] VALUES (12,'وقت صلاة العيد','38','0','0');
+INSERT INTO [t26142] VALUES (13,'لا أذان ولا إقامة للعيدين','41','0','0');
+INSERT INTO [t26142] VALUES (14,'صفة صلاة العيد','42','0','0');
+INSERT INTO [t26142] VALUES (15,'الخطبة بعد الصلاة','50','0','0');
+INSERT INTO [t26142] VALUES (16,'الخطبة والتخيير بحضورها','52','0','0');
+INSERT INTO [t26142] VALUES (17,'اجتماع الجمعة والعيد','55','0','0');
+INSERT INTO [t26142] VALUES (18,'التهنئة بالعيد','58','0','0');
+INSERT INTO [t26142] VALUES (19,'الأضحية','61','0','0');
+INSERT INTO [t26142] VALUES (20,'أحكام الأضحية','69','0','0');
+INSERT INTO [t26142] VALUES (21,'منكرات العيد','78','0','0');
+INSERT INTO [t26142] VALUES (22,'فهل هذا هو العيد؟','88','0','0');
+INSERT INTO [t26142] VALUES (23,'الخاتمة','90','0','0');
