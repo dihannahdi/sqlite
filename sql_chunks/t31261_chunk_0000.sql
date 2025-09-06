8c5055ea75ae1ad4b0ddfd7bf9b205ef66d2d6f9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:47 +0700

    Upload SQL chunk: t31261_chunk_0000.sql

---
+++ sql_chunks/t31261_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t31261
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t31261" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31261
+INSERT INTO [t31261] VALUES (1,'الرسالة الأولى كيفية صلاة النبي','1','0','0');
+INSERT INTO [t31261] VALUES (2,'الرسالة الثانية في وجوب أداء الصلاة في الجماعة','19','0','0');
