5882dbf9067c602175e9671981c1df85e16fa68a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:49 +0700

    Upload SQL chunk: t13893_chunk_0000.sql

---
+++ sql_chunks/t13893_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t13893
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t13893" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13893
+INSERT INTO [t13893] VALUES (1,'مقدمة البحث','7','0','0');
+INSERT INTO [t13893] VALUES (2,'مدخل إلى الموضوع','11','0','0');
+INSERT INTO [t13893] VALUES (3,'أولا: كتب التفسير','27','0','0');
+INSERT INTO [t13893] VALUES (4,'ثانيا: كتب إعراب القرآن','36','0','0');
+INSERT INTO [t13893] VALUES (5,'ثالثا: كتب معاني القرآن','57','0','0');
+INSERT INTO [t13893] VALUES (6,'رابعا: كتب غريب القرآن','62','0','0');
+INSERT INTO [t13893] VALUES (7,'خامسا: كتب مشكلات القرآن','70','0','0');
+INSERT INTO [t13893] VALUES (8,'سادسا: كتب متشابه القرآن','82','0','0');
+INSERT INTO [t13893] VALUES (9,'سابعا: كتب الوجوه والنظائر','89','0','0');
+INSERT INTO [t13893] VALUES (10,'ثامنا: كتب أحكام القرآن','97','0','0');
+INSERT INTO [t13893] VALUES (11,'تاسعا: كتب الناسخ والمنسوخ','103','0','0');
+INSERT INTO [t13893] VALUES (12,'عاشرا: كتب المناسبات','112','0','0');
+INSERT INTO [t13893] VALUES (13,'حادي عشر: كتب أسباب النزول','118','0','0');
+INSERT INTO [t13893] VALUES (14,'ثاني عشر: كتب توجيه القراءات','129','0','0');
+INSERT INTO [t13893] VALUES (15,'ثالث عشر: كتب الوقف والابتداء','135','0','0');
+INSERT INTO [t13893] VALUES (16,'رابع عشر: كتب مبهمات القرآن','140','0','0');
+INSERT INTO [t13893] VALUES (17,'خاتمة البحث','145','0','0');
+INSERT INTO [t13893] VALUES (18,'فهرس القواعد العلمية','147','0','0');
+INSERT INTO [t13893] VALUES (19,'فهرس معلومات الكتاب','149','0','0');
+INSERT INTO [t13893] VALUES (20,'فهرس المراجع','158','0','0');
+INSERT INTO [t13893] VALUES (21,'الفوائد','160','0','0');
