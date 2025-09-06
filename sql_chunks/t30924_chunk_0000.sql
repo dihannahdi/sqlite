308af1010a83db365570aa12361a76ed7c192918
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:48 +0700

    Upload SQL chunk: t30924_chunk_0000.sql

---
+++ sql_chunks/t30924_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t30924
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t30924" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30924
+INSERT INTO [t30924] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t30924] VALUES (2,'الركن الأول التنظيم المناسب','5','0','0');
+INSERT INTO [t30924] VALUES (3,'الركن الثاني المعلم','9','0','0');
+INSERT INTO [t30924] VALUES (4,'الركن الثالث المتعلم نصائح لطالب العلم','18','0','0');
+INSERT INTO [t30924] VALUES (5,'النصيحة الأولى الإخلاص','18','4','0');
+INSERT INTO [t30924] VALUES (6,'النصيحة الثانية إعداد العدة كالقلم والورق','20','4','0');
+INSERT INTO [t30924] VALUES (7,'النصيحة الثالثة الطالب الذي لا يستطيع حضور الدورات','22','4','0');
+INSERT INTO [t30924] VALUES (8,'النصيحة الرابعة تحضير الدرس','23','4','0');
+INSERT INTO [t30924] VALUES (9,'النصيحة الخامسة كتابة الفوائد من المعلم','25','4','0');
+INSERT INTO [t30924] VALUES (10,'النصيحة السادسة الرحمة بين الطلاب','27','4','0');
+INSERT INTO [t30924] VALUES (11,'الخاتمة','29','0','0');
+INSERT INTO [t30924] VALUES (12,'ملحق الأسئلة والإجابات','30','0','0');
+INSERT INTO [t30924] VALUES (13,'السؤال الأول','30','12','0');
+INSERT INTO [t30924] VALUES (14,'السؤال الثاني','34','12','0');
+INSERT INTO [t30924] VALUES (15,'السؤال الثالث','35','12','0');
+INSERT INTO [t30924] VALUES (16,'السؤال الرابع','37','12','0');
+INSERT INTO [t30924] VALUES (17,'السؤال الخامس','39','12','0');
+INSERT INTO [t30924] VALUES (18,'السؤال السادس','41','12','0');
