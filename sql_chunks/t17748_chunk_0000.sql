ba0ae68ea8bb2086fc1d112236f06e398bfb1df8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:19 +0700

    Upload SQL chunk: t17748_chunk_0000.sql

---
+++ sql_chunks/t17748_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t17748
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t17748" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17748
+INSERT INTO [t17748] VALUES (1,'مقدمة المصنف','3','0','0');
+INSERT INTO [t17748] VALUES (2,'ذكر الأحاديث الواردة في الحب في الله','3','0','0');
+INSERT INTO [t17748] VALUES (3,'ذكر الأحاديث الواردة في النهي عن التقاطع والتهاجر','8','0','0');
+INSERT INTO [t17748] VALUES (4,'كلام الخطابي في الكذب المباح','11','0','0');
+INSERT INTO [t17748] VALUES (5,'آخر الرسالة','11','0','0');
