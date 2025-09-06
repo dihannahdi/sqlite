2501ac37a8976f75787937996927a08f6a9fddaf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:06 +0700

    Upload SQL chunk: t11331_chunk_0000.sql

---
+++ sql_chunks/t11331_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t11331
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t11331" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11331
+INSERT INTO [t11331] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t11331] VALUES (2,'تقريظ فضيلة الشيخ حماد بن الأنصاري','1','1','0');
+INSERT INTO [t11331] VALUES (3,'مقدمة الكتاب','4','1','0');
+INSERT INTO [t11331] VALUES (4,'ترجمة المؤلف','6','1','0');
+INSERT INTO [t11331] VALUES (5,'نسبة كتاب الصفات','11','1','0');
+INSERT INTO [t11331] VALUES (6,'عملي في الكتاب','14','1','0');
+INSERT INTO [t11331] VALUES (7,'أحاديث صفة القدم','15','0','0');
+INSERT INTO [t11331] VALUES (8,'أحاديث صفة اليد والإصبع','26','0','0');
+INSERT INTO [t11331] VALUES (9,'أحاديث صفة الضحك والكرسي موضع القدمين','37','0','0');
+INSERT INTO [t11331] VALUES (10,'إعادة أحاديث صفات الإصبع','42','0','0');
+INSERT INTO [t11331] VALUES (11,'أحاديث صفة الوجه وبحث لابن خزيمة في توضيح معنى حديث إذا ضرب أحدكم فليجتنب الوجه','48','0','0');
+INSERT INTO [t11331] VALUES (12,'أقوال الأئمة في أحاديث الصفات وردهم على من أنكرها','60','0','0');
+INSERT INTO [t11331] VALUES (13,'الفهارس','70','0','0');
+INSERT INTO [t11331] VALUES (14,'فهرس المراجع','70','13','0');
+INSERT INTO [t11331] VALUES (15,'فهرس أحاديث كتاب الصفات','73','13','0');
+INSERT INTO [t11331] VALUES (16,'فهرس الأعلام','76','13','0');
+INSERT INTO [t11331] VALUES (17,'فهرس الموضوعات','81','13','0');
