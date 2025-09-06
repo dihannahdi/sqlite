1fda705c10aafd3ec2b27b6510bba4102e7cf00b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:08:44 +0700

    Upload SQL chunk: t12987_chunk_0000.sql

---
+++ sql_chunks/t12987_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t12987
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t12987" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12987
+INSERT INTO [t12987] VALUES (1,'باب الوضوء','1','0','0');
+INSERT INTO [t12987] VALUES (2,'باب الغسل من الجنابة','51','0','0');
+INSERT INTO [t12987] VALUES (3,'باب المسح على الخفين','63','0','0');
+INSERT INTO [t12987] VALUES (4,'باب التيمم','76','0','0');
+INSERT INTO [t12987] VALUES (5,'باب الأذان','85','0','0');
+INSERT INTO [t12987] VALUES (6,'باب افتتاح الصلاة','102','0','0');
+INSERT INTO [t12987] VALUES (7,'باب السهو','184','0','0');
+INSERT INTO [t12987] VALUES (8,'باب صلاة العيدين','291','0','0');
+INSERT INTO [t12987] VALUES (9,'في الأضحى','310','0','0');
+INSERT INTO [t12987] VALUES (10,'باب صلاة الخوف','379','0','0');
+INSERT INTO [t12987] VALUES (11,'في غسل الميت وكفنه','383','0','0');
+INSERT INTO [t12987] VALUES (12,'باب الزكاة','428','0','0');
+INSERT INTO [t12987] VALUES (13,'باب المناسك والحج','458','0','0');
+INSERT INTO [t12987] VALUES (14,'باب لبس المحرم وطيبه','467','0','0');
+INSERT INTO [t12987] VALUES (15,'باب القران، وما يجب عليه من الطواف والسعي','484','0','0');
+INSERT INTO [t12987] VALUES (16,'باب التمتع','495','0','0');
+INSERT INTO [t12987] VALUES (17,'باب المحصر','505','0','0');
+INSERT INTO [t12987] VALUES (18,'باب الصيد','509','0','0');
+INSERT INTO [t12987] VALUES (19,'أبواب الطلاق','598','0','0');
+INSERT INTO [t12987] VALUES (20,'باب الخيار','641','0','0');
+INSERT INTO [t12987] VALUES (21,'باب العدة','653','0','0');
+INSERT INTO [t12987] VALUES (22,'باب الإيلاء','684','0','0');
+INSERT INTO [t12987] VALUES (23,'باب الظهار','699','0','0');
+INSERT INTO [t12987] VALUES (24,'باب المتعة','711','0','0');
+INSERT INTO [t12987] VALUES (25,'باب اللعان','716','0','0');
+INSERT INTO [t12987] VALUES (26,'باب في العزل','725','0','0');
+INSERT INTO [t12987] VALUES (27,'باب القضاء','729','0','0');
+INSERT INTO [t12987] VALUES (28,'في الفرائض','788','0','0');
+INSERT INTO [t12987] VALUES (29,'في الوصايا','801','0','0');
+INSERT INTO [t12987] VALUES (30,'في الصيام','811','0','0');
+INSERT INTO [t12987] VALUES (31,'في البيوع والسلف','848','0','0');
+INSERT INTO [t12987] VALUES (32,'في المزارعة','877','0','0');
+INSERT INTO [t12987] VALUES (33,'في المكاتب والمدبر وأم الولد','882','0','0');
+INSERT INTO [t12987] VALUES (34,'باب الغزو والجيش','897','0','0');
+INSERT INTO [t12987] VALUES (35,'باب الديات','987','0','0');
+INSERT INTO [t12987] VALUES (36,'باب الأشربة','1018','0','0');
+INSERT INTO [t12987] VALUES (37,'باب في لبس الحرير والذهب','1041','0','0');
+INSERT INTO [t12987] VALUES (38,'في الخضاب والأخذ من اللحية والشارب','1060','0','0');
+INSERT INTO [t12987] VALUES (39,'في الذبائح والجبن','1087','0','0');
