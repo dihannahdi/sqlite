04595031def363478200c3708d684f79b4908e7b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:36 +0700

    Upload SQL chunk: t22808_chunk_0000.sql

---
+++ sql_chunks/t22808_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t22808
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t22808" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22808
+INSERT INTO [t22808] VALUES (1,'ملخص البحث','2','0','0');
+INSERT INTO [t22808] VALUES (2,'الفصل الأول: الدراسة','5','0','0');
+INSERT INTO [t22808] VALUES (3,'اسمه ونسبه','5','2','0');
+INSERT INTO [t22808] VALUES (4,'ولادته، ونشأته وأسرته','6','2','0');
+INSERT INTO [t22808] VALUES (5,'صفاته، وشجاعته وكرمه','7','2','0');
+INSERT INTO [t22808] VALUES (6,'تعبده وزهده وورعه','8','2','0');
+INSERT INTO [t22808] VALUES (7,'شيوخه','9','2','0');
+INSERT INTO [t22808] VALUES (8,'تلاميذه','9','2','0');
+INSERT INTO [t22808] VALUES (9,'مؤلفاته','9','2','0');
+INSERT INTO [t22808] VALUES (10,'وفاته','11','2','0');
+INSERT INTO [t22808] VALUES (11,'وصف النسخة الخطية','11','2','0');
+INSERT INTO [t22808] VALUES (12,'اسم الكتاب','11','11','0');
+INSERT INTO [t22808] VALUES (13,'نسبة الكتاب للمؤلف','12','11','0');
+INSERT INTO [t22808] VALUES (14,'عدد أوراقها ومسطرتها','12','11','0');
+INSERT INTO [t22808] VALUES (15,'منهج ابن تيمية - رحمه الله -','13','11','0');
+INSERT INTO [t22808] VALUES (16,'النص المحقق','13','0','0');
+INSERT INTO [t22808] VALUES (17,'سبب تسمية الرافضة','24','16','0');
+INSERT INTO [t22808] VALUES (18,'الخاتمة','25','0','0');
+INSERT INTO [t22808] VALUES (19,'فائدة','25','0','0');
+INSERT INTO [t22808] VALUES (20,'الهوامش والتعليقات','28','0','0');
+INSERT INTO [t22808] VALUES (21,'المصادر والمراجع','29','0','0');
