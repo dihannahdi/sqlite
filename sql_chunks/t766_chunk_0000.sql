ec36c9e78b4da97bd462a63c5130c0ec8648f50a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:41:22 +0700

    Upload SQL chunk: t766_chunk_0000.sql

---
+++ sql_chunks/t766_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t766
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t766" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t766
+INSERT INTO [t766] VALUES (1,'أولا التكفير حكم شرعي مرده إلى الله ورسوله','1','0','0');
+INSERT INTO [t766] VALUES (2,'ثانيا استباحة الدماء وانتهاك الأعراض وأمثالها من الأعمال محرمة','6','0','0');
+INSERT INTO [t766] VALUES (3,'ثالثا الإسلام بريء من هذا المعتقد الخاطئ','8','0','0');
+INSERT INTO [t766] VALUES (4,'فتوى الشيخ عبد العزيز بن باز رحمه الله في حادث التفجير بالرياض','11','0','0');
