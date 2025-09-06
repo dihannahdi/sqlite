f8951125eebbe57f8198c58bc3ee5127d1efe589
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:47 +0700

    Upload SQL chunk: t467_chunk_0000.sql

---
+++ sql_chunks/t467_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t467
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t467" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t467
+INSERT INTO [t467] VALUES (1,'الذي يظهر أنها إقرار بالاشتراك','2','0','0');
+INSERT INTO [t467] VALUES (2,'ما يفهم منه مناقضة الإقرار ليس صريحا في ذلك','2','0','0');
+INSERT INTO [t467] VALUES (3,'النظر في عباراته وذكر النقول عن كتب الفقه في هذا الباب','3','0','0');
