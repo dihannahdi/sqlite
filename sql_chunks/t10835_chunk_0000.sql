959e4128def0bb883db6bbef25749f58d818eb22
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:48 +0700

    Upload SQL chunk: t10835_chunk_0000.sql

---
+++ sql_chunks/t10835_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10835
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10835" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10835
+INSERT INTO [t10835] VALUES (1,'كتاب الخلعيات لأبي الحسن الخلعي','2','0','0');
