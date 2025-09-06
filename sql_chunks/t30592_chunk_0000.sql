77cf812a7683b2803f0c342a69994d32498a5e03
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:17 +0700

    Upload SQL chunk: t30592_chunk_0000.sql

---
+++ sql_chunks/t30592_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30592
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30592" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30592
+INSERT INTO [t30592] VALUES (1,'الجزء الأول من الفوائد المنتقاة','1','0','0');
