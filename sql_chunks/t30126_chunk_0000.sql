29d60e294b20d24ab02edaf9049b99276abdae21
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:52 +0700

    Upload SQL chunk: t30126_chunk_0000.sql

---
+++ sql_chunks/t30126_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t30126
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t30126" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30126
+INSERT INTO [t30126] VALUES (1,'الجزء السادس والعشرون من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30126] VALUES (2,'من حديث محمد بن عبد الله بن سليمان الحضرمي مطين','3','0','0');
+INSERT INTO [t30126] VALUES (3,'من فوائد أبي حاتم اللبان الرازي','16','0','0');
+INSERT INTO [t30126] VALUES (4,'من فوائد هناد النسفي','19','0','0');
+INSERT INTO [t30126] VALUES (5,'من حديث أبي العباس بن قريش','25','0','0');
+INSERT INTO [t30126] VALUES (6,'من حديث ابن مالك القطيعي','40','0','0');
+INSERT INTO [t30126] VALUES (7,'من فوائد هناد','44','0','0');
+INSERT INTO [t30126] VALUES (8,'من فوائد ابن أبي سعد السبط','47','0','0');
+INSERT INTO [t30126] VALUES (9,'وحكاية واحدة','64','0','0');
+INSERT INTO [t30126] VALUES (10,'حديث واحد','65','0','0');
+INSERT INTO [t30126] VALUES (11,'من حديث ابن المخبزي عن القاضي أبي الطيب الطبري وعن والده','67','0','0');
+INSERT INTO [t30126] VALUES (12,'حديث واحد عن ابن الدامغاني','71','0','0');
+INSERT INTO [t30126] VALUES (13,'حديث آخر عنه','73','0','0');
+INSERT INTO [t30126] VALUES (14,'من فوائد أبي عمر بن حيويه','75','0','0');
