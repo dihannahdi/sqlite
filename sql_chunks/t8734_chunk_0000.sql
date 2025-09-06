f61ba358b1c6a102c1181774797d9cc70e5931bc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:51 +0700

    Upload SQL chunk: t8734_chunk_0000.sql

---
+++ sql_chunks/t8734_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t8734
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t8734" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8734
+INSERT INTO [t8734] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8734] VALUES (2,'تمهيد','9','0','0');
+INSERT INTO [t8734] VALUES (3,'الدور الأول','13','0','0');
+INSERT INTO [t8734] VALUES (4,'التمهيد للدور الأول','13','3','0');
+INSERT INTO [t8734] VALUES (5,'فتور الدعوة لعدم القيادة','15','3','0');
+INSERT INTO [t8734] VALUES (6,'الدو رالثاني "دور انتسشار الإسلام "','17','3','0');
+INSERT INTO [t8734] VALUES (7,'الدعاة في هذا الدور','18','3','0');
+INSERT INTO [t8734] VALUES (8,'التحقيق','21','3','0');
+INSERT INTO [t8734] VALUES (9,'أهداف مشايخ الصوفية في دعوتهم','23','3','0');
+INSERT INTO [t8734] VALUES (10,'تمكين التصوف لجراثيم الشيوعية','28','3','0');
+INSERT INTO [t8734] VALUES (11,'الدور الثالث والآخير "دور التصحيح"','31','3','0');
+INSERT INTO [t8734] VALUES (12,'دعاة محليون','33','3','0');
+INSERT INTO [t8734] VALUES (13,'شروط الدعوة','40','3','0');
+INSERT INTO [t8734] VALUES (14,'سير الدعوة في القارة الأفريقية','41','3','0');
+INSERT INTO [t8734] VALUES (15,'دور الجامعيين الأفارقة','46','3','0');
