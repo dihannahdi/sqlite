b527e5781ef8e424c8a044599000777bf1876f4b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:02 +0700

    Upload SQL chunk: t95674_chunk_0000.sql

---
+++ sql_chunks/t95674_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t95674
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t95674" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95674
+INSERT INTO [t95674] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95674] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t95674] VALUES (3,'وقفة مع الدعوة','6','0','0');
+INSERT INTO [t95674] VALUES (4,'لماذا نتطرق لهذا الموضوع؟','9','0','0');
+INSERT INTO [t95674] VALUES (5,'محاور رئيسة في الدعوة إلى الله -عز وجل-','12','0','0');
+INSERT INTO [t95674] VALUES (6,'الرفق','15','0','0');
+INSERT INTO [t95674] VALUES (7,'طريق تبليغ الدعوة','18','0','0');
+INSERT INTO [t95674] VALUES (8,'البلاغ','21','0','0');
+INSERT INTO [t95674] VALUES (9,'متى أدعوا؟','22','0','0');
+INSERT INTO [t95674] VALUES (10,'بماذا أدعو؟','24','0','0');
+INSERT INTO [t95674] VALUES (11,'من الوسائل المعينة على الدعوة','27','0','0');
+INSERT INTO [t95674] VALUES (12,'من الوسائل المعينة على الدعوة','30','0','0');
+INSERT INTO [t95674] VALUES (13,'من الوسائل الهامة التي غفل عنها بعض الناس','32','0','0');
+INSERT INTO [t95674] VALUES (14,'من وسائل الدعوة','34','0','0');
+INSERT INTO [t95674] VALUES (15,'عوائق في وجه الدعوة','35','0','0');
+INSERT INTO [t95674] VALUES (16,'عوائق في وجه الدعوة','36','0','0');
+INSERT INTO [t95674] VALUES (17,'من العقبات المتكررة','55','0','0');
+INSERT INTO [t95674] VALUES (18,'أشد العقبات','58','0','0');
+INSERT INTO [t95674] VALUES (19,'من المعوقات ... !','60','0','0');
+INSERT INTO [t95674] VALUES (20,'كيف أدعو إلى الله؟','62','0','0');
+INSERT INTO [t95674] VALUES (21,'وقفات مهمة','65','0','0');
