2c704a10c19f2ba3034f2f88595e4a79df4c7aea
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:36 +0700

    Upload SQL chunk: t5709_chunk_0000.sql

---
+++ sql_chunks/t5709_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t5709
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t5709" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5709
+INSERT INTO [t5709] VALUES (1,'تم الجزء','20','0','0');
