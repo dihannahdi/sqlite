36be856de38203292c109a0b020efacc4a585997
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:44 +0700

    Upload SQL chunk: t17806_chunk_0000.sql

---
+++ sql_chunks/t17806_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t17806
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t17806" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17806
+INSERT INTO [t17806] VALUES (1,'مقدمة المعتني','3','0','0');
+INSERT INTO [t17806] VALUES (2,'ترجمة المؤلف','5','1','0');
+INSERT INTO [t17806] VALUES (3,'مقدمة [المصنف]','11','0','0');
+INSERT INTO [t17806] VALUES (4,'الباب الأول فيمن أظهرها','18','0','0');
+INSERT INTO [t17806] VALUES (5,'الباب الثاني في فضلها','22','0','0');
+INSERT INTO [t17806] VALUES (6,'الباب الثالث في بيان أفضل أنواع الماء مطلقا وبيان الترتيب في الفضيلة بينها','30','0','0');
+INSERT INTO [t17806] VALUES (7,'خاتمة','34','0','0');
+INSERT INTO [t17806] VALUES (8,'المصادر والمراجع','39','0','0');
