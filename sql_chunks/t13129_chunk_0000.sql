7878500f627b4c3a82359bdd3c03625dbbe5cb8a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:10:57 +0700

    Upload SQL chunk: t13129_chunk_0000.sql

---
+++ sql_chunks/t13129_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t13129
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t13129" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13129
+INSERT INTO [t13129] VALUES (1,'الأربعين في الأحاديث النبوية للرسام أحمد بن أبي بكر الحموي','2','0','0');
