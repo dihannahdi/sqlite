8d33ca64b341cd813478a9a80cce08baf05751c5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:11 +0700

    Upload SQL chunk: t17869_chunk_0000.sql

---
+++ sql_chunks/t17869_chunk_0000.sql
@@ -0,0 +1,50 @@
+-- Table: t17869
+-- Chunk: 1
+-- Rows: 43
+
+CREATE TABLE "t17869" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17869
+INSERT INTO [t17869] VALUES (1,'[مقدمة التحقيق]','3','0','0');
+INSERT INTO [t17869] VALUES (2,'بين يدى الكتاب','4','1','0');
+INSERT INTO [t17869] VALUES (3,'ترجمة المصنف','8','1','0');
+INSERT INTO [t17869] VALUES (4,'مراجع الترجمة','8','1','0');
+INSERT INTO [t17869] VALUES (5,'عملي في الكتاب','9','1','0');
+INSERT INTO [t17869] VALUES (6,'وصف المخطوطة','10','1','0');
+INSERT INTO [t17869] VALUES (7,'تعريف المروءة','11','0','0');
+INSERT INTO [t17869] VALUES (8,'في ذى الوجهين','12','0','0');
+INSERT INTO [t17869] VALUES (9,'في الصدود','12','0','0');
+INSERT INTO [t17869] VALUES (10,'في كثرة المال وقلته','13','0','0');
+INSERT INTO [t17869] VALUES (11,'قوله في الحمق','13','0','0');
+INSERT INTO [t17869] VALUES (12,'بيان أثر الهوى على الإنسان وفضل العقل','14','0','0');
+INSERT INTO [t17869] VALUES (13,'في الوجوه الشفافة','15','0','0');
+INSERT INTO [t17869] VALUES (14,'الإعداد لكل أمر بعدته','15','0','0');
+INSERT INTO [t17869] VALUES (15,'قال في العتاب','16','0','0');
+INSERT INTO [t17869] VALUES (16,'حقوق الأخوة','17','0','0');
+INSERT INTO [t17869] VALUES (17,'في مضيف الطعام','18','0','0');
+INSERT INTO [t17869] VALUES (18,'في صفة العدو والصديق','19','0','0');
+INSERT INTO [t17869] VALUES (19,'في العقل والأدب','20','0','0');
+INSERT INTO [t17869] VALUES (20,'في المراء','21','0','0');
+INSERT INTO [t17869] VALUES (21,'في الحكمة','21','0','0');
+INSERT INTO [t17869] VALUES (22,'في الكلام وإخراجه','22','0','0');
+INSERT INTO [t17869] VALUES (23,'في طلاقة الوجه وحسن الخلق','23','0','0');
+INSERT INTO [t17869] VALUES (24,'ذم الكذب','24','0','0');
+INSERT INTO [t17869] VALUES (25,'في الإخوان','24','0','0');
+INSERT INTO [t17869] VALUES (26,'كيفية اختيار الصديق الصدوق','25','0','0');
+INSERT INTO [t17869] VALUES (27,'في الجهل','25','0','0');
+INSERT INTO [t17869] VALUES (28,'في رؤية الرجل وخبره','26','0','0');
+INSERT INTO [t17869] VALUES (29,'في النهى عن القبيح','27','0','0');
+INSERT INTO [t17869] VALUES (30,'في المؤاخاة','28','0','0');
+INSERT INTO [t17869] VALUES (31,'في المداراة','29','0','0');
+INSERT INTO [t17869] VALUES (32,'في الإعسار والإيسار','30','0','0');
+INSERT INTO [t17869] VALUES (33,'في الصلة والتفضل','30','0','0');
+INSERT INTO [t17869] VALUES (34,'أحوال القدر','31','0','0');
+INSERT INTO [t17869] VALUES (35,'في الفضائل','31','0','0');
+INSERT INTO [t17869] VALUES (36,'أخلاق الأتقياء والأشقياء','32','0','0');
+INSERT INTO [t17869] VALUES (37,'طبيعة بني آدم','33','0','0');
+INSERT INTO [t17869] VALUES (38,'خير الاستشارة','33','0','0');
+INSERT INTO [t17869] VALUES (39,'مجالسة أهل الأهواء وجدالهم','34','0','0');
+INSERT INTO [t17869] VALUES (40,'النميمة وأثرها','35','0','0');
+INSERT INTO [t17869] VALUES (41,'من نواقض المروءة كثرة الكلام','36','0','0');
+INSERT INTO [t17869] VALUES (42,'في النفس وآدابها','36','0','0');
+INSERT INTO [t17869] VALUES (43,'في الحاسد','36','0','0');
