4d4e4711b8d97b72b93da2febdf9e6c2322add2c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:03:51 +0700

    Upload SQL chunk: t12270_chunk_0000.sql

---
+++ sql_chunks/t12270_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t12270
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t12270" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12270
+INSERT INTO [t12270] VALUES (1,'المسألة الأولى: حكم القصر والإتمام في السفر','1','0','0');
+INSERT INTO [t12270] VALUES (2,'المسألة الثانية: مسافة القصر','12','0','0');
+INSERT INTO [t12270] VALUES (3,'المسألة الثالثة: جمع تأخير دون تقديم','16','0','0');
