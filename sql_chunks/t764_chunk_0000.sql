007150979edce26cfea87ffaafc3ac141eda1f90
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:41:07 +0700

    Upload SQL chunk: t764_chunk_0000.sql

---
+++ sql_chunks/t764_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t764
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t764" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t764
+INSERT INTO [t764] VALUES (1,'نص الكتاب','1','0','0');
+INSERT INTO [t764] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
