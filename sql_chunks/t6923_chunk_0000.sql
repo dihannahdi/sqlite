47372bb773e3577439bbdf19c7acc0e551d03eac
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:49 +0700

    Upload SQL chunk: t6923_chunk_0000.sql

---
+++ sql_chunks/t6923_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6923
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6923" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6923
+INSERT INTO [t6923] VALUES (1,'مقدمة المصنف','1','0','0');
+INSERT INTO [t6923] VALUES (2,'تم والحمد لله وحده','56','0','0');
