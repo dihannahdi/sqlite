989b0874f8dd37d6946b50b21fee13c06641a71f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:55 +0700

    Upload SQL chunk: t2914_chunk_0000.sql

---
+++ sql_chunks/t2914_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t2914
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t2914" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2914
+INSERT INTO [t2914] VALUES (1,'التعريف بالقرآن الكريم','1','0','0');
+INSERT INTO [t2914] VALUES (2,'فضله، مكانته، وكونه المصدر الأول للتشريع الإسلامي','1','0','0');
+INSERT INTO [t2914] VALUES (3,'لا بد من قراءة القرآن، قراءة متدبرة واعية','2','0','0');
+INSERT INTO [t2914] VALUES (4,'حقا، إن القرآن الكريم بألفاظه ومعانيه كلام الله','3','0','0');
+INSERT INTO [t2914] VALUES (5,'والقرآن الكريم هو المصدر الأول للتشريع','4','0','0');
