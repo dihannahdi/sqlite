051c61218e283fa90ce895a96eb2d02616fc0533
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:28 +0700

    Upload SQL chunk: t26986_chunk_0000.sql

---
+++ sql_chunks/t26986_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t26986
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t26986" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26986
+INSERT INTO [t26986] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26986] VALUES (2,'الجزء فيه من الفوائد والأخبار عن أبي بكر بن دري','2','0','0');
+INSERT INTO [t26986] VALUES (3,'الأخبار الملحقة بكتاب الفوائد والأخبار','41','0','0');
