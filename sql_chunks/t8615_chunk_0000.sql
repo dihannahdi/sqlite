a8782508d9a4c5fb6ef3bc07bd7c870af3cb2df1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:49 +0700

    Upload SQL chunk: t8615_chunk_0000.sql

---
+++ sql_chunks/t8615_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t8615
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t8615" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8615
+INSERT INTO [t8615] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8615] VALUES (2,'أجزاء الحياة الانسانية','2','0','0');
+INSERT INTO [t8615] VALUES (3,'الوجهة الفكرية','4','0','0');
+INSERT INTO [t8615] VALUES (4,'منهاج التفكر','6','0','0');
+INSERT INTO [t8615] VALUES (5,'مسألة مهمة','8','0','0');
+INSERT INTO [t8615] VALUES (6,'الفكرة السنية','10','0','0');
+INSERT INTO [t8615] VALUES (7,'الفكرة السنية الطبيعية','14','0','0');
+INSERT INTO [t8615] VALUES (8,'الفكرة السنية النفسية','19','0','0');
+INSERT INTO [t8615] VALUES (9,'الأصول المتبعة','21','0','0');
+INSERT INTO [t8615] VALUES (10,'الأمثلة','25','0','0');
+INSERT INTO [t8615] VALUES (11,'شهادة التاريخ','27','0','0');
