c3327999f7771482c9bcd24672fc245fb30f6cd1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:34 +0700

    Upload SQL chunk: t11023_chunk_0000.sql

---
+++ sql_chunks/t11023_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t11023
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t11023" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11023
+INSERT INTO [t11023] VALUES (1,'بسم الله الرحمن الرحيم وبه التوفيق والهداية','1','0','0');
+INSERT INTO [t11023] VALUES (2,'باب ذكر مواسم العمر','2','0','0');
+INSERT INTO [t11023] VALUES (3,'الباب الأول في ذكر الموسم الأول','3','0','0');
+INSERT INTO [t11023] VALUES (4,'فصل','5','3','0');
+INSERT INTO [t11023] VALUES (5,'فصل','5','3','0');
+INSERT INTO [t11023] VALUES (6,'فصل','5','3','0');
+INSERT INTO [t11023] VALUES (7,'الباب الثاني في ذكر الموسم الثاني','9','0','0');
+INSERT INTO [t11023] VALUES (8,'فصل','11','7','0');
+INSERT INTO [t11023] VALUES (9,'فلينظر الشاب في أي مقام هو، فليس لمقامه مثل، وليتلمح شرف بضاعته وثمنها','13','7','0');
+INSERT INTO [t11023] VALUES (10,'الباب الثالث في الموسم الثالث وهو حال الكهولة','15','0','0');
+INSERT INTO [t11023] VALUES (11,'الباب الرابع في الموسم الرابع وهو الشيخوخة','17','0','0');
+INSERT INTO [t11023] VALUES (12,'الباب الخامس في الموسم الخامس وهو حال الهرم','22','0','0');
