953765ff6a76791e38a3065ad1dd89f481f17d10
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:31:25 +0700

    Upload SQL chunk: t17774_chunk_0000.sql

---
+++ sql_chunks/t17774_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t17774
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t17774" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17774
+INSERT INTO [t17774] VALUES (1,'الأصل الأول','13','0','0');
+INSERT INTO [t17774] VALUES (2,'الأصل الثاني','22','0','0');
+INSERT INTO [t17774] VALUES (3,'الأصل الأول: الإيمان بالله','27','2','0');
+INSERT INTO [t17774] VALUES (4,'الأصل الثاني: الإيمان بالملائكة','27','2','0');
+INSERT INTO [t17774] VALUES (5,'الأصل الثالث: الإيمان بالكتب','28','2','0');
+INSERT INTO [t17774] VALUES (6,'الأصل الرابع: الإيمان بالرسل','28','2','0');
+INSERT INTO [t17774] VALUES (7,'الأصل الخامس: الإيمان باليوم الآخر','28','2','0');
+INSERT INTO [t17774] VALUES (8,'الأصل السادس: الإيمان بالقدر','28','2','0');
+INSERT INTO [t17774] VALUES (9,'الأصل الثالث','34','0','0');
+INSERT INTO [t17774] VALUES (10,'الإيمان بالبعث بعد الموت','40','0','0');
+INSERT INTO [t17774] VALUES (11,'الإيمان بالرسل','43','0','0');
+INSERT INTO [t17774] VALUES (12,'الكفر بالطاغوت','45','0','0');
