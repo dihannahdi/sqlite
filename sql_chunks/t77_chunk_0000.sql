d82a893d4bc8f7617081e8cd2c27ed999e1eeeef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:10 +0700

    Upload SQL chunk: t77_chunk_0000.sql

---
+++ sql_chunks/t77_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t77
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t77" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t77
+INSERT INTO [t77] VALUES (1,'العبرة الأولى','3','0','0');
+INSERT INTO [t77] VALUES (2,'أما الدرس الثاني','5','0','0');
