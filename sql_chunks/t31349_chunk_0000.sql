717026752b07a663ec0903351200b5157a9b6293
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:09 +0700

    Upload SQL chunk: t31349_chunk_0000.sql

---
+++ sql_chunks/t31349_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t31349
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t31349" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31349
+INSERT INTO [t31349] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t31349] VALUES (2,'مظاهر قطيعة الرحم','5','0','0');
+INSERT INTO [t31349] VALUES (3,'أسباب قطيعة الرحم','8','0','0');
+INSERT INTO [t31349] VALUES (4,'علاج قطيعة الرحم','18','0','0');
+INSERT INTO [t31349] VALUES (5,'ما صلة الرحم','19','0','0');
+INSERT INTO [t31349] VALUES (6,'بأي شيء تكون صلة الرحم','20','0','0');
+INSERT INTO [t31349] VALUES (7,'فضائل صلة الرحم','23','0','0');
+INSERT INTO [t31349] VALUES (8,'الأمور المعينة على الصلة','31','0','0');
