f43b9121f162ff0a85ee2bc0f65053802e56a512
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:52 +0700

    Upload SQL chunk: t21841_chunk_0000.sql

---
+++ sql_chunks/t21841_chunk_0000.sql
@@ -0,0 +1,39 @@
+-- Table: t21841
+-- Chunk: 1
+-- Rows: 32
+
+CREATE TABLE "t21841" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21841
+INSERT INTO [t21841] VALUES (1,'مقدمة الطبعة الفرنسية','5','0','0');
+INSERT INTO [t21841] VALUES (2,'مقدمة الطبعة العربية','10','0','0');
+INSERT INTO [t21841] VALUES (3,'الباب الأول الحاضر والتاريخ','13','0','0');
+INSERT INTO [t21841] VALUES (4,'أنشودة رمزية','14','3','0');
+INSERT INTO [t21841] VALUES (5,'دور الأبطال','16','3','0');
+INSERT INTO [t21841] VALUES (6,'دور السياسة والفكرة','19','3','0');
+INSERT INTO [t21841] VALUES (7,'دورة الوثنية','25','3','0');
+INSERT INTO [t21841] VALUES (8,'الباب الثاني المستقبل','34','0','0');
+INSERT INTO [t21841] VALUES (9,'أنشودة رمزية','35','8','0');
+INSERT INTO [t21841] VALUES (10,'من التكديس إلى البناء','36','8','0');
+INSERT INTO [t21841] VALUES (11,'الدورة الخالدة','43','8','0');
+INSERT INTO [t21841] VALUES (12,'العدة الدائمة','56','8','0');
+INSERT INTO [t21841] VALUES (13,'أثر الفكرة الدينية في تكوين الحضارة','57','8','0');
+INSERT INTO [t21841] VALUES (14,'العنصر الأول: الإنسان','69','8','0');
+INSERT INTO [t21841] VALUES (15,'فكرة التوجيه','73','8','0');
+INSERT INTO [t21841] VALUES (16,'توجيه الثقافة','74','8','0');
+INSERT INTO [t21841] VALUES (17,'التوجيه الأخلاقي','83','8','0');
+INSERT INTO [t21841] VALUES (18,'التوجيه الجمالي','86','8','0');
+INSERT INTO [t21841] VALUES (19,'المنطق العملي','90','8','0');
+INSERT INTO [t21841] VALUES (20,'الصناعة','92','8','0');
+INSERT INTO [t21841] VALUES (21,'المبدأ الأخلاقي والذوق الجمالي في بناء الحضارة','94','8','0');
+INSERT INTO [t21841] VALUES (22,'توجيه العمل','101','8','0');
+INSERT INTO [t21841] VALUES (23,'توجيه رأس المال','104','8','0');
+INSERT INTO [t21841] VALUES (24,'مشكلة المرأة','109','8','0');
+INSERT INTO [t21841] VALUES (25,'مشكلة الزي','117','8','0');
+INSERT INTO [t21841] VALUES (26,'الفنون الجميلة','120','8','0');
+INSERT INTO [t21841] VALUES (27,'العنصر الثاني: التراب','124','8','0');
+INSERT INTO [t21841] VALUES (28,'العنصر الثالث: الوقت','131','8','0');
+INSERT INTO [t21841] VALUES (29,'الإستعمار والشعوب المستعمرة','136','8','0');
+INSERT INTO [t21841] VALUES (30,'المعامل الإستعماري','137','8','0');
+INSERT INTO [t21841] VALUES (31,'معامل القابلية للاستعمار','144','8','0');
+INSERT INTO [t21841] VALUES (32,'مشكلة التكيف','148','8','0');
