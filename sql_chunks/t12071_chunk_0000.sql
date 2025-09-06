86d960a18a3c81cf1d187f44fc737ee497b04baa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:30 +0700

    Upload SQL chunk: t12071_chunk_0000.sql

---
+++ sql_chunks/t12071_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t12071
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t12071" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12071
+INSERT INTO [t12071] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12071] VALUES (2,'تمهيد','4','0','0');
+INSERT INTO [t12071] VALUES (3,'المبحث الأول: الغريب اللغوي والغريب القرآني','5','0','0');
+INSERT INTO [t12071] VALUES (4,'المبحث الثاني: غريب القرآن وبداية المعجم العربي','11','0','0');
+INSERT INTO [t12071] VALUES (5,'المبحث الثالث: نمو الحاجة إلى تفسير غريب القرآن','15','0','0');
+INSERT INTO [t12071] VALUES (6,'المبحث الرابع: اختلافات في تأليف الغريب','22','0','0');
+INSERT INTO [t12071] VALUES (7,'المبحث الخامس: مسرد معاجم غريب القرآن مرتبة حسب وفيات أصحابها','37','0','0');
+INSERT INTO [t12071] VALUES (8,'مصادر ومراجع','67','0','0');
