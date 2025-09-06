2c2a86c82bbe02d9a0359c6d06c295cbc66e16c0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:11 +0700

    Upload SQL chunk: t6248_chunk_0000.sql

---
+++ sql_chunks/t6248_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6248
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6248" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6248
+INSERT INTO [t6248] VALUES (1,'وهو حسبي وعليه أتوكل','1','0','0');
+INSERT INTO [t6248] VALUES (2,'لا تنه عن خلق وتأتي مثله','9','0','0');
