e1329b87e0da5a7e6acf7a95a267c1fb029d0220
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:25 +0700

    Upload SQL chunk: t7525_chunk_0000.sql

---
+++ sql_chunks/t7525_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t7525
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t7525" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7525
+INSERT INTO [t7525] VALUES (1,'إسناد شيخ الإسلام ابن تيمية','2','0','0');
+INSERT INTO [t7525] VALUES (2,'الذكر فى دبرالصلاة وعند النوم','3','0','0');
+INSERT INTO [t7525] VALUES (3,'فضيلة الجهر والإسرار بالقرآن','4','0','0');
+INSERT INTO [t7525] VALUES (4,'نهي الحائض والجنب عن قراءة القرآن','6','0','0');
+INSERT INTO [t7525] VALUES (5,'دعاء الصباح والمساء','7','0','0');
+INSERT INTO [t7525] VALUES (6,'ذكر باب من أبواب الفتن','9','0','0');
+INSERT INTO [t7525] VALUES (7,'الجماعة في صلاة الليل','10','0','0');
+INSERT INTO [t7525] VALUES (8,'اتخاذ الكلب','12','0','0');
+INSERT INTO [t7525] VALUES (9,'أفضل الصدقة','13','0','0');
+INSERT INTO [t7525] VALUES (10,'التيسير للأعمال','14','0','0');
+INSERT INTO [t7525] VALUES (11,'إزالة المنى عن الثوب','16','0','0');
+INSERT INTO [t7525] VALUES (12,'شفاعة النبي صلى الله عليه وسلم يوم القيامة','18','0','0');
+INSERT INTO [t7525] VALUES (13,'من أشراط الساعة','20','0','0');
