f230307b44f7d66780a1f681ac548a4066aee152
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:44 +0700

    Upload SQL chunk: t6049_chunk_0000.sql

---
+++ sql_chunks/t6049_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t6049
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t6049" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6049
+INSERT INTO [t6049] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6049] VALUES (2,'ذيل الكتاب','86','0','0');
+INSERT INTO [t6049] VALUES (3,'ذيل الكتاب','87','0','0');
+INSERT INTO [t6049] VALUES (4,'نهاية الملحق وبه تم تحقيق النسخة كلها','94','0','0');
