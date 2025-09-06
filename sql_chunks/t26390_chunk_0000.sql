3afb9e0253d3a5ba65ab1abff7479cc8725c68a4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:15 +0700

    Upload SQL chunk: t26390_chunk_0000.sql

---
+++ sql_chunks/t26390_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t26390
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t26390" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26390
+INSERT INTO [t26390] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26390] VALUES (2,'في فضل الصلاة','2','0','0');
+INSERT INTO [t26390] VALUES (3,'في الصلاة بين المغرب والعشاء','43','0','0');
+INSERT INTO [t26390] VALUES (4,'الصلاة بعد عشاء الآخرة','51','0','0');
+INSERT INTO [t26390] VALUES (5,'في صلاة الحاجة','57','0','0');
+INSERT INTO [t26390] VALUES (6,'في صلاة الضحى','67','0','0');
+INSERT INTO [t26390] VALUES (7,'في الصلاة عند الزوال','71','0','0');
+INSERT INTO [t26390] VALUES (8,'في صلاة التسابيح','77','0','0');
+INSERT INTO [t26390] VALUES (9,'في الصلاة يوم الجمعة','111','0','0');
+INSERT INTO [t26390] VALUES (10,'الوضوء','118','0','0');
