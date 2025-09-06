c86cc1cb0acf01aefaefa6c289fd785afff328d6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:10 +0700

    Upload SQL chunk: t6956_chunk_0000.sql

---
+++ sql_chunks/t6956_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t6956
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t6956" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6956
+INSERT INTO [t6956] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6956] VALUES (2,'باب معرفة الذهب والفضة وامتحانهما','2','0','0');
+INSERT INTO [t6956] VALUES (3,'باب ما يعتبر من الجواهر النفيسة ومعرفتها وقيمتها','3','0','0');
+INSERT INTO [t6956] VALUES (4,'باب معرفة الطيب والعطر والروائح الطيبة','8','0','0');
+INSERT INTO [t6956] VALUES (5,'باب يجلب من البلدان من طرائف السلع والأمتعة والجواري والأحجار وغير ذلك','17','0','0');
+INSERT INTO [t6956] VALUES (6,'باب ما يختار من البزاة والشواهين والبواشق والصقور','26','0','0');
+INSERT INTO [t6956] VALUES (7,'وغير ذلك من جوارح الطير','26','0','0');
+INSERT INTO [t6956] VALUES (8,'باب آخر','28','0','0');
