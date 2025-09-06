241b786b12faa2ea4d7be12ccbac5aa4e0ce072f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:44 +0700

    Upload SQL chunk: t11064_chunk_0000.sql

---
+++ sql_chunks/t11064_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t11064
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t11064" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11064
+INSERT INTO [t11064] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t11064] VALUES (2,'حديث إكرام الجار','2','0','0');
+INSERT INTO [t11064] VALUES (3,'حديث أبي هريرة','2','2','0');
+INSERT INTO [t11064] VALUES (4,'عن أبي هريرة','2','2','0');
+INSERT INTO [t11064] VALUES (5,'عن أبي هريرة','2','2','0');
+INSERT INTO [t11064] VALUES (6,'حديث أبي أيوب','3','2','0');
+INSERT INTO [t11064] VALUES (7,'حديث زيد بن خالد','3','2','0');
+INSERT INTO [t11064] VALUES (8,'حديث أنس','4','2','0');
+INSERT INTO [t11064] VALUES (9,'حديث أبي شريح','4','2','0');
+INSERT INTO [t11064] VALUES (10,'عن أبي شريح','4','2','0');
+INSERT INTO [t11064] VALUES (11,'حديث المزني','4','2','0');
+INSERT INTO [t11064] VALUES (12,'حديث عائشة','4','2','0');
+INSERT INTO [t11064] VALUES (13,'حديث الإحسان إلى الجار','5','0','0');
+INSERT INTO [t11064] VALUES (14,'حديث النهي عن أذى الجار','6','0','0');
+INSERT INTO [t11064] VALUES (15,'باب منه','6','0','0');
+INSERT INTO [t11064] VALUES (16,'باب منه','6','15','0');
+INSERT INTO [t11064] VALUES (17,'باب منه','9','15','0');
+INSERT INTO [t11064] VALUES (18,'باب منه','9','15','0');
+INSERT INTO [t11064] VALUES (19,'باب قوله عليه السلام يوصيني بالجار','11','0','0');
+INSERT INTO [t11064] VALUES (20,'حتى ظننت أنه سيورثه','11','0','0');
+INSERT INTO [t11064] VALUES (21,'باب منه','14','20','0');
+INSERT INTO [t11064] VALUES (22,'باب منه','14','20','0');
+INSERT INTO [t11064] VALUES (23,'إن أعظم الزنا هو بحليلة الجار','17','20','0');
+INSERT INTO [t11064] VALUES (24,'باب إطعام الجار','20','0','0');
+INSERT INTO [t11064] VALUES (25,'باب منه','22','24','0');
+INSERT INTO [t11064] VALUES (26,'باب شفعة الجوار','24','0','0');
+INSERT INTO [t11064] VALUES (27,'مندوب إليها لأجل حق الجوار','24','0','0');
+INSERT INTO [t11064] VALUES (28,'جار الدار أحق بالدار','26','0','0');
+INSERT INTO [t11064] VALUES (29,'باب منه','26','28','0');
+INSERT INTO [t11064] VALUES (30,'باب منه','26','28','0');
+INSERT INTO [t11064] VALUES (31,'باب قوله ليس المؤمن من بات شبعان','28','0','0');
+INSERT INTO [t11064] VALUES (32,'وجاره جائع','28','0','0');
+INSERT INTO [t11064] VALUES (33,'باب جودة الجار','33','0','0');
+INSERT INTO [t11064] VALUES (34,'باب منه','34','33','0');
+INSERT INTO [t11064] VALUES (35,'باب منه','34','33','0');
+INSERT INTO [t11064] VALUES (36,'فصل','36','33','0');
+INSERT INTO [t11064] VALUES (37,'فصل','36','33','0');
+INSERT INTO [t11064] VALUES (38,'فصل','36','33','0');
+INSERT INTO [t11064] VALUES (39,'فصل','36','33','0');
