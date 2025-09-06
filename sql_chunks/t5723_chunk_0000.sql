a85f24316f6e30d075656333ff51276316f8f7d0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:46 +0700

    Upload SQL chunk: t5723_chunk_0000.sql

---
+++ sql_chunks/t5723_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5723
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5723" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5723
+INSERT INTO [t5723] VALUES (1,'مشيخة ابن شاذان الصغرى','2','0','0');
+INSERT INTO [t5723] VALUES (2,'تم الجزء','41','0','0');
