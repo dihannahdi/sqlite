b15566e8fe6081103747183a635ffdf8cffc8abf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:08 +0700

    Upload SQL chunk: t23839_chunk_0000.sql

---
+++ sql_chunks/t23839_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t23839
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t23839" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23839
+INSERT INTO [t23839] VALUES (1,'كتاب الصيام (١)','2','0','0');
+INSERT INTO [t23839] VALUES (2,'تعريف الكتاب - تعريف الصيام - الأدلة على مشروعية الصيام','2','1','0');
+INSERT INTO [t23839] VALUES (3,'أسئله – منوعه:','2','1','0');
+INSERT INTO [t23839] VALUES (4,'تعريف الكتاب:','10','1','0');
+INSERT INTO [t23839] VALUES (5,'تعريف الصيام:','11','1','0');
+INSERT INTO [t23839] VALUES (6,'الدليل على أن الصيام ركن من أركان الإسلام، والكلام على رواية تقديم الحج على الصيام:','11','1','0');
+INSERT INTO [t23839] VALUES (7,'كتاب الصيام (٢)','34','0','0');
+INSERT INTO [t23839] VALUES (8,'كتاب الصيام (٣)','66','0','0');
+INSERT INTO [t23839] VALUES (9,'كتاب الصيام (٤)','97','0','0');
+INSERT INTO [t23839] VALUES (10,'كتاب الصيام (٥)','126','0','0');
