0e2e44b050faf27b3e00960468d7127dac25ae8a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:27 +0700

    Upload SQL chunk: t10257_chunk_0000.sql

---
+++ sql_chunks/t10257_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t10257
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t10257" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10257
+INSERT INTO [t10257] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10257] VALUES (2,'أولا: إرتيريا والحبشة','8','0','0');
+INSERT INTO [t10257] VALUES (3,'ثانيا: كينيا','9','0','0');
+INSERT INTO [t10257] VALUES (4,'ثالثا: أوغندا','10','0','0');
+INSERT INTO [t10257] VALUES (5,'رابعا: تنزانيا','12','0','0');
+INSERT INTO [t10257] VALUES (6,'خامسا: بروندي','13','0','0');
+INSERT INTO [t10257] VALUES (7,'سادسا: الكونغو','14','0','0');
+INSERT INTO [t10257] VALUES (8,'سابعا: زامبيا (روديسيا الشمالية سابقا)','15','0','0');
+INSERT INTO [t10257] VALUES (9,'ثامنا: روديسيا','17','0','0');
+INSERT INTO [t10257] VALUES (10,'تاسعا: ملاوي','18','0','0');
