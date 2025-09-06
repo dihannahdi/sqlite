28987021ec2ff5f3c203de93776657ad4fbf3e18
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:02 +0700

    Upload SQL chunk: t26514_chunk_0000.sql

---
+++ sql_chunks/t26514_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26514
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26514" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26514
+INSERT INTO [t26514] VALUES (1,'الأفراد لابن شاهين','2','0','0');
