968bdf7e33bf46474c31ebacad34e99543fdc0ec
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:44 +0700

    Upload SQL chunk: t96259_chunk_0000.sql

---
+++ sql_chunks/t96259_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t96259
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t96259" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96259
+INSERT INTO [t96259] VALUES (1,'ماذا يعني بر الوالدين؟ !','1','0','0');
+INSERT INTO [t96259] VALUES (2,'كيف يكون بر الوالدين؟ !','3','0','0');
+INSERT INTO [t96259] VALUES (3,'طاعتهما فيما يأمران به','3','2','0');
+INSERT INTO [t96259] VALUES (4,'خفض الصوت عندهما ومخاطبتهما بلين','4','2','0');
+INSERT INTO [t96259] VALUES (5,'توقيرهما والتذلل لهما','4','2','0');
+INSERT INTO [t96259] VALUES (6,'استئذانهما إذا نوى السفر الطويل','5','2','0');
+INSERT INTO [t96259] VALUES (7,'الإنفاق عليهما وسد حاجتهما','6','2','0');
+INSERT INTO [t96259] VALUES (8,'الدعاء لهما، سواء في حياتهما أو بعد موتهما','6','2','0');
+INSERT INTO [t96259] VALUES (9,'فضائل بر الوالدين','7','0','0');
+INSERT INTO [t96259] VALUES (10,'بر الوالدين في الكبر وبعد موتهما','9','0','0');
+INSERT INTO [t96259] VALUES (11,'صور صادقة في بر الوالدين','11','0','0');
