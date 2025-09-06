ed2ad0e48415e190325fcb682070218ba398168d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:10 +0700

    Upload SQL chunk: t6625_chunk_0000.sql

---
+++ sql_chunks/t6625_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t6625
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t6625" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6625
+INSERT INTO [t6625] VALUES (1,'أخبار ملوك بني عبيد وسيرتهم لأبي عبد الله محمد بن علي بن حماد','1','0','0');
+INSERT INTO [t6625] VALUES (2,'المخطوط الحمد لله','2','0','0');
+INSERT INTO [t6625] VALUES (3,'عبيد الله','4','0','0');
+INSERT INTO [t6625] VALUES (4,'محمد القائم','22','0','0');
+INSERT INTO [t6625] VALUES (5,'المنصور','28','0','0');
+INSERT INTO [t6625] VALUES (6,'المعز','52','0','0');
+INSERT INTO [t6625] VALUES (7,'العزيز','62','0','0');
+INSERT INTO [t6625] VALUES (8,'الحاكم','63','0','0');
+INSERT INTO [t6625] VALUES (9,'الظاهر','72','0','0');
+INSERT INTO [t6625] VALUES (10,'المستنصر','73','0','0');
+INSERT INTO [t6625] VALUES (11,'المستعلي','74','0','0');
+INSERT INTO [t6625] VALUES (12,'الآمر','74','0','0');
+INSERT INTO [t6625] VALUES (13,'الحافظ عبد المجيد','75','0','0');
+INSERT INTO [t6625] VALUES (14,'الظافر','75','0','0');
+INSERT INTO [t6625] VALUES (15,'الفائز','77','0','0');
+INSERT INTO [t6625] VALUES (16,'العاضد','77','0','0');
