1df87811d633378e138f8ae73456dd948e135420
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:11 +0700

    Upload SQL chunk: t5996_chunk_0000.sql

---
+++ sql_chunks/t5996_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t5996
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t5996" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5996
+INSERT INTO [t5996] VALUES (1,'باب الجدة','32','0','0');
+INSERT INTO [t5996] VALUES (2,'باب من روى عن أبيه ولم يسم الأب','34','0','0');
+INSERT INTO [t5996] VALUES (3,'باب الأم','38','0','0');
+INSERT INTO [t5996] VALUES (4,'باب العم','43','0','0');
+INSERT INTO [t5996] VALUES (5,'باب العمة','54','0','0');
+INSERT INTO [t5996] VALUES (6,'وهذا باب من لا يعرف له اسم','55','0','0');
+INSERT INTO [t5996] VALUES (7,'باب الخال','57','0','0');
+INSERT INTO [t5996] VALUES (8,'باب الأخ','58','0','0');
+INSERT INTO [t5996] VALUES (9,'باب الابن','59','0','0');
+INSERT INTO [t5996] VALUES (10,'وفي هذا الباب من لا يعرف إلا به','66','0','0');
+INSERT INTO [t5996] VALUES (11,'باب الرجل','69','0','0');
+INSERT INTO [t5996] VALUES (12,'باب الزوج والزوجة','101','0','0');
+INSERT INTO [t5996] VALUES (13,'باب المرأة','104','0','0');
+INSERT INTO [t5996] VALUES (14,'باب امرأة فلان','115','0','0');
+INSERT INTO [t5996] VALUES (15,'باب ابنة فلان','122','0','0');
+INSERT INTO [t5996] VALUES (16,'باب من المبهمات','124','0','0');
+INSERT INTO [t5996] VALUES (17,'فصل','128','0','0');
+INSERT INTO [t5996] VALUES (18,'باب آخر المؤلفة قلوبهم','133','0','0');
+INSERT INTO [t5996] VALUES (19,'الأحابيش','136','0','0');
