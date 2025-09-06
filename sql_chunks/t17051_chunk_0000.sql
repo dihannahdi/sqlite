96148241cf57671513f605182a2405b5c6db5c7c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:28:10 +0700

    Upload SQL chunk: t17051_chunk_0000.sql

---
+++ sql_chunks/t17051_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t17051
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t17051" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17051
+INSERT INTO [t17051] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t17051] VALUES (2,'خاتمة','9','0','0');
