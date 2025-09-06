f972326d13f07870336a5a1912beaf41cb19798b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:18 +0700

    Upload SQL chunk: t10206_chunk_0000.sql

---
+++ sql_chunks/t10206_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t10206
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t10206" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10206
+INSERT INTO [t10206] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10206] VALUES (2,'العرب','3','0','0');
+INSERT INTO [t10206] VALUES (3,'أصل العرب','5','0','0');
+INSERT INTO [t10206] VALUES (4,'القبائل العربية الحديثة','8','0','0');
+INSERT INTO [t10206] VALUES (5,'القبائل القحطانية','10','0','0');
+INSERT INTO [t10206] VALUES (6,'أصول القبائل القحطانية','12','0','0');
+INSERT INTO [t10206] VALUES (7,'القبائل العدنانية','14','0','0');
+INSERT INTO [t10206] VALUES (8,'أصول القبائل العدنانية','17','0','0');
+INSERT INTO [t10206] VALUES (9,'لغة العرب','19','0','0');
+INSERT INTO [t10206] VALUES (10,'المراحل التي مرت بها اللغة العربية','23','0','0');
+INSERT INTO [t10206] VALUES (11,'نتائج البحث','26','0','0');
+INSERT INTO [t10206] VALUES (12,'ملحق','31','0','0');
