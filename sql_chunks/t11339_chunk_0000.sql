15cfc90f331d51d2e2bda4bd22e534f915a567cd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:10 +0700

    Upload SQL chunk: t11339_chunk_0000.sql

---
+++ sql_chunks/t11339_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t11339
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t11339" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11339
+INSERT INTO [t11339] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11339] VALUES (2,'تمهيد','4','0','0');
+INSERT INTO [t11339] VALUES (3,'الفصل الأول: سيرة بيان الحق ومؤلفاته','19','0','0');
+INSERT INTO [t11339] VALUES (4,'مدخل','19','3','0');
+INSERT INTO [t11339] VALUES (5,'ترجمته من معجم الأدباء','20','3','0');
+INSERT INTO [t11339] VALUES (6,'زيادات متفرقة','21','3','0');
+INSERT INTO [t11339] VALUES (7,'مذهبه في العقيدة والفقه','23','3','0');
+INSERT INTO [t11339] VALUES (8,'مؤلفاته','25','3','0');
+INSERT INTO [t11339] VALUES (9,'الفصل الثاني: كتاب جمل الغرائب','27','0','0');
+INSERT INTO [t11339] VALUES (10,'مدخل','27','9','0');
+INSERT INTO [t11339] VALUES (11,'غاية التأليف','28','9','0');
+INSERT INTO [t11339] VALUES (12,'مصادر الكتاب','32','9','0');
+INSERT INTO [t11339] VALUES (13,'ترتيب الكتاب','38','9','0');
+INSERT INTO [t11339] VALUES (14,'منهجه في تفسير الحديث','42','9','0');
+INSERT INTO [t11339] VALUES (15,'مآخذ على الكتاب','50','9','0');
+INSERT INTO [t11339] VALUES (16,'مصادر ومراجع','56','0','0');
