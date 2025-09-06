8e1e85ec2d26a2b674738c516a42b527b0194b43
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:20:24 +0700

    Upload SQL chunk: t14569_chunk_0000.sql

---
+++ sql_chunks/t14569_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t14569
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t14569" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14569
+INSERT INTO [t14569] VALUES (1,'الحروف المقصورة','2','0','0');
+INSERT INTO [t14569] VALUES (2,'مما يكتب بالياء','2','1','0');
+INSERT INTO [t14569] VALUES (3,'ومما يكتب بالألف','4','1','0');
+INSERT INTO [t14569] VALUES (4,'أسماء ممدودة','5','0','0');
+INSERT INTO [t14569] VALUES (5,'فمن الممدود على ألفاظها','5','4','0');
+INSERT INTO [t14569] VALUES (6,'ومن المقصور على ألفاظها','6','4','0');
+INSERT INTO [t14569] VALUES (7,'حروف المد المستعمل المخفوض الأول','8','4','0');
+INSERT INTO [t14569] VALUES (8,'ومن الممدود المفتوح الأول','12','4','0');
+INSERT INTO [t14569] VALUES (9,'الممدود المضموم الأول','15','4','0');
+INSERT INTO [t14569] VALUES (10,'ما يمد ويقصر، فإذا قصر كتب بالياء','17','0','0');
+INSERT INTO [t14569] VALUES (11,'ما يقصر، فإذا غير بعض حركات بنائه مد','18','0','0');
