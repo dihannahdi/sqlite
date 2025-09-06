e5844c449dc410898dcb3124f584212cf61f0e80
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:01 +0700

    Upload SQL chunk: t12105_chunk_0000.sql

---
+++ sql_chunks/t12105_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t12105
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t12105" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12105
+INSERT INTO [t12105] VALUES (1,'جذور وأصول المبتدعة وتطورها التاريخي','1','0','0');
+INSERT INTO [t12105] VALUES (2,'موقف الطوائف والفرق من السنة النبوية ورواتها','9','0','0');
+INSERT INTO [t12105] VALUES (3,'أمثلة من كيد الأعداء قديما وحديثا','14','0','0');
+INSERT INTO [t12105] VALUES (4,'كيف تم التحالف بين هذين المخططين في العصر الحاضر','21','0','0');
+INSERT INTO [t12105] VALUES (5,'أهم معالم سياسة المخطط الصهيوني الصليبي الجديدة واتجاهاتها وأسسها','24','0','0');
+INSERT INTO [t12105] VALUES (6,'مصادر ومراجع','55','0','0');
