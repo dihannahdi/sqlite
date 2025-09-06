bd3dc1f58c3037f3fd872ba6f5c5474674271635
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:59 +0700

    Upload SQL chunk: t30956_chunk_0000.sql

---
+++ sql_chunks/t30956_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t30956
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t30956" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30956
+INSERT INTO [t30956] VALUES (1,'مقدمة في مفهوم الحسبة في الإسلام','1','0','0');
+INSERT INTO [t30956] VALUES (2,'العلاقة بين الحسبة والأمر بالمعروف والنهي عن المنكر','2','0','0');
+INSERT INTO [t30956] VALUES (3,'مجالات الحسبة','3','0','0');
+INSERT INTO [t30956] VALUES (4,'أهمية الحسبة في النظام الإسلامي','4','0','0');
+INSERT INTO [t30956] VALUES (5,'غايات الحسبة','5','0','0');
+INSERT INTO [t30956] VALUES (6,'الحسبة في الإسلام نظام فعال','6','0','0');
+INSERT INTO [t30956] VALUES (7,'صفات المحتسب','7','0','0');
+INSERT INTO [t30956] VALUES (8,'أولا الإخلاص والتجرد','8','7','0');
+INSERT INTO [t30956] VALUES (9,'ثانيا العلم والحكمة','9','7','0');
+INSERT INTO [t30956] VALUES (10,'ثالثا الرفق والحلم','10','7','0');
