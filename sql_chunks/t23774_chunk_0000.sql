b5dc8483624a8c924a8449c278aec4d1f4b15713
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:48:51 +0700

    Upload SQL chunk: t23774_chunk_0000.sql

---
+++ sql_chunks/t23774_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t23774
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t23774" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23774
+INSERT INTO [t23774] VALUES (1,'شرح حديث جابر في صفة حج النبي صلى الله عليه وسلم (١)','2','0','0');
+INSERT INTO [t23774] VALUES (2,'شرح حديث جابر في صفة حج النبي صلى الله عليه وسلم (٢)','29','0','0');
+INSERT INTO [t23774] VALUES (3,'شرح حديث جابر في صفة حج النبي صلى الله عليه وسلم (٣)','57','0','0');
