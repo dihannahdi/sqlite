598a0fd245fe21c4cb547ad7f10cf91ac3881ac8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:16 +0700

    Upload SQL chunk: t7819_chunk_0000.sql

---
+++ sql_chunks/t7819_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t7819
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t7819" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7819
+INSERT INTO [t7819] VALUES (1,'تم والحمد الله أولا وآخر','26','0','0');
