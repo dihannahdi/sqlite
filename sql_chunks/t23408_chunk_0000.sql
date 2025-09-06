5c6fa6c4d37dda5913dd54713a295b7ec0dce602
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:30 +0700

    Upload SQL chunk: t23408_chunk_0000.sql

---
+++ sql_chunks/t23408_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t23408
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t23408" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23408
+INSERT INTO [t23408] VALUES (1,'شرح المنظومة الحائية (١)','2','0','0');
+INSERT INTO [t23408] VALUES (2,'شرح المنظومة الحائية (٢)','23','0','0');
+INSERT INTO [t23408] VALUES (3,'شرح المنظومة الحائية (٣)','38','0','0');
+INSERT INTO [t23408] VALUES (4,'شرح المنظومة الحائية (٤)','57','0','0');
+INSERT INTO [t23408] VALUES (5,'شرح المنظومة الحائية (٥)','80','0','0');
