e4fcb10a9343a3c6fd691555162a5a12baaf8221
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:15 +0700

    Upload SQL chunk: t26395_chunk_0000.sql

---
+++ sql_chunks/t26395_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26395
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26395" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26395
+INSERT INTO [t26395] VALUES (1,'جزء الغضائري','2','0','0');
