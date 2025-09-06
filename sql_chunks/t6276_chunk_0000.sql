092f3020d8577484eae99b63b71e5a71e368a47f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:21 +0700

    Upload SQL chunk: t6276_chunk_0000.sql

---
+++ sql_chunks/t6276_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t6276
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t6276" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6276
+INSERT INTO [t6276] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t6276] VALUES (2,'فصل في تحذير النبي صلى الله عليه وسلم أصحابه ومن بعدهم من البدع ومحدثات الأمور','3','0','0');
+INSERT INTO [t6276] VALUES (3,'فصل فيما اشتهر من البدع في بلاد الإسلام','23','0','0');
+INSERT INTO [t6276] VALUES (4,'فصل في الفرق بين صلاة الرغائب وغيرها من صلاة البدع وبين التطوع','47','0','0');
+INSERT INTO [t6276] VALUES (5,'فصل في مخالفة صلاة الرغائب للشرع','50','0','0');
+INSERT INTO [t6276] VALUES (6,'فصل فيما ابتدع واستميلت به قلوب العوام','72','0','0');
+INSERT INTO [t6276] VALUES (7,'فصل فيما ابتدع في قيام رمضان','77','0','0');
+INSERT INTO [t6276] VALUES (8,'فصل في البدع المشعرة بأنها من السنن','79','0','0');
+INSERT INTO [t6276] VALUES (9,'فصل في البدع في مناسك الحج','85','0','0');
+INSERT INTO [t6276] VALUES (10,'فصل في بدع الجمعة','88','0','0');
