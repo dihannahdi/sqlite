da1dea5230bfc3f383d519f0eae1eb4f4c80fe6d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:07 +0700

    Upload SQL chunk: t4134_chunk_0000.sql

---
+++ sql_chunks/t4134_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t4134
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t4134" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4134
+INSERT INTO [t4134] VALUES (1,'تمهيد','1','0','0');
+INSERT INTO [t4134] VALUES (2,'الفصل الأول: المنهج المبتكر في (نخبة الفكر)','3','0','0');
+INSERT INTO [t4134] VALUES (3,'الفصل الثاني: رضا الحافظ عن كتابه هذا، وثناء العلماء عليه، وتاريخ تأليفه','13','0','0');
+INSERT INTO [t4134] VALUES (4,'الفصل الثالث: الجهود المبذولة في خدمة كتاب (نخبة الفكر)','24','0','0');
+INSERT INTO [t4134] VALUES (5,'الفصل الرابع: التعريف بأول شروح النخبة المسمى (نتيجة النظر) للكمال الشمني، (ت٨٢١هـ) .','51','0','0');
+INSERT INTO [t4134] VALUES (6,'الخاتمة','57','0','0');
+INSERT INTO [t4134] VALUES (7,'مصادر ومراجع','60','0','0');
