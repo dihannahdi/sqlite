6a91b954304e108eb1ae47c3c5cc1d4c800f3c25
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:47 +0700

    Upload SQL chunk: t23079_chunk_0000.sql

---
+++ sql_chunks/t23079_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t23079
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t23079" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23079
+INSERT INTO [t23079] VALUES (1,'لمحات تاريخية من حياة إبن تيمية','5','0','0');
+INSERT INTO [t23079] VALUES (2,'مدخل','5','1','0');
+INSERT INTO [t23079] VALUES (3,'ابن تيمية والصوفية الفلسفية','7','1','0');
+INSERT INTO [t23079] VALUES (4,'مناظراته ودفاعه عن العقيدة','9','1','0');
+INSERT INTO [t23079] VALUES (5,'مدخل','12','0','0');
+INSERT INTO [t23079] VALUES (6,'أبن تيمية وعهد التتار','13','0','0');
+INSERT INTO [t23079] VALUES (7,'التتار على أبواب الشام','15','0','0');
+INSERT INTO [t23079] VALUES (8,'موقف أبن تيمية من الاضطرابات','17','0','0');
+INSERT INTO [t23079] VALUES (9,'أبن تيمية في مقابلة ملك التتار','18','0','0');
+INSERT INTO [t23079] VALUES (10,'نتأئج هذه المقابلة','19','0','0');
+INSERT INTO [t23079] VALUES (11,'أبن تيمية يسافر إلى مصر','21','0','0');
+INSERT INTO [t23079] VALUES (12,'أبن تيمية في الميدان','23','0','0');
