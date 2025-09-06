11e387bec61906f1b9917460bc7484a81c08b837
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:53:59 +0700

    Upload SQL chunk: t94_chunk_0000.sql

---
+++ sql_chunks/t94_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t94
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t94" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t94
+INSERT INTO [t94] VALUES (1,'مقدمة التحقيق','2','0','0');
+INSERT INTO [t94] VALUES (2,'الملخص','2','1','0');
+INSERT INTO [t94] VALUES (3,'المقدمة','3','1','0');
+INSERT INTO [t94] VALUES (4,'المصنِّف (ابن الخشاب)','4','3','0');
+INSERT INTO [t94] VALUES (5,'مولده ونشأته','4','4','0');
+INSERT INTO [t94] VALUES (6,'صفاته وأخلاقه','4','4','0');
+INSERT INTO [t94] VALUES (7,'شيوخه','5','4','0');
+INSERT INTO [t94] VALUES (8,'تلاميذه','5','4','0');
+INSERT INTO [t94] VALUES (9,'مؤلفاته','5','4','0');
+INSERT INTO [t94] VALUES (10,'شعره','6','4','0');
+INSERT INTO [t94] VALUES (11,'وفاته','6','4','0');
+INSERT INTO [t94] VALUES (12,'المصنَّف (القصيدة)','7','3','0');
+INSERT INTO [t94] VALUES (13,'نسبتها','7','12','0');
+INSERT INTO [t94] VALUES (14,'لمن كتبها؟','8','12','0');
+INSERT INTO [t94] VALUES (15,'ما هي القصيدة؟','10','12','0');
+INSERT INTO [t94] VALUES (16,'جوها','10','12','0');
+INSERT INTO [t94] VALUES (17,'فنونها','10','12','0');
+INSERT INTO [t94] VALUES (18,'التحقيق','11','3','0');
+INSERT INTO [t94] VALUES (19,'نسختاه','11','18','0');
+INSERT INTO [t94] VALUES (20,'النسخة الأصل','11','19','0');
+INSERT INTO [t94] VALUES (21,'النسخة الثانية','11','19','0');
+INSERT INTO [t94] VALUES (22,'منهجه','12','18','0');
+INSERT INTO [t94] VALUES (23,'القصيدة','17','0','0');
+INSERT INTO [t94] VALUES (24,'المصادر والمراجع','27','0','0');
