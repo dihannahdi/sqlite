2070af5c5687c87c5f68265dd651db8a387fe560
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:49 +0700

    Upload SQL chunk: t5726_chunk_0000.sql

---
+++ sql_chunks/t5726_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t5726
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t5726" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5726
+INSERT INTO [t5726] VALUES (1,'إجازة الشيخ أحمد العطار للشيخ شاكر العقاد','1','0','0');
+INSERT INTO [t5726] VALUES (2,'سنده في الفقه الشافعي','10','0','0');
+INSERT INTO [t5726] VALUES (3,'الأحاديث المسلسلة','11','0','0');
