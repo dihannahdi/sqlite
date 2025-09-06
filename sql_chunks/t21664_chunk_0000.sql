74f67b34e91148a61666d0924421c395adbbf4ad
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:05 +0700

    Upload SQL chunk: t21664_chunk_0000.sql

---
+++ sql_chunks/t21664_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21664
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21664" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21664
+INSERT INTO [t21664] VALUES (1,'مجلس من أمالي الباطرقاني - مخطوط','2','0','0');
