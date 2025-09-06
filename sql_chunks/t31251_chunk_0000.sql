92ac82e709c964a2debb08a41b4d1ac1040333a7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:39 +0700

    Upload SQL chunk: t31251_chunk_0000.sql

---
+++ sql_chunks/t31251_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t31251
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t31251" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31251
+INSERT INTO [t31251] VALUES (1,'تحريم حلق اللحى','1','0','0');
+INSERT INTO [t31251] VALUES (2,'وجوب إعفاء اللحية وتحريم حلقها وتقصيرها','12','0','0');
+INSERT INTO [t31251] VALUES (3,'حكم حلق اللحية أو قصها وهل يكون من حلقها متعمدا معتقدا حل ذلك كافر','12','2','0');
+INSERT INTO [t31251] VALUES (4,'فتاوى تتعلق بحلق اللحية','17','2','0');
