0043f2046221ee8b64d37c2d6c9e3dafd9969d5c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:15 +0700

    Upload SQL chunk: t696_chunk_0000.sql

---
+++ sql_chunks/t696_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t696
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t696" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t696
+INSERT INTO [t696] VALUES (1,'كلمة الإخلاص','1','0','0');
