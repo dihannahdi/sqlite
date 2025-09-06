eafa2bdf2366bb5798087894650302b2f3c20a59
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:32 +0700

    Upload SQL chunk: t10799_chunk_0000.sql

---
+++ sql_chunks/t10799_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10799
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10799" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10799
+INSERT INTO [t10799] VALUES (1,'ذكر شهر رمضان وفضله','10','0','0');
+INSERT INTO [t10799] VALUES (2,'القيام في شهر رمضان','43','0','0');
+INSERT INTO [t10799] VALUES (3,'في السحور','61','0','0');
