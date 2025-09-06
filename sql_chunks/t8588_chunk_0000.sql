fec5a1387d83e89fcd2a80f7263fa44aeebafdea
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:34 +0700

    Upload SQL chunk: t8588_chunk_0000.sql

---
+++ sql_chunks/t8588_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t8588
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t8588" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8588
+INSERT INTO [t8588] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8588] VALUES (2,'فصل في بيان توحيد الربوبية','10','0','0');
+INSERT INTO [t8588] VALUES (3,'فصل في توحيد الألوهية','16','0','0');
+INSERT INTO [t8588] VALUES (4,'فصل في توحيد الأسماء والصفات','37','0','0');
