d31c902311ac6a5f22ad70605944da2a34b967ce
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:00:07 +0700

    Upload SQL chunk: t1187_chunk_0000.sql

---
+++ sql_chunks/t1187_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t1187
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t1187" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1187
+INSERT INTO [t1187] VALUES (1,'تصدير','1','0','0');
+INSERT INTO [t1187] VALUES (2,'نبذة عن تدليس المتن','4','0','0');
+INSERT INTO [t1187] VALUES (3,'مقدمة الكتاب','9','0','0');
+INSERT INTO [t1187] VALUES (4,'الرواة على حروف المعجم','12','0','0');
+INSERT INTO [t1187] VALUES (5,'الكنى','105','0','0');
+INSERT INTO [t1187] VALUES (6,'قال الحافظ العلائي','108','0','0');
