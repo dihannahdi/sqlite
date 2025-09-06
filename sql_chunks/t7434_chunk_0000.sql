e19a5a73e16577e0e34f330feeb02c7300fe05b7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:24 +0700

    Upload SQL chunk: t7434_chunk_0000.sql

---
+++ sql_chunks/t7434_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t7434
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t7434" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7434
+INSERT INTO [t7434] VALUES (1,'المجلد الأول','1','0','0');
+INSERT INTO [t7434] VALUES (2,'خطبة الكتاب','1','1','0');
+INSERT INTO [t7434] VALUES (3,'حرف الألف','57','1','0');
+INSERT INTO [t7434] VALUES (4,'حرف الباء الموحدة','208','1','0');
+INSERT INTO [t7434] VALUES (5,'حرف التاء المثناة','222','1','0');
+INSERT INTO [t7434] VALUES (6,'حرف الثاء المثلثة','225','1','0');
+INSERT INTO [t7434] VALUES (7,'حرف الجيم','233','1','0');
+INSERT INTO [t7434] VALUES (8,'حرف الحاء المهملة','254','1','0');
+INSERT INTO [t7434] VALUES (9,'حرف الخاء المعجمة','315','1','0');
+INSERT INTO [t7434] VALUES (10,'حرف الدال','329','1','0');
+INSERT INTO [t7434] VALUES (11,'حرف الذال المعجمة','340','1','0');
+INSERT INTO [t7434] VALUES (12,'حرف الراء المهملة','344','1','0');
+INSERT INTO [t7434] VALUES (13,'حرف الزاي المنقوطة.','359','1','0');
+INSERT INTO [t7434] VALUES (14,'حرف السين المهملة','381','1','0');
+INSERT INTO [t7434] VALUES (15,'حرف الشين المعجمة','445','1','0');
+INSERT INTO [t7434] VALUES (16,'حرف الصاد المهملة','457','1','0');
+INSERT INTO [t7434] VALUES (17,'حرف الضاد المعجمة','472','1','0');
+INSERT INTO [t7434] VALUES (18,'حرف الطاء المهملة','476','1','0');
+INSERT INTO [t7434] VALUES (19,'حرف الظاء المشالة بنقطة','486','1','0');
+INSERT INTO [t7434] VALUES (20,'المجلد الثاني','487','0','0');
+INSERT INTO [t7434] VALUES (21,'حرف العين المهملة','487','20','0');
+INSERT INTO [t7434] VALUES (22,'حرف الغين المعجمة','856','20','0');
+INSERT INTO [t7434] VALUES (23,'حرف الفاء','857','20','0');
+INSERT INTO [t7434] VALUES (24,'حرف القاف','862','20','0');
+INSERT INTO [t7434] VALUES (25,'حرف الكاف','877','20','0');
+INSERT INTO [t7434] VALUES (26,'حرف اللام','887','20','0');
+INSERT INTO [t7434] VALUES (27,'حرف الميم','888','20','0');
