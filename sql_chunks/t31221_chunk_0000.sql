4684d945b3f58bc0569925a96aec46d770d86731
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:22 +0700

    Upload SQL chunk: t31221_chunk_0000.sql

---
+++ sql_chunks/t31221_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t31221
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t31221" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31221
+INSERT INTO [t31221] VALUES (1,'تمهيد','1','0','0');
+INSERT INTO [t31221] VALUES (2,'أركان الإسلام الخمسة','1','1','0');
+INSERT INTO [t31221] VALUES (3,'الركن الأول شهادة أن لا إله إلا الله وأن محمدا رسول الله','1','2','0');
+INSERT INTO [t31221] VALUES (4,'الركن الثاني والثالث إقام الصلاة وإيتاء الزكاة','6','2','0');
+INSERT INTO [t31221] VALUES (5,'الركن الرابع صوم شهر رمضان','7','2','0');
+INSERT INTO [t31221] VALUES (6,'الركن الخامس حج البيت لمن استطاع إليه سبيلا','8','2','0');
+INSERT INTO [t31221] VALUES (7,'أهمية الصلاة','8','0','0');
+INSERT INTO [t31221] VALUES (8,'الطهارة','12','0','0');
+INSERT INTO [t31221] VALUES (9,'طهارة الجسم الغسل والوضوء','12','8','0');
+INSERT INTO [t31221] VALUES (10,'التيمم','14','8','0');
+INSERT INTO [t31221] VALUES (11,'الصلوات المفروضة','15','0','0');
+INSERT INTO [t31221] VALUES (12,'صفة الصلاة','16','0','0');
+INSERT INTO [t31221] VALUES (13,'صلاة الجماعة','23','0','0');
+INSERT INTO [t31221] VALUES (14,'صلاة الجمعة','24','0','0');
+INSERT INTO [t31221] VALUES (15,'صلاة المسافر','26','0','0');
+INSERT INTO [t31221] VALUES (16,'الأذكار المسنونة','29','0','0');
+INSERT INTO [t31221] VALUES (17,'السنن الرواتب','30','0','0');
