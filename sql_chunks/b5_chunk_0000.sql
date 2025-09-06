83cfcfb8b83d34b8577373b7332261886032df57
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 00:12:01 +0700

    Upload SQL chunk: b5_chunk_0000.sql

---
+++ sql_chunks/b5_chunk_0000.sql
@@ -0,0 +1,178 @@
+-- Table: b5
+-- Chunk: 1
+-- Rows: 72
+
+CREATE TABLE "b5"(id INTEGER PRIMARY KEY,content TEXT, part TEXT, page TEXT, number TEXT, services TEXT, is_deleted TEXT);
+
+-- Insert data for b5
+INSERT INTO [b5] VALUES (1,'بسم الله الرحمن الرحيم
+الحمد لله ملهم التوفيق ومنه الفيض والتحقيق.
+وبعد فهذه رسالة لطيفة تشتمل على اسماء المدلسين من رجال الحديث على أحرف المعجم ليكون أسهل في الكشف عما يراد.
+والله ولي التوفيق وهو حسبي ونعم الوكيل
+_________
+<span data-type="title">[التعليق]</span>
+ توضيح:
+لقد جرى تفريق بين كلام السيوطي وكلام المحقق بجعل كلام الأول تحت الاسم مباشرة وبحرف أكبر حجما من كلام المحقق الذي وضع تحت خط فاصل.',NULL,'25',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (2,'١-<span data-type="title" id=toc-1> إبراهيم بن يزيد النخعي</span>
+ذكره الحاكم وغيره من المدلسين وحكى خلف بن سلام عن عدة من مشايخه أن تدليسه من أعمض شيء وكانوا يتعجبون منه. انتهى',NULL,'27',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (3,'٢-<span data-type="title" id=toc-3> إسماعيل بن أبي يحيى </span>شيخ الشافعية
+وصفه أحمد بالتدليس',NULL,'29',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (4,'٣-<span data-type="title" id=toc-4> إسماعيل بن خالد</span>
+ذكره النسائي وغيره. وقال الخطيب: حدث عنه الحكم بن عتبة ويحيى بن هشام السمسار وبين وفاتيهما نحو من مائة وعشر سنين روى له جماعة وقال عنه الذهبي توفي سنة ١٤٦',NULL,'30',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (5,'٤-<span data-type="title" id=toc-5> إسماعيل بن إسحاق بن الوليد</span>
+ذكره غير واحد',NULL,'32',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (6,'٥-<span data-type="title" id=toc-7> تليد بن سليمان الكوفي</span>
+كان يدلس قاله أحمد بن صالح عن جابر',NULL,'33',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (7,'٦-<span data-type="title" id=toc-9> جابر الجعفي</span>
+قال أبو نعيم: قال سفيان الثوري: قل ما قال فيه جابر سمعت وحدثنا فاشتدد يديك يه وما كان سوى ذلك فوقه.',NULL,'34',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (8,'٧-<span data-type="title" id=toc-11> حبيب بن أبي ثابت</span>
+قال ابن حبان: كان مدلسا. وروى أبو بكر بن عياش عن الأعمش قال: قال لي حبيب بن أبي ثابت: لو أن رجلا حدثني عنك ما باليت أني أرويه عنك.',NULL,'36',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (9,'٨-<span data-type="title" id=toc-12> حجاج بن أرطاة</span>
+مشهور بالتدليس.',NULL,'37',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (10,'٩-<span data-type="title" id=toc-13> الحسن بن أبي ذكوان الحسن المصري </span>(١)
+كذلك الحسن بن ذكوان ذكره محمد بن نصر المروزي
+_________
+<span data-type="title">[التعليق]</span>
+ (١) ورد في المخطوط: البصري، وصوابه كما في المراجع: المصري، وليس البصري.',NULL,'39',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (11,'١٠-<span data-type="title" id=toc-14> الحسين بن واقد المروزي</span>
+ذكره أبو يعلى فيمن دلس',NULL,'41',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (12,'١١-<span data-type="title" id=toc-15> حفص بن غياث الكوفي</span>
+ذكره أحمد في رواية الأشربة',NULL,'42',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (13,'١٢-<span data-type="title" id=toc-16>الحكم بن عتيبة</span>
+ذكره غير واحد يدلس',NULL,'44',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (14,'١٣-<span data-type="title" id=toc-17> حماد بن أسامة</span>
+ذكره أبو شامة. قال المعيطي: كان كثير التدليس ثم تذكر بعد ذلك وذكر الأزدي عن سفيان الثوري أنه قال: إني لأتعجب كيف كان حديث أبي أسامة كان من أشرف الناس. وقد أرود بعضهم هذا الكلام على الأزدي.',NULL,'46',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (15,'١٤-<span data-type="title" id=toc-18> حميد الطويل</span>
+ذكره غير واحد من المدلسين',NULL,'47',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (16,'١٥-<span data-type="title" id=toc-20> زكريا بن أبي زائدة</span>
+قال أبو حاتم الرازي: يدلس على الشعبي عن ابن جريج. انتهى',NULL,'48',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (17,'١٦-<span data-type="title" id=toc-21> سالم بن أبي النجود</span>
+ذكره الذهبي في الميزان
+_________
+<span data-type="title">[التعليق]</span>
+١٦- لعله سالم بن أبي الجعد.',NULL,'50',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (18,'١٧-<span data-type="title" id=toc-23> سعيد بن أبي عروبة</span>
+مشهور به [بالتدليس]',NULL,'50',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (19,'١٨-<span data-type="title" id=toc-24> سفيان الثوري</span>
+مشهور به [بالتدليس]',NULL,'51',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (20,'١٩ -<span data-type="title" id=toc-25> سفيان بن عيينة</span>
+مشهور به',NULL,'53',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (21,'٢٠-<span data-type="title" id=toc-26> سليمان التيمي</span>
+مشهور به [بالتدليس]',NULL,'54',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (22,'٢١-<span data-type="title" id=toc-27> سليمان الأعمش</span>
+مشهور به [بالتدليس]',NULL,'55',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (23,'٢٢-<span data-type="title" id=toc-28> يزيد بن سعيد الحدراني</span>
+ذكره غير واحد
+_________
+<span data-type="title">[التعليق]</span>
+٢٢- لعله سعيد بن يزيد الأصبحي المدني الإسكندري الصباحي، أبو خالد ولد سنة١٥٤هـ.',NULL,'57',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (24,'٢٣-<span data-type="title" id=toc-30> شباك الضبي</span>
+كذلك ذكره غير واحد',NULL,'58',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (25,'٢٤-<span data-type="title" id=toc-31> شريك بن عبد الله النخعي</span>
+كان قليل التدليس',NULL,'58',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (26,'٢٥-<span data-type="title" id=toc-32> شعيب بن أيوب</span>
+قال ابن حبان: كان يدلس',NULL,'61',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (27,'٢٦-<span data-type="title" id=toc-34> طاووس بن كيسان اليماني</span>
+قاله حسن الكرابيسي: إنه أخذ عن عكرمة كثيراً وعن ابن عباس وكان يرسله بعد ذلك عنه فقيل هو مرسل عن ثقة. انتهى.',NULL,'62',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (28,'٢٧-<span data-type="title" id=toc-35> طلحة بن نافع بن سفيان</span>
+ذكره الحاكم مدلساً',NULL,'63',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (29,'٢٨-<span data-type="title" id=toc-36> عباد بن منصور </span>المتأخر
+قال: سألت أحمد عنه، فقال: كان مدلساً قدرياً',NULL,'64',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (30,'٢٩ -<span data-type="title" id=toc-38><span data-type="title" id=toc-39> عبد الله بن لهيعة</span></span>
+وصف بالتدليس',NULL,'66',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (31,'٣٠-<span data-type="title" id=toc-40> عبد الله بن أبي نجيح البجلي</span>
+ذكره النسائي في المدلسين',NULL,'68',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (32,'٣١-<span data-type="title" id=toc-41> عبد الله بن أبي زياد الإفريقي</span>
+قال ابن حبان: كان يدلس',NULL,'68',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (33,'٣٢-<span data-type="title" id=toc-42> عبد الله بن عمير</span>
+مشهور به [بالتدليس]',NULL,'69',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (34,'٣٣-<span data-type="title" id=toc-43> عبد الله بن زيد الجرمي أبو قلابة</span>
+قال الذهبي في [الميزان] : كان يدلس عمن لحقهم وكان لهم صفحف يحدث منها ويدلس.',NULL,'69',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (35,'٣٤-<span data-type="title" id=toc-44> عبد المجيد بن عبد العزيز بن أبي داود</span>
+ذكره أحمد من حديث رواه عن عبد الله بن عمر، فقال: ينبغي أن يكون عبد المجيد دلسه أحمد فحدث به. ذكره العلاء في العلل.',NULL,'71',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (36,'٣٥-<span data-type="title" id=toc-45> عبد الرحمن بن محمد المحاربي</span>
+قال عبد الله بن أحمد بن حنبل: بلغنا أنه كان يدلس.',NULL,'72',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (37,'٣٦-<span data-type="title" id=toc-46> عبد الملك بن جريج</span>
+يكثر من التدليس',NULL,'73',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (38,'٣٧-<span data-type="title" id=toc-47> عبد الوهاب بن عطاء الخفاف</span>
+قال الخطيب: كان يدلس.',NULL,'74',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (39,'٣٨-<span data-type="title" id=toc-48> عكرمة بن عمار</span>
+ذكره ابن أبي حاتم الرازي بذلك.',NULL,'75',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (40,'٣٩-<span data-type="title" id=toc-49> علي بن عواب أبو الحسن الكوفي</span>
+قال أحمد: يدلس',NULL,'76',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (41,'٤٠-<span data-type="title" id=toc-50> عمر بن علي المقدسي</span>
+يدلس قرأه الأثرم عن أحمد بن حنبل.',NULL,'77',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (42,'٤١-<span data-type="title" id=toc-51>عمرو بن عبد الله أبو إسحاق</span>
+مشهور بالتدليس',NULL,'77',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (43,'٤٢-<span data-type="title" id=toc-52> عيسى بن موسى غنجار</span>
+قال الحاكم: يدلس عن الضعفاء',NULL,'79',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (44,'٤٣-<span data-type="title" id=toc-53> عمر بن علي بن عطاء بن مقدم</span>',NULL,'80',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (45,'٤٤-<span data-type="title" id=toc-55> قتادة</span>
+مشهور بالتدليس',NULL,'80',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (46,'٤٥-<span data-type="title" id=toc-57> محمد بن إسحاق</span>
+كثير التدليس ويعرف بالإمام',NULL,'81',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (47,'٤٦-<span data-type="title" id=toc-58> محمد بن حازم أبو معاوية</span>
+قال أحمد بن طاهر: كان يدلس',NULL,'83',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (48,'٤٧-<span data-type="title" id=toc-59> محمد بن شهاب الزهري</span>
+مشهور بالتدليس',NULL,'84',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (49,'٤٨-<span data-type="title" id=toc-60> محمد بن صدقة الولي أبو عبد الله</span>
+ذكره ابن الأثير في اختصار كتاب الأنساب أنه كان مدلساً',NULL,'84',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (50,'٤٩-<span data-type="title" id=toc-61> المبارك بن فضالة</span>
+قال أبو زرعة: يدلس كثيراً. وقال أبو داود: شديد التدليس.',NULL,'85',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (51,'٥٠-<span data-type="title" id=toc-62> محمد بن عبد الرحمن الطفاوي</span>
+قال أحمد: كان يدلس',NULL,'87',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (52,'٥١-<span data-type="title" id=toc-63> محمد بن عجلان المدني</span>
+ذكره ابن أبي حاتم في حديثه عن الأعرج عن أبي هريرة فقال: إنما سمعه ربيعة بن عثمان عن الأعرج. وقال غيره: إنه كان يدلس.',NULL,'88',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (53,'٥٢-<span data-type="title" id=toc-64> محمد بن عيسى بن سميع</span>
+قال ابن حبان: روى حديثاً بقتل عثمان بن أبي دريب فلم يسمعه منه إنما سمعه من إسماعيل بن يحيى أحد الضعفاء عنه. انتهى',NULL,'89',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (54,'٥٣-<span data-type="title" id=toc-65> محمد بن عيسى الطباع</span>
+قال أبو داود: يدلس.',NULL,'90',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (55,'٥٤-<span data-type="title" id=toc-66> محمد بن مسلم أبو الزبير</span>
+مشهور بالتدليس',NULL,'91',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (56,'٥٥-<span data-type="title" id=toc-67> مروان بن معاوية الفزاري</span>
+قال ابن معين: ما رأيت أحب للتدليس منه',NULL,'92',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (57,'٥٦-<span data-type="title" id=toc-68> معمر بن راشد</span>
+قال ابن العطار: إنه يدلس.',NULL,'94',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (58,'٥٧-<span data-type="title" id=toc-69> مغيرة بن نعيم المضير </span>(١)
+_________
+(١) قال أحمد بن سالم المصري (عضو في ملتقى أهل الحديث) :
+قلتُ: هو: ((مُغِيرَة بن مِقْسَم الضَّبِّيّ)) ، وقد وقع محرَّفاً في المخطوطة الأزهرية (٤/ب) ، والتصويب من كُتُب الرجال فانظر:
+١- "تَهذيب الكمال" للمزي (٧/٢٠١/٦٧٣٨-ط٢.مؤسسة الرسالة)
+٢- "ميزان الاعتدال" للذهبي (٥/٢٩٠-٢٩١/٨٧٢٣) .
+٣- "التبيين لأسماء المدلسين" (٧٩) .
+٤- "طبقات المدلسين" لابن حجر (١٠٧) .
+ومما يؤكد ذلك أنَّ العبارة التي نقلها السيوطي عن ابن فضيل هي موجودة في ترجمة ((مغيرة بن مقسم)) ، والله أعلم.',NULL,'95',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (59,'٥٨-<span data-type="title" id=toc-70> موسى بن عقبة</span>
+من صحيح البخاري روايته عن الزهري وفي بعضها قال الزهري: قال الإسماعيلي: يقال إنه لم يسمع من الزهري شيئاً.',NULL,'96',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (60,'٥٩-<span data-type="title" id=toc-71> ميمون بن موسى المرائي</span>
+قال أحمد: مدلس',NULL,'97',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (61,'٦٠-<span data-type="title" id=toc-73>هشام بن حسان</span>
+عن محمد بن المنكدر أن جابر رفعه: "عليكم بالأئمة". فسألت عنه قال: حديث منكر لم يروه عن ابن المنكدر إلا الضفعاء إسماعيل بن مسلم ونحوه ولعل هشام بن حبان أخذه من إسماعيل، فإنه كان يدلس.',NULL,'98',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (62,'٦١-<span data-type="title" id=toc-74> هشام بن عروة</span>
+قال ابن المديني سمعت يحيى يعني ابن سعد يقول: كان هشام بن عروة يتحدث عن أبيه عن عائشة قال ما خير رسول الله بين أمرين الحديث، وما خيرت الحديث. فلما سألت قال أخبرني أبي عن عائشة قال: ما خير رسول الله بين أمرين، لم أسمع من أبي إلا هذا. والباقي لم أسمعه إنما هو عن الزهري رواه الحاكم عن ابن المديني.',NULL,'100',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (63,'٦٢-<span data-type="title" id=toc-75> هشام بن كثير </span>(١)
+يكثر من التدليس
+_________
+(١) قال أحمد بن سالم المصري (عضو في ملتقى أهل الحديث) :
+قلتُ: هو: ((هُشَيْم بن بَشِير)) ، وقد وقع محرّفاً أيضاً في المخطوطة الأزهرية (٥/ب) ، والتصويب من كتب الرجال، فانظر:
+١- "التبيين لأسماء المدلسين" (٨٥) .
+٢- "طبقات المدلسين" لابن حجر (١١١) .
+ومما يؤكد ما ذكرته أنَّ السيوطي قال عنه: [مكثر من التدليس] ، وقال عنه سبط ابن العجمي في "التبيين": [مشهور بالتدليس مكثر منه] .',NULL,'102',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (64,'٦٣-<span data-type="title" id=toc-77> الوليد بن مسلم</span>
+كذلك [يكثر من التدليس]',NULL,'102',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (65,'٦٤-<span data-type="title" id=toc-79> يحيى بن أبي حميد أبو حبابة</span>
+قال أبو زرعة: صدوق مدلس',NULL,'103',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (66,'٦٥-<span data-type="title" id=toc-80> يحيى بن سعيد الأنصاري</span>
+كان يدلس ذكره عبد الغني بن سعيد البصري عن علي بن المديني في ترجمته.',NULL,'104',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (67,'٦٦-<span data-type="title" id=toc-81> محمد بن عمرو بن علقمة</span>
+في كتابه الكافي',NULL,'105',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (68,'٦٧-<span data-type="title" id=toc-82> يحيى بن أبي كثير</span>
+مشهور بالتدليس. ذكره النسائي',NULL,'106',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (69,'٦٨-<span data-type="title" id=toc-83> يزيد بن أبي زياد</span>
+ذكره الحاكم في علم الحديث بالتدليس',NULL,'108',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (70,'٦٩-<span data-type="title" id=toc-84> يزيد بن أبي مالك الهمداني</span>
+ذكره أبو مسهر بالتدليس',NULL,'109',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (71,'٧٠-<span data-type="title" id=toc-85> يونس بن عبيد</span>
+يدلس. ذكره النسائي',NULL,'110',NULL,NULL,'0');
+INSERT INTO [b5] VALUES (72,'٧١-<span data-type="title" id=toc-87> أبو سعد البقال سعيد بن المرزبان</span>
+تكلم فيه قال: ابن المبارك: قلت لشريك بن عبد الله تعرف أبا سعيد البقال؟ قال: أعرفه على الإسناد. أنا حدثته عن عبد الكريم الحريري، عنه زياد بن أبي مريم، عن عبد الله بن مغفل، عن ابن مسعود، حديث الذكر توبة فتركني وترك عبد الكريم وزياد بن أبي مريم، وروى عن عبد الله بن مغفل عن أبي مسعود الحديث. انتهى.',NULL,'111',NULL,NULL,'0');
