28f21e742e7eb2da8740fcbab6924e9367c03b9f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:11 +0700

    Upload SQL chunk: t8996_chunk_0000.sql

---
+++ sql_chunks/t8996_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8996
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8996" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8996
+INSERT INTO [t8996] VALUES (1,'إلى الذين يحادون مفاتح الغيب باسم العلم الحديث','1','0','0');
+INSERT INTO [t8996] VALUES (2,'أما عن الأبيات','12','0','0');
