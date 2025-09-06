1ef593e895502257ad619aafb4a78be7a0b44bf9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:28 +0700

    Upload SQL chunk: t8871_chunk_0000.sql

---
+++ sql_chunks/t8871_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8871
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8871" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8871
+INSERT INTO [t8871] VALUES (1,'القرآن يتحدى','1','0','0');
+INSERT INTO [t8871] VALUES (2,'للأستاذ الدكتور محمد سيد طنطاوي','1','0','0');
