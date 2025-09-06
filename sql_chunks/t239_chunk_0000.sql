2936d2ba1574779afe4d2e21b99c4003f1bd6fa5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:13 +0700

    Upload SQL chunk: t239_chunk_0000.sql

---
+++ sql_chunks/t239_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t239
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t239" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t239
+INSERT INTO [t239] VALUES (1,'مقدمة الناشر','2','0','0');
+INSERT INTO [t239] VALUES (2,'الأصول الثلاثة','6','0','0');
+INSERT INTO [t239] VALUES (3,'[ما يجب على كل مسلم أن يتعلمه]','6','2','0');
+INSERT INTO [t239] VALUES (4,'الحنيفية ملة إبراهيم هي عبادة الله وحده','8','2','0');
+INSERT INTO [t239] VALUES (5,'الأصل الأول: معرفة الرب','9','2','0');
+INSERT INTO [t239] VALUES (6,'أنواع العبادة التي أمر الله بها','10','5','0');
+INSERT INTO [t239] VALUES (7,'الأصل الثاني معرفة دين الإسلام بالأدلة','14','2','0');
+INSERT INTO [t239] VALUES (8,'المرتبة الأولى الإسلام','14','7','0');
+INSERT INTO [t239] VALUES (9,'المرتبة الثانية الإيمان','16','7','0');
+INSERT INTO [t239] VALUES (10,'المرتبة الثالثة الإحسان','17','7','0');
+INSERT INTO [t239] VALUES (11,'الأصل الثالث معرفة نبيكم محمد صلى الله عليه وسلم','20','2','0');
+INSERT INTO [t239] VALUES (12,'شروط الصلاة وأركانها','26','0','0');
+INSERT INTO [t239] VALUES (13,'شروط الصلاة','26','12','0');
+INSERT INTO [t239] VALUES (14,'أركان الصلاة','31','12','0');
+INSERT INTO [t239] VALUES (15,'القواعد الأربع','40','0','0');
