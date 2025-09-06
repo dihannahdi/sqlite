de875ea0d54c3ccc9eb970b7415d882071e2d05f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:27 +0700

    Upload SQL chunk: t394_chunk_0000.sql

---
+++ sql_chunks/t394_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t394
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t394" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t394
+INSERT INTO [t394] VALUES (1,'حفظ الله للدين متناول للسنة','2','0','0');
+INSERT INTO [t394] VALUES (2,'من يزعم أن الجهاد في تمييز الحق من الباطل في السنة قد انتهى دوره','3','0','0');
+INSERT INTO [t394] VALUES (3,'مناقشة الفرقة الثانية منهم','4','0','0');
+INSERT INTO [t394] VALUES (4,'مناقشة الفرقة الثالثة منهم','5','0','0');
