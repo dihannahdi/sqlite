25fcb1d16638af39133449746632a5bf22685d4c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:17:34 +0700

    Upload SQL chunk: t38159_chunk_0000.sql

---
+++ sql_chunks/t38159_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t38159
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t38159" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t38159
+INSERT INTO [t38159] VALUES (1,'نص الرحلة','1','0','0');
+INSERT INTO [t38159] VALUES (2,'الانطلاق','2','1','0');
+INSERT INTO [t38159] VALUES (3,'خاتمة الرحلة','26','1','0');
