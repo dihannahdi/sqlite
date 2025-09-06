ef557d62ec526484b9e82f1bd4d340fb357dea15
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:13:22 +0700

    Upload SQL chunk: t132964_chunk_0000.sql

---
+++ sql_chunks/t132964_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t132964
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t132964" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t132964
+INSERT INTO [t132964] VALUES (1,'طرة الكتاب','2','0','0');
+INSERT INTO [t132964] VALUES (2,'آخر الحواشي التي كتبها شيخنا ابن حجر على نسخته من تنقيح الزركشي','84','0','0');
