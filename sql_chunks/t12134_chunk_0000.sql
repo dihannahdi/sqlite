b25b998b1504bed5c118d0df5ac23024a57676d1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:14 +0700

    Upload SQL chunk: t12134_chunk_0000.sql

---
+++ sql_chunks/t12134_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t12134
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t12134" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12134
+INSERT INTO [t12134] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12134] VALUES (2,'التمهيد','4','0','0');
+INSERT INTO [t12134] VALUES (3,'المطلب الأول: تعريف الاضطباع والرمل.','5','0','0');
+INSERT INTO [t12134] VALUES (4,'المطلب الثاني: دليل مشروعية الاضطباع والرمل.','11','0','0');
+INSERT INTO [t12134] VALUES (5,'المطلب الثالث: الحكمة من مشروعية الاضطباع والرمل.','13','0','0');
+INSERT INTO [t12134] VALUES (6,'المطلب الرابع: حكم الاضطباع والرمل.','16','0','0');
+INSERT INTO [t12134] VALUES (7,'المطلب الخامس: وقت الاضطباع والرمل.','35','0','0');
+INSERT INTO [t12134] VALUES (8,'المطلب السادس: الطواف الذي يشرع فيه الاضطباع والرمل','52','0','0');
+INSERT INTO [t12134] VALUES (9,'المطلب السابع: من يشرع له الاضطباع والرمل.','77','0','0');
+INSERT INTO [t12134] VALUES (10,'الخاتمة','89','0','0');
+INSERT INTO [t12134] VALUES (11,'مصادر ومراجع','94','0','0');
