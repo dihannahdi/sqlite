aad0606facc3029e786756c9d89fb6726ecb5646
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:48:44 +0700

    Upload SQL chunk: t23743_chunk_0000.sql

---
+++ sql_chunks/t23743_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t23743
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t23743" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23743
+INSERT INTO [t23743] VALUES (1,'شرح المنظومة البيقونية (١)','2','0','0');
+INSERT INTO [t23743] VALUES (2,'شرح المنظومة البيقونية (٢)','39','0','0');
+INSERT INTO [t23743] VALUES (3,'شرح المنظومة البيقونية (٣)','73','0','0');
+INSERT INTO [t23743] VALUES (4,'شرح المنظومة البيقونية (٤)','105','0','0');
