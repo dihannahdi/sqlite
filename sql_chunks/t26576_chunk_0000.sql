450848365a19bae7ebf1ef6a0d588702dfed022a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:40 +0700

    Upload SQL chunk: t26576_chunk_0000.sql

---
+++ sql_chunks/t26576_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26576
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26576" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26576
+INSERT INTO [t26576] VALUES (1,'حديث محمد بن بشار بندار','1','0','0');
