07b43f584cc34c88d629fc03609bb2228150dedf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:51 +0700

    Upload SQL chunk: t6827_chunk_0000.sql

---
+++ sql_chunks/t6827_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t6827
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t6827" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6827
+INSERT INTO [t6827] VALUES (1,'فهذه كلمات مختصرة في أعمال القلوب التي تسمى المقامات والأحوال','1','0','0');
