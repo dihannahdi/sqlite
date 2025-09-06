c3e0e612584df453b1dab900fcc0bb6360e912f8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:22 +0700

    Upload SQL chunk: t21792_chunk_0000.sql

---
+++ sql_chunks/t21792_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t21792
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t21792" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21792
+INSERT INTO [t21792] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t21792] VALUES (2,'الإحرام والأخطاء فيه','2','0','0');
+INSERT INTO [t21792] VALUES (3,'الطواف والأخطاء الفعلية فيه','4','0','0');
+INSERT INTO [t21792] VALUES (4,'الطواف والأخطاء القولية فيه','6','0','0');
+INSERT INTO [t21792] VALUES (5,'الوقوف بعرفة والخطأ فيه','10','0','0');
+INSERT INTO [t21792] VALUES (6,'رمي الجمرات والخطأ فيه','11','0','0');
+INSERT INTO [t21792] VALUES (7,'طواف الوداع والأخطاء فيه','15','0','0');
