e876be2c26aa95122f6f9ce4bfd8ae3ded1d2988
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:59 +0700

    Upload SQL chunk: t5850_chunk_0000.sql

---
+++ sql_chunks/t5850_chunk_0000.sql
@@ -0,0 +1,63 @@
+-- Table: t5850
+-- Chunk: 1
+-- Rows: 56
+
+CREATE TABLE "t5850" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5850
+INSERT INTO [t5850] VALUES (1,'حرف الألف','1','0','0');
+INSERT INTO [t5850] VALUES (2,'حرف الباء','37','0','0');
+INSERT INTO [t5850] VALUES (3,'حرف التاء','47','0','0');
+INSERT INTO [t5850] VALUES (4,'حرف الثاء','50','0','0');
+INSERT INTO [t5850] VALUES (5,'حرف الجيم','55','0','0');
+INSERT INTO [t5850] VALUES (6,'حرف الحاء','64','0','0');
+INSERT INTO [t5850] VALUES (7,'حرف الخاء','105','0','0');
+INSERT INTO [t5850] VALUES (8,'حرف الدال','116','0','0');
+INSERT INTO [t5850] VALUES (9,'حرف الذال','120','0','0');
+INSERT INTO [t5850] VALUES (10,'حرف الزاي','135','0','0');
+INSERT INTO [t5850] VALUES (11,'حرف السين','144','0','0');
+INSERT INTO [t5850] VALUES (12,'حرف الشين','179','0','0');
+INSERT INTO [t5850] VALUES (13,'حرف الصاد','185','0','0');
+INSERT INTO [t5850] VALUES (14,'حرف الضاد','193','0','0');
+INSERT INTO [t5850] VALUES (15,'حرف الطاء','197','0','0');
+INSERT INTO [t5850] VALUES (16,'حرف الظاء','199','0','0');
+INSERT INTO [t5850] VALUES (17,'حرف العين','199','0','0');
+INSERT INTO [t5850] VALUES (18,'حرف الغين','305','0','0');
+INSERT INTO [t5850] VALUES (19,'حرف الفاء','308','0','0');
+INSERT INTO [t5850] VALUES (20,'حرف القاف','315','0','0');
+INSERT INTO [t5850] VALUES (21,'حرف الكاف','326','0','0');
+INSERT INTO [t5850] VALUES (22,'حرف اللام','334','0','0');
+INSERT INTO [t5850] VALUES (23,'حرف الميم','336','0','0');
+INSERT INTO [t5850] VALUES (24,'حرف النون','394','0','0');
+INSERT INTO [t5850] VALUES (25,'حرف الهاء','404','0','0');
+INSERT INTO [t5850] VALUES (26,'حرف الواو','414','0','0');
+INSERT INTO [t5850] VALUES (27,'حرف الياء','419','0','0');
+INSERT INTO [t5850] VALUES (28,'باب الكنى','440','0','0');
+INSERT INTO [t5850] VALUES (29,'حرف الألف','440','0','0');
+INSERT INTO [t5850] VALUES (30,'حرف الباء','447','0','0');
+INSERT INTO [t5850] VALUES (31,'حرف التاء','450','0','0');
+INSERT INTO [t5850] VALUES (32,'حرف الثاء','451','0','0');
+INSERT INTO [t5850] VALUES (33,'حرف الجيم','453','0','0');
+INSERT INTO [t5850] VALUES (34,'حرف الحار','454','0','0');
+INSERT INTO [t5850] VALUES (35,'حرف الخاء','459','0','0');
+INSERT INTO [t5850] VALUES (36,'حرف الدال','461','0','0');
+INSERT INTO [t5850] VALUES (37,'حرف الراء','463','0','0');
+INSERT INTO [t5850] VALUES (38,'حرف الزاي','466','0','0');
+INSERT INTO [t5850] VALUES (39,'حرف السين','467','0','0');
+INSERT INTO [t5850] VALUES (40,'حرف الشين','472','0','0');
+INSERT INTO [t5850] VALUES (41,'حرف الصاد','474','0','0');
+INSERT INTO [t5850] VALUES (42,'حرف الطاء','477','0','0');
+INSERT INTO [t5850] VALUES (43,'حرف العين','478','0','0');
+INSERT INTO [t5850] VALUES (44,'حرف الغين','488','0','0');
+INSERT INTO [t5850] VALUES (45,'حرف الفاء','490','0','0');
+INSERT INTO [t5850] VALUES (46,'حرف القاف','492','0','0');
+INSERT INTO [t5850] VALUES (47,'حرف الكاف','493','0','0');
+INSERT INTO [t5850] VALUES (48,'حرف الميم','495','0','0');
+INSERT INTO [t5850] VALUES (49,'حرف النون','500','0','0');
+INSERT INTO [t5850] VALUES (50,'حرف الهاء','501','0','0');
+INSERT INTO [t5850] VALUES (51,'حرف الواو','504','0','0');
+INSERT INTO [t5850] VALUES (52,'حرف الياء','505','0','0');
+INSERT INTO [t5850] VALUES (53,'معجم النساء','555','0','0');
+INSERT INTO [t5850] VALUES (54,'حرف الألف','555','0','0');
+INSERT INTO [t5850] VALUES (55,'الكنى','566','0','0');
+INSERT INTO [t5850] VALUES (56,'استدراكات الحافظ بن حجر','575','0','0');
