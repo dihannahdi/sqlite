fb11d5b253eb6ebd330136aa4b9b6897a07f44e0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:30 +0700

    Upload SQL chunk: t10498_chunk_0000.sql

---
+++ sql_chunks/t10498_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10498
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10498" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10498
+INSERT INTO [t10498] VALUES (1,'الجزء_الث..','2','0','0');
