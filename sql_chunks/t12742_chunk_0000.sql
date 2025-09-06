3738fd66a4aba507e35e4ba822a892aa98a6a91a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:23 +0700

    Upload SQL chunk: t12742_chunk_0000.sql

---
+++ sql_chunks/t12742_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t12742
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t12742" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12742
+INSERT INTO [t12742] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t12742] VALUES (2,'جزء فيه فضائل أبي بكر الصديق','2','0','0');
