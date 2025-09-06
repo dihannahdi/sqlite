2f810255117fedbd26ec783b0794cc249690e0f2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:07 +0700

    Upload SQL chunk: t36553_chunk_0000.sql

---
+++ sql_chunks/t36553_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t36553
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t36553" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36553
+INSERT INTO [t36553] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36553] VALUES (2,'مستشفى طائر للتنصير بتكلفة ٢٥ مليون دولار ...','3','0','0');
+INSERT INTO [t36553] VALUES (3,'وهم يعملون','6','0','0');
+INSERT INTO [t36553] VALUES (4,'كيف أخدم الإسلام؟','7','0','0');
+INSERT INTO [t36553] VALUES (5,'ماذا يعود علي إذا خدمت الإسلام؟','9','0','0');
+INSERT INTO [t36553] VALUES (6,'أين أثرك؟','10','0','0');
+INSERT INTO [t36553] VALUES (7,'كيف تخدم الإسلام','14','0','0');
+INSERT INTO [t36553] VALUES (8,'تأمل نعمة الله عليك','17','0','0');
+INSERT INTO [t36553] VALUES (9,'الدعوة إلى الله','18','0','0');
+INSERT INTO [t36553] VALUES (10,'كيف نصل بالدعوة؟','21','0','0');
+INSERT INTO [t36553] VALUES (11,'كيف أخدم الإسلام؟','22','0','0');
+INSERT INTO [t36553] VALUES (12,'متى تعمل؟','24','0','0');
+INSERT INTO [t36553] VALUES (13,'من خدمة الإسلام','25','0','0');
+INSERT INTO [t36553] VALUES (14,'لقد فتح لك الباب','29','0','0');
+INSERT INTO [t36553] VALUES (15,'احذر','30','0','0');
+INSERT INTO [t36553] VALUES (16,'في جنب هؤلاء','33','0','0');
+INSERT INTO [t36553] VALUES (17,'وقفات','34','0','0');
+INSERT INTO [t36553] VALUES (18,'همسة','36','0','0');
+INSERT INTO [t36553] VALUES (19,'في منتصف الطريق','38','0','0');
+INSERT INTO [t36553] VALUES (20,'الدعوة في بيوتنا','39','0','0');
+INSERT INTO [t36553] VALUES (21,'فتوى','41','0','0');
+INSERT INTO [t36553] VALUES (22,'من يحمل هم هذا الدين؟','42','0','0');
+INSERT INTO [t36553] VALUES (23,'لا تطلب الجزاء','43','0','0');
+INSERT INTO [t36553] VALUES (24,'تيسر أمر الدعوة','44','0','0');
+INSERT INTO [t36553] VALUES (25,'نكتة لطيفة','46','0','0');
+INSERT INTO [t36553] VALUES (26,'مواقف','47','0','0');
+INSERT INTO [t36553] VALUES (27,'إمام المسجد','50','0','0');
+INSERT INTO [t36553] VALUES (28,'الدعوة في المدارس','53','0','0');
+INSERT INTO [t36553] VALUES (29,'داعية بدون وسائل كاملة','55','0','0');
+INSERT INTO [t36553] VALUES (30,'من أعمال السائرين','56','0','0');
+INSERT INTO [t36553] VALUES (31,'تأمل معي','60','0','0');
+INSERT INTO [t36553] VALUES (32,'يا باغي الخير أقبل','62','0','0');
+INSERT INTO [t36553] VALUES (33,'ما الذي يمنعك؟','63','0','0');
+INSERT INTO [t36553] VALUES (34,'وقفات دعوية','65','0','0');
+INSERT INTO [t36553] VALUES (35,'النبي القدوة','68','0','0');
+INSERT INTO [t36553] VALUES (36,'وفي الختام','70','0','0');
