b1c54d5ea240bddf47ed0286e6039e0c1a1b218d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:04 +0700

    Upload SQL chunk: t37065_chunk_0000.sql

---
+++ sql_chunks/t37065_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t37065
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t37065" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37065
+INSERT INTO [t37065] VALUES (1,'الجزء الأول','1','0','0');
+INSERT INTO [t37065] VALUES (2,'الجزء الثاني','235','0','0');
