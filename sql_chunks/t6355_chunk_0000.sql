17a0dc2ea81142e92c0c0c4ed3b884efa4074afd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:06 +0700

    Upload SQL chunk: t6355_chunk_0000.sql

---
+++ sql_chunks/t6355_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t6355
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t6355" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6355
+INSERT INTO [t6355] VALUES (1,'الفصل الأول','2','0','0');
+INSERT INTO [t6355] VALUES (2,'الفصل الثاني','15','0','0');
+INSERT INTO [t6355] VALUES (3,'الفصل الثالث','52','0','0');
+INSERT INTO [t6355] VALUES (4,'الفصل الرابع','98','0','0');
+INSERT INTO [t6355] VALUES (5,'الفصل الخامس','123','0','0');
+INSERT INTO [t6355] VALUES (6,'الفصل السادس','142','0','0');
