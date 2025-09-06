37d5c8aa8b525c75d3830661cbd7d89b3adf48f7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:36 +0700

    Upload SQL chunk: t95865_chunk_0000.sql

---
+++ sql_chunks/t95865_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t95865
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t95865" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95865
+INSERT INTO [t95865] VALUES (1,'باب أسباب الميراث وشروطه وموانعه','2','0','0');
+INSERT INTO [t95865] VALUES (2,'باب الوارثين من الرجال والوارثات من النساء','3','0','0');
+INSERT INTO [t95865] VALUES (3,'باب الفروض المقدرة في كتاب الله وأهلها وقدر ما لكل','3','0','0');
+INSERT INTO [t95865] VALUES (4,'باب التعصيب وأقسامه','4','0','0');
+INSERT INTO [t95865] VALUES (5,'باب حجب النقص والإسقاط','5','0','0');
+INSERT INTO [t95865] VALUES (6,'فصل في حجب الإسقاط','5','0','0');
+INSERT INTO [t95865] VALUES (7,'باب الحمارية والمالكية','6','0','0');
+INSERT INTO [t95865] VALUES (8,'باب أحوال الجد','6','0','0');
+INSERT INTO [t95865] VALUES (9,'باب الأكدرية','7','0','0');
+INSERT INTO [t95865] VALUES (10,'باب الأصول السبعة','7','0','0');
+INSERT INTO [t95865] VALUES (11,'فصل في العول','8','0','0');
+INSERT INTO [t95865] VALUES (12,'باب الحساب','8','0','0');
+INSERT INTO [t95865] VALUES (13,'فصل في التصحيح','9','0','0');
+INSERT INTO [t95865] VALUES (14,'باب المناسخة','9','0','0');
+INSERT INTO [t95865] VALUES (15,'خاتمة في الخنثى والحمل والمفقود والإقرار','10','0','0');
