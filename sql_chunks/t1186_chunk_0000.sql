c23a47886c209e5d42f447fec46a71c0aef08e6d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:00:07 +0700

    Upload SQL chunk: t1186_chunk_0000.sql

---
+++ sql_chunks/t1186_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t1186
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t1186" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1186
+INSERT INTO [t1186] VALUES (1,'مقدمة المحقق','2','0','0');
+INSERT INTO [t1186] VALUES (2,'مقدمة ابن حجر','9','0','0');
+INSERT INTO [t1186] VALUES (3,'فصل','12','0','0');
+INSERT INTO [t1186] VALUES (4,'المرتبة الاولى','14','0','0');
+INSERT INTO [t1186] VALUES (5,'المرتبة الثانية','24','0','0');
+INSERT INTO [t1186] VALUES (6,'المرتبة الثالثة','35','0','0');
+INSERT INTO [t1186] VALUES (7,'المرتبة الرابعة','47','0','0');
+INSERT INTO [t1186] VALUES (8,'المرتبة الخامسة','50','0','0');
+INSERT INTO [t1186] VALUES (9,'فصل','56','0','0');
+INSERT INTO [t1186] VALUES (10,'ملحق باسماء من وصف بالتدليس ممن لم يذكروا في تعريف أهل التقديس بمراتب الموصوفين بالتدليس','58','0','0');
+INSERT INTO [t1186] VALUES (11,'منظومة الحافظ العلامة المؤرخ ابي عبد الله محمد بن احمد بن عثمان الذهبي','67','0','0');
+INSERT INTO [t1186] VALUES (12,'منظومة أبي محمود المقدسي','68','0','0');
