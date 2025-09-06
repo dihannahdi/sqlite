d154f4835dd316a5b5ed470f9eaf2cf2baa3252b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:22 +0700

    Upload SQL chunk: t10034_chunk_0000.sql

---
+++ sql_chunks/t10034_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t10034
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t10034" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10034
+INSERT INTO [t10034] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10034] VALUES (2,'الوقف في الإسلام','5','0','0');
+INSERT INTO [t10034] VALUES (3,'أثر الوقف في المجتمع المسلم','10','0','0');
+INSERT INTO [t10034] VALUES (4,'المدارس الوقفية في المدينة المنورة','20','0','0');
+INSERT INTO [t10034] VALUES (5,'مصادر ومراجع','41','0','0');
