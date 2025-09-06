1b54d0ed1bc916bf13cab99f60c71412c779d29e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:11 +0700

    Upload SQL chunk: t2121_chunk_0000.sql

---
+++ sql_chunks/t2121_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t2121
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t2121" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2121
+INSERT INTO [t2121] VALUES (1,'الكلام هو: اللفظ المركب المفيد بالوضع','11','0','0');
+INSERT INTO [t2121] VALUES (2,'(فصل: المعربات قسمان)','30','0','0');
+INSERT INTO [t2121] VALUES (3,'باب مرفوعات الأسماء','46','0','0');
+INSERT INTO [t2121] VALUES (4,'باب الفاعل','47','0','0');
+INSERT INTO [t2121] VALUES (5,'باب المبتدأ والخبر','51','0','0');
+INSERT INTO [t2121] VALUES (6,'باب العوامل الداخلة على المبتدأ والخبر','56','0','0');
+INSERT INTO [t2121] VALUES (7,'باب النعت','63','0','0');
+INSERT INTO [t2121] VALUES (8,'المعرفة والنكرة','66','0','0');
+INSERT INTO [t2121] VALUES (9,'باب العطف','71','0','0');
+INSERT INTO [t2121] VALUES (10,'باب التوكيد','77','0','0');
+INSERT INTO [t2121] VALUES (11,'باب البدل','79','0','0');
+INSERT INTO [t2121] VALUES (12,'باب منصوبات الأسماء','82','0','0');
+INSERT INTO [t2121] VALUES (13,'باب المفعول به','83','0','0');
+INSERT INTO [t2121] VALUES (14,'باب المصدر','85','0','0');
+INSERT INTO [t2121] VALUES (15,'باب ظرف الزمان وظرف المكان','88','0','0');
+INSERT INTO [t2121] VALUES (16,'باب الحال','91','0','0');
+INSERT INTO [t2121] VALUES (17,'باب التمييز','93','0','0');
+INSERT INTO [t2121] VALUES (18,'باب الاستثناء','94','0','0');
+INSERT INTO [t2121] VALUES (19,'باب '' لا ''','98','0','0');
+INSERT INTO [t2121] VALUES (20,'باب المنادى','99','0','0');
+INSERT INTO [t2121] VALUES (21,'باب المفعول من أجله','101','0','0');
+INSERT INTO [t2121] VALUES (22,'باب المفعول معه','102','0','0');
+INSERT INTO [t2121] VALUES (23,'باب مخفوضات الأسماء','103','0','0');
