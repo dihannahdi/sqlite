563cbb9efc5907fffeaa7fcb6760dcb152e38135
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:55 +0700

    Upload SQL chunk: t725_chunk_0000.sql

---
+++ sql_chunks/t725_chunk_0000.sql
@@ -0,0 +1,66 @@
+-- Table: t725
+-- Chunk: 1
+-- Rows: 59
+
+CREATE TABLE "t725" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t725
+INSERT INTO [t725] VALUES (1,'الباب الأول أخبار الديارات','1','0','0');
+INSERT INTO [t725] VALUES (2,'دير أبلق','1','1','0');
+INSERT INTO [t725] VALUES (3,'دير الأعلى','1','1','0');
+INSERT INTO [t725] VALUES (4,'دير الأنوار','2','1','0');
+INSERT INTO [t725] VALUES (5,'دير بولس','3','1','0');
+INSERT INTO [t725] VALUES (6,'دير الثعالب','4','1','0');
+INSERT INTO [t725] VALUES (7,'دير الثعالب','4','1','0');
+INSERT INTO [t725] VALUES (8,'دير الجاثليق','5','1','0');
+INSERT INTO [t725] VALUES (9,'دير الجماجم','6','1','0');
+INSERT INTO [t725] VALUES (10,'دير حزقيال','7','1','0');
+INSERT INTO [t725] VALUES (11,'دير حنة - الأكيراح','7','1','0');
+INSERT INTO [t725] VALUES (12,'دير حنة بالحيرة','8','1','0');
+INSERT INTO [t725] VALUES (13,'دير حنظلة الطائي','8','1','0');
+INSERT INTO [t725] VALUES (14,'دير حنظلة اللخمي','9','1','0');
+INSERT INTO [t725] VALUES (15,'دير الخصيان','9','1','0');
+INSERT INTO [t725] VALUES (16,'دير درزيجان','12','1','0');
+INSERT INTO [t725] VALUES (17,'دير الرصافة','12','1','0');
+INSERT INTO [t725] VALUES (18,'دير الرصافة','12','1','0');
+INSERT INTO [t725] VALUES (19,'في رصافة هشام بن عبد الملك','12','1','0');
+INSERT INTO [t725] VALUES (20,'دير الرها','13','1','0');
+INSERT INTO [t725] VALUES (21,'دير زرارة','13','1','0');
+INSERT INTO [t725] VALUES (22,'دير زكى','13','1','0');
+INSERT INTO [t725] VALUES (23,'دير سعد','14','1','0');
+INSERT INTO [t725] VALUES (24,'عمر سفر يشوع','14','1','0');
+INSERT INTO [t725] VALUES (25,'دير سليمان','15','1','0');
+INSERT INTO [t725] VALUES (26,'دير سمالو','15','1','0');
+INSERT INTO [t725] VALUES (27,'دير سمعان','15','1','0');
+INSERT INTO [t725] VALUES (28,'دير سمعان','16','1','0');
+INSERT INTO [t725] VALUES (29,'بدمشق','16','1','0');
+INSERT INTO [t725] VALUES (30,'دير السوسي','16','1','0');
+INSERT INTO [t725] VALUES (31,'دير صليبا','16','1','0');
+INSERT INTO [t725] VALUES (32,'دير صليبا','17','1','0');
+INSERT INTO [t725] VALUES (33,'دير عبد المسيح','18','1','0');
+INSERT INTO [t725] VALUES (34,'دير عدس','18','1','0');
+INSERT INTO [t725] VALUES (35,'دير العذارى','19','1','0');
+INSERT INTO [t725] VALUES (36,'دير العذارى','19','1','0');
+INSERT INTO [t725] VALUES (37,'دير علقمة','20','1','0');
+INSERT INTO [t725] VALUES (38,'دير فطرس ودير بولس','20','1','0');
+INSERT INTO [t725] VALUES (39,'دير القائم الأقصى','20','1','0');
+INSERT INTO [t725] VALUES (40,'دير قرة','21','1','0');
+INSERT INTO [t725] VALUES (41,'قلاية القس','22','1','0');
+INSERT INTO [t725] VALUES (42,'دير كعب','22','1','0');
+INSERT INTO [t725] VALUES (43,'دير لبى','22','1','0');
+INSERT INTO [t725] VALUES (44,'دير اللج','22','1','0');
+INSERT INTO [t725] VALUES (45,'دير مارة مريم','23','1','0');
+INSERT INTO [t725] VALUES (46,'بالحيرة','23','1','0');
+INSERT INTO [t725] VALUES (47,'دير مارة مريم بالشام','24','1','0');
+INSERT INTO [t725] VALUES (48,'دير ماسرجبيس','24','1','0');
+INSERT INTO [t725] VALUES (49,'دير ماسرجس','24','1','0');
+INSERT INTO [t725] VALUES (50,'دير الماطرون','24','1','0');
+INSERT INTO [t725] VALUES (51,'دير مران','25','1','0');
+INSERT INTO [t725] VALUES (52,'دير مران قرب دمشق','25','1','0');
+INSERT INTO [t725] VALUES (53,'دير مرماري','26','1','0');
+INSERT INTO [t725] VALUES (54,'دير المزعوق','27','1','0');
+INSERT INTO [t725] VALUES (55,'دير نجران','27','1','0');
+INSERT INTO [t725] VALUES (56,'دير هند الصغرى','27','1','0');
+INSERT INTO [t725] VALUES (57,'دير هند الكبرى','28','1','0');
+INSERT INTO [t725] VALUES (58,'الباب الثاني أخبار أديرة مجهولة','29','0','0');
+INSERT INTO [t725] VALUES (59,'الباب الثالث أعياد الشعانين','30','0','0');
