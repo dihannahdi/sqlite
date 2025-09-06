8672ac8d9fac2b7b209bba49e24a42c93fefe917
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:06 +0700

    Upload SQL chunk: t440_chunk_0000.sql

---
+++ sql_chunks/t440_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t440
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t440" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t440
+INSERT INTO [t440] VALUES (1,'مقدمتان بين يدي البحث في المسألة','2','0','0');
+INSERT INTO [t440] VALUES (2,'الأولى: التنفل يوم الجمعة قبل الزوال','2','1','0');
+INSERT INTO [t440] VALUES (3,'الثانية: تحقيق وقت الجمعة','4','1','0');
+INSERT INTO [t440] VALUES (4,'اختيار المؤلف في المسألة','12','0','0');
+INSERT INTO [t440] VALUES (5,'أدلة القائلين بالسنة القبلية للجمعة والجواب عنها','15','0','0');
+INSERT INTO [t440] VALUES (6,'خاتمة: في نصيحة للمثبتين','32','0','0');
