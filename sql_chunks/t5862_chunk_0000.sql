61c0c8c25d4fda41d771a74916d45892b7a47985
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:05 +0700

    Upload SQL chunk: t5862_chunk_0000.sql

---
+++ sql_chunks/t5862_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t5862
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t5862" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5862
+INSERT INTO [t5862] VALUES (1,'محمد بن إسماعيل','1','0','0');
+INSERT INTO [t5862] VALUES (2,'مولده','3','1','0');
+INSERT INTO [t5862] VALUES (3,'صفته','3','1','0');
+INSERT INTO [t5862] VALUES (4,'مناقبه','3','1','0');
+INSERT INTO [t5862] VALUES (5,'امتحانه','7','1','0');
+INSERT INTO [t5862] VALUES (6,'موته','15','1','0');
+INSERT INTO [t5862] VALUES (7,'حرف الألف','17','0','0');
+INSERT INTO [t5862] VALUES (8,'ومن حرف الألف أيضا من اسمه إبراهيم','37','7','0');
+INSERT INTO [t5862] VALUES (9,'ومن حرف الألف من اسمه إسماعيل','39','7','0');
+INSERT INTO [t5862] VALUES (10,'ومن حرف الألف من اسمه إسحاق','41','7','0');
+INSERT INTO [t5862] VALUES (11,'من اسمه آدم','46','7','0');
+INSERT INTO [t5862] VALUES (12,'من اسمه أسيد','48','7','0');
+INSERT INTO [t5862] VALUES (13,'من اسمه أصبغ','50','7','0');
+INSERT INTO [t5862] VALUES (14,'حرف الباء','52','0','0');
+INSERT INTO [t5862] VALUES (15,'حرف الثاء','57','0','0');
+INSERT INTO [t5862] VALUES (16,'حرف الجيم','59','0','0');
+INSERT INTO [t5862] VALUES (17,'حرف الحاء','60','0','0');
+INSERT INTO [t5862] VALUES (18,'حرف الخاء','69','0','0');
+INSERT INTO [t5862] VALUES (19,'حرف الدال','74','0','0');
+INSERT INTO [t5862] VALUES (20,'حرف الراء','75','0','0');
+INSERT INTO [t5862] VALUES (21,'حرف الزاي','76','0','0');
+INSERT INTO [t5862] VALUES (22,'حرف السين','79','0','0');
+INSERT INTO [t5862] VALUES (23,'حرف الشين','87','0','0');
+INSERT INTO [t5862] VALUES (24,'حرف الصاد','88','0','0');
+INSERT INTO [t5862] VALUES (25,'حرف الضاد','89','0','0');
+INSERT INTO [t5862] VALUES (26,'حرف الطاء','89','0','0');
+INSERT INTO [t5862] VALUES (27,'حرف العين','90','0','0');
+INSERT INTO [t5862] VALUES (28,'حرف الفاء','120','0','0');
+INSERT INTO [t5862] VALUES (29,'حرف القاف','123','0','0');
+INSERT INTO [t5862] VALUES (30,'حرف الميم','124','0','0');
+INSERT INTO [t5862] VALUES (31,'حرف النون','163','0','0');
+INSERT INTO [t5862] VALUES (32,'حرف الواو','164','0','0');
+INSERT INTO [t5862] VALUES (33,'حرف الياء','171','0','0');
