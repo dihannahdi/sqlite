31dcb50070df8a7ffe10f8e96abbc9ff82c1bb0f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:39 +0700

    Upload SQL chunk: t12548_chunk_0000.sql

---
+++ sql_chunks/t12548_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t12548
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t12548" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12548
+INSERT INTO [t12548] VALUES (1,'مجلس من أمالي النجاد و ٣ أحاديث من حديثه - مخطوط','2','0','0');
