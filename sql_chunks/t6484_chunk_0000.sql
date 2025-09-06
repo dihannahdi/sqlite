7761817abd6ae52dde622d0424c324a8969f9d6b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:00 +0700

    Upload SQL chunk: t6484_chunk_0000.sql

---
+++ sql_chunks/t6484_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t6484
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t6484" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6484
+INSERT INTO [t6484] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6484] VALUES (2,'بسم الله الرحمن الرحيم','4','0','0');
+INSERT INTO [t6484] VALUES (3,'العلو','5','0','0');
+INSERT INTO [t6484] VALUES (4,'القضاء والقدر','6','0','0');
+INSERT INTO [t6484] VALUES (5,'الملائكة','6','0','0');
+INSERT INTO [t6484] VALUES (6,'آدم عليه السلام','6','0','0');
+INSERT INTO [t6484] VALUES (7,'الجنة والنار','7','0','0');
+INSERT INTO [t6484] VALUES (8,'الإيمان','7','0','0');
+INSERT INTO [t6484] VALUES (9,'القرآن','8','0','0');
+INSERT INTO [t6484] VALUES (10,'الصفات','9','0','0');
+INSERT INTO [t6484] VALUES (11,'الآجال','10','0','0');
+INSERT INTO [t6484] VALUES (12,'القبر','10','0','0');
+INSERT INTO [t6484] VALUES (13,'النشور والحساب','11','0','0');
+INSERT INTO [t6484] VALUES (14,'الجنة','12','0','0');
+INSERT INTO [t6484] VALUES (15,'الرؤية','12','0','0');
+INSERT INTO [t6484] VALUES (16,'طاعة الأئمة والأمراء ومنع الخروج عليهم','14','0','0');
+INSERT INTO [t6484] VALUES (17,'الإمساك عن تكفير أهل القبلة','14','0','0');
+INSERT INTO [t6484] VALUES (18,'الصحابة','15','0','0');
+INSERT INTO [t6484] VALUES (19,'الصلاة وراء الأئمة والجهاد معهم والحج','17','0','0');
+INSERT INTO [t6484] VALUES (20,'قصر الصلاة والاختيار بين الصيام والإفطار في الأسفار','19','0','0');
+INSERT INTO [t6484] VALUES (21,'اجتماع أئمة الهدى الماضين على هذه المقالات','19','0','0');
+INSERT INTO [t6484] VALUES (22,'المحافظة على أداء الفرائض والرواتب واجتناب المحرمات','20','0','0');
