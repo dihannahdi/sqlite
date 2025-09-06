643eef5b52b70f507d3412dc4bf0c4fed61bb89a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:30 +0700

    Upload SQL chunk: t453_chunk_0000.sql

---
+++ sql_chunks/t453_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t453
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t453" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t453
+INSERT INTO [t453] VALUES (1,'مقدمة في سبب تأليف الرسالة وعظم خطر الحرية','2','0','0');
+INSERT INTO [t453] VALUES (2,'سبب الرق والنظر في تحققه في عصر المؤلف فما قبله','4','0','0');
+INSERT INTO [t453] VALUES (3,'كيف يثبت الرق','5','0','0');
+INSERT INTO [t453] VALUES (4,'نشوء بيع الأحرار في زمن المؤلف','12','0','0');
+INSERT INTO [t453] VALUES (5,'أقوال الشافعية في إقرار الرقيق بالرق أو الحرية','12','0','0');
