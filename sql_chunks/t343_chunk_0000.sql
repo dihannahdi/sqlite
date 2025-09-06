48fdfb3ad5f89d3fc912ceaba7a3a9c372ce550b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:05:38 +0700

    Upload SQL chunk: t343_chunk_0000.sql

---
+++ sql_chunks/t343_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t343
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t343" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t343
+INSERT INTO [t343] VALUES (1,'المسح على الجوربين والنعلين','1','0','0');
