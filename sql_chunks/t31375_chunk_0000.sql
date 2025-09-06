d2ca27ef4d4675a83a1dc9f7d5e50ca36718a7c0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:20 +0700

    Upload SQL chunk: t31375_chunk_0000.sql

---
+++ sql_chunks/t31375_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t31375
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t31375" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31375
+INSERT INTO [t31375] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t31375] VALUES (2,'الرسالة الأولى تحريم الغيبة','2','0','0');
+INSERT INTO [t31375] VALUES (3,'الإجماع على تحريم الغيبة','30','2','0');
+INSERT INTO [t31375] VALUES (4,'الرسالة الثانية المرهم الشافي للداء الخافي','54','0','0');
+INSERT INTO [t31375] VALUES (5,'النية','57','4','0');
+INSERT INTO [t31375] VALUES (6,'الإخلاص','57','4','0');
+INSERT INTO [t31375] VALUES (7,'التقوى','68','4','0');
+INSERT INTO [t31375] VALUES (8,'اليقين','72','4','0');
+INSERT INTO [t31375] VALUES (9,'الصبر','72','4','0');
+INSERT INTO [t31375] VALUES (10,'وأما الرضا','81','4','0');
+INSERT INTO [t31375] VALUES (11,'وأما التفويض','90','4','0');
+INSERT INTO [t31375] VALUES (12,'وأما الزهد','91','4','0');
+INSERT INTO [t31375] VALUES (13,'وأما القناعة','101','4','0');
