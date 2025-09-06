fc79b4c3018d2022983c4749ef146aac8b5ffcba
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:03 +0700

    Upload SQL chunk: t96177_chunk_0000.sql

---
+++ sql_chunks/t96177_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t96177
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t96177" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96177
+INSERT INTO [t96177] VALUES (1,'باب العقائد','4','0','0');
+INSERT INTO [t96177] VALUES (2,'باب الطهارة','6','0','0');
+INSERT INTO [t96177] VALUES (3,'إزالة النجاسة','7','2','0');
+INSERT INTO [t96177] VALUES (4,'الوضوء','7','2','0');
+INSERT INTO [t96177] VALUES (5,'قضاء الحاجة','9','2','0');
+INSERT INTO [t96177] VALUES (6,'نواقض الوضوء','10','2','0');
+INSERT INTO [t96177] VALUES (7,'الغسل','11','2','0');
+INSERT INTO [t96177] VALUES (8,'التيمم','12','2','0');
+INSERT INTO [t96177] VALUES (9,'المسح على الجبيرة والخفين','14','2','0');
+INSERT INTO [t96177] VALUES (10,'الحيض والنفاس','15','2','0');
+INSERT INTO [t96177] VALUES (11,'باب الصلاة','16','0','0');
+INSERT INTO [t96177] VALUES (12,'قضاء الفوائت','17','11','0');
+INSERT INTO [t96177] VALUES (13,'الأذان','18','11','0');
+INSERT INTO [t96177] VALUES (14,'شرائط الصلاة','19','11','0');
+INSERT INTO [t96177] VALUES (15,'فرائض الصلاة','20','11','0');
+INSERT INTO [t96177] VALUES (16,'باب السهو','24','11','0');
+INSERT INTO [t96177] VALUES (17,'الجماعة وشروط الإمام والمأموم','25','11','0');
+INSERT INTO [t96177] VALUES (18,'الجمعة','26','11','0');
+INSERT INTO [t96177] VALUES (19,'صلاة السفر','27','11','0');
+INSERT INTO [t96177] VALUES (20,'السنن الموكدات','28','11','0');
+INSERT INTO [t96177] VALUES (21,'الجنائز','31','11','0');
+INSERT INTO [t96177] VALUES (22,'باب الزكاة','32','0','0');
+INSERT INTO [t96177] VALUES (23,'باب الصوم','35','0','0');
+INSERT INTO [t96177] VALUES (24,'باب الاعتكاف','36','0','0');
+INSERT INTO [t96177] VALUES (25,'باب الحج','37','0','0');
+INSERT INTO [t96177] VALUES (26,'باب الأضحية والعقيقة والذكاة','41','0','0');
+INSERT INTO [t96177] VALUES (27,'باب النكاح والطلاق','42','0','0');
+INSERT INTO [t96177] VALUES (28,'العدل والقسم في المبيت','45','27','0');
+INSERT INTO [t96177] VALUES (29,'الطلاق','45','27','0');
+INSERT INTO [t96177] VALUES (30,'باب البيوع','47','0','0');
+INSERT INTO [t96177] VALUES (31,'باب الفرائض','48','0','0');
+INSERT INTO [t96177] VALUES (32,'موانع الميراث','51','31','0');
+INSERT INTO [t96177] VALUES (33,'باب جمل من الفرائض والآداب والأخلاق','51','0','0');
