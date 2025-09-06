83b4a766385953861a0705e394e65e475a75f64f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:17:33 +0700

    Upload SQL chunk: t14265_chunk_0000.sql

---
+++ sql_chunks/t14265_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t14265
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t14265" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14265
+INSERT INTO [t14265] VALUES (1,'مقدمة الطبعة الثالثة','4','0','0');
+INSERT INTO [t14265] VALUES (2,'مقدمة الطبعة الثانية','5','0','0');
+INSERT INTO [t14265] VALUES (3,'مقدمة الطبعة الأولى','13','0','0');
+INSERT INTO [t14265] VALUES (4,'أين الأقلام؟','14','0','0');
+INSERT INTO [t14265] VALUES (5,'إن هذا العلم دين','19','0','0');
+INSERT INTO [t14265] VALUES (6,'بطون جائعة وأموال ضائعة','25','0','0');
+INSERT INTO [t14265] VALUES (7,'مستقبل الأدب','30','0','0');
+INSERT INTO [t14265] VALUES (8,'رجل في ملابس النساء','36','0','0');
+INSERT INTO [t14265] VALUES (9,'وكم في مصر من بنات أمبان','42','0','0');
+INSERT INTO [t14265] VALUES (10,'تاجر حرب','47','0','0');
+INSERT INTO [t14265] VALUES (11,'إنذار','53','0','0');
+INSERT INTO [t14265] VALUES (12,'إلى القرية يا شباب','58','0','0');
+INSERT INTO [t14265] VALUES (13,'في منظار الخفيف','64','0','0');
+INSERT INTO [t14265] VALUES (14,'دفاع عن الأدب','71','0','0');
+INSERT INTO [t14265] VALUES (15,'إلى علماء الأزهر','74','0','0');
+INSERT INTO [t14265] VALUES (16,'الأمانة','80','0','0');
+INSERT INTO [t14265] VALUES (17,'دفاع عن الفضيلة','85','0','0');
+INSERT INTO [t14265] VALUES (18,'من أخلاقنا','92','0','0');
+INSERT INTO [t14265] VALUES (19,'داء الشباب','98','0','0');
+INSERT INTO [t14265] VALUES (20,'أخلاقنا','105','0','0');
+INSERT INTO [t14265] VALUES (21,'يا أيها الأغنياء','112','0','0');
+INSERT INTO [t14265] VALUES (22,'حق الضيافة','117','0','0');
+INSERT INTO [t14265] VALUES (23,'العربية والإسلامية','121','0','0');
+INSERT INTO [t14265] VALUES (24,'عربية إسلامية','131','0','0');
+INSERT INTO [t14265] VALUES (25,'في القهوة','136','0','0');
+INSERT INTO [t14265] VALUES (26,'أسئلة','143','0','0');
+INSERT INTO [t14265] VALUES (27,'أسلوب جديد في التعليم','148','0','0');
+INSERT INTO [t14265] VALUES (28,'مناظرة هادئة','155','0','0');
+INSERT INTO [t14265] VALUES (29,'لو أقر المجمع','163','0','0');
+INSERT INTO [t14265] VALUES (30,'المشكلة الكبرى (١)','169','0','0');
+INSERT INTO [t14265] VALUES (31,'المشكلة الكبرى (٢)','177','0','0');
+INSERT INTO [t14265] VALUES (32,'إلى علماء مصر','185','0','0');
+INSERT INTO [t14265] VALUES (33,'الأدباء الرسميون','190','0','0');
+INSERT INTO [t14265] VALUES (34,'بين الزوجين','194','0','0');
+INSERT INTO [t14265] VALUES (35,'صديقي رمضان','200','0','0');
+INSERT INTO [t14265] VALUES (36,'النشيد السوري','204','0','0');
