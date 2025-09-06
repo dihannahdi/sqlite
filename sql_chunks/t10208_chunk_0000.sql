da37c15e73a81412dddaa7b3eb7427b510e0eef2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:19 +0700

    Upload SQL chunk: t10208_chunk_0000.sql

---
+++ sql_chunks/t10208_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t10208
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t10208" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10208
+INSERT INTO [t10208] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10208] VALUES (2,'حقيقة علم الأصول','2','0','0');
+INSERT INTO [t10208] VALUES (3,'مباحث علم الأصول إجمالا','9','0','0');
+INSERT INTO [t10208] VALUES (4,'الحكم الشرعي','11','0','0');
+INSERT INTO [t10208] VALUES (5,'الحاكم','13','0','0');
+INSERT INTO [t10208] VALUES (6,'التكليف والمكلف','14','0','0');
+INSERT INTO [t10208] VALUES (7,'أفعال العباد','15','0','0');
+INSERT INTO [t10208] VALUES (8,'تعليل الأحكام','16','0','0');
+INSERT INTO [t10208] VALUES (9,'الكتاب','17','0','0');
+INSERT INTO [t10208] VALUES (10,'السنة النبوية','18','0','0');
+INSERT INTO [t10208] VALUES (11,'الإجماع','20','0','0');
+INSERT INTO [t10208] VALUES (12,'الألفاظ وتفسيرها','21','0','0');
+INSERT INTO [t10208] VALUES (13,'القياس والمصادر العقلية الملحقة به','22','0','0');
+INSERT INTO [t10208] VALUES (14,'الإجتهاد','23','0','0');
+INSERT INTO [t10208] VALUES (15,'التقليد','24','0','0');
+INSERT INTO [t10208] VALUES (16,'مباحث متفرقة','25','0','0');
+INSERT INTO [t10208] VALUES (17,'أهداف الشريعة العامة ومبادئها','26','0','0');
+INSERT INTO [t10208] VALUES (18,'الثبات والمرونة في شريعة الإسلام','27','0','0');
