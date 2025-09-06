4c1863c9c4f09fdb76b77d4a6439e55b334e1422
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:21 +0700

    Upload SQL chunk: t447_chunk_0000.sql

---
+++ sql_chunks/t447_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t447
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t447" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t447
+INSERT INTO [t447] VALUES (1,'الأحاديث في وصف سيره - صلى الله عليه وسلم - في الحج','2','0','0');
+INSERT INTO [t447] VALUES (2,'الحكمة من الإسراع في وادي محسر','5','0','0');
+INSERT INTO [t447] VALUES (3,'فصل (هل يعتبر وادي محسر تابعا لمنى أم مزدلفة أم منفصلا عنهما؟)','8','0','0');
