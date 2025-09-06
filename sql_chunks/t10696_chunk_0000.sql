d9eb443fe778a80b5e551a3eebcbc8958754d493
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:50:41 +0700

    Upload SQL chunk: t10696_chunk_0000.sql

---
+++ sql_chunks/t10696_chunk_0000.sql
@@ -0,0 +1,51 @@
+-- Table: t10696
+-- Chunk: 1
+-- Rows: 44
+
+CREATE TABLE "t10696" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10696
+INSERT INTO [t10696] VALUES (1,'بسم الله الرحمن الرحيم وبه الإعانة','1','0','0');
+INSERT INTO [t10696] VALUES (2,'باب حرف الألف','14','0','0');
+INSERT INTO [t10696] VALUES (3,'حكاية الرؤيا المعبرة','16','0','0');
+INSERT INTO [t10696] VALUES (4,'اعتكاف','18','0','0');
+INSERT INTO [t10696] VALUES (5,'باب حرف الباء','30','0','0');
+INSERT INTO [t10696] VALUES (6,'باب حرف التاء','42','0','0');
+INSERT INTO [t10696] VALUES (7,'باب حرف الثاء','46','0','0');
+INSERT INTO [t10696] VALUES (8,'باب حرف الجيم','49','0','0');
+INSERT INTO [t10696] VALUES (9,'ما جاء في الحيوان على حرف الجيم','55','0','0');
+INSERT INTO [t10696] VALUES (10,'باب حرف الحاء','59','0','0');
+INSERT INTO [t10696] VALUES (11,'ما جاء في الحيوان على حرف الحاء','68','0','0');
+INSERT INTO [t10696] VALUES (12,'باب حرف الخاء','72','0','0');
+INSERT INTO [t10696] VALUES (13,'باب حرف الدال','85','0','0');
+INSERT INTO [t10696] VALUES (14,'باب حرف الذال','98','0','0');
+INSERT INTO [t10696] VALUES (15,'باب حرف الراء','100','0','0');
+INSERT INTO [t10696] VALUES (16,'باب حرف الزاي','110','0','0');
+INSERT INTO [t10696] VALUES (17,'باب حرف السين','117','0','0');
+INSERT INTO [t10696] VALUES (18,'باب في سور القرآن العظيم لتاليها','145','0','0');
+INSERT INTO [t10696] VALUES (19,'باب حرف الشين','154','0','0');
+INSERT INTO [t10696] VALUES (20,'باب حرف الصاد','172','0','0');
+INSERT INTO [t10696] VALUES (21,'باب حرف الضاد','186','0','0');
+INSERT INTO [t10696] VALUES (22,'باب حرف الطاء','189','0','0');
+INSERT INTO [t10696] VALUES (23,'باب حرف الظاء','195','0','0');
+INSERT INTO [t10696] VALUES (24,'باب حرف العين','197','0','0');
+INSERT INTO [t10696] VALUES (25,'باب حرف الغين','213','0','0');
+INSERT INTO [t10696] VALUES (26,'باب حرف الفاء','219','0','0');
+INSERT INTO [t10696] VALUES (27,'باب حرف القاف','228','0','0');
+INSERT INTO [t10696] VALUES (28,'باب حرف الكاف','245','0','0');
+INSERT INTO [t10696] VALUES (29,'باب حرف اللام','257','0','0');
+INSERT INTO [t10696] VALUES (30,'باب حرف الميم','266','0','0');
+INSERT INTO [t10696] VALUES (31,'ذكر المدائن','289','0','0');
+INSERT INTO [t10696] VALUES (32,'باب حرف النون','291','0','0');
+INSERT INTO [t10696] VALUES (33,'ما جاء في الحيوان على حرف النون','294','0','0');
+INSERT INTO [t10696] VALUES (34,'باب حرف الواو','306','0','0');
+INSERT INTO [t10696] VALUES (35,'باب حرف الهاء','311','0','0');
+INSERT INTO [t10696] VALUES (36,'باب حرف الياء','315','0','0');
+INSERT INTO [t10696] VALUES (37,'باب في الصناع على حرف الألف','320','0','0');
+INSERT INTO [t10696] VALUES (38,'باب حرف الفاء','327','0','0');
+INSERT INTO [t10696] VALUES (39,'باب حرف الكاف','328','0','0');
+INSERT INTO [t10696] VALUES (40,'باب حرف اللام','329','0','0');
+INSERT INTO [t10696] VALUES (41,'باب حرف الميم','329','0','0');
+INSERT INTO [t10696] VALUES (42,'باب حرف النون','330','0','0');
+INSERT INTO [t10696] VALUES (43,'باب حرف الواو','331','0','0');
+INSERT INTO [t10696] VALUES (44,'باب حرف الهاء','331','0','0');
