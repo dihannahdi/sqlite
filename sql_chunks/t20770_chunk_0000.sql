66b622df60b2baad2b072c2205a41a9f34cc2b97
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:36:58 +0700

    Upload SQL chunk: t20770_chunk_0000.sql

---
+++ sql_chunks/t20770_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t20770
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t20770" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t20770
+INSERT INTO [t20770] VALUES (1,'قال السيوطي في "الإتقان"','2','0','0');
+INSERT INTO [t20770] VALUES (2,'قال الشيخ رحمه الله في شرحها','2','0','0');
