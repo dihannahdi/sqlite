3b268683c32b00c8b6bee12446eeb589afc3d082
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:36:57 +0700

    Upload SQL chunk: t20769_chunk_0000.sql

---
+++ sql_chunks/t20769_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t20769
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t20769" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t20769
+INSERT INTO [t20769] VALUES (1,'اعلم أولا أن المصالح التي عليها مدار التشريع السماوي ثلاث','2','0','0');
+INSERT INTO [t20769] VALUES (2,'المصلحة بالإضافة إلى شهادة الشرع ثلاثة أقسام','12','0','0');
