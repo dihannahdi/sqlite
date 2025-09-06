77d64a447a39e336e09ef78f7b7a3efb6777fe35
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:17:47 +0700

    Upload SQL chunk: t14296_chunk_0000.sql

---
+++ sql_chunks/t14296_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t14296
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t14296" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14296
+INSERT INTO [t14296] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t14296] VALUES (2,'نحن المسلمين','15','0','0');
+INSERT INTO [t14296] VALUES (3,'في بيت المقدس','23','0','0');
+INSERT INTO [t14296] VALUES (4,'وديعة الله','38','0','0');
+INSERT INTO [t14296] VALUES (5,'محمد الصغير','51','0','0');
+INSERT INTO [t14296] VALUES (6,'ابن الحب','60','0','0');
+INSERT INTO [t14296] VALUES (7,'قضية سمرقند','82','0','0');
+INSERT INTO [t14296] VALUES (8,'هيلانة ولويس','103','0','0');
+INSERT INTO [t14296] VALUES (9,'سيدة من بني أمية','121','0','0');
+INSERT INTO [t14296] VALUES (10,'ثلاثون ألف دينار','131','0','0');
+INSERT INTO [t14296] VALUES (11,'هند والمغيرة','148','0','0');
+INSERT INTO [t14296] VALUES (12,'هجرة معلم','156','0','0');
+INSERT INTO [t14296] VALUES (13,'ليلة الوداع','184','0','0');
+INSERT INTO [t14296] VALUES (14,'يوم اللقاء','206','0','0');
+INSERT INTO [t14296] VALUES (15,'عشية وضحاها','230','0','0');
+INSERT INTO [t14296] VALUES (16,'آخر أبطال غرناطة','244','0','0');
+INSERT INTO [t14296] VALUES (17,'رجل وامرأة','251','0','0');
+INSERT INTO [t14296] VALUES (18,'عالم','259','0','0');
+INSERT INTO [t14296] VALUES (19,'مع النابغة الذبياني','267','0','0');
+INSERT INTO [t14296] VALUES (20,'في صحن الأموي','277','0','0');
+INSERT INTO [t14296] VALUES (21,'حكاية الهميان','285','0','0');
+INSERT INTO [t14296] VALUES (22,'على أبواب المدينة','299','0','0');
+INSERT INTO [t14296] VALUES (23,'طالب علم','307','0','0');
+INSERT INTO [t14296] VALUES (24,'أبو جهل','314','0','0');
+INSERT INTO [t14296] VALUES (25,'من آثار المؤلف','332','0','0');
