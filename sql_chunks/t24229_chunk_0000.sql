33171479c304a2a137014d0823da17302dd3e445
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:16 +0700

    Upload SQL chunk: t24229_chunk_0000.sql

---
+++ sql_chunks/t24229_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t24229
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t24229" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t24229
+INSERT INTO [t24229] VALUES (1,'مجلس أبي نصر الغازي','2','0','0');
