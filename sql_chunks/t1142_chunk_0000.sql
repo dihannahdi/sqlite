22e2185997304501a4dba4d3bfb3b04007b2f762
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:16 +0700

    Upload SQL chunk: t1142_chunk_0000.sql

---
+++ sql_chunks/t1142_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t1142
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t1142" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1142
+INSERT INTO [t1142] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t1142] VALUES (2,'كتاب الطهارة','7','0','0');
+INSERT INTO [t1142] VALUES (3,'[كتاب الصيام]','3010','0','0');
+INSERT INTO [t1142] VALUES (4,'(كتاب البيع)','3925','0','0');
+INSERT INTO [t1142] VALUES (5,'كتاب السلم','4531','0','0');
+INSERT INTO [t1142] VALUES (6,'(كتاب الرهن)','4733','0','0');
+INSERT INTO [t1142] VALUES (7,'(كتاب الحجر)','5006','0','0');
+INSERT INTO [t1142] VALUES (8,'(كتاب الصلح)','5025','0','0');
+INSERT INTO [t1142] VALUES (9,'(كتاب الحوالة)','5068','0','0');
+INSERT INTO [t1142] VALUES (10,'(كتاب الشركة)','5135','0','0');
+INSERT INTO [t1142] VALUES (11,'(كتاب الوكالة)','5190','0','0');
+INSERT INTO [t1142] VALUES (12,'(كتاب الاقرار.)','5277','0','0');
+INSERT INTO [t1142] VALUES (13,'(كتاب العارية)','5397','0','0');
+INSERT INTO [t1142] VALUES (14,'(كتاب الغصب)','5427','0','0');
+INSERT INTO [t1142] VALUES (15,'(كتاب الشفعة)','5549','0','0');
+INSERT INTO [t1142] VALUES (16,'(كتاب المساقاة)','5791','0','0');
