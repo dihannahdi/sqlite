db021779e2ea2a0d7498163ee3795646ed3a40c0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:18 +0700

    Upload SQL chunk: t26039_chunk_0000.sql

---
+++ sql_chunks/t26039_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t26039
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t26039" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26039
+INSERT INTO [t26039] VALUES (1,'كتاب الطهارة','1','0','0');
+INSERT INTO [t26039] VALUES (2,'كتاب الصلاة','36','0','0');
+INSERT INTO [t26039] VALUES (3,'كتاب الجنائز','121','0','0');
+INSERT INTO [t26039] VALUES (4,'كتاب الزكاة','140','0','0');
+INSERT INTO [t26039] VALUES (5,'كتاب الصيام','162','0','0');
+INSERT INTO [t26039] VALUES (6,'كتاب الحج','185','0','0');
+INSERT INTO [t26039] VALUES (7,'كتاب البيوع','235','0','0');
+INSERT INTO [t26039] VALUES (8,'كتاب الفرائض','270','0','0');
+INSERT INTO [t26039] VALUES (9,'كتاب النكاح','279','0','0');
+INSERT INTO [t26039] VALUES (10,'كتاب الجنايات','331','0','0');
+INSERT INTO [t26039] VALUES (11,'كتاب الجهاد','352','0','0');
+INSERT INTO [t26039] VALUES (12,'كتاب الحدود','393','0','0');
+INSERT INTO [t26039] VALUES (13,'كتاب الامارة','415','0','0');
+INSERT INTO [t26039] VALUES (14,'كتاب الاقضية','432','0','0');
+INSERT INTO [t26039] VALUES (15,'كتاب التفسير','449','0','0');
+INSERT INTO [t26039] VALUES (16,'كتاب الجامع','495','0','0');
+INSERT INTO [t26039] VALUES (17,'كتاب الإيمان','500','0','0');
+INSERT INTO [t26039] VALUES (18,'كتاب الأدب','521','0','0');
+INSERT INTO [t26039] VALUES (19,'كتاب الملاحم','526','0','0');
+INSERT INTO [t26039] VALUES (20,'كتاب المعجزات والمناقب والفضائل','537','0','0');
