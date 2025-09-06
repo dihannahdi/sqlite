7480f70df8c9a5824570eba1df4cd67d79c85982
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:58 +0700

    Upload SQL chunk: t8762_chunk_0000.sql

---
+++ sql_chunks/t8762_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t8762
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t8762" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8762
+INSERT INTO [t8762] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8762] VALUES (2,'تأثيرات كامنة','8','0','0');
+INSERT INTO [t8762] VALUES (3,'تجاهل مقصود','9','0','0');
+INSERT INTO [t8762] VALUES (4,'الإعلام المقنع','10','0','0');
+INSERT INTO [t8762] VALUES (5,'صراع وصراع','11','0','0');
+INSERT INTO [t8762] VALUES (6,'تصحيح المفاهيم','16','0','0');
+INSERT INTO [t8762] VALUES (7,'ميدان الدعوة','17','0','0');
