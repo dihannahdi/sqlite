eb37832d5c51c168a251c3fb893f57245f856652
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:58 +0700

    Upload SQL chunk: t26872_chunk_0000.sql

---
+++ sql_chunks/t26872_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t26872
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t26872" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26872
+INSERT INTO [t26872] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26872] VALUES (2,'المحبة لله','2','0','0');
+INSERT INTO [t26872] VALUES (3,'باب من كان يسأل الله تعالى أن يرزقه حبه','75','0','0');
+INSERT INTO [t26872] VALUES (4,'الجزء الثاني','154','0','0');
