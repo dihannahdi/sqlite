7b4826512a319b56a502bc0b8c738398de991da5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:18 +0700

    Upload SQL chunk: t12737_chunk_0000.sql

---
+++ sql_chunks/t12737_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t12737
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t12737" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12737
+INSERT INTO [t12737] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t12737] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t12737] VALUES (3,'باب صفة شجرة الجنة','44','0','0');
+INSERT INTO [t12737] VALUES (4,'باب شجرة طوبى','56','0','0');
+INSERT INTO [t12737] VALUES (5,'أنهار الجنة','68','0','0');
+INSERT INTO [t12737] VALUES (6,'باب طعام أهل الجنة','103','0','0');
+INSERT INTO [t12737] VALUES (7,'باب شراب أهل الجنة','122','0','0');
+INSERT INTO [t12737] VALUES (8,'باب لباس أهل الجنة','149','0','0');
+INSERT INTO [t12737] VALUES (9,'باب فراش أهل الجنة','157','0','0');
+INSERT INTO [t12737] VALUES (10,'باب قصور أهل الجنة','174','0','0');
+INSERT INTO [t12737] VALUES (11,'باب درجات أهل الجنة','185','0','0');
+INSERT INTO [t12737] VALUES (12,'باب ملك أهل الجنة','202','0','0');
+INSERT INTO [t12737] VALUES (13,'باب خدم أهل الجنة','210','0','0');
+INSERT INTO [t12737] VALUES (14,'باب لسان أهل الجنة','217','0','0');
+INSERT INTO [t12737] VALUES (15,'باب حلي أهل الجنة','222','0','0');
+INSERT INTO [t12737] VALUES (16,'أبواب أهل الجنة','226','0','0');
+INSERT INTO [t12737] VALUES (17,'باب تزاور أهل الجنة ومنتزهاتهم','244','0','0');
+INSERT INTO [t12737] VALUES (18,'باب سوق أهل الجنة','254','0','0');
+INSERT INTO [t12737] VALUES (19,'باب غناء أهل الجنة','259','0','0');
+INSERT INTO [t12737] VALUES (20,'باب جماع أهل الجنة','269','0','0');
+INSERT INTO [t12737] VALUES (21,'باب الحور العين','292','0','0');
+INSERT INTO [t12737] VALUES (22,'باب صفة الحور العين','300','0','0');
+INSERT INTO [t12737] VALUES (23,'باب جامع من ذكر الجنة','366','0','0');
