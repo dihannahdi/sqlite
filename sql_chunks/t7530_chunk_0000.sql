dc87de868d17a232e94881437b4853cee6aacda8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:29 +0700

    Upload SQL chunk: t7530_chunk_0000.sql

---
+++ sql_chunks/t7530_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7530
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7530" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7530
+INSERT INTO [t7530] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7530] VALUES (2,'أسباب اكتساب حسن الخلق','3','0','0');
