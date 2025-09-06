f76cf1b81fa0fdae0dc224ab378228b94d3d9774
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:46:43 +0700

    Upload SQL chunk: t851_chunk_0000.sql

---
+++ sql_chunks/t851_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t851
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t851" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t851
+INSERT INTO [t851] VALUES (1,'باب الالف','3','0','0');
+INSERT INTO [t851] VALUES (2,'باب الباء الموحدة','163','0','0');
+INSERT INTO [t851] VALUES (3,'باب التاء المثناة','210','0','0');
+INSERT INTO [t851] VALUES (4,'باب الثاء المثلثة','346','0','0');
+INSERT INTO [t851] VALUES (5,'باب الجيم الموحدة','350','0','0');
+INSERT INTO [t851] VALUES (6,'باب الحاء المهملة','390','0','0');
+INSERT INTO [t851] VALUES (7,'باب الخاء المعجمة','427','0','0');
+INSERT INTO [t851] VALUES (8,'باب الدال المهملة','443','0','0');
+INSERT INTO [t851] VALUES (9,'باب الذال المعجمة','541','0','0');
+INSERT INTO [t851] VALUES (10,'باب الراء المهملة','547','0','0');
+INSERT INTO [t851] VALUES (11,'باب الزاء المعجمة','607','0','0');
+INSERT INTO [t851] VALUES (12,'باب السين المهملة','626','0','0');
+INSERT INTO [t851] VALUES (13,'باب الشين المعجمة','662','0','0');
+INSERT INTO [t851] VALUES (14,'باب الصاد المهملة','687','0','0');
+INSERT INTO [t851] VALUES (15,'باب الضاد المعجمة','698','0','0');
+INSERT INTO [t851] VALUES (16,'باب الطاء المهملة','701','0','0');
+INSERT INTO [t851] VALUES (17,'باب الظاء المعجمة','715','0','0');
+INSERT INTO [t851] VALUES (18,'باب العين المهملة','715','0','0');
+INSERT INTO [t851] VALUES (19,'باب الغين المعجمة','760','0','0');
+INSERT INTO [t851] VALUES (20,'باب الفاء','777','0','0');
+INSERT INTO [t851] VALUES (21,'باب القاف','842','0','0');
+INSERT INTO [t851] VALUES (22,'باب الكاف','882','0','0');
+INSERT INTO [t851] VALUES (23,'باب اللام','1021','0','0');
+INSERT INTO [t851] VALUES (24,'باب الميم','1043','0','0');
+INSERT INTO [t851] VALUES (25,'باب النون','1238','0','0');
+INSERT INTO [t851] VALUES (26,'باب الواو','1324','0','0');
+INSERT INTO [t851] VALUES (27,'باب الهاء','1340','0','0');
+INSERT INTO [t851] VALUES (28,'باب الياء المثناة','1355','0','0');
