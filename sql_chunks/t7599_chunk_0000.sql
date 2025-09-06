19e381db592e297f7a4eb76d591f91a4a0712543
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:23 +0700

    Upload SQL chunk: t7599_chunk_0000.sql

---
+++ sql_chunks/t7599_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t7599
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t7599" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7599
+INSERT INTO [t7599] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7599] VALUES (2,'تمهيد','7','0','0');
+INSERT INTO [t7599] VALUES (3,'المبحث الأول: في عصمة الأنبياء','11','0','0');
+INSERT INTO [t7599] VALUES (4,'المبحث الثاني: في اجتهاد الأنبياء عليهم السلام','21','0','0');
+INSERT INTO [t7599] VALUES (5,'المبحث الثالث: بسط أدلة القول بأن السنة وحي كالقرآن','43','0','0');
+INSERT INTO [t7599] VALUES (6,'الخاتمة','66','0','0');
+INSERT INTO [t7599] VALUES (7,'مصادر ومراجع','68','0','0');
