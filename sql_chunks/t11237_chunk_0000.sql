26743997d15451a9b723a487cbd9cde051c3d332
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:48 +0700

    Upload SQL chunk: t11237_chunk_0000.sql

---
+++ sql_chunks/t11237_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t11237
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t11237" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11237
+INSERT INTO [t11237] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t11237] VALUES (2,'سؤالات البرقاني للدارقطني رواية الكرجي عنه','12','0','0');
+INSERT INTO [t11237] VALUES (3,'فهرس المصادر','80','0','0');
