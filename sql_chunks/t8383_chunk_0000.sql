ae03d2a5e96acda347b7a76caba4aef7606d7803
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:46:08 +0700

    Upload SQL chunk: t8383_chunk_0000.sql

---
+++ sql_chunks/t8383_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t8383
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t8383" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8383
+INSERT INTO [t8383] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8383] VALUES (2,'باب أحكام المياة','3','0','0');
+INSERT INTO [t8383] VALUES (3,'باب الآنية','7','0','0');
+INSERT INTO [t8383] VALUES (4,'باب الاستنجاء','11','0','0');
+INSERT INTO [t8383] VALUES (5,'باب السواك وسنن الوضوء','16','0','0');
+INSERT INTO [t8383] VALUES (6,'باب فروض الوضوء وصفته','19','0','0');
+INSERT INTO [t8383] VALUES (7,'باب المسح على الخفين','24','0','0');
+INSERT INTO [t8383] VALUES (8,'باب نواقض الوضوء','28','0','0');
+INSERT INTO [t8383] VALUES (9,'باب الغسل','33','0','0');
+INSERT INTO [t8383] VALUES (10,'باب التيمم','37','0','0');
+INSERT INTO [t8383] VALUES (11,'باب إزالة النجاسة الحكمية','41','0','0');
+INSERT INTO [t8383] VALUES (12,'باب الحيض','44','0','0');
+INSERT INTO [t8383] VALUES (13,'باب النفاس','50','0','0');
