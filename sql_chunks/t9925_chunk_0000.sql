37111b2177ea8353dfe561e4c82d663688bf0ed5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:03 +0700

    Upload SQL chunk: t9925_chunk_0000.sql

---
+++ sql_chunks/t9925_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t9925
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t9925" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9925
+INSERT INTO [t9925] VALUES (1,'السنة والتشريع','1','0','0');
+INSERT INTO [t9925] VALUES (2,'[مقدمة]:','2','0','0');
+INSERT INTO [t9925] VALUES (3,'الصلاة على النبي - صلى الله عليه وسلم -:','13','0','0');
+INSERT INTO [t9925] VALUES (4,'إساءة المستشرقين:','14','0','0');
+INSERT INTO [t9925] VALUES (5,'الشبهات:','16','0','0');
+INSERT INTO [t9925] VALUES (6,'اجتهاد الرسول - صلى الله عليه وسلم -:','17','0','0');
+INSERT INTO [t9925] VALUES (7,'اللفظ والمراد منه:','19','0','0');
+INSERT INTO [t9925] VALUES (8,'السنة: تشريع وغير تشريع:','21','0','0');
+INSERT INTO [t9925] VALUES (9,'المعاملات حلال وحرام وتشريع من لله:','27','0','0');
+INSERT INTO [t9925] VALUES (10,'حديث تأبير النخل:','30','0','0');
+INSERT INTO [t9925] VALUES (11,'هواة التحلل:','31','0','0');
+INSERT INTO [t9925] VALUES (12,'احتمال دخول المعاملات مستبعد:','37','0','0');
+INSERT INTO [t9925] VALUES (13,'شبهات أخر:','46','0','0');
