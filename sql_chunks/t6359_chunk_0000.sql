dc97c3a274fa1b011e981b5ba8757b2ac81fa08f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:09 +0700

    Upload SQL chunk: t6359_chunk_0000.sql

---
+++ sql_chunks/t6359_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t6359
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t6359" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6359
+INSERT INTO [t6359] VALUES (1,'أدلة القائلين بجواز التقليد','2','0','0');
+INSERT INTO [t6359] VALUES (2,'أقوال العلماء في النهي عن التقليد','25','0','0');
+INSERT INTO [t6359] VALUES (3,'أقوال الأئمة الأربعة في النهي عن التقليد','38','0','0');
+INSERT INTO [t6359] VALUES (4,'القول بإنسداد باب الاجتهاد بدعة شنيعة','46','0','0');
+INSERT INTO [t6359] VALUES (5,'إبطال التقليد','55','0','0');
+INSERT INTO [t6359] VALUES (6,'معنى أن كل مجتهد مصيب','71','0','0');
+INSERT INTO [t6359] VALUES (7,'أسئلة للمقلدين','73','0','0');
+INSERT INTO [t6359] VALUES (8,'نصيحة بليغة لمن يتصدر للفتيا والقضاء من المقلدين','81','0','0');
