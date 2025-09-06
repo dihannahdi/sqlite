fa0e20c2e4c730d5ec2e83b607cf9e951919c1fa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:20 +0700

    Upload SQL chunk: t8208_chunk_0000.sql

---
+++ sql_chunks/t8208_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t8208
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t8208" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8208
+INSERT INTO [t8208] VALUES (1,'باب ما يكون بدعة','8','0','0');
+INSERT INTO [t8208] VALUES (2,'باب " كل محدثة بدعة "','54','0','0');
+INSERT INTO [t8208] VALUES (3,'باب إحداث البدع','83','0','0');
+INSERT INTO [t8208] VALUES (4,'باب تغير البدع','90','0','0');
+INSERT INTO [t8208] VALUES (5,'ما جاء في اتباع الآثار','104','0','0');
+INSERT INTO [t8208] VALUES (6,'ما جاء في ليلة النصف من شعبان','113','0','0');
+INSERT INTO [t8208] VALUES (7,'كراهية اجتماع الناس عشية عرفة','116','0','0');
+INSERT INTO [t8208] VALUES (8,'النهي عن الجلوس مع أهل البدع وخلطتهم والمشي معهم','122','0','0');
+INSERT INTO [t8208] VALUES (9,'باب: هل لصاحب البدعة توبة','149','0','0');
+INSERT INTO [t8208] VALUES (10,'قصة صبيغ العراقي','157','0','0');
+INSERT INTO [t8208] VALUES (11,'باب في نقض عرى الإسلام ودفن الدين وإظهار البدع','161','0','0');
+INSERT INTO [t8208] VALUES (12,'باب: فيما يدال الناس بعضهم من بعض والبقاع','219','0','0');
