1b50c6bf089417ae6135dde0b9c9504228e5078f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:29 +0700

    Upload SQL chunk: t10791_chunk_0000.sql

---
+++ sql_chunks/t10791_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t10791
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t10791" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10791
+INSERT INTO [t10791] VALUES (1,'[المقدمة]','2','0','0');
+INSERT INTO [t10791] VALUES (2,'ديار العرب','12','0','0');
+INSERT INTO [t10791] VALUES (3,'بحر فارس','28','0','0');
+INSERT INTO [t10791] VALUES (4,'ديار المغرب','36','0','0');
+INSERT INTO [t10791] VALUES (5,'ديار مصر','48','0','0');
+INSERT INTO [t10791] VALUES (6,'ارض الشام','55','0','0');
+INSERT INTO [t10791] VALUES (7,'بحر الروم','68','0','0');
+INSERT INTO [t10791] VALUES (8,'ارض الجزيرة','71','0','0');
+INSERT INTO [t10791] VALUES (9,'العراق','78','0','0');
+INSERT INTO [t10791] VALUES (10,'خوزستان','88','0','0');
+INSERT INTO [t10791] VALUES (11,'بلاد فارس','96','0','0');
+INSERT INTO [t10791] VALUES (12,'بلاد كرمان','158','0','0');
+INSERT INTO [t10791] VALUES (13,'بلاد السند','170','0','0');
+INSERT INTO [t10791] VALUES (14,'ارمينية والران واذربيجان','180','0','0');
+INSERT INTO [t10791] VALUES (15,'الجبال','194','0','0');
+INSERT INTO [t10791] VALUES (16,'الديلم','203','0','0');
+INSERT INTO [t10791] VALUES (17,'بحر الخزر','216','0','0');
+INSERT INTO [t10791] VALUES (18,'مفازة خراسان','226','0','0');
+INSERT INTO [t10791] VALUES (19,'سجستان','237','0','0');
+INSERT INTO [t10791] VALUES (20,'ذكر خراسان','252','0','0');
+INSERT INTO [t10791] VALUES (21,'ما وراء النهر','285','0','0');
