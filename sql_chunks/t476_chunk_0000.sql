7fcf73db12c932e2f27784757fda9b7434e86140
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:57 +0700

    Upload SQL chunk: t476_chunk_0000.sql

---
+++ sql_chunks/t476_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t476
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t476" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t476
+INSERT INTO [t476] VALUES (1,'نص الكتاب','1','0','0');
+INSERT INTO [t476] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t476] VALUES (3,'وبه ثقتي','1','0','0');
