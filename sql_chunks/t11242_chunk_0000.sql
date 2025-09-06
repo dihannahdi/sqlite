04687f1e63e0fba92b6b5e7497b1b1400644962b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:53 +0700

    Upload SQL chunk: t11242_chunk_0000.sql

---
+++ sql_chunks/t11242_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t11242
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t11242" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11242
+INSERT INTO [t11242] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11242] VALUES (2,'الفصل الأول','6','0','0');
+INSERT INTO [t11242] VALUES (3,'المبحث الأول: ضرورة اعتماد السنة لسلامة فهم القرآن','6','2','0');
+INSERT INTO [t11242] VALUES (4,'المبحث الثاني: خلاصة جهود من سبقنا في الدفاع عن السنة','12','2','0');
+INSERT INTO [t11242] VALUES (5,'المبحث الثالث: حكم منكر حجية السنة','17','2','0');
+INSERT INTO [t11242] VALUES (6,'الفصل الثاني','22','0','0');
+INSERT INTO [t11242] VALUES (7,'الشبهة الأولى','22','6','0');
+INSERT INTO [t11242] VALUES (8,'الشبهة الثانية','26','6','0');
+INSERT INTO [t11242] VALUES (9,'الشبهة الثالثة','30','6','0');
+INSERT INTO [t11242] VALUES (10,'الشبهة الرابعة','34','6','0');
+INSERT INTO [t11242] VALUES (11,'الشبهة الخامسة','37','6','0');
+INSERT INTO [t11242] VALUES (12,'الشبهة السادسة','54','6','0');
+INSERT INTO [t11242] VALUES (13,'الشبهة السابعة','58','6','0');
+INSERT INTO [t11242] VALUES (14,'الشبهة الثامنة','61','6','0');
+INSERT INTO [t11242] VALUES (15,'مصادر ومراجع','63','0','0');
