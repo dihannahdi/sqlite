fe97d2fc159b6482e6831a21adf8085bf4dfd403
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:17 +0700

    Upload SQL chunk: t12736_chunk_0000.sql

---
+++ sql_chunks/t12736_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t12736
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t12736" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12736
+INSERT INTO [t12736] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t12736] VALUES (2,'باب الحساب','2','0','0');
+INSERT INTO [t12736] VALUES (3,'باب الصور','12','0','0');
+INSERT INTO [t12736] VALUES (4,'باب ذكر النار وأهلها','50','0','0');
