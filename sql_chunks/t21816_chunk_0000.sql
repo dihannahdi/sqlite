f50239ec456c86465f974399770d2a45daa89349
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:44 +0700

    Upload SQL chunk: t21816_chunk_0000.sql

---
+++ sql_chunks/t21816_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t21816
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t21816" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21816
+INSERT INTO [t21816] VALUES (1,'فتكبر وتقول: الله أكبر','1','0','0');
+INSERT INTO [t21816] VALUES (2,'[[الركوع]]','4','0','0');
+INSERT INTO [t21816] VALUES (3,'[[السجود]]','5','0','0');
+INSERT INTO [t21816] VALUES (4,'[[بين السجدتين]]','7','0','0');
+INSERT INTO [t21816] VALUES (5,'جلسة الاستراحة','10','0','0');
+INSERT INTO [t21816] VALUES (6,'التشهد','11','0','0');
