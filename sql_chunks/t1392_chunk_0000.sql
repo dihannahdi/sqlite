8cb7bcf4a8a556adbd5683c702700b282edbae34
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:02 +0700

    Upload SQL chunk: t1392_chunk_0000.sql

---
+++ sql_chunks/t1392_chunk_0000.sql
@@ -0,0 +1,47 @@
+-- Table: t1392
+-- Chunk: 1
+-- Rows: 40
+
+CREATE TABLE "t1392" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1392
+INSERT INTO [t1392] VALUES (1,'المال في القرآن','2','0','0');
+INSERT INTO [t1392] VALUES (2,'في هذا اللقاء','2','0','0');
+INSERT INTO [t1392] VALUES (3,'المال وسيلة','7','0','0');
+INSERT INTO [t1392] VALUES (4,'حق الفقير','9','0','0');
+INSERT INTO [t1392] VALUES (5,'إخفاء الصدقة:','11','0','0');
+INSERT INTO [t1392] VALUES (6,'ثانيا: تحريم المن','13','0','0');
+INSERT INTO [t1392] VALUES (7,'ثالثا: اختيار الصدقة','14','0','0');
+INSERT INTO [t1392] VALUES (8,'حق الجماعة','15','0','0');
+INSERT INTO [t1392] VALUES (9,'قيمة الإنسان','21','0','0');
+INSERT INTO [t1392] VALUES (10,'هل يمدح الإسلام الفقر','24','0','0');
+INSERT INTO [t1392] VALUES (11,'الربا والفطرة','30','0','0');
+INSERT INTO [t1392] VALUES (12,'القرض الحسن','33','0','0');
+INSERT INTO [t1392] VALUES (13,'آية الدين وشهادة النساء','37','0','0');
+INSERT INTO [t1392] VALUES (14,'إملاء العقود','40','13','0');
+INSERT INTO [t1392] VALUES (15,'الإشهاد على الدين','42','13','0');
+INSERT INTO [t1392] VALUES (16,'شهادة النساء','43','13','0');
+INSERT INTO [t1392] VALUES (17,'اليد العاملة يكرمها','50','0','0');
+INSERT INTO [t1392] VALUES (18,'مصادر الكسب','51','0','0');
+INSERT INTO [t1392] VALUES (19,'عمل المرأة','57','0','0');
+INSERT INTO [t1392] VALUES (20,'عمل الأمة','60','0','0');
+INSERT INTO [t1392] VALUES (21,'اليد العاجزة','63','0','0');
+INSERT INTO [t1392] VALUES (22,'كفالة اليتيم','64','0','0');
+INSERT INTO [t1392] VALUES (23,'والأسير ضعيف','69','22','0');
+INSERT INTO [t1392] VALUES (24,'اليد العابثة','70','0','0');
+INSERT INTO [t1392] VALUES (25,'الميراث في القرآن','73','0','0');
+INSERT INTO [t1392] VALUES (26,'النظام اليهودي','74','25','0');
+INSERT INTO [t1392] VALUES (27,'نظام الميراث عند العرب','74','25','0');
+INSERT INTO [t1392] VALUES (28,'الزكاة ومصارفها','81','0','0');
+INSERT INTO [t1392] VALUES (29,'عدالة الزكاة','81','28','0');
+INSERT INTO [t1392] VALUES (30,'حق الجماعة','82','28','0');
+INSERT INTO [t1392] VALUES (31,'حق الله','83','28','0');
+INSERT INTO [t1392] VALUES (32,'مصارف الصدقة','84','28','0');
+INSERT INTO [t1392] VALUES (33,'تحديد المصارف','84','28','0');
+INSERT INTO [t1392] VALUES (34,'الفقير والمسكين','85','28','0');
+INSERT INTO [t1392] VALUES (35,'والعاملون على الزكاة','86','28','0');
+INSERT INTO [t1392] VALUES (36,'والمؤلفة قلوبهم','86','28','0');
+INSERT INTO [t1392] VALUES (37,'وفي الرقاب','88','28','0');
+INSERT INTO [t1392] VALUES (38,'الغارمون','89','28','0');
+INSERT INTO [t1392] VALUES (39,'في سبيل الله','90','28','0');
+INSERT INTO [t1392] VALUES (40,'وابن السبيل','91','28','0');
