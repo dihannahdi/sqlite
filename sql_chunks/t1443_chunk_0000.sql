27ec5edb594ccc43e82fb2ed3e09376659e57855
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:18:48 +0700

    Upload SQL chunk: t1443_chunk_0000.sql

---
+++ sql_chunks/t1443_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t1443
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t1443" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1443
+INSERT INTO [t1443] VALUES (1,'مقدمة النظم','2','0','0');
+INSERT INTO [t1443] VALUES (2,'توطئة','3','0','0');
+INSERT INTO [t1443] VALUES (3,'أولا: الهمزة الابتدائية','4','0','0');
+INSERT INTO [t1443] VALUES (4,'استثناء','5','3','0');
+INSERT INTO [t1443] VALUES (5,'ثانيا: الهمزة المتوسطة','6','0','0');
+INSERT INTO [t1443] VALUES (6,'ترتيب الحركات والسكون من حيث القوة','7','5','0');
+INSERT INTO [t1443] VALUES (7,'ما يناسب كل حركة وسكون','8','5','0');
+INSERT INTO [t1443] VALUES (8,'استثناءات من القاعدة العامة','9','5','0');
+INSERT INTO [t1443] VALUES (9,'الاستثناء الأول','9','8','0');
+INSERT INTO [t1443] VALUES (10,'الاستثناء الثانى','10','8','0');
+INSERT INTO [t1443] VALUES (11,'الاستثناء الثالث','11','8','0');
+INSERT INTO [t1443] VALUES (12,'ثالثا: الهمزة المتطرفة','12','0','0');
+INSERT INTO [t1443] VALUES (13,'الخاتمة','13','0','0');
