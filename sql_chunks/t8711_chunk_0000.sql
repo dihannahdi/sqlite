54f6b23311ea6968454fb14dac0fc1cd2a4be7a0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:46 +0700

    Upload SQL chunk: t8711_chunk_0000.sql

---
+++ sql_chunks/t8711_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t8711
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t8711" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8711
+INSERT INTO [t8711] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t8711] VALUES (2,'المبحث الأول: في المجمع عليه','3','0','0');
+INSERT INTO [t8711] VALUES (3,'تعريف الإجماع في اللغة والاصطلاح','3','2','0');
+INSERT INTO [t8711] VALUES (4,'سند الإجماع','7','2','0');
+INSERT INTO [t8711] VALUES (5,'حجية الإجماع','10','2','0');
+INSERT INTO [t8711] VALUES (6,'أنواع الإجماع','16','2','0');
+INSERT INTO [t8711] VALUES (7,'خاتمة المبحث الأول','24','2','0');
