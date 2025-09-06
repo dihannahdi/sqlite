d0a5b21655b4dc8dac1576d46fa8b822aebbe50e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:45:41 +0700

    Upload SQL chunk: t8349_chunk_0000.sql

---
+++ sql_chunks/t8349_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t8349
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t8349" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8349
+INSERT INTO [t8349] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8349] VALUES (2,'الطبقة الأولى: الذين عرضوا على رسول الله صلى الله عليه وسلم رضي الله عنهم','4','0','0');
+INSERT INTO [t8349] VALUES (3,'الطبقة الثانية: وهم الذين عرضوا على بعض المذكورين قبلهم','16','0','0');
+INSERT INTO [t8349] VALUES (4,'الطبقة الثالثة: وهم من التابعين','28','0','0');
+INSERT INTO [t8349] VALUES (5,'الطبقة الرابعة','53','0','0');
+INSERT INTO [t8349] VALUES (6,'الطبقة الخامسة','83','0','0');
+INSERT INTO [t8349] VALUES (7,'الطبقة السادسة','96','0','0');
+INSERT INTO [t8349] VALUES (8,'الطبقة السابعة','127','0','0');
+INSERT INTO [t8349] VALUES (9,'الطبقة الثامنة','147','0','0');
+INSERT INTO [t8349] VALUES (10,'الطبقة التاسعة','179','0','0');
+INSERT INTO [t8349] VALUES (11,'الطبقة العاشرة','207','0','0');
+INSERT INTO [t8349] VALUES (12,'الطبقة الحادية عشرة','230','0','0');
+INSERT INTO [t8349] VALUES (13,'الطبقة الثانية عشرة','253','0','0');
+INSERT INTO [t8349] VALUES (14,'الطبقة الثالثة عشرة','279','0','0');
+INSERT INTO [t8349] VALUES (15,'الطبقة الرابعة عشرة','307','0','0');
+INSERT INTO [t8349] VALUES (16,'الطبقة الخامسة عشرة','335','0','0');
+INSERT INTO [t8349] VALUES (17,'الطبقة السادسة عشرة','354','0','0');
+INSERT INTO [t8349] VALUES (18,'الطبقة السابعة عشرة','370','0','0');
+INSERT INTO [t8349] VALUES (19,'الطبقة الثامنة عشرة','391','0','0');
+INSERT INTO [t8349] VALUES (20,'الفهرس','399','0','0');
