14a6f25e35a5bb42397a81a81c2617fd509e338b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:34 +0700

    Upload SQL chunk: t12149_chunk_0000.sql

---
+++ sql_chunks/t12149_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t12149
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t12149" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12149
+INSERT INTO [t12149] VALUES (1,'حكم تكرار الجماعة في المسجد','1','0','0');
+INSERT INTO [t12149] VALUES (2,'مقدمة','25','0','0');
+INSERT INTO [t12149] VALUES (3,'مصادر ومراجع','28','0','0');
