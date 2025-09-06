28b34c15f36e626fbb29f440f385cc2a7fe95e9d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:56 +0700

    Upload SQL chunk: t21576_chunk_0000.sql

---
+++ sql_chunks/t21576_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t21576
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t21576" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21576
+INSERT INTO [t21576] VALUES (1,'السؤال الاول:','5','0','0');
+INSERT INTO [t21576] VALUES (2,'السؤال الثاني:','7','0','0');
+INSERT INTO [t21576] VALUES (3,'السؤال الثالث:','7','0','0');
+INSERT INTO [t21576] VALUES (4,'السؤال الرابع:','9','0','0');
+INSERT INTO [t21576] VALUES (5,'السؤال الخامس:','9','0','0');
+INSERT INTO [t21576] VALUES (6,'السؤال السادس:','10','0','0');
+INSERT INTO [t21576] VALUES (7,'السؤال السابع:','11','0','0');
+INSERT INTO [t21576] VALUES (8,'السؤال الثامن:','12','0','0');
+INSERT INTO [t21576] VALUES (9,'السؤال التاسع:','13','0','0');
+INSERT INTO [t21576] VALUES (10,'السؤال العاشر:','14','0','0');
+INSERT INTO [t21576] VALUES (11,'السؤال الحادي عشر:','15','0','0');
+INSERT INTO [t21576] VALUES (12,'السؤال الثاني عشر:','15','0','0');
+INSERT INTO [t21576] VALUES (13,'السؤال الثالث عشر:','16','0','0');
+INSERT INTO [t21576] VALUES (14,'السؤال الرابع عشر:','18','0','0');
+INSERT INTO [t21576] VALUES (15,'السؤال الخامس عشر:','19','0','0');
+INSERT INTO [t21576] VALUES (16,'السؤال السادس عشر:','20','0','0');
+INSERT INTO [t21576] VALUES (17,'السؤال السابع عشر:','21','0','0');
+INSERT INTO [t21576] VALUES (18,'السؤال الثامن عشر:','22','0','0');
+INSERT INTO [t21576] VALUES (19,'السؤال التاسع عشر:','23','0','0');
+INSERT INTO [t21576] VALUES (20,'السؤال العشرون:','23','0','0');
+INSERT INTO [t21576] VALUES (21,'السؤال الحادي والعشرون:','25','0','0');
+INSERT INTO [t21576] VALUES (22,'السؤال الثاني والعشرون:','26','0','0');
