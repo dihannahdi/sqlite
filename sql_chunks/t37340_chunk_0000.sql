20f34ee3bc73c2322aac4e105738537c47904b76
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:25 +0700

    Upload SQL chunk: t37340_chunk_0000.sql

---
+++ sql_chunks/t37340_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t37340
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t37340" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37340
+INSERT INTO [t37340] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t37340] VALUES (2,'الستون لماذا؟!','4','0','0');
+INSERT INTO [t37340] VALUES (3,'الستون في القرآن والسنة','10','0','0');
+INSERT INTO [t37340] VALUES (4,'الستون عند العلماء وأهل الفضل','20','0','0');
+INSERT INTO [t37340] VALUES (5,'الستون عند أهل الأمم الأخرى','24','0','0');
+INSERT INTO [t37340] VALUES (6,'الستون ... والشيب','27','0','0');
+INSERT INTO [t37340] VALUES (7,'الستون في الأدب والشعر','36','0','0');
+INSERT INTO [t37340] VALUES (8,'ذكر بعض من مات وهو في الستين','38','0','0');
+INSERT INTO [t37340] VALUES (9,'محاذير في هذا السن، لابد من التصريح بها','41','0','0');
+INSERT INTO [t37340] VALUES (10,'ما ينبغي لصاحب الستين فما فوق','45','0','0');
+INSERT INTO [t37340] VALUES (11,'الخاتمة','49','0','0');
