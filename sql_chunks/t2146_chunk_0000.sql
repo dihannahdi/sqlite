f31051cb8bb797be5e3599514b5e889fde8c2e19
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:21 +0700

    Upload SQL chunk: t2146_chunk_0000.sql

---
+++ sql_chunks/t2146_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t2146
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t2146" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2146
+INSERT INTO [t2146] VALUES (1,'المقدمة','3','0','0');
+INSERT INTO [t2146] VALUES (2,'الإله','8','0','0');
+INSERT INTO [t2146] VALUES (3,'تصور الإله عند أهل الجاهلية','10','0','0');
+INSERT INTO [t2146] VALUES (4,'الرب','23','0','0');
+INSERT INTO [t2146] VALUES (5,'استعمال كلمة (الرب) في القرآن','25','0','0');
+INSERT INTO [t2146] VALUES (6,'تصورات الأمم الضالة في باب الربوبية','27','0','0');
+INSERT INTO [t2146] VALUES (7,'العبادة','60','0','0');
+INSERT INTO [t2146] VALUES (8,'الدين','71','0','0');
+INSERT INTO [t2146] VALUES (9,'استعمال كلمة (الدين) في القرآن','73','0','0');
+INSERT INTO [t2146] VALUES (10,'ملحق بتخريج الأحاديث الواردة في الكتاب','80','0','0');
