0b609fa9fe19f9f5869aef16a3c2183053bc00c6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:23:11 +0700

    Upload SQL chunk: t5465_chunk_0000.sql

---
+++ sql_chunks/t5465_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t5465
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t5465" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5465
+INSERT INTO [t5465] VALUES (1,'تقديم','1','0','0');
+INSERT INTO [t5465] VALUES (2,'المقدمة','2','0','0');
+INSERT INTO [t5465] VALUES (3,'تطور الكتابة العربية','3','0','0');
+INSERT INTO [t5465] VALUES (4,'كتابة القرآن الكريم','4','0','0');
+INSERT INTO [t5465] VALUES (5,'نقط المصحف الشريف','5','0','0');
+INSERT INTO [t5465] VALUES (6,'تجويد الخط','6','0','0');
+INSERT INTO [t5465] VALUES (7,'أدوات الكتابة','7','0','0');
+INSERT INTO [t5465] VALUES (8,'الزخرفة والتذهيب','8','0','0');
+INSERT INTO [t5465] VALUES (9,'تاريخ الطباعة','9','0','0');
+INSERT INTO [t5465] VALUES (10,'الطبعات المبكرة للمصحف الشريف','10','0','0');
+INSERT INTO [t5465] VALUES (11,'طباعة المصحف الشريف في المملكة العربية السعودية','11','0','0');
+INSERT INTO [t5465] VALUES (12,'عناية المملكة العربية السعودية بالقرآن الكريم','12','0','0');
+INSERT INTO [t5465] VALUES (13,'مجمع الملك فهد لطباعة المصحف الشريف','13','0','0');
+INSERT INTO [t5465] VALUES (14,'أسلوب العمل في كتابة مصاحف المجمع وطبعها','14','0','0');
+INSERT INTO [t5465] VALUES (15,'تسجيل تلاوة القرآن الكريم','15','0','0');
+INSERT INTO [t5465] VALUES (16,'الترجمات','16','0','0');
+INSERT INTO [t5465] VALUES (17,'الدراسات القرآنية','17','0','0');
+INSERT INTO [t5465] VALUES (18,'خدمة السنة والسيرة النبوية','18','0','0');
+INSERT INTO [t5465] VALUES (19,'تدريب العاملين في المجمع وتأهيلهم','19','0','0');
+INSERT INTO [t5465] VALUES (20,'الخاتمة','20','0','0');
+INSERT INTO [t5465] VALUES (21,'الخطوط','21','0','0');
+INSERT INTO [t5465] VALUES (22,'الصور','22','0','0');
+INSERT INTO [t5465] VALUES (23,'الإحصائيات','23','0','0');
+INSERT INTO [t5465] VALUES (24,'كشاف المراجع','24','0','0');
