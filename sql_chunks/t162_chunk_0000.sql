76afa790eca77b037100ff3ef808c413ef5948c9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:25:57 +0700

    Upload SQL chunk: t162_chunk_0000.sql

---
+++ sql_chunks/t162_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t162
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t162" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t162
+INSERT INTO [t162] VALUES (1,'توطئة','2','0','0');
+INSERT INTO [t162] VALUES (2,'أنواع الكلام','3','0','0');
+INSERT INTO [t162] VALUES (3,'باب الإعراب','6','0','0');
+INSERT INTO [t162] VALUES (4,'باب معرفة علامات الإعراب','8','0','0');
+INSERT INTO [t162] VALUES (5,'فصل: المعربات','13','0','0');
+INSERT INTO [t162] VALUES (6,'باب الأفعال','15','0','0');
+INSERT INTO [t162] VALUES (7,'باب مرفوعات الأسماء','20','0','0');
+INSERT INTO [t162] VALUES (8,'باب الفاعل','21','0','0');
+INSERT INTO [t162] VALUES (9,'باب المفعول الذي لم يسم فاعله','24','0','0');
+INSERT INTO [t162] VALUES (10,'باب المبتدإ والخبر','25','0','0');
+INSERT INTO [t162] VALUES (11,'باب العوامل الداخلة على المبتدإ والخبر','27','0','0');
+INSERT INTO [t162] VALUES (12,'باب النعت','28','0','0');
+INSERT INTO [t162] VALUES (13,'فصل في المعرفة والنكرة','29','0','0');
+INSERT INTO [t162] VALUES (14,'باب العطف','31','0','0');
+INSERT INTO [t162] VALUES (15,'باب التوكيد','32','0','0');
+INSERT INTO [t162] VALUES (16,'باب البدل','33','0','0');
+INSERT INTO [t162] VALUES (17,'باب منصوبات الأسماء','34','0','0');
+INSERT INTO [t162] VALUES (18,'باب المفعول به','34','0','0');
+INSERT INTO [t162] VALUES (19,'باب المصدر','35','0','0');
+INSERT INTO [t162] VALUES (20,'باب ظرف الزمان وظرف المكان','36','0','0');
+INSERT INTO [t162] VALUES (21,'باب الحال','37','0','0');
+INSERT INTO [t162] VALUES (22,'باب التمييز','37','0','0');
+INSERT INTO [t162] VALUES (23,'باب الاستثناء','38','0','0');
+INSERT INTO [t162] VALUES (24,'باب «لا»','40','0','0');
+INSERT INTO [t162] VALUES (25,'باب المنادى','41','0','0');
+INSERT INTO [t162] VALUES (26,'باب المفعول من أجله','41','0','0');
+INSERT INTO [t162] VALUES (27,'باب المفعول معه','42','0','0');
+INSERT INTO [t162] VALUES (28,'باب المخفوضات من الأسماء','42','0','0');
