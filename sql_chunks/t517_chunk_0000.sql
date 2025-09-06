7dc344e605a2007f62a1651b709edbdd8d15a35a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:54 +0700

    Upload SQL chunk: t517_chunk_0000.sql

---
+++ sql_chunks/t517_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t517
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t517" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t517
+INSERT INTO [t517] VALUES (1,'القسم الأول: التنبيه على الأحاديث التي ذكرها ابن مالك في شواهد التوضيح وبيان مواضعها من صحيح البخاري','2','0','0');
+INSERT INTO [t517] VALUES (2,'القسم الثاني: أحاديث من غير صحيح البخاري وبيان مواضعها','41','0','0');
+INSERT INTO [t517] VALUES (3,'القسم الثالث: تتمات وملاحظات لتعليقات الأستاذ الفاضل محمد فؤاد عبد الباقي على شواهد التوضيح لابن مالك','45','0','0');
