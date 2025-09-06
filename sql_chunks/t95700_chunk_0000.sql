29ff3533ab416ab3794e95b0212ff4b966a15c9f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:22 +0700

    Upload SQL chunk: t95700_chunk_0000.sql

---
+++ sql_chunks/t95700_chunk_0000.sql
@@ -0,0 +1,54 @@
+-- Table: t95700
+-- Chunk: 1
+-- Rows: 47
+
+CREATE TABLE "t95700" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95700
+INSERT INTO [t95700] VALUES (1,'مقدمة التحقيق','5','0','0');
+INSERT INTO [t95700] VALUES (2,'١ - التمهيد:','6','1','0');
+INSERT INTO [t95700] VALUES (3,'٢ - التعريف بالكتاب:','6','1','0');
+INSERT INTO [t95700] VALUES (4,'٣ - سبب إملاء الخطابي للكتاب:','7','1','0');
+INSERT INTO [t95700] VALUES (5,'٤ - أهمية الكتاب:','8','1','0');
+INSERT INTO [t95700] VALUES (6,'٥ - المخطوطات المعتمدة في التحقيق:','9','1','0');
+INSERT INTO [t95700] VALUES (7,'٦ - الحصول على النسخ ودورها في التحقيق:','12','1','0');
+INSERT INTO [t95700] VALUES (8,'٧ - دراسة الأسانيد والقراءات والسماعات:','13','1','0');
+INSERT INTO [t95700] VALUES (9,'٨ - منهج التحقيق:','17','1','0');
+INSERT INTO [t95700] VALUES (10,'٩ - طريقة الإخراج:','18','1','0');
+INSERT INTO [t95700] VALUES (11,'ترجمة المصنف','30','1','0');
+INSERT INTO [t95700] VALUES (12,'تمهيد:','32','11','0');
+INSERT INTO [t95700] VALUES (13,'أ- القرن الرابع الهجري:','32','12','0');
+INSERT INTO [t95700] VALUES (14,'ب- لمحة عن الحياة العلمية:','32','12','0');
+INSERT INTO [t95700] VALUES (15,'الترجمة','33','11','0');
+INSERT INTO [t95700] VALUES (16,'شأن الدعاء','47','0','0');
+INSERT INTO [t95700] VALUES (17,'مقدمة المصنف','47','16','0');
+INSERT INTO [t95700] VALUES (18,'معنى الدعاء','49','16','0');
+INSERT INTO [t95700] VALUES (19,'(باب تفسير هذه الأسماء)','68','16','0');
+INSERT INTO [t95700] VALUES (20,'تفسير هذه الأسماء','75','19','0');
+INSERT INTO [t95700] VALUES (21,'فصل','156','16','0');
+INSERT INTO [t95700] VALUES (22,'من باب ما يقول إذا أصبح','160','16','0');
+INSERT INTO [t95700] VALUES (23,'ومن لواحق الدعاء الذي لم يذكر في المأثور','223','16','0');
+INSERT INTO [t95700] VALUES (24,'ومن دعائه [- صلى الله عليه وسلم -] في الاستسقاء:','244','16','0');
+INSERT INTO [t95700] VALUES (25,'المصادر المعتمدة في التحقيق','255','0','0');
+INSERT INTO [t95700] VALUES (26,'(أ)','255','25','0');
+INSERT INTO [t95700] VALUES (27,'(ب)','256','25','0');
+INSERT INTO [t95700] VALUES (28,'(ت)','256','25','0');
+INSERT INTO [t95700] VALUES (29,'(ج)','257','25','0');
+INSERT INTO [t95700] VALUES (30,'(ح)','258','25','0');
+INSERT INTO [t95700] VALUES (31,'(خ)','258','25','0');
+INSERT INTO [t95700] VALUES (32,'(د)','258','25','0');
+INSERT INTO [t95700] VALUES (33,'(ر)','259','25','0');
+INSERT INTO [t95700] VALUES (34,'(ز)','259','25','0');
+INSERT INTO [t95700] VALUES (35,'(س)','259','25','0');
+INSERT INTO [t95700] VALUES (36,'(ش)','260','25','0');
+INSERT INTO [t95700] VALUES (37,'(ص)','261','25','0');
+INSERT INTO [t95700] VALUES (38,'(ع)','262','25','0');
+INSERT INTO [t95700] VALUES (39,'(غ)','262','25','0');
+INSERT INTO [t95700] VALUES (40,'(ف)','262','25','0');
+INSERT INTO [t95700] VALUES (41,'(ق)','263','25','0');
+INSERT INTO [t95700] VALUES (42,'(ك)','263','25','0');
+INSERT INTO [t95700] VALUES (43,'(ل)','263','25','0');
+INSERT INTO [t95700] VALUES (44,'(م)','264','25','0');
+INSERT INTO [t95700] VALUES (45,'(ن)','266','25','0');
+INSERT INTO [t95700] VALUES (46,'(هـ)','266','25','0');
+INSERT INTO [t95700] VALUES (47,'(و)','266','25','0');
