b8f1833ba30cb32ce7d89724ea5f8aa1910f8ada
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:55 +0700

    Upload SQL chunk: t702_chunk_0000.sql

---
+++ sql_chunks/t702_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t702
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t702" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t702
+INSERT INTO [t702] VALUES (1,'مساجلة علمية','1','0','0');
