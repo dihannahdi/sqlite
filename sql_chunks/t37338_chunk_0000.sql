0f2e3a63b5b03f3f93953deb3d0b65d07c6b1dd7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:23 +0700

    Upload SQL chunk: t37338_chunk_0000.sql

---
+++ sql_chunks/t37338_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t37338
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t37338" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37338
+INSERT INTO [t37338] VALUES (1,'الفصل الأول في اسمها ووقت ظهورها','3','0','0');
+INSERT INTO [t37338] VALUES (2,'الفصل الثاني في مضارها في العقل والبدن','6','0','0');
+INSERT INTO [t37338] VALUES (3,'الفصل الثالث في أنها مسكرة ومفسدة للعقل','14','0','0');
+INSERT INTO [t37338] VALUES (4,'الفصل الرابع في أنها حرام','27','0','0');
+INSERT INTO [t37338] VALUES (5,'الفصل الخامس في أنها طاهرة أو نجسة','34','0','0');
+INSERT INTO [t37338] VALUES (6,'الفصل السادس في أنه هل يجب فيها الحد','37','0','0');
+INSERT INTO [t37338] VALUES (7,'الفصل السابع في فروع متفرقة ومولدات','40','0','0');
