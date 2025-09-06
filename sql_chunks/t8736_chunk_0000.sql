50b6ed57642ef6d32b40470a1ad2ff6af044ea07
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:53 +0700

    Upload SQL chunk: t8736_chunk_0000.sql

---
+++ sql_chunks/t8736_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8736
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8736" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8736
+INSERT INTO [t8736] VALUES (1,'الإسلام والحركات الهدامة المعاصرة','1','0','0');
+INSERT INTO [t8736] VALUES (2,'بقلم الشيخ: عبد القادر شيبة الحمد','1','0','0');
