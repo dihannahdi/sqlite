7ccf00ffa7cc6ecd22145f9233dd7a07889bfb3b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:20 +0700

    Upload SQL chunk: t4924_chunk_0000.sql

---
+++ sql_chunks/t4924_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t4924
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t4924" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4924
+INSERT INTO [t4924] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t4924] VALUES (2,'من أحكام الفه الإسلامي','3','0','0');
+INSERT INTO [t4924] VALUES (3,'الاقتصاد في النفقات','8','0','0');
+INSERT INTO [t4924] VALUES (4,'طرق الكسب الحرام','15','0','0');
+INSERT INTO [t4924] VALUES (5,'التحذير من المعاملات الربوية','23','0','0');
+INSERT INTO [t4924] VALUES (6,'حكم البيع إلى أجل وبيع التورق والعينة والقرض بالفائدة','28','0','0');
+INSERT INTO [t4924] VALUES (7,'نصيحة في النهي غن الربا للشيخ محمد بن عبد اللطيف آل الشيخ','36','0','0');
+INSERT INTO [t4924] VALUES (8,'من أحكام الفقه الإسلامي (المداينة)','43','0','0');
+INSERT INTO [t4924] VALUES (9,'خاتمة','60','0','0');
