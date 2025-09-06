d545693405ca55f6cc85dfefe65d025e4ae2727d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:21 +0700

    Upload SQL chunk: t9855_chunk_0000.sql

---
+++ sql_chunks/t9855_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9855
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9855" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9855
+INSERT INTO [t9855] VALUES (1,'الجزء فيه أحاديث من مسموعات للشيخ الحافظ أبي ذر عبيد بن أحمد بن محمد الهروي','1','0','0');
+INSERT INTO [t9855] VALUES (2,'آخر الجزء والحمد لله وحده','16','0','0');
