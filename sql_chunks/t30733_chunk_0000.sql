a197a894446347b2b51bddc068d9c04c5533feff
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:42 +0700

    Upload SQL chunk: t30733_chunk_0000.sql

---
+++ sql_chunks/t30733_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30733
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30733" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30733
+INSERT INTO [t30733] VALUES (1,'جزء في طرق حديث لا تسبوا أصحابي','1','0','0');
