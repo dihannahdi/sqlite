80c180d44b123621fc117c2e35ece188b342ab98
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:47 +0700

    Upload SQL chunk: t9564_chunk_0000.sql

---
+++ sql_chunks/t9564_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t9564
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t9564" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9564
+INSERT INTO [t9564] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t9564] VALUES (2,'المقدمة','2','0','0');
+INSERT INTO [t9564] VALUES (3,'الجزء الأول','3','0','0');
+INSERT INTO [t9564] VALUES (4,'الجزء الثاني','223','0','0');
+INSERT INTO [t9564] VALUES (5,'أول الجزء الثالث من أجزاء ابن النقور','346','0','0');
+INSERT INTO [t9564] VALUES (6,'الجزء الرابع','347','0','0');
+INSERT INTO [t9564] VALUES (7,'الجزء السابع','509','0','0');
