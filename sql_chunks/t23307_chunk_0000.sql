5b7bd1fd368a55b9c71e4a03536d9d0cb12637a4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:20 +0700

    Upload SQL chunk: t23307_chunk_0000.sql

---
+++ sql_chunks/t23307_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t23307
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t23307" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23307
+INSERT INTO [t23307] VALUES (1,'المستشرقون والتراث','1','0','0');
+INSERT INTO [t23307] VALUES (2,'مدخل إلى القضية:','6','0','0');
+INSERT INTO [t23307] VALUES (3,'اتجاهات النشر عند المستشرقين:','8','0','0');
+INSERT INTO [t23307] VALUES (4,'الاتجاه الفكري للمستشرقين:','16','0','0');
+INSERT INTO [t23307] VALUES (5,'الأغاني:','23','4','0');
+INSERT INTO [t23307] VALUES (6,'هذه آثارهم:','24','0','0');
+INSERT INTO [t23307] VALUES (7,'خيانة المنهج:','28','0','0');
+INSERT INTO [t23307] VALUES (8,'نماذج من تحريف النصوص وخيانة المنهج:','29','7','0');
+INSERT INTO [t23307] VALUES (9,'جولدتسيهر:','29','8','0');
+INSERT INTO [t23307] VALUES (10,'ول ديورانت:','32','8','0');
+INSERT INTO [t23307] VALUES (11,'فان فلوتن:','37','8','0');
+INSERT INTO [t23307] VALUES (12,'الخاتمة:','43','0','0');
+INSERT INTO [t23307] VALUES (13,'المحتويات:','45','0','0');
