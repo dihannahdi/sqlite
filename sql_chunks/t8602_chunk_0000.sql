76bc5004c6ea89f869399487c1be6cac6fbd3745
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:43 +0700

    Upload SQL chunk: t8602_chunk_0000.sql

---
+++ sql_chunks/t8602_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t8602
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t8602" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8602
+INSERT INTO [t8602] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8602] VALUES (2,'ترجمة العلائي','4','0','0');
+INSERT INTO [t8602] VALUES (3,'الكلام على الكتاب','10','0','0');
+INSERT INTO [t8602] VALUES (4,'مقدمة العلائي','13','0','0');
+INSERT INTO [t8602] VALUES (5,'أقسام الحديث المحتج به','15','0','0');
+INSERT INTO [t8602] VALUES (6,'كتاب البخاري ومسلم','16','0','0');
+INSERT INTO [t8602] VALUES (7,'سنن أبي داود وابن ماجة','18','0','0');
+INSERT INTO [t8602] VALUES (8,'الحكم على الحديث بالوضع','22','0','0');
+INSERT INTO [t8602] VALUES (9,'بيان الأحاديث التي أنتقدت على صاحب المصابيح وما ينبغي الحكم عليها به','23','0','0');
+INSERT INTO [t8602] VALUES (10,'مصادر ومراجع','54','0','0');
