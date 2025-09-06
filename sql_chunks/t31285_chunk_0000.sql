2a3bf5da12b892ba36259bda241f131d15fc17e4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:54 +0700

    Upload SQL chunk: t31285_chunk_0000.sql

---
+++ sql_chunks/t31285_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t31285
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t31285" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31285
+INSERT INTO [t31285] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t31285] VALUES (2,'كتاب الطهارة','4','0','0');
+INSERT INTO [t31285] VALUES (3,'المياه','4','2','0');
+INSERT INTO [t31285] VALUES (4,'الآنية','6','2','0');
+INSERT INTO [t31285] VALUES (5,'الاستنجاء','8','2','0');
+INSERT INTO [t31285] VALUES (6,'الوضوء','10','2','0');
+INSERT INTO [t31285] VALUES (7,'الغسل','14','2','0');
+INSERT INTO [t31285] VALUES (8,'التيمم','16','2','0');
+INSERT INTO [t31285] VALUES (9,'المسح على الخفين','17','2','0');
+INSERT INTO [t31285] VALUES (10,'الحيض والاستحاضة والنفاس','19','2','0');
+INSERT INTO [t31285] VALUES (11,'كتاب الصلاة','25','0','0');
+INSERT INTO [t31285] VALUES (12,'الأذان والإقامة','25','11','0');
+INSERT INTO [t31285] VALUES (13,'مواقيت الصلاة','27','11','0');
+INSERT INTO [t31285] VALUES (14,'صفة الصلاة','31','11','0');
+INSERT INTO [t31285] VALUES (15,'صلاة التطوع','40','11','0');
+INSERT INTO [t31285] VALUES (16,'صلاة المريض','43','11','0');
+INSERT INTO [t31285] VALUES (17,'صلاة المسافر','44','11','0');
+INSERT INTO [t31285] VALUES (18,'صلاة الخوف','46','11','0');
+INSERT INTO [t31285] VALUES (19,'الإمامة وصلاة الجماعة','47','11','0');
+INSERT INTO [t31285] VALUES (20,'صلاة الجمعة','52','11','0');
+INSERT INTO [t31285] VALUES (21,'صلاة العيدين','55','11','0');
+INSERT INTO [t31285] VALUES (22,'صلاة الكسوف','58','11','0');
+INSERT INTO [t31285] VALUES (23,'صلاة الاستسقاء','61','11','0');
+INSERT INTO [t31285] VALUES (24,'الجنائز','64','11','0');
+INSERT INTO [t31285] VALUES (25,'كتاب الزكاة','71','0','0');
+INSERT INTO [t31285] VALUES (26,'زكاة الفطر','80','25','0');
+INSERT INTO [t31285] VALUES (27,'كتاب الصيام','82','0','0');
+INSERT INTO [t31285] VALUES (28,'كتاب الحج والعمرة','89','0','0');
