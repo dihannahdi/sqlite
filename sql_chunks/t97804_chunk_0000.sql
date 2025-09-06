ee3a0d012d1cc7f643074661d8bda9e990fe6bed
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:01:37 +0700

    Upload SQL chunk: t97804_chunk_0000.sql

---
+++ sql_chunks/t97804_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t97804
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t97804" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t97804
+INSERT INTO [t97804] VALUES (1,'المقدمة','5','0','0');
+INSERT INTO [t97804] VALUES (2,'الأذكار الشريفة بين القديم والحديث','10','0','0');
+INSERT INTO [t97804] VALUES (3,'ما يقول إذا أوى إلى فراشه وإذا استيقظ من نومه','15','0','0');
+INSERT INTO [t97804] VALUES (4,'ما يقول إذا لبس ثوبا','22','0','0');
+INSERT INTO [t97804] VALUES (5,'ما يقول إذا خلع ثوبه','30','0','0');
+INSERT INTO [t97804] VALUES (6,'ما يقول إذا نظر في المرآة','34','0','0');
+INSERT INTO [t97804] VALUES (7,'ما يقول إذا خرج من بيته','38','0','0');
+INSERT INTO [t97804] VALUES (8,'ما يقول إذا ركب دابة أو سيارة','43','0','0');
+INSERT INTO [t97804] VALUES (9,'ما يقول إذا أراد سفرا','48','0','0');
+INSERT INTO [t97804] VALUES (10,'ما يقول إذا رأى مبتلى بمرض أو غيره','52','0','0');
+INSERT INTO [t97804] VALUES (11,'ما يقول إذا دخل السوق','57','0','0');
+INSERT INTO [t97804] VALUES (12,'ما يقول عند الطعام وبعده','61','0','0');
+INSERT INTO [t97804] VALUES (13,'ما يقول إذا صعب عليه أمر','66','0','0');
+INSERT INTO [t97804] VALUES (14,'ما يقول إذا عسر أمر معيشته','71','0','0');
+INSERT INTO [t97804] VALUES (15,'ما يقول عند ورود نعمة أو إذا رأى ما يحب أو يكره','77','0','0');
+INSERT INTO [t97804] VALUES (16,'ما يقول من مات له ميت','82','0','0');
+INSERT INTO [t97804] VALUES (17,'ما يقول إذا رأى الهلال','86','0','0');
+INSERT INTO [t97804] VALUES (18,'الاستخارة','89','0','0');
+INSERT INTO [t97804] VALUES (19,'التهنئة بالمولود','93','0','0');
+INSERT INTO [t97804] VALUES (20,'كفارة المجالس','97','0','0');
+INSERT INTO [t97804] VALUES (21,'ما يقول من ضيع شيئا ما','100','0','0');
