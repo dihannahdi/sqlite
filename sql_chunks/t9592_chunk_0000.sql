afa9a7544c488bf680422734a3b4301e69b1bb87
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:40 +0700

    Upload SQL chunk: t9592_chunk_0000.sql

---
+++ sql_chunks/t9592_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t9592
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t9592" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9592
+INSERT INTO [t9592] VALUES (1,'اللهم إني أعوذ بك من زوال نعمتك، وتحول عافيتك، وفجأة نقمتك، وجميع','1','0','0');
+INSERT INTO [t9592] VALUES (2,'دخلت الجنة، فرأيت قصرا ودارا، فقلت: لمن هذه؟ قيل: لرجل من قريش، فرجوت أن أكون أنا هو،','2','0','0');
+INSERT INTO [t9592] VALUES (3,'لا تطروني كما أطرت النصارى ابن مريم، فإنما أنا عبد، فقولوا: عبده ورسوله ". ثم كان من خبرنا،','4','0','0');
