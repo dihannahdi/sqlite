5832a9a04232f67a348f5c8ec9ec7332015d26a2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:29:39 +0700

    Upload SQL chunk: t17360_chunk_0000.sql

---
+++ sql_chunks/t17360_chunk_0000.sql
@@ -0,0 +1,47 @@
+-- Table: t17360
+-- Chunk: 1
+-- Rows: 40
+
+CREATE TABLE "t17360" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17360
+INSERT INTO [t17360] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t17360] VALUES (2,'وقفة عند ريع اللصوص','7','0','0');
+INSERT INTO [t17360] VALUES (3,'لعبة شطرنج','23','0','0');
+INSERT INTO [t17360] VALUES (4,'أخبار من التاريخ','28','0','0');
+INSERT INTO [t17360] VALUES (5,'طرائف من التاريخ','38','0','0');
+INSERT INTO [t17360] VALUES (6,'طاقة أخبار','48','0','0');
+INSERT INTO [t17360] VALUES (7,'حادثة من التاريخ','58','0','0');
+INSERT INTO [t17360] VALUES (8,'من نصوص «الحسبة»','64','0','0');
+INSERT INTO [t17360] VALUES (9,'من طرائف الأخبار','68','0','0');
+INSERT INTO [t17360] VALUES (10,'وقفة على الفسطاط','72','0','0');
+INSERT INTO [t17360] VALUES (11,'الإمام الأوزاعي','82','0','0');
+INSERT INTO [t17360] VALUES (12,'العجيبة الثامنة','86','0','0');
+INSERT INTO [t17360] VALUES (13,'كتاب «الأغاني» لأبي الفرج الأصفهاني','98','0','0');
+INSERT INTO [t17360] VALUES (14,'دفاع عن الأصمعي','102','0','0');
+INSERT INTO [t17360] VALUES (15,'لون من الترف العقلي','105','0','0');
+INSERT INTO [t17360] VALUES (16,'ما هي السماء؟','115','0','0');
+INSERT INTO [t17360] VALUES (17,'من هو العربي؟','129','0','0');
+INSERT INTO [t17360] VALUES (18,'هذي شهورنا','133','0','0');
+INSERT INTO [t17360] VALUES (19,'اقتراح في التعليم','135','0','0');
+INSERT INTO [t17360] VALUES (20,'لغة أضاعها أهلوها','145','0','0');
+INSERT INTO [t17360] VALUES (21,'آفة اللغة هذا النحو','152','0','0');
+INSERT INTO [t17360] VALUES (22,'كيف كنا وكيف صرتم','163','0','0');
+INSERT INTO [t17360] VALUES (23,'المطالعة','173','0','0');
+INSERT INTO [t17360] VALUES (24,'كلمة في اختيار نصوص الدراسة الأدبية','182','0','0');
+INSERT INTO [t17360] VALUES (25,'في أصول الأدب','186','0','0');
+INSERT INTO [t17360] VALUES (26,'الوصف الخيالي والوصف الواقعي','200','0','0');
+INSERT INTO [t17360] VALUES (27,'حرفة الأدب','208','0','0');
+INSERT INTO [t17360] VALUES (28,'مقدمة «باب البيان»','215','0','0');
+INSERT INTO [t17360] VALUES (29,'كلمات في الأدب','221','0','0');
+INSERT INTO [t17360] VALUES (30,'إلى الأستاذ الرافعي','232','0','0');
+INSERT INTO [t17360] VALUES (31,'أسلوب الرافعي','236','0','0');
+INSERT INTO [t17360] VALUES (32,'طه حسين في دمشق','238','0','0');
+INSERT INTO [t17360] VALUES (33,'طه حسين في الميزان','242','0','0');
+INSERT INTO [t17360] VALUES (34,'صوت من وراء القرون','246','0','0');
+INSERT INTO [t17360] VALUES (35,'ثالث الحرمين','250','0','0');
+INSERT INTO [t17360] VALUES (36,'إلى أخي أبي الحسن الندوي','252','0','0');
+INSERT INTO [t17360] VALUES (37,'ابن عابدين ورسائله','255','0','0');
+INSERT INTO [t17360] VALUES (38,'مصادر الثقافة وتصنيف العلوم','271','0','0');
+INSERT INTO [t17360] VALUES (39,'كتاب في البيزرة','281','0','0');
+INSERT INTO [t17360] VALUES (40,'اختراع الخراع','291','0','0');
