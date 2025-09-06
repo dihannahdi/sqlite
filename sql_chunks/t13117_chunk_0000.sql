555d9c70a6d4ecad480338c850794f24e1e863c3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:10:49 +0700

    Upload SQL chunk: t13117_chunk_0000.sql

---
+++ sql_chunks/t13117_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t13117
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t13117" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13117
+INSERT INTO [t13117] VALUES (1,'اجتنبوا أم الخبائث','1','0','0');
+INSERT INTO [t13117] VALUES (2,'الخمر مجمع الخبائث','3','0','0');
+INSERT INTO [t13117] VALUES (3,'إياكم والخمر','5','0','0');
+INSERT INTO [t13117] VALUES (4,'الخمر مفتاح الكبائر','7','0','0');
+INSERT INTO [t13117] VALUES (5,'شدة حرمة الخمر','10','0','0');
+INSERT INTO [t13117] VALUES (6,'الخمر هي الخمر','13','0','0');
+INSERT INTO [t13117] VALUES (7,'حكم الزبيب','15','0','0');
+INSERT INTO [t13117] VALUES (8,'الخمر حرام','17','0','0');
+INSERT INTO [t13117] VALUES (9,'شارب الخمر لا يؤمن بالله','19','0','0');
+INSERT INTO [t13117] VALUES (10,'ليس في الخمر شفاء','21','0','0');
+INSERT INTO [t13117] VALUES (11,'حكم النبيذ','23','0','0');
+INSERT INTO [t13117] VALUES (12,'كل مسكر خمر','25','0','0');
+INSERT INTO [t13117] VALUES (13,'كل مسكر حرام','28','0','0');
+INSERT INTO [t13117] VALUES (14,'قليل المسكر كثيره','30','0','0');
+INSERT INTO [t13117] VALUES (15,'مم يصنع الخمر','36','0','0');
+INSERT INTO [t13117] VALUES (16,'كل مشروب يسكر فهو خمر','38','0','0');
+INSERT INTO [t13117] VALUES (17,'احذر الخمر','40','0','0');
+INSERT INTO [t13117] VALUES (18,'النبيذ حلال','42','0','0');
+INSERT INTO [t13117] VALUES (19,'شدة نجاسة الخمر','50','0','0');
+INSERT INTO [t13117] VALUES (20,'تعريف الخمر','53','0','0');
+INSERT INTO [t13117] VALUES (21,'مواد الخمر','55','0','0');
+INSERT INTO [t13117] VALUES (22,'النبيذ فتنة','59','0','0');
+INSERT INTO [t13117] VALUES (23,'إياكم والأحمرين','61','0','0');
+INSERT INTO [t13117] VALUES (24,'أنواع السكر','77','0','0');
+INSERT INTO [t13117] VALUES (25,'إياك والنبيذ المسكر','79','0','0');
+INSERT INTO [t13117] VALUES (26,'قصة وعظة','81','0','0');
+INSERT INTO [t13117] VALUES (27,'السكر جوامع الشر','83','0','0');
+INSERT INTO [t13117] VALUES (28,'اشتروا عقولكم','86','0','0');
+INSERT INTO [t13117] VALUES (29,'أفتني في الباذق','90','0','0');
+INSERT INTO [t13117] VALUES (30,'شارب الخمر ناقص المروءة','94','0','0');
