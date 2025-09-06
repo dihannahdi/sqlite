76f411a87721a032de82bb0f20419e4b39f443b3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:16 +0700

    Upload SQL chunk: t5780_chunk_0000.sql

---
+++ sql_chunks/t5780_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t5780
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t5780" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5780
+INSERT INTO [t5780] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
