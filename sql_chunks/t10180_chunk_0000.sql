33ff2cc039aa7c99fc4039c14ebcb7ee2466e1bc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:11 +0700

    Upload SQL chunk: t10180_chunk_0000.sql

---
+++ sql_chunks/t10180_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t10180
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t10180" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10180
+INSERT INTO [t10180] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10180] VALUES (2,'أمثلة على الإعجاز العلمي للقرآن','4','0','0');
+INSERT INTO [t10180] VALUES (3,'العسل','4','2','0');
+INSERT INTO [t10180] VALUES (4,'مدخل','4','3','0');
+INSERT INTO [t10180] VALUES (5,'مكونات العسل','6','3','0');
+INSERT INTO [t10180] VALUES (6,'خمائر العسل','6','5','0');
+INSERT INTO [t10180] VALUES (7,'الاملاح المعدنية الموجودة في العسل','7','5','0');
+INSERT INTO [t10180] VALUES (8,'العسل قلوي','8','5','0');
+INSERT INTO [t10180] VALUES (9,'فيتامينات العسل','10','5','0');
+INSERT INTO [t10180] VALUES (10,'إستعمال العسل','12','3','0');
+INSERT INTO [t10180] VALUES (11,'مدخل','12','10','0');
+INSERT INTO [t10180] VALUES (12,'أهم الأمراض التي يستعمل العسل في علاجها','13','10','0');
+INSERT INTO [t10180] VALUES (13,'إستعمال العسل بدل السكر','16','10','0');
+INSERT INTO [t10180] VALUES (14,'الرطب','18','2','0');
+INSERT INTO [t10180] VALUES (15,'الخمر','19','2','0');
