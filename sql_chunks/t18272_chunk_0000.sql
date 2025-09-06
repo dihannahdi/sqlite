72f77f005c32e5543af444d1916eddd798118ae8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:33:37 +0700

    Upload SQL chunk: t18272_chunk_0000.sql

---
+++ sql_chunks/t18272_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t18272
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t18272" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t18272
+INSERT INTO [t18272] VALUES (1,'(حرف الهمزة)','7','0','0');
+INSERT INTO [t18272] VALUES (2,'(حرف الباء)','660','0','0');
+INSERT INTO [t18272] VALUES (3,'(حرف التاء)','678','0','0');
+INSERT INTO [t18272] VALUES (4,'(حرف الثاء)','703','0','0');
+INSERT INTO [t18272] VALUES (5,'(حرف الجيم)','733','0','0');
+INSERT INTO [t18272] VALUES (6,'(حرف الحاء)','743','0','0');
+INSERT INTO [t18272] VALUES (7,'(حرف الخاء)','770','0','0');
+INSERT INTO [t18272] VALUES (8,'(حرف الدال)','806','0','0');
+INSERT INTO [t18272] VALUES (9,'(حرف الذال)','823','0','0');
+INSERT INTO [t18272] VALUES (10,'(حرف الراء)','831','0','0');
+INSERT INTO [t18272] VALUES (11,'(حرف الزاي)','857','0','0');
+INSERT INTO [t18272] VALUES (12,'(حرف السين)','863','0','0');
+INSERT INTO [t18272] VALUES (13,'(حرف الشين)','896','0','0');
+INSERT INTO [t18272] VALUES (14,'(حرف الصاد)','911','0','0');
+INSERT INTO [t18272] VALUES (15,'(حرف الضاد)','942','0','0');
+INSERT INTO [t18272] VALUES (16,'(حرف الطاء)','948','0','0');
+INSERT INTO [t18272] VALUES (17,'(حرف الظاء)','961','0','0');
+INSERT INTO [t18272] VALUES (18,'(حرف العين)','962','0','0');
+INSERT INTO [t18272] VALUES (19,'(حرف الغين)','1015','0','0');
+INSERT INTO [t18272] VALUES (20,'(حرف الفاء)','1024','0','0');
+INSERT INTO [t18272] VALUES (21,'(حرف القاف)','1053','0','0');
+INSERT INTO [t18272] VALUES (22,'(حرف الكاف)','1090','0','0');
+INSERT INTO [t18272] VALUES (23,'(باب كان وهي من الشمائل الشريفة)','1125','22','0');
+INSERT INTO [t18272] VALUES (24,'(حرف اللام)','1204','0','0');
+INSERT INTO [t18272] VALUES (25,'(حرف الميم)','1272','0','0');
+INSERT INTO [t18272] VALUES (26,'(حرف النون)','1440','0','0');
+INSERT INTO [t18272] VALUES (27,'(باب المناهي)','1448','26','0');
+INSERT INTO [t18272] VALUES (28,'(حرف الهاء)','1475','0','0');
+INSERT INTO [t18272] VALUES (29,'(حرف الواو)','1478','0','0');
+INSERT INTO [t18272] VALUES (30,'(حرف اللام ألف)','1489','0','0');
+INSERT INTO [t18272] VALUES (31,'(حرف الياء)','1524','0','0');
