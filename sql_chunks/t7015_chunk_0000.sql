af62f41c1af7084ed1329edaee2ed77572244e79
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:53 +0700

    Upload SQL chunk: t7015_chunk_0000.sql

---
+++ sql_chunks/t7015_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t7015
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t7015" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7015
+INSERT INTO [t7015] VALUES (1,'حرف الهمزة','3','0','0');
+INSERT INTO [t7015] VALUES (2,'حرف الباء','60','0','0');
+INSERT INTO [t7015] VALUES (3,'حرف التاء','134','0','0');
+INSERT INTO [t7015] VALUES (4,'حرف الثاء','150','0','0');
+INSERT INTO [t7015] VALUES (5,'حرف الجيم','173','0','0');
+INSERT INTO [t7015] VALUES (6,'حرف الحاء','241','0','0');
+INSERT INTO [t7015] VALUES (7,'حرف الخاء','336','0','0');
+INSERT INTO [t7015] VALUES (8,'حرف الدال','396','0','0');
+INSERT INTO [t7015] VALUES (9,'حرف الذال','442','0','0');
+INSERT INTO [t7015] VALUES (10,'حرف الراء','460','0','0');
+INSERT INTO [t7015] VALUES (11,'حرف الزاى','541','0','0');
+INSERT INTO [t7015] VALUES (12,'حرف السين','582','0','0');
+INSERT INTO [t7015] VALUES (13,'حرف الشين','655','0','0');
+INSERT INTO [t7015] VALUES (14,'حرف الصاد','715','0','0');
+INSERT INTO [t7015] VALUES (15,'حرف الضاد','764','0','0');
+INSERT INTO [t7015] VALUES (16,'حرف الطاء','792','0','0');
+INSERT INTO [t7015] VALUES (17,'حرف الظاء','813','0','0');
+INSERT INTO [t7015] VALUES (18,'حرف العين','823','0','0');
+INSERT INTO [t7015] VALUES (19,'حرف الغين','929','0','0');
+INSERT INTO [t7015] VALUES (20,'حرف الفاء','968','0','0');
+INSERT INTO [t7015] VALUES (21,'حرف القاف','1036','0','0');
+INSERT INTO [t7015] VALUES (22,'حرف الكاف','1124','0','0');
+INSERT INTO [t7015] VALUES (23,'حرف اللام','1176','0','0');
+INSERT INTO [t7015] VALUES (24,'حرف الميم','1224','0','0');
+INSERT INTO [t7015] VALUES (25,'حرف النون','1282','0','0');
+INSERT INTO [t7015] VALUES (26,'حرف الواو','1364','0','0');
+INSERT INTO [t7015] VALUES (27,'حرف الهاء','1414','0','0');
+INSERT INTO [t7015] VALUES (28,'حرف الياء','1452','0','0');
