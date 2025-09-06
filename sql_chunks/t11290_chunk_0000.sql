70ff918055186392b40def3a8f10d8c33e7b2747
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:30 +0700

    Upload SQL chunk: t11290_chunk_0000.sql

---
+++ sql_chunks/t11290_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t11290
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t11290" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11290
+INSERT INTO [t11290] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11290] VALUES (2,'آثار السلف في لزوم الأثر','2','0','0');
+INSERT INTO [t11290] VALUES (3,'نقول عن السلف في ذلك','6','0','0');
+INSERT INTO [t11290] VALUES (4,'من الأمثلة العملية لتطبيق أهل السنة والجماعة لهذا المنهج','19','0','0');
+INSERT INTO [t11290] VALUES (5,'تعريف موجز برسالة ابن تيمية المحققة','25','0','0');
+INSERT INTO [t11290] VALUES (6,'نص الرسالة','27','0','0');
