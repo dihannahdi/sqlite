78f350174ec043507c6460586ac30cf747755c21
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:54 +0700

    Upload SQL chunk: t30935_chunk_0000.sql

---
+++ sql_chunks/t30935_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t30935
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t30935" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30935
+INSERT INTO [t30935] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t30935] VALUES (2,'المنهج','3','0','0');
+INSERT INTO [t30935] VALUES (3,'أقم الصلاة','10','0','0');
+INSERT INTO [t30935] VALUES (4,'وهنا على وهن','18','0','0');
+INSERT INTO [t30935] VALUES (5,'وإيتاء ذي القربى','26','0','0');
+INSERT INTO [t30935] VALUES (6,'وكونوا مع الصادقين','34','0','0');
+INSERT INTO [t30935] VALUES (7,'قرضا حسنا','44','0','0');
+INSERT INTO [t30935] VALUES (8,'وكان بين ذلك قواما','53','0','0');
+INSERT INTO [t30935] VALUES (9,'حتى تستأنسوا وتسلموا','60','0','0');
+INSERT INTO [t30935] VALUES (10,'ويدرؤون بالحسنة السيئة','71','0','0');
+INSERT INTO [t30935] VALUES (11,'أيحب أحدكم','84','0','0');
+INSERT INTO [t30935] VALUES (12,'السحاب الثقال','94','0','0');
+INSERT INTO [t30935] VALUES (13,'واخفض جناحك','101','0','0');
+INSERT INTO [t30935] VALUES (14,'والجار الجنب','112','0','0');
+INSERT INTO [t30935] VALUES (15,'ولئن صبرتم','122','0','0');
+INSERT INTO [t30935] VALUES (16,'والعافين عن الناس','133','0','0');
+INSERT INTO [t30935] VALUES (17,'ورتل القرآن ترتيلا','141','0','0');
+INSERT INTO [t30935] VALUES (18,'خاتمة','149','0','0');
