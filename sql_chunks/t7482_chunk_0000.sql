fcf769857b1112988df2fbbc00b32fe80ada4b43
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:49 +0700

    Upload SQL chunk: t7482_chunk_0000.sql

---
+++ sql_chunks/t7482_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t7482
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t7482" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7482
+INSERT INTO [t7482] VALUES (1,'أثر دعوة الشيخ محمد بن عبد الوهاب في حركة عثمان بن فودي الإصلاحية في غرب أفريقيا (مطبوع ضمن بحوث ندوة دعوة الشيخ محمد بن عبد الوهاب)','1','0','0');
+INSERT INTO [t7482] VALUES (2,'ظهور الدعوة:','1','0','0');
+INSERT INTO [t7482] VALUES (3,'انتشار الدعوة:','3','0','0');
+INSERT INTO [t7482] VALUES (4,'الإسلام وغرب أفريقية:','4','0','0');
+INSERT INTO [t7482] VALUES (5,'عثمان بن فودي والدعوة إلى الإصلاح:','8','0','0');
