5755202084dbd00f7cca5fe7b62764a096181ef2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:23 +0700

    Upload SQL chunk: t11279_chunk_0000.sql

---
+++ sql_chunks/t11279_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t11279
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t11279" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11279
+INSERT INTO [t11279] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11279] VALUES (2,'مدخل','22','0','0');
+INSERT INTO [t11279] VALUES (3,'الإستواء','25','0','0');
+INSERT INTO [t11279] VALUES (4,'اثبات صفة الوجه','33','0','0');
+INSERT INTO [t11279] VALUES (5,'النزول','36','0','0');
+INSERT INTO [t11279] VALUES (6,'إثبات صفة اليدين','42','0','0');
+INSERT INTO [t11279] VALUES (7,'النفس','44','0','0');
+INSERT INTO [t11279] VALUES (8,'الرؤية','47','0','0');
+INSERT INTO [t11279] VALUES (9,'كلام الله','50','0','0');
+INSERT INTO [t11279] VALUES (10,'القدر','67','0','0');
+INSERT INTO [t11279] VALUES (11,'الاسراء والمعراج','70','0','0');
+INSERT INTO [t11279] VALUES (12,'رؤية النبي صلى الله عليه وسلم ربه','73','0','0');
+INSERT INTO [t11279] VALUES (13,'الشفاعة','77','0','0');
+INSERT INTO [t11279] VALUES (14,'الايمان بالحوض','79','0','0');
+INSERT INTO [t11279] VALUES (15,'الإيمان بعذاب القبر','81','0','0');
+INSERT INTO [t11279] VALUES (16,'الإيمان بالميزان','84','0','0');
+INSERT INTO [t11279] VALUES (17,'الإيمان','85','0','0');
+INSERT INTO [t11279] VALUES (18,'الفرق بين الإسلام والإيمان','88','0','0');
+INSERT INTO [t11279] VALUES (19,'خروج الدجال ونزول عيسى عليه السلام','91','0','0');
+INSERT INTO [t11279] VALUES (20,'الإيمان بملك الموت','93','0','0');
+INSERT INTO [t11279] VALUES (21,'فضائل المصطفى صلي الله عليه وسلم وخصائصه','95','0','0');
+INSERT INTO [t11279] VALUES (22,'الخلفاء رضي الله عنهم','98','0','0');
+INSERT INTO [t11279] VALUES (23,'فضل الاتباع','101','0','0');
+INSERT INTO [t11279] VALUES (24,'مصادر ومراجع','114','0','0');
