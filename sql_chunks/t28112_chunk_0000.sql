daa7c8851bf10ae2facd763793e08f3e7a80fef7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:22 +0700

    Upload SQL chunk: t28112_chunk_0000.sql

---
+++ sql_chunks/t28112_chunk_0000.sql
@@ -0,0 +1,62 @@
+-- Table: t28112
+-- Chunk: 1
+-- Rows: 55
+
+CREATE TABLE "t28112" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t28112
+INSERT INTO [t28112] VALUES (1,'المقامة المسكية','1','0','0');
+INSERT INTO [t28112] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
+INSERT INTO [t28112] VALUES (3,'قال تعالى فيما تلاه الدارسون (يسقون من رحيق مختوم، ختامه مسك وفي ذلك فلينافس','4','1','0');
+INSERT INTO [t28112] VALUES (4,'العنبر','6','1','0');
+INSERT INTO [t28112] VALUES (5,'الزعفران','8','1','0');
+INSERT INTO [t28112] VALUES (6,'الزباد','9','1','0');
+INSERT INTO [t28112] VALUES (7,'المقامة الوردية','10','0','0');
+INSERT INTO [t28112] VALUES (8,'بسم الله الرحمن الرحيم','10','7','0');
+INSERT INTO [t28112] VALUES (9,'فهجم الورد بشوكته ونجم من بين الرياحين معجبا بإشراق صورته، وقال: بسم الله','10','7','0');
+INSERT INTO [t28112] VALUES (10,'النرجس','12','7','0');
+INSERT INTO [t28112] VALUES (11,'الياسمين','13','7','0');
+INSERT INTO [t28112] VALUES (12,'البان','14','7','0');
+INSERT INTO [t28112] VALUES (13,'النسرين','15','7','0');
+INSERT INTO [t28112] VALUES (14,'البنفسج','16','7','0');
+INSERT INTO [t28112] VALUES (15,'النيلوفر','17','7','0');
+INSERT INTO [t28112] VALUES (16,'الآس','18','7','0');
+INSERT INTO [t28112] VALUES (17,'الريحان','19','7','0');
+INSERT INTO [t28112] VALUES (18,'وأنا الوارد في عليكم بالمرزنحوش فشموه فإنه جيد للخشام، والمؤذن لأصحاب الأرق','20','7','0');
+INSERT INTO [t28112] VALUES (19,'المقامة التفاحية','23','0','0');
+INSERT INTO [t28112] VALUES (20,'الرمان','23','19','0');
+INSERT INTO [t28112] VALUES (21,'الأترج','27','19','0');
+INSERT INTO [t28112] VALUES (22,'السفرجل','29','19','0');
+INSERT INTO [t28112] VALUES (23,'التفاح Pyrus malus','30','19','0');
+INSERT INTO [t28112] VALUES (24,'الكمثري','32','19','0');
+INSERT INTO [t28112] VALUES (25,'النبق Rhammus frangula','33','19','0');
+INSERT INTO [t28112] VALUES (26,'وما أدراك ما النبق. قال الملك المعبود: (في سدر مخضود) [الواقعة٢٨] . وفي الحديث عن','33','19','0');
+INSERT INTO [t28112] VALUES (27,'الخوخ Pronus Spinosa','34','19','0');
+INSERT INTO [t28112] VALUES (28,'المقامة الزمردية','36','0','0');
+INSERT INTO [t28112] VALUES (29,'القرع Cucurbita Pepo','36','28','0');
+INSERT INTO [t28112] VALUES (30,'الهندبا cichorium intybus','38','28','0');
+INSERT INTO [t28112] VALUES (31,'الخس','39','28','0');
+INSERT INTO [t28112] VALUES (32,'الرجلة','39','28','0');
+INSERT INTO [t28112] VALUES (33,'البامية','41','28','0');
+INSERT INTO [t28112] VALUES (34,'الملوخيا','41','28','0');
+INSERT INTO [t28112] VALUES (35,'الخبازي','41','28','0');
+INSERT INTO [t28112] VALUES (36,'المقامة الفستقية','42','0','0');
+INSERT INTO [t28112] VALUES (37,'الفستق','42','36','0');
+INSERT INTO [t28112] VALUES (38,'وأما اللوز','43','36','0');
+INSERT INTO [t28112] VALUES (39,'وأما اللوز المقلي','43','36','0');
+INSERT INTO [t28112] VALUES (40,'وأما اللوز الأخضر','43','36','0');
+INSERT INTO [t28112] VALUES (41,'وأما الجوز','44','36','0');
+INSERT INTO [t28112] VALUES (42,'وأما البندق','44','36','0');
+INSERT INTO [t28112] VALUES (43,'وأما الشاهبلوط','44','36','0');
+INSERT INTO [t28112] VALUES (44,'وأما حب الزلم','45','36','0');
+INSERT INTO [t28112] VALUES (45,'وأما حب الصنوبر','45','36','0');
+INSERT INTO [t28112] VALUES (46,'المقامة الياقوتية','45','0','0');
+INSERT INTO [t28112] VALUES (47,'بسم الله الرحمن الرحيم','45','46','0');
+INSERT INTO [t28112] VALUES (48,'فقال في الياقوت','45','46','0');
+INSERT INTO [t28112] VALUES (49,'الحمد لله الذي خلقني في أحسن تقويم، وجعلني أبهى في العين من الدر النظيم، وشرفني','45','46','0');
+INSERT INTO [t28112] VALUES (50,'وقال اللؤلؤ','47','46','0');
+INSERT INTO [t28112] VALUES (51,'وقال الزمرد','49','46','0');
+INSERT INTO [t28112] VALUES (52,'وقال المرجان','50','46','0');
+INSERT INTO [t28112] VALUES (53,'وقال الزبرجد','51','46','0');
+INSERT INTO [t28112] VALUES (54,'وقال العقيق','52','46','0');
+INSERT INTO [t28112] VALUES (55,'وقال الفيروزج','53','46','0');
