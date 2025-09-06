b734d1dfe3e046a05619f2a935f1acdf94764676
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:07 +0700

    Upload SQL chunk: t1918_chunk_0000.sql

---
+++ sql_chunks/t1918_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t1918
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t1918" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1918
+INSERT INTO [t1918] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1918] VALUES (2,'العدل','2','0','0');
+INSERT INTO [t1918] VALUES (3,'مفهوم العدل','2','2','0');
+INSERT INTO [t1918] VALUES (4,'أنواع العدل','4','2','0');
+INSERT INTO [t1918] VALUES (5,'ثمرة العدل','13','2','0');
+INSERT INTO [t1918] VALUES (6,'تطبيقات العدل في الإسلام','16','2','0');
+INSERT INTO [t1918] VALUES (7,'الحرية','21','0','0');
+INSERT INTO [t1918] VALUES (8,'مفهوم الحرية','21','7','0');
+INSERT INTO [t1918] VALUES (9,'أنواع الحرية','23','7','0');
+INSERT INTO [t1918] VALUES (10,'المساواة','48','0','0');
+INSERT INTO [t1918] VALUES (11,'تعريفها','48','10','0');
+INSERT INTO [t1918] VALUES (12,'أهميتها','49','10','0');
+INSERT INTO [t1918] VALUES (13,'أنواعها','50','10','0');
+INSERT INTO [t1918] VALUES (14,'من مظاهر التسوية بين المسلمين','62','10','0');
+INSERT INTO [t1918] VALUES (15,'الإخاء','65','0','0');
+INSERT INTO [t1918] VALUES (16,'مفهوم الإخاء','65','15','0');
+INSERT INTO [t1918] VALUES (17,'أنواع الإخاء','67','15','0');
+INSERT INTO [t1918] VALUES (18,'بناء الإسلام الأخوة على أساس الإيمان دون النسب والجنس','70','15','0');
+INSERT INTO [t1918] VALUES (19,'مقومات الأخوة الإسلامية','71','15','0');
+INSERT INTO [t1918] VALUES (20,'ثمرة الإخاء','77','15','0');
+INSERT INTO [t1918] VALUES (21,'الجمال','79','0','0');
+INSERT INTO [t1918] VALUES (22,'تعريفه','79','21','0');
+INSERT INTO [t1918] VALUES (23,'أهميته','80','21','0');
+INSERT INTO [t1918] VALUES (24,'مقومات الجمال','82','21','0');
+INSERT INTO [t1918] VALUES (25,'أنواع الجمال','85','21','0');
+INSERT INTO [t1918] VALUES (26,'ميادين الجمال','89','21','0');
