a270d2ce6fef5d1cb7732c19fa4e94419f8de326
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:06 +0700

    Upload SQL chunk: t1242_chunk_0000.sql

---
+++ sql_chunks/t1242_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t1242
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t1242" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1242
+INSERT INTO [t1242] VALUES (1,'المقدمة','3','0','0');
+INSERT INTO [t1242] VALUES (2,'حرف الألف','7','0','0');
+INSERT INTO [t1242] VALUES (3,'حرف الباء','509','0','0');
+INSERT INTO [t1242] VALUES (4,'حرف التاء','630','0','0');
+INSERT INTO [t1242] VALUES (5,'حرف الثاء','659','0','0');
+INSERT INTO [t1242] VALUES (6,'حرف الجيم','670','0','0');
+INSERT INTO [t1242] VALUES (7,'حرف الحاء','736','0','0');
+INSERT INTO [t1242] VALUES (8,'حرف الدال','862','0','0');
+INSERT INTO [t1242] VALUES (9,'حرف الذال','914','0','0');
+INSERT INTO [t1242] VALUES (10,'حرف الراء','919','0','0');
+INSERT INTO [t1242] VALUES (11,'حرف الزاي','967','0','0');
+INSERT INTO [t1242] VALUES (12,'حرف السين','1001','0','0');
+INSERT INTO [t1242] VALUES (13,'حرف الشين','1094','0','0');
+INSERT INTO [t1242] VALUES (14,'حرف الصاد','1183','0','0');
+INSERT INTO [t1242] VALUES (15,'حرف الضاد','1225','0','0');
+INSERT INTO [t1242] VALUES (16,'حرف الطاء','1227','0','0');
+INSERT INTO [t1242] VALUES (17,'حرف الظاء','1261','0','0');
+INSERT INTO [t1242] VALUES (18,'حرف العين','1262','0','0');
+INSERT INTO [t1242] VALUES (19,'حرف الغين','1411','0','0');
+INSERT INTO [t1242] VALUES (20,'حرف الفاء','1428','0','0');
+INSERT INTO [t1242] VALUES (21,'حرف القاف','1484','0','0');
+INSERT INTO [t1242] VALUES (22,'حرف الكاف','1543','0','0');
+INSERT INTO [t1242] VALUES (23,'حرف اللام','1589','0','0');
+INSERT INTO [t1242] VALUES (24,'حرف الميم','1607','0','0');
+INSERT INTO [t1242] VALUES (25,'حرف النون','1838','0','0');
+INSERT INTO [t1242] VALUES (26,'حرف الهاء','1892','0','0');
+INSERT INTO [t1242] VALUES (27,'حرف الواو','1910','0','0');
+INSERT INTO [t1242] VALUES (28,'حرف الياء','1933','0','0');
