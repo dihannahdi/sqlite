ef962e7adfec932142cb8de816393839d356c1a4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:38 +0700

    Upload SQL chunk: t6167_chunk_0000.sql

---
+++ sql_chunks/t6167_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t6167
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t6167" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6167
+INSERT INTO [t6167] VALUES (1,'مقدمة الشارح','1','0','0');
+INSERT INTO [t6167] VALUES (2,'المنفرجة لابن النحوي','4','0','0');
+INSERT INTO [t6167] VALUES (3,'المنفرجة للغزالي','100','0','0');
+INSERT INTO [t6167] VALUES (4,'شعر','100','3','0');
+INSERT INTO [t6167] VALUES (5,'قصيدة المنفرجة','100','3','0');
+INSERT INTO [t6167] VALUES (6,'حجة الإسلام أبي حامد الغزالي','100','3','0');
+INSERT INTO [t6167] VALUES (7,'وصف الشدائد والكروب والمحن وطلب الفرج والعفو والنجاة من الله والإلحاح في الطلب','100','3','0');
+INSERT INTO [t6167] VALUES (8,'القسم على الله تعالى بهذه الأمور','101','3','0');
+INSERT INTO [t6167] VALUES (9,'دعاء وابتهال','102','3','0');
+INSERT INTO [t6167] VALUES (10,'حديث لوم وعتاب ونصح للنفس','103','3','0');
+INSERT INTO [t6167] VALUES (11,'دعوة للإقتداء بالنبي الهادي وآله وصحابته','103','3','0');
+INSERT INTO [t6167] VALUES (12,'ختم القصيدة','104','3','0');
