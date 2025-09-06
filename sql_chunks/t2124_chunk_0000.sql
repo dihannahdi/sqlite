b40633075561b23c98925c9ef180efd9cf90bef1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:13 +0700

    Upload SQL chunk: t2124_chunk_0000.sql

---
+++ sql_chunks/t2124_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t2124
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t2124" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2124
+INSERT INTO [t2124] VALUES (1,'-شذرات','1','0','0');
+INSERT INTO [t2124] VALUES (2,'منزلةالعلم بخصائص لسان العربية ومنهج الإبانة فيه','5','0','0');
+INSERT INTO [t2124] VALUES (3,'التحريض على الجهاد في سبيل الله تعالى','15','0','0');
+INSERT INTO [t2124] VALUES (4,'السياق','15','3','0');
+INSERT INTO [t2124] VALUES (5,'القراءات القرآنية في الآيات','20','3','0');
+INSERT INTO [t2124] VALUES (6,'المفردة القرآنية','27','3','0');
+INSERT INTO [t2124] VALUES (7,'السمات البلاغية وفقه المعنى القرآني','34','3','0');
+INSERT INTO [t2124] VALUES (8,'بلاغة النداء في قوله تعالى:','35','3','0');
+INSERT INTO [t2124] VALUES (9,'بلاغة الاستفهام في قوله تعالى:','39','3','0');
+INSERT INTO [t2124] VALUES (10,'بلاغة الاستفهام في قوله تعالى:','48','3','0');
+INSERT INTO [t2124] VALUES (11,'بلاغة القصر في قوله تعالى','52','3','0');
+INSERT INTO [t2124] VALUES (12,'بلاغة أسلوب الشرط في قوله تعالى','55','3','0');
+INSERT INTO [t2124] VALUES (13,'بلاغة الشرط في قوله تعالى:','63','3','0');
+INSERT INTO [t2124] VALUES (14,'خاتمة','105','0','0');
