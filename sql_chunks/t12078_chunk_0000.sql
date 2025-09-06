388bce92175a40e6c712009261660f11107209be
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:36 +0700

    Upload SQL chunk: t12078_chunk_0000.sql

---
+++ sql_chunks/t12078_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t12078
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t12078" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12078
+INSERT INTO [t12078] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t12078] VALUES (2,'باب يتغير الزمان حتى تعبد الأوثان','31','0','0');
+INSERT INTO [t12078] VALUES (3,'باب في وجوب عداوة أعداء الله من الكفار والمرتدين والمنافقين','32','0','0');
