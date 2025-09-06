5caf4a09cce45880e35555f2c68b0938235e33e4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:52 +0700

    Upload SQL chunk: t10103_chunk_0000.sql

---
+++ sql_chunks/t10103_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t10103
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t10103" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10103
+INSERT INTO [t10103] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10103] VALUES (2,'باب: دراسة حياة المصنف','4','0','0');
+INSERT INTO [t10103] VALUES (3,'الدراسات السابقة عن القاضي بدر الدين محمد بن إبراهيم بن جماعة التي وقفت عليها','4','2','0');
+INSERT INTO [t10103] VALUES (4,'ترجمة المصنف','6','2','0');
+INSERT INTO [t10103] VALUES (5,'أسمه ونسبه وكنيته ولقبه ومذهبه','7','2','0');
+INSERT INTO [t10103] VALUES (6,'مولده','9','2','0');
+INSERT INTO [t10103] VALUES (7,'شيوخه','10','2','0');
+INSERT INTO [t10103] VALUES (8,'تلاميذه','12','2','0');
+INSERT INTO [t10103] VALUES (9,'أعماله','13','2','0');
+INSERT INTO [t10103] VALUES (10,'عقيدته','15','2','0');
+INSERT INTO [t10103] VALUES (11,'مصنفاته','16','2','0');
+INSERT INTO [t10103] VALUES (12,'ثناء العلماء عليه','27','2','0');
+INSERT INTO [t10103] VALUES (13,'باب: دراسة الكتاب','29','0','0');
+INSERT INTO [t10103] VALUES (14,'نسبة الكتاب إلى المؤلف','29','13','0');
+INSERT INTO [t10103] VALUES (15,'أهمية الكتاب','30','13','0');
+INSERT INTO [t10103] VALUES (16,'منهجه','31','13','0');
+INSERT INTO [t10103] VALUES (17,'مصادر المصنف','33','13','0');
+INSERT INTO [t10103] VALUES (18,'وصف النسختين المعتمدتين في التحقيق','34','13','0');
+INSERT INTO [t10103] VALUES (19,'عملي في التحقيق','37','13','0');
+INSERT INTO [t10103] VALUES (20,'نماذج من مخطوطتي الكتاب','38','13','0');
+INSERT INTO [t10103] VALUES (21,'افتتاحية المصنف لكتابه','41','13','0');
+INSERT INTO [t10103] VALUES (22,'سبب تأليفه الكتاب','42','13','0');
+INSERT INTO [t10103] VALUES (23,'تعريف المزارعة والمخابرة في اللغة','43','13','0');
+INSERT INTO [t10103] VALUES (24,'تعريف المزارعة والمخابرة في الاصطلاح','45','13','0');
+INSERT INTO [t10103] VALUES (25,'ضربا المزارعة والمخابرة','47','13','0');
