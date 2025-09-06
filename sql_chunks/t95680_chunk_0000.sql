0db717c109176230c12106e4209e1f7e55549729
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:09 +0700

    Upload SQL chunk: t95680_chunk_0000.sql

---
+++ sql_chunks/t95680_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t95680
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t95680" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95680
+INSERT INTO [t95680] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t95680] VALUES (2,'نسبها وولادتها','1','0','0');
+INSERT INTO [t95680] VALUES (3,'ولادتها','1','0','0');
+INSERT INTO [t95680] VALUES (4,'نشأتها','2','0','0');
+INSERT INTO [t95680] VALUES (5,'في ميدان التجارة','2','0','0');
+INSERT INTO [t95680] VALUES (6,'الطاهرة (سيدة قريش)','3','0','0');
+INSERT INTO [t95680] VALUES (7,'الأمين على تجارة خديجة','3','0','0');
+INSERT INTO [t95680] VALUES (8,'العودة الميمونة','5','0','0');
+INSERT INTO [t95680] VALUES (9,'بين الواقع والرؤيا','5','0','0');
+INSERT INTO [t95680] VALUES (10,'رجع الرؤيا','6','0','0');
+INSERT INTO [t95680] VALUES (11,'الزواج الميمون','6','0','0');
+INSERT INTO [t95680] VALUES (12,'زهرات بيت النبوة','8','0','0');
+INSERT INTO [t95680] VALUES (13,'الحكمة النبوية','9','0','0');
+INSERT INTO [t95680] VALUES (14,'عام الحزن','10','0','0');
+INSERT INTO [t95680] VALUES (15,'خاتمة','11','0','0');
