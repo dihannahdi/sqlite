89dc301e7e7e67e47b6644d0866cab53a859e707
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:10 +0700

    Upload SQL chunk: t36417_chunk_0000.sql

---
+++ sql_chunks/t36417_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t36417
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t36417" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36417
+INSERT INTO [t36417] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36417] VALUES (2,'الكنز الغالي','4','0','0');
+INSERT INTO [t36417] VALUES (3,'أول حساب يوم القيامة','5','0','0');
+INSERT INTO [t36417] VALUES (4,'لماذا لا تصلي','7','0','0');
+INSERT INTO [t36417] VALUES (5,'إلى من صلى ولكن','9','0','0');
+INSERT INTO [t36417] VALUES (6,'الخشوع هو الصلاة','12','0','0');
+INSERT INTO [t36417] VALUES (7,'الصلاة راحة .. وفرج .. ونور .. ونجاة','15','0','0');
+INSERT INTO [t36417] VALUES (8,'الصلاة كفارة ..','19','0','0');
+INSERT INTO [t36417] VALUES (9,'أخي .. تلك هي الصلاة','20','0','0');
+INSERT INTO [t36417] VALUES (10,'أخي المسلم','21','0','0');
