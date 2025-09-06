a092551825b5b0319f766bf98681fcfa738e19bf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:09 +0700

    Upload SQL chunk: t23144_chunk_0000.sql

---
+++ sql_chunks/t23144_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t23144
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t23144" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23144
+INSERT INTO [t23144] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t23144] VALUES (2,'الفصل الأول: ما حول الأرض من نجوم وكواكب','17','0','0');
+INSERT INTO [t23144] VALUES (3,'الفصل الثاني: الغلاف الجوي حول الأرض','27','0','0');
+INSERT INTO [t23144] VALUES (4,'الفصل الثالث: اليابسة','47','0','0');
+INSERT INTO [t23144] VALUES (5,'الفصل الرابع: الغلاف المائي للقشرة الأرضية','76','0','0');
+INSERT INTO [t23144] VALUES (6,'الفصل الخامس: الزمان والمكان','91','0','0');
+INSERT INTO [t23144] VALUES (7,'الفصل السادس: الضياء والنار والطاقة','117','0','0');
+INSERT INTO [t23144] VALUES (8,'الفصل السابع: ما ينفع الناس','128','0','0');
+INSERT INTO [t23144] VALUES (9,'الخاتمة','186','0','0');
+INSERT INTO [t23144] VALUES (10,'مراجع','192','0','0');
+INSERT INTO [t23144] VALUES (11,'أشكال','196','0','0');
+INSERT INTO [t23144] VALUES (12,'الفهرس','201','0','0');
