c7c4953dce963ffd607e12ea96195ad977183dc6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:26 +0700

    Upload SQL chunk: t178_chunk_0000.sql

---
+++ sql_chunks/t178_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t178
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t178" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t178
+INSERT INTO [t178] VALUES (1,'بسم الله الرحمن الرحيم وبه نستعين','1','0','0');
+INSERT INTO [t178] VALUES (2,'جزء من اتباع السنن واجتناب البدع','1','0','0');
+INSERT INTO [t178] VALUES (3,'باب ذكر فتنة النساء','19','0','0');
+INSERT INTO [t178] VALUES (4,'باب تعظيم مس امرأة غير محرم','22','0','0');
+INSERT INTO [t178] VALUES (5,'باب في تحريم الدخول على النساء والخلوة بهن والسفر إلا مع ذي محرم','24','0','0');
+INSERT INTO [t178] VALUES (6,'باب الأمر بغض البصر','29','0','0');
+INSERT INTO [t178] VALUES (7,'باب ذكر أن زنا العينين النظر','34','0','0');
+INSERT INTO [t178] VALUES (8,'باب في كراهية النظر إلى الأحداث','35','0','0');
+INSERT INTO [t178] VALUES (9,'باب ما كره من الغناء','40','0','0');
+INSERT INTO [t178] VALUES (10,'باب ما كره من المزمار والطبل','48','0','0');
+INSERT INTO [t178] VALUES (11,'باب ما كره من الرقص ونحوه','56','0','0');
