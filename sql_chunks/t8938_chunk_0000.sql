58bf61e3027f661bd50b2cd71b12a24510bd481e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:47 +0700

    Upload SQL chunk: t8938_chunk_0000.sql

---
+++ sql_chunks/t8938_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t8938
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t8938" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8938
+INSERT INTO [t8938] VALUES (1,'تقديم وتعريف','1','0','0');
+INSERT INTO [t8938] VALUES (2,'تقديم وتعريف','1','1','0');
+INSERT INTO [t8938] VALUES (3,'حجية المصلحة المرسلة','3','0','0');
+INSERT INTO [t8938] VALUES (4,'حجية المصلحة المرسلة','3','3','0');
+INSERT INTO [t8938] VALUES (5,'المصلحة بين الافراط والتفريط','4','3','0');
+INSERT INTO [t8938] VALUES (6,'المصلحة المرسلة عند الجمهورر','9','3','0');
+INSERT INTO [t8938] VALUES (7,'شروط المصلحة','12','3','0');
+INSERT INTO [t8938] VALUES (8,'مانسب الى الصحابة خطأ حول المصلحة','14','0','0');
+INSERT INTO [t8938] VALUES (9,'مدخل','14','8','0');
+INSERT INTO [t8938] VALUES (10,'ابطال سهم الؤلفة قلوبهم','16','8','0');
+INSERT INTO [t8938] VALUES (11,'في قتل الجماعة بالواحد','18','8','0');
+INSERT INTO [t8938] VALUES (12,'تعطيل حد السرقة عام المجاعة','19','8','0');
+INSERT INTO [t8938] VALUES (13,'ايقاع طلاق الثلاث بلفظ واحد طلاقا بائنا','20','8','0');
+INSERT INTO [t8938] VALUES (14,'نظرة في المصلحة المرسلة','22','0','0');
+INSERT INTO [t8938] VALUES (15,'نظرة في المصلحة المرسلة','22','14','0');
