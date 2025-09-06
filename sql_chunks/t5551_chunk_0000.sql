07f8f7efc9bf806b4f24aa134998f2aa61a39951
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:23:38 +0700

    Upload SQL chunk: t5551_chunk_0000.sql

---
+++ sql_chunks/t5551_chunk_0000.sql
@@ -0,0 +1,44 @@
+-- Table: t5551
+-- Chunk: 1
+-- Rows: 37
+
+CREATE TABLE "t5551" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5551
+INSERT INTO [t5551] VALUES (1,'مقدمة المؤلف','3','0','0');
+INSERT INTO [t5551] VALUES (2,'سورة البقرة','8','0','0');
+INSERT INTO [t5551] VALUES (3,'سورة آل عمران','26','0','0');
+INSERT INTO [t5551] VALUES (4,'سورة الأنعام','31','0','0');
+INSERT INTO [t5551] VALUES (5,'سورة الأعراف','36','0','0');
+INSERT INTO [t5551] VALUES (6,'سورة التوبة','41','0','0');
+INSERT INTO [t5551] VALUES (7,'سورة يونس','42','0','0');
+INSERT INTO [t5551] VALUES (8,'سورة هود','58','0','0');
+INSERT INTO [t5551] VALUES (9,'سورة الرعد','60','0','0');
+INSERT INTO [t5551] VALUES (10,'سورة إبراهيم','70','0','0');
+INSERT INTO [t5551] VALUES (11,'سورة الحجر','72','0','0');
+INSERT INTO [t5551] VALUES (12,'سورة النحل','78','0','0');
+INSERT INTO [t5551] VALUES (13,'سورة الإسراء','81','0','0');
+INSERT INTO [t5551] VALUES (14,'سورة الكهف','83','0','0');
+INSERT INTO [t5551] VALUES (15,'سورة مريم','84','0','0');
+INSERT INTO [t5551] VALUES (16,'سورة طه','85','0','0');
+INSERT INTO [t5551] VALUES (17,'سورة الأنبياء','91','0','0');
+INSERT INTO [t5551] VALUES (18,'سورة الحج','95','0','0');
+INSERT INTO [t5551] VALUES (19,'سورة المؤمنين','98','0','0');
+INSERT INTO [t5551] VALUES (20,'سورة النور','99','0','0');
+INSERT INTO [t5551] VALUES (21,'سورة الفرقان','102','0','0');
+INSERT INTO [t5551] VALUES (22,'سورة الشعراء','104','0','0');
+INSERT INTO [t5551] VALUES (23,'سورة النمل','105','0','0');
+INSERT INTO [t5551] VALUES (24,'سورة العنكبوت','106','0','0');
+INSERT INTO [t5551] VALUES (25,'سورة الروم','107','0','0');
+INSERT INTO [t5551] VALUES (26,'سورة لقمان','109','0','0');
+INSERT INTO [t5551] VALUES (27,'سورة السجدة','110','0','0');
+INSERT INTO [t5551] VALUES (28,'سورة سبأ','111','0','0');
+INSERT INTO [t5551] VALUES (29,'سورة فاطر','112','0','0');
+INSERT INTO [t5551] VALUES (30,'سورة يس','113','0','0');
+INSERT INTO [t5551] VALUES (31,'سورة الصافات','121','0','0');
+INSERT INTO [t5551] VALUES (32,'سورة الشورى','122','0','0');
+INSERT INTO [t5551] VALUES (33,'سورة الدخان','123','0','0');
+INSERT INTO [t5551] VALUES (34,'سورة ق','124','0','0');
+INSERT INTO [t5551] VALUES (35,'سورة القمر','126','0','0');
+INSERT INTO [t5551] VALUES (36,'سورة الطلاق','128','0','0');
+INSERT INTO [t5551] VALUES (37,'آيات من سور متفرقة','131','0','0');
