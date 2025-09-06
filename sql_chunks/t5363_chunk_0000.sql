f288dce2dec7eb186a120e393ed53f9fb85d4a15
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:22 +0700

    Upload SQL chunk: t5363_chunk_0000.sql

---
+++ sql_chunks/t5363_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5363
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5363" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5363
+INSERT INTO [t5363] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5363] VALUES (2,'آخر الكتاب','99','0','0');
