fc167369972437d736e58953bad52961daa451e7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:40 +0700

    Upload SQL chunk: t6558_chunk_0000.sql

---
+++ sql_chunks/t6558_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6558
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6558" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6558
+INSERT INTO [t6558] VALUES (1,'بسم الله الرحمن الرحيم وبه نستعين','1','0','0');
+INSERT INTO [t6558] VALUES (2,'تم بهذا الكتاب','196','0','0');
