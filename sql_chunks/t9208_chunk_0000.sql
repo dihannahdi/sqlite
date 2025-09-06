dfa6479c3525341bb057a4a8aacc012cf9ccdecc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:51:31 +0700

    Upload SQL chunk: t9208_chunk_0000.sql

---
+++ sql_chunks/t9208_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t9208
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t9208" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9208
+INSERT INTO [t9208] VALUES (1,'ومما يذكر به غزارة الإبل','47','0','0');
+INSERT INTO [t9208] VALUES (2,'ما يذكر به البكاء','48','0','0');
+INSERT INTO [t9208] VALUES (3,'ومما يذكر من أسماء الإبل','83','0','0');
+INSERT INTO [t9208] VALUES (4,'ومما يذكر من أدواء الإبل','86','0','0');
+INSERT INTO [t9208] VALUES (5,'ومما يذكر من سير الإبل','96','0','0');
+INSERT INTO [t9208] VALUES (6,'ومما يذكر من ألوان الإبل','103','0','0');
+INSERT INTO [t9208] VALUES (7,'ومما يذكر من أظماء الإبل','106','0','0');
+INSERT INTO [t9208] VALUES (8,'ومما يذكر في المواسم والتزنيم','114','0','0');
+INSERT INTO [t9208] VALUES (9,'كتاب الإبل','121','0','0');
+INSERT INTO [t9208] VALUES (10,'ومن سير الإبل','121','0','0');
+INSERT INTO [t9208] VALUES (11,'ألوان الإبل','121','0','0');
+INSERT INTO [t9208] VALUES (12,'أسماء الأظماء','121','0','0');
+INSERT INTO [t9208] VALUES (13,'أدواء الإبل','121','0','0');
+INSERT INTO [t9208] VALUES (14,'أسماء عدد الإبل','121','0','0');
