83dac8c6c671663ee671f34533bb828afd21a094
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:14:03 +0700

    Upload SQL chunk: t37563_chunk_0000.sql

---
+++ sql_chunks/t37563_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t37563
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t37563" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37563
+INSERT INTO [t37563] VALUES (1,'مقدمة المؤلف','3','0','0');
+INSERT INTO [t37563] VALUES (2,'خصائص حضارتنا','4','0','0');
+INSERT INTO [t37563] VALUES (3,'آثار حضارتنا في التاريخ','13','0','0');
+INSERT INTO [t37563] VALUES (4,'النزعة الإنسانية','27','0','0');
+INSERT INTO [t37563] VALUES (5,'المساواة العنصرية','40','0','0');
+INSERT INTO [t37563] VALUES (6,'التسامح الديني','42','0','0');
+INSERT INTO [t37563] VALUES (7,'أخلاقنا الحربية','63','0','0');
+INSERT INTO [t37563] VALUES (8,'الرفق بالحيوان','85','0','0');
+INSERT INTO [t37563] VALUES (9,'المؤسسات الخيرية','96','0','0');
+INSERT INTO [t37563] VALUES (10,'المدارس والمعاهد العلمية','108','0','0');
+INSERT INTO [t37563] VALUES (11,'المستشفيات والمعاهد الطبية','120','0','0');
+INSERT INTO [t37563] VALUES (12,'المكتبات الخاصة والعامة','138','0','0');
+INSERT INTO [t37563] VALUES (13,'المجالس والندوات العلمية','151','0','0');
+INSERT INTO [t37563] VALUES (14,'العواصم والمدن الكبرى','162','0','0');
+INSERT INTO [t37563] VALUES (15,'المراجع','176','0','0');
