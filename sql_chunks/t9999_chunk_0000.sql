9dab7ab5aa8b6fd4e93a78bff04ad1d2318a0ce3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:50 +0700

    Upload SQL chunk: t9999_chunk_0000.sql

---
+++ sql_chunks/t9999_chunk_0000.sql
@@ -0,0 +1,57 @@
+-- Table: t9999
+-- Chunk: 1
+-- Rows: 50
+
+CREATE TABLE "t9999" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9999
+INSERT INTO [t9999] VALUES (1,'المقدمات','1','0','0');
+INSERT INTO [t9999] VALUES (2,'مقدمة الطبعة الثانية','1','1','0');
+INSERT INTO [t9999] VALUES (3,'بين يدي الكتاب','3','1','0');
+INSERT INTO [t9999] VALUES (4,'الباب الأول: شؤون العرب التجارية بين الجاهلية والإسلام','11','0','0');
+INSERT INTO [t9999] VALUES (5,'تجارة العرب','11','4','0');
+INSERT INTO [t9999] VALUES (6,'بيوع الجاهلية','42','4','0');
+INSERT INTO [t9999] VALUES (7,'ربا الجاهلية','56','4','0');
+INSERT INTO [t9999] VALUES (8,'المحلون والمحرمون والحمس','66','4','0');
+INSERT INTO [t9999] VALUES (9,'الباب الثاني: أحداث قريش التجارية','84','0','0');
+INSERT INTO [t9999] VALUES (10,'قريش التجار','84','9','0');
+INSERT INTO [t9999] VALUES (11,'إيلاف قريش','140','9','0');
+INSERT INTO [t9999] VALUES (12,'حرب الفجار','155','9','0');
+INSERT INTO [t9999] VALUES (13,'حلف الفضول','174','9','0');
+INSERT INTO [t9999] VALUES (14,'الباب الثالث: أسواق العرب','184','0','0');
+INSERT INTO [t9999] VALUES (15,'مدخل','184','14','0');
+INSERT INTO [t9999] VALUES (16,'في الجاهلية','210','14','0');
+INSERT INTO [t9999] VALUES (17,'سوق دومة الجندل','210','16','0');
+INSERT INTO [t9999] VALUES (18,'سوق المشقر','218','16','0');
+INSERT INTO [t9999] VALUES (19,'سوق هجر','223','16','0');
+INSERT INTO [t9999] VALUES (20,'سوق عمان','230','16','0');
+INSERT INTO [t9999] VALUES (21,'سوق حباشة','236','16','0');
+INSERT INTO [t9999] VALUES (22,'سوق صحار','239','16','0');
+INSERT INTO [t9999] VALUES (23,'سوق دبى','242','16','0');
+INSERT INTO [t9999] VALUES (24,'سوق الشحر','244','16','0');
+INSERT INTO [t9999] VALUES (25,'سوق عدن أبين','246','16','0');
+INSERT INTO [t9999] VALUES (26,'سوق صنعاء','249','16','0');
+INSERT INTO [t9999] VALUES (27,'سوق حضر موت','253','16','0');
+INSERT INTO [t9999] VALUES (28,'سوق عكاظ','255','16','0');
+INSERT INTO [t9999] VALUES (29,'سوق مجنة','322','16','0');
+INSERT INTO [t9999] VALUES (30,'سوق ذي المجاز','325','16','0');
+INSERT INTO [t9999] VALUES (31,'سوق نطاة خيبر','334','16','0');
+INSERT INTO [t9999] VALUES (32,'سوق حجر','336','16','0');
+INSERT INTO [t9999] VALUES (33,'سوق دير أيوب','340','16','0');
+INSERT INTO [t9999] VALUES (34,'سوق بصرى','342','16','0');
+INSERT INTO [t9999] VALUES (35,'سوق أذرعات','350','16','0');
+INSERT INTO [t9999] VALUES (36,'سوق الحيرة','352','16','0');
+INSERT INTO [t9999] VALUES (37,'في الإسلام','368','14','0');
+INSERT INTO [t9999] VALUES (38,'مدخل','368','37','0');
+INSERT INTO [t9999] VALUES (39,'البصره - المربد','370','37','0');
+INSERT INTO [t9999] VALUES (40,'سوق المربد','382','37','0');
+INSERT INTO [t9999] VALUES (41,'استدراك','428','0','0');
+INSERT INTO [t9999] VALUES (42,'مسارد الكتاب','430','0','0');
+INSERT INTO [t9999] VALUES (43,'مسرد الآيات الكريمة','430','42','0');
+INSERT INTO [t9999] VALUES (44,'مسرد الأحاديث النبوية','432','42','0');
+INSERT INTO [t9999] VALUES (45,'مسرد الأعلام','434','42','0');
+INSERT INTO [t9999] VALUES (46,'مسرد الجماعات','453','42','0');
+INSERT INTO [t9999] VALUES (47,'مسرد الأماكن','464','42','0');
+INSERT INTO [t9999] VALUES (48,'مسرد الأشعار','477','42','0');
+INSERT INTO [t9999] VALUES (49,'مسرد الكتب','488','42','0');
+INSERT INTO [t9999] VALUES (50,'فهرس الموصوعات','495','42','0');
