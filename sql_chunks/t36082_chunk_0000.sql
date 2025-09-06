61442bea5c1568dd42f9d4b6ccf0d5f3b2ad8c39
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:08 +0700

    Upload SQL chunk: t36082_chunk_0000.sql

---
+++ sql_chunks/t36082_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t36082
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t36082" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36082
+INSERT INTO [t36082] VALUES (1,'باب الحماسة','2','0','0');
+INSERT INTO [t36082] VALUES (2,'باب المراثي','119','0','0');
+INSERT INTO [t36082] VALUES (3,'الأدب','154','0','0');
+INSERT INTO [t36082] VALUES (4,'باب النسيب','174','0','0');
+INSERT INTO [t36082] VALUES (5,'باب الهجاء','202','0','0');
+INSERT INTO [t36082] VALUES (6,'باب السماحة والأضياف','234','0','0');
+INSERT INTO [t36082] VALUES (7,'باب الصفات','265','0','0');
+INSERT INTO [t36082] VALUES (8,'باب المشيب','271','0','0');
+INSERT INTO [t36082] VALUES (9,'باب الملح','278','0','0');
+INSERT INTO [t36082] VALUES (10,'باب مذمة النساء','286','0','0');
