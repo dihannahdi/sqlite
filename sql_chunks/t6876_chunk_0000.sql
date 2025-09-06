a8b951a226c2a7def3f0489641c8f9a81abdffce
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:19 +0700

    Upload SQL chunk: t6876_chunk_0000.sql

---
+++ sql_chunks/t6876_chunk_0000.sql
@@ -0,0 +1,60 @@
+-- Table: t6876
+-- Chunk: 1
+-- Rows: 53
+
+CREATE TABLE "t6876" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6876
+INSERT INTO [t6876] VALUES (1,'قسم البدايات','9','0','0');
+INSERT INTO [t6876] VALUES (2,'باب اليقظة قال الله عز وجل','9','0','0');
+INSERT INTO [t6876] VALUES (3,'باب التوبة','11','0','0');
+INSERT INTO [t6876] VALUES (4,'باب المحاسبة','14','0','0');
+INSERT INTO [t6876] VALUES (5,'باب الإنابة','14','0','0');
+INSERT INTO [t6876] VALUES (6,'باب التفكر','15','0','0');
+INSERT INTO [t6876] VALUES (7,'باب التذكر','17','0','0');
+INSERT INTO [t6876] VALUES (8,'باب الاعتصام','18','0','0');
+INSERT INTO [t6876] VALUES (9,'باب الفرار','20','0','0');
+INSERT INTO [t6876] VALUES (10,'باب الرياضة','21','0','0');
+INSERT INTO [t6876] VALUES (11,'باب السماع','21','0','0');
+INSERT INTO [t6876] VALUES (12,'قسم الأبواب','23','0','0');
+INSERT INTO [t6876] VALUES (13,'باب الحزن','23','0','0');
+INSERT INTO [t6876] VALUES (14,'باب الخوف','24','0','0');
+INSERT INTO [t6876] VALUES (15,'باب الاشفاق','25','0','0');
+INSERT INTO [t6876] VALUES (16,'باب الخشوع','26','0','0');
+INSERT INTO [t6876] VALUES (17,'باب الاخبات','27','0','0');
+INSERT INTO [t6876] VALUES (18,'باب الزهد','28','0','0');
+INSERT INTO [t6876] VALUES (19,'باب الورع','29','0','0');
+INSERT INTO [t6876] VALUES (20,'باب التبتل','30','0','0');
+INSERT INTO [t6876] VALUES (21,'باب الرجاء','31','0','0');
+INSERT INTO [t6876] VALUES (22,'باب الرغبة','33','0','0');
+INSERT INTO [t6876] VALUES (23,'قسم المعاملات','35','0','0');
+INSERT INTO [t6876] VALUES (24,'باب الرعاية','35','0','0');
+INSERT INTO [t6876] VALUES (25,'باب المراقبة','36','0','0');
+INSERT INTO [t6876] VALUES (26,'باب الحرمة','37','0','0');
+INSERT INTO [t6876] VALUES (27,'باب الإخلاص','38','0','0');
+INSERT INTO [t6876] VALUES (28,'باب التهذيب','39','0','0');
+INSERT INTO [t6876] VALUES (29,'باب الاستقامة','40','0','0');
+INSERT INTO [t6876] VALUES (30,'باب التوكل','41','0','0');
+INSERT INTO [t6876] VALUES (31,'باب التفويض','43','0','0');
+INSERT INTO [t6876] VALUES (32,'باب الثقة','44','0','0');
+INSERT INTO [t6876] VALUES (33,'باب التسليم','45','0','0');
+INSERT INTO [t6876] VALUES (34,'قسم الأخلاق','47','0','0');
+INSERT INTO [t6876] VALUES (35,'باب الصبر','47','0','0');
+INSERT INTO [t6876] VALUES (36,'باب الرضى','49','0','0');
+INSERT INTO [t6876] VALUES (37,'باب الشكر','51','0','0');
+INSERT INTO [t6876] VALUES (38,'باب الحياء','52','0','0');
+INSERT INTO [t6876] VALUES (39,'باب الصدق','53','0','0');
+INSERT INTO [t6876] VALUES (40,'باب الإيثار','55','0','0');
+INSERT INTO [t6876] VALUES (41,'باب الخلق','56','0','0');
+INSERT INTO [t6876] VALUES (42,'باب التواضع','58','0','0');
+INSERT INTO [t6876] VALUES (43,'باب الفتوة','59','0','0');
+INSERT INTO [t6876] VALUES (44,'باب الانبساط','60','0','0');
+INSERT INTO [t6876] VALUES (45,'قسم الأصول','62','0','0');
+INSERT INTO [t6876] VALUES (46,'باب القصد','62','0','0');
+INSERT INTO [t6876] VALUES (47,'باب العزم','63','0','0');
+INSERT INTO [t6876] VALUES (48,'باب الإرادة','64','0','0');
+INSERT INTO [t6876] VALUES (49,'باب الأدب','65','0','0');
+INSERT INTO [t6876] VALUES (50,'باب اليقين','66','0','0');
+INSERT INTO [t6876] VALUES (51,'باب الأنس','67','0','0');
+INSERT INTO [t6876] VALUES (52,'باب الذكر','68','0','0');
+INSERT INTO [t6876] VALUES (53,'باب الفقر','69','0','0');
