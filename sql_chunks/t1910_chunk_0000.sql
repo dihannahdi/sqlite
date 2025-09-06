cfbebd78cc0669851d20724592ebc5990b0e248a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:00 +0700

    Upload SQL chunk: t1910_chunk_0000.sql

---
+++ sql_chunks/t1910_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t1910
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t1910" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1910
+INSERT INTO [t1910] VALUES (1,'لماذا الحاجة إلى الرسل','1','0','0');
+INSERT INTO [t1910] VALUES (2,'النبوة منحة إلهية','4','0','0');
+INSERT INTO [t1910] VALUES (3,'بشرية الرسل','7','0','0');
+INSERT INTO [t1910] VALUES (4,'لماذا كان الرسل من البشر','7','3','0');
+INSERT INTO [t1910] VALUES (5,'مقتضى بشرية الرسل','9','3','0');
+INSERT INTO [t1910] VALUES (6,'اصطفاء الرسل','12','0','0');
+INSERT INTO [t1910] VALUES (7,'صفات الرسل','20','0','0');
+INSERT INTO [t1910] VALUES (8,'أولا الصدق','20','7','0');
+INSERT INTO [t1910] VALUES (9,'ثانيا الأمانة','22','7','0');
+INSERT INTO [t1910] VALUES (10,'ثالثا الفطنة','25','7','0');
+INSERT INTO [t1910] VALUES (11,'رابعا العصمة','28','7','0');
+INSERT INTO [t1910] VALUES (12,'وظائف الرسل','33','0','0');
+INSERT INTO [t1910] VALUES (13,'أولا البلاغ المبين','33','12','0');
+INSERT INTO [t1910] VALUES (14,'ثانيا دعوة الناس إلى الدين الحق ببيان ما يجب عليهم','35','12','0');
+INSERT INTO [t1910] VALUES (15,'ثالثا التبشير والإنذار','40','12','0');
+INSERT INTO [t1910] VALUES (16,'رابعا تقويم الفكر المنحرف والعقائد الزائفة','43','12','0');
+INSERT INTO [t1910] VALUES (17,'خامسا تدبير شئون الأمة عامة وسياسة أمرها','47','12','0');
