1d88cb7b028230d4514cb233006a06fae41f3055
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:20 +0700

    Upload SQL chunk: t30305_chunk_0000.sql

---
+++ sql_chunks/t30305_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30305
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30305" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30305
+INSERT INTO [t30305] VALUES (1,'عفان بن مسلم','1','0','0');
