a778f1db952d6f962241161f77acce01d0eef44f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:22 +0700

    Upload SQL chunk: t10357_chunk_0000.sql

---
+++ sql_chunks/t10357_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t10357
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t10357" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10357
+INSERT INTO [t10357] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10357] VALUES (2,'امتصاصه من الجسم وتوزيعه','3','0','0');
+INSERT INTO [t10357] VALUES (3,'فما تأثيره على العين؟','4','0','0');
+INSERT INTO [t10357] VALUES (4,'فماذا يفعل الكحول على هذه الأعصاب السابقة؟','6','0','0');
+INSERT INTO [t10357] VALUES (5,'تأثير الكحول الإيثيلي على العصب البصري:','8','0','0');
+INSERT INTO [t10357] VALUES (6,'تأثير الكحول المثيلي على العصب البصري:','10','0','0');
+INSERT INTO [t10357] VALUES (7,'الخمر وعمى الألوان:','14','0','0');
+INSERT INTO [t10357] VALUES (8,'تأثير الكحول على الأعصاب اللاإرادية','16','0','0');
