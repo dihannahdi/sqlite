42c64e5ba62fadb3fc4f8e3b840fa7af4b3f0d62
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:36 +0700

    Upload SQL chunk: t3982_chunk_0000.sql

---
+++ sql_chunks/t3982_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t3982
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t3982" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3982
+INSERT INTO [t3982] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t3982] VALUES (2,'تعريف موجز بمذهب السلف في التوحيد','6','0','0');
+INSERT INTO [t3982] VALUES (3,'تمهيد','6','2','0');
+INSERT INTO [t3982] VALUES (4,'قول الفلاسفة في التوحيد','27','0','0');
+INSERT INTO [t3982] VALUES (5,'تمهيد','27','4','0');
+INSERT INTO [t3982] VALUES (6,'المبحث الأول: قول ملاحدة الفلاسفة','30','4','0');
+INSERT INTO [t3982] VALUES (7,'المبحث الثاني: قول المؤلهة','37','4','0');
