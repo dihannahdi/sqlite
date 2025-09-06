120d103a56589580e82218617ef278da711f0903
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:43 +0700

    Upload SQL chunk: t11376_chunk_0000.sql

---
+++ sql_chunks/t11376_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t11376
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t11376" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11376
+INSERT INTO [t11376] VALUES (1,'الكلمة وأقسامها','1','0','0');
+INSERT INTO [t11376] VALUES (2,'باب المبتدأ والخبر مرفوعان','7','0','0');
+INSERT INTO [t11376] VALUES (3,'باب النواسخ لحكم المبتدإ والخبر ثلاثة أنواع','9','0','0');
+INSERT INTO [t11376] VALUES (4,'باب الفاعل مرفوع','13','0','0');
+INSERT INTO [t11376] VALUES (5,'باب النائب عن الفاعل','15','0','0');
+INSERT INTO [t11376] VALUES (6,'باب الاشتغال','16','0','0');
+INSERT INTO [t11376] VALUES (7,'باب في التنازع','18','0','0');
+INSERT INTO [t11376] VALUES (8,'باب المفعول منصوب وهو خمسة','19','0','0');
+INSERT INTO [t11376] VALUES (9,'باب الحال','22','0','0');
+INSERT INTO [t11376] VALUES (10,'باب يخفض الاسم إما بحرف مشترك','24','0','0');
+INSERT INTO [t11376] VALUES (11,'باب يعمل عمل فعله سبعة','25','0','0');
+INSERT INTO [t11376] VALUES (12,'باب التوابع','27','0','0');
+INSERT INTO [t11376] VALUES (13,'باب العدد من ثلاثة إلى تسعة يؤنث مع المذكر ويذكر مع المؤنث دائما','30','0','0');
+INSERT INTO [t11376] VALUES (14,'باب موانع صرف الاسم تسعة','31','0','0');
+INSERT INTO [t11376] VALUES (15,'باب التعجب له صيغتان','32','0','0');
+INSERT INTO [t11376] VALUES (16,'باب الوقف في الأفصح','33','0','0');
