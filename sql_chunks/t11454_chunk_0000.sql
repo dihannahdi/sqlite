75f6af5d0f3a7a7e2f8867474de75875832bb2fc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:34 +0700

    Upload SQL chunk: t11454_chunk_0000.sql

---
+++ sql_chunks/t11454_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11454
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11454" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11454
+INSERT INTO [t11454] VALUES (1,'الجزء فيه حديثان أحدهما في فضل رجب','1','0','0');
