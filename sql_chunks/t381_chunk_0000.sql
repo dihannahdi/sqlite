5f2a8291ff1c1e90adc51cf7883e95f1a69c7eb6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:17:52 +0700

    Upload SQL chunk: t381_chunk_0000.sql

---
+++ sql_chunks/t381_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t381
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t381" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t381
+INSERT INTO [t381] VALUES (1,'دلالة السياق','3','0','0');
+INSERT INTO [t381] VALUES (2,'المقابلة بين الصلاة الإبراهيمية وقوله تعالى: {رحمت الله وبركاته عليكم أهل البيت}','6','0','0');
+INSERT INTO [t381] VALUES (3,'وجه تذكير الخطاب في قوله: «عنكم» و «يطهركم»','8','0','0');
+INSERT INTO [t381] VALUES (4,'«أهل البيت» له استعمالان','9','0','0');
