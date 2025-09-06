2f41f32b2543dc8f720d0ba0fbd4b1c18235cbe6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:38:37 +0700

    Upload SQL chunk: t21489_chunk_0000.sql

---
+++ sql_chunks/t21489_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t21489
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t21489" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21489
+INSERT INTO [t21489] VALUES (1,'الحديث الأول','3','0','0');
+INSERT INTO [t21489] VALUES (2,'الحديث الثاني','9','0','0');
+INSERT INTO [t21489] VALUES (3,'الحديث الثالث','11','0','0');
+INSERT INTO [t21489] VALUES (4,'الحديث الرابع','13','0','0');
+INSERT INTO [t21489] VALUES (5,'الحديث الخامس','15','0','0');
+INSERT INTO [t21489] VALUES (6,'الحديث السادس','16','0','0');
+INSERT INTO [t21489] VALUES (7,'الحديث السابع','18','0','0');
+INSERT INTO [t21489] VALUES (8,'الحديث الثامن','20','0','0');
+INSERT INTO [t21489] VALUES (9,'الحديث التاسع','24','0','0');
+INSERT INTO [t21489] VALUES (10,'الحديث العاشر','27','0','0');
