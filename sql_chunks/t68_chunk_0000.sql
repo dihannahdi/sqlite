ce39b5f4d98126783a2f6ba8f5cbf6e7c8c413d1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:31 +0700

    Upload SQL chunk: t68_chunk_0000.sql

---
+++ sql_chunks/t68_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t68
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t68" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t68
+INSERT INTO [t68] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t68] VALUES (2,'سبب نفي المتكلمين للصفات','2','0','0');
+INSERT INTO [t68] VALUES (3,'أولا: أدلتهم وحججهم العقلية','2','2','0');
+INSERT INTO [t68] VALUES (4,'ثانيا: موقفهم من أدلة السمع المثبتة للصفات','3','2','0');
+INSERT INTO [t68] VALUES (5,'مناقشة الأدلة العقلية','4','0','0');
+INSERT INTO [t68] VALUES (6,'الدليل الأول: دليل الأعراض وحدوث الأجسام','4','5','0');
+INSERT INTO [t68] VALUES (7,'الدليل الثاني: دليل التركيب والتجسيم','10','5','0');
+INSERT INTO [t68] VALUES (8,'الدليل الثالث: دليل الاختصاص','19','5','0');
