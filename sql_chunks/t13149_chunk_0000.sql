c6dc10e5e7baac039993b31dfe7abf679e6e00c5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:11:17 +0700

    Upload SQL chunk: t13149_chunk_0000.sql

---
+++ sql_chunks/t13149_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t13149
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t13149" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13149
+INSERT INTO [t13149] VALUES (1,'باب: في قضاء الحوائج','24','0','0');
+INSERT INTO [t13149] VALUES (2,'باب طلب الحوائج إلى حسان الوجوه','52','0','0');
+INSERT INTO [t13149] VALUES (3,'باب في شكر الصنيعة','72','0','0');
