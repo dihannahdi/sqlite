0cf9a2a0c9a0aedfd0516c45bc6b2a6181f0425b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:03:34 +0700

    Upload SQL chunk: t9869_chunk_0000.sql

---
+++ sql_chunks/t9869_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t9869
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t9869" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9869
+INSERT INTO [t9869] VALUES (1,'حرف الألف','2','0','0');
+INSERT INTO [t9869] VALUES (2,'حرف الباء','72','0','0');
+INSERT INTO [t9869] VALUES (3,'حرف التاء','75','0','0');
+INSERT INTO [t9869] VALUES (4,'حرف الثاء','76','0','0');
+INSERT INTO [t9869] VALUES (5,'حرف الجيم','77','0','0');
+INSERT INTO [t9869] VALUES (6,'حرف الحاء','79','0','0');
+INSERT INTO [t9869] VALUES (7,'حرف الخاء','85','0','0');
+INSERT INTO [t9869] VALUES (8,'حرف الدال','89','0','0');
+INSERT INTO [t9869] VALUES (9,'حرف الذال','92','0','0');
+INSERT INTO [t9869] VALUES (10,'حرف الراء','93','0','0');
+INSERT INTO [t9869] VALUES (11,'حرف الزاي','95','0','0');
+INSERT INTO [t9869] VALUES (12,'حرف السين','96','0','0');
+INSERT INTO [t9869] VALUES (13,'حرف الشين','103','0','0');
+INSERT INTO [t9869] VALUES (14,'حرف الصاد','105','0','0');
+INSERT INTO [t9869] VALUES (15,'حرف الضاد','110','0','0');
+INSERT INTO [t9869] VALUES (16,'حرف الطاء','111','0','0');
+INSERT INTO [t9869] VALUES (17,'حرف الظاء','111','0','0');
+INSERT INTO [t9869] VALUES (18,'حرف العين','112','0','0');
+INSERT INTO [t9869] VALUES (19,'حرف الغين','187','0','0');
+INSERT INTO [t9869] VALUES (20,'حرف الفاء','188','0','0');
+INSERT INTO [t9869] VALUES (21,'حرف القاف','189','0','0');
+INSERT INTO [t9869] VALUES (22,'حرف الكاف','193','0','0');
+INSERT INTO [t9869] VALUES (23,'حرف اللام','199','0','0');
+INSERT INTO [t9869] VALUES (24,'حرف الميم','200','0','0');
+INSERT INTO [t9869] VALUES (25,'حرف النون','282','0','0');
+INSERT INTO [t9869] VALUES (26,'حرف الهاء','286','0','0');
+INSERT INTO [t9869] VALUES (27,'حرف الواو','289','0','0');
+INSERT INTO [t9869] VALUES (28,'حرف الياء','290','0','0');
+INSERT INTO [t9869] VALUES (29,'الكنى','298','0','0');
