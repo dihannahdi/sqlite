20acded5a81feb1755cecad40e5c1ae17805fe3c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:27 +0700

    Upload SQL chunk: t10046_chunk_0000.sql

---
+++ sql_chunks/t10046_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10046
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10046" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10046
+INSERT INTO [t10046] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10046] VALUES (2,'تحذير أهل الإيمان عن الحكم بغير ما أنزل الرحمن','3','0','0');
