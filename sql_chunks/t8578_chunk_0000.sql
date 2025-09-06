308c5a277e5c788ceff8a7c870d3202f5430b884
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:31 +0700

    Upload SQL chunk: t8578_chunk_0000.sql

---
+++ sql_chunks/t8578_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t8578
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t8578" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8578
+INSERT INTO [t8578] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8578] VALUES (2,'الباب الأول في الإيمان','3','0','0');
+INSERT INTO [t8578] VALUES (3,'الفصل الأول في تعريف الإيمان','3','2','0');
+INSERT INTO [t8578] VALUES (4,'الفصل الثاني في بيان المؤمن الصادق من المؤمن الكاذب','5','2','0');
+INSERT INTO [t8578] VALUES (5,'الباب الثاني وفيه فصلان','6','0','0');
+INSERT INTO [t8578] VALUES (6,'الفصل الأول في بيان محاب الله تعالى ورسوله صلى الله عليه وسلم','6','5','0');
+INSERT INTO [t8578] VALUES (7,'الفصل الثاني في بيان مكاره الله تعالى','25','5','0');
+INSERT INTO [t8578] VALUES (8,'الخاتمة','46','0','0');
