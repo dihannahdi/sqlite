461dcdca4bb63c3a391c2fdbb28166a802c5dca8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:10 +0700

    Upload SQL chunk: t11093_chunk_0000.sql

---
+++ sql_chunks/t11093_chunk_0000.sql
@@ -0,0 +1,47 @@
+-- Table: t11093
+-- Chunk: 1
+-- Rows: 40
+
+CREATE TABLE "t11093" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11093
+INSERT INTO [t11093] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11093] VALUES (2,'مصدر التلقي','39','0','0');
+INSERT INTO [t11093] VALUES (3,'صفة العلو','63','0','0');
+INSERT INTO [t11093] VALUES (4,'صفة الوجه','94','0','0');
+INSERT INTO [t11093] VALUES (5,'صفة النزول','106','0','0');
+INSERT INTO [t11093] VALUES (6,'صفة اليدين','124','0','0');
+INSERT INTO [t11093] VALUES (7,'صفة المحبة','143','0','0');
+INSERT INTO [t11093] VALUES (8,'صفة المشيئة والإرادة','145','0','0');
+INSERT INTO [t11093] VALUES (9,'صفة الضحك','148','0','0');
+INSERT INTO [t11093] VALUES (10,'صفة الفرح','150','0','0');
+INSERT INTO [t11093] VALUES (11,'صفة العجب','152','0','0');
+INSERT INTO [t11093] VALUES (12,'صفة البغض','153','0','0');
+INSERT INTO [t11093] VALUES (13,'صفة السخط','156','0','0');
+INSERT INTO [t11093] VALUES (14,'صفة الكره','157','0','0');
+INSERT INTO [t11093] VALUES (15,'صفة الرضا','158','0','0');
+INSERT INTO [t11093] VALUES (16,'النفس','162','0','0');
+INSERT INTO [t11093] VALUES (17,'رؤية المؤمنين ربهم يوم القيامة','170','0','0');
+INSERT INTO [t11093] VALUES (18,'صفة الكلام','186','0','0');
+INSERT INTO [t11093] VALUES (19,'القرآن كلام الله','198','0','0');
+INSERT INTO [t11093] VALUES (20,'الإيمان بالقضاء والقدر','234','0','0');
+INSERT INTO [t11093] VALUES (21,'الإسراء والمعراج','256','0','0');
+INSERT INTO [t11093] VALUES (22,'رؤية النبي صلى الله عليه وسلم ربه','261','0','0');
+INSERT INTO [t11093] VALUES (23,'إثبات الشفاعة','268','0','0');
+INSERT INTO [t11093] VALUES (24,'الإيمان بالحوض','277','0','0');
+INSERT INTO [t11093] VALUES (25,'الإيمان بعذاب القبر','282','0','0');
+INSERT INTO [t11093] VALUES (26,'فتنة القبر','285','0','0');
+INSERT INTO [t11093] VALUES (27,'الإيمان بالجنة والنار','288','0','0');
+INSERT INTO [t11093] VALUES (28,'الإيمان بالميزان','291','0','0');
+INSERT INTO [t11093] VALUES (29,'مسائل الإيمان','292','0','0');
+INSERT INTO [t11093] VALUES (30,'زيادة الإيمان ونقصانه','299','0','0');
+INSERT INTO [t11093] VALUES (31,'الاستثناء في الإيمان','307','0','0');
+INSERT INTO [t11093] VALUES (32,'الفرق بين الإسلام والإيمان','313','0','0');
+INSERT INTO [t11093] VALUES (33,'الإيمان بأشراط الساعة','321','0','0');
+INSERT INTO [t11093] VALUES (34,'خروج المسيح الدجال','323','0','0');
+INSERT INTO [t11093] VALUES (35,'نزول عيسى عليه السلام','327','0','0');
+INSERT INTO [t11093] VALUES (36,'الإيمان بملك الموت وأن موسى عليه السلام فقأ عينه','333','0','0');
+INSERT INTO [t11093] VALUES (37,'ذبح الموت يوم القيامة','336','0','0');
+INSERT INTO [t11093] VALUES (38,'خصائص المصطفى صلى الله عليه وسلم','340','0','0');
+INSERT INTO [t11093] VALUES (39,'فضائل الصحابة','347','0','0');
+INSERT INTO [t11093] VALUES (40,'فصل في فضل الاتباع','360','0','0');
