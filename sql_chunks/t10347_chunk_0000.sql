f5356aed3dcd54d9e4bfb35dc06abef824f24830
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:15 +0700

    Upload SQL chunk: t10347_chunk_0000.sql

---
+++ sql_chunks/t10347_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t10347
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t10347" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10347
+INSERT INTO [t10347] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10347] VALUES (2,'حكمة تحديد عقوبة السرقة','2','0','0');
+INSERT INTO [t10347] VALUES (3,'حكمة الشدة في عقوبة السرقة','4','0','0');
+INSERT INTO [t10347] VALUES (4,'معنى الجزاء','6','0','0');
+INSERT INTO [t10347] VALUES (5,'معنى النكال','8','0','0');
+INSERT INTO [t10347] VALUES (6,'حكمة العقاب بقطع اليد','10','0','0');
+INSERT INTO [t10347] VALUES (7,'لا سلطة لغير الشارع في تكييف عقوبة السرقة','12','0','0');
+INSERT INTO [t10347] VALUES (8,'رأي المجوزين','13','0','0');
+INSERT INTO [t10347] VALUES (9,'جواز إسقاط الأحكام','17','0','0');
+INSERT INTO [t10347] VALUES (10,'استدلالهم بالنسخ','18','0','0');
+INSERT INTO [t10347] VALUES (11,'استدلالهم بقول ابن القيم','21','0','0');
+INSERT INTO [t10347] VALUES (12,'استدلالهم بعمر','24','0','0');
