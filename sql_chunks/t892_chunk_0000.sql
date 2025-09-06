f594dfe95706fbd9ff21f37ba598fa28d38c5b5d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:43 +0700

    Upload SQL chunk: t892_chunk_0000.sql

---
+++ sql_chunks/t892_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t892
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t892" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t892
+INSERT INTO [t892] VALUES (1,'الإمام تقي الدين أبي العباس أحمد بن عبد الحليم ابن تميمة','1','0','0');
+INSERT INTO [t892] VALUES (2,'عملنا في هذا الكتاب','4','0','0');
+INSERT INTO [t892] VALUES (3,'ما تقول السادة العلماء أئمة الدين','5','0','0');
+INSERT INTO [t892] VALUES (4,'الجواب','6','0','0');
