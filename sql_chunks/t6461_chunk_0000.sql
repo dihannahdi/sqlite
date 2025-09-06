fa2d6f6d003b840dc2c98ed04c338acc295ac1f8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:50 +0700

    Upload SQL chunk: t6461_chunk_0000.sql

---
+++ sql_chunks/t6461_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t6461
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t6461" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6461
+INSERT INTO [t6461] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6461] VALUES (2,'وهو حسبي','1','0','0');
+INSERT INTO [t6461] VALUES (3,'خواص النفس النباتية وتسمى الشهوانية','15','0','0');
+INSERT INTO [t6461] VALUES (4,'خواص النفس الحيوانية وتسمى الغضبية','17','0','0');
+INSERT INTO [t6461] VALUES (5,'خواص النفس الإنسانية وهي الناطقة','17','0','0');
+INSERT INTO [t6461] VALUES (6,'خواص النفس الحكمية الفلسفية','18','0','0');
+INSERT INTO [t6461] VALUES (7,'خواص النفس النبوية','22','0','0');
+INSERT INTO [t6461] VALUES (8,'خواص النفس الكلية','24','0','0');
+INSERT INTO [t6461] VALUES (9,'أحدها','57','0','0');
+INSERT INTO [t6461] VALUES (10,'والمعنى الثاني','59','0','0');
+INSERT INTO [t6461] VALUES (11,'والمعنى الثالث','60','0','0');
+INSERT INTO [t6461] VALUES (12,'فصل','76','0','0');
+INSERT INTO [t6461] VALUES (13,'والمعنى الثاني','78','0','0');
+INSERT INTO [t6461] VALUES (14,'والمعنى الثالث','79','0','0');
+INSERT INTO [t6461] VALUES (15,'والمعنى الرابع','79','0','0');
+INSERT INTO [t6461] VALUES (16,'الحواس الخمس','80','0','0');
+INSERT INTO [t6461] VALUES (17,'فصل','81','0','0');
+INSERT INTO [t6461] VALUES (18,'فصل','86','0','0');
+INSERT INTO [t6461] VALUES (19,'برهان أول','92','0','0');
+INSERT INTO [t6461] VALUES (20,'برهان ثان','93','0','0');
+INSERT INTO [t6461] VALUES (21,'برهان ثالث','94','0','0');
+INSERT INTO [t6461] VALUES (22,'برهان رابع','95','0','0');
+INSERT INTO [t6461] VALUES (23,'برهان خامس','96','0','0');
+INSERT INTO [t6461] VALUES (24,'برهان سادس','98','0','0');
+INSERT INTO [t6461] VALUES (25,'برهان سابع','99','0','0');
+INSERT INTO [t6461] VALUES (26,'برهان ثامن','100','0','0');
