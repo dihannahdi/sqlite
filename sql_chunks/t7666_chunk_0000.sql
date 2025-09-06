a58f5978dd0470c8216c7337a4a9e2cdd77a775e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:41:19 +0700

    Upload SQL chunk: t7666_chunk_0000.sql

---
+++ sql_chunks/t7666_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t7666
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t7666" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7666
+INSERT INTO [t7666] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7666] VALUES (2,'الطبقة الثالثة','71','0','0');
+INSERT INTO [t7666] VALUES (3,'الطبقة الرابعة: من التابعين من أهل المدينة','139','0','0');
+INSERT INTO [t7666] VALUES (4,'الطبقة الخامسة: من التابعين من أهل المدينة.','314','0','0');
+INSERT INTO [t7666] VALUES (5,'الطبقة السادسة: من التابعين من أهل المدينة.','412','0','0');
+INSERT INTO [t7666] VALUES (6,'مصادر ومراجع','452','0','0');
