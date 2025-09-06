8faaaa562328c4c3bef19033ca6adfe347454204
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:38 +0700

    Upload SQL chunk: t9074_chunk_0000.sql

---
+++ sql_chunks/t9074_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t9074
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t9074" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9074
+INSERT INTO [t9074] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t9074] VALUES (2,'كتاب الأمر بالمعروف لعبد الغني المقدسي','2','0','0');
