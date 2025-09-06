6f6a5e1094c94d04bef1aa3f150499f41722455d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:00 +0700

    Upload SQL chunk: t23120_chunk_0000.sql

---
+++ sql_chunks/t23120_chunk_0000.sql
@@ -0,0 +1,42 @@
+-- Table: t23120
+-- Chunk: 1
+-- Rows: 35
+
+CREATE TABLE "t23120" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23120
+INSERT INTO [t23120] VALUES (1,'مقدمات','1','0','0');
+INSERT INTO [t23120] VALUES (2,'تقديم','1','1','0');
+INSERT INTO [t23120] VALUES (3,'مقدمة المؤلف','2','1','0');
+INSERT INTO [t23120] VALUES (4,'نشأة التلمود وأثره على اليهود','4','0','0');
+INSERT INTO [t23120] VALUES (5,'مدخل','4','4','0');
+INSERT INTO [t23120] VALUES (6,'نشأة المشناه','7','4','0');
+INSERT INTO [t23120] VALUES (7,'مباحث المشناه','9','4','0');
+INSERT INTO [t23120] VALUES (8,'مهندسو المشناه','13','4','0');
+INSERT INTO [t23120] VALUES (9,'سنهدرين senhedrin','17','4','0');
+INSERT INTO [t23120] VALUES (10,'تلمود أورشليم "فلسطين" talmud yerushaimi','19','4','0');
+INSERT INTO [t23120] VALUES (11,'تلمود أورشليم اليوم','22','4','0');
+INSERT INTO [t23120] VALUES (12,'تلمود بابل','24','4','0');
+INSERT INTO [t23120] VALUES (13,'طبعات تلمود بابل','26','4','0');
+INSERT INTO [t23120] VALUES (14,'نشأة التلمود والقانون الشفهي وأهميتها','28','4','0');
+INSERT INTO [t23120] VALUES (15,'الفريسيون phareesis','31','4','0');
+INSERT INTO [t23120] VALUES (16,'شماي وهليل','36','4','0');
+INSERT INTO [t23120] VALUES (17,'الطوائف اليهودية الأخرى','38','4','0');
+INSERT INTO [t23120] VALUES (18,'تلمودا بابل وفلسطين - مقارنة','40','4','0');
+INSERT INTO [t23120] VALUES (19,'حرق التلمود وإعدامه','41','0','0');
+INSERT INTO [t23120] VALUES (20,'معتقدات التلمود','51','0','0');
+INSERT INTO [t23120] VALUES (21,'الكرائية وأثر الإسلام','51','20','0');
+INSERT INTO [t23120] VALUES (22,'تشبث اليهود بالتلمود','56','20','0');
+INSERT INTO [t23120] VALUES (23,'المرأة في التلمود','60','20','0');
+INSERT INTO [t23120] VALUES (24,'التلمود وقضية المسيح','61','20','0');
+INSERT INTO [t23120] VALUES (25,'قواعد المنطق التلمودي','66','20','0');
+INSERT INTO [t23120] VALUES (26,'رواية التلمود عن تدمير الهيكل','69','20','0');
+INSERT INTO [t23120] VALUES (27,'الدين التلمودي والهندوسية','75','20','0');
+INSERT INTO [t23120] VALUES (28,'خرافات التلمود','78','0','0');
+INSERT INTO [t23120] VALUES (29,'مدخل','78','28','0');
+INSERT INTO [t23120] VALUES (30,'السحر','80','28','0');
+INSERT INTO [t23120] VALUES (31,'العرافة','87','28','0');
+INSERT INTO [t23120] VALUES (32,'الحاخام يخاف الموت','92','28','0');
+INSERT INTO [t23120] VALUES (33,'حكم التلمود','93','28','0');
+INSERT INTO [t23120] VALUES (34,'خلاصة التلمود','97','28','0');
+INSERT INTO [t23120] VALUES (35,'الفهارس','103','0','0');
