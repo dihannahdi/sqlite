c78deed32ebe77d6f9f958d75c14a7b834d8a93e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:05 +0700

    Upload SQL chunk: t8791_chunk_0000.sql

---
+++ sql_chunks/t8791_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t8791
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t8791" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8791
+INSERT INTO [t8791] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8791] VALUES (2,'تمهيد','8','0','0');
+INSERT INTO [t8791] VALUES (3,'المبحث الأول: تعريف البدعة','9','0','0');
+INSERT INTO [t8791] VALUES (4,'المبحث الثاني: أقسام البدعة','10','0','0');
