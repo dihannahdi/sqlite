6d9788da08a83e42afad53fcf727dda77abb321e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:02 +0700

    Upload SQL chunk: t22885_chunk_0000.sql

---
+++ sql_chunks/t22885_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t22885
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t22885" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22885
+INSERT INTO [t22885] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t22885] VALUES (2,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t22885] VALUES (3,'التفسير','3','0','0');
+INSERT INTO [t22885] VALUES (4,'الطويل','51','3','0');
+INSERT INTO [t22885] VALUES (5,'المديد','51','3','0');
+INSERT INTO [t22885] VALUES (6,'فصل','53','3','0');
+INSERT INTO [t22885] VALUES (7,'في أبيات أنواع الحدود','53','0','0');
+INSERT INTO [t22885] VALUES (8,'فصل','55','7','0');
+INSERT INTO [t22885] VALUES (9,'البسيط','60','7','0');
+INSERT INTO [t22885] VALUES (10,'الوافر','61','7','0');
+INSERT INTO [t22885] VALUES (11,'الكامل','62','7','0');
+INSERT INTO [t22885] VALUES (12,'الهزج','63','7','0');
+INSERT INTO [t22885] VALUES (13,'الرجز','64','7','0');
+INSERT INTO [t22885] VALUES (14,'الرمل','64','7','0');
+INSERT INTO [t22885] VALUES (15,'السريع','65','7','0');
+INSERT INTO [t22885] VALUES (16,'المنسرح','66','7','0');
+INSERT INTO [t22885] VALUES (17,'الخفيف','67','7','0');
+INSERT INTO [t22885] VALUES (18,'المضارع','67','7','0');
+INSERT INTO [t22885] VALUES (19,'المقتضب','68','7','0');
+INSERT INTO [t22885] VALUES (20,'المجتث','68','7','0');
+INSERT INTO [t22885] VALUES (21,'المتقارب','68','7','0');
+INSERT INTO [t22885] VALUES (22,'المتقاطر','69','7','0');
+INSERT INTO [t22885] VALUES (23,'فصل','71','7','0');
+INSERT INTO [t22885] VALUES (24,'في مثل ذلك من التصريف','71','0','0');
+INSERT INTO [t22885] VALUES (25,'فصل','91','24','0');
+INSERT INTO [t22885] VALUES (26,'في أحكام حروف الوصل إذا كانت رويا','91','0','0');
+INSERT INTO [t22885] VALUES (27,'فصل','95','26','0');
+INSERT INTO [t22885] VALUES (28,'في اختلاف الحروف والحركات','95','0','0');
+INSERT INTO [t22885] VALUES (29,'وما يعاب من ذلك وما لا يعاب','95','28','0');
+INSERT INTO [t22885] VALUES (30,'ذكر التوجيه','95','28','0');
+INSERT INTO [t22885] VALUES (31,'ذكر الحذو والردف','96','28','0');
+INSERT INTO [t22885] VALUES (32,'ذكر الرسن والتأسيس','98','28','0');
+INSERT INTO [t22885] VALUES (33,'ذكر الدخيل والإشباع','100','28','0');
+INSERT INTO [t22885] VALUES (34,'ذكر الروي والمجري','101','28','0');
+INSERT INTO [t22885] VALUES (35,'ذكر الوصل والنفاذ والخروج','102','28','0');
+INSERT INTO [t22885] VALUES (36,'المذاهب','135','0','0');
+INSERT INTO [t22885] VALUES (37,'اختلاف الناس في الحجة بالخبر بعد النبي','271','0','0');
+INSERT INTO [t22885] VALUES (38,'صلى الله عليه وسلم','271','37','0');
