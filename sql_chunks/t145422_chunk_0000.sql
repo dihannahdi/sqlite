2decc2e1c328480c258dd36e716cfc51bdf39b6a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:19:59 +0700

    Upload SQL chunk: t145422_chunk_0000.sql

---
+++ sql_chunks/t145422_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t145422
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t145422" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t145422
+INSERT INTO [t145422] VALUES (1,'التسويف في نشر الرسالة الجامعية','1','0','0');
+INSERT INTO [t145422] VALUES (2,'إن قلت: بأنك لن تجد دار نشر تقبل رسالتك، أو أنها جزء من مشروع لا يناسب في الغالب طباعتها دون البقية؟','3','0','0');
+INSERT INTO [t145422] VALUES (3,'سأذكر ما أعلم حسب التسلسل الزمني','3','0','0');
+INSERT INTO [t145422] VALUES (4,'الوسواس العلمي','5','0','0');
+INSERT INTO [t145422] VALUES (5,'إلى أهل العلم الأفاضل','8','0','0');
+INSERT INTO [t145422] VALUES (6,'إضافات القراء','9','0','0');
