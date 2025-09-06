581427f9480bcb1eff75eabe28a7069ce9b629cb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:54 +0700

    Upload SQL chunk: t7489_chunk_0000.sql

---
+++ sql_chunks/t7489_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7489
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7489" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7489
+INSERT INTO [t7489] VALUES (1,'أحكام تمني الموت','1','0','0');
+INSERT INTO [t7489] VALUES (2,'انتهى، والحمد لله رب العالمين','76','0','0');
