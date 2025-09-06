4047654b2a2bdb0e0b6f7a2f8823df1998e2e5e2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:02 +0700

    Upload SQL chunk: t5748_chunk_0000.sql

---
+++ sql_chunks/t5748_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t5748
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t5748" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5748
+INSERT INTO [t5748] VALUES (1,'(ذكر تراجم الأبواب)','3','0','0');
+INSERT INTO [t5748] VALUES (2,'(الباب الأول)','6','0','0');
+INSERT INTO [t5748] VALUES (3,'(الباب الثاني)','8','0','0');
+INSERT INTO [t5748] VALUES (4,'(الباب الثالث)','9','0','0');
+INSERT INTO [t5748] VALUES (5,'(الباب الرابع)','11','0','0');
+INSERT INTO [t5748] VALUES (6,'(الباب الخامس)','13','0','0');
+INSERT INTO [t5748] VALUES (7,'(الباب السادس)','14','0','0');
+INSERT INTO [t5748] VALUES (8,'(الباب السابع)','27','0','0');
+INSERT INTO [t5748] VALUES (9,'(الباب الثامن)','30','0','0');
+INSERT INTO [t5748] VALUES (10,'(الباب التاسع)','35','0','0');
+INSERT INTO [t5748] VALUES (11,'(الباب العاشر)','45','0','0');
+INSERT INTO [t5748] VALUES (12,'(الباب الحادي عشر)','46','0','0');
+INSERT INTO [t5748] VALUES (13,'(الباب الثاني عشر)','50','0','0');
+INSERT INTO [t5748] VALUES (14,'(الباب الثالث عشر)','53','0','0');
+INSERT INTO [t5748] VALUES (15,'(الباب الرابع عشر)','55','0','0');
+INSERT INTO [t5748] VALUES (16,'(الباب الخامس عشر)','56','0','0');
+INSERT INTO [t5748] VALUES (17,'(الباب السادس عشر)','77','0','0');
+INSERT INTO [t5748] VALUES (18,'(الباب السابع عشر)','93','0','0');
+INSERT INTO [t5748] VALUES (19,'(الباب الثامن عشر)','121','0','0');
+INSERT INTO [t5748] VALUES (20,'(الباب التاسع عشر)','126','0','0');
+INSERT INTO [t5748] VALUES (21,'(الباب العشرون)','136','0','0');
+INSERT INTO [t5748] VALUES (22,'(الباب الحادي والعشرون)','153','0','0');
+INSERT INTO [t5748] VALUES (23,'(الباب الثاني والعشرون)','164','0','0');
+INSERT INTO [t5748] VALUES (24,'(الباب الثالث والعشرون)','195','0','0');
+INSERT INTO [t5748] VALUES (25,'(الباب الرابع والعشرون)','206','0','0');
+INSERT INTO [t5748] VALUES (26,'(الباب الخامس والعشرون)','219','0','0');
+INSERT INTO [t5748] VALUES (27,'(الباب السادس والعشرون)','222','0','0');
+INSERT INTO [t5748] VALUES (28,'(الباب السابع والعشرون)','225','0','0');
+INSERT INTO [t5748] VALUES (29,'(الباب الثامن والعشرون)','227','0','0');
