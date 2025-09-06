e5c5dcafbbbeaec06f24b98061b59372597eecc4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:10 +0700

    Upload SQL chunk: t5995_chunk_0000.sql

---
+++ sql_chunks/t5995_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t5995
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t5995" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5995
+INSERT INTO [t5995] VALUES (1,'فصل','7','0','0');
+INSERT INTO [t5995] VALUES (2,'فصل','12','0','0');
+INSERT INTO [t5995] VALUES (3,'فصل','17','0','0');
+INSERT INTO [t5995] VALUES (4,'فصل','22','0','0');
+INSERT INTO [t5995] VALUES (5,'فصل','24','0','0');
+INSERT INTO [t5995] VALUES (6,'فصل','28','0','0');
+INSERT INTO [t5995] VALUES (7,'فصل','30','0','0');
+INSERT INTO [t5995] VALUES (8,'فصل','31','0','0');
+INSERT INTO [t5995] VALUES (9,'فصل','35','0','0');
+INSERT INTO [t5995] VALUES (10,'فصل المسلسل بخالد','37','0','0');
+INSERT INTO [t5995] VALUES (11,'فصل المسلسل بالزيدين','38','0','0');
+INSERT INTO [t5995] VALUES (12,'فصل المسلسل بسليمان','41','0','0');
+INSERT INTO [t5995] VALUES (13,'فصل المسلسل بعبد الله','42','0','0');
+INSERT INTO [t5995] VALUES (14,'فصل المسلسل بعبد الرحمن','54','0','0');
+INSERT INTO [t5995] VALUES (15,'فصل المسلسل بعبد الواحد رواية ثلاثة يسمى كل واحد منهم عبد الواحد بعضهم عن بعض','55','0','0');
+INSERT INTO [t5995] VALUES (16,'فصل المسلسل بعمر بن أحمد بن عمر ذكر رواية عمر بن أحمد بن عمر عن عمر بن أحمد بن عمر عن عمر بن أحمد بن عمر','56','0','0');
+INSERT INTO [t5995] VALUES (17,'فصل المسلسل بعلي أبي الحسن','57','0','0');
+INSERT INTO [t5995] VALUES (18,'فصل المسلسل بعمرو','59','0','0');
+INSERT INTO [t5995] VALUES (19,'المسلسل بهشام','62','0','0');
+INSERT INTO [t5995] VALUES (20,'فصل المسلسل بيحيى','63','0','0');
+INSERT INTO [t5995] VALUES (21,'كتاب النساء','66','0','0');
+INSERT INTO [t5995] VALUES (22,'كتاب الكنى','70','0','0');
+INSERT INTO [t5995] VALUES (23,'فصل المسلسل بابى بكر','72','0','0');
+INSERT INTO [t5995] VALUES (24,'فصل المسلسل بأبي جعفر','74','0','0');
