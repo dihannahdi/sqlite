0f27ee790e4cc1a7bd570e44ef7a12fbcb7d6d42
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:35 +0700

    Upload SQL chunk: t6992_chunk_0000.sql

---
+++ sql_chunks/t6992_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6992
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6992" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6992
+INSERT INTO [t6992] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6992] VALUES (2,'اسم الفاعل المراد به الاستمرار في جميع الأزمنة','1','0','0');
+INSERT INTO [t6992] VALUES (3,'وكتبه الفقير أحمد بن قاسم العبادي','10','0','0');
