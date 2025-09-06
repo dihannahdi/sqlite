90aa6d466851c41922382a8c024d6c208d609dad
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:39 +0700

    Upload SQL chunk: t6666_chunk_0000.sql

---
+++ sql_chunks/t6666_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t6666
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t6666" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6666
+INSERT INTO [t6666] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6666] VALUES (2,'فصل','11','0','0');
+INSERT INTO [t6666] VALUES (3,'فصل','14','0','0');
+INSERT INTO [t6666] VALUES (4,'الثالث هداية الخلق ودعوتهم إلى الله ورسوله','16','0','0');
+INSERT INTO [t6666] VALUES (5,'فصل','21','0','0');
+INSERT INTO [t6666] VALUES (6,'فصل','25','0','0');
+INSERT INTO [t6666] VALUES (7,'فصل','32','0','0');
+INSERT INTO [t6666] VALUES (8,'فصل','34','0','0');
+INSERT INTO [t6666] VALUES (9,'فصل','35','0','0');
+INSERT INTO [t6666] VALUES (10,'فصل','38','0','0');
+INSERT INTO [t6666] VALUES (11,'فصل','42','0','0');
