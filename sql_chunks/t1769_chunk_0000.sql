c267ab27ed9f62a7d8b188a12ee4fd15e2d3208f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:30:59 +0700

    Upload SQL chunk: t1769_chunk_0000.sql

---
+++ sql_chunks/t1769_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t1769
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t1769" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1769
+INSERT INTO [t1769] VALUES (1,'حرمة المسلم على المسلم','1','0','0');
+INSERT INTO [t1769] VALUES (2,'المقدمة','1','0','0');
+INSERT INTO [t1769] VALUES (3,'الغيبة','1','0','0');
+INSERT INTO [t1769] VALUES (4,'النميمة','9','0','0');
+INSERT INTO [t1769] VALUES (5,'الكذب','12','0','0');
+INSERT INTO [t1769] VALUES (6,'الحقد','14','0','0');
+INSERT INTO [t1769] VALUES (7,'الحسد','17','0','0');
+INSERT INTO [t1769] VALUES (8,'الغش','19','0','0');
+INSERT INTO [t1769] VALUES (9,'تنبيه لمحاسبة النفس','20','0','0');
+INSERT INTO [t1769] VALUES (10,'أكل حقوق الآخرين','21','0','0');
+INSERT INTO [t1769] VALUES (11,'جميع المعاصي محاربة لله','22','0','0');
+INSERT INTO [t1769] VALUES (12,'قطيعة الرحم','22','0','0');
+INSERT INTO [t1769] VALUES (13,'إظهار العيوب','24','0','0');
+INSERT INTO [t1769] VALUES (14,'الإشاعة','25','0','0');
+INSERT INTO [t1769] VALUES (15,'وجوب حفظ اللسان','28','0','0');
+INSERT INTO [t1769] VALUES (16,'التخلق بالأخلاق الإسلامية','31','0','0');
+INSERT INTO [t1769] VALUES (17,'الزنا','32','0','0');
+INSERT INTO [t1769] VALUES (18,'صفات المؤمنين والمؤمنات','34','0','0');
+INSERT INTO [t1769] VALUES (19,'التحذير من أذية الجار','34','0','0');
+INSERT INTO [t1769] VALUES (20,'دماء المسلمين','36','0','0');
+INSERT INTO [t1769] VALUES (21,'اللسان نعمة','57','0','0');
+INSERT INTO [t1769] VALUES (22,'الهمز واللمز','59','0','0');
+INSERT INTO [t1769] VALUES (23,'إفشاء السر','65','0','0');
+INSERT INTO [t1769] VALUES (24,'شهادة الزور','65','0','0');
+INSERT INTO [t1769] VALUES (25,'القذف','66','0','0');
+INSERT INTO [t1769] VALUES (26,'الجدال والمراء','67','0','0');
+INSERT INTO [t1769] VALUES (27,'الخصام','68','0','0');
+INSERT INTO [t1769] VALUES (28,'السخرية والاستهزاء','70','0','0');
+INSERT INTO [t1769] VALUES (29,'الغضب','70','0','0');
+INSERT INTO [t1769] VALUES (30,'الظلم ظلمات','71','0','0');
+INSERT INTO [t1769] VALUES (31,'سوء الظن','73','0','0');
+INSERT INTO [t1769] VALUES (32,'التجسس','73','0','0');
+INSERT INTO [t1769] VALUES (33,'إخلاف الوعد','76','0','0');
+INSERT INTO [t1769] VALUES (34,'حق الحياة','77','0','0');
+INSERT INTO [t1769] VALUES (35,'ذات البين','81','0','0');
+INSERT INTO [t1769] VALUES (36,'ختاما','84','0','0');
