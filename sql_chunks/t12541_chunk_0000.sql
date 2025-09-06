70e3cdd6f6a388603f301f6d9459ecc56819920b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:37 +0700

    Upload SQL chunk: t12541_chunk_0000.sql

---
+++ sql_chunks/t12541_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t12541
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t12541" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12541
+INSERT INTO [t12541] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t12541] VALUES (2,'وأنت - حفظك الله - إذا نظرت إلى الدنيا وجدتها قائمة على هذه الأركان','3','0','0');
+INSERT INTO [t12541] VALUES (3,'وهذه الجملة - أكرمك الله - أنت أحوجتني إليها','7','0','0');
