76fd8eeaebda01b26e1961aa153d49369c48ac00
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:38 +0700

    Upload SQL chunk: t8908_chunk_0000.sql

---
+++ sql_chunks/t8908_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t8908
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t8908" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8908
+INSERT INTO [t8908] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8908] VALUES (2,'تمهيد','5','0','0');
+INSERT INTO [t8908] VALUES (3,'المبحث الأول: المعنى اللغوي والاصطلاحي للمداراة وبعض مرادفاتها','16','0','0');
+INSERT INTO [t8908] VALUES (4,'المبحث الثاني: ما ورد في القرآن الكريم عن المداراة ومدلولاتها','21','0','0');
+INSERT INTO [t8908] VALUES (5,'المبحث الثالث: ما ورد في السنة، وآثار الصحابة والتابعين عن المداراة وكيف فهموها؟!','35','0','0');
+INSERT INTO [t8908] VALUES (6,'المبحث الرابع: المداراة وأثرها في المجتمع','59','0','0');
+INSERT INTO [t8908] VALUES (7,'الخاتمة','76','0','0');
+INSERT INTO [t8908] VALUES (8,'مصادر ومراجع','79','0','0');
