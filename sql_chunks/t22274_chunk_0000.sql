7bc204b69da9e1620d36f114cbf1730872a6bb0f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:10 +0700

    Upload SQL chunk: t22274_chunk_0000.sql

---
+++ sql_chunks/t22274_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t22274
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t22274" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22274
+INSERT INTO [t22274] VALUES (1,'ذهب فلان إلى أوروبا وما ننكر من أمره شيئا، فلبث فيها بضع سنين، ثم عاد وما بقي مما كنا نعرفه منه شيء.','2','0','0');
+INSERT INTO [t22274] VALUES (2,'قال: ليس لي في الحياة إلا أمل واحد هو أن أغمض عيني ثم أفتحهما فلا أرى برقعا على وجه امرأة في هذا البلد.','5','0','0');
+INSERT INTO [t22274] VALUES (3,'قلت: ذلك ما لا تملكه ولا رأي لك فيه','5','0','0');
