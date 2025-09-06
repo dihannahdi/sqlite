960cd1dea21ab38246734711fe60ae7b47db7d30
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:09 +0700

    Upload SQL chunk: t10469_chunk_0000.sql

---
+++ sql_chunks/t10469_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10469
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10469" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10469
+INSERT INTO [t10469] VALUES (1,'الأربعين المسلسلة المتباينة الأسانيد','3','0','0');
