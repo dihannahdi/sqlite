d8650cb31bf5e56eccef45ea095261ed3ad4ab7a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:40 +0700

    Upload SQL chunk: t27092_chunk_0000.sql

---
+++ sql_chunks/t27092_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t27092
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t27092" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t27092
+INSERT INTO [t27092] VALUES (1,'مقدمة المعتني','3','0','0');
+INSERT INTO [t27092] VALUES (2,'ترجمة صاحب الجزء','5','1','0');
+INSERT INTO [t27092] VALUES (3,'اسمه ونسبه','5','2','0');
+INSERT INTO [t27092] VALUES (4,'شيوخه','5','2','0');
+INSERT INTO [t27092] VALUES (5,'بعض من حدث عنه','6','2','0');
+INSERT INTO [t27092] VALUES (6,'ثناء العلماء عليه','6','2','0');
+INSERT INTO [t27092] VALUES (7,'وفاته','7','2','0');
+INSERT INTO [t27092] VALUES (8,'آثاره','7','2','0');
+INSERT INTO [t27092] VALUES (9,'وصف النسخة','10','1','0');
+INSERT INTO [t27092] VALUES (10,'موضوع الجزء','11','1','0');
+INSERT INTO [t27092] VALUES (11,'سند الجزء','14','1','0');
+INSERT INTO [t27092] VALUES (12,'مقدمة الجزء','22','0','0');
+INSERT INTO [t27092] VALUES (13,'سند كتاب شروط سيدنا عمر بن الخطاب إلى النصارى','23','12','0');
+INSERT INTO [t27092] VALUES (14,'نص الكتاب','24','0','0');
+INSERT INTO [t27092] VALUES (15,'سند حديث واصل الدمشقي','27','0','0');
+INSERT INTO [t27092] VALUES (16,'نص حديث واصل الدمشقي','28','0','0');
