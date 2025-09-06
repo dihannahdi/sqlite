5155acf9a3b2a3b0f6892af3c02ea5d826ba322d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:13 +0700

    Upload SQL chunk: t36561_chunk_0000.sql

---
+++ sql_chunks/t36561_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t36561
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t36561" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36561
+INSERT INTO [t36561] VALUES (1,'الرؤيا','1','0','0');
+INSERT INTO [t36561] VALUES (2,'الخطبة','2','0','0');
+INSERT INTO [t36561] VALUES (3,'الزواج','3','0','0');
+INSERT INTO [t36561] VALUES (4,'في بيت رسول الله','3','0','0');
+INSERT INTO [t36561] VALUES (5,'الزوجة الوفية','4','0','0');
+INSERT INTO [t36561] VALUES (6,'الزوجة الغيور','5','0','0');
+INSERT INTO [t36561] VALUES (7,'يا نساء النبي','6','0','0');
+INSERT INTO [t36561] VALUES (8,'حديث الإفك','7','0','0');
+INSERT INTO [t36561] VALUES (9,'البراءة من السماء','11','0','0');
+INSERT INTO [t36561] VALUES (10,'بعد رسول الله','13','0','0');
+INSERT INTO [t36561] VALUES (11,'في بيت عائشة','13','0','0');
+INSERT INTO [t36561] VALUES (12,'الوفاة','15','0','0');
