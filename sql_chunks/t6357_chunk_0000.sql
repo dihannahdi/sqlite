af18c25681df4a06b4c0e48e3d58bed19e09cd99
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:08 +0700

    Upload SQL chunk: t6357_chunk_0000.sql

---
+++ sql_chunks/t6357_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t6357
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t6357" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6357
+INSERT INTO [t6357] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t6357] VALUES (2,'فصل في المجتهد المطلق المنتسب','15','0','0');
+INSERT INTO [t6357] VALUES (3,'فصل في المجتهد في المذهب وفيه مسائل','16','0','0');
+INSERT INTO [t6357] VALUES (4,'فصل في المتبحر في المذهب وهو الحافظ لكتب مذهبه وفيه مسائل','19','0','0');
+INSERT INTO [t6357] VALUES (5,'فصل في العامي','28','0','0');
