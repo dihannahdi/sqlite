7f40e244cccb1af30c27a596ec3d88788ad6851f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:46 +0700

    Upload SQL chunk: t36392_chunk_0000.sql

---
+++ sql_chunks/t36392_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t36392
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t36392" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36392
+INSERT INTO [t36392] VALUES (1,'الحمد لله تعالى واسع العطاء ..','1','0','0');
+INSERT INTO [t36392] VALUES (2,'أخي المسلم','1','0','0');
+INSERT INTO [t36392] VALUES (3,'أخي المسلم','2','0','0');
+INSERT INTO [t36392] VALUES (4,'أخي المسلم','3','0','0');
+INSERT INTO [t36392] VALUES (5,'أخي المسلم','4','0','0');
+INSERT INTO [t36392] VALUES (6,'أخي المسلم','6','0','0');
+INSERT INTO [t36392] VALUES (7,'أخي المسلم','6','0','0');
+INSERT INTO [t36392] VALUES (8,'إن من راقب الله تعالى؛ فاز بثمار المراقبة، وأعلى هذه الثمار في الدنيا: بلوغه درجة الإحسان. وفي الآخرة: دخول الجنة','7','0','0');
+INSERT INTO [t36392] VALUES (9,'* ومراقبة الله تعالى؛ علامة كمال الإيمان','7','8','0');
+INSERT INTO [t36392] VALUES (10,'* ومراقبة الله تعالى؛ زينة للقلب','7','8','0');
+INSERT INTO [t36392] VALUES (11,'* ومراقبة الله تعالى؛ عون على غض البصر','7','8','0');
+INSERT INTO [t36392] VALUES (12,'* ومراقبة الله تعالى؛ سبب في الفوز بظل العرش يوم القيامة','7','8','0');
+INSERT INTO [t36392] VALUES (13,'أخي المسلم','9','0','0');
