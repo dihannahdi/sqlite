eeed3ebb2ca8e470cd20fa22fc15cc46ebd8f376
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:58 +0700

    Upload SQL chunk: t6696_chunk_0000.sql

---
+++ sql_chunks/t6696_chunk_0000.sql
@@ -0,0 +1,57 @@
+-- Table: t6696
+-- Chunk: 1
+-- Rows: 50
+
+CREATE TABLE "t6696" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6696
+INSERT INTO [t6696] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t6696] VALUES (2,'عدنان','6','0','0');
+INSERT INTO [t6696] VALUES (3,'وأما أنساب العرب','10','0','0');
+INSERT INTO [t6696] VALUES (4,'قحطان','16','0','0');
+INSERT INTO [t6696] VALUES (5,'قضاعة','21','0','0');
+INSERT INTO [t6696] VALUES (6,'نزار','27','0','0');
+INSERT INTO [t6696] VALUES (7,'مضر','28','0','0');
+INSERT INTO [t6696] VALUES (8,'خندف','29','0','0');
+INSERT INTO [t6696] VALUES (9,'قريش','30','0','0');
+INSERT INTO [t6696] VALUES (10,'كنانة وهذيل والقارة وأسد بن خزيمة','39','0','0');
+INSERT INTO [t6696] VALUES (11,'ثم القارة','43','0','0');
+INSERT INTO [t6696] VALUES (12,'تميم','45','0','0');
+INSERT INTO [t6696] VALUES (13,'مزينة والرباب وضبة','48','0','0');
+INSERT INTO [t6696] VALUES (14,'وأما ضبة','50','0','0');
+INSERT INTO [t6696] VALUES (15,'وأما الرباب','51','0','0');
+INSERT INTO [t6696] VALUES (16,'قيس بن عيلان بن مضر','54','0','0');
+INSERT INTO [t6696] VALUES (17,'وهذا ذكر بطون قيس وأفخاذها وشعوبها','57','0','0');
+INSERT INTO [t6696] VALUES (18,'جديلة قيس','59','0','0');
+INSERT INTO [t6696] VALUES (19,'ثقيف','66','0','0');
+INSERT INTO [t6696] VALUES (20,'خزاعة','71','0','0');
+INSERT INTO [t6696] VALUES (21,'ربيعة بن نزار','76','0','0');
+INSERT INTO [t6696] VALUES (22,'بجيلة وخثعم','82','0','0');
+INSERT INTO [t6696] VALUES (23,'عاملة','86','0','0');
+INSERT INTO [t6696] VALUES (24,'لخم وجذام','88','0','0');
+INSERT INTO [t6696] VALUES (25,'جمائع قبائل اليمن وشعوبها من الرواة','91','0','0');
+INSERT INTO [t6696] VALUES (26,'ثم أحمس بن الغوث أخو الأزد بن الغوث','100','0','0');
+INSERT INTO [t6696] VALUES (27,'ثم كندة','101','0','0');
+INSERT INTO [t6696] VALUES (28,'ثم الصدف','103','0','0');
+INSERT INTO [t6696] VALUES (29,'ثم السكون','104','0','0');
+INSERT INTO [t6696] VALUES (30,'السكاسك','105','0','0');
+INSERT INTO [t6696] VALUES (31,'ثم تجيب','106','0','0');
+INSERT INTO [t6696] VALUES (32,'خولان','107','0','0');
+INSERT INTO [t6696] VALUES (33,'الأشعرون','108','0','0');
+INSERT INTO [t6696] VALUES (34,'طيىء','109','0','0');
+INSERT INTO [t6696] VALUES (35,'مذحج','110','0','0');
+INSERT INTO [t6696] VALUES (36,'ثم النخع بن عمرو','111','0','0');
+INSERT INTO [t6696] VALUES (37,'ثم بنو الحارث بن كعب','112','0','0');
+INSERT INTO [t6696] VALUES (38,'وصداء','113','0','0');
+INSERT INTO [t6696] VALUES (39,'وسعد العشيرة','114','0','0');
+INSERT INTO [t6696] VALUES (40,'وأود','115','0','0');
+INSERT INTO [t6696] VALUES (41,'وزبيد','116','0','0');
+INSERT INTO [t6696] VALUES (42,'ومعافر بن يعفر','117','0','0');
+INSERT INTO [t6696] VALUES (43,'ومراد','118','0','0');
+INSERT INTO [t6696] VALUES (44,'وعنس','120','0','0');
+INSERT INTO [t6696] VALUES (45,'همدان','121','0','0');
+INSERT INTO [t6696] VALUES (46,'والهان','122','0','0');
+INSERT INTO [t6696] VALUES (47,'الأوزاع','123','0','0');
+INSERT INTO [t6696] VALUES (48,'وأما حضرموت','124','0','0');
+INSERT INTO [t6696] VALUES (49,'مهرة','125','0','0');
+INSERT INTO [t6696] VALUES (50,'قبائل قضاعة وبطونها','126','0','0');
