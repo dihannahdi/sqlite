b05be5eccb0f235066b86ecf31f9b394b8095adc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:13 +0700

    Upload SQL chunk: t36424_chunk_0000.sql

---
+++ sql_chunks/t36424_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t36424
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t36424" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36424
+INSERT INTO [t36424] VALUES (1,'الحمد لله تعالى واهب المنن','1','0','0');
+INSERT INTO [t36424] VALUES (2,'أخي في الله','2','0','0');
+INSERT INTO [t36424] VALUES (3,'أخي المسلم','2','0','0');
+INSERT INTO [t36424] VALUES (4,'أخي المسلم','3','0','0');
+INSERT INTO [t36424] VALUES (5,'أخي المسلم','3','0','0');
+INSERT INTO [t36424] VALUES (6,'أخي المسلم','5','0','0');
+INSERT INTO [t36424] VALUES (7,'أخي في الله','5','0','0');
+INSERT INTO [t36424] VALUES (8,'أخي المسلم','5','0','0');
+INSERT INTO [t36424] VALUES (9,'أخي في الله','6','0','0');
+INSERT INTO [t36424] VALUES (10,'أخي المسلم','7','0','0');
+INSERT INTO [t36424] VALUES (11,'أخي المسلم: من الأسباب الجالبة للمحبة','7','0','0');
+INSERT INTO [t36424] VALUES (12,'أخي في الله','9','0','0');
+INSERT INTO [t36424] VALUES (13,'أخي المسلم','10','0','0');
+INSERT INTO [t36424] VALUES (14,'أخي في الله','11','0','0');
