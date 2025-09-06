9b64dbd4dd82fb58b37dc8eacbb0f7c4540d5070
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:17 +0700

    Upload SQL chunk: t8653_chunk_0000.sql

---
+++ sql_chunks/t8653_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t8653
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t8653" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8653
+INSERT INTO [t8653] VALUES (1,'المقدمات','1','0','0');
+INSERT INTO [t8653] VALUES (2,'مقدمة الطبعة الثالثة','1','1','0');
+INSERT INTO [t8653] VALUES (3,'مقدمة الطبعة الأولى','3','1','0');
+INSERT INTO [t8653] VALUES (4,'مدخل','3','3','0');
+INSERT INTO [t8653] VALUES (5,'التفكير في وضع قواعد النحو والصرف وسببه','5','3','0');
+INSERT INTO [t8653] VALUES (6,'شيوع اللحن في المفردات والأساليب','5','5','0');
+INSERT INTO [t8653] VALUES (7,'أقدم كتاب وصلنا اسمه كان في موضوع صرفي','6','5','0');
+INSERT INTO [t8653] VALUES (8,'قيمة كتاب سيبويه وأثره','8','5','0');
+INSERT INTO [t8653] VALUES (9,'المؤلفون الذين أفردوا الصرف بتأليف مستقل','9','5','0');
+INSERT INTO [t8653] VALUES (10,'الحديث عن تصريف المازني','11','5','0');
+INSERT INTO [t8653] VALUES (11,'الحديث عن المنصف لابن جني','16','5','0');
