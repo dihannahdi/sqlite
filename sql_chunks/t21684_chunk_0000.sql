d75e239fd7d91f666ae6000a278a6321d84ad9cc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:13 +0700

    Upload SQL chunk: t21684_chunk_0000.sql

---
+++ sql_chunks/t21684_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21684
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21684" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21684
+INSERT INTO [t21684] VALUES (1,'من أحاديث محمد بن يحيى الذهلي رواية المعقلي عنه - مخطوط','2','0','0');
