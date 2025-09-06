8dd0d59565344f4af15051427de22b0b6e0d5a1d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:16:15 +0700

    Upload SQL chunk: t1399_chunk_0000.sql

---
+++ sql_chunks/t1399_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t1399
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t1399" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1399
+INSERT INTO [t1399] VALUES (1,'درر الحكم لأبي منصور الثعالبي','2','0','0');
+INSERT INTO [t1399] VALUES (2,'مقدمة','2','0','0');
