053f3251fd0da857fb75a94b635dba060a2c7592
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:10 +0700

    Upload SQL chunk: t26890_chunk_0000.sql

---
+++ sql_chunks/t26890_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t26890
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t26890" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26890
+INSERT INTO [t26890] VALUES (1,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t26890] VALUES (2,'فصل','5','0','0');
+INSERT INTO [t26890] VALUES (3,'أحاديث أنس','6','0','0');
+INSERT INTO [t26890] VALUES (4,'حديث أبي بكر رضي الله عنه','15','0','0');
+INSERT INTO [t26890] VALUES (5,'حديث عمران بن حصين','16','0','0');
+INSERT INTO [t26890] VALUES (6,'حديث أبي موسى الأشعري','18','0','0');
+INSERT INTO [t26890] VALUES (7,'حديث أبي بكر الثقفي','21','0','0');
+INSERT INTO [t26890] VALUES (8,'حديث أم حبيبة','22','0','0');
+INSERT INTO [t26890] VALUES (9,'أحاديث أبي سعد الخدري','23','0','0');
+INSERT INTO [t26890] VALUES (10,'أحاديث أبي هريرة','30','0','0');
+INSERT INTO [t26890] VALUES (11,'أحاديث أبي أمامة الباهلي','40','0','0');
+INSERT INTO [t26890] VALUES (12,'حديث العباس','45','0','0');
+INSERT INTO [t26890] VALUES (13,'أحاديث ابن عباس','46','0','0');
+INSERT INTO [t26890] VALUES (14,'أحاديث ابن عمر','49','0','0');
+INSERT INTO [t26890] VALUES (15,'أحاديث عبد الله بن عمرو','53','0','0');
+INSERT INTO [t26890] VALUES (16,'حديث عبد الله بن بسر','55','0','0');
+INSERT INTO [t26890] VALUES (17,'حديث لابن مسعود','56','0','0');
+INSERT INTO [t26890] VALUES (18,'حديث لابن أبي الجدعاء','57','0','0');
+INSERT INTO [t26890] VALUES (19,'رواية جابر بن عبد الله','58','0','0');
+INSERT INTO [t26890] VALUES (20,'حديث عمر مرسل','63','0','0');
+INSERT INTO [t26890] VALUES (21,'[حديث الحسن البصري]','64','0','0');
+INSERT INTO [t26890] VALUES (22,'حديث عوف بن مالك الأشجعي','65','0','0');
+INSERT INTO [t26890] VALUES (23,'حديث عقبة بن عامر','67','0','0');
+INSERT INTO [t26890] VALUES (24,'حديث الحارث بن أقيش','69','0','0');
+INSERT INTO [t26890] VALUES (25,'حديث عبد الرحمن بن أبي عقيل الثقفي','70','0','0');
+INSERT INTO [t26890] VALUES (26,'حديث مصعب','71','0','0');
+INSERT INTO [t26890] VALUES (27,'حديث أم سلمة','72','0','0');
