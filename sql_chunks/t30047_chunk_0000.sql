2dca15c7463ab875e12c46db99e401693000915b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:23 +0700

    Upload SQL chunk: t30047_chunk_0000.sql

---
+++ sql_chunks/t30047_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30047
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30047" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30047
+INSERT INTO [t30047] VALUES (1,'حديث آخر مسلسل على شرط السؤال عن الإخلاص ما هو؟','4','0','0');
