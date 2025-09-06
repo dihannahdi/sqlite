c5795e76313af912fd63c837bf04493b993c5d31
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:49 +0700

    Upload SQL chunk: t680_chunk_0000.sql

---
+++ sql_chunks/t680_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t680
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t680" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t680
+INSERT INTO [t680] VALUES (1,'صلاة التراويح','1','0','0');
