9517c9a069dc2ea512d6354502792b470e2e4e54
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:25 +0700

    Upload SQL chunk: t719_chunk_0000.sql

---
+++ sql_chunks/t719_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t719
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t719" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t719
+INSERT INTO [t719] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t719] VALUES (2,'صلى الله على مولانا محمد وآله وصحبه','1','0','0');
+INSERT INTO [t719] VALUES (3,'جمل تنضبط بها قواعد المنطق وأحكامه','1','0','0');
