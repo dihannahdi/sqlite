c8605a6ddbaa7b689bfac8d5ec82e736e9d9641a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:28 +0700

    Upload SQL chunk: t11441_chunk_0000.sql

---
+++ sql_chunks/t11441_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11441
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11441" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11441
+INSERT INTO [t11441] VALUES (1,'حديث الضب','2','0','0');
