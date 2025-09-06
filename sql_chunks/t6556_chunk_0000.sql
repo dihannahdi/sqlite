3994221d564c4177be8f78d46453f30637b6a768
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:39 +0700

    Upload SQL chunk: t6556_chunk_0000.sql

---
+++ sql_chunks/t6556_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6556
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6556" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6556
+INSERT INTO [t6556] VALUES (1,'مناصحة الإمام وهب بن منبه لرجل تأثر بمذهب الخوارج','1','0','0');
+INSERT INTO [t6556] VALUES (2,'اعتنى بنشرها عبد السلام بن برجس آل عبد الكريم','1','0','0');
