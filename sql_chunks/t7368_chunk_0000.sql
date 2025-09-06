2518bc17b875b652adf3fb90fcfcf31eed70b815
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:02 +0700

    Upload SQL chunk: t7368_chunk_0000.sql

---
+++ sql_chunks/t7368_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t7368
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t7368" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7368
+INSERT INTO [t7368] VALUES (1,'ترجمة مؤلف المتن','2','0','0');
+INSERT INTO [t7368] VALUES (2,'مقدمة في أقسام الكلام','3','0','0');
+INSERT INTO [t7368] VALUES (3,'باب الإعراب','18','0','0');
+INSERT INTO [t7368] VALUES (4,'باب معرفة علامات الإعراب','22','0','0');
+INSERT INTO [t7368] VALUES (5,'فصل في أقسام المعربات','35','0','0');
+INSERT INTO [t7368] VALUES (6,'باب الأفعال','40','0','0');
+INSERT INTO [t7368] VALUES (7,'باب مرفوعات الأسماء','53','0','0');
+INSERT INTO [t7368] VALUES (8,'باب الفاعل','55','0','0');
+INSERT INTO [t7368] VALUES (9,'باب المفعول الذي لم يسم فاعله','60','0','0');
+INSERT INTO [t7368] VALUES (10,'باب المبتدأ والخبر','63','0','0');
+INSERT INTO [t7368] VALUES (11,'باب العوامل الداخلة على المبتدأ والخبر','67','0','0');
+INSERT INTO [t7368] VALUES (12,'باب النعت','75','0','0');
+INSERT INTO [t7368] VALUES (13,'باب العطف','80','0','0');
+INSERT INTO [t7368] VALUES (14,'باب التوكيد','84','0','0');
+INSERT INTO [t7368] VALUES (15,'باب البدل','86','0','0');
+INSERT INTO [t7368] VALUES (16,'باب منصوبات الأسماء','88','0','0');
+INSERT INTO [t7368] VALUES (17,'باب المفعول به','91','0','0');
+INSERT INTO [t7368] VALUES (18,'باب المصدر','94','0','0');
+INSERT INTO [t7368] VALUES (19,'باب ظرف الزمان وظرف المكان','96','0','0');
+INSERT INTO [t7368] VALUES (20,'باب الحال','99','0','0');
+INSERT INTO [t7368] VALUES (21,'باب التمييز','101','0','0');
+INSERT INTO [t7368] VALUES (22,'باب الاستثناء','103','0','0');
+INSERT INTO [t7368] VALUES (23,'باب لا النافية للجنس','106','0','0');
+INSERT INTO [t7368] VALUES (24,'باب المنادى','108','0','0');
+INSERT INTO [t7368] VALUES (25,'باب المفعول من أجله','110','0','0');
+INSERT INTO [t7368] VALUES (26,'باب المفعول معه','111','0','0');
+INSERT INTO [t7368] VALUES (27,'باب مخفوضات الأسماء','113','0','0');
+INSERT INTO [t7368] VALUES (28,'تتمة','115','0','0');
