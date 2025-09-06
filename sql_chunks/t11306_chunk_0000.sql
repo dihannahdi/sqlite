4d46b43dea16c2d0161cf0d48e6bf336060e1bc8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:44 +0700

    Upload SQL chunk: t11306_chunk_0000.sql

---
+++ sql_chunks/t11306_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t11306
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t11306" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11306
+INSERT INTO [t11306] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11306] VALUES (2,'عناية المحدثين بالإسناد وبيان قيمته في حفظ السنة','5','0','0');
+INSERT INTO [t11306] VALUES (3,'مدخل','5','2','0');
+INSERT INTO [t11306] VALUES (4,'الجرح والتعديل لغة واصطلاحا','7','2','0');
+INSERT INTO [t11306] VALUES (5,'مشروعيته','10','2','0');
+INSERT INTO [t11306] VALUES (6,'نشأة الجرح والتعديل وتطوره','12','2','0');
+INSERT INTO [t11306] VALUES (7,'بداية الكلام في الرواة','14','2','0');
+INSERT INTO [t11306] VALUES (8,'نماذج من عناية العلماء بالإسناد والجرح والتعديل ومناهجهم في النقد','18','0','0');
+INSERT INTO [t11306] VALUES (9,'شعبة ونقد الرواة','18','8','0');
+INSERT INTO [t11306] VALUES (10,'مكانة شعبة في علم الرجال','23','8','0');
+INSERT INTO [t11306] VALUES (11,'منهجه في هذا الميدان','39','8','0');
+INSERT INTO [t11306] VALUES (12,'قواعد الجرح والتعديل','45','0','0');
+INSERT INTO [t11306] VALUES (13,'*','45','12','0');
+INSERT INTO [t11306] VALUES (14,'آثار علم الجرح والتعديل','51','12','0');
+INSERT INTO [t11306] VALUES (15,'كتب علم الجرح والتعديل وكتب علم الرجال وأثرهما في حفظ السنة','54','0','0');
+INSERT INTO [t11306] VALUES (16,'مدخل','54','15','0');
+INSERT INTO [t11306] VALUES (17,'مصادر ومراجع','57','0','0');
