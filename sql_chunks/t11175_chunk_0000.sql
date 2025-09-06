e16069d4340a0ac530c9b0c8d4da1857ddb066f5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:00 +0700

    Upload SQL chunk: t11175_chunk_0000.sql

---
+++ sql_chunks/t11175_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t11175
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t11175" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11175
+INSERT INTO [t11175] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11175] VALUES (2,'الفصل الأول في نص الخطبة','4','0','0');
+INSERT INTO [t11175] VALUES (3,'الفصل الثاني في تخريج الخطبة','7','0','0');
+INSERT INTO [t11175] VALUES (4,'الخاتمة','34','0','0');
