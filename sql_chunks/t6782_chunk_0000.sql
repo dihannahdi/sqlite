2505e9bb3fadc995fa641625874cea6b5386070f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:33:41 +0700

    Upload SQL chunk: t6782_chunk_0000.sql

---
+++ sql_chunks/t6782_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t6782
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t6782" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6782
+INSERT INTO [t6782] VALUES (1,'باب نجاة الإنسان بالصمت وحفظ اللسان','3','0','0');
+INSERT INTO [t6782] VALUES (2,'باب السكوت ولزوم البيوت','13','0','0');
+INSERT INTO [t6782] VALUES (3,'باب ما يجب عند ظهور الفتن من طلب السلامة ولزوم الوطن','19','0','0');
+INSERT INTO [t6782] VALUES (4,'باب الإشتغال بما يغني وترك الخوض فيما لا يعني','24','0','0');
