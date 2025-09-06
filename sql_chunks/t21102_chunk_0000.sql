42a71a02bbaedefa6b2acc5143bb5567e7735575
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:37:54 +0700

    Upload SQL chunk: t21102_chunk_0000.sql

---
+++ sql_chunks/t21102_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t21102
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t21102" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21102
+INSERT INTO [t21102] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t21102] VALUES (2,'الفصل الأول: الصورة الأدبية في النقد الأدبي القديم','6','0','0');
+INSERT INTO [t21102] VALUES (3,'الفصل الثاني: الصورة الأدبية في النقد الأدبي الحديث','95','0','0');
+INSERT INTO [t21102] VALUES (4,'الفصل الثالث: معالم الصورة الأدبية','146','0','0');
+INSERT INTO [t21102] VALUES (5,'ثبت الموضوعات','172','0','0');
