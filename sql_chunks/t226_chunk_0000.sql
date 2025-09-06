7a85b6e8fb94d0844971b2bf21331296cac30852
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:44:11 +0700

    Upload SQL chunk: t226_chunk_0000.sql

---
+++ sql_chunks/t226_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t226
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t226" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t226
+INSERT INTO [t226] VALUES (1,'التنبيهات على ما في نوادر أبي زياد','1','0','0');
+INSERT INTO [t226] VALUES (2,'التنبيهات على ما في نوادر','7','0','0');
+INSERT INTO [t226] VALUES (3,'هذا آخر ما في نوادر أبي عمرو','12','0','0');
+INSERT INTO [t226] VALUES (4,'التنبيهات على ما في كتاب النبات','19','0','0');
+INSERT INTO [t226] VALUES (5,'المستدرك على التنبيهات','35','0','0');
