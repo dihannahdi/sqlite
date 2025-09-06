8bb578281adbf0038a4fa5a4d61ec31607d6b233
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:38 +0700

    Upload SQL chunk: t11371_chunk_0000.sql

---
+++ sql_chunks/t11371_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t11371
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t11371" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11371
+INSERT INTO [t11371] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t11371] VALUES (2,'باب الإعراب','2','0','0');
+INSERT INTO [t11371] VALUES (3,'باب معرفة علامات الإعراب','3','0','0');
+INSERT INTO [t11371] VALUES (4,'باب الأفعال','7','0','0');
+INSERT INTO [t11371] VALUES (5,'باب مرفوعات الأسماء','8','0','0');
+INSERT INTO [t11371] VALUES (6,'باب الفاعل','9','0','0');
+INSERT INTO [t11371] VALUES (7,'باب المفعول الذي لم يسم فاعله','10','0','0');
+INSERT INTO [t11371] VALUES (8,'باب المبتدأ والخبر','11','0','0');
+INSERT INTO [t11371] VALUES (9,'باب العوامل الداخلة على المبتدأ والخبر','13','0','0');
+INSERT INTO [t11371] VALUES (10,'باب النعت','15','0','0');
+INSERT INTO [t11371] VALUES (11,'باب العطف','16','0','0');
+INSERT INTO [t11371] VALUES (12,'باب التوكيد','17','0','0');
+INSERT INTO [t11371] VALUES (13,'باب البدل','18','0','0');
+INSERT INTO [t11371] VALUES (14,'باب منصوبات الأسماء','19','0','0');
+INSERT INTO [t11371] VALUES (15,'باب المفعول به','20','0','0');
+INSERT INTO [t11371] VALUES (16,'باب المصدر','21','0','0');
+INSERT INTO [t11371] VALUES (17,'باب ظرف الزمان وظرف المكان','22','0','0');
+INSERT INTO [t11371] VALUES (18,'باب الحال','23','0','0');
+INSERT INTO [t11371] VALUES (19,'باب التمييز','24','0','0');
+INSERT INTO [t11371] VALUES (20,'باب الإستثناء','25','0','0');
+INSERT INTO [t11371] VALUES (21,'باب لا','26','0','0');
+INSERT INTO [t11371] VALUES (22,'باب المنادى','27','0','0');
+INSERT INTO [t11371] VALUES (23,'باب المفعول من أجله','28','0','0');
+INSERT INTO [t11371] VALUES (24,'باب المفعول معه','29','0','0');
+INSERT INTO [t11371] VALUES (25,'باب المخفوضات من الأسماء','30','0','0');
