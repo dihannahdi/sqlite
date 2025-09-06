4d367fd825ebc79f3b860735d848b37615bd198e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:00 +0700

    Upload SQL chunk: t7568_chunk_0000.sql

---
+++ sql_chunks/t7568_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t7568
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t7568" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7568
+INSERT INTO [t7568] VALUES (1,'البيان المبدي لشناعة القول المجدي','1','0','0');
+INSERT INTO [t7568] VALUES (2,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t7568] VALUES (3,'الحمد لله الذي أزال شناعات القول المجدي','157','0','0');
