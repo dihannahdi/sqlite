0bf5d84af07332fb2f6ce3a8115949ba371db80f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:15 +0700

    Upload SQL chunk: t9568_chunk_0000.sql

---
+++ sql_chunks/t9568_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t9568
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t9568" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9568
+INSERT INTO [t9568] VALUES (1,'بسم الله الرحمن الرحيم وبه نستعين','1','0','0');
+INSERT INTO [t9568] VALUES (2,'قال العلامة حجة الإسلام أبو جعفر الوراق الطحاوي','1','0','0');
+INSERT INTO [t9568] VALUES (3,'هذا ذكر بيان عقيدة أهل السنة والجماعة','1','0','0');
+INSERT INTO [t9568] VALUES (4,'وتمت المقابلة بالأصل','124','0','0');
