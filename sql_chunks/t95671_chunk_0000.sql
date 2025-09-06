5f365810ad4e0bd13f955d1f91e77fcaffc57418
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:59 +0700

    Upload SQL chunk: t95671_chunk_0000.sql

---
+++ sql_chunks/t95671_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t95671
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t95671" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95671
+INSERT INTO [t95671] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95671] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t95671] VALUES (3,'النظرة سهم','5','0','0');
+INSERT INTO [t95671] VALUES (4,'فتنة النظر','6','0','0');
+INSERT INTO [t95671] VALUES (5,'النظر والمرأة','7','0','0');
+INSERT INTO [t95671] VALUES (6,'إطلاق البصر','14','0','0');
+INSERT INTO [t95671] VALUES (7,'قصة العابد','21','0','0');
+INSERT INTO [t95671] VALUES (8,'النظرة الثانية','23','0','0');
+INSERT INTO [t95671] VALUES (9,'الحمو الموت','28','0','0');
+INSERT INTO [t95671] VALUES (10,'فوائد غض البصر','32','0','0');
+INSERT INTO [t95671] VALUES (11,'يا معشر الشباب','34','0','0');
+INSERT INTO [t95671] VALUES (12,'فانكحوا','39','0','0');
+INSERT INTO [t95671] VALUES (13,'ترك المعاصي','43','0','0');
+INSERT INTO [t95671] VALUES (14,'قبل الإسلام','48','0','0');
+INSERT INTO [t95671] VALUES (15,'المحاسبة','50','0','0');
+INSERT INTO [t95671] VALUES (16,'عبرة','57','0','0');
+INSERT INTO [t95671] VALUES (17,'بشارة','62','0','0');
+INSERT INTO [t95671] VALUES (18,'المصادر','63','0','0');
