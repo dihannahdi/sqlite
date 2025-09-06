ef78f4d2067907ea3954b9a9ce95f0d41bf79f31
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:05:53 +0700

    Upload SQL chunk: t126_chunk_0000.sql

---
+++ sql_chunks/t126_chunk_0000.sql
@@ -0,0 +1,42 @@
+-- Table: t126
+-- Chunk: 1
+-- Rows: 35
+
+CREATE TABLE "t126" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t126
+INSERT INTO [t126] VALUES (1,'النسخة المعتمدة للبيقونية','1','0','0');
+INSERT INTO [t126] VALUES (2,'مقدمة الشارح','1','0','0');
+INSERT INTO [t126] VALUES (3,'متن المنظومة البيقونية','2','0','0');
+INSERT INTO [t126] VALUES (4,'(الحديث الصحيح)','13','0','0');
+INSERT INTO [t126] VALUES (5,'(الحديث الحسن)','23','0','0');
+INSERT INTO [t126] VALUES (6,'(الحديث الضعيف)','33','0','0');
+INSERT INTO [t126] VALUES (7,'(الحديث المرفوع)','40','0','0');
+INSERT INTO [t126] VALUES (8,'(الحديث المقطوع)','44','0','0');
+INSERT INTO [t126] VALUES (9,'(الحديث المسند)','45','0','0');
+INSERT INTO [t126] VALUES (10,'(الحديث المتصل)','47','0','0');
+INSERT INTO [t126] VALUES (11,'(الحديث المسلسل)','48','0','0');
+INSERT INTO [t126] VALUES (12,'(الحديث العزيز)','52','0','0');
+INSERT INTO [t126] VALUES (13,'(الحديث المشهور)','56','0','0');
+INSERT INTO [t126] VALUES (14,'(الحديث المعنعن)','59','0','0');
+INSERT INTO [t126] VALUES (15,'(الحديث المبهم)','65','0','0');
+INSERT INTO [t126] VALUES (16,'(الحديث العالي)','71','0','0');
+INSERT INTO [t126] VALUES (17,'(الحديث النازل)','73','0','0');
+INSERT INTO [t126] VALUES (18,'(الحديث الموقوف)','74','0','0');
+INSERT INTO [t126] VALUES (19,'(الحديث المرسل)','77','0','0');
+INSERT INTO [t126] VALUES (20,'(الحديث الغريب)','82','0','0');
+INSERT INTO [t126] VALUES (21,'(الحديث المنقطع)','84','0','0');
+INSERT INTO [t126] VALUES (22,'(الحديث المعضل)','85','0','0');
+INSERT INTO [t126] VALUES (23,'(الحديث المدلس)','87','0','0');
+INSERT INTO [t126] VALUES (24,'(الحديث الشاذ)','97','0','0');
+INSERT INTO [t126] VALUES (25,'(الحديث المقلوب)','101','0','0');
+INSERT INTO [t126] VALUES (26,'(الحديث الفرد)','104','0','0');
+INSERT INTO [t126] VALUES (27,'(الحديث المعلل)','106','0','0');
+INSERT INTO [t126] VALUES (28,'(الحديث المضطرب)','110','0','0');
+INSERT INTO [t126] VALUES (29,'(الحديث المدرج)','114','0','0');
+INSERT INTO [t126] VALUES (30,'(الحديث المدبج)','117','0','0');
+INSERT INTO [t126] VALUES (31,'(الحديث المتفق والمفترق)','118','0','0');
+INSERT INTO [t126] VALUES (32,'(الحديث المؤتلف والمختلف)','119','0','0');
+INSERT INTO [t126] VALUES (33,'(الحديث المنكر)','120','0','0');
+INSERT INTO [t126] VALUES (34,'(الحديث المتروك)','121','0','0');
+INSERT INTO [t126] VALUES (35,'(الحديث الموضوع)','122','0','0');
