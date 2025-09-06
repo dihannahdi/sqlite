ce4920337d9fe171b2092dc94993e56b2373e76d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:47 +0700

    Upload SQL chunk: t5418_chunk_0000.sql

---
+++ sql_chunks/t5418_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t5418
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t5418" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5418
+INSERT INTO [t5418] VALUES (1,'المساكين','1','0','0');
+INSERT INTO [t5418] VALUES (2,'الشيخ علي','1','0','0');
+INSERT INTO [t5418] VALUES (3,'في وحي الروح','5','0','0');
+INSERT INTO [t5418] VALUES (4,'التراب المتكلم أمام التراب الصامت','5','0','0');
+INSERT INTO [t5418] VALUES (5,'الفقر والفقير','9','0','0');
+INSERT INTO [t5418] VALUES (6,'مسكينة! مسكينة!','15','0','0');
+INSERT INTO [t5418] VALUES (7,'لؤم المال ووهم التعاسة','19','0','0');
+INSERT INTO [t5418] VALUES (8,'وهم الحياة والسعادة','25','0','0');
+INSERT INTO [t5418] VALUES (9,'سحق اللؤلؤة ...','32','0','0');
+INSERT INTO [t5418] VALUES (10,'الرجل البخيل','33','0','0');
+INSERT INTO [t5418] VALUES (11,'في الحب','37','0','0');
+INSERT INTO [t5418] VALUES (12,'في الحفلات','37','0','0');
+INSERT INTO [t5418] VALUES (13,'في الرقص','37','0','0');
+INSERT INTO [t5418] VALUES (14,'في الموسيقى','37','0','0');
+INSERT INTO [t5418] VALUES (15,'يا ليل','38','0','0');
+INSERT INTO [t5418] VALUES (16,'على المائدة','40','0','0');
+INSERT INTO [t5418] VALUES (17,'فصل خامس في السنة','41','0','0');
+INSERT INTO [t5418] VALUES (18,'شهر النحل','46','0','0');
+INSERT INTO [t5418] VALUES (19,'وبعد','47','0','0');
+INSERT INTO [t5418] VALUES (20,'الحظ','48','0','0');
+INSERT INTO [t5418] VALUES (21,'الحرب','53','0','0');
+INSERT INTO [t5418] VALUES (22,'على الكوكب الهاوي. حسناء أفقرتها الحرب، وكيف تلقاها الحقيقة؟','57','0','0');
+INSERT INTO [t5418] VALUES (23,'الجمال والحب','58','0','0');
+INSERT INTO [t5418] VALUES (24,'الدين ولادة ثانية','62','0','0');
