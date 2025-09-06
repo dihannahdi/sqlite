f114ff72ee9f06dbbfc9ea7d80dde47920f60988
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:28 +0700

    Upload SQL chunk: t9976_chunk_0000.sql

---
+++ sql_chunks/t9976_chunk_0000.sql
@@ -0,0 +1,56 @@
+-- Table: t9976
+-- Chunk: 1
+-- Rows: 49
+
+CREATE TABLE "t9976" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9976
+INSERT INTO [t9976] VALUES (1,'المقدمات','1','0','0');
+INSERT INTO [t9976] VALUES (2,'بين يدي هذا الكتاب','1','1','0');
+INSERT INTO [t9976] VALUES (3,'مقدمة الطبعة الأولى','3','1','0');
+INSERT INTO [t9976] VALUES (4,'الباب الأول: الكلمة','6','0','0');
+INSERT INTO [t9976] VALUES (5,'تحديد نوع الكلمة','6','4','0');
+INSERT INTO [t9976] VALUES (6,'حالة الكلمة (الأعراب والبناء)','10','4','0');
+INSERT INTO [t9976] VALUES (7,'الإعراب','12','4','0');
+INSERT INTO [t9976] VALUES (8,'علامات الإعراب','14','4','0');
+INSERT INTO [t9976] VALUES (9,'الإعراب الظاهر والإعراب المقدر','20','4','0');
+INSERT INTO [t9976] VALUES (10,'البناء','28','4','0');
+INSERT INTO [t9976] VALUES (11,'النوع الأول','28','10','0');
+INSERT INTO [t9976] VALUES (12,'النوع الثاني','29','10','0');
+INSERT INTO [t9976] VALUES (13,'النوع الثالث: الأسماء المبنية','35','10','0');
+INSERT INTO [t9976] VALUES (14,'الباب الثاني: الجملة وشبه الجملة','75','0','0');
+INSERT INTO [t9976] VALUES (15,'الفصل الأول: الجملة الأسمية','75','14','0');
+INSERT INTO [t9976] VALUES (16,'مدخل','75','15','0');
+INSERT INTO [t9976] VALUES (17,'المبتدأ','77','15','0');
+INSERT INTO [t9976] VALUES (18,'الخبر','88','15','0');
+INSERT INTO [t9976] VALUES (19,'النواسخ','103','15','0');
+INSERT INTO [t9976] VALUES (20,'الفصل الثاني: الجملة الفعلية','164','14','0');
+INSERT INTO [t9976] VALUES (21,'الفاعل','164','20','0');
+INSERT INTO [t9976] VALUES (22,'نائب الفاعل','174','20','0');
+INSERT INTO [t9976] VALUES (23,'المفاعيل','179','20','0');
+INSERT INTO [t9976] VALUES (24,'الحال','237','20','0');
+INSERT INTO [t9976] VALUES (25,'التمييز','247','20','0');
+INSERT INTO [t9976] VALUES (26,'الفصل الثالث: الجمل الأسلوبية','252','14','0');
+INSERT INTO [t9976] VALUES (27,'مدخل','252','26','0');
+INSERT INTO [t9976] VALUES (28,'جملة الأستثناء','253','26','0');
+INSERT INTO [t9976] VALUES (29,'جملة النداء','266','26','0');
+INSERT INTO [t9976] VALUES (30,'جمل الأمر والنهي والعرض','286','26','0');
+INSERT INTO [t9976] VALUES (31,'جملة الأستفهام','291','26','0');
+INSERT INTO [t9976] VALUES (32,'جملة التعجب','295','26','0');
+INSERT INTO [t9976] VALUES (33,'جملة المدح والذم','302','26','0');
+INSERT INTO [t9976] VALUES (34,'جملة الشرط','310','26','0');
+INSERT INTO [t9976] VALUES (35,'جملة القسم','315','26','0');
+INSERT INTO [t9976] VALUES (36,'الفصل الرابع: مواقع الجملة','320','14','0');
+INSERT INTO [t9976] VALUES (37,'الجملة التي لها محل من الإعراب','320','36','0');
+INSERT INTO [t9976] VALUES (38,'الجملة التي لا محل لها من الإعراب','338','36','0');
+INSERT INTO [t9976] VALUES (39,'الفصل الخامس: شبه الجملة','347','14','0');
+INSERT INTO [t9976] VALUES (40,'الملاحق','363','0','0');
+INSERT INTO [t9976] VALUES (41,'ملحق رقم (١) التوابع','363','40','0');
+INSERT INTO [t9976] VALUES (42,'النعت','363','41','0');
+INSERT INTO [t9976] VALUES (43,'التوكيد','368','41','0');
+INSERT INTO [t9976] VALUES (44,'البدل','372','41','0');
+INSERT INTO [t9976] VALUES (45,'عطف البيان','375','41','0');
+INSERT INTO [t9976] VALUES (46,'عطف النسق','376','41','0');
+INSERT INTO [t9976] VALUES (47,'الممنوع من الصرف','381','41','0');
+INSERT INTO [t9976] VALUES (48,'ملحق رقم (٢) متفرقات تطبيقية','386','40','0');
+INSERT INTO [t9976] VALUES (49,'فهرست','416','0','0');
