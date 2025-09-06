b8fa0281608a5ffbd316db6b6f5e649d801dc395
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:36 +0700

    Upload SQL chunk: t1791_chunk_0000.sql

---
+++ sql_chunks/t1791_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t1791
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t1791" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1791
+INSERT INTO [t1791] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t1791] VALUES (2,'موسم العبادة','7','0','0');
+INSERT INTO [t1791] VALUES (3,'هذا هو الدليل','12','0','0');
+INSERT INTO [t1791] VALUES (4,'الاستعداد ليوم المعاد','19','0','0');
+INSERT INTO [t1791] VALUES (5,'بين يدي الله','24','0','0');
+INSERT INTO [t1791] VALUES (6,'حي على الصلاة','32','0','0');
+INSERT INTO [t1791] VALUES (7,'من روض النبوة','40','0','0');
+INSERT INTO [t1791] VALUES (8,'التجارة الرابحة','46','0','0');
+INSERT INTO [t1791] VALUES (9,'ما قدروا الله حق قدره!','49','0','0');
+INSERT INTO [t1791] VALUES (10,'طريق الهدى','54','0','0');
+INSERT INTO [t1791] VALUES (11,'الإيمان يصنع النصر','57','0','0');
+INSERT INTO [t1791] VALUES (12,'الله أكبر','62','0','0');
+INSERT INTO [t1791] VALUES (13,'الباب الذي لا يغلق في وجه سائل','67','0','0');
+INSERT INTO [t1791] VALUES (14,'يا الله!','79','0','0');
+INSERT INTO [t1791] VALUES (15,'فطرة الإيمان','85','0','0');
+INSERT INTO [t1791] VALUES (16,'عبرة السيرة','92','0','0');
+INSERT INTO [t1791] VALUES (17,'بين الدنيا والآخرة','102','0','0');
+INSERT INTO [t1791] VALUES (18,'النفس الأمارة بالسوء والنفس اللوامة','107','0','0');
+INSERT INTO [t1791] VALUES (19,'الرزق مقسوم ولكن العمل له واجب','114','0','0');
+INSERT INTO [t1791] VALUES (20,'رحلة الحج','126','0','0');
+INSERT INTO [t1791] VALUES (21,'أعرابي في بلودان','135','0','0');
+INSERT INTO [t1791] VALUES (22,'روح الصلاة','142','0','0');
+INSERT INTO [t1791] VALUES (23,'أين التائبون؟','148','0','0');
+INSERT INTO [t1791] VALUES (24,'طريق الإسلام','156','0','0');
+INSERT INTO [t1791] VALUES (25,'جواب على سؤال','166','0','0');
+INSERT INTO [t1791] VALUES (26,'هذا هو الطريق','171','0','0');
+INSERT INTO [t1791] VALUES (27,'هل تهدي الفطرة إلى الدين؟','177','0','0');
+INSERT INTO [t1791] VALUES (28,'يا سيدي يا رسول الله','182','0','0');
+INSERT INTO [t1791] VALUES (29,'النية الصالحة أصل كل خير','189','0','0');
+INSERT INTO [t1791] VALUES (30,'هل نحن مؤمنون؟','194','0','0');
+INSERT INTO [t1791] VALUES (31,'يا أيها المذنبون','199','0','0');
+INSERT INTO [t1791] VALUES (32,'التوبة','203','0','0');
+INSERT INTO [t1791] VALUES (33,'طريق الدنيا وطريق الآخرة','209','0','0');
+INSERT INTO [t1791] VALUES (34,'يا شباب الإسلام','222','0','0');
+INSERT INTO [t1791] VALUES (35,'في ذكرى المولد','225','0','0');
+INSERT INTO [t1791] VALUES (36,'أياصوفيا','232','0','0');
+INSERT INTO [t1791] VALUES (37,'الحرب والإيمان','242','0','0');
+INSERT INTO [t1791] VALUES (38,'المحتويات','261','0','0');
+INSERT INTO [t1791] VALUES (39,'من آثار المؤلف','263','0','0');
