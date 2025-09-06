9432de68208f9806259c45e854ba8325f0e458f4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:38 +0700

    Upload SQL chunk: t12827_chunk_0000.sql

---
+++ sql_chunks/t12827_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t12827
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t12827" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12827
+INSERT INTO [t12827] VALUES (1,'تمهيد','2','0','0');
+INSERT INTO [t12827] VALUES (2,'موضوع الرسالة','7','0','0');
+INSERT INTO [t12827] VALUES (3,'وصف النسخ','9','0','0');
+INSERT INTO [t12827] VALUES (4,'النص المحقق','13','0','0');
+INSERT INTO [t12827] VALUES (5,'الدليل الأول:','14','4','0');
+INSERT INTO [t12827] VALUES (6,'الدليل الثاني:','15','4','0');
+INSERT INTO [t12827] VALUES (7,'الدليل الثالث:','15','4','0');
+INSERT INTO [t12827] VALUES (8,'الدليل الرابع:','16','4','0');
+INSERT INTO [t12827] VALUES (9,'الدليل الخامس:','18','4','0');
+INSERT INTO [t12827] VALUES (10,'الدليل السادس:','19','4','0');
+INSERT INTO [t12827] VALUES (11,'الدليل السابع:','21','4','0');
+INSERT INTO [t12827] VALUES (12,'الدليل الثامن:','22','4','0');
+INSERT INTO [t12827] VALUES (13,'الدليل التاسع:','23','4','0');
+INSERT INTO [t12827] VALUES (14,'الدليل العاشر:','23','4','0');
+INSERT INTO [t12827] VALUES (15,'الدليل الحادي عشر:','23','4','0');
+INSERT INTO [t12827] VALUES (16,'الدليل الثاني عشر:','24','4','0');
+INSERT INTO [t12827] VALUES (17,'الدليل الثالث عشر:','26','4','0');
+INSERT INTO [t12827] VALUES (18,'الدليل الرابع عشر:','27','4','0');
+INSERT INTO [t12827] VALUES (19,'الدليل الخامس عشر:','30','4','0');
+INSERT INTO [t12827] VALUES (20,'الدليل السادس عشر:','31','4','0');
+INSERT INTO [t12827] VALUES (21,'الدليل السابع عشر:','33','4','0');
+INSERT INTO [t12827] VALUES (22,'الدليل الثامن عشر:','35','4','0');
+INSERT INTO [t12827] VALUES (23,'الدليل التاسع عشر:','39','4','0');
+INSERT INTO [t12827] VALUES (24,'الدليل العشرون:','41','4','0');
+INSERT INTO [t12827] VALUES (25,'الدليل الحادي والعشرون:','42','4','0');
