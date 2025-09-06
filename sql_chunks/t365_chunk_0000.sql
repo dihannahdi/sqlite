dd1de6086ecd5f38548f2e538b6fca1a0009e257
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:31 +0700

    Upload SQL chunk: t365_chunk_0000.sql

---
+++ sql_chunks/t365_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t365
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t365" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t365
+INSERT INTO [t365] VALUES (1,'أول الكتاب','1','0','0');
