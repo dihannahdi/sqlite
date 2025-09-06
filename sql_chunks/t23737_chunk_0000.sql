ab22d551ec3ccf41d438b1cda6a525f2cabd64d0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:48:42 +0700

    Upload SQL chunk: t23737_chunk_0000.sql

---
+++ sql_chunks/t23737_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t23737
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t23737" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23737
+INSERT INTO [t23737] VALUES (1,'الكلام على الحمدلة والصلاة على الآل والأصحاب','2','0','0');
+INSERT INTO [t23737] VALUES (2,'الكلام على (أما بعد) وأقسام الحديث، ورواية الأحاديث الصحيحة','20','0','0');
+INSERT INTO [t23737] VALUES (3,'طبقات الرجال','39','0','0');
