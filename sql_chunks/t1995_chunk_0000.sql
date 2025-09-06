a94a980b4f27d36ba22ff909ee2d5e9f0c564041
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:59 +0700

    Upload SQL chunk: t1995_chunk_0000.sql

---
+++ sql_chunks/t1995_chunk_0000.sql
@@ -0,0 +1,31 @@
+-- Table: t1995
+-- Chunk: 1
+-- Rows: 24
+
+CREATE TABLE "t1995" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1995
+INSERT INTO [t1995] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1995] VALUES (2,'تمهيد','3','0','0');
+INSERT INTO [t1995] VALUES (3,'الوسائل الدعوية','8','0','0');
+INSERT INTO [t1995] VALUES (4,'القسم الأول الوسيلة المعنوية','8','3','0');
+INSERT INTO [t1995] VALUES (5,'القسم الثاني الوسيلة الخاصة','9','3','0');
+INSERT INTO [t1995] VALUES (6,'التأليف','12','3','0');
+INSERT INTO [t1995] VALUES (7,'الرسائل','14','3','0');
+INSERT INTO [t1995] VALUES (8,'المقدمات لمؤلفات الآخرين','15','3','0');
+INSERT INTO [t1995] VALUES (9,'الصحف والمجلات','16','3','0');
+INSERT INTO [t1995] VALUES (10,'الإنترنت','18','3','0');
+INSERT INTO [t1995] VALUES (11,'التعليم','19','3','0');
+INSERT INTO [t1995] VALUES (12,'الخطبة','21','3','0');
+INSERT INTO [t1995] VALUES (13,'الموعظة','22','3','0');
+INSERT INTO [t1995] VALUES (14,'المحاضرة','23','3','0');
+INSERT INTO [t1995] VALUES (15,'الندوة','24','3','0');
+INSERT INTO [t1995] VALUES (16,'الإذاعة','25','3','0');
+INSERT INTO [t1995] VALUES (17,'التلفزيون والقنوات الفضائية','26','3','0');
+INSERT INTO [t1995] VALUES (18,'الشريط الإسلامي','27','3','0');
+INSERT INTO [t1995] VALUES (19,'ثالثا وسيلة المؤسسات العلمية والدعوية','28','3','0');
+INSERT INTO [t1995] VALUES (20,'الذهاب إلى أماكن المدعوين','29','3','0');
+INSERT INTO [t1995] VALUES (21,'إجابة الدعوة','30','3','0');
+INSERT INTO [t1995] VALUES (22,'السعي في مصالحهم والإصلاح بينهم','32','3','0');
+INSERT INTO [t1995] VALUES (23,'الراحلة','35','3','0');
+INSERT INTO [t1995] VALUES (24,'الخاتمة','36','0','0');
