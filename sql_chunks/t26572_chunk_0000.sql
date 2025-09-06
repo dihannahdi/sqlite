f46999f27c3389c2e3c0d2fcc35a58c5bca39954
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:37 +0700

    Upload SQL chunk: t26572_chunk_0000.sql

---
+++ sql_chunks/t26572_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26572
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26572" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26572
+INSERT INTO [t26572] VALUES (1,'العوالي لأبي الشيخ','1','0','0');
