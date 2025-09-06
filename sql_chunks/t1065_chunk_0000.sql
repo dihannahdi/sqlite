cf4c3740e07c41da64967ba3defb2e364c75675e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:50:32 +0700

    Upload SQL chunk: t1065_chunk_0000.sql

---
+++ sql_chunks/t1065_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t1065
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t1065" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1065
+INSERT INTO [t1065] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1065] VALUES (2,'باب الألف','6','0','0');
+INSERT INTO [t1065] VALUES (3,'باب الباء','59','0','0');
+INSERT INTO [t1065] VALUES (4,'باب التاء','62','0','0');
+INSERT INTO [t1065] VALUES (5,'باب الثاء','65','0','0');
+INSERT INTO [t1065] VALUES (6,'باب الجيم','70','0','0');
+INSERT INTO [t1065] VALUES (7,'باب الحاء','71','0','0');
+INSERT INTO [t1065] VALUES (8,'باب الخاء','72','0','0');
+INSERT INTO [t1065] VALUES (9,'باب الدال','74','0','0');
+INSERT INTO [t1065] VALUES (10,'باب الذال','74','0','0');
+INSERT INTO [t1065] VALUES (11,'باب الراء','77','0','0');
+INSERT INTO [t1065] VALUES (12,'باب الزاء','79','0','0');
+INSERT INTO [t1065] VALUES (13,'باب السين','80','0','0');
+INSERT INTO [t1065] VALUES (14,'باب الشين','82','0','0');
+INSERT INTO [t1065] VALUES (15,'باب الصاد','83','0','0');
+INSERT INTO [t1065] VALUES (16,'باب الطاء','84','0','0');
+INSERT INTO [t1065] VALUES (17,'باب الظاء','84','0','0');
+INSERT INTO [t1065] VALUES (18,'باب العين','84','0','0');
+INSERT INTO [t1065] VALUES (19,'باب الغين','86','0','0');
+INSERT INTO [t1065] VALUES (20,'باب الفاء','87','0','0');
+INSERT INTO [t1065] VALUES (21,'باب القاف','115','0','0');
+INSERT INTO [t1065] VALUES (22,'باب الكاف','139','0','0');
+INSERT INTO [t1065] VALUES (23,'باب اللام','145','0','0');
+INSERT INTO [t1065] VALUES (24,'باب الميم','157','0','0');
+INSERT INTO [t1065] VALUES (25,'باب النون','164','0','0');
+INSERT INTO [t1065] VALUES (26,'باب الهاء','166','0','0');
+INSERT INTO [t1065] VALUES (27,'باب الواو','171','0','0');
+INSERT INTO [t1065] VALUES (28,'باب الياء','265','0','0');
