98c64411affc3d9a1e20d6cef00e886e8080376d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:06 +0700

    Upload SQL chunk: t2749_chunk_0000.sql

---
+++ sql_chunks/t2749_chunk_0000.sql
@@ -0,0 +1,62 @@
+-- Table: t2749
+-- Chunk: 1
+-- Rows: 55
+
+CREATE TABLE "t2749" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2749
+INSERT INTO [t2749] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t2749] VALUES (2,'الأحاديث التي ذكرها المازري','3','0','0');
+INSERT INTO [t2749] VALUES (3,'الحديث الأول','3','2','0');
+INSERT INTO [t2749] VALUES (4,'الحديث الثاني','9','2','0');
+INSERT INTO [t2749] VALUES (5,'الحديث الثالث','21','2','0');
+INSERT INTO [t2749] VALUES (6,'الحديث الرابع','25','2','0');
+INSERT INTO [t2749] VALUES (7,'الحديث الخامس','29','2','0');
+INSERT INTO [t2749] VALUES (8,'الحديث السادس','35','2','0');
+INSERT INTO [t2749] VALUES (9,'الحديث السابع','37','2','0');
+INSERT INTO [t2749] VALUES (10,'الحديث الثامن','39','2','0');
+INSERT INTO [t2749] VALUES (11,'الحديث التاسع','42','2','0');
+INSERT INTO [t2749] VALUES (12,'الحديث العاشر','44','2','0');
+INSERT INTO [t2749] VALUES (13,'الحديث الحادى عشر','48','2','0');
+INSERT INTO [t2749] VALUES (14,'الحديث الثاني عشر','51','2','0');
+INSERT INTO [t2749] VALUES (15,'الحديث الثالث عشر','58','2','0');
+INSERT INTO [t2749] VALUES (16,'أحاديث ملحقة','59','0','0');
+INSERT INTO [t2749] VALUES (17,'الحديث الأول','59','16','0');
+INSERT INTO [t2749] VALUES (18,'الحديث الثاني','65','16','0');
+INSERT INTO [t2749] VALUES (19,'الحديث الثالث','75','16','0');
+INSERT INTO [t2749] VALUES (20,'الحديث الرابع','78','16','0');
+INSERT INTO [t2749] VALUES (21,'الحديث الخامس','79','16','0');
+INSERT INTO [t2749] VALUES (22,'الحديث السادس','85','16','0');
+INSERT INTO [t2749] VALUES (23,'الحديث السابع','92','16','0');
+INSERT INTO [t2749] VALUES (24,'الحديث الثامن','93','16','0');
+INSERT INTO [t2749] VALUES (25,'الحديث التاسع','95','16','0');
+INSERT INTO [t2749] VALUES (26,'الحديث العاشر','98','16','0');
+INSERT INTO [t2749] VALUES (27,'الحديث الحادى عشر','102','16','0');
+INSERT INTO [t2749] VALUES (28,'الحديث الثاني عشر','105','16','0');
+INSERT INTO [t2749] VALUES (29,'الحديث الثالث عشر','110','16','0');
+INSERT INTO [t2749] VALUES (30,'الحديث الرابع عشر','113','16','0');
+INSERT INTO [t2749] VALUES (31,'الحديث الخامس عشر','115','16','0');
+INSERT INTO [t2749] VALUES (32,'الحديث السادس عشر','117','16','0');
+INSERT INTO [t2749] VALUES (33,'الحديث السابع عشر','118','16','0');
+INSERT INTO [t2749] VALUES (34,'الحديث الثامن عشر','121','16','0');
+INSERT INTO [t2749] VALUES (35,'الحديث التاسع عشر','123','16','0');
+INSERT INTO [t2749] VALUES (36,'الحديث العشرون','127','16','0');
+INSERT INTO [t2749] VALUES (37,'فصل','129','0','0');
+INSERT INTO [t2749] VALUES (38,'ومما يظن أنه مقطوع على مذهب عبد الله الحاكم وغيره وليس كذلك','129','37','0');
+INSERT INTO [t2749] VALUES (39,'حديث آخر','132','37','0');
+INSERT INTO [t2749] VALUES (40,'حديث آخر','132','37','0');
+INSERT INTO [t2749] VALUES (41,'حديث آخر','134','37','0');
+INSERT INTO [t2749] VALUES (42,'حديث آخر','144','37','0');
+INSERT INTO [t2749] VALUES (43,'حديث آخر','144','37','0');
+INSERT INTO [t2749] VALUES (44,'حديث آخر','147','37','0');
+INSERT INTO [t2749] VALUES (45,'حديث آخر','149','37','0');
+INSERT INTO [t2749] VALUES (46,'حديث آخر','150','37','0');
+INSERT INTO [t2749] VALUES (47,'حديث آخر','153','37','0');
+INSERT INTO [t2749] VALUES (48,'حديث آخر','155','37','0');
+INSERT INTO [t2749] VALUES (49,'حديث آخر','158','37','0');
+INSERT INTO [t2749] VALUES (50,'حديث آخر','161','37','0');
+INSERT INTO [t2749] VALUES (51,'حديث آخر','162','37','0');
+INSERT INTO [t2749] VALUES (52,'حديث آخر','167','37','0');
+INSERT INTO [t2749] VALUES (53,'حديث آخر','170','37','0');
+INSERT INTO [t2749] VALUES (54,'حديث آخر','172','37','0');
+INSERT INTO [t2749] VALUES (55,'حديث آخر','188','37','0');
