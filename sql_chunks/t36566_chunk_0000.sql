845cf94ded3363e46bfbe9aa9e9013652d071397
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:16 +0700

    Upload SQL chunk: t36566_chunk_0000.sql

---
+++ sql_chunks/t36566_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t36566
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t36566" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36566
+INSERT INTO [t36566] VALUES (1,'حوار مع المقوقس','1','0','0');
+INSERT INTO [t36566] VALUES (2,'نسبها','2','0','0');
+INSERT INTO [t36566] VALUES (3,'في الطريق إلى المدينة','3','0','0');
+INSERT INTO [t36566] VALUES (4,'في المدينة','4','0','0');
+INSERT INTO [t36566] VALUES (5,'وسيرين','4','0','0');
+INSERT INTO [t36566] VALUES (6,'أم إبراهيم','4','0','0');
+INSERT INTO [t36566] VALUES (7,'مارية الصابرة','5','0','0');
+INSERT INTO [t36566] VALUES (8,'الحكمة الربانية','5','0','0');
+INSERT INTO [t36566] VALUES (9,'الموكب الحزين','6','0','0');
+INSERT INTO [t36566] VALUES (10,'الرسول الأمين','7','0','0');
+INSERT INTO [t36566] VALUES (11,'مارية المؤمنة الصابرة','7','0','0');
+INSERT INTO [t36566] VALUES (12,'بعد النبي - صلى الله عليه وسلم -','8','0','0');
+INSERT INTO [t36566] VALUES (13,'الوفاة','8','0','0');
