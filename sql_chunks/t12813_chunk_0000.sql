9f1aca17eab9f1a591d5a1bcef4c6410f4c2c67d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:07:26 +0700

    Upload SQL chunk: t12813_chunk_0000.sql

---
+++ sql_chunks/t12813_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t12813
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t12813" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12813
+INSERT INTO [t12813] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t12813] VALUES (2,'أخي المسلم','1','0','0');
+INSERT INTO [t12813] VALUES (3,'وقال عامر بن عبد قيس رحمه الله: «ثلاث آيات من كتاب الله عز وجل اكتفيت بهن عن جميع الخلائق.','6','0','0');
+INSERT INTO [t12813] VALUES (4,'أولهن','7','3','0');
+INSERT INTO [t12813] VALUES (5,'والآية الثانية','7','3','0');
+INSERT INTO [t12813] VALUES (6,'والثالثة','7','3','0');
+INSERT INTO [t12813] VALUES (7,'أخي المسلم','7','0','0');
+INSERT INTO [t12813] VALUES (8,'خاتمة','9','0','0');
