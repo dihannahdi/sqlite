d20baeb5bb0a1a7b1a139e050a287fc8a343f71a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:20 +0700

    Upload SQL chunk: t8829_chunk_0000.sql

---
+++ sql_chunks/t8829_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t8829
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t8829" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8829
+INSERT INTO [t8829] VALUES (1,'س١: ماهو تعريف الغزو الفكري في رأيكم؟','1','0','0');
+INSERT INTO [t8829] VALUES (2,'س٢: هل يتعرض العرب عامة والمملكة خاصة لهذا النوع من الغزو؟','2','0','0');
+INSERT INTO [t8829] VALUES (3,'س٣: ماهي الوسائل التي يستخدمها الغرب لترويج أفكاره؟','4','0','0');
