6caf8bd679b7cf64151e58f27a41bb63862e5263
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:11 +0700

    Upload SQL chunk: t21680_chunk_0000.sql

---
+++ sql_chunks/t21680_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21680
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21680" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21680
+INSERT INTO [t21680] VALUES (1,'مسلسلات التيمي','2','0','0');
