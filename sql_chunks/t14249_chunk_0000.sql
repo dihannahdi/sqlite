2f89853bcd9a0d1bae18f6b00ef3ed0aa907b96d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:17:19 +0700

    Upload SQL chunk: t14249_chunk_0000.sql

---
+++ sql_chunks/t14249_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t14249
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t14249" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14249
+INSERT INTO [t14249] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t14249] VALUES (2,'مقدمة لكتاب الاعتقاد معينة لقارئها على المراد','19','0','0');
+INSERT INTO [t14249] VALUES (3,'كتاب أم القواعد، وما انطوت عليه من العقائد','29','0','0');
+INSERT INTO [t14249] VALUES (4,'مقدمة من الأصول معينة في فروعها على الوصول','105','0','0');
+INSERT INTO [t14249] VALUES (5,'كتاب الطهارة','119','0','0');
+INSERT INTO [t14249] VALUES (6,'كتاب الصلاة','229','0','0');
+INSERT INTO [t14249] VALUES (7,'كتاب الزكاة','400','0','0');
+INSERT INTO [t14249] VALUES (8,'كتاب الصيام','445','0','0');
+INSERT INTO [t14249] VALUES (9,'كتاب الحج','488','0','0');
+INSERT INTO [t14249] VALUES (10,'كتاب مباديء التصوف!، وهوادي التعرف','537','0','0');
