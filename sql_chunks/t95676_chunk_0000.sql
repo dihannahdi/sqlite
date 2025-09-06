7922d091a944ce965e9d5c41d3c946690f791fc0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:04 +0700

    Upload SQL chunk: t95676_chunk_0000.sql

---
+++ sql_chunks/t95676_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t95676
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t95676" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95676
+INSERT INTO [t95676] VALUES (1,'من خديجة إلى سودة - رضي الله عنهما -','1','0','0');
+INSERT INTO [t95676] VALUES (2,'بين القبول والرد','2','0','0');
+INSERT INTO [t95676] VALUES (3,'نسبها ونشأتها','3','0','0');
+INSERT INTO [t95676] VALUES (4,'إسلامها','3','0','0');
+INSERT INTO [t95676] VALUES (5,'هجرتها','4','0','0');
+INSERT INTO [t95676] VALUES (6,'العودة من الحبشة','4','0','0');
+INSERT INTO [t95676] VALUES (7,'الأيم','4','0','0');
+INSERT INTO [t95676] VALUES (8,'أم المؤمنين','5','0','0');
+INSERT INTO [t95676] VALUES (9,'يومي لعائشة إذا','6','0','0');
+INSERT INTO [t95676] VALUES (10,'وتمت الخطبة في مكة','6','0','0');
+INSERT INTO [t95676] VALUES (11,'شهادة النبي - صلى الله عليه وسلم - في حق سودة','7','0','0');
+INSERT INTO [t95676] VALUES (12,'المتصدقة الكريمة السخية','8','0','0');
+INSERT INTO [t95676] VALUES (13,'الوفية بالعهد','8','0','0');
+INSERT INTO [t95676] VALUES (14,'المأذون لها من رسول الله - صلى الله عليه وسلم -','8','0','0');
+INSERT INTO [t95676] VALUES (15,'المقسوم لها يوم خيبر','9','0','0');
+INSERT INTO [t95676] VALUES (16,'الوفاة','10','0','0');
