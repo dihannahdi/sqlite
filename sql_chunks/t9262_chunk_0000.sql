2599c9536f37cfdc282d1bed49a8a7c6ed614bef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:52:09 +0700

    Upload SQL chunk: t9262_chunk_0000.sql

---
+++ sql_chunks/t9262_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t9262
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t9262" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9262
+INSERT INTO [t9262] VALUES (1,'المقدمة','4','0','0');
+INSERT INTO [t9262] VALUES (2,'المبحث الأول الأحاديث الواردة في الطائفة الظاهرة','12','0','0');
+INSERT INTO [t9262] VALUES (3,'المبحث الثاني تحريف المراد بأحاديث الطائفة الظاهرة','18','0','0');
+INSERT INTO [t9262] VALUES (4,'المبحث الثالث محل وجود الطائفة الظاهرة','31','0','0');
+INSERT INTO [t9262] VALUES (5,'المبحث الرابع بطلان ما ادعاه الكاتب أن الغرباء هم حزب التحرير فقط','45','0','0');
+INSERT INTO [t9262] VALUES (6,'الخاتمة منهاج الفرقة الناجية','56','0','0');
+INSERT INTO [t9262] VALUES (7,'قائمة المصادر','61','0','0');
