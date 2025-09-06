34779a630356437ca9550dcbca7b27b94ab14aa8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:52 +0700

    Upload SQL chunk: t11071_chunk_0000.sql

---
+++ sql_chunks/t11071_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11071
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11071" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11071
+INSERT INTO [t11071] VALUES (1,'بغية المرتاد في الرد على المتفلسفة والقرامطة والباطنية','1','0','0');
+INSERT INTO [t11071] VALUES (2,'لشيخ الإسلام ابن تيمية','1','0','0');
