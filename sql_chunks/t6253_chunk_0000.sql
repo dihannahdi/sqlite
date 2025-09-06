428db6202b9c68fcea8858ebaa20d327b8d0ff6f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:29:13 +0700

    Upload SQL chunk: t6253_chunk_0000.sql

---
+++ sql_chunks/t6253_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6253
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6253" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6253
+INSERT INTO [t6253] VALUES (1,'مسألة','1','0','0');
+INSERT INTO [t6253] VALUES (2,'الجواب','1','0','0');
