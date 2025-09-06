6d3fa1e527c9d14c488272b246e8e647839645cf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:49 +0700

    Upload SQL chunk: t1217_chunk_0000.sql

---
+++ sql_chunks/t1217_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t1217
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t1217" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1217
+INSERT INTO [t1217] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t1217] VALUES (2,'باب الألف','6','0','0');
+INSERT INTO [t1217] VALUES (3,'باب الباء','22','0','0');
+INSERT INTO [t1217] VALUES (4,'باب التاء','41','0','0');
+INSERT INTO [t1217] VALUES (5,'باب الثاء','51','0','0');
+INSERT INTO [t1217] VALUES (6,'باب الجيم','57','0','0');
+INSERT INTO [t1217] VALUES (7,'باب الحاء','74','0','0');
+INSERT INTO [t1217] VALUES (8,'باب الخاء','95','0','0');
+INSERT INTO [t1217] VALUES (9,'باب الدال','109','0','0');
+INSERT INTO [t1217] VALUES (10,'باب الذال','121','0','0');
+INSERT INTO [t1217] VALUES (11,'باب الراء','125','0','0');
+INSERT INTO [t1217] VALUES (12,'باب الزاي','140','0','0');
+INSERT INTO [t1217] VALUES (13,'باب السين','147','0','0');
+INSERT INTO [t1217] VALUES (14,'باب الشين','189','0','0');
+INSERT INTO [t1217] VALUES (15,'باب الصاد','205','0','0');
+INSERT INTO [t1217] VALUES (16,'باب الضاد','219','0','0');
+INSERT INTO [t1217] VALUES (17,'باب الطاء','223','0','0');
+INSERT INTO [t1217] VALUES (18,'باب الظاء','230','0','0');
+INSERT INTO [t1217] VALUES (19,'باب العين','232','0','0');
+INSERT INTO [t1217] VALUES (20,'باب الغين.','252','0','0');
+INSERT INTO [t1217] VALUES (21,'باب الفاء.','258','0','0');
+INSERT INTO [t1217] VALUES (22,'باب القاف','269','0','0');
+INSERT INTO [t1217] VALUES (23,'باب اللام','301','0','0');
+INSERT INTO [t1217] VALUES (24,'باب الميم','310','0','0');
+INSERT INTO [t1217] VALUES (25,'باب النون.','347','0','0');
+INSERT INTO [t1217] VALUES (26,'باب الهاء.','360','0','0');
+INSERT INTO [t1217] VALUES (27,'باب الواو.','364','0','0');
+INSERT INTO [t1217] VALUES (28,'باب الياء','372','0','0');
+INSERT INTO [t1217] VALUES (29,'[خاتمة المؤلف]','375','0','0');
