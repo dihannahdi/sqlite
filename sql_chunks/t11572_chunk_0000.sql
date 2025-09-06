64e2d725458a0dc364d9f4faeb0d462614b8461a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:37 +0700

    Upload SQL chunk: t11572_chunk_0000.sql

---
+++ sql_chunks/t11572_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11572
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11572" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11572
+INSERT INTO [t11572] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11572] VALUES (2,'مسائل لخصها الإمام الشيخ محمد بن عبد الوهاب من كلام شيخ الإسلام ابن تيمية','5','0','0');
