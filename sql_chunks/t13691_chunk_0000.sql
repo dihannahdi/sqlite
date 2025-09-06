ae7aab94137573f472adb11027465761047cfeef
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:19 +0700

    Upload SQL chunk: t13691_chunk_0000.sql

---
+++ sql_chunks/t13691_chunk_0000.sql
@@ -0,0 +1,46 @@
+-- Table: t13691
+-- Chunk: 1
+-- Rows: 39
+
+CREATE TABLE "t13691" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13691
+INSERT INTO [t13691] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t13691] VALUES (2,'عام جديد','4','0','0');
+INSERT INTO [t13691] VALUES (3,'السعادة','13','0','0');
+INSERT INTO [t13691] VALUES (4,'تسعة قروش','23','0','0');
+INSERT INTO [t13691] VALUES (5,'القبر التائه','31','0','0');
+INSERT INTO [t13691] VALUES (6,'في الليل','39','0','0');
+INSERT INTO [t13691] VALUES (7,'إلى أخي النازح إلى باريس','47','0','0');
+INSERT INTO [t13691] VALUES (8,'اعرف نفسك!','56','0','0');
+INSERT INTO [t13691] VALUES (9,'مجانين','62','0','0');
+INSERT INTO [t13691] VALUES (10,'بيني وبين نفسي','73','0','0');
+INSERT INTO [t13691] VALUES (11,'شهيد العيد','81','0','0');
+INSERT INTO [t13691] VALUES (12,'أعرابي في حمام','87','0','0');
+INSERT INTO [t13691] VALUES (13,'أعرابي في سينما','97','0','0');
+INSERT INTO [t13691] VALUES (14,'الأعرابي والشعر','106','0','0');
+INSERT INTO [t13691] VALUES (15,'هيكل عظمي','118','0','0');
+INSERT INTO [t13691] VALUES (16,'في الترام','125','0','0');
+INSERT INTO [t13691] VALUES (17,'مرثية غراي','134','0','0');
+INSERT INTO [t13691] VALUES (18,'بين البهائم والوحوش','142','0','0');
+INSERT INTO [t13691] VALUES (19,'لا أؤمن بالإنسان','150','0','0');
+INSERT INTO [t13691] VALUES (20,'حكمة القدر','156','0','0');
+INSERT INTO [t13691] VALUES (21,'بين الطبيعة والله','164','0','0');
+INSERT INTO [t13691] VALUES (22,'وحي صورة','179','0','0');
+INSERT INTO [t13691] VALUES (23,'يا بنتي','192','0','0');
+INSERT INTO [t13691] VALUES (24,'يا ابني','204','0','0');
+INSERT INTO [t13691] VALUES (25,'رمضان','214','0','0');
+INSERT INTO [t13691] VALUES (26,'طبقات الأصدقاء','221','0','0');
+INSERT INTO [t13691] VALUES (27,'أنا والإذاعة','227','0','0');
+INSERT INTO [t13691] VALUES (28,'يوم مع الشيطان','236','0','0');
+INSERT INTO [t13691] VALUES (29,'نداء إلى أدباء مصر','261','0','0');
+INSERT INTO [t13691] VALUES (30,'نحن المذنبون','268','0','0');
+INSERT INTO [t13691] VALUES (31,'في الحب','278','0','0');
+INSERT INTO [t13691] VALUES (32,'ديوان الأصمعي','293','0','0');
+INSERT INTO [t13691] VALUES (33,'زورق الأحلام','313','0','0');
+INSERT INTO [t13691] VALUES (34,'رقم مكسور','323','0','0');
+INSERT INTO [t13691] VALUES (35,'وقفة على طلل','331','0','0');
+INSERT INTO [t13691] VALUES (36,'فلسفة العيد','342','0','0');
+INSERT INTO [t13691] VALUES (37,'كتاب تعزية','347','0','0');
+INSERT INTO [t13691] VALUES (38,'فن الكلام','353','0','0');
+INSERT INTO [t13691] VALUES (39,'من آثار المؤلف','361','0','0');
