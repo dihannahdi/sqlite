1d5e40c4db33dda718764f20bb7d3baabebb19b7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:13 +0700

    Upload SQL chunk: t11526_chunk_0000.sql

---
+++ sql_chunks/t11526_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t11526
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t11526" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11526
+INSERT INTO [t11526] VALUES (1,'تنبيه','5','0','0');
+INSERT INTO [t11526] VALUES (2,'مدخل','6','0','0');
+INSERT INTO [t11526] VALUES (3,'الفصل الأول','11','0','0');
+INSERT INTO [t11526] VALUES (4,'الفصل الثاني','34','0','0');
+INSERT INTO [t11526] VALUES (5,'الفصل الثالث','61','0','0');
+INSERT INTO [t11526] VALUES (6,'الفصل الرابع','96','0','0');
+INSERT INTO [t11526] VALUES (7,'الفصل الخامس','109','0','0');
+INSERT INTO [t11526] VALUES (8,'الفصل السادس','121','0','0');
+INSERT INTO [t11526] VALUES (9,'ملخص','130','0','0');
