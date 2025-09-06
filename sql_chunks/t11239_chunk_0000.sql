f6a7023daa7f72c1355a715387f609eeb7316d66
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:54:49 +0700

    Upload SQL chunk: t11239_chunk_0000.sql

---
+++ sql_chunks/t11239_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t11239
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t11239" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11239
+INSERT INTO [t11239] VALUES (1,'خطبة الكتاب وبيان الخفاجي','1','0','0');
+INSERT INTO [t11239] VALUES (2,'فصل في الأصوات','4','0','0');
+INSERT INTO [t11239] VALUES (3,'فصل في الحروف','12','0','0');
+INSERT INTO [t11239] VALUES (4,'فصل في الكلام','21','0','0');
+INSERT INTO [t11239] VALUES (5,'فصل في اللغة','38','0','0');
+INSERT INTO [t11239] VALUES (6,'الكلام في الفصاحة','49','0','0');
+INSERT INTO [t11239] VALUES (7,'الكلام في الألفاظ المؤلفة','84','0','0');
+INSERT INTO [t11239] VALUES (8,'الكلام في المعاني مفردة','227','0','0');
+INSERT INTO [t11239] VALUES (9,'فصل في ذكر الأقوال الفاسدة في نقد الكلام','272','0','0');
+INSERT INTO [t11239] VALUES (10,'فصل في ذكر الفرق بين المنظوم والمنثور وما يقال في تفضيل أحدهما على الآخر','280','0','0');
+INSERT INTO [t11239] VALUES (11,'فصل فيما يحتاج مؤلف الكلام إلى معرفته','283','0','0');
