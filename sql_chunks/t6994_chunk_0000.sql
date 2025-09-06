fff72dbcfbc235d647c439ee9a3b4ba110836aac
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:37 +0700

    Upload SQL chunk: t6994_chunk_0000.sql

---
+++ sql_chunks/t6994_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t6994
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t6994" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6994
+INSERT INTO [t6994] VALUES (1,'تم الكتاب بحمد الله وعونه','8','0','0');
