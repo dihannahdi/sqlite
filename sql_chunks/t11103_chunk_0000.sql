9aaced158005d2571663def8127d404950fc9855
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:19 +0700

    Upload SQL chunk: t11103_chunk_0000.sql

---
+++ sql_chunks/t11103_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t11103
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t11103" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11103
+INSERT INTO [t11103] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11103] VALUES (2,'فضل تعلم القرآن الكريم وتعليمه','3','0','0');
+INSERT INTO [t11103] VALUES (3,'أهداف الحلقات','6','0','0');
+INSERT INTO [t11103] VALUES (4,'أسلوب التعليم في الحلقات القرآنية','8','0','0');
+INSERT INTO [t11103] VALUES (5,'أخطاء في أساليب التعليم في الحلقات القرآنية','10','0','0');
+INSERT INTO [t11103] VALUES (6,'الأسلوب الأمثل لتعليم القرآن في الحلقات القرآنية','17','0','0');
+INSERT INTO [t11103] VALUES (7,'الخاتمة','26','0','0');
+INSERT INTO [t11103] VALUES (8,'اقتراحات','27','0','0');
+INSERT INTO [t11103] VALUES (9,'مصادر ومراجع','29','0','0');
