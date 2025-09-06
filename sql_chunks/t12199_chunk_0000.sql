b305092b2e8b53357ccbe5ad8fa16ffd7bd19535
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:52 +0700

    Upload SQL chunk: t12199_chunk_0000.sql

---
+++ sql_chunks/t12199_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t12199
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t12199" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12199
+INSERT INTO [t12199] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t12199] VALUES (2,'مشروعية الصيام','4','0','0');
+INSERT INTO [t12199] VALUES (3,'آداب الصيام وأحكامه','15','0','0');
