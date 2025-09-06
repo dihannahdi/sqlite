85d2599d5c87073869cf05eb5918fac5c4fe63d0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:10:06 +0700

    Upload SQL chunk: t13078_chunk_0000.sql

---
+++ sql_chunks/t13078_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t13078
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t13078" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13078
+INSERT INTO [t13078] VALUES (1,'باب الورع في النظر','59','0','0');
+INSERT INTO [t13078] VALUES (2,'باب الورع في السمع','80','0','0');
+INSERT INTO [t13078] VALUES (3,'باب الورع في الشم','89','0','0');
+INSERT INTO [t13078] VALUES (4,'باب الورع في اللسان','94','0','0');
+INSERT INTO [t13078] VALUES (5,'باب الورع في البطش','111','0','0');
+INSERT INTO [t13078] VALUES (6,'باب الورع في البطن','120','0','0');
+INSERT INTO [t13078] VALUES (7,'باب الورع في الفرج','139','0','0');
+INSERT INTO [t13078] VALUES (8,'باب الورع في المسعى','146','0','0');
+INSERT INTO [t13078] VALUES (9,'باب أخبار الورعين','155','0','0');
+INSERT INTO [t13078] VALUES (10,'باب الورع في الشراء والبيع','171','0','0');
+INSERT INTO [t13078] VALUES (11,'باب ثواب الورعين','191','0','0');
+INSERT INTO [t13078] VALUES (12,'باب في الورعين','199','0','0');
