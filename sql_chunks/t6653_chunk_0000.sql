8226754df8c9f425d5d90aa9287ae7229dc2ebbb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:29 +0700

    Upload SQL chunk: t6653_chunk_0000.sql

---
+++ sql_chunks/t6653_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6653
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6653" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6653
+INSERT INTO [t6653] VALUES (1,'ابن تيمية','1','0','0');
+INSERT INTO [t6653] VALUES (2,'البرزالي','8','0','0');
+INSERT INTO [t6653] VALUES (3,'المزي','16','0','0');
