71661efb64c9be48e107854cf3a19b6ca619fc7b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:01:38 +0700

    Upload SQL chunk: t97806_chunk_0000.sql

---
+++ sql_chunks/t97806_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t97806
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t97806" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t97806
+INSERT INTO [t97806] VALUES (1,'المقدمة','4','0','0');
+INSERT INTO [t97806] VALUES (2,'• توظيفه التحريف لنص مقدمة الرسالة','12','1','0');
+INSERT INTO [t97806] VALUES (3,'• توظيفه التحريف لمعانيها بصرفها عن طريقة السلف','17','1','0');
+INSERT INTO [t97806] VALUES (4,'مقدمة ابن أبي زيد القيرواني','50','0','0');
+INSERT INTO [t97806] VALUES (5,'عقيدة ابن أبي زيد كما نظمها الشيخ أحمد بن مشرف الأحسائي','60','0','0');
+INSERT INTO [t97806] VALUES (6,'باب ما تعتقده القلوب وتنطق به الألسن من واجب أمور الديانات','60','5','0');
+INSERT INTO [t97806] VALUES (7,'فصل فى الايمان بالقدر خيره وشره','62','5','0');
+INSERT INTO [t97806] VALUES (8,'فصل فى عذاب القبر وفتنته','62','5','0');
+INSERT INTO [t97806] VALUES (9,'فصل فى البعث بعد الموت والجزاء','62','5','0');
+INSERT INTO [t97806] VALUES (10,'فصل فى الإيمان بالحوض','64','5','0');
