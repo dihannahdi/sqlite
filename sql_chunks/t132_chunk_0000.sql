ebd3df1bafe9d5da44fc4009eb73b084722b6a43
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:13:33 +0700

    Upload SQL chunk: t132_chunk_0000.sql

---
+++ sql_chunks/t132_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t132
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t132" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t132
+INSERT INTO [t132] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t132] VALUES (2,'معنى العلم','1','0','0');
