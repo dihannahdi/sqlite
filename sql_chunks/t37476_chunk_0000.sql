9d3980a904326f1f747748222521bedef9897d85
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:20 +0700

    Upload SQL chunk: t37476_chunk_0000.sql

---
+++ sql_chunks/t37476_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t37476
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t37476" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37476
+INSERT INTO [t37476] VALUES (1,'الفصل الأول إلغاء العوامل','8','0','0');
+INSERT INTO [t37476] VALUES (2,'الفصل الثاني تطبيقه للنحو من غير عامل في باب التنازع','24','0','0');
+INSERT INTO [t37476] VALUES (3,'الفصل الثالث تطبيقه للنحو من غير عامل في باب الاشتغال','33','0','0');
+INSERT INTO [t37476] VALUES (4,'تطبيقه في باب نواصب المضارع','53','3','0');
+INSERT INTO [t37476] VALUES (5,'الفصل الرابع إسقاط العلل الثواني والثوالث','64','0','0');
+INSERT INTO [t37476] VALUES (6,'الفصل الخامس إسقاط التمارين','72','0','0');
