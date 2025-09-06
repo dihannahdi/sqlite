7e02516a3f7d158792bc7b08c80523949b71bb79
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:27 +0700

    Upload SQL chunk: t22992_chunk_0000.sql

---
+++ sql_chunks/t22992_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t22992
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t22992" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22992
+INSERT INTO [t22992] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t22992] VALUES (2,'صيغة (أفعل) بين العربية وأخواتها في السامية','3','0','0');
+INSERT INTO [t22992] VALUES (3,'معنى التعدية وأثرها','7','0','0');
+INSERT INTO [t22992] VALUES (4,'أفعل بمعنى فعل عند أهل اللغة','19','0','0');
+INSERT INTO [t22992] VALUES (5,'أراء العلماء في فعل وأفعل بمعنى واحد','31','0','0');
+INSERT INTO [t22992] VALUES (6,'رأي ابن قتيبة','34','0','0');
+INSERT INTO [t22992] VALUES (7,'أفعال ينطقها العامة بالهمزة والفصيح بدونه','39','0','0');
+INSERT INTO [t22992] VALUES (8,'تعقيب على بعض المواد:','46','0','0');
+INSERT INTO [t22992] VALUES (9,'وقف وأوقف','48','0','0');
+INSERT INTO [t22992] VALUES (10,'غلقت وأغلقت','50','0','0');
+INSERT INTO [t22992] VALUES (11,'عتق وأعتق','52','0','0');
+INSERT INTO [t22992] VALUES (12,'منى وأمنى','53','0','0');
+INSERT INTO [t22992] VALUES (13,'أيديت ويديت','54','0','0');
+INSERT INTO [t22992] VALUES (14,'غلى وأغلى','56','0','0');
+INSERT INTO [t22992] VALUES (15,'غفيت وأغفيت','57','0','0');
+INSERT INTO [t22992] VALUES (16,'أولعت بالشيء','59','0','0');
+INSERT INTO [t22992] VALUES (17,'أديربي','60','0','0');
+INSERT INTO [t22992] VALUES (18,'غاظ وأغاظ','62','0','0');
+INSERT INTO [t22992] VALUES (19,'صرف وأصرف','63','0','0');
