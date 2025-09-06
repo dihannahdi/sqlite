9415078bdffd7169425b61ad38f01a9bb874f186
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:41:08 +0700

    Upload SQL chunk: t7650_chunk_0000.sql

---
+++ sql_chunks/t7650_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t7650
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t7650" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7650
+INSERT INTO [t7650] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7650] VALUES (2,'الباب الأول','2','0','0');
+INSERT INTO [t7650] VALUES (3,'في مبتدأ التاريخ','2','2','0');
+INSERT INTO [t7650] VALUES (4,'ذكر مبدأ التاريخ الهجري','6','2','0');
+INSERT INTO [t7650] VALUES (5,'الباب الثاني','12','0','0');
+INSERT INTO [t7650] VALUES (6,'في فوائد التاريخ','12','5','0');
+INSERT INTO [t7650] VALUES (7,'الباب الثالث','14','0','0');
+INSERT INTO [t7650] VALUES (8,'في فوائد شتى تتعلق به','14','7','0');
