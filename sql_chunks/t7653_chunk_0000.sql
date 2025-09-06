4dfa4306dd6faafb9322fc14cf339c8ea18bfa94
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:41:10 +0700

    Upload SQL chunk: t7653_chunk_0000.sql

---
+++ sql_chunks/t7653_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7653
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7653" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7653
+INSERT INTO [t7653] VALUES (1,'الشيخ عمر بن محمد فلاته رحمه الله وكيف عرفته','1','0','0');
+INSERT INTO [t7653] VALUES (2,'محاضرة ألقاها: عبد المحسن بن حمد العباد البدر','1','0','0');
