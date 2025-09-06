a6ba5f45e83c6c3cbd5e303a9c1474a747c5b93f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:56 +0700

    Upload SQL chunk: t6935_chunk_0000.sql

---
+++ sql_chunks/t6935_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t6935
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t6935" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6935
+INSERT INTO [t6935] VALUES (1,'الإتباع للسيوطي','1','0','0');
+INSERT INTO [t6935] VALUES (2,'في الشبرم إنه حار يار وقال الكسائي حار من الحرارة ويار إتباع كقولهم عطشان نطشان وجائع نائع وحسن بسن ومثله كثير في الكلام وإنما سمى اتباعا لأن الكلمة الثانية إنما هي تابعة للأولى على وجه التوكيد لها وليس يتكلم بالثانية','1','0','0');
+INSERT INTO [t6935] VALUES (3,'ذكر أمثلة من الاتباع','3','0','0');
