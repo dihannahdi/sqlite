57cea17672866d8bf005c0e2b8cc7c3a009c2c7e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:35 +0700

    Upload SQL chunk: t9984_chunk_0000.sql

---
+++ sql_chunks/t9984_chunk_0000.sql
@@ -0,0 +1,48 @@
+-- Table: t9984
+-- Chunk: 1
+-- Rows: 41
+
+CREATE TABLE "t9984" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9984
+INSERT INTO [t9984] VALUES (1,'مقدمات','1','0','0');
+INSERT INTO [t9984] VALUES (2,'بين يدي الكتاب','1','1','0');
+INSERT INTO [t9984] VALUES (3,'عملي في هذه الطبعة','2','1','0');
+INSERT INTO [t9984] VALUES (4,'مدخل في مدارس النحو وأعلامها','3','1','0');
+INSERT INTO [t9984] VALUES (5,'مؤلف الكتاب','7','1','0');
+INSERT INTO [t9984] VALUES (6,'مصادر ترجمة الفيروزابادي','37','1','0');
+INSERT INTO [t9984] VALUES (7,'التأليف في التراجم','39','1','0');
+INSERT INTO [t9984] VALUES (8,'كتاب البلغة','40','0','0');
+INSERT INTO [t9984] VALUES (9,'مدخل','40','8','0');
+INSERT INTO [t9984] VALUES (10,'التحقيق','43','8','0');
+INSERT INTO [t9984] VALUES (11,'حرف الألف','45','8','0');
+INSERT INTO [t9984] VALUES (12,'حرف الباء','83','8','0');
+INSERT INTO [t9984] VALUES (13,'حرف التاء','87','8','0');
+INSERT INTO [t9984] VALUES (14,'حرف الثاء','88','8','0');
+INSERT INTO [t9984] VALUES (15,'حرف الجيم','90','8','0');
+INSERT INTO [t9984] VALUES (16,'حرف الحاء','93','8','0');
+INSERT INTO [t9984] VALUES (17,'حرف الخاء','118','8','0');
+INSERT INTO [t9984] VALUES (18,'حرف الدال','122','8','0');
+INSERT INTO [t9984] VALUES (19,'حرف الراء','123','8','0');
+INSERT INTO [t9984] VALUES (20,'حرف الزاي','124','8','0');
+INSERT INTO [t9984] VALUES (21,'حرف السين','127','8','0');
+INSERT INTO [t9984] VALUES (22,'حرف الشين','137','8','0');
+INSERT INTO [t9984] VALUES (23,'حرف الصاد','139','8','0');
+INSERT INTO [t9984] VALUES (24,'حرف الضاد','142','8','0');
+INSERT INTO [t9984] VALUES (25,'حرف الطاء','143','8','0');
+INSERT INTO [t9984] VALUES (26,'حرف العين','145','8','0');
+INSERT INTO [t9984] VALUES (27,'حرف الغين','211','8','0');
+INSERT INTO [t9984] VALUES (28,'حرف الفاء','213','8','0');
+INSERT INTO [t9984] VALUES (29,'حرف القاف','215','8','0');
+INSERT INTO [t9984] VALUES (30,'حرف الكاف','220','8','0');
+INSERT INTO [t9984] VALUES (31,'حرف اللام','221','8','0');
+INSERT INTO [t9984] VALUES (32,'حرف الميم','224','8','0');
+INSERT INTO [t9984] VALUES (33,'حرف النون','282','8','0');
+INSERT INTO [t9984] VALUES (34,'حرف الهاء','285','8','0');
+INSERT INTO [t9984] VALUES (35,'حرف الواو','288','8','0');
+INSERT INTO [t9984] VALUES (36,'حرف الياء','289','8','0');
+INSERT INTO [t9984] VALUES (37,'الخاتمة','301','0','0');
+INSERT INTO [t9984] VALUES (38,'الفهرس','302','0','0');
+INSERT INTO [t9984] VALUES (39,'فهرس التراجم','302','38','0');
+INSERT INTO [t9984] VALUES (40,'مراجع البحث والتحقيق','329','38','0');
+INSERT INTO [t9984] VALUES (41,'فهرس الكتاب','337','38','0');
