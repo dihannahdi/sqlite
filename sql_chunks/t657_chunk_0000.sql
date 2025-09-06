142c6e1a8074a746ddf0711d0205564868e4fd28
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:31:44 +0700

    Upload SQL chunk: t657_chunk_0000.sql

---
+++ sql_chunks/t657_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t657
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t657" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t657
+INSERT INTO [t657] VALUES (1,'صفة صلاة النبي','1','0','0');
