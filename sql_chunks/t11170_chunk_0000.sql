4ae48171db360b8ce6c6d67e1aad936159d26c17
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:56 +0700

    Upload SQL chunk: t11170_chunk_0000.sql

---
+++ sql_chunks/t11170_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t11170
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t11170" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11170
+INSERT INTO [t11170] VALUES (1,'مجدد القرن الثاني عشر','1','0','0');
+INSERT INTO [t11170] VALUES (2,'ولادته ونشأته','3','0','0');
+INSERT INTO [t11170] VALUES (3,'مشايخ الإمام محمد بن عبد الوهاب','5','0','0');
+INSERT INTO [t11170] VALUES (4,'من ثناء أهل العلم على الإمام محمد بن عبد الوهاب','15','0','0');
+INSERT INTO [t11170] VALUES (5,'تلامذة الشيخ محمد بن عبد الوهاب','24','0','0');
+INSERT INTO [t11170] VALUES (6,'مصنفات الإمام محمد بن عبد الوهاب','29','0','0');
+INSERT INTO [t11170] VALUES (7,'دعوة الإمام محمد بن عبد الوهاب الى ما عليه السلف الصالح','32','0','0');
+INSERT INTO [t11170] VALUES (8,'من آثار دعوة الإمام محمد بن عبد الوهاب','35','0','0');
+INSERT INTO [t11170] VALUES (9,'وفاة الإمام محمد بن عبد الوهاب','37','0','0');
