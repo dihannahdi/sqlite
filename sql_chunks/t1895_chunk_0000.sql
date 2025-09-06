00dac569f6df9d45b5012ed284fb9b134e50c3fc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:51 +0700

    Upload SQL chunk: t1895_chunk_0000.sql

---
+++ sql_chunks/t1895_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t1895
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t1895" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1895
+INSERT INTO [t1895] VALUES (1,'الشريط الأول','2','0','0');
+INSERT INTO [t1895] VALUES (2,'الشريط الثاني','12','0','0');
+INSERT INTO [t1895] VALUES (3,'الشريط الثالث','32','0','0');
+INSERT INTO [t1895] VALUES (4,'الشريط الرابع','56','0','0');
+INSERT INTO [t1895] VALUES (5,'الشريط الخامس','80','0','0');
