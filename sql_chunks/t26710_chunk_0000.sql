c67e0e30d8237098986bbeaabc829f8f3d76db35
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:15 +0700

    Upload SQL chunk: t26710_chunk_0000.sql

---
+++ sql_chunks/t26710_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t26710
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t26710" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26710
+INSERT INTO [t26710] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26710] VALUES (2,'صفية بنت عبد المطلب','3','0','0');
+INSERT INTO [t26710] VALUES (3,'أم عمارة نسيبة بنت كعب','5','0','0');
