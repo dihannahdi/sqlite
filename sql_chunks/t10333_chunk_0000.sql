e4b249181321a29a465194f576b6eb93d47f1790
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:06 +0700

    Upload SQL chunk: t10333_chunk_0000.sql

---
+++ sql_chunks/t10333_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10333
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10333" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10333
+INSERT INTO [t10333] VALUES (1,'استمرار الدعوة المسلمين:','1','0','0');
+INSERT INTO [t10333] VALUES (2,'القوةو والإستعداد والآت الحرب من وسائل النصر','7','0','0');
+INSERT INTO [t10333] VALUES (3,'المستشرقون والدعوة الإسلامية','12','0','0');
