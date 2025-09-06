d8e0a45474199e7b9517c7775e4ea6bc4885f450
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:11 +0700

    Upload SQL chunk: t9548_chunk_0000.sql

---
+++ sql_chunks/t9548_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t9548
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t9548" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9548
+INSERT INTO [t9548] VALUES (1,'أول ما يجب على المكلف','2','0','0');
+INSERT INTO [t9548] VALUES (2,'فصل في الطهارة','4','0','0');
+INSERT INTO [t9548] VALUES (3,'فصل في التيمم','8','0','0');
+INSERT INTO [t9548] VALUES (4,'فصل في الحيض','9','0','0');
+INSERT INTO [t9548] VALUES (5,'فصل في النفاس','10','0','0');
+INSERT INTO [t9548] VALUES (6,'فصل في الأوقات','10','0','0');
+INSERT INTO [t9548] VALUES (7,'فصل في شروط الصلاة','11','0','0');
+INSERT INTO [t9548] VALUES (8,'باب في السهو','16','0','0');
