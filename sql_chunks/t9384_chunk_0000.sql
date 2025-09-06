4aeb1a566054b067a4b8607fbde9e442e58b047c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:53:18 +0700

    Upload SQL chunk: t9384_chunk_0000.sql

---
+++ sql_chunks/t9384_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t9384
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t9384" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9384
+INSERT INTO [t9384] VALUES (1,'الرقة والبكاء لابن قدامة','1','0','0');
+INSERT INTO [t9384] VALUES (2,'الفصل الأول','1','0','0');
+INSERT INTO [t9384] VALUES (3,'ذكر طرف من صفاتهم','1','0','0');
+INSERT INTO [t9384] VALUES (4,'الفصل الثاني','16','0','0');
+INSERT INTO [t9384] VALUES (5,'ذكر طرف من أخبار الأنبياء عليهم السلام آدم عليه السلام','16','0','0');
+INSERT INTO [t9384] VALUES (6,'إبراهيم عليه السلام','21','5','0');
+INSERT INTO [t9384] VALUES (7,'داود عليه السلام','22','5','0');
+INSERT INTO [t9384] VALUES (8,'يحيى عليه السلام','30','5','0');
+INSERT INTO [t9384] VALUES (9,'زكريا عليه السلام','32','5','0');
+INSERT INTO [t9384] VALUES (10,'أيوب عليه السلام','33','5','0');
+INSERT INTO [t9384] VALUES (11,'الذبيح عليه السلام','41','5','0');
+INSERT INTO [t9384] VALUES (12,'عيسى عليه السلام','45','5','0');
+INSERT INTO [t9384] VALUES (13,'الفصل الثالث','57','0','0');
+INSERT INTO [t9384] VALUES (14,'ذكر طرف من أخبار نبينا','57','0','0');
+INSERT INTO [t9384] VALUES (15,'ذكر وفاة رسول الله صلى الله عليه وسلم','87','0','0');
