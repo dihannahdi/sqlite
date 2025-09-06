3c59648bfa870e11882f4abfdfc2112d02bb9e85
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:03 +0700

    Upload SQL chunk: t9527_chunk_0000.sql

---
+++ sql_chunks/t9527_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t9527
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t9527" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9527
+INSERT INTO [t9527] VALUES (1,'خطبة الرسالة','2','0','0');
+INSERT INTO [t9527] VALUES (2,'فصل: تعريف أصول الفقه','4','0','0');
+INSERT INTO [t9527] VALUES (3,'فصل: الأحكام التي يدور عليها الفقه','7','0','0');
+INSERT INTO [t9527] VALUES (4,'فصل: الأدلة التي يستمد منها الفقه','13','0','0');
+INSERT INTO [t9527] VALUES (5,'فصل فى الكتاب والسنة ودلالتهما','15','0','0');
+INSERT INTO [t9527] VALUES (6,'فصل: أصول يضطر إليها الفقيه','23','0','0');
+INSERT INTO [t9527] VALUES (7,'فصل: عودة إلى نصوص الوحيين','29','0','0');
+INSERT INTO [t9527] VALUES (8,'فصل الإجماع والقياس الصحيح','38','0','0');
+INSERT INTO [t9527] VALUES (9,'فصل أصول مستنبطة من الكتاب والسنة','43','0','0');
+INSERT INTO [t9527] VALUES (10,'فصل: قول الصحابي وحجيته','50','0','0');
+INSERT INTO [t9527] VALUES (11,'فصل: الأمر والنهي، ألفاظ العموم، الاجتهاد والتقليد','51','0','0');
