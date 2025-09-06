489843d679e2cf3efcb4779aebf4b5258415aba2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:59 +0700

    Upload SQL chunk: t6481_chunk_0000.sql

---
+++ sql_chunks/t6481_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t6481
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t6481" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6481
+INSERT INTO [t6481] VALUES (1,'مقدمة','4','0','0');
+INSERT INTO [t6481] VALUES (2,'الجواب الأول','7','0','0');
+INSERT INTO [t6481] VALUES (3,'الجواب الثاني','15','0','0');
+INSERT INTO [t6481] VALUES (4,'الجواب الثالث','17','0','0');
+INSERT INTO [t6481] VALUES (5,'الجواب الرابع','23','0','0');
+INSERT INTO [t6481] VALUES (6,'الجواب الرابع','24','0','0');
+INSERT INTO [t6481] VALUES (7,'الجواب الخامس','46','0','0');
+INSERT INTO [t6481] VALUES (8,'خاتمة','48','0','0');
