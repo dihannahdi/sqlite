e947699024d33ad3e5635c1dd7540963eb7e743a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:26 +0700

    Upload SQL chunk: t21703_chunk_0000.sql

---
+++ sql_chunks/t21703_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21703
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21703" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21703
+INSERT INTO [t21703] VALUES (1,'منتقى من حديث الجصاص والحنائي لأحمد بن عبد الدائم المقدسي - مخطوط','2','0','0');
