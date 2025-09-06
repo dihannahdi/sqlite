ddbfc92a7c125518293259db0397e6e6d46781b5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:37 +0700

    Upload SQL chunk: t23067_chunk_0000.sql

---
+++ sql_chunks/t23067_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t23067
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t23067" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23067
+INSERT INTO [t23067] VALUES (1,'تنزيه الأصحاب عن تنقص أبي تراب','1','0','0');
+INSERT INTO [t23067] VALUES (2,'وهذا آخر ما تيسر إيراده','39','0','0');
