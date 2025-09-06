b715e03f18e2853fcc783eb4492bf60e31f890f8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:03 +0700

    Upload SQL chunk: t95675_chunk_0000.sql

---
+++ sql_chunks/t95675_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t95675
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t95675" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95675
+INSERT INTO [t95675] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t95675] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t95675] VALUES (3,'أسباب الرزق','9','0','0');
+INSERT INTO [t95675] VALUES (4,'كثرة المال ونتاجه','30','0','0');
+INSERT INTO [t95675] VALUES (5,'فوائد المال','34','0','0');
+INSERT INTO [t95675] VALUES (6,'ترك التجارة للعبادة','48','0','0');
+INSERT INTO [t95675] VALUES (7,'بيان علاج الحرص والطمع والدواء الذي تكتسب به صفة القناعة','55','0','0');
