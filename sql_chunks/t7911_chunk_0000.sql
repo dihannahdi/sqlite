3424b3ced88ad2e4c1b6890d592125e8702d1480
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:39 +0700

    Upload SQL chunk: t7911_chunk_0000.sql

---
+++ sql_chunks/t7911_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t7911
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t7911" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7911
+INSERT INTO [t7911] VALUES (1,'حديث الدراج','1','0','0');
