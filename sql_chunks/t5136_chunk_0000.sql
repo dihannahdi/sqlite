4ce84f27d577b7c5ac2bd45dd4efa0ee03d7bc35
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:47 +0700

    Upload SQL chunk: t5136_chunk_0000.sql

---
+++ sql_chunks/t5136_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t5136
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t5136" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5136
+INSERT INTO [t5136] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t5136] VALUES (2,'الكلمات المشتركة','9','0','0');
+INSERT INTO [t5136] VALUES (3,'لفظ الجلالة (الله) هل هو مشتق أو مرتجل؟','11','0','0');
+INSERT INTO [t5136] VALUES (4,'مثال يدل على تقارب اللغتين العربية والعبرانية','12','0','0');
+INSERT INTO [t5136] VALUES (5,'أمثلة من الكلمات التي قيل إنها وقعت في القرآن من غير العربية','13','0','0');
+INSERT INTO [t5136] VALUES (6,'أباريق','13','5','0');
+INSERT INTO [t5136] VALUES (7,'الأب','15','5','0');
+INSERT INTO [t5136] VALUES (8,'ابلعي','17','5','0');
+INSERT INTO [t5136] VALUES (9,'أخلد','19','5','0');
+INSERT INTO [t5136] VALUES (10,'الأرائك','20','5','0');
+INSERT INTO [t5136] VALUES (11,'أسباط','21','5','0');
+INSERT INTO [t5136] VALUES (12,'إستبرق','23','5','0');
+INSERT INTO [t5136] VALUES (13,'السندس','24','5','0');
+INSERT INTO [t5136] VALUES (14,'أسفار','25','5','0');
