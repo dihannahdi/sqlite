5be3cab641dd12816c5e8f9be2a6d36f56836392
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:51 +0700

    Upload SQL chunk: t27104_chunk_0000.sql

---
+++ sql_chunks/t27104_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t27104
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t27104" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t27104
+INSERT INTO [t27104] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t27104] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t27104] VALUES (3,'الشيخ الأول','3','0','0');
+INSERT INTO [t27104] VALUES (4,'الشيخ الثاني','5','0','0');
+INSERT INTO [t27104] VALUES (5,'الشيخ الثالث','7','0','0');
+INSERT INTO [t27104] VALUES (6,'الشيخ الرابع','7','0','0');
+INSERT INTO [t27104] VALUES (7,'الشيخ الخامس','9','0','0');
+INSERT INTO [t27104] VALUES (8,'الشيخ السادس','10','0','0');
+INSERT INTO [t27104] VALUES (9,'الشيخ السابع','11','0','0');
+INSERT INTO [t27104] VALUES (10,'الشيخ الثامن','13','0','0');
+INSERT INTO [t27104] VALUES (11,'الشيخ التاسع','15','0','0');
+INSERT INTO [t27104] VALUES (12,'الشيخ العاشر','16','0','0');
+INSERT INTO [t27104] VALUES (13,'الشيخ الحادي عشر','17','0','0');
+INSERT INTO [t27104] VALUES (14,'الشيخ الثاني عشر','18','0','0');
+INSERT INTO [t27104] VALUES (15,'الشيخ الثالث عشر','21','0','0');
+INSERT INTO [t27104] VALUES (16,'الشيخ الرابع عشر','22','0','0');
+INSERT INTO [t27104] VALUES (17,'الشيخ الخامس عشر','23','0','0');
+INSERT INTO [t27104] VALUES (18,'الشيخ السادس عشر','25','0','0');
+INSERT INTO [t27104] VALUES (19,'الشيخ السابع عشر','26','0','0');
