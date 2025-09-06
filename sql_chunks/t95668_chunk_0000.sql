8afd91429796221a82f60bae111d266c30ec0c2d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:55 +0700

    Upload SQL chunk: t95668_chunk_0000.sql

---
+++ sql_chunks/t95668_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t95668
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t95668" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95668
+INSERT INTO [t95668] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95668] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t95668] VALUES (3,'تحريم الكذب','3','0','0');
+INSERT INTO [t95668] VALUES (4,'الحالات التي يباح فيها الكذب','6','0','0');
+INSERT INTO [t95668] VALUES (5,'المعاريض','7','0','0');
+INSERT INTO [t95668] VALUES (6,'الأسباب الباعثة على الكذب','9','0','0');
+INSERT INTO [t95668] VALUES (7,'ما لا يحسبه الناس كذبا','10','0','0');
+INSERT INTO [t95668] VALUES (8,'ومما لا يحسبه الناس كذبا ويتساهلون فيه','12','0','0');
+INSERT INTO [t95668] VALUES (9,'الكذب من أنواع الكفر الأكبر','14','0','0');
+INSERT INTO [t95668] VALUES (10,'الكذب على الله - عز وجل - وعلى رسوله - صلى الله عليه وسلم -','16','0','0');
+INSERT INTO [t95668] VALUES (11,'درر من أقوال السلف','17','0','0');
+INSERT INTO [t95668] VALUES (12,'علاج الكذب','19','0','0');
+INSERT INTO [t95668] VALUES (13,'الخاتمة','21','0','0');
