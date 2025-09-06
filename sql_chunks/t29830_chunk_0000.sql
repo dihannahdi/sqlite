667d15b6b2435ffe01e0d92d4733ba9148fcdedc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:58:02 +0700

    Upload SQL chunk: t29830_chunk_0000.sql

---
+++ sql_chunks/t29830_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t29830
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t29830" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29830
+INSERT INTO [t29830] VALUES (1,'إذا اجتمع عيدان في يوم أجزأهم الأول. حديث ضعيف - الفراوي محمد بن الفضل بن أحمد بن محمد بن','2','0','0');
