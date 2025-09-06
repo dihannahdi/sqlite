ce08e974a589a5503845bd41f46a87a1e48a1cfb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:22 +0700

    Upload SQL chunk: t11109_chunk_0000.sql

---
+++ sql_chunks/t11109_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t11109
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t11109" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11109
+INSERT INTO [t11109] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11109] VALUES (2,'تعريف الفرائض','2','0','0');
+INSERT INTO [t11109] VALUES (3,'الحقوق المتعلقة بالتركة وبيان المقدم منها','3','0','0');
+INSERT INTO [t11109] VALUES (4,'أسباب الإرث','6','0','0');
+INSERT INTO [t11109] VALUES (5,'أقسام القرابة باعتبار جهاتهم','7','0','0');
+INSERT INTO [t11109] VALUES (6,'شروط الإرث','9','0','0');
+INSERT INTO [t11109] VALUES (7,'موانع الإرث','11','0','0');
+INSERT INTO [t11109] VALUES (8,'أقسام الورثة باعتبار نوع الإرث','13','0','0');
+INSERT INTO [t11109] VALUES (9,'أصحاب الفروض ومقدار نصيب كل وارث','15','0','0');
+INSERT INTO [t11109] VALUES (10,'مقدمة','15','9','0');
+INSERT INTO [t11109] VALUES (11,'ميراث الزوج','16','9','0');
+INSERT INTO [t11109] VALUES (12,'ميراث الزوجة','18','9','0');
+INSERT INTO [t11109] VALUES (13,'ميراث الأم','19','9','0');
+INSERT INTO [t11109] VALUES (14,'ميراث الأب','21','9','0');
+INSERT INTO [t11109] VALUES (15,'ميراث الجدة','23','9','0');
+INSERT INTO [t11109] VALUES (16,'ميراث الجد','25','9','0');
+INSERT INTO [t11109] VALUES (17,'ميراث البنات','27','9','0');
+INSERT INTO [t11109] VALUES (18,'ميراث بنات الابن','29','9','0');
+INSERT INTO [t11109] VALUES (19,'ميراث الأخوات من غير أم','32','9','0');
+INSERT INTO [t11109] VALUES (20,'ميراث أولاد الأم','36','9','0');
+INSERT INTO [t11109] VALUES (21,'تتمة','38','9','0');
+INSERT INTO [t11109] VALUES (22,'تمرينات','40','9','0');
+INSERT INTO [t11109] VALUES (23,'العصبة','43','0','0');
+INSERT INTO [t11109] VALUES (24,'مقدمة','43','23','0');
+INSERT INTO [t11109] VALUES (25,'أقسام العصبة','44','23','0');
+INSERT INTO [t11109] VALUES (26,'ترتيب العصبة','48','23','0');
+INSERT INTO [t11109] VALUES (27,'تمرينات','53','23','0');
+INSERT INTO [t11109] VALUES (28,'الحجب','56','0','0');
+INSERT INTO [t11109] VALUES (29,'الرد','59','0','0');
+INSERT INTO [t11109] VALUES (30,'ذوو الارحام','63','0','0');
+INSERT INTO [t11109] VALUES (31,'تمرينات','64','0','0');
