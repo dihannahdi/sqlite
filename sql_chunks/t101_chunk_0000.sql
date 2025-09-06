8631a0f259ccd6a12ac21b3825fb2640bbf6030d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:17 +0700

    Upload SQL chunk: t101_chunk_0000.sql

---
+++ sql_chunks/t101_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t101
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t101" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t101
+INSERT INTO [t101] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t101] VALUES (2,'آخر كتاب معرفة الرجال عن يحيى بن معين وغيره من الشيوخ','351','0','0');
