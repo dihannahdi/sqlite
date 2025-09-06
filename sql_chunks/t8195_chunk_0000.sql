abefcc451b7a1ae206434d2d83ea51b6fc092611
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:08 +0700

    Upload SQL chunk: t8195_chunk_0000.sql

---
+++ sql_chunks/t8195_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t8195
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t8195" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8195
+INSERT INTO [t8195] VALUES (1,'(١) الحديث الصحيح:','3','0','0');
+INSERT INTO [t8195] VALUES (2,'(٢) الحسن:','5','0','0');
+INSERT INTO [t8195] VALUES (3,'(٣) الضعيف:','12','0','0');
+INSERT INTO [t8195] VALUES (4,'(٤) المطروح:','13','0','0');
+INSERT INTO [t8195] VALUES (5,'(٥) الموضوع:','15','0','0');
+INSERT INTO [t8195] VALUES (6,'(٦) المرسل:','17','0','0');
+INSERT INTO [t8195] VALUES (7,'(٧) المعضل:','19','0','0');
+INSERT INTO [t8195] VALUES (8,'(٨) المنقطع:','19','0','0');
+INSERT INTO [t8195] VALUES (9,'(٩) الموقوف:','20','0','0');
+INSERT INTO [t8195] VALUES (10,'(١٠) المرفوع:','20','0','0');
+INSERT INTO [t8195] VALUES (11,'(١١) المتصل:','21','0','0');
+INSERT INTO [t8195] VALUES (12,'(١٢) المسند:','21','0','0');
+INSERT INTO [t8195] VALUES (13,'(١٣) الشاذ:','21','0','0');
+INSERT INTO [t8195] VALUES (14,'(١٤) المنكر:','21','0','0');
+INSERT INTO [t8195] VALUES (15,'(١٥) الغريب:','22','0','0');
+INSERT INTO [t8195] VALUES (16,'(١٦) المسلسل:','22','0','0');
+INSERT INTO [t8195] VALUES (17,'(١٧) المعنعن:','23','0','0');
+INSERT INTO [t8195] VALUES (18,'(١٨) المدلس:','26','0','0');
+INSERT INTO [t8195] VALUES (19,'(١٩) المضطرب والمعلل:','30','0','0');
+INSERT INTO [t8195] VALUES (20,'(٢٠) المدرج:','32','0','0');
+INSERT INTO [t8195] VALUES (21,'(٢١) ألفاظ الأداء:','34','0','0');
+INSERT INTO [t8195] VALUES (22,'(٢٢) المقلوب:','39','0','0');
+INSERT INTO [t8195] VALUES (23,'فصل','40','0','0');
+INSERT INTO [t8195] VALUES (24,'(١) مسألة:','41','0','0');
+INSERT INTO [t8195] VALUES (25,'(٢) مسألة:','42','0','0');
+INSERT INTO [t8195] VALUES (26,'(٣) مسألة:','42','0','0');
+INSERT INTO [t8195] VALUES (27,'(٤) مسألة:','43','0','0');
+INSERT INTO [t8195] VALUES (28,'(٥) مسألة:','43','0','0');
+INSERT INTO [t8195] VALUES (29,'(٦) مسألة:','43','0','0');
+INSERT INTO [t8195] VALUES (30,'(٢٣) آداب المحدث:','44','0','0');
+INSERT INTO [t8195] VALUES (31,'فصل','57','0','0');
+INSERT INTO [t8195] VALUES (32,'فصل','58','0','0');
+INSERT INTO [t8195] VALUES (33,'فصل','60','0','0');
+INSERT INTO [t8195] VALUES (34,'(٢٤) المؤتلف والمختلف:','71','0','0');
