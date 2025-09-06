1daa1971d2039dbf1bf29fcc337353d30b5892bd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:09 +0700

    Upload SQL chunk: t29449_chunk_0000.sql

---
+++ sql_chunks/t29449_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t29449
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t29449" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29449
+INSERT INTO [t29449] VALUES (1,'الحديث المسلسل بالأولية','1','0','0');
+INSERT INTO [t29449] VALUES (2,'المسلسل بالدمشقيين','3','0','0');
+INSERT INTO [t29449] VALUES (3,'المسلسل بإجابة الدعاء في الملتزم','5','0','0');
+INSERT INTO [t29449] VALUES (4,'المسلسل بإني أحبك','7','0','0');
+INSERT INTO [t29449] VALUES (5,'المسلسل بيوم العيد','9','0','0');
