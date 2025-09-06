faa4e3b10ec09e6fa30e42b7896f1a8e461e3150
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:46 +0700

    Upload SQL chunk: t21635_chunk_0000.sql

---
+++ sql_chunks/t21635_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21635
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21635" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21635
+INSERT INTO [t21635] VALUES (1,'المنتقى من مسموعات مرو للضياء المقدسي - مخطوط','2','0','0');
