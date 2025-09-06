ac295e45a1601fb53f81438984c1f7525cf78292
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:38 +0700

    Upload SQL chunk: t1959_chunk_0000.sql

---
+++ sql_chunks/t1959_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t1959
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t1959" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1959
+INSERT INTO [t1959] VALUES (1,'أياما معدودات','1','0','0');
+INSERT INTO [t1959] VALUES (2,'وكلوا واشربوا ولا تسرفوا','6','0','0');
+INSERT INTO [t1959] VALUES (3,'رمضان شهر الفرح','13','0','0');
+INSERT INTO [t1959] VALUES (4,'الصوم والإخلاص','20','0','0');
+INSERT INTO [t1959] VALUES (5,'رمضان شهر الدعوة','31','0','0');
+INSERT INTO [t1959] VALUES (6,'رمضان شهر البر','39','0','0');
+INSERT INTO [t1959] VALUES (7,'رمضان شهر الصحة','52','0','0');
+INSERT INTO [t1959] VALUES (8,'رمضان شهر التوبة','60','0','0');
+INSERT INTO [t1959] VALUES (9,'رمضان شهر الصلة','68','0','0');
+INSERT INTO [t1959] VALUES (10,'رمضان شهر القوة','83','0','0');
+INSERT INTO [t1959] VALUES (11,'شهر الصيام آثاره وأسراره','90','0','0');
+INSERT INTO [t1959] VALUES (12,'رمضان شهر السخاء والجود','104','0','0');
+INSERT INTO [t1959] VALUES (13,'حقوق الجار','113','0','0');
+INSERT INTO [t1959] VALUES (14,'فإنه أغض للبصر','121','0','0');
+INSERT INTO [t1959] VALUES (15,'أثر الصيام في اكتساب العزة','131','0','0');
+INSERT INTO [t1959] VALUES (16,'رمضان وتربية الأولاد','140','0','0');
+INSERT INTO [t1959] VALUES (17,'ليلة القدر','168','0','0');
+INSERT INTO [t1959] VALUES (18,'سر الاعتكاف ومقصوده وآدابه','170','0','0');
+INSERT INTO [t1959] VALUES (19,'رمضان شهر الحرية','174','0','0');
+INSERT INTO [t1959] VALUES (20,'في السحور بركة','190','0','0');
+INSERT INTO [t1959] VALUES (21,'رمضان شهر الدعاء','194','0','0');
+INSERT INTO [t1959] VALUES (22,'رمضان شهر الذكر','208','0','0');
+INSERT INTO [t1959] VALUES (23,'من فضائل الحلم','217','0','0');
+INSERT INTO [t1959] VALUES (24,'رمضان شهر العفة','223','0','0');
+INSERT INTO [t1959] VALUES (25,'من ترك شيئا لله عوضه الله خيرا منه','229','0','0');
+INSERT INTO [t1959] VALUES (26,'تقسيمات وضوابط في الحياء','244','0','0');
+INSERT INTO [t1959] VALUES (27,'رمضان فرصة لترك التدخين','250','0','0');
