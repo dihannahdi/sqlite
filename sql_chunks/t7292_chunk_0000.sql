5da7e880e5d85463b19860a2451d07c6b2943532
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:13 +0700

    Upload SQL chunk: t7292_chunk_0000.sql

---
+++ sql_chunks/t7292_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t7292
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t7292" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7292
+INSERT INTO [t7292] VALUES (1,'المقدمات','1','0','0');
+INSERT INTO [t7292] VALUES (2,'مقدمة الطبعة الثانية','1','1','0');
+INSERT INTO [t7292] VALUES (3,'مقدمة المحقق للطبعة الأولى','3','1','0');
+INSERT INTO [t7292] VALUES (4,'أسباب الإختلاف في اعتبار "السنة" المصدر التشريعي الثاني','7','1','0');
+INSERT INTO [t7292] VALUES (5,'هذا الكتاب','14','1','0');
+INSERT INTO [t7292] VALUES (6,'ابن قتيبة','18','1','0');
+INSERT INTO [t7292] VALUES (7,'صور المخطوطة','34','1','0');
+INSERT INTO [t7292] VALUES (8,'مقدمة ابن قتيبة','35','1','0');
+INSERT INTO [t7292] VALUES (9,'الرد على مطاعن المناهضين','49','0','0');
+INSERT INTO [t7292] VALUES (10,'الرد على أصحاب الكلام','49','9','0');
+INSERT INTO [t7292] VALUES (11,'الرد على أصحاب الرأي','90','9','0');
+INSERT INTO [t7292] VALUES (12,'ذكر أصحاب الحديث','115','9','0');
+INSERT INTO [t7292] VALUES (13,'ذكر الأحاديث التي ادعوا عليها التناقض','131','9','0');
+INSERT INTO [t7292] VALUES (14,'قالوا: أحكام قد أجمع عليها، يبطلها القرآن، ويحتج بها الخوارج','263','9','0');
+INSERT INTO [t7292] VALUES (15,'الفهارس','474','0','0');
