c3d11dc09998e6834b85297bacbc6bd02cf42991
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:15:59 +0700

    Upload SQL chunk: t37757_chunk_0000.sql

---
+++ sql_chunks/t37757_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t37757
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t37757" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37757
+INSERT INTO [t37757] VALUES (1,'[التعريف بالمؤلف]','2','0','0');
+INSERT INTO [t37757] VALUES (2,'مولده ونشأته','3','1','0');
+INSERT INTO [t37757] VALUES (3,'رحلته المكية','4','1','0');
+INSERT INTO [t37757] VALUES (4,'بعض ما قيل عنه','5','1','0');
+INSERT INTO [t37757] VALUES (5,'انتصاره لأهل السنة والجماعة (بمؤتمر النجف)','6','1','0');
+INSERT INTO [t37757] VALUES (6,'مؤلفاته وآثاره العلمية','7','1','0');
+INSERT INTO [t37757] VALUES (7,'ما طبع من آثاره المخطوطة','8','1','0');
+INSERT INTO [t37757] VALUES (8,'نموذج من شعره','8','1','0');
+INSERT INTO [t37757] VALUES (9,'وفاته','8','0','0');
+INSERT INTO [t37757] VALUES (10,'[مقدمة الناشر]','9','0','0');
+INSERT INTO [t37757] VALUES (11,'مقدمة المؤلف','18','0','0');
+INSERT INTO [t37757] VALUES (12,'مسألتان لا تستطيع الشيعة الجواب عنهما','36','0','0');
+INSERT INTO [t37757] VALUES (13,'المؤتمر في يومه الأول','39','0','0');
+INSERT INTO [t37757] VALUES (14,'المؤتمر في يومه الثاني','44','0','0');
+INSERT INTO [t37757] VALUES (15,'ادعاء مذهب جعفر الصادق','53','0','0');
