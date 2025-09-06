73aa55c037b1d5833dcfe10ed3a27d63088d58ab
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:10 +0700

    Upload SQL chunk: t5769_chunk_0000.sql

---
+++ sql_chunks/t5769_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t5769
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t5769" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5769
+INSERT INTO [t5769] VALUES (1,'الجزء ١','1','0','0');
+INSERT INTO [t5769] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
+INSERT INTO [t5769] VALUES (3,'حرف الهمزة','6','1','0');
+INSERT INTO [t5769] VALUES (4,'فصل في المحلى بال من هذا الحرف','418','1','0');
+INSERT INTO [t5769] VALUES (5,'حرف الباء','428','1','0');
+INSERT INTO [t5769] VALUES (6,'حرف التاء','440','1','0');
+INSERT INTO [t5769] VALUES (7,'حرف الثاء','461','1','0');
+INSERT INTO [t5769] VALUES (8,'حرف الجيم','483','1','0');
+INSERT INTO [t5769] VALUES (9,'حرف الحاء','491','1','0');
+INSERT INTO [t5769] VALUES (10,'حرف الخاء','510','1','0');
+INSERT INTO [t5769] VALUES (11,'الجزء ٢','539','0','0');
+INSERT INTO [t5769] VALUES (12,'بسم الله الرحمن الرحيم','539','11','0');
+INSERT INTO [t5769] VALUES (13,'حرف الدال','539','11','0');
+INSERT INTO [t5769] VALUES (14,'حرف الذال','554','11','0');
+INSERT INTO [t5769] VALUES (15,'حرف الراء','560','11','0');
+INSERT INTO [t5769] VALUES (16,'حرف الزاي','580','11','0');
+INSERT INTO [t5769] VALUES (17,'حرف السين','584','11','0');
+INSERT INTO [t5769] VALUES (18,'حرف الشين','611','11','0');
+INSERT INTO [t5769] VALUES (19,'حرف الصاد','624','11','0');
+INSERT INTO [t5769] VALUES (20,'حرف الضاد','646','11','0');
+INSERT INTO [t5769] VALUES (21,'حرف الطاء','650','11','0');
+INSERT INTO [t5769] VALUES (22,'حرف الظاء','661','11','0');
+INSERT INTO [t5769] VALUES (23,'حرف العين','661','11','0');
+INSERT INTO [t5769] VALUES (24,'حرف الغين','696','11','0');
+INSERT INTO [t5769] VALUES (25,'حرف الفاء','702','11','0');
+INSERT INTO [t5769] VALUES (26,'حرف القاف','718','11','0');
+INSERT INTO [t5769] VALUES (27,'حرف الكاف','740','11','0');
+INSERT INTO [t5769] VALUES (28,'باب كان وهي الشمائل الشريفة','765','11','0');
+INSERT INTO [t5769] VALUES (29,'حرف اللام','823','11','0');
+INSERT INTO [t5769] VALUES (30,'حرف الميم','873','11','0');
+INSERT INTO [t5769] VALUES (31,'حرف النون','996','11','0');
+INSERT INTO [t5769] VALUES (32,'باب المناهي','1002','11','0');
+INSERT INTO [t5769] VALUES (33,'حرف الهاء','1016','11','0');
+INSERT INTO [t5769] VALUES (34,'حرف الواو','1018','11','0');
+INSERT INTO [t5769] VALUES (35,'حرف لا','1024','11','0');
+INSERT INTO [t5769] VALUES (36,'حرف الياء','1043','11','0');
