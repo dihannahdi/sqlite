cb2e7dc2180db600d123c7c219eb9af9f84d609d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:34 +0700

    Upload SQL chunk: t23424_chunk_0000.sql

---
+++ sql_chunks/t23424_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t23424
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t23424" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23424
+INSERT INTO [t23424] VALUES (1,'رسالة أبي داود لأهل مكة (١)','2','0','0');
+INSERT INTO [t23424] VALUES (2,'رسالة أبي داود لأهل مكة (٢)','23','0','0');
+INSERT INTO [t23424] VALUES (3,'رسالة أبي داود لأهل مكة (٣)','43','0','0');
+INSERT INTO [t23424] VALUES (4,'رسالة أبي داود لأهل مكة (٤)','64','0','0');
