9970e1d73dfcf4dd3abf157b49d86e20bf42eee5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:14:39 +0700

    Upload SQL chunk: t13576_chunk_0000.sql

---
+++ sql_chunks/t13576_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t13576
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t13576" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13576
+INSERT INTO [t13576] VALUES (1,'تعقيب على مقالات علي الصابوني','2','0','0');
+INSERT INTO [t13576] VALUES (2,'ولي على كلا الشيخين ملاحظات','3','0','0');
+INSERT INTO [t13576] VALUES (3,'وبهذا يتضح قطعا','7','0','0');
