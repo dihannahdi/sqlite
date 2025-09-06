d8cabb9fe99cac597932df28a0cd35cfcca1de8c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:07 +0700

    Upload SQL chunk: t483_chunk_0000.sql

---
+++ sql_chunks/t483_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t483
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t483" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t483
+INSERT INTO [t483] VALUES (1,'التحذير من الاغترار بالدنيا','1','0','0');
+INSERT INTO [t483] VALUES (2,'الاستعداد لنزول الموت','5','0','0');
+INSERT INTO [t483] VALUES (3,'ذم طول الأمل','9','0','0');
+INSERT INTO [t483] VALUES (4,'قصر الأمل','12','0','0');
+INSERT INTO [t483] VALUES (5,'سكرات الموت','16','0','0');
+INSERT INTO [t483] VALUES (6,'عذاب القبر للكفار وبعض عصاة المؤمنين','20','0','0');
+INSERT INTO [t483] VALUES (7,'أحوال الموتى','24','0','0');
+INSERT INTO [t483] VALUES (8,'أشراط الساعة وأحوالها','28','0','0');
+INSERT INTO [t483] VALUES (9,'النفخ في الصور','32','0','0');
+INSERT INTO [t483] VALUES (10,'شفاعة النبي صلى الله عليه وسلم','35','0','0');
+INSERT INTO [t483] VALUES (11,'الحساب','37','0','0');
+INSERT INTO [t483] VALUES (12,'فصل في الميزان','41','0','0');
+INSERT INTO [t483] VALUES (13,'المرور على الصراط والحوض','45','0','0');
+INSERT INTO [t483] VALUES (14,'الشفاعة الكبرى','50','0','0');
+INSERT INTO [t483] VALUES (15,'عذاب الكافرين في جهنم','55','0','0');
+INSERT INTO [t483] VALUES (16,'الخلود في النار للكافرين','62','0','0');
+INSERT INTO [t483] VALUES (17,'الجنة وما لأهلها من النعيم','68','0','0');
+INSERT INTO [t483] VALUES (18,'صفة الحور العين','75','0','0');
+INSERT INTO [t483] VALUES (19,'رؤية الله تبارك وتعالى في الجنة','80','0','0');
