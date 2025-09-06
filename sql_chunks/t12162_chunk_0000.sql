887e762679288ee19cfd829912e855d86722a703
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:02:39 +0700

    Upload SQL chunk: t12162_chunk_0000.sql

---
+++ sql_chunks/t12162_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t12162
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t12162" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12162
+INSERT INTO [t12162] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t12162] VALUES (2,'اسمه','2','0','0');
+INSERT INTO [t12162] VALUES (3,'ذكاؤه وقوة لسنه','3','0','0');
+INSERT INTO [t12162] VALUES (4,'شيوخه ومعاصروه','5','0','0');
+INSERT INTO [t12162] VALUES (5,'تلاميذه','7','0','0');
+INSERT INTO [t12162] VALUES (6,'آثاره','8','0','0');
+INSERT INTO [t12162] VALUES (7,'وفاته','9','0','0');
+INSERT INTO [t12162] VALUES (8,'مفهوم الباقلاني للإعجاز القرآني','10','0','0');
