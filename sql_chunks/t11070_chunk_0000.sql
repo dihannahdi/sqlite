fa4eb4dd15c97dcfdcf7cf81f2ecfcbf6627f75f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:51 +0700

    Upload SQL chunk: t11070_chunk_0000.sql

---
+++ sql_chunks/t11070_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11070
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11070" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11070
+INSERT INTO [t11070] VALUES (1,'بعض فوائد صلح الحديبية','1','0','0');
+INSERT INTO [t11070] VALUES (2,'مصادر ومراجع','15','0','0');
