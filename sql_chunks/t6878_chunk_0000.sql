6a3f2d88ae79a328d046905081e6a15604b736ac
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:34:20 +0700

    Upload SQL chunk: t6878_chunk_0000.sql

---
+++ sql_chunks/t6878_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t6878
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t6878" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6878
+INSERT INTO [t6878] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t6878] VALUES (2,'فصل','16','0','0');
+INSERT INTO [t6878] VALUES (3,'فصل','23','0','0');
+INSERT INTO [t6878] VALUES (4,'فصل','30','0','0');
+INSERT INTO [t6878] VALUES (5,'فصل','46','0','0');
+INSERT INTO [t6878] VALUES (6,'فصل','56','0','0');
+INSERT INTO [t6878] VALUES (7,'فصل','64','0','0');
+INSERT INTO [t6878] VALUES (8,'فصل','66','0','0');
+INSERT INTO [t6878] VALUES (9,'فصل','81','0','0');
+INSERT INTO [t6878] VALUES (10,'فصل','92','0','0');
+INSERT INTO [t6878] VALUES (11,'فصل','104','0','0');
+INSERT INTO [t6878] VALUES (12,'فصل','112','0','0');
+INSERT INTO [t6878] VALUES (13,'فصل','121','0','0');
+INSERT INTO [t6878] VALUES (14,'فصل','134','0','0');
+INSERT INTO [t6878] VALUES (15,'فصل','140','0','0');
+INSERT INTO [t6878] VALUES (16,'فصل','145','0','0');
+INSERT INTO [t6878] VALUES (17,'فصل','150','0','0');
+INSERT INTO [t6878] VALUES (18,'فصل','174','0','0');
+INSERT INTO [t6878] VALUES (19,'فصل','191','0','0');
+INSERT INTO [t6878] VALUES (20,'فصل','198','0','0');
+INSERT INTO [t6878] VALUES (21,'فصل','200','0','0');
+INSERT INTO [t6878] VALUES (22,'فصل','202','0','0');
+INSERT INTO [t6878] VALUES (23,'فصل','211','0','0');
