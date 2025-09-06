f0b174306a57beb365c7ff4d386ad913ed0594b8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:23 +0700

    Upload SQL chunk: t8851_chunk_0000.sql

---
+++ sql_chunks/t8851_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t8851
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t8851" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8851
+INSERT INTO [t8851] VALUES (1,'تمهيد','1','0','0');
+INSERT INTO [t8851] VALUES (2,'أهمية القدس لدى المسلمين','3','0','0');
+INSERT INTO [t8851] VALUES (3,'الروايات المختلفة في كيفية وتاريخ فتحها','5','0','0');
+INSERT INTO [t8851] VALUES (4,'أحداث الفتح والقدوة في سلوك الخليفة وأصحابه','7','0','0');
+INSERT INTO [t8851] VALUES (5,'وصف المصادر النصرانية لدخول الخليفة عمر بن الخطاب رضي الله عنه القدس','11','0','0');
+INSERT INTO [t8851] VALUES (6,'وصف المصادر الإسلامية لدخول المسلمين مدينة القدس','13','0','0');
+INSERT INTO [t8851] VALUES (7,'المسجد الذي بناه الخليفة عمر بن الخطاب في ساحة المسجد الأقصى','14','0','0');
+INSERT INTO [t8851] VALUES (8,'الخاتمة','17','0','0');
+INSERT INTO [t8851] VALUES (9,'مصادر ومراجع','18','0','0');
