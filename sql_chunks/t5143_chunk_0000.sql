dad58ee93b01deabffe23cda05eb6d3080129911
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:48 +0700

    Upload SQL chunk: t5143_chunk_0000.sql

---
+++ sql_chunks/t5143_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t5143
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t5143" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5143
+INSERT INTO [t5143] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t5143] VALUES (2,'فصل: " الإيمان بالله "','3','0','0');
+INSERT INTO [t5143] VALUES (3,'فصل: " الإيمان بالملائكة"','12','0','0');
+INSERT INTO [t5143] VALUES (4,'فصل: "الإبمان بالكتب "','15','0','0');
+INSERT INTO [t5143] VALUES (5,'فصل: " الإيمان بالرسل"','18','0','0');
+INSERT INTO [t5143] VALUES (6,'فصل: " الإيمان باليوم الآخرة "','23','0','0');
+INSERT INTO [t5143] VALUES (7,'فصل: " الإيمان بالقدر"','28','0','0');
+INSERT INTO [t5143] VALUES (8,'فصل: " ثمرات هذه العقيدة"','33','0','0');
