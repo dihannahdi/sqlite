2e08ef7b4b24f63b8804f39a7aae2dba6d70c10b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:37 +0700

    Upload SQL chunk: t10511_chunk_0000.sql

---
+++ sql_chunks/t10511_chunk_0000.sql
@@ -0,0 +1,61 @@
+-- Table: t10511
+-- Chunk: 1
+-- Rows: 54
+
+CREATE TABLE "t10511" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10511
+INSERT INTO [t10511] VALUES (1,'مقدمة كتاب المحاسن والأضداد','1','0','0');
+INSERT INTO [t10511] VALUES (2,'مقدمة','13','0','0');
+INSERT INTO [t10511] VALUES (3,'محاسن الكتابة والكتب','15','0','0');
+INSERT INTO [t10511] VALUES (4,'مساويء اللحن في اللغة','20','0','0');
+INSERT INTO [t10511] VALUES (5,'محاسن المخاطبات','22','0','0');
+INSERT INTO [t10511] VALUES (6,'محاسن المكاتبات','27','0','0');
+INSERT INTO [t10511] VALUES (7,'محاسن الجواب','31','0','0');
+INSERT INTO [t10511] VALUES (8,'محاسن حفظ اللسان','34','0','0');
+INSERT INTO [t10511] VALUES (9,'محاسن كتمان السر','38','0','0');
+INSERT INTO [t10511] VALUES (10,'محاسن المشورة','43','0','0');
+INSERT INTO [t10511] VALUES (11,'محاسن الشكر','45','0','0');
+INSERT INTO [t10511] VALUES (12,'محاسن الصدق','50','0','0');
+INSERT INTO [t10511] VALUES (13,'محاسن العفو','55','0','0');
+INSERT INTO [t10511] VALUES (14,'محاسن الصبر على الحبس','59','0','0');
+INSERT INTO [t10511] VALUES (15,'محاسن المودة','63','0','0');
+INSERT INTO [t10511] VALUES (16,'محاسن الولايات','66','0','0');
+INSERT INTO [t10511] VALUES (17,'محاسن الصحبة','68','0','0');
+INSERT INTO [t10511] VALUES (18,'محاسن التطير','70','0','0');
+INSERT INTO [t10511] VALUES (19,'محاسن الوفاء','72','0','0');
+INSERT INTO [t10511] VALUES (20,'محاسن السخاء','76','0','0');
+INSERT INTO [t10511] VALUES (21,'مساوىء البخل','84','0','0');
+INSERT INTO [t10511] VALUES (22,'محاسن الشجاعة','93','0','0');
+INSERT INTO [t10511] VALUES (23,'محاسن حب الوطن','105','0','0');
+INSERT INTO [t10511] VALUES (24,'محاسن الدهاء والحيل','112','0','0');
+INSERT INTO [t10511] VALUES (25,'محاسن المفاخرة','118','0','0');
+INSERT INTO [t10511] VALUES (26,'محاسن الثقة بالله','139','0','0');
+INSERT INTO [t10511] VALUES (27,'محاسن طلب الرزق','141','0','0');
+INSERT INTO [t10511] VALUES (28,'محاسن المواعظ','144','0','0');
+INSERT INTO [t10511] VALUES (29,'محاسن الدنيا [ومساوئها]','146','0','0');
+INSERT INTO [t10511] VALUES (30,'محاسن الزهد','152','0','0');
+INSERT INTO [t10511] VALUES (31,'محاسن النساء [الشاعرات]','156','0','0');
+INSERT INTO [t10511] VALUES (32,'النساء الماجنات','161','0','0');
+INSERT INTO [t10511] VALUES (33,'الأعرابيات','169','0','0');
+INSERT INTO [t10511] VALUES (34,'محاسن المتكلمات','171','0','0');
+INSERT INTO [t10511] VALUES (35,'صفة النساء الحسان','177','0','0');
+INSERT INTO [t10511] VALUES (36,'صفة الزوجة الصالحة','182','0','0');
+INSERT INTO [t10511] VALUES (37,'أمثال في التزويج','187','0','0');
+INSERT INTO [t10511] VALUES (38,'رأي المرأة بالرجل','190','0','0');
+INSERT INTO [t10511] VALUES (39,'نساء الخلفاء','194','0','0');
+INSERT INTO [t10511] VALUES (40,'المطلقات','197','0','0');
+INSERT INTO [t10511] VALUES (41,'محاسن وفاء النساء','202','0','0');
+INSERT INTO [t10511] VALUES (42,'غدر النساء','209','0','0');
+INSERT INTO [t10511] VALUES (43,'حديث الزباء','214','0','0');
+INSERT INTO [t10511] VALUES (44,'محاسن مكر النساء','219','0','0');
+INSERT INTO [t10511] VALUES (45,'محاسن الغيرة والحجاب','225','0','0');
+INSERT INTO [t10511] VALUES (46,'محاسن القيادة','246','0','0');
+INSERT INTO [t10511] VALUES (47,'أخبار الشعراء','262','0','0');
+INSERT INTO [t10511] VALUES (48,'أحاديث الجنس','278','0','0');
+INSERT INTO [t10511] VALUES (49,'النيروز والمهرجان','287','0','0');
+INSERT INTO [t10511] VALUES (50,'محاسن الهدايا','292','0','0');
+INSERT INTO [t10511] VALUES (51,'القيان','305','50','0');
+INSERT INTO [t10511] VALUES (52,'محاسن الموت','310','0','0');
+INSERT INTO [t10511] VALUES (53,'فهرس الكتاب','313','0','0');
+INSERT INTO [t10511] VALUES (54,'فهرس الأعلام','316','0','0');
