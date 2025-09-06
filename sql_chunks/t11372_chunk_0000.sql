e611e5eda599aa08069b42ba83e63bea2ad7aaf2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:38 +0700

    Upload SQL chunk: t11372_chunk_0000.sql

---
+++ sql_chunks/t11372_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t11372
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t11372" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11372
+INSERT INTO [t11372] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11372] VALUES (2,'باب أسباب الميراث','3','0','0');
+INSERT INTO [t11372] VALUES (3,'باب موانع الإرث','4','0','0');
+INSERT INTO [t11372] VALUES (4,'باب الوارثين من الرجال','5','0','0');
+INSERT INTO [t11372] VALUES (5,'باب الوارثات من النساء','6','0','0');
+INSERT INTO [t11372] VALUES (6,'باب الفروض المقدرة في كتاب الله تعلى','7','0','0');
+INSERT INTO [t11372] VALUES (7,'باب النصف','8','0','0');
+INSERT INTO [t11372] VALUES (8,'باب الربع','9','0','0');
+INSERT INTO [t11372] VALUES (9,'باب الثمن','10','0','0');
+INSERT INTO [t11372] VALUES (10,'باب الثلثين','11','0','0');
+INSERT INTO [t11372] VALUES (11,'باب الثلث','12','0','0');
+INSERT INTO [t11372] VALUES (12,'باب السدس','14','0','0');
+INSERT INTO [t11372] VALUES (13,'باب التعصيب','16','0','0');
+INSERT INTO [t11372] VALUES (14,'باب الحجب','18','0','0');
+INSERT INTO [t11372] VALUES (15,'باب المشتركة','20','0','0');
+INSERT INTO [t11372] VALUES (16,'باب الجد والاخوة','21','0','0');
+INSERT INTO [t11372] VALUES (17,'باب الأكدرية','23','0','0');
+INSERT INTO [t11372] VALUES (18,'باب الحساب','24','0','0');
+INSERT INTO [t11372] VALUES (19,'باب السهام','26','0','0');
+INSERT INTO [t11372] VALUES (20,'باب المناسخة','28','0','0');
+INSERT INTO [t11372] VALUES (21,'باب الخنثى المشكل','29','0','0');
+INSERT INTO [t11372] VALUES (22,'باب الغرقى والهدمي والحرقى','30','0','0');
