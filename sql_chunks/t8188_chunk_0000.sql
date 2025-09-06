39a5dbf845e6e2a92d16fdc2a0778f8897523095
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:04 +0700

    Upload SQL chunk: t8188_chunk_0000.sql

---
+++ sql_chunks/t8188_chunk_0000.sql
@@ -0,0 +1,62 @@
+-- Table: t8188
+-- Chunk: 1
+-- Rows: 55
+
+CREATE TABLE "t8188" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8188
+INSERT INTO [t8188] VALUES (1,'أسواق الذهب','1','0','0');
+INSERT INTO [t8188] VALUES (2,'طبع بعد وفاته نظم أحمد شوقي بك','1','0','0');
+INSERT INTO [t8188] VALUES (3,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t8188] VALUES (4,'مقدمة','2','0','0');
+INSERT INTO [t8188] VALUES (5,'الوطن','3','0','0');
+INSERT INTO [t8188] VALUES (6,'الحقيقة الواحدة','5','0','0');
+INSERT INTO [t8188] VALUES (7,'الجندي المجهول','18','0','0');
+INSERT INTO [t8188] VALUES (8,'قناة السويس','25','0','0');
+INSERT INTO [t8188] VALUES (9,'الذكرى','35','0','0');
+INSERT INTO [t8188] VALUES (10,'الشمس','39','0','0');
+INSERT INTO [t8188] VALUES (11,'الموت','42','0','0');
+INSERT INTO [t8188] VALUES (12,'الشباب','42','0','0');
+INSERT INTO [t8188] VALUES (13,'دعاء الصلاة العامة','46','0','0');
+INSERT INTO [t8188] VALUES (14,'الخير','50','0','0');
+INSERT INTO [t8188] VALUES (15,'الظلم','51','0','0');
+INSERT INTO [t8188] VALUES (16,'القلب','52','0','0');
+INSERT INTO [t8188] VALUES (17,'الذكرى','53','0','0');
+INSERT INTO [t8188] VALUES (18,'شاهد الزور','55','0','0');
+INSERT INTO [t8188] VALUES (19,'الصبر','56','0','0');
+INSERT INTO [t8188] VALUES (20,'شهادة الدراسة','57','0','0');
+INSERT INTO [t8188] VALUES (21,'الحياة','57','0','0');
+INSERT INTO [t8188] VALUES (22,'شهادة الحياة','57','0','0');
+INSERT INTO [t8188] VALUES (23,'الحياة أيضا','61','0','0');
+INSERT INTO [t8188] VALUES (24,'الحياة أيضا','61','0','0');
+INSERT INTO [t8188] VALUES (25,'اللسان','63','0','0');
+INSERT INTO [t8188] VALUES (26,'البيان','63','0','0');
+INSERT INTO [t8188] VALUES (27,'المال','66','0','0');
+INSERT INTO [t8188] VALUES (28,'الأهرام','68','0','0');
+INSERT INTO [t8188] VALUES (29,'الأمس','70','0','0');
+INSERT INTO [t8188] VALUES (30,'اليوم','71','0','0');
+INSERT INTO [t8188] VALUES (31,'الغد','72','0','0');
+INSERT INTO [t8188] VALUES (32,'المسجد الحرام م','74','0','0');
+INSERT INTO [t8188] VALUES (33,'الشهادة','78','0','0');
+INSERT INTO [t8188] VALUES (34,'الصلاة','80','0','0');
+INSERT INTO [t8188] VALUES (35,'الصلاة','80','0','0');
+INSERT INTO [t8188] VALUES (36,'الطهارة','80','0','0');
+INSERT INTO [t8188] VALUES (37,'الصوم','83','0','0');
+INSERT INTO [t8188] VALUES (38,'الزكاة','84','0','0');
+INSERT INTO [t8188] VALUES (39,'الحج','85','0','0');
+INSERT INTO [t8188] VALUES (40,'خطيب المساجد','87','0','0');
+INSERT INTO [t8188] VALUES (41,'الطلاق','87','0','0');
+INSERT INTO [t8188] VALUES (42,'البحر الأبيض المتوسط','90','0','0');
+INSERT INTO [t8188] VALUES (43,'صفة الظبي','96','0','0');
+INSERT INTO [t8188] VALUES (44,'صفة الأسد','97','0','0');
+INSERT INTO [t8188] VALUES (45,'الأسد في حديقة الحيوانات','99','0','0');
+INSERT INTO [t8188] VALUES (46,'الجمال','103','0','0');
+INSERT INTO [t8188] VALUES (47,'الأمومة','104','0','0');
+INSERT INTO [t8188] VALUES (48,'الكاتب العمومي','105','0','0');
+INSERT INTO [t8188] VALUES (49,'العلم','106','0','0');
+INSERT INTO [t8188] VALUES (50,'السجع','108','0','0');
+INSERT INTO [t8188] VALUES (51,'النقد','109','0','0');
+INSERT INTO [t8188] VALUES (52,'الزهرة','110','0','0');
+INSERT INTO [t8188] VALUES (53,'الساقية','111','0','0');
+INSERT INTO [t8188] VALUES (54,'الشيخ المهندم','111','0','0');
+INSERT INTO [t8188] VALUES (55,'خواطر','112','0','0');
