71c8c7436509c770267e05086cf472b310e65239
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:56 +0700

    Upload SQL chunk: t475_chunk_0000.sql

---
+++ sql_chunks/t475_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t475
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t475" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t475
+INSERT INTO [t475] VALUES (1,'نص الكتاب','1','0','0');
+INSERT INTO [t475] VALUES (2,'بسم الله الرحمن الرحيم','1','1','0');
+INSERT INTO [t475] VALUES (3,'الفصل الأول','13','0','0');
+INSERT INTO [t475] VALUES (4,'في الأمور التي هي محل الاستخارة','13','3','0');
+INSERT INTO [t475] VALUES (5,'الفصل الثاني','13','0','0');
+INSERT INTO [t475] VALUES (6,'في سرد بعض أحاديثها','13','5','0');
+INSERT INTO [t475] VALUES (7,'الفصل الثالث','15','0','0');
+INSERT INTO [t475] VALUES (8,'الفصل الرابع','15','0','0');
+INSERT INTO [t475] VALUES (9,'الفصل الخامس','16','0','0');
+INSERT INTO [t475] VALUES (10,'الفصل السادس','16','0','0');
+INSERT INTO [t475] VALUES (11,'الفصل السابع','16','0','0');
+INSERT INTO [t475] VALUES (12,'الفصل الثامن','17','0','0');
+INSERT INTO [t475] VALUES (13,'الفصل التاسع','18','0','0');
+INSERT INTO [t475] VALUES (14,'بسم الله الرحمن الرحيم','73','13','0');
+INSERT INTO [t475] VALUES (15,'خاتمة','149','0','0');
+INSERT INTO [t475] VALUES (16,'ذيل الخاتمة','217','0','0');
+INSERT INTO [t475] VALUES (17,'خاتمة الطبع','219','0','0');
+INSERT INTO [t475] VALUES (18,'تقاريظ على كتاب " غرائب الاغتراب "','220','0','0');
+INSERT INTO [t475] VALUES (19,'لبعض أهل الفضل والآداب','220','18','0');
