ac22b79871560cc9cb16ff51276a61a6c454a55a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:57 +0700

    Upload SQL chunk: t7637_chunk_0000.sql

---
+++ sql_chunks/t7637_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t7637
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t7637" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7637
+INSERT INTO [t7637] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t7637] VALUES (2,'باب أنواع التوحيد','2','0','0');
+INSERT INTO [t7637] VALUES (3,'توحيد الربوبية','2','2','0');
+INSERT INTO [t7637] VALUES (4,'توحيد الألوهية','3','2','0');
+INSERT INTO [t7637] VALUES (5,'توحيد الذات والأسماء والصفات','5','2','0');
+INSERT INTO [t7637] VALUES (6,'أنو اع الشرك','6','0','0');
+INSERT INTO [t7637] VALUES (7,'النوع الأول: الشرك الأكبر','6','6','0');
+INSERT INTO [t7637] VALUES (8,'النوع الثاني: الشرك الأصغر','9','6','0');
+INSERT INTO [t7637] VALUES (9,'النوع الثالث: الشرك الخفي','10','6','0');
+INSERT INTO [t7637] VALUES (10,'أنواع الكفر','11','0','0');
+INSERT INTO [t7637] VALUES (11,'النوع الأول: الكفر الذي يخرج من الملة','11','10','0');
+INSERT INTO [t7637] VALUES (12,'النوع الثاني: كفر أصغر','13','10','0');
+INSERT INTO [t7637] VALUES (13,'أنواع النفاق','14','0','0');
+INSERT INTO [t7637] VALUES (14,'الإعتقادي','14','13','0');
+INSERT INTO [t7637] VALUES (15,'العملي','15','13','0');
