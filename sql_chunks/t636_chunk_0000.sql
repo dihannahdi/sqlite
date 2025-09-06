b2b2641c847828112112b29e195428ad8df96718
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:15 +0700

    Upload SQL chunk: t636_chunk_0000.sql

---
+++ sql_chunks/t636_chunk_0000.sql
@@ -0,0 +1,66 @@
+-- Table: t636
+-- Chunk: 1
+-- Rows: 59
+
+CREATE TABLE "t636" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t636
+INSERT INTO [t636] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t636] VALUES (2,'أشعر الشعراء في الجاهلية ثلاثة','1','0','0');
+INSERT INTO [t636] VALUES (3,'المقلون من الشعراء ثلاثة','1','0','0');
+INSERT INTO [t636] VALUES (4,'أشعر الشعراء واحدة ثلاثة','2','0','0');
+INSERT INTO [t636] VALUES (5,'أشعر شعراء الإسلام ثلاثة','2','0','0');
+INSERT INTO [t636] VALUES (6,'المغلبون من الشعراء ثلاثة','2','0','0');
+INSERT INTO [t636] VALUES (7,'الرجز','2','0','0');
+INSERT INTO [t636] VALUES (8,'أول الرجاز','2','7','0');
+INSERT INTO [t636] VALUES (9,'فرسان العرب ثلاثة','2','0','0');
+INSERT INTO [t636] VALUES (10,'أجواد العرب في الجاهلية ثلاثة','3','0','0');
+INSERT INTO [t636] VALUES (11,'أجواد العرب في الإسلام','4','0','0');
+INSERT INTO [t636] VALUES (12,'رجليو العرب ثلاثة','4','0','0');
+INSERT INTO [t636] VALUES (13,'أغربة العرب ثلاثة','6','0','0');
+INSERT INTO [t636] VALUES (14,'أوفياء العرب في الجاهلية ثلاثة','7','0','0');
+INSERT INTO [t636] VALUES (15,'الرداد على من استغاث بهم ثلاثة','10','0','0');
+INSERT INTO [t636] VALUES (16,'غدرة العرب ثلاثة','11','0','0');
+INSERT INTO [t636] VALUES (17,'الباغون ثلاثة','12','0','0');
+INSERT INTO [t636] VALUES (18,'الوافيات في الجاهلية ثلاث','12','0','0');
+INSERT INTO [t636] VALUES (19,'المنجبات في الجاهلية ثلاث','12','0','0');
+INSERT INTO [t636] VALUES (20,'جمرات العرب في الجاهلية ثلاث','13','0','0');
+INSERT INTO [t636] VALUES (21,'حروب العرب الطوال في الجاهلية ثلاث','13','0','0');
+INSERT INTO [t636] VALUES (22,'أيام العرب العظام في الجاهلية ثلاثة','13','0','0');
+INSERT INTO [t636] VALUES (23,'بيوتات العرب في الجاهلية ثلاثة','13','0','0');
+INSERT INTO [t636] VALUES (24,'المتنافرون في الجاهلية ثلاثة','15','0','0');
+INSERT INTO [t636] VALUES (25,'المرتشون في الحكومة في الجاهلية ثلاثة','17','0','0');
+INSERT INTO [t636] VALUES (26,'حكماء العرب العدول ثلاثة','17','0','0');
+INSERT INTO [t636] VALUES (27,'المدركو الأوتار في الجاهلية ثلاثة','17','0','0');
+INSERT INTO [t636] VALUES (28,'أغلى العرب فداء ثلاثة','20','0','0');
+INSERT INTO [t636] VALUES (29,'جماجم العرب','20','0','0');
+INSERT INTO [t636] VALUES (30,'الحمس','20','0','0');
+INSERT INTO [t636] VALUES (31,'حكماء العرب ثلاثة','20','0','0');
+INSERT INTO [t636] VALUES (32,'حلماء العرب','20','0','0');
+INSERT INTO [t636] VALUES (33,'الأرداف','20','0','0');
+INSERT INTO [t636] VALUES (34,'اللقاح','20','0','0');
+INSERT INTO [t636] VALUES (35,'أحجار العرب','20','0','0');
+INSERT INTO [t636] VALUES (36,'البراجم','21','0','0');
+INSERT INTO [t636] VALUES (37,'اللهازم','21','0','0');
+INSERT INTO [t636] VALUES (38,'الرباب','21','0','0');
+INSERT INTO [t636] VALUES (39,'الربائع','21','0','0');
+INSERT INTO [t636] VALUES (40,'الأثاقي','21','0','0');
+INSERT INTO [t636] VALUES (41,'الأقارع','21','0','0');
+INSERT INTO [t636] VALUES (42,'ضبيعات العرب','21','0','0');
+INSERT INTO [t636] VALUES (43,'دهاة العرب','21','0','0');
+INSERT INTO [t636] VALUES (44,'عقماء العرب','21','0','0');
+INSERT INTO [t636] VALUES (45,'أعرق العرب في القتل','21','0','0');
+INSERT INTO [t636] VALUES (46,'الذهلان:','21','45','0');
+INSERT INTO [t636] VALUES (47,'السعدان:','21','45','0');
+INSERT INTO [t636] VALUES (48,'الأنكدان:','21','45','0');
+INSERT INTO [t636] VALUES (49,'الشعثمان:','21','45','0');
+INSERT INTO [t636] VALUES (50,'الزهدمان:','21','45','0');
+INSERT INTO [t636] VALUES (51,'العمران:','21','45','0');
+INSERT INTO [t636] VALUES (52,'العمران:','21','45','0');
+INSERT INTO [t636] VALUES (53,'الأقرعان:','21','45','0');
+INSERT INTO [t636] VALUES (54,'المرثدان:','21','45','0');
+INSERT INTO [t636] VALUES (55,'الأجربان:','21','45','0');
+INSERT INTO [t636] VALUES (56,'الحليفان:','21','45','0');
+INSERT INTO [t636] VALUES (57,'الضمران','21','0','0');
+INSERT INTO [t636] VALUES (58,'مفاخر العرب ثلاثة','21','0','0');
+INSERT INTO [t636] VALUES (59,'من اجتمع عليه نزار','21','0','0');
