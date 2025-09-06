57c3907ff83518d01c31468ebddaaad436a2c5fe
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:20:20 +0700

    Upload SQL chunk: t14565_chunk_0000.sql

---
+++ sql_chunks/t14565_chunk_0000.sql
@@ -0,0 +1,50 @@
+-- Table: t14565
+-- Chunk: 1
+-- Rows: 43
+
+CREATE TABLE "t14565" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14565
+INSERT INTO [t14565] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t14565] VALUES (2,'المسموع','8','0','0');
+INSERT INTO [t14565] VALUES (3,'باب الألف','8','2','0');
+INSERT INTO [t14565] VALUES (4,'باب الباء','16','2','0');
+INSERT INTO [t14565] VALUES (5,'باب التاء','23','2','0');
+INSERT INTO [t14565] VALUES (6,'باب الثاء','25','2','0');
+INSERT INTO [t14565] VALUES (7,'باب الجيم','27','2','0');
+INSERT INTO [t14565] VALUES (8,'باب الحاء','33','2','0');
+INSERT INTO [t14565] VALUES (9,'باب الخاء','40','2','0');
+INSERT INTO [t14565] VALUES (10,'باب الدال','46','2','0');
+INSERT INTO [t14565] VALUES (11,'باب الذال','51','2','0');
+INSERT INTO [t14565] VALUES (12,'باب الراء','53','2','0');
+INSERT INTO [t14565] VALUES (13,'باب الزاء','59','2','0');
+INSERT INTO [t14565] VALUES (14,'باب السين','61','2','0');
+INSERT INTO [t14565] VALUES (15,'باب الشين','67','2','0');
+INSERT INTO [t14565] VALUES (16,'باب الصاد','72','2','0');
+INSERT INTO [t14565] VALUES (17,'باب الضاد','76','2','0');
+INSERT INTO [t14565] VALUES (18,'باب الطاء','78','2','0');
+INSERT INTO [t14565] VALUES (19,'باب الظاء','81','2','0');
+INSERT INTO [t14565] VALUES (20,'باب العين','81','2','0');
+INSERT INTO [t14565] VALUES (21,'باب الغين','91','2','0');
+INSERT INTO [t14565] VALUES (22,'باب الفاء','95','2','0');
+INSERT INTO [t14565] VALUES (23,'باب القاف','99','2','0');
+INSERT INTO [t14565] VALUES (24,'باب الكاف','106','2','0');
+INSERT INTO [t14565] VALUES (25,'باب اللام','109','2','0');
+INSERT INTO [t14565] VALUES (26,'باب الميم','114','2','0');
+INSERT INTO [t14565] VALUES (27,'باب النون','123','2','0');
+INSERT INTO [t14565] VALUES (28,'باب الواو','127','2','0');
+INSERT INTO [t14565] VALUES (29,'باب الهاء','131','2','0');
+INSERT INTO [t14565] VALUES (30,'باب الياء','134','2','0');
+INSERT INTO [t14565] VALUES (31,'المقيس','136','0','0');
+INSERT INTO [t14565] VALUES (32,'باب المقصور','136','31','0');
+INSERT INTO [t14565] VALUES (33,'باب التحديد والعلامات فيما يعلم أنه منقوص','139','31','0');
+INSERT INTO [t14565] VALUES (34,'باب الممدود المعروف بالعلامات والنظائر','146','31','0');
+INSERT INTO [t14565] VALUES (35,'باب تثنية المقصور','151','31','0');
+INSERT INTO [t14565] VALUES (36,'باب جمع المقصور','153','31','0');
+INSERT INTO [t14565] VALUES (37,'باب جمع المقصور مكسرا','154','31','0');
+INSERT INTO [t14565] VALUES (38,'باب ما كان من الأسماء على أربعة أحرف أو خمسة ولم يكن في آخره ألف التأنيث','156','31','0');
+INSERT INTO [t14565] VALUES (39,'باب ما كان على أربعة أحرف من المقصور فصاعدا وآخره ألف التأنيث','157','31','0');
+INSERT INTO [t14565] VALUES (40,'باب تثنية الممدود','160','31','0');
+INSERT INTO [t14565] VALUES (41,'باب جمع الممدود','161','31','0');
+INSERT INTO [t14565] VALUES (42,'باب المقصور في الخط','163','31','0');
+INSERT INTO [t14565] VALUES (43,'باب الخط في الممدود','166','31','0');
