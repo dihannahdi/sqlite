5a1a411a7243e470b8e778f2f7d0e8de690a9d3a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:59 +0700

    Upload SQL chunk: t26874_chunk_0000.sql

---
+++ sql_chunks/t26874_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t26874
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t26874" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26874
+INSERT INTO [t26874] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26874] VALUES (2,'الجزء الأول','2','0','0');
+INSERT INTO [t26874] VALUES (3,'الجزء الثاني','25','0','0');
