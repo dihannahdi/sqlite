e28bddd64fbefa3cd9557d7d409ab87a001295af
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:50 +0700

    Upload SQL chunk: t31271_chunk_0000.sql

---
+++ sql_chunks/t31271_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t31271
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t31271" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31271
+INSERT INTO [t31271] VALUES (1,'الرسالة الأولى الطهارة','1','0','0');
+INSERT INTO [t31271] VALUES (2,'الوضوء','1','1','0');
+INSERT INTO [t31271] VALUES (3,'الغسل','4','1','0');
+INSERT INTO [t31271] VALUES (4,'التيمم','5','1','0');
+INSERT INTO [t31271] VALUES (5,'المسح على الخفين','5','1','0');
+INSERT INTO [t31271] VALUES (6,'كيف يتطهر المريض','10','1','0');
+INSERT INTO [t31271] VALUES (7,'كيف يصلي المريض','13','1','0');
+INSERT INTO [t31271] VALUES (8,'الرسالة الثانية الصلاة','17','0','0');
+INSERT INTO [t31271] VALUES (9,'تعريفها وفضلها','17','8','0');
+INSERT INTO [t31271] VALUES (10,'كيفية الصلاة','23','8','0');
+INSERT INTO [t31271] VALUES (11,'أحكام سجود السهو','30','8','0');
+INSERT INTO [t31271] VALUES (12,'سجود التلاوة','37','8','0');
+INSERT INTO [t31271] VALUES (13,'حكم تارك الصلاة','40','8','0');
+INSERT INTO [t31271] VALUES (14,'التوبة','45','8','0');
+INSERT INTO [t31271] VALUES (15,'الرسالة الثالثة الجنائز','48','0','0');
+INSERT INTO [t31271] VALUES (16,'أحكام تغسيل الميت','48','15','0');
+INSERT INTO [t31271] VALUES (17,'كيفية تغسيل الميت','51','15','0');
+INSERT INTO [t31271] VALUES (18,'كيفية تكفين الميت','53','15','0');
+INSERT INTO [t31271] VALUES (19,'كيفية الصلاة على الميت','54','15','0');
+INSERT INTO [t31271] VALUES (20,'كيفية دفن الميت','56','15','0');
