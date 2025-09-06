dcc742af506069cf528cea76598e486f5553cc4c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:35 +0700

    Upload SQL chunk: t374_chunk_0000.sql

---
+++ sql_chunks/t374_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t374
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t374" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t374
+INSERT INTO [t374] VALUES (1,'من عجائب القرآن','4','0','0');
+INSERT INTO [t374] VALUES (2,'الكلام على قوله تعالى: {لا إكراه في الدين}','43','0','0');
