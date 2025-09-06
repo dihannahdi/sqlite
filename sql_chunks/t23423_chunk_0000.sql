ea885723968afb80097bbd826802d8812c651364
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:46:33 +0700

    Upload SQL chunk: t23423_chunk_0000.sql

---
+++ sql_chunks/t23423_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t23423
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t23423" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23423
+INSERT INTO [t23423] VALUES (1,'شرح الأربعين النووية (١)','2','0','0');
+INSERT INTO [t23423] VALUES (2,'شرح الأربعين النووية (٢)','37','0','0');
+INSERT INTO [t23423] VALUES (3,'شرح الأربعين النووية (٣)','68','0','0');
+INSERT INTO [t23423] VALUES (4,'شرح الأربعين النووية (٤)','100','0','0');
+INSERT INTO [t23423] VALUES (5,'شرح الأربعين النووية (٥)','132','0','0');
+INSERT INTO [t23423] VALUES (6,'شرح الأربعين النووية (٦)','158','0','0');
+INSERT INTO [t23423] VALUES (7,'شرح الأربعين النووية (٧)','191','0','0');
+INSERT INTO [t23423] VALUES (8,'شرح الأربعين النووية (٨)','221','0','0');
+INSERT INTO [t23423] VALUES (9,'شرح الأربعين النووية (٩)','253','0','0');
+INSERT INTO [t23423] VALUES (10,'شرح الأربعين النووية (١٠)','283','0','0');
+INSERT INTO [t23423] VALUES (11,'شرح الأربعين النووية (١١)','311','0','0');
+INSERT INTO [t23423] VALUES (12,'شرح الأربعين النووية (١٢)','337','0','0');
+INSERT INTO [t23423] VALUES (13,'شرح الأربعين النووية (١٣)','371','0','0');
+INSERT INTO [t23423] VALUES (14,'شرح الأربعين النووية (١٤)','404','0','0');
