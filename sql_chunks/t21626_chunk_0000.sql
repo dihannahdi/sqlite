6605195adaee9eea9081bb45ad3831f9b3a3f937
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:38 +0700

    Upload SQL chunk: t21626_chunk_0000.sql

---
+++ sql_chunks/t21626_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21626
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21626" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21626
+INSERT INTO [t21626] VALUES (1,'الرواة الأربعة عشر للضياء المقدسي - مخطوط','1','0','0');
