2537bee48e093ed82d19ec68ba58daf2e7d9a304
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:53 +0700

    Upload SQL chunk: t472_chunk_0000.sql

---
+++ sql_chunks/t472_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t472
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t472" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t472
+INSERT INTO [t472] VALUES (1,'نقول عن كتب الفقه الشافعي في هذه المسألة','2','0','0');
