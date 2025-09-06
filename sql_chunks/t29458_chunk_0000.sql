724d2a8149717ee71c31c3356fb60ce7e4cdab31
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:10 +0700

    Upload SQL chunk: t29458_chunk_0000.sql

---
+++ sql_chunks/t29458_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t29458
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t29458" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29458
+INSERT INTO [t29458] VALUES (1,'الرسالة الأولى: ذكر المصافحة وما ورد فيها من الأخبار عن النبي المصطفى المختار','1','0','0');
+INSERT INTO [t29458] VALUES (2,'رواية أبي إسحاق عن البراء','5','0','0');
+INSERT INTO [t29458] VALUES (3,'رواية أبي العلاء يزيد بن عبد الله بن الشخير عن البراء','8','0','0');
+INSERT INTO [t29458] VALUES (4,'رواية أبي بحر عن البراء','10','0','0');
+INSERT INTO [t29458] VALUES (5,'رواية عبد الرحمن بن أبي ليلى عن البراء','12','0','0');
