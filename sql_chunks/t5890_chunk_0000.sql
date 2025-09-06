bacf43167b92c50160ec29bed9b69f83b20d4a9f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:14 +0700

    Upload SQL chunk: t5890_chunk_0000.sql

---
+++ sql_chunks/t5890_chunk_0000.sql
@@ -0,0 +1,58 @@
+-- Table: t5890
+-- Chunk: 1
+-- Rows: 51
+
+CREATE TABLE "t5890" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5890
+INSERT INTO [t5890] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5890] VALUES (2,'حرف الألف','9','0','0');
+INSERT INTO [t5890] VALUES (3,'من اسمه إبراهيم','148','2','0');
+INSERT INTO [t5890] VALUES (4,'من اسمه إسحاق','179','2','0');
+INSERT INTO [t5890] VALUES (5,'من اسمه إسماعيل','191','2','0');
+INSERT INTO [t5890] VALUES (6,'مفاريد حرف الألف','215','2','0');
+INSERT INTO [t5890] VALUES (7,'حرف الباء','224','0','0');
+INSERT INTO [t5890] VALUES (8,'حرف التاء','229','0','0');
+INSERT INTO [t5890] VALUES (9,'حرف الثاء','231','0','0');
+INSERT INTO [t5890] VALUES (10,'حرف الجيم','232','0','0');
+INSERT INTO [t5890] VALUES (11,'حرف الحاء','247','0','0');
+INSERT INTO [t5890] VALUES (12,'من اسمه الحسين','279','11','0');
+INSERT INTO [t5890] VALUES (13,'مفاريد حرف الحاء','289','11','0');
+INSERT INTO [t5890] VALUES (14,'حرف الخاء','307','0','0');
+INSERT INTO [t5890] VALUES (15,'حرف الدال','319','0','0');
+INSERT INTO [t5890] VALUES (16,'حرف الذال','327','0','0');
+INSERT INTO [t5890] VALUES (17,'حرف الراء','328','0','0');
+INSERT INTO [t5890] VALUES (18,'حرف الزاى','337','0','0');
+INSERT INTO [t5890] VALUES (19,'حرف السين','344','0','0');
+INSERT INTO [t5890] VALUES (20,'حرف الشين','374','0','0');
+INSERT INTO [t5890] VALUES (21,'حرف الصاد','379','0','0');
+INSERT INTO [t5890] VALUES (22,'حرف الضاد','389','0','0');
+INSERT INTO [t5890] VALUES (23,'حرف الطاء','390','0','0');
+INSERT INTO [t5890] VALUES (24,'حرف الظاء','399','0','0');
+INSERT INTO [t5890] VALUES (25,'حرف العين','400','0','0');
+INSERT INTO [t5890] VALUES (26,'من اسمه عبيد الله','463','25','0');
+INSERT INTO [t5890] VALUES (27,'ذكر من اسمه عبد الرحمن','472','25','0');
+INSERT INTO [t5890] VALUES (28,'ذكر بقية مفاريد العبادلة','513','25','0');
+INSERT INTO [t5890] VALUES (29,'من اسمه عثمان','591','25','0');
+INSERT INTO [t5890] VALUES (30,'من اسمه علي','601','25','0');
+INSERT INTO [t5890] VALUES (31,'من اسمه عباس','668','25','0');
+INSERT INTO [t5890] VALUES (32,'ذكر جماعة من مفاريد حرف العين','674','25','0');
+INSERT INTO [t5890] VALUES (33,'من اسمه عمر وعمرو','684','25','0');
+INSERT INTO [t5890] VALUES (34,'حرف الغين','703','0','0');
+INSERT INTO [t5890] VALUES (35,'حرف الفاء','704','0','0');
+INSERT INTO [t5890] VALUES (36,'حرف القاف','715','0','0');
+INSERT INTO [t5890] VALUES (37,'حرف الكاف','720','0','0');
+INSERT INTO [t5890] VALUES (38,'حرف اللام','721','0','0');
+INSERT INTO [t5890] VALUES (39,'حرف الميم','722','0','0');
+INSERT INTO [t5890] VALUES (40,'من اسمه محمد','722','39','0');
+INSERT INTO [t5890] VALUES (41,'ذكر من اسمه محمود','937','39','0');
+INSERT INTO [t5890] VALUES (42,'من اسمه موسى','945','39','0');
+INSERT INTO [t5890] VALUES (43,'مفاريد حرف الميم','952','39','0');
+INSERT INTO [t5890] VALUES (44,'حرف النون','990','0','0');
+INSERT INTO [t5890] VALUES (45,'حرف الهاء','1008','0','0');
+INSERT INTO [t5890] VALUES (46,'حرف الواو','1021','0','0');
+INSERT INTO [t5890] VALUES (47,'حرف الياء','1023','0','0');
+INSERT INTO [t5890] VALUES (48,'ذكر من اسمه يزيد','1053','47','0');
+INSERT INTO [t5890] VALUES (49,'من اسمه يعقوب','1055','47','0');
+INSERT INTO [t5890] VALUES (50,'ذكر من اسمه يوسف','1063','47','0');
+INSERT INTO [t5890] VALUES (51,'ذكر من اشتهر بكنيته ولم يذكر له اسم','1084','0','0');
