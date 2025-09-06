965ca1129661d08f3cb5c40c2c602d1acb8d4d6e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:56 +0700

    Upload SQL chunk: t30505_chunk_0000.sql

---
+++ sql_chunks/t30505_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30505
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30505" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30505
+INSERT INTO [t30505] VALUES (1,'الجزء الرابع من الفوائد المنتقاة الحسان من الصحاح والغرائب','1','0','0');
