a64e9c384e84ded0f367f10208c52e6f28c25f7b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:35:41 +0700

    Upload SQL chunk: t6999_chunk_0000.sql

---
+++ sql_chunks/t6999_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t6999
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t6999" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6999
+INSERT INTO [t6999] VALUES (1,'(حرف الألف)','3','0','0');
+INSERT INTO [t6999] VALUES (2,'(حرف الباء)','7','0','0');
+INSERT INTO [t6999] VALUES (3,'(حرف التاء)','10','0','0');
+INSERT INTO [t6999] VALUES (4,'(حرف الثاء)','12','0','0');
+INSERT INTO [t6999] VALUES (5,'(حرف الجيم)','15','0','0');
+INSERT INTO [t6999] VALUES (6,'(حرف الحاء)','17','0','0');
+INSERT INTO [t6999] VALUES (7,'(حرف الخاء)','19','0','0');
+INSERT INTO [t6999] VALUES (8,'(حرف الدال)','22','0','0');
+INSERT INTO [t6999] VALUES (9,'(حرف الذال)','25','0','0');
+INSERT INTO [t6999] VALUES (10,'(حرف الراء)','26','0','0');
+INSERT INTO [t6999] VALUES (11,'(حرف الزاء)','28','0','0');
+INSERT INTO [t6999] VALUES (12,'(حرف السين)','31','0','0');
+INSERT INTO [t6999] VALUES (13,'(حرف الشين)','34','0','0');
+INSERT INTO [t6999] VALUES (14,'(حرف الصاد)','36','0','0');
+INSERT INTO [t6999] VALUES (15,'(حرف الضاد)','38','0','0');
+INSERT INTO [t6999] VALUES (16,'(حرف الطاء)','40','0','0');
+INSERT INTO [t6999] VALUES (17,'(حرف الظاء)','42','0','0');
+INSERT INTO [t6999] VALUES (18,'(حرف العين)','42','0','0');
+INSERT INTO [t6999] VALUES (19,'(حرف الغين)','45','0','0');
+INSERT INTO [t6999] VALUES (20,'(حرف الفاء)','46','0','0');
+INSERT INTO [t6999] VALUES (21,'(حرف القاف)','47','0','0');
+INSERT INTO [t6999] VALUES (22,'(حرف الكاف)','49','0','0');
+INSERT INTO [t6999] VALUES (23,'(حرف اللام)','51','0','0');
+INSERT INTO [t6999] VALUES (24,'(حرف الميم)','55','0','0');
+INSERT INTO [t6999] VALUES (25,'(حرف النون)','58','0','0');
+INSERT INTO [t6999] VALUES (26,'(حرف الهاء)','62','0','0');
+INSERT INTO [t6999] VALUES (27,'(حرف الواو)','63','0','0');
+INSERT INTO [t6999] VALUES (28,'(حرف الياء)','64','0','0');
