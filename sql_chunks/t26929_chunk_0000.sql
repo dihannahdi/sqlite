b86c9a903ea29c458b91d8ad4b6c035442684147
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:54:26 +0700

    Upload SQL chunk: t26929_chunk_0000.sql

---
+++ sql_chunks/t26929_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t26929
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t26929" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26929
+INSERT INTO [t26929] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26929] VALUES (2,'محمد صلى الله عليه وسلم','2','0','0');
+INSERT INTO [t26929] VALUES (3,'محمد بن عبد الله','4','0','0');
+INSERT INTO [t26929] VALUES (4,'ابن عبد المطلب','6','0','0');
+INSERT INTO [t26929] VALUES (5,'ابن هاشم','6','0','0');
+INSERT INTO [t26929] VALUES (6,'ابن عبد مناف','8','0','0');
+INSERT INTO [t26929] VALUES (7,'ابن قصي','8','0','0');
+INSERT INTO [t26929] VALUES (8,'ابن كلاب','9','0','0');
+INSERT INTO [t26929] VALUES (9,'ابن مرة','10','0','0');
+INSERT INTO [t26929] VALUES (10,'ابن كعب','11','0','0');
+INSERT INTO [t26929] VALUES (11,'ابن لؤي','11','0','0');
+INSERT INTO [t26929] VALUES (12,'ابن غالب','12','0','0');
+INSERT INTO [t26929] VALUES (13,'ابن فهر','13','0','0');
+INSERT INTO [t26929] VALUES (14,'ابن مالك','14','0','0');
+INSERT INTO [t26929] VALUES (15,'ابن النضر','14','0','0');
+INSERT INTO [t26929] VALUES (16,'ابن كنانة','14','0','0');
+INSERT INTO [t26929] VALUES (17,'ابن خزيمة','15','0','0');
+INSERT INTO [t26929] VALUES (18,'ابن مدركة','15','0','0');
+INSERT INTO [t26929] VALUES (19,'ابن إلياس','16','0','0');
+INSERT INTO [t26929] VALUES (20,'ابن مضر','16','0','0');
+INSERT INTO [t26929] VALUES (21,'ابن نزار','16','0','0');
+INSERT INTO [t26929] VALUES (22,'ابن معد','17','0','0');
