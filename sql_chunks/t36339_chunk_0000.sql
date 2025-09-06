43e2a86a9b515e6285057b27844711c4ee1f182e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:23 +0700

    Upload SQL chunk: t36339_chunk_0000.sql

---
+++ sql_chunks/t36339_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t36339
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t36339" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36339
+INSERT INTO [t36339] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36339] VALUES (2,'النفوس ثلاثة','2','0','0');
+INSERT INTO [t36339] VALUES (3,'الخاتمة','10','0','0');
