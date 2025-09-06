83d37789e2d08dab00508f3f70d4285f3974118e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:59 +0700

    Upload SQL chunk: t8182_chunk_0000.sql

---
+++ sql_chunks/t8182_chunk_0000.sql
@@ -0,0 +1,54 @@
+-- Table: t8182
+-- Chunk: 1
+-- Rows: 47
+
+CREATE TABLE "t8182" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8182
+INSERT INTO [t8182] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t8182] VALUES (2,'كتاب الطهارة','2','0','0');
+INSERT INTO [t8182] VALUES (3,'كتاب الصلاة','8','0','0');
+INSERT INTO [t8182] VALUES (4,'كتاب الزكاة','37','0','0');
+INSERT INTO [t8182] VALUES (5,'كتاب الصوم','55','0','0');
+INSERT INTO [t8182] VALUES (6,'كتاب المناسك','62','0','0');
+INSERT INTO [t8182] VALUES (7,'كتاب النكاح','81','0','0');
+INSERT INTO [t8182] VALUES (8,'كتاب الطلاق','128','0','0');
+INSERT INTO [t8182] VALUES (9,'كتاب العتاق','197','0','0');
+INSERT INTO [t8182] VALUES (10,'كتاب المكاتب والولاء','217','0','0');
+INSERT INTO [t8182] VALUES (11,'كتاب الأيمان والنذور والكفارات','235','0','0');
+INSERT INTO [t8182] VALUES (12,'كتاب الحدود','254','0','0');
+INSERT INTO [t8182] VALUES (13,'كتاب السرقة','276','0','0');
+INSERT INTO [t8182] VALUES (14,'كتاب السير','286','0','0');
+INSERT INTO [t8182] VALUES (15,'فصل قتال سريتان من المسلمين ظنتا خطئا أن الأخرى من المشركين','309','14','0');
+INSERT INTO [t8182] VALUES (16,'فصل اشترى المستأمن من أرض الخراج','312','14','0');
+INSERT INTO [t8182] VALUES (17,'كتاب الاستحسان والتحري والإباق','315','0','0');
+INSERT INTO [t8182] VALUES (18,'كتاب الغصب','331','0','0');
+INSERT INTO [t8182] VALUES (19,'كتاب الصيد والذبائح','342','0','0');
+INSERT INTO [t8182] VALUES (20,'كتاب العارية','350','0','0');
+INSERT INTO [t8182] VALUES (21,'كتاب الوديعة','353','0','0');
+INSERT INTO [t8182] VALUES (22,'كتاب الشركة','356','0','0');
+INSERT INTO [t8182] VALUES (23,'كتاب الهبة','369','0','0');
+INSERT INTO [t8182] VALUES (24,'كتاب البيوع','375','0','0');
+INSERT INTO [t8182] VALUES (25,'فصل إذا اشترى دابة أو قميصا على أنه بالخيار','386','24','0');
+INSERT INTO [t8182] VALUES (26,'كتاب الصرف','425','0','0');
+INSERT INTO [t8182] VALUES (27,'كتاب الشفعة','440','0','0');
+INSERT INTO [t8182] VALUES (28,'كتاب القسمة','459','0','0');
+INSERT INTO [t8182] VALUES (29,'كتاب الإجارات','464','0','0');
+INSERT INTO [t8182] VALUES (30,'كتاب الشهادات','476','0','0');
+INSERT INTO [t8182] VALUES (31,'كتاب الدعوى','492','0','0');
+INSERT INTO [t8182] VALUES (32,'كتاب الإقرار','513','0','0');
+INSERT INTO [t8182] VALUES (33,'كتاب الوكالة','540','0','0');
+INSERT INTO [t8182] VALUES (34,'كتاب الكفالة والحوالة','563','0','0');
+INSERT INTO [t8182] VALUES (35,'كتاب الصلح','573','0','0');
+INSERT INTO [t8182] VALUES (36,'كتاب الإكراه','584','0','0');
+INSERT INTO [t8182] VALUES (37,'كتاب الحجر','593','0','0');
+INSERT INTO [t8182] VALUES (38,'كتاب المضاربة','596','0','0');
+INSERT INTO [t8182] VALUES (39,'كتاب الشرب','603','0','0');
+INSERT INTO [t8182] VALUES (40,'كتاب الأشربة','607','0','0');
+INSERT INTO [t8182] VALUES (41,'كتاب الرهن','609','0','0');
+INSERT INTO [t8182] VALUES (42,'كتاب الوصايا','618','0','0');
+INSERT INTO [t8182] VALUES (43,'كتاب المأذون','640','0','0');
+INSERT INTO [t8182] VALUES (44,'فصل اشترى المحجور عليه عبدا فأجازه المولى','650','43','0');
+INSERT INTO [t8182] VALUES (45,'كتاب الديات والجنايات','653','0','0');
+INSERT INTO [t8182] VALUES (46,'مسائل متفرقة','659','45','0');
+INSERT INTO [t8182] VALUES (47,'مسألة رجل وكل رجلا بالبيع والشراء ففعل غيره فأجاز الموكل','659','45','0');
