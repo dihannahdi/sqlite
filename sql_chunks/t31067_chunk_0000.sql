caa6f9e587af2c6712e824ff7b064dbfd88fcba1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:03 +0700

    Upload SQL chunk: t31067_chunk_0000.sql

---
+++ sql_chunks/t31067_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t31067
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t31067" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31067
+INSERT INTO [t31067] VALUES (1,'كلمة بين يدي البحث','1','0','0');
+INSERT INTO [t31067] VALUES (2,'الفصل الأول من آداب الوقوف على المنبر','5','0','0');
+INSERT INTO [t31067] VALUES (3,'الفصل الثاني وقفات سريعة حول خطبة الجمعة','13','0','0');
+INSERT INTO [t31067] VALUES (4,'الفصل الثالث أخطاء ينبغي تجنبها على منبر الجمعة','24','0','0');
+INSERT INTO [t31067] VALUES (5,'الفصل الرابع اقتراحات تتعلق بالخطيب وبموضوع الخطبة','33','0','0');
+INSERT INTO [t31067] VALUES (6,'الخاتمة','46','0','0');
