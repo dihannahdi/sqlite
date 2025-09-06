c49ae72b4f89067245a8f033ee4a027c3016f3fb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:02:19 +0700

    Upload SQL chunk: t30871_chunk_0000.sql

---
+++ sql_chunks/t30871_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t30871
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t30871" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30871
+INSERT INTO [t30871] VALUES (1,'[المقدمة]','1','0','0');
+INSERT INTO [t30871] VALUES (2,'[المبحث الأول حقيقة الولاء والبراء]','2','0','0');
+INSERT INTO [t30871] VALUES (3,'[المبحث الثاني أدلة الولاء والبراء]','3','0','0');
+INSERT INTO [t30871] VALUES (4,'[أدلته من الكتاب العزيز]','4','3','0');
+INSERT INTO [t30871] VALUES (5,'[أدلته من السنة]','5','3','0');
+INSERT INTO [t30871] VALUES (6,'الاستدلال للولاء والبراء بالإجماع','6','3','0');
+INSERT INTO [t30871] VALUES (7,'المبحث الثالث علاقته بأصل الإيمان','7','0','0');
+INSERT INTO [t30871] VALUES (8,'المبحث الرابع توافق الولاء والبراء مع سماحة الإسلام','9','0','0');
+INSERT INTO [t30871] VALUES (9,'[المبحث الخامس مظاهر الغلو في الولاء والبراء وبراءته منها]','15','0','0');
+INSERT INTO [t30871] VALUES (10,'[مظاهر غلو الإفراط]','15','9','0');
+INSERT INTO [t30871] VALUES (11,'مظاهر غلو التفريط','18','9','0');
+INSERT INTO [t30871] VALUES (12,'الخاتمة','19','0','0');
+INSERT INTO [t30871] VALUES (13,'[أهم نتائج البحث]','19','12','0');
+INSERT INTO [t30871] VALUES (14,'التوصيات','20','12','0');
+INSERT INTO [t30871] VALUES (15,'المصادر والمراجع','21','0','0');
