867b8d9ae6ceee5ca30657e0bdb019d3ac8cd37b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:27 +0700

    Upload SQL chunk: t6650_chunk_0000.sql

---
+++ sql_chunks/t6650_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6650
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6650" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6650
+INSERT INTO [t6650] VALUES (1,'ذكر أخبار عمر بن عبد العزيز رحمه الله وسيرته في المسلمين حتي توفي رحمه الله','1','0','0');
+INSERT INTO [t6650] VALUES (2,'بسم الله الرحمن الرحيم','13','0','0');
