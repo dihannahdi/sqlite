d91a48ff0a992ec8f5d52db193a1715b4271e83e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:40:58 +0700

    Upload SQL chunk: t21650_chunk_0000.sql

---
+++ sql_chunks/t21650_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t21650
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t21650" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21650
+INSERT INTO [t21650] VALUES (1,'كرامات الأولياء','2','0','0');
