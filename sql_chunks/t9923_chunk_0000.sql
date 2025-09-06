b50eeab4b57c323f8b3b6347582bcbf80e3f2bc1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:04:01 +0700

    Upload SQL chunk: t9923_chunk_0000.sql

---
+++ sql_chunks/t9923_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t9923
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t9923" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9923
+INSERT INTO [t9923] VALUES (1,'ترجمة المؤلف - شيخ الإسلام الإمام محمد بن عبد الوهاب','1','0','0');
+INSERT INTO [t9923] VALUES (2,'ترجمة الشارح - الشيخ محمد بن صالح العثيمين رحمه الله محمد بن صالح العثيمين','3','0','0');
+INSERT INTO [t9923] VALUES (3,'المقدمة','6','0','0');
+INSERT INTO [t9923] VALUES (4,'شرح كشف الشبهات','7','0','0');
+INSERT INTO [t9923] VALUES (5,'شرح الأصول الستة','100','0','0');
+INSERT INTO [t9923] VALUES (6,'الأصل الأول','104','5','0');
+INSERT INTO [t9923] VALUES (7,'الأصل الثاني','110','5','0');
+INSERT INTO [t9923] VALUES (8,'الأصل الثالث','117','5','0');
+INSERT INTO [t9923] VALUES (9,'الأصل الرابع','122','5','0');
+INSERT INTO [t9923] VALUES (10,'والعلم له فضائل كثيرة:','124','9','0');
+INSERT INTO [t9923] VALUES (11,'الأصل الخامس','127','5','0');
+INSERT INTO [t9923] VALUES (12,'الأصل السادس','137','5','0');
