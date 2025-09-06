f94b86f8cb2b4a3f6a590b21c6f5cb1a1298aa08
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:02 +0700

    Upload SQL chunk: t439_chunk_0000.sql

---
+++ sql_chunks/t439_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t439
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t439" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t439
+INSERT INTO [t439] VALUES (1,'متى فرضت صلاة الجمعة','2','0','0');
+INSERT INTO [t439] VALUES (2,'فصل (في إضافة كلمة اليوم أو الصلاة إلى «الجمعة»)','6','0','0');
+INSERT INTO [t439] VALUES (3,'الأسماء القديمة لأيام الأسبوع في الجاهلية','10','0','0');
+INSERT INTO [t439] VALUES (4,'هل أسماء الأيام إسلامية أم مما تكلمت به العرب قديما','11','0','0');
+INSERT INTO [t439] VALUES (5,'فصل: ما وجه التسمية (بيوم الجمعة)','14','0','0');
+INSERT INTO [t439] VALUES (6,'الكلام في نسخة السدي المسندة من التفسير','16','0','0');
+INSERT INTO [t439] VALUES (7,'فصل (في إضافة الصلاة إلى لفظ الجمعة)','19','0','0');
