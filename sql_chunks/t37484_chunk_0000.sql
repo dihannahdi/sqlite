35066db550dee1cc28fde27d89de8a21703c38d6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:28 +0700

    Upload SQL chunk: t37484_chunk_0000.sql

---
+++ sql_chunks/t37484_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t37484
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t37484" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37484
+INSERT INTO [t37484] VALUES (1,'باب الكلام وأقسامه وعلامة العلم وأعلامه','3','0','0');
+INSERT INTO [t37484] VALUES (2,'باب الاسم واشتقاقه','4','0','0');
+INSERT INTO [t37484] VALUES (3,'باب الاسم وأقسامه','5','0','0');
+INSERT INTO [t37484] VALUES (4,'فصل فيما ينصرف وما لا ينصرف','6','0','0');
+INSERT INTO [t37484] VALUES (5,'باب الإعراب والبناء','8','0','0');
+INSERT INTO [t37484] VALUES (6,'باب المبتدأ والخبر','8','0','0');
+INSERT INTO [t37484] VALUES (7,'باب الأفعال وأقسامها','10','0','0');
+INSERT INTO [t37484] VALUES (8,'باب الفاعل والمفعول','12','0','0');
+INSERT INTO [t37484] VALUES (9,'باب الحال','13','0','0');
+INSERT INTO [t37484] VALUES (10,'باب التمييز','13','0','0');
+INSERT INTO [t37484] VALUES (11,'باب البدل','14','0','0');
+INSERT INTO [t37484] VALUES (12,'باب الصفة','16','0','0');
+INSERT INTO [t37484] VALUES (13,'باب العطف','17','0','0');
+INSERT INTO [t37484] VALUES (14,'باب التوكيد','18','0','0');
+INSERT INTO [t37484] VALUES (15,'باب حروف الجر','18','0','0');
+INSERT INTO [t37484] VALUES (16,'باب لا النافية وما فيها من الأسرار الخافية','19','0','0');
+INSERT INTO [t37484] VALUES (17,'باب في ألفاظ مختلفة أفادت معاني مختلفة','21','0','0');
+INSERT INTO [t37484] VALUES (18,'باب الندبة','24','0','0');
