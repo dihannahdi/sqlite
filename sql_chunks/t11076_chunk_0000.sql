10772e13a79efe5ee32d334e397463445c934639
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:55 +0700

    Upload SQL chunk: t11076_chunk_0000.sql

---
+++ sql_chunks/t11076_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11076
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11076" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11076
+INSERT INTO [t11076] VALUES (1,'تأثر الدعوات الإصلاحية في أندونيسيا بدعوة الشيخ محمد بن عبد الوهاب','1','0','0');
+INSERT INTO [t11076] VALUES (2,'للأستاذ نجيح عبد الله','1','0','0');
