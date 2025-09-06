8e24bd5d99232ca22126fe2ef5ac1255b736a9bb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:17:17 +0700

    Upload SQL chunk: t14246_chunk_0000.sql

---
+++ sql_chunks/t14246_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t14246
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t14246" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14246
+INSERT INTO [t14246] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t14246] VALUES (2,'المقدمات','10','0','0');
+INSERT INTO [t14246] VALUES (3,'السماع','51','0','0');
+INSERT INTO [t14246] VALUES (4,'الإجماع','141','0','0');
+INSERT INTO [t14246] VALUES (5,'القياس','156','0','0');
+INSERT INTO [t14246] VALUES (6,'الاستصحاب','327','0','0');
+INSERT INTO [t14246] VALUES (7,'أدلة شتى','334','0','0');
+INSERT INTO [t14246] VALUES (8,'التعارض والتراجيح','353','0','0');
+INSERT INTO [t14246] VALUES (9,'أحوال مستنبط هذا العلم','394','0','0');
