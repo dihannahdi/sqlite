9615bb28d4e63af95235c4fd7019b68ad6c8dff2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:51 +0700

    Upload SQL chunk: t21840_chunk_0000.sql

---
+++ sql_chunks/t21840_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t21840
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t21840" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21840
+INSERT INTO [t21840] VALUES (1,'دور المسلم في الثلث الأخير من القرن العشرين','8','0','0');
+INSERT INTO [t21840] VALUES (2,'رسالة المسلم في الثلث الأخير من القرن العشرين','40','0','0');
