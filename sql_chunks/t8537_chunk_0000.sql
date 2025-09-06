6808bf37a5ab3b33dba582cc5c53f30beed187f1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:46:57 +0700

    Upload SQL chunk: t8537_chunk_0000.sql

---
+++ sql_chunks/t8537_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t8537
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t8537" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8537
+INSERT INTO [t8537] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8537] VALUES (2,'الحقبة الأولى (١٩٢٤-١٩٤٩)','2','0','0');
+INSERT INTO [t8537] VALUES (3,'الحقبة الثانية (١٩٤٩-١٩٨٢)','3','0','0');
+INSERT INTO [t8537] VALUES (4,'الحقبة الثالثة (١٩٨٢-٢٠٠٣)','5','0','0');
+INSERT INTO [t8537] VALUES (5,'الأعمال في الحقبة الأولى (١٩٢٤-١٩٤٩)','7','0','0');
+INSERT INTO [t8537] VALUES (6,'الأعمال في الحقبتين الثانية والثالثة','9','0','0');
+INSERT INTO [t8537] VALUES (7,'حجية السنة ومكانتها','9','6','0');
+INSERT INTO [t8537] VALUES (8,'تاريخ الحديث','12','6','0');
+INSERT INTO [t8537] VALUES (9,'علوم الحديث','13','6','0');
+INSERT INTO [t8537] VALUES (10,'أصول الحديث ومصطلحاته','16','6','0');
+INSERT INTO [t8537] VALUES (11,'مصادر الحديث','20','6','0');
+INSERT INTO [t8537] VALUES (12,'علم الرجال والجرح والتعديل','23','6','0');
+INSERT INTO [t8537] VALUES (13,'المحدثون وآثارهم','25','6','0');
+INSERT INTO [t8537] VALUES (14,'بحوث في جغرافيا الحديث','29','6','0');
+INSERT INTO [t8537] VALUES (15,'فهم الحديث والسنة','31','6','0');
+INSERT INTO [t8537] VALUES (16,'شرح الحديث وتأويله','33','6','0');
+INSERT INTO [t8537] VALUES (17,'نقد الحديث','36','6','0');
+INSERT INTO [t8537] VALUES (18,'الأحاديث الموضوعة','38','6','0');
+INSERT INTO [t8537] VALUES (19,'مناقشات المحدثين','40','6','0');
+INSERT INTO [t8537] VALUES (20,'الحديث واللغة','42','6','0');
+INSERT INTO [t8537] VALUES (21,'الحديث والثقافة','43','6','0');
+INSERT INTO [t8537] VALUES (22,'الحديث والتربية','45','6','0');
+INSERT INTO [t8537] VALUES (23,'الحديث والكلام','47','6','0');
+INSERT INTO [t8537] VALUES (24,'الحديث والمذاهب','49','6','0');
+INSERT INTO [t8537] VALUES (25,'الحديث والتصوف','51','6','0');
+INSERT INTO [t8537] VALUES (26,'بحوث عن النبي صلى الله عليه وسلم','53','6','0');
+INSERT INTO [t8537] VALUES (27,'النبي والسياسة','57','6','0');
+INSERT INTO [t8537] VALUES (28,'بحوث عن الصحابة','60','6','0');
+INSERT INTO [t8537] VALUES (29,'بحوث عن الموضوعات المختلفة','62','6','0');
+INSERT INTO [t8537] VALUES (30,'الأعمال في مجال التحقيق','64','6','0');
+INSERT INTO [t8537] VALUES (31,'النتيجة','66','0','0');
+INSERT INTO [t8537] VALUES (32,'مدخل','66','31','0');
+INSERT INTO [t8537] VALUES (33,'رسائل الماجستير','72','31','0');
+INSERT INTO [t8537] VALUES (34,'الندوات','74','31','0');
+INSERT INTO [t8537] VALUES (35,'المجلات','76','31','0');
+INSERT INTO [t8537] VALUES (36,'المقالات','77','31','0');
+INSERT INTO [t8537] VALUES (37,'الموسوعة الإسلامية','79','31','0');
+INSERT INTO [t8537] VALUES (38,'مصادر ومراجع','81','0','0');
