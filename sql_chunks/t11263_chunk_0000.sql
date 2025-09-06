585b223f124d0ef6152a06d60900e82e9a073fdf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:12 +0700

    Upload SQL chunk: t11263_chunk_0000.sql

---
+++ sql_chunks/t11263_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t11263
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t11263" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11263
+INSERT INTO [t11263] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11263] VALUES (2,'تمهيد','3','0','0');
+INSERT INTO [t11263] VALUES (3,'كتاب الصلاة','5','0','0');
+INSERT INTO [t11263] VALUES (4,'باب آداب المشي إلى الصلاة','5','3','0');
+INSERT INTO [t11263] VALUES (5,'باب صفة الصلاة','12','3','0');
+INSERT INTO [t11263] VALUES (6,'باب صلاة التطوع','81','3','0');
+INSERT INTO [t11263] VALUES (7,'باب صلاة الجماعة','113','3','0');
+INSERT INTO [t11263] VALUES (8,'باب صلاة أهل الأعذار','132','3','0');
+INSERT INTO [t11263] VALUES (9,'باب صلاة الجمعة','140','3','0');
+INSERT INTO [t11263] VALUES (10,'باب صلاة العيدين','151','3','0');
+INSERT INTO [t11263] VALUES (11,'باب صلاة الكسوف','158','3','0');
+INSERT INTO [t11263] VALUES (12,'باب صلاة الاستسقاء','161','3','0');
+INSERT INTO [t11263] VALUES (13,'باب الجنائز','168','3','0');
+INSERT INTO [t11263] VALUES (14,'كتاب الزكاة','191','0','0');
+INSERT INTO [t11263] VALUES (15,'مدخل','191','14','0');
+INSERT INTO [t11263] VALUES (16,'باب زكاة بهيمة الأنعام','197','14','0');
+INSERT INTO [t11263] VALUES (17,'باب زكاة الخارج من الأرض','203','14','0');
+INSERT INTO [t11263] VALUES (18,'باب زكاة النقدين','207','14','0');
+INSERT INTO [t11263] VALUES (19,'باب زكاة العروض','211','14','0');
+INSERT INTO [t11263] VALUES (20,'باب زكاة الفطر','212','14','0');
+INSERT INTO [t11263] VALUES (21,'باب إخراج الزكاة','215','14','0');
+INSERT INTO [t11263] VALUES (22,'باب أهل الزكاة','217','14','0');
+INSERT INTO [t11263] VALUES (23,'كتاب الصيام','224','0','0');
+INSERT INTO [t11263] VALUES (24,'مدخل','224','23','0');
+INSERT INTO [t11263] VALUES (25,'باب ما يفسد الصوم','229','23','0');
