c2648c4cc9f73529cdb0865568b649cbb6628931
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:59 +0700

    Upload SQL chunk: t36403_chunk_0000.sql

---
+++ sql_chunks/t36403_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t36403
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t36403" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36403
+INSERT INTO [t36403] VALUES (1,'حياة القلب بالإيمان هي الهدف','2','0','0');
+INSERT INTO [t36403] VALUES (2,'العبادات وسائل','3','0','0');
+INSERT INTO [t36403] VALUES (3,'تهيئة الأجواء لتحقيق الهدف','3','0','0');
+INSERT INTO [t36403] VALUES (4,'غياب الرؤية','4','0','0');
+INSERT INTO [t36403] VALUES (5,'إحسان العمل أولا','4','0','0');
+INSERT INTO [t36403] VALUES (6,'سل الواقع','5','0','0');
+INSERT INTO [t36403] VALUES (7,'تحصيل الثواب','5','0','0');
+INSERT INTO [t36403] VALUES (8,'أين الثمرة؟','6','0','0');
+INSERT INTO [t36403] VALUES (9,'إحسان ثم إكثار','7','0','0');
+INSERT INTO [t36403] VALUES (10,'الفهم الصحيح أولا','7','0','0');
+INSERT INTO [t36403] VALUES (11,'حتى لا يضيع علينا رمضان','7','0','0');
+INSERT INTO [t36403] VALUES (12,'الغنيمة الباردة','8','0','0');
+INSERT INTO [t36403] VALUES (13,'في رمضان العمل مع أنفسنا أهم واجباتنا','8','0','0');
+INSERT INTO [t36403] VALUES (14,'البنا يؤكد','9','0','0');
