2cbf1bf312602a1741b2b91486f39d2813563b01
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:33 +0700

    Upload SQL chunk: t36366_chunk_0000.sql

---
+++ sql_chunks/t36366_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t36366
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t36366" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36366
+INSERT INTO [t36366] VALUES (1,'الحمد لله البر الرحيم','1','0','0');
+INSERT INTO [t36366] VALUES (2,'أخي المسلم: خلق الله آدم عليه الصلاة والسلام بيده ونفخ فيه من روحه','1','0','0');
+INSERT INTO [t36366] VALUES (3,'أخي في الله: ذلك هو إبليس علم المعاصي وسيد العصاة وكل عاص لله تعالى','2','0','0');
+INSERT INTO [t36366] VALUES (4,'أخي المسلم: لا تقربن أدغال المعاصي والذنوب','3','0','0');
+INSERT INTO [t36366] VALUES (5,'أخي: المعاصي شر ما ألفها قلب فأفلح','5','0','0');
+INSERT INTO [t36366] VALUES (6,'أخي المسلم: تلك هي عواقب المعاصي','9','0','0');
+INSERT INTO [t36366] VALUES (7,'فمنها: حرمان العلم .. ومنها','10','0','0');
