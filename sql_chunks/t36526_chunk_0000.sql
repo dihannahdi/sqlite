9fdf6b68bb67a609ba0e4b68cccb6e14991485f1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:09:47 +0700

    Upload SQL chunk: t36526_chunk_0000.sql

---
+++ sql_chunks/t36526_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t36526
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t36526" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36526
+INSERT INTO [t36526] VALUES (1,'الحمد لله قابل التوب شديد العقاب ..','1','0','0');
+INSERT INTO [t36526] VALUES (2,'أخي المسلم .. بمثل ذلك أقبل التائبون على الله','2','0','0');
+INSERT INTO [t36526] VALUES (3,'أخي في الله: عليك بالسير على درب الصالحين ..','3','0','0');
+INSERT INTO [t36526] VALUES (4,'أخي في الله: إنها التوبة ..','4','0','0');
+INSERT INTO [t36526] VALUES (5,'أخي في الله: التوبة','5','0','0');
+INSERT INTO [t36526] VALUES (6,'أخي: فها هي التوبة معروضة ..','6','0','0');
+INSERT INTO [t36526] VALUES (7,'أخي في الله','9','0','0');
