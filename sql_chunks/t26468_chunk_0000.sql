918ef5a7d4057485512d634f6e26d5048a365636
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:41 +0700

    Upload SQL chunk: t26468_chunk_0000.sql

---
+++ sql_chunks/t26468_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26468
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26468" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26468
+INSERT INTO [t26468] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26468] VALUES (2,'فضل شهر رمضان لابن عساكر','3','0','0');
