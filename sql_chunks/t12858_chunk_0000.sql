4da3608b6c42bd3c5effce06eae829c01b4e9aa5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:08:03 +0700

    Upload SQL chunk: t12858_chunk_0000.sql

---
+++ sql_chunks/t12858_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t12858
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t12858" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12858
+INSERT INTO [t12858] VALUES (1,'المدخل زهير بن نمير','1','0','0');
+INSERT INTO [t12858] VALUES (2,'الفصل الأول','5','1','0');
+INSERT INTO [t12858] VALUES (3,'توابع الشعراء','5','0','0');
+INSERT INTO [t12858] VALUES (4,'شيطان امرئ القيس','5','3','0');
+INSERT INTO [t12858] VALUES (5,'شيطان طرفة','7','3','0');
+INSERT INTO [t12858] VALUES (6,'شيطان قيس بن الخطيم','10','3','0');
+INSERT INTO [t12858] VALUES (7,'صاحب أبي تمام','12','3','0');
+INSERT INTO [t12858] VALUES (8,'صاحب البحتري','16','3','0');
+INSERT INTO [t12858] VALUES (9,'صاحب أبي نواس','18','3','0');
+INSERT INTO [t12858] VALUES (10,'صاحب أبي الطيب','25','3','0');
+INSERT INTO [t12858] VALUES (11,'الفصل الثاني','29','3','0');
+INSERT INTO [t12858] VALUES (12,'توابع الكتاب','29','0','0');
+INSERT INTO [t12858] VALUES (13,'صاحبا الجاحظ وعبد الحميد','29','12','0');
+INSERT INTO [t12858] VALUES (14,'رسالة الحلواء','33','12','0');
+INSERT INTO [t12858] VALUES (15,'صاحب الإفليلي','38','12','0');
+INSERT INTO [t12858] VALUES (16,'صفة برغوث','39','12','0');
+INSERT INTO [t12858] VALUES (17,'صفة ثعلب','40','12','0');
+INSERT INTO [t12858] VALUES (18,'صاحب بديع الزمان','41','12','0');
+INSERT INTO [t12858] VALUES (19,'رجع إلى أنف الناقة','43','12','0');
+INSERT INTO [t12858] VALUES (20,'صاحب أبي إسحاق بن حمام','45','12','0');
+INSERT INTO [t12858] VALUES (21,'الفصل الثالث','46','12','0');
+INSERT INTO [t12858] VALUES (22,'نقاد الجن','46','12','0');
+INSERT INTO [t12858] VALUES (23,'مجلس أدب','46','12','0');
+INSERT INTO [t12858] VALUES (24,'الفصل الرابع','61','12','0');
+INSERT INTO [t12858] VALUES (25,'حيوان الجن','61','0','0');
+INSERT INTO [t12858] VALUES (26,'لغة الحمير','61','25','0');
+INSERT INTO [t12858] VALUES (27,'الإوزة الأدبية','63','25','0');
