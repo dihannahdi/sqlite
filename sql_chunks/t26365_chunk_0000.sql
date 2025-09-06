bf32a7fb76a4d4f9db7a976f4c3bd56aa46f683b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:51:04 +0700

    Upload SQL chunk: t26365_chunk_0000.sql

---
+++ sql_chunks/t26365_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t26365
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t26365" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26365
+INSERT INTO [t26365] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26365] VALUES (2,'عوالي مالك برواية الحاكم الكبير','2','0','0');
