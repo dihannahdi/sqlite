4da12189ff4c70cbbfd74de8ca2842f0a8cd0c92
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:21 +0700

    Upload SQL chunk: t10354_chunk_0000.sql

---
+++ sql_chunks/t10354_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t10354
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t10354" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10354
+INSERT INTO [t10354] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t10354] VALUES (2,'التدرج في تحريم الخمر','2','0','0');
+INSERT INTO [t10354] VALUES (3,'ماهي الخمر','10','0','0');
+INSERT INTO [t10354] VALUES (4,'أهم الأنواع التي إستحدثت من الخمر','14','0','0');
+INSERT INTO [t10354] VALUES (5,'سر تحريم الخمر','15','0','0');
+INSERT INTO [t10354] VALUES (6,'تأثير الخمر على جسم الإنسان','20','0','0');
+INSERT INTO [t10354] VALUES (7,'حكم التداوي بالخمر','30','0','0');
+INSERT INTO [t10354] VALUES (8,'عقوبة شارب الخمر','36','0','0');
