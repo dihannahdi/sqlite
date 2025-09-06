7005bb0b4fcb4c059be0f5a6d14304b10eaab717
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:33 +0700

    Upload SQL chunk: t500_chunk_0000.sql

---
+++ sql_chunks/t500_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t500
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t500" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t500
+INSERT INTO [t500] VALUES (1,'الباب الأول','2','0','0');
+INSERT INTO [t500] VALUES (2,'المسألة الأولى','3','0','0');
