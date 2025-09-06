f083f6ef6f2d5eeede8e55ead4d6366ec86b3375
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:39 +0700

    Upload SQL chunk: t18085_chunk_0000.sql

---
+++ sql_chunks/t18085_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t18085
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t18085" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t18085
+INSERT INTO [t18085] VALUES (1,'التنبيه الأول','3','0','0');
+INSERT INTO [t18085] VALUES (2,'التنبيه الثاني','6','0','0');
+INSERT INTO [t18085] VALUES (3,'التنبيه الثالث','6','0','0');
+INSERT INTO [t18085] VALUES (4,'التنبيه الرابع','13','0','0');
+INSERT INTO [t18085] VALUES (5,'التنبيه الخامس','17','0','0');
+INSERT INTO [t18085] VALUES (6,'التنبيه السادس','18','0','0');
+INSERT INTO [t18085] VALUES (7,'التنبيه السابع','32','0','0');
+INSERT INTO [t18085] VALUES (8,'التنبيه الثامن','39','0','0');
+INSERT INTO [t18085] VALUES (9,'التنبيه التاسع','47','0','0');
+INSERT INTO [t18085] VALUES (10,'التنبيه العاشر','48','0','0');
+INSERT INTO [t18085] VALUES (11,'التنبيه الحادي عشر','49','0','0');
+INSERT INTO [t18085] VALUES (12,'التنبيه الثاني عشر','51','0','0');
+INSERT INTO [t18085] VALUES (13,'التنبيه الثالث عشر','53','0','0');
