44931f352395454a002dc58238aabe0203b0f7d6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:19 +0700

    Upload SQL chunk: t740_chunk_0000.sql

---
+++ sql_chunks/t740_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t740
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t740" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t740
+INSERT INTO [t740] VALUES (1,'القسم الأول','1','0','0');
+INSERT INTO [t740] VALUES (2,'قصيدة لقيط بن يعمر','2','1','0');
+INSERT INTO [t740] VALUES (3,'قصيدة قعنب','7','1','0');
+INSERT INTO [t740] VALUES (4,'قصيدة أعشى باهلة','9','1','0');
+INSERT INTO [t740] VALUES (5,'قصيدة حاتم الطائي','12','1','0');
+INSERT INTO [t740] VALUES (6,'قصيدة بشامة بن عمرو','15','1','0');
+INSERT INTO [t740] VALUES (7,'قصيدة النمر بن تولب','17','1','0');
+INSERT INTO [t740] VALUES (8,'قصيدة الشنفرى','19','1','0');
+INSERT INTO [t740] VALUES (9,'قصيدة كعب بن سعد الغنوي','26','1','0');
+INSERT INTO [t740] VALUES (10,'قصيدة للمتلمس','28','1','0');
+INSERT INTO [t740] VALUES (11,'وللمتلمس','31','10','0');
+INSERT INTO [t740] VALUES (12,'قصيدة لطرفة بن العبد','34','1','0');
+INSERT INTO [t740] VALUES (13,'ولطرفة أيضا','40','12','0');
+INSERT INTO [t740] VALUES (14,'القسم الثاني','43','0','0');
+INSERT INTO [t740] VALUES (15,'قصيدة لزهير','43','14','0');
+INSERT INTO [t740] VALUES (16,'لزهير أيضا','46','15','0');
+INSERT INTO [t740] VALUES (17,'ولزهير أيضا','47','15','0');
+INSERT INTO [t740] VALUES (18,'ولزهير أيضا','49','15','0');
+INSERT INTO [t740] VALUES (19,'بشر بن أبي خازم','59','14','0');
+INSERT INTO [t740] VALUES (20,'مختار شعر عبيد بن الأبرص الأسدي','73','14','0');
+INSERT INTO [t740] VALUES (21,'القسم الثالث','96','0','0');
+INSERT INTO [t740] VALUES (22,'مختار شعر الحطيئة وأخباره','96','21','0');
