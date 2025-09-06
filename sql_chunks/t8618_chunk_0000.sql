205bcbc66a96ab36e9200a22572d3676e20d4e4c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:50 +0700

    Upload SQL chunk: t8618_chunk_0000.sql

---
+++ sql_chunks/t8618_chunk_0000.sql
@@ -0,0 +1,40 @@
+-- Table: t8618
+-- Chunk: 1
+-- Rows: 33
+
+CREATE TABLE "t8618" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8618
+INSERT INTO [t8618] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t8618] VALUES (2,'كتاب الطهارة','4','0','0');
+INSERT INTO [t8618] VALUES (3,'كتاب الصلاة','45','0','0');
+INSERT INTO [t8618] VALUES (4,'كتاب الجنائز','92','0','0');
+INSERT INTO [t8618] VALUES (5,'كتاب الزكاة','104','0','0');
+INSERT INTO [t8618] VALUES (6,'كتاب الصوم','125','0','0');
+INSERT INTO [t8618] VALUES (7,'كتاب الحج','138','0','0');
+INSERT INTO [t8618] VALUES (8,'كتاب الجهاد','167','0','0');
+INSERT INTO [t8618] VALUES (9,'كتاب البيوع','170','0','0');
+INSERT INTO [t8618] VALUES (10,'كتاب الشركة','204','0','0');
+INSERT INTO [t8618] VALUES (11,'كتاب العارية','207','0','0');
+INSERT INTO [t8618] VALUES (12,'كتاب الغصب','212','0','0');
+INSERT INTO [t8618] VALUES (13,'كتاب الوقف','224','0','0');
+INSERT INTO [t8618] VALUES (14,'كتاب الوصايا','240','0','0');
+INSERT INTO [t8618] VALUES (15,'كتاب الفرائض','248','0','0');
+INSERT INTO [t8618] VALUES (16,'كتاب العتق','311','0','0');
+INSERT INTO [t8618] VALUES (17,'كتاب النكاح','324','0','0');
+INSERT INTO [t8618] VALUES (18,'كتاب الصداق','344','0','0');
+INSERT INTO [t8618] VALUES (19,'كتاب الخلع','360','0','0');
+INSERT INTO [t8618] VALUES (20,'كتاب الطلاق','372','0','0');
+INSERT INTO [t8618] VALUES (21,'كتاب الإيلاء','396','0','0');
+INSERT INTO [t8618] VALUES (22,'كتاب الظهار','402','0','0');
+INSERT INTO [t8618] VALUES (23,'كتاب اللعان','406','0','0');
+INSERT INTO [t8618] VALUES (24,'كتاب العدة','411','0','0');
+INSERT INTO [t8618] VALUES (25,'كتاب الرضاع','424','0','0');
+INSERT INTO [t8618] VALUES (26,'كتاب النفقات','427','0','0');
+INSERT INTO [t8618] VALUES (27,'كتاب الجنايات','431','0','0');
+INSERT INTO [t8618] VALUES (28,'كتاب الديات','442','0','0');
+INSERT INTO [t8618] VALUES (29,'كتاب الحدود','469','0','0');
+INSERT INTO [t8618] VALUES (30,'كتاب الأطعمة','483','0','0');
+INSERT INTO [t8618] VALUES (31,'كتاب الأيمان','499','0','0');
+INSERT INTO [t8618] VALUES (32,'كتاب القضاء','507','0','0');
+INSERT INTO [t8618] VALUES (33,'كتاب الشهادات','509','0','0');
