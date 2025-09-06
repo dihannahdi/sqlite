58c24bb7b348148de0f93531298d4f101c2db33c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:30 +0700

    Upload SQL chunk: t30631_chunk_0000.sql

---
+++ sql_chunks/t30631_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30631
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30631" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30631
+INSERT INTO [t30631] VALUES (1,'مدرج على شيوخ يوسف القواس','6','0','0');
