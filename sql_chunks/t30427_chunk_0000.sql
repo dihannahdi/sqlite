f7334b91de0d3b6835eeff6e0a8585e788c4f8af
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:41 +0700

    Upload SQL chunk: t30427_chunk_0000.sql

---
+++ sql_chunks/t30427_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t30427
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t30427" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30427
+INSERT INTO [t30427] VALUES (1,'الجزء فيه من حديث إسحاق بن إبراهيم بن عباد الدبري، عن عبد الرزاق بن همام','1','0','0');
