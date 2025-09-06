707b71bb1c2a0737827ba32ecf2e2a1b6ba124ba
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:44 +0700

    Upload SQL chunk: t1878_chunk_0000.sql

---
+++ sql_chunks/t1878_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t1878
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t1878" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1878
+INSERT INTO [t1878] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1878] VALUES (2,'الاقتراض اللغوي وأثره في بعض المفردات العسكرية، البارود','5','0','0');
+INSERT INTO [t1878] VALUES (3,'البصطار','9','0','0');
+INSERT INTO [t1878] VALUES (4,'البندق','10','0','0');
+INSERT INTO [t1878] VALUES (5,'الجورب','11','0','0');
+INSERT INTO [t1878] VALUES (6,'الجونتي','14','0','0');
+INSERT INTO [t1878] VALUES (7,'الخندق','16','0','0');
+INSERT INTO [t1878] VALUES (8,'الخوذة','18','0','0');
+INSERT INTO [t1878] VALUES (9,'الرصاص','20','0','0');
+INSERT INTO [t1878] VALUES (10,'السرداب','23','0','0');
+INSERT INTO [t1878] VALUES (11,'الطربال','25','0','0');
+INSERT INTO [t1878] VALUES (12,'العسكر','27','0','0');
+INSERT INTO [t1878] VALUES (13,'القايش','30','0','0');
+INSERT INTO [t1878] VALUES (14,'القبوع','32','0','0');
+INSERT INTO [t1878] VALUES (15,'القيافة','34','0','0');
+INSERT INTO [t1878] VALUES (16,'كردون','36','0','0');
+INSERT INTO [t1878] VALUES (17,'المسدس','39','0','0');
+INSERT INTO [t1878] VALUES (18,'المناوب','40','0','0');
+INSERT INTO [t1878] VALUES (19,'المنجنيق','41','0','0');
+INSERT INTO [t1878] VALUES (20,'المنصة','45','0','0');
+INSERT INTO [t1878] VALUES (21,'الميز - الميس','47','0','0');
+INSERT INTO [t1878] VALUES (22,'الورنيك','49','0','0');
+INSERT INTO [t1878] VALUES (23,'مصادر ومراجع','50','0','0');
