bf52657b6bfee488393cfa44ef9f148b3279e6d3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:22 +0700

    Upload SQL chunk: t448_chunk_0000.sql

---
+++ sql_chunks/t448_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t448
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t448" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t448
+INSERT INTO [t448] VALUES (1,'ما هو العيد','2','0','0');
+INSERT INTO [t448] VALUES (2,'منشأ الأعياد','2','0','0');
+INSERT INTO [t448] VALUES (3,'الأعياد الدينية','4','0','0');
+INSERT INTO [t448] VALUES (4,'نظرية الإسلام في الأعياد','4','0','0');
+INSERT INTO [t448] VALUES (5,'لم تلتفت الشريعة للأعياد الاصطلاحية','9','0','0');
+INSERT INTO [t448] VALUES (6,'الجمعة يوم عيد','9','0','0');
+INSERT INTO [t448] VALUES (7,'فائدة: في حكمة النهي عن تخصيص يوم الجمعة بصيام','12','0','0');
+INSERT INTO [t448] VALUES (8,'لماذا لم يتخذ يوم مولده - صلى الله عليه وسلم - أو مبعثه عيدا','14','0','0');
+INSERT INTO [t448] VALUES (9,'مراعاة الإسلام لأهم المقاصد السياسية في الأعياد','17','0','0');
