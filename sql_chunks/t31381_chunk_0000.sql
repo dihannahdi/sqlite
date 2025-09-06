c830b323859ca5ad27bbfaba2a59f9398e2daa6f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:04:25 +0700

    Upload SQL chunk: t31381_chunk_0000.sql

---
+++ sql_chunks/t31381_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t31381
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t31381" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31381
+INSERT INTO [t31381] VALUES (1,'مختصر فتح رب الأرباب','1','0','0');
+INSERT INTO [t31381] VALUES (2,'حرف الهمزة','1','1','0');
+INSERT INTO [t31381] VALUES (3,'حرف الباء الموحدة','4','1','0');
+INSERT INTO [t31381] VALUES (4,'حرف التاء المثناة الفوقية','7','1','0');
+INSERT INTO [t31381] VALUES (5,'حرف الثاء المثلثة','9','1','0');
+INSERT INTO [t31381] VALUES (6,'حرف الجيم','10','1','0');
+INSERT INTO [t31381] VALUES (7,'حرف الحاء المهملة','13','1','0');
+INSERT INTO [t31381] VALUES (8,'حرف الخاء المعجمة','15','1','0');
+INSERT INTO [t31381] VALUES (9,'حرف الدال المهملة','17','1','0');
+INSERT INTO [t31381] VALUES (10,'حرف الذال المعجمة','20','1','0');
+INSERT INTO [t31381] VALUES (11,'حرف الراء المهملة','20','1','0');
+INSERT INTO [t31381] VALUES (12,'حرف الزاي','22','1','0');
+INSERT INTO [t31381] VALUES (13,'حرف السين المهملة','23','1','0');
+INSERT INTO [t31381] VALUES (14,'حرف الشين المعجمة','27','1','0');
+INSERT INTO [t31381] VALUES (15,'حرف الصاد المهملة','31','1','0');
+INSERT INTO [t31381] VALUES (16,'حرف الضاد المعجمة','33','1','0');
+INSERT INTO [t31381] VALUES (17,'حرف الطاء المهملة','33','1','0');
+INSERT INTO [t31381] VALUES (18,'حرف الظاء المعجمة','34','1','0');
+INSERT INTO [t31381] VALUES (19,'حرف العين المهملة','36','1','0');
+INSERT INTO [t31381] VALUES (20,'حرف الغين المهملة','41','1','0');
+INSERT INTO [t31381] VALUES (21,'حرف الفاء','42','1','0');
+INSERT INTO [t31381] VALUES (22,'حرف القاف','44','1','0');
+INSERT INTO [t31381] VALUES (23,'حرف الكاف','48','1','0');
+INSERT INTO [t31381] VALUES (24,'حرف اللام','51','1','0');
+INSERT INTO [t31381] VALUES (25,'حرف الميم','52','1','0');
+INSERT INTO [t31381] VALUES (26,'حرف النون','59','1','0');
+INSERT INTO [t31381] VALUES (27,'حرف الواو','63','1','0');
+INSERT INTO [t31381] VALUES (28,'حرف الهاء','64','1','0');
+INSERT INTO [t31381] VALUES (29,'حرف الياء المثناة التحتية','66','1','0');
