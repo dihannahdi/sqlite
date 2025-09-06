252bce2c5b57d3e26c6800a9f55a0c1f9b4e6f51
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:45:14 +0700

    Upload SQL chunk: t8273_chunk_0000.sql

---
+++ sql_chunks/t8273_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t8273
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t8273" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8273
+INSERT INTO [t8273] VALUES (1,'باب المطر','1','0','0');
+INSERT INTO [t8273] VALUES (2,'باب الرعد','91','0','0');
+INSERT INTO [t8273] VALUES (3,'باب في البرق','121','0','0');
+INSERT INTO [t8273] VALUES (4,'باب في الريح','130','0','0');
