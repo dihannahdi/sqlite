4913851f18a536afa484e26f71de5112a3b2ee87
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:26 +0700

    Upload SQL chunk: t26557_chunk_0000.sql

---
+++ sql_chunks/t26557_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26557
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26557" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26557
+INSERT INTO [t26557] VALUES (1,'أحاديث عوال من مسموعات ابن هامل','2','0','0');
