f68e4b51269ab2aa61187795c8f4726dc9dfe988
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:50:02 +0700

    Upload SQL chunk: t10550_chunk_0000.sql

---
+++ sql_chunks/t10550_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10550
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10550" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10550
+INSERT INTO [t10550] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10550] VALUES (2,'الفلك الدائر على المثل السائر','7','0','0');
+INSERT INTO [t10550] VALUES (3,'الفهارس','297','0','0');
