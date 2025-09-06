555ceaba552d1cbc03f833caea9650112936c575
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:54:49 +0700

    Upload SQL chunk: t95661_chunk_0000.sql

---
+++ sql_chunks/t95661_chunk_0000.sql
@@ -0,0 +1,64 @@
+-- Table: t95661
+-- Chunk: 1
+-- Rows: 57
+
+CREATE TABLE "t95661" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95661
+INSERT INTO [t95661] VALUES (1,'ترجمة الشيخ عبدالحميد ابن باديس','5','0','0');
+INSERT INTO [t95661] VALUES (2,'في جامع الزيتونة','7','1','0');
+INSERT INTO [t95661] VALUES (3,'في المدينة النبوية','9','1','0');
+INSERT INTO [t95661] VALUES (4,'العودة إلى الجزائر','10','1','0');
+INSERT INTO [t95661] VALUES (5,'آثار ابن باديس','11','1','0');
+INSERT INTO [t95661] VALUES (6,'*','16','0','0');
+INSERT INTO [t95661] VALUES (7,'واضع فن الأصول','18','6','0');
+INSERT INTO [t95661] VALUES (8,'تعريف علم الأصول','19','6','0');
+INSERT INTO [t95661] VALUES (9,'الباب الأول','20','0','0');
+INSERT INTO [t95661] VALUES (10,'الباب الثاني','20','0','0');
+INSERT INTO [t95661] VALUES (11,'الوضع','23','10','0');
+INSERT INTO [t95661] VALUES (12,'تفريق بينهما','25','10','0');
+INSERT INTO [t95661] VALUES (13,'الأحكام الشرعية في الخطابات الإلهية','26','10','0');
+INSERT INTO [t95661] VALUES (14,'العزيمة والرخصة','29','10','0');
+INSERT INTO [t95661] VALUES (15,'التصحيح والإبطال','30','10','0');
+INSERT INTO [t95661] VALUES (16,'مقتضيات الحكم','32','10','0');
+INSERT INTO [t95661] VALUES (17,'المحكوم فيه','33','10','0');
+INSERT INTO [t95661] VALUES (18,'المحكوم عليه','34','10','0');
+INSERT INTO [t95661] VALUES (19,'المخاطب بالأحكام','35','10','0');
+INSERT INTO [t95661] VALUES (20,'الباب الثالث','37','0','0');
+INSERT INTO [t95661] VALUES (21,'الكتاب','38','20','0');
+INSERT INTO [t95661] VALUES (22,'السنة','39','20','0');
+INSERT INTO [t95661] VALUES (23,'الإجماع','40','20','0');
+INSERT INTO [t95661] VALUES (24,'القياس','42','20','0');
+INSERT INTO [t95661] VALUES (25,'الباب الرابع','43','0','0');
+INSERT INTO [t95661] VALUES (26,'قاعدة في حمل اللفظ','44','25','0');
+INSERT INTO [t95661] VALUES (27,'قاعدة','45','25','0');
+INSERT INTO [t95661] VALUES (28,'قاعدة النهي','46','25','0');
+INSERT INTO [t95661] VALUES (29,'قاعدة في الأخذ بالمأمور','47','25','0');
+INSERT INTO [t95661] VALUES (30,'قواعد المنطوق والمفهوم','48','25','0');
+INSERT INTO [t95661] VALUES (31,'أنواع دليل الخطاب','50','25','0');
+INSERT INTO [t95661] VALUES (32,'تقييد','52','31','0');
+INSERT INTO [t95661] VALUES (33,'قاعدة النص','53','25','0');
+INSERT INTO [t95661] VALUES (34,'قاعدة الظاهر','54','25','0');
+INSERT INTO [t95661] VALUES (35,'قاعدة المؤول','54','25','0');
+INSERT INTO [t95661] VALUES (36,'قاعدة المبين بالكسر','55','25','0');
+INSERT INTO [t95661] VALUES (37,'قاعدة المجمل','56','25','0');
+INSERT INTO [t95661] VALUES (38,'أسباب الإجمال','56','37','0');
+INSERT INTO [t95661] VALUES (39,'قاعدة المبين بالفتح','57','25','0');
+INSERT INTO [t95661] VALUES (40,'قاعدة العام','58','25','0');
+INSERT INTO [t95661] VALUES (41,'قاعدة في فرق العام','61','25','0');
+INSERT INTO [t95661] VALUES (42,'قاعدة في التخصيص','62','25','0');
+INSERT INTO [t95661] VALUES (43,'قاعدة في المخصص وتقسيمه','63','25','0');
+INSERT INTO [t95661] VALUES (44,'قاعدة في المطلق','65','25','0');
+INSERT INTO [t95661] VALUES (45,'قاعدة في حمل المطلق على المقيد','66','25','0');
+INSERT INTO [t95661] VALUES (46,'قاعدة في المحكم والناسخ والمنسوخ','69','25','0');
+INSERT INTO [t95661] VALUES (47,'متى يقع النسخ','70','46','0');
+INSERT INTO [t95661] VALUES (48,'مورد النسخ','70','46','0');
+INSERT INTO [t95661] VALUES (49,'حكمة النسخ','71','46','0');
+INSERT INTO [t95661] VALUES (50,'وجوه النسخ','72','46','0');
+INSERT INTO [t95661] VALUES (51,'تنبيه','73','46','0');
+INSERT INTO [t95661] VALUES (52,'قاعدة في أفعاله','74','25','0');
+INSERT INTO [t95661] VALUES (53,'قاعدة في تقريره','75','25','0');
+INSERT INTO [t95661] VALUES (54,'سببه ثان','76','53','0');
+INSERT INTO [t95661] VALUES (55,'خاتمة في الاجتهاد','77','25','0');
+INSERT INTO [t95661] VALUES (56,'التقليد','78','25','0');
+INSERT INTO [t95661] VALUES (57,'الإتباع','78','25','0');
