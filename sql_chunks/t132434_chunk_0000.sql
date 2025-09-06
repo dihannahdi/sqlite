be8d2c3800c4751306e252059bc22539ecbd931a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:12:36 +0700

    Upload SQL chunk: t132434_chunk_0000.sql

---
+++ sql_chunks/t132434_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t132434
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t132434" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t132434
+INSERT INTO [t132434] VALUES (1,'رسالة عمر بن عثمان الجرسيفي','1','0','0');
