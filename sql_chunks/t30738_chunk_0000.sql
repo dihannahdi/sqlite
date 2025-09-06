ede2d155fd900834aa8bb2f361ca23e7504f2548
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:43 +0700

    Upload SQL chunk: t30738_chunk_0000.sql

---
+++ sql_chunks/t30738_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t30738
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t30738" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30738
+INSERT INTO [t30738] VALUES (1,'سمعت جبريل عليه السلام يقول: يا محمد، من قال من أمتك كل يوم مائة مرة: لا إله إلا الله','2','0','0');
+INSERT INTO [t30738] VALUES (2,'من تنصل إليه فلم يقبل لم يرد علي الحوض','3','0','0');
+INSERT INTO [t30738] VALUES (3,'علامة حب الله حب ذكر الله , وعلامة بغض الله بغض ذكر الله','4','0','0');
+INSERT INTO [t30738] VALUES (4,'الدنيا سجن المؤمن وجنة الكافر وبإسناده إلى أبي عبد الرحمن السلمي، قال: ذو النون المصري','5','0','0');
