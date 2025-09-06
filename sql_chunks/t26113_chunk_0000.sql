8c9b747c3d546d95500d5b5f848b1f509c6a9bff
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:30 +0700

    Upload SQL chunk: t26113_chunk_0000.sql

---
+++ sql_chunks/t26113_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t26113
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t26113" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26113
+INSERT INTO [t26113] VALUES (1,'قاعدة في الجرح والتعديل','1','0','0');
+INSERT INTO [t26113] VALUES (2,'من ثبتت إمامته وعدالته وكثر مادحوه ومزكوه وندر جارحوه وكانت هناك قرينة دالة على سبب جرحه من تعصب مذهبي أو غيره فإنا لا نلتفت إلى الجرح فيه ونعمل فيه بالعدالة','1','0','0');
+INSERT INTO [t26113] VALUES (3,'فليس لنا الترضي عنهم والسكوت عما جرى بينهم','39','0','0');
