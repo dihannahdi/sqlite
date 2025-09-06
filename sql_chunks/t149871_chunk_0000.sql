7b2befa9cd43a63d81703857bfce00a3cfdcc3dc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:53 +0700

    Upload SQL chunk: t149871_chunk_0000.sql

---
+++ sql_chunks/t149871_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t149871
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t149871" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t149871
+INSERT INTO [t149871] VALUES (1,'حرف الباء','1','0','0');
+INSERT INTO [t149871] VALUES (2,'حرف التاء','19','0','0');
+INSERT INTO [t149871] VALUES (3,'حرف الجيم','23','0','0');
+INSERT INTO [t149871] VALUES (4,'حرف الحاء','27','0','0');
+INSERT INTO [t149871] VALUES (5,'حرف الدال','29','0','0');
+INSERT INTO [t149871] VALUES (6,'حرف الراء','47','0','0');
+INSERT INTO [t149871] VALUES (7,'حرف العين','68','0','0');
+INSERT INTO [t149871] VALUES (8,'حرف الفاء','70','0','0');
+INSERT INTO [t149871] VALUES (9,'حرف الكاف','74','0','0');
+INSERT INTO [t149871] VALUES (10,'حرف اللام','76','0','0');
+INSERT INTO [t149871] VALUES (11,'حرف الميم','92','0','0');
+INSERT INTO [t149871] VALUES (12,'حرف النون','123','0','0');
+INSERT INTO [t149871] VALUES (13,'حرف الياء','129','0','0');
