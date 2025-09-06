5d17c2eb71df9315b8291e3d0c6df0a52ae26c5c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:15:27 +0700

    Upload SQL chunk: t376_chunk_0000.sql

---
+++ sql_chunks/t376_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t376
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t376" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t376
+INSERT INTO [t376] VALUES (1,'الأصل في كلمة «الخوف»','3','0','0');
+INSERT INTO [t376] VALUES (2,'الأقوال في تفسير {وإن خفتم ألا تقسطوا} الآية','3','0','0');
+INSERT INTO [t376] VALUES (3,'معنى الطيب في {ما طاب لكم}','6','0','0');
+INSERT INTO [t376] VALUES (4,'إذا كانت الآية ظاهرة في العموم لم تصرف عنه إلى الخصوص لظاهر تفسير بعض السلف','7','0','0');
+INSERT INTO [t376] VALUES (5,'تحري الطيب في النكاح أقرب الوجوه إلى القسط','8','0','0');
+INSERT INTO [t376] VALUES (6,'الأصل في سبيل الإقساط: الجمع بين اثنتين أو ثلاث أو أربع','10','0','0');
+INSERT INTO [t376] VALUES (7,'العدل بين الزوجات','12','0','0');
+INSERT INTO [t376] VALUES (8,'معنى العدل في قوله: {فإن خفتم ألا تعدلوا}','14','7','0');
