8e33d2ea2d5764c4f7dc4f2b2d128b410cd4729c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:50:07 +0700

    Upload SQL chunk: t10609_chunk_0000.sql

---
+++ sql_chunks/t10609_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t10609
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t10609" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10609
+INSERT INTO [t10609] VALUES (1,'مضمون الرسالة','1','0','0');
+INSERT INTO [t10609] VALUES (2,'مقدمة','1','1','0');
+INSERT INTO [t10609] VALUES (3,'كيف يدعو المدرس المسلم طلابه إلى الله؟','3','1','0');
+INSERT INTO [t10609] VALUES (4,'ملحق','25','1','0');
