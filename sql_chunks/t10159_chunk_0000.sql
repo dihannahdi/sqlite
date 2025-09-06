9bcf5f14214a40655c7758b71e3fec8abba9abe3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:05 +0700

    Upload SQL chunk: t10159_chunk_0000.sql

---
+++ sql_chunks/t10159_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t10159
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t10159" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10159
+INSERT INTO [t10159] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10159] VALUES (2,'المطلب الأول: مفهوم القيمة في زكاة الفطر','3','0','0');
+INSERT INTO [t10159] VALUES (3,'أولا: زكاة الفطر','3','2','0');
+INSERT INTO [t10159] VALUES (4,'ثانيا: القيمة','6','2','0');
+INSERT INTO [t10159] VALUES (5,'ثالثا: السعر','8','2','0');
+INSERT INTO [t10159] VALUES (6,'المطلب الثاني: حكم زكاة الفطر','10','0','0');
+INSERT INTO [t10159] VALUES (7,'المطلب الثالث: الحكمة في كل من فريضة زكاة الفطر ومقدارها','13','0','0');
+INSERT INTO [t10159] VALUES (8,'المطلب الرابع: مذاهب الفقهاء في إخراج زكاة الفطر قيمة','16','0','0');
+INSERT INTO [t10159] VALUES (9,'الخاتمة','45','0','0');
+INSERT INTO [t10159] VALUES (10,'مصادر ومراجع','47','0','0');
