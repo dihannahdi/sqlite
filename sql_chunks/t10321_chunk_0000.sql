19c7c5682abf28edbcb73222b6f06bedd725bd0c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:02 +0700

    Upload SQL chunk: t10321_chunk_0000.sql

---
+++ sql_chunks/t10321_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t10321
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t10321" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10321
+INSERT INTO [t10321] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10321] VALUES (2,'التمهيد','4','0','0');
+INSERT INTO [t10321] VALUES (3,'مقدمة المؤلف','31','0','0');
+INSERT INTO [t10321] VALUES (4,'باب تقسيم البدع الى الأحكام الخمسة','37','0','0');
+INSERT INTO [t10321] VALUES (5,'باب تقسيم السنة إلى حسنة وسيئة من حيث اللغة','46','0','0');
