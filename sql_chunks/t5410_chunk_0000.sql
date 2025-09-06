78c0d2a8e6e0e2b99f4cb80d1f2a2a3ae2bde634
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:43 +0700

    Upload SQL chunk: t5410_chunk_0000.sql

---
+++ sql_chunks/t5410_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t5410
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t5410" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5410
+INSERT INTO [t5410] VALUES (1,'تفريج الكرب عن قلوب أهل الأرب في معرفة لامية العرب','1','0','0');
+INSERT INTO [t5410] VALUES (2,'تأليف أبي عبد الله محمد بن قاسم بن عبد الواحد بن زاكور الفارسي المتوفى سنة','1','0','0');
+INSERT INTO [t5410] VALUES (3,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t5410] VALUES (4,'لامية العرب','17','0','0');
