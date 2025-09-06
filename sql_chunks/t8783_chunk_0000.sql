59cfde3a7337ae35b2fa038fb3035bb80879e9f2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:04 +0700

    Upload SQL chunk: t8783_chunk_0000.sql

---
+++ sql_chunks/t8783_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8783
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8783" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8783
+INSERT INTO [t8783] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8783] VALUES (2,'الأنوار الرحمانية لهداية الفرقة التيجانية','3','0','0');
