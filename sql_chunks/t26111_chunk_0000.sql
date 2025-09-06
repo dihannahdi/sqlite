1ad96521b6ec2d3c115de24f9ee265cf28165af2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:28 +0700

    Upload SQL chunk: t26111_chunk_0000.sql

---
+++ sql_chunks/t26111_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t26111
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t26111" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26111
+INSERT INTO [t26111] VALUES (1,'١- فالصحابة الذين أوردهم','2','0','0');
+INSERT INTO [t26111] VALUES (2,'٢- وسرد من التابعين عددا','3','0','0');
+INSERT INTO [t26111] VALUES (3,'٣- فلما مضى القرن الأول ودخل الثاني','5','0','0');
+INSERT INTO [t26111] VALUES (4,'٤- فلما كان عند آخر عصر التابعين','5','0','0');
+INSERT INTO [t26111] VALUES (5,'٥- ثم طبقة أخرى بعد هؤلاء','7','0','0');
+INSERT INTO [t26111] VALUES (6,'٦- ثم طبقة أخرى في زمانهم','7','0','0');
+INSERT INTO [t26111] VALUES (7,'٧- ثم انتدب في زمانهم أيضا لنقد الرجال','8','0','0');
+INSERT INTO [t26111] VALUES (8,'٨- ثم كان بعدهم ممن إذا قال سمع منه','8','0','0');
+INSERT INTO [t26111] VALUES (9,'٩-وبعدهم طبقة أخرى','9','0','0');
+INSERT INTO [t26111] VALUES (10,'١٠- ثم صنفت الكتب ودونت','10','0','0');
+INSERT INTO [t26111] VALUES (11,'١١- ثم خلفهم طبقة أخرى متصلة بهم منهم','14','0','0');
+INSERT INTO [t26111] VALUES (12,'١٢- ثم من بعدهم','14','0','0');
+INSERT INTO [t26111] VALUES (13,'١٣- ثم من بعدهم','16','0','0');
+INSERT INTO [t26111] VALUES (14,'١٤- ثم من بعدهم','17','0','0');
+INSERT INTO [t26111] VALUES (15,'١٥- ثم طبقة أخرى منهم','19','0','0');
+INSERT INTO [t26111] VALUES (16,'١٦- ثم من بعدهم','19','0','0');
+INSERT INTO [t26111] VALUES (17,'١٧- ثم بعدهم','20','0','0');
+INSERT INTO [t26111] VALUES (18,'١٨- ثم بعدهم','21','0','0');
+INSERT INTO [t26111] VALUES (19,'١٩- ثم بعدهم','23','0','0');
+INSERT INTO [t26111] VALUES (20,'٢٠-ثم بعدهم','25','0','0');
+INSERT INTO [t26111] VALUES (21,'٢١- ثم بعدهم','26','0','0');
+INSERT INTO [t26111] VALUES (22,'٢٢-ثم بعدهم','29','0','0');
+INSERT INTO [t26111] VALUES (23,'٢٣- ثم بعدهم','30','0','0');
+INSERT INTO [t26111] VALUES (24,'٢٤- ثم بعدهم','31','0','0');
+INSERT INTO [t26111] VALUES (25,'٢٥- ثم بعدهم','34','0','0');
+INSERT INTO [t26111] VALUES (26,'٢٦- ثم بعدهم','36','0','0');
+INSERT INTO [t26111] VALUES (27,'وقد قسم الذهبي من تكلم في الرجال أقساما','44','0','0');
+INSERT INTO [t26111] VALUES (28,'قال وهم الكل على ثلاثة أقسام أيضا','45','0','0');
