7405ad2521a37fe661a7e3bcad53efbfb17bb6b2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:43 +0700

    Upload SQL chunk: t21815_chunk_0000.sql

---
+++ sql_chunks/t21815_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t21815
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t21815" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21815
+INSERT INTO [t21815] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t21815] VALUES (2,'الحديث','1','0','0');
+INSERT INTO [t21815] VALUES (3,'الشرح','2','0','0');
+INSERT INTO [t21815] VALUES (4,'اللهم أهدنا فيمن هديت','3','0','0');
+INSERT INTO [t21815] VALUES (5,'وعافنا فيمن عافيت','4','0','0');
+INSERT INTO [t21815] VALUES (6,'وتولنا فيمن توليت','4','0','0');
+INSERT INTO [t21815] VALUES (7,'وبارك لنا فيما أعطيت وقنا شر ما قضيت','5','0','0');
+INSERT INTO [t21815] VALUES (8,'انك تقضي ولا يقضى عليك, إنه لا يزل من واليت ولا يعز من عاديت','7','0','0');
+INSERT INTO [t21815] VALUES (9,'تباركت ربنا وتعاليت','9','0','0');
+INSERT INTO [t21815] VALUES (10,'سؤال وجواب','10','0','0');
