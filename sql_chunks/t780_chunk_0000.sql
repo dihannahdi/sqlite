2def084c81cff7536d2d9f5ec170cae5ae409c7d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:14 +0700

    Upload SQL chunk: t780_chunk_0000.sql

---
+++ sql_chunks/t780_chunk_0000.sql
@@ -0,0 +1,30 @@
+-- Table: t780
+-- Chunk: 1
+-- Rows: 23
+
+CREATE TABLE "t780" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t780
+INSERT INTO [t780] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t780] VALUES (2,'وبه الإعانة','1','1','0');
+INSERT INTO [t780] VALUES (3,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t780] VALUES (4,'حرف الهمزة','1','0','0');
+INSERT INTO [t780] VALUES (5,'حرف الباء','8','0','0');
+INSERT INTO [t780] VALUES (6,'حرف التاء','18','0','0');
+INSERT INTO [t780] VALUES (7,'حرف الحاء','20','0','0');
+INSERT INTO [t780] VALUES (8,'حرف الدال','21','0','0');
+INSERT INTO [t780] VALUES (9,'حرف الراء','37','0','0');
+INSERT INTO [t780] VALUES (10,'حرف الزاي','44','0','0');
+INSERT INTO [t780] VALUES (11,'حرف السين','45','0','0');
+INSERT INTO [t780] VALUES (12,'حرف الشين','45','0','0');
+INSERT INTO [t780] VALUES (13,'حرف الضاد','46','0','0');
+INSERT INTO [t780] VALUES (14,'حرف العين','47','0','0');
+INSERT INTO [t780] VALUES (15,'حرف الفاء','51','0','0');
+INSERT INTO [t780] VALUES (16,'حرف القاف','52','0','0');
+INSERT INTO [t780] VALUES (17,'حرف الكاف','55','0','0');
+INSERT INTO [t780] VALUES (18,'حرف اللام','57','0','0');
+INSERT INTO [t780] VALUES (19,'حرف الميم','77','0','0');
+INSERT INTO [t780] VALUES (20,'حرف النون','96','0','0');
+INSERT INTO [t780] VALUES (21,'حرف الهاء','103','0','0');
+INSERT INTO [t780] VALUES (22,'حرف الياء','105','0','0');
+INSERT INTO [t780] VALUES (23,'آخر ما كتب في الأصل','105','22','0');
