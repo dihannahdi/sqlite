d6e3f4a902a9bcf68b44790707a77fd4b9d4b409
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:41:26 +0700

    Upload SQL chunk: t21709_chunk_0000.sql

---
+++ sql_chunks/t21709_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t21709
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t21709" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21709
+INSERT INTO [t21709] VALUES (1,'شرح كتاب التوحيد (١)','2','0','0');
+INSERT INTO [t21709] VALUES (2,'شرح كتاب التوحيد (٢)','31','0','0');
+INSERT INTO [t21709] VALUES (3,'شرح كتاب التوحيد (٣)','64','0','0');
+INSERT INTO [t21709] VALUES (4,'شرح كتاب التوحيد (٤)','97','0','0');
+INSERT INTO [t21709] VALUES (5,'شرح كتاب التوحيد (٥)','127','0','0');
+INSERT INTO [t21709] VALUES (6,'شرح كتاب التوحيد (٦)','163','0','0');
+INSERT INTO [t21709] VALUES (7,'شرح كتاب التوحيد (٧)','198','0','0');
+INSERT INTO [t21709] VALUES (8,'شرح كتاب التوحيد (٨)','238','0','0');
+INSERT INTO [t21709] VALUES (9,'شرح كتاب التوحيد (٩)','273','0','0');
+INSERT INTO [t21709] VALUES (10,'شرح كتاب التوحيد (١٠)','305','0','0');
+INSERT INTO [t21709] VALUES (11,'شرح كتاب التوحيد (١١)','336','0','0');
