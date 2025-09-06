ee7becbdef2f28e1c5bc62f8ab33cfe142abefae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:36 +0700

    Upload SQL chunk: t28237_chunk_0000.sql

---
+++ sql_chunks/t28237_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t28237
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t28237" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t28237
+INSERT INTO [t28237] VALUES (1,'باب هواتف القبور','41','0','0');
+INSERT INTO [t28237] VALUES (2,'باب هواتف الدعاء','60','0','0');
+INSERT INTO [t28237] VALUES (3,'باب هواتف الجن','74','0','0');
