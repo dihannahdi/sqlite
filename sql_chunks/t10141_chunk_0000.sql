a90cf87d424b31d58c3b182bee9cff53b012fc4d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:00 +0700

    Upload SQL chunk: t10141_chunk_0000.sql

---
+++ sql_chunks/t10141_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10141
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10141" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10141
+INSERT INTO [t10141] VALUES (1,'حديث مع زائر كريم','1','0','0');
+INSERT INTO [t10141] VALUES (2,'درسا في الأديان موضوعه التلمود','1','0','0');
+INSERT INTO [t10141] VALUES (3,'كلمة تلموذ بالذال المعجمة في اللغة العبرانية مشتقة من المصدر (لاموذ)','1','0','0');
