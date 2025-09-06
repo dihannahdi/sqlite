32dec9b6d0e2c8145519cf039dd7d7d84d7425ab
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:51:38 +0700

    Upload SQL chunk: t10805_chunk_0000.sql

---
+++ sql_chunks/t10805_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t10805
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t10805" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10805
+INSERT INTO [t10805] VALUES (1,'باب الهمزة','1','0','0');
+INSERT INTO [t10805] VALUES (2,'(باب إبراهيم)','1','1','0');
+INSERT INTO [t10805] VALUES (3,'باب أبان','4','1','0');
+INSERT INTO [t10805] VALUES (4,'باب أبي','5','1','0');
+INSERT INTO [t10805] VALUES (5,'باب أيوب','5','1','0');
+INSERT INTO [t10805] VALUES (6,'باب إسماعيل','6','1','0');
+INSERT INTO [t10805] VALUES (7,'باب إسحاق','8','1','0');
+INSERT INTO [t10805] VALUES (8,'باب أسامة وغيره','9','1','0');
+INSERT INTO [t10805] VALUES (9,'باب الباء','13','0','0');
+INSERT INTO [t10805] VALUES (10,'باب التاء','16','0','0');
+INSERT INTO [t10805] VALUES (11,'باب الثاء','17','0','0');
+INSERT INTO [t10805] VALUES (12,'باب الجيم','18','0','0');
+INSERT INTO [t10805] VALUES (13,'باب الحاء','19','0','0');
+INSERT INTO [t10805] VALUES (14,'باب الخاء','26','0','0');
+INSERT INTO [t10805] VALUES (15,'باب الدال','28','0','0');
+INSERT INTO [t10805] VALUES (16,'باب الذال','30','0','0');
+INSERT INTO [t10805] VALUES (17,'باب الراء','30','0','0');
+INSERT INTO [t10805] VALUES (18,'باب الزاي','33','0','0');
+INSERT INTO [t10805] VALUES (19,'باب السين','36','0','0');
+INSERT INTO [t10805] VALUES (20,'باب الشين','46','0','0');
+INSERT INTO [t10805] VALUES (21,'باب الصاد','47','0','0');
+INSERT INTO [t10805] VALUES (22,'باب الضاد','49','0','0');
+INSERT INTO [t10805] VALUES (23,'باب الطاء','50','0','0');
+INSERT INTO [t10805] VALUES (24,'باب الظاء','51','0','0');
+INSERT INTO [t10805] VALUES (25,'باب العين','51','0','0');
+INSERT INTO [t10805] VALUES (26,'باب الغين','76','0','0');
+INSERT INTO [t10805] VALUES (27,'باب الفاء','77','0','0');
+INSERT INTO [t10805] VALUES (28,'باب القاف','78','0','0');
+INSERT INTO [t10805] VALUES (29,'باب الكاف','79','0','0');
+INSERT INTO [t10805] VALUES (30,'باب اللام','80','0','0');
+INSERT INTO [t10805] VALUES (31,'باب النون','90','0','0');
+INSERT INTO [t10805] VALUES (32,'باب الواو','93','0','0');
+INSERT INTO [t10805] VALUES (33,'باب الهاء','94','0','0');
+INSERT INTO [t10805] VALUES (34,'باب لا','96','0','0');
+INSERT INTO [t10805] VALUES (35,'باب الياء','96','0','0');
+INSERT INTO [t10805] VALUES (36,'باب الكنى','102','0','0');
