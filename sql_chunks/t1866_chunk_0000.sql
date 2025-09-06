382dcf53a4f7ea7dc7bf81943698a878ac2df932
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:36 +0700

    Upload SQL chunk: t1866_chunk_0000.sql

---
+++ sql_chunks/t1866_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t1866
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t1866" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1866
+INSERT INTO [t1866] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1866] VALUES (2,'دعوة الرسل إلى الله تعالى تدور على ثلاثة أمور','3','0','0');
+INSERT INTO [t1866] VALUES (3,'الدعوة إلى الله تعالى أحد أركان الأعمال الصالحة','4','0','0');
+INSERT INTO [t1866] VALUES (4,'الدعوة إلى الله عز وجل بين طرفين ووسط','5','0','0');
+INSERT INTO [t1866] VALUES (5,'الجانب الأول جانب الإفراط','5','4','0');
+INSERT INTO [t1866] VALUES (6,'الجانب الثاني جانب التفريط','11','4','0');
+INSERT INTO [t1866] VALUES (7,'هدي النبي صلي الله عليه وسلم وهدي أصحابه هو مقياس الشدة واللين في الدعوة','14','4','0');
+INSERT INTO [t1866] VALUES (8,'الاعتدال بين الغلو والتقصير','18','4','0');
