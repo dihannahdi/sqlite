eff95ceb87af9497c7ca0aeeb704a66dfadd1872
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:55:57 +0700

    Upload SQL chunk: t2918_chunk_0000.sql

---
+++ sql_chunks/t2918_chunk_0000.sql
@@ -0,0 +1,36 @@
+-- Table: t2918
+-- Chunk: 1
+-- Rows: 29
+
+CREATE TABLE "t2918" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t2918
+INSERT INTO [t2918] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t2918] VALUES (2,'حرف الهمزة','6','0','0');
+INSERT INTO [t2918] VALUES (3,'حرف الباء','26','0','0');
+INSERT INTO [t2918] VALUES (4,'حرف التاء','43','0','0');
+INSERT INTO [t2918] VALUES (5,'حرف الثاء','46','0','0');
+INSERT INTO [t2918] VALUES (6,'حرف الجيم','52','0','0');
+INSERT INTO [t2918] VALUES (7,'حرف الحاء','70','0','0');
+INSERT INTO [t2918] VALUES (8,'حرف الخاء','106','0','0');
+INSERT INTO [t2918] VALUES (9,'حرف الدال','123','0','0');
+INSERT INTO [t2918] VALUES (10,'حرف الذال','130','0','0');
+INSERT INTO [t2918] VALUES (11,'حرف الراء','135','0','0');
+INSERT INTO [t2918] VALUES (12,'حرف الزاي','153','0','0');
+INSERT INTO [t2918] VALUES (13,'حرف السين','157','0','0');
+INSERT INTO [t2918] VALUES (14,'حرف الشين','184','0','0');
+INSERT INTO [t2918] VALUES (15,'حرف الصاد','201','0','0');
+INSERT INTO [t2918] VALUES (16,'حرف الضاد','215','0','0');
+INSERT INTO [t2918] VALUES (17,'حرف الطاء','222','0','0');
+INSERT INTO [t2918] VALUES (18,'حرف الظاء','233','0','0');
+INSERT INTO [t2918] VALUES (19,'حرف العين','235','0','0');
+INSERT INTO [t2918] VALUES (20,'حرف الغين','266','0','0');
+INSERT INTO [t2918] VALUES (21,'حرف الفاء','276','0','0');
+INSERT INTO [t2918] VALUES (22,'حرف القاف','288','0','0');
+INSERT INTO [t2918] VALUES (23,'حرف الكاف','308','0','0');
+INSERT INTO [t2918] VALUES (24,'حرف اللام','322','0','0');
+INSERT INTO [t2918] VALUES (25,'حرف الميم','330','0','0');
+INSERT INTO [t2918] VALUES (26,'حرف النون','340','0','0');
+INSERT INTO [t2918] VALUES (27,'حرف الهاء','360','0','0');
+INSERT INTO [t2918] VALUES (28,'حرف الواو','365','0','0');
+INSERT INTO [t2918] VALUES (29,'حرف الياء','387','0','0');
