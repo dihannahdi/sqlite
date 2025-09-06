1c433f9165401a70b18fbf4528b5d13b9ae5b355
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:23 +0700

    Upload SQL chunk: t10920_chunk_0000.sql

---
+++ sql_chunks/t10920_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t10920
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t10920" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10920
+INSERT INTO [t10920] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t10920] VALUES (2,'صلى الله على محمد وعلى آله وسلم تسليما','1','1','0');
+INSERT INTO [t10920] VALUES (3,'باب ذكر من نقط المصاحف أولا','2','0','0');
+INSERT INTO [t10920] VALUES (4,'من التابعين ومن كره ذلك ومن ترخص فيه من العلماء','2','0','0');
+INSERT INTO [t10920] VALUES (5,'باب ذكر مواضع الحركات من الحروف','4','0','0');
+INSERT INTO [t10920] VALUES (6,'وتراكب التنوين وتتابعه','4','0','0');
+INSERT INTO [t10920] VALUES (7,'فصل','4','6','0');
+INSERT INTO [t10920] VALUES (8,'فصل','5','0','0');
+INSERT INTO [t10920] VALUES (9,'فصل','5','8','0');
+INSERT INTO [t10920] VALUES (10,'باب ذكر علامة السكون والتشديد في الحروف','6','0','0');
+INSERT INTO [t10920] VALUES (11,'باب ذكر حكم النون الساكنة وما بعدها','8','0','0');
+INSERT INTO [t10920] VALUES (12,'باب ذكر أحكام المظهر والمدغم','9','0','0');
+INSERT INTO [t10920] VALUES (13,'فصل','9','0','0');
+INSERT INTO [t10920] VALUES (14,'باب ذكر أحكام تليين الهمزات','10','0','0');
+INSERT INTO [t10920] VALUES (15,'باب ذكر أحكام الصلات في الوصل','13','0','0');
+INSERT INTO [t10920] VALUES (16,'فصل','13','0','0');
+INSERT INTO [t10920] VALUES (17,'باب ذكر أحكام نقط ما نقص من هجائه','14','0','0');
+INSERT INTO [t10920] VALUES (18,'باب ذكر أحكام نقط ما زيد في هجائه','15','0','0');
+INSERT INTO [t10920] VALUES (19,'فصل','17','0','0');
+INSERT INTO [t10920] VALUES (20,'باب ذكر امتحان مواضع الهمزات من الكلم','18','0','0');
+INSERT INTO [t10920] VALUES (21,'باب ذكر اللام ألف','18','0','0');
