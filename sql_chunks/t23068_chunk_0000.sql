17662888650e61299cff64d7e1054710a9d4341c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:38 +0700

    Upload SQL chunk: t23068_chunk_0000.sql

---
+++ sql_chunks/t23068_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t23068
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t23068" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23068
+INSERT INTO [t23068] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t23068] VALUES (2,'الباب الأول: المادتان الأوليتان من إعلان حقوق الإنسان','5','0','0');
+INSERT INTO [t23068] VALUES (3,'الباب الثاني: المواد (٧- ٨- ٩- ١٠- ١١) من حقوق الإنسان','14','0','0');
+INSERT INTO [t23068] VALUES (4,'الباب الثالث','21','0','0');
+INSERT INTO [t23068] VALUES (5,'الباب الرابع','27','0','0');
+INSERT INTO [t23068] VALUES (6,'الباب السادس','59','0','0');
+INSERT INTO [t23068] VALUES (7,'الباب السابع','65','0','0');
