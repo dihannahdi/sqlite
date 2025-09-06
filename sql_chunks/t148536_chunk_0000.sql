ae010fd9180708bd45d39aba912edcdfa53bc0fb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:33 +0700

    Upload SQL chunk: t148536_chunk_0000.sql

---
+++ sql_chunks/t148536_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t148536
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t148536" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t148536
+INSERT INTO [t148536] VALUES (1,'ديوان ابن أبي حصينة','1','0','0');
+INSERT INTO [t148536] VALUES (2,'ديوان ابن أبي حصينة','1','0','0');
+INSERT INTO [t148536] VALUES (3,'المستدرك من شعر ابن أبي حصينة','339','0','0');
