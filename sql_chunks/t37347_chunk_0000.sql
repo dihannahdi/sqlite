c248d7711423b2c0b933c2dee510025072dde9d9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:12:32 +0700

    Upload SQL chunk: t37347_chunk_0000.sql

---
+++ sql_chunks/t37347_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t37347
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t37347" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37347
+INSERT INTO [t37347] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t37347] VALUES (2,'درجة الاستدلال بالحديث','15','0','0');
+INSERT INTO [t37347] VALUES (3,'شروط الاستدلال بحديث الآحاد','16','0','0');
+INSERT INTO [t37347] VALUES (4,'أنواع طريق الاستدلال','18','0','0');
+INSERT INTO [t37347] VALUES (5,'أولا: طرق استدلال الأحكام من الألفاظ','18','4','0');
+INSERT INTO [t37347] VALUES (6,'ثانيا: طرق الاستدلال من الألفاظ الواضحة وغير الواضحة','30','4','0');
+INSERT INTO [t37347] VALUES (7,'أ - الألفاظ الواضحة','30','6','0');
+INSERT INTO [t37347] VALUES (8,'ب - الألفاظ غير الواضحة هي','33','6','0');
+INSERT INTO [t37347] VALUES (9,'ثالثا: طرق الاستدلال من الدلالات','36','4','0');
+INSERT INTO [t37347] VALUES (10,'١ - عبارة النص','36','9','0');
+INSERT INTO [t37347] VALUES (11,'٢ - إشارة النص','36','9','0');
+INSERT INTO [t37347] VALUES (12,'٣ - دلالة النص','37','9','0');
+INSERT INTO [t37347] VALUES (13,'٤ - دلالة الاقتضاء','39','9','0');
+INSERT INTO [t37347] VALUES (14,'بعض مراجع البحث','50','0','0');
+INSERT INTO [t37347] VALUES (15,'الفهرس','54','0','0');
