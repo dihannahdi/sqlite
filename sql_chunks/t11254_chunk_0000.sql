738638c04648946142c68f6c801f39c489d95393
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:55:03 +0700

    Upload SQL chunk: t11254_chunk_0000.sql

---
+++ sql_chunks/t11254_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t11254
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t11254" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11254
+INSERT INTO [t11254] VALUES (1,'متن المنظومة البيقونية','1','0','0');
+INSERT INTO [t11254] VALUES (2,'مقدمة في علم مصطلح الحديث','3','0','0');
+INSERT INTO [t11254] VALUES (3,'شرح المنظومة البيقونية','6','0','0');
+INSERT INTO [t11254] VALUES (4,'شرح البسملة','6','3','0');
+INSERT INTO [t11254] VALUES (5,'معنى الحمد والصلاة على النبي صلى الله عليه وسلم','11','3','0');
+INSERT INTO [t11254] VALUES (6,'أقسام الحديث','15','3','0');
+INSERT INTO [t11254] VALUES (7,'الحديث الصحيح وشروطه','17','3','0');
+INSERT INTO [t11254] VALUES (8,'بقية شروط الصحيح','23','3','0');
+INSERT INTO [t11254] VALUES (9,'مباحث حديثية','29','3','0');
+INSERT INTO [t11254] VALUES (10,'الحديث الحسن','33','3','0');
+INSERT INTO [t11254] VALUES (11,'الضعيف وأقسامه','34','3','0');
+INSERT INTO [t11254] VALUES (12,'المرفوع والمقطوع','38','3','0');
+INSERT INTO [t11254] VALUES (13,'المسند وضابطه','46','3','0');
+INSERT INTO [t11254] VALUES (14,'المتصل وضابطه','49','3','0');
+INSERT INTO [t11254] VALUES (15,'المسلسل ومثاله','52','3','0');
+INSERT INTO [t11254] VALUES (16,'من صور المسلسل ومثالها','54','3','0');
+INSERT INTO [t11254] VALUES (17,'العزيز والمشهور','56','3','0');
+INSERT INTO [t11254] VALUES (18,'المعنعن والمبهم','61','3','0');
+INSERT INTO [t11254] VALUES (19,'العلو والنزول','64','3','0');
+INSERT INTO [t11254] VALUES (20,'الموقوف وضابطه','66','3','0');
+INSERT INTO [t11254] VALUES (21,'المرسل','68','3','0');
+INSERT INTO [t11254] VALUES (22,'الغريب وتعريفه لغة واصطلاحا','69','3','0');
+INSERT INTO [t11254] VALUES (23,'المنقطع وضابطه','70','3','0');
+INSERT INTO [t11254] VALUES (24,'المعضل والمدلس','72','3','0');
+INSERT INTO [t11254] VALUES (25,'أنواع التدليس','73','3','0');
+INSERT INTO [t11254] VALUES (26,'الشاذ والمقلوب','76','3','0');
+INSERT INTO [t11254] VALUES (27,'أقسام المقلوب','83','3','0');
+INSERT INTO [t11254] VALUES (28,'الفرد وأنواعه','88','3','0');
+INSERT INTO [t11254] VALUES (29,'الحديث المعلل','91','3','0');
+INSERT INTO [t11254] VALUES (30,'المضطرب وبيانه','93','3','0');
+INSERT INTO [t11254] VALUES (31,'المدرجات في الحديث','99','3','0');
+INSERT INTO [t11254] VALUES (32,'المدبج','102','3','0');
+INSERT INTO [t11254] VALUES (33,'المتفق والمفترق','104','3','0');
+INSERT INTO [t11254] VALUES (34,'المؤتلف والمختلف','105','3','0');
+INSERT INTO [t11254] VALUES (35,'المنكر وبيانه','107','3','0');
+INSERT INTO [t11254] VALUES (36,'المتروك وبيانه','108','3','0');
+INSERT INTO [t11254] VALUES (37,'الموضوع وبيانه','109','3','0');
+INSERT INTO [t11254] VALUES (38,'عدد أبيات المنظومة وخاتمتها','111','3','0');
