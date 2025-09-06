e4093c444920ffc5baa911140e27767a8671c768
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:50:31 +0700

    Upload SQL chunk: t26114_chunk_0000.sql

---
+++ sql_chunks/t26114_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t26114
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t26114" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26114
+INSERT INTO [t26114] VALUES (1,'قاعدة في المؤرخين','1','0','0');
+INSERT INTO [t26114] VALUES (2,'وقال في كتابه معيد النعم ومبيد النقم','12','0','0');
+INSERT INTO [t26114] VALUES (3,'وقال أيضا في كتاب طبقات الشافعية الكبرى','13','0','0');
