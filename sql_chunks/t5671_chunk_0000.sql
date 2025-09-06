e03e61dd25a231373bcbb7a203716e11242bfee7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:24:14 +0700

    Upload SQL chunk: t5671_chunk_0000.sql

---
+++ sql_chunks/t5671_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t5671
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t5671" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5671
+INSERT INTO [t5671] VALUES (1,'جزء لؤلؤ','4','0','0');
+INSERT INTO [t5671] VALUES (2,'من احكام اللعب بالطاولة','5','0','0');
