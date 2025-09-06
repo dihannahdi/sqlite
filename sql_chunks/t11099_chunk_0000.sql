aa763d75067193e5977ecab54bfc46cb2e3f58b6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:15 +0700

    Upload SQL chunk: t11099_chunk_0000.sql

---
+++ sql_chunks/t11099_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t11099
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t11099" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11099
+INSERT INTO [t11099] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11099] VALUES (2,'سورة الفاتحة','4','0','0');
+INSERT INTO [t11099] VALUES (3,'سورة البقرة','17','0','0');
+INSERT INTO [t11099] VALUES (4,'سورة آل عمران','41','0','0');
+INSERT INTO [t11099] VALUES (5,'سورة الأنعام','48','0','0');
+INSERT INTO [t11099] VALUES (6,'سورة الأعراف','64','0','0');
+INSERT INTO [t11099] VALUES (7,'قصة آدم وإبليس','76','0','0');
+INSERT INTO [t11099] VALUES (8,'سورة يونس','108','0','0');
+INSERT INTO [t11099] VALUES (9,'سورة هود','110','0','0');
+INSERT INTO [t11099] VALUES (10,'سورة يوسف','122','0','0');
+INSERT INTO [t11099] VALUES (11,'سورة الحجر','178','0','0');
+INSERT INTO [t11099] VALUES (12,'سورة النحل','231','0','0');
+INSERT INTO [t11099] VALUES (13,'سورة الكهف','232','0','0');
+INSERT INTO [t11099] VALUES (14,'قصة موسى والخضر','244','0','0');
+INSERT INTO [t11099] VALUES (15,'سورة طه','255','0','0');
+INSERT INTO [t11099] VALUES (16,'سورة المؤمنون','261','0','0');
+INSERT INTO [t11099] VALUES (17,'سورة النور','263','0','0');
+INSERT INTO [t11099] VALUES (18,'سورة القصص','272','0','0');
+INSERT INTO [t11099] VALUES (19,'قصة موسى وفرعون في السور الأخرى','286','0','0');
+INSERT INTO [t11099] VALUES (20,'سورة الزمر','308','0','0');
+INSERT INTO [t11099] VALUES (21,'سورة الحجرات','340','0','0');
+INSERT INTO [t11099] VALUES (22,'سورة الجن','346','0','0');
+INSERT INTO [t11099] VALUES (23,'سورة المدثر','355','0','0');
+INSERT INTO [t11099] VALUES (24,'سورة العلق','358','0','0');
+INSERT INTO [t11099] VALUES (25,'تفسير آيات من السور القصار','363','0','0');
+INSERT INTO [t11099] VALUES (26,'قصة سبب نزول: {تبت يدا أبي لهب وتب} إلى آخر السورة','369','0','0');
+INSERT INTO [t11099] VALUES (27,'سورة الإخلاص','371','0','0');
+INSERT INTO [t11099] VALUES (28,'سورة الفلق','372','0','0');
+INSERT INTO [t11099] VALUES (29,'سورة الناس','374','0','0');
+INSERT INTO [t11099] VALUES (30,'مصادر ومراجع','377','0','0');
