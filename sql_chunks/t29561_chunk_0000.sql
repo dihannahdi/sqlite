2e4cb964870d4d1fa8586b9431039148a53b45c5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:23 +0700

    Upload SQL chunk: t29561_chunk_0000.sql

---
+++ sql_chunks/t29561_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t29561
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t29561" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29561
+INSERT INTO [t29561] VALUES (1,'المقدمة','3','0','0');
+INSERT INTO [t29561] VALUES (2,'ترجمة المؤلف','3','1','0');
+INSERT INTO [t29561] VALUES (3,'إجازاته العلمية','3','2','0');
+INSERT INTO [t29561] VALUES (4,'تلاميذه','4','2','0');
+INSERT INTO [t29561] VALUES (5,'من أبرزهم','4','4','0');
+INSERT INTO [t29561] VALUES (6,'مؤلفاته','4','2','0');
+INSERT INTO [t29561] VALUES (7,'الشيخ فيصل وجهوده الفقهيه','5','2','0');
+INSERT INTO [t29561] VALUES (8,'وفاته','6','2','0');
+INSERT INTO [t29561] VALUES (9,'١ - التعريف بالرسالة الأولى','7','1','0');
+INSERT INTO [t29561] VALUES (10,'٢ - التعريف بالرسالة الثانية','8','1','0');
+INSERT INTO [t29561] VALUES (11,'(فضل علم الفرائض)','15','0','0');
+INSERT INTO [t29561] VALUES (12,'(فصل في أنواع الإرث)','16','0','0');
+INSERT INTO [t29561] VALUES (13,'١ - (الإرث بالفرض)','16','12','0');
+INSERT INTO [t29561] VALUES (14,'٢ - (الإرث بالتعصيب)','19','12','0');
+INSERT INTO [t29561] VALUES (15,'٣ - (ذوو الأرحام)','22','12','0');
+INSERT INTO [t29561] VALUES (16,'[فصل]','23','0','0');
+INSERT INTO [t29561] VALUES (17,'تتمة','29','0','0');
