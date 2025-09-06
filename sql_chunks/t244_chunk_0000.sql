57fa1d820692f2c3ae97eb4ed07b73d7f3a26c9f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:49:21 +0700

    Upload SQL chunk: t244_chunk_0000.sql

---
+++ sql_chunks/t244_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t244
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t244" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t244
+INSERT INTO [t244] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t244] VALUES (2,'وبه نستعين','1','0','0');
+INSERT INTO [t244] VALUES (3,'مقدمة','1','0','0');
+INSERT INTO [t244] VALUES (4,'ذكر المعقول في بر الوالدين وصلة الرحم','1','0','0');
+INSERT INTO [t244] VALUES (5,'ذكر ما أمر الله به من بر الوالدين وصلة الرحم','1','0','0');
+INSERT INTO [t244] VALUES (6,'ذكر ما أمرت به السنة من بر الوالدين','2','0','0');
+INSERT INTO [t244] VALUES (7,'تقديم بر الوالدين على الجهاد والهجرة','2','0','0');
+INSERT INTO [t244] VALUES (8,'أحب الأعمال إلى الله بر الوالدين','2','0','0');
+INSERT INTO [t244] VALUES (9,'البر يزيد في العمر','2','0','0');
+INSERT INTO [t244] VALUES (10,'كيفة بر الوالدين','2','0','0');
+INSERT INTO [t244] VALUES (11,'تقديم الأم في البر','3','0','0');
+INSERT INTO [t244] VALUES (12,'ما يجزي به الولد والديه','4','0','0');
+INSERT INTO [t244] VALUES (13,'ثواب بر الوالدين','4','0','0');
+INSERT INTO [t244] VALUES (14,'من كان يبالغ في بر الوالدين','5','0','0');
+INSERT INTO [t244] VALUES (15,'إثم عقوق الوالدين','6','0','0');
+INSERT INTO [t244] VALUES (16,'شؤم العاق لوالديه','7','0','0');
+INSERT INTO [t244] VALUES (17,'كيفية العقوق','8','0','0');
+INSERT INTO [t244] VALUES (18,'إجابة دعوة الوالدين للولد','8','0','0');
+INSERT INTO [t244] VALUES (19,'إجابة دعوة الوالدين على الولد','9','0','0');
+INSERT INTO [t244] VALUES (20,'من تبرأ من والديه أو ولده','9','0','0');
+INSERT INTO [t244] VALUES (21,'إثم من دعي لغير أبيه','9','0','0');
+INSERT INTO [t244] VALUES (22,'إثم من تسبب في شتم الأبوين','9','0','0');
+INSERT INTO [t244] VALUES (23,'جواز رجوع الوالد في هبته','9','0','0');
+INSERT INTO [t244] VALUES (24,'صلة الوالدين بعد موتهما','9','0','0');
+INSERT INTO [t244] VALUES (25,'صلة أقاربهما وأصدقائهما','10','0','0');
+INSERT INTO [t244] VALUES (26,'زيارة قبرهما','10','0','0');
+INSERT INTO [t244] VALUES (27,'ثواب صلة الرحم وعقوبة قطعه','11','0','0');
