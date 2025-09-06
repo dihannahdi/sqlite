5d9553e1ce00af23f99794fa603ae21e8187c0dc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:23:53 +0700

    Upload SQL chunk: t1510_chunk_0000.sql

---
+++ sql_chunks/t1510_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t1510
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t1510" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1510
+INSERT INTO [t1510] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1510] VALUES (2,'الداعية: حياته وخدماته','15','0','0');
+INSERT INTO [t1510] VALUES (3,'الجزيرة العربية أيام ازدهاد المسلين','15','2','0');
+INSERT INTO [t1510] VALUES (4,'في ميزان الحكم','60','0','0');
+INSERT INTO [t1510] VALUES (5,'المؤلفات','116','0','0');
+INSERT INTO [t1510] VALUES (6,'الدعوة وحقيقتها','127','0','0');
+INSERT INTO [t1510] VALUES (7,'افتراءات وأكاذيب','142','0','0');
+INSERT INTO [t1510] VALUES (8,'نظرة في المراجع','163','0','0');
+INSERT INTO [t1510] VALUES (9,'مصادر ومراجع','184','0','0');
