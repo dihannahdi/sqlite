b7ea41f7546e9c100779eaaf3fdc72174933745e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:10 +0700

    Upload SQL chunk: t1017_chunk_0000.sql

---
+++ sql_chunks/t1017_chunk_0000.sql
@@ -0,0 +1,44 @@
+-- Table: t1017
+-- Chunk: 1
+-- Rows: 37
+
+CREATE TABLE "t1017" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1017
+INSERT INTO [t1017] VALUES (1,'بدر شاكر السياب دراسة في حياته وشعره','1','0','0');
+INSERT INTO [t1017] VALUES (2,'مقدمة','2','0','0');
+INSERT INTO [t1017] VALUES (3,'البحث عن النخلة','9','0','0');
+INSERT INTO [t1017] VALUES (4,'أحجار جيكور','10','0','0');
+INSERT INTO [t1017] VALUES (5,'صورة وذكريات','18','0','0');
+INSERT INTO [t1017] VALUES (6,'بواكير الشعر','23','0','0');
+INSERT INTO [t1017] VALUES (7,'راع دون قطيع','32','0','0');
+INSERT INTO [t1017] VALUES (8,'بين الأقحوانة وذات المنديل','39','0','0');
+INSERT INTO [t1017] VALUES (9,'الهوى البكر','63','0','0');
+INSERT INTO [t1017] VALUES (10,'الانتماء الشيوعي','82','0','0');
+INSERT INTO [t1017] VALUES (11,'من الوثبة إلى النكبة','93','0','0');
+INSERT INTO [t1017] VALUES (12,'المنتظرة','102','0','0');
+INSERT INTO [t1017] VALUES (13,'المعلم المفصول','116','0','0');
+INSERT INTO [t1017] VALUES (14,'أساطير','125','0','0');
+INSERT INTO [t1017] VALUES (15,'فجر السلام','142','0','0');
+INSERT INTO [t1017] VALUES (16,'حفار القبور','152','0','0');
+INSERT INTO [t1017] VALUES (17,'يا مهلك موسى ومنجي فرعون','162','0','0');
+INSERT INTO [t1017] VALUES (18,'الأسلحة والأطفال','174','0','0');
+INSERT INTO [t1017] VALUES (19,'المومس العمياء','186','0','0');
+INSERT INTO [t1017] VALUES (20,'أنشودة المطر','199','0','0');
+INSERT INTO [t1017] VALUES (21,'تجلي إرم','208','0','0');
+INSERT INTO [t1017] VALUES (22,'انفصام الرابطة الحزبية','210','0','0');
+INSERT INTO [t1017] VALUES (23,'فترة الآداب','218','0','0');
+INSERT INTO [t1017] VALUES (24,'نحو القومية','236','0','0');
+INSERT INTO [t1017] VALUES (25,'الينابيع الثقافية','243','0','0');
+INSERT INTO [t1017] VALUES (26,'الألوهية والقصائد الكهفية','260','0','0');
+INSERT INTO [t1017] VALUES (27,'تعتيم في المبنى والموضوع','269','0','0');
+INSERT INTO [t1017] VALUES (28,'سلال الصبار في بابل','276','0','0');
+INSERT INTO [t1017] VALUES (29,'بين النقد والصحافة','278','0','0');
+INSERT INTO [t1017] VALUES (30,'ولكن..متى كنت شيوعيا','285','0','0');
+INSERT INTO [t1017] VALUES (31,'تموز - المسيح','296','0','0');
+INSERT INTO [t1017] VALUES (32,'سربروس في بابل','315','0','0');
+INSERT INTO [t1017] VALUES (33,'عولس ينتظر المعجزة','332','0','0');
+INSERT INTO [t1017] VALUES (34,'اضمحلال الرموز','361','0','0');
+INSERT INTO [t1017] VALUES (35,'أسطورتان','371','0','0');
+INSERT INTO [t1017] VALUES (36,'مذكرات أمس اليوم','378','0','0');
+INSERT INTO [t1017] VALUES (37,'خاتمة','398','0','0');
