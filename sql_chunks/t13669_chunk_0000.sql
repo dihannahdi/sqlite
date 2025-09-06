69a2bb1ee8275a5de090bfd9edde4b5848888f83
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:15 +0700

    Upload SQL chunk: t13669_chunk_0000.sql

---
+++ sql_chunks/t13669_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t13669
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t13669" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13669
+INSERT INTO [t13669] VALUES (1,'إحصائيات وأرقام','1','0','0');
+INSERT INTO [t13669] VALUES (2,'تعقيب وتحليل','4','0','0');
+INSERT INTO [t13669] VALUES (3,'كيف يمكن حسم القضية إذن؟','4','0','0');
+INSERT INTO [t13669] VALUES (4,'الموضوعات الطبية التى يجب أن تعالج معالجة علمية وفنية','8','0','0');
+INSERT INTO [t13669] VALUES (5,'موضوعات درامية متنوعة','9','0','0');
+INSERT INTO [t13669] VALUES (6,'ملحوظة','10','0','0');
+INSERT INTO [t13669] VALUES (7,'الغلاف','11','0','0');
+INSERT INTO [t13669] VALUES (8,'شهادة','12','0','0');
