1ebe685c5492b2d287ef70385860d1b45709d37a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:43 +0700

    Upload SQL chunk: t35888_chunk_0000.sql

---
+++ sql_chunks/t35888_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t35888
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t35888" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t35888
+INSERT INTO [t35888] VALUES (1,'الباب الأول في الحكايات','1','0','0');
+INSERT INTO [t35888] VALUES (2,'الباب الثاني مناظرة النرجس والورد','64','0','0');
+INSERT INTO [t35888] VALUES (3,'الباب الثالث مقاطيع جيدة وقصائد رائقة','77','0','0');
+INSERT INTO [t35888] VALUES (4,'الباب الرابع لامية','144','0','0');
+INSERT INTO [t35888] VALUES (5,'الباب الخامس تغريد الصادح','156','0','0');
+INSERT INTO [t35888] VALUES (6,'ضرب مثل','183','5','0');
+INSERT INTO [t35888] VALUES (7,'ضرب مثل','185','5','0');
+INSERT INTO [t35888] VALUES (8,'ضرب مثل','189','5','0');
+INSERT INTO [t35888] VALUES (9,'ضرب مثل','192','5','0');
+INSERT INTO [t35888] VALUES (10,'مثل آخر','193','5','0');
+INSERT INTO [t35888] VALUES (11,'الحكمة من الشعر والأمثال','193','5','0');
