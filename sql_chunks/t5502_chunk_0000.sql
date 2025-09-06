ae8945a636409e4f5f5d3643c25d91827c2c59c1
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:23:19 +0700

    Upload SQL chunk: t5502_chunk_0000.sql

---
+++ sql_chunks/t5502_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t5502
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t5502" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5502
+INSERT INTO [t5502] VALUES (1,'باب في نقد الشعر','1','0','0');
+INSERT INTO [t5502] VALUES (2,'من أحسن ما قيل في الأوصاف والتشبيه','24','0','0');
+INSERT INTO [t5502] VALUES (3,'مما قيل في الليلة المقمرة والليالي المظلمة','37','0','0');
+INSERT INTO [t5502] VALUES (4,'مما يستحسن في وصف الشمس','40','0','0');
+INSERT INTO [t5502] VALUES (5,'مما يستحسن من تشبيهات ابن المعتز','42','0','0');
+INSERT INTO [t5502] VALUES (6,'مليح التشبيه للمحدثين','50','0','0');
+INSERT INTO [t5502] VALUES (7,'أنواع التشبيه عند العرب','55','0','0');
+INSERT INTO [t5502] VALUES (8,'من عجيب التشبيه','59','0','0');
+INSERT INTO [t5502] VALUES (9,'كلام يحيى بن خالد','113','0','0');
+INSERT INTO [t5502] VALUES (10,'تاريخ العربية','116','0','0');
+INSERT INTO [t5502] VALUES (11,'من أخبار النحاة والعلماء','120','0','0');
+INSERT INTO [t5502] VALUES (12,'مختارات من الشعر والخبر','124','0','0');
+INSERT INTO [t5502] VALUES (13,'مختار من كلام البلغاء','212','0','0');
