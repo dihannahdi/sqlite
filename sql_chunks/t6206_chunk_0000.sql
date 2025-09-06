bd0c487c769677261c0ce142480982f17d826285
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:28:50 +0700

    Upload SQL chunk: t6206_chunk_0000.sql

---
+++ sql_chunks/t6206_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t6206
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t6206" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6206
+INSERT INTO [t6206] VALUES (1,'من سورة المؤمنون','78','0','0');
+INSERT INTO [t6206] VALUES (2,'من سورة النور','78','0','0');
+INSERT INTO [t6206] VALUES (3,'من سورة المجادلة','106','0','0');
+INSERT INTO [t6206] VALUES (4,'من سورة الصف','117','0','0');
+INSERT INTO [t6206] VALUES (5,'من سورة الجمعة','125','0','0');
+INSERT INTO [t6206] VALUES (6,'من سورة المنافقون','155','0','0');
+INSERT INTO [t6206] VALUES (7,'من سورة التغابن','157','0','0');
+INSERT INTO [t6206] VALUES (8,'من سورة الطلاق','162','0','0');
