f7977d5cf034c14d8c81dcc9f7e0a663c41bd85d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:32 +0700

    Upload SQL chunk: t7534_chunk_0000.sql

---
+++ sql_chunks/t7534_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t7534
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t7534" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7534
+INSERT INTO [t7534] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7534] VALUES (2,'كتاب الطهارة','2','0','0');
+INSERT INTO [t7534] VALUES (3,'كتاب الصلاة','14','0','0');
+INSERT INTO [t7534] VALUES (4,'كتاب الزكاة','35','0','0');
+INSERT INTO [t7534] VALUES (5,'كتاب الصوم','48','0','0');
+INSERT INTO [t7534] VALUES (6,'كتاب الحج','57','0','0');
+INSERT INTO [t7534] VALUES (7,'كتاب البيع','59','0','0');
+INSERT INTO [t7534] VALUES (8,'كتاب الرهن','81','0','0');
+INSERT INTO [t7534] VALUES (9,'كتاب الوكالة','91','0','0');
+INSERT INTO [t7534] VALUES (10,'كتاب الاقرار','94','0','0');
+INSERT INTO [t7534] VALUES (11,'كتاب الغصب','96','0','0');
+INSERT INTO [t7534] VALUES (12,'كتاب الشفعة','101','0','0');
+INSERT INTO [t7534] VALUES (13,'كتاب الإجارة','103','0','0');
+INSERT INTO [t7534] VALUES (14,'كتاب المأذون','106','0','0');
+INSERT INTO [t7534] VALUES (15,'كتاب الوديعة','110','0','0');
+INSERT INTO [t7534] VALUES (16,'كتاب النكاح','113','0','0');
+INSERT INTO [t7534] VALUES (17,'كتاب الطلاق','135','0','0');
+INSERT INTO [t7534] VALUES (18,'كتاب الحدود','148','0','0');
+INSERT INTO [t7534] VALUES (19,'كتاب السرقة','151','0','0');
+INSERT INTO [t7534] VALUES (20,'كتاب الجهاد','155','0','0');
+INSERT INTO [t7534] VALUES (21,'كتاب الصيد','160','0','0');
+INSERT INTO [t7534] VALUES (22,'كتاب الأيمان','165','0','0');
+INSERT INTO [t7534] VALUES (23,'كتاب أدب القاضي','169','0','0');
+INSERT INTO [t7534] VALUES (24,'كتاب الشهادات','173','0','0');
+INSERT INTO [t7534] VALUES (25,'كتاب العتاق','176','0','0');
+INSERT INTO [t7534] VALUES (26,'خاتمة','179','0','0');
