7cda0d61e362314c5f880aa5bfad1b3f4779a876
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:18 +0700

    Upload SQL chunk: t7060_chunk_0000.sql

---
+++ sql_chunks/t7060_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t7060
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t7060" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7060
+INSERT INTO [t7060] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t7060] VALUES (2,'كتاب ما عربته العرب من الكلام الأعجمي','1','0','0');
+INSERT INTO [t7060] VALUES (3,'هذا آخر ما ذكره الشيخ أبو محمد بن بري','132','0','0');
