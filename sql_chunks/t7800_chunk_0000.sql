be4e27b760608be35a1e7d3ea2a087453e91eb79
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:43:11 +0700

    Upload SQL chunk: t7800_chunk_0000.sql

---
+++ sql_chunks/t7800_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t7800
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t7800" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7800
+INSERT INTO [t7800] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7800] VALUES (2,'فصل في تهاون الناس في صلاة الفجر','5','0','0');
+INSERT INTO [t7800] VALUES (3,'فصل في الترغيب في حضور الفجر جماعة والترهيب من تركها','8','0','0');
+INSERT INTO [t7800] VALUES (4,'فصل في فضل قيام الليل','15','0','0');
+INSERT INTO [t7800] VALUES (5,'فصل فيما يعود على المسلم من قيام الليل في الدنيا والآخرة','20','0','0');
+INSERT INTO [t7800] VALUES (6,'فصل في الأسباب المعينة على قيام الليل','30','0','0');
+INSERT INTO [t7800] VALUES (7,'فصل في الأسباب الصارفة عن القيام','48','0','0');
+INSERT INTO [t7800] VALUES (8,'فصل في الترهيب من ترك قيام الليل','54','0','0');
+INSERT INTO [t7800] VALUES (9,'فصل فيما جاء عن رسول الله -صلى الله عليه وسلم- في قيام الليل','55','0','0');
+INSERT INTO [t7800] VALUES (10,'فصل بعض الآثار عن السلف الصالح في قيام الليل','57','0','0');
+INSERT INTO [t7800] VALUES (11,'خاتمة','61','0','0');
