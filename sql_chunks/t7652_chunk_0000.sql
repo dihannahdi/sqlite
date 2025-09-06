94d31e954250d719490710cb813d5ef5db641723
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:41:09 +0700

    Upload SQL chunk: t7652_chunk_0000.sql

---
+++ sql_chunks/t7652_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7652
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7652" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7652
+INSERT INTO [t7652] VALUES (1,'الشيخ عبد العزيز بن باز رحمه الله نموذج من الرعيل الأول','1','0','0');
+INSERT INTO [t7652] VALUES (2,'محاضرة ألقاها: عبد المحسن بن حمد العباد البدر','1','0','0');
