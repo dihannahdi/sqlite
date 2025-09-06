a4a427dd2e565a2c06d7cb739e812c5f9b1425d6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:35:31 +0700

    Upload SQL chunk: t193_chunk_0000.sql

---
+++ sql_chunks/t193_chunk_0000.sql
@@ -0,0 +1,50 @@
+-- Table: t193
+-- Chunk: 1
+-- Rows: 43
+
+CREATE TABLE "t193" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t193
+INSERT INTO [t193] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t193] VALUES (2,'وبه نستعين على القوم الكافرين','1','0','0');
+INSERT INTO [t193] VALUES (3,'الحديث الأول','1','0','0');
+INSERT INTO [t193] VALUES (4,'الحديث الثاني','1','0','0');
+INSERT INTO [t193] VALUES (5,'الحديث الثالث','1','0','0');
+INSERT INTO [t193] VALUES (6,'الحديث الرابع','1','0','0');
+INSERT INTO [t193] VALUES (7,'الحديث الخامس','1','0','0');
+INSERT INTO [t193] VALUES (8,'الحديث السادس','1','0','0');
+INSERT INTO [t193] VALUES (9,'الحديث السابع','1','0','0');
+INSERT INTO [t193] VALUES (10,'الحديث الثامن','1','0','0');
+INSERT INTO [t193] VALUES (11,'الحديث التاسع','1','0','0');
+INSERT INTO [t193] VALUES (12,'الحديث العاشر','1','0','0');
+INSERT INTO [t193] VALUES (13,'الحديث الحادي عشر','1','0','0');
+INSERT INTO [t193] VALUES (14,'الحديث الثاني عشر','1','0','0');
+INSERT INTO [t193] VALUES (15,'الحديث الثالث عشر','1','0','0');
+INSERT INTO [t193] VALUES (16,'الحديث الرابع عشر','1','0','0');
+INSERT INTO [t193] VALUES (17,'الحديث الخامس عشر','1','0','0');
+INSERT INTO [t193] VALUES (18,'الحديث السادس عشر','1','0','0');
+INSERT INTO [t193] VALUES (19,'الحديث السابع عشر','1','0','0');
+INSERT INTO [t193] VALUES (20,'الحديث الثامن عشر','2','0','0');
+INSERT INTO [t193] VALUES (21,'الحديث التاسع عشر','2','0','0');
+INSERT INTO [t193] VALUES (22,'الحديث العشرون','2','0','0');
+INSERT INTO [t193] VALUES (23,'الحديث الحادي والعشرون','2','0','0');
+INSERT INTO [t193] VALUES (24,'الحديث الثاني والعشرون','2','0','0');
+INSERT INTO [t193] VALUES (25,'الحديث الثالث والعشرون','2','0','0');
+INSERT INTO [t193] VALUES (26,'الحديث الرابع والعشرون','2','0','0');
+INSERT INTO [t193] VALUES (27,'الحديث الخامس والعشرون','2','0','0');
+INSERT INTO [t193] VALUES (28,'الحديث السادس والعشرون','2','0','0');
+INSERT INTO [t193] VALUES (29,'الحديث السابع والعشرون','2','0','0');
+INSERT INTO [t193] VALUES (30,'الحديث الثامن والعشرون','2','0','0');
+INSERT INTO [t193] VALUES (31,'الحديث التاسع والعشرون','2','0','0');
+INSERT INTO [t193] VALUES (32,'الحديث الثلاثون','2','0','0');
+INSERT INTO [t193] VALUES (33,'الحديث الحادي والثلاثون','3','0','0');
+INSERT INTO [t193] VALUES (34,'الحديث الثاني والثلاثون','3','0','0');
+INSERT INTO [t193] VALUES (35,'الحديث الثالث والثلاثون','3','0','0');
+INSERT INTO [t193] VALUES (36,'الحديث الرابع والثلاثون','3','0','0');
+INSERT INTO [t193] VALUES (37,'الحديث الخامس والثلاثون','3','0','0');
+INSERT INTO [t193] VALUES (38,'الحديث السادس والثلاثون','3','0','0');
+INSERT INTO [t193] VALUES (39,'الحديث السابع والثلاثون','3','0','0');
+INSERT INTO [t193] VALUES (40,'الحديث الثامن والثلاثون','3','0','0');
+INSERT INTO [t193] VALUES (41,'الحديث التاسع والثلاثون','3','0','0');
+INSERT INTO [t193] VALUES (42,'الحديث الأربعون','3','0','0');
+INSERT INTO [t193] VALUES (43,'خاتمة','3','0','0');
