b8ece60d0d3361afe5951474e680400db0ab288b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:09 +0700

    Upload SQL chunk: t11417_chunk_0000.sql

---
+++ sql_chunks/t11417_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t11417
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t11417" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11417
+INSERT INTO [t11417] VALUES (1,'بلغة الطالب الحثيث في صحيح عوالي الحديث للضياء - الجزء ٢ و ٧ - مخطوط','2','0','0');
