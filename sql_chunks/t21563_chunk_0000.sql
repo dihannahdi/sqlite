527e30d28b71481a4f15a797384d93b953a1f8dc
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:39:44 +0700

    Upload SQL chunk: t21563_chunk_0000.sql

---
+++ sql_chunks/t21563_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t21563
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t21563" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21563
+INSERT INTO [t21563] VALUES (1,'باب ما جاء في وصف النساء','2','0','0');
+INSERT INTO [t21563] VALUES (2,'باب يذكر فيه من صيره العشق إلى الأخلاط والجنون','27','0','0');
+INSERT INTO [t21563] VALUES (3,'باب ما جاء في الغيرة','72','0','0');
+INSERT INTO [t21563] VALUES (4,'باب تابع لما قبله','85','0','0');
+INSERT INTO [t21563] VALUES (5,'باب ما جاء في وفاء النساء','115','0','0');
+INSERT INTO [t21563] VALUES (6,'باب ما جاء في غدر النساء','134','0','0');
+INSERT INTO [t21563] VALUES (7,'باب ما جاء في الزنا والتحذير من عواقبه','158','0','0');
+INSERT INTO [t21563] VALUES (8,'باب ما جاء في ما لا يحاط به','212','0','0');
+INSERT INTO [t21563] VALUES (9,'باب ما جاء في خلق النساء','218','0','0');
