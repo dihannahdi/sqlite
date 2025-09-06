7226d13337df9013961c22e0ac3585af1ed73cce
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:24 +0700

    Upload SQL chunk: t8213_chunk_0000.sql

---
+++ sql_chunks/t8213_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t8213
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t8213" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8213
+INSERT INTO [t8213] VALUES (1,'وفي هذا الباب كراهية التقلل من المطعم ودخول المفاوز بغير زاد ونفقة','86','0','0');
+INSERT INTO [t8213] VALUES (2,'الحجة على الذين يزعمون أنهم يتوكلون فيتركون العمل','105','0','0');
+INSERT INTO [t8213] VALUES (3,'باب جامع التوكل لمن استعمله على الصدق','120','0','0');
