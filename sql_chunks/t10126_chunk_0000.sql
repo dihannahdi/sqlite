f138e5f65cd303b1b9ac029ebc220bdd6af4c191
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:55 +0700

    Upload SQL chunk: t10126_chunk_0000.sql

---
+++ sql_chunks/t10126_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t10126
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t10126" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10126
+INSERT INTO [t10126] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10126] VALUES (2,'المنهج الشيطاني','4','0','0');
+INSERT INTO [t10126] VALUES (3,'المصيدة الرهيبة','6','0','0');
+INSERT INTO [t10126] VALUES (4,'من هنا تسلل قرن الشيطان','8','0','0');
+INSERT INTO [t10126] VALUES (5,'وكرامة الأمة أمانة أيضا','9','0','0');
+INSERT INTO [t10126] VALUES (6,'عتاب وإعجاب','12','0','0');
+INSERT INTO [t10126] VALUES (7,'مؤلف اللأغاني بمنظار الثقات','14','0','0');
+INSERT INTO [t10126] VALUES (8,'شعوبية أم شيعية','17','0','0');
+INSERT INTO [t10126] VALUES (9,'مفتاح هذه الشخصية','19','0','0');
+INSERT INTO [t10126] VALUES (10,'هارون الرشيد بين الحقيقة والخيال','21','0','0');
+INSERT INTO [t10126] VALUES (11,'شهادات حق','24','0','0');
+INSERT INTO [t10126] VALUES (12,'دسائس ومؤامرات','25','0','0');
+INSERT INTO [t10126] VALUES (13,'مع كتاب الأغاني.. وقفات ومناقشات','27','0','0');
+INSERT INTO [t10126] VALUES (14,'المدرسة الإبليسية','33','0','0');
+INSERT INTO [t10126] VALUES (15,'حتى ناصر السنة لم ينج من إفكه','38','0','0');
+INSERT INTO [t10126] VALUES (16,'ألغام في أساس الدولة العباسية','40','0','0');
+INSERT INTO [t10126] VALUES (17,'التناقضات المهلكات','43','0','0');
+INSERT INTO [t10126] VALUES (18,'وحتى حفيدة رسول الله لم تسلم من أذاه','45','0','0');
+INSERT INTO [t10126] VALUES (19,'الشهادة الفاضحة','47','0','0');
