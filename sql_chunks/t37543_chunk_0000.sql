c6ee39cf671c4fd493783a531671efd91cbae528
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:13:47 +0700

    Upload SQL chunk: t37543_chunk_0000.sql

---
+++ sql_chunks/t37543_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t37543
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t37543" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37543
+INSERT INTO [t37543] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t37543] VALUES (2,'الذكر أغلى بضاعة','1','0','0');
+INSERT INTO [t37543] VALUES (3,'الذكر أنيس الذاكرين','4','0','0');
+INSERT INTO [t37543] VALUES (4,'كيف تكون من الذاكرين؟','7','0','0');
+INSERT INTO [t37543] VALUES (5,'المحافظة على الصلوات الخمس','7','4','0');
+INSERT INTO [t37543] VALUES (6,'المحافظة على الأذكار دبر الصلوات','8','4','0');
+INSERT INTO [t37543] VALUES (7,'الإكثار من قراءة كتاب الله تعالى','8','4','0');
+INSERT INTO [t37543] VALUES (8,'المحافظة على الأذكار','9','4','0');
+INSERT INTO [t37543] VALUES (9,'المحافظة على الأذكار التي ورد فضلها','9','4','0');
+INSERT INTO [t37543] VALUES (10,'الذكر غيث القلوب!','10','0','0');
+INSERT INTO [t37543] VALUES (11,'الغافلون عن الذكر','13','0','0');
+INSERT INTO [t37543] VALUES (12,'من كلام العلماء في الذكر','17','0','0');
+INSERT INTO [t37543] VALUES (13,'فضائل الذكر','20','0','0');
+INSERT INTO [t37543] VALUES (14,'نماذج من حياة الذاكرين','25','0','0');
