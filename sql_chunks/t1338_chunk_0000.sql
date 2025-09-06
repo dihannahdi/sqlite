277cec6b1b758956e90e846095ad827831c9b2e7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:14:15 +0700

    Upload SQL chunk: t1338_chunk_0000.sql

---
+++ sql_chunks/t1338_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t1338
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t1338" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1338
+INSERT INTO [t1338] VALUES (1,'(المستشرقون والسنة النبوية)','1','0','0');
+INSERT INTO [t1338] VALUES (2,'شبهة العادة المقدسة والدحض الحتمي','2','0','0');
+INSERT INTO [t1338] VALUES (3,'سلسلتان ... احداهما منقطعة','5','0','0');
