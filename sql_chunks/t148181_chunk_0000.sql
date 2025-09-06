5794b5bc2b01bc6faaf4c4fada8072ebd3be7c41
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:16 +0700

    Upload SQL chunk: t148181_chunk_0000.sql

---
+++ sql_chunks/t148181_chunk_0000.sql
@@ -0,0 +1,37 @@
+-- Table: t148181
+-- Chunk: 1
+-- Rows: 30
+
+CREATE TABLE "t148181" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t148181
+INSERT INTO [t148181] VALUES (1,'تصدير','3','0','0');
+INSERT INTO [t148181] VALUES (2,'مقدمة','5','0','0');
+INSERT INTO [t148181] VALUES (3,'مصطلحات الكتاب','11','0','0');
+INSERT INTO [t148181] VALUES (4,'كلمات جديدة','13','0','0');
+INSERT INTO [t148181] VALUES (5,'مصطلحات الضبط','15','0','0');
+INSERT INTO [t148181] VALUES (6,'(أ)','17','0','0');
+INSERT INTO [t148181] VALUES (7,'(ب)','44','0','0');
+INSERT INTO [t148181] VALUES (8,'(ت)','73','0','0');
+INSERT INTO [t148181] VALUES (9,'(ج)','85','0','0');
+INSERT INTO [t148181] VALUES (10,'(ح)','93','0','0');
+INSERT INTO [t148181] VALUES (11,'(خ)','94','0','0');
+INSERT INTO [t148181] VALUES (12,'(د)','97','0','0');
+INSERT INTO [t148181] VALUES (13,'(ر)','105','0','0');
+INSERT INTO [t148181] VALUES (14,'(ز)','110','0','0');
+INSERT INTO [t148181] VALUES (15,'(س)','111','0','0');
+INSERT INTO [t148181] VALUES (16,'(ش)','122','0','0');
+INSERT INTO [t148181] VALUES (17,'(ص)','130','0','0');
+INSERT INTO [t148181] VALUES (18,'(ط)','132','0','0');
+INSERT INTO [t148181] VALUES (19,'(ع)','136','0','0');
+INSERT INTO [t148181] VALUES (20,'(غ)','138','0','0');
+INSERT INTO [t148181] VALUES (21,'(ف)','141','0','0');
+INSERT INTO [t148181] VALUES (22,'(ق)','152','0','0');
+INSERT INTO [t148181] VALUES (23,'(ك)','157','0','0');
+INSERT INTO [t148181] VALUES (24,'(ل)','178','0','0');
+INSERT INTO [t148181] VALUES (25,'(م)','182','0','0');
+INSERT INTO [t148181] VALUES (26,'(ن)','196','0','0');
+INSERT INTO [t148181] VALUES (27,'(هـ)','202','0','0');
+INSERT INTO [t148181] VALUES (28,'(و)','207','0','0');
+INSERT INTO [t148181] VALUES (29,'(ي)','209','0','0');
+INSERT INTO [t148181] VALUES (30,'مقترحات لغوية','218','0','0');
