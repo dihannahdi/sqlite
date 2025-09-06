587db100f389e08f54b13b664dd535299e4579b7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:57:29 +0700

    Upload SQL chunk: t11443_chunk_0000.sql

---
+++ sql_chunks/t11443_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t11443
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t11443" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11443
+INSERT INTO [t11443] VALUES (1,'تمهيد','1','0','0');
+INSERT INTO [t11443] VALUES (2,'وانسبرة ((j. wansborough)) وأعوانه انتكاسهم في الفكرة الخاطئة القديمة','6','0','0');
+INSERT INTO [t11443] VALUES (3,'يهودا دي نيفو (yehuda de nevo) والمنقحون (the revisionist)','13','0','0');
+INSERT INTO [t11443] VALUES (4,'مخطوطات صنعاء القرآنية وتخمينات المستشرقين المتجددة','22','0','0');
+INSERT INTO [t11443] VALUES (5,'أخطاء بيلامي (j.a. bellamy) الخطيرة','29','0','0');
+INSERT INTO [t11443] VALUES (6,'الخاتمة','36','0','0');
