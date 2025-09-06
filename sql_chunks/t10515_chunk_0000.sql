1c4ebea38de04921a12ab9ed60ac40dddbad10aa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:49:39 +0700

    Upload SQL chunk: t10515_chunk_0000.sql

---
+++ sql_chunks/t10515_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10515
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10515" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10515
+INSERT INTO [t10515] VALUES (1,'الجزء الرابع من الفوائد المنتقاة للقطيعي ٣٦٨ - رواية الجوهري انتقاء ابن أبي السري - مخطوط','1','0','0');
