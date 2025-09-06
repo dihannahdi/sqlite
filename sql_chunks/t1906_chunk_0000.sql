132eacc55e4dbc0e5d0675eec453788f6200bc02
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:57 +0700

    Upload SQL chunk: t1906_chunk_0000.sql

---
+++ sql_chunks/t1906_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t1906
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t1906" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1906
+INSERT INTO [t1906] VALUES (1,'خصائص النظرة الإسلامية للأسرة','1','0','0');
+INSERT INTO [t1906] VALUES (2,'مكانة الزواج','4','0','0');
+INSERT INTO [t1906] VALUES (3,'التشريعات الإسلامية لحماية الحياة الزوجية','8','0','0');
+INSERT INTO [t1906] VALUES (4,'الحقوق الزوجية','13','0','0');
+INSERT INTO [t1906] VALUES (5,'حقوق الزوجة على زوجها','13','4','0');
+INSERT INTO [t1906] VALUES (6,'حقوق الزوج على زوجته','16','4','0');
+INSERT INTO [t1906] VALUES (7,'الحقوق المشتركة','18','4','0');
+INSERT INTO [t1906] VALUES (8,'حقوق الأولاد','21','0','0');
