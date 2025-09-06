ef16e607378c91e2d1d7ca21dd9291effb3df811
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:32:53 +0700

    Upload SQL chunk: t18111_chunk_0000.sql

---
+++ sql_chunks/t18111_chunk_0000.sql
@@ -0,0 +1,53 @@
+-- Table: t18111
+-- Chunk: 1
+-- Rows: 46
+
+CREATE TABLE "t18111" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t18111
+INSERT INTO [t18111] VALUES (1,'مقدمة','2','0','0');
+INSERT INTO [t18111] VALUES (2,'الدراسات السابقة','4','1','0');
+INSERT INTO [t18111] VALUES (3,'مشكلة الدراسة','6','1','0');
+INSERT INTO [t18111] VALUES (4,'خطة البحث','7','1','0');
+INSERT INTO [t18111] VALUES (5,'منهج الدراسة','8','1','0');
+INSERT INTO [t18111] VALUES (6,'عملي في تحقيق نص المنظومة البيقونية','9','1','0');
+INSERT INTO [t18111] VALUES (7,'ترجمة الناظم','12','1','0');
+INSERT INTO [t18111] VALUES (8,'اسم الناظم','12','1','0');
+INSERT INTO [t18111] VALUES (9,'الحديث الصحيح','15','0','0');
+INSERT INTO [t18111] VALUES (10,'الحديث الحسن','17','0','0');
+INSERT INTO [t18111] VALUES (11,'١ - الحسن لذاته','18','10','0');
+INSERT INTO [t18111] VALUES (12,'٢ - الحسن لغيره','19','10','0');
+INSERT INTO [t18111] VALUES (13,'الحديث الضعيف','20','0','0');
+INSERT INTO [t18111] VALUES (14,'الحديث المرفوع والموقوف والمقطوع','22','0','0');
+INSERT INTO [t18111] VALUES (15,'الحديث المسند','27','0','0');
+INSERT INTO [t18111] VALUES (16,'الحديث المتصل','28','0','0');
+INSERT INTO [t18111] VALUES (17,'الحديث المسلسل','29','0','0');
+INSERT INTO [t18111] VALUES (18,'الحديث العزيز والمشهور والغريب','31','0','0');
+INSERT INTO [t18111] VALUES (19,'الحديث المعنعن','34','0','0');
+INSERT INTO [t18111] VALUES (20,'الحديث المبهم','35','0','0');
+INSERT INTO [t18111] VALUES (21,'الحديث العالي والنازل','36','0','0');
+INSERT INTO [t18111] VALUES (22,'الحديث الموقوف','38','0','0');
+INSERT INTO [t18111] VALUES (23,'الحديث المرسل','38','0','0');
+INSERT INTO [t18111] VALUES (24,'الحديث الغريب','39','0','0');
+INSERT INTO [t18111] VALUES (25,'الحديث المنقطع','40','0','0');
+INSERT INTO [t18111] VALUES (26,'الحديث المعضل','41','0','0');
+INSERT INTO [t18111] VALUES (27,'الحديث المدلس','42','0','0');
+INSERT INTO [t18111] VALUES (28,'الحديث الشاذ','44','0','0');
+INSERT INTO [t18111] VALUES (29,'الحديث المقلوب','46','0','0');
+INSERT INTO [t18111] VALUES (30,'الحديث الفرد','49','0','0');
+INSERT INTO [t18111] VALUES (31,'الحديث المعلل','51','0','0');
+INSERT INTO [t18111] VALUES (32,'الحديث المضطرب','53','0','0');
+INSERT INTO [t18111] VALUES (33,'الحديث المدرج','55','0','0');
+INSERT INTO [t18111] VALUES (34,'الحديث المدبج','57','0','0');
+INSERT INTO [t18111] VALUES (35,'المتفق والمفترق','58','0','0');
+INSERT INTO [t18111] VALUES (36,'المؤتلف والمختلف','59','0','0');
+INSERT INTO [t18111] VALUES (37,'الحديث المنكر','60','0','0');
+INSERT INTO [t18111] VALUES (38,'الحديث المتروك','61','0','0');
+INSERT INTO [t18111] VALUES (39,'الحديث الموضوع','62','0','0');
+INSERT INTO [t18111] VALUES (40,'الخاتمة','63','0','0');
+INSERT INTO [t18111] VALUES (41,'الخاتمة وفيها أهم نتائج البحث والتوصيات','65','0','0');
+INSERT INTO [t18111] VALUES (42,'أولا: أهم نتائج الدراسة','66','41','0');
+INSERT INTO [t18111] VALUES (43,'ثانيا: أهم التوصيات','67','41','0');
+INSERT INTO [t18111] VALUES (44,'ثبت المصادر والمراجع','69','0','0');
+INSERT INTO [t18111] VALUES (45,'فهرس المصطلحات الحديثية الواردة في الدراسة','79','0','0');
+INSERT INTO [t18111] VALUES (46,'متن المنظومة البيقونية','86','0','0');
