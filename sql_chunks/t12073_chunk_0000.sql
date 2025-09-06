8a91af2c3087bac19ae801f6f7d21591563183e9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:01:32 +0700

    Upload SQL chunk: t12073_chunk_0000.sql

---
+++ sql_chunks/t12073_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t12073
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t12073" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12073
+INSERT INTO [t12073] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t12073] VALUES (2,'معالم في منهج السلوك','10','0','0');
+INSERT INTO [t12073] VALUES (3,'مصدر تلقي السلوك هو الكتاب والسنة','10','2','0');
+INSERT INTO [t12073] VALUES (4,'الوسطية','18','2','0');
+INSERT INTO [t12073] VALUES (5,'من معالم أهل السنة في السلوك: موافقة النصوص الشرعية لفظا ومعنى','25','2','0');
+INSERT INTO [t12073] VALUES (6,'مراعات أحوال المكلفين، وتحقيق الجانب الواقعي العملي','31','2','0');
+INSERT INTO [t12073] VALUES (7,'مراعات قدرات الناس في فعل الطاعات','39','2','0');
+INSERT INTO [t12073] VALUES (8,'موضوعات سلوكية','44','0','0');
+INSERT INTO [t12073] VALUES (9,'أهل السنة يعلمون الحق ويرحمون الخلق','44','8','0');
+INSERT INTO [t12073] VALUES (10,'تزكية النفوس','51','8','0');
