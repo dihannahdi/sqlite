da5b5d58969ee2c748f8c387725d6d23e587888d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:38:14 +0700

    Upload SQL chunk: t737_chunk_0000.sql

---
+++ sql_chunks/t737_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t737
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t737" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t737
+INSERT INTO [t737] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t737] VALUES (2,'باب','1','1','0');
+INSERT INTO [t737] VALUES (3,'عدة القوافي','1','0','0');
+INSERT INTO [t737] VALUES (4,'باب','2','3','0');
+INSERT INTO [t737] VALUES (5,'الروي','2','0','0');
+INSERT INTO [t737] VALUES (6,'باب','5','5','0');
+INSERT INTO [t737] VALUES (7,'ما يلزم القوافي من الحركات','5','0','0');
+INSERT INTO [t737] VALUES (8,'هذا باب','12','7','0');
+INSERT INTO [t737] VALUES (9,'ما لا يكون رويا','12','0','0');
+INSERT INTO [t737] VALUES (10,'هذا باب','13','9','0');
+INSERT INTO [t737] VALUES (11,'ما يجوز من الساكن مع المتحرك في ضرب واحد','13','0','0');
+INSERT INTO [t737] VALUES (12,'باب','14','11','0');
+INSERT INTO [t737] VALUES (13,'التقييد والإطلاق','14','0','0');
+INSERT INTO [t737] VALUES (14,'باب','15','13','0');
+INSERT INTO [t737] VALUES (15,'ما يجتمع في آخر ساكنان في قافية','15','0','0');
+INSERT INTO [t737] VALUES (16,'هذا باب','15','15','0');
+INSERT INTO [t737] VALUES (17,'ما يكون فيه حرف اللين مما ليس فيه ساكنان','15','0','0');
+INSERT INTO [t737] VALUES (18,'هذا باب','17','17','0');
+INSERT INTO [t737] VALUES (19,'إجماع العرب في الإنشاء واختلافها','17','0','0');
