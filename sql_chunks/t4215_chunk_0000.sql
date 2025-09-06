4e56e4b05f39235105235624364ae6a06fa78845
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:23 +0700

    Upload SQL chunk: t4215_chunk_0000.sql

---
+++ sql_chunks/t4215_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t4215
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t4215" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4215
+INSERT INTO [t4215] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t4215] VALUES (2,'أولا: تحقيقها بين ألقاب الإعراب','3','0','0');
+INSERT INTO [t4215] VALUES (3,'ثانيا: في إبدال بعض الحروف من بعض','5','0','0');
+INSERT INTO [t4215] VALUES (4,'إبدال الهمزة من الهاء','5','3','0');
+INSERT INTO [t4215] VALUES (5,'إبدال الهاء من الهمزة','8','3','0');
+INSERT INTO [t4215] VALUES (6,'قلب الهمزة واوا','11','3','0');
+INSERT INTO [t4215] VALUES (7,'ثالثا: في زيادة بعض الحروف','14','0','0');
