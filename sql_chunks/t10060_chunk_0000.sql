2e426dc5c6b9d59df73c4ce2a91630e79f1c165a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:35 +0700

    Upload SQL chunk: t10060_chunk_0000.sql

---
+++ sql_chunks/t10060_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t10060
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t10060" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10060
+INSERT INTO [t10060] VALUES (1,'ترجمة الشيخ عبد الرحمن الأفريقي','1','0','0');
+INSERT INTO [t10060] VALUES (2,'لفضيلة الشيخ عمر بن محمد فلاته','1','0','0');
+INSERT INTO [t10060] VALUES (3,'ميلاده ونشأته','2','0','0');
+INSERT INTO [t10060] VALUES (4,'وصول الشيخ إلى الحرمين الشريفين','7','0','0');
+INSERT INTO [t10060] VALUES (5,'شيوخه وتلاميذه:','13','0','0');
+INSERT INTO [t10060] VALUES (6,'صفاته وأخلاقه','14','0','0');
+INSERT INTO [t10060] VALUES (7,'مؤلفاته:','16','0','0');
+INSERT INTO [t10060] VALUES (8,'وفاته','20','0','0');
