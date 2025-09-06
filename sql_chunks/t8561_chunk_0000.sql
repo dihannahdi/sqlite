dce9611b1df3db1d6f525c50bafcc8dda7cba7ff
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:15 +0700

    Upload SQL chunk: t8561_chunk_0000.sql

---
+++ sql_chunks/t8561_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t8561
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t8561" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8561
+INSERT INTO [t8561] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8561] VALUES (2,'المقام الأول','8','0','0');
+INSERT INTO [t8561] VALUES (3,'المقام الثاني','10','0','0');
+INSERT INTO [t8561] VALUES (4,'المقام الثالث','12','0','0');
+INSERT INTO [t8561] VALUES (5,'المقام الرابع','15','0','0');
+INSERT INTO [t8561] VALUES (6,'المقام الخامس','18','0','0');
+INSERT INTO [t8561] VALUES (7,'المقام السادس','20','0','0');
+INSERT INTO [t8561] VALUES (8,'المقام السابع','21','0','0');
+INSERT INTO [t8561] VALUES (9,'المقام الثامن','23','0','0');
+INSERT INTO [t8561] VALUES (10,'المقام التاسع','25','0','0');
