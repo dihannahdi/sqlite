1703f98c3bb04f4fb281929731a18c4718e4b2c6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:09 +0700

    Upload SQL chunk: t18428_chunk_0000.sql

---
+++ sql_chunks/t18428_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t18428
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t18428" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t18428
+INSERT INTO [t18428] VALUES (1,'مقدمة المؤلف','2','0','0');
+INSERT INTO [t18428] VALUES (2,'ما هي الخطابة؟','3','0','0');
+INSERT INTO [t18428] VALUES (3,'يقابل الخطابة أربعة فنون','4','2','0');
+INSERT INTO [t18428] VALUES (4,'الخطابة عند الأدباء والبلغاء','7','2','0');
+INSERT INTO [t18428] VALUES (5,'شرف الخطابة','8','0','0');
+INSERT INTO [t18428] VALUES (6,'ماذا تفعل الخطابة؟','10','0','0');
+INSERT INTO [t18428] VALUES (7,'أطوار الخطابة','12','0','0');
+INSERT INTO [t18428] VALUES (8,'أسباب ارتقاء الخطابة','16','0','0');
+INSERT INTO [t18428] VALUES (9,'تعلم الخطابة','17','0','0');
+INSERT INTO [t18428] VALUES (10,'إعطاء الحروف حقها','19','0','0');
+INSERT INTO [t18428] VALUES (11,'حسن الإلقاء','21','0','0');
+INSERT INTO [t18428] VALUES (12,'الإشارة في الخطابة','22','0','0');
+INSERT INTO [t18428] VALUES (13,'القيام بمكان مرتفع حال الخطبة','24','0','0');
+INSERT INTO [t18428] VALUES (14,'الإرتاج في الخطبة','26','0','0');
+INSERT INTO [t18428] VALUES (15,'الارتجال في الخطبة','28','0','0');
+INSERT INTO [t18428] VALUES (16,'الخاتمة','29','0','0');
