d62d85c32623bfb009d4e01c1b2a452e4a54293f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:24:04 +0700

    Upload SQL chunk: t151138_chunk_0000.sql

---
+++ sql_chunks/t151138_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t151138
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t151138" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t151138
+INSERT INTO [t151138] VALUES (1,'فصل في قوله تعالى: {إنما يخشى الله من عباده العلماء}','2','0','0');
+INSERT INTO [t151138] VALUES (2,'فصل','17','0','0');
+INSERT INTO [t151138] VALUES (3,'فصل','39','0','0');
