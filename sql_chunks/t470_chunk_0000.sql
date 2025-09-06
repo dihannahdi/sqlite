dec96d6c9ca7a563915a3f1d921c178cbb13b3e3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:51 +0700

    Upload SQL chunk: t470_chunk_0000.sql

---
+++ sql_chunks/t470_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t470
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t470" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t470
+INSERT INTO [t470] VALUES (1,'يكفي في الضمان لفظ يشعر بالالتزام','2','0','0');
+INSERT INTO [t470] VALUES (2,'قضية أخرى في الضمان','5','0','0');
