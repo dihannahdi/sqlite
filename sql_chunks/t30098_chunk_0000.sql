d623d02dfb8e7b6176d042548fc12feeb2e99ac7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:39 +0700

    Upload SQL chunk: t30098_chunk_0000.sql

---
+++ sql_chunks/t30098_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t30098
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t30098" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30098
+INSERT INTO [t30098] VALUES (1,'الجزء الثاني والثلاثون من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30098] VALUES (2,'من حديث أبي حصين الوادعي','2','0','0');
+INSERT INTO [t30098] VALUES (3,'ومن حديث أبي حصين أيضا','11','0','0');
+INSERT INTO [t30098] VALUES (4,'من فوائد أبي الحسين بن الطيوري','16','0','0');
+INSERT INTO [t30098] VALUES (5,'فوائد أبي محمد الخلال الحافظ','17','0','0');
+INSERT INTO [t30098] VALUES (6,'من فوائد أبي بكر الشافعي','20','0','0');
+INSERT INTO [t30098] VALUES (7,'حديث واحد','24','0','0');
+INSERT INTO [t30098] VALUES (8,'حديث آخر عن شيخ آخر وهو أخوه','26','0','0');
+INSERT INTO [t30098] VALUES (9,'حديث آخر عن غيرهما','28','0','0');
+INSERT INTO [t30098] VALUES (10,'حديثان من حديث أبي بكر بن شاذان','30','0','0');
+INSERT INTO [t30098] VALUES (11,'حديث واحد','33','0','0');
+INSERT INTO [t30098] VALUES (12,'حديث واحد','35','0','0');
+INSERT INTO [t30098] VALUES (13,'إنشادات من رواية ابن الطيوري','37','0','0');
