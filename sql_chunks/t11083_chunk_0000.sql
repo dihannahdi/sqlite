1502c1daea256891c7fb219632ad2bd3207e1211
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:02 +0700

    Upload SQL chunk: t11083_chunk_0000.sql

---
+++ sql_chunks/t11083_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t11083
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t11083" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11083
+INSERT INTO [t11083] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11083] VALUES (2,'نص المحقق','28','0','0');
+INSERT INTO [t11083] VALUES (3,'مقدمة المؤلف','28','2','0');
+INSERT INTO [t11083] VALUES (4,'باب الألف','35','2','0');
+INSERT INTO [t11083] VALUES (5,'باب الباء','49','2','0');
+INSERT INTO [t11083] VALUES (6,'باب التاء','52','2','0');
+INSERT INTO [t11083] VALUES (7,'باب الثاء','53','2','0');
+INSERT INTO [t11083] VALUES (8,'باب الجيم','55','2','0');
+INSERT INTO [t11083] VALUES (9,'باب الحاء','59','2','0');
+INSERT INTO [t11083] VALUES (10,'باب الخاء','72','2','0');
+INSERT INTO [t11083] VALUES (11,'باب الدال','76','2','0');
+INSERT INTO [t11083] VALUES (12,'باب الذال','78','2','0');
+INSERT INTO [t11083] VALUES (13,'باب الراء','79','2','0');
+INSERT INTO [t11083] VALUES (14,'باب الزاء','81','2','0');
+INSERT INTO [t11083] VALUES (15,'باب السين','85','2','0');
+INSERT INTO [t11083] VALUES (16,'باب الشين','97','2','0');
+INSERT INTO [t11083] VALUES (17,'باب الصاد','99','2','0');
+INSERT INTO [t11083] VALUES (18,'باب الضاد','103','2','0');
+INSERT INTO [t11083] VALUES (19,'باب الطاء','104','2','0');
+INSERT INTO [t11083] VALUES (20,'باب العين','106','2','0');
+INSERT INTO [t11083] VALUES (21,'باب الغين','142','2','0');
+INSERT INTO [t11083] VALUES (22,'باب الفاء','144','2','0');
+INSERT INTO [t11083] VALUES (23,'باب القاف','148','2','0');
+INSERT INTO [t11083] VALUES (24,'باب الكاف','151','2','0');
+INSERT INTO [t11083] VALUES (25,'باب اللام','152','2','0');
+INSERT INTO [t11083] VALUES (26,'باب الميم','153','2','0');
+INSERT INTO [t11083] VALUES (27,'باب النون','174','2','0');
+INSERT INTO [t11083] VALUES (28,'باب الواو','178','2','0');
+INSERT INTO [t11083] VALUES (29,'باب الهاء','181','2','0');
+INSERT INTO [t11083] VALUES (30,'باب الياء','184','2','0');
+INSERT INTO [t11083] VALUES (31,'مصادر ومراجع','192','0','0');
