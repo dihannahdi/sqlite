e290b7abfd6dd962ba6f19bcb466b1ca90590626
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:32:51 +0700

    Upload SQL chunk: t6678_chunk_0000.sql

---
+++ sql_chunks/t6678_chunk_0000.sql
@@ -0,0 +1,41 @@
+-- Table: t6678
+-- Chunk: 1
+-- Rows: 34
+
+CREATE TABLE "t6678" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6678
+INSERT INTO [t6678] VALUES (1,'الجزء ١','1','0','0');
+INSERT INTO [t6678] VALUES (2,'حرف الهمزة والألف','5','1','0');
+INSERT INTO [t6678] VALUES (3,'حرف الباء','430','1','0');
+INSERT INTO [t6678] VALUES (4,'حرف التاء المثناة فوق','453','1','0');
+INSERT INTO [t6678] VALUES (5,'حرف الجيم','477','1','0');
+INSERT INTO [t6678] VALUES (6,'حرف الحاء المهملة','492','1','0');
+INSERT INTO [t6678] VALUES (7,'الجزء ٢','499','0','0');
+INSERT INTO [t6678] VALUES (8,'حرف الخاء المعجمة','625','7','0');
+INSERT INTO [t6678] VALUES (9,'حرف الدال المهملة','636','7','0');
+INSERT INTO [t6678] VALUES (10,'حرف الذال المعجمة','655','7','0');
+INSERT INTO [t6678] VALUES (11,'حرف الراء','656','7','0');
+INSERT INTO [t6678] VALUES (12,'حرف الزاي','669','7','0');
+INSERT INTO [t6678] VALUES (13,'حرف السين المهملة','696','7','0');
+INSERT INTO [t6678] VALUES (14,'حرف الشين المعجمة','718','7','0');
+INSERT INTO [t6678] VALUES (15,'حرف الصاد المهملة','734','7','0');
+INSERT INTO [t6678] VALUES (16,'حرف الضاد المعجمة خالي','757','7','0');
+INSERT INTO [t6678] VALUES (17,'حرف الطاء المهملة المشالة','757','7','0');
+INSERT INTO [t6678] VALUES (18,'حرف الظاء المعجمة','758','7','0');
+INSERT INTO [t6678] VALUES (19,'حرف العين المهملة','759','7','0');
+INSERT INTO [t6678] VALUES (20,'الجزء ٣','974','0','0');
+INSERT INTO [t6678] VALUES (21,'بسم الله الرحمن الرحيم','974','20','0');
+INSERT INTO [t6678] VALUES (22,'حرف الغين المعجمة','1216','20','0');
+INSERT INTO [t6678] VALUES (23,'حرف الفاء','1226','20','0');
+INSERT INTO [t6678] VALUES (24,'حرف القاف','1264','20','0');
+INSERT INTO [t6678] VALUES (25,'حرف الكاف','1271','20','0');
+INSERT INTO [t6678] VALUES (26,'حرف اللام','1275','20','0');
+INSERT INTO [t6678] VALUES (27,'حرف الميم','1279','20','0');
+INSERT INTO [t6678] VALUES (28,'الجزء ٤','1468','0','0');
+INSERT INTO [t6678] VALUES (29,'بسم الله الرحمن الرحيم','1468','28','0');
+INSERT INTO [t6678] VALUES (30,'حرف النون','1910','28','0');
+INSERT INTO [t6678] VALUES (31,'حرف الهاء','1925','28','0');
+INSERT INTO [t6678] VALUES (32,'حرف الواو','1927','28','0');
+INSERT INTO [t6678] VALUES (33,'حرف لام ألف خالى','1928','28','0');
+INSERT INTO [t6678] VALUES (34,'حرف الياء','1928','28','0');
