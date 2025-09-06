1c1096dee88e41d91dd02cbd46bf4e34c0b6d124
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:15 +0700

    Upload SQL chunk: t59_chunk_0000.sql

---
+++ sql_chunks/t59_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t59
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t59" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t59
+INSERT INTO [t59] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t59] VALUES (2,'السمة الأولى قطع العلائق عن الخلائق','3','0','0');
+INSERT INTO [t59] VALUES (3,'السمة الثانية العيش مع القرآن','6','0','0');
+INSERT INTO [t59] VALUES (4,'السمة الثالثة جمعية القلب وصدق إقباله','17','0','0');
+INSERT INTO [t59] VALUES (5,'السمة الرابعة استشعار معية الله لعبده','19','0','0');
+INSERT INTO [t59] VALUES (6,'السمة الخامسة تعظيم الله تعالى','21','0','0');
+INSERT INTO [t59] VALUES (7,'السمة السادسة افتقار العبد إلى ربه وشعوره بالحاجة إليه','26','0','0');
+INSERT INTO [t59] VALUES (8,'السمة السابعة استحضار منة الله وفضله','30','0','0');
+INSERT INTO [t59] VALUES (9,'السمة الثامنة الاعتراف بالذنب والتقصير','34','0','0');
+INSERT INTO [t59] VALUES (10,'السمة التاسعة الإقبال على الله بمداومة الذكر','37','0','0');
+INSERT INTO [t59] VALUES (11,'السمة العاشرة الإقبال على الله بكثرة الدعاء','43','0','0');
+INSERT INTO [t59] VALUES (12,'السمة الحادية عشرة الإخبات والخشوع','48','0','0');
+INSERT INTO [t59] VALUES (13,'وختاما','58','0','0');
