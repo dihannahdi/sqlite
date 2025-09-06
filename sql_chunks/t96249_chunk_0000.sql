78e16dc04b119218821ab3cccc63616576d2328b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:35 +0700

    Upload SQL chunk: t96249_chunk_0000.sql

---
+++ sql_chunks/t96249_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t96249
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t96249" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96249
+INSERT INTO [t96249] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t96249] VALUES (2,'القضاء والقدر','1','0','0');
+INSERT INTO [t96249] VALUES (3,'التوكل على الله','31','0','0');
+INSERT INTO [t96249] VALUES (4,'شبهات وإشكالات وتساؤلات في باب التوكل والقدر','37','0','0');
+INSERT INTO [t96249] VALUES (5,'السؤال الأول','37','4','0');
+INSERT INTO [t96249] VALUES (6,'السؤال الثاني','40','4','0');
+INSERT INTO [t96249] VALUES (7,'السؤال الثالث','43','4','0');
+INSERT INTO [t96249] VALUES (8,'السؤال الرابع','43','4','0');
+INSERT INTO [t96249] VALUES (9,'السؤال الخامس','44','4','0');
+INSERT INTO [t96249] VALUES (10,'السؤال السادس','48','4','0');
+INSERT INTO [t96249] VALUES (11,'السؤال السابع','48','4','0');
+INSERT INTO [t96249] VALUES (12,'السؤال الثامن','49','4','0');
+INSERT INTO [t96249] VALUES (13,'السؤال التاسع','51','4','0');
+INSERT INTO [t96249] VALUES (14,'السؤال العاشر','55','4','0');
+INSERT INTO [t96249] VALUES (15,'السؤال الحادي عشر','61','4','0');
+INSERT INTO [t96249] VALUES (16,'السؤال الثاني عشر','63','4','0');
+INSERT INTO [t96249] VALUES (17,'السؤال الثالث عشر','65','4','0');
+INSERT INTO [t96249] VALUES (18,'الخاتمة','67','0','0');
