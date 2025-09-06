4f23f2b2cec4e709420ef47c99e5b8c6a309ca85
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:54 +0700

    Upload SQL chunk: t10441_chunk_0000.sql

---
+++ sql_chunks/t10441_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t10441
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t10441" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10441
+INSERT INTO [t10441] VALUES (1,'الأربعين_..','2','0','0');
