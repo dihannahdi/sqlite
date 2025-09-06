7327820ad1f29ae259d5ef3d0b7fe4b8d408f490
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:53:09 +0700

    Upload SQL chunk: t26677_chunk_0000.sql

---
+++ sql_chunks/t26677_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t26677
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t26677" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26677
+INSERT INTO [t26677] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t26677] VALUES (2,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t26677] VALUES (3,'الباب الأول في بيان شرف العمر والحث على اغتنامه في الخير','4','0','0');
+INSERT INTO [t26677] VALUES (4,'الباب الثاني في ذكر من كان يبادر العمر ويبالغ في حفظ لحظاته','10','0','0');
+INSERT INTO [t26677] VALUES (5,'الباب الثالث في ذكر سبب تضييع العمر','29','0','0');
