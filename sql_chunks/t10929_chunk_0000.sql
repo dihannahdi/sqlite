d08f1290a5f4872656e24aefff5e028574368f03
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:52:29 +0700

    Upload SQL chunk: t10929_chunk_0000.sql

---
+++ sql_chunks/t10929_chunk_0000.sql
@@ -0,0 +1,63 @@
+-- Table: t10929
+-- Chunk: 1
+-- Rows: 56
+
+CREATE TABLE "t10929" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10929
+INSERT INTO [t10929] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t10929] VALUES (2,'باب مخارج الحروف وألقابها وصفاتها','4','0','0');
+INSERT INTO [t10929] VALUES (3,'فصل','7','2','0');
+INSERT INTO [t10929] VALUES (4,'فصل','11','2','0');
+INSERT INTO [t10929] VALUES (5,'فصل في الحروف المشربة','11','2','0');
+INSERT INTO [t10929] VALUES (6,'فائدتان','18','2','0');
+INSERT INTO [t10929] VALUES (7,'فصل الألف المتحركة','19','2','0');
+INSERT INTO [t10929] VALUES (8,'فصل الباء','22','2','0');
+INSERT INTO [t10929] VALUES (9,'فصل التاء','23','2','0');
+INSERT INTO [t10929] VALUES (10,'فصل الثاء المثلثة','25','2','0');
+INSERT INTO [t10929] VALUES (11,'فصل الجيم','26','2','0');
+INSERT INTO [t10929] VALUES (12,'فصل الهاء','27','2','0');
+INSERT INTO [t10929] VALUES (13,'فصل الحاء','27','2','0');
+INSERT INTO [t10929] VALUES (14,'فصل الدال','29','2','0');
+INSERT INTO [t10929] VALUES (15,'فصل الذال','30','2','0');
+INSERT INTO [t10929] VALUES (16,'فصل الراء','31','2','0');
+INSERT INTO [t10929] VALUES (17,'فصل الزاي','34','2','0');
+INSERT INTO [t10929] VALUES (18,'فصل الطاء المهملة','34','2','0');
+INSERT INTO [t10929] VALUES (19,'فصل الظاء المعجمة المشالة','35','2','0');
+INSERT INTO [t10929] VALUES (20,'فصل الكاف','46','2','0');
+INSERT INTO [t10929] VALUES (21,'فصل اللام','47','2','0');
+INSERT INTO [t10929] VALUES (22,'فصل الميم','49','2','0');
+INSERT INTO [t10929] VALUES (23,'فصل النون','51','2','0');
+INSERT INTO [t10929] VALUES (24,'فصل الصاد','54','2','0');
+INSERT INTO [t10929] VALUES (25,'فصل الضاد المعجمة','55','2','0');
+INSERT INTO [t10929] VALUES (26,'فصل العين المهملة','60','2','0');
+INSERT INTO [t10929] VALUES (27,'فصل الغين المعجمة','61','2','0');
+INSERT INTO [t10929] VALUES (28,'فصل الفاء','61','2','0');
+INSERT INTO [t10929] VALUES (29,'فصل القاف','62','2','0');
+INSERT INTO [t10929] VALUES (30,'فصل السين','63','2','0');
+INSERT INTO [t10929] VALUES (31,'فصل الشين المعجمة','65','2','0');
+INSERT INTO [t10929] VALUES (32,'فصل الهاء','65','2','0');
+INSERT INTO [t10929] VALUES (33,'فصل الواو','66','2','0');
+INSERT INTO [t10929] VALUES (34,'فصل لا','68','2','0');
+INSERT INTO [t10929] VALUES (35,'فصل الياء','70','2','0');
+INSERT INTO [t10929] VALUES (36,'باب أحكام النون الساكنة والتنوين','72','0','0');
+INSERT INTO [t10929] VALUES (37,'فوا ئد','75','36','0');
+INSERT INTO [t10929] VALUES (38,'باب لأستعاذة','77','0','0');
+INSERT INTO [t10929] VALUES (39,'باب البسملة','79','0','0');
+INSERT INTO [t10929] VALUES (40,'باب القصر والمد','80','0','0');
+INSERT INTO [t10929] VALUES (41,'فصل','85','40','0');
+INSERT INTO [t10929] VALUES (42,'تنبيهات','87','40','0');
+INSERT INTO [t10929] VALUES (43,'باب المشدد','92','0','0');
+INSERT INTO [t10929] VALUES (44,'باب ألفات الوصل','94','0','0');
+INSERT INTO [t10929] VALUES (45,'فصل','97','44','0');
+INSERT INTO [t10929] VALUES (46,'فصل','99','44','0');
+INSERT INTO [t10929] VALUES (47,'باب الوقف والابتداء','100','0','0');
+INSERT INTO [t10929] VALUES (48,'فصل في الوقف الكافي والاكفى','106','47','0');
+INSERT INTO [t10929] VALUES (49,'فصل في الوقف الحسن والأحسن','107','47','0');
+INSERT INTO [t10929] VALUES (50,'فصل في الوقف القبيح والأقبح','108','47','0');
+INSERT INTO [t10929] VALUES (51,'فصل في الابتداء','110','47','0');
+INSERT INTO [t10929] VALUES (52,'الباب الثانى فى معرفة كيفية الوقف على أخر الكلمة','113','47','0');
+INSERT INTO [t10929] VALUES (53,'تنبيه','114','47','0');
+INSERT INTO [t10929] VALUES (54,'فوائد','115','47','0');
+INSERT INTO [t10929] VALUES (55,'فصل في الوقف على الراء','118','47','0');
+INSERT INTO [t10929] VALUES (56,'باب في الوقف على المشدد','122','47','0');
