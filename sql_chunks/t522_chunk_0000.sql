cb3b685a4a3719e18d090f5d14a84463956cf5be
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:22:02 +0700

    Upload SQL chunk: t522_chunk_0000.sql

---
+++ sql_chunks/t522_chunk_0000.sql
@@ -0,0 +1,59 @@
+-- Table: t522
+-- Chunk: 1
+-- Rows: 52
+
+CREATE TABLE "t522" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t522
+INSERT INTO [t522] VALUES (1,'كتاب الطهارة','1','0','0');
+INSERT INTO [t522] VALUES (2,'تأويل قوله عز وجل يا أيها الذين آمنوا إذا قمتم الآية','1','1','0');
+INSERT INTO [t522] VALUES (3,'باب حد الغسل','74','1','0');
+INSERT INTO [t522] VALUES (4,'باب فرض الوضوء','81','1','0');
+INSERT INTO [t522] VALUES (5,'كتاب المياه','164','0','0');
+INSERT INTO [t522] VALUES (6,'باب التوقيت','166','5','0');
+INSERT INTO [t522] VALUES (7,'كتاب الحيض والاستحاضة','171','0','0');
+INSERT INTO [t522] VALUES (8,'كتاب الغسل والتيمم','187','0','0');
+INSERT INTO [t522] VALUES (9,'باب الرخصة في ذلك','192','8','0');
+INSERT INTO [t522] VALUES (10,'كتاب الصلاة','207','0','0');
+INSERT INTO [t522] VALUES (11,'كتاب الأذان','289','0','0');
+INSERT INTO [t522] VALUES (12,'كتاب المساجد','318','0','0');
+INSERT INTO [t522] VALUES (13,'كتاب القبلة','348','0','0');
+INSERT INTO [t522] VALUES (14,'كتاب الإمامة','361','0','0');
+INSERT INTO [t522] VALUES (15,'كتاب الافتتاح','408','0','0');
+INSERT INTO [t522] VALUES (16,'كتاب الجمعة','605','0','0');
+INSERT INTO [t522] VALUES (17,'كتاب تقصير الصلاة في السفر','635','0','0');
+INSERT INTO [t522] VALUES (18,'كتاب الكسوف','643','0','0');
+INSERT INTO [t522] VALUES (19,'كتاب الاستسقاء','669','0','0');
+INSERT INTO [t522] VALUES (20,'كتاب صلاة الخوف','681','0','0');
+INSERT INTO [t522] VALUES (21,'كتاب العيدين','692','0','0');
+INSERT INTO [t522] VALUES (22,'كتاب قيام الليل','710','0','0');
+INSERT INTO [t522] VALUES (23,'كتاب الجنائز','767','0','0');
+INSERT INTO [t522] VALUES (24,'كتاب الصيام','882','0','0');
+INSERT INTO [t522] VALUES (25,'كتاب الزكاة','978','0','0');
+INSERT INTO [t522] VALUES (26,'كتاب مناسك الحج','1084','0','0');
+INSERT INTO [t522] VALUES (27,'كتاب الجهاد','1247','0','0');
+INSERT INTO [t522] VALUES (28,'كتاب النكاح','1297','0','0');
+INSERT INTO [t522] VALUES (29,'كتاب الطلاق','1379','0','0');
+INSERT INTO [t522] VALUES (30,'كتاب الخيل','1452','0','0');
+INSERT INTO [t522] VALUES (31,'كتاب الأحباس','1466','0','0');
+INSERT INTO [t522] VALUES (32,'كتاب النحل','1493','0','0');
+INSERT INTO [t522] VALUES (33,'كتاب الهبة','1496','0','0');
+INSERT INTO [t522] VALUES (34,'كتاب الرقيى','1501','0','0');
+INSERT INTO [t522] VALUES (35,'كتاب العمرى','1504','0','0');
+INSERT INTO [t522] VALUES (36,'كتاب الأيمان والنذور','1512','0','0');
+INSERT INTO [t522] VALUES (37,'كتاب المزارعة','1540','0','0');
+INSERT INTO [t522] VALUES (38,'كتاب عشرة النساء','1562','0','0');
+INSERT INTO [t522] VALUES (39,'كتاب تحريم الدم','1576','0','0');
+INSERT INTO [t522] VALUES (40,'كتاب قسم الفيء','1623','0','0');
+INSERT INTO [t522] VALUES (41,'كتاب البيعة','1632','0','0');
+INSERT INTO [t522] VALUES (42,'كتاب العقيقة','1657','0','0');
+INSERT INTO [t522] VALUES (43,'كتاب الفرع والعتيرة','1662','0','0');
+INSERT INTO [t522] VALUES (44,'كتاب الصيد والذبائح','1674','0','0');
+INSERT INTO [t522] VALUES (45,'كتاب الضحايا','1704','0','0');
+INSERT INTO [t522] VALUES (46,'كتاب البيوع','1730','0','0');
+INSERT INTO [t522] VALUES (47,'كتاب القسامة والقود والديات','1810','0','0');
+INSERT INTO [t522] VALUES (48,'كتاب قطع السارق','1868','0','0');
+INSERT INTO [t522] VALUES (49,'كتاب الزينة','1922','0','0');
+INSERT INTO [t522] VALUES (50,'كتاب آداب القضاة','2008','0','0');
+INSERT INTO [t522] VALUES (51,'كتاب الاستعاذة','2033','0','0');
+INSERT INTO [t522] VALUES (52,'كتاب الأشربة','2062','0','0');
