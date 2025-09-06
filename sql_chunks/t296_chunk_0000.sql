0be4ed813a20f5d93ff5009123ba030379253c4d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:56:48 +0700

    Upload SQL chunk: t296_chunk_0000.sql

---
+++ sql_chunks/t296_chunk_0000.sql
@@ -0,0 +1,35 @@
+-- Table: t296
+-- Chunk: 1
+-- Rows: 28
+
+CREATE TABLE "t296" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t296
+INSERT INTO [t296] VALUES (1,'مقدمة التحقيق','3','0','0');
+INSERT INTO [t296] VALUES (2,'فصل: في كفر الساب والمستهزئ بالله أو رسوله والرد على المرجئة وأتباعهم','6','1','0');
+INSERT INTO [t296] VALUES (3,'ترجمة المؤلف','13','1','0');
+INSERT INTO [t296] VALUES (4,'اسمه ومولده ونشأته','13','3','0');
+INSERT INTO [t296] VALUES (5,'صفاته وثناء العلماء عليه.','15','3','0');
+INSERT INTO [t296] VALUES (6,'مصنفاته.','16','3','0');
+INSERT INTO [t296] VALUES (7,'وفاته','17','3','0');
+INSERT INTO [t296] VALUES (8,'وصف النسخة الخطية','21','1','0');
+INSERT INTO [t296] VALUES (9,'مقدمة المصنف','26','0','0');
+INSERT INTO [t296] VALUES (10,'المسألة الأولى: أن من سبه - صلى الله عليه وسلم - من مسلم وكافر فإنه يجب قتله','28','0','0');
+INSERT INTO [t296] VALUES (11,'[الأدلة على وجوب قتل الساب]','32','10','0');
+INSERT INTO [t296] VALUES (12,'فصل','38','10','0');
+INSERT INTO [t296] VALUES (13,'فصل','49','10','0');
+INSERT INTO [t296] VALUES (14,'فصل','75','10','0');
+INSERT INTO [t296] VALUES (15,'المسألة الثانية: أنه يتعين قتله، ولا يجوز استرقاقه، ولا المن عليه، ولا فداؤه','83','0','0');
+INSERT INTO [t296] VALUES (16,'المسألة الثالثة: أنه يقتل ولا يستتاب سواء كان مسلما أو كافرا','87','0','0');
+INSERT INTO [t296] VALUES (17,'المسألة الرابعة: في بيان السب المذكور، والفرق بينه وبين مجرد الكفر','93','0','0');
+INSERT INTO [t296] VALUES (18,'فصل','95','17','0');
+INSERT INTO [t296] VALUES (19,'فصل','100','17','0');
+INSERT INTO [t296] VALUES (20,'فصل','101','17','0');
+INSERT INTO [t296] VALUES (21,'فصل فيمن سب الله تعالى','108','17','0');
+INSERT INTO [t296] VALUES (22,'فصل','109','17','0');
+INSERT INTO [t296] VALUES (23,'فصل','112','17','0');
+INSERT INTO [t296] VALUES (24,'فصل','113','17','0');
+INSERT INTO [t296] VALUES (25,'فصل','113','17','0');
+INSERT INTO [t296] VALUES (26,'فصل','114','17','0');
+INSERT INTO [t296] VALUES (27,'فصل','124','17','0');
+INSERT INTO [t296] VALUES (28,'فهرس المراجع','127','0','0');
