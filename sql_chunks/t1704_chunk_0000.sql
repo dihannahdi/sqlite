8a8cd0efd21399112eafddb0fa4dfb07a602adf8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:28:08 +0700

    Upload SQL chunk: t1704_chunk_0000.sql

---
+++ sql_chunks/t1704_chunk_0000.sql
@@ -0,0 +1,45 @@
+-- Table: t1704
+-- Chunk: 1
+-- Rows: 38
+
+CREATE TABLE "t1704" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1704
+INSERT INTO [t1704] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t1704] VALUES (2,'الفصل الأول','3','0','0');
+INSERT INTO [t1704] VALUES (3,'الفصل الثاني','10','0','0');
+INSERT INTO [t1704] VALUES (4,'في القطع التي لم تنفرد بنوار وإنما اشتملت على نورين أو أنوار','10','0','0');
+INSERT INTO [t1704] VALUES (5,'نسخة الكتاب','18','0','0');
+INSERT INTO [t1704] VALUES (6,'بسم الله الرحمن الرحيم','18','0','0');
+INSERT INTO [t1704] VALUES (7,'ونسخة الكتاب','18','0','0');
+INSERT INTO [t1704] VALUES (8,'من نواوير فصل الربيع الأزهر إلى الأقحوان والخيري الأصفر','18','0','0');
+INSERT INTO [t1704] VALUES (9,'بسم الله الرحمن الرحيم','18','0','0');
+INSERT INTO [t1704] VALUES (10,'بسم الله الرحمن الرحيم','20','0','0');
+INSERT INTO [t1704] VALUES (11,'بسم الله الرحمن الرحيم','20','0','0');
+INSERT INTO [t1704] VALUES (12,'شهادة البنفسج','20','0','0');
+INSERT INTO [t1704] VALUES (13,'شهادة النرجس','20','0','0');
+INSERT INTO [t1704] VALUES (14,'شهادة الخيري','20','0','0');
+INSERT INTO [t1704] VALUES (15,'شهادة الأقحوان','20','0','0');
+INSERT INTO [t1704] VALUES (16,'شهادة الخيري الأصفر','21','0','0');
+INSERT INTO [t1704] VALUES (17,'والرسالة','24','0','0');
+INSERT INTO [t1704] VALUES (18,'الفصل الثالث','26','0','0');
+INSERT INTO [t1704] VALUES (19,'في القطع المنفردة كل قطعة منها بنور على حدة','26','0','0');
+INSERT INTO [t1704] VALUES (20,'الآس','26','0','0');
+INSERT INTO [t1704] VALUES (21,'الياسمين','27','0','0');
+INSERT INTO [t1704] VALUES (22,'[البهار]','29','0','0');
+INSERT INTO [t1704] VALUES (23,'البنفسج','31','0','0');
+INSERT INTO [t1704] VALUES (24,'الخيري النمام','32','0','0');
+INSERT INTO [t1704] VALUES (25,'الخيري الأصفر','34','0','0');
+INSERT INTO [t1704] VALUES (26,'النرجس الأصفر','34','0','0');
+INSERT INTO [t1704] VALUES (27,'الورد','35','0','0');
+INSERT INTO [t1704] VALUES (28,'السوسن','38','0','0');
+INSERT INTO [t1704] VALUES (29,'الخرم','40','0','0');
+INSERT INTO [t1704] VALUES (30,'النيلوفر','41','0','0');
+INSERT INTO [t1704] VALUES (31,'نور اللوز','43','0','0');
+INSERT INTO [t1704] VALUES (32,'الأقحوان','43','0','0');
+INSERT INTO [t1704] VALUES (33,'الشقر','44','0','0');
+INSERT INTO [t1704] VALUES (34,'نور الباقلاء','44','0','0');
+INSERT INTO [t1704] VALUES (35,'نور الكتان','45','0','0');
+INSERT INTO [t1704] VALUES (36,'نور الغالبة','46','0','0');
+INSERT INTO [t1704] VALUES (37,'نور الرمان','46','0','0');
+INSERT INTO [t1704] VALUES (38,'الجلنار','46','0','0');
