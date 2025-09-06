9fde7f6979b9fedfb5880dcb024771900a19be46
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:13 +0700

    Upload SQL chunk: t4151_chunk_0000.sql

---
+++ sql_chunks/t4151_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t4151
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t4151" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4151
+INSERT INTO [t4151] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t4151] VALUES (2,'نبذة عن الكبائر , تعريف الكبيرة','3','0','0');
+INSERT INTO [t4151] VALUES (3,'المؤلفات في الكبائر','5','0','0');
+INSERT INTO [t4151] VALUES (4,'ترجمة المؤلف','7','0','0');
+INSERT INTO [t4151] VALUES (5,'شيوخة','8','0','0');
+INSERT INTO [t4151] VALUES (6,'تلاميذه','9','0','0');
+INSERT INTO [t4151] VALUES (7,'طلبه للعلم ورحلاته','10','0','0');
+INSERT INTO [t4151] VALUES (8,'توثيقه وثناء العلماء عليه','12','0','0');
+INSERT INTO [t4151] VALUES (9,'مؤلفاته','12','0','0');
+INSERT INTO [t4151] VALUES (10,'وفاته','14','0','0');
+INSERT INTO [t4151] VALUES (11,'التعريف بالكتاب','15','0','0');
+INSERT INTO [t4151] VALUES (12,'موضوعه وأهميته','16','0','0');
+INSERT INTO [t4151] VALUES (13,'منهج المؤلف فيه','17','0','0');
+INSERT INTO [t4151] VALUES (14,'الملاحظات علي الكتاب','19','0','0');
+INSERT INTO [t4151] VALUES (15,'وصف النسخ الخطية','21','0','0');
+INSERT INTO [t4151] VALUES (16,'تراجم رواة الكتاب','23','0','0');
+INSERT INTO [t4151] VALUES (17,'سماعات الكتاب','26','0','0');
+INSERT INTO [t4151] VALUES (18,'نماذج من المخطوط','29','0','0');
+INSERT INTO [t4151] VALUES (19,'النص المحقق','30','0','0');
+INSERT INTO [t4151] VALUES (20,'مصادر ومراجع','74','0','0');
