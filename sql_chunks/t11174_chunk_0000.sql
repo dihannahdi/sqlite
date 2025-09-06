80dfaabaf4a50308a41360c18e280088af4f38b2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:59 +0700

    Upload SQL chunk: t11174_chunk_0000.sql

---
+++ sql_chunks/t11174_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t11174
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t11174" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11174
+INSERT INTO [t11174] VALUES (1,'خصائص مسند الإمام أحمد','1','0','0');
+INSERT INTO [t11174] VALUES (2,'وهذا الكتاب أصل كبير ومرجع وثيق لأصحاب الحديث','5','0','0');
