e4b25b365454c23a0f7f8997cacfb90c63d2f29a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:58:41 +0700

    Upload SQL chunk: t96533_chunk_0000.sql

---
+++ sql_chunks/t96533_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t96533
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t96533" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96533
+INSERT INTO [t96533] VALUES (1,'المقدمة','2','0','0');
+INSERT INTO [t96533] VALUES (2,'أولا: ولادتها:','4','0','0');
+INSERT INTO [t96533] VALUES (3,'ثانيا: نشأتها:','4','0','0');
+INSERT INTO [t96533] VALUES (4,'ثالثا: زواجها:','5','0','0');
+INSERT INTO [t96533] VALUES (5,'رابعا:','6','0','0');
+INSERT INTO [t96533] VALUES (6,'خامسا:','11','0','0');
+INSERT INTO [t96533] VALUES (7,'سادسا:','11','0','0');
+INSERT INTO [t96533] VALUES (8,'سابعا:','14','0','0');
+INSERT INTO [t96533] VALUES (9,'ثامنا:','15','0','0');
+INSERT INTO [t96533] VALUES (10,'تاسعا: صلاتها:','16','0','0');
+INSERT INTO [t96533] VALUES (11,'عاشرا: نوافل الصلاة:','16','0','0');
+INSERT INTO [t96533] VALUES (12,'الحادي عشر: صيامها:','17','0','0');
+INSERT INTO [t96533] VALUES (13,'الثاني عشر: ذكرها لله تعالى:','17','0','0');
+INSERT INTO [t96533] VALUES (14,'الثالث عشر: حجها:','18','0','0');
+INSERT INTO [t96533] VALUES (15,'الرابع عشر: عمرها:','19','0','0');
+INSERT INTO [t96533] VALUES (16,'الخامس عشر: صدقاتها وكرمها:','19','0','0');
+INSERT INTO [t96533] VALUES (17,'السادس عشر: أمرها بالمعروف ونهيها عن المنكر:','22','0','0');
+INSERT INTO [t96533] VALUES (18,'السابع عشر: برها بأمها وأبيها','26','0','0');
+INSERT INTO [t96533] VALUES (19,'الثامن عشر: ذريتها:','26','0','0');
+INSERT INTO [t96533] VALUES (20,'التاسع عشر: مرضها العظيم:','27','0','0');
+INSERT INTO [t96533] VALUES (21,'العشرون:','29','0','0');
+INSERT INTO [t96533] VALUES (22,'الحادي والعشرون:','31','0','0');
