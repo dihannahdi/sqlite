f656fa44da296dd28c0243b1145104f46b3ed3e5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:07 +0700

    Upload SQL chunk: t36081_chunk_0000.sql

---
+++ sql_chunks/t36081_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t36081
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t36081" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36081
+INSERT INTO [t36081] VALUES (1,'[مقدمة المؤلف]','2','0','0');
+INSERT INTO [t36081] VALUES (2,'[النكتة ١]','3','0','0');
+INSERT INTO [t36081] VALUES (3,'[النكتة ٢]','13','0','0');
+INSERT INTO [t36081] VALUES (4,'[النكتة ٣]','15','0','0');
+INSERT INTO [t36081] VALUES (5,'[النكتة ٤]','18','0','0');
+INSERT INTO [t36081] VALUES (6,'[النكتة ٥]','20','0','0');
+INSERT INTO [t36081] VALUES (7,'[النكتة ٦]','24','0','0');
+INSERT INTO [t36081] VALUES (8,'[النكتة ٧]','26','0','0');
+INSERT INTO [t36081] VALUES (9,'[النكتة ٨]','28','0','0');
+INSERT INTO [t36081] VALUES (10,'[النكتة ٩]','29','0','0');
+INSERT INTO [t36081] VALUES (11,'[النكتة ١٠]','32','0','0');
+INSERT INTO [t36081] VALUES (12,'[النكتة ١١]','34','0','0');
+INSERT INTO [t36081] VALUES (13,'[النكتة ١٢]','37','0','0');
+INSERT INTO [t36081] VALUES (14,'[النكتة ١٣]','38','0','0');
+INSERT INTO [t36081] VALUES (15,'[النكتة ١٤]','41','0','0');
+INSERT INTO [t36081] VALUES (16,'[النكتة ١٥]','42','0','0');
+INSERT INTO [t36081] VALUES (17,'[النكتة ١٦]','44','0','0');
+INSERT INTO [t36081] VALUES (18,'[النكتة ١٧]','45','0','0');
+INSERT INTO [t36081] VALUES (19,'[النكتة ١٨]','47','0','0');
+INSERT INTO [t36081] VALUES (20,'[النكتة ١٩]','48','0','0');
+INSERT INTO [t36081] VALUES (21,'[النكتة ٢٠]','49','0','0');
+INSERT INTO [t36081] VALUES (22,'[النكتة ٢١]','53','0','0');
+INSERT INTO [t36081] VALUES (23,'[النكتة ٢٢]','54','0','0');
+INSERT INTO [t36081] VALUES (24,'[النكتة ٢٣]','57','0','0');
+INSERT INTO [t36081] VALUES (25,'[النكتة ٢٤]','61','0','0');
+INSERT INTO [t36081] VALUES (26,'[النكتة ٢٥]','63','0','0');
+INSERT INTO [t36081] VALUES (27,'[النكتة ٢٦]','65','0','0');
+INSERT INTO [t36081] VALUES (28,'[النكتة ٢٧]','66','0','0');
+INSERT INTO [t36081] VALUES (29,'[النكتة ٢٨]','67','0','0');
+INSERT INTO [t36081] VALUES (30,'[النكتة ٢٩]','68','0','0');
+INSERT INTO [t36081] VALUES (31,'[النكتة ٣٠]','69','0','0');
+INSERT INTO [t36081] VALUES (32,'[النكتة ٣١]','70','0','0');
+INSERT INTO [t36081] VALUES (33,'[النكتة ٣٢]','71','0','0');
+INSERT INTO [t36081] VALUES (34,'[النكتة ٣٣]','73','0','0');
+INSERT INTO [t36081] VALUES (35,'[النكتة ٣٤]','75','0','0');
+INSERT INTO [t36081] VALUES (36,'[النكتة ٣٥]','76','0','0');
