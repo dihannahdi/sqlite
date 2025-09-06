9ce28d218672a762fce5a16a8349e61d9d1c83c8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:53 +0700

    Upload SQL chunk: t26592_chunk_0000.sql

---
+++ sql_chunks/t26592_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t26592
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t26592" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26592
+INSERT INTO [t26592] VALUES (1,'مسند أبي هريرة لأبي إسحاق العسكري','1','0','0');
