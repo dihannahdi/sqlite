3ebea94548c0865cf7858ff68501360b1f42ff03
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:17:39 +0700

    Upload SQL chunk: t14278_chunk_0000.sql

---
+++ sql_chunks/t14278_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t14278
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t14278" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t14278
+INSERT INTO [t14278] VALUES (1,'قافية الألف','1','0','0');
+INSERT INTO [t14278] VALUES (2,'قافية الباء','4','0','0');
+INSERT INTO [t14278] VALUES (3,'قافية الجيم','10','0','0');
+INSERT INTO [t14278] VALUES (4,'قافية الحاء','11','0','0');
+INSERT INTO [t14278] VALUES (5,'قافية الدال','12','0','0');
+INSERT INTO [t14278] VALUES (6,'قافية الذال','17','0','0');
+INSERT INTO [t14278] VALUES (7,'قافية الراء','18','0','0');
+INSERT INTO [t14278] VALUES (8,'قافية السين','30','0','0');
+INSERT INTO [t14278] VALUES (9,'قافية العين','38','0','0');
+INSERT INTO [t14278] VALUES (10,'قافية الفاء','43','0','0');
+INSERT INTO [t14278] VALUES (11,'قافية اللام','48','0','0');
+INSERT INTO [t14278] VALUES (12,'قافية الميم','54','0','0');
+INSERT INTO [t14278] VALUES (13,'قافية النون','55','0','0');
