7d4008be800b334b453310a989201e0b8963dad7
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:22:37 +0700

    Upload SQL chunk: t148540_chunk_0000.sql

---
+++ sql_chunks/t148540_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t148540
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t148540" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t148540
+INSERT INTO [t148540] VALUES (1,'شعر أبي حية النميري','1','0','0');
+INSERT INTO [t148540] VALUES (2,'ما ينسب إلى أبي حية النميري وإلى غيره من الشعراء','153','0','0');
