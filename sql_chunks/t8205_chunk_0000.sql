5ce865fe002f4481a0d6e026e6feca165be9fd1b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:44:17 +0700

    Upload SQL chunk: t8205_chunk_0000.sql

---
+++ sql_chunks/t8205_chunk_0000.sql
@@ -0,0 +1,34 @@
+-- Table: t8205
+-- Chunk: 1
+-- Rows: 27
+
+CREATE TABLE "t8205" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8205
+INSERT INTO [t8205] VALUES (1,'باب الإيمان','1','0','0');
+INSERT INTO [t8205] VALUES (2,'باب الترغيب في قضاء حوائج المسلمين','6','0','0');
+INSERT INTO [t8205] VALUES (3,'باب التشديد في أذى المسلم','8','0','0');
+INSERT INTO [t8205] VALUES (4,'باب التشديد في مخالفة السنة','11','0','0');
+INSERT INTO [t8205] VALUES (5,'باب التشديد في تضييع الأمانة','14','0','0');
+INSERT INTO [t8205] VALUES (6,'باب في التشديد في الكذب والخيانة','16','0','0');
+INSERT INTO [t8205] VALUES (7,'باب الأعمال بالنية','18','0','0');
+INSERT INTO [t8205] VALUES (8,'باب الوضوء','20','0','0');
+INSERT INTO [t8205] VALUES (9,'باب المسح على الخفين','26','0','0');
+INSERT INTO [t8205] VALUES (10,'باب الغسل من الجنابة','28','0','0');
+INSERT INTO [t8205] VALUES (11,'باب الحيض','30','0','0');
+INSERT INTO [t8205] VALUES (12,'باب الصلاة','32','0','0');
+INSERT INTO [t8205] VALUES (13,'باب الجمعة','36','0','0');
+INSERT INTO [t8205] VALUES (14,'باب الجنائز','38','0','0');
+INSERT INTO [t8205] VALUES (15,'باب فضل تشييع الجنائز','40','0','0');
+INSERT INTO [t8205] VALUES (16,'باب الزكاة','42','0','0');
+INSERT INTO [t8205] VALUES (17,'باب الحج','45','0','0');
+INSERT INTO [t8205] VALUES (18,'باب بر الوالدين','48','0','0');
+INSERT INTO [t8205] VALUES (19,'باب النصيحة','51','0','0');
+INSERT INTO [t8205] VALUES (20,'باب الصوم','53','0','0');
+INSERT INTO [t8205] VALUES (21,'باب الجهاد','55','0','0');
+INSERT INTO [t8205] VALUES (22,'باب صلة الرحم','57','0','0');
+INSERT INTO [t8205] VALUES (23,'باب الوصية','59','0','0');
+INSERT INTO [t8205] VALUES (24,'باب البيوع','61','0','0');
+INSERT INTO [t8205] VALUES (25,'باب الطلاق','63','0','0');
+INSERT INTO [t8205] VALUES (26,'باب النفقة على العيال','65','0','0');
+INSERT INTO [t8205] VALUES (27,'باب','67','0','0');
