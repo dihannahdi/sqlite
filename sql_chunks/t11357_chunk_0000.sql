54b4d756dae4bacd30426919aaf42482eaf1ce5a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:56:25 +0700

    Upload SQL chunk: t11357_chunk_0000.sql

---
+++ sql_chunks/t11357_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t11357
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t11357" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11357
+INSERT INTO [t11357] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11357] VALUES (2,'تعريف السحر','4','0','0');
+INSERT INTO [t11357] VALUES (3,'أقسام السحر','13','0','0');
+INSERT INTO [t11357] VALUES (4,'حكم حل السحر بسحر مثله','20','0','0');
+INSERT INTO [t11357] VALUES (5,'كيفية إبطال السحر','23','0','0');
+INSERT INTO [t11357] VALUES (6,'ضرر السحر على الفرد والمجتمع','30','0','0');
+INSERT INTO [t11357] VALUES (7,'تسلط السحرة في هذا الزمان الأسباب والعلاج','31','0','0');
+INSERT INTO [t11357] VALUES (8,'جهود المملكة العربية السعودية في محاربة السحر','41','0','0');
+INSERT INTO [t11357] VALUES (9,'التحصينات الشرعية من السحر','44','0','0');
