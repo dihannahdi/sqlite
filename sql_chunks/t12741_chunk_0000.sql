7200db5f29a0039102cb2d5a0a8ba3ed453a476e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:06:22 +0700

    Upload SQL chunk: t12741_chunk_0000.sql

---
+++ sql_chunks/t12741_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t12741
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t12741" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12741
+INSERT INTO [t12741] VALUES (1,'الغلاف','1','0','0');
+INSERT INTO [t12741] VALUES (2,'جزء عوالي الإمام أبي حنيفة رضي الله عنه','2','0','0');
