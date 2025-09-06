4cae72808eb89e3929d2858a330e066f209fc62b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:58:40 +0700

    Upload SQL chunk: t11577_chunk_0000.sql

---
+++ sql_chunks/t11577_chunk_0000.sql
@@ -0,0 +1,17 @@
+-- Table: t11577
+-- Chunk: 1
+-- Rows: 10
+
+CREATE TABLE "t11577" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11577
+INSERT INTO [t11577] VALUES (1,'القسم الدراسي','1','0','0');
+INSERT INTO [t11577] VALUES (2,'دراسة حياة المصنف','1','1','0');
+INSERT INTO [t11577] VALUES (3,'دراسة الكتاب','10','1','0');
+INSERT INTO [t11577] VALUES (4,'القسم التحقيق','18','0','0');
+INSERT INTO [t11577] VALUES (5,'كتاب الطهارة','18','4','0');
+INSERT INTO [t11577] VALUES (6,'كتاب الصلاة','81','4','0');
+INSERT INTO [t11577] VALUES (7,'كتاب الزكاة','175','4','0');
+INSERT INTO [t11577] VALUES (8,'كتاب الصيام','213','4','0');
+INSERT INTO [t11577] VALUES (9,'كتاب الحج والعمرة','242','4','0');
+INSERT INTO [t11577] VALUES (10,'مصادر ومراجع','289','0','0');
