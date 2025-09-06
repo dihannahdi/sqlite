125869088153327825f4364e0bcf1087c624f769
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:35 +0700

    Upload SQL chunk: t95864_chunk_0000.sql

---
+++ sql_chunks/t95864_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t95864
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t95864" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95864
+INSERT INTO [t95864] VALUES (1,'الطهارة','4','0','0');
+INSERT INTO [t95864] VALUES (2,'الوضوء','4','0','0');
+INSERT INTO [t95864] VALUES (3,'نواقض الوضوء','5','0','0');
+INSERT INTO [t95864] VALUES (4,'ما يمنعه الحدث','6','0','0');
+INSERT INTO [t95864] VALUES (5,'الغسل','6','0','0');
+INSERT INTO [t95864] VALUES (6,'فرائض الغسل','7','0','0');
+INSERT INTO [t95864] VALUES (7,'ما يمنعه الأكبر','7','0','0');
+INSERT INTO [t95864] VALUES (8,'التيمم','8','0','0');
+INSERT INTO [t95864] VALUES (9,'الحيض','9','0','0');
+INSERT INTO [t95864] VALUES (10,'النفاس','9','0','0');
+INSERT INTO [t95864] VALUES (11,'أوقات الصلاة','10','0','0');
+INSERT INTO [t95864] VALUES (12,'شروط الصلاة','10','0','0');
+INSERT INTO [t95864] VALUES (13,'فرائض الصلاة','11','0','0');
+INSERT INTO [t95864] VALUES (14,'مكانة الصلاة','13','0','0');
+INSERT INTO [t95864] VALUES (15,'الأحوال التي تؤدى عليه الصلاة','13','0','0');
+INSERT INTO [t95864] VALUES (16,'قضاء الفوائت','14','0','0');
+INSERT INTO [t95864] VALUES (17,'السهو','14','0','0');
