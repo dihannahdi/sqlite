661e04cb678959dffb9f46f46f13ddae97849655
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:38 +0700

    Upload SQL chunk: t753_chunk_0000.sql

---
+++ sql_chunks/t753_chunk_0000.sql
@@ -0,0 +1,24 @@
+-- Table: t753
+-- Chunk: 1
+-- Rows: 17
+
+CREATE TABLE "t753" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t753
+INSERT INTO [t753] VALUES (1,'الجزء الأول','1','0','0');
+INSERT INTO [t753] VALUES (2,'العراقيات الأولى','1','1','0');
+INSERT INTO [t753] VALUES (3,'الشاميات','12','1','0');
+INSERT INTO [t753] VALUES (4,'الجزء الثاني','89','0','0');
+INSERT INTO [t753] VALUES (5,'قصائد بدر بن عمار','115','4','0');
+INSERT INTO [t753] VALUES (6,'قصائد ابن طغج','179','4','0');
+INSERT INTO [t753] VALUES (7,'قصائد أبي العشائر الحمداني','197','4','0');
+INSERT INTO [t753] VALUES (8,'الجزء الثالث','211','0','0');
+INSERT INTO [t753] VALUES (9,'السيفيات','211','8','0');
+INSERT INTO [t753] VALUES (10,'الجزء الرابع','370','0','0');
+INSERT INTO [t753] VALUES (11,'الكافوريات وهي المصريات','370','10','0');
+INSERT INTO [t753] VALUES (12,'خروج المتنبي من مصر إلى الكوفة','410','10','0');
+INSERT INTO [t753] VALUES (13,'خبره مع فاتك','416','10','0');
+INSERT INTO [t753] VALUES (14,'العراقيات الأخيرة','422','10','0');
+INSERT INTO [t753] VALUES (15,'العميديات','431','10','0');
+INSERT INTO [t753] VALUES (16,'العضديات','443','10','0');
+INSERT INTO [t753] VALUES (17,'زيادات من شعر المتنبي','470','10','0');
