d71b69b4ba46a7b607753dfcee24058ca89be3e0
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:15:56 +0700

    Upload SQL chunk: t13900_chunk_0000.sql

---
+++ sql_chunks/t13900_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t13900
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t13900" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t13900
+INSERT INTO [t13900] VALUES (1,'القول في بيان إعجاز القرآن','3','0','0');
