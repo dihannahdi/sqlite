1dabf81ec8605af33f616e4e40e7adf7ebeb3547
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:59:37 +0700

    Upload SQL chunk: t11781_chunk_0000.sql

---
+++ sql_chunks/t11781_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t11781
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t11781" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11781
+INSERT INTO [t11781] VALUES (1,'الحديث المسلسل بيوم العيد','4','0','0');
+INSERT INTO [t11781] VALUES (2,'الحديث المسلسل بقراءة سورة الصف','9','0','0');
+INSERT INTO [t11781] VALUES (3,'الحديث المسلسل بالفقهاء','10','0','0');
+INSERT INTO [t11781] VALUES (4,'الحديث المسلسل بالمشابكة الباغوزارية','10','0','0');
+INSERT INTO [t11781] VALUES (5,'الحديث المسلسل بالمصافحة النبوية','13','0','0');
+INSERT INTO [t11781] VALUES (6,'الحديث المسلسل بالمصافحة المعمرية','14','0','0');
+INSERT INTO [t11781] VALUES (7,'الحديث المسلسل بالمصافحة الخضرية','15','0','0');
