ebd0bfd26cd3d4789f0a01cd728f334e843a3973
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:58:35 +0700

    Upload SQL chunk: t29904_chunk_0000.sql

---
+++ sql_chunks/t29904_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t29904
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t29904" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t29904
+INSERT INTO [t29904] VALUES (1,'أكثروا ذكر هادم اللذات الموت','2','0','0');
+INSERT INTO [t29904] VALUES (2,'اللهم إني أعوذ بك من زوال نعمتك، وتحول عافيتك، وفجاءة نقمتك، وجميع','3','0','0');
+INSERT INTO [t29904] VALUES (3,'الخلق كلهم عيال الله عز وجل، فأحبهم إلى الله عز وجل أنفعهم لعياله.','4','0','0');
+INSERT INTO [t29904] VALUES (4,'يعجبه الحلوى والعسل.','5','0','0');
+INSERT INTO [t29904] VALUES (5,'اللهم أصلح ذات بيننا، واهدنا سبل الإسلام، وأخرجنا من الظلمات إلى النور، وعافنا في','6','0','0');
