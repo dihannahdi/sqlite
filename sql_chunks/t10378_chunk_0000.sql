91f67cbc308a66efbb5e4d9eb2eeb4f18e7535b9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:31 +0700

    Upload SQL chunk: t10378_chunk_0000.sql

---
+++ sql_chunks/t10378_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t10378
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t10378" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10378
+INSERT INTO [t10378] VALUES (1,'الرأي العام في المجنمع الإسلامي','1','0','0');
+INSERT INTO [t10378] VALUES (2,'تعريفه','1','1','0');
+INSERT INTO [t10378] VALUES (3,'تكوين الرأي العام','3','0','0');
+INSERT INTO [t10378] VALUES (4,'المؤثرات في تكوين الرأي العام في المجتمع الإسلامي','5','0','0');
+INSERT INTO [t10378] VALUES (5,'المسجد','5','4','0');
+INSERT INTO [t10378] VALUES (6,'العلماء','6','4','0');
+INSERT INTO [t10378] VALUES (7,'فريضة الأمر بالمعروف والنهي عن المنكر','8','4','0');
+INSERT INTO [t10378] VALUES (8,'مؤثرات أخرى','9','4','0');
+INSERT INTO [t10378] VALUES (9,'وظيفة الرأي العام','13','0','0');
+INSERT INTO [t10378] VALUES (10,'رعاية تطبيق الأحكام الشرعية','13','9','0');
+INSERT INTO [t10378] VALUES (11,'حراسة المبادئ الإسلامية وصيانة قيمتها في المجتمع','15','9','0');
+INSERT INTO [t10378] VALUES (12,'حسن استنباط الأحكام الشرعية من أدلتها','20','9','0');
+INSERT INTO [t10378] VALUES (13,'رفع الروح المعنوية للأمة وتقوية أواصرها وتعاونها','23','9','0');
+INSERT INTO [t10378] VALUES (14,'استثارة مشاعر الأمة للقيام بواجب معين','26','9','0');
