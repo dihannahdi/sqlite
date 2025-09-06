a3168f829a744f225e19ebd3cf440e79fca3688e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:57 +0700

    Upload SQL chunk: t10444_chunk_0000.sql

---
+++ sql_chunks/t10444_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t10444
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t10444" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10444
+INSERT INTO [t10444] VALUES (1,'الأحاديث السباعيات الألف للشحامي','2','0','0');
+INSERT INTO [t10444] VALUES (2,'الجزء الأول','2','0','0');
+INSERT INTO [t10444] VALUES (3,'الجزء الثاني','21','0','0');
+INSERT INTO [t10444] VALUES (4,'الجزء الخامس','41','0','0');
+INSERT INTO [t10444] VALUES (5,'الجزء السادس','61','0','0');
+INSERT INTO [t10444] VALUES (6,'الجزء السابع','81','0','0');
+INSERT INTO [t10444] VALUES (7,'الجزء الثامن','99','0','0');
