c24dcf5183624d3db4d2c661e34380550eecea28
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:06:38 +0700

    Upload SQL chunk: t3569_chunk_0000.sql

---
+++ sql_chunks/t3569_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t3569
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t3569" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t3569
+INSERT INTO [t3569] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t3569] VALUES (2,'المبحث الأول: المؤلفات الأصولية الشافعية','10','0','0');
+INSERT INTO [t3569] VALUES (3,'المبحث الثاني: المتون الأصولية الحنفية المشروحة من قبل علماء الشافعية','76','0','0');
+INSERT INTO [t3569] VALUES (4,'الخاتمة','79','0','0');
+INSERT INTO [t3569] VALUES (5,'مصادر ومراجع','88','0','0');
