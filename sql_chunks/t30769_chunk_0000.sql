0f1875adc18082107220f2539103fecca0376294
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:50 +0700

    Upload SQL chunk: t30769_chunk_0000.sql

---
+++ sql_chunks/t30769_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t30769
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t30769" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30769
+INSERT INTO [t30769] VALUES (1,'مجلس لأبي عبد الله بن منده في شعبان سنة أربع وثمانين','1','0','0');
+INSERT INTO [t30769] VALUES (2,'مجلس آخر في شعبان','25','0','0');
+INSERT INTO [t30769] VALUES (3,'مجلس آخر','50','0','0');
+INSERT INTO [t30769] VALUES (4,'مجلس آخر من الجزء الثالث','78','0','0');
+INSERT INTO [t30769] VALUES (5,'مجلس آخر','107','0','0');
+INSERT INTO [t30769] VALUES (6,'مجلس آخر','137','0','0');
+INSERT INTO [t30769] VALUES (7,'مجلس آخر في ذي القعدة سنة أربعمائة','168','0','0');
+INSERT INTO [t30769] VALUES (8,'مجلس آخر','192','0','0');
+INSERT INTO [t30769] VALUES (9,'مجلس آخر في ذي الحجة','226','0','0');
+INSERT INTO [t30769] VALUES (10,'مجلس آخر من هذا الجزء','315','0','0');
+INSERT INTO [t30769] VALUES (11,'مجلس آخر في المحرم سنة خمس وثمانين وثلاثمائة','345','0','0');
+INSERT INTO [t30769] VALUES (12,'مجلس آخر','375','0','0');
+INSERT INTO [t30769] VALUES (13,'مجلس آخر','403','0','0');
