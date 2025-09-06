7e78000bd817ec404c52de5845f228b01c1ac90a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:36:23 +0700

    Upload SQL chunk: t710_chunk_0000.sql

---
+++ sql_chunks/t710_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t710
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t710" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t710
+INSERT INTO [t710] VALUES (1,'حول فقه الواقع','1','0','0');
