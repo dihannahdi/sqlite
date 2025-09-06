dd79c5d6a180609ffc90570eb1113110f068072a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:55:12 +0700

    Upload SQL chunk: t95684_chunk_0000.sql

---
+++ sql_chunks/t95684_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t95684
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t95684" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t95684
+INSERT INTO [t95684] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t95684] VALUES (2,'الوقت','2','0','0');
+INSERT INTO [t95684] VALUES (3,'بيان أهمية الوقت','2','0','0');
+INSERT INTO [t95684] VALUES (4,'موقفين للإنسان','5','0','0');
+INSERT INTO [t95684] VALUES (5,'الساعات ثلاث','7','0','0');
+INSERT INTO [t95684] VALUES (6,'الناس رجلان','11','0','0');
+INSERT INTO [t95684] VALUES (7,'ما ينبغي للمؤمن','14','0','0');
+INSERT INTO [t95684] VALUES (8,'راحلة الأيام','19','0','0');
+INSERT INTO [t95684] VALUES (9,'زمن الشباب','21','0','0');
+INSERT INTO [t95684] VALUES (10,'الندم على ما فات','27','0','0');
+INSERT INTO [t95684] VALUES (11,'جماع الخير','31','0','0');
+INSERT INTO [t95684] VALUES (12,'الوقت عند ابن الجوزي','32','0','0');
+INSERT INTO [t95684] VALUES (13,'هلم إلى الدخول','34','0','0');
+INSERT INTO [t95684] VALUES (14,'الواجب نحو الوقت','36','0','0');
+INSERT INTO [t95684] VALUES (15,'تعويد الأبناء','37','0','0');
+INSERT INTO [t95684] VALUES (16,'وقت الإنسان صوت ينادي','44','0','0');
+INSERT INTO [t95684] VALUES (17,'الصحة والشباب','45','0','0');
+INSERT INTO [t95684] VALUES (18,'خصائص الوقت','51','0','0');
