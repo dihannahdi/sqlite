1b3460542aba35fde7b8cb48f8c627812eb27d5f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:25:54 +0700

    Upload SQL chunk: t5843_chunk_0000.sql

---
+++ sql_chunks/t5843_chunk_0000.sql
@@ -0,0 +1,33 @@
+-- Table: t5843
+-- Chunk: 1
+-- Rows: 26
+
+CREATE TABLE "t5843" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5843
+INSERT INTO [t5843] VALUES (1,'الألف','14','0','0');
+INSERT INTO [t5843] VALUES (2,'الباء','24','0','0');
+INSERT INTO [t5843] VALUES (3,'التاء','26','0','0');
+INSERT INTO [t5843] VALUES (4,'الثاء','27','0','0');
+INSERT INTO [t5843] VALUES (5,'الجيم','28','0','0');
+INSERT INTO [t5843] VALUES (6,'الحاء','30','0','0');
+INSERT INTO [t5843] VALUES (7,'الخاء','34','0','0');
+INSERT INTO [t5843] VALUES (8,'الدال','36','0','0');
+INSERT INTO [t5843] VALUES (9,'الذال','38','0','0');
+INSERT INTO [t5843] VALUES (10,'الراء','39','0','0');
+INSERT INTO [t5843] VALUES (11,'الزاي','41','0','0');
+INSERT INTO [t5843] VALUES (12,'السين','44','0','0');
+INSERT INTO [t5843] VALUES (13,'الشين','50','0','0');
+INSERT INTO [t5843] VALUES (14,'الصاد','51','0','0');
+INSERT INTO [t5843] VALUES (15,'الضاد','53','0','0');
+INSERT INTO [t5843] VALUES (16,'الطاء','54','0','0');
+INSERT INTO [t5843] VALUES (17,'العين','55','0','0');
+INSERT INTO [t5843] VALUES (18,'الغين','85','0','0');
+INSERT INTO [t5843] VALUES (19,'الفاء','86','0','0');
+INSERT INTO [t5843] VALUES (20,'القاف','89','0','0');
+INSERT INTO [t5843] VALUES (21,'الكاف','91','0','0');
+INSERT INTO [t5843] VALUES (22,'الميم','93','0','0');
+INSERT INTO [t5843] VALUES (23,'النون','109','0','0');
+INSERT INTO [t5843] VALUES (24,'الواو','114','0','0');
+INSERT INTO [t5843] VALUES (25,'الهاء','117','0','0');
+INSERT INTO [t5843] VALUES (26,'الياء','118','0','0');
