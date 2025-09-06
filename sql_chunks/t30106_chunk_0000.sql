ea6614e22d3b6b90b56ae9e56cdb89b5f6763a2d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:42 +0700

    Upload SQL chunk: t30106_chunk_0000.sql

---
+++ sql_chunks/t30106_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t30106
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t30106" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30106
+INSERT INTO [t30106] VALUES (1,'الجزء الحادي والثلاثون من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30106] VALUES (2,'حديث واحد','3','0','0');
+INSERT INTO [t30106] VALUES (3,'من حديث أبي محمد الجوهري','5','0','0');
+INSERT INTO [t30106] VALUES (4,'حديث واحد','8','0','0');
+INSERT INTO [t30106] VALUES (5,'من حديث القاضي أبي الحسين بن الغريق','10','0','0');
+INSERT INTO [t30106] VALUES (6,'من حديث أبي حفص بن شاهين','19','0','0');
+INSERT INTO [t30106] VALUES (7,'من حديث أحمد بن حنبل','22','0','0');
