f313399acb9bfbe3b839a10d3b4261e20850d031
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:00:47 +0700

    Upload SQL chunk: t9737_chunk_0000.sql

---
+++ sql_chunks/t9737_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t9737
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t9737" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9737
+INSERT INTO [t9737] VALUES (1,'بسم الله الرحمن الرحيم','1','0','0');
+INSERT INTO [t9737] VALUES (2,'غرض الكتاب','1','0','0');
+INSERT INTO [t9737] VALUES (3,'مبدأ المفارقة','2','0','0');
+INSERT INTO [t9737] VALUES (4,'تعريف النفس','3','0','0');
+INSERT INTO [t9737] VALUES (5,'القول في القوة الغاذية','4','0','0');
+INSERT INTO [t9737] VALUES (6,'القول في القوة الحساسة','5','0','0');
+INSERT INTO [t9737] VALUES (7,'قوى النفس الحساسة','7','0','0');
+INSERT INTO [t9737] VALUES (8,'الحواس الخمس','8','0','0');
+INSERT INTO [t9737] VALUES (9,'القول في قوة البصر','8','0','0');
+INSERT INTO [t9737] VALUES (10,'القول في السمع','9','0','0');
+INSERT INTO [t9737] VALUES (11,'القول في الشم','10','0','0');
+INSERT INTO [t9737] VALUES (12,'القول في الذوق','11','0','0');
+INSERT INTO [t9737] VALUES (13,'القول في اللمس','12','0','0');
+INSERT INTO [t9737] VALUES (14,'القول في الحس المشترك','15','0','0');
+INSERT INTO [t9737] VALUES (15,'القول في التخيل','17','0','0');
+INSERT INTO [t9737] VALUES (16,'القول في القوة الناطقة','19','0','0');
+INSERT INTO [t9737] VALUES (17,'الصور الهيولانية','21','0','0');
+INSERT INTO [t9737] VALUES (18,'الصور المعقولة','22','0','0');
+INSERT INTO [t9737] VALUES (19,'الصور بالقوة والصور بالفعل','27','0','0');
+INSERT INTO [t9737] VALUES (20,'القول في القوة النزوعية','28','0','0');
