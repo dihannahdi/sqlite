758f80e8549c265e0e347290588ba8abfe6a1ec3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:08:26 +0700

    Upload SQL chunk: t12913_chunk_0000.sql

---
+++ sql_chunks/t12913_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t12913
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t12913" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t12913
+INSERT INTO [t12913] VALUES (1,'تصدير','1','0','0');
+INSERT INTO [t12913] VALUES (2,'مقدمة علمية هامة','5','0','0');
+INSERT INTO [t12913] VALUES (3,'تنبيه','16','0','0');
+INSERT INTO [t12913] VALUES (4,'تتمة نافعة في بيان السنة والبدعة','19','0','0');
+INSERT INTO [t12913] VALUES (5,'فرع مهم في بيان الفرق بين البدعة والمصالح المرسلة','28','0','0');
+INSERT INTO [t12913] VALUES (6,'الموالد عامة وحكم الإسلام عليها','38','0','0');
+INSERT INTO [t12913] VALUES (7,'المولد النبوي الشريف وحكم الإسلام فيه','48','0','0');
+INSERT INTO [t12913] VALUES (8,'علل غير كافية في إقامة المولد','51','0','0');
+INSERT INTO [t12913] VALUES (9,'شبه ضعيفة احتج بها المرخصون في الاحتفال بالمولد','56','0','0');
+INSERT INTO [t12913] VALUES (10,'البديل الخير','66','0','0');
+INSERT INTO [t12913] VALUES (11,'غلو في المولد شائن','69','0','0');
+INSERT INTO [t12913] VALUES (12,'إجحاف غير لائق','74','0','0');
+INSERT INTO [t12913] VALUES (13,'الخاتمة','79','0','0');
