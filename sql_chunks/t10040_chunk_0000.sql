108f81dec87f393ea9c722f4d8bd5808e17ad4b3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:46:26 +0700

    Upload SQL chunk: t10040_chunk_0000.sql

---
+++ sql_chunks/t10040_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t10040
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t10040" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10040
+INSERT INTO [t10040] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10040] VALUES (2,'المبحث الأول: تعريف الإعتكاف','4','0','0');
+INSERT INTO [t10040] VALUES (3,'المبحث الثاني: حكم الإعتكاف','5','0','0');
+INSERT INTO [t10040] VALUES (4,'المبحث الثالث: تعريف المباشرة','6','0','0');
+INSERT INTO [t10040] VALUES (5,'المبحث الرابع: أحكام المباشرة بالجماع','8','0','0');
+INSERT INTO [t10040] VALUES (6,'المبحث الخامس: أحكام المباشرة فيما دون الجماع','9','0','0');
+INSERT INTO [t10040] VALUES (7,'المبحث السادس: المساجد التي يعتكف فيها','11','0','0');
+INSERT INTO [t10040] VALUES (8,'الخاتمة','16','0','0');
+INSERT INTO [t10040] VALUES (9,'مصادر ومراجع','18','0','0');
