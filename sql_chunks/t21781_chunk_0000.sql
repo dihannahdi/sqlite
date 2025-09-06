f3c6399f22c6ccc62a0a29f6847f759edcfbaae5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:42:14 +0700

    Upload SQL chunk: t21781_chunk_0000.sql

---
+++ sql_chunks/t21781_chunk_0000.sql
@@ -0,0 +1,19 @@
+-- Table: t21781
+-- Chunk: 1
+-- Rows: 12
+
+CREATE TABLE "t21781" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t21781
+INSERT INTO [t21781] VALUES (1,'تقديم','6','0','0');
+INSERT INTO [t21781] VALUES (2,'مقدمة','9','0','0');
+INSERT INTO [t21781] VALUES (3,'الصعوبات بوصفها علامة نمو في المجتمع العربي','14','0','0');
+INSERT INTO [t21781] VALUES (4,'المسوغات في المجتمع','28','0','0');
+INSERT INTO [t21781] VALUES (5,'قيم إنسانية وقيم اقتصادية','43','0','0');
+INSERT INTO [t21781] VALUES (6,'الديمقراطية في الإسلام','56','0','0');
+INSERT INTO [t21781] VALUES (7,'التضامن الإفريقي الآسيوي','87','0','0');
+INSERT INTO [t21781] VALUES (8,'الفعالية','113','0','0');
+INSERT INTO [t21781] VALUES (9,'الثقافة','128','0','0');
+INSERT INTO [t21781] VALUES (10,'كيف نبني مجتمعا أفضل؟','141','0','0');
+INSERT INTO [t21781] VALUES (11,'خواطر عن نهضتنا العربية','161','0','0');
+INSERT INTO [t21781] VALUES (12,'رسالتنا في العالم','188','0','0');
