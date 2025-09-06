e6a937ee145447784ea6477813c88b7866a06df4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:18:38 +0700

    Upload SQL chunk: t399_chunk_0000.sql

---
+++ sql_chunks/t399_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t399
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t399" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t399
+INSERT INTO [t399] VALUES (1,'حجة النبي','1','0','0');
