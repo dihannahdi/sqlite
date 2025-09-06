5b7f91b9c7dbe5f0b350853da586f072147920e8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:01 +0700

    Upload SQL chunk: t36073_chunk_0000.sql

---
+++ sql_chunks/t36073_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t36073
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t36073" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36073
+INSERT INTO [t36073] VALUES (1,'الباب الأول: في حده ورسمه','2','0','0');
+INSERT INTO [t36073] VALUES (2,'الباب الثاني: في وقوعه في القرآن الكريم وحديث نبيه عليه أشرف الصلاة وأفضل التسليم وكلام العرب','6','0','0');
+INSERT INTO [t36073] VALUES (3,'الباب الثالث: في أقسامه وأمثلته','19','0','0');
+INSERT INTO [t36073] VALUES (4,'القسم الأول','20','3','0');
+INSERT INTO [t36073] VALUES (5,'القسم الثاني','48','3','0');
+INSERT INTO [t36073] VALUES (6,'القسم الثالث','57','3','0');
+INSERT INTO [t36073] VALUES (7,'القسم الرابع','59','3','0');
+INSERT INTO [t36073] VALUES (8,'القسم الخامس','62','3','0');
+INSERT INTO [t36073] VALUES (9,'القسم السادس','66','3','0');
