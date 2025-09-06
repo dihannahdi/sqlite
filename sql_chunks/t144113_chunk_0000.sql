da5f13f20fce9ddd9838b74e6c77b01e66e70fa3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:18:42 +0700

    Upload SQL chunk: t144113_chunk_0000.sql

---
+++ sql_chunks/t144113_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t144113
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t144113" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t144113
+INSERT INTO [t144113] VALUES (1,'بداية المنظومة','1','0','0');
