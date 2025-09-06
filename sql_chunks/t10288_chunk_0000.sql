4933ce4bf1f7d145d4f6e486efbe5dc2b51c9bd8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:46 +0700

    Upload SQL chunk: t10288_chunk_0000.sql

---
+++ sql_chunks/t10288_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t10288
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t10288" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10288
+INSERT INTO [t10288] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10288] VALUES (2,'الإبداع في منظور التربية الإسلامية','5','0','0');
+INSERT INTO [t10288] VALUES (3,'مفهوم الإبداع','5','2','0');
+INSERT INTO [t10288] VALUES (4,'حدود الإبداع','15','2','0');
+INSERT INTO [t10288] VALUES (5,'معوقات الإبداع','20','2','0');
+INSERT INTO [t10288] VALUES (6,'فوائد التربية الإبداعية','28','2','0');
+INSERT INTO [t10288] VALUES (7,'العقل في منظور التربية الإسلامية','37','0','0');
+INSERT INTO [t10288] VALUES (8,'مفهوم العقل','37','7','0');
+INSERT INTO [t10288] VALUES (9,'مكان العقل','39','7','0');
+INSERT INTO [t10288] VALUES (10,'العقل الفطري والمكتسب','42','7','0');
+INSERT INTO [t10288] VALUES (11,'علاقة الإبداع بالعقل','44','7','0');
+INSERT INTO [t10288] VALUES (12,'أهمية العقل','46','7','0');
+INSERT INTO [t10288] VALUES (13,'النظرة البشرية للعقل','49','7','0');
+INSERT INTO [t10288] VALUES (14,'جوانب التربية العقلية والإبداعية','54','0','0');
+INSERT INTO [t10288] VALUES (15,'مدخل','54','14','0');
+INSERT INTO [t10288] VALUES (16,'وسائل حفظ العقل','55','14','0');
+INSERT INTO [t10288] VALUES (17,'وسائل تربية العقل','59','14','0');
+INSERT INTO [t10288] VALUES (18,'المفسدات العقلية','72','14','0');
+INSERT INTO [t10288] VALUES (19,'الخاتمة','75','0','0');
+INSERT INTO [t10288] VALUES (20,'مصادر ومراجع','76','0','0');
