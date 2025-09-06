94845656e5dc9a04f8973107a8f488068e4b24ba
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:04 +0700

    Upload SQL chunk: t8546_chunk_0000.sql

---
+++ sql_chunks/t8546_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t8546
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t8546" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8546
+INSERT INTO [t8546] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8546] VALUES (2,'الباب الأول: فضل أهل المدينة','2','0','0');
+INSERT INTO [t8546] VALUES (3,'الباب الثاني: شرف أهل المدينة','4','0','0');
