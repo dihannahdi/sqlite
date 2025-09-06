9a9d3126109dd0431f00ca5892a175180d1e78b2
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:31 +0700

    Upload SQL chunk: t30086_chunk_0000.sql

---
+++ sql_chunks/t30086_chunk_0000.sql
@@ -0,0 +1,29 @@
+-- Table: t30086
+-- Chunk: 1
+-- Rows: 22
+
+CREATE TABLE "t30086" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30086
+INSERT INTO [t30086] VALUES (1,'من حديث أبي فتح الأزدي الحافظ','2','0','0');
+INSERT INTO [t30086] VALUES (2,'من حديث أبي بكر الشافعي','8','0','0');
+INSERT INTO [t30086] VALUES (3,'من حديث أبي محمد الجوهري','12','0','0');
+INSERT INTO [t30086] VALUES (4,'حديث واحد عن شيخة','15','0','0');
+INSERT INTO [t30086] VALUES (5,'شيخ آخر من حديث ابن شاهين أبي حفص','17','0','0');
+INSERT INTO [t30086] VALUES (6,'شيخ حديث واحد','20','0','0');
+INSERT INTO [t30086] VALUES (7,'شيخ حديث واحد','22','0','0');
+INSERT INTO [t30086] VALUES (8,'شيء من حديث عبد الله بن أحمد بن حنبل','24','0','0');
+INSERT INTO [t30086] VALUES (9,'شيخ من حديث أبي بكر القطيعي','27','0','0');
+INSERT INTO [t30086] VALUES (10,'شيخ من حديث عبد الله بن أحمد بن حنبل','30','0','0');
+INSERT INTO [t30086] VALUES (11,'شيخ من حديث الجوهري','40','0','0');
+INSERT INTO [t30086] VALUES (12,'ومن حديثه أيضا','44','0','0');
+INSERT INTO [t30086] VALUES (13,'حديث واحد عن شيخين','48','0','0');
+INSERT INTO [t30086] VALUES (14,'قطعة من شعر البصروي','50','0','0');
+INSERT INTO [t30086] VALUES (15,'من فوائد أبي الحسين الأهوازي وهو الأصبهاني','51','0','0');
+INSERT INTO [t30086] VALUES (16,'من حديث أبي بكر الشافعي','52','0','0');
+INSERT INTO [t30086] VALUES (17,'من فوائد أبي غالب الكرجي','55','0','0');
+INSERT INTO [t30086] VALUES (18,'شيخ حديث واحد','56','0','0');
+INSERT INTO [t30086] VALUES (19,'من فوائد ابن السراج','58','0','0');
+INSERT INTO [t30086] VALUES (20,'من حديث الشافعي','59','0','0');
+INSERT INTO [t30086] VALUES (21,'من فوائد أبي القاسم الشافعي','65','0','0');
+INSERT INTO [t30086] VALUES (22,'من فوائد ابن الطيوري','69','0','0');
