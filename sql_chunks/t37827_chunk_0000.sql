0bff093142a350800d59452b9f62a0342b0d5513
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:16:32 +0700

    Upload SQL chunk: t37827_chunk_0000.sql

---
+++ sql_chunks/t37827_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t37827
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t37827" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37827
+INSERT INTO [t37827] VALUES (1,'الإهداء','6','0','0');
+INSERT INTO [t37827] VALUES (2,'تقديم','7','0','0');
+INSERT INTO [t37827] VALUES (3,'تنبيه','13','0','0');
+INSERT INTO [t37827] VALUES (4,'مدخل الدراسة','14','0','0');
+INSERT INTO [t37827] VALUES (5,'الفصل الأول مجتمع ما بعد الموحدين','19','0','0');
+INSERT INTO [t37827] VALUES (6,'الظاهرة الدورية','20','5','0');
+INSERT INTO [t37827] VALUES (7,'إنسان ما بعد الموحدين','29','5','0');
+INSERT INTO [t37827] VALUES (8,'الاتصال الأول بين أوربة والعالم الإسلامي','34','5','0');
+INSERT INTO [t37827] VALUES (9,'الفصل الثاني النهضة','39','0','0');
+INSERT INTO [t37827] VALUES (10,'حركة الاصلاح','40','9','0');
+INSERT INTO [t37827] VALUES (11,'الحركة الحديثة','56','9','0');
+INSERT INTO [t37827] VALUES (12,'الفصل الثالث فوضى العالم الإسلامي الحديث','67','0','0');
+INSERT INTO [t37827] VALUES (13,'العوامل الداخلية','68','12','0');
+INSERT INTO [t37827] VALUES (14,'العوامل الخارجية','99','12','0');
+INSERT INTO [t37827] VALUES (15,'الفصل الرابع فوضى العالم الغربي','110','0','0');
+INSERT INTO [t37827] VALUES (16,'الفصل الخامس الطرق الجديدة','129','0','0');
+INSERT INTO [t37827] VALUES (17,'الفصل السادس بواكير العالم الإسلامي','151','0','0');
+INSERT INTO [t37827] VALUES (18,'خاتمة','166','0','0');
+INSERT INTO [t37827] VALUES (19,'المآل الروحي لعالم الإسلام','167','18','0');
+INSERT INTO [t37827] VALUES (20,'المسارد','173','0','0');
