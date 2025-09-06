0f87565c305186dfa3d1872b26cef6e2aa1f0361
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:48:45 +0700

    Upload SQL chunk: t10420_chunk_0000.sql

---
+++ sql_chunks/t10420_chunk_0000.sql
@@ -0,0 +1,10 @@
+-- Table: t10420
+-- Chunk: 1
+-- Rows: 3
+
+CREATE TABLE "t10420" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10420
+INSERT INTO [t10420] VALUES (1,'المدخل','1','0','0');
+INSERT INTO [t10420] VALUES (2,'المديح','3','0','0');
+INSERT INTO [t10420] VALUES (3,'تعقيب لا تثريب','21','0','0');
