e2b1eaba128bb61fbca67fff40b1ea815fa43efb
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:56 +0700

    Upload SQL chunk: t4365_chunk_0000.sql

---
+++ sql_chunks/t4365_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t4365
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t4365" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4365
+INSERT INTO [t4365] VALUES (1,'مدخل','1','0','0');
+INSERT INTO [t4365] VALUES (2,'الرواية الأولى','2','0','0');
+INSERT INTO [t4365] VALUES (3,'الرواية الثانية','5','0','0');
+INSERT INTO [t4365] VALUES (4,'الرواية الثالثة','7','0','0');
+INSERT INTO [t4365] VALUES (5,'الرواية الرابعة','9','0','0');
+INSERT INTO [t4365] VALUES (6,'الرواية الخامسة','11','0','0');
+INSERT INTO [t4365] VALUES (7,'الرواية السادسة','13','0','0');
