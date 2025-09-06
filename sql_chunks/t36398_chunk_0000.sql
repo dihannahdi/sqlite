8123a488a15a61fb58dd85d037293bf8d4582e3c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:54 +0700

    Upload SQL chunk: t36398_chunk_0000.sql

---
+++ sql_chunks/t36398_chunk_0000.sql
@@ -0,0 +1,22 @@
+-- Table: t36398
+-- Chunk: 1
+-- Rows: 15
+
+CREATE TABLE "t36398" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36398
+INSERT INTO [t36398] VALUES (1,'رسالة الأخت نور','1','0','0');
+INSERT INTO [t36398] VALUES (2,'إندونيسيا','2','0','0');
+INSERT INTO [t36398] VALUES (3,'نشرات الأخبار','2','0','0');
+INSERT INTO [t36398] VALUES (4,'لماذا تركنا الله عز وجل؟','2','0','0');
+INSERT INTO [t36398] VALUES (5,'القرآن يجيب','2','0','0');
+INSERT INTO [t36398] VALUES (6,'أنا وأنت السبب؟','3','0','0');
+INSERT INTO [t36398] VALUES (7,'لماذا العقاب لنا وحدنا؟','3','0','0');
+INSERT INTO [t36398] VALUES (8,'ماذا يريد الله منا؟','3','0','0');
+INSERT INTO [t36398] VALUES (9,'فإن لن نفعل؟','4','0','0');
+INSERT INTO [t36398] VALUES (10,'إلى متى الغفلة؟!','4','0','0');
+INSERT INTO [t36398] VALUES (11,'حب الدنيا','4','0','0');
+INSERT INTO [t36398] VALUES (12,'وضوح الرؤية','5','0','0');
+INSERT INTO [t36398] VALUES (13,'إنه القرآن','5','0','0');
+INSERT INTO [t36398] VALUES (14,'القرآن هو الحل','6','0','0');
+INSERT INTO [t36398] VALUES (15,'الطريق إلى التأثر','6','0','0');
