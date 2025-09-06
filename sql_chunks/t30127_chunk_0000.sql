cea86e221ca023c6889c7cbb78bc94be8b9c6031
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:59:53 +0700

    Upload SQL chunk: t30127_chunk_0000.sql

---
+++ sql_chunks/t30127_chunk_0000.sql
@@ -0,0 +1,25 @@
+-- Table: t30127
+-- Chunk: 1
+-- Rows: 18
+
+CREATE TABLE "t30127" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30127
+INSERT INTO [t30127] VALUES (1,'الجزء العاشر من المشيخة البغدادية','1','0','0');
+INSERT INTO [t30127] VALUES (2,'حكاية واحدة','4','0','0');
+INSERT INTO [t30127] VALUES (3,'من حديث أبي محمد الخلال الحافظ','5','0','0');
+INSERT INTO [t30127] VALUES (4,'حديث واحد','10','0','0');
+INSERT INTO [t30127] VALUES (5,'من فوائد ابن الطيوري','12','0','0');
+INSERT INTO [t30127] VALUES (6,'من فوائد أبي محمد الجوهري','17','0','0');
+INSERT INTO [t30127] VALUES (7,'من فوائد أبي محمد الخلال','21','0','0');
+INSERT INTO [t30127] VALUES (8,'من حديث الخلال أيضا','28','0','0');
+INSERT INTO [t30127] VALUES (9,'حكاية واحدة','46','0','0');
+INSERT INTO [t30127] VALUES (10,'من حديث عبد الله بن أحمد بن حنبل','47','0','0');
+INSERT INTO [t30127] VALUES (11,'من فوائد أبي الحسين ابن الطيوري','49','0','0');
+INSERT INTO [t30127] VALUES (12,'من حديث أبي أحمد بن منصور اليشكري','52','0','0');
+INSERT INTO [t30127] VALUES (13,'من فوائد أبي بكر أحمد بن سلمان النجاد','54','0','0');
+INSERT INTO [t30127] VALUES (14,'من فوائد ابن الآبنوسي','61','0','0');
+INSERT INTO [t30127] VALUES (15,'من فوائد أبي القاسم بن بشران عن أبي سهل بن زياد','67','0','0');
+INSERT INTO [t30127] VALUES (16,'حديث واحد','70','0','0');
+INSERT INTO [t30127] VALUES (17,'فوائد عن ابن الطيوري','72','0','0');
+INSERT INTO [t30127] VALUES (18,'من فوائد أبي علي بن البرداني الحافظ','73','0','0');
