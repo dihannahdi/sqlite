7947ef3b88a36402f987f66ae4ce6d692f532d1d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:49:14 +0700

    Upload SQL chunk: t8814_chunk_0000.sql

---
+++ sql_chunks/t8814_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t8814
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t8814" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8814
+INSERT INTO [t8814] VALUES (1,'الدنيا دار ابتلاء وامتحان','1','0','0');
+INSERT INTO [t8814] VALUES (2,'موسم من مواسم الآخرة','2','0','0');
+INSERT INTO [t8814] VALUES (3,'زيادة في الخير','4','0','0');
+INSERT INTO [t8814] VALUES (4,'من خير إلى خير','5','0','0');
+INSERT INTO [t8814] VALUES (5,'العبرة من شهر الصوم','7','0','0');
+INSERT INTO [t8814] VALUES (6,'مدخل','7','5','0');
+INSERT INTO [t8814] VALUES (7,'أولا','8','5','0');
+INSERT INTO [t8814] VALUES (8,'ثانيا','10','5','0');
+INSERT INTO [t8814] VALUES (9,'ثالثا','12','5','0');
+INSERT INTO [t8814] VALUES (10,'رابعا','13','5','0');
+INSERT INTO [t8814] VALUES (11,'كلمة ختامية','17','0','0');
