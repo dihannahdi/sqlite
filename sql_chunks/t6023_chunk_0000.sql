fcb21da6cbfd46beff6bd7b0366df1a937424e74
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:27:30 +0700

    Upload SQL chunk: t6023_chunk_0000.sql

---
+++ sql_chunks/t6023_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t6023
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t6023" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6023
+INSERT INTO [t6023] VALUES (1,'كتاب سلسلة الذهب','2','0','0');
+INSERT INTO [t6023] VALUES (2,'تم الكتاب','67','0','0');
