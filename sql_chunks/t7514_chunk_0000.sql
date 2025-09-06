42fe2fb6523a413d1cf74c4259998c17ecfc0a0d
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:18 +0700

    Upload SQL chunk: t7514_chunk_0000.sql

---
+++ sql_chunks/t7514_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7514
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7514" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7514
+INSERT INTO [t7514] VALUES (1,'أغلو في بعض القرابة وجفاء في الأنبياء والصحابة','1','0','0');
+INSERT INTO [t7514] VALUES (2,'يدعى ياسر الحبيب، وليس له من اسمه نصيب','1','0','0');
