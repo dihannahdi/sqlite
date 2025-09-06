72b561495d9a2fbd732fc442a18216af969cd8d4
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:30:08 +0700

    Upload SQL chunk: t17439_chunk_0000.sql

---
+++ sql_chunks/t17439_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t17439
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t17439" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t17439
+INSERT INTO [t17439] VALUES (1,'[المقدمة]','4','0','0');
+INSERT INTO [t17439] VALUES (2,'[الجوانب التي تتميز بها سيرة الأنبياء]','9','0','0');
