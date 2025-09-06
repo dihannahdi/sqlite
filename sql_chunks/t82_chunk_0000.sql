45c28e6f7293e2d3ea0baaef2f4d1ec99481e8cd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:45:21 +0700

    Upload SQL chunk: t82_chunk_0000.sql

---
+++ sql_chunks/t82_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t82
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t82" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t82
+INSERT INTO [t82] VALUES (1,'المقدمة: وحديثين جليلين كريمين','1','0','0');
+INSERT INTO [t82] VALUES (2,'الذنوب سبب المصائب','2','0','0');
+INSERT INTO [t82] VALUES (3,'معنى الاستغفار','2','0','0');
+INSERT INTO [t82] VALUES (4,'ملازمة الاستغفار من أجل العبادات','3','0','0');
+INSERT INTO [t82] VALUES (5,'ثمار الاستغفار وفوائده العظيمة','4','0','0');
+INSERT INTO [t82] VALUES (6,'وصية للحسن غالية','6','0','0');
+INSERT INTO [t82] VALUES (7,'تنبيه مهم حول الاستغفار باللسان','7','0','0');
+INSERT INTO [t82] VALUES (8,'خاتمة','7','0','0');
