b674612799b45aece22a7e2a8ffddac9e602ae5c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:19:28 +0700

    Upload SQL chunk: t424_chunk_0000.sql

---
+++ sql_chunks/t424_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t424
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t424" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t424
+INSERT INTO [t424] VALUES (1,'الأحاديث الواردة في فضل السواك','2','0','0');
+INSERT INTO [t424] VALUES (2,'حكمة ترغيب الشارع في السواك','4','0','0');
