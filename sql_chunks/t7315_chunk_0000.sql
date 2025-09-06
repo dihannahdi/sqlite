a86432b74223020b6ef897040df43ed2e7118b91
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:37:25 +0700

    Upload SQL chunk: t7315_chunk_0000.sql

---
+++ sql_chunks/t7315_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t7315
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t7315" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7315
+INSERT INTO [t7315] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7315] VALUES (2,'مدخل','6','0','0');
+INSERT INTO [t7315] VALUES (3,'الطبقة الثانية والعشرون، وعدتها ١٥','7','0','0');
+INSERT INTO [t7315] VALUES (4,'الطبقة الثالثة والعشرون، وعدتهم ١١','14','0','0');
+INSERT INTO [t7315] VALUES (5,'الطبقة الرابعة والعشرون وعدتها ٩','19','0','0');
+INSERT INTO [t7315] VALUES (6,'الطبقة الخامسة والعشرون، وعدتها ١١','24','0','0');
+INSERT INTO [t7315] VALUES (7,'فهرس تراجم أعلام تذكرة الحفاظ، وذيول تذكرة الحفاظ','29','0','0');
+INSERT INTO [t7315] VALUES (8,'فهرس المحتويات','76','0','0');
