dd90f67a0a8ce5e4e1033a7dfbd8c8b6c2889c9b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:16:57 +0700

    Upload SQL chunk: t38050_chunk_0000.sql

---
+++ sql_chunks/t38050_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t38050
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t38050" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t38050
+INSERT INTO [t38050] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t38050] VALUES (2,'١ - أحكام الاستعاذة والبسملة','1','0','0');
+INSERT INTO [t38050] VALUES (3,'٢ - أحكام النون الساكنة والتنوين','3','0','0');
+INSERT INTO [t38050] VALUES (4,'٣ - أحكام الميم الساكنة','5','0','0');
+INSERT INTO [t38050] VALUES (5,'٤ - أحكام الميم والنون المشددتين','6','0','0');
+INSERT INTO [t38050] VALUES (6,'٥ - أحكام الإدغام','6','0','0');
+INSERT INTO [t38050] VALUES (7,'٦ - أحكام اللام الساكنة','7','0','0');
+INSERT INTO [t38050] VALUES (8,'٨ - أحكام المدود وأقسامها','10','0','0');
+INSERT INTO [t38050] VALUES (9,'١١ - أحكام السكتات','20','0','0');
