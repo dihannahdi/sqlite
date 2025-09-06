9e4c663c409a889587f460ecba72858311098d0f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:45:40 +0700

    Upload SQL chunk: t23071_chunk_0000.sql

---
+++ sql_chunks/t23071_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t23071
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t23071" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t23071
+INSERT INTO [t23071] VALUES (1,'دراسات في السنة النبوية','24','0','0');
+INSERT INTO [t23071] VALUES (2,'مصادر ومراجع','29','0','0');
