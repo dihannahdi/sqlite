ee449ff4d923cfd80fa7eaa2fe8ff8cc671dc14f
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:07 +0700

    Upload SQL chunk: t8550_chunk_0000.sql

---
+++ sql_chunks/t8550_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8550
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8550" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8550
+INSERT INTO [t8550] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8550] VALUES (2,'قسم التحقيق','30','0','0');
