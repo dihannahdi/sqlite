87efb2740c0897d2e9cfcced3d35519a1a1120db
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:36:44 +0700

    Upload SQL chunk: t20650_chunk_0000.sql

---
+++ sql_chunks/t20650_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t20650
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t20650" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t20650
+INSERT INTO [t20650] VALUES (1,'القضاء المغربي وخواصه','5','0','0');
+INSERT INTO [t20650] VALUES (2,'القضاء','6','1','0');
+INSERT INTO [t20650] VALUES (3,'النوازل','17','1','0');
+INSERT INTO [t20650] VALUES (4,'الوثائق','20','1','0');
+INSERT INTO [t20650] VALUES (5,'الفرائض','23','1','0');
+INSERT INTO [t20650] VALUES (6,'الاسطرلاب','26','1','0');
+INSERT INTO [t20650] VALUES (7,'العرف','30','0','0');
+INSERT INTO [t20650] VALUES (8,'أثر الفقه المالكي في التشريعات الغربية','37','0','0');
+INSERT INTO [t20650] VALUES (9,'بيبلوغرافية الفقه المالكي','47','0','0');
+INSERT INTO [t20650] VALUES (10,'المعجم التاريخي للفقه المالكي','185','0','0');
+INSERT INTO [t20650] VALUES (11,'العمل المحلي','268','10','0');
