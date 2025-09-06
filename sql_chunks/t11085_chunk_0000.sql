79f3afb5b1bb12918a1275b48c237ae074e62d54
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:53:03 +0700

    Upload SQL chunk: t11085_chunk_0000.sql

---
+++ sql_chunks/t11085_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t11085
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t11085" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t11085
+INSERT INTO [t11085] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t11085] VALUES (2,'أسس الدعوة','30','0','0');
+INSERT INTO [t11085] VALUES (3,'التوحيد هو الأساس','30','2','0');
+INSERT INTO [t11085] VALUES (4,'الإتباع للسلف','75','2','0');
+INSERT INTO [t11085] VALUES (5,'طاعة أولي الأمر والنصح لهم','81','2','0');
+INSERT INTO [t11085] VALUES (6,'فقه الدعوة والدعاة','87','2','0');
+INSERT INTO [t11085] VALUES (7,'تبليغ الدعوة','106','0','0');
+INSERT INTO [t11085] VALUES (8,'المنطلق','106','7','0');
+INSERT INTO [t11085] VALUES (9,'الوسائل','113','7','0');
+INSERT INTO [t11085] VALUES (10,'إعداد الرجال','118','7','0');
+INSERT INTO [t11085] VALUES (11,'توزيع الوظائف في دولة الدعوة','121','7','0');
+INSERT INTO [t11085] VALUES (12,'إنصاف الدعوة وآثارها خارج الجزيرة العربية','129','0','0');
+INSERT INTO [t11085] VALUES (13,'ندوة دعوة الشيخ محمد بن عبد الوهاب النتائج والتوصيات','148','0','0');
