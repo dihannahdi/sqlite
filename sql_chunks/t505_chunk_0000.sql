b382462d8ecbcc7ca518274f1b613bd4ff81b452
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:38 +0700

    Upload SQL chunk: t505_chunk_0000.sql

---
+++ sql_chunks/t505_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t505
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t505" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t505
+INSERT INTO [t505] VALUES (1,'ضبط فعلين في متن الأزهار','1','0','0');
+INSERT INTO [t505] VALUES (2,'الاعتراض الأول','3','1','0');
+INSERT INTO [t505] VALUES (3,'الجواب الأول','5','2','0');
+INSERT INTO [t505] VALUES (4,'الجواب الثاني','11','2','0');
+INSERT INTO [t505] VALUES (5,'فائدتان','19','0','0');
+INSERT INTO [t505] VALUES (6,'خاطرة في قول الشاعر: (ولكنني من حبها لعميد)','19','5','0');
+INSERT INTO [t505] VALUES (7,'المعارف التي بعد اسم الإشارة','21','5','0');
