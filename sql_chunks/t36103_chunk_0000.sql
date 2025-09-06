196083857fa86cd5517cb6b9e392c7470dd10a9c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:07:22 +0700

    Upload SQL chunk: t36103_chunk_0000.sql

---
+++ sql_chunks/t36103_chunk_0000.sql
@@ -0,0 +1,53 @@
+-- Table: t36103
+-- Chunk: 1
+-- Rows: 46
+
+CREATE TABLE "t36103" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36103
+INSERT INTO [t36103] VALUES (1,'١','2','0','0');
+INSERT INTO [t36103] VALUES (2,'عناصر الدرس','2','1','0');
+INSERT INTO [t36103] VALUES (3,'٢','32','0','0');
+INSERT INTO [t36103] VALUES (4,'عناصر الدرس','32','3','0');
+INSERT INTO [t36103] VALUES (5,'٣','58','0','0');
+INSERT INTO [t36103] VALUES (6,'عناصر الدرس','58','5','0');
+INSERT INTO [t36103] VALUES (7,'٤','84','0','0');
+INSERT INTO [t36103] VALUES (8,'عناصر الدرس','84','7','0');
+INSERT INTO [t36103] VALUES (9,'٥','114','0','0');
+INSERT INTO [t36103] VALUES (10,'عناصر الدرس','114','9','0');
+INSERT INTO [t36103] VALUES (11,'٦','136','0','0');
+INSERT INTO [t36103] VALUES (12,'عناصر الدرس','136','11','0');
+INSERT INTO [t36103] VALUES (13,'٧','165','0','0');
+INSERT INTO [t36103] VALUES (14,'عناصر الدرس','165','13','0');
+INSERT INTO [t36103] VALUES (15,'٨','187','0','0');
+INSERT INTO [t36103] VALUES (16,'عناصر الدرس','187','15','0');
+INSERT INTO [t36103] VALUES (17,'٩','212','0','0');
+INSERT INTO [t36103] VALUES (18,'عناصر الدرس','212','17','0');
+INSERT INTO [t36103] VALUES (19,'١٠','235','0','0');
+INSERT INTO [t36103] VALUES (20,'عناصر الدرس','235','19','0');
+INSERT INTO [t36103] VALUES (21,'١١','257','0','0');
+INSERT INTO [t36103] VALUES (22,'عناصر الدرس','257','21','0');
+INSERT INTO [t36103] VALUES (23,'١٢','283','0','0');
+INSERT INTO [t36103] VALUES (24,'عناصر الدرس','283','23','0');
+INSERT INTO [t36103] VALUES (25,'١٣','308','0','0');
+INSERT INTO [t36103] VALUES (26,'عناصر الدرس','308','25','0');
+INSERT INTO [t36103] VALUES (27,'١٤','337','0','0');
+INSERT INTO [t36103] VALUES (28,'عناصر الدرس','337','27','0');
+INSERT INTO [t36103] VALUES (29,'١٥','360','0','0');
+INSERT INTO [t36103] VALUES (30,'عناصر الدرس','360','29','0');
+INSERT INTO [t36103] VALUES (31,'١٦','378','0','0');
+INSERT INTO [t36103] VALUES (32,'عناصر الدرس','378','31','0');
+INSERT INTO [t36103] VALUES (33,'١٧','418','0','0');
+INSERT INTO [t36103] VALUES (34,'عناصر الدرس','418','33','0');
+INSERT INTO [t36103] VALUES (35,'١٨','443','0','0');
+INSERT INTO [t36103] VALUES (36,'عناصر الدرس','443','35','0');
+INSERT INTO [t36103] VALUES (37,'١٩','480','0','0');
+INSERT INTO [t36103] VALUES (38,'عناصر الدرس','480','37','0');
+INSERT INTO [t36103] VALUES (39,'٢٠','500','0','0');
+INSERT INTO [t36103] VALUES (40,'عناصر الدرس','500','39','0');
+INSERT INTO [t36103] VALUES (41,'٢١','535','0','0');
+INSERT INTO [t36103] VALUES (42,'عناصر الدرس','535','41','0');
+INSERT INTO [t36103] VALUES (43,'٢٢','568','0','0');
+INSERT INTO [t36103] VALUES (44,'عناصر الدرس','568','43','0');
+INSERT INTO [t36103] VALUES (45,'٢٣','598','0','0');
+INSERT INTO [t36103] VALUES (46,'عناصر الدرس','598','45','0');
