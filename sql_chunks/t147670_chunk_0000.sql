3fc1b78a4d21cb9285816b5fe71a6ae519d1f1ae
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:21:49 +0700

    Upload SQL chunk: t147670_chunk_0000.sql

---
+++ sql_chunks/t147670_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t147670
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t147670" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t147670
+INSERT INTO [t147670] VALUES (1,'مقدمة التحقيق','5','0','0');
+INSERT INTO [t147670] VALUES (2,'وصف النسخة الخطية','8','1','0');
+INSERT INTO [t147670] VALUES (3,'نماذج من المخطوطة المعتمدة في التحقيق','9','1','0');
+INSERT INTO [t147670] VALUES (4,'مسألة في توحيد الفلاسفة','13','0','0');
+INSERT INTO [t147670] VALUES (5,'فصل','37','0','0');
+INSERT INTO [t147670] VALUES (6,'فصل','45','0','0');
