edf7c67e1e4a92485b2427501895d629ddbb82a3
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:56:46 +0700

    Upload SQL chunk: t96264_chunk_0000.sql

---
+++ sql_chunks/t96264_chunk_0000.sql
@@ -0,0 +1,27 @@
+-- Table: t96264
+-- Chunk: 1
+-- Rows: 20
+
+CREATE TABLE "t96264" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t96264
+INSERT INTO [t96264] VALUES (1,'مقدمة','3','0','0');
+INSERT INTO [t96264] VALUES (2,'الإباضية من فرق الخوارج','6','0','0');
+INSERT INTO [t96264] VALUES (3,'إلى من تنسب الإباضية','7','0','0');
+INSERT INTO [t96264] VALUES (4,'أماكن تواجدهم','10','0','0');
+INSERT INTO [t96264] VALUES (5,'عقيدة الإباضية','11','0','0');
+INSERT INTO [t96264] VALUES (6,'١ - التوحيد عند الإباضية','12','5','0');
+INSERT INTO [t96264] VALUES (7,'حكم ذلك عند أهل السنة','13','6','0');
+INSERT INTO [t96264] VALUES (8,'٢ - خلق القرآن عند الإباضية','15','5','0');
+INSERT INTO [t96264] VALUES (9,'٣ - إنكار رؤية المؤمنين ربهم يوم القيامة','17','5','0');
+INSERT INTO [t96264] VALUES (10,'٤ - إنكار الشفاعة والقول بتخليد العصاة في النار','18','5','0');
+INSERT INTO [t96264] VALUES (11,'٥ - الإيمان عند الإباضية','19','5','0');
+INSERT INTO [t96264] VALUES (12,'٦ - الأسماء والصفات','20','5','0');
+INSERT INTO [t96264] VALUES (13,'٧ - الصراط','23','5','0');
+INSERT INTO [t96264] VALUES (14,'٨ - موقفهم من الصحابة','25','5','0');
+INSERT INTO [t96264] VALUES (15,'تكفير إمامهم ابن إباض لعثمان رضي الله عنه','31','14','0');
+INSERT INTO [t96264] VALUES (16,'٩ - التقية عند الإباضية','37','5','0');
+INSERT INTO [t96264] VALUES (17,'أقوال أئمة الدعوة في الإباضية','38','0','0');
+INSERT INTO [t96264] VALUES (18,'حكمهم عند أهل السنة وقتالهم','46','0','0');
+INSERT INTO [t96264] VALUES (19,'مسند الربيع بن حبيب','49','0','0');
+INSERT INTO [t96264] VALUES (20,'الوثائق','51','0','0');
