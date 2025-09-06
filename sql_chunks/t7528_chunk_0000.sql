0b4385d44406e61cca78ce1ae6329d131b873b3c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:39:27 +0700

    Upload SQL chunk: t7528_chunk_0000.sql

---
+++ sql_chunks/t7528_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t7528
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t7528" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7528
+INSERT INTO [t7528] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7528] VALUES (2,'الأدب الصغير','5','0','0');
+INSERT INTO [t7528] VALUES (3,'الأدب الكبير','55','0','0');
+INSERT INTO [t7528] VALUES (4,'الفهارس','127','0','0');
