b82ed9f6789ae6fdf35aa6ef282b078d4b29f54b
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:12:47 +0700

    Upload SQL chunk: t132516_chunk_0000.sql

---
+++ sql_chunks/t132516_chunk_0000.sql
@@ -0,0 +1,32 @@
+-- Table: t132516
+-- Chunk: 1
+-- Rows: 25
+
+CREATE TABLE "t132516" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t132516
+INSERT INTO [t132516] VALUES (1,'تمهيد','3','0','0');
+INSERT INTO [t132516] VALUES (2,'تعريف أصول الفقه','4','0','0');
+INSERT INTO [t132516] VALUES (3,'الأحكام','4','0','0');
+INSERT INTO [t132516] VALUES (4,'العلم','5','0','0');
+INSERT INTO [t132516] VALUES (5,'الكلام','5','0','0');
+INSERT INTO [t132516] VALUES (6,'الأمر','6','0','0');
+INSERT INTO [t132516] VALUES (7,'النهي','7','0','0');
+INSERT INTO [t132516] VALUES (8,'فصل','7','0','0');
+INSERT INTO [t132516] VALUES (9,'العام','7','0','0');
+INSERT INTO [t132516] VALUES (10,'الخاص','8','0','0');
+INSERT INTO [t132516] VALUES (11,'المطلق والمقيد','8','0','0');
+INSERT INTO [t132516] VALUES (12,'المجمل والمبين','8','0','0');
+INSERT INTO [t132516] VALUES (13,'الظاهر والمؤول','8','0','0');
+INSERT INTO [t132516] VALUES (14,'النسخ','9','0','0');
+INSERT INTO [t132516] VALUES (15,'الأخبار','9','0','0');
+INSERT INTO [t132516] VALUES (16,'فصل','10','0','0');
+INSERT INTO [t132516] VALUES (17,'الإجماع','11','0','0');
+INSERT INTO [t132516] VALUES (18,'القياس','11','0','0');
+INSERT INTO [t132516] VALUES (19,'فصل','11','0','0');
+INSERT INTO [t132516] VALUES (20,'التعارض','12','0','0');
+INSERT INTO [t132516] VALUES (21,'فصل','12','0','0');
+INSERT INTO [t132516] VALUES (22,'المفتي والمستفتي','13','0','0');
+INSERT INTO [t132516] VALUES (23,'فصل','13','0','0');
+INSERT INTO [t132516] VALUES (24,'الاجتهاد','13','0','0');
+INSERT INTO [t132516] VALUES (25,'التقليد','14','0','0');
