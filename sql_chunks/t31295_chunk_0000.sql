14b7ff01a8b06f5c8850f2397ad44ead0683885c
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:58 +0700

    Upload SQL chunk: t31295_chunk_0000.sql

---
+++ sql_chunks/t31295_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t31295
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t31295" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31295
+INSERT INTO [t31295] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t31295] VALUES (2,'أولا الجهاد أمره عظيم في الإسلام','6','0','0');
+INSERT INTO [t31295] VALUES (3,'ثانيا تعريف الجهاد','15','0','0');
+INSERT INTO [t31295] VALUES (4,'ثالثا شروط وجوب الجهاد على المسلم','16','0','0');
+INSERT INTO [t31295] VALUES (5,'رابعا أحكام الجهاد','19','0','0');
+INSERT INTO [t31295] VALUES (6,'خامسا تنبيهات حول الجهاد اليوم','26','0','0');
+INSERT INTO [t31295] VALUES (7,'الخوض في وجوب الجهاد وعدمه عند المستجدات كلام في حكم الله','26','6','0');
+INSERT INTO [t31295] VALUES (8,'إياك واتهام أهل العلم وإن تكلموا بغير ما في نفسك','35','6','0');
+INSERT INTO [t31295] VALUES (9,'الأمر بالجهاد من مهمات وحقوق الإمام','39','6','0');
+INSERT INTO [t31295] VALUES (10,'تنبيه','48','6','0');
+INSERT INTO [t31295] VALUES (11,'سادسا معالم لأهل السنة غائبة عن كثير من شباب الأمة','52','0','0');
+INSERT INTO [t31295] VALUES (12,'خاتمة','55','0','0');
+INSERT INTO [t31295] VALUES (13,'فهرس بعض المراجع','58','0','0');
