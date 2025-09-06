c40a9e445d2ca336d2a31287fafcc18657c783fd
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:55 +0700

    Upload SQL chunk: t5970_chunk_0000.sql

---
+++ sql_chunks/t5970_chunk_0000.sql
@@ -0,0 +1,38 @@
+-- Table: t5970
+-- Chunk: 1
+-- Rows: 31
+
+CREATE TABLE "t5970" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t5970
+INSERT INTO [t5970] VALUES (1,'الحديث الصحيح','3','0','0');
+INSERT INTO [t5970] VALUES (2,'الحديث الحسن','5','0','0');
+INSERT INTO [t5970] VALUES (3,'الحديث الضعيف','9','0','0');
+INSERT INTO [t5970] VALUES (4,'الحديث المرفوع','12','0','0');
+INSERT INTO [t5970] VALUES (5,'الحديث المقطوع','13','0','0');
+INSERT INTO [t5970] VALUES (6,'الحديث المسند','14','0','0');
+INSERT INTO [t5970] VALUES (7,'الحديث المتصل','17','0','0');
+INSERT INTO [t5970] VALUES (8,'الحديث المسلسل','19','0','0');
+INSERT INTO [t5970] VALUES (9,'الحديث العزيز','22','0','0');
+INSERT INTO [t5970] VALUES (10,'الحديث المشهور','24','0','0');
+INSERT INTO [t5970] VALUES (11,'الحديث المعنعن','27','0','0');
+INSERT INTO [t5970] VALUES (12,'الحديث المبهم','29','0','0');
+INSERT INTO [t5970] VALUES (13,'الحديث العالي والحديث النازل','31','0','0');
+INSERT INTO [t5970] VALUES (14,'الحديث الموقوف','34','0','0');
+INSERT INTO [t5970] VALUES (15,'الحديث المرسل','36','0','0');
+INSERT INTO [t5970] VALUES (16,'الحديث الغريب','39','0','0');
+INSERT INTO [t5970] VALUES (17,'الحديث المنقطع','42','0','0');
+INSERT INTO [t5970] VALUES (18,'الحديث المعضل','45','0','0');
+INSERT INTO [t5970] VALUES (19,'الحديث المدلس','47','0','0');
+INSERT INTO [t5970] VALUES (20,'الحديث الشاذ','52','0','0');
+INSERT INTO [t5970] VALUES (21,'الحديث المقلوب','55','0','0');
+INSERT INTO [t5970] VALUES (22,'الحديث الفرد','59','0','0');
+INSERT INTO [t5970] VALUES (23,'الحديث المعلل','62','0','0');
+INSERT INTO [t5970] VALUES (24,'الحديث المضطرب','66','0','0');
+INSERT INTO [t5970] VALUES (25,'المدرج في الحديث','69','0','0');
+INSERT INTO [t5970] VALUES (26,'الحديث المدبج','72','0','0');
+INSERT INTO [t5970] VALUES (27,'الحديث المتفق والمتفرق','75','0','0');
+INSERT INTO [t5970] VALUES (28,'الحديث المؤتلف والمختلف','78','0','0');
+INSERT INTO [t5970] VALUES (29,'الحديث المنكر','81','0','0');
+INSERT INTO [t5970] VALUES (30,'الحديث المتروك','83','0','0');
+INSERT INTO [t5970] VALUES (31,'الحديث الموضوع','85','0','0');
