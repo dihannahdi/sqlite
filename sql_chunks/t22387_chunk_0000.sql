d6bf02152e1ae6f9a19d669127af8a090cdc6c08
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:43:23 +0700

    Upload SQL chunk: t22387_chunk_0000.sql

---
+++ sql_chunks/t22387_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t22387
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t22387" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t22387
+INSERT INTO [t22387] VALUES (1,'الحمد لله تعالى الولي عند الضراء','1','0','0');
+INSERT INTO [t22387] VALUES (2,'* أيها المذنب! ما علمك بتلك المحطة','1','0','0');
+INSERT INTO [t22387] VALUES (3,'* أيها المذنب! الحذر .. الحذر','2','0','0');
+INSERT INTO [t22387] VALUES (4,'* أيها المذنب! احذر تقلب القلوب','5','0','0');
+INSERT INTO [t22387] VALUES (5,'* أيها المذنب! الذنوب من أسباب سوء الخاتمة','5','0','0');
+INSERT INTO [t22387] VALUES (6,'* أيها المذنب! احذر الإصرار على الذنوب','6','0','0');
+INSERT INTO [t22387] VALUES (7,'* يا من غره الذنب! إليك مصارع المذنبين','8','0','0');
+INSERT INTO [t22387] VALUES (8,'أيها المذنب','10','0','0');
