7649634506df0bcae010eb5f514c912595b35ce5
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:21:10 +0700

    Upload SQL chunk: t4864_chunk_0000.sql

---
+++ sql_chunks/t4864_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t4864
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t4864" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4864
+INSERT INTO [t4864] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t4864] VALUES (2,'مفتاح الجنة في الإحتجاج بالسنة','3','0','0');
