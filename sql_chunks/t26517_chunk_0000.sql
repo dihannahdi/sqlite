727f78269ae9778240756553f523cd93fca0f9ce
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:52:03 +0700

    Upload SQL chunk: t26517_chunk_0000.sql

---
+++ sql_chunks/t26517_chunk_0000.sql
@@ -0,0 +1,12 @@
+-- Table: t26517
+-- Chunk: 1
+-- Rows: 5
+
+CREATE TABLE "t26517" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t26517
+INSERT INTO [t26517] VALUES (1,'حديث ابن شاهين','2','0','0');
+INSERT INTO [t26517] VALUES (2,'بسم الله الرحمن الرحمن الرحيم','2','0','0');
+INSERT INTO [t26517] VALUES (3,'مجلس آخر','11','0','0');
+INSERT INTO [t26517] VALUES (4,'مجلس آخر','16','0','0');
+INSERT INTO [t26517] VALUES (5,'مجلس آخر في التاريخ المقدم','33','0','0');
