b5b6bb4b06079be1115a6bba936fa44e55874072
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:14:41 +0700

    Upload SQL chunk: t37631_chunk_0000.sql

---
+++ sql_chunks/t37631_chunk_0000.sql
@@ -0,0 +1,16 @@
+-- Table: t37631
+-- Chunk: 1
+-- Rows: 9
+
+CREATE TABLE "t37631" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t37631
+INSERT INTO [t37631] VALUES (1,'نبذة عن مؤلف الأصل','1','0','0');
+INSERT INTO [t37631] VALUES (2,'مقدمة','3','0','0');
+INSERT INTO [t37631] VALUES (3,'المقالة الأولى في تفصيل الفرق','5','0','0');
+INSERT INTO [t37631] VALUES (4,'المقالة الثانية في الآيات الشاهدة بكفر الشيعة والأحاديث الواردة في حقهم','9','0','0');
+INSERT INTO [t37631] VALUES (5,'المقصد الأول في الآيات','9','4','0');
+INSERT INTO [t37631] VALUES (6,'المقصد الثاني في الأحاديث','13','4','0');
+INSERT INTO [t37631] VALUES (7,'المقالة الثالثة في إفتاء العلماء بكفرهم','15','0','0');
+INSERT INTO [t37631] VALUES (8,'المقالة الرابعة في بيان حال المتأخرين منهم وحكم دارهم وإفتاء المسلمين في حقهم','17','0','0');
+INSERT INTO [t37631] VALUES (9,'خاتمة في التكلم إجمالا فيما سبق وبيان ما حصل منه','22','0','0');
