1888895b429542e03f8d234b298cfc6e97dcbabf
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:44 +0700

    Upload SQL chunk: t8707_chunk_0000.sql

---
+++ sql_chunks/t8707_chunk_0000.sql
@@ -0,0 +1,28 @@
+-- Table: t8707
+-- Chunk: 1
+-- Rows: 21
+
+CREATE TABLE "t8707" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8707
+INSERT INTO [t8707] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8707] VALUES (2,'التمهيد:','3','0','0');
+INSERT INTO [t8707] VALUES (3,'الفصل الأول: الإبدال في الحروف (الصوامت) :','5','0','0');
+INSERT INTO [t8707] VALUES (4,'توطئة:','5','3','0');
+INSERT INTO [t8707] VALUES (5,'المبحث الأول: اللغات الملقبة:','9','3','0');
+INSERT INTO [t8707] VALUES (6,'الاستنطاء:','9','5','0');
+INSERT INTO [t8707] VALUES (7,'الطمطمانية:','16','5','0');
+INSERT INTO [t8707] VALUES (8,'المعاقبة:','21','5','0');
+INSERT INTO [t8707] VALUES (9,'المبحث الثاني: اللغات غير الملقبة:','30','3','0');
+INSERT INTO [t8707] VALUES (10,'إبدال التاء دالا:','30','9','0');
+INSERT INTO [t8707] VALUES (11,'إبدال التاء هاء:','32','9','0');
+INSERT INTO [t8707] VALUES (12,'إبدال السين زايا:','37','9','0');
+INSERT INTO [t8707] VALUES (13,'إبدال الصاد تاء:','40','9','0');
+INSERT INTO [t8707] VALUES (14,'إبدال النون هاء:','43','9','0');
+INSERT INTO [t8707] VALUES (15,'إبدال الياء ألفا:','47','9','0');
+INSERT INTO [t8707] VALUES (16,'الفصل الثاني: الإبدال في الحركات (الصوائت) :','51','0','0');
+INSERT INTO [t8707] VALUES (17,'توطئة:','51','16','0');
+INSERT INTO [t8707] VALUES (18,'المبحث الأول: بين الفتح والكسر في أحرف المضارعة:','52','16','0');
+INSERT INTO [t8707] VALUES (19,'المبحث الثاني: بين الفتح والكسر في اللام الجارة:','58','16','0');
+INSERT INTO [t8707] VALUES (20,'الخاتمة:','61','0','0');
+INSERT INTO [t8707] VALUES (21,'مصادر ومراجع','63','0','0');
