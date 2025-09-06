0743da1158bc950b30ad7933094ac6c55ec94b66
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:00:03 +0700

    Upload SQL chunk: t30248_chunk_0000.sql

---
+++ sql_chunks/t30248_chunk_0000.sql
@@ -0,0 +1,13 @@
+-- Table: t30248
+-- Chunk: 1
+-- Rows: 6
+
+CREATE TABLE "t30248" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30248
+INSERT INTO [t30248] VALUES (1,'الأحاديث الأربعون المتباينة الأسانيد والمتون','2','0','0');
+INSERT INTO [t30248] VALUES (2,'الحديث الأول: عن أبي بكر الصديق رضي الله عنه','20','0','0');
+INSERT INTO [t30248] VALUES (3,'الحديث الثاني: عن عمر بن الخطاب رضي الله عنه','23','0','0');
+INSERT INTO [t30248] VALUES (4,'الحديث الثالث: عن سعيد بن زيد رضي الله عنه','29','0','0');
+INSERT INTO [t30248] VALUES (5,'الحديث العاشر: عن أبي عبيدة بن الجراح رضي الله عنه','33','0','0');
+INSERT INTO [t30248] VALUES (6,'الحديث الحادي عشر: عن ابن عباس رضي الله عنهما','35','0','0');
