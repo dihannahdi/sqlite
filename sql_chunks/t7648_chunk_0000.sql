25383a71982b38309f0a4ab074a72204bcf54c59
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:41:05 +0700

    Upload SQL chunk: t7648_chunk_0000.sql

---
+++ sql_chunks/t7648_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t7648
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t7648" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7648
+INSERT INTO [t7648] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t7648] VALUES (2,'تمهيد','14','0','0');
+INSERT INTO [t7648] VALUES (3,'الفصل الأول: الحياة.. وهذا الصراع بين الأحياء','18','0','0');
+INSERT INTO [t7648] VALUES (4,'الفصل الثاني: الشيح.. وراية الجهاد','28','0','0');
+INSERT INTO [t7648] VALUES (5,'الفصل الثالث: في ميدان الجهاد','35','0','0');
+INSERT INTO [t7648] VALUES (6,'الفصل الرابع: صراع بين الحق والباطل','39','0','0');
+INSERT INTO [t7648] VALUES (7,'الفصل الخامس: الدعوة والثائرون عليها','59','0','0');
