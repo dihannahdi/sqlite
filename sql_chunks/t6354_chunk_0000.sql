c26fcc6411ba5ee0c5c160090759644b8d673658
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:30:05 +0700

    Upload SQL chunk: t6354_chunk_0000.sql

---
+++ sql_chunks/t6354_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t6354
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t6354" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t6354
+INSERT INTO [t6354] VALUES (1,'مقدمة المؤلف','1','0','0');
+INSERT INTO [t6354] VALUES (2,'مسائل الاجتهاد والتقليد','2','0','0');
+INSERT INTO [t6354] VALUES (3,'الكلام في هذه المسائل على فصول','3','0','0');
+INSERT INTO [t6354] VALUES (4,'الفصل الأول','4','0','0');
+INSERT INTO [t6354] VALUES (5,'فصل','8','0','0');
+INSERT INTO [t6354] VALUES (6,'فصل','31','0','0');
+INSERT INTO [t6354] VALUES (7,'فصل','44','0','0');
+INSERT INTO [t6354] VALUES (8,'دليل منع التلفيق وتوجيهه من قبل الشيخ','55','0','0');
+INSERT INTO [t6354] VALUES (9,'مثال توضيحي لما سبق','58','0','0');
+INSERT INTO [t6354] VALUES (10,'رد المؤلف على المانعين','60','0','0');
+INSERT INTO [t6354] VALUES (11,'فصل','74','0','0');
+INSERT INTO [t6354] VALUES (12,'والمعنى الثاني','79','0','0');
+INSERT INTO [t6354] VALUES (13,'المعنى الصحيح للعبارة لا تقليد بعد العمل','81','0','0');
+INSERT INTO [t6354] VALUES (14,'فصل','90','0','0');
+INSERT INTO [t6354] VALUES (15,'والصورة الثانية','103','0','0');
+INSERT INTO [t6354] VALUES (16,'والقول الثاني','104','0','0');
