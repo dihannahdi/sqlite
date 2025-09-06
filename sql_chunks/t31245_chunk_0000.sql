4c2315523ad589348104607fba4546d6322fcf59
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:03:35 +0700

    Upload SQL chunk: t31245_chunk_0000.sql

---
+++ sql_chunks/t31245_chunk_0000.sql
@@ -0,0 +1,20 @@
+-- Table: t31245
+-- Chunk: 1
+-- Rows: 13
+
+CREATE TABLE "t31245" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t31245
+INSERT INTO [t31245] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t31245] VALUES (2,'تعريف المشروع في المسجد','4','0','0');
+INSERT INTO [t31245] VALUES (3,'تعريف الممنوع في المسجد','4','0','0');
+INSERT INTO [t31245] VALUES (4,'أهمية المساجد في حياة المسلمين','4','0','0');
+INSERT INTO [t31245] VALUES (5,'مشروعية عمارة المساجد الحسية','8','0','0');
+INSERT INTO [t31245] VALUES (6,'الأدلة من الكتاب على مشروعية عمارة المساجد','8','0','0');
+INSERT INTO [t31245] VALUES (7,'الأدلة من السنة على مشروعية عمارة المساجد','11','0','0');
+INSERT INTO [t31245] VALUES (8,'الأدلة على مشروعية المساجد الحسية من آثار الصحابة والتابعين','13','0','0');
+INSERT INTO [t31245] VALUES (9,'مشروعية الوقف على المساجد','20','0','0');
+INSERT INTO [t31245] VALUES (10,'مشروعية الصرف على عمارة المساجد وصيانتها','23','0','0');
+INSERT INTO [t31245] VALUES (11,'الممنوعات في المساجد','25','0','0');
+INSERT INTO [t31245] VALUES (12,'مشروعية عمارة المساجد المعنوية','31','0','0');
+INSERT INTO [t31245] VALUES (13,'خاتمة','32','0','0');
