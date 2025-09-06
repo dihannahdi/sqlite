8f836aa520fecb15f5d6e9b2b9086598ea639003
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 02:34:55 +0700

    Upload SQL chunk: t1905_chunk_0000.sql

---
+++ sql_chunks/t1905_chunk_0000.sql
@@ -0,0 +1,18 @@
+-- Table: t1905
+-- Chunk: 1
+-- Rows: 11
+
+CREATE TABLE "t1905" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t1905
+INSERT INTO [t1905] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t1905] VALUES (2,'تعريف الاقتصاد','6','0','0');
+INSERT INTO [t1905] VALUES (3,'أنواع النظم الاقتصادية','8','0','0');
+INSERT INTO [t1905] VALUES (4,'مفهوم النظام الاقتصادي','10','0','0');
+INSERT INTO [t1905] VALUES (5,'دور الدولة في الاقتصاد الإسلامي','12','0','0');
+INSERT INTO [t1905] VALUES (6,'أقسام السلطات','13','0','0');
+INSERT INTO [t1905] VALUES (7,'أولا التدخل غير المباشر للدولة','13','6','0');
+INSERT INTO [t1905] VALUES (8,'ثانيا التدخل المباشر للدولة','18','6','0');
+INSERT INTO [t1905] VALUES (9,'مجالات تدخل الدولة','20','0','0');
+INSERT INTO [t1905] VALUES (10,'خاتمة','23','0','0');
+INSERT INTO [t1905] VALUES (11,'أهم مراجع البحث','26','0','0');
