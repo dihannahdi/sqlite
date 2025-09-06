52108a24cf0d9aecf18c5d008b0e4f3e16660619
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:50:21 +0700

    Upload SQL chunk: t9027_chunk_0000.sql

---
+++ sql_chunks/t9027_chunk_0000.sql
@@ -0,0 +1,11 @@
+-- Table: t9027
+-- Chunk: 1
+-- Rows: 4
+
+CREATE TABLE "t9027" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9027
+INSERT INTO [t9027] VALUES (1,'أهمية صلاة الإستسقاء في الاسلام والاستغاثة المشروعة','1','0','0');
+INSERT INTO [t9027] VALUES (2,'أشرح بإيجاز حديث عمر بن الخطاب رضي الله عنه في الاستسقاء بالعباس','3','0','0');
+INSERT INTO [t9027] VALUES (3,'صفة ما دعا به العباس في هذه الواقعة','4','0','0');
+INSERT INTO [t9027] VALUES (4,'التوسل بذوات الأموات والأحياء فهي غير مشروعة','5','0','0');
