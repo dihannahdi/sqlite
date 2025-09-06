b219007ad2307173c3dd58c10efaf53255a20caa
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:20:46 +0700

    Upload SQL chunk: t4673_chunk_0000.sql

---
+++ sql_chunks/t4673_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t4673
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t4673" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t4673
+INSERT INTO [t4673] VALUES (1,'أول واجب على المكلف','19','0','0');
+INSERT INTO [t4673] VALUES (2,'السمعيات','41','0','0');
+INSERT INTO [t4673] VALUES (3,'مصدر التلقي العقل مصدرا ومعارضا','59','0','0');
+INSERT INTO [t4673] VALUES (4,'اعتقادهم أن نصوص الوحي لا تفيد اليقين','68','0','0');
+INSERT INTO [t4673] VALUES (5,'التأويل','79','0','0');
+INSERT INTO [t4673] VALUES (6,'جناية التأويل على الإسلام','89','0','0');
+INSERT INTO [t4673] VALUES (7,'نموذج للانحراف المنهجي وآثاره','89','6','0');
+INSERT INTO [t4673] VALUES (8,'إسقاط قيمة النصوص في مجال العقيدة','99','0','0');
+INSERT INTO [t4673] VALUES (9,'الكشف والذوق والإلهام','120','0','0');
+INSERT INTO [t4673] VALUES (10,'مصادر ومعايير أخرى','144','0','0');
+INSERT INTO [t4673] VALUES (11,'التلقي المباشر','144','10','0');
+INSERT INTO [t4673] VALUES (12,'النبوات','172','0','0');
+INSERT INTO [t4673] VALUES (13,'الإيمان','198','0','0');
+INSERT INTO [t4673] VALUES (14,'الخاتمة','207','0','0');
