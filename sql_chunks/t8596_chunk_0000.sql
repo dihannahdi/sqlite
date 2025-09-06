b109b1bab287d8554b9a0925dffb02a30d8ab469
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:47:38 +0700

    Upload SQL chunk: t8596_chunk_0000.sql

---
+++ sql_chunks/t8596_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t8596
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t8596" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8596
+INSERT INTO [t8596] VALUES (1,'النصيرية طغاة سورية أو العلويون كما سماهم الفرنسيون','1','0','0');
+INSERT INTO [t8596] VALUES (2,'تأليف: أحمد بن عبد الحليم ابن تيمية الحراني','1','0','0');
