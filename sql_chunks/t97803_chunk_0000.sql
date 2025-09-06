333f27434f3d75c605069d86d6792d90c0d074d6
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:01:36 +0700

    Upload SQL chunk: t97803_chunk_0000.sql

---
+++ sql_chunks/t97803_chunk_0000.sql
@@ -0,0 +1,50 @@
+-- Table: t97803
+-- Chunk: 1
+-- Rows: 43
+
+CREATE TABLE "t97803" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t97803
+INSERT INTO [t97803] VALUES (1,'مقدمة المحقق','2','0','0');
+INSERT INTO [t97803] VALUES (2,'تعريف عام بالمنظومة','4','1','0');
+INSERT INTO [t97803] VALUES (3,'شراحها','7','1','0');
+INSERT INTO [t97803] VALUES (4,'من تصانيفه','8','1','0');
+INSERT INTO [t97803] VALUES (5,'من تصانيفه','9','1','0');
+INSERT INTO [t97803] VALUES (6,'أما بيانات المخطوط، فهي كالتالي','10','1','0');
+INSERT INTO [t97803] VALUES (7,'بعض الكتب المسماة بعقود الجمان','11','1','0');
+INSERT INTO [t97803] VALUES (8,'ترجمة الإمام السيوطي','15','1','0');
+INSERT INTO [t97803] VALUES (9,'مقدمة','24','0','0');
+INSERT INTO [t97803] VALUES (10,'الفن الأول: علم المعاني','27','0','0');
+INSERT INTO [t97803] VALUES (11,'مسألة','27','10','0');
+INSERT INTO [t97803] VALUES (12,'أحوال الإسناد الخبري','28','10','0');
+INSERT INTO [t97803] VALUES (13,'أحوال المسند إليه','31','10','0');
+INSERT INTO [t97803] VALUES (14,'مسألة','37','10','0');
+INSERT INTO [t97803] VALUES (15,'أحوال المسند','39','10','0');
+INSERT INTO [t97803] VALUES (16,'تنبيه','43','10','0');
+INSERT INTO [t97803] VALUES (17,'أحوال متعلقات الفعل وما يعمل عمله','43','10','0');
+INSERT INTO [t97803] VALUES (18,'الباب الخامس: القصر','45','10','0');
+INSERT INTO [t97803] VALUES (19,'الباب السادس: الإنشاء','49','10','0');
+INSERT INTO [t97803] VALUES (20,'تنبيه','52','10','0');
+INSERT INTO [t97803] VALUES (21,'فصل','54','10','0');
+INSERT INTO [t97803] VALUES (22,'تنبيه','56','10','0');
+INSERT INTO [t97803] VALUES (23,'الوصل والفصل','57','10','0');
+INSERT INTO [t97803] VALUES (24,'تذنيب','62','10','0');
+INSERT INTO [t97803] VALUES (25,'المساواة والإطناب والإيجاز','64','10','0');
+INSERT INTO [t97803] VALUES (26,'الفن الثاني: علم البيان','68','0','0');
+INSERT INTO [t97803] VALUES (27,'التشبيه','68','26','0');
+INSERT INTO [t97803] VALUES (28,'فصل','74','26','0');
+INSERT INTO [t97803] VALUES (29,'فصل','75','26','0');
+INSERT INTO [t97803] VALUES (30,'أقسام التشبيه','77','26','0');
+INSERT INTO [t97803] VALUES (31,'خاتمة','79','26','0');
+INSERT INTO [t97803] VALUES (32,'الحقيقة والمجاز','80','26','0');
+INSERT INTO [t97803] VALUES (33,'فصل','84','26','0');
+INSERT INTO [t97803] VALUES (34,'فصل','85','26','0');
+INSERT INTO [t97803] VALUES (35,'فصل','86','26','0');
+INSERT INTO [t97803] VALUES (36,'خاتمة','86','26','0');
+INSERT INTO [t97803] VALUES (37,'الكناية','87','26','0');
+INSERT INTO [t97803] VALUES (38,'الفن الثالث: علم البديع','90','0','0');
+INSERT INTO [t97803] VALUES (39,'القسم الأول: المعنوي','90','0','0');
+INSERT INTO [t97803] VALUES (40,'القسم الثاني: اللفظي','105','0','0');
+INSERT INTO [t97803] VALUES (41,'خاتمة في السرقات الشعرية وما يتصل بها','113','0','0');
+INSERT INTO [t97803] VALUES (42,'فصل فيما يتصل بالسرقات','116','41','0');
+INSERT INTO [t97803] VALUES (43,'فصل','119','41','0');
