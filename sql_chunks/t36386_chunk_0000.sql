cc5628a30ee55a152c195ef0aad99dda7a5f7c23
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:08:42 +0700

    Upload SQL chunk: t36386_chunk_0000.sql

---
+++ sql_chunks/t36386_chunk_0000.sql
@@ -0,0 +1,23 @@
+-- Table: t36386
+-- Chunk: 1
+-- Rows: 16
+
+CREATE TABLE "t36386" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36386
+INSERT INTO [t36386] VALUES (1,'نسبها ومولدها','1','0','0');
+INSERT INTO [t36386] VALUES (2,'نشأتها','1','0','0');
+INSERT INTO [t36386] VALUES (3,'إسلامها','2','0','0');
+INSERT INTO [t36386] VALUES (4,'زواجها الأول','2','0','0');
+INSERT INTO [t36386] VALUES (5,'حفصة المهاجرة','3','0','0');
+INSERT INTO [t36386] VALUES (6,'الأرملة الحزينة','3','0','0');
+INSERT INTO [t36386] VALUES (7,'أحزان عمر','4','0','0');
+INSERT INTO [t36386] VALUES (8,'خير من عثمان','5','0','0');
+INSERT INTO [t36386] VALUES (9,'حديث عمر','5','0','0');
+INSERT INTO [t36386] VALUES (10,'حفصة من نسائه - صلى الله عليه وسلم - في الجنة','6','0','0');
+INSERT INTO [t36386] VALUES (11,'بعد رسول الله - صلى الله عليه وسلم -','7','0','0');
+INSERT INTO [t36386] VALUES (12,'في عهد الخليفتين','7','0','0');
+INSERT INTO [t36386] VALUES (13,'حفصة المتحدثة','8','0','0');
+INSERT INTO [t36386] VALUES (14,'إكرام النبي - صلى الله عليه وسلم - لحفصة','8','0','0');
+INSERT INTO [t36386] VALUES (15,'أخلاقها وفضائلها','9','0','0');
+INSERT INTO [t36386] VALUES (16,'وفاتها رضي الله عنها','9','0','0');
