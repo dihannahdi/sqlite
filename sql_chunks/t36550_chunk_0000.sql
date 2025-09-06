9c71b012a5d43f7b9fc3bb46711fe78580e2d060
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:10:04 +0700

    Upload SQL chunk: t36550_chunk_0000.sql

---
+++ sql_chunks/t36550_chunk_0000.sql
@@ -0,0 +1,15 @@
+-- Table: t36550
+-- Chunk: 1
+-- Rows: 8
+
+CREATE TABLE "t36550" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t36550
+INSERT INTO [t36550] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t36550] VALUES (2,'مدخل','2','0','0');
+INSERT INTO [t36550] VALUES (3,'الهمم العالية','37','0','0');
+INSERT INTO [t36550] VALUES (4,'التخطيط لتحصيل العلوم النافعة','45','0','0');
+INSERT INTO [t36550] VALUES (5,'الرحلة في طلب العلم','46','0','0');
+INSERT INTO [t36550] VALUES (6,'حفظ الوقت','68','0','0');
+INSERT INTO [t36550] VALUES (7,'استغل وقتك في الأنفس من العلوم','91','0','0');
+INSERT INTO [t36550] VALUES (8,'توقير العلماء واحترامهم','126','0','0');
