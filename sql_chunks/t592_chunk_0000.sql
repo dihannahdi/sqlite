6665fc848273dab967069b4f8d2a36478686234e
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:26:40 +0700

    Upload SQL chunk: t592_chunk_0000.sql

---
+++ sql_chunks/t592_chunk_0000.sql
@@ -0,0 +1,8 @@
+-- Table: t592
+-- Chunk: 1
+-- Rows: 1
+
+CREATE TABLE "t592" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t592
+INSERT INTO [t592] VALUES (1,'صحيح السيرة النبوية','1','0','0');
