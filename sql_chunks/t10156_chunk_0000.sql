9607d50bd53f1254dac03cfbf078e2d44666166a
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:04 +0700

    Upload SQL chunk: t10156_chunk_0000.sql

---
+++ sql_chunks/t10156_chunk_0000.sql
@@ -0,0 +1,26 @@
+-- Table: t10156
+-- Chunk: 1
+-- Rows: 19
+
+CREATE TABLE "t10156" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10156
+INSERT INTO [t10156] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t10156] VALUES (2,'المبحث الأول: في تعريف السحر وأنواعه','2','0','0');
+INSERT INTO [t10156] VALUES (3,'أولأ: تعريفه','2','2','0');
+INSERT INTO [t10156] VALUES (4,'ثانيا: أنواع السحر','5','2','0');
+INSERT INTO [t10156] VALUES (5,'المبحث الثاني: السحر له حقبقه أم لا؟','15','0','0');
+INSERT INTO [t10156] VALUES (6,'القول الأول: قول أهل السنة','15','5','0');
+INSERT INTO [t10156] VALUES (7,'القول الأول: قول عامة المعتزلة','22','5','0');
+INSERT INTO [t10156] VALUES (8,'المبحث الثالث: حكم السحر والسحرة','28','0','0');
+INSERT INTO [t10156] VALUES (9,'أولا: حكم تعلم السحر وتعليمه','28','8','0');
+INSERT INTO [t10156] VALUES (10,'ثانيا: حكم العمل بالسحر','34','8','0');
+INSERT INTO [t10156] VALUES (11,'ثالثا: عقوبة الساحر','39','8','0');
+INSERT INTO [t10156] VALUES (12,'رابعا: توبة الساحر','52','8','0');
+INSERT INTO [t10156] VALUES (13,'المبحث الرابع: علاج السحر (النشرة)','57','0','0');
+INSERT INTO [t10156] VALUES (14,'تقديم','57','13','0');
+INSERT INTO [t10156] VALUES (15,'حكمها','58','13','0');
+INSERT INTO [t10156] VALUES (16,'صفتها','60','13','0');
+INSERT INTO [t10156] VALUES (17,'الخاتمة','66','0','0');
+INSERT INTO [t10156] VALUES (18,'الخاتمة','66','17','0');
+INSERT INTO [t10156] VALUES (19,'مصادر ومراجع','68','0','0');
