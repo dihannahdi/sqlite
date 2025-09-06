5f6e807ace19063aa4c6ec4ab81990b050f9a4e8
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:48:23 +0700

    Upload SQL chunk: t8661_chunk_0000.sql

---
+++ sql_chunks/t8661_chunk_0000.sql
@@ -0,0 +1,14 @@
+-- Table: t8661
+-- Chunk: 1
+-- Rows: 7
+
+CREATE TABLE "t8661" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t8661
+INSERT INTO [t8661] VALUES (1,'مقدمة','1','0','0');
+INSERT INTO [t8661] VALUES (2,'الباب الأول الرسالة وعناصر نجاحها','25','0','0');
+INSERT INTO [t8661] VALUES (3,'الباب الثاني مشكلات ما قبل الكتابة','42','0','0');
+INSERT INTO [t8661] VALUES (4,'الباب الثالث كتابة الرسالة','92','0','0');
+INSERT INTO [t8661] VALUES (5,'الباب الرابع هيئة الرسالة','142','0','0');
+INSERT INTO [t8661] VALUES (6,'الباب الخامس كتابة الرسالة بالآلة الكاتبة وتجليدها','163','0','0');
+INSERT INTO [t8661] VALUES (7,'الباب السادس المناقشة والنتيجة','204','0','0');
