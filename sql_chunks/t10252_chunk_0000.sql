3b71cf229624a1f5e8ffbd49c03b2f3372e86d16
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 01:47:26 +0700

    Upload SQL chunk: t10252_chunk_0000.sql

---
+++ sql_chunks/t10252_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t10252
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t10252" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t10252
+INSERT INTO [t10252] VALUES (1,'ذم الفرقة والاختلاف في الكتاب والسنة','1','0','0');
+INSERT INTO [t10252] VALUES (2,'للشيخ عبد الله بن محمد الغنيمان','1','0','0');
