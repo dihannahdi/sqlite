04aec16cb97a686c6b7cf9d759ded53f71756034
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:40:55 +0700

    Upload SQL chunk: t7635_chunk_0000.sql

---
+++ sql_chunks/t7635_chunk_0000.sql
@@ -0,0 +1,9 @@
+-- Table: t7635
+-- Chunk: 1
+-- Rows: 2
+
+CREATE TABLE "t7635" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t7635
+INSERT INTO [t7635] VALUES (1,'الرسالة المدنية في تحقيق المجاز والحقيقة في صفات الله','1','0','0');
+INSERT INTO [t7635] VALUES (2,'التأليف: أبو العباس أحمد بن عبد الحليم بن تيمية الحراني','1','0','0');
