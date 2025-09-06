af238b468bae1828a1ff1bcd220ce5e6e893b1f9
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 04:02:14 +0700

    Upload SQL chunk: t9807_chunk_0000.sql

---
+++ sql_chunks/t9807_chunk_0000.sql
@@ -0,0 +1,21 @@
+-- Table: t9807
+-- Chunk: 1
+-- Rows: 14
+
+CREATE TABLE "t9807" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t9807
+INSERT INTO [t9807] VALUES (1,'بسم الله الرحمن الرحيم','2','0','0');
+INSERT INTO [t9807] VALUES (2,'سنة خمس وأربعين وثماني مائة','4','0','0');
+INSERT INTO [t9807] VALUES (3,'سنة ست أربعين وثماني مائة','10','0','0');
+INSERT INTO [t9807] VALUES (4,'سنة سبع وأربعين وثمانمائة','19','0','0');
+INSERT INTO [t9807] VALUES (5,'سنة ثمان وأربعين وثمانمائة','24','0','0');
+INSERT INTO [t9807] VALUES (6,'سنة تسع وأربعين وثماني مائة','34','0','0');
+INSERT INTO [t9807] VALUES (7,'سنة خمسين وثماني مائة','42','0','0');
+INSERT INTO [t9807] VALUES (8,'سنة إحدى وخمسين وثماني مائة','48','0','0');
+INSERT INTO [t9807] VALUES (9,'سنة اثنين وخمسين وثمانمائة','56','0','0');
+INSERT INTO [t9807] VALUES (10,'سنة ثلاث وخمسين وثماني مائة','78','0','0');
+INSERT INTO [t9807] VALUES (11,'سنة أربع وخمسين وثمانمائة','105','0','0');
+INSERT INTO [t9807] VALUES (12,'سنة خمس وخمسين وثماني مائة','160','0','0');
+INSERT INTO [t9807] VALUES (13,'سنة ست وخمسين وثماني مائة','187','0','0');
+INSERT INTO [t9807] VALUES (14,'التراجم','212','0','0');
