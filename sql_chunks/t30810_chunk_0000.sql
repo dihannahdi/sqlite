7604e7522e5f2e2730f0c7c17208efa45a8b80ee
Dihan Nahdi <90670352+dihannahdi@users.noreply.github.com>
2025-09-07 03:01:58 +0700

    Upload SQL chunk: t30810_chunk_0000.sql

---
+++ sql_chunks/t30810_chunk_0000.sql
@@ -0,0 +1,43 @@
+-- Table: t30810
+-- Chunk: 1
+-- Rows: 36
+
+CREATE TABLE "t30810" (id INTEGER PRIMARY KEY, content TEXT, page TEXT, parent TEXT, is_deleted TEXT);
+
+-- Insert data for t30810
+INSERT INTO [t30810] VALUES (1,'المقدمة','1','0','0');
+INSERT INTO [t30810] VALUES (2,'القسم الأول الدراسة الاستشراق والتنصير مدى العلاقة بين ظاهرتين','6','0','0');
+INSERT INTO [t30810] VALUES (3,'المدخل','6','2','0');
+INSERT INTO [t30810] VALUES (4,'مؤيدات هذا الهدف','10','2','0');
+INSERT INTO [t30810] VALUES (5,'فئات المستشرقين المنصرين','21','2','0');
+INSERT INTO [t30810] VALUES (6,'مزيد من التركيز','26','2','0');
+INSERT INTO [t30810] VALUES (7,'التفريق بين ظاهرتين','27','2','0');
+INSERT INTO [t30810] VALUES (8,'لا تناقض','28','2','0');
+INSERT INTO [t30810] VALUES (9,'الاستغلال','30','2','0');
+INSERT INTO [t30810] VALUES (10,'القسم الثاني التراجم نماذج من المستشرقين المنصرين','33','0','0');
+INSERT INTO [t30810] VALUES (11,'المد خل','33','10','0');
+INSERT INTO [t30810] VALUES (12,'مستشرقون منصرون','39','10','0');
+INSERT INTO [t30810] VALUES (13,'حرف الألف','39','12','0');
+INSERT INTO [t30810] VALUES (14,'حرف الباء','48','12','0');
+INSERT INTO [t30810] VALUES (15,'حرف التاء','63','12','0');
+INSERT INTO [t30810] VALUES (16,'حرف الجيم','66','12','0');
+INSERT INTO [t30810] VALUES (17,'حرف الحاء','75','12','0');
+INSERT INTO [t30810] VALUES (18,'حرف الخاء','77','12','0');
+INSERT INTO [t30810] VALUES (19,'حرف الدال','79','12','0');
+INSERT INTO [t30810] VALUES (20,'حرف الراء','93','12','0');
+INSERT INTO [t30810] VALUES (21,'حرف الزاي','99','12','0');
+INSERT INTO [t30810] VALUES (22,'حرف السين','101','12','0');
+INSERT INTO [t30810] VALUES (23,'حرف الشين','107','12','0');
+INSERT INTO [t30810] VALUES (24,'حرف الصاد','109','12','0');
+INSERT INTO [t30810] VALUES (25,'حرف العين','110','12','0');
+INSERT INTO [t30810] VALUES (26,'حرف الغين','112','12','0');
+INSERT INTO [t30810] VALUES (27,'حرف الفاء','113','12','0');
+INSERT INTO [t30810] VALUES (28,'حرف القاف','117','12','0');
+INSERT INTO [t30810] VALUES (29,'حرف الكاف','118','12','0');
+INSERT INTO [t30810] VALUES (30,'حرف اللام','130','12','0');
+INSERT INTO [t30810] VALUES (31,'حرف الميم','138','12','0');
+INSERT INTO [t30810] VALUES (32,'حرف النون','146','12','0');
+INSERT INTO [t30810] VALUES (33,'حرف الهاء','150','12','0');
+INSERT INTO [t30810] VALUES (34,'حرف الواو','153','12','0');
+INSERT INTO [t30810] VALUES (35,'حرف الياء','156','12','0');
+INSERT INTO [t30810] VALUES (36,'قائمة وراقية بالمصادر والمراجع الأساس','158','0','0');
